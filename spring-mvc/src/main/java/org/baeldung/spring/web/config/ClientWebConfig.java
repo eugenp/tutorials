45c0260fc51bc1bb32d9bd96673785834f327bd1
eugenp <hanriseldon@gmail.com>
2013-04-29 21:43:51 +0300

    mvc work

---
+++ spring-mvc/src/main/java/org/baeldung/spring/web/config/ClientWebConfig.java
@@ -0,0 +1,34 @@
+package org.baeldung.spring.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@Configuration
+// @ComponentScan("org.package1.to.scan.web")
+public class ClientWebConfig extends WebMvcConfigurerAdapter {
+
+	@Override
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		super.addViewControllers(registry);
+
+		registry.addViewController("/sample.html");
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+		bean.setViewClass(JstlView.class);
+		bean.setPrefix("/WEB-INF/view/");
+		bean.setSuffix(".jsp");
+
+		return bean;
+	}
+}
