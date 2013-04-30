35fbbd7822410d63ef12e8804ddedc9a2236363d
Eugen <eparaschiv@optaros.com>
2013-04-30 12:20:25 +0300

    minor cleanup

--- spring-mvc/src/main/java/org/baeldung/spring/web/config/ClientWebConfig.java
+++ spring-mvc/src/main/java/org/baeldung/spring/web/config/ClientWebConfig.java
@@ -14,6 +14,12 @@ import org.springframework.web.servlet.view.JstlView;
 // @ComponentScan("org.package1.to.scan.web")
 public class ClientWebConfig extends WebMvcConfigurerAdapter {
 
+	public ClientWebConfig() {
+		super();
+	}
+
+	// API
+
 	@Override
 	public void addViewControllers(final ViewControllerRegistry registry) {
 		super.addViewControllers(registry);
