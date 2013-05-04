dfd157bb9db0d69608e83114ad1ae543681b88f3
eugenp <hanriseldon@gmail.com>
2013-05-04 21:38:51 +0300

    initial work on DAO - spring with hibernate

---
+++ spring-hibernate-dao/README.md
@@ -0,0 +1,3 @@
+Spring MVC with Java Configuration
+=========
+
---
+++ spring-hibernate-dao/src/main/java/org/baeldung/spring/web/config/PersistenceConfig.java
@@ -0,0 +1,14 @@
+package org.baeldung.spring.web.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PersistenceConfig {
+
+	public PersistenceConfig() {
+		super();
+	}
+
+	// API
+
+}
