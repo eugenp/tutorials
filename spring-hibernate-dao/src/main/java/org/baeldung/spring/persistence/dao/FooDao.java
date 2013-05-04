480edf4a3c9ce53c3977e0e143915a1b427a64a2
eugenp <hanriseldon@gmail.com>
2013-05-04 22:45:59 +0300

    initial service work

--- spring-hibernate-dao/src/main/java/org/baeldung/spring/persistence/dao/FooDao.java
+++ spring-hibernate-dao/src/main/java/org/baeldung/spring/persistence/dao/FooDao.java
@@ -3,7 +3,7 @@ package org.baeldung.spring.persistence.dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class FooDao {
+public class FooDao implements IFooDao {
 
     public FooDao() {
         super();
