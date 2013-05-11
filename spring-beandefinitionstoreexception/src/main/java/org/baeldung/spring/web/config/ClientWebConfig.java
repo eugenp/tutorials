80c15f30f394cbc940b2a9f3c3fed526593fb9f2
eugenp <hanriseldon@gmail.com>
2013-05-11 23:59:10 +0300

    exception work

--- spring-beandefinitionstoreexception/src/main/java/org/baeldung/spring/web/config/ClientWebConfig.java
+++ spring-beandefinitionstoreexception/src/main/java/org/baeldung/spring/web/config/ClientWebConfig.java
@@ -11,29 +11,30 @@ import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
+// @ImportResource("beans.xml")
 public class ClientWebConfig extends WebMvcConfigurerAdapter {
 
-	public ClientWebConfig() {
-		super();
-	}
+    public ClientWebConfig() {
+        super();
+    }
 
-	// API
+    // API
 
-	@Override
-	public void addViewControllers(final ViewControllerRegistry registry) {
-		super.addViewControllers(registry);
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        super.addViewControllers(registry);
 
-		registry.addViewController("/sample.html");
-	}
+        registry.addViewController("/sample.html");
+    }
 
-	@Bean
-	public ViewResolver viewResolver() {
-		final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+    @Bean
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
 
-		bean.setViewClass(JstlView.class);
-		bean.setPrefix("/WEB-INF/view/");
-		bean.setSuffix(".jsp");
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
 
-		return bean;
-	}
+        return bean;
+    }
 }
