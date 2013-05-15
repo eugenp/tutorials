4a1370d1dd7984515887c396fda27a854f681205
Eugen <eparaschiv@optaros.com>
2013-05-15 18:52:45 +0300

    minor fixes

--- spring-mvc/src/main/java/org/baeldung/spring/web/controller/FooController.java
+++ spring-mvc/src/main/java/org/baeldung/spring/web/controller/FooController.java
@@ -2,6 +2,8 @@ package org.baeldung.spring.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FooController {
@@ -13,9 +15,21 @@ public class FooController {
 	// API
 
 	@RequestMapping(value = "/foos")
+	@ResponseBody
 	public String getFoos() {
-		System.out.println("simplePattern method was called");
-		return "someResult";
+		return "Get some Foos";
+	}
+
+	@RequestMapping(value = "/foos", method = RequestMethod.POST)
+	@ResponseBody
+	public String getFoosPost() {
+		return "Post some Foos";
+	}
+
+	@RequestMapping(value = "/foos", method = RequestMethod.POST, headers = "key=val")
+	@ResponseBody
+	public String getFoosWithHeader() {
+		return "Get some Foos with Header";
 	}
 
 }
