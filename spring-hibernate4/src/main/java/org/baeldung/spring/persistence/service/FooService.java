852c5b96de233efbad9341537c9b8ce9ff6c71d9
Eugen <eparaschiv@optaros.com>
2013-05-17 12:22:20 +0300

    service work

--- spring-hibernate4/src/main/java/org/baeldung/spring/persistence/service/FooService.java
+++ spring-hibernate4/src/main/java/org/baeldung/spring/persistence/service/FooService.java
@@ -1,6 +1,7 @@
 package org.baeldung.spring.persistence.service;
 
 import org.baeldung.spring.persistence.dao.IFooDao;
+import org.baeldung.spring.persistence.model.Foo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,12 +11,16 @@ import org.springframework.transaction.annotation.Transactional;
 public class FooService {
 
     @Autowired
-    private IFooDao fooDao;
+    private IFooDao dao;
 
     public FooService() {
         super();
     }
 
     // API
+
+    public void create(final Foo entity) {
+        dao.create(entity);
+    }
 
 }
--- spring-hibernate4/src/test/java/org/baeldung/spring/persistence/service/FooServicePersistenceIntegrationTest.java
+++ spring-hibernate4/src/test/java/org/baeldung/spring/persistence/service/FooServicePersistenceIntegrationTest.java
@@ -1,7 +1,10 @@
 package org.baeldung.spring.persistence.service;
 
 import org.baeldung.spring.persistence.config.PersistenceConfig;
+import org.baeldung.spring.persistence.model.Foo;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -9,5 +12,20 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { PersistenceConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class FooServicePersistenceIntegrationTest {
-    //
+
+    @Autowired
+    private FooService service;
+
+    // tests
+
+    @Test
+    public final void whenContextIsBootstrapped_thenNoExceptions() {
+        //
+    }
+
+    @Test
+    public final void whenEntityisCreated_thenNoExceptions() {
+        service.create(new Foo());
+    }
+
 }
