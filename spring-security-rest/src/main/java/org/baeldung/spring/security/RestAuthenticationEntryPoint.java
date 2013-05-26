74bafb99a9e7325cad578171e96713f3e17977ee
eugenp <hanriseldon@gmail.com>
2013-05-26 18:51:36 +0300

    security work

---
+++ spring-security-rest/src/main/java/org/baeldung/spring/security/RestAuthenticationEntryPoint.java
@@ -0,0 +1,19 @@
+package org.baeldung.spring.security;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+@Component
+public final class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+}
