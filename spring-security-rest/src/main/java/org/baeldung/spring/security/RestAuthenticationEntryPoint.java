3572f7ecd53bb0b82d7fdb19bf13d04803ea6d5f
eugenp <hanriseldon@gmail.com>
2013-05-27 00:25:50 +0300

    security work

--- spring-security-rest/src/main/java/org/baeldung/spring/security/RestAuthenticationEntryPoint.java
+++ spring-security-rest/src/main/java/org/baeldung/spring/security/RestAuthenticationEntryPoint.java
@@ -9,6 +9,9 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+/**
+ * The Entry Point will not redirect to any sort of Login - it will return the 401
+ */
 @Component
 public final class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
@@ -16,4 +19,5 @@ public final class RestAuthenticationEntryPoint implements AuthenticationEntryPo
     public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException) throws IOException {
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
+
 }
--- spring-security-rest/src/main/webapp/WEB-INF/view/homepage.jsp
+++
@@ -1,9 +0,0 @@
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<html>
-<head></head>
-
-<body>
-	<h1>This is the body of the sample view</h1>
-	<a href="<c:url value="/perform_logout" />">Logout</a>
-</body>
-</html>
