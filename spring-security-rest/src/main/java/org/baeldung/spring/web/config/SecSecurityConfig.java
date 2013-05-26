9ca6724a17a7e829af232cdf414769cbfe131d84
eugenp <hanriseldon@gmail.com>
2013-05-26 18:44:06 +0300

    minor cleanup

--- spring-security-rest/src/main/java/org/baeldung/spring/web/config/SecSecurityConfig.java
+++ spring-security-rest/src/main/java/org/baeldung/spring/web/config/SecSecurityConfig.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.ImportResource;
 @ImportResource({ "classpath:webSecurityConfig.xml" })
 public class SecSecurityConfig {
 
-	public SecSecurityConfig() {
-		super();
-	}
+    public SecSecurityConfig() {
+        super();
+    }
 
 }
