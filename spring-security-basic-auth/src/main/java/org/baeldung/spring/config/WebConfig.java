2a64bb227a5bac21b25fec81be664d91e4c07ed2
Eugen <eparaschiv@optaros.com>
2013-05-27 18:49:58 +0300

    security work

--- spring-security-basic-auth/src/main/java/org/baeldung/spring/config/WebConfig.java
+++ spring-security-basic-auth/src/main/java/org/baeldung/spring/config/WebConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@ComponentScan("org.baeldung.spring.web.controller")
+@ComponentScan("org.baeldung.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     public WebConfig() {
