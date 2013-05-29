acd6f0432ed080c39fc1b1f44d0b63b6a3316829
Eugen <eparaschiv@optaros.com>
2013-05-29 18:29:25 +0300

    minor cleanup

--- spring-security-basic-auth/src/main/java/org/baeldung/spring/config/SecSecurityConfig.java
+++ spring-security-basic-auth/src/main/java/org/baeldung/spring/config/SecSecurityConfig.java
@@ -1,10 +1,12 @@
 package org.baeldung.spring.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource({ "classpath:webSecurityConfig.xml" })
+@ComponentScan("org.baeldung.security")
 public class SecSecurityConfig {
 
     public SecSecurityConfig() {
