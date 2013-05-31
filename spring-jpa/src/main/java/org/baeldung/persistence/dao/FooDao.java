a7464803266148b7359e30ae99814c8b946d3d8e
eugenp <hanriseldon@gmail.com>
2013-06-01 00:40:51 +0300

    dao work

--- spring-jpa/src/main/java/org/baeldung/persistence/dao/FooDao.java
+++ spring-jpa/src/main/java/org/baeldung/persistence/dao/FooDao.java
@@ -1,60 +1,17 @@
 package org.baeldung.persistence.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.baeldung.persistence.model.Foo;
 import org.springframework.stereotype.Repository;
 
-import com.google.common.base.Preconditions;
-
 @Repository
-public class FooDao implements IFooDao {
-
-    @PersistenceContext
-    private EntityManager entityManager;
+public class FooDao extends AbstractJpaDAO<Foo> implements IFooDao {
 
     public FooDao() {
         super();
+
+        setClazz(Foo.class);
     }
 
     // API
-
-    @Override
-    public Foo findOne(final long id) {
-        return entityManager.find(Foo.class, id);
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public List<Foo> findAll() {
-        return entityManager.createQuery("from " + Foo.class.getName()).getResultList();
-    }
-
-    @Override
-    public void create(final Foo entity) {
-        Preconditions.checkNotNull(entity);
-        entityManager.persist(entity);
-    }
-
-    @Override
-    public Foo update(final Foo entity) {
-        Preconditions.checkNotNull(entity);
-        return entityManager.merge(entity);
-    }
-
-    @Override
-    public void delete(final Foo entity) {
-        Preconditions.checkNotNull(entity);
-        entityManager.remove(entity);
-    }
-
-    @Override
-    public void deleteById(final long entityId) {
-        final Foo entity = findOne(entityId);
-        delete(entity);
-    }
 
 }
--- spring-jpa/src/main/java/org/baeldung/persistence/model/Foo.java
+++ spring-jpa/src/main/java/org/baeldung/persistence/model/Foo.java
@@ -1,5 +1,7 @@
 package org.baeldung.persistence.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Foo {
+public class Foo implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
