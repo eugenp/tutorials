9722a641271389fd9f79c2f30b6780d7f4e09c6e
eugenp <hanriseldon@gmail.com>
2013-06-01 13:59:11 +0300

    properties work

--- spring-all/src/test/java/org/baeldung/properties/core/PropertiesWithMultipleXmlsIntegrationTest.java
+++ spring-all/src/test/java/org/baeldung/properties/core/PropertiesWithMultipleXmlsIntegrationTest.java
@@ -1,7 +1,7 @@
 package org.baeldung.properties.core;
 
-import org.baeldung.properties.spring.PropertiesWithJavaConfig;
-import org.baeldung.properties.spring.PropertiesWithJavaConfigOther;
+import org.baeldung.properties.spring.PropertiesWithXmlConfigOne;
+import org.baeldung.properties.spring.PropertiesWithXmlConfigTwo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +12,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { PropertiesWithJavaConfig.class, PropertiesWithJavaConfigOther.class }, loader = AnnotationConfigContextLoader.class)
-public class PropertiesWithJavaIntegrationTest {
+@ContextConfiguration(classes = { PropertiesWithXmlConfigOne.class, PropertiesWithXmlConfigTwo.class }, loader = AnnotationConfigContextLoader.class)
+public class PropertiesWithMultipleXmlsIntegrationTest {
 
     @Autowired
     private Environment env;
