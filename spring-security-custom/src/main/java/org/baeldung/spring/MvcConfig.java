0eea508ede08428700915ef7edd2b2ee2bcb7c5b
eparaschiv <eparaschiv@optaros.com>
2013-06-02 20:08:13 +0300

    overall security cleanup and fixes

--- spring-security-custom/src/main/java/org/baeldung/spring/MvcConfig.java
+++ spring-security-custom/src/main/java/org/baeldung/spring/MvcConfig.java
@@ -1,7 +1,6 @@
 package org.baeldung.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -9,8 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+// @Configuration
 @EnableWebMvc
-@Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     public MvcConfig() {
