4610d3e1ba91962785ddfa99baf5ce90a0c7a83b
Eugen <eparaschiv@optaros.com>
2013-06-03 18:42:57 +0300

    service work

--- spring-security-rest-full/src/main/java/org/baeldung/spring/SecSecurityConfig.java
+++ spring-security-rest-full/src/main/java/org/baeldung/spring/SecSecurityConfig.java
@@ -1,12 +1,10 @@
 package org.baeldung.spring;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
 @ImportResource({ "classpath:webSecurityConfig.xml" })
-@ComponentScan("org.baeldung.security")
 public class SecSecurityConfig {
 
     public SecSecurityConfig() {
