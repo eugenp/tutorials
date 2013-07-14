76351a44eae68b453582b8c03dec2dd0f580ca82
eugenp <hanriseldon@gmail.com>
2013-07-14 14:48:25 +0300

    spring all project

---
+++ spring-all/src/main/java/org/baeldung/properties/spring/PropertiesWithXmlConfigTwo.java
@@ -0,0 +1,14 @@
+package org.baeldung.properties.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:configForPropertiesTwo.xml")
+public class PropertiesWithXmlConfigTwo {
+
+    public PropertiesWithXmlConfigTwo() {
+        super();
+    }
+
+}
