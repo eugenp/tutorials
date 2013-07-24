4232fa188ac2cbb3fe47cf20b949a47479ace5b0
eparaschiv <eparaschiv@optaros.com>
2013-07-24 23:36:55 +0300

    adding controller, cleanup

--- spring-security-rest/src/main/java/org/baeldung/spring/WebConfig.java
+++ spring-security-rest/src/main/java/org/baeldung/spring/WebConfig.java
@@ -2,10 +2,13 @@ package org.baeldung.spring;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan("org.baeldung.web")
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     public WebConfig() {
         super();
