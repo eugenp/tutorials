e412b13818ba6aea0188b9622d3db78e069b94d9
eugenp <hanriseldon@gmail.com>
2013-08-19 21:34:24 +0300

    hibernate and other maven upgrades

--- spring-hibernate4/src/main/java/org/baeldung/persistence/dao/impl/ChildDao.java
+++ spring-hibernate4/src/main/java/org/baeldung/persistence/dao/impl/ChildDao.java
@@ -3,15 +3,10 @@ package org.baeldung.persistence.dao.impl;
 import org.baeldung.persistence.dao.IChildDao;
 import org.baeldung.persistence.dao.common.AbstractHibernateDao;
 import org.baeldung.persistence.model.Child;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ChildDao extends AbstractHibernateDao<Child> implements IChildDao {
-
-    @Autowired
-    private SessionFactory sessionFactory;
 
     public ChildDao() {
         super();
--- spring-hibernate4/src/main/java/org/baeldung/persistence/dao/impl/ParentDao.java
+++ spring-hibernate4/src/main/java/org/baeldung/persistence/dao/impl/ParentDao.java
@@ -3,15 +3,10 @@ package org.baeldung.persistence.dao.impl;
 import org.baeldung.persistence.dao.IParentDao;
 import org.baeldung.persistence.dao.common.AbstractHibernateDao;
 import org.baeldung.persistence.model.Parent;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ParentDao extends AbstractHibernateDao<Parent> implements IParentDao {
-
-    @Autowired
-    private SessionFactory sessionFactory;
 
     public ParentDao() {
         super();
