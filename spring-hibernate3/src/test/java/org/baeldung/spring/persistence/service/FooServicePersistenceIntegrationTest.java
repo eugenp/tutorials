df46e27e23ec56ae3f931ed3279cfeb687cd862d
eugenp <hanriseldon@gmail.com>
2013-12-18 15:41:36 +0200

    upgrading to spring 4

--- spring-hibernate3/src/test/java/org/baeldung/spring/persistence/service/FooServicePersistenceIntegrationTest.java
+++ spring-hibernate3/src/test/java/org/baeldung/spring/persistence/service/FooServicePersistenceIntegrationTest.java
@@ -5,10 +5,10 @@ import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import org.baeldung.persistence.model.Foo;
 import org.baeldung.persistence.service.FooService;
 import org.baeldung.spring.PersistenceConfig;
-import org.hibernate.exception.DataException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.test.context.ContextConfiguration;
@@ -51,7 +51,7 @@ public class FooServicePersistenceIntegrationTest {
         service.create(entity);
     }
 
-    @Test(expected = DataException.class)
+    @Test(expected = DataAccessException.class)
     public final void temp_whenInvalidEntityIsCreated_thenDataException() {
         service.create(new Foo(randomAlphabetic(2048)));
     }
