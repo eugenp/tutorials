458db43570987dc4ed3149262f7929b5654a3b60
eugenp <hanriseldon@gmail.com>
2014-01-06 21:35:23 +0200

    maven added dependency and IO work

--- core-java/src/test/java/org/baeldung/java/CoreJavaIoUnitTest.java
+++ core-java/src/test/java/org/baeldung/java/CoreJavaIoUnitTest.java
@@ -1,8 +1,17 @@
 package org.baeldung.java;
 
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 import org.apache.commons.io.FileUtils;
@@ -11,12 +20,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Charsets;
+import com.google.common.io.ByteSource;
+import com.google.common.io.CharStreams;
 import com.google.common.io.Files;
+import com.google.common.io.InputSupplier;
 
 public class CoreJavaIoUnitTest {
     protected final Logger logger = LoggerFactory.getLogger(getClass());
 
-    // tests -
+    // tests - iterate lines in a file
 
     @Test
     public final void givenUsingGuava_whenIteratingAFile_thenCorrect() throws IOException {
@@ -70,7 +82,45 @@ public class CoreJavaIoUnitTest {
         logMemory();
     }
 
-    //
+    // tests - InputStream to String
+
+    @Test
+    public final void givenUsingJava7_whenConvertingAnInputStreamToAString_thenCorrect() throws IOException {
+        final String originalString = randomAlphabetic(8);
+        final InputStream inputStream = new ByteArrayInputStream(originalString.getBytes()); // exampleString.getBytes(StandardCharsets.UTF_8);
+
+        // When
+        String text = null;
+        try (Scanner scanner = new Scanner(inputStream, StandardCharsets.UTF_8.name())) {
+            text = scanner.useDelimiter("\\A").next();
+        }
+
+        assertThat(text, equalTo(originalString));
+    }
+
+    @Test
+    public final void givenUsingGuavaNoEncoding_whenConvertingAnInputStreamToAString_thenCorrect() throws IOException {
+        final String originalString = randomAlphabetic(8);
+        final InputSupplier<StringReader> readerSupplier = CharStreams.newReaderSupplier(originalString);
+
+        // When
+        final String text = CharStreams.toString(readerSupplier);
+
+        assertThat(text, equalTo(originalString));
+    }
+
+    @Test
+    public final void givenUsingGuavaWithEncoding_whenConvertingAnInputStreamToAString_thenCorrect() throws IOException {
+        final String originalString = randomAlphabetic(8);
+        final InputSupplier<Reader> readerSupplier = ByteSource.wrap(originalString.getBytes()).asCharSource(Charsets.UTF_8);
+
+        // When
+        final String text = CharStreams.toString(readerSupplier);
+
+        assertThat(text, equalTo(originalString));
+    }
+
+    // utils
 
     private final void logMemory() {
         logger.info("Max Memory: {} Mb", Runtime.getRuntime().maxMemory() / 1048576);
@@ -79,4 +129,3 @@ public class CoreJavaIoUnitTest {
     }
 
 }
-//
