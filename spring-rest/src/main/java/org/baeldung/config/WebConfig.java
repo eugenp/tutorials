72edb72fd51157e779d9b35ebe4f834f3fe58232
eugenp <hanriseldon@gmail.com>
2014-01-10 13:42:21 +0200

    further marshalling work

--- spring-rest/src/main/java/org/baeldung/config/WebConfig.java
+++ spring-rest/src/main/java/org/baeldung/config/WebConfig.java
@@ -1,16 +1,44 @@
 package org.baeldung.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
+import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@EnableWebMvc
 @Configuration
+@EnableWebMvc
 @ComponentScan({ "org.baeldung.web" })
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
 
     public WebConfig() {
         super();
+    }
+
+    // API
+
+    @Override
+    public void configureMessageConverters(final List<HttpMessageConverter<?>> messageConverters) {
+        messageConverters.add(marshallingHttpMessageConverter());
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
+
+        super.configureMessageConverters(messageConverters);
+    }
+
+    // UTIL
+
+    private final MarshallingHttpMessageConverter marshallingHttpMessageConverter() {
+        final MarshallingHttpMessageConverter marshallingHttpMessageConverter = new MarshallingHttpMessageConverter();
+        final XStreamMarshaller xstreamMarshaller = new XStreamMarshaller();
+        marshallingHttpMessageConverter.setMarshaller(xstreamMarshaller);
+        marshallingHttpMessageConverter.setUnmarshaller(xstreamMarshaller);
+
+        return marshallingHttpMessageConverter;
     }
 
 }
