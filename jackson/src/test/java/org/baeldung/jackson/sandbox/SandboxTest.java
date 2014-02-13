31fd5453a5e0f40eb16f87e74de75c1f6e99248a
eugenp <hanriseldon@gmail.com>
2014-02-13 23:05:21 +0200

    jackson cleanup work

--- jackson/src/test/java/org/baeldung/jackson/sandbox/SandboxTest.java
+++ jackson/src/test/java/org/baeldung/jackson/sandbox/SandboxTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.junit.Test;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -16,6 +17,8 @@ public class SandboxTest {
         testElement.setX(10);
         testElement.setY("adasd");
         final ObjectMapper om = new ObjectMapper();
+        om.setVisibilityChecker(om.getSerializationConfig().getDefaultVisibilityChecker().withFieldVisibility(JsonAutoDetect.Visibility.ANY).withGetterVisibility(JsonAutoDetect.Visibility.NONE));
+
         final String serialized = om.writeValueAsString(testElement);
         System.err.println(serialized);
     }
