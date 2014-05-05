15f753ce553bf926eb8d41d6eeacb573268e0bc7
eugenp <hanriseldon@gmail.com>
2014-05-05 16:34:10 +0300

    hibernate 4 testing work

--- spring-hibernate4/src/test/java/org/baeldung/persistence/service/FooServicePersistenceIntegrationTest.java
+++ spring-hibernate4/src/test/java/org/baeldung/persistence/service/FooServicePersistenceIntegrationTest.java
@@ -3,7 +3,6 @@ package org.baeldung.persistence.service;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 
 import org.baeldung.persistence.model.Foo;
-import org.baeldung.persistence.service.IFooService;
 import org.baeldung.spring.PersistenceConfig;
 import org.junit.Ignore;
 import org.junit.Test;
