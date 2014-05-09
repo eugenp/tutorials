9bc262f2d40028f4d6c7b9e0ebc8dbefe1f58dee
egmp777 <egmp777@gmail.com>
2014-05-09 13:16:23 -0500

    committing hibernate part with model Bar and Foo

--- spring-hibernate4/src/main/java/org/baeldung/persistence/model/Foo.java
+++ spring-hibernate4/src/main/java/org/baeldung/persistence/model/Foo.java
@@ -1,7 +1,6 @@
 package org.baeldung.persistence.model;
 
 import java.io.Serializable;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
