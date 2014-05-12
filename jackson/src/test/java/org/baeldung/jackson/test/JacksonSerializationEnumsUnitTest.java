07419451d73213bde7d5ae94a26afe3acc7e31d7
eugenp <hanriseldon@gmail.com>
2014-05-12 17:57:33 +0300

    simple jackson test for enums

--- jackson/src/test/java/org/baeldung/jackson/test/JacksonSerializationEnumsUnitTest.java
+++ jackson/src/test/java/org/baeldung/jackson/test/JacksonSerializationEnumsUnitTest.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import org.baeldung.jackson.dtos.withEnum.MyDtoWithEnum;
 import org.baeldung.jackson.dtos.withEnum.MyDtoWithEnumCustom;
 import org.baeldung.jackson.dtos.withEnum.TypeEnum;
+import org.baeldung.jackson.dtos.withEnum.TypeEnumSimple;
 import org.baeldung.jackson.dtos.withEnum.TypeEnumWithCustomSerializer;
 import org.junit.Test;
 
@@ -19,9 +20,18 @@ public class JacksonSerializationEnumsUnitTest {
     // tests - enums
 
     @Test
-    public final void whenSerializingSimpleEnum_thenCorrect() throws JsonParseException, IOException {
+    public final void whenSerializingASimpleEnum_thenCorrect() throws JsonParseException, IOException {
         final ObjectMapper mapper = new ObjectMapper();
-        final String dtoAsString = mapper.writeValueAsString(TypeEnum.TYPE1);
+        final String dtoAsString = mapper.writeValueAsString(TypeEnumSimple.TYPE1.name());
+        System.out.println(dtoAsString);
+
+        assertThat(dtoAsString, containsString("TYPE1"));
+    }
+
+    @Test
+    public final void whenSerializingAnEnum_thenCorrect() throws JsonParseException, IOException {
+        final ObjectMapper mapper = new ObjectMapper();
+        final String dtoAsString = mapper.writeValueAsString(TypeEnum.TYPE1.name());
 
         System.out.println(dtoAsString);
         assertThat(dtoAsString, containsString("\"name\":\"Type A\""));
