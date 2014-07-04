8434f826e6a631409bdc9fbd705c4240fd7c5207
Dheeraj-Baluja <dheeraj.baluja@gmail.com>
2014-07-05 01:27:10 +0530

    Formatted the code

--- spring-mvc-forms/src/com/demo/controllers/EmployeeController.java
+++ spring-mvc-forms/src/com/demo/controllers/EmployeeController.java
@@ -15,20 +15,19 @@ import com.demo.form.Employee;
 @Controller
 public class EmployeeController {
 
-	@RequestMapping(value = "/employee", method = RequestMethod.GET)
-	public ModelAndView showForm() {
-		return new ModelAndView("employeeHome", "employee", new Employee());
-	}
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public ModelAndView showForm() {
+        return new ModelAndView("employeeHome", "employee", new Employee());
+    }
 
-	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
-	public String submit(@Valid @ModelAttribute("employee")Employee employee, BindingResult result,
-			ModelMap model) {
-		if (result.hasErrors()) {
-			return "error";
-		}
-		model.addAttribute("name", employee.getName());
-		model.addAttribute("contactNumber", employee.getContactNumber());
-		model.addAttribute("id", employee.getId());
-		return "employeeAdded";
-	}
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@Valid @ModelAttribute("employee") Employee employee, BindingResult result, ModelMap model) {
+        if (result.hasErrors()) {
+            return "error";
+        }
+        model.addAttribute("name", employee.getName());
+        model.addAttribute("contactNumber", employee.getContactNumber());
+        model.addAttribute("id", employee.getId());
+        return "employeeAdded";
+    }
 }
--- spring-mvc-forms/src/com/demo/form/Employee.java
+++ spring-mvc-forms/src/com/demo/form/Employee.java
@@ -2,32 +2,32 @@ package com.demo.form;
 
 public class Employee {
 
-	private String name;
-	private long id;
-	private String contactNumber;
+    private String name;
+    private long id;
+    private String contactNumber;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getContactNumber() {
-		return contactNumber;
-	}
+    public String getContactNumber() {
+        return contactNumber;
+    }
 
-	public void setContactNumber(String contactNumber) {
-		this.contactNumber = contactNumber;
-	}
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 
 }
