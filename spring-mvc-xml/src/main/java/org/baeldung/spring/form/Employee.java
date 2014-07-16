1ee5b0580097a781d9d87b387d7aaa99a5b1bf9f
Dheeraj-Baluja <dheeraj.baluja@gmail.com>
2014-07-17 01:36:40 +0530

    adding spring form changes

---
+++ spring-mvc-xml/src/main/java/org/baeldung/spring/form/Employee.java
@@ -0,0 +1,33 @@
+package org.baeldung.spring.form;
+
+public class Employee {
+
+    private String name;
+    private long id;
+    private String contactNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+}
