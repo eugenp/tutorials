09b4633741d022a8f74d6ad7c5ca430fe8fb0a80
Rachel Shu <shufangrui@gmail.com>
2014-07-22 09:09:34 -0400

    Reduce fields not necessary.

--- jackson/src/test/java/org/baeldung/jackson/test/JacksonFieldUnitTest.java
+++ jackson/src/test/java/org/baeldung/jackson/test/JacksonFieldUnitTest.java
@@ -23,7 +23,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class JacksonFieldUnitTest {
 
     @Test
-    public final void givenDifferentAccessLevels_whenPrivateOrPackage_thenNotSerializable_whenPublic_thenSerializable() throws JsonProcessingException {
+    public final void givenDifferentAccessLevels_whenPublic_thenSerializable() throws JsonProcessingException {
         final ObjectMapper mapper = new ObjectMapper();
 
         final MyDtoAccessLevel dtoObject = new MyDtoAccessLevel();
@@ -49,42 +49,39 @@ public class JacksonFieldUnitTest {
 
     @Test
     public final void givenDifferentAccessLevels_whenGetterAdded_thenDeserializable() throws JsonProcessingException, JsonMappingException, IOException {
-        final String jsonAsString = "{\"stringValue\":\"dtoString\",\"booleanValue\":\"true\"}";
+        final String jsonAsString = "{\"stringValue\":\"dtoString\"}";
         final ObjectMapper mapper = new ObjectMapper();
 
         final MyDtoGetterImplicitDeserialization dtoObject = mapper.readValue(jsonAsString, MyDtoGetterImplicitDeserialization.class);
 
         assertNotNull(dtoObject);
         assertThat(dtoObject.getStringValue(), equalTo("dtoString"));
-        assertThat(dtoObject.booleanValue, equalTo(true));
     }
 
     @Test
     public final void givenDifferentAccessLevels_whenSetterAdded_thenDeserializable() throws JsonProcessingException, JsonMappingException, IOException {
-        final String jsonAsString = "{\"stringValue\":\"dtoString\",\"intValue\":1}";
+        final String jsonAsString = "{\"intValue\":1}";
         final ObjectMapper mapper = new ObjectMapper();
 
         final MyDtoSetter dtoObject = mapper.readValue(jsonAsString, MyDtoSetter.class);
 
         assertNotNull(dtoObject);
-        assertThat(dtoObject.getStringValue(), equalTo("dtoString"));
         assertThat(dtoObject.anotherGetIntValue(), equalTo(1));
     }
 
     @Test
-    public final void givenDifferentAccessLevels_whenSetterAdded_thenStillNotSerializable() throws JsonProcessingException, JsonMappingException, IOException {
+    public final void givenDifferentAccessLevels_whenSetterAdded_thenStillNotSerializable() throws JsonProcessingException {
         final ObjectMapper mapper = new ObjectMapper();
 
         final MyDtoSetter dtoObject = new MyDtoSetter();
 
         final String dtoAsString = mapper.writeValueAsString(dtoObject);
-        assertThat(dtoAsString, containsString("stringValue"));
         assertThat(dtoAsString, not(containsString("intValue")));
         System.out.println(dtoAsString);
     }
 
     @Test
-    public final void givenDifferentAccessLevels_whenSetVisibility_thenSerializable() throws JsonProcessingException, JsonMappingException, IOException {
+    public final void givenDifferentAccessLevels_whenSetVisibility_thenSerializable() throws JsonProcessingException {
         final ObjectMapper mapper = new ObjectMapper();
         mapper.setVisibility(PropertyAccessor.ALL, Visibility.NONE);
         mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
