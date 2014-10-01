fbe55d05842421a6cd9faece025357a2f9a8bcef
mgooty <mgooty@paypal.com>
2014-10-01 14:30:11 +0530

    Added example code for usage and configuration of @Scheduled annotation.

---
+++ spring-all/src/main/java/org/baeldung/scheduling/SpringSchedulingConfig.java
@@ -0,0 +1,20 @@
+package org.baeldung.scheduling;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@ComponentScan("com.baeldung.spring.integration")
+@PropertySource("classpath:springIntegration.properties")
+public class SpringSchedulingConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+}
