88bd851199a6c5fef390aabb5dccd8b1c081cb3d
DOHA <dohae243@gmail.com>
2015-01-18 13:25:52 +0200

    add spring jpa specifications test

--- spring-jpa/src/test/java/org/baeldung/persistence/query/JPASpecificationsTest.java
+++ spring-jpa/src/test/java/org/baeldung/persistence/query/JPASpecificationsTest.java
@@ -1,16 +1,20 @@
 package org.baeldung.persistence.query;
 
+import static org.baeldung.persistence.dao.UserSpecifications.ageIsGreaterThan;
+import static org.baeldung.persistence.dao.UserSpecifications.firstNameIsLike;
+import static org.baeldung.persistence.dao.UserSpecifications.lastNameIsLike;
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
 import org.baeldung.config.PersistenceJPAConfig;
+import org.baeldung.persistence.dao.UserRepository;
 import org.baeldung.persistence.model.User;
-import org.baeldung.persistence.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -21,10 +25,10 @@ import org.springframework.transaction.annotation.Transactional;
 @ContextConfiguration(classes = { PersistenceJPAConfig.class }, loader = AnnotationConfigContextLoader.class)
 @Transactional
 @TransactionConfiguration
-public class JPACriteriaQueryTest {
+public class JPASpecificationsTest {
 
     @Autowired
-    private UserService userService;
+    private UserRepository repository;
 
     private User userJohn;
 
@@ -37,33 +41,33 @@ public class JPACriteriaQueryTest {
         userJohn.setLastName("Doe");
         userJohn.setEmail("john@doe.com");
         userJohn.setAge(22);
-        userService.saveUser(userJohn);
+        repository.save(userJohn);
 
         userTom = new User();
         userTom.setFirstName("Tom");
         userTom.setLastName("Doe");
         userTom.setEmail("tom@doe.com");
         userTom.setAge(26);
-        userService.saveUser(userTom);
+        repository.save(userTom);
+    }
+
+    @Test
+    public void givenLast_whenGettingListOfUsers_thenCorrect() {
+        final List<User> result = repository.findAll(lastNameIsLike("doe"));
+        assertEquals(2, result.size());
     }
 
     @Test
     public void givenFirstAndLastName_whenGettingListOfUsers_thenCorrect() {
-        final List<User> result = userService.searchUser("John", "Doe", 0);
+        final List<User> result = repository.findAll(Specifications.where(lastNameIsLike("doe")).and(firstNameIsLike("john")));
 
         assertEquals(1, result.size());
         assertEquals(userJohn.getEmail(), result.get(0).getEmail());
     }
 
     @Test
-    public void givenLast_whenGettingListOfUsers_thenCorrect() {
-        final List<User> result = userService.searchUser("", "doe", 0);
-        assertEquals(2, result.size());
-    }
-
-    @Test
     public void givenLastAndAge_whenGettingListOfUsers_thenCorrect() {
-        final List<User> result = userService.searchUser("", "doe", 25);
+        final List<User> result = repository.findAll(Specifications.where(lastNameIsLike("doe")).and(ageIsGreaterThan(25)));
 
         assertEquals(1, result.size());
         assertEquals(userTom.getEmail(), result.get(0).getEmail());
@@ -71,13 +75,13 @@ public class JPACriteriaQueryTest {
 
     @Test
     public void givenWrongFirstAndLast_whenGettingListOfUsers_thenCorrect() {
-        final List<User> result = userService.searchUser("Adam", "Fox", 0);
+        final List<User> result = repository.findAll(Specifications.where(lastNameIsLike("Adam")).and(firstNameIsLike("Fox")));
         assertEquals(0, result.size());
     }
 
     @Test
     public void givenPartialFirst_whenGettingListOfUsers_thenCorrect() {
-        final List<User> result = userService.searchUser("jo", "", 0);
+        final List<User> result = repository.findAll(firstNameIsLike("jo"));
 
         assertEquals(1, result.size());
         assertEquals(userJohn.getEmail(), result.get(0).getEmail());
