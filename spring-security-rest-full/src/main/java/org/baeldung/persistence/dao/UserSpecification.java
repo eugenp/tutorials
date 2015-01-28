f479bc8a2178a3b5f7e46a05c6f85f115a0bda93
eugenp <hanriseldon@gmail.com>
2015-01-28 11:19:10 +0200

    minor formatting cleanup

--- spring-security-rest-full/src/main/java/org/baeldung/persistence/dao/UserSpecification.java
+++ spring-security-rest-full/src/main/java/org/baeldung/persistence/dao/UserSpecification.java
@@ -38,7 +38,7 @@ public class UserSpecification implements Specification<User> {
             return builder.lessThanOrEqualTo(root.<String> get(criteria.getKey()), criteria.getValue().toString());
         } else if (criteria.getOperation().equalsIgnoreCase(":")) {
             if (root.get(criteria.getKey()).getJavaType() == String.class) {
-                return builder.like(root.<String>get(criteria.getKey()), "%" + criteria.getValue() + "%");
+                return builder.like(root.<String> get(criteria.getKey()), "%" + criteria.getValue() + "%");
             } else {
                 return builder.equal(root.get(criteria.getKey()), criteria.getValue());
             }
