03e182d6c75c4425240455c41db24b1b4919df70
DOHA <dohae243@gmail.com>
2015-02-12 14:12:11 +0200

    add exception handler

---
+++ spring-security-login-and-registration/src/main/java/org/baeldung/web/error/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,35 @@
+package org.baeldung.web.error;
+
+import org.springframework.mail.MailAuthenticationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    public RestResponseEntityExceptionHandler() {
+        super();
+    }
+
+    // API
+
+    // 500
+    @ExceptionHandler({ MailAuthenticationException.class })
+    public @ResponseBody String handleMail(final RuntimeException ex, final WebRequest request) throws JsonProcessingException {
+        logger.error("500 Status Code", ex);
+        return new ObjectMapper().writeValueAsString(ex.getClass().toString());
+    }
+
+    @ExceptionHandler({ NullPointerException.class, IllegalArgumentException.class, IllegalStateException.class })
+    public @ResponseBody String handleInternal(final RuntimeException ex, final WebRequest request) throws JsonProcessingException {
+        logger.error("500 Status Code", ex);
+        return new ObjectMapper().writeValueAsString(ex.getClass().toString());
+    }
+
+}
