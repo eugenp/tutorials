4d8824c809faa7077baf83450cc71ba13d1f2398
DOHA <dohae243@gmail.com>
2015-02-14 17:52:46 +0200

    modify spring security registration

--- spring-security-login-and-registration/src/main/java/org/baeldung/registration/listener/RegistrationListener.java
+++ spring-security-login-and-registration/src/main/java/org/baeldung/registration/listener/RegistrationListener.java
@@ -8,11 +8,14 @@ import org.baeldung.registration.OnRegistrationCompleteEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:email.properties")
 public class RegistrationListener implements ApplicationListener<OnRegistrationCompleteEvent> {
     @Autowired
     private IUserService service;
@@ -22,6 +25,9 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
 
     @Autowired
     private JavaMailSender mailSender;
+
+    @Autowired
+    private Environment env;
 
     // API
 
@@ -44,13 +50,13 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
     private final SimpleMailMessage constructEmailMessage(final OnRegistrationCompleteEvent event, final User user, final String token) {
         final String recipientAddress = user.getEmail();
         final String subject = "Registration Confirmation";
-        final String confirmationUrl = "http://" + event.getAppUrl() + "/regitrationConfirm.html?token=" + token;
+        final String confirmationUrl = event.getAppUrl() + "/regitrationConfirm.html?token=" + token;
         final String message = messages.getMessage("message.regSucc", null, event.getLocale());
         final SimpleMailMessage email = new SimpleMailMessage();
         email.setTo(recipientAddress);
         email.setSubject(subject);
         email.setText(message + " \r\n" + confirmationUrl);
-        email.setFrom("eugen@baeldung.com");
+        email.setFrom(env.getProperty("support.email"));
         return email;
     }
 
--- spring-security-login-and-registration/src/main/webapp/WEB-INF/view/updatePassword.jsp
+++ spring-security-login-and-registration/src/main/webapp/WEB-INF/view/updatePassword.jsp
@@ -17,16 +17,23 @@
 	<div class="container">
 		<div class="span12">
 			<H1>
-				<spring:message code="message.updatePassword"></spring:message>
+				<spring:message code="message.resetYourPassword"></spring:message>
 			</H1>
 			<form:form action="user/savePassword" method="POST" enctype="utf8">
 				<br>
 				
 				<tr>
 					<td><label><spring:message code="label.user.password"></spring:message></label></td>
-					<td><input name="password" type="password" value="" /></td>
+					<td><input id="pass" name="password" type="password" value="" /></td>
 				</tr>
-				
+				<tr>
+                    <td><label><spring:message code="label.user.confirmPass"></spring:message></label></td>
+                    <td>
+                        <input id="passConfirm" type="password" value="" />
+                        <span id="error" class="alert alert-error" style="display:none"><spring:message code="PasswordMatches.user"></spring:message></span>
+                    </td>
+                </tr>
+				<br><br>
 				<button type="submit">
 					<spring:message code="message.updatePassword"></spring:message>
 				</button>
@@ -35,7 +42,18 @@
 		</div>
 	</div>
 	</sec:authorize>
-	
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	  $('form').on('submit', function(e){
+		var valid = $("#pass").val() == $("#passConfirm").val();
+	    if(!valid) {
+	      e.preventDefault();
+	      $("#error").show();
+	    }
+	  });
+	});
+</script>	
 </body>
 
 </html>
