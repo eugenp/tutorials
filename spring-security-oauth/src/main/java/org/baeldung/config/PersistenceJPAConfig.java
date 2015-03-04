1ebefa3b9c57775860f29ec1ea4fc7beb335d22c
eugenp <hanriseldon@gmail.com>
2015-03-04 13:16:03 +0200

    ignoring the persistence.properties

--- spring-security-oauth/src/main/java/org/baeldung/config/PersistenceJPAConfig.java
+++ spring-security-oauth/src/main/java/org/baeldung/config/PersistenceJPAConfig.java
@@ -67,7 +67,7 @@ public class PersistenceJPAConfig {
 
     final Properties additionalProperties() {
         final Properties hibernateProperties = new Properties();
-        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+        hibernateProperties.setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto", "create-drop"));
         hibernateProperties.setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
         return hibernateProperties;
     }
