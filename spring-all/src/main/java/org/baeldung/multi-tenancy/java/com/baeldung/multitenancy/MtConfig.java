eaa4a1683f305899adf4b44e1ef03d664158142d
abhibavishi <bavishiabhi@gmail.com>
2015-06-17 10:10:58 +0530

    Update MtConfig.java

--- spring-all/src/main/java/org/baeldung/multi-tenancy/java/com/baeldung/multitenancy/MtConfig.java
+++ spring-all/src/main/java/org/baeldung/multi-tenancy/java/com/baeldung/multitenancy/MtConfig.java
@@ -10,7 +10,7 @@ import java.util.Map;
 import org.apache.log4j.Logger;
 
 @Configuration
-public class MTConfig {
+public class MtConfig {
 
     @Bean
     public SessionFactoryBean sessionFactory(Map<String, DataSource> dataSourceMap, DataSource dataSource){
