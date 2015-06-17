bd51d366c46e92bac4a1e2003ff912f2bff88c28
abhibavishi <bavishiabhi@gmail.com>
2015-06-17 10:14:08 +0530

    Update Book.java

--- spring-all/src/main/java/org/baeldung/multi-tenancy/java/com/baeldung/multitenancy/entities/Book.java
+++ spring-all/src/main/java/org/baeldung/multi-tenancy/java/com/baeldung/multitenancy/entities/Book.java
@@ -14,6 +14,18 @@ public class Book {
     @NotNull
     @Column(name = "name")
     String name;
-
-    // standard getters and setters
+    
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
 }
