d685c93a5467a82771f45ef603aa2b56a91b900d
abhibavishi <bavishiabhi@gmail.com>
2015-06-19 02:00:12 +0530

    Update CustomerDataService.java

--- spring-all/src/main/java/org/baeldung/caching/example/CustomerDataService.java
+++ spring-all/src/main/java/org/baeldung/caching/example/CustomerDataService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class CustomerDataService {
 
     @Autowired
-    CacheManager cacheManager;
+    private CacheManager cacheManager;
 
     /**
      * The method returns the customer's address,
