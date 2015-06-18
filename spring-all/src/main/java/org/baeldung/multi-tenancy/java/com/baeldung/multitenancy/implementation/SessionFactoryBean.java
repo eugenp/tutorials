6021f86629689ffbc61747917170f422125aecf9
abhibavishi <bavishiabhi@gmail.com>
2015-06-19 01:59:00 +0530

    Update SessionFactoryBean.java

--- spring-all/src/main/java/org/baeldung/multi-tenancy/java/com/baeldung/multitenancy/implementation/SessionFactoryBean.java
+++ spring-all/src/main/java/org/baeldung/multi-tenancy/java/com/baeldung/multitenancy/implementation/SessionFactoryBean.java
@@ -15,21 +15,16 @@ public class SessionFactoryBean extends org.springframework.orm.hibernate4.Local
     @Override
     public void afterPropertiesSet() throws IOException {
         super.afterPropertiesSet();
-
         for (Map.Entry<String, DataSource> entry : dataSourceMap.entrySet()) {
-
             Connection connection = null;
-
             try {
                 connection = entry.getValue().getConnection();
                 SchemaExport export = new SchemaExport(getConfiguration(), connection);
                 export.setOutputFile(entry.getKey() + "-schema.sql");
                 export.setDelimiter(";");
-
                 // define your rules, here.
                 //The below code allows creation, but does not allow dropping entries
                 export.execute(true, true, false, true);
-
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
