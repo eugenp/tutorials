fbc3539b6eebd21aeb0d50545c8463296904992c
alex-semenyuk <alexemenyuk88@gmail.com>
2015-07-26 16:01:12 +0200

    Initial commit

---
+++ spring-data-mongodb/src/main/java/org/baeldung/model/User.java
@@ -0,0 +1,37 @@
+package org.baeldung.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
