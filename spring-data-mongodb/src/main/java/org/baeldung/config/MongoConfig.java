fe7c28fef90d983f1043d9bca4b8bdedfb16d469
coach88 <alexsemenyuk88@gmail.com>
2015-07-28 11:07:22 +0300

    MongoTemplate configuration with the annotation @Configuration

---
+++ spring-data-mongodb/src/main/java/org/baeldung/config/MongoConfig.java
@@ -0,0 +1,28 @@
+package org.baeldung.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+
+@Configuration
+@EnableMongoRepositories
+public class MongoConfig extends AbstractMongoConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "test";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("127.0.0.1", 27017);
+    }
+
+    @Override
+    protected String getMappingBasePackage() {
+        return "org.baeldung";
+    }
+}
