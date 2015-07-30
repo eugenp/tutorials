2e54ddc841f5be0107f8b8961768b11a9c07fa8b
coach88 <alexsemenyuk88@gmail.com>
2015-07-30 15:00:47 +0300

    Added the example with MongoRepository

--- spring-data-mongodb/src/main/java/org/baeldung/config/MongoConfig.java
+++ spring-data-mongodb/src/main/java/org/baeldung/config/MongoConfig.java
@@ -2,11 +2,13 @@ package org.baeldung.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 
 @Configuration
+@EnableMongoRepositories(basePackages = "org.baeldung.repository")
 public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
@@ -20,7 +22,7 @@ public class MongoConfig extends AbstractMongoConfiguration {
     }
 
     @Override
-    protected String getMappingBasePackage() {
+    public String getMappingBasePackage() {
         return "org.baeldung";
     }
 }
