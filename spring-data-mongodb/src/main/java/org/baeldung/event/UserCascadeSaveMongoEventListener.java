c9d5aff1ff9eff3c2d5e3bd03fa8ce38ded8b73b
alex-semenyuk <alexemenyuk88@gmail.com>
2015-08-23 20:46:34 +0200

    Added simple CascadeSave email for User

--- spring-data-mongodb/src/main/java/org/baeldung/event/UserCascadeSaveMongoEventListener.java
+++ spring-data-mongodb/src/main/java/org/baeldung/event/UserCascadeSaveMongoEventListener.java
@@ -1,17 +1,18 @@
 package org.baeldung.event;
 
+import org.baeldung.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
-import org.springframework.util.ReflectionUtils;
 
-public class CascadeSaveMongoEventListener extends AbstractMongoEventListener<Object> {
-
+public class UserCascadeSaveMongoEventListener extends AbstractMongoEventListener<Object> {
     @Autowired
     private MongoOperations mongoOperations;
 
     @Override
     public void onBeforeConvert(final Object source) {
-        ReflectionUtils.doWithFields(source.getClass(), new CascadeCallback(source, mongoOperations));
+        if (source instanceof User && ((User) source).getEmailAddress() != null) {
+            mongoOperations.save(((User) source).getEmailAddress());
+        }
     }
 }
