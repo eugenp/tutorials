d7bb016ea2f535efe815057e2580e27add4f6982
DOHA <dohae243@gmail.com>
2015-11-06 16:26:30 +0200

    minor fix

--- spring-security-oauth/spring-security-oauth-server/src/main/resources/schema.sql
+++ spring-security-oauth/spring-security-oauth-server/src/main/resources/schema.sql
@@ -1,50 +1,50 @@
 -- used in tests that use HSQL
 create table if not exists oauth_client_details (
-  client_id VARCHAR(256) PRIMARY KEY,
-  resource_ids VARCHAR(256),
-  client_secret VARCHAR(256),
-  scope VARCHAR(256),
-  authorized_grant_types VARCHAR(256),
-  web_server_redirect_uri VARCHAR(256),
-  authorities VARCHAR(256),
+  client_id VARCHAR(255) PRIMARY KEY,
+  resource_ids VARCHAR(255),
+  client_secret VARCHAR(255),
+  scope VARCHAR(255),
+  authorized_grant_types VARCHAR(255),
+  web_server_redirect_uri VARCHAR(255),
+  authorities VARCHAR(255),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additional_information VARCHAR(4096),
-  autoapprove VARCHAR(256)
+  autoapprove VARCHAR(255)
 );
 
 create table if not exists oauth_client_token (
-  token_id VARCHAR(256),
+  token_id VARCHAR(255),
   token LONG VARBINARY,
-  authentication_id VARCHAR(256) PRIMARY KEY,
-  user_name VARCHAR(256),
-  client_id VARCHAR(256)
+  authentication_id VARCHAR(255) PRIMARY KEY,
+  user_name VARCHAR(255),
+  client_id VARCHAR(255)
 );
 
 create table if not exists oauth_access_token (
-  token_id VARCHAR(256),
+  token_id VARCHAR(255),
   token LONG VARBINARY,
-  authentication_id VARCHAR(256) PRIMARY KEY,
-  user_name VARCHAR(256),
-  client_id VARCHAR(256),
+  authentication_id VARCHAR(255) PRIMARY KEY,
+  user_name VARCHAR(255),
+  client_id VARCHAR(255),
   authentication LONG VARBINARY,
-  refresh_token VARCHAR(256)
+  refresh_token VARCHAR(255)
 );
 
 create table if not exists oauth_refresh_token (
-  token_id VARCHAR(256),
+  token_id VARCHAR(255),
   token LONG VARBINARY,
   authentication LONG VARBINARY
 );
 
 create table if not exists oauth_code (
-  code VARCHAR(256), authentication LONG VARBINARY
+  code VARCHAR(255), authentication LONG VARBINARY
 );
 
 create table if not exists oauth_approvals (
-	userId VARCHAR(256),
-	clientId VARCHAR(256),
-	scope VARCHAR(256),
+	userId VARCHAR(255),
+	clientId VARCHAR(255),
+	scope VARCHAR(255),
 	status VARCHAR(10),
 	expiresAt TIMESTAMP,
 	lastModifiedAt TIMESTAMP
@@ -52,15 +52,15 @@ create table if not exists oauth_approvals (
 
 -- customized oauth_client_details table
 create table if not exists ClientDetails (
-  appId VARCHAR(256) PRIMARY KEY,
-  resourceIds VARCHAR(256),
-  appSecret VARCHAR(256),
-  scope VARCHAR(256),
-  grantTypes VARCHAR(256),
-  redirectUrl VARCHAR(256),
-  authorities VARCHAR(256),
+  appId VARCHAR(255) PRIMARY KEY,
+  resourceIds VARCHAR(255),
+  appSecret VARCHAR(255),
+  scope VARCHAR(255),
+  grantTypes VARCHAR(255),
+  redirectUrl VARCHAR(255),
+  authorities VARCHAR(255),
   access_token_validity INTEGER,
   refresh_token_validity INTEGER,
   additionalInformation VARCHAR(4096),
-  autoApproveScopes VARCHAR(256)
+  autoApproveScopes VARCHAR(255)
 );
