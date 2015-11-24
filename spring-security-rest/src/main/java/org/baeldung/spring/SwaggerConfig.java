09ec3d08ec4b353445c62d2eccf31650519bbf57
Alex V <ovengrovsk@i.ua>
2015-11-24 22:05:43 +0200

    Setting Up Swagger 2 with a Spring REST API

---
+++ spring-security-rest/src/main/java/org/baeldung/spring/SwaggerConfig.java
@@ -0,0 +1,57 @@
+package main.java.org.baeldung.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.collect.Lists.newArrayList;
+
+/**
+ * Created by O.Vengrovskiy on 24.11.2015.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+          .select()
+          .apis(RequestHandlerSelectors.basePackage("org.baeldung.web.controller"))
+          .paths(PathSelectors.ant("/foos/*"))
+          .build()
+          .apiInfo(apiInfo())
+          .useDefaultResponseMessages(false)
+          .globalResponseMessage(RequestMethod.GET,
+                  newArrayList(new ResponseMessageBuilder()
+                                  .code(500)
+                                  .message("500 message")
+                                  .responseModel(new ModelRef("Error"))
+                                  .build(),
+                          new ResponseMessageBuilder()
+                                  .code(403)
+                                  .message("Forbidden!!!!!")
+                                  .build()));
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+          "My Project's REST API",
+          "This is a description of your API.",
+          "API TOS",
+          "Terms of service",
+          "me@wherever.com",
+          "API License",
+          "API License URL"
+        );
+        return apiInfo;
+    }
+}
