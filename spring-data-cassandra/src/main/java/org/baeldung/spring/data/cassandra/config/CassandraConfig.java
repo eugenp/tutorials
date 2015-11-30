51649533eedb38b76c7466cfb113aca4c8008a0c
sameira <sameira@gmail.com>
2015-11-30 22:46:32 +0530

    Adding tests with embedded cassandra support

--- spring-data-cassandra/src/main/java/org/baeldung/spring/data/cassandra/config/CassandraConfig.java
+++ spring-data-cassandra/src/main/java/org/baeldung/spring/data/cassandra/config/CassandraConfig.java
@@ -1,8 +1,12 @@
 package org.baeldung.spring.data.cassandra.config;
 
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.data.cassandra.config.CassandraClusterFactoryBean;
 import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
 import org.springframework.data.cassandra.mapping.BasicCassandraMappingContext;
@@ -10,19 +14,26 @@ import org.springframework.data.cassandra.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @Configuration
+@PropertySource(value = { "classpath:cassandra.properties" })
 @EnableCassandraRepositories(basePackages = "org.baeldung.spring.data.cassandra.repository")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
+    private static final Log LOGGER = LogFactory.getLog(CassandraConfig.class);
+    @Autowired
+    private Environment environment;
+
     @Override
     protected String getKeyspaceName() {
-        return "event";
+        return environment.getProperty("cassandra.keyspace");
     }
 
     @Bean
     public CassandraClusterFactoryBean cluster() {
         CassandraClusterFactoryBean cluster = new CassandraClusterFactoryBean();
-        cluster.setContactPoints("127.0.0.1");
-        cluster.setPort(9142);
+        cluster.setContactPoints(environment.getProperty("cassandra.contactpoints"));
+        cluster.setPort(Integer.parseInt(environment.getProperty("cassandra.port")));
+        LOGGER.info("Cluster created with contact points ["+ environment.getProperty("cassandra.contactpoints") +"] " +
+                "& port [" + Integer.parseInt(environment.getProperty("cassandra.port")) + "].");
         return cluster;
     }
 
