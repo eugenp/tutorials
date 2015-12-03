175f1195738a8a747085507c8f3d98bb53763c80
Dmitry Zinkevich <zinkevich.da@gmail.com>
2015-12-03 13:46:25 +0300

    Create AOP examples
    - Use different types of advice
    - Use various types of pointcut expressions

--- spring-mvc-java/src/main/java/org/baeldung/dao/FooDao.java
+++ spring-mvc-java/src/main/java/org/baeldung/dao/FooDao.java
@@ -1,10 +1,16 @@
 package org.baeldung.dao;
 
+import org.baeldung.model.Foo;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class FooDao {
+
     public String findById(Long id) {
         return "Bazz";
+    }
+
+    public Foo create(Long id, String name) {
+        return new Foo(id, name);
     }
 }
