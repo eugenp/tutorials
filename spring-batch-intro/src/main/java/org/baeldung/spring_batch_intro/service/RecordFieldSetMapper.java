c2576ca3f4323acaf59b8d095b8e62054fec3d35
Devendra Desale <devendra.desale@gmail.com>
2015-12-11 15:16:48 +0800

    Adding spring batch project

---
+++ spring-batch-intro/src/main/java/org/baeldung/spring_batch_intro/service/RecordFieldSetMapper.java
@@ -0,0 +1,33 @@
+package org.baeldung.spring_batch_intro.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.baeldung.spring_batch_intro.model.Transaction;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class RecordFieldSetMapper implements FieldSetMapper<Transaction> {
+
+	public Transaction mapFieldSet(FieldSet fieldSet) throws BindException {
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		Transaction transaction = new Transaction();
+		
+		transaction.setUsername(fieldSet.readString("username"));
+		transaction.setUserId(fieldSet.readInt(1));
+		transaction.setAmount(fieldSet.readDouble(3));
+		//Converting the date
+		String dateString = fieldSet.readString(2);
+		try {
+			transaction.setTransactionDate(dateFormat.parse(dateString));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+
+		return transaction;
+
+	}
+
+}
