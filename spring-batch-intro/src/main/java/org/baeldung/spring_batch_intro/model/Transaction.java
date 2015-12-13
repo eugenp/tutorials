57b27f6c9c952a96aed157935873d54dd814bd4e
Devendra Desale <devendra.desale@gmail.com>
2015-12-13 22:12:09 +0800

    adding java based spring batch configuration

--- spring-batch-intro/src/main/java/org/baeldung/spring_batch_intro/model/Transaction.java
+++ spring-batch-intro/src/main/java/org/baeldung/spring_batch_intro/model/Transaction.java
@@ -7,48 +7,48 @@ import javax.xml.bind.annotation.XmlRootElement;
 @SuppressWarnings("restriction")
 @XmlRootElement(name = "transactionRecord")
 public class Transaction {
-	private String username;
-	private int userId;
-	private Date transactionDate;
-	private double amount;
+    private String username;
+    private int userId;
+    private Date transactionDate;
+    private double amount;
 
-	public String getUsername() {
-		return username;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public int getUserId() {
-		return userId;
-	}
+    public int getUserId() {
+        return userId;
+    }
 
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 
-	public Date getTransactionDate() {
-		return transactionDate;
-	}
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
 
-	public void setTransactionDate(Date transactionDate) {
-		this.transactionDate = transactionDate;
-	}
+    public void setTransactionDate(Date transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 
-	public double getAmount() {
-		return amount;
-	}
+    public double getAmount() {
+        return amount;
+    }
 
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
 
-	@Override
-	public String toString() {
-		return "Transaction [username=" + username + ", userId=" + userId
-				+ ", transactionDate=" + transactionDate + ", amount=" + amount
-				+ "]";
-	}
+    @Override
+    public String toString() {
+        return "Transaction [username=" + username + ", userId=" + userId
+                + ", transactionDate=" + transactionDate + ", amount=" + amount
+                + "]";
+    }
 
 }
--- spring-batch-intro/src/main/java/org/baeldung/spring_batch_intro/service/CustomItemProcessor.java
+++ spring-batch-intro/src/main/java/org/baeldung/spring_batch_intro/service/CustomItemProcessor.java
@@ -1,14 +1,13 @@
 package org.baeldung.spring_batch_intro.service;
 
-
 import org.baeldung.spring_batch_intro.model.Transaction;
 import org.springframework.batch.item.ItemProcessor;
 
-public class CustomItemProcessor implements ItemProcessor<Transaction, Transaction> {
+public class CustomItemProcessor implements
+        ItemProcessor<Transaction, Transaction> {
 
-	public Transaction process(Transaction item) throws Exception {
-		
-		System.out.println("Processing..." + item);
-		return item;
-	}
+    public Transaction process(Transaction item) throws Exception {
+        System.out.println("Processing..." + item);
+        return item;
+    }
 }
