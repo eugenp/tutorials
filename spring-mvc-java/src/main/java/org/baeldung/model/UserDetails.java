c55dddfcfbc1a78e36bf6394fc871ca4048f8e52
vkadapa <vishwanath.kadapa@gmail.com>
2015-12-18 06:37:00 +0530

    Vishwanth: Merged my changes of Thymeleaf POC (Proof of Concept)

---
+++ spring-mvc-java/src/main/java/org/baeldung/model/UserDetails.java
@@ -0,0 +1,32 @@
+package org.baeldung.model;
+
+public class UserDetails {
+    private String firstname;
+    private String lastname;
+    private String emailId;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(final String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(final String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(final String emailId) {
+        this.emailId = emailId;
+    }
+
+}
