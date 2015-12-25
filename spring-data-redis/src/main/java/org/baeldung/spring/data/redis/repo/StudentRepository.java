31b5d9ff16dc3322a7f6a9f11ef7206d014fe2e1
sameira <sameira@gmail.com>
2015-12-25 23:54:54 +0530

    Adding Spring Data Redis Examples

---
+++ spring-data-redis/src/main/java/org/baeldung/spring/data/redis/repo/StudentRepository.java
@@ -0,0 +1,17 @@
+package org.baeldung.spring.data.redis.repo;
+
+import org.baeldung.spring.data.redis.model.Student;
+
+import java.util.Map;
+
+public interface StudentRepository {
+    void saveStudent(Student person);
+
+    void updateStudent(Student student);
+
+    Student findStudent(String id);
+
+    Map<Object, Object> findAllStudents();
+
+    void deleteStudent(String id);
+}
