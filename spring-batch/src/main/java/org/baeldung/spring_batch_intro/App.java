c8d9bd4f21cefac4dbeb33648871babf585d3f54
eugenp <hanriseldon@gmail.com>
2015-12-28 18:23:13 +0200

    spring batch work

--- spring-batch/src/main/java/org/baeldung/spring_batch_intro/App.java
+++ spring-batch/src/main/java/org/baeldung/spring_batch_intro/App.java
@@ -4,31 +4,27 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         // Spring Java config
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(SpringConfig.class);
         context.register(SpringBatchConfig.class);
         context.refresh();
 
         // Spring xml config
-        // ApplicationContext context = new ClassPathXmlApplicationContext(
-        // "spring-batch-intro.xml");
+        // ApplicationContext context = new ClassPathXmlApplicationContext("spring-batch.xml");
 
-        JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-        Job job = (Job) context.getBean("firstBatchJob");
+        final JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+        final Job job = (Job) context.getBean("firstBatchJob");
         System.out.println("Starting the batch job");
         try {
-            JobExecution execution = jobLauncher.run(job, new JobParameters());
+            final JobExecution execution = jobLauncher.run(job, new JobParameters());
             System.out.println("Job Status : " + execution.getStatus());
             System.out.println("Job succeeded");
-        } catch (Exception e) {
+        } catch (final Exception e) {
             e.printStackTrace();
             System.out.println("Job failed");
         }
