dfc96b2dd101a98d214e9b289f05b6a0cc885d25
amedviediev <amedvyedyev@gmail.com>
2016-02-02 20:15:51 +0200

    - Fixed tests

--- spring-rest/src/test/java/org/baeldung/web/controller/status/ExampleControllerTest.java
+++ spring-rest/src/test/java/org/baeldung/web/controller/status/ExampleControllerTest.java
@@ -1,13 +1,14 @@
-package com.baeldung;
+package org.baeldung.web.controller.status;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.baeldung.config.WebConfig;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
