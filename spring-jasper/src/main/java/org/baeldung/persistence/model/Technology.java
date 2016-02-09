b14575f367feb1728d4fe511380de382dc666ee5
vipin <dasvipin5585>
2016-02-09 23:50:28 +0530

    Jasper reports with spring project

---
+++ spring-jasper/src/main/java/org/baeldung/persistence/model/Technology.java
@@ -0,0 +1,14 @@
+package org.baeldung.persistence.model;
+
+public class Technology {
+
+	private String techonologyName;
+
+	public String getTechonologyName() {
+		return techonologyName;
+	}
+
+	public void setTechonologyName(String techonologyName) {
+		this.techonologyName = techonologyName;
+	}
+}
