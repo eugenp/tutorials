45358317b6b4f33ead9c4d923c3d7d3bf3e7c281
DOHA <dohae243@gmail.com>
2016-02-13 22:25:31 +0200

    OpenID Connect example

---
+++ spring-openid/src/main/java/org/baeldung/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package org.baeldung.config;
+
+import org.baeldung.security.OpenIdConnectFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private OAuth2RestTemplate restTemplate;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Bean
+    public OpenIdConnectFilter myFilter() {
+        final OpenIdConnectFilter filter = new OpenIdConnectFilter("/google-login");
+        filter.setRestTemplate(restTemplate);
+        return filter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+        .addFilterAfter(new OAuth2ClientContextFilter(), AbstractPreAuthenticatedProcessingFilter.class)
+        .addFilterAfter(myFilter(), OAuth2ClientContextFilter.class)
+        .httpBasic().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/google-login"))
+        .and()
+        .authorizeRequests()
+       // .antMatchers("/","/index*").permitAll()
+        .anyRequest().authenticated()
+        ;
+
+     // @formatter:on
+    }
+}
