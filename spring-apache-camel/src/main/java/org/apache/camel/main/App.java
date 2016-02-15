aedffc1d139ce5c54fec86d1e0d411ee09a8c590
ypatil <yrpatil07@gmail.com>
2016-02-15 15:29:13 +0530

    data folder moved under test/data. Test cases added. Code & POM updated.

--- spring-apache-camel/src/main/java/org/apache/camel/main/App.java
+++ spring-apache-camel/src/main/java/org/apache/camel/main/App.java
@@ -4,9 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(final String[] args) throws Exception {
-    	//Load application context
     	ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("camel-context.xml");
-        Thread.sleep(5000);
+        // Keep main thread alive for some time to let application finish processing the input files.
+    	Thread.sleep(5000);
         applicationContext.close();
     }
 }
--- spring-apache-camel/src/main/java/org/apache/camel/processor/FileProcessor.java
+++ spring-apache-camel/src/main/java/org/apache/camel/processor/FileProcessor.java
@@ -6,13 +6,8 @@ import org.apache.camel.Processor;
 public class FileProcessor implements Processor {
 
 	public void process(Exchange exchange) throws Exception {
-		//Read file content
-		String originalFileContent = (String)exchange.getIn().getBody(String.class);
-		
-		//Convert file content to upper case.
+		String originalFileContent = exchange.getIn().getBody(String.class);
 		String upperCaseFileContent = originalFileContent.toUpperCase();
-		
-		//Update file content.
 		exchange.getIn().setBody(upperCaseFileContent);
 	}
 
