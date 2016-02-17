eabccf2b8ff2225a135b5d7981623cd67c053bc3
ankur-singhal <getankur86@gmail.com>
2016-02-17 23:43:27 +0530

    Performance of GSON and Jackson Code Changes
    Performance of GSON and Jackson Code Changes

---
+++ gson-jackson-performance/src/main/java/org/baeldung/pojo/simple/CustomerPortfolioSimple.java
@@ -0,0 +1,16 @@
+package org.baeldung.pojo.simple;
+
+import java.util.List;
+
+public class CustomerPortfolioSimple {
+
+	private List<Customer> customerLists;
+
+	public List<Customer> getCustomerLists() {
+		return customerLists;
+	}
+
+	public void setCustomerLists(List<Customer> customerLists) {
+		this.customerLists = customerLists;
+	}
+}
