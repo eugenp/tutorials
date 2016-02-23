8c90c744834b84dd25ced0830236e96884f047c5
Dmitry Zinkevich <zinkevich.da@gmail.com>
2016-02-23 12:44:13 +0300

    Add test cases with different query types

--- spring-data-elasticsearch/src/main/java/com/baeldung/spring/data/es/model/Article.java
+++ spring-data-elasticsearch/src/main/java/com/baeldung/spring/data/es/model/Article.java
@@ -1,22 +1,34 @@
 package com.baeldung.spring.data.es.model;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldIndex;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.annotations.*;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.springframework.data.elasticsearch.annotations.FieldIndex.not_analyzed;
+import static org.springframework.data.elasticsearch.annotations.FieldType.Nested;
+import static org.springframework.data.elasticsearch.annotations.FieldType.String;
 
 @Document(indexName = "blog", type = "article")
 public class Article {
 
     @Id
     private String id;
-    @Field(type = FieldType.String, index = FieldIndex.not_analyzed)
+
+    @MultiField(
+        mainField = @Field(type = String),
+        otherFields = {
+            @NestedField(index = not_analyzed, dotSuffix = "verbatim", type = String)
+        }
+    )
     private String title;
-    @Field(type = FieldType.Nested)
+
+    @Field(type = Nested)
     private List<Author> authors;
+
+    @Field(type = String, index = not_analyzed)
+    private String[] tags;
 
     public Article() {
     }
@@ -49,12 +61,21 @@ public class Article {
         this.authors = authors;
     }
 
+    public String[] getTags() {
+        return tags;
+    }
+
+    public void setTags(String... tags) {
+        this.tags = tags;
+    }
+
     @Override
     public String toString() {
         return "Article{" +
                 "id='" + id + '\'' +
                 ", title='" + title + '\'' +
                 ", authors=" + authors +
+                ", tags=" + Arrays.toString(tags) +
                 '}';
     }
 }
