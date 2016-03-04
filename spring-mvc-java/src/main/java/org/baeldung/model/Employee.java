c5bc35b764ad285f892782c1966c4e3beba8a5c3
Roshan Thomas <roshanathomas@gmail.com>
2016-03-03 20:25:14 -0500

    Including review comments
    Separate config file , formatting and jdk 1.8 for spring-mvc-java
    project

--- spring-mvc-java/src/main/java/org/baeldung/model/Employee.java
+++ spring-mvc-java/src/main/java/org/baeldung/model/Employee.java
@@ -7,53 +7,52 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class Employee {
 
-	private long id;
+    private long id;
 
-	@NotNull
-	@Size(min = 3)
-	private String name;
-	@NotNull
-	@Size(min = 1)
-	private String contactNumber;
+    @NotNull
+    @Size(min = 3)
+    private String name;
+    @NotNull
+    @Size(min = 1)
+    private String contactNumber;
 
-	public Employee() {
-		super();
-	}
+    public Employee() {
+        super();
+    }
 
-	public Employee(final long id, final String name, final String contactNumber) {
-		this.id = id;
-		this.name = name;
-		this.contactNumber = contactNumber;
-	}
+    public Employee(final long id, final String name, final String contactNumber) {
+        this.id = id;
+        this.name = name;
+        this.contactNumber = contactNumber;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(final String name) {
-		this.name = name;
-	}
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(final long id) {
-		this.id = id;
-	}
+    public void setId(final long id) {
+        this.id = id;
+    }
 
-	public String getContactNumber() {
-		return contactNumber;
-	}
+    public String getContactNumber() {
+        return contactNumber;
+    }
 
-	public void setContactNumber(final String contactNumber) {
-		this.contactNumber = contactNumber;
-	}
+    public void setContactNumber(final String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", contactNumber="
-				+ contactNumber + "]";
-	}
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", contactNumber=" + contactNumber + "]";
+    }
 
 }
