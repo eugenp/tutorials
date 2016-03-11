594fb230b911f701a79bee1603c8974cf24f3633
RoshanThomas <roshanathomas@gmail.com>
2016-03-10 19:53:00 -0500

    Adding html mediaType

--- spring-mvc-java/src/main/java/org/baeldung/spring/web/config/ContentManagementWebConfig.java
+++ spring-mvc-java/src/main/java/org/baeldung/spring/web/config/ContentManagementWebConfig.java
@@ -30,8 +30,8 @@ public class ContentManagementWebConfig extends WebMvcConfigurerAdapter {
         useJaf(false).
         defaultContentType(MediaType.TEXT_HTML).
         mediaType("xml", MediaType.APPLICATION_XML).
-        mediaType("json",
-        MediaType.APPLICATION_JSON);
+        mediaType("html", MediaType.TEXT_HTML).
+        mediaType("json", MediaType.APPLICATION_JSON);
     }
 
     @Override
