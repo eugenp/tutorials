800285f30ce556f8b788ae4d0f5f9c55b8e9b486
Roshan Thomas <roshanathomas@gmail.com>
2016-03-11 22:54:45 -0500

    Content Negotiation Strategy

--- spring-mvc-xml/src/main/java/com/baeldung/spring/form/Employee.java
+++ spring-mvc-xml/src/main/java/com/baeldung/spring/form/Employee.java
@@ -2,46 +2,48 @@ package com.baeldung.spring.form;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 public class Employee {
 
-    private long id;
+	private long id;
 
-    @NotNull
-    @Size(min = 1)
-    private String name;
-    @NotNull
-    @Size(min = 1)
-    private String contactNumber;
+	@NotNull
+	@Size(min = 1)
+	private String name;
+	@NotNull
+	@Size(min = 1)
+	private String contactNumber;
 
-    public Employee() {
-        super();
-    }
+	public Employee() {
+		super();
+	}
 
-    //
+	//
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(final String name) {
-        this.name = name;
-    }
+	public void setName(final String name) {
+		this.name = name;
+	}
 
-    public long getId() {
-        return id;
-    }
+	public long getId() {
+		return id;
+	}
 
-    public void setId(final long id) {
-        this.id = id;
-    }
+	public void setId(final long id) {
+		this.id = id;
+	}
 
-    public String getContactNumber() {
-        return contactNumber;
-    }
+	public String getContactNumber() {
+		return contactNumber;
+	}
 
-    public void setContactNumber(final String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
+	public void setContactNumber(final String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
 
 }
