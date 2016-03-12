b9ddf89745a8f0692d70f2da746e91401a0b6acd
Roshan Thomas <roshanathomas@gmail.com>
2016-03-11 22:55:41 -0500

    Content Negotiation Strategy

--- spring-mvc-xml/src/main/java/com/baeldung/spring/controller/EmployeeController.java
+++ spring-mvc-xml/src/main/java/com/baeldung/spring/controller/EmployeeController.java
@@ -1,34 +1,56 @@
 package com.baeldung.spring.controller;
 
-import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
-import com.baeldung.spring.form.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.baeldung.spring.form.Employee;
 
 @Controller
 public class EmployeeController {
 
-    @RequestMapping(value = "/employee", method = RequestMethod.GET)
-    public ModelAndView showForm() {
-        return new ModelAndView("employeeHome", "employee", new Employee());
-    }
+	Map<Long, Employee> employeeMap = new HashMap<>();
 
-    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
-    public String submit(@Valid @ModelAttribute("employee") final Employee employee, final BindingResult result, final ModelMap model) {
-        if (result.hasErrors()) {
-            return "error";
-        }
+	@RequestMapping(value = "/employee", method = RequestMethod.GET)
+	public ModelAndView showForm() {
+		return new ModelAndView("employeeHome", "employee", new Employee());
+	}
 
-        model.addAttribute("name", employee.getName());
-        model.addAttribute("contactNumber", employee.getContactNumber());
-        model.addAttribute("id", employee.getId());
-        return "employeeView";
-    }
+	@RequestMapping(value = "/employee/{Id}", produces = { "application/json", "application/xml" }, method = RequestMethod.GET)
+	public @ResponseBody Employee getEmployeeById(@PathVariable final long Id) {
+		return employeeMap.get(Id);
+	}
+
+	@RequestMapping(value = "/employee/{Id}", method = RequestMethod.GET)
+	public String getEmployeeByIdHtmlView(@PathVariable final long Id, final ModelMap model) {
+		model.addAttribute("name", employeeMap.get(Id).getName());
+		model.addAttribute("contactNumber", employeeMap.get(Id).getContactNumber());
+		model.addAttribute("id", employeeMap.get(Id).getId());
+
+		return "employeeView";
+	}
+
+	@RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+	public String submit(@ModelAttribute("employee") final Employee employee, final BindingResult result, final ModelMap model) {
+		if (result.hasErrors()) {
+			return "error";
+		}
+		model.addAttribute("name", employee.getName());
+		model.addAttribute("contactNumber", employee.getContactNumber());
+		model.addAttribute("id", employee.getId());
+
+		employeeMap.put(employee.getId(), employee);
+
+		return "employeeView";
+	}
 
 }
