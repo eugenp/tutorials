c68da24b1e8e823444b72b82bfddf77861127ebd
Roshan Thomas <roshanathomas@gmail.com>
2016-03-23 10:45:09 -0400

    Removing MvcUriComponentsBuider

--- spring-mvc-java/src/main/java/org/baeldung/web/service/CustomerMockServiceImpl.java
+++ spring-mvc-java/src/main/java/org/baeldung/web/service/CustomerMockServiceImpl.java
@@ -10,26 +10,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class CustomerMockServiceImpl {
 
-    private HashMap<String, Customer> customerMap;
+	private HashMap<String, Customer> customerMap;
 
-    public CustomerMockServiceImpl() {
-        customerMap = new HashMap<String, Customer>();
+	public CustomerMockServiceImpl() {
+		customerMap = new HashMap<String, Customer>();
 
-        customerMap.put("10A", new Customer("10A", "Jane", "ABC Company"));
-        customerMap.put("20B", new Customer("20B", "Bob", "XYZ Company"));
-    }
+		customerMap.put("10A", new Customer("10A", "Jane", "ABC Company"));
+		customerMap.put("20B", new Customer("20B", "Bob", "XYZ Company"));
+	}
 
-    public Customer createCustomer(final Customer customer) {
-        customerMap.put(customer.getCustomerId(), customer);
-        return customer;
-    }
+	public List<Customer> allCustomers() {
+		return new ArrayList<Customer>(customerMap.values());
+	}
 
-    public List<Customer> allCustomers() {
-        return new ArrayList<Customer>(customerMap.values());
-    }
-
-    public Customer getCustomerDetail(final String id) {
-        return customerMap.get(id);
-    }
+	public Customer getCustomerDetail(final String id) {
+		return customerMap.get(id);
+	}
 
 }
