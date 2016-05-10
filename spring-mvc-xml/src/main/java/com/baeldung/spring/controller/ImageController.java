d2ca517fde67bd520ebb2428e08e61cf0d3863d6
Slavisa Baeldung <slavisa.baeldung@gmail.com>
2016-05-10 13:56:18 +0200

    image-download - removing unused import

--- spring-mvc-xml/src/main/java/com/baeldung/spring/controller/ImageController.java
+++ spring-mvc-xml/src/main/java/com/baeldung/spring/controller/ImageController.java
@@ -14,7 +14,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.TimeUnit;
 
 @Controller
 public class ImageController {
