64298e6ff83d5c66641483ad9c86c3a15b940c66
priya gachchinamat <priya.gachchinamat@ing.be>
2016-05-14 00:43:27 +0200

    Enum examples
    Examples demonstrating enum basics, EnumSet, EnumMap and some design
    patterns that can be implemented using enums

---
+++ core-java/src/main/java/com/baeldung/enums/PizzaDeliveryStrategy.java
@@ -0,0 +1,21 @@
+package com.baeldung.enums;
+
+/**
+ * Created by D-YW44CN on 2/05/2016.
+ */
+public enum PizzaDeliveryStrategy {
+    EXPRESS {
+        @Override
+        public void deliver(Pizza pz) {
+            System.out.println("Pizza will be delivered in express mode");
+        }
+    },
+    NORMAL {
+        @Override
+        public void deliver(Pizza pz) {
+            System.out.println("Pizza will be delivered in normal mode");
+        }
+    };
+
+    public abstract void deliver(Pizza pz);
+}
