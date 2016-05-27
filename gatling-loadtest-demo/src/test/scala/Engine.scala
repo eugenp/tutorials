ce7c3954175b84d5987835af0f39164fa04befff
Ivan Paolillo <ivan.paolillo@gmail.com>
2016-05-27 15:49:07 +0200

    A Gatling load test demo

---
+++ gatling-loadtest-demo/src/test/scala/Engine.scala
@@ -0,0 +1,13 @@
+import io.gatling.app.Gatling
+import io.gatling.core.config.GatlingPropertiesBuilder
+
+object Engine extends App {
+
+	val props = new GatlingPropertiesBuilder
+	props.dataDirectory(IDEPathHelper.dataDirectory.toString)
+	props.resultsDirectory(IDEPathHelper.resultsDirectory.toString)
+	props.bodiesDirectory(IDEPathHelper.bodiesDirectory.toString)
+	props.binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString)
+
+	Gatling.fromMap(props.build)
+}
---
+++ gatling-loadtest-demo/src/test/scala/IDEPathHelper.scala
@@ -0,0 +1,22 @@
+import java.nio.file.Path
+
+import io.gatling.commons.util.PathHelper._
+
+object IDEPathHelper {
+
+	val gatlingConfUrl: Path = getClass.getClassLoader.getResource("gatling.conf").toURI
+	val projectRootDir = gatlingConfUrl.ancestor(3)
+
+	val mavenSourcesDirectory = projectRootDir / "src" / "test" / "scala"
+	val mavenResourcesDirectory = projectRootDir / "src" / "test" / "resources"
+	val mavenTargetDirectory = projectRootDir / "target"
+	val mavenBinariesDirectory = mavenTargetDirectory / "test-classes"
+
+	val dataDirectory = mavenResourcesDirectory / "data"
+	val bodiesDirectory = mavenResourcesDirectory / "bodies"
+
+	val recorderOutputDirectory = mavenSourcesDirectory
+	val resultsDirectory = mavenTargetDirectory / "gatling"
+
+	val recorderConfigFile = mavenResourcesDirectory / "recorder.conf"
+}
