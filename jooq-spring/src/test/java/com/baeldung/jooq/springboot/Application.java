32ef5315e032a4fc7bea8b1c5b623664c43cd025
Grzegorz Piwowarek <gpw@touk.pl>
2016-06-08 08:24:42 +0200

    Remove unnecessary @PropertySource

--- jooq-spring/src/test/java/com/baeldung/jooq/springboot/Application.java
+++ jooq-spring/src/test/java/com/baeldung/jooq/springboot/Application.java
@@ -1,11 +1,9 @@
 package com.baeldung.jooq.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-@PropertySource("classpath:application.properties")
 public class Application {
 }
