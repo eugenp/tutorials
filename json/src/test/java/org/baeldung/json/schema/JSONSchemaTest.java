fd6848dddf9ada60011e33e361b3c29303f59035
Ivan Paolillo <ivan.paolillo@gmail.com>
2016-07-07 15:57:39 +0200

    Add JSON Schema validation

--- json/src/test/java/org/baeldung/json/schema/JSONSchemaTest.java
+++ json/src/test/java/org/baeldung/json/schema/JSONSchemaTest.java
@@ -11,10 +11,30 @@ import org.junit.Test;
 public class JSONSchemaTest {
 
     @Test
-    public void validateJSON() {
+    public void givenInvalidInput_whenValidating_thenInvalid() {
 
         JSONObject jsonSchema = new JSONObject(new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/schema.json")));
-        JSONObject jsonSubject = new JSONObject(new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/product.json")));
+        JSONObject jsonSubject = new JSONObject(new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/product_invalid.json")));
+
+        Schema schema = SchemaLoader.load(jsonSchema);
+
+        try {
+
+            schema.validate(jsonSubject);
+        }
+
+        catch (ValidationException e) {
+
+            System.out.println(e.getMessage());
+            e.getCausingExceptions().stream().map(ValidationException::getMessage).forEach(System.out::println);
+        }
+    }
+
+    @Test
+    public void givenValidInput_whenValidating_thenValid() {
+
+        JSONObject jsonSchema = new JSONObject(new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/schema.json")));
+        JSONObject jsonSubject = new JSONObject(new JSONTokener(JSONSchemaTest.class.getResourceAsStream("/product_valid.json")));
 
         Schema schema = SchemaLoader.load(jsonSchema);
 
