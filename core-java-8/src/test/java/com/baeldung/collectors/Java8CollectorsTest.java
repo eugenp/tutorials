e93772ae605aabfeb95efc86ad7c1ff465e8f433
Grzegorz Piwowarek <gpw@touk.pl>
2016-07-13 02:13:26 +0300

    TDD

---
+++ core-java-8/src/test/java/com/baeldung/collectors/Java8CollectorsTest.java
@@ -0,0 +1,76 @@
+package com.baeldung.collectors;
+
+import org.junit.Test;
+
+public class Java8CollectorsTest {
+
+    @Test
+    public void whenCollectingToList_shouldCollectToList() throws Exception {
+
+    }
+
+    @Test
+    public void whenCollectingToList_shouldCollectToSet() throws Exception {
+
+    }
+
+    @Test
+    public void whenCollectingToCollection_shouldCollectToCollection() throws Exception {
+
+    }
+
+    @Test
+    public void whenCollectingToImmutableCollection_shouldThrowException() throws Exception {
+
+    }
+
+    @Test
+    public void whenCollectingToMap_shouldCollectToMap() throws Exception {
+
+    }
+
+    @Test
+    public void whenCollectingAndThen_shouldCollect() throws Exception {
+
+    }
+
+    @Test
+    public void whenJoining_shouldJoin() throws Exception {
+
+    }
+
+    @Test
+    public void whenPartitioningBy_shouldPartition() throws Exception {
+
+    }
+
+    @Test
+    public void whenCounting_shouldCount() throws Exception {
+
+    }
+
+    @Test
+    public void whenSummarizing_shouldSummarize() throws Exception {
+
+    }
+
+    @Test
+    public void whenAveraging_shouldAverage() throws Exception {
+
+    }
+
+    @Test
+    public void whenSumming_shouldSum() throws Exception {
+
+    }
+
+    @Test
+    public void whenMaxingBy_shouldMaxBy() throws Exception {
+
+    }
+
+    @Test
+    public void whenCreatingCustomCollector_shouldCollect() throws Exception {
+
+    }
+}
