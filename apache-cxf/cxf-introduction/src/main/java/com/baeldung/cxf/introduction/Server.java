1c1495299ff272aa8eb1e55803dcf3f6eb45dda1
Thai Nguyen <namthainguyen@gmail.com>
2016-07-12 08:13:43 +0700

    Refactors code for Introduction to Apache CXF

--- apache-cxf/cxf-introduction/src/main/java/com/baeldung/cxf/introduction/Server.java
+++ apache-cxf/cxf-introduction/src/main/java/com/baeldung/cxf/introduction/Server.java
@@ -3,15 +3,11 @@ package com.baeldung.cxf.introduction;
 import javax.xml.ws.Endpoint;
 
 public class Server {
-    private Server() {
+    public static void main(String args[]) throws InterruptedException {
         BaeldungImpl implementor = new BaeldungImpl();
         String address = "http://localhost:8080/baeldung";
         Endpoint.publish(address, implementor);
-    }
-
-    public static void main(String args[]) throws InterruptedException {
-        new Server();
-        System.out.println("Server ready");
+        System.out.println("Server ready...");
         Thread.sleep(60 * 1000);        
         System.out.println("Server exiting");
         System.exit(0);
