060d1594aa33ddc4a7090fe563be5621d725473e
Grzegorz Piwowarek <gpw@touk.pl>
2016-07-12 20:58:01 +0300

    Formatting

--- assertj/src/test/java/com/baeldung/assertj/introduction/AssertJJava8Test.java
+++ assertj/src/test/java/com/baeldung/assertj/introduction/AssertJJava8Test.java
@@ -21,8 +21,8 @@ public class AssertJJava8Test {
         final Optional<String> givenOptional = Optional.of("something");
 
         assertThat(givenOptional)
-                .isPresent()
-                .hasValue("something");
+          .isPresent()
+          .hasValue("something");
     }
 
     @Test
@@ -30,10 +30,10 @@ public class AssertJJava8Test {
         final Predicate<String> predicate = s -> s.length() > 4;
 
         assertThat(predicate)
-                .accepts("aaaaa", "bbbbb")
-                .rejects("a", "b")
-                .acceptsAll(asList("aaaaa", "bbbbb"))
-                .rejectsAll(asList("a", "b"));
+          .accepts("aaaaa", "bbbbb")
+          .rejects("a", "b")
+          .acceptsAll(asList("aaaaa", "bbbbb"))
+          .rejectsAll(asList("a", "b"));
     }
 
     @Test
@@ -42,12 +42,12 @@ public class AssertJJava8Test {
         final LocalDate todayDate = LocalDate.now();
 
         assertThat(givenLocalDate)
-                .isBefore(LocalDate.of(2020, 7, 8))
-                .isAfterOrEqualTo(LocalDate.of(1989, 7, 8));
+          .isBefore(LocalDate.of(2020, 7, 8))
+          .isAfterOrEqualTo(LocalDate.of(1989, 7, 8));
 
         assertThat(todayDate)
-                .isAfter(LocalDate.of(1989, 7, 8))
-                .isToday();
+          .isAfter(LocalDate.of(1989, 7, 8))
+          .isToday();
     }
 
     @Test
@@ -55,7 +55,7 @@ public class AssertJJava8Test {
         final LocalDateTime givenLocalDate = LocalDateTime.of(2016, 7, 8, 12, 0);
 
         assertThat(givenLocalDate)
-                .isBefore(LocalDateTime.of(2020, 7, 8, 11, 2));
+          .isBefore(LocalDateTime.of(2020, 7, 8, 11, 2));
     }
 
     @Test
@@ -63,7 +63,7 @@ public class AssertJJava8Test {
         final LocalTime givenLocalTime = LocalTime.of(12, 15);
 
         assertThat(givenLocalTime)
-                .hasSameHourAs(LocalTime.of(12, 0));
+          .hasSameHourAs(LocalTime.of(12, 0));
     }
 
     @Test
@@ -71,8 +71,8 @@ public class AssertJJava8Test {
         final List<LocalDate> givenList = asList(ofYearDay(2016, 5), ofYearDay(2015, 6));
 
         assertThat(givenList)
-                .flatExtracting(LocalDate::getYear)
-                .contains(2015);
+          .flatExtracting(LocalDate::getYear)
+          .contains(2015);
     }
 
     @Test
@@ -80,8 +80,8 @@ public class AssertJJava8Test {
         final List<LocalDate> givenList = asList(ofYearDay(2016, 5), ofYearDay(2015, 6));
 
         assertThat(givenList)
-                .flatExtracting(LocalDate::getYear, LocalDate::getDayOfMonth)
-                .contains(2015, 6);
+          .flatExtracting(LocalDate::getYear, LocalDate::getDayOfMonth)
+          .contains(2015, 6);
     }
 
     @Test
@@ -89,10 +89,10 @@ public class AssertJJava8Test {
         final String givenString = "someString";
 
         assertThat(givenString)
-                .satisfies(s -> {
-                    assertThat(s).isNotEmpty();
-                    assertThat(s).hasSize(10);
-                });
+          .satisfies(s -> {
+              assertThat(s).isNotEmpty();
+              assertThat(s).hasSize(10);
+          });
     }
 
     @Test
@@ -100,7 +100,7 @@ public class AssertJJava8Test {
         final String givenString = "";
 
         assertThat(givenString)
-                .matches(String::isEmpty);
+          .matches(String::isEmpty);
     }
 
     @Test
@@ -108,6 +108,6 @@ public class AssertJJava8Test {
         final List<String> givenList = Arrays.asList("");
 
         assertThat(givenList)
-                .hasOnlyOneElementSatisfying(s -> assertThat(s).isEmpty());
+          .hasOnlyOneElementSatisfying(s -> assertThat(s).isEmpty());
     }
 }
