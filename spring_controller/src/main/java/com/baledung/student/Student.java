8f0b2146b88f4392f79dc5efbe2129a37ad08c36
prashant1067 <prashant1067@yahoo.co.in>
2016-07-14 01:56:07 +0530

    changing package name to baledung

---
+++ spring_controller/src/main/java/com/baledung/student/Student.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.baledung.student;
+
+public class Student
+{
+	private String name;
+	
+	private int id;
+
+//----------------------------- getters and setters--------------------------------------------------------
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+}
