e3c28f6dcda252a1dfd4e15cddd089c41b4cf83b
chernykhalexander <chernykh@gmail.com>
2016-07-16 01:38:48 +0300

    merge into spring-boot module

--- spring-boot/src/main/java/org/baeldung/Application.java
+++ spring-boot/src/main/java/org/baeldung/Application.java
@@ -2,9 +2,6 @@ package org.baeldung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Queue;
 
 @org.springframework.boot.autoconfigure.SpringBootApplication
 public class Application {
