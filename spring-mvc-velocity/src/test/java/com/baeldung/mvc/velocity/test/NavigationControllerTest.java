80cc50b850f757ac48d391a43cf6dd0f1bfb53f2
Raquel Garrido Crespo <raquela82@gmail.com>
2016-07-19 20:54:56 +0200

    Test get

--- spring-mvc-velocity/src/test/java/com/baeldung/mvc/velocity/test/NavigationControllerTest.java
+++ spring-mvc-velocity/src/test/java/com/baeldung/mvc/velocity/test/NavigationControllerTest.java
@@ -1,50 +1,32 @@
 package com.baeldung.mvc.velocity.test;
 
 
-import static org.hamcrest.Matchers.allOf;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.velocity.VelocityLayoutViewResolver;
 
-import com.baeldung.mvc.velocity.config.TestContext;
 import com.baeldung.mvc.velocity.controller.MainController;
 import com.baeldung.mvc.velocity.domain.Tutorial;
-import com.baeldung.mvc.velocity.service.ITutorialsService;
 import com.baeldung.mvc.velocity.service.TutorialsService;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(locations={"classpath:mvc-servlet.xml"})
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/mvc-servlet.xml"})
 public class NavigationControllerTest {
 
 	private MainController mainController = new MainController(Mockito.mock(TutorialsService.class));
@@ -81,7 +63,6 @@ public class NavigationControllerTest {
         assertEquals("index", view);
         assertEquals(tutorials, model.asMap().get("tutorials"));   
 	}
-
 
 	private static List<Tutorial> createTutorialList() {
 		return Arrays.asList(new Tutorial(1, "TestAuthor", "Test Title", "Test Description"));
