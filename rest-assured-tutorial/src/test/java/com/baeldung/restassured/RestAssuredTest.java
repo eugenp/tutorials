ec8bd5368aef5cb085e44ac1890f1898c12828a1
egimaben <egimaben@gmail.com>
2016-07-19 21:59:52 +0300

    fixed unit tests with wiremock

--- rest-assured-tutorial/src/test/java/com/baeldung/restassured/RestAssuredTest.java
+++ rest-assured-tutorial/src/test/java/com/baeldung/restassured/RestAssuredTest.java
@@ -1,61 +1,68 @@
 package com.baeldung.restassured;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.post;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.module.jsv.JsonSchemaValidatorSettings.settings;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.containsString;
-import io.restassured.module.jsv.JsonSchemaValidator;
-import static org.hamcrest.xml.HasXPath.hasXPath;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.github.fge.jsonschema.SchemaVersion;
 import com.github.fge.jsonschema.cfg.ValidationConfiguration;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 public class RestAssuredTest {
 
-	@Test
-	public void givenJsonResponse_whenKeyValuePairMatches_thenCorrect() {
-		JsonSchemaFactory factory = JsonSchemaFactory
-				.newBuilder()
-				.setValidationConfiguration(
-						ValidationConfiguration.newBuilder()
-						.setDefaultVersion(SchemaVersion.DRAFTV3)
-						.freeze()).freeze();
-		JsonSchemaValidator.settings = settings().with()
-				.jsonSchemaFactory(factory).and().with()
-				.checkedValidation(false);
+	private WireMockServer wireMockServer = new WireMockServer();
+	private static final String EVENTS_PATH = "/events?id=390";
+	private static final String APPLICATION_JSON = "application/json";
+	private static final String GAME_ODDS = getEventJson();
+
+	@Before
+	public void before() throws Exception {
+		System.out.println("Setting up!");
+		wireMockServer.start();
+		configureFor("localhost", 8080);
+		stubFor(get(urlEqualTo(EVENTS_PATH)).willReturn(
+				aResponse().withStatus(200)
+						.withHeader("Content-Type", APPLICATION_JSON)
+						.withBody(GAME_ODDS)));
 	}
 
 	@Test
-	public void givenJsonArrayOfSimilarObjects_whenHasGivenValuesForGivenKey_thenCorrect() {
-
+	public void givenUrl_whenCheckingFloatValuePasses_thenCorrect() {
+		get("/events?id=390").then().assertThat()
+				.body("odd.ck", equalTo(12.2f));
 	}
 
 	@Test
 	public void givenUrl_whenSuccessOnGetsResponse_andJsonHasRequiredKV_thenCorrect() {
 
 		get("/events?id=390").then().statusCode(200).assertThat()
-		.body("data.id", equalTo(390));
+				.body("id", equalTo("390"));
 
 	}
 
 	@Test
 	public void givenUrl_whenJsonResponseHasArrayWithGivenValuesUnderKey_thenCorrect() {
-
 		get("/events?id=390").then().assertThat()
-		.body("odds.price", hasItems("1.30", "5.25"));
-
+				.body("odds.price", hasItems(1.30f, 5.25f, 2.70f, 1.20f));
 	}
 
 	@Test
 	public void givenUrl_whenJsonResponseConformsToSchema_thenCorrect() {
+
 		get("/events?id=390").then().assertThat()
-		.body(matchesJsonSchemaInClasspath("event_0.json"));
+				.body(matchesJsonSchemaInClasspath("event_0.json"));
 	}
 
 	@Test
@@ -64,14 +71,14 @@ public class RestAssuredTest {
 				.newBuilder()
 				.setValidationConfiguration(
 						ValidationConfiguration.newBuilder()
-						.setDefaultVersion(SchemaVersion.DRAFTV4)
-						.freeze()).freeze();
+								.setDefaultVersion(SchemaVersion.DRAFTV4)
+								.freeze()).freeze();
 
 		get("/events?id=390")
-		.then()
-		.assertThat()
-		.body(matchesJsonSchemaInClasspath("event_0.json").using(
-				jsonSchemaFactory));
+				.then()
+				.assertThat()
+				.body(matchesJsonSchemaInClasspath("event_0.json").using(
+						jsonSchemaFactory));
 
 	}
 
@@ -79,73 +86,21 @@ public class RestAssuredTest {
 	public void givenUrl_whenValidatesResponseWithStaticSettings_thenCorrect() {
 
 		get("/events?id=390")
-		.then()
-		.assertThat()
-		.body(matchesJsonSchemaInClasspath("event_0.json").using(
-				settings().with().checkedValidation(false)));
-
+				.then()
+				.assertThat()
+				.body(matchesJsonSchemaInClasspath("event_0.json").using(
+						settings().with().checkedValidation(false)));
 	}
 
-	@Test
-	public void givenUrl_whenCheckingFloatValuePasses_thenCorrect() {
-		get("/odd").then().assertThat().body("odd.ck", equalTo(12.2f));
+	@After
+	public void after() throws Exception {
+		System.out.println("Running: tearDown");
+		wireMockServer.stop();
 	}
 
-	@Test
-	public void givenUrl_whenXmlResponseValueTestsEqual_thenCorrect() {
-		post("/employees").then().assertThat()
-		.body("employees.employee.first-name", equalTo("Jane"));
-	}
-
-	@Test
-	public void givenUrl_whenMultipleXmlValuesTestEqual_thenCorrect() {
-		post("/employees").then().assertThat()
-		.body("employees.employee.first-name", equalTo("Jane"))
-		.body("employees.employee.last-name", equalTo("Daisy"))
-		.body("employees.employee.sex", equalTo("f"));
-	}
-
-	@Test
-	public void givenUrl_whenMultipleXmlValuesTestEqualInShortHand_thenCorrect() {
-		post("/employees")
-		.then()
-		.assertThat()
-		.body("employees.employee.first-name", equalTo("Jane"),
-				"employees.employee.last-name", equalTo("Daisy"),
-				"employees.employee.sex", equalTo("f"));
-	}
-
-	@Test
-	public void givenUrl_whenValidatesXmlUsingXpath_thenCorrect() {
-		post("/employees")
-		.then()
-		.assertThat()
-		.body(hasXPath("/employees/employee/first-name",
-				containsString("Ja")));
-
-	}
-
-	@Test
-	public void givenUrl_whenValidatesXmlUsingXpath2_thenCorrect() {
-		post("/employees")
-		.then()
-		.assertThat()
-		.body(hasXPath("/employees/employee/first-name[text()='Jane']"));
-
-	}
-
-	@Test
-	public void givenUrl_whenVerifiesScienceTeacherFromXml_thenCorrect() {
-		get("/teachers")
-		.then()
-		.body("teachers.teacher.find { it.@department == 'science' }.subject",
-				hasItems("math", "physics"));
-	}
-
-	@Test
-	public void givenUrl_whenVerifiesOddPricesAccuratelyByStatus_thenCorrect() {
-		get("/odds").then().body("odds.findAll { it.status > 0 }.price",
-				hasItems(1.30f, 1.20f));
+	private static String getEventJson() {
+		return Util.inputStreamToString(new RestAssuredTest().getClass()
+				.getResourceAsStream("/event_0.json"));
 	}
 
 }
