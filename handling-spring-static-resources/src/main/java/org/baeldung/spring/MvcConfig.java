bc913386ff95145aa7559536ab115a5404835bba
emre <emre@exentriquesolutions.com>
2016-07-30 15:43:28 +0300

    Clean up the project & add static asset caching/versioning

--- handling-spring-static-resources/src/main/java/org/baeldung/spring/MvcConfig.java
+++ handling-spring-static-resources/src/main/java/org/baeldung/spring/MvcConfig.java
@@ -1,7 +1,5 @@
 package org.baeldung.spring;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -18,14 +17,18 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.resource.CssLinkResourceTransformer;
 import org.springframework.web.servlet.resource.GzipResourceResolver;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-import org.springframework.web.servlet.resource.ResourceUrlEncodingFilter;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 @Configuration
-@ComponentScan(basePackages = { "org.baeldung.web.controller", "org.baeldung.persistence.service", "org.baeldung.persistence.dao" })
+@ComponentScan(basePackages = { "org.baeldung.web.controller"})
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
     @Autowired
@@ -58,15 +61,40 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // For examples using Spring 4.1.0
         if ((env.getProperty("resource.handler.conf")).equals("4.1.0")) {
-            registry.addResourceHandler("/js/**").addResourceLocations("/js/").setCachePeriod(3600).resourceChain(true).addResolver(new GzipResourceResolver()).addResolver(new PathResourceResolver());
-            registry.addResourceHandler("/resources/**").addResourceLocations("/resources/", "classpath:/other-resources/").setCachePeriod(3600).resourceChain(true).addResolver(new PathResourceResolver());
-            registry.addResourceHandler("/files/**").addResourceLocations("file:/Users/Elena/").setCachePeriod(3600).resourceChain(true).addResolver(new PathResourceResolver());
-            registry.addResourceHandler("/other-files/**").addResourceLocations("file:/Users/Elena/").setCachePeriod(3600).resourceChain(true).addResolver(new GzipResourceResolver());
+            registry.addResourceHandler("/js/**")
+                    .addResourceLocations("/js/")
+                    .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                    .resourceChain(false)
+                    .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
+//                    .addResolver(new VersionResourceResolver().addFixedVersionStrategy("1.2", "/**"))
+                    .addResolver(new GzipResourceResolver())
+                    .addResolver(new PathResourceResolver());
+
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/resources/", "classpath:/other-resources/")
+                    .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                    .resourceChain(false)
+                    .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**"))
+                    .addTransformer(new CssLinkResourceTransformer());
+
+            registry.addResourceHandler("/files/**")
+                    .addResourceLocations("file:/Users/Elena/")
+                    .setCachePeriod(3600).resourceChain(true)
+                    .addResolver(new PathResourceResolver());
+
+            registry.addResourceHandler("/other-files/**")
+                    .addResourceLocations("file:/Users/Elena/")
+                    .setCachePeriod(3600)
+                    .resourceChain(true)
+                    .addResolver(new GzipResourceResolver());
         }
         // For examples using Spring 4.0.7
         else if ((env.getProperty("resource.handler.conf")).equals("4.0.7")) {
-            registry.addResourceHandler("/resources/**").addResourceLocations("/", "/resources/", "classpath:/other-resources/");
-            registry.addResourceHandler("/files/**").addResourceLocations("file:/Users/Elena/");
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/", "/resources/", "classpath:/other-resources/");
+
+            registry.addResourceHandler("/files/**")
+                    .addResourceLocations("file:/Users/Elena/");
 
         }
     }
@@ -76,13 +104,6 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         registry.addInterceptor(localeChangeInterceptor);
-    }
-
-    @Bean
-    public ResourceUrlEncodingFilter resourceUrlEncodingFilter() {
-        ResourceUrlEncodingFilter filter = new ResourceUrlEncodingFilter();
-
-        return filter;
     }
 
     @Bean
