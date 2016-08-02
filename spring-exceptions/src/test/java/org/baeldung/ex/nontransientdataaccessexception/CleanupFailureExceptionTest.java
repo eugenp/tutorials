7da64900af323dcffdabc16f9a99e5abce58dfa2
lcrusoveanu@optaros.com <lcrusoveanu@optaros.com>
2016-08-02 12:12:44 +0300

    fix cleanupfailure test

--- spring-exceptions/src/test/java/org/baeldung/ex/nontransientdataaccessexception/CleanupFailureExceptionTest.java
+++ spring-exceptions/src/test/java/org/baeldung/ex/nontransientdataaccessexception/CleanupFailureExceptionTest.java
@@ -8,15 +8,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.CleanupFailureDataAccessException;
-import org.springframework.dao.NonTransientDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { Cause1NonTransientConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class CleanupFailureExceptionTest {
 
+	private static final Logger LOG = Logger.getLogger(CleanupFailureExceptionTest.class.getName());
+	
     @Autowired
     private SessionFactory sessionFactory;
 
@@ -31,8 +35,8 @@ public class CleanupFailureExceptionTest {
         } finally {
             try {
                 sessionFactory.close();
-            } catch (final NonTransientDataAccessException exc) {
-                throw new CleanupFailureDataAccessException("Closing connection failed", exc.getCause());
+            } catch (final CleanupFailureDataAccessException exc) {
+                LOG.log(Level.SEVERE, exc.getMessage());
             }
         }
     }
