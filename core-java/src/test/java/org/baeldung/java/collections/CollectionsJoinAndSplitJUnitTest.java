5a305a25ae7b222a8954ce980c466d7701732081
Alex Theedom <alex.theedom@gmail.com>
2016-08-02 19:37:55 +0100

    Minor changes following review

--- core-java/src/test/java/org/baeldung/java/collections/CollectionsJoinAndSplitJUnitTest.java
+++ core-java/src/test/java/org/baeldung/java/collections/CollectionsJoinAndSplitJUnitTest.java
@@ -1,53 +1,42 @@
 package org.baeldung.java.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CollectionsJoinAndSplitJUnitTest {
 
-    public ArrayList<String> sauces = new ArrayList<String>();
-    public ArrayList<String> cheeses = new ArrayList<String>();
-    public ArrayList<String> vegetables = new ArrayList<String>();
-    
-    public ArrayList<ArrayList<String>> ingredients = new ArrayList<ArrayList<String>>();
+    private ArrayList<String> sauces = new ArrayList<>();
+    private ArrayList<String> cheeses = new ArrayList<>();
+    private ArrayList<String> vegetables = new ArrayList<>();
 
-    public CollectionsJoinAndSplitJUnitTest() throws Exception {
-        whenGeneratingTestData_ShouldSucceed();
-    }
+    private ArrayList<ArrayList<String>> ingredients = new ArrayList<>();
 
-    @Test
-    public void whenGeneratingTestData_ShouldSucceed() throws Exception {
-        sauces.clear();
+    @Before
+    public void init() {
         sauces.add("Olive Oil");
         sauces.add("Marinara");
-        
-        cheeses.clear();
-        cheeses.add("Mozarella");
+
+        cheeses.add("Mozzarella");
         cheeses.add("Feta");
         cheeses.add("Parmesan");
-        
-        vegetables.clear();
+
         vegetables.add("Olives");
         vegetables.add("Spinach");
         vegetables.add("Green Peppers");
-        
-        ingredients.clear();
+
         ingredients.add(sauces);
         ingredients.add(cheeses);
         ingredients.add(vegetables);
-
-        Assert.assertTrue(sauces.size() == 2);
-        Assert.assertTrue(cheeses.size() == 3);
-        Assert.assertTrue(vegetables.size() == 3);
-        Assert.assertTrue(ingredients.size() == 3);
     }
-        
+
     @Test
-    public void givenThreeArrayLists_whenJoiningIntoOneArrayList_ShouldSucceed() throws Exception {
-        ArrayList<ArrayList<String>> toppings = new ArrayList<ArrayList<String>>();
-        
+    public void givenThreeArrayLists_whenJoiningIntoOneArrayList_shouldSucceed() {
+        ArrayList<ArrayList<String>> toppings = new ArrayList<>();
+
         toppings.add(sauces);
         toppings.add(cheeses);
         toppings.add(vegetables);
@@ -57,18 +46,17 @@ public class CollectionsJoinAndSplitJUnitTest {
         Assert.assertTrue(toppings.contains(cheeses));
         Assert.assertTrue(toppings.contains(vegetables));
     }
-    
-   @Test
-   public void givenOneArrayList_whenSplittingIntoTwoArrayLists_shouldSucceed() throws Exception {
-       ArrayList<ArrayList<String>> toppings = ingredients;
-       ArrayList<ArrayList<String>> removedToppings = new ArrayList<ArrayList<String>>(); 
-       
-       removedToppings.add(toppings.remove(toppings.indexOf(vegetables)));
-       
-       Assert.assertTrue(removedToppings.contains(vegetables));
-       Assert.assertTrue(removedToppings.size() == 1);
-       Assert.assertTrue(toppings.size() == 2);
-       Assert.assertTrue(toppings.contains(sauces));
-       Assert.assertTrue(toppings.contains(cheeses));
-   }
+
+    @Test
+    public void givenOneArrayList_whenSplittingIntoTwoArrayLists_shouldSucceed() {
+
+        ArrayList<ArrayList<String>> removedToppings = new ArrayList<>();
+        removedToppings.add(ingredients.remove(ingredients.indexOf(vegetables)));
+
+        Assert.assertTrue(removedToppings.contains(vegetables));
+        Assert.assertTrue(removedToppings.size() == 1);
+        Assert.assertTrue(ingredients.size() == 2);
+        Assert.assertTrue(ingredients.contains(sauces));
+        Assert.assertTrue(ingredients.contains(cheeses));
+    }
 }
