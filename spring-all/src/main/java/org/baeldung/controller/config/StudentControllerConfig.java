46e50680e33d5c11d7381933baf49e522e23b79a
prashant1067 <prashant1067@yahoo.co.in>
2016-08-02 11:49:23 +0530

    Update StudentControllerConfig.java
    Making formatting changes

--- spring-all/src/main/java/org/baeldung/controller/config/StudentControllerConfig.java
+++ spring-all/src/main/java/org/baeldung/controller/config/StudentControllerConfig.java
@@ -11,27 +11,21 @@ import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class StudentControllerConfig implements WebApplicationInitializer {
+   	
+   @Override
+   public void onStartup(ServletContext sc) throws ServletException {
+	AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+	root.register(WebConfig.class);
 
+	root.setServletContext(sc);
 
-		@Override
-		public void onStartup(ServletContext sc) throws ServletException {
-			AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
-			root.register(WebConfig.class);
-			
-			root.refresh();
-			root.setServletContext(sc);
-			
-			// Manages the lifecycle of the root application context
-			sc.addListener(new ContextLoaderListener(root));
-			
-			
-			
-			DispatcherServlet dv =new DispatcherServlet(new GenericWebApplicationContext());
-			
-			ServletRegistration.Dynamic appServlet = sc.addServlet("test-mvc",dv ); 
-			appServlet.setLoadOnStartup(1); 
-			appServlet.addMapping("/test/*");
-		}
+	// Manages the lifecycle of the root application context
+	sc.addListener(new ContextLoaderListener(root));
 
+	DispatcherServlet dv = new DispatcherServlet(new GenericWebApplicationContext());
+
+	ServletRegistration.Dynamic appServlet = sc.addServlet("test-mvc", dv);
+	appServlet.setLoadOnStartup(1);
+	appServlet.addMapping("/test/*");
 	}
-
+}
