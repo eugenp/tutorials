d13794a0c1e3a5727b095f1347541bda63ce960d
Grzegorz Piwowarek <gpw@touk.pl>
2016-08-02 08:42:44 +0200

    Fix formatting in StudentControllerConfig

--- spring-all/src/main/java/org/baeldung/controller/config/StudentControllerConfig.java
+++ spring-all/src/main/java/org/baeldung/controller/config/StudentControllerConfig.java
@@ -1,37 +1,33 @@
 package org.baeldung.controller.config;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
-
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 public class StudentControllerConfig implements WebApplicationInitializer {
 
 
-		@Override
-		public void onStartup(ServletContext sc) throws ServletException {
-			AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
-			root.register(WebConfig.class);
-			
-			root.refresh();
-			root.setServletContext(sc);
-			
-			// Manages the lifecycle of the root application context
-			sc.addListener(new ContextLoaderListener(root));
-			
-			
-			
-			DispatcherServlet dv =new DispatcherServlet(new GenericWebApplicationContext());
-			
-			ServletRegistration.Dynamic appServlet = sc.addServlet("test-mvc",dv ); 
-			appServlet.setLoadOnStartup(1); 
-			appServlet.addMapping("/test/*");
-		}
+    @Override
+    public void onStartup(ServletContext sc) throws ServletException {
+        AnnotationConfigWebApplicationContext root = new AnnotationConfigWebApplicationContext();
+        root.register(WebConfig.class);
 
-	}
+        root.refresh();
+        root.setServletContext(sc);
+
+        sc.addListener(new ContextLoaderListener(root));
+
+        DispatcherServlet dv = new DispatcherServlet(new GenericWebApplicationContext());
+
+        ServletRegistration.Dynamic appServlet = sc.addServlet("test-mvc", dv);
+        appServlet.setLoadOnStartup(1);
+        appServlet.addMapping("/test/*");
+    }
+}
 
