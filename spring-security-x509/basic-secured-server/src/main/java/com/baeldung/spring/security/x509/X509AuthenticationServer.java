8dc92aa09ccc2f5882436867be55f0d1c7028e9b
Christian Rädel <ducks.2788@gmail.com>
2016-08-04 17:54:02 +0200

    Working demo to demonstrate keystore creation and SSL.

---
+++ spring-security-x509/basic-secured-server/src/main/java/com/baeldung/spring/security/x509/X509AuthenticationServer.java
@@ -0,0 +1,11 @@
+package com.baeldung.spring.security.x509;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X509AuthenticationServer {
+    public static void main(String[] args) {
+        SpringApplication.run(X509AuthenticationServer.class, args);
+    }
+}
