58b8a0c8e054aae5985894c3d7b0ced9ea0be671
Jeremy <jdbranham@hotmail.com>
2016-08-04 21:55:53 -0500

    Added for JIRA BAEL-167

---
+++ spring-boot-jar/src/main/java/org/baeldung/Application.java
@@ -0,0 +1,14 @@
+package org.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class Application {
+    @SuppressWarnings("unused")
+    private static ApplicationContext applicationContext;
+
+    public static void main(String[] args) {
+        applicationContext = SpringApplication.run(Application.class, args);
+    }
+}
