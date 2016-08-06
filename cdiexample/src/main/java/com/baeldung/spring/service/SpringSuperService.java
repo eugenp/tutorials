ffa78dc1758525d6856ae325c3b9b031be0f2a10
chernykhalexander <chernykh@gmail.com>
2016-08-06 22:59:54 +0300

    remove old module

--- cdiexample/src/main/java/com/baeldung/spring/service/SpringSuperService.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.spring.service;
-
-public class SpringSuperService {
-    public String getInfoFromService(String code){
-        System.out.println("Doing calculations");
-        return code;
-    }
-}
