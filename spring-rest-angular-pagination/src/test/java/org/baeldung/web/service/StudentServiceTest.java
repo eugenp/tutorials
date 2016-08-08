d273566d4ad493a9f4f81b005fe227526bf64fc8
Alex Theedom <alex.theedom@gmail.com>
2016-08-08 20:57:23 +0100

    Move project up one level

--- spring-rest-angular-pagination/src/test/java/org/baeldung/web/service/StudentServiceTest.java
+++ spring-rest-angular-pagination/src/test/java/org/baeldung/web/service/StudentServiceTest.java
@@ -1,30 +1,17 @@
 package org.baeldung.web.service;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.IsCollectionContaining.hasItems;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.util.Assert.isTrue;
-
 import org.apache.commons.lang3.RandomStringUtils;
 import org.baeldung.web.main.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.springframework.web.client.RestTemplate;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -32,7 +19,7 @@ import org.springframework.web.client.RestTemplate;
 @IntegrationTest("server.port:8888")
 public class StudentServiceTest {
 
-    private static final String ENDPOINT = "http://localhost:8080/StudentDirectory/student/get";
+    private static final String ENDPOINT = "http://localhost:8888/StudentDirectory/student/get";
 
     @Test
     public void givenRequestForStudents_whenPageIsOne_expectContainsNames() {
@@ -77,16 +64,17 @@ public class StudentServiceTest {
     }
 
     @Test
+    public void givenRequestForStudents_whenPageIsFive_expectFiveItems() {
+        given().params("page", "1", "size", "5").get(ENDPOINT)
+                .then()
+                .body("content.studentId.max()", equalTo("5"));
+    }
+
+    @Test
     public void givenResourcesExist_whenFirstPageIsRetrieved_thenPageContainsResources() {
         given().params("page", "1", "size", "2").get(ENDPOINT)
                 .then()
                 .assertThat().body("first", equalTo(true));
     }
 
-    @Test
-    public void givenRequestForStudents_whenPageIsFive_expectFiveItems() {
-        given().params("page", "1", "size", "5").get(ENDPOINT)
-                .then()
-                .body("content.studentId.max()", equalTo("5"));
-    }
 }
