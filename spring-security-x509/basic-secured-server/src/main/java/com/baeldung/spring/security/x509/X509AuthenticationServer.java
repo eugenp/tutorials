b6ef8de7002ebb7511af1274ea5fdb84a14ba3a8
Slavisa Baeldung <slavisa.baeldung@gmail.com>
2016-08-11 16:41:51 +0200

    BAEL-124 - Minor refactors

--- spring-security-x509/basic-secured-server/src/main/java/com/baeldung/spring/security/x509/X509AuthenticationServer.java
+++ spring-security-x509/basic-secured-server/src/main/java/com/baeldung/spring/security/x509/X509AuthenticationServer.java
@@ -15,29 +15,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class X509AuthenticationServer extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(X509AuthenticationServer.class, args);
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().authenticated()
-                .and()
-                .x509().subjectPrincipalRegex("CN=(.*?)(?:,|$)").userDetailsService(userDetailsService());
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                if (username.equals("cid")) {
-                    return new User(username, "", AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
-                }
-                throw new UsernameNotFoundException("User not found!");
-            }
-        };
     }
 }
