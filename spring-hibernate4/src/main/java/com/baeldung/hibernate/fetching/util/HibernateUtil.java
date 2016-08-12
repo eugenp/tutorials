414821e5baf744ac28c80e69636943800d1bcf91
Grzegorz Piwowarek <gpw@touk.pl>
2016-08-12 09:29:31 +0200

    Refactor Eager Loading vs Lazy Loading

--- spring-hibernate4/src/main/java/com/baeldung/hibernate/fetching/util/HibernateUtil.java
+++ spring-hibernate4/src/main/java/com/baeldung/hibernate/fetching/util/HibernateUtil.java
@@ -1,33 +1,30 @@
 package com.baeldung.hibernate.fetching.util;
 
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	private static SessionFactory factory;
 
-	@SuppressWarnings("deprecation")
-	public static Session getHibernateSession(String fetchMethod) {
-		//two config files are there
-		//one with lazy loading enabled 
-		//another lazy = false
-		SessionFactory sf = null;
-		if ("lazy".equals(fetchMethod)) {
-			sf = new Configuration().configure("fetchingLazy.cfg.xml").buildSessionFactory();
-		} else {
-			sf = new Configuration().configure("fetching.cfg.xml").buildSessionFactory();
-		}
-		// fetching.cfg.xml is used for this example
-		final Session session = sf.openSession();
-		return session;
-	}
+    @SuppressWarnings("deprecation")
+    public static Session getHibernateSession(String fetchMethod) {
+        //two config files are there
+        //one with lazy loading enabled
+        //another lazy = false
 
-	public static Session getHibernateSession() {
-		SessionFactory sf = null;
-		sf = new Configuration().configure("fetching.cfg.xml").buildSessionFactory();
-		final Session session = sf.openSession();
-		return session;
-	}
+        final String configFileName = "lazy".equals(fetchMethod) ?
+          "fetchingLazy.cfg.xml" :
+          "fetching.cfg.xml";
+
+        return new Configuration()
+          .configure(configFileName)
+          .buildSessionFactory().openSession();
+    }
+
+    public static Session getHibernateSession() {
+        return new Configuration()
+          .configure("fetching.cfg.xml")
+          .buildSessionFactory()
+          .openSession();
+    }
 
 }
