cd2ee40c061b50c118f1a26c713df4b853e6ee62
Grzegorz Piwowarek <gpw@touk.pl>
2016-08-13 13:20:45 +0200

    Use scope prototype

--- spring-all/src/main/java/org/baeldung/startup/InitializingBeanExampleBean.java
+++ spring-all/src/main/java/org/baeldung/startup/InitializingBeanExampleBean.java
@@ -2,7 +2,9 @@ package org.baeldung.startup;
 
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
+@Component
 @Scope(value = "prototype")
 public class InitializingBeanExampleBean implements InitializingBean {
 
--- spring-all/src/main/java/org/baeldung/startup/PostConstructExampleBean.java
+++ spring-all/src/main/java/org/baeldung/startup/PostConstructExampleBean.java
@@ -1,10 +1,12 @@
 package org.baeldung.startup;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(value = "prototype")
 public class PostConstructExampleBean {
 
     @Autowired
