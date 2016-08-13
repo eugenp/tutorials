2ea8328576e19da3662ae2b132bda5d51510888c
chernykhalexander <chernykh@gmail.com>
2016-08-13 18:58:09 +0300

    Fixed formatting

--- cdi/src/test/java/com/baeldung/test/TestSpringInterceptor.java
+++ cdi/src/test/java/com/baeldung/test/TestSpringInterceptor.java
@@ -1,25 +1,21 @@
 package com.baeldung.test;
 
-import com.baeldung.spring.configuration.AppConfig;
-import com.baeldung.spring.service.SpringSuperService;
+import static org.hamcrest.CoreMatchers.is;
+
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 
-import javax.inject.Inject;
-import java.util.List;
-
-import static org.hamcrest.CoreMatchers.is;
+import com.baeldung.spring.configuration.AppConfig;
+import com.baeldung.spring.service.SpringSuperService;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {AppConfig.class})
+@ContextConfiguration(classes = { AppConfig.class })
 public class TestSpringInterceptor {
     @Autowired
     SpringSuperService springSuperService;
@@ -28,11 +24,11 @@ public class TestSpringInterceptor {
     private List<String> accumulator;
 
     @Test
-    public void givenService_whenServiceAndAspectExecuted_thenOk(){
+    public void givenService_whenServiceAndAspectExecuted_thenOk() {
         String code = "123456";
         String result = springSuperService.getInfoFromService(code);
         Assert.assertThat(accumulator.size(), is(2));
-        Assert.assertThat(accumulator.get(0),is("Call to getInfoFromService"));
-        Assert.assertThat(accumulator.get(1),is("Method called successfully: getInfoFromService"));
+        Assert.assertThat(accumulator.get(0), is("Call to getInfoFromService"));
+        Assert.assertThat(accumulator.get(1), is("Method called successfully: getInfoFromService"));
     }
 }
