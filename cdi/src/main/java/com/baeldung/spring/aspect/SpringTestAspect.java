eb1230b865ea6d2a039beb927f68c09df8e7df80
chernykhalexander <chernykh@gmail.com>
2016-08-13 18:50:10 +0300

    Fixed formatting

--- cdi/src/main/java/com/baeldung/spring/aspect/SpringTestAspect.java
+++ cdi/src/main/java/com/baeldung/spring/aspect/SpringTestAspect.java
@@ -1,14 +1,11 @@
 package com.baeldung.spring.aspect;
 
-import org.aspectj.lang.JoinPoint;
+import java.util.List;
+
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import javax.inject.Inject;
-import java.util.List;
 
 @Aspect
 public class SpringTestAspect {
@@ -18,9 +15,9 @@ public class SpringTestAspect {
     @Around("execution(* com.baeldung.spring.service.SpringSuperService.*(..))")
     public Object auditMethod(ProceedingJoinPoint jp) throws Throwable {
         String methodName = jp.getSignature().getName();
-        accumulator.add("Call to "+methodName);
+        accumulator.add("Call to " + methodName);
         Object obj = jp.proceed();
-        accumulator.add("Method called successfully: "+methodName);
+        accumulator.add("Method called successfully: " + methodName);
         return obj;
     }
 }
