ac7407e4b3c2f059ffafc546c474be89c1eef28d
jesus-dayo <jesus_dayo@yahoo.com>
2016-08-15 08:12:08 +0800

    cleanup code

--- spring-rest-angular-pagination/src/main/java/org/baeldung/web/dao/StudentRepository.java
+++ spring-rest-angular-pagination/src/main/java/org/baeldung/web/dao/StudentRepository.java
@@ -2,9 +2,7 @@ package org.baeldung.web.dao;
 
 import org.baeldung.web.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
 }
