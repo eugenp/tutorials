fd32feeed30919973e01b9278ae609b7a7af94a5
eugenp <hanriseldon@gmail.com>
2016-08-22 12:35:23 +0300

    minor formatting cleanup

--- spring-security-rest-full/src/main/java/org/baeldung/web/controller/UserController.java
+++ spring-security-rest-full/src/main/java/org/baeldung/web/controller/UserController.java
@@ -40,94 +40,94 @@ import cz.jirutka.rsql.parser.ast.Node;
 @RequestMapping(value = "/auth/")
 public class UserController {
 
-	@Autowired
-	private IUserDAO service;
+    @Autowired
+    private IUserDAO service;
 
-	@Autowired
-	private UserRepository dao;
+    @Autowired
+    private UserRepository dao;
 
-	@Autowired
-	private MyUserRepository myUserRepository;
+    @Autowired
+    private MyUserRepository myUserRepository;
 
-	public UserController() {
-		super();
-	}
+    public UserController() {
+        super();
+    }
 
-	// API - READ
+    // API - READ
 
-	@RequestMapping(method = RequestMethod.GET, value = "/users")
-	@ResponseBody
-	public List<User> findAll(@RequestParam(value = "search", required = false) final String search) {
-		final List<SearchCriteria> params = new ArrayList<SearchCriteria>();
-		if (search != null) {
-			final Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
-			final Matcher matcher = pattern.matcher(search + ",");
-			while (matcher.find()) {
-				params.add(new SearchCriteria(matcher.group(1), matcher.group(2), matcher.group(3)));
-			}
-		}
-		return service.searchUser(params);
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/users")
+    @ResponseBody
+    public List<User> findAll(@RequestParam(value = "search", required = false) final String search) {
+        final List<SearchCriteria> params = new ArrayList<SearchCriteria>();
+        if (search != null) {
+            final Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+            final Matcher matcher = pattern.matcher(search + ",");
+            while (matcher.find()) {
+                params.add(new SearchCriteria(matcher.group(1), matcher.group(2), matcher.group(3)));
+            }
+        }
+        return service.searchUser(params);
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/users/spec")
-	@ResponseBody
-	public List<User> findAllBySpecification(@RequestParam(value = "search") final String search) {
-		final UserSpecificationsBuilder builder = new UserSpecificationsBuilder();
-		final String operationSetExper = Joiner.on("|").join(SearchOperation.SIMPLE_OPERATION_SET);
-		final Pattern pattern = Pattern.compile("(\\w+?)(" + operationSetExper + ")(\\p{Punct}?)(\\w+?)(\\p{Punct}?),");
-		final Matcher matcher = pattern.matcher(search + ",");
-		while (matcher.find()) {
-			builder.with(matcher.group(1), matcher.group(2), matcher.group(4), matcher.group(3), matcher.group(5));
-		}
+    @RequestMapping(method = RequestMethod.GET, value = "/users/spec")
+    @ResponseBody
+    public List<User> findAllBySpecification(@RequestParam(value = "search") final String search) {
+        final UserSpecificationsBuilder builder = new UserSpecificationsBuilder();
+        final String operationSetExper = Joiner.on("|").join(SearchOperation.SIMPLE_OPERATION_SET);
+        final Pattern pattern = Pattern.compile("(\\w+?)(" + operationSetExper + ")(\\p{Punct}?)(\\w+?)(\\p{Punct}?),");
+        final Matcher matcher = pattern.matcher(search + ",");
+        while (matcher.find()) {
+            builder.with(matcher.group(1), matcher.group(2), matcher.group(4), matcher.group(3), matcher.group(5));
+        }
 
-		final Specification<User> spec = builder.build();
-		return dao.findAll(spec);
-	}
+        final Specification<User> spec = builder.build();
+        return dao.findAll(spec);
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/myusers")
-	@ResponseBody
-	public Iterable<MyUser> findAllByQuerydsl(@RequestParam(value = "search") final String search) {
-		final MyUserPredicatesBuilder builder = new MyUserPredicatesBuilder();
-		if (search != null) {
-			final Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
-			final Matcher matcher = pattern.matcher(search + ",");
-			while (matcher.find()) {
-				builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
-			}
-		}
-		final BooleanExpression exp = builder.build();
-		return myUserRepository.findAll(exp);
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/myusers")
+    @ResponseBody
+    public Iterable<MyUser> findAllByQuerydsl(@RequestParam(value = "search") final String search) {
+        final MyUserPredicatesBuilder builder = new MyUserPredicatesBuilder();
+        if (search != null) {
+            final Pattern pattern = Pattern.compile("(\\w+?)(:|<|>)(\\w+?),");
+            final Matcher matcher = pattern.matcher(search + ",");
+            while (matcher.find()) {
+                builder.with(matcher.group(1), matcher.group(2), matcher.group(3));
+            }
+        }
+        final BooleanExpression exp = builder.build();
+        return myUserRepository.findAll(exp);
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/users/rsql")
-	@ResponseBody
-	public List<User> findAllByRsql(@RequestParam(value = "search") final String search) {
-		final Node rootNode = new RSQLParser().parse(search);
-		final Specification<User> spec = rootNode.accept(new CustomRsqlVisitor<User>());
-		return dao.findAll(spec);
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/users/rsql")
+    @ResponseBody
+    public List<User> findAllByRsql(@RequestParam(value = "search") final String search) {
+        final Node rootNode = new RSQLParser().parse(search);
+        final Specification<User> spec = rootNode.accept(new CustomRsqlVisitor<User>());
+        return dao.findAll(spec);
+    }
 
-	@RequestMapping(method = RequestMethod.GET, value = "/api/myusers")
-	@ResponseBody
-	public Iterable<MyUser> findAllByWebQuerydsl(@QuerydslPredicate(root = MyUser.class) final Predicate predicate) {
-		return myUserRepository.findAll(predicate);
-	}
+    @RequestMapping(method = RequestMethod.GET, value = "/api/myusers")
+    @ResponseBody
+    public Iterable<MyUser> findAllByWebQuerydsl(@QuerydslPredicate(root = MyUser.class) final Predicate predicate) {
+        return myUserRepository.findAll(predicate);
+    }
 
-	// API - WRITE
+    // API - WRITE
 
-	@RequestMapping(method = RequestMethod.POST, value = "/users")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void create(@RequestBody final User resource) {
-		Preconditions.checkNotNull(resource);
-		dao.save(resource);
-	}
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create(@RequestBody final User resource) {
+        Preconditions.checkNotNull(resource);
+        dao.save(resource);
+    }
 
-	@RequestMapping(method = RequestMethod.POST, value = "/myusers")
-	@ResponseStatus(HttpStatus.CREATED)
-	public void addMyUser(@RequestBody final MyUser resource) {
-		Preconditions.checkNotNull(resource);
-		myUserRepository.save(resource);
+    @RequestMapping(method = RequestMethod.POST, value = "/myusers")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addMyUser(@RequestBody final MyUser resource) {
+        Preconditions.checkNotNull(resource);
+        myUserRepository.save(resource);
 
-	}
+    }
 
 }
