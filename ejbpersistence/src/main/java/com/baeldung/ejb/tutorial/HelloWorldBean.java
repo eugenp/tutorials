6b4439991ec3bc70a83a33b30ce97724bed6b4e6
Pritam Banerjee <pritambanerjee999@gmail.com>
2016-08-23 01:56:26 -0700

    Project Structure

---
+++ ejbpersistence/src/main/java/com/baeldung/ejb/tutorial/HelloWorldBean.java
@@ -0,0 +1,19 @@
+package com.baeldung.ejb.tutorial;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldBean
+ */
+@Stateless
+public class HelloWorldBean implements HelloWorldBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldBean() {
+        // TODO Auto-generated constructor stub
+    	
+    }
+
+}
