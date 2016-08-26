f2ed42bcd22a803943410c1a9c7ffdfc0289d834
diego <diego.pardo.martin@gmail.com>
2016-08-26 04:07:09 +0200

    Added Web Initializer to Cafe Address application

---
+++ wicket-intro/CafeAddress/src/main/java/com/baeldung/wicket/examples/cafeaddress/ApplicationConfiguration.java
@@ -0,0 +1,10 @@
+package com.baeldung.wicket.examples.cafeaddress;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class ApplicationConfiguration {
+
+}
