4d2692536a6052366282cfc4000d4185147954a3
Christian Raedel <christian.raedel@gmail.com>
2016-08-27 01:19:53 +0200

    BAEL-168: Simplified EurekaClientApplication. Added docker-compose file for creating a test-scenario.

--- spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/EurekaClientApplication.java
+++ spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/EurekaClientApplication.java
@@ -1,31 +1,21 @@
 package com.baeldung.spring.cloud.eureka.client;
 
-import com.netflix.discovery.EurekaClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
 @RestController
-public class EurekaClientApplication implements GreetingController {
-    @Autowired
-    private EurekaClient eurekaClient;
-
-    @Value("${spring.application.name}")
-    private String appName;
-
+public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
 
-    @Override
+    @RequestMapping("/greeting")
     public String greeting() {
-        return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+        return "Hello from EurekaClient!";
     }
 }
--- spring-cloud-eureka/spring-cloud-eureka-client/src/main/java/com/baeldung/spring/cloud/eureka/client/GreetingController.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.spring.cloud.eureka.client;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public interface GreetingController {
-    @RequestMapping("/greeting")
-    String greeting();
-}
