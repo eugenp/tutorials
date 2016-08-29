d105c56c6a478452a5d521c1f93e64b5fd922136
Anil Bhaskar <UserEmailAddressbhskr.anil>
2016-08-29 20:08:47 +0530

    WebJars Source Code Added

---
+++ spring-boot/src/main/java/org/baeldung/webjars/WebjarsdemoApplication.java
@@ -0,0 +1,12 @@
+package org.baeldung.webjars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebjarsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebjarsdemoApplication.class, args);
+	}
+}
