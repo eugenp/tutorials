3f8aabce2c07c12b8799ca365de4ec5800a7cb2b
gaurirasa <ishwar.das@null.net>
2016-08-30 22:04:18 -0500

    Update CollectionsJoinAndSplitJUnitTest.java

--- core-java/src/test/java/org/baeldung/java/collections/CollectionsJoinAndSplitJUnitTest.java
+++ core-java/src/test/java/org/baeldung/java/collections/CollectionsJoinAndSplitJUnitTest.java
@@ -1,65 +1,111 @@
 package org.baeldung.java.collections;
 
 import java.util.Collection;
-import java.util.ArrayList;
 import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.collections4.CollectionUtils;
 
 public class CollectionsJoinAndSplitJUnitTest {
 
-    private Collection<String> sauces = new ArrayList<>();
-    private Collection<String> cheeses = new ArrayList<>();
-    private Collection<String> vegetables = new ArrayList<>();
+    private Collection<String> veggieToppings = Arrays.asList("Olive Oil",
+      "Alfredo", "Mozzarella", "Olives", "Spinach");
+    private Collection<String> supremeToppings = Arrays.asList("Olive Oil", "Alfredo",
+      "Mozzarella", "Mozzarella", "Feta", "Olives", "Spinach", "Green Peppers", "Artichoke Hearts");
+    private Collection<String> comboToppings = Arrays.asList("Olive Oil", "Alfredo", "Mozzarella",
+      "Mozzarella", "Feta", "Olives", "Spinach", "Green Peppers", "Artichoke Hearts");
 
-    private Collection<String> ingredients = new ArrayList<>();
+    private Collection<String> allVegetables = Arrays.asList("Olives", "Spinach",
+      "Green Peppers", "Artichoke Hearts");
 
-    @Before
-    public void init() {
-        sauces.add("Olive Oil");
-        sauces.add("Marinara");
+    @Test
+    public void givenThreeCollections_whenJoiningWithJavaAddAll_shouldSucceed() {
+        Collection<String> sauces = Arrays.asList("Olive Oil", "Alfredo");
+        Collection<String> cheeses = Arrays.asList("Mozzarella");
+        Collection<String> vegetables = Arrays.asList("Olives", "Spinach");
 
-        cheeses.add("Mozzarella");
-        cheeses.add("Feta");
-        cheeses.add("Parmesan");
+        Collection<String> veggiePizza = new ArrayList<>(sauces);
+        veggiePizza.addAll(cheeses);
+        veggiePizza.addAll(vegetables);
 
-        vegetables.add("Olives");
-        vegetables.add("Spinach");
-        vegetables.add("Green Peppers");
-
-        //ingredients.add(sauces);
-        //ingredients.add(cheeses);
-        //ingredients.add(vegetables);
+        Assert.assertArrayEquals(veggiePizza.toArray(), new String[] {"Olive Oil",
+          "Alfredo", "Mozzarella", "Olives", "Spinach"});
     }
 
     @Test
-    public void givenThreeCollections_whenJoiningIntoOneCollection_shouldSucceed() {
-        Collection<String> toppings = new ArrayList<>();
-
+    public void givenThreeCollections_whenJoiningWithJavaStream_shouldSucceed() {
+        Collection<String> sauces = Arrays.asList("Olive Oil", "Alfredo");
+        Collection<String> cheeses = Arrays.asList("Mozzarella", "Mozzarella", "Feta");
+        Collection<String> vegetables = Arrays.asList("Olives", "Spinach",
+          "Green Peppers", "Artichoke Hearts");
         
-
-        //toppings.add(sauces);
-        //toppings.add(cheeses);
-        //toppings.add(vegetables);
-
-        //Assert.assertTrue(toppings.size() == 3);
-        //Assert.assertTrue(toppings.contains(sauces));
-        //Assert.assertTrue(toppings.contains(cheeses));
-        //Assert.assertTrue(toppings.contains(vegetables));
+        Collection<String> supremePizza = Stream.of(sauces, cheeses, vegetables)
+          .flatMap(Collection::stream)
+          .collect(Collectors.toCollection(ArrayList::new));
+          
+        Assert.assertArrayEquals(supremePizza.toArray(), new String[] {"Olive Oil",
+          "Alfredo", "Mozzarella", "Mozzarella", "Feta", "Olives", "Spinach",
+          "Green Peppers", "Artichoke Hearts"});
     }
 
     @Test
-    public void givenOneArrayList_whenSplittingIntoTwoArrayLists_shouldSucceed() {
+    public void givenThreeCollections_whenMergingWithCommonsUnion_shouldSucceed() {
+        Collection<String> comboPizza = new ArrayList<>();
+        comboPizza = CollectionUtils.union(veggieToppings, supremeToppings);
+        assertEquals(new TreeSet(comboPizza), new TreeSet(Arrays.asList("Olive Oil",
+          "Alfredo", "Mozzarella", "Mozzarella", "Feta", "Olives", "Spinach",
+          "Green Peppers", "Artichoke Hearts")));
+    }
 
-        ArrayList<ArrayList<String>> removedToppings = new ArrayList<>();
-        removedToppings.add(ingredients.remove(ingredients.indexOf(vegetables)));
+    @Test
+    public void givenThreeCollections_whenMergingAsJavaTreeSets_shouldSucceed() {
+        TreeSet set = new TreeSet(veggieToppings);
+        set.addAll(supremeToppings);
+        Collection<String> comboPizza = new ArrayList<>(set);
+        assertEquals(new TreeSet(comboPizza), new TreeSet(Arrays.asList("Olive Oil",
+          "Alfredo", "Mozzarella", "Mozzarella", "Feta", "Olives", "Spinach",
+          "Green Peppers", "Artichoke Hearts")));
+    }
+    
+    @Test
+    public void givenOneCollection_whenSplittingWithCommonsSubtract_shouldSucceed() {
+        Collection<String> cheesePizza =
+          CollectionUtils.subtract(supremeToppings, allVegetables);
+        Collection<String> removedToppings =
+          CollectionUtils.subtract(supremeToppings, cheesePizza);
+        assertEquals(new TreeSet(cheesePizza), new TreeSet(Arrays.asList("Olive Oil",
+          "Alfredo", "Mozzarella", "Mozzarella", "Feta")));
+        assertEquals(new TreeSet(removedToppings), new TreeSet(Arrays.asList("Olives",
+          "Spinach", "Green Peppers", "Artichoke Hearts")));
+    }
 
-        Assert.assertTrue(removedToppings.contains(vegetables));
-        Assert.assertTrue(removedToppings.size() == 1);
-        Assert.assertTrue(ingredients.size() == 2);
-        Assert.assertTrue(ingredients.contains(sauces));
-        Assert.assertTrue(ingredients.contains(cheeses));
+    @Test
+    public void givenOneCollection_whenSplittingWithStreamFilter_shouldSucceed() {
+        Collection<String> noSauce = veggieToppings.stream()
+          .filter(veggieToppings -> !veggieToppings.contains("Alfredo"))
+          .collect(Collectors.toCollection(ArrayList::new));
+        assertEquals(new TreeSet(noSauce), new TreeSet(Arrays.asList("Olive Oil",
+          "Mozzarella", "Olives", "Spinach")));
+    }
+    
+    @Test
+    public void givenOneCollection_whenSplittingWithSubList_shouldSucceed() {
+        List lineOne = (new ArrayList(supremeToppings))
+          .subList(0, supremeToppings.size()/2);
+        List lineTwo = (new ArrayList(supremeToppings))
+          .subList(supremeToppings.size()/2, supremeToppings.size());
+        assertEquals(new TreeSet(lineOne), new TreeSet(Arrays.asList("Olive Oil",
+          "Alfredo", "Mozzarella", "Mozzarella")));
+        assertEquals(new TreeSet(lineTwo), new TreeSet(Arrays.asList("Feta",
+          "Olives", "Spinach", "Green Peppers", "Artichoke Hearts")));
     }
 }
