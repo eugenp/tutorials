0122de34f9837dff69759affb90943ceadf1dfdb
pttrung91 <pttrung91@gmail.com>
2016-09-08 21:50:45 +0700

    Update that following unit test naming given_

--- core-java/src/test/java/org/baeldung/java/JavaStringConversionUnitTest.java
+++ core-java/src/test/java/org/baeldung/java/JavaStringConversionUnitTest.java
@@ -20,105 +20,105 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaStringConversionUnitTest {
     @Test
-    public void whenConvertingStringToPrimitiveBoolean_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveBoolean_thenCorrect() {
         boolean primitiveBoolean = Boolean.parseBoolean("true");
 
         Assert.assertTrue(primitiveBoolean);
     }
 
     @Test
-    public void whenConvertingStringToWrapperBoolean_thenCorrect() {
+    public void givenString_whenConvertingToWrapperBoolean_thenCorrect() {
         Boolean wrapperBoolean = Boolean.valueOf("false");
 
         Assert.assertFalse(wrapperBoolean);
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveByte_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveByte_thenCorrect() {
         byte primitiveByte = Byte.parseByte("1");
 
         assertThat(primitiveByte == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToWrapperByte_thenCorrect() {
+    public void givenString_whenConvertingToWrapperByte_thenCorrect() {
         Byte wrapperByte = Byte.valueOf("1");
 
         assertThat(wrapperByte == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveShort_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveShort_thenCorrect() {
         short primitiveShort = Short.parseShort("1");
 
         assertThat(primitiveShort == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToWrapperShort_thenCorrect() {
+    public void givenString_whenConvertingToWrapperShort_thenCorrect() {
         Short wrapperShort = Short.valueOf("1");
 
         assertThat(wrapperShort == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveInt_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveInt_thenCorrect() {
         int primitiveInt = Integer.parseInt("1");
 
         assertThat(primitiveInt == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToWrapperInteger_thenCorrect() {
+    public void givenString_whenConvertingToWrapperInteger_thenCorrect() {
         Integer wrapperInt = Integer.valueOf("1");
 
         assertThat(wrapperInt == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveLong_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveLong_thenCorrect() {
         long primitiveLong = Long.parseLong("1");
 
         assertThat(primitiveLong == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToWrapperLong_thenCorrect() {
+    public void givenString_whenConvertingToWrapperLong_thenCorrect() {
         Long wrapperLong = Long.valueOf("1");
 
         assertThat(wrapperLong == 1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveFloat_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveFloat_thenCorrect() {
         float primitiveFloat = Float.parseFloat("1.1");
 
         assertThat(primitiveFloat == 1.1F).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToWrapperFloat_thenCorrect() {
+    public void givenString_whenConvertingToWrapperFloat_thenCorrect() {
         Float wrapperFloat = Float.valueOf("1.1");
 
         assertThat(wrapperFloat == 1.1F).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveDouble_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveDouble_thenCorrect() {
         double primitiveDouble = Double.parseDouble("1.1");
 
         assertThat(primitiveDouble == 1.1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToWrapperDouble_thenCorrect() {
+    public void givenString_whenConvertingToWrapperDouble_thenCorrect() {
         Double wrapperDouble = Double.valueOf("1.1");
 
         assertThat(wrapperDouble == 1.1).isTrue();
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveChar_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveChar_thenCorrect() {
         String primitiveChar = "C";
         char h = primitiveChar.charAt(0);
 
@@ -127,7 +127,7 @@ public class JavaStringConversionUnitTest {
 
 
     @Test
-    public void whenConvertingStringToPrimitiveCharArray_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveCharArray_thenCorrect() {
         String primitiveChars = "Hello world";
         char[] chars = primitiveChars.toCharArray();
 
@@ -135,7 +135,7 @@ public class JavaStringConversionUnitTest {
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveByteArray_default_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveByteArray_default_thenCorrect() {
         String primitiveBytes = "Hello world";
         byte[] bytes = primitiveBytes.getBytes();
 
@@ -143,7 +143,7 @@ public class JavaStringConversionUnitTest {
     }
 
     @Test
-    public void stringToPrimitiveByteArray_CharsetName_thenCorrect() throws UnsupportedEncodingException {
+    public void givenString_whenConvertingToPrimitiveByteArray_CharsetName_thenCorrect() throws UnsupportedEncodingException {
         String primitiveBytes = "Hello world";
         byte[] bytes = primitiveBytes.getBytes("UTF-8");
 
@@ -151,7 +151,7 @@ public class JavaStringConversionUnitTest {
     }
 
     @Test
-    public void whenConvertingStringToPrimitiveByteArray_Charset_thenCorrect() {
+    public void givenString_whenConvertingToPrimitiveByteArray_Charset_thenCorrect() {
         String primitiveBytes = "Hello world";
         byte[] bytes = primitiveBytes.getBytes(StandardCharsets.UTF_8);
 
@@ -159,7 +159,7 @@ public class JavaStringConversionUnitTest {
     }
 
     @Test
-    public void whenConvertingStringToDate_java7_thenCorrect() throws ParseException {
+    public void givenString_whenConvertingToDate_java7_thenCorrect() throws ParseException {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         String dateInString = "26/06/2016";
         Date date = formatter.parse(dateInString);
@@ -168,14 +168,14 @@ public class JavaStringConversionUnitTest {
     }
 
     @Test
-    public void whenConvertingStringToDate_java8_thenCorrect() {
+    public void givenString_whenConvertingToDate_java8_thenCorrect() {
         LocalDate localDate = LocalDate.parse("2016-06-26", DateTimeFormatter.ISO_LOCAL_DATE);
 
         assertThat(localDate.equals(LocalDate.of(2016,06,26))).isTrue();
     }
 
     @Test
-    public void whenConvertingDateToString_java7_thenCorrect() {
+    public void givenDate_whenConvertingToString_java7_thenCorrect() {
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
         String dateToString = formatter.format(Date.from(LocalDate.of(2016,06,26).atStartOfDay(ZoneId.systemDefault()).toInstant()));
 
@@ -183,7 +183,7 @@ public class JavaStringConversionUnitTest {
     }
 
     @Test
-    public void whenConvertingDateToString_java8_thenCorrect() {
+    public void givenDate_whenConvertingToString_java8_thenCorrect() {
         LocalDate localDate = LocalDate.of(2016,06,26);
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_LOCAL_DATE;
         String dateToString = dateTimeFormatter.format(localDate);
