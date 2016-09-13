e4e35f7002f1341276edc659be200b1f557eca44
nbosecker <nbosecker@gmail.com>
2016-09-12 20:18:51 -0700

    removed comments

--- eclipse/src/test/java/org/baeldung/equalshashcode/entities/PrimitiveClassTest.java
+++ eclipse/src/test/java/org/baeldung/equalshashcode/entities/PrimitiveClassTest.java
@@ -12,18 +12,10 @@ public class PrimitiveClassTest {
         PrimitiveClass bObject = new PrimitiveClass(false, 2);
         PrimitiveClass dObject = new PrimitiveClass(true, 2);
 
-<<<<<<< HEAD
         Assert.assertTrue(aObject.equals(bObject) && bObject.equals(aObject));
 
         Assert.assertTrue(aObject.hashCode() == bObject.hashCode());
 
-=======
-        // equals()
-        Assert.assertTrue(aObject.equals(bObject) && bObject.equals(aObject));
-        // hashCode()
-        Assert.assertTrue(aObject.hashCode() == bObject.hashCode());
-        // non-equal objects are not equals() and have different hashCode()
->>>>>>> 25a939665971866fadd0b1b78e09dd732d736193
         Assert.assertFalse(aObject.equals(dObject));
         Assert.assertFalse(aObject.hashCode() == dObject.hashCode());
     }
--- eclipse/src/test/java/org/baeldung/equalshashcode/entities/SquareClassTest.java
+++ eclipse/src/test/java/org/baeldung/equalshashcode/entities/SquareClassTest.java
@@ -15,18 +15,10 @@ public class SquareClassTest {
 
         Square dObject = new Square(20, Color.BLUE);
 
-<<<<<<< HEAD
         Assert.assertTrue(aObject.equals(bObject) && bObject.equals(aObject));
 
         Assert.assertTrue(aObject.hashCode() == bObject.hashCode());
 
-=======
-        // equals()
-        Assert.assertTrue(aObject.equals(bObject) && bObject.equals(aObject));
-        // hashCode()
-        Assert.assertTrue(aObject.hashCode() == bObject.hashCode());
-        // non-equal objects are not equals() and have different hashCode()
->>>>>>> 25a939665971866fadd0b1b78e09dd732d736193
         Assert.assertFalse(aObject.equals(dObject));
         Assert.assertFalse(aObject.hashCode() == dObject.hashCode());
     }
