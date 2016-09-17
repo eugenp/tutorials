143d9f63a849c1efe63e6e836431753a0da68bd6
prd <test@test.com>
2016-09-17 19:28:19 +0200

    random element from list guide

--- core-java-8/src/test/java/com/baeldung/RandomListElementTest.java
+++ core-java-8/src/test/java/com/baeldung/RandomListElementTest.java
@@ -3,15 +3,41 @@ package com.baeldung;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class RandomListElementTest {
 
     @Test
-    public void givenList_whenRandomNumberChosen_shouldReturnARandomElement() {
+    public void givenList_whenRandomNumberChosen_shouldReturnARandomElementUsingRandom() {
         List<Integer> givenList = Arrays.asList(1, 2, 3);
         Random rand = new Random();
         givenList.get(rand.nextInt(givenList.size()));
     }
+
+    @Test
+    public void givenList_whenRandomNumberChosen_shouldReturnARandomElementUsingMathRandom() {
+        List<Integer> givenList = Arrays.asList(1, 2, 3);
+        givenList.get((int)Math.random() * 3);
+    }
+
+    @Test
+    public void givenList_whenSeriesLengthChosen_shouldReturnRandomSeries() {
+        List<Integer> givenList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        int randomSeriesLength = 3;
+        Collections.shuffle(givenList);
+        givenList.subList(0, randomSeriesLength - 1);
+    }
+
+    @Test
+    public void givenList_whenSeriesLengthChosen_shouldReturnOrderedSeries() {
+        List<Integer> givenList = Arrays.asList(1, 2, 3, 4, 5, 6);
+        Random rand = new Random();
+        int startIndex = rand.nextInt(givenList.size());
+        int endIndex = startIndex + rand.nextInt(givenList.size() - startIndex);
+        Collections.sort(givenList);
+        givenList.subList(startIndex, endIndex);
+    }
+
 }
