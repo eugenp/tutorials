d9ae777b2e62e38eda6b04ecc414ebc3c57760f1
Kiran <kiransgonella@gmail.com>
2016-09-28 16:50:56 -0400

    Updated indentation

--- spring-mvc-tiles/src/main/java/com/baeldung/tiles/springmvc/ApplicationInitializer.java
+++ spring-mvc-tiles/src/main/java/com/baeldung/tiles/springmvc/ApplicationInitializer.java
@@ -4,19 +4,19 @@ import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatche
 
 public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-	@Override
-	protected Class<?>[] getRootConfigClasses() {
-		return new Class[] { ApplicationConfiguration.class };
-	}
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { ApplicationConfiguration.class };
+    }
 
-	@Override
-	protected Class<?>[] getServletConfigClasses() {
-		return null;
-	}
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
 
-	@Override
-	protected String[] getServletMappings() {
-		return new String[] { "/" };
-	}
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
 
 }
