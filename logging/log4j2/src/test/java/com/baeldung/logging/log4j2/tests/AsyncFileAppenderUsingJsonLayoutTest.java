a929225e1345ba4563240b4efb90cb934510d35a
Christian Rädel <christian.raedel@gmail.com>
2016-10-02 20:50:41 +0200

    BAEL-41: Added including tests: - rolling file appender using XML layout - async file appender using JSON layout

--- logging/log4j2/src/test/java/com/baeldung/logging/log4j2/tests/AsyncFileAppenderUsingJsonLayoutTest.java
+++ logging/log4j2/src/test/java/com/baeldung/logging/log4j2/tests/AsyncFileAppenderUsingJsonLayoutTest.java
@@ -1,19 +1,14 @@
 package com.baeldung.logging.log4j2.tests;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LogEvent;
 import org.apache.logging.log4j.junit.LoggerContextRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertTrue;
 
@@ -21,7 +16,7 @@ import static org.junit.Assert.assertTrue;
 public class AsyncFileAppenderUsingJsonLayoutTest {
 
     @Rule
-    public LoggerContextRule contextRule = new LoggerContextRule("log4j2-async-file-appender_json-layout_colored.xml");
+    public LoggerContextRule contextRule = new LoggerContextRule("log4j2-async-file-appender_json-layout.xml");
 
     @Test
     public void givenLoggerWithAsyncConfig_shouldLogToJsonFile() throws Exception {
@@ -30,16 +25,7 @@ public class AsyncFileAppenderUsingJsonLayoutTest {
         for (int i = 0; i < count; i++) {
             logger.info("This is async JSON message #{} at INFO level.", count);
         }
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<LogEvent> logEvents = Files.readAllLines(Paths.get("target/logfile.json")).stream()
-          .map(s -> {
-              try {
-                  return objectMapper.readValue(s.getBytes(), LogEvent.class);
-              } catch (IOException e) {
-                  throw new RuntimeException("Failed to import LogEvent!", e);
-              }
-          })
-          .collect(Collectors.toList());
-        assertTrue(logEvents.size() <= count);
+        long logEventsCount = Files.lines(Paths.get("target/logfile.json")).count();
+        assertTrue(logEventsCount == count);
     }
 }
