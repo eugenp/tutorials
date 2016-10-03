b04ede30f5da9f7adf9d8fbe7483eb1c21108250
Christian Rädel <christian.raedel@gmail.com>
2016-10-03 22:28:16 +0200

    BAEL-41: Added including tests: - marker filter

--- logging/log4j2/src/test/java/com/baeldung/logging/log4j2/tests/ConsoleAppenderUsingPatternLayoutWithColorsTest.java
+++ logging/log4j2/src/test/java/com/baeldung/logging/log4j2/tests/ConsoleAppenderUsingPatternLayoutWithColorsTest.java
@@ -1,6 +1,8 @@
 package com.baeldung.logging.log4j2.tests;
 
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.apache.logging.log4j.junit.LoggerContextRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -16,12 +18,21 @@ public class ConsoleAppenderUsingPatternLayoutWithColorsTest {
     @Test
     public void givenLoggerWithConsoleConfig_shouldLogToConsoleInColors() throws Exception {
         Logger logger = contextRule.getLogger(getClass().getSimpleName());
-        logger.trace("This is a colored message at TRACE level. This is the minimum visible level.");
-        logger.debug("This is a colored message at DEBUG level.");
+        logger.trace("This is a colored message at TRACE level.");
+        logger.debug("This is a colored message at DEBUG level. This is the minimum visible level.");
         logger.info("This is a colored message at INFO level.");
         logger.warn("This is a colored message at WARN level.");
         Exception e = new RuntimeException("This is only a test!");
         logger.error("This is a colored message at ERROR level.", e);
         logger.fatal("This is a colored message at FATAL level.");
+    }
+
+    @Test
+    public void givenLoggerWithConsoleConfig_shouldFilterByMarker() throws Exception {
+        Logger logger = contextRule.getLogger("ConnTrace");
+        Marker appError = MarkerManager.getMarker("APP_ERROR");
+        logger.error(appError, "This marker message at ERROR level should be hidden.");
+        Marker connectionTrace = MarkerManager.getMarker("CONN_TRACE");
+        logger.trace(connectionTrace, "This is a marker message at TRACE level.");
     }
 }
