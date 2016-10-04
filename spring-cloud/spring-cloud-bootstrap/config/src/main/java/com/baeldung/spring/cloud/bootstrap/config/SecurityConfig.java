95b967858ed5e43dd9c109ad73206032b2026903
Tim Schimandle <tschiman@gmail.com>
2016-10-03 23:15:54 -0600

    BAEL-315 reformatting some code.

--- spring-cloud/spring-cloud-bootstrap/config/src/main/java/com/baeldung/spring/cloud/bootstrap/config/SecurityConfig.java
+++ spring-cloud/spring-cloud-bootstrap/config/src/main/java/com/baeldung/spring/cloud/bootstrap/config/SecurityConfig.java
@@ -31,9 +31,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                    .anyRequest().authenticated()
                 .and()
-                .httpBasic().and()
-                .csrf().disable();
+                    .httpBasic()
+                .and()
+                    .csrf().disable();
     }
 }
