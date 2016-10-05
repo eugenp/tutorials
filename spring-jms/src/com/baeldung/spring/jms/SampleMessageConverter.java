ee9f0336b54030124a6937d828755a609dff9314
Kiran <kiransgonella@gmail.com>
2016-10-05 00:14:21 -0400

    Updated indentation

--- spring-jms/src/com/baeldung/spring/jms/SampleMessageConverter.java
+++ spring-jms/src/com/baeldung/spring/jms/SampleMessageConverter.java
@@ -8,22 +8,20 @@ import javax.jms.Session;
 import org.springframework.jms.support.converter.MessageConversionException;
 import org.springframework.jms.support.converter.MessageConverter;
 
-public class SampleMessageConverter implements MessageConverter{
+public class SampleMessageConverter implements MessageConverter {
 
-        public Message toMessage(Object object, Session session)
-                        throws JMSException, MessageConversionException {               
-                Employee person = (Employee) object;
-                MapMessage message = session.createMapMessage();
-                message.setString("name", person.getName());
-                message.setInt("age", person.getAge());
-                return message;
-        }
+    public Message toMessage(Object object, Session session) throws JMSException, MessageConversionException {
+        Employee person = (Employee) object;
+        MapMessage message = session.createMapMessage();
+        message.setString("name", person.getName());
+        message.setInt("age", person.getAge());
+        return message;
+    }
 
-        public Object fromMessage(Message message) throws JMSException,
-                        MessageConversionException {
-                MapMessage mapMessage = (MapMessage) message;
-                Employee person = new Employee(mapMessage.getString("name"), mapMessage.getInt("age"));
-                return person;
-        }
+    public Object fromMessage(Message message) throws JMSException, MessageConversionException {
+        MapMessage mapMessage = (MapMessage) message;
+        Employee person = new Employee(mapMessage.getString("name"), mapMessage.getInt("age"));
+        return person;
+    }
 
 }
