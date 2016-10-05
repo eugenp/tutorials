a310184efa1af80857929b5a23aa0fb5adfa364f
Tim Schimandle <tschiman@gmail.com>
2016-10-05 07:18:23 -0600

    BAEL-315 change access in the config server to only users with SYSTEM level access.

--- spring-cloud/spring-cloud-bootstrap/config/src/main/java/com/baeldung/spring/cloud/bootstrap/config/SecurityConfig.java
+++ spring-cloud/spring-cloud-bootstrap/config/src/main/java/com/baeldung/spring/cloud/bootstrap/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .anyRequest().authenticated()
+                    .anyRequest().hasRole("SYSTEM")
                 .and()
                     .httpBasic()
                 .and()
