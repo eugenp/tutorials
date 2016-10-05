03dbd93be250f1bc2bdb9dd8709828a1ae9adef1
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2016-10-05 22:35:11 +0300

    custom user repository and test

--- spring-security-custom-permission/src/main/java/org/baeldung/persistence/dao/UserRepository.java
+++ spring-security-custom-permission/src/main/java/org/baeldung/persistence/dao/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(final String username);
+    
+    void removeUserByUsername(String username);
 
 }
---
+++ spring-security-custom-permission/src/test/java/org/baeldung/web/CustomUserDetailsServiceTest.java
@@ -0,0 +1,83 @@
+package org.baeldung.web;
+
+import org.baeldung.config.MvcConfig;
+import org.baeldung.config.PersistenceConfig;
+import org.baeldung.config.SecurityConfig;
+import org.baeldung.persistence.dao.UserRepository;
+import org.baeldung.persistence.model.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import static org.junit.Assert.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = { MvcConfig.class, SecurityConfig.class, PersistenceConfig.class })
+@WebAppConfiguration
+public class CustomUserDetailsServiceTest {
+
+    private static final Logger LOG = Logger.getLogger("CustomUserDetailsServiceTest");
+
+    public static final String USERNAME = "user";
+    public static final String PASSWORD = "pass";
+    public static final String USERNAME2 = "user2";
+
+    @Autowired
+    UserRepository myUserRepository;
+
+    @Autowired
+    AuthenticationProvider authenticationProvider;
+
+    @Test
+    public void givenExistingUser_whenAuthenticate_thenRetrieveFromDb() {
+        try {
+            User user = new User();
+            user.setUsername(USERNAME);
+            user.setPassword(PASSWORD);
+
+            myUserRepository.save(user);
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(USERNAME, PASSWORD);
+            Authentication authentication = authenticationProvider.authenticate(auth);
+
+            assertEquals(authentication.getName(), USERNAME);
+
+        } catch (Exception exc) {
+            LOG.log(Level.SEVERE, "Error creating account");
+        } finally {
+            myUserRepository.removeUserByUsername(USERNAME);
+        }
+    }
+    
+    @Test (expected = BadCredentialsException.class)
+    public void givenIncorrectUser_whenAuthenticate_thenBadCredentialsException() {
+        try {
+            User user = new User();
+            user.setUsername(USERNAME);
+            user.setPassword(PASSWORD);
+
+            try {
+                myUserRepository.save(user);
+            }
+            catch (Exception exc) {
+                LOG.log(Level.SEVERE, "Error creating account");
+            } 
+            
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(USERNAME2, PASSWORD);
+            Authentication authentication = authenticationProvider.authenticate(auth);
+        }  
+        finally {
+            myUserRepository.removeUserByUsername(USERNAME);
+        }
+    }
+
+}
