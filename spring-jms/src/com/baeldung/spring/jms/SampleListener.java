53a353916b1bf91653c8df6907e630b3751582e1
Kiran <kiransgonella@gmail.com>
2016-10-09 13:55:01 -0400

    Updated indentation and refactored code
    Added a a new method receiveMessage() to receive custom converted messages

--- spring-jms/src/com/baeldung/spring/jms/SampleListener.java
+++ spring-jms/src/com/baeldung/spring/jms/SampleListener.java
@@ -1,23 +1,37 @@
 package com.baeldung.spring.jms;
 
+import java.util.Map;
+
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
+import org.springframework.jms.core.JmsTemplate;
+
 public class SampleListener implements MessageListener {
 
-    private String textMessage;
+    public JmsTemplate getJmsTemplate() {
+        return getJmsTemplate();
+    }
 
     public void onMessage(Message message) {
         if (message instanceof TextMessage) {
             try {
-                textMessage = ((TextMessage) message).getText();
+
+                String msg = ((TextMessage) message).getText();
+                System.out.println("Message has been consumed : " + msg);
             } catch (JMSException ex) {
                 throw new RuntimeException(ex);
             }
         } else {
             throw new IllegalArgumentException("Message Error");
         }
+    }
+
+    public Employee receiveMessage() throws JMSException {
+        Map map = (Map) getJmsTemplate().receiveAndConvert();
+        Employee employee = new Employee((String) map.get("name"), (Integer) map.get("age"));
+        return employee;
     }
 }
