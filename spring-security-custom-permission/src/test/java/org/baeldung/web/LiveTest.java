96bd33715a3178ddd56aeb8a69923d513360278a
DOHA <dohae243@gmail.com>
2016-10-12 19:58:56 +0200

    modify live test configuration

--- spring-security-custom-permission/src/test/java/org/baeldung/web/LiveTest.java
+++ spring-security-custom-permission/src/test/java/org/baeldung/web/LiveTest.java
@@ -3,45 +3,35 @@ package org.baeldung.web;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.baeldung.Application;
 import org.baeldung.persistence.model.Foo;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.authentication.FormAuthConfig;
 import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
-@IntegrationTest("server.port:8082")
 public class LiveTest {
 
-    private final FormAuthConfig formAuthConfig = new FormAuthConfig("http://localhost:8082/login", "username", "password");
+    private final FormAuthConfig formAuthConfig = new FormAuthConfig("http://localhost:8082/spring-security-custom-permission/login", "username", "password");
 
     @Test
     public void givenUserWithReadPrivilegeAndHasPermission_whenGetFooById_thenOK() {
-        final Response response = givenAuth("john", "123").get("http://localhost:8082/foos/1");
+        final Response response = givenAuth("john", "123").get("http://localhost:8082/spring-security-custom-permission/foos/1");
         assertEquals(200, response.getStatusCode());
         assertTrue(response.asString().contains("id"));
     }
 
     @Test
     public void givenUserWithNoWritePrivilegeAndHasPermission_whenPostFoo_thenForbidden() {
-        final Response response = givenAuth("john", "123").contentType(MediaType.APPLICATION_JSON_VALUE).body(new Foo("sample")).post("http://localhost:8082/foos");
+        final Response response = givenAuth("john", "123").contentType(MediaType.APPLICATION_JSON_VALUE).body(new Foo("sample")).post("http://localhost:8082/spring-security-custom-permission/foos");
         assertEquals(403, response.getStatusCode());
     }
 
     @Test
     public void givenUserWithWritePrivilegeAndHasPermission_whenPostFoo_thenOk() {
-        final Response response = givenAuth("tom", "111").contentType(MediaType.APPLICATION_JSON_VALUE).body(new Foo("sample")).post("http://localhost:8082/foos");
+        final Response response = givenAuth("tom", "111").contentType(MediaType.APPLICATION_JSON_VALUE).body(new Foo("sample")).post("http://localhost:8082/spring-security-custom-permission/foos");
         assertEquals(201, response.getStatusCode());
         assertTrue(response.asString().contains("id"));
     }
@@ -50,14 +40,14 @@ public class LiveTest {
 
     @Test
     public void givenUserMemberInOrganization_whenGetOrganization_thenOK() {
-        final Response response = givenAuth("john", "123").get("http://localhost:8082/organizations/1");
+        final Response response = givenAuth("john", "123").get("http://localhost:8082/spring-security-custom-permission/organizations/1");
         assertEquals(200, response.getStatusCode());
         assertTrue(response.asString().contains("id"));
     }
 
     @Test
     public void givenUserMemberNotInOrganization_whenGetOrganization_thenForbidden() {
-        final Response response = givenAuth("john", "123").get("http://localhost:8082/organizations/2");
+        final Response response = givenAuth("john", "123").get("http://localhost:8082/spring-security-custom-permission/organizations/2");
         assertEquals(403, response.getStatusCode());
     }
 
@@ -65,7 +55,7 @@ public class LiveTest {
 
     @Test
     public void givenDisabledSecurityExpression_whenGetFooByName_thenError() {
-        final Response response = givenAuth("john", "123").get("http://localhost:8082/foos?name=sample");
+        final Response response = givenAuth("john", "123").get("http://localhost:8082/spring-security-custom-permission/foos?name=sample");
         assertEquals(500, response.getStatusCode());
         assertTrue(response.asString().contains("method hasAuthority() not allowed"));
     }
