1e9d1a75663a4c02bbdab5aeebd85345bb6d1ef6
DOHA <dohae243@gmail.com>
2016-10-17 13:30:13 +0200

    add integration test profile

--- spring-security-x509/spring-security-x509-basic-auth/src/test/java/com/baeldung/spring/security/x509/X509AuthenticationServerIntegrationTest.java
+++ spring-security-x509/spring-security-x509-basic-auth/src/test/java/com/baeldung/spring/security/x509/X509AuthenticationServerIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class X509AuthenticationServerTests {
+public class X509AuthenticationServerIntegrationTest {
     @Test
     public void contextLoads() {
     }
