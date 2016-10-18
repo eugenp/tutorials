6772125f848ee3f7105bdc73a561ee0baf407aca
Christian Rädel <christian.raedel@gmail.com>
2016-10-18 17:04:02 +0200

    BAEL-86: Added localization and themes.

---
+++ spring-dispatcher-servlet/src/main/java/com/baeldung/spring/dispatcher/servlet/WebInitializer.java
@@ -0,0 +1,28 @@
+package com.baeldung.spring.dispatcher.servlet;
+
+import com.baeldung.spring.dispatcher.servlet.web.filters.RequestLoggingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfiguration.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfiguration.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/*"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{new RequestLoggingFilter()};
+    }
+}
