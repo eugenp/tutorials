7ae936874af60ef16e3b75b74a38ae0eb7e4c09a
Christian Rädel <christian.raedel@gmail.com>
2016-10-19 15:19:30 +0200

    BAEL-86: - Split controller into interfaces and implementations. - Added ability to up/download files.

--- spring-dispatcher-servlet/src/main/java/com/baeldung/spring/dispatcher/servlet/web/TaskController.java
+++ spring-dispatcher-servlet/src/main/java/com/baeldung/spring/dispatcher/servlet/web/TaskController.java
@@ -1,54 +1,49 @@
 package com.baeldung.spring.dispatcher.servlet.web;
 
 import com.baeldung.spring.dispatcher.servlet.models.Task;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
-@Controller
 @RequestMapping("/tasks")
-public class TaskController {
-    @Autowired
-    private Map<String, List<Task>> taskMap;
-
+public interface TaskController {
     @GetMapping("/{username}/list")
-    public String listForm(
-      Model model,
-      @PathVariable("username") String username
-    ) {
-        List<Task> tasks = taskMap.get(username).stream()
-          .sorted(Comparator.comparing(Task::getDue))
-          .collect(Collectors.toList());
-        model.addAttribute("username", username);
-        model.addAttribute("tasks", tasks);
-        return "task-list";
-    }
+    String listTasks(
+      @PathVariable("username") String username,
+      Model model
+    );
 
     @GetMapping("/{username}/add")
-    public String addForm(
-      Model model,
-      @PathVariable("username") String username
-    ) {
-        model.addAttribute("username", username);
-        model.addAttribute("task", new Task(new Date()));
-        return "task-add";
-    }
+    String addTask(
+      @PathVariable("username") String username,
+      Model model
+    );
 
     @PostMapping("/{username}/add")
-    public String addSubmit(
+    String addTask(
       @PathVariable("username") String username,
       @ModelAttribute Task task
-    ) {
-        List<Task> taskList = taskMap.get(username);
-        if (taskList == null) {
-            taskList = new ArrayList<>();
-        }
-        taskList.add(task);
-        taskMap.put(username, taskList);
-        return "redirect:list";
-    }
+    );
+
+    @GetMapping("/{username}/get/{id}")
+    String getTask(
+      @PathVariable("username") String username,
+      @PathVariable("id") int id,
+      Model model
+    );
+
+    @GetMapping("/{username}/get/{id}/attach")
+    String attachToTask(
+      @PathVariable("username") String username,
+      @PathVariable("id") int id,
+      Model model
+    );
+
+    @PostMapping("/{username}/get/{id}/attach")
+    String attachToTask(
+      @PathVariable("username") String username,
+      @PathVariable("id") int id,
+      @RequestParam("attachment") MultipartFile attachment,
+      @RequestParam("description") String description
+    );
 }
