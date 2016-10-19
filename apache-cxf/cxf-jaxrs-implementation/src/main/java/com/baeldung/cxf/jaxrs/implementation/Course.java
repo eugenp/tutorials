72bdf6c2be5671079e7959683a051523177c554e
Thai Nguyen <namthainguyen@gmail.com>
2016-10-19 22:27:02 +0700

    Changes HTTP request handling methods, adds negative test cases

--- apache-cxf/cxf-jaxrs-implementation/src/main/java/com/baeldung/cxf/jaxrs/implementation/Course.java
+++ apache-cxf/cxf-jaxrs-implementation/src/main/java/com/baeldung/cxf/jaxrs/implementation/Course.java
@@ -3,6 +3,7 @@ package com.baeldung.cxf.jaxrs.implementation;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.xml.bind.annotation.XmlRootElement;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,31 +36,42 @@ public class Course {
     public void setStudents(List<Student> students) {
         this.students = students;
     }
-    
+
     @GET
     @Path("{studentOrder}")
-    public Student getStudent(@PathParam("studentOrder")int studentOrder) {
+    public Student getStudent(@PathParam("studentOrder") int studentOrder) {
         return students.get(studentOrder);
     }
-    
+
     @POST
-    public Response postStudent(Student student) {
+    public Response createStudent(Student student) {
         if (students == null) {
             students = new ArrayList<>();
+        }
+        if (students.contains(student)) {
+            return Response.status(Response.Status.CONFLICT).build();
         }
         students.add(student);
         return Response.ok(student).build();
     }
-    
+
     @DELETE
     @Path("{studentOrder}")
     public Response deleteStudent(@PathParam("studentOrder") int studentOrder) {
-        Student student = students.get(studentOrder);
-        if (student != null) {
-            students.remove(studentOrder);
-            return Response.ok().build();
-        } else {
-            return Response.notModified().build();
+        if (students == null || studentOrder >= students.size()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
+        students.remove(studentOrder);
+        return Response.ok().build();
+    }
+
+    @Override
+    public int hashCode() {
+        return id + name.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof Course) && (id == ((Course) obj).getId()) && (name.equals(((Course) obj).getName()));
     }
 }
