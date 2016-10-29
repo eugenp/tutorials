771a81d5bd3a409c00279e8acddcdf9535fde3a9
mujah <mo.mujah@gmail.com>
2016-10-29 21:46:34 +0800

    Spring Data Solr project created

---
+++ spring-data-solr/src/main/java/com/baeldung/spring/data/solr/model/Product.java
@@ -0,0 +1,62 @@
+package com.baeldung.spring.data.solr.model;
+
+import org.apache.solr.client.solrj.beans.Field;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.solr.core.mapping.SolrDocument;
+
+@SolrDocument(solrCoreName="product")
+public class Product {
+
+    @Id
+    @Field("id")
+    private String id;
+
+    @Field("name")
+    private String name;
+
+    @Field("category")
+    private String category;
+
+    @Field("description")
+    private String description;
+
+    
+    public Product(String id,String name,String category){
+        this.id = id;
+        this.name = name;
+        this.category = category;
+    }
+    
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
