f66494192ca6068147bbd2068a97a3bcdb691f5f
Thai Nguyen <namthainguyen@gmail.com>
2016-10-30 21:30:18 +0700

    Revert "Change text/xml to application/xml"
    This reverts commit b61b82cd2a31831c43df1993d36997c762e21907.

--- apache-cxf/cxf-jaxrs-implementation/src/main/java/com/baeldung/cxf/jaxrs/implementation/CourseRepository.java
+++ apache-cxf/cxf-jaxrs-implementation/src/main/java/com/baeldung/cxf/jaxrs/implementation/CourseRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 @Path("baeldung")
-@Produces("application/xml")
+@Produces("text/xml")
 public class CourseRepository {
     private Map<Integer, Course> courses = new HashMap<>();
 
