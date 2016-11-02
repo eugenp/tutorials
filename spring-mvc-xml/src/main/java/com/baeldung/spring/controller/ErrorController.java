362bc6ff9b72fd39c4983ccbd4f13a34c23afdd3
Ankush Sharma <ankush.s@adsizzlergroup.com>
2016-11-02 22:04:17 +0530

    Unused imports removed

--- spring-mvc-xml/src/main/java/com/baeldung/spring/controller/ErrorController.java
+++ spring-mvc-xml/src/main/java/com/baeldung/spring/controller/ErrorController.java
@@ -2,7 +2,6 @@ package com.baeldung.spring.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.converter.xml.MarshallingHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
