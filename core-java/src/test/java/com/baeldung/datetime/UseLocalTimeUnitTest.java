8ce7e1f5884bda6cbfd2c3bd38a1d95b0ab94296
eugenp <hanriseldon@gmail.com>
2016-11-03 18:04:09 +0200

    formatting work

--- core-java/src/test/java/com/baeldung/datetime/UseLocalTimeUnitTest.java
+++ core-java/src/test/java/com/baeldung/datetime/UseLocalTimeUnitTest.java
@@ -6,31 +6,31 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UseLocalTimeUnitTest {
-    
+
     UseLocalTime useLocalTime = new UseLocalTime();
-    
+
     @Test
-    public void givenValues_whenUsingFactoryOf_thenLocalTime(){
-        Assert.assertEquals("07:07:07",useLocalTime.getLocalTimeUsingFactoryOfMethod(7,7,7).toString());
+    public void givenValues_whenUsingFactoryOf_thenLocalTime() {
+        Assert.assertEquals("07:07:07", useLocalTime.getLocalTimeUsingFactoryOfMethod(7, 7, 7).toString());
     }
-    
+
     @Test
-    public void givenString_whenUsingParse_thenLocalTime(){
-        Assert.assertEquals("06:30",useLocalTime.getLocalTimeUsingParseMethod("06:30").toString());
+    public void givenString_whenUsingParse_thenLocalTime() {
+        Assert.assertEquals("06:30", useLocalTime.getLocalTimeUsingParseMethod("06:30").toString());
     }
-    
+
     @Test
-    public void givenTime_whenAddHour_thenLocalTime(){
-        Assert.assertEquals("07:30",useLocalTime.addAnHour(LocalTime.of(6,30)).toString());
+    public void givenTime_whenAddHour_thenLocalTime() {
+        Assert.assertEquals("07:30", useLocalTime.addAnHour(LocalTime.of(6, 30)).toString());
     }
-    
+
     @Test
-    public void getHourFromLocalTime(){
-        Assert.assertEquals(1, useLocalTime.getHourFromLocalTime(LocalTime.of(1,1)));
+    public void getHourFromLocalTime() {
+        Assert.assertEquals(1, useLocalTime.getHourFromLocalTime(LocalTime.of(1, 1)));
     }
-    
+
     @Test
-    public void getLocalTimeWithMinuteSetToValue(){
-        Assert.assertEquals(LocalTime.of(10, 20), useLocalTime.getLocalTimeWithMinuteSetToValue(LocalTime.of(10,10), 20));
+    public void getLocalTimeWithMinuteSetToValue() {
+        Assert.assertEquals(LocalTime.of(10, 20), useLocalTime.getLocalTimeWithMinuteSetToValue(LocalTime.of(10, 10), 20));
     }
 }
