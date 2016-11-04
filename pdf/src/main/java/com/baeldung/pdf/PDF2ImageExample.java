a028c5c9f226db3f12f266b70bf2a807f970da53
Grzegorz Piwowarek <gpw@touk.pl>
2016-11-04 15:45:06 +0000

    Refactor PDF2ImageExample

--- pdf/src/main/java/com/baeldung/pdf/PDF2ImageExample.java
+++ pdf/src/main/java/com/baeldung/pdf/PDF2ImageExample.java
@@ -1,13 +1,13 @@
 package com.baeldung.pdf;
 
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class PDF2ImageExample {
 
@@ -28,7 +28,7 @@ public class PDF2ImageExample {
 		PDFRenderer pdfRenderer = new PDFRenderer(document);
 		for (int page = 0; page < document.getNumberOfPages(); ++page) {
 			BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
-			ImageIOUtil.writeImage(bim, "src/output/pdf" + "-" + (page + 1) + "." + extension, 300);
+			ImageIOUtil.writeImage(bim, String.format("src/output/pdf-%d.%s", page + 1, extension), 300);
 		}
 		document.close();
 	}
