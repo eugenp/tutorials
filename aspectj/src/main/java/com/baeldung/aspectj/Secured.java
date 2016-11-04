108adf84f6847cc4221c59024dfb0567748e00d3
nguyenminhtuanfit@gmail.com <nguyenminhtuanfit@gmail.com>
2016-11-04 07:57:11 +0700

    aspectj introduction

---
+++ aspectj/src/main/java/com/baeldung/aspectj/Secured.java
@@ -0,0 +1,12 @@
+package com.baeldung.aspectj;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Secured {
+    public boolean isLocked() default false; 
+}
