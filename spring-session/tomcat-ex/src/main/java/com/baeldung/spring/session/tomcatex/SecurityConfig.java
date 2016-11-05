9938a1a0c7c27c66b52c6db2d4cc675e2c2c9af7
tschiman <tschiman@gmail.com>
2016-11-05 17:35:46 +0100

    BAEL-89 remove csrf disable

--- spring-session/tomcat-ex/src/main/java/com/baeldung/spring/session/tomcatex/SecurityConfig.java
+++ spring-session/tomcat-ex/src/main/java/com/baeldung/spring/session/tomcatex/SecurityConfig.java
@@ -22,7 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         protected void configure(HttpSecurity http) throws Exception {
                 http
                         .httpBasic().and()
-//                        .csrf().disable()
                         .authorizeRequests()
                                 .antMatchers("/").permitAll()
                                 .antMatchers("/tomcat").hasRole("USER")
