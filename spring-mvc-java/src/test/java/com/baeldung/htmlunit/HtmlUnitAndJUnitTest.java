d2d0fdcaf87a36aecc232e5c0ff6989da56eb8fa
Juan García Heredero <juan.garcia.heredero@gmail.com>
2016-11-08 22:30:54 +0100

    Update HtmlUnitAndJUnitTest.java

---
+++ spring-mvc-java/src/test/java/com/baeldung/htmlunit/HtmlUnitAndJUnitTest.java
@@ -0,0 +1,30 @@
+package com.baeldung.htmlunit;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class HtmlUnitAndJUnitTest {
+
+	@Before
+	public void init() throws Exception {
+	    webClient = new WebClient();
+	}
+
+	@After
+	public void close() throws Exception {
+	    webClient.close();
+	}
+
+	@Test
+	public void givenAClient_whenEnteringBaeldung_thenPageTitleIsOk()
+	  throws Exception {
+	    HtmlPage page = webClient.getPage("http://www.baeldung.com/");
+	    Assert.assertEquals(
+	      "Baeldung | Java, Spring and Web Development tutorials",
+		page.getTitleText());
+	}
+
+}
