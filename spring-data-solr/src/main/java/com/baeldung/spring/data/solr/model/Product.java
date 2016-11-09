2ab1eeb036e0d5344eef5751c2602927a2040c93
mujah <mo.mujah@gmail.com>
2016-11-09 18:36:32 +0800

    refactoring model

--- spring-data-solr/src/main/java/com/baeldung/spring/data/solr/model/Product.java
+++ spring-data-solr/src/main/java/com/baeldung/spring/data/solr/model/Product.java
@@ -6,58 +6,50 @@ import org.springframework.data.solr.core.mapping.SolrDocument;
 
 @SolrDocument(solrCoreName = "product")
 public class Product {
-
+    
     @Id
-    @Indexed(name="id",type = "string")
+    @Indexed(name = "id", type = "string")
     private String id;
-
     
-    @Indexed(name="name",type = "string")
+    @Indexed(name = "name", type = "string")
     private String name;
-
     
-    @Indexed(name="category",type = "string")
+    @Indexed(name = "category", type = "string")
     private String category;
-
     
-    @Indexed(name="description",type = "string")
+    @Indexed(name = "description", type = "string")
     private String description;
     
-    public Product(){
-        
-    }
-
-
     public String getId() {
         return id;
     }
-
+    
     public void setId(String id) {
         this.id = id;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     public void setName(String name) {
         this.name = name;
     }
-
+    
     public String getCategory() {
         return category;
     }
-
+    
     public void setCategory(String category) {
         this.category = category;
     }
-
+    
     public String getDescription() {
         return description;
     }
-
+    
     public void setDescription(String description) {
         this.description = description;
     }
-
+    
 }
