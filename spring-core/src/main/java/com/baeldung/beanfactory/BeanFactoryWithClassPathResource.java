40841f93e5a1562c274e9a7792b05ecc610a4f06
Shubham Aggarwal <sbmaggarwal@gmail.com>
2016-11-14 20:22:24 +0530

    Added Test for BeanFactory methods

--- spring-core/src/main/java/com/baeldung/beanfactory/BeanFactoryWithClassPathResource.java
+++ spring-core/src/main/java/com/baeldung/beanfactory/BeanFactoryWithClassPathResource.java
@@ -1,16 +1,31 @@
 package com.baeldung.beanfactory;
 
+import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class BeanFactoryWithClassPathResource {
 
+    Resource res = new ClassPathResource("spring-app.xml");
+    BeanFactory factory = new XmlBeanFactory(res);
+
+    @Test
     public void createBeanFactory() {
 
-        Resource res = new ClassPathResource("spring-app.xml");
-        BeanFactory factory = new XmlBeanFactory(res);
         Employee emp = (Employee) factory.getBean("employee");
+
+        assertFalse(factory.isSingleton("employee"));
+
+        assertTrue(factory.getBean("employee") instanceof Employee);
+
+        assertTrue(factory.isTypeMatch("employee", Employee.class));
+
+        //we have empalias as an alias for employee
+        assertTrue(factory.getAliases("employee").length > 0);
     }
 }
