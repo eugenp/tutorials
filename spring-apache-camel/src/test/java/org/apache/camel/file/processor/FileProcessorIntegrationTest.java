d33c3868bcbb42163db7edae2955fae16e296e00
Ante Pocedulic <ante.pocedulic@gmail.com>
2016-11-14 19:52:48 +0100

    - changed names of jUnit tests due to Baeldung name convenction

--- spring-apache-camel/src/test/java/org/apache/camel/file/processor/FileProcessorIntegrationTest.java
+++ spring-apache-camel/src/test/java/org/apache/camel/file/processor/FileProcessorIntegrationTest.java
@@ -12,7 +12,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.baeldung.camel.file.FileProcessor;
 
 
-public class FileProcessorTest {
+public class FileProcessorIntegrationTest {
 
     private static final long DURATION_MILIS = 10000;
     private static final String SOURCE_FOLDER = "src/test/source-folder";
