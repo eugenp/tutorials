62d58599c5dc9fc4b4ed309d3340362bd4801f17
slavisa-baeldung <slavisa.baeldung@gmail.com>
2016-11-15 21:23:04 +0100

    BAEL-89 - removing @RestController from application classes

--- spring-session/jetty-session-demo/src/main/java/com/baeldung/spring/session/jettyex/JettyWebApplication.java
+++ spring-session/jetty-session-demo/src/main/java/com/baeldung/spring/session/jettyex/JettyWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class JettyWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(JettyWebApplication.class, args);
--- spring-session/tomcat-session-demo/src/main/java/com/baeldung/spring/session/tomcatex/TomcatWebApplication.java
+++ spring-session/tomcat-session-demo/src/main/java/com/baeldung/spring/session/tomcatex/TomcatWebApplication.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TomcatWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(TomcatWebApplication.class, args);
