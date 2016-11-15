d2e52b4368c49d4072daed7d32157adecc5f4faa
Diane Duan <duanou1992@163.com>
2016-11-15 12:37:17 +0800

    add unit tests

--- core-java/src/test/java/com/baeldung/properties/PropertiesDefaultListTest.java
+++ core-java/src/test/java/com/baeldung/properties/PropertiesDefaultListTest.java
@@ -1,11 +1,18 @@
 package com.baeldung.properties;
 
+import org.junit.Test;
+
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-public class DefaultListDemo {
-    public static void main(String[] args) throws IOException {
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class PropertiesDefaultListTest {
+    @Test
+    public void testDefaultList() throws IOException {
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         String defaultConfigPath = rootPath + "default.properties";
         String appConfigPath = rootPath + "app.properties";
@@ -16,8 +23,8 @@ public class DefaultListDemo {
         Properties appProps = new Properties(defaultProps);
         appProps.load(new FileInputStream(appConfigPath));
 
-        System.out.println(appProps.getProperty("name"));
-        System.out.println(appProps.getProperty("version"));
-        System.out.println(appProps.getProperty("site"));
+        assertThat(appProps.getProperty("name"), equalTo("TestApp"));
+        assertThat(appProps.getProperty("version"), equalTo("1.0"));
+        assertThat(appProps.getProperty("site"), equalTo("www.google.com"));
     }
 }
--- core-java/src/test/java/com/baeldung/properties/PropertiesLoadTest.java
+++ core-java/src/test/java/com/baeldung/properties/PropertiesLoadTest.java
@@ -1,15 +1,17 @@
 package com.baeldung.properties;
 
+import org.junit.Test;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class LoadDemo {
-    public static void main(String[] args) throws IOException {
+public class PropertiesLoadTest {
+    @Test
+    public void testLoadFromPropertiesFiles() throws IOException {
         String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
         String appConfigPath = rootPath + "app.properties";
         String catalogConfigPath = rootPath + "catalog";
-        String iconConfigPath = rootPath + "icons.xml";
 
         // load from properties file which use .properties as the suffix
         Properties appProps = new Properties();
@@ -18,8 +20,12 @@ public class LoadDemo {
         // load from properties file which not use .properties as the suffix
         Properties catalogProps = new Properties();
         catalogProps.load(new FileInputStream(catalogConfigPath));
+    }
 
-        // load from XML file
+    @Test
+    public void testLoadFromXmlFile() throws IOException {
+        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+        String iconConfigPath = rootPath + "icons.xml";
         Properties iconProps = new Properties();
         iconProps.loadFromXML(new FileInputStream(iconConfigPath));
     }
