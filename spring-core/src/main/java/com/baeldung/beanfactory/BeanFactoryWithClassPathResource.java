89ebafc9a6c92d6df5beb6dcfcf312f85266d038
Shubham Aggarwal <sbmaggarwal@gmail.com>
2016-11-16 08:29:47 +0530

    Added assertions.

--- spring-core/src/main/java/com/baeldung/beanfactory/BeanFactoryWithClassPathResource.java
+++ spring-core/src/main/java/com/baeldung/beanfactory/BeanFactoryWithClassPathResource.java
@@ -11,21 +11,16 @@ import static org.junit.Assert.assertTrue;
 
 public class BeanFactoryWithClassPathResource {
 
-    Resource res = new ClassPathResource("spring-app.xml");
-    BeanFactory factory = new XmlBeanFactory(res);
-
     @Test
     public void createBeanFactory() {
 
+        Resource res = new ClassPathResource("spring-app.xml");
+        BeanFactory factory = new XmlBeanFactory(res);
         Employee emp = (Employee) factory.getBean("employee");
 
         assertFalse(factory.isSingleton("employee"));
-
         assertTrue(factory.getBean("employee") instanceof Employee);
-
         assertTrue(factory.isTypeMatch("employee", Employee.class));
-
-        //we have empalias as an alias for employee
         assertTrue(factory.getAliases("employee").length > 0);
     }
 }
