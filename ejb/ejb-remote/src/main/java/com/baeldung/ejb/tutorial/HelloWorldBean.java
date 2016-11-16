0262d2728f4b2cfcee4b74f6ed44fa09f6f06970
slavisa-baeldung <slavisa.baeldung@gmail.com>
2016-11-16 07:21:01 +0100

    BAEL-317 - Added junit and surefire, updated the authentication info so that it fits the article

--- ejb/ejb-remote/src/main/java/com/baeldung/ejb/tutorial/HelloWorldBean.java
+++ ejb/ejb-remote/src/main/java/com/baeldung/ejb/tutorial/HelloWorldBean.java
@@ -1,14 +1,9 @@
 package com.baeldung.ejb.tutorial;
 
-import javax.annotation.Resource;
-import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 
 @Stateless(name = "HelloWorld")
 public class HelloWorldBean implements HelloWorld {
-
-    @Resource
-    private SessionContext context;
 
     @Override
     public String getHelloWorld() {
