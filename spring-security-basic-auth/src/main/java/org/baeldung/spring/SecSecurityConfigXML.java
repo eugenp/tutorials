7e0bf584a1dedab7e9d038c20e1f22a8299d7891
oreva <reva.olga@gmail.com>
2016-11-17 13:36:07 +0200

    Java-based configuration implemented.

--- spring-security-basic-auth/src/main/java/org/baeldung/spring/SecSecurityConfigXML.java
+++ spring-security-basic-auth/src/main/java/org/baeldung/spring/SecSecurityConfigXML.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
-@Configuration
-@ImportResource({ "classpath:webSecurityConfig.xml" })
-@ComponentScan("org.baeldung.security")
-public class SecSecurityConfig {
+//@Configuration
+//@ImportResource({ "classpath:webSecurityConfig.xml" })
+//@ComponentScan("org.baeldung.security")
+public class SecSecurityConfigXML {
 
-    public SecSecurityConfig() {
+    public SecSecurityConfigXML() {
         super();
     }
 
