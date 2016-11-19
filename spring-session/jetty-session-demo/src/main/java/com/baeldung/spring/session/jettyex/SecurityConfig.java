5f99e5aedd6fc64c4266104c72674b372b7d57e7
slavisa-baeldung <slavisa.baeldung@gmail.com>
2016-11-19 09:47:17 +0100

    BAEL-89 - Adding surefire, fixing formatting

--- spring-session/jetty-session-demo/src/main/java/com/baeldung/spring/session/jettyex/SecurityConfig.java
+++ spring-session/jetty-session-demo/src/main/java/com/baeldung/spring/session/jettyex/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
-            .authorizeRequests().anyRequest().hasRole("ADMIN");
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER).and()
+                .authorizeRequests().anyRequest().hasRole("ADMIN");
     }
 }
--- spring-session/tomcat-session-demo/src/main/java/com/baeldung/spring/session/tomcatex/SecurityConfig.java
+++ spring-session/tomcat-session-demo/src/main/java/com/baeldung/spring/session/tomcatex/SecurityConfig.java
@@ -14,16 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .inMemoryAuthentication()
-            .withUser("admin").password("password").roles("ADMIN");
+                .inMemoryAuthentication()
+                .withUser("admin").password("password").roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .httpBasic().and()
-            .authorizeRequests()
-            .antMatchers("/tomcat/admin").hasRole("ADMIN")
-            .anyRequest().authenticated();
+                .httpBasic().and()
+                .authorizeRequests()
+                .antMatchers("/tomcat/admin").hasRole("ADMIN")
+                .anyRequest().authenticated();
     }
 }
