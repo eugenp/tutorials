ce0363ea37d0e09fbbd6405ba132d445091bd0eb
danidemi <demichelis@danidemi.com>
2016-11-19 14:08:18 +0100

    First test with log4j rolling appenders

---
+++ log4j/src/main/java/com/baeldung/log4j/Log4jRollingExample.java
@@ -0,0 +1,16 @@
+package com.baeldung.log4j;
+
+
+import java.util.stream.IntStream;
+
+import org.apache.log4j.Logger;
+
+public class Log4jRollingExample {
+
+    private final static Logger logger = Logger.getLogger(Log4jRollingExample.class);
+
+    public static void main(String[] args) {
+        IntStream.range(0, 10).forEach((i) -> logger.info("This is the " + i + " time I say 'Hello World'."));
+    }
+
+}
