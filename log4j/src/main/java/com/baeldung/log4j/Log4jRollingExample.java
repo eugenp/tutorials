bb47c277ee3149286b8ecfeb0a9172b38f809bab
danidemi <demichelis@danidemi.com>
2016-11-19 15:45:10 +0100

    Log4j rolling appender

--- log4j/src/main/java/com/baeldung/log4j/Log4jRollingExample.java
+++ log4j/src/main/java/com/baeldung/log4j/Log4jRollingExample.java
@@ -1,6 +1,5 @@
 package com.baeldung.log4j;
 
-
 import java.util.stream.IntStream;
 
 import org.apache.log4j.Logger;
@@ -10,7 +9,18 @@ public class Log4jRollingExample {
     private final static Logger logger = Logger.getLogger(Log4jRollingExample.class);
 
     public static void main(String[] args) {
-        IntStream.range(0, 10).forEach((i) -> logger.info("This is the " + i + " time I say 'Hello World'."));
+        IntStream.range(0, 10).forEach(i -> {
+            writeLogLine(i);
+        });
+    }
+
+    private static void writeLogLine(int i) {
+        logger.info("This is the " + i + " time I say 'Hello World'.");
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            // should not happen
+        }
     }
 
 }
