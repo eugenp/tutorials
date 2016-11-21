e84957c768eace2e933a935d6c76d87bc251fcd8
DianeDuan <duanou1992@163.com>
2016-11-21 21:51:20 +0800

    change expect exception in ut

--- spring-core/src/test/java/com/baeldung/factorybean/FactoryBeanInitializeTest.java
+++ spring-core/src/test/java/com/baeldung/factorybean/FactoryBeanInitializeTest.java
@@ -1,15 +1,16 @@
 package com.baeldung.factorybean;
 
 import org.junit.Test;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class FactoryBeanInitializeTest {
-    @Test(expected = Exception.class)
+    @Test(expected = BeanCreationException.class)
     public void testInitializationToolFactory() {
         new ClassPathXmlApplicationContext("classpath:factorybean-init-spring-ctx.xml");
     }
 
-    @Test(expected = Exception.class)
+    @Test(expected = BeanCreationException.class)
     public void testPostConstructToolFactory() {
         new ClassPathXmlApplicationContext("classpath:factorybean-postconstruct-spring-ctx.xml");
     }
