4d4afa6ca454b0172260e8cc210e7215442746c3
Ivan Paolillo <ivan.paolillo@gmail.com>
2016-11-21 13:53:37 +0100

    Code improvement

--- spring-rest/src/test/java/org/baeldung/okhttp/OkHttpGetLiveTest.java
+++ spring-rest/src/test/java/org/baeldung/okhttp/OkHttpGetLiveTest.java
@@ -8,10 +8,6 @@ import java.io.IOException;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import okhttp3.Call;
 import okhttp3.Callback;
