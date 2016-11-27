deb5183dece944d7d0942c674353b1a67a775ddc
danidemi <escher75@gmail.com>
2016-11-27 18:37:40 +0100

    log4j2 more detailed example

--- log4j/src/main/java/com/baeldung/log4j/Log4jRollingExample.java
+++ log4j/src/main/java/com/baeldung/log4j/Log4jRollingExample.java
@@ -9,7 +9,7 @@ public class Log4jRollingExample {
     private final static Logger logger = Logger.getLogger(Log4jRollingExample.class);
 
     public static void main(String[] args) {
-        IntStream.range(0, 1000).forEach(i -> {
+        IntStream.range(0, 5000).forEach(i -> {
             logger.info("This is the " + i + " time I say 'Hello World'.");
             try {
                 Thread.sleep(0);
