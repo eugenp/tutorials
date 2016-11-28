3c0d504fe54b2004b7dd21f8e63f7bb17e75645c
Shubham Aggarwal <sbmaggarwal@gmail.com>
2016-11-28 11:03:13 +0530

    Test made using Mockito and JUnit

--- javax-servlets/src/main/java/com/root/FormServlet.java
+++ javax-servlets/src/main/java/com/root/FormServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "FormServlet", urlPatterns = "/calculateServlet")
 public class FormServlet extends HttpServlet {
@@ -25,19 +26,15 @@ public class FormServlet extends HttpServlet {
             response.setHeader("Test", "Success");
             response.setHeader("BMI", String.valueOf(bmi));
 
+            PrintWriter out= response.getWriter();
+            out.write(String.valueOf(bmi));
+
             RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
             dispatcher.forward(request, response);
         } catch (Exception e) {
 
             response.sendRedirect("index.jsp");
         }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        // do something else here
     }
 
     private Double calculateBMI(Double weight, Double height) {
