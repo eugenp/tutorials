cf560f3346c2678b2b7c5b6a3d003c34e6c1efb2
Juan García Heredero <juan.garcia.heredero@gmail.com>
2016-11-28 11:09:41 +0100

    Update HtmlUnitAndJUnitTest.java

--- spring-mvc-java/src/test/java/com/baeldung/htmlunit/HtmlUnitAndJUnitTest.java
+++ spring-mvc-java/src/test/java/com/baeldung/htmlunit/HtmlUnitAndJUnitTest.java
@@ -21,6 +21,7 @@ public class HtmlUnitAndJUnitTest {
 	@Test
 	public void givenAClient_whenEnteringBaeldung_thenPageTitleIsOk()
 	  throws Exception {
+	    webClient.getOptions().setThrowExceptionOnScriptError(false);
 	    HtmlPage page = webClient.getPage("http://www.baeldung.com/");
 	    Assert.assertEquals(
 	      "Baeldung | Java, Spring and Web Development tutorials",
