333d3bc1226787a45fe3113d8f7999db76e738e7
pivovarit <gpiwowarek@gmail.com>
2016-11-28 22:15:24 +0100

    Refactor AsyncEchoServer2

--- core-java/src/test/java/com/baeldung/java/nio2/async/AsyncFileTest.java
+++ core-java/src/test/java/com/baeldung/java/nio2/async/AsyncFileTest.java
@@ -60,7 +60,7 @@ public class AsyncFileTest {
     @Test
     public void givenPathAndContent_whenWritesToFileWithFuture_thenCorrect() throws IOException, ExecutionException, InterruptedException {
         String fileName = UUID.randomUUID().toString();
-        Path path = Paths.get(fileName);
+        Path path = Paths.get(Paths.get(HOME));
         AsynchronousFileChannel fileChannel = AsynchronousFileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.CREATE,StandardOpenOption.DELETE_ON_CLOSE);
 
         ByteBuffer buffer = ByteBuffer.allocate(1024);
