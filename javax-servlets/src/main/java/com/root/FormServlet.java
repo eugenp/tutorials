115f70c9173590414c8a5dca946a758aa0d66e49
Shubham Aggarwal <sbmaggarwal@gmail.com>
2016-11-28 09:36:42 +0530

    RequestDispatcher added to servlet

--- javax-servlets/src/main/java/com/root/FormServlet.java
+++ javax-servlets/src/main/java/com/root/FormServlet.java
@@ -1,5 +1,6 @@
 package com.root;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +8,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "FormServlet", urlPatterns = "/informationServlet")
+@WebServlet(name = "FormServlet", urlPatterns = "/calculateServlet")
 public class FormServlet extends HttpServlet {
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String userName = request.getParameter("userName");
-        String userPlanet = request.getParameter("userPlanet");
+        String height = request.getParameter("height");
+        String weight = request.getParameter("weight");
+
+        try {
+            Double bmi = calculateBMI(Double.parseDouble(weight), Double.parseDouble(height));
+
+            request.setAttribute("bmi", bmi);
+            RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+
+            response.sendRedirect("index.jsp");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // do something else here
+    }
+
+    private Double calculateBMI(Double weight, Double height) {
+
+        return weight / (height * height);
     }
 }
