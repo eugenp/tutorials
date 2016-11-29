0e3b7fe17c866e3a6279642c294770c88fde0ddc
pivovarit <gpiwowarek@gmail.com>
2016-11-29 17:43:17 +0100

    Formatting

--- spring-core/src/test/java/com/baeldung/factorybean/FactoryBeanJavaConfigTest.java
+++ spring-core/src/test/java/com/baeldung/factorybean/FactoryBeanJavaConfigTest.java
@@ -1,16 +1,15 @@
 package com.baeldung.factorybean;
 
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertThat;
-
-import javax.annotation.Resource;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = FactoryBeanAppConfig.class)
@@ -18,6 +17,7 @@ public class FactoryBeanJavaConfigTest {
 
     @Autowired
     private Tool tool;
+
     @Resource(name = "&tool")
     private ToolFactory toolFactory;
 
