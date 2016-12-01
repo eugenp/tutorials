31268f28f7a544935c8da3d96376c68a40cee4f5
Anton Krosnev <anton.krosnev@ocado.com>
2016-12-01 19:01:54 +0200

    Adding mock server for tests: http://www.mock-server.com/running_mock_server.html

--- core-java-9/src/test/java/com/baeldung/java9/httpclient/AsyncHttpClientTest.java
+++ core-java-9/src/test/java/com/baeldung/java9/httpclient/AsyncHttpClientTest.java
@@ -1,6 +1,8 @@
 package com.baeldung.java9.httpclient;
 
+import org.junit.After;
 import static org.junit.Assert.assertTrue;
+
 
 import java.io.IOException;
 import java.net.URI;
@@ -8,38 +10,57 @@ import java.net.URISyntaxException;
 import java.net.http.HttpHeaders;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 import javax.security.auth.login.LoginException;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.Result;
+import org.mockserver.client.server.MockServerClient;
+import static org.mockserver.integration.ClientAndProxy.startClientAndProxy;
+import static org.mockserver.integration.ClientAndServer.startClientAndServer;
+import org.mockserver.model.Delay;
+import org.mockserver.model.Header;
+import org.mockserver.model.Parameter;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class AsyncHttpClientTest {
 
     private static final int size = 3;
-    private URI [] URIs = new URI[size] ;
+    private ArrayList<URI> URIs = new ArrayList<>();
+    private Iterator<URI> uriIterator;
     private String status;
     private HttpRequest [] requests = new HttpRequest[size];
     private CompletableFuture<HttpResponse>[] futureResponses = new CompletableFuture[size];
     private static final AtomicInteger counter = new AtomicInteger(0);
-
+    private ClientAndProxy proxy;
+    private ClientAndServer mockServer;
+    private static final String serverHost = "localhost";
+    private static final int serverPort = 1080;
+    private static final String baseURL = "http://"+serverHost+":"+ serverPort;
+    
     @Before
     public void init() throws URISyntaxException {
-        URIs[0] = new URI("http://www.baeldung.com/");
-        URIs[1] = new URI("http://www.baeldung.com/category/java/");
-        URIs[2] = new URI("http://www.baeldung.com/category/http/");
+        URIs.add( new URI("http://www.baeldung.com/") );
+        URIs.add( new URI("http://www.baeldung.com/category/java/") );
+        URIs.add( new URI("http://www.baeldung.com/category/http/") );
+        uriIterator = URIs.iterator();
+        
+        setupMockHttpServer();
     }
 
     @Test
     public void asynchronousGet() throws URISyntaxException, IOException, InterruptedException, ExecutionException {
         long before = System.currentTimeMillis();
-        CompletableFuture<HttpResponse> futureResponses0 = HttpRequest.create(URIs[0]).GET().responseAsync();
+        CompletableFuture<HttpResponse> futureResponses0 = HttpRequest.create(uriIterator.next()).GET().responseAsync();
         CompletableFuture<HttpResponse> futureResponses1 = futureResponses0.thenCompose(this::checkResponseAndFireRequest);
-        futureResponses1.thenAccept(this::processResponse);
+        futureResponses1.thenAccept(this::processResponseBody);
         
         futureResponses1.get();
         System.out.println("End.");
@@ -67,13 +88,13 @@ public class AsyncHttpClientTest {
     	if(response.statusCode() == 200){
     		System.out.println("Success");
     		int c = counter.incrementAndGet();
-    		return HttpRequest.create(URIs[c]).GET().responseAsync();
+    		return HttpRequest.create(uriIterator.next()).GET().responseAsync();
     	}else
     		return null;
     }
     
     
-   private void processResponse(HttpResponse response) {
+   private void processResponseBody(HttpResponse response) {
        String responseBody = response.body(HttpResponse.asString());
        if(! responseBody.contains("Login Success")){
            status = "FAIL";
@@ -88,4 +109,42 @@ public class AsyncHttpClientTest {
        th.printStackTrace();
        return null;
    }
+   
+   @After
+   public void stopProxy() {
+       proxy.stop();
+       mockServer.stop();
+   }
+   
+   private void setupMockHttpServer() {
+	   mockServer = startClientAndServer(serverPort);
+	   proxy = startClientAndProxy(serverPort + 10);
+	   
+	   new MockServerClient("127.0.0.1", serverPort)
+       .when(
+               request()
+                       .withMethod("POST")
+                       .withPath("/login")
+                       .withQueryStringParameters(
+                               new Parameter("returnUrl", "/account")
+                       )
+                       //.withCookies(
+                       //        new Cookie("sessionId", "2By8LOhBmaW5nZXJwcmludCIlMDAzMW")
+                       //)
+                       .withBody(exact("{username: 'foo', password: 'bar'}")),
+               exactly(1)
+       )
+       .respond(
+               response()
+                       .withStatusCode(401)
+                       .withHeaders(
+                               new Header("Content-Type", "application/json; charset=utf-8"),
+                               new Header("Cache-Control", "public, max-age=86400")
+                       )
+                       .withBody("{ message: 'incorrect username and password combination' }")
+                       .withDelay(new Delay(SECONDS, 1))
+       );
+   }
+   
+   
 }
