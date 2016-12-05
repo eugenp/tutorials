fbae4042c629fbd7bbe5025ac45536d0189bb139
dineshmadhwal <dineshmadhwal@gmail.com>
2016-12-06 02:37:17 +0800

    Changed File name

--- spring-mvc-handlers/src/main/java/com/baeldung/spring/controller/RequestMappingHandlerAdapterExample.java
+++ spring-mvc-handlers/src/main/java/com/baeldung/spring/controller/RequestMappingHandlerAdapterExample.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class RequestMappingHandlerAdapterExampleController {
+public class RequestMappingHandlerAdapterExample {
 	@RequestMapping("/requestName")
 	public ModelAndView getEmployeeName() {
 		ModelAndView model = new ModelAndView("Greeting");
-		model.addObject("message","Madhwal");
+		model.addObject("message", "Madhwal");
 		return model;
 	}
 }
