3b53e82b8b7a61a8a5d0f4cbac764feca98e4c95
Marek Lewandowski <marek.m.lewandowski@gmail.com>
2016-12-12 20:23:34 +0100

    Rename args

--- spring-mvc-handlers/src/main/java/com/baeldung/spring/controller/SimpleControllerHandlerAdapterExample.java
+++ spring-mvc-handlers/src/main/java/com/baeldung/spring/controller/SimpleControllerHandlerAdapterExample.java
@@ -9,8 +9,8 @@ import org.springframework.web.servlet.mvc.AbstractController;
 public class SimpleControllerHandlerAdapterExample extends
 		AbstractController {
 	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest arg0,
-			HttpServletResponse arg1) throws Exception {
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 		ModelAndView model = new ModelAndView("Greeting");
 		model.addObject("message", "Dinesh Madhwal");
 		return model;
