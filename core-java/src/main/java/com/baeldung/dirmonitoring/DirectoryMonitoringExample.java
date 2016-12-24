4a95d8dd2fa4edb6c467dec1e7a680eb84c64148
Saptarshi Basu <sapb2004@gmail.com>
2016-12-24 15:59:09 +0530

    Indentation fixed

--- core-java/src/main/java/com/baeldung/dirmonitoring/DirectoryMonitoringExample.java
+++ core-java/src/main/java/com/baeldung/dirmonitoring/DirectoryMonitoringExample.java
@@ -9,30 +9,30 @@ import org.apache.commons.io.monitor.FileAlterationObserver;
 
 public class DirectoryMonitoringExample {
 
-    public static final int POLL_INTERVAL = 500;
+	public static final int POLL_INTERVAL = 500;
 
-    public static void main(String[] args) throws Exception {
-        FileAlterationObserver observer = new FileAlterationObserver(System.getProperty("user.home"));
-        FileAlterationMonitor monitor = new FileAlterationMonitor(POLL_INTERVAL);
-        FileAlterationListener listener = new FileAlterationListenerAdaptor() {
-            @Override
-            public void onFileCreate(File file) {
-                System.out.println("File: " + file.getName() + " created");
-            }
+	public static void main(String[] args) throws Exception {
+		FileAlterationObserver observer = new FileAlterationObserver(System.getProperty("user.home"));
+		FileAlterationMonitor monitor = new FileAlterationMonitor(POLL_INTERVAL);
+		FileAlterationListener listener = new FileAlterationListenerAdaptor() {
+			@Override
+			public void onFileCreate(File file) {
+				System.out.println("File: " + file.getName() + " created");
+			}
 
-            @Override
-            public void onFileDelete(File file) {
-                System.out.println("File: " + file.getName() + " deleted");
-            }
+			@Override
+			public void onFileDelete(File file) {
+				System.out.println("File: " + file.getName() + " deleted");
+			}
 
-            @Override
-            public void onFileChange(File file) {
-                System.out.println("File: " + file.getName() + " changed");
-            }
-        };
-        observer.addListener(listener);
-        monitor.addObserver(observer);
-        monitor.start();
+			@Override
+			public void onFileChange(File file) {
+				System.out.println("File: " + file.getName() + " changed");
+			}
+		};
+		observer.addListener(listener);
+		monitor.addObserver(observer);
+		monitor.start();
 
-    }
+	}
 }
