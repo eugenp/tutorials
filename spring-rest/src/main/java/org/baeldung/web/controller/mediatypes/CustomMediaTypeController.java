05369fb1554a45544719e80835e68215d0a7cbe9
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2016-12-28 11:44:29 +0100

    add test, add DTO, remove example of API versioning to make example simpler

--- spring-rest/src/main/java/org/baeldung/web/controller/mediatypes/CustomMediaTypeController.java
+++ spring-rest/src/main/java/org/baeldung/web/controller/mediatypes/CustomMediaTypeController.java
@@ -1,26 +1,19 @@
 package org.baeldung.web.controller.mediatypes;
 
 
+import org.baeldung.web.dto.BaeldungItem;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/", produces = "application/vnd.baeldung.api.v1+json",
-        consumes = "application/vnd.baeldung.api.v1+json")
 public class CustomMediaTypeController {
 
 
     @RequestMapping(value = "/public/api/endpoint", produces = "application/vnd.baeldung.api.v1+json",
             consumes = "application/vnd.baeldung.api.v1+json")
-    public ResponseEntity<String> getItem() {
-        return new ResponseEntity<>("our item", HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/public/api/endpoint", produces = "application/vnd.baeldung.api.v2+json",
-            consumes = "application/vnd.baeldung.api.v2+json")
-    public ResponseEntity<Integer> getItemSecondVersion() {
-        return new ResponseEntity<>(1, HttpStatus.OK);
+    public ResponseEntity<BaeldungItem> getItem() {
+        return new ResponseEntity<>(new BaeldungItem("itemId1"), HttpStatus.OK);
     }
 }
