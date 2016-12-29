482a6575d6b3bc57b4367ce203593f1f1da3a39f
slavisa-baeldung <slavisa.baeldung@gmail.com>
2016-12-29 16:46:40 +0100

    BAEL-97 - adding handler mapping examples

---
+++ spring-mvc-java/src/main/java/com/baeldung/spring/web/config/ControllerClassNameHandlerMappingConfig.java
@@ -0,0 +1,42 @@
+package com.baeldung.spring.web.config;
+
+import com.baeldung.ExampleTwoController;
+import com.baeldung.WelcomeController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.support.ControllerClassNameHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class ControllerClassNameHandlerMappingConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Bean
+	public ControllerClassNameHandlerMapping controllerClassNameHandlerMapping() {
+		ControllerClassNameHandlerMapping controllerClassNameHandlerMapping = new ControllerClassNameHandlerMapping();
+		return controllerClassNameHandlerMapping;
+	}
+
+	@Bean
+	public WelcomeController welcome() {
+		WelcomeController welcome = new WelcomeController();
+		return welcome;
+	}
+
+	@Bean
+	public ExampleTwoController exampleTwo() {
+		ExampleTwoController exampleTwo = new ExampleTwoController();
+		return exampleTwo;
+	}
+
+}
