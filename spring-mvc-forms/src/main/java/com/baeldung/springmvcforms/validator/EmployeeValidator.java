cc25ad1e52675a6ede7561952f717c1430322b71
gatmeister <almarissa.elemino@gmail.com>
2016-12-30 10:01:49 +0800

    BAEL-112 | Spring MVC Validation - a Custom Validator
    formats the EmployeeValidator class

--- spring-mvc-forms/src/main/java/com/baeldung/springmvcforms/validator/EmployeeValidator.java
+++ spring-mvc-forms/src/main/java/com/baeldung/springmvcforms/validator/EmployeeValidator.java
@@ -10,14 +10,13 @@ import com.baeldung.springmvcforms.domain.Employee;
 @Component
 public class EmployeeValidator implements Validator {
 
-	public boolean supports(Class<?> clazz) {
-		return Employee.class.isAssignableFrom(clazz);
-	}
+    public boolean supports(Class<?> clazz) {
+        return Employee.class.isAssignableFrom(clazz);
+    }
 
-	public void validate(Object target, Errors errors) 
-	{
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.name", "Name is required.");
-		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "contactNumber", "error.contactNumber", "Contact Number is required.");
-	}
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "name", "error.name", "Name is required.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "contactNumber", "error.contactNumber", "Contact Number is required.");
+    }
 
 }
