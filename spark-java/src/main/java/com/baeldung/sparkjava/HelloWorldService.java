0a82f60abf23027a40e708919aa67cbee2ed5bba
maverick <joshiparthin@gmail.com>
2016-12-31 15:45:42 +0530

    Suggested changes in print messages.

--- spark-java/src/main/java/com/baeldung/sparkjava/HelloWorldService.java
+++ spark-java/src/main/java/com/baeldung/sparkjava/HelloWorldService.java
@@ -3,7 +3,7 @@ import static spark.Spark.*;
 
 public class HelloWorldService {
     public static void main(String[] args) {
-        get("/hello", (req,res)->"hello world");
+        get("/hello", (req,res)->"Hello, Baeldung");
         
         get("/hello/:name", (req,res)->{
             return "Hello: "+ req.params(":name");
