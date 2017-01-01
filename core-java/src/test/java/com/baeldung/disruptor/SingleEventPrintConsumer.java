4a30e523191218df6a7b97f253b101001d15a932
eralmas7 <eralmas7@yahoo.com>
2017-01-02 00:10:45 +0530

    BAEL-519 Added support for disruptor library

---
+++ core-java/src/test/java/com/baeldung/disruptor/SingleEventPrintConsumer.java
@@ -0,0 +1,17 @@
+package com.baeldung.disruptor;
+
+import com.lmax.disruptor.EventHandler;
+
+public class SingleEventPrintConsumer implements EventConsumer {
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public EventHandler<ValueEvent>[] getEventHandler() {
+        final EventHandler<ValueEvent> eventHandler = (event, sequence, endOfBatch) -> print(event.getValue(), sequence);
+        return new EventHandler[] {eventHandler};
+    }
+
+    private void print(final int id, final long sequenceId) {
+        System.out.println("Id is " + id + " sequence id that was used is " + sequenceId);
+    }
+}
