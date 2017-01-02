17148ca5e91a79f9cce9177f098c887751091da0
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-01-02 16:22:27 +0100

    BAEL-382 Add extension function that works on list

--- kotlin/src/main/kotlin/com/baeldung/Item.kt
+++ kotlin/src/main/kotlin/com/baeldung/Item.kt
@@ -1,4 +1,4 @@
 package com.baeldung
 
 
-data class Item(val id: String, val name: String)
+data class Item(val id: String, val name: String = "unknown_name")
---
+++ kotlin/src/test/kotlin/com/baeldung/ListExtensionTest.kt
@@ -0,0 +1,18 @@
+package com.baeldung
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class ListExtensionTest {
+    @Test
+    fun givenList_whenExecuteExtensionFunctionOnList_shouldReturnRandomElementOfList(){
+        //given
+        val elements = listOf("a", "b", "c")
+
+        //when
+        val result = ListExtension().getRandomElementOfList(elements)
+
+        //then
+        assertTrue(elements.contains(result))
+    }
+}
