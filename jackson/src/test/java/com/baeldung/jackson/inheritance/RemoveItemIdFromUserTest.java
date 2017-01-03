d937fcb736d4a38956ff86d22daa32333c252240
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-01-03 17:09:22 +0100

    BAEL-19 test for json inheritance

---
+++ jackson/src/test/java/com/baeldung/jackson/inheritance/RemoveItemIdFromUserTest.java
@@ -0,0 +1,27 @@
+package com.baeldung.jackson.inheritance;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class RemoveItemIdFromUserTest {
+    @Test
+    public void givenRemoveItemJson_whenDeserialize_shouldHaveProperClassType() throws IOException {
+        //given
+        Event event = new RemoveItemIdFromUser(new Event.Metadata("1", 12345567L), "item_1", 2L);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String eventJson = objectMapper.writeValueAsString(event);
+
+        //when
+        Event result = new ObjectMapper().readValue(eventJson, Event.class);
+
+        //then
+        assertTrue(result instanceof RemoveItemIdFromUser);
+        assertEquals("item_1", ((RemoveItemIdFromUser) result).getItemId());
+    }
+
+}
