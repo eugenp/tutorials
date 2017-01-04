f72311412835ae41510520bb43d3c4928fc946c5
Yasser Afifi <afifiyas@gmail.com>
2017-01-04 13:10:02 +0000

    fixed handlerMapping code and added tests

--- spring-mvc-java/src/main/java/com/baeldung/HandlerMapping/Controller/BaeldungController.java
+++ spring-mvc-java/src/main/java/com/baeldung/HandlerMapping/Controller/BaeldungController.java
@@ -6,12 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class WelcomeTwoController extends AbstractController {
+public class BaeldungController extends AbstractController {
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("Inside Welcome two Controller");
+        System.out.println("Inside Welcome Baeldung Controller");
 
-        ModelAndView model = new ModelAndView("welcome");
+        ModelAndView model = new ModelAndView("baeldung");
 
         return model;
     }
--- spring-mvc-java/src/main/java/com/baeldung/HandlerMapping/Controller/TestController.java
+++ spring-mvc-java/src/main/java/com/baeldung/HandlerMapping/Controller/TestController.java
@@ -6,12 +6,12 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-public class WelcomeBaeldungController extends AbstractController {
+public class TestController extends AbstractController {
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        System.out.println("Inside Welcome Baeldung Controller");
+        System.out.println("Inside Welcome two Controller");
 
-        ModelAndView model = new ModelAndView("welcome");
+        ModelAndView model = new ModelAndView("test");
 
         return model;
     }
