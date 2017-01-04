494e539700bb3cda5fe1bb3b4cceec84c38a9ce0
Diane Duan <duanou1992@163.com>
2017-01-04 17:50:53 +0800

    init

---
+++ metrics/src/main/java/com/baeldung/metrics/core/MetricsDemo.java
@@ -0,0 +1,4 @@
+package com.baeldung.metrics.core;
+
+public class MetricsDemo {
+}
---
+++ metrics/src/main/java/com/baeldung/metrics/core/MetricsRegistryDemo.java
@@ -0,0 +1,6 @@
+package com.baeldung.metrics.core;
+
+public class MetricsRegistryDemo {
+    public static void main(String[] args) {
+    }
+}
