6bd99217dc27c68564deb0794fd98a0abcb8b25d
Saptarshi Basu <sapb2004@gmail.com>
2017-01-04 10:18:26 +0530

    JAX-RS API using Jersey [BAEL-558]

--- jersey/src/test/java/com/baeldung/server/JerseyApiLiveTest.java
+++ jersey/src/test/java/com/baeldung/server/JerseyApiLiveTest.java
@@ -21,21 +21,21 @@ public class JerseyApiLiveTest {
     public void getEmployeeListResponseCode() throws ClientProtocolException, IOException {
         final HttpUriRequest request = new HttpGet("http://localhost:8082/JerseyTutorial/resources/employees");
         final HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assert(httpResponse.getStatusLine().getStatusCode()==HttpStatus.SC_OK);
+        assert(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK);
     }
 
     @Test
     public void getEmployeeResponseCode() throws ClientProtocolException, IOException {
         final HttpUriRequest request = new HttpGet("http://localhost:8082/JerseyTutorial/resources/employees/1");
         final HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assert(httpResponse.getStatusLine().getStatusCode()==HttpStatus.SC_OK);
+        assert(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_OK);
     }
 
     @Test
     public void getEmployeeNotfoundResponseCode() throws ClientProtocolException, IOException {
         final HttpUriRequest request = new HttpGet("http://localhost:8082/JerseyTutorial/resources/employees/1000");
         final HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assert(httpResponse.getStatusLine().getStatusCode()==HttpStatus.SC_NOT_FOUND);
+        assert(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_NOT_FOUND);
     }
 
     @Test
@@ -47,18 +47,18 @@ public class JerseyApiLiveTest {
         Employee emp = mapper.readValue(httpResponse.getEntity().getContent(), Employee.class);
         assert(emp.getFirstName().equals("Jane"));
     }
-    
+
     @Test
     public void createEmployeeJson() throws ClientProtocolException, IOException {
         final HttpPost request = new HttpPost("http://localhost:8082/JerseyTutorial/resources/employees");
-        Employee emp = new Employee(5,"Johny", "Doe", 33);
+        Employee emp = new Employee(5, "Johny", "Doe", 33);
         ObjectMapper mapper = new ObjectMapper();
         String empJson = mapper.writeValueAsString(emp);
         StringEntity input = new StringEntity(empJson);
         input.setContentType("application/json");
         request.setEntity(input);
         final HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-        assert(httpResponse.getStatusLine().getStatusCode()==HttpStatus.SC_CREATED);
+        assert(httpResponse.getStatusLine().getStatusCode() == HttpStatus.SC_CREATED);
     }
 
 }
