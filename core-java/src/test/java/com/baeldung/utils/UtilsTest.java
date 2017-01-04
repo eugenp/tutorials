6a56eff02e4a131c704ae7f91412a8a885642fa8
dsharew <dsharew@apposit.com>
2017-01-04 15:44:54 +0300

    Fixed formatting issue.

--- core-java/src/test/java/com/baeldung/utils/UtilsTest.java
+++ core-java/src/test/java/com/baeldung/utils/UtilsTest.java
@@ -10,21 +10,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilsTest {
 
-    @Test
-    public void givenString_whenCallingStringToCharArray_shouldConvertToCharArray() {
-        String givenString = "abcd";
+        @Test public void givenString_whenCallingStringToCharArray_shouldConvertToCharArray() {
+                String givenString = "abcd";
 
-        char[] result = Utils.stringToCharArray(givenString);
+                char[] result = Utils.stringToCharArray(givenString);
 
-        assertTrue(Arrays.equals(result, new char[] { 'a', 'b', 'c', 'd' }));
-    }
+                assertTrue(Arrays.equals(result, new char[] { 'a', 'b', 'c', 'd' }));
+        }
 
-    @Test
-    public void givenCharArray_whenCallingCharArrayToString_shouldConvertToString() {
+        @Test public void givenCharArray_whenCallingCharArrayToString_shouldConvertToString() {
 
-        char[] givenCharArray = new char[] { 'a', 'b', 'c', 'd' };
+                char[] givenCharArray = new char[] { 'a', 'b', 'c', 'd' };
 
-        assertThat(Utils.charArrayToString(givenCharArray)).isEqualTo("abcd");
-    }
+                assertThat(Utils.charArrayToString(givenCharArray)).isEqualTo("abcd");
+        }
 
 }
