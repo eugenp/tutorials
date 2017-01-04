2bb1acad66df1d8d1ddbd3d3fbe7a8092ad8f9b0
dsharew <dsharew@apposit.com>
2017-01-04 11:42:13 +0300

    Fixed code format issue.

--- core-java/src/main/java/com/baeldung/algorithms/annealing/City.java
+++ core-java/src/main/java/com/baeldung/algorithms/annealing/City.java
@@ -2,35 +2,36 @@ package com.baeldung.algorithms.annealing;
 
 import lombok.Data;
 
-@Data public class City {
+@Data
+public class City {
 
-        private int x;
-        private int y;
+    private int x;
+    private int y;
 
-        public City() {
-                this.x = (int) (Math.random() * 500);
-                this.y = (int) (Math.random() * 500);
-        }
+    public City() {
+        this.x = (int) (Math.random() * 500);
+        this.y = (int) (Math.random() * 500);
+    }
 
-        public double distanceToCity(City city) {
-                int x = Math.abs(getX() - city.getX());
-                int y = Math.abs(getY() - city.getY());
-                return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-        }
+    public double distanceToCity(City city) {
+        int x = Math.abs(getX() - city.getX());
+        int y = Math.abs(getY() - city.getY());
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    }
 
-        public int getX() {
-                return x;
-        }
+    public int getX() {
+        return x;
+    }
 
-        public void setX(int x) {
-                this.x = x;
-        }
+    public void setX(int x) {
+        this.x = x;
+    }
 
-        public int getY() {
-                return y;
-        }
+    public int getY() {
+        return y;
+    }
 
-        public void setY(int y) {
-                this.y = y;
-        }
+    public void setY(int y) {
+        this.y = y;
+    }
 }
--- core-java/src/test/java/com/baeldung/utils/UtilsTest.java
+++ core-java/src/test/java/com/baeldung/utils/UtilsTest.java
@@ -10,7 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UtilsTest {
 
-    @Test public void givenString_whenCallingStringToCharArray_shouldConvertToCharArray() {
+    @Test
+    public void givenString_whenCallingStringToCharArray_shouldConvertToCharArray() {
         String givenString = "abcd";
 
         char[] result = Utils.stringToCharArray(givenString);
@@ -18,7 +19,8 @@ public class UtilsTest {
         assertTrue(Arrays.equals(result, new char[] { 'a', 'b', 'c', 'd' }));
     }
 
-    @Test public void givenCharArray_whenCallingCharArrayToString_shouldConvertToString() {
+    @Test
+    public void givenCharArray_whenCallingCharArrayToString_shouldConvertToString() {
 
         char[] givenCharArray = new char[] { 'a', 'b', 'c', 'd' };
 
