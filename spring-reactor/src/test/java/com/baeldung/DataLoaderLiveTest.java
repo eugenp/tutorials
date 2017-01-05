8f4ef41eb3d53c40c69a77502b5f96df88b59b77
pivovarit <gpiwowarek@gmail.com>
2017-01-05 18:14:55 +0100

    Refactor DataLoader

--- spring-reactor/src/test/java/com/baeldung/DataLoaderLiveTest.java
+++ spring-reactor/src/test/java/com/baeldung/DataLoaderLiveTest.java
@@ -1,14 +1,9 @@
 package com.baeldung;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { Application.class })
-public class DataLoaderTest {
+public class DataLoaderLiveTest {
 
     @Test
     public void exampleTest() {
