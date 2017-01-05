ba75c373660ac7496fbce1d189fd23f29f3c9ca1
pivovarit <gpiwowarek@gmail.com>
2017-01-05 15:15:53 +0100

    Refactor DataLoader

--- spring-reactor/src/test/java/com/baeldung/DataLoaderTest.java
+++ spring-reactor/src/test/java/com/baeldung/DataLoaderTest.java
@@ -6,15 +6,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {Application.class}) 
-public class DataLoader {
+@ContextConfiguration(classes = { Application.class })
+public class DataLoaderTest {
 
-	@Test
-	public void exampleTest() {
+    @Test
+    public void exampleTest() {
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.getForObject("http://localhost:8080/startNotification/10", String.class);
-	}
-	
+    }
+
 }
