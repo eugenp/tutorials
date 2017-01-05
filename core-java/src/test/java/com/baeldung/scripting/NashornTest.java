2e96b9931b6274771c2b5edffe25c15173d5d041
Danil Kornishev <danil.kornishev@gmail.com>
2017-01-04 19:05:04 -0500

    Change Nashorn to be Junit tests

--- core-java/src/test/java/com/baeldung/scripting/NashornTest.java
+++ core-java/src/test/java/com/baeldung/scripting/NashornTest.java
@@ -1,6 +1,8 @@
 package com.baeldung.scripting;
 
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.io.InputStreamReader;
 import java.util.List;
@@ -12,43 +14,58 @@ import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
-public class Nashorn {
-    public static void main(String[] args) throws ScriptException, NoSuchMethodException {
-        ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+public class NashornTest {
 
+    private ScriptEngine engine;
+
+    @Before
+    public void setUp() {
+        engine = new ScriptEngineManager().getEngineByName("nashorn");
+    }
+
+    @Test
+    public void ok() {
+        System.out.println("~~~~~~~~~~~~~~~~~~");
+    }
+
+    @Test
+    public void trim() throws ScriptException {
+        engine.eval(new InputStreamReader(NashornTest.class.getResourceAsStream("/js/trim.js")));
+    }
+
+    @Test
+    public void locations() throws ScriptException {
+        engine.eval(new InputStreamReader(NashornTest.class.getResourceAsStream("/js/locations.js")));
+    }
+
+    @Test
+    public void bindProperties() throws ScriptException {
+        engine.eval(new InputStreamReader(NashornTest.class.getResourceAsStream("/js/bind.js")));
+    }
+
+    @Test
+    public void magicMethods() throws ScriptException {
+        engine.eval("var demo = load('classpath:js/no_such.js');" +
+                "var tmp = demo.doesNotExist;" +
+                "var none = demo.callNonExistingMethod()");
+    }
+
+    @Test
+    public void typedArrays() throws ScriptException {
+        engine.eval(new InputStreamReader(NashornTest.class.getResourceAsStream("/js/typed_arrays.js")));
+    }
+
+    @Test
+    public void basicUsage() throws ScriptException {
         Object result = engine.eval("var greeting='hello world';" +
                 "print(greeting);" +
                 "greeting");
 
         Assert.assertEquals("hello world", result);
-
-        bindingsExamples(engine);
-
-        jvmBoundaryExamples(engine);
-
-        extensionsExamples(engine);
-
-        loadExamples(engine);
-
-        tryCatchGuard(engine);
-
-        engine.eval(new InputStreamReader(Nashorn.class.getResourceAsStream("/js/typed_arrays.js")));
-
-        engine.eval("var demo = load('classpath:js/no_such.js');" +
-                "var tmp = demo.doesNotExist;" +
-                "var none = demo.callNonExistingMethod()");
-
-        engine.eval(new InputStreamReader(Nashorn.class.getResourceAsStream("/js/bind.js")));
-
-        engine.eval(new InputStreamReader(Nashorn.class.getResourceAsStream("/js/locations.js")));
-
-        engine.eval(new InputStreamReader(Nashorn.class.getResourceAsStream("/js/trim.js")));
-
-        jsonObjectExample(engine);
-
     }
 
-    private static void jsonObjectExample(ScriptEngine engine) throws ScriptException {
+    @Test
+    public void jsonObjectExample() throws ScriptException {
         Object obj = engine.eval("Java.asJSONCompatible({ number: 42, greet: 'hello', primes: [2,3,5,7,11,13] })");
         Map<String, Object> map = (Map<String, Object>) obj;
 
@@ -56,12 +73,14 @@ public class Nashorn {
         Assert.assertTrue(List.class.isAssignableFrom(map.get("primes").getClass()));
     }
 
-    private static void tryCatchGuard(ScriptEngine engine) throws ScriptException {
+    @Test
+    public void tryCatchGuard() throws ScriptException {
         engine.eval("var math = loadWithNewGlobal('classpath:js/math_module.js');" +
                 "math.failFunc();");
     }
 
-    private static void extensionsExamples(ScriptEngine engine) throws ScriptException {
+    @Test
+    public void extensionsExamples() throws ScriptException {
         String script = "var list = [1, 2, 3, 4, 5];" + "var result = '';" +
                 "for each (var i in list) {" +
                 "result+=i+'-';" +
@@ -70,7 +89,8 @@ public class Nashorn {
         engine.eval(script);
     }
 
-    private static void bindingsExamples(ScriptEngine engine) throws ScriptException {
+    @Test
+    public void bindingsExamples() throws ScriptException {
         Bindings bindings = engine.createBindings();
         bindings.put("count", 3);
         bindings.put("name", "baeldung");
@@ -85,7 +105,8 @@ public class Nashorn {
         Assert.assertEquals("Hello baeldung baeldung baeldung ", bindingsResult);
     }
 
-    private static void jvmBoundaryExamples(ScriptEngine engine) throws ScriptException, NoSuchMethodException {
+    @Test
+    public void jvmBoundaryExamples() throws ScriptException, NoSuchMethodException {
         engine.eval("function composeGreeting(name) {" +
                 "return 'Hello ' + name" +
                 "}");
@@ -103,7 +124,8 @@ public class Nashorn {
         Assert.assertTrue(Map.class.isAssignableFrom(map.getClass()));
     }
 
-    private static void loadExamples(ScriptEngine engine) throws ScriptException {
+    @Test
+    public void loadExamples() throws ScriptException {
         Object loadResult = engine.eval("load('classpath:js/script.js');" +
                 "increment(5)");
 
@@ -113,6 +135,5 @@ public class Nashorn {
                 "math.increment(5);");
 
         Assert.assertEquals(6.0, math);
-
     }
 }
