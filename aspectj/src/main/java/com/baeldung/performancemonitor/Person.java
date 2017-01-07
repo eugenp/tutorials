bbcfa086687efc9cb8a97ecff9dff1ec421b46d4
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2017-01-07 17:58:45 +0200

    spring performance monitoring interceptors

---
+++ aspectj/src/main/java/com/baeldung/performancemonitor/Person.java
@@ -0,0 +1,13 @@
+package com.baeldung.performancemonitor;
+
+public class Person {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
