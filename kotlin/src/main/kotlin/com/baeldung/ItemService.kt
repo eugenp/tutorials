7b8d1dc6b2365b62e99039ebfde6eb769ab78c82
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-01-07 12:10:59 +0100

    BAEL-19 break long line

--- kotlin/src/main/kotlin/com/baeldung/ItemService.kt
+++ kotlin/src/main/kotlin/com/baeldung/ItemService.kt
@@ -12,7 +12,8 @@ class ItemService {
 class ItemManager(val categoryId: String, val dbConnection: String) {
     var email = ""
 
-    constructor(categoryId: String, dbConnection: String, email: String) : this(categoryId, dbConnection) {
+    constructor(categoryId: String, dbConnection: String, email: String)
+    : this(categoryId, dbConnection) {
         this.email = email
     }
 
