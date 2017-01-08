9bb3a09b737ca0f0f92c9933ff2ebf2132087872
Nikhil Khatwani <nkhatwani85@gmail.com>
2017-01-08 23:38:27 +0530

    Review Comments

--- spring-amqp/src/main/java/com/baeldung/springamqp/producer/Producer.java
+++ spring-amqp/src/main/java/com/baeldung/springamqp/producer/Producer.java
@@ -1,5 +1,6 @@
 package com.baeldung.springamqp.producer;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,7 +9,7 @@ public class Producer {
 
     public static void main(String[] args) throws InterruptedException {
         AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-        RabbitTemplate template = ctx.getBean(RabbitTemplate.class);
+        AmqpTemplate template = ctx.getBean(RabbitTemplate.class);
         template.convertAndSend("Hello, world!");
         Thread.sleep(1000);
         ctx.destroy();
