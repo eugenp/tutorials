adccc13c322a8c1be3e811bf6d6586dac9010eff
slavisa-baeldung <slavisa.baeldung@gmail.com>
2017-01-08 16:46:47 +0100

    BAEL-97 - simplifying code

--- spring-mvc-java/src/main/java/com/baeldung/spring/web/config/HandlerMappingPrioritiesConfigNoOrder.java
+++ spring-mvc-java/src/main/java/com/baeldung/spring/web/config/HandlerMappingPrioritiesConfigNoOrder.java
@@ -1,8 +1,5 @@
 package com.baeldung.spring.web.config;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.baeldung.web.controller.handlermapping.SimpleUrlMappingController;
 import com.baeldung.web.controller.handlermapping.BeanNameHandlerMappingController;
 import com.baeldung.web.controller.handlermapping.WelcomeController;
@@ -10,34 +7,48 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.BeanNameUrlHandlerMapping;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.mvc.support.ControllerClassNameHandlerMapping;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
-public class HandlerMappingPrioritiesConfig {
-
-    @Bean
-    BeanNameUrlHandlerMapping beanNameUrlHandlerMapping() {
-        BeanNameUrlHandlerMapping beanNameUrlHandlerMapping = new BeanNameUrlHandlerMapping();
-        beanNameUrlHandlerMapping.setOrder(0);
-        return beanNameUrlHandlerMapping;
-    }
+public class HandlerMappingPrioritiesConfigNoOrder {
 
     @Bean
     public SimpleUrlHandlerMapping simpleUrlHandlerMapping() {
         SimpleUrlHandlerMapping simpleUrlHandlerMapping = new SimpleUrlHandlerMapping();
         Map<String, Object> urlMap = new HashMap<>();
-        urlMap.put("/welcome", simpleUrlMapping());
+        urlMap.put("/welcome", test());
         simpleUrlHandlerMapping.setUrlMap(urlMap);
         return simpleUrlHandlerMapping;
     }
 
     @Bean
-    public SimpleUrlMappingController simpleUrlMapping() {
-        return new SimpleUrlMappingController();
+    public ControllerClassNameHandlerMapping controllerClassNameHandlerMapping() {
+        ControllerClassNameHandlerMapping bean = new ControllerClassNameHandlerMapping();
+        return bean;
+    }
+
+    @Bean
+    BeanNameUrlHandlerMapping beanNameUrlHandlerMapping() {
+        BeanNameUrlHandlerMapping beanNameUrlHandlerMapping = new BeanNameUrlHandlerMapping();
+        return beanNameUrlHandlerMapping;
     }
 
     @Bean("/welcome")
-    public BeanNameHandlerMappingController beanNameHandlerMapping() {
+    public SimpleUrlMappingController welcomeBaeldungController() {
+        return new SimpleUrlMappingController();
+    }
+
+    @Bean
+    public BeanNameHandlerMappingController test() {
         return new BeanNameHandlerMappingController();
+    }
+
+    @Bean
+    public WelcomeController welcomeController() {
+        return new WelcomeController();
     }
 }
