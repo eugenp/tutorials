6e06115db0d486681a761226ec7a5c8cd2e4c6f2
Shubham Aggarwal <sbmaggarwal@gmail.com>
2017-01-09 11:04:36 +0530

    Added mongo driver dependencies.

--- java-mongodb/src/main/java/com/baeldung/App.java
+++ java-mongodb/src/main/java/com/baeldung/App.java
@@ -6,8 +6,8 @@ package com.baeldung;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        
+    	
     }
 }
