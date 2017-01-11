39e7ebe35fa7202f099f38aa6923d4fad2f7d801
danidemi <escher75@gmail.com>
2017-01-11 22:53:36 +0100

    java.time.* instead of java.util.Date

--- spring-remoting/remoting-http/server/src/main/java/com/baeldung/server/CabBookingServiceImpl.java
+++ spring-remoting/remoting-http/server/src/main/java/com/baeldung/server/CabBookingServiceImpl.java
@@ -15,13 +15,13 @@ import static java.util.UUID.randomUUID;
 public class CabBookingServiceImpl implements CabBookingService {
 
     @Override public Booking bookPickUp(Address pickUpLocation, Address dropOffLocation, int pax) throws BookingException {
-        if(random()<0.3){
+        if (random() < 0.3) {
             throw new BookingException("Cab unavailable");
         }
         int tripTimeInMinutes = (int) (5 + random() * 15);
         int costInCent = 15_00 + tripTimeInMinutes * 5 * pax;
-        LocalDateTime pickUpDate = now().plus(15L * (long) (random()*10), MINUTES);
-        return new Booking(pickUpLocation, pickUpDate, dropOffLocation, costInCent, tripTimeInMinutes * 60,
-                randomUUID().toString());
+        LocalDateTime pickUpDate = now().plus(15L * (long) (random() * 10), MINUTES);
+
+        return new Booking(pickUpLocation, pickUpDate, dropOffLocation, costInCent, tripTimeInMinutes * 60, randomUUID().toString());
     }
 }
