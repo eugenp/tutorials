ea2d0459cb070e65a81f51b82b4db2b09cfd37cb
dsharew <dsharew@apposit.com>
2017-01-11 16:28:41 +0300

    Moved code to test class

--- core-java/src/test/java/com/baeldung/chartoarray/CharArrayUtilsTest.java
+++ core-java/src/test/java/com/baeldung/chartoarray/CharArrayUtilsTest.java
@@ -11,35 +11,44 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CharArrayUtilsTest {
 
     @Test
-    public void givenString_whenCallingStringToCharArray1_shouldConvertToCharArray() {
+    public void givenString_whenUsingToCharArray_thenCorrect() {
         String givenString = "abcd";
 
-        char[] result = CharArrayUtils.stringToCharArray1(givenString);
+        char[] result = givenString.toCharArray();
 
         assertTrue(Arrays.equals(result, new char[] { 'a', 'b', 'c', 'd' }));
     }
 
     @Test
-    public void givenString_whenCallingStringToCharArray2_shouldConvertToCharArray() {
+    public void givenString_whenUsingIteration_thenCorrect() {
         String givenString = "abcd";
 
-        char[] result = CharArrayUtils.stringToCharArray2(givenString);
+        char[] charArray = new char[givenString.length()];
 
-        assertTrue(Arrays.equals(result, new char[] { 'a', 'b', 'c', 'd' }));
+        for(int i = 0; i < givenString.length(); ++i)
+            charArray[i] = givenString.charAt(i);;
+
+        assertTrue(Arrays.equals(charArray, new char[] { 'a', 'b', 'c', 'd' }));
     }
 
     @Test
-    public void givenCharArray_whenCallingCharArrayToString1_shouldConvertToString() {
+    public void givenCharArray_whenUsingStringBuilder_thenCorrect() {
         char[] givenCharArray = new char[] { 'a', 'b', 'c', 'd' };
 
-        assertThat(CharArrayUtils.charArrayToString1(givenCharArray)).isEqualTo("abcd");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (char c : givenCharArray) {
+            stringBuilder.append(c);
+        }
+
+        assertThat(stringBuilder.toString()).isEqualTo("abcd");
     }
 
     @Test
-    public void givenCharArray_whenCallingCharArrayToString2_shouldConvertToString() {
+    public void givenCharArray_whenUsingValueOf_thenCorrect() {
         char[] givenCharArray = new char[] { 'a', 'b', 'c', 'd' };
 
-        assertThat(CharArrayUtils.charArrayToString2(givenCharArray)).isEqualTo("abcd");
+        assertThat(String.valueOf(givenCharArray)).isEqualTo("abcd");
     }
 
 }
