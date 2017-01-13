c010a239e7f4d1a01231631aebf2e920dd38524d
gatmeister <almarissa.elemino@gmail.com>
2017-01-14 01:35:54 +0800

    BAEL-112 | Spring MVC Validation - a Custom Validator
    Reverts the changes made to Employee and Employee Controller. Created a
    new class, Customer as implementation for custom validation

--- spring-mvc-forms/src/main/java/com/baeldung/springmvcforms/controller/EmployeeController.java
+++ spring-mvc-forms/src/main/java/com/baeldung/springmvcforms/controller/EmployeeController.java
@@ -1,24 +1,25 @@
 package com.baeldung.springmvcforms.controller;
 
-import com.baeldung.springmvcforms.domain.Employee;
-import com.baeldung.springmvcforms.validator.EmployeeValidator;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.baeldung.springmvcforms.domain.Employee;
+
 @Controller
 public class EmployeeController {
-    
-    @Autowired
-    EmployeeValidator validator;
 
     Map<Long, Employee> employeeMap = new HashMap<>();
 
@@ -34,11 +35,10 @@ public class EmployeeController {
 
     @PostMapping("/addEmployee")
     public String submit(@Valid @ModelAttribute("employee") final Employee employee, final BindingResult result, final ModelMap model) {
-        validator.validate(employee, result);
         if (result.hasErrors()) {
             return "employeeHome";
         }
-        
+
         model.addAttribute("name", employee.getName());
         model.addAttribute("contactNumber", employee.getContactNumber());
         model.addAttribute("id", employee.getId());
