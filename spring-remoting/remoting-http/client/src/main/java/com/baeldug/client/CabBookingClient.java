46d244435e6a389610aeaa8fc800b26a12513a76
danidemi <escher75@gmail.com>
2017-01-16 22:29:25 +0100

    Exception removed from run()

--- spring-remoting/remoting-http/client/src/main/java/com/baeldug/client/CabBookingClient.java
+++ spring-remoting/remoting-http/client/src/main/java/com/baeldug/client/CabBookingClient.java
@@ -10,15 +10,11 @@ public class CabBookingClient {
         this.cabService = cabService;
     }
 
-    public void run() {
+    public void run() throws BookingException {
 
         Address pickUp = new Address("13 Seagate Blvd, Key Largo, FL 33037", "US");
         Address dropDown = new Address("91831 Overseas Hwy, Tavernier, FL 33070", "US");
-        try {
-            System.out.println( cabService.bookPickUp(pickUp, dropDown, 3) );
-        } catch (BookingException e) {
-            e.printStackTrace();
-        }
+        System.out.println( cabService.bookPickUp(pickUp, dropDown, 3) );
 
     }
 
