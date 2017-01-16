bae4e04005487a5bf08c31da1d095d0e2cb60018
Saptarshi Basu <sapb2004@gmail.com>
2017-01-16 16:49:06 +0530

    messagebody readers and writers removed

--- spring-jersey/src/main/java/com/baeldung/client/rest/RestClient.java
+++ spring-jersey/src/main/java/com/baeldung/client/rest/RestClient.java
@@ -7,10 +7,6 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import com.baeldung.client.messagebodyreader.JsonEmployeeReader;
-import com.baeldung.client.messagebodyreader.XmlEmployeeReader;
-import com.baeldung.client.messagebodywriter.JsonEmployeeWriter;
-import com.baeldung.client.messagebodywriter.XmlEmployeeWriter;
 import com.baeldung.server.model.Employee;
 
 public class RestClient {
@@ -19,20 +15,20 @@ public class RestClient {
     private Client client = ClientBuilder.newClient();
 
     public Response createJsonEmployee(Employee emp) {
-        return client.target(REST_URI).register(JsonEmployeeWriter.class).request(MediaType.APPLICATION_JSON).post(Entity.entity(emp, MediaType.APPLICATION_JSON));
+        return client.target(REST_URI).request(MediaType.APPLICATION_JSON).post(Entity.entity(emp, MediaType.APPLICATION_JSON));
     }
 
     public Employee getJsonEmployee(int id) {
-        return client.target(REST_URI).register(JsonEmployeeReader.class).path(new Integer(id).toString()).request(MediaType.APPLICATION_JSON).get(new GenericType<Employee>() {
+        return client.target(REST_URI).path(new Integer(id).toString()).request(MediaType.APPLICATION_JSON).get(new GenericType<Employee>() {
         });
     }
 
     public Response createXmlEmployee(Employee emp) {
-        return client.target(REST_URI).register(XmlEmployeeWriter.class).request(MediaType.APPLICATION_XML).post(Entity.entity(emp, MediaType.APPLICATION_XML));
+        return client.target(REST_URI).request(MediaType.APPLICATION_XML).post(Entity.entity(emp, MediaType.APPLICATION_XML));
     }
 
     public Employee getXmlEmployee(int id) {
-        return client.target(REST_URI).register(XmlEmployeeReader.class).path(new Integer(id).toString()).request(MediaType.APPLICATION_XML).get(new GenericType<Employee>() {
+        return client.target(REST_URI).path(new Integer(id).toString()).request(MediaType.APPLICATION_XML).get(new GenericType<Employee>() {
         });
     }
 }
