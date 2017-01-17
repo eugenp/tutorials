9df3a2e86605f52ba89400cf0eb5d6830708a0bc
Shishir Kumar <shishir_kumar@apple.com>
2017-01-17 11:04:33 +0530

    Changes to demonstrate Field Injection vs Constructor Injection in Spring

--- spring-userservice/src/main/java/org/baeldung/user/service/MyUserService.java
+++ spring-userservice/src/main/java/org/baeldung/user/service/MyUserService.java
@@ -10,13 +10,22 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class MyUserService {
+public class MyUserService implements UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
     @Autowired
     MyUserDAO myUserDAO;
+
+    /*
+     * (non-Javadoc)
+     * @see org.baeldung.user.service.UserService#getUserByUsername(java.lang.String)
+     */
+    @Override
+    public MyUser getUserByUsername(final String username) {
+        return myUserDAO.findByUsername(username);
+    }
 
     public MyUser registerNewUserAccount(final MyUserDto accountDto) throws Exception {
         if (usernameExists(accountDto.getUsername())) {
@@ -29,12 +38,7 @@ public class MyUserService {
         return myUserDAO.save(user);
     }
 
-    public MyUser getUserByUsername(final String username) {
-        final MyUser user = myUserDAO.findByUsername(username);
-        return user;
-    }
-    
-    public void removeUserByUsername(String username){
+    public void removeUserByUsername(String username) {
         myUserDAO.removeUserByUsername(username);
     }
 
