c60c8705060224c6f606cb7428747278638e3945
Andrew Morgan <andrew.morgan@opencredo.com>
2017-01-18 22:50:56 +0000

    BAEL-613

---
+++ core-java/src/test/java/com/baeldung/concurrent/countdownlatch/CountdownLatchExampleTest.java
@@ -0,0 +1,64 @@
+package com.baeldung.concurrent.countdownlatch;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CountdownLatchExampleTest {
+    @Test
+    public void shouldBlockUntilLatchIsReleased() throws InterruptedException {
+
+        // Given
+        List<String> outputScraper = Collections.synchronizedList(new ArrayList<>());
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        List<Thread> workers = Stream
+          .generate(() -> new Thread(new Worker(outputScraper, countDownLatch)))
+          .limit(5)
+          .collect(toList());
+
+        // When
+        workers.forEach(Thread::start);
+        countDownLatch.await(); // Block until workers finish
+        outputScraper.add("Latch released");
+
+        // Then
+        outputScraper.forEach(Object::toString);
+        assertThat(outputScraper)
+          .containsExactly(
+            "Counted down",
+            "Counted down",
+            "Counted down",
+            "Counted down",
+            "Counted down",
+            "Latch released"
+          );
+    }
+
+    @Test
+    public void shouldEventuallyTimeout() throws InterruptedException {
+        // Given
+        List<String> outputScraper = Collections.synchronizedList(new ArrayList<>());
+        CountDownLatch countDownLatch = new CountDownLatch(5);
+        List<Thread> workers = Stream
+          .generate(() -> new Thread(new BrokenWorker(outputScraper, countDownLatch)))
+          .limit(5)
+          .collect(toList());
+
+        // When
+        workers.forEach(Thread::start);
+        final boolean result = countDownLatch.await(3L, TimeUnit.SECONDS);
+
+        // Then
+        assertThat(result).isTrue();
+    }
+}
