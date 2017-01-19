03902d250731ab61aad1493be72aeb13bc1c6181
Stephen Braimah <stephen.braimah@gmail.com>
2017-01-19 09:53:56 +0000

    BAEL-583: Corrected naming for parallel stream test case

--- core-java/src/test/java/com/baeldung/java8/Java8FindAnyFindFirstTest.java
+++ core-java/src/test/java/com/baeldung/java8/Java8FindAnyFindFirstTest.java
@@ -24,7 +24,7 @@ public class Java8FindAnyFindFirstTest {
     }
 
     @Test
-    public void createParallelStream_whenFindAnyResultIsNotFirst_ThenCorrect() throws Exception {
+    public void createParallelStream_whenFindAnyResultIsPresent_ThenCorrect() throws Exception {
         List<Integer> list = Arrays.asList(1,2,3,4,5);
         Optional<Integer> result = list
                 .stream()
