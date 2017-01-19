ce2acc8ea2db0aeed2c5ef2294219f39294637f6
maverick <joshiparthin@gmail.com>
2017-01-19 16:49:27 +0530

    Added Application/json in response type.

--- spark-java/src/main/java/com/baeldung/sparkjava/SparkRestExample.java
+++ spark-java/src/main/java/com/baeldung/sparkjava/SparkRestExample.java
@@ -13,6 +13,8 @@ public class SparkRestExample {
         final UserService userService = new UserServiceMapImpl();
         
         post("/users", (request, response) -> {
+            response.type("application/json");
+            
             User user = new Gson().fromJson(request.body(), User.class);
             userService.addUser(user);
 
@@ -20,40 +22,49 @@ public class SparkRestExample {
         });
 
         get("/users", (request, response) -> {
+            response.type("application/json");
+            
             return new Gson().toJson(
                 new StandardResponse(StatusResponse.SUCCESS,new Gson()
                     .toJsonTree(userService.getUsers())));
         });
 
         get("/users/:id", (request, response) -> {
+            response.type("application/json");
+            
             return new Gson().toJson(
                 new StandardResponse(StatusResponse.SUCCESS,new Gson()
                   .toJsonTree(userService.getUser(request.params(":id")))));
         });
 
         put("/users/:id", (request, response) -> {
-                User toEdit = new Gson().fromJson(request.body(), User.class);
-                User editedUser = userService.editUser(toEdit);
-                
-                if (editedUser != null) {
-                    return new Gson().toJson(
-                        new StandardResponse(StatusResponse.SUCCESS,new Gson()
-                            .toJsonTree(editedUser)));
-                }else  {
-                    return new Gson().toJson(
-                        new StandardResponse(StatusResponse.ERROR,new Gson()
-                            .toJson("User not found or error in edit")));
-                }
+            response.type("application/json");
             
+            User toEdit = new Gson().fromJson(request.body(), User.class);
+            User editedUser = userService.editUser(toEdit);
+                
+            if (editedUser != null) {
+                return new Gson().toJson(
+                    new StandardResponse(StatusResponse.SUCCESS,new Gson()
+                        .toJsonTree(editedUser)));
+            }else  {
+                return new Gson().toJson(
+                    new StandardResponse(StatusResponse.ERROR,new Gson()
+                        .toJson("User not found or error in edit")));
+            }
         });
 
         delete("/users/:id", (request, response) -> {
+            response.type("application/json");
+            
             userService.deleteUser(request.params(":id"));
             return new Gson().toJson(
                 new StandardResponse(StatusResponse.SUCCESS, "user deleted"));
         });
 
         options("/users/:id", (request, response) -> {
+            response.type("application/json");
+            
             return new Gson().toJson(
                 new StandardResponse(StatusResponse.SUCCESS, 
                     (userService.userExist(
