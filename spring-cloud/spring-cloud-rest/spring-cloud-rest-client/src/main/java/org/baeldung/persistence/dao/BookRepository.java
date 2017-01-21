60f5dd09381317849e44abf998a6a173e0c07aba
DOHA <dohae243@gmail.com>
2017-01-21 12:02:06 +0200

    add live test

--- spring-cloud/spring-cloud-rest/spring-cloud-rest-client/src/main/java/org/baeldung/persistence/dao/BookRepository.java
+++ spring-cloud/spring-cloud-rest/spring-cloud-rest-client/src/main/java/org/baeldung/persistence/dao/BookRepository.java
@@ -1,10 +1,13 @@
 package org.baeldung.persistence.dao;
 
 import org.baeldung.persistence.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(collectionResourceRel = "books", path = "books")
-public interface BookRepository extends JpaRepository<Book, Long> {
-
+public interface BookRepository extends CrudRepository<Book, Long> {
+    Page<Book> findByTitle(@Param("title") String title, Pageable pageable);
 }
