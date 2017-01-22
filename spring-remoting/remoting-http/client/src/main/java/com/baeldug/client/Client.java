6f870ab5f9a9ee57ec3979fa1b4b07ef2d2745a4
danidemi <escher75@gmail.com>
2017-01-22 16:25:15 +0100

    Unused classes removed.

--- spring-remoting/remoting-http/client/src/main/java/com/baeldug/client/Client.java
+++ spring-remoting/remoting-http/client/src/main/java/com/baeldug/client/Client.java
@@ -1,10 +1,8 @@
 package com.baeldug.client;
 
-import com.baeldung.api.Booking;
 import com.baeldung.api.BookingException;
 import com.baeldung.api.CabBookingService;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.remoting.httpinvoker.HttpInvokerProxyFactoryBean;
@@ -23,9 +21,8 @@ public class Client {
     }
 
     public static void main(String[] args) throws BookingException {
-        ConfigurableApplicationContext ctx = SpringApplication.run(Client.class, args);
-        Booking booking = ctx.getBean(CabBookingService.class).bookRide("13 Seagate Blvd, Key Largo, FL 33037");
-        out.println(booking);
+        CabBookingService service = SpringApplication.run(Client.class, args).getBean(CabBookingService.class);
+        out.println(service.bookRide("13 Seagate Blvd, Key Largo, FL 33037"));
     }
 
 }
