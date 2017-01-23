b7482176a40f337c1d00fc7325b4499f57971ecd
dsharew <dsharew@apposit.com>
2017-01-23 17:33:06 +0300

    Added direct POI dependency and exclude POI from BIRT lib

--- spring-boot-birt-report/src/main/java/com/baeldung/birt/repository/CustomerRepository.java
+++ spring-boot-birt-report/src/main/java/com/baeldung/birt/repository/CustomerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer, String> {
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
 
 }
