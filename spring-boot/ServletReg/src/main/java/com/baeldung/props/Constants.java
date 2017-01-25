57958b46407e89b21ffb10f26aa76d73fa07d06b
Adam Gerard <Adam Gerard>
2017-01-25 10:09:49 +0000

    Servlet Reg

---
+++ spring-boot/ServletReg/src/main/java/com/baeldung/props/Constants.java
@@ -0,0 +1,27 @@
+package com.baeldung.props;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Properties;
+
+public final class Constants {
+
+      /**  @Autowired
+        PropertySourcesLoader psl;
+
+        //Reuse
+        public static final String breakLine = System.getProperty("line.separator");
+
+        //Prop Loader Resources
+        private static final PropertyLoader pl = new PropertyLoader();
+        private static final Properties mainProps = pl.getProperties("custom.properties");
+
+        //Dispatcher Servlet
+        public static final String DISPATCHER_SERVLET_NAME = mainProps.getProperty("dispatcher.servlet.name");
+        public static final String DISPATCHER_SERVLET_MAPPING = mainProps.getProperty("dispatcher.servlet.mapping");
+
+        //PropertySource Loader
+        private final String EXAMPLE_SERVLET_NAME = psl.getProperty("example.servlet.name");
+        private final String EXAMPLE_SERVLET_MAPPING = psl.getProperty("example.servlet.mapping"); */
+
+}
