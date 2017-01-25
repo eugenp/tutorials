7870c613b62f4285599758dd511b71787a43d982
DOHA <dohae243@gmail.com>
2017-01-25 20:47:33 +0200

    rename server

--- spring-cloud/spring-cloud-rest/spring-cloud-rest-config/src/main/java/org/baeldung/SpringCloudRestConfigApplication.java
+++ spring-cloud/spring-cloud-rest/spring-cloud-rest-config/src/main/java/org/baeldung/SpringCloudRestConfigApplication.java
@@ -3,11 +3,10 @@ package org.baeldung;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
+// @EnableEurekaClient
 public class SpringCloudRestConfigApplication {
 
     public static void main(String[] args) {
