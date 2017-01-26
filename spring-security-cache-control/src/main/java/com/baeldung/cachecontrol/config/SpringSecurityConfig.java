292c11293626704e0459b4720694c5383a54b947
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-01-26 20:18:23 +0100

    BAEL-542 use defaults

--- spring-security-cache-control/src/main/java/com/baeldung/cachecontrol/config/SpringSecurityConfig.java
+++ spring-security-cache-control/src/main/java/com/baeldung/cachecontrol/config/SpringSecurityConfig.java
@@ -14,10 +14,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().permitAll()
-                .and().headers()
+        http.headers()
                 .defaultsDisabled()
                 .cacheControl();
     }
