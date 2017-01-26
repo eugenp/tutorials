c3cc42f458e92afcad3a906513a1358f5ca8fb32
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-01-26 20:34:40 +0100

    BAEL-542 wrote live tests

---
+++ spring-security-cache-control/src/test/java/com/baeldung/cachecontrol/TestConfig.java
@@ -0,0 +1,11 @@
+package com.baeldung.cachecontrol;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan({ "com.baeldung" })
+public class TestConfig {
+
+}
