fb61d1b187bf173065d1690083fb6ef724b63d61
Pedja <predrag4@gmail.com>
2017-01-27 12:24:31 +0100

    BAEL-614 Changed to use ThreadLocalRandom instead of Random

--- core-java/src/main/java/com/baeldung/concurrent/blockingqueue/NumbersProducer.java
+++ core-java/src/main/java/com/baeldung/concurrent/blockingqueue/NumbersProducer.java
@@ -1,10 +1,10 @@
 package com.baeldung.concurrent.blockingqueue;
 
-import java.util.Random;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class NumbersProducer implements Runnable {
-    private final Random random = new Random();
+
     private final BlockingQueue<Integer> numbersQueue;
 
     public NumbersProducer(BlockingQueue<Integer> numbersQueue) {
@@ -21,7 +21,7 @@ public class NumbersProducer implements Runnable {
 
     private void generateNumbers() throws InterruptedException {
         for (int i = 0; i < 100; i++) {
-            numbersQueue.put(random.nextInt(100));
+            numbersQueue.put(ThreadLocalRandom.current().nextInt(100));
         }
     }
 }
