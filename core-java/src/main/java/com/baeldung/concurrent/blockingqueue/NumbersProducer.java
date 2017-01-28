2c0494dd03a08ecfb6bcd7657c8de4d915f986c7
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-01-28 10:45:57 +0100

    BAEL-614 introduced poison pill message to stop our producers form run indefinitely

--- core-java/src/main/java/com/baeldung/concurrent/blockingqueue/NumbersProducer.java
+++ core-java/src/main/java/com/baeldung/concurrent/blockingqueue/NumbersProducer.java
@@ -8,10 +8,14 @@ import java.util.concurrent.BlockingQueue;
 class NumbersProducer implements Runnable {
     private final Random random = new Random();
     private final BlockingQueue<Integer> numbersQueue;
+    private final int poisonPill;
+    private final int poisonPillPerProducer;
 
 
-    public NumbersProducer(BlockingQueue<Integer> numbersQueue) {
+    public NumbersProducer(BlockingQueue<Integer> numbersQueue, int poisonPill, int poisonPillPerProducer) {
         this.numbersQueue = numbersQueue;
+        this.poisonPill = poisonPill;
+        this.poisonPillPerProducer = poisonPillPerProducer;
     }
 
 
@@ -26,6 +30,9 @@ class NumbersProducer implements Runnable {
     private void generateNumbers() throws InterruptedException {
         for (int i = 0; i < 100; i++) {
             numbersQueue.put(random.nextInt(100));
+        }
+        for (int j = 0; j < poisonPillPerProducer; j++) {
+            numbersQueue.put(poisonPill);
         }
     }
 }
