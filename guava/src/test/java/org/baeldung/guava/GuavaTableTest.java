6ca88b3b354682f7a2a72ec3cb85029ff7ebf722
eralmas7 <eralmas7@yahoo.com>
2017-01-29 14:50:43 +0530

    BAEL-588 Test to support Guava's Table.

--- guava/src/test/java/org/baeldung/guava/GuavaTableTest.java
+++ guava/src/test/java/org/baeldung/guava/GuavaTableTest.java
@@ -17,13 +17,13 @@ public class GuavaTableTest {
     @Test
     public void givenTable_whenGet_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
-        final int seatCount = universityCourseSeatTable.get("Mumbai University", "IT");
-        final Integer seatCountForNoEntry = universityCourseSeatTable.get("Oxford University", "IT");
+        final int seatCount = universityCourseSeatTable.get("Mumbai", "IT");
+        final Integer seatCountForNoEntry = universityCourseSeatTable.get("Oxford", "IT");
 
         assertThat(seatCount).isEqualTo(60);
         assertThat(seatCountForNoEntry).isEqualTo(null);
@@ -32,15 +32,15 @@ public class GuavaTableTest {
     @Test
     public void givenTable_whenContains_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
-        final boolean entryIsPresent = universityCourseSeatTable.contains("Mumbai University", "IT");
-        final boolean entryIsAbsent = universityCourseSeatTable.contains("Oxford University", "IT");
+        final boolean entryIsPresent = universityCourseSeatTable.contains("Mumbai", "IT");
+        final boolean entryIsAbsent = universityCourseSeatTable.contains("Oxford", "IT");
         final boolean courseIsPresent = universityCourseSeatTable.containsColumn("IT");
-        final boolean universityIsPresent = universityCourseSeatTable.containsRow("Mumbai University");
+        final boolean universityIsPresent = universityCourseSeatTable.containsRow("Mumbai");
         final boolean seatCountIsPresent = universityCourseSeatTable.containsValue(60);
 
         assertThat(entryIsPresent).isEqualTo(true);
@@ -53,37 +53,37 @@ public class GuavaTableTest {
     @Test
     public void givenTable_whenRemove_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
 
-        final int seatCount = universityCourseSeatTable.remove("Mumbai University", "IT");
+        final int seatCount = universityCourseSeatTable.remove("Mumbai", "IT");
 
         assertThat(seatCount).isEqualTo(60);
-        assertThat(universityCourseSeatTable.remove("Mumbai University", "IT")).isEqualTo(null);
+        assertThat(universityCourseSeatTable.remove("Mumbai", "IT")).isEqualTo(null);
     }
 
     @Test
     public void givenTable_whenColumn_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
         final Map<String, Integer> universitySeatMap = universityCourseSeatTable.column("IT");
 
         assertThat(universitySeatMap).hasSize(2);
-        assertThat(universitySeatMap.get("Mumbai University")).isEqualTo(60);
-        assertThat(universitySeatMap.get("Harvard University")).isEqualTo(120);
+        assertThat(universitySeatMap.get("Mumbai")).isEqualTo(60);
+        assertThat(universitySeatMap.get("Harvard")).isEqualTo(120);
     }
 
     @Test
     public void givenTable_whenColumnMap_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
         final Map<String, Map<String, Integer>> courseKeyUniversitySeatMap = universityCourseSeatTable.columnMap();
 
@@ -96,12 +96,12 @@ public class GuavaTableTest {
     @Test
     public void givenTable_whenRow_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
-        final Map<String, Integer> courseSeatMap = universityCourseSeatTable.row("Mumbai University");
+        final Map<String, Integer> courseSeatMap = universityCourseSeatTable.row("Mumbai");
 
         assertThat(courseSeatMap).hasSize(2);
         assertThat(courseSeatMap.get("IT")).isEqualTo(60);
@@ -111,10 +111,10 @@ public class GuavaTableTest {
     @Test
     public void givenTable_whenRowKeySet_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
         final Set<String> universitySet = universityCourseSeatTable.rowKeySet();
 
@@ -124,10 +124,10 @@ public class GuavaTableTest {
     @Test
     public void givenTable_whenColKeySet_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = HashBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
         final Set<String> courseSet = universityCourseSeatTable.columnKeySet();
 
@@ -137,37 +137,37 @@ public class GuavaTableTest {
     @Test
     public void givenTreeTable_whenGet_returnsSuccessfully() {
         final Table<String, String, Integer> universityCourseSeatTable = TreeBasedTable.create();
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
-        final int seatCount = universityCourseSeatTable.get("Mumbai University", "IT");
+        final int seatCount = universityCourseSeatTable.get("Mumbai", "IT");
 
         assertThat(seatCount).isEqualTo(60);
     }
 
     @Test
     public void givenImmutableTable_whenGet_returnsSuccessfully() {
-        final Table<String, String, Integer> universityCourseSeatTable = ImmutableTable.<String, String, Integer> builder().put("Mumbai University", "Chemical", 120).put("Mumbai University", "IT", 60).put("Harvard University", "Electrical", 60)
-                .put("Harvard University", "IT", 120).build();
+        final Table<String, String, Integer> universityCourseSeatTable = ImmutableTable.<String, String, Integer> builder().put("Mumbai", "Chemical", 120).put("Mumbai", "IT", 60).put("Harvard", "Electrical", 60)
+                .put("Harvard", "IT", 120).build();
 
-        final int seatCount = universityCourseSeatTable.get("Mumbai University", "IT");
+        final int seatCount = universityCourseSeatTable.get("Mumbai", "IT");
 
         assertThat(seatCount).isEqualTo(60);
     }
 
     @Test
     public void givenArrayTable_whenGet_returnsSuccessfully() {
-        final List<String> universityRowTable = Lists.newArrayList("Mumbai University", "Harvard University");
+        final List<String> universityRowTable = Lists.newArrayList("Mumbai", "Harvard");
         final List<String> courseColumnTables = Lists.newArrayList("Chemical", "IT", "Electrical");
         final Table<String, String, Integer> universityCourseSeatTable = ArrayTable.create(universityRowTable, courseColumnTables);
-        universityCourseSeatTable.put("Mumbai University", "Chemical", 120);
-        universityCourseSeatTable.put("Mumbai University", "IT", 60);
-        universityCourseSeatTable.put("Harvard University", "Electrical", 60);
-        universityCourseSeatTable.put("Harvard University", "IT", 120);
+        universityCourseSeatTable.put("Mumbai", "Chemical", 120);
+        universityCourseSeatTable.put("Mumbai", "IT", 60);
+        universityCourseSeatTable.put("Harvard", "Electrical", 60);
+        universityCourseSeatTable.put("Harvard", "IT", 120);
 
-        final int seatCount = universityCourseSeatTable.get("Mumbai University", "IT");
+        final int seatCount = universityCourseSeatTable.get("Mumbai", "IT");
 
         assertThat(seatCount).isEqualTo(60);
     }
