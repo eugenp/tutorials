0d28961d3d7108dd91f0aabbacff95474360163b
Adam Gerard <Adam Gerard>
2017-01-31 18:31:27 +0000

    BAEL-9: Comments Removed

--- spring-boot/ServletReg/src/main/java/com/baeldung/props/Constants.java
+++ spring-boot/ServletReg/src/main/java/com/baeldung/props/Constants.java
@@ -6,22 +6,18 @@ import java.util.Properties;
 
 public final class Constants {
 
-      /**  @Autowired
-        PropertySourcesLoader psl;
 
-        //Reuse
+        @Autowired PropertySourcesLoader psl;
+
         public static final String breakLine = System.getProperty("line.separator");
 
-        //Prop Loader Resources
         private static final PropertyLoader pl = new PropertyLoader();
         private static final Properties mainProps = pl.getProperties("custom.properties");
 
-        //Dispatcher Servlet
         public static final String DISPATCHER_SERVLET_NAME = mainProps.getProperty("dispatcher.servlet.name");
         public static final String DISPATCHER_SERVLET_MAPPING = mainProps.getProperty("dispatcher.servlet.mapping");
 
-        //PropertySource Loader
         private final String EXAMPLE_SERVLET_NAME = psl.getProperty("example.servlet.name");
-        private final String EXAMPLE_SERVLET_MAPPING = psl.getProperty("example.servlet.mapping"); */
+        private final String EXAMPLE_SERVLET_MAPPING = psl.getProperty("example.servlet.mapping");
 
 }
