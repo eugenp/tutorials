9df2a49b165baafa183295434e3aa5c84daf3041
Chima Ejiofor <Chima Ejiofor>
2017-02-01 14:00:03 +0100

    Fixed Unit test for Hibernate one to many mapping tutorial

--- spring-hibernate4/src/test/java/com/baeldung/hibernate/oneToMany/main/HibernateOneToManyAnnotationMainTest.java
+++ spring-hibernate4/src/test/java/com/baeldung/hibernate/oneToMany/main/HibernateOneToManyAnnotationMainTest.java
@@ -1,12 +1,23 @@
 
 package com.baeldung.hibernate.oneToMany.main;
 
+import com.baeldung.hibernate.oneToMany.config.HibernateAnnotationUtil;
 import com.baeldung.hibernate.oneToMany.model.Cart;
 import com.baeldung.hibernate.oneToMany.model.Items;
+import com.baeldung.persistence.model.Person;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.Entity;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.dialect.HSQLDialect;
+import org.hibernate.service.ServiceRegistry;
 import org.junit.After;
 import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -15,28 +26,33 @@ import static org.junit.Assert.*;
 
 public class HibernateOneToManyAnnotationMainTest {
     
+    private static SessionFactory sessionFactory;
+
+    private Session session;
+    
     public HibernateOneToManyAnnotationMainTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
     
-    @AfterClass
-    public static void tearDownClass() {
+    @BeforeClass
+    public static void beforeTests() {
+        Configuration configuration = new Configuration().addAnnotatedClass(Cart.class).addAnnotatedClass(Items.class).setProperty("hibernate.dialect", HSQLDialect.class.getName()).setProperty("hibernate.connection.driver_class", org.hsqldb.jdbcDriver.class.getName())
+                .setProperty("hibernate.connection.url", "jdbc:hsqldb:mem:test").setProperty("hibernate.connection.username", "sa").setProperty("hibernate.connection.password", "").setProperty("hibernate.hbm2ddl.auto", "update");
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
     }
     
     @Before
     public void setUp() {
+        session = sessionFactory.openSession();
+        session.beginTransaction();  
+        
     }
     
-    @After
-    public void tearDown() {
-    }
+    
 
     @Test
-    public void testMain() {
-        System.out.println("main");
+    public void testAddItemsToCart() {
         Cart cart = new Cart();
         Items item1 = new Items("I10", 10, 1, cart);
         Set<Items> itemsSet = new HashSet<Items>();
@@ -44,7 +60,32 @@ public class HibernateOneToManyAnnotationMainTest {
         assertNotNull(item1);
         cart.setItems(itemsSet);
         assertNotNull(cart);
+        System.out.println("Items added to cart");
       
+    }
+    
+    @Test
+    public void testSaveCart(){
+       Cart cart = new Cart();
+       Items item1 = new Items("I10", 10, 1, cart);
+       Set<Items> itemsSet = new HashSet<Items>();
+       itemsSet.add(item1);
+       cart.setItems(itemsSet);
+       session.persist(cart);
+       session.getTransaction().commit();
+       session.close();
+        
+    }
+    
+    @After
+    public void tearDown() {
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @AfterClass
+    public static void afterTests() {
+        sessionFactory.close();
     }
     
 }
