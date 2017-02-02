0f28a71d235fbe840aa6d1c4adf87a3bc8ce8aac
Chima Ejiofor <Chima Ejiofor>
2017-02-02 13:04:57 +0100

    Unit test for Hibernate One to many Tutorial

--- spring-hibernate4/src/test/java/com/baeldung/hibernate/oneToMany/main/HibernateOneToManyAnnotationMainTest.java
+++ spring-hibernate4/src/test/java/com/baeldung/hibernate/oneToMany/main/HibernateOneToManyAnnotationMainTest.java
@@ -6,12 +6,14 @@ import com.baeldung.hibernate.oneToMany.model.Cart;
 import com.baeldung.hibernate.oneToMany.model.Items;
 import java.util.HashSet;
 import java.util.Set;
+import static org.hamcrest.Matchers.hasSize;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.dialect.HSQLDialect;
 import org.hibernate.service.ServiceRegistry;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -48,10 +50,12 @@ public class HibernateOneToManyAnnotationMainTest {
     @Test
     public void testAddItemsToCart() {
         Cart cart = new Cart();
+        Set <Items> cartItems = new HashSet<>();
+        cartItems = cart.getItems();
+        Assert.assertNull(cartItems);
         Items item1 = new Items("I10", 10, 1, cart);
-        Set<Items> itemsSet = new HashSet<Items>();
-        assertEquals(0, itemsSet.size());
         assertNotNull(item1);
+        Set<Items> itemsSet = new HashSet<Items>();
         cart.setItems(itemsSet);
         assertNotNull(cart);
         System.out.println("Items added to cart");
@@ -61,10 +65,20 @@ public class HibernateOneToManyAnnotationMainTest {
     @Test
     public void testSaveCart(){
        Cart cart = new Cart();
-       Items item1 = new Items("I10", 10, 1, cart);
+       Set <Items> cartItems = new HashSet<>();
+       cartItems = cart.getItems();
+       Assert.assertNull(cartItems);
+       Items item1 = new Items();
+       item1.setItemId("I10");
+       item1.setItemTotal(10);
+       item1.setQuantity(1);
+       item1.setCart(cart);
+       assertNotNull(item1);
        Set<Items> itemsSet = new HashSet<Items>();
        itemsSet.add(item1);
+       assertNotNull(itemsSet);
        cart.setItems(itemsSet);
+       assertNotNull(cart);
        session.persist(cart);
        session.getTransaction().commit();
        session.close();
