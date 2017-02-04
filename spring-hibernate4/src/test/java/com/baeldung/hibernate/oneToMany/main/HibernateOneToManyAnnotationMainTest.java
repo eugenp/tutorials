bd6fe7269e19db914d2ef0b7dbfc8899d5ba38b7
eugenp <hanriseldon@gmail.com>
2017-02-04 14:14:23 +0200

    minor cleanup work - formatting

--- spring-hibernate4/src/test/java/com/baeldung/hibernate/oneToMany/main/HibernateOneToManyAnnotationMainTest.java
+++ spring-hibernate4/src/test/java/com/baeldung/hibernate/oneToMany/main/HibernateOneToManyAnnotationMainTest.java
@@ -1,12 +1,12 @@
 
 package com.baeldung.hibernate.oneToMany.main;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
-import com.baeldung.hibernate.oneToMany.model.Cart;
-import com.baeldung.hibernate.oneToMany.model.Items;
 import java.util.HashSet;
 import java.util.Set;
-import static org.hamcrest.Matchers.hasSize;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -17,51 +17,47 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.baeldung.hibernate.oneToMany.model.Cart;
+import com.baeldung.hibernate.oneToMany.model.Items;
 
 //@RunWith(SpringJUnit4ClassRunner.class)
 public class HibernateOneToManyAnnotationMainTest {
-    
+
     private static SessionFactory sessionFactory;
 
     private Session session;
-    
+
     public HibernateOneToManyAnnotationMainTest() {
     }
-    
-    
+
     @BeforeClass
     public static void beforeTests() {
-        Configuration configuration = new Configuration().addAnnotatedClass(Cart.class).addAnnotatedClass(Items.class).setProperty("hibernate.dialect", HSQLDialect.class.getName()).setProperty("hibernate.connection.driver_class", org.hsqldb.jdbcDriver.class.getName())
-                .setProperty("hibernate.connection.url", "jdbc:hsqldb:mem:test").setProperty("hibernate.connection.username", "sa").setProperty("hibernate.connection.password", "").setProperty("hibernate.hbm2ddl.auto", "update");
+        Configuration configuration = new Configuration().addAnnotatedClass(Cart.class).addAnnotatedClass(Items.class).setProperty("hibernate.dialect", HSQLDialect.class.getName())
+                .setProperty("hibernate.connection.driver_class", org.hsqldb.jdbcDriver.class.getName()).setProperty("hibernate.connection.url", "jdbc:hsqldb:mem:test").setProperty("hibernate.connection.username", "sa")
+                .setProperty("hibernate.connection.password", "").setProperty("hibernate.hbm2ddl.auto", "update");
         ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
         sessionFactory = configuration.buildSessionFactory(serviceRegistry);
     }
-    
+
     @Before
     public void setUp() {
         session = sessionFactory.openSession();
-        session.beginTransaction();  
-        
+        session.beginTransaction();
+
     }
-    
+
     @Test
-    public void givenSession_checkIfDatabaseIsEmpty(){
+    public void givenSession_checkIfDatabaseIsEmpty() {
         Cart cart = (Cart) session.get(Cart.class, new Long(1));
         assertNull(cart);
-    
+
     }
-    
-    
-    
-    
 
     @Test
     public void testAddItemsToCart() {
         Cart cart = new Cart();
-        Set <Items> cartItems = new HashSet<>();
+        Set<Items> cartItems = new HashSet<>();
         cartItems = cart.getItems();
         Assert.assertNull(cartItems);
         Items item1 = new Items("I10", 10, 1, cart);
@@ -70,33 +66,32 @@ public class HibernateOneToManyAnnotationMainTest {
         cart.setItems(itemsSet);
         assertNotNull(cart);
         System.out.println("Items added to cart");
-      
+
     }
-    
+
     @Test
-    public void givenSession_checkIfDatabaseIsPopulated_afterCommit(){
-       Cart cart = new Cart();
-       Set <Items> cartItems = new HashSet<>();
-       cartItems = cart.getItems();
-       Assert.assertNull(cartItems);
-       Items item1 = new Items();
-       item1.setItemId("I10");
-       item1.setItemTotal(10);
-       item1.setQuantity(1);
-       item1.setCart(cart);
-       assertNotNull(item1);
-       Set<Items> itemsSet = new HashSet<Items>();
-       itemsSet.add(item1);
-       assertNotNull(itemsSet);
-       cart.setItems(itemsSet);
-       assertNotNull(cart);
-       session.persist(cart);
-       session.getTransaction().commit();
-       cart = (Cart) session.get(Cart.class, new Long(1));
-       assertNotNull(cart);
-       session.close();
-        
+    public void givenSession_checkIfDatabaseIsPopulated_afterCommit() {
+        Cart cart = new Cart();
+        Set<Items> cartItems = new HashSet<>();
+        cartItems = cart.getItems();
+        Assert.assertNull(cartItems);
+        Items item1 = new Items();
+        item1.setItemId("I10");
+        item1.setItemTotal(10);
+        item1.setQuantity(1);
+        item1.setCart(cart);
+        assertNotNull(item1);
+        Set<Items> itemsSet = new HashSet<Items>();
+        itemsSet.add(item1);
+        assertNotNull(itemsSet);
+        cart.setItems(itemsSet);
+        assertNotNull(cart);
+        session.persist(cart);
+        session.getTransaction().commit();
+        cart = (Cart) session.get(Cart.class, new Long(1));
+        assertNotNull(cart);
+        session.close();
+
     }
-    
-    
+
 }
