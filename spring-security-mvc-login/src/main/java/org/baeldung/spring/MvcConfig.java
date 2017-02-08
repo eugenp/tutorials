41bf9841b6b170121a4fe72fcf07579bff2c04c2
Crusoveanu <loredana.crusoveanu@yahoo.com>
2017-02-08 13:36:27 +0200

    fix formatting, remove imports

--- spring-security-mvc-login/src/main/java/org/baeldung/spring/MvcConfig.java
+++ spring-security-mvc-login/src/main/java/org/baeldung/spring/MvcConfig.java
@@ -1,7 +1,6 @@
 package org.baeldung.spring;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.view.JstlView;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages="org.baeldung.spring")
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     public MvcConfig() {
--- spring-security-mvc-login/src/main/java/org/baeldung/spring/SecSecurityConfig.java
+++ spring-security-mvc-login/src/main/java/org/baeldung/spring/SecSecurityConfig.java
@@ -5,7 +5,6 @@ import org.baeldung.security.CustomLogoutSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
