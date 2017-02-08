85696120f690087527837e60c58a061be913edb8
mfarid <mohd.farid@devfactory.com>
2017-02-08 17:53:40 +0530

    BAEL-668 find min/max from a list/collection

---
+++ core-java/src/test/java/com/baeldung/java8/Java8MaxMinTest.java
@@ -0,0 +1,45 @@
+package com.baeldung.java8;
+
+import com.baeldung.java_8_features.Person;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class Java8MaxMinTest {
+
+    @Test
+    public void whenListIsOfIntegerThenMaxCanBeDoneUsingIntegerComparator() {
+        //given
+        final List<Integer> listOfIntegers = Arrays.asList(1, 2, 3, 4, 56, 7, 89, 10);
+        final Integer expectedResult = 89;
+
+        //when
+        Optional<Integer> result = listOfIntegers.stream().max(Integer::compare);
+        Integer resultMax = result.isPresent() ? result.get() : null;
+
+        //then
+        assertEquals(expectedResult, resultMax);
+    }
+
+    @Test
+    public void whenListIsOfPersonObjectThenMinCanBeDoneUsingCustomComparatorThroughLambda() {
+        //given
+        final Person alex = new Person("Alex", 23);
+        final Person john = new Person("John", 40);
+        final Person peter = new Person("Peter", 32);
+        final List<Person> people = Arrays.asList(alex, john, peter);
+
+        //when
+        Optional<Person> minAge =
+            people.stream().min((o1, o2) -> o1.getAge().compareTo(o2.getAge()));
+        Person resultMin = minAge.isPresent() ? minAge.get() : null;
+
+        //then
+        assertEquals("Alex must be having min age", alex, resultMin);
+    }
+
+}
