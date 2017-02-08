5efa0382b974b04c68696df3a4ad5e9177f893d1
aiet <lovetcn@gmail.com>
2017-02-08 20:41:49 +0800

    add tests for #BAEL-100 and upgrade spring boot to 1.5.1.RELEASE

--- spring-boot/src/main/java/com/baeldung/webjar/TestController.java
+++ spring-boot/src/main/java/com/baeldung/webjar/TestController.java
@@ -1,4 +1,4 @@
-package com.baeldung;
+package com.baeldung.webjar;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- spring-boot/src/main/java/com/baeldung/webjar/WebjarsdemoApplication.java
+++ spring-boot/src/main/java/com/baeldung/webjar/WebjarsdemoApplication.java
@@ -1,7 +1,8 @@
-package com.baeldung;
+package com.baeldung.webjar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class WebjarsdemoApplication {
