c0c7241d30860f34894599fa09209ecdfd36efc0
michal_aibin <michal.aibin@gmail.com>
2017-02-09 16:59:15 +0100

    Simple Genetic Algorithm improvements

--- core-java/src/test/java/com/baeldung/algorithms/BinaryGeneticAlgorithmUnitTest.java
+++ core-java/src/test/java/com/baeldung/algorithms/BinaryGeneticAlgorithmUnitTest.java
@@ -7,9 +7,10 @@ import com.baeldung.algorithms.ga.binary.SimpleGeneticAlgorithm;
 
 public class BinaryGeneticAlgorithmUnitTest {
 
-    @Test
-    public void testGA() {
-        Assert.assertTrue(SimpleGeneticAlgorithm.runAlgorithm(50, "1011000100000100010000100000100111001000000100000100000000001111"));
-    }
+	@Test
+	public void testGA() {
+		SimpleGeneticAlgorithm ga = new SimpleGeneticAlgorithm();
+		Assert.assertTrue(ga.runAlgorithm(50, "1011000100000100010000100000100111001000000100000100000000001111"));
+	}
 
 }
