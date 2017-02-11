c1b9675c4317b525a5c9e935b82517ba8391b974
tschiman <tschiman@gmail.com>
2017-02-11 11:52:16 -0700

    BAEL-684 Adding spring JPA to resource servers

--- spring-cloud/spring-cloud-bootstrap/svc-rating/src/main/java/com/baeldung/spring/cloud/bootstrap/svcrating/SecurityConfig.java
+++ spring-cloud/spring-cloud-bootstrap/svc-rating/src/main/java/com/baeldung/spring/cloud/bootstrap/svcrating/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.baeldung.spring.cloud.bootstrap.svcrating;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,8 +23,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic()
             .disable()
         .authorizeRequests()
-            .antMatchers("/ratings").hasRole("USER")
-            .antMatchers("/ratings/all").hasRole("ADMIN")
+            .regexMatchers("^/ratings\\?bookId.*$").authenticated()
+            .antMatchers(HttpMethod.POST,"/ratings").authenticated()
+            .antMatchers(HttpMethod.PATCH,"/ratings/*").hasRole("ADMIN")
+            .antMatchers(HttpMethod.DELETE,"/ratings/*").hasRole("ADMIN")
+            .antMatchers(HttpMethod.GET,"/ratings").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and()
         .csrf()
