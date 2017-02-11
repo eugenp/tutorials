2ee0a8a7a13145df12259429012f9c57b5509509
tschiman <tschiman@gmail.com>
2017-02-11 19:52:16 +0100

    BAEL-684 Adding spring JPA to resource servers

--- spring-cloud/spring-cloud-bootstrap/svc-book/src/main/java/com/baeldung/spring/cloud/bootstrap/svcbook/book/Book.java
+++ spring-cloud/spring-cloud-bootstrap/svc-book/src/main/java/com/baeldung/spring/cloud/bootstrap/svcbook/book/Book.java
@@ -1,15 +1,20 @@
-package com.baeldung.spring.cloud.bootstrap.svcbook;
+package com.baeldung.spring.cloud.bootstrap.svcbook.book;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String author;
     private String title;
-
-    public Book(Long id, String title, String author) {
-        this.id = id;
-        this.author = author;
-        this.title = title;
-    }
 
     public Book() {
     }
