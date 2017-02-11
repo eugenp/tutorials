c670ac916623acc7b738c0f7f28119f4b3c23062
Abhinab Kanrar <abhinabkanrar@gmail.com>
2017-02-11 20:59:57 +0530

    adding more functionality in split/join, added chars() usage (#1155)
    * rest with spark java
    
    * 4
    
    * Update Application.java
    
    * indentation changes
    
    * spring @requestmapping shortcuts
    
    * removing spring requestmapping and pushing spring-mvc-java
    
    * Joining/Splitting Strings with Java and Stream API
    
    * adding more join/split functionality

--- core-java/src/test/java/com/baeldung/stream/JoinerSplitterTest.java
+++ core-java/src/test/java/com/baeldung/stream/JoinerSplitterTest.java
@@ -13,7 +13,9 @@ public class JoinerSplitterTest {
 
 	@Test
 	public void provided_array_convert_to_stream_and_convert_to_string() {
+
 		String[] programming_languages = {"java", "python", "nodejs", "ruby"};
+
 		String expectation = "java,python,nodejs,ruby";
 		
 		String result  = JoinerSplitter.join(programming_languages);
@@ -21,7 +23,17 @@ public class JoinerSplitterTest {
 	}
 	
 	@Test
-	public void provided_list_convert_to_stream_and_convert_to_list() {
+	public void provided_array_convert_to_stream_and_convert_to_prefixPostfixString() {
+		String[] programming_languages = {"java", "python", 
+				"nodejs", "ruby"};
+		String expectation = "[java,python,nodejs,ruby]";
+		
+		String result  = JoinerSplitter.joinWithPrefixPostFix(programming_languages);
+		assertEquals(result, expectation);
+	}
+	
+	@Test
+	public void provided_string_convert_to_stream_and_convert_to_listOfString() {
 		String programming_languages = "java,python,nodejs,ruby";
 		
 		List<String> expectation = new ArrayList<String>();
@@ -34,5 +46,20 @@ public class JoinerSplitterTest {
 		
 		assertEquals(result, expectation);
 	}
-	
+  
+	@Test
+	public void provided_string_convert_to_stream_and_convert_to_listOfChar() {
+		String programming_languages = "java,python,nodejs,ruby";
+		
+		List<Character> expectation = new ArrayList<Character>();
+		char[] charArray = programming_languages.toCharArray();
+		for (char c : charArray) {
+			expectation.add(c);
+		}
+		
+		List<Character> result  = JoinerSplitter.splitToListOfChar(programming_languages);
+		assertEquals(result, expectation);
+		
+	}
+  
 }
