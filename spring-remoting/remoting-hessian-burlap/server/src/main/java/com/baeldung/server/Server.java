9f9edd753bb1b154bf906ac4f3eb65922483a5a1
danidemi <escher75@gmail.com>
2017-02-11 15:30:09 +0100

    Burlap & Hessian server added

---
+++ spring-remoting/remoting-hessian-burlap/server/src/main/java/com/baeldung/server/Server.java
@@ -0,0 +1,38 @@
+package com.baeldung.server;
+
+import com.baeldung.api.CabBookingService;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.caucho.HessianServiceExporter;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Server {
+
+    @Bean CabBookingService bookingService() {
+        return new CabBookingServiceImpl();
+    }
+
+    @Bean(name = "/h_booking") HessianServiceExporter hessianService(CabBookingService service) {
+        HessianServiceExporter exporter = new HessianServiceExporter();
+        exporter.setService(bookingService());
+        exporter.setServiceInterface( CabBookingService.class );
+        return exporter;
+    }
+
+//    @Bean(name = "/b_booking") BurlapServiceExporter burlapService(CabBookingService service) {
+//        BurlapServiceExporter exporter = new BurlapServiceExporter();
+//        exporter.setService(bookingService());
+//        exporter.setServiceInterface( CabBookingService.class );
+//        return exporter;
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server.class, args);
+    }
+
+}
