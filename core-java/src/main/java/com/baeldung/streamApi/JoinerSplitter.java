5218116a80abba1bfee47875ef7221ba8d18b154
pivovarit <gpiwowarek@gmail.com>
2017-02-11 18:40:14 +0100

    Reformat JoinerSplitter

--- core-java/src/main/java/com/baeldung/streamApi/JoinerSplitter.java
+++ core-java/src/main/java/com/baeldung/streamApi/JoinerSplitter.java
@@ -7,30 +7,34 @@ import java.util.stream.Stream;
 
 public class JoinerSplitter {
 
-	public static String join ( String[] arrayOfString ) {
-		return Arrays.asList(arrayOfString)
-				.stream()
-				.map(x -> x)
-				.collect(Collectors.joining(","));
-	}
-	
-	public static String joinWithPrefixPostFix ( String[] arrayOfString ) {
-		return Arrays.asList(arrayOfString)
-				.stream()
-				.map(x -> x)
-				.collect(Collectors.joining(",","[","]"));
-	}
-	
-	public static List<String> split ( String str ) {
-		return Stream.of(str.split(","))
-				.map (elem -> new String(elem))
-				.collect(Collectors.toList());
-	}
-	
-	public static List<Character> splitToListOfChar ( String str ) {
-		return str.chars()
-				.mapToObj(item -> (char) item)
-				.collect(Collectors.toList());
-	}
-		
+    public static String join(String[] arrayOfString) {
+        return Arrays
+          .asList(arrayOfString)
+          .stream()
+          .map(x -> x)
+          .collect(Collectors.joining(","));
+    }
+
+    public static String joinWithPrefixPostFix(String[] arrayOfString) {
+        return Arrays
+          .asList(arrayOfString)
+          .stream()
+          .map(x -> x)
+          .collect(Collectors.joining(",", "[", "]"));
+    }
+
+    public static List<String> split(String str) {
+        return Stream
+          .of(str.split(","))
+          .map(elem -> new String(elem))
+          .collect(Collectors.toList());
+    }
+
+    public static List<Character> splitToListOfChar(String str) {
+        return str
+          .chars()
+          .mapToObj(item -> (char) item)
+          .collect(Collectors.toList());
+    }
+
 }
