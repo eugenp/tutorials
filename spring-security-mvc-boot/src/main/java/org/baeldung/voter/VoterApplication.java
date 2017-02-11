63b042a9d8d6a3eb3ff2b52a2c0f8974b521782e
eugenp <hanriseldon@gmail.com>
2017-02-11 14:07:50 +0200

    maven cleanup

--- spring-security-mvc-boot/src/main/java/org/baeldung/voter/VoterApplication.java
+++ spring-security-mvc-boot/src/main/java/org/baeldung/voter/VoterApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @EnableAutoConfiguration
