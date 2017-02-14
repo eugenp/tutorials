7f4c7157eb9dc040e737eff02fc923c02c0f1179
maibin <michal.aibin@gmail.com>
2017-02-14 17:49:10 +0100

    Struts2 refactoring (#1176)
    * Modifications to model on Hibernate One to manyTutorial
    
    * Modifications to model on Hibernate One to manyTutorial
    
    * Modifications to model on Hibernate One to manyTutorial
    
    * Simple Genetic Algorithm improvements
    
    * Struts2 refactoring

--- struts2/src/main/java/com/baeldung/struts/CarAction.java
+++ struts2/src/main/java/com/baeldung/struts/CarAction.java
@@ -3,7 +3,6 @@ package com.baeldung.struts;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
 import org.apache.struts2.convention.annotation.Result;
-import org.apache.struts2.convention.annotation.ResultPath;
 
 @Namespace("/tutorial")
 @Action("/car")
