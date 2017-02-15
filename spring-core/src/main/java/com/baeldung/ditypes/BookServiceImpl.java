0e57c72addaf24526ec1208fa4c88942a244b494
Mohamed Sanaulla <sanaulla123@gmail.com>
2017-02-15 23:14:40 +0300

    remove sysout

--- spring-core/src/main/java/com/baeldung/ditypes/BookServiceImpl.java
+++ spring-core/src/main/java/com/baeldung/ditypes/BookServiceImpl.java
@@ -5,22 +5,18 @@ import org.springframework.stereotype.Service;
 @Service("bookServiceImpl")
 public class BookServiceImpl implements BookService {
     public Boolean create(Book book) {
-        System.out.println("Saving: " + book.toString());
         return true;
     }
 
     public Book read(String isbn) {
-        System.out.println("Reading: " + isbn);
         return new Book("Title", "1234SDK");
     }
 
     public Boolean update(String isbn, Book book) {
-        System.out.println("Updating " + isbn + " with " + book.toString());
         return true;
     }
 
     public Boolean delete(String isbn) {
-        System.out.println("Deleting " + isbn);
         return true;
     }
 }
