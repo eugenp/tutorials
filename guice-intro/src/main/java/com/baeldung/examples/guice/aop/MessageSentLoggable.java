d5bd1214a7481bfcb77f2c8298641d865af8717a
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:31:31 -0500

    Update MessageSentLoggable.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/aop/MessageSentLoggable.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/aop/MessageSentLoggable.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.examples.guice.aop;
 
 import java.lang.annotation.ElementType;
@@ -12,7 +7,7 @@ import java.lang.annotation.Target;
 
 /**
  *
- * @author Tayo
+ * @author Baeldung
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
