156c7a2458a223859a6d1abccdd7c6513b06c015
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:31:51 -0500

    Update AOPModule.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/binding/AOPModule.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/binding/AOPModule.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.examples.guice.binding;
 
 import com.baeldung.examples.guice.aop.MessageLogger;
@@ -12,7 +7,7 @@ import com.google.inject.matcher.Matchers;
 
 /**
  *
- * @author Tayo
+ * @author Baeldung
  */
 public class AOPModule extends AbstractModule {
 
