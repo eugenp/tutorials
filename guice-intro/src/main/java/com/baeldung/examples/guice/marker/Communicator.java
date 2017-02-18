43c3b521583faaa6c5d089acf9bc25d70bf07e7f
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:32:43 -0500

    Update Communicator.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/marker/Communicator.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/marker/Communicator.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.examples.guice.marker;
 
 /**
  *
- * @author Tayo
+ * @author Baeldung
  */
 public interface Communicator {
     
