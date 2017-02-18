e9281a8de586ecaa432a8edfe22bf96389cf8bb2
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:29:45 -0500

    Update DefaultCommunicator.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/DefaultCommunicator.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/DefaultCommunicator.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.baeldung.examples.guice;
 
 import com.baeldung.examples.guice.marker.Communicator;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
 
-/**
- *
- * @author Tayo
- */
+
 public class DefaultCommunicator implements Communicator {
 
     private CommunicationMode defaultCommsMode;
