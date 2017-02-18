6d46dfe4c0932f9602c37ba51e73eef4616ca8e7
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:31:13 -0500

    Update MessageLogger.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/aop/MessageLogger.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/aop/MessageLogger.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.examples.guice.aop;
 
 import com.google.inject.Inject;
@@ -12,7 +7,7 @@ import org.aopalliance.intercept.MethodInvocation;
 
 /**
  *
- * @author Tayo
+ * @author Baeldung
  */
 public class MessageLogger implements MethodInterceptor {
 
