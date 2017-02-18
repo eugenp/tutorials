efc2d27a84435647e60578ceb194dd4603842259
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:29:27 -0500

    Update CommunicationMode.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/CommunicationMode.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/CommunicationMode.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.baeldung.examples.guice;
 
 import com.baeldung.examples.guice.constant.CommunicationModel;
