1122e932b6b84c27dc52271a48d3c282a8cd81c0
k0l0ssus <tkole2002@yahoo.co.uk>
2017-02-18 00:29:12 -0500

    Update Communication.java

--- guice-intro/src/main/java/com/baeldung/examples/guice/Communication.java
+++ guice-intro/src/main/java/com/baeldung/examples/guice/Communication.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.baeldung.examples.guice;
 
 import com.google.inject.Inject;
@@ -14,7 +10,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Tayo
+ * @author Baeldung
  */
 public class Communication {
 
