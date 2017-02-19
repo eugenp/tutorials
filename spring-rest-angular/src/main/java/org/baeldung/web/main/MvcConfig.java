1ac67142f1745674944e961632ba1e4d3bd13847
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2017-02-20 00:27:33 +0200

    fix formatting, remove extra imports

--- spring-rest-angular/src/main/java/org/baeldung/web/main/MvcConfig.java
+++ spring-rest-angular/src/main/java/org/baeldung/web/main/MvcConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.DefaultServletHandlerCo
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @EnableWebMvc
