00b39f32c4de7c7395f9639cbf9e6f35f8c217b4
Daniel Cassiani <dcassiani@gmail.com>
2017-02-20 16:13:30 -0300

    switching for Annotations

--- spring-all/spring-bean-injection/src/main/java/com/sql/Main.java
+++ spring-all/spring-bean-injection/src/main/java/com/sql/Main.java
@@ -4,30 +4,32 @@ package com.sql;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.sql.database.DatabaseBean;
+import com.sql.security.MySecureBean;
 
 public class Main {
 
     public static void main(String[] args) {
-        
+
         Logger logger = LoggerFactory.getLogger(DatabaseBean.class);
-        
-        ApplicationContext context = 
-            new ClassPathXmlApplicationContext(new String[] { "beans.xml" });
-        DatabaseBean databaseBean = (DatabaseBean) context.getBean("databaseBean");
-        
+
+        ApplicationContext context 
+        = new AnnotationConfigApplicationContext(DatabaseBean.class, MySecureBean.class);
+        DatabaseBean databaseBean = context.getBean(DatabaseBean.class);
+
         String credentials = databaseBean.getLogin()
             .concat(":")
-            .concat(databaseBean.getPassword().getStr())
+            .concat(databaseBean.getPassword()
+                .getStr())
             .concat("@")
             .concat(databaseBean.getDatabasePath())
             .concat(":")
-            .concat(Integer.toString(databaseBean.getPort()));
-        
+            .concat(databaseBean.getPort());
+
         logger.info(credentials);
-        
+
     }
 
 }
