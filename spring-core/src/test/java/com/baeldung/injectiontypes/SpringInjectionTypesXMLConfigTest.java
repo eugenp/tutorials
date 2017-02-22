729e0bd8285a1f80d4049679a5c4664cba3d7c07
unknown <mohamed_younis@RAM-NB-51742.HeadQ.raya.corp>
2017-02-22 17:10:04 +0200

    code formatting

--- spring-core/src/test/java/com/baeldung/injectiontypes/SpringInjectionTypesXMLConfigTest.java
+++ spring-core/src/test/java/com/baeldung/injectiontypes/SpringInjectionTypesXMLConfigTest.java
@@ -8,27 +8,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import com.baeldung.injectiontypes.domain.Student;
 import com.baeldung.injectiontypes.xmlconfig.service.StudentService;
 
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:beans.xml")
 public class SpringInjectionTypesXMLConfigTest {
-
 
     @Autowired
     private StudentService serviceInjectDependencyByConstrucot;
 
     @Autowired
     private StudentService serviceInjectDependencyBySetter;
-   
 
     @Test
     public void apologizeWithTranslatedMessage() {
-    	Student student = new Student("Alex", 25);
-		Student student2 = new Student("David", 26);
+        Student student = new Student("Alex", 25);
+        Student student2 = new Student("David", 26);
 
-		serviceInjectDependencyByConstrucot.addStudent(student);
-		serviceInjectDependencyBySetter.addStudent(student2);
-    	System.out.println("testing done");
+        serviceInjectDependencyByConstrucot.addStudent(student);
+        serviceInjectDependencyBySetter.addStudent(student2);
+        System.out.println("testing done");
     }
 
 }
