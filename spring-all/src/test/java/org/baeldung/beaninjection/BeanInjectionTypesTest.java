ee2536aceacb6306e67c84f935fea80fabccd63c
Marcos R Oliveira <mroger.oliveira@gmail.com>
2017-02-21 21:25:11 -0300

    Adding test for XML injection configuration

---
+++ spring-all/src/test/java/org/baeldung/beaninjection/BeanInjectionTypesTest.java
@@ -0,0 +1,39 @@
+package org.baeldung.beaninjection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:beaninjection-config.xml")
+public class BeanInjectionTypesTest implements ApplicationContextAware {
+
+    private ApplicationContext context;
+    
+    @Test
+    public void givenDeployService_whenSendingNotification_thenMessageIsDelivered() {
+        DeployService deployService = context.getBean(DeployService.class);
+        deployService.deploy();
+    }
+    
+    @Test
+    public void givenStockService_whenUpdateProduct_thenDaoIsCalled() {
+        Product product = new Product();
+        product.setId(1);
+        product.setName("Product 1");
+        product.setQuantity(5);
+        
+        StockService stockService = context.getBean(StockService.class);
+        stockService.updateStock(product);
+    }
+    
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        this.context = context;
+    }
+    
+}
