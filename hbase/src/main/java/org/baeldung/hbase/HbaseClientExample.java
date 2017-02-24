2ca001d4d66e7f840d0e81df746c8ae16579472d
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-02-24 10:09:39 +0100

    BAEL-655 add client

--- hbase/src/main/java/org/baeldung/hbase/HbaseClientExample.java
+++ hbase/src/main/java/org/baeldung/hbase/HbaseClientExample.java
@@ -1,7 +1,38 @@
 package org.baeldung.hbase;
 
-/**
- * Created by tomasz.lelek on 23/02/17.
- */
+
+import com.google.protobuf.ServiceException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+import java.io.IOException;
+
 public class HbaseClientExample {
+
+    public static void main(String[] args) throws IOException, ServiceException {
+        new HbaseClientExample().connect();
+    }
+
+    private void connect() throws IOException, ServiceException {
+        Configuration config = HBaseConfiguration.create();
+
+        String path = this.getClass().getClassLoader().getResource("hbase-site.xml").getPath();
+
+        config.addResource(new Path(path));
+
+
+        try {
+            HBaseAdmin.checkHBaseAvailable(config);
+        } catch (MasterNotRunningException e) {
+            System.out.println("HBase is not running.");
+            System.exit(1);
+        }
+
+        Sample sample = new Sample();
+        sample.run(config);
+    }
+
 }
