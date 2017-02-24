f64e24e6de1e8c0295927af66094ac0275abb639
eugenp <hanriseldon@gmail.com>
2017-02-24 14:08:44 +0200

    fully working version of Spring 5 project

--- spring-5/src/main/java/com/baeldung/Spring5Application.java
+++ spring-5/src/main/java/com/baeldung/Spring5Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Spring5Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5Application.class, args);
+    }
 }
--- spring-5/src/test/java/com/baeldung/Spring5ApplicationTests.java
+++ spring-5/src/test/java/com/baeldung/Spring5ApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Spring5ApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
