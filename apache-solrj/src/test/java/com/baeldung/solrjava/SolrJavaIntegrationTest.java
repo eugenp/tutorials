99cfc7303e34b9580c6f125f09371296944ad70f
nbosecker <nbosecker@gmail.com>
2017-02-26 15:45:34 -0800

    More examples for indexing,delete,and query for solrj

--- apache-solrj/src/test/java/com/baeldung/solrjava/SolrJavaIntegrationTest.java
+++ apache-solrj/src/test/java/com/baeldung/solrjava/SolrJavaIntegrationTest.java
@@ -24,10 +24,28 @@ public class SolrJavaIntegrationTest {
     }
 
     @Test
-    public void whenAdd_thenVerifyAdded() throws SolrServerException, IOException {
+    public void whenAdd_thenVerifyAddedByQueryOnId() throws SolrServerException, IOException {
 
         SolrQuery query = new SolrQuery();
         query.set("q", "id:123456");
+        QueryResponse response = null;
+
+        response = solrJavaIntegration.getSolrClient().query(query);
+
+        SolrDocumentList docList = response.getResults();
+        assertEquals(docList.getNumFound(), 1);
+
+        for (SolrDocument doc : docList) {
+            assertEquals((String) doc.getFieldValue("name"), "Kenmore Dishwasher");
+            assertEquals((Double) doc.getFieldValue("price"), (Double) 599.99);
+        }
+    }
+
+    @Test
+    public void whenAdd_thenVerifyAddedByQueryOnPrice() throws SolrServerException, IOException {
+
+        SolrQuery query = new SolrQuery();
+        query.set("q", "price:599.99");
         QueryResponse response = null;
 
         response = solrJavaIntegration.getSolrClient().query(query);
@@ -42,9 +60,41 @@ public class SolrJavaIntegrationTest {
     }
 
     @Test
-    public void whenDelete_thenVerifyDeleted() throws SolrServerException, IOException {
+    public void whenAdd_thenVerifyAddedByQuery() throws SolrServerException, IOException {
 
-        solrJavaIntegration.deleteSolrDocument("123456");
+        SolrDocument doc = solrJavaIntegration.getSolrClient().getById("123456");
+        assertEquals((String) doc.getFieldValue("name"), "Kenmore Dishwasher");
+        assertEquals((Double) doc.getFieldValue("price"), (Double) 599.99);
+    }
+
+    @Test
+    public void whenAddBean_thenVerifyAddedByQuery() throws SolrServerException, IOException {
+
+        ProductBean pBean = new ProductBean("888", "Apple iPhone 6s", "299.99");
+        solrJavaIntegration.addProductBean(pBean);
+
+        SolrDocument doc = solrJavaIntegration.getSolrClient().getById("888");
+        assertEquals((String) doc.getFieldValue("name"), "Apple iPhone 6s");
+        assertEquals((Double) doc.getFieldValue("price"), (Double) 299.99);
+    }
+
+    @Test
+    public void whenDeleteById_thenVerifyDeleted() throws SolrServerException, IOException {
+
+        solrJavaIntegration.deleteSolrDocumentById("123456");
+
+        SolrQuery query = new SolrQuery();
+        query.set("q", "id:123456");
+        QueryResponse response = solrJavaIntegration.getSolrClient().query(query);
+
+        SolrDocumentList docList = response.getResults();
+        assertEquals(docList.getNumFound(), 0);
+    }
+
+    @Test
+    public void whenDeleteByQuery_thenVerifyDeleted() throws SolrServerException, IOException {
+
+        solrJavaIntegration.deleteSolrDocumentByQuery("name:Kenmore Dishwasher");
 
         SolrQuery query = new SolrQuery();
         query.set("q", "id:123456");
