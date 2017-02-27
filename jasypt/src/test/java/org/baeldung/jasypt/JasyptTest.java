29fb2b339bdade9174c4b6bedd3427bb3369a888
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-02-27 19:29:20 +0100

    BAEL-311 two tests that works when installed legal_policy.jar

--- jasypt/src/test/java/org/baeldung/jasypt/JasyptTest.java
+++ jasypt/src/test/java/org/baeldung/jasypt/JasyptTest.java
@@ -1,7 +1,10 @@
 package org.baeldung.jasypt;
 
 
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.util.text.BasicTextEncryptor;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertNotSame;
@@ -17,11 +20,49 @@ public class JasyptTest {
         textEncryptor.setPasswordCharArray(password.toCharArray());
 
         //when
-        String myEncryptedText = textEncryptor.encrypt("secret-pass");
-        assertNotSame("secret-pass", myEncryptedText); //myEncryptedText can be save in db
+        String myEncryptedText = textEncryptor.encrypt(password);
+        assertNotSame(password, myEncryptedText); //myEncryptedText can be save in db
 
         //then
         String plainText = textEncryptor.decrypt(myEncryptedText);
-        assertEquals(plainText, "secret-pass");
+        assertEquals(plainText, password);
+    }
+
+
+    @Test
+    @Ignore
+    public void givenTextPassword_whenDecrypt_shouldCompareToEncryptedWithCustomAlgorithm() {
+        //given
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        String password = "secret-pass";
+        encryptor.setPassword("secret-pass");
+        encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
+
+        //when
+        String encryptedText = encryptor.encrypt("secret-pass");
+        assertNotSame(password, encryptedText);
+
+        //then
+        String plainText = encryptor.decrypt(encryptedText);
+        assertEquals(plainText, password);
+    }
+
+    @Test
+    @Ignore
+    public void givenTextPassword_whenDecryptOnHighPerformance_shouldDecrypt(){
+        //given
+        String password = "secret-pass";
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        encryptor.setPoolSize(4);
+        encryptor.setPassword(password);
+        encryptor.setAlgorithm("PBEWithMD5AndTripleDES");
+
+        //when
+        String encryptedText = encryptor.encrypt(password);
+        assertNotSame(password, encryptedText);
+
+        //then
+        String plainText = encryptor.decrypt(encryptedText);
+        assertEquals(plainText, password);
     }
 }
