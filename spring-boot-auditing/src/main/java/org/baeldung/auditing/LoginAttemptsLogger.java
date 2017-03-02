0e1706e9e4eef26e0f1d586236765aba16114521
Wim Deblauwe <wim.deblauwe@gmail.com>
2017-03-02 19:58:40 +0100

    BEAL-75 - Spring Boot Audit Support
    Update to use SLF4J logger instead of System.out.println

--- spring-boot-auditing/src/main/java/org/baeldung/auditing/LoginAttemptsLogger.java
+++ spring-boot-auditing/src/main/java/org/baeldung/auditing/LoginAttemptsLogger.java
@@ -1,5 +1,7 @@
 package org.baeldung.auditing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.audit.AuditEvent;
 import org.springframework.boot.actuate.audit.listener.AuditApplicationEvent;
 import org.springframework.context.event.EventListener;
@@ -8,15 +10,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class LoginAttemptsLogger {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginAttemptsLogger.class);
 
     @EventListener
     public void auditEventHappened(AuditApplicationEvent auditApplicationEvent) {
         AuditEvent auditEvent = auditApplicationEvent.getAuditEvent();
-        System.out.println("Principal " + auditEvent.getPrincipal() + " - " + auditEvent.getType());
+        LOGGER.debug("Principal " + auditEvent.getPrincipal() + " - " + auditEvent.getType());
 
         WebAuthenticationDetails details = (WebAuthenticationDetails) auditEvent.getData().get("details");
-        System.out.println("  Remote IP address: " + details.getRemoteAddress());
-        System.out.println("  Session Id: " + details.getSessionId());
-        System.out.println("  Request URL: " + auditEvent.getData().get("requestUrl"));
+        LOGGER.debug("  Remote IP address: " + details.getRemoteAddress());
+        LOGGER.debug("  Session Id: " + details.getSessionId());
+        LOGGER.debug("  Request URL: " + auditEvent.getData().get("requestUrl"));
     }
 }
