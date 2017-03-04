9e9c9035f77b915d6778c7bd6142f4102743ff67
Alex Vargas <arekssu@gmail.com>
2017-03-04 14:54:08 -0800

    Applying formatt - No spaces

--- java-websocket/src/main/java/com/baeldung/websocket/ChatEndpoint.java
+++ java-websocket/src/main/java/com/baeldung/websocket/ChatEndpoint.java
@@ -18,53 +18,54 @@ import com.baeldung.model.Message;
 
 @ServerEndpoint(value = "/chat/{username}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
 public class ChatEndpoint {
-	private Session session;
-	private static final Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<>();
-	private static HashMap<String, String> users = new HashMap<>();
+    private Session session;
+    private static final Set<ChatEndpoint> chatEndpoints = new CopyOnWriteArraySet<>();
+    private static HashMap<String, String> users = new HashMap<>();
 
-	@OnOpen
-	public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException {
+    @OnOpen
+    public void onOpen(Session session, @PathParam("username") String username) throws IOException, EncodeException {
 
-		this.session = session;
-		chatEndpoints.add(this);
-		users.put(session.getId(), username);
+        this.session = session;
+        chatEndpoints.add(this);
+        users.put(session.getId(), username);
 
-		Message message = new Message();
-		message.setFrom(username);
-		message.setContent("Connected!");
-		broadcast(message);
-	}
+        Message message = new Message();
+        message.setFrom(username);
+        message.setContent("Connected!");
+        broadcast(message);
+    }
 
-	@OnMessage
-	public void onMessage(Session session, Message message) throws IOException, EncodeException {
-		message.setFrom(users.get(session.getId()));
-		broadcast(message);
-	}
+    @OnMessage
+    public void onMessage(Session session, Message message) throws IOException, EncodeException {
+        message.setFrom(users.get(session.getId()));
+        broadcast(message);
+    }
 
-	@OnClose
-	public void onClose(Session session) throws IOException, EncodeException {
-		chatEndpoints.remove(this);
-		Message message = new Message();
-		message.setFrom(users.get(session.getId()));
-		message.setContent("Disconnected!");
-		broadcast(message);
-	}
+    @OnClose
+    public void onClose(Session session) throws IOException, EncodeException {
+        chatEndpoints.remove(this);
+        Message message = new Message();
+        message.setFrom(users.get(session.getId()));
+        message.setContent("Disconnected!");
+        broadcast(message);
+    }
 
-	@OnError
-	public void onError(Session session, Throwable throwable) {
-		// Do error handling here
-	}
+    @OnError
+    public void onError(Session session, Throwable throwable) {
+        // Do error handling here
+    }
 
-	private static void broadcast(Message message) throws IOException, EncodeException {
-		chatEndpoints.forEach(endpoint -> {
-			synchronized (endpoint) {
-				try {
-					endpoint.session.getBasicRemote().sendObject(message);
-				} catch (IOException | EncodeException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private static void broadcast(Message message) throws IOException, EncodeException {
+        chatEndpoints.forEach(endpoint -> {
+            synchronized (endpoint) {
+                try {
+                    endpoint.session.getBasicRemote()
+                        .sendObject(message);
+                } catch (IOException | EncodeException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 }
