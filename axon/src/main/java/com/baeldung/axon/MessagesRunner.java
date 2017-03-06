4bd44e505e1a07e4d2dd865b434574a8171db4a1
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-03-06 13:22:53 +0100

    BEEL-550 remove not needed comments

--- axon/src/main/java/com/baeldung/axon/MessagesRunner.java
+++ axon/src/main/java/com/baeldung/axon/MessagesRunner.java
@@ -23,28 +23,22 @@ import java.util.UUID;
 public class MessagesRunner {
 
     public static void main(String[] args) {
-        // let's start with the Command Bus
         CommandBus commandBus = new SimpleCommandBus();
 
-        // the CommandGateway provides a friendlier API
         CommandGateway commandGateway = new DefaultCommandGateway(commandBus);
 
-        // we'll store Events on the FileSystem, in the "events/" folder
         EventStore eventStore = new FileSystemEventStore(new SimpleEventFileResolver(new File("./events")));
 
-        // a Simple Event Bus will do
         EventBus eventBus = new SimpleEventBus();
 
-        // we need to configure the repository
-        EventSourcingRepository<MessagesAggregate> repository = new EventSourcingRepository<MessagesAggregate>(MessagesAggregate.class, eventStore);
+        EventSourcingRepository<MessagesAggregate> repository =
+                new EventSourcingRepository<MessagesAggregate>(MessagesAggregate.class, eventStore);
         repository.setEventBus(eventBus);
 
-        // Axon needs to know that our Messages Aggregate can handle commands
         AggregateAnnotationCommandHandler.subscribe(MessagesAggregate.class, repository, commandBus);
 
         AnnotationEventListenerAdapter.subscribe(new MessagesEventHandler(), eventBus);
 
-        // and let's send some Commands on the CommandBus.
         final String itemId = UUID.randomUUID().toString();
         commandGateway.send(new CreateMessageCommand(itemId, "Need to do this"));
         commandGateway.send(new MarkReadMessageCommand(itemId));
