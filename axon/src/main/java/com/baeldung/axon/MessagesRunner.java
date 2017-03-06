3e035d5b8bc105068128e8ba0cbde88d64fe087f
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-03-06 13:35:53 +0100

    BEEL-550 proper message

--- axon/src/main/java/com/baeldung/axon/MessagesRunner.java
+++ axon/src/main/java/com/baeldung/axon/MessagesRunner.java
@@ -40,7 +40,7 @@ public class MessagesRunner {
         AnnotationEventListenerAdapter.subscribe(new MessagesEventHandler(), eventBus);
 
         final String itemId = UUID.randomUUID().toString();
-        commandGateway.send(new CreateMessageCommand(itemId, "Need to do this"));
+        commandGateway.send(new CreateMessageCommand(itemId, "Hello, how is your day? :-)"));
         commandGateway.send(new MarkReadMessageCommand(itemId));
     }
 }
