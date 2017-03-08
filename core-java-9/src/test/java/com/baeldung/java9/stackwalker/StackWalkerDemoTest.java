b21286f728b1b8c03531ff2d01935a628459d42c
yasinb <yasin.bhojawala@gmail.com>
2017-03-08 21:01:26 +0530

    BAEL-608 Introduction to Java9 StackWalking API
    improving method names

--- core-java-9/src/test/java/com/baeldung/java9/stackwalker/StackWalkerDemoTest.java
+++ core-java-9/src/test/java/com/baeldung/java9/stackwalker/StackWalkerDemoTest.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 public class StackWalkerDemoTest {
 
     @Test
-    public void shouldWalkTheStack() {
+    public void giveStalkWalker_walkingTheStack_thenShowStackFrames() {
         new StackWalkerDemo().methodOne();
     }
     
     @Test
-    public void shouldFindCaller() {
+    public void giveStalkWalker_whenInvokingFindCaller_thenFindCallingClass() {
         new StackWalkerDemo().findCaller();
     }
 }
