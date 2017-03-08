7afcbea4bf128af56c73b3366aacce9d86ef2a85
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-03-08 09:50:00 +0100

    BEEL-550 migrate to vesrion 3 successfull

--- axon/src/main/java/com/baeldung/axon/MessagesRunner.java
+++ axon/src/main/java/com/baeldung/axon/MessagesRunner.java
@@ -10,15 +10,11 @@ import org.axonframework.commandhandling.SimpleCommandBus;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.commandhandling.gateway.DefaultCommandGateway;
 import org.axonframework.eventhandling.AnnotationEventListenerAdapter;
-import org.axonframework.eventhandling.EventBus;
-import org.axonframework.eventhandling.SimpleEventBus;
 import org.axonframework.eventsourcing.EventSourcingRepository;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 
-import java.io.File;
 import java.util.UUID;
 
 public class MessagesRunner {
@@ -29,17 +25,27 @@ public class MessagesRunner {
         CommandGateway commandGateway = new DefaultCommandGateway(commandBus);
 
         EventStore eventStore = new EmbeddedEventStore(new InMemoryEventStorageEngine());
-        EventBus eventBus = new SimpleEventBus();
 
         EventSourcingRepository<MessagesAggregate> repository =
-                new EventSourcingRepository<MessagesAggregate>(MessagesAggregate.class, eventStore);
+                new EventSourcingRepository<>(MessagesAggregate.class, eventStore);
 
 
         AggregateAnnotationCommandHandler<MessagesAggregate> messagesAggregateAggregateAnnotationCommandHandler =
                 new AggregateAnnotationCommandHandler<MessagesAggregate>(MessagesAggregate.class, repository);
         messagesAggregateAggregateAnnotationCommandHandler.subscribe(commandBus);
 
-        AnnotationEventListenerAdapter annotationEventListenerAdapter = new AnnotationEventListenerAdapter(new MessagesEventHandler());
+        final AnnotationEventListenerAdapter annotationEventListenerAdapter =
+                new AnnotationEventListenerAdapter(new MessagesEventHandler());
+        eventStore.subscribe(eventMessages -> eventMessages.forEach(e -> {
+                    try {
+                        annotationEventListenerAdapter.handle(e);
+                    } catch (Exception e1) {
+                        throw new RuntimeException(e1);
+
+                    }
+                }
+
+        ));
 
         final String itemId = UUID.randomUUID().toString();
         commandGateway.send(new CreateMessageCommand(itemId, "Hello, how is your day? :-)"));
