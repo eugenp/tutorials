72abae2aa2b40aed151b18e12aa862263641f288
AndrewEvans <aevans30@msudenver.edu>
2017-03-09 21:25:08 -0700

    javaslang updates

---
+++ spring-data-javaslang/src/com/baeldung/spring_data_javaslang/repository/BookRepository.java
@@ -0,0 +1,20 @@
+package com.baeldung.spring_data_javaslang.repository;
+
+import com.baeldung.spring_data_javaslang.model.Book;
+
+
+import javaslang.collection.Seq;
+import javaslang.control.Option;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long>{
+    Book save(Book book);
+        
+    Option<Book> findById(Long id);
+        
+    Option<Seq<Book>> findByTitleContaining(String title);
+
+}
