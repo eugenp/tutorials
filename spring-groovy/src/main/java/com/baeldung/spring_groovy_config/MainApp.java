dcc7dede28815a60cfc30b19b7efd67479e0e6a3
AndrewEvans <aevans30@msudenver.edu>
2017-03-11 14:02:05 -0700

    cleanup

--- spring-groovy/src/main/java/com/baeldung/spring_groovy_config/MainApp.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.spring_groovy_config;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericGroovyApplicationContext;
-
-public class MainApp {
-    ApplicationContext context = new GenericGroovyApplicationContext("file:config/groovyPropConfig.groovy");
-    TestClassB test = (TestClassB) context.getBean("testClassB"); //see the test class AppTest.java for more examples
-}
