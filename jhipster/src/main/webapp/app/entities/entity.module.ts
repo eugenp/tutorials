fb867c6134b35c2853d3353be0c144e277fad043
Felipe Reis <felipe@glr.com.br>
2017-03-15 18:41:53 -0300

    feat: Create basic Monolithic JHipster project
    Commit just after creating a JHipster project, before making any modifications.
    
    Resolves: BAEL-137

---
+++ jhipster/src/main/webapp/app/entities/entity.module.ts
@@ -0,0 +1,14 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+/* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
+
+@NgModule({
+    imports: [
+        /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
+    ],
+    declarations: [],
+    entryComponents: [],
+    providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class BaeldungEntityModule {}
