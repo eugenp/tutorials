7e273de903a737e26b51402c2355c91bf56d73df
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-03-16 14:40:48 +0100

    BAEL-724 add javaslang test and property testing example

---
+++ javaslang/src/test/java/com/baeldung/javaslang/PropertyBasedTest.java
@@ -0,0 +1,66 @@
+package com.baeldung.javaslang;
+
+
+import javaslang.CheckedFunction1;
+import javaslang.collection.Stream;
+import javaslang.test.Arbitrary;
+import javaslang.test.CheckResult;
+import javaslang.test.Property;
+import org.junit.Test;
+
+public class PropertyBasedTest {
+
+    public Stream<String> stringsSupplier() {
+        return Stream.from(0).map(i -> {
+            boolean divByTwo = i % 2 == 0;
+            boolean divByFive = i % 5 == 0;
+
+            if(divByFive && divByTwo){
+                return "DividedByTwoAndFiveWithoutReminder";
+            }else if(divByFive){
+                return "DividedByFiveWithoutReminder";
+            }else if(divByTwo){
+                return "DividedByTwoWithoutReminder";
+            }
+            return "";
+        });
+    }
+
+    @Test
+    public void givenArbitrarySeq_whenCheckThatEverySecondElementIsEqualToString_thenTestPass() {
+        //given
+        Arbitrary<Integer> multiplesOf2 = Arbitrary.integer()
+                .filter(i -> i > 0)
+                .filter(i -> i % 2 == 0 && i % 5 != 0);
+
+        //when
+        CheckedFunction1<Integer, Boolean> mustStartWith =
+                i -> stringsSupplier().get(i).equals("DividedByTwoWithoutReminder");
+
+
+        //then
+        CheckResult result = Property
+                .def("Every second element must equal to DividedByTwoWithoutReminder")
+                .forAll(multiplesOf2)
+                .suchThat(mustStartWith)
+                .check(10_000, 100);
+
+        result.assertIsSatisfied();
+    }
+
+    @Test
+    public void givenArbitrarySeq_whenCheckThatEveryFifthElementIsEqualToString_thenTestPass() {
+        Arbitrary<Integer> multiplesOf5 = Arbitrary.integer()
+                .filter(i -> i > 0)
+                .filter(i -> i % 5 == 0 && i % 2 == 0);
+
+        CheckedFunction1<Integer, Boolean> mustStartWith = i ->
+                stringsSupplier().get(i).endsWith("DividedByTwoAndFiveWithoutReminder");
+
+        Property.def("Every fifth element must equal to DividedByTwoAndFiveWithoutReminder")
+                .forAll(multiplesOf5)
+                .suchThat(mustStartWith)
+                .check(10_000, 1_000)
+                .assertIsSatisfied();
+    }
+}
