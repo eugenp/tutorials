9c6afde90052222a584c6104a802b4bc981c1bad
Felipe Reis <felipe@glr.com.br>
2017-03-16 20:22:57 -0300

    feat: Create entities Post and Comment
    Relates to: BAEL-137

--- jhipster/src/main/java/com/baeldung/config/CacheConfiguration.java
+++ jhipster/src/main/java/com/baeldung/config/CacheConfiguration.java
@@ -40,6 +40,8 @@ public class CacheConfiguration {
             cm.createCache(com.baeldung.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.baeldung.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.baeldung.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.baeldung.domain.Post.class.getName(), jcacheConfiguration);
+            cm.createCache(com.baeldung.domain.Comment.class.getName(), jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
