9dce52d34530b4ad018ea2a0967953816edc83f3
XealRedan <alexandre.lombard@live.fr>
2017-03-16 21:12:04 +0100

    Example code showing how to return images using @ResponseBody

--- spring-rest/src/main/java/com/baeldung/produceimage/controller/DataProducerController.java
+++ spring-rest/src/main/java/com/baeldung/produceimage/controller/DataProducerController.java
@@ -12,20 +12,25 @@ import java.io.InputStream;
 @Controller
 public class DataProducerController {
 
-    @GetMapping(value = "/get-image-without-mediatype")
-    @ResponseBody public byte[] getImageWithoutMediaType() throws IOException {
+    @GetMapping("/get-text")
+    public @ResponseBody String getText() {
+        return "Hello world";
+    }
+
+    @GetMapping(value = "/get-image")
+    public @ResponseBody byte[] getImage() throws IOException {
         final InputStream in = getClass().getResourceAsStream("/com/baeldung/produceimage/image.jpg");
         return IOUtils.toByteArray(in);
     }
 
-    @GetMapping(value = "/get-image", produces = MediaType.IMAGE_JPEG_VALUE)
-    @ResponseBody public byte[] getImage() throws IOException {
+    @GetMapping(value = "/get-image-with-media-type", produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody byte[] getImageWithMediaType() throws IOException {
         final InputStream in = getClass().getResourceAsStream("/com/baeldung/produceimage/image.jpg");
         return IOUtils.toByteArray(in);
     }
 
     @GetMapping(value = "/get-file", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    @ResponseBody public byte[] getFile() throws IOException {
+    public @ResponseBody byte[] getFile() throws IOException {
         final InputStream in = getClass().getResourceAsStream("/com/baeldung/produceimage/data.txt");
         return IOUtils.toByteArray(in);
     }
