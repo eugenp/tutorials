e6cd066425e9ab25f415905775a687fe7b4fa4da
Kunwarjee Pathak <kunwarjee.pathak@gmail.com>
2017-03-18 01:04:00 +0530

    Revert "Files committed for article on different types of Bean injection"
    This reverts commit dce024d07af268014d71e49c8e5bcfdc360f30d4.

--- spring-core/src/main/java/com/baeldung/injection/consumer/AccountTransfer.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.injection.consumer;
-
-import java.math.BigDecimal;
-
-public interface AccountTransfer {
-    boolean deposit(BigDecimal amount);
-
-    boolean withdraw(BigDecimal amount);
-}
--- spring-core/src/main/java/com/baeldung/noinjection/consumer/AccountTransfer.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.noinjection.consumer;
-
-import java.math.BigDecimal;
-
-public interface AccountTransfer {
-    void deposit(BigDecimal amount);
-
-    void withdraw(BigDecimal amount);
-}
