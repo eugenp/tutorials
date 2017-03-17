925fb502914c96ab74b814533036d97608749995
Kunwarjee Pathak <kunwarjee.pathak@gmail.com>
2017-03-17 15:42:26 +0530

    Revert "different types of dependency injection"
    This reverts commit ebcc2ac0c73ace73b4165c53731a2f8df7f8495e.

--- spring-core/src/main/java/com/baeldung/injection/withinjection/consumer/AccountTransaction.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.injection.withinjection.consumer;
-
-import java.math.BigDecimal;
-
-public interface AccountTransaction {
-    void validateTransaction(BigDecimal amount);
-
-}
--- spring-core/src/main/java/com/baeldung/injection/withinjection/consumer/AccountTransfer.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.injection.withinjection.consumer;
-
-import java.math.BigDecimal;
-
-public interface AccountTransfer {
-	boolean deposit(BigDecimal amount);
-
-	boolean withdraw(BigDecimal amount);
-}
