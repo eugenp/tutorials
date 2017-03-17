e7e6326a0034fd40a5bc43ac5d35a00d5371cab7
root <jboadas@gmail.com>
2017-03-17 13:57:34 -0400

    Spring MVC Custom Validator

---
+++ spring-mvc-java/src/main/java/com/baeldung/customvalidator/ContactNumberValidator.java
@@ -0,0 +1,19 @@
+package com.baeldung.customvalidator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ContactNumberValidator implements ConstraintValidator<ContactNumberConstraint, String> {
+
+    @Override
+    public void initialize(ContactNumberConstraint contactNumber) {}
+
+    @Override
+    public boolean isValid(String contactField, ConstraintValidatorContext cxt) {
+        if(contactField == null) {
+            return false;
+        }
+        return contactField.matches("[0-9]+") && (contactField.length() > 8) && (contactField.length() < 14);        
+    }
+
+}
---
+++ spring-mvc-java/src/main/java/com/baeldung/web/controller/ValidatedPhoneController.java
@@ -0,0 +1,35 @@
+package com.baeldung.web.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import com.baeldung.model.ValidatedPhone;
+
+@Controller
+@EnableWebMvc
+public class ValidatedPhoneController {
+
+    @RequestMapping(value="/validatePhone", method=RequestMethod.GET)
+    public String loadFormPage(Model m) {
+            m.addAttribute("validatedPhone", new ValidatedPhone());
+            return "phoneHome";
+    }
+    
+    @RequestMapping(value="/addValidatePhone", method=RequestMethod.POST)
+    public String submitForm(@Valid ValidatedPhone validatedPhone, BindingResult result, Model m) {
+            if(result.hasErrors()) {
+                    return "phoneHome";
+            }
+            
+            m.addAttribute("message", "Successfully saved phone: " + validatedPhone.toString());
+            return "phoneHome";
+    }
+    
+    
+}
