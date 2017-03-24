11cdf67fad28587f3be0c1c4ca498ff541de81e2
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-03-24 20:06:32 +0100

    Merge modules (#1471)
    * Merge modules
    
    * Update pom.xml

--- spring-security-mvc-digest-auth/src/main/java/org/baeldung/basic/MyBasicAuthenticationEntryPoint.java
+++ spring-security-mvc-digest-auth/src/main/java/org/baeldung/basic/MyBasicAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package org.baeldung.security.basic;
+package org.baeldung.basic;
 
 import java.io.IOException;
 import java.io.PrintWriter;
--- spring-security-rest-basic-auth/src/main/java/org/baeldung/basic/MyBasicAuthenticationEntryPoint.java
+++ spring-security-rest-basic-auth/src/main/java/org/baeldung/basic/MyBasicAuthenticationEntryPoint.java
@@ -1,15 +1,14 @@
-package org.baeldung.security.basic;
-
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+package org.baeldung.basic;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Component
 public class MyBasicAuthenticationEntryPoint extends BasicAuthenticationEntryPoint {
