544c137f44f6dffb5f78c7797fb47cf676c7db8e
pivovarit <gpiwowarek@gmail.com>
2017-03-26 09:23:33 +0200

    Rename Spring5 tests

--- spring-5/src/test/java/com/baeldung/Example2IntegrationTest.java
+++ spring-5/src/test/java/com/baeldung/Example2IntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class IntegrationTestExample2 {
+public class Example2IntegrationTest {
 
     @Test
     public void test1a() {
