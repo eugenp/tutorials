eceed2593112afcdf4e747ca36fd9832800b6d5c
susetech <susetechnologies@gmail.com>
2017-03-26 11:23:44 +0530

    Json Response

---
+++ spring-boot/src/main/java/org/baeldung/domain/ResponseVO.java
@@ -0,0 +1,27 @@
+package org.baeldung.domain;
+
+public class ResponseVO {
+
+    private boolean success;
+    private String message;
+
+    public ResponseVO() {
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
