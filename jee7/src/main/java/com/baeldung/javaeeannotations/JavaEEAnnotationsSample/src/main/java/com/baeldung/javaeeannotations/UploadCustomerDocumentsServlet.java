757633e19aeaaf8ed7d27c4b75f0acbb2175c30b
buddhini81 <buddhini81@users.noreply.github.com>
2017-03-29 17:57:59 +0530

    Fix error - add missing import

--- jee7/src/main/java/com/baeldung/javaeeannotations/JavaEEAnnotationsSample/src/main/java/com/baeldung/javaeeannotations/UploadCustomerDocumentsServlet.java
+++ jee7/src/main/java/com/baeldung/javaeeannotations/JavaEEAnnotationsSample/src/main/java/com/baeldung/javaeeannotations/UploadCustomerDocumentsServlet.java
@@ -1,6 +1,7 @@
 package com.baeldung.javaeeannotations;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
