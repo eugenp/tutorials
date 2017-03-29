837761232c7f89bcb19585af3a0803ca9c5b34ba
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-03-29 19:33:38 +0200

    simpler wordCount example

--- libraries/src/main/java/com/baeldung/flink/WordCount.java
+++ libraries/src/main/java/com/baeldung/flink/WordCount.java
@@ -10,22 +10,11 @@ import java.util.List;
 
 public class WordCount {
 
-    public static void main(String[] args) throws Exception {
-
-        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
-
-        DataSet<Tuple2<String, Integer>> wordCountResult = startWordCount(env, Arrays.asList(args));
-        wordCountResult.print();
-
-        env.execute();
-    }
-
     public static DataSet<Tuple2<String, Integer>> startWordCount(ExecutionEnvironment env, List<String> lines) throws Exception {
         DataSet<String> text = env.fromCollection(lines);
 
         return text.flatMap(new LineSplitter())
                 .groupBy(0)
-                //.sum(1)
                 .aggregate(Aggregations.SUM, 1);
 
     }
