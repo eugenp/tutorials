859a5e6913c5d5db19f2264ff7ef81e3fec552eb
Eunice Adutwumwaa Obugyei <eunice.obugyei@gmail.com>
2017-03-30 06:41:46 +0000

    Applied baeldung formatter in Eclipse

--- jee7/src/main/java/com/baeldung/jaxws/exception/EmployeeNotFound.java
+++ jee7/src/main/java/com/baeldung/jaxws/exception/EmployeeNotFound.java
@@ -3,14 +3,15 @@ package com.baeldung.jaxws.exception;
 import javax.xml.ws.WebFault;
 import java.io.Serializable;
 
-@WebFault public class EmployeeNotFound extends Exception implements Serializable {
+@WebFault
+public class EmployeeNotFound extends Exception implements Serializable {
 
-        public EmployeeNotFound() {
-                super("The specified employee does not exist");
-        }
+    public EmployeeNotFound() {
+        super("The specified employee does not exist");
+    }
 
-        public EmployeeNotFound(String str) {
-                super(str);
-        }
+    public EmployeeNotFound(String str) {
+        super(str);
+    }
 
 }
