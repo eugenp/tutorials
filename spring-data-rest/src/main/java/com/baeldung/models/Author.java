8968fc74e6e1becd59a5e2317bba15fd35069e9d
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2017-03-31 18:29:39 +0300

    in memory dbs config

--- spring-data-rest/src/main/java/com/baeldung/models/Author.java
+++ spring-data-rest/src/main/java/com/baeldung/models/Author.java
@@ -6,6 +6,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -15,7 +16,7 @@ import javax.persistence.ManyToMany;
 public class Author {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
