fae61792c698bc228f458dab816c1516a15133be
Eunice Adutwumwaa Obugyei <eunice.obugyei@gmail.com>
2017-04-02 07:22:27 +0000

    Introduction to JAX-WS[http://jira.baeldung.com/browse/BAEL-611]

--- jee7/src/main/java/com/baeldung/jaxws/exception/EmployeeNotFound.java
+++ jee7/src/main/java/com/baeldung/jaxws/exception/EmployeeNotFound.java
@@ -1,10 +1,9 @@
 package com.baeldung.jaxws.exception;
 
 import javax.xml.ws.WebFault;
-import java.io.Serializable;
 
 @WebFault
-public class EmployeeNotFound extends Exception implements Serializable {
+public class EmployeeNotFound extends Exception {
 
     public EmployeeNotFound() {
         super("The specified employee does not exist");
