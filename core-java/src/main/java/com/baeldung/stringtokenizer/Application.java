abd1167398e498a035a4fcab05f6cc964252440b
abhinabkanrar <abhinabkanrar@gmail.com>
2017-04-05 18:41:24 +0530

    adding csv test

--- core-java/src/main/java/com/baeldung/stringtokenizer/Application.java
+++ core-java/src/main/java/com/baeldung/stringtokenizer/Application.java
@@ -1,5 +1,8 @@
 package com.baeldung.stringtokenizer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -8,15 +11,34 @@ public class Application {
 
 	public List<String> getTokens(String str) {
 		List<String> tokens = new ArrayList<String>();
-//		StringTokenizer tokenizer = new StringTokenizer( str );
-		StringTokenizer tokenizer = new StringTokenizer( str , "," );
-//		StringTokenizer tokenizer = new StringTokenizer( str , "," , true );
+		// StringTokenizer tokenizer = new StringTokenizer( str );
+		StringTokenizer tokenizer = new StringTokenizer(str, ",");
+		// StringTokenizer tokenizer = new StringTokenizer( str , "," , true );
 		while (tokenizer.hasMoreElements()) {
-			tokens.add( tokenizer.nextToken() );
-//			tokens.add( tokenizer.nextToken("e") );
+			tokens.add(tokenizer.nextToken());
+			// tokens.add( tokenizer.nextToken("e") );
 		}
 		int tokenLength = tokens.size();
 		return tokens;
 	}
+
+	public List<String> getTokensFromFile(String path, String delim) {
+		List<String> tokens = new ArrayList<String>();
+		String currLine = "";
+		StringTokenizer tokenizer;
+		try (BufferedReader br = new BufferedReader(
+				new InputStreamReader(Application.class.getResourceAsStream("/" + path)))) {
+			while ((currLine = br.readLine()) != null) {
+				tokenizer = new StringTokenizer(currLine, delim);
+				while (tokenizer.hasMoreElements()) {
+					tokens.add(tokenizer.nextToken());
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return tokens;
+	}
 	
+
 }
