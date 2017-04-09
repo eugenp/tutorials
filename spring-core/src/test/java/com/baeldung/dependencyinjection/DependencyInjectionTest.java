3dca8db0629ba72e7a9dcc5babb92296b87a2f14
Bereket Tesfamichael <neheworld@gmail.com>
2017-04-09 07:04:22 +0300

    renamed method argument setApplicationContext

--- spring-core/src/test/java/com/baeldung/dependencyinjection/DependencyInjectionTest.java
+++ spring-core/src/test/java/com/baeldung/dependencyinjection/DependencyInjectionTest.java
@@ -17,20 +17,19 @@ import com.baeldung.dependencyinjection.domain.Building;
 public class DependencyInjectionTest implements ApplicationContextAware {
 
     private ApplicationContext beanInjectedContext;
-    
-    
+
     @Test
-    public void testDependencyInjection(){
-       
+    public void testDependencyInjection() {
+
         Building buildingBean = beanInjectedContext.getBean(Building.class);
-        
+
         assertEquals("Escalator is available and Air-Conditioner is available", buildingBean.toString());
     }
-    
+
     @Override
-    public void setApplicationContext(ApplicationContext arg0) throws BeansException {
-        // TODO Auto-generated method stub
-        this.beanInjectedContext = arg0;
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+        this.beanInjectedContext = applicationContext;
     }
 
 }
