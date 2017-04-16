76409e6d53b9b4a428dfbc5b4f1294bc7e21ca63
baljeet20 <baljeet20@gmail.com>
2017-04-16 21:57:01 +0530

    BAEL-788 Added java config (#1660)
    * BAEL-788 Added java config
    
    * BAEL-788 Removed XML config

--- mybatis/src/main/java/com/baeldung/mybatis/utils/MyBatisUtil.java
+++ mybatis/src/main/java/com/baeldung/mybatis/utils/MyBatisUtil.java
@@ -1,24 +1,39 @@
 package com.baeldung.mybatis.utils;
+
+import com.baeldung.mybatis.mapper.AddressMapper;
+import com.baeldung.mybatis.mapper.PersonMapper;
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.jdbc.SQL;
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MyBatisUtil {
+    public static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
+    public static final String URL = "jdbc:derby:testdb1;create=true";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "pass123";
     private static SqlSessionFactory sqlSessionFactory;
-    static {
-        String resource = "mybatis-config.xml";
-        InputStream inputStream;
-        try {
-            inputStream = Resources.getResourceAsStream(resource);
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+    public static SqlSessionFactory buildqlSessionFactory(){
+        DataSource dataSource=new PooledDataSource(DRIVER, URL, USERNAME, PASSWORD);
+        Environment environment=new Environment("Development",new JdbcTransactionFactory(),dataSource);
+        Configuration configuration = new Configuration(environment);
+        configuration.addMapper(PersonMapper.class);
+        configuration.addMapper(AddressMapper.class);
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(configuration);
+        return factory;
+
     }
+
     public static SqlSessionFactory getSqlSessionFactory(){
         return sqlSessionFactory;
     }
