d1194fb8f1b3fe4c6b72033e96407b1fe9a5a3df
DOHA <dohae243@gmail.com>
2017-04-18 14:37:17 +0200

    remove log4j dependencies

--- core-java/src/test/java/org/baeldung/core/exceptions/FileNotFoundExceptionUnitTest.java
+++ core-java/src/test/java/org/baeldung/core/exceptions/FileNotFoundExceptionUnitTest.java
@@ -6,12 +6,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.junit.Test;
 
 public class FileNotFoundExceptionUnitTest {
 
-    private static final Logger LOG = Logger.getLogger(FileNotFoundExceptionUnitTest.class);
+    private static final Logger LOG = LoggerFactory.getLogger(FileNotFoundExceptionUnitTest.class);
 
     private String fileName = Double.toString(Math.random());
 
