1da57f530bc52b7cddbe5ee5b494dd42a8a78f07
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-04-18 15:54:41 +0200

    BAEL-845 m to m example

--- core-java/src/main/java/com/baeldung/transferqueue/TransferQueueExampleMultipleProducersMultipleConsumers.java
+++ core-java/src/main/java/com/baeldung/transferqueue/TransferQueueExampleMultipleProducersMultipleConsumers.java
@@ -6,18 +6,20 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedTransferQueue;
 import java.util.concurrent.TransferQueue;
 
-public class TransferQueueExampleMultipleProducersOneConsumer {
+public class TransferQueueExampleMultipleProducersMultipleConsumers {
     static TransferQueue<String> transferQueue = new LinkedTransferQueue<>();
 
     public static void main(String[] args) {
         ExecutorService exService = Executors.newFixedThreadPool(3);
         Producer producer1 = new Producer(transferQueue, "1", 3);
         Producer producer2 = new Producer(transferQueue, "2", 3);
-        Consumer consumer = new Consumer(transferQueue, "1", 6);
+        Consumer consumer1 = new Consumer(transferQueue, "1", 3);
+        Consumer consumer2 = new Consumer(transferQueue, "2", 3);
 
         exService.execute(producer1);
         exService.execute(producer2);
-        exService.execute(consumer);
+        exService.execute(consumer1);
+        exService.execute(consumer2);
 
         exService.shutdown();
     }
