5696b258890cffdf6d1794b8f3b1b18869a01877
Adam Zawada <adam.zawada@roche.com>
2017-04-18 09:14:09 +0200

    adam.zawada@gmail.com - Different Types of Bean Injection in Spring switch to Java based configuration

--- spring-dependency-injection/src/main/java/com/baeldung/spring/dependencyInjection/DependencyInjectionApplication.java
+++ spring-dependency-injection/src/main/java/com/baeldung/spring/dependencyInjection/DependencyInjectionApplication.java
@@ -1,19 +1,19 @@
 package com.baeldung.spring.dependencyInjection;
 
 import com.baeldung.spring.dependencyInjection.beans.MrBean;
+import com.baeldung.spring.dependencyInjection.config.BeanConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DependencyInjectionApplication.class, args);
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-
-        MrBean bean = (MrBean) context.getBean("mrBean");
+        ApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+        MrBean bean = context.getBean(MrBean.class);
         bean.sayHello();
     }
 
