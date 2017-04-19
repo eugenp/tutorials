709857b9aab7e6b63f0a051aa816a1a9c028722b
Mateusz Mrozewski <matixo@gmail.com>
2017-04-19 12:02:42 +0400

    BAEL-427 Examples for topic and fanout exchanges. (#1648)
    * BAEL-427 Examples for topic and fanout exchanges.
    
    * BAEL-427 Separating code for the new article from the old one

--- spring-amqp-simple/src/main/java/com/baeldung/springamqpsimple/MessageConsumer.java
+++ spring-amqp-simple/src/main/java/com/baeldung/springamqpsimple/MessageConsumer.java
@@ -2,7 +2,7 @@ package com.baeldung.springamqpsimple;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +10,7 @@ public class MessageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
+    @RabbitListener(queues = {SpringAmqpConfig.queueName})
     public void receiveMessage(String message) {
         logger.info("Received Message: " + message);
     }
