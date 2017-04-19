51d28bcc95ba18e5d08296040d8957f599423f5e
DOHA <dohae243@gmail.com>
2017-04-19 13:10:07 +0200

    merge

--- spring-amqp-simple/src/main/java/com/baeldung/springamqpsimple/MessageConsumer.java
+++ spring-amqp-simple/src/main/java/com/baeldung/springamqpsimple/MessageConsumer.java
@@ -2,7 +2,11 @@ package com.baeldung.springamqpsimple;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+<<<<<<< HEAD
 
+=======
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+>>>>>>> eugenp/master
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +14,7 @@ public class MessageConsumer {
 
     private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
 
+    @RabbitListener(queues = {SpringAmqpConfig.queueName})
     public void receiveMessage(String message) {
         logger.info("Received Message: " + message);
     }
---
+++ spring-boot-custom-starter/greeter/src/main/java/com/baeldung/greeter/GreetingConfig.java
@@ -0,0 +1,9 @@
+package com.baeldung.greeter;
+
+import java.util.Properties;
+
+public class GreetingConfig extends Properties{
+    
+    private static final long serialVersionUID = 5662570853707247891L;
+
+}
