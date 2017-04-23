39092270f96ac9b4872089902f83c6e12c06f1ab
Chandravadan <s.chandravadan@gmail.com>
2017-04-24 00:04:25 +0530

    Renamed ConfigPropertiesTest to Integration test
    (BAEL-697)

--- spring-boot/src/test/java/org/baeldung/properties/ConfigPropertiesIntegrationTest.java
+++ spring-boot/src/test/java/org/baeldung/properties/ConfigPropertiesIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ConfigPropertiesDemoApplication.class)
 @TestPropertySource("classpath:configprops-test.properties")
-public class ConfigPropertiesTest {
+public class ConfigPropertiesIntegrationTest {
 
     @Autowired
     private ConfigProperties properties;
