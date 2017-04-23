dee0e4c98b09338a32941a2ca7e1c6cdbc3c2660
Chandravadan <s.chandravadan@gmail.com>
2017-04-23 23:35:53 +0530

    Optimisation of imports
    (BAEL-697)

--- spring-boot/src/main/java/org/baeldung/properties/ConfigPropertiesDemoApplication.java
+++ spring-boot/src/main/java/org/baeldung/properties/ConfigPropertiesDemoApplication.java
@@ -1,11 +1,10 @@
 package org.baeldung.properties;
 
-import com.baeldung.autoconfiguration.MySQLAutoconfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
+
+import com.baeldung.autoconfiguration.MySQLAutoconfiguration;
 
 @EnableAutoConfiguration(exclude = MySQLAutoconfiguration.class)
 @ComponentScan(basePackageClasses = ConfigProperties.class)
