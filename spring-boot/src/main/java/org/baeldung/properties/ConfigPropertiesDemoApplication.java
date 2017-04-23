3fca81e81b378cb183f50475dacb8c1830efd14f
Chandravadan <s.chandravadan@gmail.com>
2017-04-23 09:49:39 +0530

    Initial commit for @ConfigurationProperties
    (BAEL-697)

---
+++ spring-boot/src/main/java/org/baeldung/properties/ConfigPropertiesDemoApplication.java
@@ -0,0 +1,11 @@
+package org.baeldung.properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigPropertiesDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigPropertiesDemoApplication.class);
+    }
+}
