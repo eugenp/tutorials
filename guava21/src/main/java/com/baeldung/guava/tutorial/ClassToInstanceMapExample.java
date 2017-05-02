99c20436e990b4966b3892b6caf8309608339b26
Mateusz Mrozewski <mateusz@tlt.local>
2017-05-02 16:09:59 +0400

    BAEL-642 Improved example for ClassToInstanceMap

--- guava21/src/main/java/com/baeldung/guava/tutorial/ClassToInstanceMapExample.java
+++ guava21/src/main/java/com/baeldung/guava/tutorial/ClassToInstanceMapExample.java
@@ -1,15 +1,25 @@
 package com.baeldung.guava.tutorial;
 
 import com.google.common.collect.ClassToInstanceMap;
+import com.google.common.collect.ImmutableClassToInstanceMap;
 import com.google.common.collect.MutableClassToInstanceMap;
+
+import java.util.HashMap;
 
 public class ClassToInstanceMapExample {
     public static void main(String[] args) {
-        ClassToInstanceMap<Action> map = MutableClassToInstanceMap.create();
+        ClassToInstanceMap<Action> map = MutableClassToInstanceMap.create(new HashMap());
 
         map.put(Save.class, new Save());
-        map.put(Open.class, new Open());
-        map.putInstance(Delete.class, new Delete());
+        Open newOpen = (Open) map.put(Open.class, new Open());
+        Delete newDelete = map.putInstance(Delete.class, new Delete());
+
+
+        ImmutableClassToInstanceMap.<Action>builder()
+                .put(Save.class, new Save())
+                .put(Open.class, new Open())
+                .put(Delete.class, new Delete())
+                .build();
 
         Action saveAction = map.get(Save.class);
         Open openAction = (Open) map.get(Open.class);
