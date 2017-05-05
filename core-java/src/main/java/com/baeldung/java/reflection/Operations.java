55ee963480be8ea85da6cf64e7ea9123b965cfe0
Raúl Juárez <juarezjaramillo@gmail.com>
2017-05-05 11:25:42 -0500

    BAEL-870 How to Call a Method During Runtime Using Reflection

--- core-java/src/main/java/com/baeldung/java/reflection/Operations.java
+++ core-java/src/main/java/com/baeldung/java/reflection/Operations.java
@@ -1,17 +1,21 @@
 package com.baeldung.java.reflection;
 
 public class Operations {
-    
+
     public double sum(int a, double b) {
         return a + b;
     }
-    
-    public static double multiply(float a, long b){
+
+    public static double multiply(float a, long b) {
         return a * b;
     }
 
     private boolean and(boolean a, boolean b) {
         return a && b;
     }
-    
+
+    protected int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
 }
