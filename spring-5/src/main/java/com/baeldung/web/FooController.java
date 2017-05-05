fbdada5df9f6ac554a887cf67bffef1a8c53710b
pivovarit <gpiwowarek@gmail.com>
2017-05-05 07:17:31 +0200

    Fix Spring5 snippets

--- spring-5/src/main/java/com/baeldung/web/FooController.java
+++ spring-5/src/main/java/com/baeldung/web/FooController.java
@@ -1,26 +1,16 @@
 package com.baeldung.web;
 
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-
+import com.baeldung.persistence.FooRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.baeldung.persistence.FooRepository;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.util.List;
 
 @RestController("/foos")
 public class FooController {
@@ -34,7 +24,7 @@ public class FooController {
     @ResponseBody
     @Validated
     public Foo findById(@PathVariable @Min(0) final long id) {
-        return repo.findOne(id).orElse(null);
+        return repo.findById(id).orElse(null);
     }
 
     @RequestMapping(method = RequestMethod.GET)
