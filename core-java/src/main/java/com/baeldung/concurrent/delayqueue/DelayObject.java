aca4bc61677f196fea3dfe1c6b82a3a0e5e1cbcb
Tomasz Lelek <tomasz.lelek@allegrogroup.com>
2017-05-06 12:48:12 +0200

    BAEL-882 Delay_queue article code

---
+++ core-java/src/main/java/com/baeldung/concurrent/delayqueue/DelayObject.java
@@ -0,0 +1,40 @@
+package com.baeldung.concurrent.delayqueue;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+public class DelayObject implements Delayed {
+    private String data;
+    private long startTime;
+
+    public DelayObject(String data, long delayInMilliseconds) {
+        this.data = data;
+        this.startTime = System.currentTimeMillis() + delayInMilliseconds;
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) { //It needs to get delay in nanoseconds, so need to convert
+        long diff = startTime - System.currentTimeMillis();
+        System.out.println(diff);
+        return unit.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        if (this.startTime < ((DelayObject) o).startTime) {
+            return -1;
+        }
+        if (this.startTime > ((DelayObject) o).startTime) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "data='" + data + '\'' +
+                ", startTime=" + startTime +
+                '}';
+    }
+}
