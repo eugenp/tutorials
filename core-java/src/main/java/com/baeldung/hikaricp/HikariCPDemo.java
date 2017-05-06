c301535b23648343228ab5ad4cc19e41f0e41a4b
abhinabkanrar <abhinabkanrar@gmail.com>
2017-05-06 19:16:31 +0530

    try-with-resources

--- core-java/src/main/java/com/baeldung/hikaricp/HikariCPDemo.java
+++ core-java/src/main/java/com/baeldung/hikaricp/HikariCPDemo.java
@@ -8,14 +8,10 @@ import java.sql.SQLException;
 public class HikariCPDemo {
 
 	public static void fetchData() {
-		Connection con = null;
-		PreparedStatement pst = null;
-		ResultSet rs = null;
 		final String SQL_QUERY = "select * from user_auth_details";
-		try {
-			con = DataSource.getConnection();
-			pst = con.prepareStatement(SQL_QUERY);
-			rs = pst.executeQuery();
+		try ( Connection con = DataSource.getConnection(); 
+				PreparedStatement pst = con.prepareStatement(SQL_QUERY); 
+				ResultSet rs = pst.executeQuery(); ) {
 			while (rs.next()) {
 				System.out.println(
 						String.format("Username : %s, Password : %s, Role : %s, Mail : %s", rs.getString("username"),
@@ -23,20 +19,6 @@ public class HikariCPDemo {
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null) {
-					rs.close();
-				}
-				if (pst != null) {
-					pst.close();
-				}
-				if (con != null) {
-					con.close();
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
 		}
 	}
 
