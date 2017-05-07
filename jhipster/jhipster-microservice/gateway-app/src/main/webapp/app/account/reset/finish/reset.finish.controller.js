3183513573cbb0111c7217d630dec5e1e8bc8119
Felipe Reis <felipe@glr.com.br>
2017-05-07 20:03:06 +1000

    feat: Add JHipster Microservice modules
    closes BAEL-521

---
+++ jhipster/jhipster-microservice/gateway-app/src/main/webapp/app/account/reset/finish/reset.finish.controller.js
@@ -0,0 +1,39 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('gatewayApp')
+        .controller('ResetFinishController', ResetFinishController);
+
+    ResetFinishController.$inject = ['$stateParams', '$timeout', 'Auth', 'LoginService'];
+
+    function ResetFinishController ($stateParams, $timeout, Auth, LoginService) {
+        var vm = this;
+
+        vm.keyMissing = angular.isUndefined($stateParams.key);
+        vm.confirmPassword = null;
+        vm.doNotMatch = null;
+        vm.error = null;
+        vm.finishReset = finishReset;
+        vm.login = LoginService.open;
+        vm.resetAccount = {};
+        vm.success = null;
+
+        $timeout(function (){angular.element('#password').focus();});
+
+        function finishReset() {
+            vm.doNotMatch = null;
+            vm.error = null;
+            if (vm.resetAccount.password !== vm.confirmPassword) {
+                vm.doNotMatch = 'ERROR';
+            } else {
+                Auth.resetPasswordFinish({key: $stateParams.key, newPassword: vm.resetAccount.password}).then(function () {
+                    vm.success = 'OK';
+                }).catch(function () {
+                    vm.success = null;
+                    vm.error = 'ERROR';
+                });
+            }
+        }
+    }
+})();
---
+++ jhipster/jhipster-microservice/gateway-app/src/main/webapp/app/services/auth/register.service.js
@@ -0,0 +1,13 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('gatewayApp')
+        .factory('Register', Register);
+
+    Register.$inject = ['$resource'];
+
+    function Register ($resource) {
+        return $resource('api/register', {}, {});
+    }
+})();
