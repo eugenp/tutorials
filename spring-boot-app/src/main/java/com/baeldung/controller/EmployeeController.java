f824122d69ce8894ec965f1b4bcbf500fb3207db
Loredana <lcrusoveanu@optaros.com>
2017-05-08 15:22:46 +0300

    boot example app

---
+++ spring-boot-app/src/main/java/com/baeldung/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.baeldung.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import com.baeldung.model.Employee;
+import com.baeldung.repository.EmployeeRepository;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @PostMapping("/employees")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addEmployee(@RequestBody Employee employee) {
+        employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeRepository.findAll();
+    }
+}
