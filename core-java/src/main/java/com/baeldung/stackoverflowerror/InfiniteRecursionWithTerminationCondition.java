5aa3f50b5d3fd69cb5a45512e5174165bb332c92
buddhini81 <buddhini81@users.noreply.github.com>
2017-05-09 08:40:35 +0530

    Refactor code for BAEL-876

--- core-java/src/main/java/com/baeldung/stackoverflowerror/InfiniteRecursionWithTerminationCondition.java
+++ core-java/src/main/java/com/baeldung/stackoverflowerror/InfiniteRecursionWithTerminationCondition.java
@@ -1,15 +1,7 @@
 package com.baeldung.stackoverflowerror;
 
 public class InfiniteRecursionWithTerminationCondition {
-    
-    public static int calculateFactorial(final int number) {
+    public int calculateFactorial(final int number) {
        return number == 1 ? 1 : number * calculateFactorial(number-1);
     }
-
-    public static void main(String[] args) {
-        final int numberToCalcFactorial= -1;
-        System.out.println(calculateFactorial(numberToCalcFactorial));
-        
-    }
-
 }
