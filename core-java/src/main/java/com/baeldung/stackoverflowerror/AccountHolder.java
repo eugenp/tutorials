dc813b8c3eedf573f70a7aa5aa49926382e87ab9
buddhini81 <buddhini81@users.noreply.github.com>
2017-05-09 08:36:09 +0530

    Refactoring code for BAEL-876

--- core-java/src/main/java/com/baeldung/stackoverflowerror/AccountHolder.java
+++ core-java/src/main/java/com/baeldung/stackoverflowerror/AccountHolder.java
@@ -1,18 +1,9 @@
 package com.baeldung.stackoverflowerror;
 
-
+　
 public class AccountHolder {
-    
-    private String firstName = "Sam";
-    private String lastName = "Alex"; 
+    private String firstName;
+    private String lastName;
     
     AccountHolder jointAccountHolder = new AccountHolder();
-    
-    public static void main(String[] args) {
-        AccountHolder holder = new AccountHolder();
-        System.out.println("Account Holder Name : " + holder.firstName + " " 
-        + holder.lastName);
-        System.out.println("Joint Account Holder Name : " 
-        + holder.jointAccountHolder.firstName + " " + holder.jointAccountHolder.lastName);
-    }
 }
