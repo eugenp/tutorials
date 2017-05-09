fc1e4678a5c7a3b9aa699d5e53c5fc7fa00c8553
buddhini81 <buddhini81@users.noreply.github.com>
2017-05-09 08:41:37 +0530

    Refactor code for BAEL-876

--- core-java/src/main/java/com/baeldung/stackoverflowerror/RecursionWithCorrectTerminationCondition.java
+++ core-java/src/main/java/com/baeldung/stackoverflowerror/RecursionWithCorrectTerminationCondition.java
@@ -4,10 +4,4 @@ public class RecursionWithCorrectTerminationCondition {
     public static int calculateFactorial(final int number) {
         return number <= 1 ? 1 : number * calculateFactorial(number-1);
      }
-
-     public static void main(String[] args) {
-         final int numberToCalcFactorial= -1;
-         System.out.println(calculateFactorial(numberToCalcFactorial));
-         
-     }
 }
