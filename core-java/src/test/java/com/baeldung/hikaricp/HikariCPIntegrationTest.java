62556c37103817841e6f38f2566c41c60cb433c2
DOHA <dohae243@gmail.com>
2017-05-15 18:23:39 +0200

    fix test names

---
+++ core-java/src/test/java/com/baeldung/hikaricp/HikariCPIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.baeldung.hikaricp;
+
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class HikariCPIntegrationTest {
+
+	@Test
+//	@Ignore
+    public void givenConnection_thenFetchDbData() {
+		List<Employee> employees = HikariCPDemo.fetchData();
+		assertEquals(4, employees.size());
+	}
+	
+}
