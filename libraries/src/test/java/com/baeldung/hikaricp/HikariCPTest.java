f22d3ae1d6e5b63cb60743dcf9bc53504ee8cf31
abhinabkanrar <abhinabkanrar@gmail.com>
2017-05-17 13:54:46 +0530

    merge conflict

--- libraries/src/test/java/com/baeldung/hikaricp/HikariCPTest.java
+++ libraries/src/test/java/com/baeldung/hikaricp/HikariCPTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class HikariCPTest {
 
 	@Test
-    public void givenConnection_thenFetchDbData() {
+        public void givenConnection_thenFetchDbData() {
 		List<Employee> employees = HikariCPDemo.fetchData();
 		assertEquals(4, employees.size());
 	}
