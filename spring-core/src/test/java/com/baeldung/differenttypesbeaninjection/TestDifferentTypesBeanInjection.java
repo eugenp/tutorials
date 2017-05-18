93f81fa98fe7cd3f398ba8132ae9c2e51e6ddc4b
hariprasad108 <petr.hariprasad@gmail.com>
2017-05-18 13:46:07 +0200

    Create TestDifferentTypesBeanInjection.java
    Additionally added. File disappeared.

---
+++ spring-core/src/test/java/com/baeldung/differenttypesbeaninjection/TestDifferentTypesBeanInjection.java
@@ -0,0 +1,46 @@
+package com.baeldung.autowired;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestDifferentTypesBeanInjection {
+    InitBeans tb = new InitBeans();
+
+    @Test
+    public void givenWiredAttibute_whenIsNotNull_thenSuccess() {
+        assertNotNull("Injection to Attribute", tb.getAttr());
+    }
+
+    @Test
+    public void givenWiredConstructorParam_whenIsNotNull_thenSuccess() {
+        assertNotNull("Injection to Constructor", tb.getCons());
+    }
+
+    @Test
+    public void givenWiredSetterMetodParam_whenIsNotNull_thenSuccess() {
+        assertNotNull("Injection to Method", tb.getSetter());
+    }
+
+    @Test
+    public void givenWiredAttibuteAndConstructorParam_whenAreNotIdentical_thenSuccess() {
+        assertNotEquals(tb.getAttr().tukTukBean(), tb.getCons().tukTukBean());
+    }
+
+    @Test
+    public void givenConstructorParamAndSetterMethodParam_whenAreNotIdentical_thenSuccess() {
+        assertNotEquals(tb.getCons().tukTukBean(), tb.getSetter().tukTukBean());
+    }
+
+    @Test
+    public void givenWiredAttibuteAndSetterMethodParam_whenAreNotIdentical_thenSuccess() {
+        assertNotEquals(tb.getAttr().tukTukBean(), tb.getSetter().tukTukBean());
+    }
+
+    protected void finalize() {
+        if (tb != null) {
+            tb.close();
+        }
+    }
+
+}
