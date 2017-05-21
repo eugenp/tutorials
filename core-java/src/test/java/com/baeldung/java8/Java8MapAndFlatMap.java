b841c9295c3e3bf0bd597fc122276886f0413643
Tomasz <tomasz.sobala@gmail.com>
2017-05-21 21:13:08 +0200

    [BAEL-872] map and flatMap difference.

---
+++ core-java/src/test/java/com/baeldung/java8/Java8MapAndFlatMap.java
@@ -0,0 +1,51 @@
+package com.baeldung.java8;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+public class Java8MapAndFlatMap {
+
+    @Test
+    public void givenStream_whenCalledMap_thenProduceOneResultValue() {
+        Object a = Stream.of("a", "b")
+                .map(String::toUpperCase)
+                .collect(Collectors.toList());
+        assertEquals(asList("A", "B"), a);
+    }
+
+    @Test
+    public void givenStream_whenCalledFlatMap_thenProduceResultValues() throws Exception {
+        List<String> a = Stream.of(asList("a"), asList("b", "c"))
+                .flatMap(List::stream)
+                .collect(Collectors.toList());
+        assertEquals(asList("a", "b", "c"), a);
+    }
+
+    @Test
+    public void givenOptional_whenCalledMap_thenProduceOneResultValue() {
+        Optional<String> s = Optional.of("test");
+        assertEquals(Optional.of("test"), s.map(Java8MapAndFlatMap::getOutput));
+    }
+
+    static String getOutput(String input) {
+        return input == null ? null : input;
+    }
+
+    @Test
+    public void givenOptional_whenCalledFlatMap_thenProduceResultValues() {
+        Optional<String> s = Optional.of("test");
+        assertEquals(Optional.of("test"), s.flatMap(Java8MapAndFlatMap::getOutputOptional));
+    }
+
+    static Optional<String> getOutputOptional(String input) {
+        return input == null ? Optional.empty() : Optional.of(input);
+    }
+
+}
