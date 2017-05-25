5f2a4a112546a09ddad379ba4bebeaa38794a932
Yasin <yasin.bhojawala@gmail.com>
2017-05-25 23:48:35 +0530

    BAEL-900 Guide to dynamic tests in Junit 5

--- junit5/src/test/java/com/baeldung/DynamicTestsExample.java
+++ junit5/src/test/java/com/baeldung/DynamicTestsExample.java
@@ -17,7 +17,13 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.ThrowingConsumer;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 
+import com.baeldung.helpers.Employee;
+import com.baeldung.helpers.EmployeeDao;
+
+@RunWith(JUnitPlatform.class)
 public class DynamicTestsExample {
 
     @TestFactory
@@ -111,6 +117,29 @@ public class DynamicTestsExample {
         
     }
 
+    @TestFactory
+    Stream<DynamicTest> dynamicTestsForEmployeeWorkflows() {
+        List<Employee> inputList =
+            Arrays.asList(new Employee(1, "Fred"), new Employee(2), new Employee(3, "John"));
+        
+        EmployeeDao dao = new EmployeeDao();
+        Stream<DynamicTest> saveEmployeeStream = inputList.stream().map(emp -> 
+            DynamicTest.dynamicTest("saveEmployee: " + emp.toString(), () -> {
+            Employee returned = dao.save(emp.getId());
+            assertEquals(returned.getId(), emp.getId());
+        }));
+        
+        Stream<DynamicTest> saveEmployeeWithFirstNameStream 
+            = inputList.stream().filter(emp -> !emp.getFirstName().isEmpty())
+            .map(emp -> DynamicTest.dynamicTest("saveEmployeeWithName" + emp.toString(), () -> {
+            Employee returned = dao.save(emp.getId(), emp.getFirstName());
+            assertEquals(returned.getId(), emp.getId());
+            assertEquals(returned.getFirstName(), emp.getFirstName());
+        }));
+        
+        return Stream.concat(saveEmployeeStream, saveEmployeeWithFirstNameStream);
+    }
+    
     class DomainNameResolver {
         
         private Map<String, String> ipByDomainName = new HashMap<>();
