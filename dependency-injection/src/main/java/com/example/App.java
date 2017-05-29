994c6f8c24d75e9082c9fa1b8d414d3f077a0682
Aatika Fatima <csentoes123@gmail.com>
2017-05-29 20:31:40 +0530

    comit

---
+++ dependency-injection/src/main/java/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
