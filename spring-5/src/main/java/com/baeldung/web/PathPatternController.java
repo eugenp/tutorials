733d84e595ca951c0e1b927b52ae50cdffc5cdd2
Mansi Kataria <mansi.kataria@bc.edu>
2017-05-30 15:31:41 +0530

    BAEL-944 Demonstrating the problems with new Url pattern matching in Spring 5

--- spring-5/src/main/java/com/baeldung/web/PathPatternController.java
+++ spring-5/src/main/java/com/baeldung/web/PathPatternController.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.web;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author mansi
- */
 @RestController
 public class PathPatternController {
 
