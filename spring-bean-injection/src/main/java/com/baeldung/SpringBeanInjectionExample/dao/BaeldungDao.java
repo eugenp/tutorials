17eb36620932c0b17367645fc6191e69985c662a
Knight.Zhou <YoungWelle@gmail.com>
2017-05-30 21:40:03 +0800

    ADDED:unit test cases with DAO,service,controller

--- spring-bean-injection/src/main/java/com/baeldung/SpringBeanInjectionExample/dao/BaeldungDao.java
+++ spring-bean-injection/src/main/java/com/baeldung/SpringBeanInjectionExample/dao/BaeldungDao.java
@@ -3,11 +3,15 @@ package com.baeldung.SpringBeanInjectionExample.dao;
 /**
  * Created by smartkit on 24/05/2017.
  */
-public class BealdungDao {
+public class BaeldungDao {
         //simple property
         private String wordpress;
         //setter
         public void setWordpress(String wordpress) {
                 this.wordpress = wordpress;
+        }
+        //getter
+        public String getWordpress() {
+                return wordpress;
         }
 }
--- spring-bean-injection/src/test/java/com/baeldung/SpringBeanInjectionExample/SpringBeanInjectionExampleApplicationTests.java
+++ spring-bean-injection/src/test/java/com/baeldung/SpringBeanInjectionExample/SpringBeanInjectionExampleApplicationTests.java
@@ -1,16 +1,61 @@
 package com.baeldung.SpringBeanInjectionExample;
 
+import com.baeldung.SpringBeanInjectionExample.controller.BaeldungController;
+import com.baeldung.SpringBeanInjectionExample.dao.BaeldungDao;
+import com.baeldung.SpringBeanInjectionExample.service.BaeldungService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(locations = {"file:src/main/resources/Beans.xml"})
 public class SpringBeanInjectionExampleApplicationTests {
+
+	@Autowired
+	private ApplicationContext context;
+	//
+	private BaeldungDao baeldungDao;
+	//
+	private BaeldungService baeldungService;
+	//
+	private BaeldungController baeldungController;
 
 	@Test
 	public void contextLoads() {
+//		context = new FileSystemXmlApplicationContext("src/main/resources/Beans.xml");
+		assertNotNull(context);
 	}
+
+	@Test
+	public  void testDao(){
+		BaeldungDao baeldungDao = (BaeldungDao)context.getBean("baeldungDao");
+		assertNotNull(baeldungDao);
+		assertNotNull(baeldungDao.getWordpress());
+		assertEquals(baeldungDao.getWordpress(),"www.baeldung.com");
+	}
+
+	@Test
+	public  void testService(){
+		baeldungService = (BaeldungService)context.getBean("baeldungService");
+		assertNotNull(baeldungService);
+	}
+
+	@Test
+	public  void testController(){
+		baeldungController = (BaeldungController)context.getBean("baeldungController");
+		assertNotNull(baeldungController);
+	}
+
+
 
 }
