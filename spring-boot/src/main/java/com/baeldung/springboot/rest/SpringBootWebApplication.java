82683eb67bc8ae204723ce7f31bfe92b35482a6c
hariprasad108 <petr.hariprasad@gmail.com>
2017-05-31 23:23:33 +0200

    Update SpringBootWebApplication.java

--- spring-boot/src/main/java/com/baeldung/springboot/rest/SpringBootWebApplication.java
+++ spring-boot/src/main/java/com/baeldung/springboot/rest/SpringBootWebApplication.java
@@ -20,7 +20,7 @@ public class SpringBootWebApplication extends SpringBootServletInitializer {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class, args);
     }
-
+    
     // http://localhost:8080/greetings
     // http://localhost:8989/DifferenceURIURLREST/greetings
 }
