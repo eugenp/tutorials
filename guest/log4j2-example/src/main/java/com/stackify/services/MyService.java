ce3bd520c81f50c1fbddac2b58f90bd603e0c1a7
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2017-06-01 14:06:49 +0300

    log4j2 examples

--- guest/log4j2-example/src/main/java/com/stackify/services/MyService.java
+++ guest/log4j2-example/src/main/java/com/stackify/services/MyService.java
@@ -1,11 +1,14 @@
 package com.stackify.services;
 
+import java.time.LocalDate;
+import java.time.Period;
+
+import com.stackify.models.User;
+
 public class MyService {
-    
-    
-    
-    public void calculate(){
-        
+
+    public int calculateUserAge(User user) {
+        return Period.between(user.getDateOfBirth(), LocalDate.now()).getYears();
     }
 
 }
