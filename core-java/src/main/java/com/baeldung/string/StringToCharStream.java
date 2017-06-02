2961a147ac8e50ecb48c8ce2a65c1c968b9d5e0b
Seun Matt <smatt382@gmail.com>
2017-06-02 09:59:36 +0100

    updated the example codes. Use assert statements

--- core-java/src/main/java/com/baeldung/string/StringToCharStream.java
+++ core-java/src/main/java/com/baeldung/string/StringToCharStream.java
@@ -1,7 +1,10 @@
 package com.baeldung.string;
 
+import org.junit.Assert;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 /**
  * Created by smatt on 26/05/2017.
@@ -10,38 +13,37 @@ public class StringToCharStream {
 
     public StringToCharStream() {
 
-        System.out.println("Before Mapping");
-
         String testString = "String";
-        //this will return IntStream Object
+
+        //this will return instance of IntStream
+        Assert.assertTrue(testString.chars() instanceof IntStream);
+
+
+        //now let's map the IntStream to Stream<Character>
+        testString.codePoints()
+                .mapToObj(c -> (char) c)
+                .forEach(c -> Assert.assertTrue(c instanceof Character));
+
+
+
+        System.out.println("Counting Occurrence of Letter");
+        testString = "Noww121";
+
+        //and we want to apply this by counting occurrence of each character in a string
+        //we don't want to use foreach, so . . .
+        Map<Character, Integer> map = new HashMap<>();
         testString.chars()
-               .forEach(System.out::println); //83, 116, 114 . . .
+                .mapToObj(c -> (char) c)
+                .filter(c -> Character.isLetter(c))
+                .forEach(c -> {
+                    if(map.containsKey(c)) {
+                        map.put(c, map.get(c) + 1);
+                    } else {
+                        map.put(c, 1);
+                    }
+                });
 
-       System.out.println();
-       System.out.println("After Mapping");
-       testString.codePoints()
-                .mapToObj(c -> (char) c) //this is where integers are converted to char type
-                .forEach(c -> System.out.println(c));
-
-
-
-       System.out.println("Counting Occurrence of Letter");
-       testString = "Noww121";
-
-       //and we want to apply this by counting occurrence of each character in a string
-       //we don't want to use foreach, so . . .
-       Map<Character, Integer> map = new HashMap<>();
-       testString.chars()
-               .mapToObj(c -> (char) c)
-               .filter(c -> Character.isLetter(c))
-               .forEach(c -> {
-                   if(map.containsKey(c)) {
-                       map.put(c, map.get(c) + 1);
-                   } else {
-                       map.put(c, 1);
-                   }
-               });
-       System.out.println(map.toString());
+        System.out.println(map.toString());
 
 
     }
