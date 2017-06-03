a5d5460a9fa422f6dbc17cd098d08e8ae8e57999
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-06-03 20:49:56 +0200

    Stream API: last Stream element example refactor (#1943)
    * Refactor
    
    * Delete ApplicationContextTestBeanInjectionTypes.java
    
    * Delete BeanInjectionJavaConfigIntegrationTest.java
    
    * Delete BeanInjectionXMLConfigIntegrationTest.java

--- core-java/src/main/java/com/baeldung/stream/StreamApi.java
+++ core-java/src/main/java/com/baeldung/stream/StreamApi.java
@@ -5,17 +5,17 @@ import java.util.stream.Stream;
 
 public class StreamApi {
 
-    public String getLastElementUsingReduce(List<String> valueList) {
+    public static String getLastElementUsingReduce(List<String> valueList) {
         Stream<String> stream = valueList.stream();
         return stream.reduce((first, second) -> second).orElse(null);
     }
     
-    public Integer getInfiniteStreamLastElementUsingReduce() {
+    public static Integer getInfiniteStreamLastElementUsingReduce() {
         Stream<Integer> stream = Stream.iterate(0, i -> i + 1);
         return stream.limit(20).reduce((first, second) -> second).orElse(null);
     }
     
-    public String getLastElementUsingSkip(List<String> valueList) {
+    public static String getLastElementUsingSkip(List<String> valueList) {
         long count = valueList.stream().count();
         Stream<String> stream = valueList.stream();
         return stream.skip(count - 1).findFirst().orElse(null);
