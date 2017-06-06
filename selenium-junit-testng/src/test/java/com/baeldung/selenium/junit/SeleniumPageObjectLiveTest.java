a6f569a0398d0585892ada9a95abf85de3c8e46d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-06-06 20:15:17 +0200

    Rename test class

--- selenium-junit-testng/src/test/java/com/baeldung/selenium/junit/SeleniumPageObjectLiveTest.java
+++ selenium-junit-testng/src/test/java/com/baeldung/selenium/junit/SeleniumPageObjectLiveTest.java
@@ -11,7 +11,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class SeleniumPageObjectTest {
+public class SeleniumPageObjectLiveTest {
 
     private SeleniumConfig config;
     private BaeldungHomePage homePage;
