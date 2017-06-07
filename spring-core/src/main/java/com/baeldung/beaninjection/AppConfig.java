ea074833dba8873ad93446fd53edd853223c7b31
patrick <patrick@kotivu.ng>
2017-06-08 03:30:04 +0900

    Add class based spring beans configuration

---
+++ spring-core/src/main/java/com/baeldung/beaninjection/AppConfig.java
@@ -0,0 +1,18 @@
+package com.baeldung.beaninjection;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public ChatMessage chatMessage() {
+        return new TextMessage();
+    }
+
+    @Bean
+    public MessageSender messageSender() {
+        return new MessageSender(chatMessage());
+    }
+}
