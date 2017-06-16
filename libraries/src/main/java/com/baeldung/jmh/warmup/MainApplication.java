fc4a8186ef429b17a4013bde37d53d5111f09ebb
abhinabkanrar <abhinabkanrar@gmail.com>
2017-06-16 12:03:32 +0530

    manual algorightm

---
+++ libraries/src/main/java/com/baeldung/jmh/warmup/MainApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.jmh.warmup;
+
+public class MainApplication {
+
+	static {
+		ManualClassLoader.load();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ libraries/src/main/java/com/baeldung/jmh/warmup/ManualClassLoader.java
@@ -0,0 +1,13 @@
+package com.baeldung.jmh.warmup;
+
+public class ManualClassLoader {
+
+	protected static void load() {
+
+		for (int i = 0; i < 1000; i++) {
+			// load all(or most) important classes
+		}
+
+	}
+
+}
