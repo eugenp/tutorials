87351d719f8df1b87492e95e8383a3b2bc5553c6
abhinabkanrar <abhinabkanrar@gmail.com>
2017-06-17 23:07:49 +0530

    with BM result

--- libraries/src/main/java/com/baeldung/jmh/warmup/MainApplication.java
+++ libraries/src/main/java/com/baeldung/jmh/warmup/MainApplication.java
@@ -2,12 +2,18 @@ package com.baeldung.jmh.warmup;
 
 public class MainApplication {
 
+	static long start;
+	static long end;
+
 	static {
 		ManualClassLoader.load();
 	}
 
 	public static void main(String[] args) {
-
+		start = System.nanoTime();
+		ManualClassLoader.load();
+		end = System.nanoTime();
+		System.out.println("Total time taken : " + (end - start));
 	}
 
 }
--- libraries/src/main/java/com/baeldung/jmh/warmup/ManualClassLoader.java
+++ libraries/src/main/java/com/baeldung/jmh/warmup/ManualClassLoader.java
@@ -1,11 +1,15 @@
 package com.baeldung.jmh.warmup;
 
+import com.baeldung.jmh.warmup.dummy.Dummy;
+
 public class ManualClassLoader {
 
 	protected static void load() {
 
-		for (int i = 0; i < 1000; i++) {
+		for (int i = 0; i < 100000; i++) {
 			// load all(or most) important classes
+			Dummy dummy = new Dummy();
+			dummy.m();
 		}
 
 	}
