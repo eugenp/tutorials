6bd430d99f5e2024456a72d59c227bbf5728f5c4
mansi.kataria <mkataria920@gmail.com>
2017-06-19 22:42:00 +0530

    BAEL-944 Replacing @RequestMapping by @GetMapping

--- spring-5/src/main/java/com/baeldung/web/PathPatternController.java
+++ spring-5/src/main/java/com/baeldung/web/PathPatternController.java
@@ -1,5 +1,6 @@
 package com.baeldung.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,32 +8,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PathPatternController {
 
-    @RequestMapping(value = "/spring5/{*id}")
+    @GetMapping(value = "/spring5/{*id}")
     public String URIVariableHandler(@PathVariable String id) {
         return id;
     }
 
-    @RequestMapping(value = "/s?ring5")
+    @GetMapping(value = "/s?ring5")
     public String wildcardTakingExactlyOneChar() {
         return "/s?ring5";
     }
 
-    @RequestMapping(value = "/spring5/*id")
+    @GetMapping(value = "/spring5/*id")
     public String wildcardTakingZeroOrMoreChar() {
         return "/spring5/*id";
     }
 
-    @RequestMapping(value = "/resources/**")
+    @GetMapping(value = "/resources/**")
     public String wildcardTakingZeroOrMorePathSegments() {
         return "/resources/**";
     }
 
-    @RequestMapping(value = "/{baeldung:[a-z]+}")
+    @GetMapping(value = "/{baeldung:[a-z]+}")
     public String regexInPathVariable(@PathVariable String baeldung) {
         return baeldung;
     }
 
-    @RequestMapping(value = "/{var1}_{var2}")
+    @GetMapping(value = "/{var1}_{var2}")
     public String multiplePathVariablesInSameSegment(@PathVariable String var1, @PathVariable String var2) {
         return "Two variables are var1=" + var1 + " and var2=" + var2;
     }
