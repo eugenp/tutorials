bfc6d480c8df0a85473b0e41f2320a10ba6a4fe6
Sunil Mogadati <sunil.mogadati@gmail.com>
2017-06-20 14:34:58 -0600

    BAEL-802: Drools using rules from Excel Files (#2115)
    * Add NDC and JBoss Logging to the demo application
    
    * NDC for Log4j, Log4j2 and JBoss Logging
    
    * Simplify NDC example by making it a single operation instead of two
    
    * Make NDC example as RestController, Use JBoss Logging only as a logging bridge
    
    * Fix merge conflicts in pull request - log-mdc pom.xml updated
    
    * BAEL-445 Update to Spring security SpEL example
    
    * BAEL-445: Change tabs to spaces in the updated code
    
    * BAEL-245: Add Enum Serialization exmaple
    
    * BAEL-245: Remove the folder jackson/src/test/java/com/baeldung/jackson/dtos/withEnum as the example is not used anymore
    
    * Add more enum serialization examples to align with previous example and prevent build fail
    
    * BAEL-611: Minor formatting changes
    
    * BAEL-611: Update Test case method names
    
    * BAEL-611 Add JAX-WS client and JUnit Test
    
    * BAEL-245: Issue 1753. Fix the typo - change from writeNumber() to writeString()
    
    * BAEL-741: Spring Security Multiple Authentication Providers
    
    * BAEL-741: Spring Security Multiple Authentication Providers
    
    * Remove unnecessary change in pom.xml
    
    * BAEL-802: Drools Excel example

--- drools/src/main/java/com/baeldung/drools/config/DroolsBeanFactory.java
+++ drools/src/main/java/com/baeldung/drools/config/DroolsBeanFactory.java
@@ -1,10 +1,15 @@
 package com.baeldung.drools.config;
 
+import org.drools.decisiontable.DecisionTableProviderImpl;
 import org.kie.api.KieServices;
 import org.kie.api.builder.*;
 import org.kie.api.io.KieResources;
+import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.internal.builder.DecisionTableConfiguration;
+import org.kie.internal.builder.DecisionTableInputType;
+import org.kie.internal.builder.KnowledgeBuilderFactory;
 import org.kie.internal.io.ResourceFactory;
 import java.io.IOException;
 import java.util.Arrays;
@@ -62,6 +67,41 @@ public class DroolsBeanFactory {
 
         return kContainer.newKieSession();
 
+    }
+
+    public KieSession getKieSession(Resource dt) {
+        KieFileSystem kieFileSystem = kieServices.newKieFileSystem()
+            .write(dt);
+
+        KieBuilder kieBuilder = kieServices.newKieBuilder(kieFileSystem)
+            .buildAll();
+
+        KieRepository kieRepository = kieServices.getRepository();
+
+        ReleaseId krDefaultReleaseId = kieRepository.getDefaultReleaseId();
+
+        KieContainer kieContainer = kieServices.newKieContainer(krDefaultReleaseId);
+
+        KieSession ksession = kieContainer.newKieSession();
+
+        return ksession;
+    }
+
+    /*
+     * Can be used for debugging
+     * Input excelFile example: com/baeldung/drools/rules/Discount.xls
+     */
+    public String getDrlFromExcel(String excelFile) {
+        DecisionTableConfiguration configuration = KnowledgeBuilderFactory.newDecisionTableConfiguration();
+        configuration.setInputType(DecisionTableInputType.XLS);
+
+        Resource dt = ResourceFactory.newClassPathResource(excelFile, getClass());
+
+        DecisionTableProviderImpl decisionTableProvider = new DecisionTableProviderImpl();
+
+        String drl = decisionTableProvider.loadFromResource(dt, null);
+
+        return drl;
     }
 
 }
