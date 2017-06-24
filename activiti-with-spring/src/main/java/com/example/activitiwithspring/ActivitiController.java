598d98acf1b98c5f92cb1c7b9951e89d5f6a05a2
mansi2392 <mkataria920@gmail.com>
2017-06-24 16:31:18 +0530

    BAEL-79 Replace RequestMapping with GetMapping

--- activiti-with-spring/src/main/java/com/example/activitiwithspring/ActivitiController.java
+++ activiti-with-spring/src/main/java/com/example/activitiwithspring/ActivitiController.java
@@ -9,6 +9,7 @@ import org.activiti.engine.task.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,14 +23,14 @@ public class ActivitiController {
     @Autowired
     private TaskService taskService;
 
-    @RequestMapping(value = "/start-process")
+    @GetMapping("/start-process")
     public String startProcess() {
         runtimeService.startProcessInstanceByKey("my-process");
         return "Process started. Number of currently running process instances = " + runtimeService.createProcessInstanceQuery()
             .count();
     }
 
-    @RequestMapping(value = "/get-tasks/{processInstanceId}")
+    @GetMapping("/get-tasks/{processInstanceId}")
     public List<TaskRepresentation> getTasks(@PathVariable String processInstanceId) {
         List<Task> usertasks = taskService.createTaskQuery()
             .processInstanceId(processInstanceId)
@@ -43,7 +44,7 @@ public class ActivitiController {
         return tasks;
     }
 
-    @RequestMapping(value = "/complete-task-A/{processInstanceId}")
+    @GetMapping("/complete-task-A/{processInstanceId}")
     public TaskRepresentation completeTaskA(@PathVariable String processInstanceId) {
         Task task = taskService.createTaskQuery()
             .processInstanceId(processInstanceId)
