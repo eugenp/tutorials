56e72b8539e6398091f01bf066cec0e8d5487a25
Syed Ali Raza <ali_raza995@hotmail.com>
2017-06-30 01:26:37 +0500

    BAEL-968: Updated code based on reviewer comments

--- libraries/src/test/java/com/baeldung/commons/beanutils/CourseServiceTest.java
+++ libraries/src/test/java/com/baeldung/commons/beanutils/CourseServiceTest.java
@@ -10,7 +10,8 @@ import org.junit.Test;
 public class CourseServiceTest {
 
     @Test
-    public void givenCourse_whenSetValuesUsingPropertyUtil_thenReturnSetValues() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    public void givenCourse_whenSetValuesUsingPropertyUtil_thenReturnSetValues() 
+        throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         Course course = new Course();
         String name = "Computer Science";
         List<String> codes = Arrays.asList("CS", "CS01");
@@ -35,7 +36,8 @@ public class CourseServiceTest {
     }
 
     @Test
-    public void givenCopyProperties_whenCopyCourseToCourseEntity_thenCopyPropertyWithSameName() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    public void givenCopyProperties_whenCopyCourseToCourseEntity_thenCopyPropertyWithSameName()
+        throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         Course course = new Course();
         course.setName("Computer Science");
         course.setCodes(Arrays.asList("CS"));
@@ -46,5 +48,6 @@ public class CourseServiceTest {
         CourseService.copyProperties(course, courseEntity);
         Assert.assertEquals(course.getName(), courseEntity.getName());
         Assert.assertEquals(course.getCodes(), courseEntity.getCodes());
+        Assert.assertNull(courseEntity.getStudent("ST-1"));
     }
 }
