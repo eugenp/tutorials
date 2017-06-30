342c356b89434dc1779aec4f787c7cbc742cc2b4
slavisa-baeldung <slavisa.baeldung@gmail.com>
2017-06-30 15:29:37 +0100

    BAEL-835 - code rerfactor

--- core-java/src/main/java/com/baeldung/outofmemoryerror/java-outofmemoryerror-demo/src/main/java/com/baeldung/outofmemoryerror/java_outofmemoryerror_demo/Test.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.outofmemoryerror.java_outofmemoryerror_demo;
-
-public class Test {
-    public static void main( String[] args )
-    {
-        OutOfMemoryGCLimitExceed.addRandomDataToMap();
-    }
-}
