9f9024c53962ef3cdb5f43dc3ee6311b2f60f7e7
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-07-01 18:47:19 +0200

    Integration test (#2187)

---
+++ spring-drools/src/test/java/com/baeldung/spring/drools/service/TaxiFareCalculatorServiceIntegrationTest.java
@@ -0,0 +1,94 @@
+package com.baeldung.spring.drools.service;
+
+import com.baeldung.spring.drools.model.Fare;
+import com.baeldung.spring.drools.model.TaxiRide;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TaxiFareConfiguration.class)
+public class TaxiFareCalculatorServiceIntegrationTest {
+
+    @Autowired
+    private TaxiFareCalculatorService taxiFareCalculatorService;
+
+    @Test
+    public void testCalculateFareScenario1() {
+        TaxiRide taxiRide = new TaxiRide();
+        taxiRide.setbNightSurcharge(false);
+        taxiRide.setDistanceInMile(9L);
+        Fare rideFare = new Fare();
+        Long totalCharge = taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+
+        assertNotNull(totalCharge);
+        assertEquals(Long.valueOf(70), totalCharge);
+    }
+
+    @Test
+    public void testCalculateFareScenario2() {
+        TaxiRide taxiRide = new TaxiRide();
+        taxiRide.setbNightSurcharge(true);
+        taxiRide.setDistanceInMile(5L);
+        Fare rideFare = new Fare();
+        Long totalCharge = taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+
+        assertNotNull(totalCharge);
+        assertEquals(Long.valueOf(100), totalCharge);
+    }
+
+    @Test
+    public void testCalculateFareScenario3() {
+        TaxiRide taxiRide = new TaxiRide();
+        taxiRide.setbNightSurcharge(false);
+        taxiRide.setDistanceInMile(50L);
+        Fare rideFare = new Fare();
+        Long totalCharge = taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+
+        assertNotNull(totalCharge);
+        assertEquals(Long.valueOf(170), totalCharge);
+    }
+
+    @Test
+    public void testCalculateFareScenario4() {
+        TaxiRide taxiRide = new TaxiRide();
+        taxiRide.setbNightSurcharge(true);
+        taxiRide.setDistanceInMile(50L);
+        Fare rideFare = new Fare();
+        Long totalCharge = taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+
+        assertNotNull(totalCharge);
+        assertEquals(Long.valueOf(250), totalCharge);
+    }
+
+    @Test
+    public void testCalculateFareScenario5() {
+        TaxiRide taxiRide = new TaxiRide();
+        taxiRide.setbNightSurcharge(false);
+        taxiRide.setDistanceInMile(100L);
+        Fare rideFare = new Fare();
+        Long totalCharge = taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+
+        assertNotNull(totalCharge);
+        assertEquals(Long.valueOf(220), totalCharge);
+    }
+
+    @Test
+    public void testCalculateFareScenario6() {
+        TaxiRide taxiRide = new TaxiRide();
+        taxiRide.setbNightSurcharge(true);
+        taxiRide.setDistanceInMile(100L);
+        Fare rideFare = new Fare();
+        Long totalCharge = taxiFareCalculatorService.calculateFare(taxiRide, rideFare);
+
+        assertNotNull(totalCharge);
+        assertEquals(Long.valueOf(350), totalCharge);
+    }
+
+}
