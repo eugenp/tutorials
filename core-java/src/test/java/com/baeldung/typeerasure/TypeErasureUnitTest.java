2ea0064880416da64acaaef206722729d09488b4
patrick <patrick@kotivu.ng>
2017-07-01 09:49:42 +0900

    Add expected exception in TypeErasureUnitTest

--- core-java/src/test/java/com/baeldung/typeerasure/TypeErasureUnitTest.java
+++ core-java/src/test/java/com/baeldung/typeerasure/TypeErasureUnitTest.java
@@ -3,15 +3,15 @@ package com.baeldung.typeerasure;
 import org.junit.Test;
 
 public class TypeErasureUnitTest {
-    
-    @Test
+
+    @Test(expected = ClassCastException.class)
     public void givenIntegerStack_whenStringPushedAndAssignPoppedValueToInteger_shouldFail() {
         IntegerStack integerStack = new IntegerStack(5);
         Stack stack = integerStack;
         stack.push("Hello");
         Integer data = integerStack.pop();
     }
-   
+
     @Test
     public void givenAnyArray_whenInvokedPrintArray_shouldSucceed() {
         Integer[] scores = new Integer[] { 100, 200, 10, 99, 20 };
