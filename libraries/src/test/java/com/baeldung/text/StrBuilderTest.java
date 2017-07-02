7d87b3aeb7ac35b2721f0e02b10a127b02d16e50
Ahmed Tawila <atawila@yahoo.com>
2017-07-02 19:44:44 +0200

    BAEL-972 - Apache Commons Text - just indentation

--- libraries/src/test/java/com/baeldung/text/StrBuilderTest.java
+++ libraries/src/test/java/com/baeldung/text/StrBuilderTest.java
@@ -5,20 +5,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class StrBuilderTest {
-    
+
     @Test
     public void whenReplaced_thenCorrect() {
         StrBuilder strBuilder = new StrBuilder("example StrBuilder!");
         strBuilder.replaceAll("example", "new");
-        
+
         Assert.assertEquals(new StrBuilder("new StrBuilder!"), strBuilder);
     }
-    
+
     @Test
     public void whenCleared_thenEmpty() {
         StrBuilder strBuilder = new StrBuilder("example StrBuilder!");
         strBuilder.clear();
-        
+
         Assert.assertEquals(new StrBuilder(""), strBuilder);
     }
 }
