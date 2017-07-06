bf7829d539acbfe3a7d580e1db617e3c56fb2922
iaforek <arkadiusz@jaworski.pl>
2017-07-06 12:19:26 +0100

    Revert "BAEL-579 Added actuator to Cloud Config Client."
    This reverts commit 076657a26a57e0aa676989a4d97966a3b9d53e1c.

--- spring-cloud/spring-cloud-config/client/src/main/java/com/baeldung/spring/cloud/config/client/ConfigClient.java
+++ spring-cloud/spring-cloud-config/client/src/main/java/com/baeldung/spring/cloud/config/client/ConfigClient.java
@@ -3,7 +3,6 @@ package com.baeldung.spring.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RefreshScope
 public class ConfigClient {
     @Value("${user.role}")
     private String role;
