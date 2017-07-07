a6ba135f7784450aff4e72043db4cd6d0539af30
iaforek <arkadiusz@jaworski.pl>
2017-07-07 18:01:53 +0100

    BAEL-579 Spring Cloud Bus (#2218)
    * Code for Dependency Injection Article.
    
    * Added Java based configuration. Downloaded formatter.xml and reformatted
    all changed files. Manually changed tab into 4 spaces in XML
    configuration files.
    
    * BAEL-434 - Spring Roo project files generated by Spring Roo. No
    formatting applied. Added POM, java and resources folders.
    
    * Moved project from roo to spring-roo folder.
    
    * BAEL-838 Initial code showing how to remove last char - helper class and tests.
    
    * BAEL-838 Corrected Helper class and associated empty string test case. Added StringUtils.substing tests.
    
    * BAEL-838 Refromatted code using formatter.xml. Added Assert.assertEquals import. Renamed test to follow convention. Reordered tests.
    
    * BAEL-838 - Added regex method and updated tests.
    
    * BAEL-838 Added new line examples.
    
    * BAEL-838 Renamed RemoveLastChar class to StringHelper and added Java8 examples. Refactord code.
    
    * BAEL-838 Changed method names
    
    * BAEL-838 Tiny change to keep code consistant. Return null or empty.
    
    * BAEL-838 Removed unresolved conflict.
    
    * BAEL-821 New class that shows different rounding techniques. Updated POM.
    
    * BAEL-821 - Added unit test for different round methods.
    
    * BAEL-821 Changed test method name to follow the convention
    
    * BAEL-821 Added more test and updated round methods.
    
    * BAEL-837 - initial commit. A few examples of adding doubles.
    
    * BAEL-837 - Couple of smaller changes
    
    * BAEL-837 - Added jUnit test.
    
    * BAEL-579 Updated Spring Cloud Version
    
    I was getting error: java.lang.NoSuchMethodError:
    org.springframework.cloud.config.environment.Environment
    After version update, all is okay.
    
    * BAEL-579 Added actuator to Cloud Config Client.
    
    * BAEL-579 Enabled cloud bus and updated dependencies.
    
    * BAEL-579 Config Client using Spring Cloud Bus.
    
    * BAEL-579 Recreated Basic Config Server.
    
    * BAEL-579 Recreated Config Client.
    
    * BAEL-579 Removed test Git URL.
    
    * BAEL-579 Added Actuator to Config Client
    
    * BAEL-579 Added Spring Cloud Bus to Client.
    
    * BAEL-579 Server changes for Spring Cloud Bus
    
    Added dependencies and removed git.clone-on-start as this was causing
    server to throw errors after git properties change.
    
    * BAEL-579 Removed Git URL.
    
    * Revert "BAEL-579 Updated Spring Cloud Version"
    
    This reverts commit f775bf91e53a1ecfb9b70596688d7c8202bf495f.
    
    * Revert "BAEL-579 Config Client using Spring Cloud Bus."
    
    This reverts commit 1d96bc5761994a33af9a7a9aa5ab68604a5b44dc.
    
    * Revert "BAEL-579 Enabled cloud bus and updated dependencies."
    
    This reverts commit 7845da922d89d53506dd0fff387ea13694c50bc1.
    
    * Revert "BAEL-579 Added actuator to Cloud Config Client."
    
    This reverts commit 076657a26a57e0aa676989a4d97966a3b9d53e1c.
    
    * BAEL-579 Added missing dependency versions.
    
    * BAEL-579 Added missing dependency versions.

--- core-java/src/test/java/com/baeldung/maths/RoundTest.java
+++ core-java/src/test/java/com/baeldung/maths/RoundTest.java
@@ -15,60 +15,60 @@ public class RoundTest {
     private double expected = 2.03d;
 
     @Test
-    public void givenDecimalNumber_whenRoundToNDecimalPlaces_thenGetExpectedResult() {
-        assertEquals(expected, Round.round(value, places), delta);
-        assertEquals(expected, Round.roundNotPrecise(value, places), delta);
-        assertEquals(expected, Round.roundAvoid(value, places), delta);
-        assertEquals(expected, Precision.round(value, places), delta);
-        assertEquals(expected, DoubleRounder.round(value, places), delta);
-
+    public void givenDecimalNumber_whenRoundToNDecimalPlaces_thenGetExpectedResult() {   
+        Assert.assertEquals(expected, Round.round(value, places), delta);
+        Assert.assertEquals(expected, Round.roundNotPrecise(value, places), delta);
+        Assert.assertEquals(expected, Round.roundAvoid(value, places), delta);
+        Assert.assertEquals(expected, Precision.round(value, places), delta);
+        Assert.assertEquals(expected, DoubleRounder.round(value, places), delta);
+        
         places = 3;
         expected = 2.035d;
-
-        assertEquals(expected, Round.round(value, places), delta);
-        assertEquals(expected, Round.roundNotPrecise(value, places), delta);
-        assertEquals(expected, Round.roundAvoid(value, places), delta);
-        assertEquals(expected, Precision.round(value, places), delta);
-        assertEquals(expected, DoubleRounder.round(value, places), delta);
-
+        
+        Assert.assertEquals(expected, Round.round(value, places), delta);
+        Assert.assertEquals(expected, Round.roundNotPrecise(value, places), delta);
+        Assert.assertEquals(expected, Round.roundAvoid(value, places), delta);
+        Assert.assertEquals(expected, Precision.round(value, places), delta);
+        Assert.assertEquals(expected, DoubleRounder.round(value, places), delta);
+        
         value = 1000.0d;
         places = 17;
         expected = 1000.0d;
-
-        assertEquals(expected, Round.round(value, places), delta);
-        assertEquals(expected, Round.roundNotPrecise(value, places), delta);
-        assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 92.23372036854776 !
-        assertEquals(expected, Precision.round(value, places), delta);
-        assertEquals(expected, DoubleRounder.round(value, places), delta);
-
+        
+        Assert.assertEquals(expected, Round.round(value, places), delta);
+        Assert.assertEquals(expected, Round.roundNotPrecise(value, places), delta);
+        Assert.assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 92.23372036854776 !
+        Assert.assertEquals(expected, Precision.round(value, places), delta);
+        Assert.assertEquals(expected, DoubleRounder.round(value, places), delta);
+        
         value = 256.025d;
         places = 2;
         expected = 256.03d;
-
-        assertEquals(expected, Round.round(value, places), delta);
-        assertNotEquals(expected, Round.roundNotPrecise(value, places), delta); // Returns: 256.02 !
-        assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 256.02 !
-        assertEquals(expected, Precision.round(value, places), delta);
-        assertNotEquals(expected, DoubleRounder.round(value, places), delta); // Returns: 256.02 !
-
-        value = 260.775d;
+        
+        Assert.assertEquals(expected, Round.round(value, places), delta);
+        Assert.assertNotEquals(expected, Round.roundNotPrecise(value, places), delta); // Returns: 256.02 !
+        Assert.assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 256.02 !
+        Assert.assertEquals(expected, Precision.round(value, places), delta);
+        Assert.assertNotEquals(expected, DoubleRounder.round(value, places), delta); // Returns: 256.02 !
+        
+        value = 260.775d; 
         places = 2;
         expected = 260.78d;
-
-        assertEquals(expected, Round.round(value, places), delta);
-        assertNotEquals(expected, Round.roundNotPrecise(value, places), delta); // Returns: 260.77 !
-        assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 260.77 !
-        assertEquals(expected, Precision.round(value, places), delta);
-        assertNotEquals(expected, DoubleRounder.round(value, places), delta); // Returns: 260.77 !
-
+        
+        Assert.assertEquals(expected, Round.round(value, places), delta);
+        Assert.assertNotEquals(expected, Round.roundNotPrecise(value, places), delta); // Returns: 260.77 !
+        Assert.assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 260.77 !
+        Assert.assertEquals(expected, Precision.round(value, places), delta);
+        Assert.assertNotEquals(expected, DoubleRounder.round(value, places), delta); // Returns: 260.77 !
+        
         value = 90080070060.1d;
         places = 9;
         expected = 90080070060.1d;
-
-        assertEquals(expected, Round.round(value, places), delta);
-        assertEquals(expected, Round.roundNotPrecise(value, places), delta);
-        assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 9.223372036854776E9 !
-        assertEquals(expected, Precision.round(value, places), delta);
-        assertEquals(expected, DoubleRounder.round(value, places), delta);
+        
+        Assert.assertEquals(expected, Round.round(value, places), delta);
+        Assert.assertEquals(expected, Round.roundNotPrecise(value, places), delta);
+        Assert.assertNotEquals(expected, Round.roundAvoid(value, places), delta); // Returns: 9.223372036854776E9 !
+        Assert.assertEquals(expected, Precision.round(value, places), delta);
+        Assert.assertEquals(expected, DoubleRounder.round(value, places), delta);
     }
 }
