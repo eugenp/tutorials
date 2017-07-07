7bfceb34c77038959c86d35637785330956ec812
hariprasad108 <petr.hariprasad@gmail.com>
2017-07-07 12:27:45 +0200

    URI, URL  BAEL-864. Article renamed and extended URI part. (#2217)

--- core-java/src/main/java/com/baeldung/javanetworking/uriurl/URLDemo.java
+++ core-java/src/main/java/com/baeldung/javanetworking/uriurl/URLDemo.java
@@ -1,9 +1,10 @@
-package com.baeldung.javanetworking.url;
+package com.baeldung.javanetworking.uriurl;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLConnection;
 
