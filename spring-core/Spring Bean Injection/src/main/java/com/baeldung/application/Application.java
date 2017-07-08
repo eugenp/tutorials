6e3521e0c1f9c3abb99ba1ee0f7bb8e21ce29f57
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2017-07-08 19:06:30 -0300

    Refactored return type in bean factory methods

--- spring-core/Spring Bean Injection/src/main/java/com/baeldung/application/Application.java
+++ spring-core/Spring Bean Injection/src/main/java/com/baeldung/application/Application.java
@@ -2,31 +2,41 @@ package com.baeldung.application;
 
 import com.baeldung.services.LowercaseTextService;
 import com.baeldung.services.TextPrinter;
+import com.baeldung.services.TextService;
 import com.baeldung.services.UppercaseTextService;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+/*
 @Configuration
 @ComponentScan
+*/
 public class Application {
 
     public static void main(String[] args) {
 
+        /*
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         TextPrinter textPrinter = context.getBean(TextPrinter.class);
         textPrinter.printText("Hello from Spring");
+        */
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        BeanFactory factory = context;
+        TextPrinter textPrinter = (TextPrinter) factory.getBean("TextPrinter");
+        textPrinter.printText("Hello from Spring");
     }
 
-    @Bean
-    public LowercaseTextService lowercaseTextService() {
+    /*
+    @Bean(name = "lowercaseTextService")
+    public TextService lowercaseTextService() {
         return new LowercaseTextService();
     }
 
-    @Bean
-    public UppercaseTextService uppercaseTextService() {
+    @Bean(name = "uppercaseTextService")
+    public TextService uppercaseTextService() {
         return new UppercaseTextService();
     }
 
@@ -34,4 +44,5 @@ public class Application {
     public TextPrinter textPrinter() {
         return new TextPrinter(lowercaseTextService());
     }
+    */
 }
