6d385003eb23afaf631dbc61de85971fea21aaa0
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2017-07-08 19:11:33 -0300

    Refactored Application class

--- spring-core/Spring Bean Injection/src/main/java/com/baeldung/application/Application.java
+++ spring-core/Spring Bean Injection/src/main/java/com/baeldung/application/Application.java
@@ -4,38 +4,29 @@ import com.baeldung.services.LowercaseTextService;
 import com.baeldung.services.TextPrinter;
 import com.baeldung.services.TextService;
 import com.baeldung.services.UppercaseTextService;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-/*
+
 @Configuration
 @ComponentScan
-*/
 public class Application {
 
     public static void main(String[] args) {
 
-        /*
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
         TextPrinter textPrinter = context.getBean(TextPrinter.class);
         textPrinter.printText("Hello from Spring");
-        */
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        BeanFactory factory = context;
-        TextPrinter textPrinter = (TextPrinter) factory.getBean("TextPrinter");
-        textPrinter.printText("Hello from Spring");
     }
 
-    /*
-    @Bean(name = "lowercaseTextService")
+    @Bean
     public TextService lowercaseTextService() {
         return new LowercaseTextService();
     }
 
-    @Bean(name = "uppercaseTextService")
+    @Bean
     public TextService uppercaseTextService() {
         return new UppercaseTextService();
     }
@@ -44,5 +35,4 @@ public class Application {
     public TextPrinter textPrinter() {
         return new TextPrinter(lowercaseTextService());
     }
-    */
 }
