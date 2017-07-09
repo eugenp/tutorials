e63a64b394d69db1f07c97bea27ce7d1956ec5a5
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-07-09 09:09:54 +0200

    Either refactor

--- vavr/src/test/java/com/baeldung/vavr/either/EitherUnitTest.java
+++ vavr/src/test/java/com/baeldung/vavr/either/EitherUnitTest.java
@@ -1,8 +1,7 @@
 package com.baeldung.vavr.either;
 
-import org.junit.Test;
-
 import io.vavr.control.Either;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -11,21 +10,28 @@ public class EitherUnitTest {
 	@Test
 	public void givenMarks_whenPassNumber_thenExpectNumber() {
 		Either<String, Integer> result = EitherDemo.computeWithEither(100);
-		int marks = EitherDemo.getMarks(result);
+		int marks = result.right()
+		  .getOrElseThrow(x -> new IllegalStateException());
+
 		assertEquals(100, marks);
 	}
 
 	@Test
 	public void givenMarks_whenFailNumber_thenExpectErrorMesssage() {
 		Either<String, Integer> result = EitherDemo.computeWithEither(50);
-		String error = EitherDemo.getError(result);
+		String error = result.left()
+          .getOrNull();
+
 		assertEquals("Marks not acceptable", error);
 	}
 
 	@Test
 	public void givenPassMarks_whenModified_thenExpectNumber() {
 		Either<String, Integer> result = EitherDemo.computeWithEither(90);
-		int marks = EitherDemo.getModifiedMarks(result);
+		int marks = result.right()
+          .map(x -> x * 2)
+          .get();
+
 		assertEquals(180, marks);
 	}
 
