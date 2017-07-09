a219184b4670d45ded0d6f94c8cdbadb356d072e
Parth Karia <parthkaria.92@gmail.com>
2017-07-09 15:53:21 +0530

    BAEL-1004 changes in variable names (#2238)

--- algorithms/src/main/java/com/baeldung/algorithms/minimax/MiniMax.java
+++ algorithms/src/main/java/com/baeldung/algorithms/minimax/MiniMax.java
@@ -18,12 +18,12 @@ public class MiniMax {
         constructTree(root);
     }
 
-    private void constructTree(Node node) {
-        List<Integer> listofPossibleHeaps = GameOfBones.getPossibleStates(node.getNoOfBones());
-        boolean isMaxPlayer = !node.isMaxPlayer();
+    private void constructTree(Node parentNode) {
+        List<Integer> listofPossibleHeaps = GameOfBones.getPossibleStates(parentNode.getNoOfBones());
+        boolean isChildMaxPlayer = !parentNode.isMaxPlayer();
         listofPossibleHeaps.forEach(n -> {
-            Node newNode = new Node(n, isMaxPlayer);
-            node.addChild(newNode);
+            Node newNode = new Node(n, isChildMaxPlayer);
+            parentNode.addChild(newNode);
             if (newNode.getNoOfBones() > 0) {
                 constructTree(newNode);
             }
