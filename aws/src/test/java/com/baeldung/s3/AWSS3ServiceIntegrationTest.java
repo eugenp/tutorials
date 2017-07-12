18aef230a15718a130c1ebc1f2d371a5f6403727
ramansahasi <ramansahasi@gmail.com>
2017-07-12 16:30:58 +0530

    Added more test cases

--- aws/src/test/java/com/baeldung/s3/AWSS3ServiceIntegrationTest.java
+++ aws/src/test/java/com/baeldung/s3/AWSS3ServiceIntegrationTest.java
@@ -1,9 +1,11 @@
 package com.baeldung.s3;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.*; 
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 
@@ -11,12 +13,17 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CopyObjectResult;
+import com.amazonaws.services.s3.model.DeleteObjectsRequest;
+import com.amazonaws.services.s3.model.DeleteObjectsResult;
 import com.amazonaws.services.s3.model.PutObjectResult; 
 
 public class AWSS3ServiceIntegrationTest {
 
     private static final String BUCKET_NAME = "bucket_name"; 
     private static final String KEY_NAME = "key_name"; 
+    private static final String BUCKET_NAME2 = "bucket_name2"; 
+    private static final String KEY_NAME2 = "key_name2"; 
  
     private AmazonS3 s3; 
     private AWSS3Service service; 
@@ -49,6 +56,12 @@ public class AWSS3ServiceIntegrationTest {
         service.listBuckets(); 
         verify(s3).listBuckets(); 
     } 
+
+    @Test 
+    public void whenDeletingBucket_thenCorrect() { 
+        service.deleteBucket(BUCKET_NAME); 
+        verify(s3).deleteBucket(BUCKET_NAME); 
+    } 
     
     @Test 
     public void whenVerifyingPutObject_thenCorrect() { 
@@ -67,8 +80,34 @@ public class AWSS3ServiceIntegrationTest {
     } 
     
     @Test 
-    public void whenVerifyingDeleteObjects_thenCorrect() { 
-        service.deleteBucket(BUCKET_NAME); 
-        verify(s3).deleteBucket(BUCKET_NAME); 
+    public void whenVerifyingGetObject_thenCorrect() { 
+        service.getObject(BUCKET_NAME, KEY_NAME); 
+        verify(s3).getObject(BUCKET_NAME, KEY_NAME); 
     } 
+    
+    @Test 
+    public void whenVerifyingCopyObject_thenCorrect() { 
+        CopyObjectResult result = mock(CopyObjectResult.class); 
+        when(s3.copyObject(anyString(), anyString(), anyString(), anyString())).thenReturn(result); 
+ 
+        assertThat(service.copyObject(BUCKET_NAME, KEY_NAME, BUCKET_NAME2, KEY_NAME2)).isEqualTo(result); 
+        verify(s3).copyObject(BUCKET_NAME, KEY_NAME, BUCKET_NAME2, KEY_NAME2);
+    } 
+    
+    @Test 
+    public void whenVerifyingDeleteObject_thenCorrect() { 
+        service.deleteObject(BUCKET_NAME, KEY_NAME); 
+        verify(s3).deleteObject(BUCKET_NAME, KEY_NAME); 
+    }
+    
+    @Test 
+    public void whenVerifyingDeleteObjects_thenCorrect() { 
+        DeleteObjectsRequest request = mock(DeleteObjectsRequest.class);
+        DeleteObjectsResult result = mock(DeleteObjectsResult.class);
+        when(s3.deleteObjects((DeleteObjectsRequest)any())).thenReturn(result); 
+        
+        assertThat(service.deleteObjects(request)).isEqualTo(result);
+        verify(s3).deleteObjects(request); 
+    }
+    
 }
