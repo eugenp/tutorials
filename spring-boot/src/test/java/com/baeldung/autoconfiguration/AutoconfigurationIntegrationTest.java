4281969f9557b7f07a7432f90177f7c2d4ae64bc
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-07-21 13:06:04 +0300

    Optimize and refactor

--- spring-boot/src/test/java/com/baeldung/autoconfiguration/AutoconfigurationIntegrationTest.java
+++ spring-boot/src/test/java/com/baeldung/autoconfiguration/AutoconfigurationIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.baeldung.autoconfiguration;
 
+import com.baeldung.autoconfiguration.example.AutoconfigurationApplication;
+import com.baeldung.autoconfiguration.example.MyUser;
+import com.baeldung.autoconfiguration.example.MyUserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,13 +10,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.baeldung.autoconfiguration.example.AutoconfigurationApplication;
-import com.baeldung.autoconfiguration.example.MyUser;
-import com.baeldung.autoconfiguration.example.MyUserRepository;
-
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = AutoconfigurationApplication.class)
-@EnableJpaRepositories(basePackages = { "com.baeldung.autoconfiguration.example" })
+@EnableJpaRepositories(basePackages = {"com.baeldung.autoconfiguration.example"})
 public class AutoconfigurationIntegrationTest {
 
     @Autowired
