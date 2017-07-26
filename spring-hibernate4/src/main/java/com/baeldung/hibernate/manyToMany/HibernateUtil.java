3a6a9cbaf981fc6ebb6232e69e1a88deba7c5710
buddhini81 <buddhini81@users.noreply.github.com>
2017-07-26 12:47:46 +0530

    Make fixes in HibernateUtil - BAEL-698

--- spring-hibernate4/src/main/java/com/baeldung/hibernate/manyToMany/HibernateUtil.java
+++ spring-hibernate4/src/main/java/com/baeldung/hibernate/manyToMany/HibernateUtil.java
@@ -1,36 +1,43 @@
 package com.baeldung.hibernate.manyToMany;
 
+import org.hibernate.SessionFactory; 
+mport org.hibernate.boot.registry.StandardServiceRegistryBuilder; 
+import org.hibernate.cfg.Configuration; 
+import org.hibernate.service.ServiceRegistry; 
+
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class HibernateUtil {
-    
     private static SessionFactory sessionFactory;
-    
+
     private static SessionFactory buildSessionFactory() {
         try {
-            StandardServiceRegistry standardServiceRegistry = new StandardServiceRegistryBuilder().configure("hibernate-annotation.cfg.xml").build();
-            Metadata metadata = new MetadataSources(standardServiceRegistry).getMetadataBuilder().build();
-            sessionFactory = metadata.getSessionFactoryBuilder().build();
+            Configuration configuration = new Configuration();
+            configuration.configure("hibernate-annotation.cfg.xml");
+            System.out.println("Hibernate Annotation Configuration loaded");
 
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+            System.out.println("Hibernate Annotation serviceRegistry created");
 
-           return sessionFactory; 
+            SessionFactory sessionFactory = configuration
+                    .buildSessionFactory(serviceRegistry);
+
+            return sessionFactory;
         } catch (Throwable ex) {
-            System.err.println("SessionFactory creation failed." + ex);
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            ex.printStackTrace();
             throw new ExceptionInInitializerError(ex);
         }
     }
- 
+
     public static SessionFactory getSessionFactory() {
-        if(sessionFactory == null) {
-            sessionFactory = buildSessionFactory(); 
-        }
-        return sessionFactory; 
+        if (sessionFactory == null)
+            sessionFactory = buildSessionFactory();
+        return sessionFactory;
 
     }
-
-
 }
