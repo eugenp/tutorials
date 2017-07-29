5488e9f6740fa8dffefc89ef97283cb41faea3e8
abhinabkanrar <abhinabkanrar@gmail.com>
2017-07-29 19:06:15 +0530

    jooby project

--- core-java/src/main/java/com/baeldung/concurrent/cyclicbarrier/CyclicBarrierExample.java
+++ core-java/src/main/java/com/baeldung/concurrent/cyclicbarrier/CyclicBarrierExample.java
@@ -4,22 +4,21 @@ import java.util.concurrent.CyclicBarrier;
 
 public class CyclicBarrierExample {
 
-	public void start() {
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(3, () -> {
-			// Task
-			System.out.println("All previous tasks are completed");
-		});
+    public void start() {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3, () -> {
+            // Task
+            System.out.println("All previous tasks are completed");
+        });
 
-		Thread t1 = new Thread(new Task(cyclicBarrier), "T1");
-		Thread t2 = new Thread(new Task(cyclicBarrier), "T2");
-		Thread t3 = new Thread(new Task(cyclicBarrier), "T3");
+        Thread t1 = new Thread(new Task(cyclicBarrier), "T1");
+        Thread t2 = new Thread(new Task(cyclicBarrier), "T2");
+        Thread t3 = new Thread(new Task(cyclicBarrier), "T3");
 
-		if (!cyclicBarrier.isBroken()) {
-			t1.start();
-			t2.start();
-			t3.start();
-		}
-
-	}
-
+        if (!cyclicBarrier.isBroken()) {
+            t1.start();
+            t2.start();
+            t3.start();
+        }
+    }
 }
+
