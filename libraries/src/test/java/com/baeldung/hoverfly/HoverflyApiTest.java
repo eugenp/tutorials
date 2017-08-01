a923f47719f633b5b55001ffd2bfb6618ab9eea7
Syed Ali Raza <ali_raza995@hotmail.com>
2017-08-01 11:49:48 +0500

    BAEL-1051: Updated examples based on review

--- libraries/src/test/java/com/baeldung/hoverfly/HoverflyApiTest.java
+++ libraries/src/test/java/com/baeldung/hoverfly/HoverflyApiTest.java
@@ -5,12 +5,18 @@ import static io.specto.hoverfly.junit.dsl.HoverflyDsl.service;
 import static io.specto.hoverfly.junit.dsl.HttpBodyConverter.jsonWithSingleQuotes;
 import static io.specto.hoverfly.junit.dsl.ResponseCreators.success;
 import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.any;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.equalsTo;
 import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.equalsToJson;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.equalsToXml;
 import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.matches;
 import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.startsWith;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.matchesJsonPath;
+import static io.specto.hoverfly.junit.dsl.matchers.HoverflyMatchers.matchesXPath;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +24,7 @@ import org.apache.commons.lang3.time.StopWatch;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -40,11 +47,24 @@ public class HoverflyApiTest {
         
       service(matches("www.*dung.com"))
         .get(startsWith("/api/student"))     
-        .queryParam("page", any())   
+        .queryParam("page", any()) 
+        .queryParam("name", equalsTo("Joe"))
         .willReturn(success())
     
         .post("/api/student")
         .body(equalsToJson(jsonWithSingleQuotes("{'id':'1','name':'Joe'}")))
+        .willReturn(success())
+    
+        .put("/api/student/1")
+        .body(matchesJsonPath("$.name")) 
+        .willReturn(success())
+      
+        .post("/api/student")
+        .body(equalsToXml("<student><id>2</id><name>John</name></student>"))
+        .willReturn(success())
+        
+        .put("/api/student/2")
+        .body(matchesXPath("/student/name")) 
         .willReturn(success()));
 
     @ClassRule
@@ -82,11 +102,40 @@ public class HoverflyApiTest {
     }
     
     @Test
-    public void givenPostCourse_whenBodyRequestMatcher_thenResponseIsDelayed() throws URISyntaxException {
+    public void givenPostStudent_whenBodyRequestMatcherJson_thenResponseContainsEqualJson() throws URISyntaxException {
         final ResponseEntity<Void> postResponse = restTemplate.postForEntity(
           "http://www.baeldung.com/api/student", "{\"id\":\"1\",\"name\":\"Joe\"}", Void.class);
 
         assertEquals(HttpStatus.OK, postResponse.getStatusCode());
     }
     
+    @Test
+    public void givenPutStudent_whenJsonPathMatcher_thenRequestJsonContainsElementInPath() throws URISyntaxException {
+        RequestEntity<String> putRequest = RequestEntity
+          .put(new URI("http://www.baeldung.com/api/student/1"))
+          .body("{\"id\":\"1\",\"name\":\"Trevor\"}");
+
+        ResponseEntity<String> putResponse = restTemplate.exchange(putRequest, String.class);
+        assertEquals(HttpStatus.OK, putResponse.getStatusCode());
+    }
+    
+    @Test
+    public void givenPostStudent_whenBodyRequestMatcherXml_thenResponseContainsEqualXml() throws URISyntaxException {
+        final ResponseEntity<Void> postResponse = restTemplate.postForEntity(
+          "http://www.baeldung.com/api/student", "<student><id>2</id><name>John</name></student>", Void.class);
+
+        assertEquals(HttpStatus.OK, postResponse.getStatusCode());
+    }
+    
+    
+    @Test
+    public void givenPutStudent_whenXPathMatcher_thenRequestXmlContainsElementInXPath() throws URISyntaxException {
+        RequestEntity<String> putRequest = RequestEntity
+          .put(new URI("http://www.baeldung.com/api/student/2"))
+          .body("<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"
+              + "<student><id>2</id><name>Monica</name></student>");
+
+        ResponseEntity<String> putResponse = restTemplate.exchange(putRequest, String.class);
+        assertEquals(HttpStatus.OK, putResponse.getStatusCode());
+    }
 }
