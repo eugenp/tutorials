6fcbc9b065e88f65e47c4785d6ae83f22739a2e7
slavisa-baeldung <slavisa.baeldung@gmail.com>
2017-08-01 22:31:21 +0100

    BAEL-1022 - minor fixes

--- grpc/src/main/java/org/baeldung/grpc/server/GrpcServer.java
+++ grpc/src/main/java/org/baeldung/grpc/server/GrpcServer.java
@@ -5,11 +5,10 @@ import java.io.IOException;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
-public class GrpcServer
-{
+public class GrpcServer {
     public static void main(String[] args) throws IOException, InterruptedException {
         Server server = ServerBuilder.forPort(8080)
-            .addService(new HelloServiceImpl()).build();
+          .addService(new HelloServiceImpl()).build();
 
         System.out.println("Starting server...");
         server.start();
