fe499df79c47d72dbef13e1d2a22592d8115bebb
Chris Oberle <chris.oberle@gmail.com>
2017-08-06 16:22:08 -0400

    remove unused imports

--- spring-core/src/main/java/com/baeldung/valuewithdefaults/ValuesWithDefaultsApp.java
+++ spring-core/src/main/java/com/baeldung/valuewithdefaults/ValuesWithDefaultsApp.java
@@ -1,9 +1,7 @@
 package com.baeldung.valuewithdefaults;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 
 import javax.annotation.PostConstruct;
 
