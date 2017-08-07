13a67180d264376bb7833a3f1054a30d5fcf3a28
tehreem.nisa <tehreem.nisa@NISB-GSS-P5233.telenor.com.pk>
2017-08-07 20:33:51 +0500

    Log4j 2 and Lambdas

---
+++ log4j2/src/test/java/com/baeldung/logging/log4j2/tests/lambda/LambdaLoggingTest.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.baeldung.logging.log4j2.tests.lambda;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ *
+ * @author tehreem.nisa
+ */
+@RunWith(JUnit4.class)
+public class LambdaLoggingTest {
+    
+    @Test
+    public void givenLoggerWithDefaultConfig_whenLogToConsole_thanOK() throws Exception {
+
+        Logger logger = LogManager.getLogger(getClass());
+
+        logger.debug("I am logging that {} happened.", () -> expensiveOperation());
+
+    }
+
+    public int expensiveOperation() {
+        
+        int Number = 8;
+        return Number;
+    }
+
+}
