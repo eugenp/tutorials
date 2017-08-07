50a7b8e400354e0eb6511876b44da584971fc957
ramansahasi <ramansahasi@gmail.com>
2017-08-08 04:29:19 +0530

    Structural Design Patterns
    Difference between several types of structural design patterns

---
+++ core-java/src/main/java/com/baeldung/designpatterns/adapter/LuxuryCarsSpeedAdapter.java
@@ -0,0 +1,7 @@
+package com.baeldung.designpatterns.adapter;
+
+public interface LuxuryCarsSpeedAdapter {
+    public double bugattiVeyronInKMPH();
+    public double mcLarenInKMPH();
+    public double astonMartinInKMPH();
+}
