d986cfc2c32a3fc3f9e34d889430e894abc77fdd
Fatos Morina <fatosi.morina@gmail.com>
2017-08-12 16:58:07 +0200

    Implement getEvenIndexedStrings method

--- core-java/src/main/java/com/baeldung/stream/StreamIndices.java
+++ core-java/src/main/java/com/baeldung/stream/StreamIndices.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import com.codepoetics.protonpack.Indexed;
+import com.codepoetics.protonpack.StreamUtils;
+
 public class StreamIndices {
 
     public static List<String> getEvenIndexedStrings(String[] names) {
@@ -12,6 +15,13 @@ public class StreamIndices {
             .mapToObj(i -> names[i])
             .collect(Collectors.toList());
         return evenIndexedNames;
+    }
+
+    public static List<Indexed<String>> getEvenIndexedStrings(List<String> names) {
+        List<Indexed<String>> list = StreamUtils.zipWithIndex(names.stream())
+            .filter(i -> i.getIndex() % 2 == 0)
+            .collect(Collectors.toList());
+        return list;
     }
 
     public static List<String> getOddIndexedStrings(String[] names) {
