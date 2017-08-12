f319220dc26b0f629722d1fda399e9766ecf6204
Fatos Morina <fatosi.morina@gmail.com>
2017-08-12 13:51:21 +0200

    Implement givenList_whenGetEvenIndexedStrings_thenReturnListOfStrings test

---
+++ core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
@@ -0,0 +1,18 @@
+package com.baeldung.stream;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class StreamIndicesTest {
+    @Test
+    public void givenList_whenGetEvenIndexedStrings_thenReturnListOfStrings() {
+        String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
+        List<String> expectedResult = Arrays.asList("Afrim", "Besim", "Durim");
+        List<String> actualResult = StreamIndices.getEvenIndexedStrings(names);
+        assertEquals(expectedResult, actualResult);
+    }
+}
