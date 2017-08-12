644a9eecf0a9b6b15d7a967ee5a0bd0347027a3d
Fatos Morina <fatosi.morina@gmail.com>
2017-08-12 17:00:07 +0200

    Implement test of getevenIndexedStrings

--- core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
+++ core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
@@ -7,12 +7,23 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.codepoetics.protonpack.Indexed;
+
 public class StreamIndicesTest {
+
     @Test
-    public void givenList_whenGetEvenIndexedStrings_thenReturnListOfStrings() {
+    public void givenList_whenGetArrayIndexedStrings_thenReturnListOfEvenIndexedStrings() {
         String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
         List<String> expectedResult = Arrays.asList("Afrim", "Besim", "Durim");
         List<String> actualResult = StreamIndices.getEvenIndexedStrings(names);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenList_whenGetEvenIndexedStrings_thenReturnListOfEvenIndexedStrings() {
+        List<String> names = Arrays.asList("Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim");
+        List<Indexed<String>> expectedResult = Arrays.asList(Indexed.index(0, "Afrim"), Indexed.index(2, "Besim"), Indexed.index(4, "Durim"));
+        List<Indexed<String>> actualResult = StreamIndices.getEvenIndexedStrings(names);
         assertEquals(expectedResult, actualResult);
     }
 
