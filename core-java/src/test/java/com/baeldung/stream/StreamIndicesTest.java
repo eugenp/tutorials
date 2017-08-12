bf67bc87936e2feb43894ffd4d430ba03dfe58f6
Fatos Morina <fatosi.morina@gmail.com>
2017-08-12 17:02:29 +0200

    Rename methods of tests

--- core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
+++ core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
@@ -20,18 +20,19 @@ public class StreamIndicesTest {
     }
 
     @Test
-    public void givenList_whenGetEvenIndexedStrings_thenReturnListOfEvenIndexedStrings() {
+    public void givenArray_whenGetIndexedStrings_thenReturnListOfOddStrings() {
+        String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
+        List<String> expectedResult = Arrays.asList("Bashkim", "Lulzim", "Shpetim");
+        List<String> actualResult = StreamIndices.getOddIndexedStrings(names);
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenList_whenGetIndexedStrings_thenReturnListOfEvenIndexedStrings() {
         List<String> names = Arrays.asList("Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim");
         List<Indexed<String>> expectedResult = Arrays.asList(Indexed.index(0, "Afrim"), Indexed.index(2, "Besim"), Indexed.index(4, "Durim"));
         List<Indexed<String>> actualResult = StreamIndices.getEvenIndexedStrings(names);
         assertEquals(expectedResult, actualResult);
     }
 
-    @Test
-    public void givenList_whenGetOddIndexedStrings_thenReturnListOfStrings() {
-        String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
-        List<String> expectedResult = Arrays.asList("Bashkim", "Lulzim", "Shpetim");
-        List<String> actualResult = StreamIndices.getOddIndexedStrings(names);
-        assertEquals(expectedResult, actualResult);
-    }
 }
