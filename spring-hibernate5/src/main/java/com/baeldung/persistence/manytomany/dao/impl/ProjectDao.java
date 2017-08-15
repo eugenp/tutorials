8bc90603db8b01390404c773dae4ab340a6b2697
buddhini81 <buddhini81@users.noreply.github.com>
2017-08-15 12:33:21 +0530

    Fix minor formatting issues

--- spring-hibernate5/src/main/java/com/baeldung/persistence/manytomany/dao/impl/ProjectDao.java
+++ spring-hibernate5/src/main/java/com/baeldung/persistence/manytomany/dao/impl/ProjectDao.java
@@ -1,7 +1,6 @@
 package com.baeldung.persistence.manytomany.dao.impl;
 
 import org.springframework.stereotype.Repository;
-
 import com.baeldung.hibernate.manytomany.model.Project;
 import com.baeldung.persistence.dao.common.AbstractHibernateDao;
 import com.baeldung.persistence.manytomany.dao.IProjectDao;
