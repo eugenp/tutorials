8af124b291e2ce15f0fb3ca66371657558253ff4
buddhini81 <buddhini81@users.noreply.github.com>
2017-08-15 12:31:42 +0530

    Fix minor formatting issues

--- spring-hibernate5/src/main/java/com/baeldung/hibernate/manytomany/util/HibernateUtil.java
+++ spring-hibernate5/src/main/java/com/baeldung/hibernate/manytomany/util/HibernateUtil.java
@@ -4,7 +4,6 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-
 import com.baeldung.hibernate.manytomany.model.Employee;
 import com.baeldung.hibernate.manytomany.model.Project;
 
