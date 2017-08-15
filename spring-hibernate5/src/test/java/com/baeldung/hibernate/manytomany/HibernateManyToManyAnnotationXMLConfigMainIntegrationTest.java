23c4ee4ff2de5d4919681deaf87b412041c52191
buddhini81 <buddhini81@users.noreply.github.com>
2017-08-15 12:36:51 +0530

    Fix minor formatting issues

--- spring-hibernate5/src/test/java/com/baeldung/hibernate/manytomany/HibernateManyToManyAnnotationXMLConfigMainIntegrationTest.java
+++ spring-hibernate5/src/test/java/com/baeldung/hibernate/manytomany/HibernateManyToManyAnnotationXMLConfigMainIntegrationTest.java
@@ -2,7 +2,6 @@ package com.baeldung.hibernate.manytomany;
 
 import static org.junit.Assert.assertNotNull;
 import static junit.framework.TestCase.assertEquals;
-
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
