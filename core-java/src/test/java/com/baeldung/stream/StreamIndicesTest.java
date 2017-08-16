4a821f237fde1c7294c5906e6a2134ed644477e8
Fatos Morina <fatosi.morina@gmail.com>
2017-08-16 18:16:07 +0200

    Add empty lines before asserts

--- core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
+++ core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
@@ -16,6 +16,7 @@ public class StreamIndicesTest {
         String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
         List<String> expectedResult = Arrays.asList("Afrim", "Besim", "Durim");
         List<String> actualResult = StreamIndices.getEvenIndexedStrings(names);
+        
         assertEquals(expectedResult, actualResult);
     }
 
@@ -24,6 +25,7 @@ public class StreamIndicesTest {
         String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
         List<String> expectedResult = Arrays.asList("Bashkim", "Lulzim", "Shpetim");
         List<String> actualResult = StreamIndices.getOddIndexedStrings(names);
+        
         assertEquals(expectedResult, actualResult);
     }
 
@@ -32,6 +34,7 @@ public class StreamIndicesTest {
         List<String> names = Arrays.asList("Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim");
         List<Indexed<String>> expectedResult = Arrays.asList(Indexed.index(0, "Afrim"), Indexed.index(2, "Besim"), Indexed.index(4, "Durim"));
         List<Indexed<String>> actualResult = StreamIndices.getEvenIndexedStrings(names);
+        
         assertEquals(expectedResult, actualResult);
     }
     
@@ -40,6 +43,7 @@ public class StreamIndicesTest {
         List<String> names = Arrays.asList("Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim");
         List<Indexed<String>> expectedResult = Arrays.asList(Indexed.index(1, "Bashkim"), Indexed.index(3, "Lulzim"), Indexed.index(5, "Shpetim"));
         List<Indexed<String>> actualResult = StreamIndices.getOddIndexedStrings(names);
+        
         assertEquals(expectedResult, actualResult);
     }
 
