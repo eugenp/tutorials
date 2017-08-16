03935b4769f97648b8c2f61b9e3061c3d2e20486
adamd1985 <adamdarmanin@gmail.com>
2017-08-16 22:02:54 +0200

    Revert "BAEL-812: List of Rules Engines in Java (#2319)"
    This reverts commit dc105bc6f22b2b4ab9d504fdbf0dd9988ba26ec1.

--- openl-tablets/src/main/java/com/baeldung/openltablets/rules/IRule.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.openltablets.rules;
-
-import com.baeldung.openltablets.model.Case;
-
-public interface IRule {
-    void helloUser(final Case aCase, final Response response);
-}
