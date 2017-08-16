dc105bc6f22b2b4ab9d504fdbf0dd9988ba26ec1
felipeazv <felipeazv@users.noreply.github.com>
2017-08-16 21:58:42 +0200

    BAEL-812: List of Rules Engines in Java (#2319)
    * spring beans DI examples
    
    * fix-1: shortening examples
    
    * List of Rules Engines in Java
    
    * BAEL-812: Openl-Tablets example added
    
    * BAEL-812: artifacts names changed

---
+++ openl-tablets/src/main/java/com/baeldung/openltablets/model/User.java
@@ -0,0 +1,14 @@
+package com.baeldung.openltablets.model;
+
+public class User {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+}
