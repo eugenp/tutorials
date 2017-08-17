fb02892dda9d85ba100b78845bb20d9deee21348
mansi2392 <mkataria920@gmail.com>
2017-08-17 12:03:44 +0530

    BAEL-1045 Lambda Behave

---
+++ testing/src/main/java/com/baeldung/lambdabehave/Calculator.java
@@ -0,0 +1,27 @@
+package com.baeldung.lambdabehave;
+
+public class Calculator {
+
+    private int x;
+    private int y;
+
+    Calculator(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int add() {
+        return this.x + this.y;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
