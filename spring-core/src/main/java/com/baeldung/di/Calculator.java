f7b75ab7af81222853da4c56ce6f289e97f8eb34
nabyla <31042612+nabyla@users.noreply.github.com>
2017-08-24 11:42:34 +0100

    Update Calculator.java

--- spring-core/src/main/java/com/baeldung/di/Calculator.java
+++ spring-core/src/main/java/com/baeldung/di/Calculator.java
@@ -1,17 +1,21 @@
 package com.baeldung.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Calculator {
 
-	private Addition addition;
+    private Addition addition;
 
+    @Autowired
     public Calculator(Addition addition) {
-		System.out.println("Inside Calculator constructor.");
-		this.addition = addition;
-	}
+        System.out.println("Inside Calculator constructor.");
+        this.addition = addition;
+    }
 
-	public int calculateAddition(int a, int b) {
-		return addition.calculateAddition(a, b);
-	}
-	
-	
+    public int calculateAddition(int a, int b) {
+        return addition.calculateAddition(a, b);
+    }
+
 }
