6862a04a55b1aa60385f0e1e42bdd988093de516
nabyla <31042612+nabyla@users.noreply.github.com>
2017-08-24 11:47:54 +0100

    Update Test names

--- spring-core/src/test/java/com/baeldung/di/DependencyInjectionTypesTest.java
+++ spring-core/src/test/java/com/baeldung/di/DependencyInjectionTypesTest.java
@@ -5,22 +5,25 @@ import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class DependencyInjectionTypesTest {
-    
+
     @Test
-    public void testConstructorDI() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("di-beans-context.xml");
+    public void calculateAddConstructBean_givenInputs_returnResult() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+          Config.class);
         Calculator calculator = (Calculator) context.getBean("calculator");
         assertThat(calculator.calculateAddition(5, 2), equalTo(7));
     }
 
     @Test
-    public void testSetterDI() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("di-beans-context-2.xml");
-        CalculatorSet calculator = (CalculatorSet) context.getBean("calculatorSet");
+    public void calculateAddSetterBean_givenInputs_returnResult() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+          Config.class);
+        CalculatorSet calculator = (CalculatorSet) context
+          .getBean("calculatorSet");
         assertThat(calculator.calculateAddition(3, 12), equalTo(15));
     }
-   
+
 }
