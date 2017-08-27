8667d418c4205138bc57c33244e0856bccfc83a2
Fatos Morina <fatosi.morina@gmail.com>
2017-08-27 18:37:09 +0200

    Remove old files

---
+++ spring-core/src/main/java/com/baeldung/injectiontypes/SpringRunner.java
@@ -0,0 +1,21 @@
+package com.baeldung.injectiontypes;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.baeldung.injectiontypes.service.NotificationService;
+
+@Component
+public class SpringRunner {
+    private NotificationService notificationService;
+
+    @Autowired
+    public SpringRunner(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @Autowired
+    public void setNotificationService(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+}
