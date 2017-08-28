cfd44cfc05dcbc602aee31bdc5a7b05a57caa078
alincojanu <alincojanu@users.noreply.github.com>
2017-08-28 22:24:10 +0300

    delete project spring_sample_annotations and spring_sample_xml ->not intended for this article

--- spring_sample_xml/src/main/java/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import model.Customer;
-
-import java.util.List;
-
-public interface CustomerRepository {
-    List<Customer> findAll();
-}
--- spring_sample_xml/src/main/java/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import model.Customer;
-
-import java.util.List;
-
-public interface CustomerService {
-    List<Customer> findAll();
-}
