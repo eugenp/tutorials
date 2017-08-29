2e802d8e50ae01d34b878dcb8a5cdb6de757ff40
Fatos Morina <fatosi.morina@gmail.com>
2017-08-29 17:36:30 +0200

    Implement test givenArray_whenGetIndexedStrings_thenReturnListOfEvenIndexedStringsVersionTwo

--- core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
+++ core-java/src/test/java/com/baeldung/stream/StreamIndicesTest.java
@@ -16,7 +16,16 @@ public class StreamIndicesTest {
         String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
         List<String> expectedResult = Arrays.asList("Afrim", "Besim", "Durim");
         List<String> actualResult = StreamIndices.getEvenIndexedStrings(names);
-        
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void givenArray_whenGetIndexedStrings_thenReturnListOfEvenIndexedStringsVersionTwo() {
+        String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
+        List<String> expectedResult = Arrays.asList("Afrim", "Besim", "Durim");
+        List<String> actualResult = StreamIndices.getEvenIndexedStrings(names);
+
         assertEquals(expectedResult, actualResult);
     }
 
@@ -25,7 +34,7 @@ public class StreamIndicesTest {
         String[] names = { "Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim" };
         List<String> expectedResult = Arrays.asList("Bashkim", "Lulzim", "Shpetim");
         List<String> actualResult = StreamIndices.getOddIndexedStrings(names);
-        
+
         assertEquals(expectedResult, actualResult);
     }
 
@@ -34,16 +43,16 @@ public class StreamIndicesTest {
         List<String> names = Arrays.asList("Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim");
         List<Indexed<String>> expectedResult = Arrays.asList(Indexed.index(0, "Afrim"), Indexed.index(2, "Besim"), Indexed.index(4, "Durim"));
         List<Indexed<String>> actualResult = StreamIndices.getEvenIndexedStrings(names);
-        
+
         assertEquals(expectedResult, actualResult);
     }
-    
+
     @Test
     public void givenList_whenGetIndexedStrings_thenReturnListOfOddIndexedStrings() {
         List<String> names = Arrays.asList("Afrim", "Bashkim", "Besim", "Lulzim", "Durim", "Shpetim");
         List<Indexed<String>> expectedResult = Arrays.asList(Indexed.index(1, "Bashkim"), Indexed.index(3, "Lulzim"), Indexed.index(5, "Shpetim"));
         List<Indexed<String>> actualResult = StreamIndices.getOddIndexedStrings(names);
-        
+
         assertEquals(expectedResult, actualResult);
     }
 
