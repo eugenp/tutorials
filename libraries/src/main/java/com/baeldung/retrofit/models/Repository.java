fd37ffb7c28c7739eac74d54dc4f48e00ee68e7f
Hany Ahmed <hanylink@gmail.com>
2017-09-04 21:47:19 +0200

    Retrofit rx (#2519)
    * Retrofit with RxJava
    
    * Correct spelling mistake
    
    * Use spaces for indentation instead of tabs in pom.xml
    
    * Use spaces for indentation instead of tabs in pom.xml
    
    * Add Retrofit integration with RxJava to libraries module
    
    * Remove standalone project for Retrofit integration with RxJava
    
    * remove retrofit-rxjava module
    
    * Fixed error in pom.xml caused by an issue while merging
    
    * Retrofit integration with RxJava
    
    * Fix test cases
    
    * Fix merge issues
    
    * BAEL-1016 Merging master

---
+++ libraries/src/main/java/com/baeldung/retrofit/models/Repository.java
@@ -0,0 +1,32 @@
+package com.baeldung.retrofit.models;
+
+/**
+ * GitHub Repository
+ * @author hany
+ *
+ */
+public class Repository {
+    
+    private String name;
+    
+    private String description;
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Repository [name=" + name + ", description=" + description + "]";
+    }
+
+}
