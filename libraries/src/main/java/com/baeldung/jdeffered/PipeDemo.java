cafdf51bd7e9010056b57a7c4016a6c11fa6c9da
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-05 19:59:42 +0200

    Refactor JDeffered (#2559)

--- libraries/src/main/java/com/baeldung/jdeffered/PipeDemo.java
+++ libraries/src/main/java/com/baeldung/jdeffered/PipeDemo.java
@@ -5,35 +5,31 @@ import org.jdeferred.DonePipe;
 import org.jdeferred.Promise;
 import org.jdeferred.impl.DeferredObject;
 
-public class PipeDemo {
+class PipeDemo {
 
-    public static enum Result {
+    public enum Result {
         SUCCESS, FAILURE
     };
 
-    static Result status;
+    private static Result status;
 
-    public static Result validate(int num) {
-        Deferred<Integer, ?, ?> d = new DeferredObject<Integer, Object, Object>();
+    static Result validate(int num) {
+        Deferred<Integer, ?, ?> d = new DeferredObject<>();
         Promise<Integer, ?, ?> p = d.promise();
 
-        p.then(new DonePipe<Integer, Integer, Exception, Void>() {
-            public Deferred<Integer, Exception, Void> pipeDone(Integer result) {
-                if (result < 90) {
-                    return new DeferredObject<Integer, Exception, Void>().resolve(result);
-                } else {
-                    return new DeferredObject<Integer, Exception, Void>().reject(new Exception("Unacceptable value"));
-                }
+        p.then((DonePipe<Integer, Integer, Exception, Void>) result -> {
+            if (result < 90) {
+                return new DeferredObject<Integer, Exception, Void>()
+                  .resolve(result);
+            } else {
+                return new DeferredObject<Integer, Exception, Void>()
+                  .reject(new Exception("Unacceptable value"));
             }
-        }).done((result) -> {
-            status = Result.SUCCESS;
-        }).fail((result) -> {
-            status = Result.FAILURE;
-        });
+        }).done(r -> status = Result.SUCCESS)
+          .fail(r -> status = Result.FAILURE);
 
         d.resolve(num);
 
         return status;
     }
-
 }
