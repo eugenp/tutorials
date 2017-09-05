0d9068c0407e53720c94902b278cdebe1143ee88
alincojanu <alincojanu@users.noreply.github.com>
2017-09-05 21:00:36 +0300

    A Guide to RxJava (#2557)
    * alin.cojanu25@gmail.com
    
    * add junit tests
    
    * core-java - sneaky throws
    
    * delete project spring_sample_annotations and spring_sample_xml ->not intended for this article
    
    * RxJava
    
    * delete old project
    
    * refactored lambdas
    
    * repaired subject test

---
+++ rxjava/src/main/java/com/baelding/rxjava/ConnectableObservableImpl.java
@@ -0,0 +1,21 @@
+package com.baelding.rxjava;
+
+import rx.Observable;
+import rx.observables.ConnectableObservable;
+
+import java.util.concurrent.TimeUnit;
+
+public class ConnectableObservableImpl {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ConnectableObservable<Long> connectable = Observable.interval(200, TimeUnit.MILLISECONDS).publish();
+        connectable.subscribe(System.out::println);
+
+        System.out.println("Connect");
+        connectable.connect();
+
+        Thread.currentThread().sleep(500);
+        System.out.println("Sleep");
+    }
+}
