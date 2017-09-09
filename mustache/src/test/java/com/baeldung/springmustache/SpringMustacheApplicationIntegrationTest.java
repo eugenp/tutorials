f01d1e257e8ec3174d9cd1ce53ba5f40c28c716c
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-09 16:55:05 +0200

    Build optimization (#2591)

--- mustache/src/test/java/com/baeldung/springmustache/SpringMustacheApplicationIntegrationTest.java
+++ mustache/src/test/java/com/baeldung/springmustache/SpringMustacheApplicationIntegrationTest.java
@@ -13,7 +13,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class SpringMustacheApplicationTests {
+public class SpringMustacheApplicationIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
