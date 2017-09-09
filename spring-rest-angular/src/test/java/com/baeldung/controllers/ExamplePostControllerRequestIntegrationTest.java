27ea4d71d3ee1c57dbac115421327ebc71cbb01e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-09 14:57:33 +0200

    Build optimization

--- spring-rest-angular/src/test/java/com/baeldung/controllers/ExamplePostControllerRequestIntegrationTest.java
+++ spring-rest-angular/src/test/java/com/baeldung/controllers/ExamplePostControllerRequestIntegrationTest.java
@@ -2,6 +2,7 @@ package com.baeldung.controllers;
 
 import com.baeldung.services.ExampleService;
 import com.baeldung.transfer.LoginForm;
+import org.baeldung.web.main.Application;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,23 +11,24 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.baeldung.web.main.Application;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class ExamplePostControllerResponseUnitTest {
+public class ExamplePostControllerRequestIntegrationTest {
 
     MockMvc mockMvc;
-    @Mock private ExampleService exampleService;
-    @InjectMocks private ExamplePostController exampleController;
+    @Mock
+    private ExampleService exampleService;
+
+    @InjectMocks
+    private ExamplePostController exampleController;
     private final String jsonBody = "{\"username\": \"username\", \"password\": \"password\"}";
     private LoginForm lf = new LoginForm();
 
@@ -45,12 +47,11 @@ public class ExamplePostControllerResponseUnitTest {
         try {
             when(exampleService.fakeAuthenticate(lf)).thenReturn(true);
             mockMvc
-              .perform(post("/post/response")
+              .perform(post("/post/request")
                 .content(jsonBody)
                 .contentType("application/json"))
               .andDo(print())
-              .andExpect(status().isOk())
-              .andExpect(content().json("{\"text\":\"Thanks For Posting!!!\"}"));
+              .andExpect(status().isOk());
         } catch (Exception e) {
             System.out.println("Exception: " + e);
         }
