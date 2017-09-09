d56dbc081d49111f2e1f041a2ff948fdd2076ca5
Ahmed Tawila <atawila@yahoo.com>
2017-09-09 20:34:03 +0200

    BAEL-1109 Introduction to JCache

---
+++ libraries/src/test/java/com/baeldung/jcache/CacheLoaderTest.java
@@ -0,0 +1,35 @@
+package com.baeldung.jcache;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.cache.Cache;
+import javax.cache.CacheManager;
+import javax.cache.Caching;
+import javax.cache.configuration.FactoryBuilder;
+import javax.cache.configuration.MutableConfiguration;
+import javax.cache.spi.CachingProvider;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CacheLoaderTest {
+
+    private Cache<Integer, String> cache;
+
+    @Before
+    public void setup() {
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+        MutableConfiguration<Integer, String> config = new MutableConfiguration<Integer, String>().setReadThrough(true)
+            .setCacheLoaderFactory(new FactoryBuilder.SingletonFactory<>(new SimpleCacheLoader()));
+        this.cache = cacheManager.createCache("SimpleCache", config);
+    }
+
+    @Test
+    public void whenReadingFromStorage_thenCorrect() {
+        for (int i = 1; i < 4; i++) {
+            String value = cache.get(i);
+            assertEquals("fromCache" + i, value);
+        }
+    }
+}
