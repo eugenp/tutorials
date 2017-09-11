63678ff14958e9fb871c6472cca55592daa99530
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-11 18:18:28 +0200

    Fix int tests (#2608)
    * Rename JdbcIntegrationTest to JdbcLiveTest
    
    * Rename JdbcIntegrationTest to JdbcLiveTest
    
    * Rename JdbcIntegrationTest to JdbcLiveTest
    
    * Rename Integration tests to Live tests
    
    * Turn off benchmarks
    
    * BroadcastIntegratonTest -> BroadcastLiveTest

--- core-java/src/test/java/com/baeldung/networking/udp/broadcast/BroadcastLiveTest.java
+++ core-java/src/test/java/com/baeldung/networking/udp/broadcast/BroadcastLiveTest.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
-public class BroadcastIntegrationTest {
+public class BroadcastLiveTest {
     private BroadcastingClient client;
 
     @Test
