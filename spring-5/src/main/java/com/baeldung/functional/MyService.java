0f22f7b82ab0e5ae7c4f96b2a85bc1ca440589d4
eugenp <hanriseldon@gmail.com>
2017-09-13 14:41:03 +0300

    formatting work

--- spring-5/src/main/java/com/baeldung/functional/MyService.java
+++ spring-5/src/main/java/com/baeldung/functional/MyService.java
@@ -4,8 +4,8 @@ import java.util.Random;
 
 public class MyService {
 
-    public int getRandomNumber(){
+    public int getRandomNumber() {
         return (new Random().nextInt(10));
     }
-    
+
 }
