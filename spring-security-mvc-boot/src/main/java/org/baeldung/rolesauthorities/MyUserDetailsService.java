2b8205ee31c974b5d4f1cb624f81df5b70e2af9d
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-09-14 08:15:23 +0300

    Refactor getAuth

--- spring-security-mvc-boot/src/main/java/org/baeldung/rolesauthorities/MyUserDetailsService.java
+++ spring-security-mvc-boot/src/main/java/org/baeldung/rolesauthorities/MyUserDetailsService.java
@@ -1,10 +1,6 @@
 package org.baeldung.rolesauthorities;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.baeldung.rolesauthorities.model.Privilege;
 import org.baeldung.rolesauthorities.model.Role;
 import org.baeldung.rolesauthorities.model.User;
 import org.baeldung.rolesauthorities.persistence.UserRepository;
@@ -16,6 +12,12 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service("userDetailsService")
 @Transactional
@@ -38,8 +40,8 @@ public class MyUserDetailsService implements UserDetailsService {
             if (user == null) {
                 throw new UsernameNotFoundException("No user found with username: " + email);
             }
-            org.springframework.security.core.userdetails.User userDetails = new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user.isEnabled(), true, true, true, getAuthorities(user.getRoles()));
-            return userDetails;
+            return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), user
+              .isEnabled(), true, true, true, getAuthorities(user.getRoles()));
         } catch (final Exception e) {
             throw new RuntimeException(e);
         }
@@ -47,15 +49,14 @@ public class MyUserDetailsService implements UserDetailsService {
 
     // UTIL
 
-    private final Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
-    	List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
-    	for (Role role: roles) {
-    		authorities.add(new SimpleGrantedAuthority(role.getName()));
-    		authorities.addAll(role.getPrivileges()
-    				.stream()
-    				.map(p -> new SimpleGrantedAuthority(p.getName()))
-    				.collect(Collectors.toList()));
-    	}
-        return authorities;
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
+        return roles.stream()
+          .flatMap(role -> {
+              return Stream.concat(
+                Stream.of(role.getName()),
+                role.getPrivileges().stream().map(Privilege::getName));
+          })
+          .map(SimpleGrantedAuthority::new)
+          .collect(Collectors.toList());
     }
 }
