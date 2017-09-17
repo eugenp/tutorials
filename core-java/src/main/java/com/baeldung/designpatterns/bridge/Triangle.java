3a9654370af0659ed23bbafc8ea7d81c112b4da7
ramansahasi <ramansahasi@gmail.com>
2017-09-18 05:29:23 +0530

    BAEL-1072 Difference between Proxy, Decorator, Adapter, and Bridge Patterns (#2635)
    * Changes as per last review. Changed method names so that they look more idiomatic.
    
    * Updated pattern, changed method names and test cases

--- core-java/src/main/java/com/baeldung/designpatterns/bridge/Triangle.java
+++ core-java/src/main/java/com/baeldung/designpatterns/bridge/Triangle.java
@@ -1,7 +1,5 @@
 package com.baeldung.designpatterns.bridge;
 
-import static com.baeldung.designpatterns.util.LogerUtil.LOG;
-
 public class Triangle extends Shape {
 
     public Triangle(Color color) {
@@ -9,8 +7,7 @@ public class Triangle extends Shape {
     }
 
     @Override
-    public void drawShape() {
-        LOG.info("Triangle drawn. ");
-        color.fillColor();
+    public String draw() {
+        return "Triangle drawn. "+ color.fill();
     }
 }
