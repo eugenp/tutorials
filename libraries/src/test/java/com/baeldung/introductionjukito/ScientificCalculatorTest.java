3954740d6681072dfb2eacd1b5a02c91def8c4b9
hugosama <hugosama1@gmail.com>
2017-09-17 11:26:57 -0600

    Introduction to Jukito

---
+++ libraries/src/test/java/com/baeldung/introductionjukito/ScientificCalculatorTest.java
@@ -0,0 +1,34 @@
+package com.baeldung.introductionjukito;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ScientificCalculatorTest {
+
+    public ScientificCalculatorTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+
+}
