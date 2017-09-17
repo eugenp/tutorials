1ced2a3b87698a0b605b7438613ab9c909060f72
Adam InTae Gerard <Thoughtscript@thoughtscript.io>
2017-09-17 02:43:49 -0700

    BAEL-1052: EthereumJ (#2614)
    * Simple Boot REST application and example
    
    * BAEL-509 - Removed extra duplicate ant matcher
    
    * Example and Unit Tests
    
    * documentation
    
    * BAEL-1085 - changes per PR code review and document review - altered integration to unit test - all's good
    
    * BAEL-1085 - Renamed unit tests and added both to pom.xml
    
    * IntelliJ formatter
    
    * REVERT - Had removed a duplicate ant matcher from BAEL-509 - this was the incorrect process - reverting now, article has been corrected, but will issue a seperate PR for this
    
    * BAEL-509: Removed duplicate ant matcher - does not impact code at runtime
    
    * BAEL-1085: Per editor's request, removed Angular client - using CURL POST commands now in article - as such moved the code into a more appropriate module: spring-rest
    
    * BAEL-1052: EthereumJ
    
    * BAEL-1052: .pom module renamed to EthereumJ
    
    * Try catch clauses removed - tested and verified everything again

---
+++ ethereumj/src/main/java/com/baeldung/ethereumj/transfer/EthResponse.java
@@ -0,0 +1,14 @@
+package com.baeldung.ethereumj.transfer;
+
+public class EthResponse {
+
+    private String response;
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+}
