f74175c0b533ef28f971207e9d2384cf14eb3db5
felipeazv <felipe.azv@gmail.com>
2017-09-18 00:08:03 +0200

    BAEL-781: remove eureka-server from the example

--- spring-cloud/spring-cloud-gateway/gateway-service/src/main/java/com/baeldung/spring/cloud/GatewayApplication.java
+++ spring-cloud/spring-cloud-gateway/gateway-service/src/main/java/com/baeldung/spring/cloud/GatewayApplication.java
@@ -2,23 +2,9 @@ package com.baeldung.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class GatewayApplication {
-
-    @Configuration
-    @EnableDiscoveryClient
-    protected static class GatewayDiscoveryConfiguration {
-        @Bean
-        public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient) {
-            return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
-        }
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
