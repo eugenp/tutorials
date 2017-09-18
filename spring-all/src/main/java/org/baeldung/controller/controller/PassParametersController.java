0e2f2da6e04329168c8c7c49cec54cc3416cae4c
ahmetcetin39 <30636222+ahmetcetin39@users.noreply.github.com>
2017-09-18 17:08:30 +0300

    Update PassParametersController.java

--- spring-all/src/main/java/org/baeldung/controller/controller/PassParametersController.java
+++ spring-all/src/main/java/org/baeldung/controller/controller/PassParametersController.java
@@ -3,8 +3,7 @@ package org.baeldung.controller.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
