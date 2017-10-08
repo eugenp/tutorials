491649a866bfd65d276bd77ab78b55f5eee3b28c
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-10-08 14:07:30 +0200

    Refactor Docx examples (#2716)

--- libraries/src/test/java/com/baeldung/docx/Docx4jReadAndWriteTest.java
+++ libraries/src/test/java/com/baeldung/docx/Docx4jReadAndWriteTest.java
@@ -1,24 +1,19 @@
 package com.baeldung.docx;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class Docx4jReadAndWriteTest {
 
-    String imagePath = "src/main/resources/image.jpg";
-    String outputPath = "helloWorld.docx";
-    
+    private static final String imagePath = "src/main/resources/image.jpg";
+    private static final String outputPath = "helloWorld.docx";
+
     @Test
-    public void givenWordPackage_whenTextExist_thenReturnTrue() {
+    public void givenWordPackage_whenTextExist_thenReturnTrue() throws Exception {
         Docx4jExample docx4j = new Docx4jExample();
-        try {
-            docx4j.createDocumentPackage(outputPath, imagePath);
-            assertTrue(docx4j.isTextExist("Hello World!"));
-            assertTrue(!docx4j.isTextExist("InexistantText"));
-        } catch (Exception e) {
-            fail();
-        }
+        docx4j.createDocumentPackage(outputPath, imagePath);
+        assertTrue(docx4j.isTextExist("Hello World!"));
+        assertTrue(!docx4j.isTextExist("InexistantText"));
     }
 }
