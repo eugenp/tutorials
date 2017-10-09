a4cf21727ea915f933fcd70ccad5d5a6ee9ec40e
Deep Jain <deepjain.iitr@yahoo.in>
2017-10-09 16:27:37 +0530

    Calculate edit distance

---
+++ algorithms/src/main/java/com/baeldung/algorithms/editdistance/EditDistanceDynamicProgramming.java
@@ -0,0 +1,32 @@
+package com.baeldung.algorithms.editdistance;
+
+public class EditDistanceDynamicProgramming extends EditDistanceBase {
+
+    private static int[][] dp;
+
+    public static int calculate(String x, String y) {
+        dp = new int[x.length()+1][y.length()+1];
+
+        for (int i=0;i<=x.length();i++){
+            for (int j=0;j<=y.length();j++) {
+                if(i==0)
+                    dp[i][j] = j;
+
+                else if(j==0)
+                    dp[i][j] = i;
+
+                else {
+                    dp[i][j] = min (
+                                 dp[i-1][j-1]
+                                         +costOfSubstitution(x.charAt(i-1), y.charAt(j-1)),
+                                 dp[i-1][j]+1,
+                                 dp[i][j-1]+1
+                               );
+                }
+            }
+        }
+
+        return dp[x.length()][y.length()];
+    }
+
+}
