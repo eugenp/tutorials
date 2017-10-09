e6339d161809811833754c7e8deccaba38e9ca3e
Yasin <yasin.bhojawala@aurea.com>
2017-10-09 21:59:55 +0530

    changes

--- saas/src/main/java/com/baeldung/saas/jira/MyJiraClient.java
+++ saas/src/main/java/com/baeldung/saas/jira/MyJiraClient.java
@@ -31,15 +31,14 @@ public class MyJiraClient {
 
     public static void main(String[] args) throws IOException {
 
-        MyJiraClient myJiraClient = new MyJiraClient("yasin.bhojawala", "jira@P455", "http://jira.baeldung.com");
+        MyJiraClient myJiraClient = new MyJiraClient("user.name", "pass", "http://jira.company.com");
 
-//        final String issueKey = myJiraClient.createIssue("ABCD", 1L, "Issue created from JRJC");
-        final String issueKey = "BAEL-1139";
-//        myJiraClient.updateIssueDescription(issueKey, "This is description from my Jira Client");
+        final String issueKey = myJiraClient.createIssue("ABCD", 1L, "Issue created from JRJC");
+        myJiraClient.updateIssueDescription(issueKey, "This is description from my Jira Client");
         Issue issue = myJiraClient.getIssue(issueKey);
         System.out.println(issue.getDescription());
 
-        /*myJiraClient.voteForAnIssue(issue);
+        myJiraClient.voteForAnIssue(issue);
 
         System.out.println(myJiraClient.getTotalVotesCount(issueKey));
 
@@ -48,7 +47,7 @@ public class MyJiraClient {
         List<Comment> comments = myJiraClient.getAllComments(issueKey);
         comments.forEach(c -> System.out.println(c.getBody()));
 
-        myJiraClient.deleteIssue(issueKey, true);*/
+        myJiraClient.deleteIssue(issueKey, true);
 
         myJiraClient.restClient.close();
     }
