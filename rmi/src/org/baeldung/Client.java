f025d5ea0d2d1b6a812994e5bd6d0fe7cec1dda2
Harihar Das <dharihar@thoughtworks.com>
2017-10-14 12:24:16 +0530

    <BAEL-1209>[Harihar] Use specific exception declaration

--- rmi/src/org/baeldung/Client.java
+++ rmi/src/org/baeldung/Client.java
@@ -1,23 +1,20 @@
 package org.baeldung;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException, NotBoundException {
         System.setProperty("java.security.policy", "file:./client.policy");
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
-        try {
-            String name = "org.baeldung.RandomNumberGenerator";
-            Registry registry = LocateRegistry.getRegistry();
-            RandomNumberGenerator randomNumberGenerator = (RandomNumberGenerator) registry.lookup(name);
-            int number = randomNumberGenerator.get();
-            System.out.println(number);
-        } catch (Exception e) {
-            System.err.println("org.baeldung.RandomNumberGenerator exception:");
-            e.printStackTrace();
-        }
+        String name = "RandomNumberGenerator";
+        Registry registry = LocateRegistry.getRegistry();
+        RandomNumberGenerator randomNumberGenerator = (RandomNumberGenerator) registry.lookup(name);
+        int number = randomNumberGenerator.get();
+        System.out.println("Received random number:" + number);
     }
 }
--- rmi/src/org/baeldung/Server.java
+++ rmi/src/org/baeldung/Server.java
@@ -1,26 +1,22 @@
 package org.baeldung;
 
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws RemoteException {
         System.setProperty("java.security.policy", "file:./server.policy");
         if (System.getSecurityManager() == null) {
             System.setSecurityManager(new SecurityManager());
         }
-        try {
-            String name = "org.baeldung.RandomNumberGenerator";
-            RandomNumberGenerator randomNumberGenerator = new RandomNumberGeneratorEngine();
-            RandomNumberGenerator stub =
-                    (RandomNumberGenerator) UnicastRemoteObject.exportObject(randomNumberGenerator, 0);
-            Registry registry = LocateRegistry.getRegistry();
-            registry.rebind(name, stub);
-            System.out.println("org.baeldung.RandomNumberGenerator bound");
-        } catch (Exception e) {
-            System.err.println("org.baeldung.RandomNumberGenerator exception:");
-            e.printStackTrace();
-        }
+        String name = "RandomNumberGenerator";
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGeneratorEngine();
+        RandomNumberGenerator stub =
+                (RandomNumberGenerator) UnicastRemoteObject.exportObject(randomNumberGenerator, 0);
+        Registry registry = LocateRegistry.getRegistry();
+        registry.rebind(name, stub);
+        System.out.println("RandomNumberGenerator bound");
     }
 }
