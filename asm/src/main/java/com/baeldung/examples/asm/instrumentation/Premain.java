c6d069f072b4beb74c5b2d7292f3fa2913610103
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-10-15 11:42:22 +0200

    Merge fix

--- asm/src/main/java/com/baeldung/examples/asm/instrumentation/Premain.java
+++ asm/src/main/java/com/baeldung/examples/asm/instrumentation/Premain.java
@@ -1,32 +1,19 @@
 package com.baeldung.examples.asm.instrumentation;
 
 import com.baeldung.examples.asm.CustomClassWriter;
-import java.lang.instrument.ClassFileTransformer;
-import java.lang.instrument.IllegalClassFormatException;
-import java.lang.instrument.Instrumentation;
-import java.security.ProtectionDomain;
 
-/**
- *
- * @author baeldung
- */
+import java.lang.instrument.Instrumentation;
+
 public class Premain {
 
     public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new ClassFileTransformer() {
+        inst.addTransformer((l, name, c, d, b) -> {
 
-            @Override
-            public byte[] transform(ClassLoader l, String name, Class c,
-                    ProtectionDomain d, byte[] b)
-                    throws IllegalClassFormatException {
-
-                if (name.equals("java/lang/Integer")) {
-                    CustomClassWriter cr = new CustomClassWriter(b);
-                    return cr.addField();
-                }
-                return b;
+            if (name.equals("java/lang/Integer")) {
+                CustomClassWriter cr = new CustomClassWriter(b);
+                return cr.addField();
             }
+            return b;
         });
     }
-
 }
