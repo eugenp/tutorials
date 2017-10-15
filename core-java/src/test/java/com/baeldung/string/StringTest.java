82fde89335c1abe44cb65e7625bbdb8ca4f8c629
Ahmed Tawila <atawila@yahoo.com>
2017-10-15 12:38:54 +0200

    BAEL-1171 java.lang.String API

--- core-java/src/test/java/com/baeldung/string/StringTest.java
+++ core-java/src/test/java/com/baeldung/string/StringTest.java
@@ -38,7 +38,7 @@ public class StringTest {
     }
 
     @Test
-    public void whenGetBytesUsingUTF16_thenCorrect() {
+    public void whenGetBytesUsingASCII_thenCorrect() {
         byte[] byteArray = "efgh".getBytes(StandardCharsets.US_ASCII);
         byte[] expected = new byte[] { 101, 102, 103, 104 };
 
@@ -154,6 +154,13 @@ public class StringTest {
         String s = "Welcome*to Baeldung";
 
         String[] result = s.split("*");
+    }
+    
+    @Test
+    public void whenCallSubSequence_thenCorrect() {
+        String s = "Welcome to Baeldung";
+
+        assertEquals("Welcome", s.subSequence(0, 7));
     }
 
     @Test
