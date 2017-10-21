7bbbacc5b4c4fbe45e91e1d47de84197bd8cf1a0
chober <chris.oberle@gmail.com>
2017-10-21 13:09:32 +0100

    update formatting and jackson version

--- jackson/src/test/java/com/baeldung/jackson/deserialization/nested/DeserializeWithNestedPropertiesUnitTest.java
+++ jackson/src/test/java/com/baeldung/jackson/deserialization/nested/DeserializeWithNestedPropertiesUnitTest.java
@@ -12,52 +12,59 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class DeserializeWithNestedPropertiesUnitTest {
 
-	private String SOURCE_JSON = "{\"id\":\"957c43f2-fa2e-42f9-bf75-6e3d5bb6960a\",\"name\":\"The Best Product\",\"brand\":{\"id\":\"9bcd817d-0141-42e6-8f04-e5aaab0980b6\",\"name\":\"ACME Products\",\"owner\":{\"id\":\"b21a80b1-0c09-4be3-9ebd-ea3653511c13\",\"name\":\"Ultimate Corp, Inc.\"}}}";
+    private String SOURCE_JSON = "{\"id\":\"957c43f2-fa2e-42f9-bf75-6e3d5bb6960a\",\"name\":\"The Best Product\",\"brand\":{\"id\":\"9bcd817d-0141-42e6-8f04-e5aaab0980b6\",\"name\":\"ACME Products\",\"owner\":{\"id\":\"b21a80b1-0c09-4be3-9ebd-ea3653511c13\",\"name\":\"Ultimate Corp, Inc.\"}}}";
 
-	@Test
-	public void whenUsingJacksonAnnotations_thenOk() throws IOException {
-		Product product = new ObjectMapper().readerFor(Product.class)
-                .readValue(SOURCE_JSON);
-
-        assertEquals(product.getName(), "The Best Product");
-        assertEquals(product.getBrandName(), "ACME Products");
-        assertEquals(product.getOwnerName(), "Ultimate Corp, Inc.");
-	}
-
-	@Test
-	public void whenUsingJacksonJsonNode_thenOk() throws IOException {
-		JsonNode productNode = new ObjectMapper().readTree(SOURCE_JSON);
-
-		Product product = new Product();
-		product.setId(productNode.get("id").textValue());
-		product.setName(productNode.get("name").textValue());
-		product.setBrandName(productNode.get("brand").get("name").textValue());
-		product.setOwnerName(productNode.get("brand").get("owner").get("name").textValue());
+    @Test
+    public void whenUsingJacksonAnnotations_thenOk() throws IOException {
+        Product product = new ObjectMapper().readerFor(Product.class)
+            .readValue(SOURCE_JSON);
 
         assertEquals(product.getName(), "The Best Product");
         assertEquals(product.getBrandName(), "ACME Products");
         assertEquals(product.getOwnerName(), "Ultimate Corp, Inc.");
-	}
+    }
 
-	@Test
-	public void whenUsingJacksonDeserializerManuallyRegistered_thenOk() throws IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		SimpleModule module = new SimpleModule();
-		module.addDeserializer(Product.class, new ProductDeserializer());
-		mapper.registerModule(module);
+    @Test
+    public void whenUsingJacksonJsonNode_thenOk() throws IOException {
+        JsonNode productNode = new ObjectMapper().readTree(SOURCE_JSON);
 
-		Product product = mapper.readValue(SOURCE_JSON, Product.class);
+        Product product = new Product();
+        product.setId(productNode.get("id")
+            .textValue());
+        product.setName(productNode.get("name")
+            .textValue());
+        product.setBrandName(productNode.get("brand")
+            .get("name")
+            .textValue());
+        product.setOwnerName(productNode.get("brand")
+            .get("owner")
+            .get("name")
+            .textValue());
+
         assertEquals(product.getName(), "The Best Product");
         assertEquals(product.getBrandName(), "ACME Products");
         assertEquals(product.getOwnerName(), "Ultimate Corp, Inc.");
-	}
+    }
 
-	@Test
-	public void whenUsingJacksonDeserializerAutoRegistered_thenOk() throws IOException {
-		ObjectMapper mapper = new ObjectMapper();
-		Product product = mapper.readValue(SOURCE_JSON, Product.class);
+    @Test
+    public void whenUsingJacksonDeserializerManuallyRegistered_thenOk() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(Product.class, new ProductDeserializer());
+        mapper.registerModule(module);
+
+        Product product = mapper.readValue(SOURCE_JSON, Product.class);
         assertEquals(product.getName(), "The Best Product");
         assertEquals(product.getBrandName(), "ACME Products");
         assertEquals(product.getOwnerName(), "Ultimate Corp, Inc.");
-	}
+    }
+
+    @Test
+    public void whenUsingJacksonDeserializerAutoRegistered_thenOk() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        Product product = mapper.readValue(SOURCE_JSON, Product.class);
+        assertEquals(product.getName(), "The Best Product");
+        assertEquals(product.getBrandName(), "ACME Products");
+        assertEquals(product.getOwnerName(), "Ultimate Corp, Inc.");
+    }
 }
--- jackson/src/test/java/com/baeldung/jackson/deserialization/nested/Product.java
+++ jackson/src/test/java/com/baeldung/jackson/deserialization/nested/Product.java
@@ -8,48 +8,48 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 @JsonDeserialize(using = ProductDeserializer.class)
 public class Product {
 
-	private String id;
-	private String name;
-	private String brandName;
-	private String ownerName;
+    private String id;
+    private String name;
+    private String brandName;
+    private String ownerName;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public String getBrandName() {
-		return brandName;
-	}
+    public String getBrandName() {
+        return brandName;
+    }
 
-	public void setBrandName(String brandName) {
-		this.brandName = brandName;
-	}
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
 
-	public String getOwnerName() {
-		return ownerName;
-	}
+    public String getOwnerName() {
+        return ownerName;
+    }
 
-	public void setOwnerName(String ownerName) {
-		this.ownerName = ownerName;
-	}
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
 
-	@SuppressWarnings("unchecked")
-	@JsonProperty("brand")
-	private void unpackNested(Map<String,Object> brand) {
-		this.brandName = (String)brand.get("name");
-		Map<String,String> owner = (Map<String,String>)brand.get("owner");
-		this.ownerName = owner.get("name");
-	}
+    @SuppressWarnings("unchecked")
+    @JsonProperty("brand")
+    private void unpackNested(Map<String, Object> brand) {
+        this.brandName = (String) brand.get("name");
+        Map<String, String> owner = (Map<String, String>) brand.get("owner");
+        this.ownerName = owner.get("name");
+    }
 }
