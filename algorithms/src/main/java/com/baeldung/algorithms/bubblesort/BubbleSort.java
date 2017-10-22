a7282d233bf78e694b914cdaa718949bdb919c1e
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-10-22 14:17:45 +0200

    Bubble sort fix (#2809)

--- algorithms/src/main/java/com/baeldung/algorithms/bubblesort/BubbleSort.java
+++ algorithms/src/main/java/com/baeldung/algorithms/bubblesort/BubbleSort.java
@@ -19,11 +19,13 @@ public class BubbleSort {
 
     void optimizedBubbleSort(Integer[] arr) {
         int i = 0, n = arr.length;
+
         boolean swapNeeded = true;
         while (i < n - 1 && swapNeeded) {
             swapNeeded = false;
-            for (int j = i + 1; j < n - i; j++) {
+            for (int j = 1; j < n - i; j++) {
                 if (arr[j - 1] > arr[j]) {
+
                     int temp = arr[j - 1];
                     arr[j - 1] = arr[j];
                     arr[j] = temp;
