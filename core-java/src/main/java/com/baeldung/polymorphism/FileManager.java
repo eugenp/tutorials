d43a9a764374ed5a66c05d5fd4cae9711a824366
Ocheja Patrick <ocheja.patrick@gmail.com>
2017-10-25 00:29:57 +0900

    Add examples for static polymorphism

--- core-java/src/main/java/com/baeldung/polymorphism/FileManager.java
+++ core-java/src/main/java/com/baeldung/polymorphism/FileManager.java
@@ -6,20 +6,22 @@ public class FileManager {
 
     public static void main(String[] args) {
         GenericFile file1 = new TextFile("SampleTextFile", "This is a sample text content", "v1.0.0");
-        System.out.println("File 1 Info: \n" + file1.getFileInfo() + "\n");
+        System.out.println("File Info: \n" + file1.getFileInfo() + "\n");
         ImageFile imageFile = new ImageFile("SampleImageFile", 200, 100, new BufferedImage(100, 200, BufferedImage.TYPE_INT_RGB).toString()
             .getBytes(), "v1.0.0");
-        System.out.println("File 2 Info: \n" + imageFile.getFileInfo());
+        System.out.println("File Info: \n" + imageFile.getFileInfo());
     }
 
-    public static void createImageFile(String name, int height, int width, byte[] content, String version) {
+    public static ImageFile createImageFile(String name, int height, int width, byte[] content, String version) {
         ImageFile imageFile = new ImageFile(name, height, width, content, version);
         System.out.println("File 2 Info: \n" + imageFile.getFileInfo());
+        return imageFile;
     }
 
-    public static void createTextFile(String name, String content, String version) {
+    public static GenericFile createTextFile(String name, String content, String version) {
         GenericFile file1 = new TextFile(name, content, version);
         System.out.println("File 1 Info: \n" + file1.getFileInfo() + "\n");
+        return file1;
     }
 
 }
--- core-java/src/main/java/com/baeldung/polymorphism/TextFile.java
+++ core-java/src/main/java/com/baeldung/polymorphism/TextFile.java
@@ -29,4 +29,16 @@ public class TextFile extends GenericFile {
             .toString();
     }
 
+    public String read(int limit) {
+        return this.getContent()
+            .toString()
+            .substring(0, limit);
+    }
+
+    public String read(int start, int stop) {
+        return this.getContent()
+            .toString()
+            .substring(start, stop);
+    }
+
 }
