9ce32992a791f5d890c14123da9bb27d3079ba06
Ahmed Tawila <atawila@yahoo.com>
2017-10-25 15:53:15 +0200

    small fix

--- core-java/src/test/java/com/baeldung/array/ArrayInitializerTest.java
+++ core-java/src/test/java/com/baeldung/array/ArrayInitializerTest.java
@@ -67,6 +67,7 @@ public class ArrayInitializerTest {
         assertArrayEquals(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, initializeArrayUsingArraysSetAll());
     }
 
+    @Test
     public void whenInitializeArrayUsingArraysUtilClone_thenCorrect() {
         assertArrayEquals(new char[] { 'a', 'b', 'c' }, initializeArrayUsingArraysUtilClone());
     }
