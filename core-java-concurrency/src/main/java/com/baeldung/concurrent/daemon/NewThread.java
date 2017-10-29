1be0d76f8c12837683846b0ba1d3da4307d8339a
Ahmed Tawila <atawila@yahoo.com>
2017-10-29 12:59:55 +0200

    BAEL-1263 Daemon Threads in Java

--- core-java-concurrency/src/main/java/com/baeldung/concurrent/daemon/NewThread.java
+++ core-java-concurrency/src/main/java/com/baeldung/concurrent/daemon/NewThread.java
@@ -3,7 +3,8 @@ package com.baeldung.concurrent.daemon;
 public class NewThread extends Thread {
 
     public void run() {
-        for (int i = 0; i < 10; i++)
-            System.out.println("New Thread is running...");
+        while (true)
+            for (int i = 0; i < 10; i++)
+                System.out.println("New Thread is running...");
     }
 }
