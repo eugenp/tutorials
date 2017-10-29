729c392019658fad952734acd0faf2a5ec0c58c6
Yasin Bhojawala <yasin.bhojawala@gmail.com>
2017-10-29 19:16:42 +0530

    BAEL-1217 fixed failing tests

--- spring-boot/src/test/java/org/baeldung/converter/CustomConverterTest.java
+++ spring-boot/src/test/java/org/baeldung/converter/CustomConverterTest.java
@@ -44,7 +44,7 @@ public class CustomConverterTest {
     @Test
     public void whenConvertingToBigDecimalUsingGenericConverter_thenSuccess() {
         assertThat(conversionService.convert(Integer.valueOf(11), BigDecimal.class)).
-          isEqualTo(BigDecimal.valueOf(11));
+          isEqualTo(BigDecimal.valueOf(11.00).setScale(2, BigDecimal.ROUND_HALF_EVEN));
         assertThat(conversionService.convert(Double.valueOf(25.23), BigDecimal.class))
           .isEqualByComparingTo(BigDecimal.valueOf(Double.valueOf(25.23)));
         assertThat(conversionService.convert("2.32", BigDecimal.class))
