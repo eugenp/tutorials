9796c48a014a599ad576208b0dcbcbda5dcfec93
alincojanu <alincojanu@users.noreply.github.com>
2017-10-30 21:34:14 +0200

    activeJDBC

---
+++ activejdbc/src/main/java/School.java
@@ -0,0 +1,6 @@
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Cached;
+
+@Cached
+public class School extends Model {
+}
---
+++ activejdbc/src/migrations/tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE students (
+  id   INT(11) NOT NULL AUTO_INCREMENT,
+  name TEXT,
+  age INT(11),
+  PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
