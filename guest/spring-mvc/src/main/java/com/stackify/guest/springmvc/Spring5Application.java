d6cbbbedb48ba9c17589ffe956a79fde53cf92a7
eugenp <hanriseldon@gmail.com>
2017-11-02 00:35:08 +0200

    making the base package aligned with the site

--- guest/spring-mvc/src/main/java/com/stackify/guest/springmvc/Spring5Application.java
+++ guest/spring-mvc/src/main/java/com/stackify/guest/springmvc/Spring5Application.java
@@ -1,11 +1,11 @@
-package com.forketyfork.guest.springmvc;
+package com.stackify.guest.springmvc;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.SpringApplication;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.forketyfork.guest.springmvc"})
+@ComponentScan(basePackages = {"com.stackify.guest.springmvc"})
 public class Spring5Application {
 
     public static void main(String[] args) {
--- guest/spring-mvc/src/main/java/com/stackify/guest/springmvc/web/RestfulWebServiceController.java
+++ guest/spring-mvc/src/main/java/com/stackify/guest/springmvc/web/RestfulWebServiceController.java
@@ -1,4 +1,4 @@
-package com.forketyfork.guest.springmvc.web;
+package com.stackify.guest.springmvc.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
