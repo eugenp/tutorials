b023f4db0f9203c68c2ac4268fbb176f6463d84e
markul <mariusz.kuligowski@fideltronik.com>
2017-11-02 21:46:38 +0100

    Add contructor with necessary Sword and Shield

--- spring-core/src/main/java/com/baeldung/beaninjection/domain/Hero.java
+++ spring-core/src/main/java/com/baeldung/beaninjection/domain/Hero.java
@@ -7,4 +7,12 @@ public class Hero {
     private Sword sword;
     private Shield shield;
     private Armor armor;
+
+
+    public Hero(Sword sword, Shield shield){
+        this.shield = shield;
+        this.sword = sword;
+    }
+
+
 }
