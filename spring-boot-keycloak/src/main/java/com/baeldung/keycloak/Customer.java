73ae3cfb1ab1e1039d1656a8bb9bacc509f527ba
Michael C Good <31810784+michaelcgood@users.noreply.github.com>
2017-11-03 10:09:31 -0400

    BAEL-1238 (#2941)
    * michael.good703@gmail.com
    
    michael.good703@gmail.com
    
    * michael.good703@gmail.com
    
    michael.good703@gmail.com
    
    * michael.good703@gmail.com
    
    michael.good703@gmail.com
    
    * update
    
    * michael.good703@gmail.com
    
    Had to add @SpringBootApplication(exclude = MySQLAutoconfiguration.class)
    
    * Updated for 3.3.0.Final
    
    BAEL-1238
    
    * Update pom.xml
    
    * BAEL-1238
    
    Added new module spring-boot-keycloak and removed Keycloak code from spring-boot module
    
    * Minor changes to pom.xml
    
    * Update CustomConverterTest.java
    
    * Update StringToEmployeeConverter.java
    
    * Update GenericBigDecimalConverter.java
    
    * Update MyFeatures.java
    
    * Update .gitignore
    
    * Formatting changes
    
    * "Resolving conflicts"
    
    * Updated spring-boot to remove keycloak
    
    * Updated to see
    
    * Update
    
    * Updated
    
    * Found remnant file and deleted it

--- spring-boot-keycloak/src/main/java/com/baeldung/keycloak/Customer.java
+++ spring-boot-keycloak/src/main/java/com/baeldung/keycloak/Customer.java
@@ -17,26 +17,33 @@ public class Customer {
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getServiceRendered() {
         return serviceRendered;
     }
+
     public void setServiceRendered(String serviceRendered) {
         this.serviceRendered = serviceRendered;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
-  
+
 }
