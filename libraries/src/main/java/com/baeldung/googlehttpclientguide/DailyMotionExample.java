3db7b366622d70d0c4831c4d716dca753685fb2d
hgomez <hugo.gomez@evolutionfinance.com>
2017-11-05 18:36:44 -0500

    Removed comments from author, reformat

--- libraries/src/main/java/com/baeldung/googlehttpclientguide/DailyMotionExample.java
+++ libraries/src/main/java/com/baeldung/googlehttpclientguide/DailyMotionExample.java
@@ -1,25 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.googlehttpclientguide;
 
+import com.google.api.client.http.HttpBackOffUnsuccessfulResponseHandler;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
-import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.JsonObjectParser;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
 
-/**
- *
- * @author Hugo
- */
 public class DailyMotionExample {
 
     static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
@@ -29,15 +20,20 @@ public class DailyMotionExample {
     private static void run() throws Exception {
         HttpRequestFactory requestFactory
                 = HTTP_TRANSPORT.createRequestFactory(
-                        new HttpRequestInitializer() {
-
-                    public void initialize(HttpRequest request) {
-                        request.setParser(new JsonObjectParser(JSON_FACTORY));
-                    }
-                });
+                        (HttpRequest request) -> {
+                            request.setParser(new JsonObjectParser(JSON_FACTORY));
+                        });
         DailyMotionUrl url = new DailyMotionUrl("https://api.dailymotion.com/videos/");
         url.fields = "id,tags,title,url";
         HttpRequest request = requestFactory.buildGetRequest(url);
+        ExponentialBackOff backoff = new ExponentialBackOff.Builder()
+                .setInitialIntervalMillis(500)
+                .setMaxElapsedTimeMillis(900000)
+                .setMaxIntervalMillis(6000)
+                .setMultiplier(1.5)
+                .setRandomizationFactor(0.5)
+                .build();
+        request.setUnsuccessfulResponseHandler(new HttpBackOffUnsuccessfulResponseHandler(backoff));
         VideoFeed videoFeed = request.execute().parseAs(VideoFeed.class);
         if (videoFeed.list.isEmpty()) {
             System.out.println("No videos found.");
--- libraries/src/main/java/com/baeldung/googlehttpclientguide/DailyMotionUrl.java
+++ libraries/src/main/java/com/baeldung/googlehttpclientguide/DailyMotionUrl.java
@@ -1,22 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.googlehttpclientguide;
 
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.util.Key;
 
-/**
- *
- * @author Hugo
- */
-  public class DailyMotionUrl extends GenericUrl {
+
+public class DailyMotionUrl extends GenericUrl {
 
     public DailyMotionUrl(String encodedUrl) {
-      super(encodedUrl);
+        super(encodedUrl);
     }
 
-    @Key public String fields;
-  }
+    @Key
+    public String fields;
+}
