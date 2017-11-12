7875307ba7f652c697b934ab6db2367c63b9c7b5
felipeazv <felipe.azv@gmail.com>
2017-11-12 16:21:56 +0100

    BAEL-803: Backward Chaining with Drools - changefix

--- drools/src/main/java/com/baeldung/drools/backward_chaining/BackwardChaining.java
+++ drools/src/main/java/com/baeldung/drools/backward_chaining/BackwardChaining.java
@@ -1,9 +1,8 @@
-package com.baeldung.drools;
+package com.baeldung.drools.backward_chaining;
 
-import org.kie.api.KieServices;
-import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
+import com.baeldung.drools.config.DroolsBeanFactory;
 import com.baeldung.drools.model.Fact;
 import com.baeldung.drools.model.Result;
 
@@ -11,17 +10,17 @@ public class BackwardChaining {
     public static void main(String[] args) {
         Result result = new BackwardChaining().backwardChaining();
         System.out.println(result.getValue());
-        result.getFacts().stream().forEach(System.out::println);
+        result.getFacts()
+            .stream()
+            .forEach(System.out::println);
     }
 
     public Result backwardChaining() {
         Result result = new Result();
-        KieServices ks = KieServices.Factory.get();
-        KieContainer kContainer = ks.getKieClasspathContainer();
-        KieSession ksession = kContainer.newKieSession("ksession-backward-chaining");
+        KieSession ksession = new DroolsBeanFactory().getKieSession();
         ksession.setGlobal("result", result);
         ksession.insert(new Fact("Asia", "Planet Earth"));
-        // ksession.insert(new Fact("China", "Asia"));
+        ksession.insert(new Fact("China", "Asia"));
         ksession.insert(new Fact("Great Wall of China", "China"));
 
         ksession.fireAllRules();
