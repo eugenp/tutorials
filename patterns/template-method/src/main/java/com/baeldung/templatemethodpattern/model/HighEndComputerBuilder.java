08be3075a5366eb8c7afee239622d1e79320b3f8
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2017-11-13 17:07:05 -0300

    Update HighEndComputerBuilder.java

--- patterns/template-method/src/main/java/com/baeldung/templatemethodpattern/model/HighEndComputerBuilder.java
+++ patterns/template-method/src/main/java/com/baeldung/templatemethodpattern/model/HighEndComputerBuilder.java
@@ -12,9 +12,6 @@ public class HighEndComputerBuilder extends ComputerBuilder {
         motherboardSetupStatus.add("Screwing the high-end motherboard to the case.");
         motherboardSetupStatus.add("Pluging in the power supply connectors.");
         motherboardSetupStatus.forEach(step -> System.out.println(step));
-        moterboardSetupStatus.add("Screwing the high-end motherboard to the case.");
-        moterboardSetupStatus.add("Pluging in the power supply connectors.");
-        moterboardSetupStatus.forEach(step -> System.out.println(step));
     }
     
     @Override
