0ffd46e07c069a8b231ab3ef43f3abbb1ffcff53
gschambial <chambial.gs@gmail.com>
2017-11-13 21:01:06 +0530

    BAEL-1331: Quick Example: Comparator vs Comparable in Java Source code and Unit Tests added.

---
+++ core-java/src/test/java/com/baeldung/comparator/ComparatorUnitTest.java
@@ -0,0 +1,42 @@
+package com.baeldung.comparator;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class ComparatorUnitTest {
+    
+    @Test
+    public void whenUsingRankingComparator_thenSortedList() {
+        ArrayList<Player> footballTeam = new ArrayList<Player>();
+        Player player1 = new Player(59, "John", 20);
+        Player player2 = new Player(67, "Roger", 22);
+        Player player3 = new Player(45, "Steven", 24);
+        footballTeam.add(player1);
+        footballTeam.add(player2);
+        footballTeam.add(player3);
+        PlayerRankingComparator playerComparator = new PlayerRankingComparator();
+        Collections.sort(footballTeam, playerComparator);
+        assertEquals(footballTeam.get(0).getName(), "Steven");
+        assertEquals(footballTeam.get(2).getRanking(), 67);
+    }
+    
+    @Test
+    public void whenUsingAgeComparator_thenSortedList() {
+        ArrayList<Player> footballTeam = new ArrayList<Player>();
+        Player player1 = new Player(59, "John", 20);
+        Player player2 = new Player(67, "Roger", 22);
+        Player player3 = new Player(45, "Steven", 24);
+        footballTeam.add(player1);
+        footballTeam.add(player2);
+        footballTeam.add(player3);
+        PlayerAgeComparator playerComparator = new PlayerAgeComparator();
+        Collections.sort(footballTeam, playerComparator);
+        assertEquals(footballTeam.get(0).getName(), "John");
+        assertEquals(footballTeam.get(2).getRanking(), 45);
+    }
+
+}
