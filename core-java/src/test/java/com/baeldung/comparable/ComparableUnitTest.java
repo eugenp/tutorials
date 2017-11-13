40ceddea2bdc3cf46caa622b19ba1350bd52f055
gschambial <chambial.gs@gmail.com>
2017-11-13 21:01:06 +0530

    BAEL-1331: Quick Example: Comparator vs Comparable in Java Source code and Unit Tests added.

---
+++ core-java/src/test/java/com/baeldung/comparable/ComparableUnitTest.java
@@ -0,0 +1,26 @@
+package com.baeldung.comparable;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import org.junit.Test;
+
+public class ComparableUnitTest {
+    
+    @Test
+    public void whenUsingComparable_thenSortedList() {
+        ArrayList<Player> footballTeam = new ArrayList<Player>();
+        Player player1 = new Player(59, "John", 20);
+        Player player2 = new Player(67, "Roger", 22);
+        Player player3 = new Player(45, "Steven", 24);
+        footballTeam.add(player1);
+        footballTeam.add(player2);
+        footballTeam.add(player3);
+        Collections.sort(footballTeam);
+        assertEquals(footballTeam.get(0).getName(), "Steven");
+        assertEquals(footballTeam.get(2).getRanking(), 67);
+    }
+
+}
