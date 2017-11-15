2d00f136942d7fb9f9da10373005d2c9aebbfa25
Ahmed Tawila <atawila@yahoo.com>
2017-11-15 23:40:35 +0200

    fix compilation error

--- core-java-concurrency/src/test/java/com/baeldung/concurrent/daemon/DaemonThreadTest.java
+++ core-java-concurrency/src/test/java/com/baeldung/concurrent/daemon/DaemonThreadTest.java
@@ -3,10 +3,7 @@ package com.baeldung.concurrent.daemon;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-<<<<<<< HEAD
-=======
 import org.junit.Ignore;
->>>>>>> d54917c7e9f0f74c40982571af8ac9f61782b7cb
 import org.junit.Test;
 
 public class DaemonThreadTest {
