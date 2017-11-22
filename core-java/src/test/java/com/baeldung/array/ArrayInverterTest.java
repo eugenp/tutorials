61dc7caaad1e35a2e77a1d52e734ebe1f3b4fa54
Andre Thiago Souza da Silva (PGR) <andrethiago@mpf.mp.br>
2017-11-22 17:37:51 -0200

    BAEL-1328 Adjusting spaces and implement inversion with commons lang3 and guava.

--- core-java/src/test/java/com/baeldung/array/ArrayInverterTest.java
+++ core-java/src/test/java/com/baeldung/array/ArrayInverterTest.java
@@ -6,30 +6,43 @@ import org.junit.Test;
 
 public class ArrayInverterTest {
 
-	private String[] fruits = {"apples", "tomatoes", "bananas", "guavas", "pineapples", "oranges"};
+    private String[] fruits = { "apples", "tomatoes", "bananas", "guavas", "pineapples", "oranges" };
 
-	@Test
-	public void invertArrayWithForLoop() {
-		ArrayInverter inverter = new ArrayInverter();
-		inverter.invertUsingFor(fruits);
+    @Test
+    public void invertArrayWithForLoop() {
+        ArrayInverter inverter = new ArrayInverter();
+        inverter.invertUsingFor(fruits);
 
-		assertArrayEquals(new String[] {"oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples"}, fruits);
-	}
+        assertArrayEquals(new String[] { "oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples" }, fruits);
+    }
 
-	@Test
-	public void invertArrayWithCollectionsReverse() {
-		ArrayInverter inverter = new ArrayInverter();
-		inverter.invertUsingCollectionsReverse(fruits);
+    @Test
+    public void invertArrayWithCollectionsReverse() {
+        ArrayInverter inverter = new ArrayInverter();
+        inverter.invertUsingCollectionsReverse(fruits);
 
-		assertArrayEquals(new String[] {"oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples"}, fruits);
-	}
+        assertArrayEquals(new String[] { "oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples" }, fruits);
+    }
 
-	@Test
-	public void invertArrayWithStreams() {
-		ArrayInverter inverter = new ArrayInverter();
-		assertArrayEquals(
-			new String[] {"oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples"},
-			inverter.invertUsingStreams(fruits));
-	}
+    @Test
+    public void invertArrayWithStreams() {
+        ArrayInverter inverter = new ArrayInverter();
+        assertArrayEquals(new String[] { "oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples" }, inverter.invertUsingStreams(fruits));
+    }
+
+    @Test
+    public void invertArrayWithCommonsLang() {
+        ArrayInverter inverter = new ArrayInverter();
+        inverter.invertUsingCommonsLang(fruits);
+
+        assertArrayEquals(new String[] { "oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples" }, fruits);
+    }
+
+    @Test
+    public void invertArrayWithGuava() {
+        ArrayInverter inverter = new ArrayInverter();
+
+        assertArrayEquals(new String[] { "oranges", "pineapples", "guavas", "bananas", "tomatoes", "apples" }, inverter.invertUsingGuava(fruits));
+    }
 
 }
