7547e9e457f344e4a2f20c9999128a488fa6c4bb
Ocheja Patrick <ocheja.patrick@gmail.com>
2017-11-23 16:06:43 +0900

    Change sysout statments to slf4j log info statements

--- core-java/src/main/java/com/baeldung/polymorphism/FileManager.java
+++ core-java/src/main/java/com/baeldung/polymorphism/FileManager.java
@@ -2,25 +2,30 @@ package com.baeldung.polymorphism;
 
 import java.awt.image.BufferedImage;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FileManager {
+
+    final static Logger logger = LoggerFactory.getLogger(FileManager.class);
 
     public static void main(String[] args) {
         GenericFile file1 = new TextFile("SampleTextFile", "This is a sample text content", "v1.0.0");
-        System.out.println("File Info: \n" + file1.getFileInfo() + "\n");
+        logger.info("File Info: \n" + file1.getFileInfo() + "\n");
         ImageFile imageFile = new ImageFile("SampleImageFile", 200, 100, new BufferedImage(100, 200, BufferedImage.TYPE_INT_RGB).toString()
             .getBytes(), "v1.0.0");
-        System.out.println("File Info: \n" + imageFile.getFileInfo());
+        logger.info("File Info: \n" + imageFile.getFileInfo());
     }
 
     public static ImageFile createImageFile(String name, int height, int width, byte[] content, String version) {
         ImageFile imageFile = new ImageFile(name, height, width, content, version);
-        System.out.println("File 2 Info: \n" + imageFile.getFileInfo());
+        logger.info("File 2 Info: \n" + imageFile.getFileInfo());
         return imageFile;
     }
 
     public static GenericFile createTextFile(String name, String content, String version) {
         GenericFile file1 = new TextFile(name, content, version);
-        System.out.println("File 1 Info: \n" + file1.getFileInfo() + "\n");
+        logger.info("File 1 Info: \n" + file1.getFileInfo() + "\n");
         return file1;
     }
 
