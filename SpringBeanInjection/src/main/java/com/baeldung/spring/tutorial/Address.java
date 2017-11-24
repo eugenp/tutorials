59592ec29a47fe4c9d477b510a58f34077ae4699
zakir_rizvi <rizvi3389ict@gmail.com>
2017-11-24 07:08:29 +0600

    Add a Spring project for Bean Injection
    i) By constructor and by property is covered

---
+++ SpringBeanInjection/src/main/java/com/baeldung/spring/tutorial/Address.java
@@ -0,0 +1,18 @@
+package com.baeldung.spring.tutorial;
+
+public class Address {
+	private String street;
+	private String postcode;
+	
+	public Address(String street, String postcode) {
+		this.street = street;
+		this.postcode = postcode;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [street=" + street + ", postcode=" + postcode + "]";
+	}
+	
+	
+}
