ea5662cfcefc85da5cb21c8919c5e01fb3f0bd7d
Bogdan Stoean <bogdan.stoean@esolutions.ro>
2017-11-27 20:45:57 +0200

    cleanup

--- flyway/spring-flyway/src/main/java/com/baeldung/springflyway/SpringFlywayApplication.java
+++ flyway/spring-flyway/src/main/java/com/baeldung/springflyway/SpringFlywayApplication.java
@@ -3,7 +3,7 @@ package com.baeldung.springflyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "com.baeldung.springflyway", "db.migration" })
+@SpringBootApplication
 public class SpringFlywayApplication {
 
     public static void main(String[] args) {
--- flyway/spring-flyway/src/test/java/com/baeldung/springflyway/CustomerRepositoryNotNullConstraintMigrationTest.java
+++ flyway/spring-flyway/src/test/java/com/baeldung/springflyway/CustomerRepositoryNotNullConstraintMigrationTest.java
@@ -20,7 +20,6 @@ public class CustomerRepositoryNotNullConstraintMigrationTest {
         customerRepository.save(Customer
           .builder()
           .build());
-
     }
 
 }
