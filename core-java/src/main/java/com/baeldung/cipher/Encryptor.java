69f9298ca888f566ca59a0ecdb7dd7c13886015f
Victor Ikoro <ikoro.victor@gmail.com>
2017-11-28 05:19:20 +0100

    BAEL-1371 Modified Encryptor class, added encryptor test

--- core-java/src/main/java/com/baeldung/cipher/Encryptor.java
+++ core-java/src/main/java/com/baeldung/cipher/Encryptor.java
@@ -12,13 +12,11 @@ public class Encryptor {
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        // Encrypt message and return encrypted message
         byte[] encryptedMessage = cipher.doFinal(message);
         return encryptedMessage;
     }
 
     public byte[] encryptMessage(byte[] message, Certificate publicKeyCertificate) throws InvalidKeyException, NoSuchPaddingException, NoSuchAlgorithmException, BadPaddingException, IllegalBlockSizeException {
-
         Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
         cipher.init(Cipher.ENCRYPT_MODE, publicKeyCertificate);
         byte[] encryptedMessage = cipher.doFinal(message);
@@ -29,7 +27,6 @@ public class Encryptor {
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         SecretKey secretKey = new SecretKeySpec(keyBytes, "AES");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
-        // Encrypt message and return encrypted message
         byte[] clearMessage = cipher.doFinal(encryptedMessage);
         return clearMessage;
     }
