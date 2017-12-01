1f7572884b8fac46762ff4d0c7977510d230ce15
Jose Carvajal <josecarvajal@OEL0126.local>
2017-12-01 16:56:34 +0100

    BAEL-1174: A Quick Guide to Spring Cloud Consul: Fix Ribbon client sasd
    a

--- spring-cloud/spring-cloud-consul/src/main/java/com/baeldung/spring/cloud/consul/ribbon/RibbonClientApplication.java
+++ spring-cloud/spring-cloud-consul/src/main/java/com/baeldung/spring/cloud/consul/ribbon/RibbonClientApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +21,7 @@ public class RibbonClientApplication {
     RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
- 
+
     @Autowired
     RestTemplate restTemplate;
 
@@ -27,10 +29,15 @@ public class RibbonClientApplication {
     public String home() {
         return restTemplate.getForObject("http://myApp/ping", String.class);
     }
-    
+
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @RequestMapping("/my-health-check")
+    public ResponseEntity<String> myCustomCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public static void main(String[] args) {
