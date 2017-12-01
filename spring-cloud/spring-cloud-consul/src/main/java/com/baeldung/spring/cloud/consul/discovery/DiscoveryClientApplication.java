f3e8ca6f59d796471b0c9196a47d8ddb8152c4e3
Jose Carvajal <josecarvajal@OEL0126.local>
2017-12-01 16:56:34 +0100

    BAEL-1174: A Quick Guide to Spring Cloud Consul: Fix Ribbon client sasd
    a

--- spring-cloud/spring-cloud-consul/src/main/java/com/baeldung/spring/cloud/consul/discovery/DiscoveryClientApplication.java
+++ spring-cloud/spring-cloud-consul/src/main/java/com/baeldung/spring/cloud/consul/discovery/DiscoveryClientApplication.java
@@ -8,7 +8,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -30,10 +33,15 @@ public class DiscoveryClientApplication {
         return restTemplate().getForEntity(serviceUrl().resolve("/ping"), String.class)
             .getBody();
     }
-    
+
     @GetMapping("/ping")
     public String ping() {
         return "pong";
+    }
+
+    @RequestMapping("/my-health-check")
+    public ResponseEntity<String> myCustomCheck() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     public URI serviceUrl() {
