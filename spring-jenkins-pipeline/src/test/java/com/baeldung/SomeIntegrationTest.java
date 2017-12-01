2de410d3759064f845d8588e5b7f07b5c0a96491
Dassi Orleando <me@dassiorleando.xyz>
2017-12-01 06:31:30 +0100

    Formating

--- spring-jenkins-pipeline/src/test/java/com/baeldung/SomeIntegrationTest.java
+++ spring-jenkins-pipeline/src/test/java/com/baeldung/SomeIntegrationTest.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -19,12 +20,12 @@ public class SomeIntegrationTest {
 
     @Before
     public void setup() {
-        Student student = new Student("Paul", "Smith", "64377473774", "me@mymail.com");
+        Student student = new Student("Paul", "Smith", "64377473774", "me@mailprovider.com");
         studentRepository.save(student);
     }
 
     @Test
-    public void testDB() {
+    public void whenInserting_andCount_thenWeDontGetZero() {
         long count = studentRepository.count();
 
         assertNotEquals(0, count);
