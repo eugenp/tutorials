ad522aaa2a8b7653f5e18b3140a96b9315f8ea3a
tschiman <tschiman@gmail.com>
2017-12-09 07:40:30 -0700

    BAEL-1176 change products endpoint to books

--- spring-cloud/spring-cloud-connectors-heroku/src/main/java/com/baeldung/spring/cloud/connectors/heroku/book/BookController.java
+++ spring-cloud/spring-cloud-connectors-heroku/src/main/java/com/baeldung/spring/cloud/connectors/heroku/book/BookController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/books")
 public class BookController {
 
     private final BookService bookService;
