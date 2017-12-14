cda8e7b2eb75d047eeb6e103321a45872ebf70c3
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2017-12-14 19:10:18 -0300

    Initial Commit

---
+++ core-java-8/src/main/java/com/baeldung/defaultstaticinterfacemethods/model/Alarm.java
@@ -0,0 +1,12 @@
+package com.baeldung.defaultstaticinterfacemethods.model;
+
+public interface Alarm {
+ 
+    default String turnAlarmOn() {
+        return "Turning the alarm on.";
+    }
+    
+    default String turnAlarmOff() {
+        return "Turning the alarm off.";
+    }
+}
