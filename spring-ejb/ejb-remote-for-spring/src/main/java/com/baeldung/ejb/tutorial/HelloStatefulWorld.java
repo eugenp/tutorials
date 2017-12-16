30fc711df9d7637adc0e61260d52f4a5cd1d79b8
Allan Vital <allan.vital@gmail.com>
2017-12-16 13:03:16 -0200

    BAEL-1218: wrapping all together

--- spring-ejb/ejb-remote-for-spring/src/main/java/com/baeldung/ejb/tutorial/HelloStatefulWorld.java
+++ spring-ejb/ejb-remote-for-spring/src/main/java/com/baeldung/ejb/tutorial/HelloStatefulWorld.java
@@ -3,6 +3,9 @@ package com.baeldung.ejb.tutorial;
 import javax.ejb.Remote;
 
 @Remote
-public interface HelloWorld {
+public interface HelloStatefulWorld {
+
+    int howManyTimes();
     String getHelloWorld();
+    
 }
--- spring-ejb/spring-ejb-client/src/test/java/com/baeldung/springejbclient/SpringEjbClientApplicationTests.java
+++ spring-ejb/spring-ejb-client/src/test/java/com/baeldung/springejbclient/SpringEjbClientApplicationTests.java
@@ -1,12 +1,7 @@
 package com.baeldung.springejbclient;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SpringEjbClientApplicationTests {
 
 	@Test
