b90188b0831d32f838e1d358e8938392ef7178b7
linhvovn <tlinh2110@gmail.com>
2017-12-19 23:30:22 +0800

    [tlinh2110@gmail.com] Update test runner

--- spring-security-core/src/test/java/org/baeldung/testmethodsecurity/TestWithMockUserAtClassLevel.java
+++ spring-security-core/src/test/java/org/baeldung/testmethodsecurity/TestWithMockUserAtClassLevel.java
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration
 @WithMockUser(username="john",roles={"VIEWER"})
 public class TestWithMockUserAtClassLevel {
