79a97a0588f8f588187aa6189f565eec75d5c1cf
andrethiago <andrethiago@devel>
2017-12-20 15:16:50 -0200

    BAEL-1326 Solving conflicts and apply code formatting.

--- core-java/src/main/java/com/baeldung/array/ArrayInverter.java
+++ core-java/src/main/java/com/baeldung/array/ArrayInverter.java
@@ -11,31 +11,33 @@ import com.google.common.collect.Lists;
 
 public class ArrayInverter {
 
-	public void invertUsingFor(Object[] array) {
-		for (int i = 0; i < array.length / 2; i++) {
-			Object temp = array[i];
-			array[i] = array[array.length - 1 - i];
-			array[array.length - 1 - i] = temp;
-		}
-	}
+    public void invertUsingFor(Object[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            Object temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+        }
+    }
 
-	public void invertUsingCollectionsReverse(Object[] array) {
-		List<Object> list = Arrays.asList(array);
-		Collections.reverse(list);
-	}
+    public void invertUsingCollectionsReverse(Object[] array) {
+        List<Object> list = Arrays.asList(array);
+        Collections.reverse(list);
+    }
 
-	public Object[] invertUsingStreams(final Object[] array) {
-		return IntStream.rangeClosed(1, array.length).mapToObj(i -> array[array.length - i]).toArray();
-	}
+    public Object[] invertUsingStreams(final Object[] array) {
+        return IntStream.rangeClosed(1, array.length)
+            .mapToObj(i -> array[array.length - i])
+            .toArray();
+    }
 
-	public void invertUsingCommonsLang(Object[] array) {
-		ArrayUtils.reverse(array);
-	}
+    public void invertUsingCommonsLang(Object[] array) {
+        ArrayUtils.reverse(array);
+    }
 
-	public Object[] invertUsingGuava(Object[] array) {
-		List<Object> list = Arrays.asList(array);
-		List<Object> reverted = Lists.reverse(list);
-		return reverted.toArray();
-	}
+    public Object[] invertUsingGuava(Object[] array) {
+        List<Object> list = Arrays.asList(array);
+        List<Object> reverted = Lists.reverse(list);
+        return reverted.toArray();
+    }
 
 }
