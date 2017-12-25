9304d25875960bd4c207fc0869f266674d42906e
Tal Avissar <talaviss@gmail.com>
2017-12-26 00:57:27 +0200

    more synactic fixes

--- beantypes/src/main/java/com/baeldung/beaninjection/beantypes/Services/ShoppingCartService.java
+++ beantypes/src/main/java/com/baeldung/beaninjection/beantypes/Services/ShoppingCartService.java
@@ -1,14 +1,18 @@
 package com.baeldung.beaninjection.beantypes.Services;
 
 import com.baeldung.beaninjection.beantypes.models.Customer;
+import com.baeldung.beaninjection.beantypes.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class ShoppingCartService {
 
     private ProductService productService;
     private OrderService orderService;
+
     @Autowired
     public ShoppingCartService(ProductService productService, OrderService orderService) {
         this.productService = productService;
@@ -18,5 +22,10 @@ public class ShoppingCartService {
     public Customer GetRelatedCustomer()
     {
         return orderService.getRelatedCustomer();
+    }
+
+    public List<Product> GetRelatedProduct()
+    {
+        return productService.getProducts();
     }
 }
