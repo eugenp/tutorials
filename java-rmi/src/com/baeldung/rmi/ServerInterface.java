dc2b77e61517033dd52cd5bc395c538d16477bba
gautamshetty <gautamshetty@users.noreply.github.com>
2017-12-27 23:17:12 -0600

    Change in file name
    Change in file name

--- java-rmi/src/com/baeldung/rmi/ServerInterface.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.rmi;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerInterface extends Remote {
-	
-	public String sendMessage(String requestMessage) throws RemoteException;
-}
