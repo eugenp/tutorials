9625741aa456c1945ff4d7aa1a5b1a918854a061
gautamshetty <gautamshetty@users.noreply.github.com>
2017-12-29 21:27:58 -0600

    BAEL-1209 : Java RMI files.
    Files for BAEL-1209: Java RMI.
    
    Run the program using following commands:
    
    Server:
    java com/baeldung/rmi/MessengerImpl
    
    Client:
    java com/baeldung/rmi/RMIClientImpl

---
+++ java-rmi/test/com/baeldung/rmi/JavaRMITest.java
@@ -0,0 +1,44 @@
+package com.baeldung.rmi;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import org.junit.Test;
+
+public class JavaRMITest {
+	
+	@Test
+	public void WhenRunServer_ServerIsStarted() {
+		
+		try {
+			MessengerImpl server = new MessengerImpl();
+			server.createStubAndbind();
+			assertTrue(true);
+		} catch (RemoteException e) {
+			fail("Exception Occured");
+		}
+	}
+	
+	@Test
+	public void WhenClientSendsMessageToServer_ServerSendsResponseMessage() {
+		
+		try {
+			Registry registry = LocateRegistry.getRegistry();	
+			MessengerInterface server = (MessengerInterface) registry.lookup("Server");			
+			String responseMessage = server.sendMessage("Client Message");			
+			String expectedMessage = "Server Message";			
+			assertEquals(responseMessage, expectedMessage);
+		} catch (RemoteException e) {
+			fail("Exception Occured");
+		} catch (NotBoundException nb) {
+			fail("Exception Occured");
+		}
+	}
+	
+}
