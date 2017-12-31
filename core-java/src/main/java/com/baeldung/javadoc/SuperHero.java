5fc964e5a2a1b3f182dda7fab6d8200bb5d487c9
Jonathan Ihm <jonathan.ihm@gmail.com>
2017-12-30 21:18:43 -0600

    Updated javadoc comments to reflect article example

--- core-java/src/main/java/com/baeldung/javadoc/SuperHero.java
+++ core-java/src/main/java/com/baeldung/javadoc/SuperHero.java
@@ -16,25 +16,17 @@ public class SuperHero extends Person {
     private int defense;
 	
     /**
-     * <p>The amount of damage prior to damage mitigation.  Whether or not. . .
+     * <p>This is a simple description of the method. . .
      * <a href="http://www.supermanisthegreatest.com">Superman!</a>
      * </p>
-     * <p>
-     * Lorem ipsum dolor sit amet, consectetur. . .
-     * </p>
      * @param incomingDamage the amount of incoming damage
-     * @param damageType the value of the type of incoming damage
      * @return the amount of health hero has after attack
      * @see <a href="http://www.link_to_jira/HERO-402">HERO-402</a>
      * @since 1.0
      */
     public int successfullyAttacked(int incomingDamage, String damageType) {
-        // 2 defense points will block 1 point of damage
-        health = Math.max(0, health - (incomingDamage - defense/2)); 
-        if ("fire".equals(damageType)) {
-            health = Math.max(0, health - 5);
-        }
-        return health;
+    	// do things
+        return 0;
     }
 
 	public String getHeroName() {
