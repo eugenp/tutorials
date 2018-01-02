e8877460f8f0df15d8476dccab8d82af9f668284
Tarang Bhalodia <tarangbhalodia@gmail.com>
2018-01-02 10:26:54 +0530

    BAEL-1422: updated souts for ThreadLocalRandom example

--- core-java/src/main/java/com/baeldung/threadlocalrandom/ThreadLocalRandomExample.java
+++ core-java/src/main/java/com/baeldung/threadlocalrandom/ThreadLocalRandomExample.java
@@ -7,13 +7,13 @@ public class ThreadLocalRandomExample {
     public static void main(String[] args) {
 
         System.out.println("Random integer number: " + ThreadLocalRandom.current().nextInt());
-        System.out.println("Random integer number between 500 and 1000: " + ThreadLocalRandom.current().nextInt(0, 100));
+        System.out.println("Random integer number between 0 and 100: " + ThreadLocalRandom.current().nextInt(0, 100));
         
         System.out.println("Random long number: " + ThreadLocalRandom.current().nextLong());
-        System.out.println("Random long number between 500 and 1000: " + ThreadLocalRandom.current().nextLong(0l, 100l));
+        System.out.println("Random long number between 0 and 100: " + ThreadLocalRandom.current().nextLong(0l, 100l));
         
         System.out.println("Random double number: " + ThreadLocalRandom.current().nextDouble());
-        System.out.println("Random double number between 500 and 1000: " + ThreadLocalRandom.current().nextDouble(0D, 100D));
+        System.out.println("Random double number between 0 and 100: " + ThreadLocalRandom.current().nextDouble(0D, 100D));
         
     }
 
