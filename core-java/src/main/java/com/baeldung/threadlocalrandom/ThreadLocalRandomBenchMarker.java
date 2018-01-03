14b4e9c6fd450c63efacd27cad1173e16c5cb403
Tarang Bhalodia <tarangbhalodia@gmail.com>
2018-01-04 00:39:56 +0530

    BAEL-1422: measure performance of Random and ThreadLocalRandom using JMH

---
+++ core-java/src/main/java/com/baeldung/threadlocalrandom/ThreadLocalRandomBenchMarker.java
@@ -0,0 +1,44 @@
+package com.baeldung.threadlocalrandom;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Warmup;
+
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 2)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class ThreadLocalRandomBenchMarker {
+
+    @Benchmark
+    public void randomValuesUsingRandom() {
+        final ExecutorService executor = Executors.newWorkStealingPool();
+        Random random = new Random();
+        for (int i = 0; i < 1000; i++) {
+            executor.submit(() -> {
+                int randomValue = random.nextInt();
+            });
+        }
+        executor.shutdown();
+    }
+
+    @Benchmark
+    public void randomValuesUsingThreadLocalRandom() {
+        final ExecutorService executor = Executors.newFixedThreadPool(5);
+        for (int i = 0; i < 1000; i++) {
+            executor.submit(() -> {
+                int randomValue = ThreadLocalRandom.current()
+                    .nextInt();
+            });
+        }
+        executor.shutdown();
+    }
+
+}
