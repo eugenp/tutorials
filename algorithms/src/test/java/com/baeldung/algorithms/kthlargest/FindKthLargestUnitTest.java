fb0deef6f306d7bc7ee33463ec6df4f401ab02f9
Mansi <mansi2392@users.noreply.github.com>
2018-01-03 12:39:40 +0530

    BAEL-1297 fixed issues and modified file name

--- algorithms/src/test/java/com/baeldung/algorithms/kthlargest/FindKthLargestUnitTest.java
+++ algorithms/src/test/java/com/baeldung/algorithms/kthlargest/FindKthLargestUnitTest.java
@@ -1,9 +1,11 @@
 package com.baeldung.algorithms.kthlargest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
-public class FindKthLargestTest {
+public class FindKthLargestUnitTest {
 
     private FindKthLargest findKthLargest;
     private Integer[] arr = {3, 7, 1, 2, 8, 10, 4, 5, 6, 9};
@@ -24,20 +26,20 @@ public class FindKthLargestTest {
     public void givenIntArray_whenFindKthLargestByQuickSelect_thenGetResult() {
         int k = 3;
         int kthLargest = arr.length - k;
-        assertEquals(8, findKthLargest.findKthLargestBySorting(arr, kthLargest));
+        assertEquals(8, findKthLargest.findKthElementByQuickSelect(arr,0,arr.length-1, kthLargest));
     }
 
     @Test
     public void givenIntArray_whenFindKthSmallestByQuickSelect_thenGetResult() {
         int k = 3;
-        assertEquals(3, findKthLargest.findKthLargestBySorting(arr, k-1));
+        assertEquals(3, findKthLargest.findKthElementByQuickSelect(arr,0,arr.length-1, k-1));
     }
 
     @Test
     public void givenIntArray_whenFindKthLargestByRandomizedQuickSelect_thenGetResult() {
         int k = 3;
         int kthLargest = arr.length - k;
-        assertEquals(8, findKthLargest.findKthElementByRandomizedQuickSelect(arr, kthLargest));
+        assertEquals(8, findKthLargest.findKthElementByRandomizedQuickSelect(arr,0,arr.length-1, kthLargest));
     }
 
 }
