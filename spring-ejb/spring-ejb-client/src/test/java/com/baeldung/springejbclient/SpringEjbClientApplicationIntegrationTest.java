4c9b684e4ddc29a118f2564eafbac2c8fabc0c32
Allan Vital <cin_avital@uolinc.com>
2018-01-04 13:47:59 -0200

    BAEL-1418: removing tabs

--- spring-ejb/spring-ejb-client/src/test/java/com/baeldung/springejbclient/SpringEjbClientApplicationIntegrationTest.java
+++ spring-ejb/spring-ejb-client/src/test/java/com/baeldung/springejbclient/SpringEjbClientApplicationIntegrationTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 
 public class SpringEjbClientApplicationIntegrationTest {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
