9aab63b5810c690ce6647f74f05762709120c89a
Magdalena Krause <magkrause@gmail.com>
2018-01-04 12:40:17 -0300

    BAEL-1277: RESTFul CRUD application with JavaLite.

---
+++ java-lite/src/main/java/app/controllers/ProductsController.java
@@ -0,0 +1,86 @@
+package app.controllers;
+
+import app.models.Product;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.javalite.activeweb.AppController;
+import org.javalite.activeweb.annotations.RESTful;
+
+import java.util.Map;
+
+@RESTful
+public class ProductsController extends AppController {
+
+    public void index() {
+      view("products", Product.findAll());
+      render().contentType("application/json");
+    }
+
+    public void create() {
+      try {
+        Map payload = new ObjectMapper().readValue(getRequestString(), Map.class);
+        Product p = new Product();
+        p.fromMap(payload);
+        p.saveIt();
+        view("message", "Successfully saved product id " + p.get("id"), "code", 200);
+        render("message");
+      } catch (Exception e) {
+        view("message", "There was an error.", "code", 200);
+        render("message");
+      }
+    }
+
+    public void update() {
+      try {
+        Map payload = new ObjectMapper().readValue(getRequestString(), Map.class);
+        String id = getId();
+        Product p = Product.findById(id);
+        if (p == null) {
+          view("message", "Product id " + id + " not found.", "code", 200);
+          render("message");
+          return;
+        }
+        p.fromMap(payload);
+        p.saveIt();
+        view("message", "Successfully updated product id " + id, "code", 200);
+        render("message");
+      } catch (Exception e) {
+        view("message", "There was an error.", "code", 200);
+        render("message");
+      }
+    }
+
+    public void show() {
+      String id = getId();
+      Product p = Product.findById(id);
+      if (p == null) {
+        view("message", "Product id " + id + " not found.", "code", 200);
+        render("message");
+        return;
+      }
+      view("product", p);
+      render("_product");
+    }
+
+    public void destroy() {
+      String id = getId();
+      Product p = Product.findById(id);
+      if (p == null) {
+        view("message", "Product id " + id + " not found.", "code", 200);
+        render("message");
+        return;
+      }
+      p.delete();
+      view("message", "Successfully deleted product id " + id, "code", 200);
+      render("message");
+    }
+
+    @Override
+    protected String getContentType() {
+      return "application/json";
+    }
+
+    @Override
+    protected String getLayout() {
+      return null;
+    }
+}
---
+++ java-lite/src/main/webapp/WEB-INF/views/system/404.ftl
@@ -0,0 +1,3 @@
+<@content for="title">404 Not Found</@content>
+<h2>404 - Resource Not Found</h2>
+${message!}
