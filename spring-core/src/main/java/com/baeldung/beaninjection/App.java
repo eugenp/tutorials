b9358d0e647e659646630ad47ba71b62381fe07d
Diaz Novandi <aprian.diaz.novandi@gmail.com>
2018-01-04 06:43:22 +0100

    Remove unused import

--- spring-core/src/main/java/com/baeldung/beaninjection/App.java
+++ spring-core/src/main/java/com/baeldung/beaninjection/App.java
@@ -1,11 +1,8 @@
 package com.baeldung.beaninjection;
 
 import com.baeldung.beaninjection.service.CarPool;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 public class App {
 
