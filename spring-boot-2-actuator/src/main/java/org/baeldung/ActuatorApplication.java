f59f7b21235d9d4e963755158a32ff008200e47a
José Carlos Valero Sánchez <josecarlosvalerosanchez@gmail.com>
2018-01-07 12:14:57 +0000

    BAEL-1430: Add Spring Boot 2 actuator module

---
+++ spring-boot-2-actuator/src/main/java/org/baeldung/ActuatorApplication.java
@@ -0,0 +1,12 @@
+package org.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+}
