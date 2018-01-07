50743f20c07ec78f69398c1c6d83497e0d38f0b9
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-01-07 17:05:18 +0200

    update restdocs, move to spring-5

--- spring-5/src/main/java/com/baeldung/restdocs/CRUDController.java
+++ spring-5/src/main/java/com/baeldung/restdocs/CRUDController.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.baeldung.restdocs;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
@@ -7,10 +7,14 @@ import java.util.List;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/crud")
 public class CRUDController {
 
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
     public List<CrudInput> read(@RequestBody CrudInput crudInput) {
         List<CrudInput> returnList = new ArrayList<CrudInput>();
         returnList.add(crudInput);
@@ -27,29 +30,30 @@ public class CRUDController {
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public HttpHeaders save(@RequestBody CrudInput crudInput) {
         HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.setLocation(linkTo(CRUDController.class).slash(crudInput.getTitle()).toUri());
+        httpHeaders.setLocation(linkTo(CRUDController.class).slash(crudInput.getId()).toUri());
         return httpHeaders;
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+   @DeleteMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.OK)
-    HttpHeaders delete(@RequestBody CrudInput crudInput) {
-        HttpHeaders httpHeaders = new HttpHeaders();
-        return httpHeaders;
+    HttpHeaders delete(@PathVariable("id") long id) {
+        return new HttpHeaders();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @PutMapping(value = "/{id}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     void put(@PathVariable("id") long id, @RequestBody CrudInput crudInput) {
 
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PATCH)
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    void patch(@PathVariable("id") long id, @RequestBody CrudInput crudInput) {
-
+    @PatchMapping(value = "/{id}")
+    public List<CrudInput> patch(@PathVariable("id") long id, @RequestBody CrudInput crudInput) {
+        List<CrudInput> returnList = new ArrayList<CrudInput>();
+        crudInput.setId(id);
+        returnList.add(crudInput);
+        return returnList;
     }
 }
