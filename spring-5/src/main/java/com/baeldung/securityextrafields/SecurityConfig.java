2b78cdbafa800694f7b5314a8c89a66c8d49a0cf
Chris Oberle <chris.oberle@gmail.com>
2018-01-15 18:34:55 -0500

    remove unnecessary class

--- spring-5/src/main/java/com/baeldung/securityextrafields/SecurityConfig.java
+++ spring-5/src/main/java/com/baeldung/securityextrafields/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -51,10 +52,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         provider.setPasswordEncoder(passwordEncoder());
         return provider;
     }
-    
 
-    public CustomAuthFailureHandler failureHandler() {
-        return new CustomAuthFailureHandler();
+    public SimpleUrlAuthenticationFailureHandler failureHandler() {
+        return new SimpleUrlAuthenticationFailureHandler("/login?error=true");
     }
 
     public PasswordEncoder passwordEncoder() {
