a8f6c9f9fb36e3479546948d7aa50e766e4dd0a2
BineshNarayanan <kbinesh@gmail.com>
2018-01-18 12:22:06 +0800

    BAEL-1276 | Adding example of ActiveJDBC

---
+++ activejdbc/src/main/java/com/baeldung/model/Role.java
@@ -0,0 +1,18 @@
+package com.baeldung.model;
+
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("EMP_ROLES")
+public class Role extends Model {
+
+    public Role(){
+
+    }
+
+    public Role(String role,String createdBy){
+        set("role_name",role);
+        set("created_by",createdBy);
+    }
+}
