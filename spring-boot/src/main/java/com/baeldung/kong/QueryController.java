b5d4a8fc2659e0b95e9763b07a89b2580db33266
BQ <lovetcn@gmail.com>
2018-01-18 23:09:17 +0800

    refactor kong samples

--- spring-boot/src/main/java/com/baeldung/kong/QueryController.java
+++ spring-boot/src/main/java/com/baeldung/kong/QueryController.java
@@ -1,9 +1,9 @@
 package com.baeldung.kong;
 
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author aiet
