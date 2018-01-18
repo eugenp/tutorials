fe1105a526a2bc3326244dc00004936034e63b3f
felipeazv <felipe.azv@gmail.com>
2018-01-18 13:04:56 +0100

    change module

--- spring-5-reactive/src/main/java/com/baeldung/reactive/websocket/ReactiveWebSocketConfiguration.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/websocket/ReactiveWebSocketConfiguration.java
@@ -1,4 +1,4 @@
-package com.baeldung;
+package com.baeldung.reactive.websocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
