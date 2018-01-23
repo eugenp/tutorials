203fb775cd0e507917b0a2662333ccfebf6765d7
pratishthacc <35722494+pratishthacc@users.noreply.github.com>
2018-01-24 00:25:46 +0530

    Adding the main file for testing bean injections

---
+++ spring-core/src/main/java/com/baeldung/beaninjectiontypes/LibraryApp.java
@@ -0,0 +1,13 @@
+package com.baeldung.beaninjectiontypes;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class LibraryApp {
+    public static void main(String args[]) {
+        @SuppressWarnings("resource")
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+        Library library = (Library) context.getBean("library");
+        System.out.println(library.getBook());
+    }
+}
