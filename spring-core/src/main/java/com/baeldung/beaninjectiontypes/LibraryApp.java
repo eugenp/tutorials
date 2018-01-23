8818537b9187b0b5efe9da56501383b02b32e6e4
pratishthacc <35722494+pratishthacc@users.noreply.github.com>
2018-01-24 00:38:21 +0530

    correcting the configuration file name

--- spring-core/src/main/java/com/baeldung/beaninjectiontypes/LibraryApp.java
+++ spring-core/src/main/java/com/baeldung/beaninjectiontypes/LibraryApp.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class LibraryApp {
     public static void main(String args[]) {
         @SuppressWarnings("resource")
-        ApplicationContext context = new ClassPathXmlApplicationContext("Spring.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("setterinjection.xml");
         LibrarySetterInjection library = (LibrarySetterInjection) context.getBean("library"); //using setter injection here
         System.out.println(library.getBook());
     }
