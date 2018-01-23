17af66114269e89a5c1c9f0063b0cd5dc9640291
eelhazati <eelhazati@gmail.com>
2018-01-23 15:24:40 +0000

    Different Types of Bean Injection in Spring

---
+++ spring-core/src/main/java/com/baeldung/di/types/annotation/AppConfig.java
@@ -0,0 +1,9 @@
+package com.baeldung.di.types.annotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+}
