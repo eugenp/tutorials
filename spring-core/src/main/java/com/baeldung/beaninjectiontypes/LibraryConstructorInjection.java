2c95256ca47def40fbf2108a7e49d24ac5a9a4f8
pratishthacc <35722494+pratishthacc@users.noreply.github.com>
2018-01-24 00:27:45 +0530

    adding java class for constructor injection

---
+++ spring-core/src/main/java/com/baeldung/beaninjectiontypes/LibraryConstructorInjection.java
@@ -0,0 +1,10 @@
+package com.baeldung.beaninjectiontypes;
+
+public class LibraryConstructorInjection {
+
+    private String book;
+
+    public Library(String book) {
+        this.book = book;
+    }
+}
