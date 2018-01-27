5b8caa513c79ff2f2edf6f28f1c7b47a475fa481
Kashif Masood <kashif.masood@yahoo.com>
2018-01-27 20:04:05 +0500

    Different types of bean injection in Spring - eval - unit tests and fixes

--- spring-core/src/main/java/com/baeldung/beaninjectiontypes/ClientApplication.java
+++ spring-core/src/main/java/com/baeldung/beaninjectiontypes/ClientApplication.java
@@ -1,9 +1,11 @@
 package com.baeldung.beaninjectiontypes;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class ClientApplication {
 
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfigConstructorInjection.class);
         WeatherUpdate weatherUpdate = context.getBean("weatherUpdate", WeatherUpdate.class);
 
         System.out.println(weatherUpdate.getUpdate());
