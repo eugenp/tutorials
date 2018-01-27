81e8da29a402f0cb8fd1c362ce249271b1e94f5d
Markus Gulden <markus.gulden@msg.group>
2018-01-27 11:14:14 +0100

    BAEL-1335

---
+++ javaxval/src/main/java/org/baeldung/javaxval/methodvalidation/constraints/ConsistentDateParameterValidator.java
@@ -0,0 +1,32 @@
+package org.baeldung.javaxval.methodvalidation.constraints;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import javax.validation.constraintvalidation.SupportedValidationTarget;
+import javax.validation.constraintvalidation.ValidationTarget;
+import java.time.LocalDate;
+
+@SupportedValidationTarget(ValidationTarget.PARAMETERS)
+public class ConsistentDateParameterValidator implements ConstraintValidator<ConsistentDateParameters, Object[]> {
+
+    @Override
+    public void initialize(ConsistentDateParameters constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(Object[] value, ConstraintValidatorContext context) {
+        if (value.length != 4 && value.length != 3) {
+            throw new IllegalArgumentException("Illegal method signature");
+        }
+
+        if (value[0] == null || value[1] == null) {
+            return false;
+        }
+
+        if (!(value[0] instanceof LocalDate) || !(value[1] instanceof LocalDate)) {
+            throw new IllegalArgumentException("Illegal method signature, expected two parameters of type LocalDate.");
+        }
+
+        return ((LocalDate) value[0]).isBefore((LocalDate) value[1]);
+    }
+}
