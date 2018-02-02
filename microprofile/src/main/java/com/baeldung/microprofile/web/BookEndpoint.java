bd879c18faf50ddd1628aaabf6816ffd7feb83ea
eelhazati <eelhazati@gmail.com>
2018-02-02 12:12:25 +0000

    change path value

--- microprofile/src/main/java/com/baeldung/microprofile/web/BookEndpoint.java
+++ microprofile/src/main/java/com/baeldung/microprofile/web/BookEndpoint.java
@@ -10,7 +10,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@Path("/books")
+@Path("books")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class BookEndpoint {
