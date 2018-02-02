14d308b6828270a38e1856e5f6aa3bdb11a5d47b
eelhazati <eelhazati@gmail.com>
2018-02-02 12:34:17 +0000

    clean

--- microprofile/src/main/java/com/baeldung/microprofile/repo/BookManager.java
+++ microprofile/src/main/java/com/baeldung/microprofile/repo/BookManager.java
@@ -2,44 +2,53 @@ package com.baeldung.microprofile.repo;
 
 import com.baeldung.microprofile.model.Book;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class BookManager {
 
-    private AtomicInteger bookId = new AtomicInteger(0);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+    private AtomicInteger bookIdGenerator = new AtomicInteger(0);
 
-    private ConcurrentMap<String, Book> inMororyDb = new ConcurrentHashMap<>();
+    private ConcurrentMap<String, Book> inMemoryStore = new ConcurrentHashMap<>();
+
+    @PostConstruct
+    public void init() {
+        Book book = new Book();
+        book.setId(getNextId());
+        book.setName("Building Microservice With Eclipse MicroProfile");
+        book.setIsbn("1");
+        book.setAuthor("baeldung");
+        book.setNbrePages(420);
+    }
+
+    private String getNextId() {
+        String date = LocalDate.now().format(formatter);
+        return String.format("%04d-%s", bookIdGenerator.incrementAndGet(), date);
+    }
 
     public String add(Book book) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
-        String date = LocalDate.now().format(formatter);
-        String id = String.format("%03d-%s", bookId.incrementAndGet(),date);
+        String id = getNextId();
         book.setId(id);
-        inMororyDb.putIfAbsent(id, book);
+        inMemoryStore.put(id, book);
         return id;
     }
 
     public Book get(String id) {
-        return inMororyDb.get(id);
+        return inMemoryStore.get(id);
     }
 
     public List<Book> list() {
-        return inMororyDb.values().stream().collect(Collectors.toList());
-    }
-
-    public void deleteAll() {
-        Iterator<String> it = inMororyDb.keySet().iterator();
-        while (it.hasNext()) {
-            inMororyDb.remove(it);
-        }
+        List<Book> books = new ArrayList<>();
+        inMemoryStore.values().forEach(book -> books.add(book));
+        return books;
     }
 }
