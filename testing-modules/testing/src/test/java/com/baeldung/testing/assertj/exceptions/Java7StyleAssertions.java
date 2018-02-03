4344479d7a724932efb897b279d040d3f157abb3
orrym <orrym@vatit.com>
2018-02-03 13:06:31 +0200

    BAEL-1517: Added Java7 style assertions

---
+++ testing-modules/testing/src/test/java/com/baeldung/testing/assertj/exceptions/Java7StyleAssertions.java
@@ -0,0 +1,23 @@
+package com.baeldung.testing.assertj.exceptions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
+
+import org.junit.Test;
+
+public class Java7StyleAssertions {
+
+    @Test
+    public void whenDividingByZero_thenArithmeticException() {
+        try {
+            int numerator = 10;
+            int denominator = 0;
+            int quotient = numerator / denominator;
+            fail("ArithmeticException expected because dividing by zero yields an ArithmeticException.");
+            failBecauseExceptionWasNotThrown(IndexOutOfBoundsException.class);
+        } catch (ArithmeticException e) {
+            assertThat(e).hasMessage("/ by zero");
+        }
+    }
+}
