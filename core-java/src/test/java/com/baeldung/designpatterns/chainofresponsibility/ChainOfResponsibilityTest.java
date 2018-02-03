3dad15b26cc595a33b4bf524baa23bfb4140391d
Tarang Bhalodia <tarangbhalodia@gmail.com>
2018-02-03 10:37:13 +0530

    BAEL-1524: added BDD style jUnit test to test unknown handler in ChainOfResponsibility design pattern

--- core-java/src/test/java/com/baeldung/designpatterns/chainofresponsibility/ChainOfResponsibilityTest.java
+++ core-java/src/test/java/com/baeldung/designpatterns/chainofresponsibility/ChainOfResponsibilityTest.java
@@ -23,25 +23,34 @@ public class ChainOfResponsibilityTest {
 
         String response = loggerChain.logMessage(AbstractLogger.DEBUG, "This is an debug level information.");
         assertTrue(response.startsWith("DEBUG"));
-        
+
     }
-    
+
     @Test
     public void givenMessage_whenLogLevelInfo_thenMessageStartsWithInfoKeyword() {
         AbstractLogger loggerChain = getChainOfLoggers();
 
         String response = loggerChain.logMessage(AbstractLogger.INFO, "This is an info level information.");
         assertTrue(response.startsWith("INFO"));
-        
+
     }
-    
+
     @Test
     public void givenMessage_whenLogLevelError_thenMessageStartsWithErrorKeyword() {
         AbstractLogger loggerChain = getChainOfLoggers();
 
         String response = loggerChain.logMessage(AbstractLogger.ERROR, "This is an error level information.");
         assertTrue(response.startsWith("ERROR"));
-        
+
+    }
+
+    @Test
+    public void givenMessage_whenLogLevelUnknown_thenMessageStartsWithConsoleKeyword() {
+        AbstractLogger loggerChain = getChainOfLoggers();
+
+        String response = loggerChain.logMessage(4, "This is an unknown level information.");
+        assertTrue(response.startsWith("CONSOLE"));
+
     }
 
 }
