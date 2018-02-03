337a0d9e744b670c08fe60a280877253938a8b74
skosoy <skosoy@gmail.com>
2018-02-03 14:28:49 +0300

    Supplementary JUnit code for http://inprogress.baeldung.com/?p=72199

--- spring-core/src/main/java/com/baeldung/injection/InjectionsApp.java
+++ spring-core/src/main/java/com/baeldung/injection/InjectionsApp.java
@@ -1,24 +1,27 @@
 package com.baeldung.injection;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-import com.baeldung.value.SomeBean;
-
-@Configuration
+@Configuration("InjectionsApp")
 @ComponentScan({ "com.baeldung.injection" })
-public class App {
-    @Autowired
-    private SomeBean someBean;
+public class InjectionsApp {
 
-    @Bean
+    @Bean(name = "preferences", autowire = Autowire.BY_NAME)
     @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public UserPreferences getUserPreferences() {
         final UserPreferences preferences = new UserPreferences();
         return preferences;
     }
+
+    @Bean(name = "constructorPreferences", autowire = Autowire.BY_NAME)
+    public UserPreferences getUserConstructor() {
+        final UserPreferences preferences = new UserPreferences("test");
+        return preferences;
+    }
+
 }
--- spring-core/src/main/java/com/baeldung/injection/UserPreferences.java
+++ spring-core/src/main/java/com/baeldung/injection/UserPreferences.java
@@ -1,5 +1,17 @@
 package com.baeldung.injection;
 
 public class UserPreferences {
+    private String name;
+
+    public UserPreferences() {
+    }
+
+    public UserPreferences(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 }
