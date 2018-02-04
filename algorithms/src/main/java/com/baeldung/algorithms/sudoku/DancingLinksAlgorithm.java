fb427ce68051bf011a8a4d9e44271f35c06dc792
iaforek <arkadiusz@jaworski.pl>
2018-02-04 11:02:54 +0000

    Extracted 'constraints' methods and renamed coverBoard variable

--- algorithms/src/main/java/com/baeldung/algorithms/sudoku/DancingLinksAlgorithm.java
+++ algorithms/src/main/java/com/baeldung/algorithms/sudoku/DancingLinksAlgorithm.java
@@ -39,70 +39,83 @@ public class DancingLinksAlgorithm {
     }
 
     private boolean[][] createExactCoverBoard() {
-        boolean[][] R = new boolean[BOARD_SIZE * BOARD_SIZE * MAX_VALUE][BOARD_SIZE * BOARD_SIZE * CONSTRAINTS];
+        boolean[][] coverBoard = new boolean[BOARD_SIZE * BOARD_SIZE * MAX_VALUE][BOARD_SIZE * BOARD_SIZE * CONSTRAINTS];
 
         int hBase = 0;
-
-        // Cell constraint.
-        for (int r = COVER_START_INDEX; r <= BOARD_SIZE; r++) {
-            for (int c = COVER_START_INDEX; c <= BOARD_SIZE; c++, hBase++) {
-                for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++) {
-                    int index = getIndex(r, c, n);
-                    R[index][hBase] = true;
-                }
-            }
-        }
-
-        // Row constrain.
-        for (int r = COVER_START_INDEX; r <= BOARD_SIZE; r++) {
-            for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++, hBase++) {
-                for (int c1 = COVER_START_INDEX; c1 <= BOARD_SIZE; c1++) {
-                    int index = getIndex(r, c1, n);
-                    R[index][hBase] = true;
-                }
-            }
-        }
-
-        // Column constraint.
-        for (int c = COVER_START_INDEX; c <= BOARD_SIZE; c++) {
-            for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++, hBase++) {
-                for (int r1 = COVER_START_INDEX; r1 <= BOARD_SIZE; r1++) {
-                    int index = getIndex(r1, c, n);
-                    R[index][hBase] = true;
-                }
-            }
-        }
+        hBase = checkCellConstraint(coverBoard, hBase);
+        hBase = checkRowConstraint(coverBoard, hBase);
+        hBase = checkColumnConstraint(coverBoard, hBase);
+        checkSubsectionConstraint(coverBoard, hBase);
         
-        // Subsection constraint
+        return coverBoard;
+    }
+
+    private int checkSubsectionConstraint(boolean[][] coverBoard, int hBase) {
         for (int br = COVER_START_INDEX; br <= BOARD_SIZE; br += SUBSECTION_SIZE) {
             for (int bc = COVER_START_INDEX; bc <= BOARD_SIZE; bc += SUBSECTION_SIZE) {
                 for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++, hBase++) {
                     for (int rDelta = 0; rDelta < SUBSECTION_SIZE; rDelta++) {
                         for (int cDelta = 0; cDelta < SUBSECTION_SIZE; cDelta++) {
                             int index = getIndex(br + rDelta, bc + cDelta, n);
-                            R[index][hBase] = true;
+                            coverBoard[index][hBase] = true;
                         }
                     }
                 }
             }
         }
-        return R;
+        return hBase;
+    }
+
+    private int checkColumnConstraint(boolean[][] coverBoard, int hBase) {
+        for (int c = COVER_START_INDEX; c <= BOARD_SIZE; c++) {
+            for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++, hBase++) {
+                for (int r1 = COVER_START_INDEX; r1 <= BOARD_SIZE; r1++) {
+                    int index = getIndex(r1, c, n);
+                    coverBoard[index][hBase] = true;
+                }
+            }
+        }
+        return hBase;
+    }
+
+    private int checkRowConstraint(boolean[][] coverBoard, int hBase) {
+        for (int r = COVER_START_INDEX; r <= BOARD_SIZE; r++) {
+            for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++, hBase++) {
+                for (int c1 = COVER_START_INDEX; c1 <= BOARD_SIZE; c1++) {
+                    int index = getIndex(r, c1, n);
+                    coverBoard[index][hBase] = true;
+                }
+            }
+        }
+        return hBase;
+    }
+
+    private int checkCellConstraint(boolean[][] coverBoard, int hBase) {
+        for (int r = COVER_START_INDEX; r <= BOARD_SIZE; r++) {
+            for (int c = COVER_START_INDEX; c <= BOARD_SIZE; c++, hBase++) {
+                for (int n = COVER_START_INDEX; n <= BOARD_SIZE; n++) {
+                    int index = getIndex(r, c, n);
+                    coverBoard[index][hBase] = true;
+                }
+            }
+        }
+        return hBase;
     }
 
     private boolean[][] initializeExactCoverBoard(int[][] board) {
-        boolean[][] R = createExactCoverBoard();
+        boolean[][] coverBoard = createExactCoverBoard();
         for (int i = COVER_START_INDEX; i <= BOARD_SIZE; i++) {
             for (int j = COVER_START_INDEX; j <= BOARD_SIZE; j++) {
                 int n = board[i - 1][j - 1];
                 if (n != NO_VALUE) {
                     for (int num = MIN_VALUE; num <= MAX_VALUE; num++) {
                         if (num != n) {
-                            Arrays.fill(R[getIndex(i, j, num)], false);
+                            Arrays.fill(coverBoard[getIndex(i, j, num)], false);
                         }
                     }
                 }
             }
         }
-        return R;
+        return coverBoard;
     }
 }
