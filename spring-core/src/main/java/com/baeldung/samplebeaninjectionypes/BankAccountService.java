91dedcc9cb07ac0c74de50c44c557744c10b449e
Raksha Rao <raksharao@Rakshas-MacBook-Air.local>
2018-02-05 16:36:21 +0530

    remove unnecessary sample files

--- spring-core/src/main/java/com/baeldung/samplebeaninjectionypes/BankAccountService.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.samplebeaninjectionypes;
-
-public class BankAccountService {
-
-        public AccountDetails openAccount(Long accountNumber, String accountType, String owner) {
-                AccountDetails accountDetails = new AccountDetails(accountNumber,accountType,owner);
-                return accountDetails;
-        }
-}
