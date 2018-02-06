52825384c3095e0738df55ef58d5007378c47756
Donato Rimenti <donatohan.rimenti@gmail.com>
2018-02-06 15:30:08 +0100

    Added flyweight pattern example.

---
+++ core-java/src/main/java/com/baeldung/designpatterns/flyweight/VehicleFactory.java
@@ -0,0 +1,50 @@
+package com.baeldung.designpatterns.flyweight;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Factory which implements the Flyweight pattern to return an existing vehicle
+ * if present or a new one otherwise.
+ * 
+ * @author Donato Rimenti
+ */
+public class VehicleFactory {
+
+	/**
+	 * Stores the already created vehicles.
+	 */
+	private static Map<Color, Vehicle> vehiclesCache = new HashMap<Color, Vehicle>();
+
+	/**
+	 * Private constructor to prevent this class instantiation.
+	 */
+	private VehicleFactory() {
+	}
+
+	/**
+	 * Returns a vehicle of the same color passed as argument. If that vehicle
+	 * was already created by this factory, that vehicle is returned, otherwise
+	 * a new one is created and returned.
+	 * 
+	 * @param color
+	 *            the color of the vehicle to return
+	 * @return a vehicle of the specified color
+	 */
+	public static Vehicle createVehicle(Color color) {
+		// Looks for the requested vehicle into the cache.
+		Vehicle newVehicle = vehiclesCache.get(color);
+
+		// If the vehicle doesn't exist inside the cache, creates a new one.
+		if (newVehicle == null) {
+			// Creates the new car.
+			Engine newEngine = new Engine();
+			newVehicle = new Car(newEngine, color);
+
+			// Puts the new car into cache before returning it.
+			vehiclesCache.put(color, newVehicle);
+		}
+		return newVehicle;
+	}
+}
