af0caebda358cef820c7635e92e3d3f9f802a8b4
Ganesh Pagade <ganesh.pagade@tesco.com>
2018-02-06 15:24:09 +0530

    handling exception generated during terminal of normal flow

--- core-java-concurrency/src/main/java/com/baeldung/concurrent/prioritytaskexecution/PriorityJobScheduler.java
+++ core-java-concurrency/src/main/java/com/baeldung/concurrent/prioritytaskexecution/PriorityJobScheduler.java
@@ -23,10 +23,10 @@ public class PriorityJobScheduler {
                 try {
                     priorityJobPoolExecutor.execute(priorityQueue.take());
                 } catch (InterruptedException e) {
-                    if (priorityQueue.size() == 0) {
-                        break;
+                    if (priorityQueue.size() > 0) {
+                        e.printStackTrace();
                     }
-                    e.printStackTrace();
+                    break;
                 }
             }
         });
