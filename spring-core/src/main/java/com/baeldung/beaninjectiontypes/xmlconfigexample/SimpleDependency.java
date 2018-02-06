c9006942c2716f542e275b0bbd1c4c60bc37d6bd
orrym <orrym@vatit.com>
2018-02-06 19:54:52 +0200

    Revert "Add XML, JavaConfig and Autowired examples."
    This reverts commit 8f4df6b903866dac1725832d06ee7382fc89d0ce.

--- spring-core/src/main/java/com/baeldung/beaninjectiontypes/xmlconfigexample/SimpleDependency.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.beaninjectiontypes.xmlconfigexample;
-
-public class SimpleDependency {
-
-    public void doSomethingElse() {
-        System.out.println("I'm a simple dependency! I'm doing something (else)!");
-    }
-
-}
