c9bf88fcd58773d3df34e2a6cb4db44c3b54777d
Raghav Jha <raghav.jha1901@gmail.com>
2018-02-07 00:06:55 +0530

    CR comments resolved

--- spring-core/src/test/java/com/baeldung/spring/di/DepdencyInjectionUnitTest.java
+++ spring-core/src/test/java/com/baeldung/spring/di/DepdencyInjectionUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -16,7 +17,7 @@ public class DepdencyInjectionUnitTest {
 
     @Before
     public  void oneTimeSetup() {
-        context = new ClassPathXmlApplicationContext("spring-di.xml");
+        context = new AnnotationConfigApplicationContext(DependencyInjectionConfig.class);
     }
 
     @Test
