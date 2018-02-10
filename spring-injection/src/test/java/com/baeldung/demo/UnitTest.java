b9ad1188d513546f311dec5ff890b16274228139
Dennis Chen <dennischen@Denniss-MacBook-Pro.local>
2018-02-10 17:39:36 -0500

    Switch it to use parent-boot-5 as parent, make it java -jar executable

---
+++ spring-injection/src/test/java/com/baeldung/demo/UnitTest.java
@@ -0,0 +1,15 @@
+package com.baeldung.demo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UnitTest {
+
+    @Test
+    public void test() {
+//        fail("Not yet implemented");
+        assertTrue(true);
+    }
+
+}
