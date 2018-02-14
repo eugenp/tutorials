bca4b6b7e247d1da5965c06c070e98124fc597c1
Jose <jose.carvajal@piksel.com>
2018-02-14 12:57:14 +0100

    Fix tests

--- core-java-concurrency/src/test/java/com/baeldung/concurrent/phaser/PhaserUnitTest.java
+++ core-java-concurrency/src/test/java/com/baeldung/concurrent/phaser/PhaserUnitTest.java
@@ -14,27 +14,21 @@ public class PhaserUnitTest {
     public void givenPhaser_whenCoordinateWorksBetweenThreads_thenShouldCoordinateBetweenMultiplePhases() {
         // given
         ExecutorService executorService = Executors.newCachedThreadPool();
-        System.out.println("1");
         Phaser ph = new Phaser(1);
         assertEquals(0, ph.getPhase());
-        System.out.println("2");
         // when
         executorService.submit(new LongRunningAction("thread-1", ph));
         executorService.submit(new LongRunningAction("thread-2", ph));
         executorService.submit(new LongRunningAction("thread-3", ph));
-        System.out.println("3");
         // then
         ph.arriveAndAwaitAdvance();
         assertEquals(1, ph.getPhase());
-        System.out.println("4");
         // and
         executorService.submit(new LongRunningAction("thread-4", ph));
         executorService.submit(new LongRunningAction("thread-5", ph));
         ph.arriveAndAwaitAdvance();
         assertEquals(2, ph.getPhase());
-        System.out.println("5");
 
         ph.arriveAndDeregister();
-        System.out.println("6");
     }
 }
