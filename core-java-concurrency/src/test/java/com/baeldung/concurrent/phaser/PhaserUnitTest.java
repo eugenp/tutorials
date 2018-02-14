4435c91fa12810185a3d952b511a468bb2284a15
Jose <jose.carvajal@piksel.com>
2018-02-14 12:57:10 +0100

    Fix tests

--- core-java-concurrency/src/test/java/com/baeldung/concurrent/phaser/PhaserUnitTest.java
+++ core-java-concurrency/src/test/java/com/baeldung/concurrent/phaser/PhaserUnitTest.java
@@ -1,41 +1,40 @@
 package com.baeldung.concurrent.phaser;
 
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
+import static junit.framework.TestCase.assertEquals;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Phaser;
 
-import static junit.framework.TestCase.assertEquals;
+import org.junit.Test;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PhaserUnitTest {
 
     @Test
     public void givenPhaser_whenCoordinateWorksBetweenThreads_thenShouldCoordinateBetweenMultiplePhases() {
-        //given
+        // given
         ExecutorService executorService = Executors.newCachedThreadPool();
+        System.out.println("1");
         Phaser ph = new Phaser(1);
         assertEquals(0, ph.getPhase());
-
-        //when
+        System.out.println("2");
+        // when
         executorService.submit(new LongRunningAction("thread-1", ph));
         executorService.submit(new LongRunningAction("thread-2", ph));
         executorService.submit(new LongRunningAction("thread-3", ph));
-
-        //then
+        System.out.println("3");
+        // then
         ph.arriveAndAwaitAdvance();
         assertEquals(1, ph.getPhase());
-
-        //and
+        System.out.println("4");
+        // and
         executorService.submit(new LongRunningAction("thread-4", ph));
         executorService.submit(new LongRunningAction("thread-5", ph));
         ph.arriveAndAwaitAdvance();
         assertEquals(2, ph.getPhase());
-
+        System.out.println("5");
 
         ph.arriveAndDeregister();
+        System.out.println("6");
     }
 }
