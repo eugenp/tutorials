e29cb3590f0ea9e18c56fe226e572b26c240642c
Stephen Braimah <yiriwayah@gmail.com>
2018-02-15 19:24:43 +0000

    Reformatting Code #BAEL-1435

--- akka-actors/src/test/java/com/baeldung/akkaactors/RunAkkaActorTest.java
+++ akka-actors/src/test/java/com/baeldung/akkaactors/RunAkkaActorTest.java
@@ -1,19 +1,15 @@
 package com.baeldung.akkaactors;
 
-import com.baeldung.akkaactors.LeadActor;
-import com.baeldung.akkaactors.Task;
+import org.junit.Test;
 
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
-import org.junit.Test;
-
-
 
 public class RunAkkaActorTest {
     private final ActorSystem system = ActorSystem.create("test-actor-system");
 
     @Test
-    public void givenTask_whenMessageIsSent_shouldReturnProperResponse(){
+    public void givenTask_whenMessageIsSent_shouldReturnProperResponse() {
         Task task = new Task("Buy Cake");
         ActorRef supervisor = system.actorOf(LeadActor.props(), "actor-supervisor");
 
