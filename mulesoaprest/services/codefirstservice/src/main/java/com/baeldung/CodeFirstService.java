f0a021b742c821021186f07ac562059a149744cf
abirkhan04 <abir1sumon@gmail.com>
2018-02-16 04:26:42 +0600

    cleaning Master

--- mulesoaprest/services/codefirstservice/src/main/java/com/baeldung/CodeFirstService.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung;
-
-import javax.jws.WebService;
-
-@WebService
-public interface CodeFirstService {
-	String showPersonDetail(String text, Integer age);
-}
--- mulesoaprest/services/wsdlfirstservice/src/main/java/com/baeldung/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://baeldung.com/")
-package com.baeldung;
