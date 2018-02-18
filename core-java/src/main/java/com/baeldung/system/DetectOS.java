35fb7777b20f595e5711cfe6d43c3aa044e0310b
shouvikbhattacharya <shouvikbhattacharya@outlook.com>
2018-02-18 20:06:10 +0530

    BAEL-1534: Changes incorporated.

--- core-java/src/main/java/com/baeldung/system/DetectOS.java
+++ core-java/src/main/java/com/baeldung/system/DetectOS.java
@@ -6,45 +6,25 @@ import org.apache.commons.lang3.SystemUtils;
 
 public class DetectOS {
 
-    private static final DetectOS INSTANCE = new DetectOS();
-    private static OperatingSystem detectedOS;
-
-    private DetectOS() {
-
+    public String getOperatingSystem() {
+        String os = System.getProperty("os.name", "generic")
+            .toLowerCase(Locale.ENGLISH);
+        if (os.contains("mac"))
+            return "Mac";
+        else if (os.contains("win"))
+            return "Windows";
+        else if (os.contains("nux"))
+            return "Linux";
+        return "Generic";
     }
 
-    public enum OperatingSystem {
-        Windows, MacOS, Linux, Other
-    }
-
-    public static DetectOS getInstance() {
-        return INSTANCE;
-    }
-
-    public OperatingSystem getOperatingSystem() {
-        if (detectedOS == null) {
-            String OS = System.getProperty("os.name", "generic")
-                .toLowerCase(Locale.ENGLISH);
-            if ((OS.indexOf("mac") >= 0)) {
-                detectedOS = OperatingSystem.MacOS;
-            } else if (OS.indexOf("win") >= 0) {
-                detectedOS = OperatingSystem.Windows;
-            } else if (OS.indexOf("nux") >= 0) {
-                detectedOS = OperatingSystem.Linux;
-            } else {
-                detectedOS = OperatingSystem.Other;
-            }
-        }
-        return detectedOS;
-    }
-
-    public OperatingSystem getOperatingSystemSystemUtils() {
+    public String getOperatingSystemSystemUtils() {
         if (SystemUtils.IS_OS_WINDOWS)
-            return OperatingSystem.Windows;
+            return "Windows";
         else if (SystemUtils.IS_OS_LINUX)
-            return OperatingSystem.Linux;
+            return "Linux";
         else if (SystemUtils.IS_OS_MAC)
-            return OperatingSystem.MacOS;
-        return OperatingSystem.Other;
+            return "Mac";
+        return "Generic";
     }
 }
