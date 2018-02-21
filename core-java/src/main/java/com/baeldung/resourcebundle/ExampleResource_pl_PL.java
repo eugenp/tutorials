76c95a5952e86b534ed07294df08bf6f1a62728f
pkucharz <kucharz.patryk@gmail.com>
2018-02-21 20:06:22 +0100

    [BAEL-1536] A Guide to the Resource Bundle #4
    Essential changes after the second review:
    - Doubles to BigDecimals

--- core-java/src/main/java/com/baeldung/resourcebundle/ExampleResource_pl_PL.java
+++ core-java/src/main/java/com/baeldung/resourcebundle/ExampleResource_pl_PL.java
@@ -1,5 +1,6 @@
 package com.baeldung.resourcebundle;
 
+import java.math.BigDecimal;
 import java.util.ListResourceBundle;
 
 public class ExampleResource_pl_PL extends ListResourceBundle {
@@ -8,7 +9,7 @@ public class ExampleResource_pl_PL extends ListResourceBundle {
     protected Object[][] getContents() {
         return new Object[][] { 
             { "currency", "polish zloty" }, 
-            { "toUsdRate", new Double("3.401") }, 
+            { "toUsdRate", new BigDecimal("3.401") },
             { "cities", new String[] { "Warsaw", "Cracow" } } 
         };
     }
--- core-java/src/test/java/com/baeldung/resourcebundle/ExampleResourceUnitTest.java
+++ core-java/src/test/java/com/baeldung/resourcebundle/ExampleResourceUnitTest.java
@@ -2,6 +2,7 @@ package com.baeldung.resourcebundle;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.ResourceBundle;
@@ -19,7 +20,7 @@ public class ExampleResourceUnitTest {
         assertTrue(exampleBundle.keySet()
             .containsAll(Arrays.asList("toUsdRate", "cities", "greeting", "currency", "language")));
         assertEquals(exampleBundle.getString("greeting"), "cześć");
-        assertEquals(exampleBundle.getObject("toUsdRate"), 3.401);
+        assertEquals(exampleBundle.getObject("toUsdRate"), new BigDecimal("3.401"));
         assertArrayEquals(exampleBundle.getStringArray("cities"), new String[] { "Warsaw", "Cracow" });
     }
 
