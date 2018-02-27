7c1523b61be48b192db11a3debf4a1241a090573
aagarwal <Qualys$303>
2018-02-27 20:45:06 +0530

    Updated as per Suggestions from Josh, for BAEL-1584

--- core-java-8/src/main/java/com/baeldung/findanelement/FindElementInAList.java
+++ core-java-8/src/main/java/com/baeldung/findanelement/FindElementInAList.java
@@ -2,23 +2,22 @@ package com.baeldung.findanelement;
 
 import java.util.List;
 import java.util.ListIterator;
-import java.util.Optional;
 import org.apache.commons.collections4.IterableUtils;
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
 
 public class FindElementInAList<T> {
 
-    public T findUsingIndexOf(T element, List<T> listOfIntegers) {
-        int index = listOfIntegers.indexOf(element);
+    public T findUsingIndexOf(T element, List<T> list) {
+        int index = list.indexOf(element);
         if (index >= 0) {
             return element;
         }
         return null;
     }
 
-    public T findUsingListIterator(T element, List<T> listOfIntegers) {
-        ListIterator<T> listIterator = listOfIntegers.listIterator();
+    public T findUsingListIterator(T element, List<T> list) {
+        ListIterator<T> listIterator = list.listIterator();
         while (listIterator.hasNext()) {
             T elementFromList = listIterator.next();
             if (elementFromList.equals(element)) {
@@ -28,8 +27,8 @@ public class FindElementInAList<T> {
         return null;
     }
 
-    public T findUsingEnhancedForLoop(T element, List<T> listOfIntegers) {
-        for (T elementFromList : listOfIntegers) {
+    public T findUsingEnhancedForLoop(T element, List<T> list) {
+        for (T elementFromList : list) {
             if (element.equals(elementFromList)) {
                 return elementFromList;
             }
@@ -37,22 +36,22 @@ public class FindElementInAList<T> {
         return null;
     }
 
-    public T findUsingStream(T element, List<T> listOfIntegers) {
-        Optional<T> foundElement = listOfIntegers.stream()
+    public T findUsingStream(T element, List<T> list) {
+        return list.stream()
             .filter(integer -> integer.equals(element))
-            .findFirst();
-        return foundElement.isPresent() ? foundElement.get() : null;
+            .findFirst()
+            .orElse(null);
     }
 
-    public T findUsingParallelStream(T element, List<T> listOfIntegers) {
-        Optional<T> foundElement = listOfIntegers.parallelStream()
+    public T findUsingParallelStream(T element, List<T> list) {
+        return list.parallelStream()
             .filter(integer -> integer.equals(element))
-            .findAny();
-        return foundElement.isPresent() ? foundElement.get() : null;
+            .findAny()
+            .orElse(null);
     }
 
-    public T findUsingGuava(T element, List<T> listOfIntegers) {
-        T foundElement = Iterables.tryFind(listOfIntegers, new Predicate<T>() {
+    public T findUsingGuava(T element, List<T> list) {
+        T foundElement = Iterables.tryFind(list, new Predicate<T>() {
             public boolean apply(T input) {
                 return element.equals(input);
             }
@@ -60,8 +59,8 @@ public class FindElementInAList<T> {
         return foundElement;
     }
 
-    public T findUsingApacheCommon(T element, List<T> listOfIntegers) {
-        T foundElement = IterableUtils.find(listOfIntegers, new org.apache.commons.collections4.Predicate<T>() {
+    public T findUsingApacheCommon(T element, List<T> list) {
+        T foundElement = IterableUtils.find(list, new org.apache.commons.collections4.Predicate<T>() {
             public boolean evaluate(T input) {
                 return element.equals(input);
             }
