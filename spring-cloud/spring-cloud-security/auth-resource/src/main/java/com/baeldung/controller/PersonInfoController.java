1abc6ed825dda82263f9fe6c6a3e53a1286b14b4
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2018-02-28 20:08:13 +0200

    Update PersonInfoController.java

--- spring-cloud/spring-cloud-security/auth-resource/src/main/java/com/baeldung/controller/PersonInfoController.java
+++ spring-cloud/spring-cloud-security/auth-resource/src/main/java/com/baeldung/controller/PersonInfoController.java
@@ -14,6 +14,5 @@ public class PersonInfoController {
     @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
     public @ResponseBody Person personInfo() {
         return new Person("abir", "Dhaka", "Bangladesh", 29, "Male");
-    }
-    
+    }   
 }
