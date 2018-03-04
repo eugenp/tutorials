fedd2813cb3c998018e19134bd4bec274cc1feea
Jose <jose.carvajal@piksel.com>
2018-03-04 07:51:19 +0100

    BAEL-1108

---
+++ apache-curator/src/test/java/com/baeldung/apache/curator/connection/ConnectionManagementManualTest.java
@@ -0,0 +1,66 @@
+package com.baeldung.apache.curator.connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryNTimes;
+import org.apache.curator.x.async.AsyncCuratorFramework;
+import org.junit.Test;
+
+public class ConnectionManagementManualTest {
+
+    @Test
+    public void whenOpenConnectionThenClientIsOpened() throws Exception {
+        int sleepMsBetweenRetries = 100;
+        int maxRetries = 3;
+        RetryPolicy retryPolicy = new RetryNTimes(maxRetries, sleepMsBetweenRetries);
+
+        try (CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy)) {
+            client.start();
+            assertThat(client.checkExists()
+                .forPath("/")).isNotNull();
+        }
+    }
+
+    @Test
+    public void whenOpenConnectionThenClientIsOpenedUsingAsyncNotBlocking() throws InterruptedException {
+        int sleepMsBetweenRetries = 100;
+        int maxRetries = 3;
+        RetryPolicy retryPolicy = new RetryNTimes(maxRetries, sleepMsBetweenRetries);
+
+        try (CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy)) {
+            client.start();
+            AsyncCuratorFramework async = AsyncCuratorFramework.wrap(client);
+
+            AtomicBoolean exists = new AtomicBoolean(false);
+            async.checkExists()
+                .forPath("/")
+                .thenAcceptAsync(s -> exists.set(s != null));
+            Thread.sleep(100);
+            assertThat(exists.get()).isTrue();
+        }
+    }
+
+    @Test
+    public void whenOpenConnectionThenClientIsOpenedUsingAsyncBlocking() throws InterruptedException {
+        int sleepMsBetweenRetries = 100;
+        int maxRetries = 3;
+        RetryPolicy retryPolicy = new RetryNTimes(maxRetries, sleepMsBetweenRetries);
+
+        try (CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181", retryPolicy)) {
+            client.start();
+            AsyncCuratorFramework async = AsyncCuratorFramework.wrap(client);
+
+            AtomicBoolean exists = new AtomicBoolean(false);
+            async.checkExists()
+                .forPath("/")
+                .thenAccept(s -> exists.set(s != null));
+            Thread.sleep(100);
+            assertThat(exists.get()).isTrue();
+        }
+    }
+}
