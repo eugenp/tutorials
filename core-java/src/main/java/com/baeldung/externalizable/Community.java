d325870d4b5fd657eeeb40a8c96a521ca4a164c5
mherbaghinyan <mbaghinyan@gmail.com>
2018-03-10 16:20:56 +0400

    Guide to Externalizable interface +tests

---
+++ core-java/src/main/java/com/baeldung/externalizable/Community.java
@@ -0,0 +1,33 @@
+package com.baeldung.externalizable;
+
+import java.io.*;
+
+public class Community implements Externalizable {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Community{" +
+                "id=" + id +
+                '}';
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+
+    }
+}
--- core-java/src/test/java/com/baeldung/externalizable/ExternalizableUnitTest.java
+++ core-java/src/test/java/com/baeldung/externalizable/ExternalizableUnitTest.java
@@ -38,4 +38,34 @@ public class ExternalizableUnitTest {
         assertTrue(c2.getCode() == c.getCode());
         assertTrue(c2.getName().equals(c.getName()));
     }
+
+    @Test
+    public void whenInheritanceSerialization_then_UseExternalizable() throws IOException, ClassNotFoundException {
+
+        Region r = new Region();
+        r.setCapital("Yerevan");
+        r.setCode(374);
+        r.setName("Armenia");
+        r.setClimate("Mediterranean");
+        r.setPopulation(120.000);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(OUTPUT_FILE);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        r.writeExternal(objectOutputStream);
+
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        fileOutputStream.close();
+
+        FileInputStream fileInputStream = new FileInputStream(OUTPUT_FILE);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        Region r2 = new Region();
+        r2.readExternal(objectInputStream);
+
+        objectInputStream.close();
+        fileInputStream.close();
+
+        assertTrue(r2.getPopulation() == null);
+    }
 }
