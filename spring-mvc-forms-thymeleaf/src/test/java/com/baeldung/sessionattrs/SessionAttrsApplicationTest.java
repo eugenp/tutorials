84080c9e955fafc9b56ab8079a520995c669af44
Chris Oberle <chris.oberle@gmail.com>
2018-03-10 07:06:26 -0500

    BAEL-1533 - Making a Spring MVC Form Remember Values

--- spring-mvc-forms-thymeleaf/src/test/java/com/baeldung/sessionattrs/SessionAttrsApplicationTest.java
+++ spring-mvc-forms-thymeleaf/src/test/java/com/baeldung/sessionattrs/SessionAttrsApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SessionAttrsApplicationIntegrationTest {
+public class SessionAttrsApplicationTest {
 
     @Test
     public void contextLoads() {
