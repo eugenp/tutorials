80c1e081b844a306e459d241c9aa1d31f177f7f6
mherbaghinyan <mbaghinyan@gmail.com>
2018-03-15 15:41:03 +0400

    test the ctx.close()

--- spring-boot/src/main/java/com/baeldung/shutdown/Application.java
+++ spring-boot/src/main/java/com/baeldung/shutdown/Application.java
@@ -1,12 +1,29 @@
 package com.baeldung.shutdown;
 
+import com.baeldung.autoconfiguration.MySQLAutoconfiguration;
+import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = MySQLAutoconfiguration.class)
 public class Application {
 
     public static void main(String [] args) {
-        SpringApplication.run(Application.class, args).close();
+
+        ConfigurableApplicationContext ctx = new SpringApplicationBuilder(Application.class).web(false).run();
+        System.out.println("Spring Boot application started");
+        ctx.getBean(TerminateBean.class);
+        ctx.close();
+//        SpringApplication.exit(ctx, () -> 0);
+        System.out.println("Exit Spring Boot");
+
+//        int exitCode = SpringApplication.exit(ctx, (ExitCodeGenerator) () -> {
+//            // no errors
+//            return 0;
+//        });
+//        System.exit(exitCode);
     }
 }
