c115c819c0a50ead9a3faa44c8d7093f0b1096db
abialas <abialas@universal-soft.com>
2018-03-16 20:41:31 +0100

    BAEL-1567 use stream api

--- javax-servlets/src/main/java/com/baeldung/servlets/CookieReader.java
+++ javax-servlets/src/main/java/com/baeldung/servlets/CookieReader.java
@@ -2,6 +2,7 @@ package com.baeldung.servlets;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 /**
  * Created by adam.
@@ -28,14 +29,11 @@ public class CookieReader {
      * @return returns cookie value (or null if cookie with given key does not exist)
      */
     public String readCookie(String key) {
-        if (request.getCookies() != null && request.getCookies().length > 0) {
-            for (Cookie cookie : request.getCookies()) {
-                if (key.equals(cookie.getName())) {
-                    return cookie.getValue();
-                }
-            }
-        }
-        return null;
+        return Arrays.stream(request.getCookies())
+                 .filter(c -> key.equals(c.getName()))
+                 .map(Cookie::getValue)
+                 .findFirst()
+                 .orElse(null);
     }
 
 }
