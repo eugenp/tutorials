117a751bc7fbbc7431fec2057659fa30d3cb0511
mherbaghinyan <mbaghinyan@gmail.com>
2018-03-18 14:13:41 +0400

    bring back the closeApplication

--- spring-boot/src/main/java/com/baeldung/shutdown/Application.java
+++ spring-boot/src/main/java/com/baeldung/shutdown/Application.java
@@ -13,8 +13,7 @@ public class Application {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(Application.class, args);
-//        closeApplication();
+        closeApplication();
 //        exitApplication();
 //        writePID();
 
