162457d93c73b3d2cd19fab1205998753bf6d182
Diaz Novandi <aprian.diaz.novandi@gmail.com>
2018-03-20 00:05:38 +0100

    Implement DynamoDB DAO and its integration test

---
+++ persistence-modules/spring-data-dynamodb/src/main/java/com/baeldung/spring/data/dynamodb/dao/ProductInfoDAO.java
@@ -0,0 +1,6 @@
+package com.baeldung.spring.data.dynamodb.dao;
+
+import com.baeldung.spring.data.dynamodb.model.ProductInfo;
+
+public class ProductInfoDAO extends AbstractDAO<ProductInfo, String> {
+}
