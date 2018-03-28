2285841c6efd2c12f74194c4d01fa0a4d43d45e4
ocheja <ocheja.patrick@gmail.com>
2018-03-28 14:35:46 +0900

    Implement examples for JSTL core tag

---
+++ spring-mvc-xml/src/main/java/com/baeldung/jstl/controllers/JSTLController.java
@@ -0,0 +1,24 @@
+package com.baeldung.jstl.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class JSTLController {
+
+    @RequestMapping(value = "/core_tags", method = RequestMethod.GET)
+    public ModelAndView coreTags(final Model model) {
+        ModelAndView mv = new ModelAndView("core_tags");
+        return mv;
+    }
+
+    @RequestMapping(value = "/core_tags_redirect", method = RequestMethod.GET)
+    public ModelAndView coreTagsRedirect(final Model model) {
+        ModelAndView mv = new ModelAndView("core_tags_redirect");
+        return mv;
+    }
+
+}
