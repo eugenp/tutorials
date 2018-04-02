3a3c5fb3c4c0b3de89aa5e3a7217a6d0c33862e1
ocheja <ocheja.patrick@gmail.com>
2018-04-03 02:16:36 +0900

    Implement examples for JSTL XML tags

--- spring-mvc-xml/src/main/java/com/baeldung/jstl/controllers/JSTLController.java
+++ spring-mvc-xml/src/main/java/com/baeldung/jstl/controllers/JSTLController.java
@@ -3,19 +3,29 @@ package com.baeldung.jstl.controllers;
 import com.baeldung.jstl.dbaccess.SQLConnection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.sql.*;
 import java.util.Calendar;
 
 @Controller
 public class JSTLController {
     private static final Logger LOGGER = LoggerFactory.getLogger(JSTLController.class.getName());
+
+    @Autowired
+    ServletContext servletContext;
 
     @PostConstruct
     public void init() {
@@ -77,6 +87,18 @@ public class JSTLController {
     public ModelAndView sqlTags(final Model model) {
         ModelAndView mv = new ModelAndView("sql_tags");
         return mv;
+    }
+
+    @RequestMapping(value = "/xml_tags", method = RequestMethod.GET)
+    public ModelAndView xmlTags(final Model model) {
+        ModelAndView mv = new ModelAndView("xml_tags");
+        return mv;
+    }
+
+    @RequestMapping(value = "/items_xml", method = RequestMethod.GET)
+    @ResponseBody public FileSystemResource getFile(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("application/xml");
+        return new FileSystemResource(new File(servletContext.getRealPath("/WEB-INF/items.xsl")));
     }
 
 
