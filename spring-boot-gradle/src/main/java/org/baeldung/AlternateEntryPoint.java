d7899b66267c607a34d0433883dcae7705182555
hyapit <yapith@gmail.com>
2018-04-02 23:20:28 -0700

    BAEL-1623 sample spring boot gradle project

---
+++ spring-boot-gradle/src/main/java/org/baeldung/AlternateEntryPoint.java
@@ -0,0 +1,11 @@
+package org.baeldung;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlternateEntryPoint {
+
+    public static void main(String[] args) {
+        System.out.println("Alternate main class triggered");
+    }
+}
