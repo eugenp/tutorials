ab1894c7eacf9041912dc0e63416c2f240d6d42b
igor_p <igor.pelesic@gmail.com>
2018-04-04 22:11:53 +0200

    improved thymeleaf conditionals

--- spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/TeacherController.java
+++ spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/TeacherController.java
@@ -1,27 +1,19 @@
 package com.baeldung.thymeleaf.controller;
 
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.baeldung.thymeleaf.utils.TeacherUtils;
+
 @Controller
 public class TeacherController {
 
-    @RequestMapping(value = "/exams", method = RequestMethod.GET)
-    public String getInfo(Model model) {
-        model.addAttribute("standardDate", new Date());
-        model.addAttribute("localDateTime", LocalDateTime.now());
-        model.addAttribute("localDate", LocalDate.now());
-        model.addAttribute("timestamp", Instant.now());
-        return "dates.html";
-    }
+	@RequestMapping(value = "/listTeachers", method = RequestMethod.GET)
+	public String getInfo(Model model) {
+		model.addAttribute("teachers", TeacherUtils.buildTeachers());
 
-    
-    
+		return "listTeachers.html";
+	}
 }
