3036997b51521a9c7c7c9ffe7f9507ca0a70160a
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-04-10 19:33:58 +0200

    Reconfigure PMD

--- custom-pmd/src/main/java/org/baeldung/pmd/UnitTestNamingConventionRule.java
+++ custom-pmd/src/main/java/org/baeldung/pmd/UnitTestNamingConventionRule.java
@@ -1,26 +1,32 @@
 package org.baeldung.pmd;
 
-import java.util.Arrays;
-import java.util.List;
-
 import net.sourceforge.pmd.lang.java.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class UnitTestNamingConventionRule extends AbstractJavaRule {
 
-	private static List<String> allowedEndings  = Arrays.asList("IntegrationTest", "LongRunningUnitTest", "ManualTest",
-			"JdbcTest", "LiveTest");
-	
-	public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
-		String className = node.getImage();
-		if (className != null && className.endsWith("Test")) {
-			System.out.println("Applying UnitTestNamingConventionRule to test - " + className);
-			boolean followsNamingConvention = allowedEndings.stream().anyMatch(ending -> className.endsWith(ending));
-			if(!followsNamingConvention) {
-				addViolation(data, node);
-			}
-		}
-		
-		return data;
-	}
+    private static List<String> allowedEndings = Arrays.asList(
+      "IntegrationTest",
+      "LongRunningUnitTest",
+      "ManualTest",
+      "JdbcTest",
+      "LiveTest");
+
+    public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
+        String className = node.getImage();
+        Objects.requireNonNull(className);
+
+        if (className.endsWith("Test") || className.endsWith("Tests")) {
+            if (allowedEndings.stream()
+              .noneMatch(className::endsWith)) {
+                addViolation(data, node);
+            }
+        }
+
+        return data;
+    }
 }
