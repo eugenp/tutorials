3c2148312594ae01169f6e8c72524f7048908b6c
sachin <sachin.sachdeva@gmail.com>
2018-04-10 12:36:47 -0400

    Fixed this issue when unit test classes were not being scanned by custom PMD rule (This happened after I pulled code from upstream project)

--- custom-pmd/src/main/java/org/baeldung/pmd/UnitTestNamingConventionRule.java
+++ custom-pmd/src/main/java/org/baeldung/pmd/UnitTestNamingConventionRule.java
@@ -12,11 +12,9 @@ public class UnitTestNamingConventionRule extends AbstractJavaRule {
 			"JdbcTest", "LiveTest");
 	
 	public Object visit(ASTClassOrInterfaceDeclaration node, Object data) {
-		
 		String className = node.getImage();
 		if (className != null && className.endsWith("Test")) {
 			System.out.println("Applying UnitTestNamingConventionRule to test - " + className);
-			
 			boolean followsNamingConvention = allowedEndings.stream().anyMatch(ending -> className.endsWith(ending));
 			if(!followsNamingConvention) {
 				addViolation(data, node);
