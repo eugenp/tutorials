8e4c51da4c0144d38607cde9d9f845964a100665
pmutisya <pmutisya@meliora.tech>
2018-04-11 10:32:08 +0300

    Removed unnecessary logs

--- spring-5-reactive/src/main/java/com/baeldung/webflux/controller/CustomerResource.java
+++ spring-5-reactive/src/main/java/com/baeldung/webflux/controller/CustomerResource.java
@@ -1,10 +1,8 @@
 package com.baeldung.webflux.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -16,8 +14,6 @@ import com.baeldung.webflux.service.CustomerService;
 @RestController
 @RequestMapping("/api")
 public class CustomerResource {
-    private final Logger log = LoggerFactory.getLogger(CustomerResource.class);
-
     private final CustomerService customerService;
 
     public CustomerResource(CustomerService customerService) {
