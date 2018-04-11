a176145b89643346871b0cacb668f04d4b8ac46b
ShyamVeda <v.shyamsundar1988@gmail.com>
2018-04-11 23:23:33 +0530

    Code committed for BAEL-1388

---
+++ logging-modules/log4j2-programmatic-configuration/custom-appenders/src/main/java/com/baeldung/log4j2/plugins/CustomConfigurationFactory.java
@@ -0,0 +1,89 @@
+package com.baeldung.log4j2.plugins;
+
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.ConfigurationFactory;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Order;
+import org.apache.logging.log4j.core.config.builder.api.AppenderComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilder;
+import org.apache.logging.log4j.core.config.builder.api.ConfigurationBuilderFactory;
+import org.apache.logging.log4j.core.config.builder.api.FilterComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.LayoutComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.LoggerComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.api.RootLoggerComponentBuilder;
+import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+
+@Plugin(name = "CustomConfigurationFactory", category = ConfigurationFactory.CATEGORY)
+@Order(50)
+public class CustomConfigurationFactory extends ConfigurationFactory {
+
+    static Configuration createConfiguration(final String name, ConfigurationBuilder<BuiltConfiguration> builder) {
+        AppenderComponentBuilder customAppender = builder.newAppender("customAppender", "MyConsoleAppender");
+        LayoutComponentBuilder layout = builder.newLayout("PatternLayout")
+            .addAttribute("pattern", "%d [%t] %-5level: %msg%n%throwable");
+        customAppender.add(layout);
+        FilterComponentBuilder filter = builder.newFilter("MarkerFilter", Filter.Result.ACCEPT, Filter.Result.DENY);
+        filter.addAttribute("marker", "FLOW");
+        customAppender.add(filter);
+        builder.add(customAppender);
+
+        ComponentBuilder triggeringPolicy = builder.newComponent("Policies")
+            .addComponent(builder.newComponent("CronTriggeringPolicy")
+                .addAttribute("schedule", "0 0 0 * * ?"))
+            .addComponent(builder.newComponent("SizeBasedTriggeringPolicy")
+                .addAttribute("size", "100M"));
+        AppenderComponentBuilder rollingFile = builder.newAppender("rolling", "RollingFile");
+        rollingFile.addAttribute("fileName", "target/rolling.log");
+        rollingFile.addAttribute("filePattern", "target/archive/rolling-%d{MM-dd-yy}.log.gz");
+        rollingFile.add(layout);
+        rollingFile.addComponent(triggeringPolicy);
+        builder.add(rollingFile);
+
+        AppenderComponentBuilder file = builder.newAppender("FileSystem", "File");
+        file.addAttribute("fileName", "target/logging.log");
+        file.add(layout);
+        builder.add(file);
+        LoggerComponentBuilder logger = builder.newLogger("com", Level.DEBUG);
+        logger.add(builder.newAppenderRef("customAppender"));
+        logger.add(builder.newAppenderRef("rolling"));
+        logger.add(builder.newAppenderRef("FileSystem"));
+        logger.addAttribute("additivity", false);
+        builder.add(logger);
+        RootLoggerComponentBuilder rootLogger = builder.newRootLogger(Level.ERROR);
+        rootLogger.add(builder.newAppenderRef("customAppender"));
+        rootLogger.add(builder.newAppenderRef("rolling"));
+        rootLogger.add(builder.newAppenderRef("FileSystem"));
+        rootLogger.addAttribute("additivity", false);
+        builder.add(rootLogger);
+        try {
+            builder.writeXmlConfiguration(System.out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return builder.build();
+
+    }
+
+    @Override
+    public Configuration getConfiguration(LoggerContext loggerContext, ConfigurationSource source) {
+        return getConfiguration(loggerContext, source.toString(), null);
+    }
+
+    public Configuration getConfiguration(final LoggerContext loggerContext, final String name, final URI configLocation) {
+        ConfigurationBuilder<BuiltConfiguration> builder = newConfigurationBuilder();
+        return createConfiguration(name, builder);
+    }
+
+    @Override
+    protected String[] getSupportedTypes() {
+        return new String[] { "*" };
+    }
+}
