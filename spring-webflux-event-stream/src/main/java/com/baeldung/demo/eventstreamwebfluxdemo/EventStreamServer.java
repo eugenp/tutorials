e69b630923ad5f25fc2e4cd7e137c1d31dc427c4
Michael Pratt <prattm@gmail.com>
2018-04-17 17:04:06 -0600

    Code for eval article

---
+++ spring-webflux-event-stream/src/main/java/com/baeldung/demo/eventstreamwebfluxdemo/EventStreamServer.java
@@ -0,0 +1,12 @@
+package com.baeldung.demo.eventstreamwebfluxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventStreamServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventStreamServer.class, args);
+	}
+}
