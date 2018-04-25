e060af1c6c33c6960cb70acfb60c779a272e2757
Jabir S. Minjibir <sminjibir@gmail.com>
2018-04-25 01:31:02 +0100

    Merged with the existing spring-thymeleaf code

--- spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/ArrayController.java
+++ spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/ArrayController.java
@@ -1,4 +1,4 @@
-package com.baeldung.arraysinthymleaf.contollers;
+package com.baeldung.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
