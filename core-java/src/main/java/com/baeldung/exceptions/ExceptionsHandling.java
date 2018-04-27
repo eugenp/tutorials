6f8f31b4793efd8cfde28a3fa1c59af28ff848d7
Arman Mussagaliyev <arman.mussagaliyev@hireright.com>
2018-04-27 18:08:58 +0300

    try-finally and try-with-resources added

--- core-java/src/main/java/com/baeldung/exceptions/ExceptionsHandling.java
+++ core-java/src/main/java/com/baeldung/exceptions/ExceptionsHandling.java
@@ -1,13 +1,28 @@
 package com.baeldung.exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class ExceptionsHandling {
-    
+
     private static String VALUE_VALID = "valid value";
     private static String VALUE_NA = "NA";
     private static String VALUE_EMPTY = "";
     
+    public static void justThrow() {
+        throw new RuntimeException("Something wrong");
+    }
+
     public static void throwUncheckedException() {
         throw new RuntimeException("Something wrong");
+    }
+
+    public static void throwException() throws Exception {
+        throw new Exception("Something wrong");
     }
 
     public static void throwSomeException() throws SomeException {
@@ -17,8 +32,47 @@ public class ExceptionsHandling {
     public static void throwYetAnotherException() throws YetAnotherException {
         throw new YetAnotherException("Something wrong");
     }
-    
-    
+
+    public static String tryCatchFinally() throws FileNotFoundException, IOException {
+        InputStream fis = null;
+        OutputStream fos = null;
+        try {
+            fis = new FileInputStream("");
+            fos = new FileOutputStream("");
+            //Streams are open
+            return VALUE_VALID;
+        } catch (Exception e) {
+            //Streams still open
+            return VALUE_EMPTY;
+        }
+        finally {
+            //Streams still open
+            if (fis != null)
+                fis.close();
+            
+            if (fos != null)
+                fos.close();
+            //Streams are close now
+        }
+    }
+
+    public static String tryWithResource() {
+        try (
+            InputStream fis = new FileInputStream("");
+            OutputStream fos = new FileOutputStream("");
+            )
+        {
+            //Streams are open
+            return VALUE_VALID;
+        } catch (Exception e) {
+            //Streams are close
+            return VALUE_EMPTY;
+         } finally {
+            //Streams are close already, no need to close them here
+        }
+        //Streams are close
+    }
+
     public static String call_throwUncheckedException() {
         throwUncheckedException();
         return VALUE_VALID;
@@ -64,16 +118,14 @@ public class ExceptionsHandling {
             throw new YetAnotherException("SomeException caught " + e.getMessage(), e);
         }
     }
-    
+
     public static String call_throwSomeOtherException_catch() {
         try {
             throwSomeException();
             return VALUE_VALID;
-        }
-        catch (SomeException e) {
+        } catch (SomeException e) {
             return VALUE_NA;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return VALUE_EMPTY;
         }
     }
@@ -89,11 +141,9 @@ public class ExceptionsHandling {
             throwYetAnotherException();
             throwSomeException();
             return VALUE_VALID;
-        }
-        catch (YetAnotherException | SomeException e) {
+        } catch (YetAnotherException | SomeException e) {
             return VALUE_NA;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return VALUE_EMPTY;
         }
     }
@@ -103,11 +153,9 @@ public class ExceptionsHandling {
             throwYetAnotherException();
             throwSomeException();
             return VALUE_VALID;
-        }
-        catch (SomeException e) {
+        } catch (SomeException e) {
             return VALUE_NA;
-        }
-        catch (YetAnotherException e) {
+        } catch (YetAnotherException e) {
             return VALUE_EMPTY;
         }
     }
@@ -118,14 +166,14 @@ public class ExceptionsHandling {
         return VALUE_VALID;
     }
 
-    public static String call_bothCheckedExceptions_catchParent() {
+    public static String call_bothCheckedExceptions_catchParent() throws FileNotFoundException, IOException {
         try {
             throwYetAnotherException();
             throwSomeException();
             return VALUE_VALID;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             return VALUE_EMPTY;
         }
     }
+
 }
