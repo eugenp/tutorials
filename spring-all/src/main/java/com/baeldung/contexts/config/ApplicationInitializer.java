a388c4bc8f5f7b2c6e1bb747d2266701e0319e9b
Alessio Stalla <alessiostalla@gmail.com>
2018-05-04 21:30:53 +0200

    BAEL-82 (#4109)
    * BAEL-82 revised example code
    
    * Fix the build

--- spring-all/src/main/java/com/baeldung/contexts/config/ApplicationInitializer.java
+++ spring-all/src/main/java/com/baeldung/contexts/config/ApplicationInitializer.java
@@ -5,31 +5,31 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
 public class ApplicationInitializer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
-        rootContext.register(RootApplicationConfig.class);
-        servletContext.addListener(new ContextLoaderListener(rootContext));
+        //XML Context
+        //XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
+        //rootContext.setConfigLocations("/WEB-INF/rootApplicationContext.xml");
+        //Annotations Context
+        //AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        //rootContext.register(RootApplicationConfig.class);
+        //Registration
+        //servletContext.addListener(new ContextLoaderListener(rootContext));
         
-        AnnotationConfigWebApplicationContext normalWebAppContext = new AnnotationConfigWebApplicationContext();
-        normalWebAppContext.register(NormalWebAppConfig.class);
+        XmlWebApplicationContext normalWebAppContext = new XmlWebApplicationContext();
+        normalWebAppContext.setConfigLocation("/WEB-INF/normal-webapp-servlet.xml");
         ServletRegistration.Dynamic normal = servletContext.addServlet("normal-webapp", new DispatcherServlet(normalWebAppContext));
         normal.setLoadOnStartup(1);
         normal.addMapping("/api/*");
-        
-        AnnotationConfigWebApplicationContext secureWebAppContext = new AnnotationConfigWebApplicationContext();
-        secureWebAppContext.register(SecureWebAppConfig.class);
-        ServletRegistration.Dynamic secure = servletContext.addServlet("secure-webapp", new DispatcherServlet(secureWebAppContext));
-        secure.setLoadOnStartup(1);
-        secure.addMapping("/s/api/*");
     }
 
 }
