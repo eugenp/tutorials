64e5b5285dfb02232eea3d60b4281d868da66072
Siben Nayak <sibnayak@amazon.com>
2018-05-05 18:24:13 +0530

    Fixed unit test cases and formatting

--- core-kotlin/src/test/kotlin/com/baeldung/filesystem/FileWriterTest.kt
+++ core-kotlin/src/test/kotlin/com/baeldung/filesystem/FileWriterTest.kt
@@ -1,6 +1,8 @@
 package com.baeldung.filesystem
 
 import org.junit.jupiter.api.Test
+import java.io.File
+import kotlin.test.assertEquals
 
 internal class FileWriterTest {
 
@@ -13,21 +15,29 @@ internal class FileWriterTest {
     @Test
     fun whenWrittenWithPrintWriter_thenCorrect() {
         fileWriter.writeFileUsingPrintWriter(fileName, fileContent)
+
+        assertEquals(fileContent, File(fileName).readText())
     }
 
     @Test
     fun whenWrittenWithBufferedWriter_thenCorrect() {
         fileWriter.writeFileUsingBufferedWriter(fileName, fileContent)
+
+        assertEquals(fileContent, File(fileName).readText())
     }
 
     @Test
     fun whenWrittenDirectly_thenCorrect() {
         fileWriter.writeFileDirectly(fileName, fileContent)
+
+        assertEquals(fileContent, File(fileName).readText())
     }
 
     @Test
     fun whenWrittenDirectlyAsBytes_thenCorrect() {
         fileWriter.writeFileDirectlyAsBytes(fileName, fileContent)
+
+        assertEquals(fileContent, File(fileName).readText())
     }
 
 }
