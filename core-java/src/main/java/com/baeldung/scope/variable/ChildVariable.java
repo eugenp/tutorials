0ee2e1adfc98a485671139503d0adea5609c959d
mherbaghinyan <mbaghinyan@gmail.com>
2018-05-07 16:17:56 +0400

    parent/child variable classes

---
+++ core-java/src/main/java/com/baeldung/scope/variable/ChildVariable.java
@@ -0,0 +1,9 @@
+package com.baeldung.scope.variable;
+
+/**
+ * Created by Gebruiker on 5/7/2018.
+ */
+public class ChildVariable extends ParentVariable {
+
+    String instanceVariable = "parent variable";
+}
---
+++ core-java/src/main/java/com/baeldung/scope/variable/ParentVariable.java
@@ -0,0 +1,9 @@
+package com.baeldung.scope.variable;
+
+/**
+ * Created by Gebruiker on 5/7/2018.
+ */
+public class ParentVariable {
+
+    String instanceVariable = "parent variable";
+}
