e07811fe7fae0149c5d8f95bf3c845a8d38107dc
ShyamVeda <v.shyamsundar1988@gmail.com>
2018-05-08 22:25:02 +0530

    Changes made for BAEL-1338

---
+++ logging-modules/log4j2/src/test/java/com/baeldung/logging/log4j2/xmlconfiguration/LogTest.java
@@ -0,0 +1,23 @@
+package com.baeldung.logging.log4j2.xmlconfiguration;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.plugins.util.PluginManager;
+import org.junit.Test;
+
+
+public class LogTest {
+    static{
+        PluginManager.addPackage("com.baeldung.logging.log4j2.xmlconfiguration");
+    }
+    
+    @Test
+    public void simpleProgrammaticConfiguration() {
+        Logger logger = LogManager.getLogger();
+        LoggerContext ctx = (LoggerContext) LogManager.getContext();
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
+    }
+}
