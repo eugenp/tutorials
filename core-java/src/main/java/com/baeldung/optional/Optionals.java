97ff392a7f38e9c0aab642738fdae3ba06aad050
Fatos Morina <fatosi.morina@gmail.com>
2018-05-10 21:32:25 +0200

    Modify Optionals

---
+++ core-java/src/main/java/com/baeldung/optional/Optionals.java
@@ -0,0 +1,21 @@
+package com.baeldung.optional;
+
+import java.util.Optional;
+
+public class Optionals {
+
+    public static Optional<String> getNameJava8(Optional<String> name) {
+        if (name.isPresent()) {
+            return name;
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<String> getName(Optional<String> name) {
+        return Optional.ofNullable(name).orElse(Optional.empty());
+    }
+
+    public static com.google.common.base.Optional<String> getOptionalGuavaName(com.google.common.base.Optional<String> name) {
+        return name.or(com.google.common.base.Optional.absent());
+    }
+}
