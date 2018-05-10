699e96ad1e0761c2fbcb35e132a32807f4587014
Fatos Morina <fatosi.morina@gmail.com>
2018-05-10 21:32:45 +0200

    Update OptionalsTest

---
+++ core-java/src/test/java/com/baeldung/optional/OptionalsTest.java
@@ -0,0 +1,49 @@
+
+package com.baeldung.optional;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+import com.baeldung.optional.Optionals;
+
+public class OptionalsTest {
+
+    @Test
+    public void givenOptional_whenEmptyValue_thenEmpty() {
+        assertEquals(Optional.empty(), Optionals.getName(Optional.ofNullable(null)));
+    }
+
+    @Test
+    public void givenOptional_whenValue_thenOptional() {
+        String name = "Filan Fisteku";
+        Optional<String> optionalString = Optional.ofNullable(name);
+        assertEquals(optionalString, Optionals.getName(optionalString));
+    }
+
+    @Test
+    public void givenOptional_whenValue_thenCustomOptional() {
+        String name = "Filan Fisteku";
+        Optional<String> optionalString = Optional.ofNullable(name);
+        assertEquals(optionalString, Optionals.getNameJava8(optionalString));
+    }
+
+    @Test
+    public void givenOptional_whenNull_thenEmptyOptional() {
+        assertEquals(Optional.empty(), Optionals.getNameJava8(Optional.ofNullable(null)));
+    }
+
+    @Test
+    public void givenGuavaOptional_whenInvoke_thenOptional() {
+        String name = "Filan Fisteku";
+        com.google.common.base.Optional<String> stringOptional = com.google.common.base.Optional.of(name);
+        assertEquals(name, Optionals.getOptionalGuavaName(stringOptional));
+    }
+
+    @Test
+    public void givenGuavaOptional_whenNull_thenDefaultText() {
+        assertEquals(com.google.common.base.Optional.absent(), Optionals.getOptionalGuavaName(com.google.common.base.Optional.fromNullable(null)));
+    }
+}
