bc4594dacfa21ab99bc67fed2b80215e698cec1b
jabyte <32500012+jabyte@users.noreply.github.com>
2018-05-10 14:43:41 +0100

    Update ThymeleafArrayController.java

--- spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/ThymeleafArrayController.java
+++ spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/ThymeleafArrayController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ThymeleafArrayController {
-    @GetMapping("/")
+    @GetMapping("/arrays")
     public String arrayController(Model model) {
         String[] continents = new String[7];
 
