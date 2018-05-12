0e252aa55ab8254e5c70d4d423ebbdda936ddc99
saiko <saiko@DESKTOP-60KAN6I>
2018-05-12 12:51:10 +0530

    Add basic reactive webflux server and client app

---
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/ReactiveWebclient.java
@@ -0,0 +1,5 @@
+package com.baeldung.reactive;
+
+public class ReactiveWebclient {
+
+}
---
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/ReactiveWebfluxServer.java
@@ -0,0 +1,38 @@
+package com.baeldung.reactive;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Date;
+import java.util.stream.Stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.util.function.Tuple2;
+
+@SpringBootApplication
+@RestController
+public class ReactiveWebfluxServer {
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE, value = "/events")
+    Flux<String> getEventStream() {
+        Flux<String> eventFlux = Flux.fromStream(Stream.generate(() -> getCurrentTimeStamp()));
+        Flux<Long> durationFlux = Flux.interval(Duration.ofSeconds(1));
+        return Flux.zip(eventFlux, durationFlux)
+            .map(Tuple2::getT1);
+
+    }
+
+    private String getCurrentTimeStamp() {
+        return new SimpleDateFormat("yyyy-MM-dd:HH-mm-ss").format(new Date());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWebfluxServer.class, args);
+    }
+
+}
