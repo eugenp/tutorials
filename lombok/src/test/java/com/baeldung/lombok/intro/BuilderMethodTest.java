33905ac52a856bcdc15ae891afc520f90ea25398
ericgoebelbecker <@592Gbetz>
2018-05-14 13:00:03 -0400

    BAEL-1787 - rename class. Add AssertJ to Lombok project.

--- lombok/src/test/java/com/baeldung/lombok/intro/BuilderMethodTest.java
+++ lombok/src/test/java/com/baeldung/lombok/intro/BuilderMethodTest.java
@@ -2,14 +2,17 @@ package com.baeldung.lombok.intro;
 
 import org.junit.jupiter.api.Test;
 
-import static junit.framework.TestCase.assertEquals;
+import static org.assertj.core.api.Assertions.*;
 
-public class TestBuilderMethod {
+public class BuilderMethodTest
+{
 
     @Test
     public void givenBuilderMethod_ClientIsBuilt() {
         ImmutableClient testImmutableClient = ClientBuilder.builder().name("foo").id(1).build();
-        assertEquals("foo", testImmutableClient.getName());
-        assertEquals(1, testImmutableClient.getId());
+        assertThat("foo")
+            .isEqualTo(testImmutableClient.getName());
+        assertThat(1)
+            .isEqualTo(testImmutableClient.getId());
     }
 }
