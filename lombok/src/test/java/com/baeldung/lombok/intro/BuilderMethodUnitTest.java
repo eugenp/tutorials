7f35a11a81c00cd37151962d41c1983ea628dece
ericgoebelbecker <@592Gbetz>
2018-05-15 09:35:57 -0400

    BAEL-1787 - rename class again. Change AssertJ tests.

--- lombok/src/test/java/com/baeldung/lombok/intro/BuilderMethodUnitTest.java
+++ lombok/src/test/java/com/baeldung/lombok/intro/BuilderMethodUnitTest.java
@@ -4,15 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
-public class BuilderMethodTest
+public class BuilderMethodUnitTest
 {
 
     @Test
     public void givenBuilderMethod_ClientIsBuilt() {
         ImmutableClient testImmutableClient = ClientBuilder.builder().name("foo").id(1).build();
-        assertThat("foo")
-            .isEqualTo(testImmutableClient.getName());
-        assertThat(1)
-            .isEqualTo(testImmutableClient.getId());
+        assertThat(testImmutableClient.getName())
+            .isEqualTo("foo");
+        assertThat(testImmutableClient.getId())
+            .isEqualTo(1);
     }
 }
