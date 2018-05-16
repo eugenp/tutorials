94552f398731c37751b4f8e5239d2e46f4c9817f
Raksha Rao <raksharao@Rakshas-MacBook-Air.local>
2018-05-16 21:00:28 +0530

    test - BAEL-1674

--- spring-data-rest/src/main/java/com/baeldung/events/AuthorEventHandler.java
+++ spring-data-rest/src/main/java/com/baeldung/events/AuthorEventHandler.java
@@ -2,10 +2,7 @@ package com.baeldung.events;
 
 import com.baeldung.models.Author;
 import com.baeldung.models.Book;
-import org.springframework.data.rest.core.annotation.HandleAfterDelete;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeSave;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
+import org.springframework.data.rest.core.annotation.*;
 
 @RepositoryEventHandler
 public class AuthorEventHandler {
@@ -13,15 +10,27 @@ public class AuthorEventHandler {
                 super();
         }
 
-        @HandleAfterDelete
-        public void handleAuthorAfterDelete(Author author){
-                System.out.println("Inside  Author After Delete ....");
-                String name = author.getName();
-        }
-
         @HandleBeforeCreate
         public void handleAuthorBeforeCreate(Author author){
                 System.out.println("Inside  Author Before Create....");
+                String name = author.getName();
+        }
+
+        @HandleAfterCreate
+        public void handleAuthorAfterCreate(Author author){
+                System.out.println("Inside  Author After Create ....");
+                String name = author.getName();
+        }
+
+        @HandleBeforeDelete
+        public void handleAuthorBeforeDelete(Author author){
+                System.out.println("Inside  Author Before Delete ....");
+                String name = author.getName();
+        }
+
+        @HandleAfterDelete
+        public void handleAuthorAfterDelete(Author author){
+                System.out.println("Inside  Author After Delete ....");
                 String name = author.getName();
         }
 
