ea758837cc5486a74ac8ee63eb256625becbbeb9
Karan Khanna <krnkhanna1989@gmail.com>
2018-05-17 23:28:12 +0200

    code samples for BAEL-1789 Returning JSON object from a servlet

---
+++ javax-servlets/src/main/java/com/baeldung/servlets/EmployeeServlet.java
@@ -0,0 +1,37 @@
+package com.baeldung.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.baeldung.model.Employee;
+import com.google.gson.Gson;
+
+
+@WebServlet(name = "EmployeeServlet", urlPatterns = "/employeeServlet")
+public class EmployeeServlet extends HttpServlet {
+	
+	@Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+		int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String department = request.getParameter("department");
+        Double salary = Double.parseDouble(request.getParameter("salary"));
+        
+        Employee employee = new Employee(id, name, department, salary);
+        
+        PrintWriter out = response.getWriter();
+        String employeeJsonString = new Gson().toJson(employee);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(employeeJsonString);
+        out.flush();   
+	}
+
+}
