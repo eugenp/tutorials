1d94492e2992aacc2fdabd98dd47fb7fa11c7d21
jabyte <32500012+jabyte@users.noreply.github.com>
2018-05-18 11:04:20 +0100

    Array declared and initialized in one line.

--- spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/ThymeleafArrayController.java
+++ spring-thymeleaf/src/main/java/com/baeldung/thymeleaf/controller/ThymeleafArrayController.java
@@ -8,16 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ThymeleafArrayController {
     @GetMapping("/arrays")
     public String arrayController(Model model) {
-        String[] continents = new String[7];
-
-        continents[0] = "Africa";
-        continents[1] = "Antarctica";
-        continents[2] = "Assia";
-        continents[3] = "Australia";
-        continents[4] = "Europe";
-        continents[5] = "North America";
-        continents[6] = "Sourth America";
-
+        String[] continents = {"Africa", "Antarctica", "Asia", "Australia", "Europe", "North America", "Sourth America"};
+        
         model.addAttribute("continents", continents);
 
         return "continents";
