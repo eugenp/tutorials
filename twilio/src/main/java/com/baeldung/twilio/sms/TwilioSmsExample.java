dc03c79a7d75f134cef8417462176a8b1e0d4801
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-05-18 16:30:05 +0300

    remove eval code, move sms code to package

--- twilio/src/main/java/com/baeldung/twilio/sms/TwilioSmsExample.java
+++ twilio/src/main/java/com/baeldung/twilio/sms/TwilioSmsExample.java
@@ -1,19 +1,20 @@
+package com.baeldung.twilio.sms;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class TwilioSmsExample {
     // Find your Account Sid and Token at twilio.com/console
-    public static final String ACCOUNT_SID = "YOUR_ACCOUNT_SID";
-    public static final String AUTH_TOKEN  = "YOUR_ACCOUNT_TOKEN";
+    public static final String ACCOUNT_SID = "SID";
+    public static final String AUTH_TOKEN  = "AUTH";
 
     // Create a phone number in the Twilio console
-    public static final String TWILIO_NUMBER = "+13334445555";
+    public static final String TWILIO_NUMBER = "+12223334444";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new PhoneNumber("+12227779999"),
+                new PhoneNumber("+17778889999"),
                 new PhoneNumber(TWILIO_NUMBER),
                 "Sample Twilio SMS using Java")
                 .create();
