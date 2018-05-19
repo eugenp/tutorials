3b15a23b52c48bd2a64cacfa66c8c6840739a797
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-05-19 17:42:38 -0300

    Initial Commit

---
+++ patterns/desing-patterns/src/test/java/com/baeldung/daopattern/test/UserUnitTest.java
@@ -0,0 +1,39 @@
+package com.baeldung.pattern.daopattern.test;
+
+import com.baeldung.pattern.daopattern.entities.User;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class UserUnitTest {
+    
+    private static User user;
+    
+    
+    @BeforeClass
+    public static void setUpUserInstance() {
+        user = new User("John", "john@domain.com");
+    }
+    
+    @Test
+    public void givenUserInstance_whenCalledsetName_thenOneAssertion() {
+        user.setName("Jake");
+        assertThat(user.getName()).isEqualTo("Jake");
+    }
+    
+    @Test
+    public void givenUserInstance_whenCalledsetEmail_thenOneAssertion() {
+        user.setEmail("jake@domain.com");
+        assertThat(user.getEmail()).isEqualTo("jake@domain.com");
+    }
+    
+    @Test
+    public void givenUserInstance_whenCalledgetName_thenOneAssertion() {
+        assertThat(user.getName()).isEqualTo("Jake");
+    }
+    
+    @Test
+    public void givenUserInstance_whenCalledgeEmail_thenOneAssertion() {
+        assertThat(user.getEmail()).isEqualTo("jake@domain.com");
+    }
+}
