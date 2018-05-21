f22d953179cd58f1aabfb294364520fbc57dbff1
hyapit <yapith@gmail.com>
2018-05-21 01:09:54 -0700

    spring-boot springfox 2.9.0

---
+++ spring-boot-swagger/src/main/java/com/baeldung/Greeting.java
@@ -0,0 +1,20 @@
+package com.baeldung;
+
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
---
+++ spring-boot-swagger/src/main/java/com/baeldung/GreetingController.java
@@ -0,0 +1,32 @@
+package com.baeldung;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController implements ErrorController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+          String.format(template, name));
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @GetMapping("/error")
+    public String customError() {
+        return "Error";
+    }
+}
