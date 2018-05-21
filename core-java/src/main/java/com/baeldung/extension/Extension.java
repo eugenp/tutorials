6b3aa4002c8b24570a51c4536a5af7df98df9a93
Rajat Garg <rg6693@gmail.com>
2018-05-21 08:21:19 +0530

    fix variable name
    Remove camel casing for variable name as it is a single word.

--- core-java/src/main/java/com/baeldung/extension/Extension.java
+++ core-java/src/main/java/com/baeldung/extension/Extension.java
@@ -5,19 +5,19 @@ import org.apache.commons.io.FilenameUtils;
 
 public class Extension {
     //Instead of file name we can also specify full path of a file eg. /baeldung/com/demo/abc.java
-    public String getExtensionByApacheCommonLib(String fileName) {
-        return FilenameUtils.getExtension(fileName);
+    public String getExtensionByApacheCommonLib(String filename) {
+        return FilenameUtils.getExtension(filename);
     }
 
-    public String getExtensionByStringHandling(String fileName) {
+    public String getExtensionByStringHandling(String filename) {
         String fileExtension = "";
-        if (fileName.contains(".") && fileName.lastIndexOf(".") != 0) {
-            fileExtension = fileName.substring(fileName.lastIndexOf(".") + 1);
+        if (filename.contains(".") && filename.lastIndexOf(".") != 0) {
+            fileExtension = filename.substring(filename.lastIndexOf(".") + 1);
         }
         return fileExtension;
     }
 
-    public String getExtensionByGuava(String fileName) {
-        return Files.getFileExtension(fileName);
+    public String getExtensionByGuava(String filename) {
+        return Files.getFileExtension(filename);
     }
 }
