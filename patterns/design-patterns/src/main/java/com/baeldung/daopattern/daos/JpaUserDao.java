5940e44a519099410e2ee1c2736d825fc647b825
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-05-20 22:41:57 -0300

    Update JpaUserDao.java

--- patterns/design-patterns/src/main/java/com/baeldung/daopattern/daos/JpaUserDao.java
+++ patterns/design-patterns/src/main/java/com/baeldung/daopattern/daos/JpaUserDao.java
@@ -1,6 +1,6 @@
 package com.baeldung.daopattern.daos;
 
-import com.baeldung.pattern.daopattern.entities.User;
+import com.baeldung.daopattern.entities.User;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -34,7 +34,7 @@ public class JpaUserDao implements Dao<User> {
     }
     
     @Override
-    public void update(User user, String[] params) throws NullPointerException {
+    public void update(User user, String[] params) {
         user.setName(Objects.requireNonNull(params[0], "Name cannot be null"));
         user.setEmail(Objects.requireNonNull(params[1], "Email cannot be null"));
         executeInsideTransaction(entityManager -> entityManager.merge(user));
