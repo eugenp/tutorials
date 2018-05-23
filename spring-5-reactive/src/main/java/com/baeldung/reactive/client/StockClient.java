5268c3e8c25a977370fb86aafeb05a956e73c542
priyeshmashelkar <mashelkar.priyesh@gmail.com>
2018-05-23 18:49:26 +0100

    Removed unused code

--- spring-5-reactive/src/main/java/com/baeldung/reactive/client/StockClient.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/client/StockClient.java
@@ -12,13 +12,5 @@ public class StockClient {
 		WebClient client = WebClient.create("localhost:9111");
 		RequestHeadersSpec<?> request = client.get().uri("/rtes/stocks/"+stockCode).accept(MediaType.TEXT_EVENT_STREAM);
 		request.retrieve().bodyToFlux(Stock.class).toStream().forEach(System.out::println);
-	}
-
-	public static void main(String[] args) throws InterruptedException {
-		new StockClient().getStockUpdates("GOOGL");
-
-		while(true) {
-			Thread.sleep(1000L);
-		}
-	}
+	}	
 }
