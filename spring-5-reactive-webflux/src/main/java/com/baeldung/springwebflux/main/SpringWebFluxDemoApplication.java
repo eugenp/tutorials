44d733e641216d95b64e51a988e58d470188162f
Ayushya <pathfinder2104.work@gmail.com>
2018-05-26 20:12:56 +0530

    By mistake removed a different annotation

--- spring-5-reactive-webflux/src/main/java/com/baeldung/springwebflux/main/SpringWebFluxDemoApplication.java
+++ spring-5-reactive-webflux/src/main/java/com/baeldung/springwebflux/main/SpringWebFluxDemoApplication.java
@@ -1,11 +1,11 @@
 package com.baeldung.springwebflux.main;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication(scanBasePackages = { "com.baeldung" })
-@EnableAutoConfiguration
+@EnableWebFlux
 public class SpringWebFluxDemoApplication {
 
     public static void main(String[] args) {
