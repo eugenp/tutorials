deb4e267053a94f3301272077eabb82b692bfe82
xenteros <adam@gurgul.com.pl>
2018-05-31 00:35:36 +0200

    Add sources for BAEL-1815
    * Added RomanNumeral enum,
    * Added converter class,
    * Added test class.

---
+++ core-java/src/main/java/com/baeldung/romannumerals/RomanArabicConverter.java
@@ -0,0 +1,40 @@
+package com.baeldung.romannumerals;
+
+class RomanArabicConverter {
+
+    public static int romanToArabic(String romanNumeral) {
+        int result = 0;
+        RomanNumeral[] romanNumerals = RomanNumeral.values();
+
+        int i = 0;
+
+        while (romanNumeral.length() > 0) {
+            RomanNumeral symbol = romanNumerals[i];
+            if (romanNumeral.startsWith(symbol.name())) {
+                result += symbol.getValue();
+                romanNumeral = romanNumeral.substring(symbol.name().length());
+            } else {
+                i++;
+            }
+        }
+
+        return result;
+    }
+
+    public static String arabicToRoman(int number) {
+        RomanNumeral[] romanNumerals = RomanNumeral.values();
+        int i = 0;
+        StringBuilder sb = new StringBuilder();
+
+        while (number > 0) {
+            RomanNumeral currentSymbol = romanNumerals[i];
+            if (currentSymbol.getValue() < number) {
+                sb.append(currentSymbol.name());
+                number -= currentSymbol.getValue();
+            } else {
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
