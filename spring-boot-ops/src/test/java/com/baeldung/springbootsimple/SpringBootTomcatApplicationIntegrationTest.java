e97d22b9aede4dc647fce5a59d72fb37d6bd9c04
Amit Pandey <amit2103@users.noreply.github.com>
2018-06-01 22:37:37 +0530

    Bael 6556 5 (#4385)
    * Added parent module on poms that have no parent defined
    
    * Removed dependency reduced pom from undertow module
    
    * Added README
    
    * [BAEL-6556] - Renamed next set of testcases
    
    * [BAEL-6556] - Renamed next set of testcases
    
    * [BAEL-6556] - Renamed next set of testcases

--- spring-boot-ops/src/test/java/com/baeldung/springbootsimple/SpringBootTomcatApplicationIntegrationTest.java
+++ spring-boot-ops/src/test/java/com/baeldung/springbootsimple/SpringBootTomcatApplicationIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootTomcatApplicationTests {
+public class SpringBootTomcatApplicationIntegrationTest {
 
 	@Test
 	public void contextLoads() {
