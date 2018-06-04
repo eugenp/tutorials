3361110bc03a29bef7cb9f1bd27403bc7a34881f
amit2103 <amit.pandey2103@gmail.com>
2018-06-04 21:45:26 +0530

    [BAEL-4461] - Fixed PMD violation

--- spring-security-thymeleaf/src/test/java/com/baeldung/springsecuritythymeleaf/SpringSecurityThymeleafApplicationIntegrationTest.java
+++ spring-security-thymeleaf/src/test/java/com/baeldung/springsecuritythymeleaf/SpringSecurityThymeleafApplicationIntegrationTest.java
@@ -11,7 +11,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringSecurityThymeleafApplicationTests {
+public class SpringSecurityThymeleafApplicationIntegrationTest {
 
 	@Autowired
 	ViewController viewController;
