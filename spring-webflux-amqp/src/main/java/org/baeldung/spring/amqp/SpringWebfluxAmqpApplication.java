912f9697cb2d568b9ea345c99f93c8895149b58f
Philippe <phil@lighthouse.com.br>
2018-06-05 00:19:08 -0300

    [refs#BAEL-1474] spring-webflow-amqp

---
+++ spring-webflux-amqp/src/main/java/org/baeldung/spring/amqp/SpringWebfluxAmqpApplication.java
@@ -0,0 +1,12 @@
+package org.baeldung.spring.amqp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxAmqpApplication.class, args);
+	}
+}
---
+++ spring-webflux-amqp/src/test/java/org/baeldung/spring/amqp/SpringWebfluxAmqpApplicationTests.java
@@ -0,0 +1,16 @@
+package org.baeldung.spring.amqp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxAmqpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
