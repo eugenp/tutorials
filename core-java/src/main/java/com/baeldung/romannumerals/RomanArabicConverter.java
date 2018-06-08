c7a32c61c972ba65e1ac2fac3f7806f440d335bf
xenteros <adam@gurgul.com.pl>
2018-06-08 23:56:08 +0200

    Add error handling

--- core-java/src/main/java/com/baeldung/romannumerals/RomanArabicConverter.java
+++ core-java/src/main/java/com/baeldung/romannumerals/RomanArabicConverter.java
@@ -2,7 +2,8 @@ package com.baeldung.romannumerals;
 
 class RomanArabicConverter {
 
-    public static int romanToArabic(String romanNumeral) {
+    public static int romanToArabic(String input) {
+        String romanNumeral = input.toUpperCase();
         int result = 0;
         RomanNumeral[] romanNumerals = RomanNumeral.values();
 
@@ -16,12 +17,18 @@ class RomanArabicConverter {
             } else {
                 i++;
             }
+            if (romanNumeral.length() > 0) {
+                throw new IllegalArgumentException(input + " cannot be converted to a Roman Numeral");
+            }
         }
 
         return result;
     }
 
     public static String arabicToRoman(int number) {
+        if (number <=0 || number > 4000) {
+            throw new IllegalArgumentException(number + " is not in range (0,4000>");
+        }
         RomanNumeral[] romanNumerals = RomanNumeral.values();
         int i = 0;
         StringBuilder sb = new StringBuilder();
