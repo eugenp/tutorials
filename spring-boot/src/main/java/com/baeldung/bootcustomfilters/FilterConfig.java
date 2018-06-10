45b74012530881ca8141b0885fa6d445f042efd0
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-06-10 22:36:58 +0300

    text fix

--- spring-boot/src/main/java/com/baeldung/bootcustomfilters/FilterConfig.java
+++ spring-boot/src/main/java/com/baeldung/bootcustomfilters/FilterConfig.java
@@ -6,20 +6,20 @@ import org.springframework.context.annotation.Configuration;
 
 import com.baeldung.bootcustomfilters.filters.RequestResponseLoggingFilter;
 
-// uncomment this and comment the @Component in the filter class definition to register only for a url pattern
-//@Configuration
+@Configuration
 public class FilterConfig {
 
-    //@Bean
-    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter(){
+    // uncomment this and comment the @Component in the filter class definition to register only for a url pattern
+    // @Bean
+    public FilterRegistrationBean<RequestResponseLoggingFilter> loggingFilter() {
         FilterRegistrationBean<RequestResponseLoggingFilter> registrationBean = new FilterRegistrationBean<>();
-        
+
         registrationBean.setFilter(new RequestResponseLoggingFilter());
-        
+
         registrationBean.addUrlPatterns("/users/*");
-        
+
         return registrationBean;
-        
+
     }
-    
+
 }
