04203792b0d765c6d2876813eb524b3db49b3616
Mariusz Kuligowski <kulig15@gmail.com>
2018-06-14 22:47:52 +0200

    Simplify test. Refactor to new class names

--- antlr/src/test/java/com/baeldung/antlr/JavaParserTest.java
+++ antlr/src/test/java/com/baeldung/antlr/JavaParserTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.antlr;
 
-import com.baeldung.antlr.java.JavaMethodDeclarationWalker;
+import com.baeldung.antlr.java.UppercaseMethodListener;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -12,17 +13,18 @@ public class JavaParserTest {
 
     @Test
     public void whenOneMethodStartsWithUpperCase_thenOneErrorReturned() throws Exception{
-        Java8Lexer java8Lexer = new Java8Lexer(
-                TestingUtils.loadResourceAsCharStream("SampleClass.java"));
+
+        String javaClassContent = "public class SampleClass { void DoSomething(){} }";
+        Java8Lexer java8Lexer = new Java8Lexer(CharStreams.fromString(javaClassContent));
         CommonTokenStream tokens = new CommonTokenStream(java8Lexer);
         Java8Parser java8Parser = new Java8Parser(tokens);
         ParseTree tree = java8Parser.compilationUnit();
         ParseTreeWalker walker = new ParseTreeWalker();
-        JavaMethodDeclarationWalker javaMethodDeclarationWalker = new JavaMethodDeclarationWalker();
-        walker.walk(javaMethodDeclarationWalker, tree);
+        UppercaseMethodListener uppercaseMethodListener = new UppercaseMethodListener();
+        walker.walk(uppercaseMethodListener, tree);
 
-        assertThat(javaMethodDeclarationWalker.getErrors().size(), is(1));
-        assertThat(javaMethodDeclarationWalker.getErrors().get(0),
-                is("Method DoSomethingElse starts with uppercase!"));
+        assertThat(uppercaseMethodListener.getErrors().size(), is(1));
+        assertThat(uppercaseMethodListener.getErrors().get(0),
+                is("Method DoSomething starts with uppercase!"));
     }
 }
