323972e0e0398704fe0bcd2745e690c96765af89
Mariusz Kuligowski <kulig15@gmail.com>
2018-06-14 22:48:48 +0200

    Simplify Test. Refactor to new class name

--- antlr/src/test/java/com/baeldung/antlr/ServerLogParserTest.java
+++ antlr/src/test/java/com/baeldung/antlr/ServerLogParserTest.java
@@ -3,9 +3,10 @@ package com.baeldung.antlr;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import com.baeldung.antlr.log.ServerLogWalker;
+import com.baeldung.antlr.log.ServerLogListener;
 import com.baeldung.antlr.log.model.LogLevel;
 import com.baeldung.antlr.log.model.LogEntry;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.junit.Test;
@@ -17,18 +18,20 @@ public class ServerLogParserTest {
 
     @Test
     public void whenLogContainsOneErrorLogEntry_thenOneErrorIsReturned() throws Exception {
-        ServerLogLexer serverLogLexer = new ServerLogLexer(TestingUtils.loadResourceAsCharStream("sample.log"));
+        String logLines = "2018-May-05 14:20:21 DEBUG entering awesome method\r\n" +
+                "2018-May-05 14:20:24 ERROR Bad thing happened\r\n";
+        ServerLogLexer serverLogLexer = new ServerLogLexer(CharStreams.fromString(logLines));
         CommonTokenStream tokens = new CommonTokenStream( serverLogLexer );
         ServerLogParser logParser = new ServerLogParser(tokens);
         ParseTreeWalker walker = new ParseTreeWalker();
-        ServerLogWalker logWalker = new ServerLogWalker();
+        ServerLogListener logWalker = new ServerLogListener();
         walker.walk(logWalker, logParser.log());
-        LogEntry logEntry = logWalker.getErrors().get(0);
 
-        assertThat(logWalker.getErrors().size(), is(1));
-        assertThat(logEntry.getLevel(), is(LogLevel.ERROR));
-        assertThat(logEntry.getMessage(), is("Bad thing happened"));
-        assertThat(logEntry.getDateTime(), is(LocalDateTime.of(2018,5,5,14,20,24)));
+        assertThat(logWalker.getEntries().size(), is(2));
+        LogEntry error = logWalker.getEntries().get(1);
+        assertThat(error.getLevel(), is(LogLevel.ERROR));
+        assertThat(error.getMessage(), is("Bad thing happened"));
+        assertThat(error.getDateTime(), is(LocalDateTime.of(2018,5,5,14,20,24)));
 
 
     }
