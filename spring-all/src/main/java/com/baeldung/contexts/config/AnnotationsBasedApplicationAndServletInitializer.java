92e394321296cc398cc553ece70aa8f65fcafa8a
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-06-17 15:29:48 +0300

    update to spring 5, fix startup

--- spring-all/src/main/java/com/baeldung/contexts/config/AnnotationsBasedApplicationAndServletInitializer.java
+++ spring-all/src/main/java/com/baeldung/contexts/config/AnnotationsBasedApplicationAndServletInitializer.java
@@ -4,9 +4,11 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
 
-public class AnnotationsBasedApplicationAndServletInitializer extends AbstractDispatcherServletInitializer {
+public class AnnotationsBasedApplicationAndServletInitializer //extends AbstractDispatcherServletInitializer 
+{
 
-    @Override
+    //uncomment to run the multiple contexts example
+    //@Override
     protected WebApplicationContext createRootApplicationContext() {
         //If this is not the only class declaring a root context, we return null because it would clash
         //with other classes, as there can only be a single root context.
