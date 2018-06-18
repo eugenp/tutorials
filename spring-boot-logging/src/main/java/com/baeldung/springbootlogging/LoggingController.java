8e08ea3774d8bf28ae198ede935fef17df9d9916
andrea <andrea@Phoenix2.0>
2018-06-18 23:03:19 +0200

    BAEL-1783

---
+++ spring-boot-logging/src/main/java/com/baeldung/springbootlogging/LoggingController.java
@@ -0,0 +1,24 @@
+package com.baeldung.springbootlogging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+
+    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+
+    @RequestMapping("/")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+}
