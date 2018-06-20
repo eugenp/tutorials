bf31ba77bc0e7f48aeb2100ffa99012009235dd4
Ivan Ljubičić <ivan.ljubicic.app.developer@gmail.com>
2018-06-20 23:03:21 +0200

    Removed System.in class, renamed Scanner class and reorganized

--- core-java/src/main/java/com/baeldung/console/ConsoleConsoleClass.java
+++ core-java/src/main/java/com/baeldung/console/ConsoleConsoleClass.java
@@ -3,17 +3,15 @@ package com.baeldung.console;
 import java.io.Console;
 
 public class ConsoleConsoleClass {
-    
-    public static void main(String[] args)
-    {
+
+    public static void main(String[] args) {
         Console console = System.console();
-        
-        if(console == null) 
-        {
+
+        if (console == null) {
             System.out.print("No console available");
             return;
         }
-        
+
         String progLanguauge = console.readLine("Enter your favourite programming language: ");
         console.printf(progLanguauge + " is very interesting!");
     }
--- core-java/src/main/java/com/baeldung/console/ConsoleScannerClass.java
+++ core-java/src/main/java/com/baeldung/console/ConsoleScannerClass.java
@@ -1,36 +1,57 @@
 package com.baeldung.console;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class ConsoleScanner {
-    
-    public static void main(String[] args)
-    {
+public class ConsoleScannerClass {
+
+    public static void main(String[] args) {
         System.out.println("Please enter your name and surname: ");
-        
+
         Scanner scanner = new Scanner(System.in);
- 
+
         String nameSurname = scanner.nextLine();
- 
+
         System.out.println("Please enter your gender: ");
-        
+
         char gender = scanner.next().charAt(0);
-        
+
         System.out.println("Please enter your age: ");
-        
+
         int age = scanner.nextInt();
-        
+
         System.out.println("Please enter your height in meters: ");
-        
+
         double height = scanner.nextDouble();
-                        
+
         System.out.println(nameSurname + ", " + age + ", is a great " + (gender == 'm' ? "guy" : "girl") + " with " + height + " meters height" + " and " + (gender == 'm' ? "he" : "she") + " reads Baeldung.");
-        
+
         System.out.print("Have a good");
         System.out.print(" one!");
-    
-        if(scanner!=null)
+
+        System.out.println("\nPlease enter number of years of experience as a developer: ");
+
+        BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int i = 0;
+
+        try {
+            i = Integer.parseInt(buffReader.readLine());
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        System.out.println("You are a " + (i > 5 ? "great" : "good") + " developer!");
+
+        if (scanner != null)
             scanner.close();
+
     }
 
 }
