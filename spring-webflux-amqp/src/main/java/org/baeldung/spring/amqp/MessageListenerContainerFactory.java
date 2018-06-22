944d202b577baeaa56385ead465d3ce660d45b12
Philippe <phil@lighthouse.com.br>
2018-06-22 10:05:49 -0300

    Refactoring

---
+++ spring-webflux-amqp/src/main/java/org/baeldung/spring/amqp/MessageListenerContainerFactory.java
@@ -0,0 +1,30 @@
+package org.baeldung.spring.amqp;
+
+import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageListenerContainerFactory {
+    
+    @Autowired
+    private ConnectionFactory connectionFactory;
+
+    
+    public MessageListenerContainerFactory() {}
+    
+    
+    public MessageListenerContainer createMessageListenerContainer(String queueName) {
+        
+        SimpleMessageListenerContainer dlmc = new SimpleMessageListenerContainer(connectionFactory);
+        
+        dlmc.addQueueNames(queueName);
+        dlmc.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        
+        return dlmc;
+    }
+
+}
