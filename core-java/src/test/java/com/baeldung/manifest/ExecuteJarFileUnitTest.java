706c304d50df3533a28fb9e4c6edaa98bc0d8398
enpy303 <enpy303@gmail.com>
2018-06-22 12:15:50 +0200

    naming conventions settled

--- core-java/src/test/java/com/baeldung/manifest/ExecuteJarFileUnitTest.java
+++ core-java/src/test/java/com/baeldung/manifest/ExecuteJarFileUnitTest.java
@@ -4,19 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class ExecuteJarFileTest {
+public class ExecuteJarFileUnitTest {
 
     private static final String ERROR_MESSAGE = "no main manifest attribute, in example.jar\n";
     private static final String SUCCESS_MESSAGE = "AppExample executed!\n";
 
     @Test
-    public final void testExecuteJarWithoutManifest() {
+    public final void givenDefaultManifest_whenManifestAttributeIsNotPresent_thenGetErrorMessage() {
         String output = ExecuteJarFile.executeJarWithoutManifestAttribute();
         assertEquals(ERROR_MESSAGE, output);
     }
 
     @Test
-    public final void testExecuteJarWithManifest() {
+    public final void givenCustomManifest_whenManifestAttributeIsPresent_thenGetSuccessMessage() {
         String output = ExecuteJarFile.executeJarWithManifestAttribute();
         assertEquals(SUCCESS_MESSAGE, output);
     }
