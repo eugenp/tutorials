1393ef22e9b77264d205da85ec9ae3f05ba00f13
Andrea Ligios <andrea.ligios@gmail.com>
2018-06-27 14:43:38 +0200

    tabs to spaces

--- spring-boot-logging-log4j2/src/main/java/com/baeldung/springbootlogging/SpringBootLoggingApplication.java
+++ spring-boot-logging-log4j2/src/main/java/com/baeldung/springbootlogging/SpringBootLoggingApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootLoggingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootLoggingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoggingApplication.class, args);
+    }
 }
