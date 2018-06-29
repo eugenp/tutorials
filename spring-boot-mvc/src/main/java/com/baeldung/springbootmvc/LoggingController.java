e85e9bbfcad5e0361a9a2a7c02d612391b7f6660
Andrea Ligios <andrea.ligios@gmail.com>
2018-06-29 15:38:22 +0200

    @GetMapping instead of the older @RequestMapping

--- spring-boot-mvc/src/main/java/com/baeldung/springbootmvc/LoggingController.java
+++ spring-boot-mvc/src/main/java/com/baeldung/springbootmvc/LoggingController.java
@@ -2,7 +2,7 @@ package com.baeldung.springbootmvc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,7 +10,7 @@ public class LoggingController {
 
     Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         logger.trace("A TRACE Message");
         logger.debug("A DEBUG Message");
@@ -20,5 +20,4 @@ public class LoggingController {
 
         return "Howdy! Check out the Logs to see the output...";
     }
-
 }
