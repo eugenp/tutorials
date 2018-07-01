4bf26aa0ce4543294ff09a7e6b8d94159cc1d734
GaCampioto <gcampioto@gmail.com>
2018-07-01 11:02:17 -0300

    add: reactive client

---
+++ reactive-client/src/test/java/com/baeldung/reactive/client/reactiveclient/ReactiveClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.baeldung.reactive.client.reactiveclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
