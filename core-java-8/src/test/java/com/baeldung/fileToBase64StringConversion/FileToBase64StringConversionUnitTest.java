06f9a5f445d2235c4ebeaeccc9dfd972b7abf445
nnhai1991@gmail.com <Hai Nguyen>
2018-07-03 23:38:07 +0800

    Update to use environment variables for testing

--- core-java-8/src/test/java/com/baeldung/fileToBase64StringConversion/FileToBase64StringConversionUnitTest.java
+++ core-java-8/src/test/java/com/baeldung/fileToBase64StringConversion/FileToBase64StringConversionUnitTest.java
@@ -5,33 +5,29 @@ import static org.junit.Assert.assertTrue;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
-@RunWith(Parameterized.class)
-public class FileToBase64StringConversionTest {
+public class FileToBase64StringConversionUnitTest {
 
-	private String inputFilePath;
-	private String outputFilePath;
-
-	public FileToBase64StringConversionTest(String inputFilePath, String outputFilePath) {
-		this.inputFilePath = inputFilePath;
-		this.outputFilePath = outputFilePath;
-	}
-
-	@Parameterized.Parameters
-	public static Collection<String[]> parameters() {
-		return Arrays.asList(new String[][] { { "", "" } });
-	}
+	private String inputFilePath = "";
+	private String outputFilePath = "";
 
 	@Test
+	// file paths are from environment arguments:
+	// mvn test
+	// -Dtest=com.baeldung.fileToBase64StringConversion.FileToBase64StringConversionUnitTest
+	// -DinputFilePath="abc.png" -DoutFilePath="abc.png"
+
 	public void fileToBase64StringConversion() throws FileNotFoundException, IOException {
+		inputFilePath = System.getProperty("inputFilePath");
+		outputFilePath = System.getProperty("outputFilePath");
+
+		if (inputFilePath == null || outputFilePath == null)
+			return;
+
 		File outputFile = new File(outputFilePath);
 		File inputFile = new File(inputFilePath);
 
