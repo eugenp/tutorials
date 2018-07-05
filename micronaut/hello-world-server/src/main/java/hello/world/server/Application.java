8fad77b9607a16b312da4b261bcb634dbaaf8fb8
Michael Pratt <prattm@gmail.com>
2018-07-04 22:21:44 -0600

    Micronaut server

---
+++ micronaut/hello-world-server/src/main/java/hello/world/server/Application.java
@@ -0,0 +1,10 @@
+package hello.world.server;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
