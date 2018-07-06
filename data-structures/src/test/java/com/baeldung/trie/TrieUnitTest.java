76d17d75a8fe3227bcba5776dff4eccc56cca28c
Imre Balassa <imre.balassa@trolex.com>
2018-07-07 01:38:35 +0200

    Delete the root element from the tree.

--- data-structures/src/test/java/com/baeldung/trie/TrieUnitTest.java
+++ data-structures/src/test/java/com/baeldung/trie/TrieUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class TrieTest {
+public class TrieUnitTest {
 
     @Test
     public void whenEmptyTrie_thenNoElements() {
