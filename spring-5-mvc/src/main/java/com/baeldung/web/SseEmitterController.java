e12d1f0676041266acccc2d273bd95a3247b40db
Thoughtscript <adam.gerard@gmail.com>
2018-07-07 19:50:07 +0100

    BAEL-1555

---
+++ spring-5-mvc/src/main/java/com/baeldung/web/SseEmitterController.java
@@ -0,0 +1,31 @@
+package com.baeldung.web;
+import com.baeldung.Constants;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Controller
+public class SseEmitterController {
+
+  @RequestMapping(Constants.API_SSE)
+  public SseEmitter handleSse() {
+    SseEmitter emitter = new SseEmitter();
+
+    ExecutorService nonBlockingService = Executors.newSingleThreadExecutor();
+    nonBlockingService.execute(() -> {
+      try {
+        emitter.send(Constants.API_SSE_MSG + " @ " + new Date());
+        emitter.complete();
+      } catch (Exception ex) {
+        System.out.println(Constants.GENERIC_EXCEPTION);
+        emitter.completeWithError(ex);
+      }
+    });
+
+    return emitter;
+  }
+
+}
