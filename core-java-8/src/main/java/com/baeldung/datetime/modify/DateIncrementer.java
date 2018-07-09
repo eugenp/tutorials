728f74cf0174da1968206fb44926e5c4f0233423
Abhinayak Swar <abhinayak.swar@logpoint.com>
2018-07-10 00:39:45 +0545

    Code check-on for (http://jira.baeldung.com/browse/BAEL-1897) How to increment a Date by one day - Added code and test to increment date by one day using java.util.Calendar and ApacheCommons and unit tests for it - Used properties for ${joda.version} - Formatted the code using IntelliJ formatter - Renamed DateIncrementerTest to DateIncrementerUnitTest - Changed test method names to follow _given_when_then convention

--- core-java-8/src/main/java/com/baeldung/datetime/modify/DateIncrementer.java
+++ core-java-8/src/main/java/com/baeldung/datetime/modify/DateIncrementer.java
@@ -1,37 +1,53 @@
 package com.baeldung.datetime.modify;
 
+import org.apache.commons.lang3.time.DateUtils;
 import org.joda.time.DateTime;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Logger;
 
 public class DateIncrementer {
     private static final Logger log = Logger.getLogger(DateIncrementer.class.getName());
     private static final int INCREMENT_BY_IN_DAYS = 1;
 
-    /**
-     * Using Java 8
-     */
     public static String addOneDay(String date) {
-        return LocalDate.parse(date)
-            .plusDays(INCREMENT_BY_IN_DAYS)
-            .toString();
+        return LocalDate
+          .parse(date)
+          .plusDays(INCREMENT_BY_IN_DAYS)
+          .toString();
     }
 
-    /**
-     * Using Joda-Time
-     */
     public static String addOneDayJodaTime(String date) {
         DateTime dateTime = new DateTime(date);
         // increment dateTime by one day and return in yyyy-MM-dd format
-        return dateTime.plusDays(INCREMENT_BY_IN_DAYS)
-            .toString("yyyy-MM-dd");
+        return dateTime
+          .plusDays(INCREMENT_BY_IN_DAYS)
+          .toString("yyyy-MM-dd");
     }
 
-    public static void main(String[] args) {
+    public static String addOneDayCalendar(String date) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        c.setTime(sdf.parse(date));
+        c.add(Calendar.DATE, 1);
+        return sdf.format(c.getTime());
+    }
+
+    public static String addOneDayApacheCommons(String date) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date incrementedDate = DateUtils.addDays(sdf.parse(date), 1);
+        return sdf.format(incrementedDate);
+    }
+
+    public static void main(String[] args) throws ParseException {
         // String representation of current date
-        String date = LocalDate.now()
-            .toString();
+        String date = LocalDate
+          .now()
+          .toString();
         log.info("Current date = " + date);
 
         String incrementedDateJava8 = DateIncrementer.addOneDay(date);
@@ -39,5 +55,11 @@ public class DateIncrementer {
 
         String incrementedDateJodaTime = DateIncrementer.addOneDayJodaTime(date);
         log.info("Date incremented by one day using (Joda-Time): " + incrementedDateJodaTime);
+
+        String incrementedDateCalendar = addOneDayCalendar(date);
+        log.info("Date incremented by one day using (java.util.Calendar): " + incrementedDateCalendar);
+
+        String incrementedDateApacheCommons = addOneDayApacheCommons(date);
+        log.info("Date incremented by one day using (Apache Commons DateUtils): " + incrementedDateApacheCommons);
     }
 }
