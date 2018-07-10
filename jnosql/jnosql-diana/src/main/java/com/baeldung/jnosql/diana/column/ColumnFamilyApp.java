b78d619868141f8cf5040fb043ca9b520575dede
eelhazati <eelhazati@gmail.com>
2018-07-10 08:56:07 +0100

    BAEL-1945 : jnosql

---
+++ jnosql/jnosql-diana/src/main/java/com/baeldung/jnosql/diana/column/ColumnFamilyApp.java
@@ -0,0 +1,31 @@
+package com.baeldung.jnosql.diana.column;
+
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
+import org.jnosql.diana.api.column.*;
+import org.jnosql.diana.cassandra.column.CassandraConfiguration;
+
+public class ColumnFamilyApp {
+
+    private static final String KEY_SPACE = "myKeySpace";
+    private static final String COLUMN_FAMILY = "books";
+
+    public static void main(String... args) throws Exception {
+
+        EmbeddedCassandraServerHelper.startEmbeddedCassandra();
+
+        ColumnConfiguration configuration = new CassandraConfiguration();
+        ColumnFamilyManagerFactory entityManagerFactory = configuration.get();
+        ColumnFamilyManager entityManager = entityManagerFactory.get(KEY_SPACE);
+
+        ColumnEntity columnEntity = ColumnEntity.of(COLUMN_FAMILY);
+        Column key = Columns.of("id", 10L);
+        Column name = Columns.of("name", "JNoSQL in Acion");
+        columnEntity.add(key);
+        columnEntity.add(name);
+        ColumnEntity saved = entityManager.insert(columnEntity);
+        System.out.println(saved);
+
+        EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
+    }
+
+}
