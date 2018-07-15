cd2d0e1578cf583f505a5bd5002ce2951c1fd38e
Alessio Stalla <alessiostalla@gmail.com>
2018-07-15 10:44:29 +0200

    #BAEL-1838 renamed logger helper function to getLogger to avoid confusion.

--- core-kotlin/src/main/kotlin/com/baeldung/kotlin/logging/Util.kt
+++ core-kotlin/src/main/kotlin/com/baeldung/kotlin/logging/Util.kt
@@ -4,7 +4,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.reflect.full.companionObject
 
-fun logger(forClass: Class<*>): Logger = LoggerFactory.getLogger(forClass)
+fun getLogger(forClass: Class<*>): Logger = LoggerFactory.getLogger(forClass)
 
 fun <T : Any> getClassForLogging(javaClass: Class<T>): Class<*> {
     return javaClass.enclosingClass?.takeIf {
