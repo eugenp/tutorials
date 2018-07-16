637fadb0a98e9ead213937e0b64fac43d74aeba7
mherbaghinyan <mbaghinyan@gmail.com>
2018-07-16 15:21:51 +0400

    running project without building tests

---
+++ maven/src/test/java/testfail/TestFail.java
@@ -0,0 +1,14 @@
+package testfail;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestFail {
+    @Test
+    public void whenMessageAssigned_thenItIsNotNull() {
+        String message = "hello there";
+        message = null;
+        assertNotNull(message);
+    }
+}
