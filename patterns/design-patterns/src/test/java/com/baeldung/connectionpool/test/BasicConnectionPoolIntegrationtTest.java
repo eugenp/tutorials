5de66105becd7886ab97d284b0efba8794f078c2
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-07-16 16:20:27 -0300

    Initial Commit

---
+++ patterns/design-patterns/src/test/java/com/baeldung/connectionpool/test/BasicConnectionPoolIntegrationtTest.java
@@ -0,0 +1,31 @@
+package com.baeldung.connectionpool.test;
+
+import com.baeldung.connectionpool.connectionpools.BasicConnectionPool;
+import com.baeldung.connectionpool.connectionpools.ConnectionPool;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class BasicConnectionPoolIntegrationtTest {
+    
+    private static ConnectionPool connectionPool;
+    
+    
+    @BeforeClass
+    public static void setUpBasicConnectionPoolInstance() throws SQLException {
+        connectionPool = BasicConnectionPool.createFromDriverManager("jdbc:mysql://localhost:3306/mydb", "user", "password");
+    }
+    
+    @Test
+    public void givenBasicConnectionPoolInstance_whenCalledgetConnection_thenCorrect() {
+        assertThat(connectionPool.getConnection(1)).isInstanceOf(Connection.class);
+    }
+    
+    @Test
+    public void givenBasicConnectionPoolInstance_whenCalledgetConnectionPool_thenCorrect() {
+        assertThat(connectionPool.getConnectionPool()).isInstanceOf(List.class);
+    }
+}
