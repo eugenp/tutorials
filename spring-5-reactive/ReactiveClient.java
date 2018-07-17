6b6880793bdcf3168f5212c54a181a9e1fa6798a
Vaibhav Sahay <vsahay28@gmail.com>
2018-07-17 15:22:20 +0530

    Add files via upload

--- spring-5-reactive/ReactiveClient.java
+++ spring-5-reactive/ReactiveClient.java
@@ -1,19 +1,11 @@
 package vaibhav.com.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.server.RequestPredicates;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
 
 import vaibhav.com.model.Event;
 
@@ -40,7 +32,7 @@ public class ReactiveClient {
     }
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ReactiveClient.class).properties(Collections.singletonMap("server.port", "7070"))
+        new SpringApplicationBuilder(ReactiveClient.class).properties(Collections.singletonMap("server.port", "8085"))
             .run(args);
     }
 }
