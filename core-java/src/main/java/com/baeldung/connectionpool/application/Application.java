e3ddc6390466518904076c5350834adb2b128724
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-07-17 21:06:40 -0300

    Update source files

--- core-java/src/main/java/com/baeldung/connectionpool/application/Application.java
+++ core-java/src/main/java/com/baeldung/connectionpool/application/Application.java
@@ -4,30 +4,25 @@ import com.baeldung.connectionpool.connectionpools.BasicConnectionPool;
 import com.baeldung.connectionpool.connectionpools.C3poDataSource;
 import com.baeldung.connectionpool.connectionpools.ConnectionPool;
 import com.baeldung.connectionpool.connectionpools.DBCPDataSource;
-import com.baeldung.connectionpool.connectionpools.HirakiDataSource;
+import com.baeldung.connectionpool.connectionpools.HikariCPDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Application {
     
     public static void main(String[] args) throws SQLException {
-        ConnectionPool cp1 = BasicConnectionPool.createFromDriverManager("jdbc:mysql://localhost:3306/mydb", "user", "password");
-        Connection con1 = cp1.getConnection(0);
+        ConnectionPool cp1 = BasicConnectionPool.create("jdbc:h2:mem:test", "user", "password");
+        Connection con1 = cp1.getConnection();
         System.out.println(con1);
         cp1.releaseConnection(con1);
-
-        ConnectionPool cp2 = BasicConnectionPool.createFromMysqlDataSource("jdbc:mysql://localhost:3306/mydb", "user", "password");
-        Connection con2 = cp2.getConnection(0);
+        
+        Connection con2 = DBCPDataSource.getConnection();
         System.out.println(con2);
-        cp2.releaseConnection(con2);
-        
-        Connection con3 = DBCPDataSource.getConnection();
-        System.out.println(con3);
 
-        Connection con4 = C3poDataSource.getConnection();
-        System.out.println(con4);
+        Connection con3 = C3poDataSource.getConnection();
+        System.out.println(con3);
         
-        Connection con5 = HirakiDataSource.getConnection();
-        System.out.println(con5);
+        Connection con4 = HikariCPDataSource.getConnection();
+        System.out.println(con4);
     }
 }
