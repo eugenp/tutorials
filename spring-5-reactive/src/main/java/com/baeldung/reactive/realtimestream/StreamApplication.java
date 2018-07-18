6193c45db73e4f288ec018052ab58329265f2e80
Kacper Koza <kkoza11@gmail.com>
2018-07-19 00:33:41 +0200

    real time events stream using Spring Webflux - kkoza11@gmail.com

---
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/realtimestream/StreamApplication.java
@@ -0,0 +1,12 @@
+package com.infinite.webflux.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamApplication.class, args);
+	}
+}
