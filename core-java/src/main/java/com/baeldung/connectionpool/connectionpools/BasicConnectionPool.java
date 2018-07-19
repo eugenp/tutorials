af4c9c36eca9397517aadba09348421fde622577
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-07-19 20:58:50 -0300

    Update BasicConnectionPool.java

--- core-java/src/main/java/com/baeldung/connectionpool/connectionpools/BasicConnectionPool.java
+++ core-java/src/main/java/com/baeldung/connectionpool/connectionpools/BasicConnectionPool.java
@@ -1,6 +1,5 @@
 package com.baeldung.connectionpool.connectionpools;
 
-import com.sun.istack.internal.logging.Logger;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
