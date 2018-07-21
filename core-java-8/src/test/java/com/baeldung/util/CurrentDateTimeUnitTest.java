aabfb166cd990953a703e6575ffe73481d5a7c8e
Felipe Santiago Corro <santiago.corro@gmail.com>
2018-07-21 18:16:34 -0300

    Overriding System time for testing

--- core-java-8/src/test/java/com/baeldung/util/CurrentDateTimeUnitTest.java
+++ core-java-8/src/test/java/com/baeldung/util/CurrentDateTimeUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.util;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.time.Clock;
 import java.time.Instant;
@@ -9,6 +10,8 @@ import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoField;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
 import org.junit.Test;
 
 public class CurrentDateTimeUnitTest {
@@ -40,4 +43,27 @@ public class CurrentDateTimeUnitTest {
         assertEquals(clock.instant().getEpochSecond(), now.getEpochSecond());
     }
 
+    @Test
+    public void shouldSetANewCurrentTimeForJodaDateTimeByFixed() {
+        DateTime now = DateTime.now();
+        Long millis = now.getMillis();
+        DateTimeUtils.setCurrentMillisFixed(millis + 2000);
+
+        DateTime currentNow = DateTime.now();
+        assertEquals(currentNow.getMillis(), millis + 2000);
+
+        DateTimeUtils.setCurrentMillisSystem();
+    }
+
+    @Test
+    public void shouldSetANewCurrentTimeForJodaDateTimeByOffset() {
+        DateTime now = DateTime.now();
+        Long millis = now.getMillis();
+        DateTimeUtils.setCurrentMillisOffset(2000);
+
+        DateTime currentNow = DateTime.now();
+        assertTrue(currentNow.getMillis() >= millis + 2000);
+
+        DateTimeUtils.setCurrentMillisSystem();
+    }
 }
