c9f188d7d65c8dd3af7ffc59d9cbdce41792c1fe
psysane <inbox.saikat@gmail.com>
2018-07-21 23:18:13 +0530

    Incorporate review changes

--- core-java-8/src/main/java/com/baeldung/datetime/UseTimeZone.java
+++ core-java-8/src/main/java/com/baeldung/datetime/UseTimeZone.java
@@ -13,25 +13,25 @@ import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
 public class UseTimeZone {
-    
+
     private UseTimeZone() {
-        
+
     }
 
     private static final String PATTERN = "E yyyy-MM-dd HH:mm:ss a";
 
     public static TimeZone setTimeZoneUsingJava7(String timeZone) {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone(timeZone));
-        
         Date nowUtc = new Date();
         TimeZone asiaSingapore = TimeZone.getTimeZone(timeZone);
-        
+
         Calendar nowAsiaSingapore = Calendar.getInstance(asiaSingapore);
         nowAsiaSingapore.setTime(nowUtc);
-        
-        System.out.println(String.format("Java7: Time now in '%s' is '%s'", 
-            nowAsiaSingapore.getTimeZone().getID(), simpleDateFormat.format(nowAsiaSingapore.getTime())));
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(PATTERN);
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone(timeZone));
+
+        System.out.println(String.format("Java7: Time now in '%s' is '%s'", nowAsiaSingapore.getTimeZone()
+            .getID(), simpleDateFormat.format(nowAsiaSingapore.getTime())));
 
         return nowAsiaSingapore.getTimeZone();
     }
@@ -39,23 +39,23 @@ public class UseTimeZone {
     public static ZoneId setTimeZoneUsingJava8(String timeZone) {
         Instant nowUtc = Instant.now();
         ZoneId asiaSingapore = ZoneId.of(timeZone);
-        
+
         ZonedDateTime nowAsiaSingapore = ZonedDateTime.ofInstant(nowUtc, asiaSingapore);
 
-        System.out.println(String.format("Java8: Time now in '%s' is '%s'", 
-            nowAsiaSingapore.getZone(), nowAsiaSingapore.format(DateTimeFormatter.ofPattern(PATTERN))));
+        System.out.println(String.format("Java8: Time now in '%s' is '%s'", nowAsiaSingapore.getZone(), nowAsiaSingapore.format(DateTimeFormatter.ofPattern(PATTERN))));
 
         return nowAsiaSingapore.getZone();
     }
 
     public static DateTimeZone setTimeZoneUsingJodaTime(String timeZone) {
-        DateTimeZone tzone = DateTimeZone.forID(timeZone);
-        DateTime dateTime = new DateTime(tzone);
+        org.joda.time.Instant nowUtc = org.joda.time.Instant.now();
+        DateTimeZone asiaSingapore = DateTimeZone.forID(timeZone);
+        
+        DateTime nowAsiaSingapore = nowUtc.toDateTime(asiaSingapore);
 
-        System.out.println(String.format("Joda-time: Time now in '%s' is '%s'", 
-            dateTime.getZone(), dateTime.toString(PATTERN)));
+        System.out.println(String.format("Joda-time: Time now in '%s' is '%s'", nowAsiaSingapore.getZone(), nowAsiaSingapore.toString(PATTERN)));
 
-        return dateTime.getZone();
+        return nowAsiaSingapore.getZone();
     }
 
 }
