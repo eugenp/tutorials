bf338f7e41b84b429a6e813415dfe154b0c35bf5
AD001\z003rv2s <huseyin.karabakla@siemens.com>
2018-07-23 10:50:27 +0300

    Mongo db related configuration classes excluded

--- spring-5-reactive/src/main/java/com/baeldung/reactive/eventstreaming/NewsSpringApplication.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/eventstreaming/NewsSpringApplication.java
@@ -2,10 +2,17 @@ package com.baeldung.reactive.eventstreaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@EnableWebFlux
-@SpringBootApplication
+
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class,
+        MongoDataAutoConfiguration.class,
+        MongoReactiveDataAutoConfiguration.class,
+        MongoReactiveAutoConfiguration.class }
+)
 public class NewsSpringApplication {
 
     public static void main(String[] args) {
