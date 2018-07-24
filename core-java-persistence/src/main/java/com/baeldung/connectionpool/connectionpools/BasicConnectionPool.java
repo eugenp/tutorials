4400bb76ea6881e5e739030e56bfce3ebbd3301a
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-07-24 20:06:49 -0300

    Update BasicConnectionPool class

--- core-java-persistence/src/main/java/com/baeldung/connectionpool/connectionpools/BasicConnectionPool.java
+++ core-java-persistence/src/main/java/com/baeldung/connectionpool/connectionpools/BasicConnectionPool.java
@@ -56,6 +56,7 @@ public class BasicConnectionPool implements ConnectionPool {
         return DriverManager.getConnection(url, user, password);
     }
 
+    @Override
     public int getSize() {
         return connectionPool.size() + usedConnections.size();
     }
