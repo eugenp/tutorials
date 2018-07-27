ea0f2b112f8b5c4e7da7f8b1d6c69b8d3275d3e2
Predrag Maric <predrag.maric@mangora.org>
2018-07-27 09:31:21 +0200

    BAEL-1818 moved the code from connectionpool.connectionpools package to connectionpool

--- core-java-persistence/src/test/java/com/baeldung/connectionpool/DBCPDataSourceUnitTest.java
+++ core-java-persistence/src/test/java/com/baeldung/connectionpool/DBCPDataSourceUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.connectionpool;
 
-import com.baeldung.connectionpool.connectionpools.DBCPDataSource;
 import java.sql.SQLException;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
