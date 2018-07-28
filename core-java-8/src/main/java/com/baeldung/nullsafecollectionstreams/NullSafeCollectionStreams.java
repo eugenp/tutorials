9a04fb8a46eed015da51775da1dd37c862dffcd4
Tonnix <kwajeanthony@gmail.com>
2018-07-28 21:45:38 +0300

    Added source files for BAEL-2031

---
+++ core-java-8/src/main/java/com/baeldung/nullsafecollectionstreams/NullSafeCollectionStreams.java
@@ -0,0 +1,43 @@
+package com.baeldung.nullsafecollectionstreams;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.stream.Stream;
+import static org.apache.commons.collections4.CollectionUtils.emptyIfNull;
+
+public class NullSafeCollectionStreams {
+
+    /**
+    * Test making created collection streams null safe through the use of a check to prevent null 
+    * dereferences
+    * 
+     * @param collection The collection that is to be converted into a stream
+     * @return The stream that has been created from the collection or an empty stream if the collection is null
+    */
+    public Stream<String> collectionAsStream1(Collection<String> collection) {
+        return collection == null? Stream.empty() : collection.stream();
+    }
+
+    /**
+    * Test making created collection streams null safe through the use of emptyIfNull() method from 
+    * Apache Commons CollectionUtils library
+    * 
+     * @param collection The collection that is to be converted into a stream
+     * @return The stream that has been created from the collection or an empty stream if the collection is null
+    */
+    public Stream<String> collectionAsStream2(Collection<String> collection) {
+        return emptyIfNull(collection).stream();
+    }
+
+    /**
+    * Test making created collection streams null safe through the use of Java SE 8’s Optional Container
+    * 
+     * @param collection The collection that is to be converted into a stream
+     * @return The stream that has been created from the collection or an empty stream if the collection is null
+    */
+    public Stream<String> collectionAsStream3(Collection<String> collection) {
+        return Optional.ofNullable(collection)
+            .map(Collection::stream)
+            .orElseGet(Stream::empty);
+    }
+}
