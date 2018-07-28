481094dcabdb2b2a1453f07ce48e4027ca99dc7d
freddyaott <valenzise.alfonso@gmail.com>
2018-07-28 11:56:29 +0200

    named and default parameters builder

---
+++ core-kotlin/src/main/kotlin/com/baeldung/builder/FoodOrderNamed.kt
@@ -0,0 +1,8 @@
+package com.baeldung.builder
+
+class FoodOrderNamed(
+        val bread: String? = null,
+        val condiments: String? = null,
+        val meat: String? = null,
+        val fish: String? = null) {
+}
--- core-kotlin/src/test/kotlin/com/baeldung/builder/BuilderPatternUnitTest.kt
+++ core-kotlin/src/test/kotlin/com/baeldung/builder/BuilderPatternUnitTest.kt
@@ -50,4 +50,90 @@ internal class BuilderPatternUnitTest {
     }
 
 
+    @Test
+    fun whenBuildingFoodOrderNamedSettingValues_thenFieldsNotNull() {
+
+        val foodOrder = FoodOrderNamed(
+                meat = "bacon",
+                fish = "salmon",
+                condiments = "olive oil",
+                bread = "white bread"
+        )
+
+        Assertions.assertNotNull(foodOrder.bread)
+        Assertions.assertNotNull(foodOrder.meat)
+        Assertions.assertNotNull(foodOrder.condiments)
+        Assertions.assertNotNull(foodOrder.fish)
+    }
+
+    @Test
+    fun whenBuildingFoodOrderNamedSettingValues_thenFieldsContainsValues() {
+
+        val foodOrder = FoodOrderNamed(
+                meat = "bacon",
+                fish = "salmon",
+                condiments = "olive oil",
+                bread = "white bread"
+        )
+
+        Assertions.assertEquals("white bread", foodOrder.bread)
+        Assertions.assertEquals("bacon", foodOrder.meat)
+        Assertions.assertEquals("olive oil", foodOrder.condiments)
+        Assertions.assertEquals("salmon", foodOrder.fish)
+    }
+
+    @Test
+    fun whenBuildingFoodOrderNamedWithoutSettingValues_thenFieldsNull() {
+
+        val foodOrder = FoodOrderNamed()
+
+        Assertions.assertNull(foodOrder.bread)
+        Assertions.assertNull(foodOrder.meat)
+        Assertions.assertNull(foodOrder.condiments)
+        Assertions.assertNull(foodOrder.fish)
+    }
+
+    @Test
+    fun whenBuildingFoodOrderApplySettingValues_thenFieldsNotNull() {
+
+        val foodOrder = FoodOrderApply().apply {
+            meat = "bacon"
+            fish = "salmon"
+            condiments = "olive oil"
+            bread = "white bread"
+        }
+
+        Assertions.assertNotNull(foodOrder.bread)
+        Assertions.assertNotNull(foodOrder.meat)
+        Assertions.assertNotNull(foodOrder.condiments)
+        Assertions.assertNotNull(foodOrder.fish)
+    }
+
+    @Test
+    fun whenBuildingFoodOrderApplySettingValues_thenFieldsContainsValues() {
+
+        val foodOrder = FoodOrderApply().apply {
+            meat = "bacon"
+            fish = "salmon"
+            condiments = "olive oil"
+            bread = "white bread"
+        }
+
+        Assertions.assertEquals("white bread", foodOrder.bread)
+        Assertions.assertEquals("bacon", foodOrder.meat)
+        Assertions.assertEquals("olive oil", foodOrder.condiments)
+        Assertions.assertEquals("salmon", foodOrder.fish)
+    }
+
+    @Test
+    fun whenBuildingFoodOrderApplyWithoutSettingValues_thenFieldsNull() {
+
+        val foodOrder = FoodOrderApply()
+
+        Assertions.assertNull(foodOrder.bread)
+        Assertions.assertNull(foodOrder.meat)
+        Assertions.assertNull(foodOrder.condiments)
+        Assertions.assertNull(foodOrder.fish)
+    }
+
 }
