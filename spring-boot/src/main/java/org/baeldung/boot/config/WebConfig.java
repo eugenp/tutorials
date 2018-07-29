14ad816b7db7cbb7bce56a04ce963b344379d5d4
Loredana Crusoveanu <loredana.crusoveanu@yahoo.com>
2018-07-29 18:12:48 +0300

    re-add code for type conversion article

--- spring-boot/src/main/java/org/baeldung/boot/config/WebConfig.java
+++ spring-boot/src/main/java/org/baeldung/boot/config/WebConfig.java
@@ -1,6 +1,8 @@
 package org.baeldung.boot.config;
 
 import org.baeldung.boot.converter.StringToEmployeeConverter;
+import org.baeldung.boot.converter.StringToEnumConverterFactory;
+import org.baeldung.boot.converter.GenericBigDecimalConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,5 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEmployeeConverter());
+        registry.addConverterFactory(new StringToEnumConverterFactory());
+        registry.addConverter(new GenericBigDecimalConverter());
     }
 }
