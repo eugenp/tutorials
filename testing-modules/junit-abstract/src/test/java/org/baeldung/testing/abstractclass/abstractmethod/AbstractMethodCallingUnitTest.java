09a7b18b898ccc2477cb55663c3e325dfd648758
kartiksingla <singla.kartik1@gmail.com>
2018-07-29 14:08:03 +0530

    Updated test cases for Junit-5

--- testing-modules/junit-abstract/src/test/java/org/baeldung/testing/abstractclass/abstractmethod/AbstractMethodCallingUnitTest.java
+++ testing-modules/junit-abstract/src/test/java/org/baeldung/testing/abstractclass/abstractmethod/AbstractMethodCallingUnitTest.java
@@ -1,32 +1,39 @@
 package org.baeldung.testing.abstractclass.abstractmethod;
 
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class AbstractMethodCallingUnitTest {
 
+    private AbstractMethodCalling cls;
+
+    @BeforeEach
+    public void setup() {
+        cls = Mockito.mock(AbstractMethodCalling.class);
+    }
+
     @Test
     public void givenDefaultImpl_whenMockAbstractFunc_thenExpectedBehaviour() {
-
         // mock classes and call real methods available
-        AbstractMethodCalling cls = Mockito.mock(AbstractMethodCalling.class);
-        Mockito.doReturn("Abstract")
-            .when(cls)
-            .abstractFunc();
+        Mockito.when(cls.abstractFunc())
+            .thenReturn("Abstract");
+        // Mockito.doReturn("Abstract")
+        // .when(cls)
+        // .abstractFunc();
         Mockito.doCallRealMethod()
             .when(cls)
             .defaultImpl();
 
         // validate result by mock abstractFunc's behaviour
-        assertEquals("Abstract Default", cls.defaultImpl());
+        Assertions.assertEquals("Abstract Default", cls.defaultImpl());
 
         // check the value with null response from abstract method
         Mockito.doReturn(null)
             .when(cls)
             .abstractFunc();
-        assertEquals("Default", cls.defaultImpl());
+        Assertions.assertEquals("Default", cls.defaultImpl());
     }
 
 }
