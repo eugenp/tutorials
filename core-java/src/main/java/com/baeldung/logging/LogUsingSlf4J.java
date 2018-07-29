9a41cbff2b5170525e185c0e7ef0f4afb22b34cb
saikat <psychsane@gmail.com>
2018-07-29 23:36:21 +0530

    Incorporate first review comments

--- core-java/src/main/java/com/baeldung/logging/LogUsingSlf4J.java
+++ core-java/src/main/java/com/baeldung/logging/LogUsingSlf4J.java
@@ -5,13 +5,12 @@ import org.slf4j.LoggerFactory;
 
 public class LogUsingSlf4J {
 
-    public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(LogUsingSlf4J.class);
-        String[] names = { "This", "will", "be", "displayed" };
+	public static void main(String[] args) {
+		Logger logger = LoggerFactory.getLogger(LogUsingSlf4J.class);
 
-        logger.error("An exception occurred!");
-        logger.error("An exception occurred! {} {} {} {}.", (Object[]) names);
-        logger.error("An exception occurred! {}", (Object[]) names, new Exception("Custom exception"));
-    }
+		logger.error("An exception occurred!");
+		logger.error("An exception occurred!", new Exception("Custom exception"));
+		logger.error("{}, {}! An exception occurred!", "Hello", "World", new Exception("Custom exception"));
+	}
 
 }
