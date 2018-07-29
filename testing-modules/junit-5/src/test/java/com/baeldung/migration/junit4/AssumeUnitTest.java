be5776455c3a59da328a552e1f22c938b0325b27
Carlo Corti <araknoid@gmail.com>
2018-07-29 19:11:17 +0200

    Remove wrong header

--- testing-modules/junit-5/src/test/java/com/baeldung/migration/junit4/AssumeUnitTest.java
+++ testing-modules/junit-5/src/test/java/com/baeldung/migration/junit4/AssumeUnitTest.java
@@ -1,29 +1,3 @@
-/************************************************************************************
- * Copyright © BMC Software Inc.                                                    *
- *                                                                                  *
- *                                                                                  *
- *  `.-.`                                                                           *
- *  +oooo/:.                                                                        *
- *  `ooo-/+oo+:.`                                                                   *
- *  `ooo  `.:+ooo/-`           bmmm`                                                *
- *  `ooo.     `-/+oo+:.`       bmmm`                                                *
- *   -+oo/-`     `.:+oo+.      bmmm`                                                *
- *    `-/+oo+:.`  `-/ooo-      bmmm:+cccb/.   +mmm:mmmm+mm/mmmmm+-    `:+cccc+:`    *
- *       `.:+oo+/+oo+/-`       bmmmmbcbmmmb/  cmmmmbbmmmmmmbbmmmm/  :bmmbccbmmb-    *
- *         `-+oooooo/.`        bmmm:` `.cdmm: cmmm:``-mmmm-``-mmmb -mmdo`  .+oo/    *
- *     `.:+ooo/:.-/+oo+:`      bmmm`    .mmmo cmmm`   bmmm`   mmmb ommm.            *
- *   `:+oo+/-`     .:ooo:      bmmm`    /dmm+ cmmm`   bmmm`   mmmb /mmd/    ---.    *
- *   /oo/.`     .:/+oo/:`      bmmmb+//cmmmb` cmmm`   bmmm`   mmmb `cmmmo//ommm/    *
- *  `ooo    `-/+oo+/-`         cmmmcbmmmmc/`  cmmm`   cmmb`   bmmc  `:cbmmmmbc:     *
- *  `ooo .-/ooo+:.`            ```` `...``    ````    ````    ````     `....`       *
- *  `ooo+ooo/-`                                                                     *
- *  -+++:.`                                                                         *
- *                                                                                  *
- *                                                                                  *
- * This source code is property of BMC Software Inc.,                               *
- * covered by Copyright. All rights reserved.                                       *
- *                                                                                  *
- ************************************************************************************/
 package com.baeldung.migration.junit4;
 
 
