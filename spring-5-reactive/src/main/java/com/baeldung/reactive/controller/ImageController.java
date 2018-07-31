00ebd0d34e89935d2903c5917cb1547cd4d3e51d
moisko <boris.r.borisov@gmail.com>
2018-07-31 22:58:53 +0300

    remove duplicate annotation

--- spring-5-reactive/src/main/java/com/baeldung/reactive/controller/ImageController.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/controller/ImageController.java
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@RestController
 @Controller
 @AllArgsConstructor
 @RequestMapping(path = "images")
