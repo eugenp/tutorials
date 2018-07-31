b7c7fc6c7ca9b242bfb4165762e841e1c28ea4ce
vizsoro <zsolt.vicze@gmail.com>
2018-07-31 18:45:58 +0200

    Formatting and renaming 2

--- core-java/src/test/java/com/baeldung/java/listInitialization/ListInitializationUnitTest.java
+++ core-java/src/test/java/com/baeldung/java/listInitialization/ListInitializationUnitTest.java
@@ -15,58 +15,59 @@ import org.junit.rules.ExpectedException;
 
 @Log
 public class ListInitializationUnitTest {
-	
-	@Rule
-	public ExpectedException exception = ExpectedException.none();
-	
-	@Test
-	public void withAnonymousInnerClass_thenInitialiseList() {
-		List<String> cities = new ArrayList() {
-			//Inside declaration of the subclass
-			
-			//You can have multiple initializer block
-			{
-				log.info("Inside the first initializer block.");
-			}
-			
-			{
-				log.info("Inside the second initializer block.");
-				add("New York");
-				add("Rio");
-				add("Tokyo");
-			}
-		};
-		
-		Assert.assertTrue(cities.contains("New York"));
-	}
-	
-	@Test
-	public void withArraysAsList_thenInitialiseList() {
-		List<String> list = Arrays.asList("foo", "bar");
-		
-		Assert.assertTrue(list.contains("foo"));
-	}
-	
-	@Test
-	public void withArraysAsList_whenAdd_thenUnsupportedException() {
-		List<String> list = Arrays.asList("foo", "bar");
-		
-		exception.expect(UnsupportedOperationException.class);
-		list.add("baz");
-	}
-	
-	@Test
-	public void withArrayAsList_whenCreated_thenShareReference() {
-		String[] array = { "foo", "bar" };
-		List<String> list = Arrays.asList(array);
-		array[0] = "baz";
-		Assert.assertEquals("baz", list.get(0));
-	}
-	
-	@Test
-	public void withStream_thenInitializeList() {
-		List<String> list = Stream.of("foo", "bar").collect(Collectors.toList());
-		
-		Assert.assertTrue(list.contains("foo"));
-	}
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void givenAnonymousInnerClass_thenInitialiseList() {
+        List<String> cities = new ArrayList() {
+            // Inside declaration of the subclass
+
+            // You can have multiple initializer block
+            {
+                log.info("Inside the first initializer block.");
+            }
+
+            {
+                log.info("Inside the second initializer block.");
+                add("New York");
+                add("Rio");
+                add("Tokyo");
+            }
+        };
+
+        Assert.assertTrue(cities.contains("New York"));
+    }
+
+    @Test
+    public void givenArraysAsList_thenInitialiseList() {
+        List<String> list = Arrays.asList("foo", "bar");
+
+        Assert.assertTrue(list.contains("foo"));
+    }
+
+    @Test
+    public void givenArraysAsList_whenAdd_thenUnsupportedException() {
+        List<String> list = Arrays.asList("foo", "bar");
+
+        exception.expect(UnsupportedOperationException.class);
+        list.add("baz");
+    }
+
+    @Test
+    public void givenArrayAsList_whenCreated_thenShareReference() {
+        String[] array = { "foo", "bar" };
+        List<String> list = Arrays.asList(array);
+        array[0] = "baz";
+        Assert.assertEquals("baz", list.get(0));
+    }
+
+    @Test
+    public void givenStream_thenInitializeList() {
+        List<String> list = Stream.of("foo", "bar")
+            .collect(Collectors.toList());
+
+        Assert.assertTrue(list.contains("foo"));
+    }
 }
