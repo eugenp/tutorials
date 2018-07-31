83807f34e89304bd06a40c8a49f0583b999831ca
hardik-r-shah <bolohardik@gmail.com>
2018-07-31 10:02:25 +0530

    Unit test fix

--- spring-5-reactive/src/test/java/com/baeldung/reactive/webflux/client/PriceControllerUnitTest.java
+++ spring-5-reactive/src/test/java/com/baeldung/reactive/webflux/client/PriceControllerUnitTest.java
@@ -1,34 +1,30 @@
 package com.baeldung.reactive.webflux.client;
 
+import com.baeldung.reactive.webflux.server.controller.PriceController;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Flux;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PriceControllerUnitTest {
 
-    @Autowired
-    private WebTestClient testClient;
-
+    private final WebTestClient testClient = WebTestClient.bindToController(new PriceController())
+            .configureClient()
+            .build();
     @Test
     public void givenStockName_whenGetCurrentPriceByStock_thenCorrectStockPrice() {
-
         testClient.get()
                 .uri("/api/stock/currentPrice?stockName=Flipkart")
                 .accept(MediaType.TEXT_EVENT_STREAM)
                 .exchange()
                 .expectStatus()
                 .isOk()
-                .expectBody(Flux.class);
-
+                .returnResult(Double.class);
     }
 }
