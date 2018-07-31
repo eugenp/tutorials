d05f4572f8a7e9eefe9e97ff6b952b96ff04988d
db <danielgbarrigas@gmail.com>
2018-07-31 22:42:52 +0100

    OAuth2 Principal and Authorities example - refactor and added example using custom authorization server

--- spring-5-security/src/main/java/com/baeldung/oauth2extractors/extractor/custom/BaeldungPrincipalExtractor.java
+++ spring-5-security/src/main/java/com/baeldung/oauth2extractors/extractor/custom/BaeldungPrincipalExtractor.java
@@ -1,13 +1,13 @@
-package com.baeldung.oauth2extractors.extractor;
+package com.baeldung.oauth2extractors.extractor.custom;
 
 import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 
 import java.util.Map;
 
-public class CustomPrincipalExtractor implements PrincipalExtractor {
+public class BaeldungPrincipalExtractor implements PrincipalExtractor {
 
     @Override
     public Object extractPrincipal(Map<String, Object> map) {
-        return map.get("login");
+        return map.get("name");
     }
 }
