c765acd1da77ed6c2460de47910e2c948af11dc8
Pello Altadill <pello.altadill@gluckgames.com>
2018-08-01 11:58:35 +0200

    BAEL-1861 - Running JUnit tests from a Java application (#4526)
    * BAEL-1562 - Thymeleaf sample working
    
    * BAEL-1562 Code added for Fragments sample
    
    * BAEL-1562 - Last correction for the test
    
    * BAEL-1562 - Thymeleaf sample working
    
    * BAEL-1562 Code added for Fragments sample
    
    * BAEL-1562 - Last correction for the test
    
    * Updates Thymeleaf version to 3.0.9.RELEASE
    
    * Added msf4j projects
    
    * updated msf4j project folder
    
    * fixed issue with spring-thymeleaf/pom.xml
    
    * Removed depedency-reduced-pom.xml
    
    * Whitespacing fix
    
    * Strange git issue with README.MD, wouldn't revert the file
    
    * Added jupiter api
    
    * Corrected junit test
    
    * Added test engine to plugin
    
    * Removed extra tag
    
    * Little fixes to junit4 and junit4 run from java
    
    * Removed scope from pom.xml
    
    * Removed bin file from testing
    
    * Slight changes for PMD
    
    * Slight changes for PMD
    
    * ok, moved code to another folder
    
    * Renamed and fixed runjunitfromjava
    
    * moved test classes to test folder
    
    * moved main to src/java
    
    * BAEL-1861 Moved test running classes to src/test/java
    
    * Added changes to runjunitfromjava
    
    * Added changes to runjunitfromjava
    
    * BAEL-1861 Changed test execution code examples
    
    * BAEL-1861 Changed test execution code examples; formatting

---
+++ testing-modules/runjunitfromjava/src/main/java/com/baeldung/junit/runfromjava/listnode/MergeLists.java
@@ -0,0 +1,22 @@
+package com.baeldung.junit.runfromjava.listnode;
+
+public class MergeLists {
+
+    public ListNode merge(ListNode list1, ListNode list2) {
+
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+
+        if (list1.getValue() <= list2.getValue()) {
+            list1.setNext(merge(list1.getNext(), list2));
+            return list1;
+        } else {
+            list2.setNext(merge(list2.getNext(), list1));
+            return list2;
+        }
+    }
+}
