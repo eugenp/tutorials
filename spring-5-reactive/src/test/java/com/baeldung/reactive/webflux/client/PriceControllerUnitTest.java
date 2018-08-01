85b9348495a02528a63a5e1d78c66f7e837f6976
hardik-r-shah <bolohardik@gmail.com>
2018-08-01 13:26:10 +0530

    StepVerifier used to test the flow

--- spring-5-reactive/src/test/java/com/baeldung/reactive/webflux/client/PriceControllerUnitTest.java
+++ spring-5-reactive/src/test/java/com/baeldung/reactive/webflux/client/PriceControllerUnitTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -19,12 +22,17 @@ public class PriceControllerUnitTest {
             .build();
     @Test
     public void givenStockName_whenGetCurrentPriceByStock_thenCorrectStockPrice() {
-        testClient.get()
+        Flux<Double> responseBody = testClient.get()
                 .uri("/api/stock/currentPrice?stockName=Flipkart")
                 .accept(MediaType.TEXT_EVENT_STREAM)
                 .exchange()
                 .expectStatus()
                 .isOk()
-                .returnResult(Double.class);
+                .returnResult(Double.class).getResponseBody();
+
+        StepVerifier.create(responseBody)
+                .expectSubscription()
+                .expectNextCount(5).thenCancel().verify();
+
     }
 }
