8820a8e65c367bd2b2865a4719e5a5e9c7b7a631
Timoteo Ponce <timo.slack@gmail.com>
2018-08-01 10:07:34 -0400

    Switched to default security encoder instead of a specific one

--- spring-5-security/src/main/java/com/baeldung/inmemory/InMemoryAuthWebSecurityConfigurer.java
+++ spring-5-security/src/main/java/com/baeldung/inmemory/InMemoryAuthWebSecurityConfigurer.java
@@ -1,12 +1,10 @@
 package com.baeldung.inmemory;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
@@ -14,11 +12,11 @@ public class InMemoryAuthWebSecurityConfigurer extends WebSecurityConfigurerAdap
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();        
         auth.inMemoryAuthentication()
             .passwordEncoder(encoder)
             .withUser("spring")
-            .password(encoder.encode("secret"))
+            .password(encoder.encode("secret"))            
             .roles("USER");
     }
 
@@ -31,14 +29,6 @@ public class InMemoryAuthWebSecurityConfigurer extends WebSecurityConfigurerAdap
             .permitAll()
             .and()
             .httpBasic();
-    }
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        // ALTHOUGH THIS SEEMS LIKE USELESS CODE,
-        // ITS REQUIRED TO PREVENT SPRING BOOT AUTO-CONFIGURATION
-        return super.authenticationManagerBean();
     }
 
 }
