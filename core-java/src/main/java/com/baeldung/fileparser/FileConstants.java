406146f959fbf4ccefc95bc786ad97d74ddb7140
RanjeetKaur17 <ranjeet@algorythma.com>
2018-08-04 16:52:27 +0400

    Removing unused file.

--- core-java/src/main/java/com/baeldung/fileparser/FileConstants.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.fileparser;
-
-public class FileConstants {
-
-    protected static final String TEXT_FILENAME = "src/main/resources/sampleTextFile.txt";
-
-    protected static final String NNUMBER_FILENAME = "src/main/resources/sampleNumberFile.txt";
-}
