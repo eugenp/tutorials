b0dd023e4e5ba481a1647df9bd58a7b20e06dcf3
waza-kaunda <wazakaunda@gmail.com>
2018-08-06 03:20:08 +0200

    Added Employee event pojo

---
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/webflux/EmployeeEvent.java
@@ -0,0 +1,16 @@
+package com.baeldung.reactive.webflux;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeEvent {
+
+	private Employee employee;
+	private Date date;
+}
