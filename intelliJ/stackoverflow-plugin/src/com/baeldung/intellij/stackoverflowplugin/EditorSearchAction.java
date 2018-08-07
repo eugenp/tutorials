5d603fdec877f236c09aee9bcdcb72dbf2b3a938
Michael Pratt <michael.pratt-nr@raytheon.com>
2018-08-07 18:48:36 +0000

    Add language tag to search action

--- intelliJ/stackoverflow-plugin/src/com/baeldung/intellij/stackoverflowplugin/EditorSearchAction.java
+++ intelliJ/stackoverflow-plugin/src/com/baeldung/intellij/stackoverflowplugin/EditorSearchAction.java
@@ -1,11 +1,18 @@
 package com.baeldung.intellij.stackoverflowplugin;
 
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.impl.ConsoleViewUtil;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
 import com.intellij.ide.BrowserUtil;
+import com.intellij.lang.Language;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.CaretModel;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileTypes.PlainSyntaxHighlighter;
+import com.intellij.openapi.fileTypes.SyntaxHighlighter;
 
 public class EditorSearchAction extends AnAction
 {
@@ -19,9 +26,12 @@ public class EditorSearchAction extends AnAction
       final Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
       CaretModel caretModel = editor.getCaretModel();
 
+      // Get the file type so we can properly tag the search
+      Language lang = e.getData(CommonDataKeys.PSI_FILE).getLanguage();
+
       if(caretModel.getCurrentCaret().hasSelection())
       {
-         String query = caretModel.getCurrentCaret().getSelectedText().replace(' ', '+');
+         String query = caretModel.getCurrentCaret().getSelectedText().replace(' ', '+') + "+[" + lang.getDisplayName().toLowerCase() + "]";
          BrowserUtil.browse("https://stackoverflow.com/search?q=" + query);
       }
    }
