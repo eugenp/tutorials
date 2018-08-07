7e433baa273514369d00615f4489b2d1a38a08a1
Grigoris Dimopoulos <grigoris.dimopoulos@camelotglobal.com>
2018-08-07 16:10:53 +0300

    Simple webflux application

---
+++ spring-webflux-simple-example/src/main/java/com/reactive/webflux/server/EventsController.java
@@ -0,0 +1,25 @@
+package com.reactive.webflux.server;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class EventsController {
+
+    private int eventNumber = 0;
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> getEvent() {
+        return Flux.fromStream(Stream.generate(() -> getNextEvent())).delayElements(Duration.ofSeconds(1));
+    }
+
+    private String getNextEvent() {
+        return "Event: "+ eventNumber++;
+    }
+}
