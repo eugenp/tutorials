54e0480075b5f71f6f12441d45e7f2dd442c71b9
Puneet Dewan <puneet@Puneets-MacBook-Pro.local>
2018-08-07 23:09:24 +0800

    Added Server Integration test and refactored the client

--- spring-5-reactive-server-sent-events/client/src/main/java/com/baeldung/Client.java
+++ spring-5-reactive-server-sent-events/client/src/main/java/com/baeldung/Client.java
@@ -4,15 +4,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class Client {
-    Logger logger = LoggerFactory.getLogger(Client.class);
+   static Logger logger = LoggerFactory.getLogger(Client.class);
 
     public static void main(String[] args) {
 
         SpringApplication.run(Client.class);
-        DataConsumer recieveController = new DataConsumer();
-        recieveController.getData();
+        getData();
+    }
+
+    private static WebClient getWebClient() {
+        return WebClient.create("http://localhost:8080");
+    }
+
+    public static void getData() {
+        WebClient client = getWebClient();
+
+        client
+          .get()
+          .uri("/data")
+          .accept(MediaType.TEXT_EVENT_STREAM)
+          .retrieve()
+          .bodyToFlux(Long.class)
+          .map(s -> String.valueOf(s))
+          .subscribe(msg -> logger.info(msg));
     }
 }
