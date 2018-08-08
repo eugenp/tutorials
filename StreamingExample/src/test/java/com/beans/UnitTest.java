a19c8c6795704f11088f404dff084b78cdf7e466
Satyam <tyagi.satyam04@gmail.com>
2018-08-08 22:15:14 +0530

    added Test case

--- StreamingExample/src/test/java/com/beans/UnitTest.java
+++ StreamingExample/src/test/java/com/beans/UnitTest.java
@@ -2,15 +2,30 @@ package com.beans;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes=Client.class)
+@AutoConfigureWebTestClient(timeout = "36000")
+@SpringBootTest(classes = Endpoint.class)
 public class UnitTest {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	private WebTestClient webTestClient;
 
+	@Test
+	public void get() {
+
+		webTestClient.get().uri("/streamOfEvents")
+				.accept(MediaType.TEXT_EVENT_STREAM)
+				.exchange()
+				.expectStatus()
+				.isOk()
+				.expectHeader().contentTypeCompatibleWith(MediaType.TEXT_EVENT_STREAM)
+				.returnResult(Event.class);
+	}
 }
