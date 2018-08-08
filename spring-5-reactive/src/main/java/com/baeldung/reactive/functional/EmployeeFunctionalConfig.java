1a5f4d0a619e1fe1e78ac698019d106dfe58aad4
Dhawal Kapil <dhawalkapil@gmail.com>
2018-08-08 19:14:22 +0530

    BAEL-2304 Improve the original spring functional web article
    -Added route for post method to update existing employee

--- spring-5-reactive/src/main/java/com/baeldung/reactive/functional/EmployeeFunctionalConfig.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/functional/EmployeeFunctionalConfig.java
@@ -1,6 +1,8 @@
 package com.baeldung.reactive.functional;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.path;
+import static org.springframework.web.reactive.function.BodyExtractors.toMono;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
@@ -14,6 +16,8 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import com.baeldung.reactive.webflux.Employee;
 import com.baeldung.reactive.webflux.EmployeeRepository;
 
+import reactor.core.publisher.Mono;
+
 @Configuration
 public class EmployeeFunctionalConfig {
 
@@ -24,19 +28,28 @@ public class EmployeeFunctionalConfig {
 
     @Bean
     RouterFunction<ServerResponse> getAllEmployeesRoute() {
-        return route(path("/employees"), 
-            req -> ok().body(employeeRepository().findAllEmployees(), Employee.class));
+        return route(GET("/employees"), req -> ok().body(employeeRepository().findAllEmployees(), Employee.class));
     }
 
     @Bean
     RouterFunction<ServerResponse> getEmployeeByIdRoute() {
-        return route(path("/employee/{id}"), 
-            req -> ok().body(employeeRepository().findEmployeeById(req.pathVariable("id")), Employee.class));
+        return route(GET("/employees/{id}"), req -> ok().body(employeeRepository().findEmployeeById(req.pathVariable("id")), Employee.class));
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> updatedEmployee() {
+        return route(POST("/employees/update"), req -> {               
+            Employee employee = req.body(toMono(Employee.class)).block();
+            Mono<Employee> updated=employeeRepository().updateEmployee(employee);
+            return ok().body(updated, Employee.class);
+        });
     }
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange()
+        http.csrf()
+            .disable()
+            .authorizeExchange()
             .anyExchange()
             .permitAll();
         return http.build();
