d90ac5ed5531738c01a7b784b6c627676b4fc075
Manoj <themanojshukla@gmail.com>
2018-08-10 13:53:44 +0530

    Added class level request mapping

--- spring-boot-webFlux/src/main/java/com/manoj/webFlux/WebFluxApplication.java
+++ spring-boot-webFlux/src/main/java/com/manoj/webFlux/WebFluxApplication.java
@@ -4,10 +4,7 @@ import com.manoj.webFlux.dto.TimeToken;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,22 +13,22 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/timetokens")
 public class WebFluxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebFluxApplication.class, args);
 	}
 
-	@GetMapping("/timetokens/{id}")
+	@GetMapping("/{id}")
 	Mono<TimeToken> getTimeTokenById(@PathVariable long id) {
 		return Mono.just(new TimeToken(id, "Mono-Token-" + System.currentTimeMillis()));
 	}
 
-	@GetMapping(path = "/timetokens", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
 	Flux<TimeToken> getTimeTokens(@RequestParam(value = "max", required=false, defaultValue = "10") Long max){
-		Flux<TimeToken> tokenFlux
-				= Flux.fromStream(Stream.generate(()
-								-> new TimeToken(System.currentTimeMillis(), "Flux-Token-"+System.currentTimeMillis())));
+		Stream generator = Stream.generate(() -> new TimeToken(System.currentTimeMillis(), "Flux-Token-"+System.currentTimeMillis()));
+		Flux<TimeToken> tokenFlux = Flux.fromStream(generator);
 		Flux<Long> interval = Flux.interval(Duration.ofSeconds(1));
 		return Flux.zip(tokenFlux, interval)
 				   .map(objects -> objects.getT1())
