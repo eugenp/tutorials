4e4d11574ac5f3719d6eb8620ef54bda31b689ca
nnhai1991@gmail.com <Hai Nguyen>
2018-08-11 16:10:53 +0800

    add more test

--- spring-boot-security-taglibs/src/main/webapp/WEB-INF/views/home.jsp
+++ spring-boot-security-taglibs/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,20 +1,29 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<%@ taglib prefix="security"
-           uri="http://www.springframework.org/security/tags" %>
-    
+	pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="sec"
+	uri="http://www.springframework.org/security/tags"%>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<sec:csrfMetaTags />
 <title>Home Page</title>
 </head>
 <body>
-     <security:authorize access="isAuthenticated()">
+	<sec:authorize access="isAuthenticated()">
         AUTHENTICATED
-     </security:authorize>
-    <security:authorize access="hasRole('ADMIN')">
+     </sec:authorize>
+	<sec:authorize access="hasRole('ADMIN')">
         ADMIN ROLE
-    </security:authorize>
+    </sec:authorize>
+
+	<h2>principal.username: <sec:authentication property="principal.username" /> </h2>
+
+	<form method="post" action="/do/something">
+		<sec:csrfInput />
+		Text Field:<br /> <input type="text" name="textField" />
+	</form>
+
 </body>
 </html>
--- spring-boot-security-taglibs/src/test/java/org/baeldung/security/HomeControllerTest.java
+++ spring-boot-security-taglibs/src/test/java/org/baeldung/security/HomeControllerTest.java
@@ -13,15 +13,30 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HomeControllerTest {
-	
+
     @Autowired
     private TestRestTemplate restTemplate;
 
-	@Test
-	public void home() throws Exception {
-		String body = this.restTemplate.withBasicAuth("ADMIN", SecurityConfig.DEFAULT_PASSWORD).getForEntity("/", String.class).getBody();
-		System.out.println(body);
-		assertTrue(body.contains("AUTHENTICATED"));
-		assertTrue(body.contains("ADMIN ROLE"));
-	}
+    @Test
+    public void home() throws Exception {
+        String body = this.restTemplate.withBasicAuth("ADMIN", SecurityConfig.DEFAULT_PASSWORD)
+            .getForEntity("/", String.class)
+            .getBody();
+        System.out.println(body);
+
+        // test <sec:authorize access="isAuthenticated()">
+        assertTrue(body.contains("AUTHENTICATED"));
+
+        // test <sec:authorize access="hasRole('ADMIN')">
+        assertTrue(body.contains("ADMIN ROLE"));
+
+        // test <sec:authentication property="principal.username" />
+        assertTrue(body.contains("principal.username: ADMIN"));
+
+        // test <sec:csrfInput />
+        assertTrue(body.contains("<input type=\"hidden\" name=\"_csrf\" value=\""));
+
+        // test <sec:csrfMetaTags />
+        assertTrue(body.contains("<meta name=\"_csrf_parameter\" content=\"_csrf\" />"));
+    }
 }
