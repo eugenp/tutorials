6afac18d13b1b4a6026831279e28cc5e7e6c789e
Darragh Pigott <darragh@acornsoftware.ie>
2018-08-13 21:17:07 +0100

    Formatting

--- json/src/main/java/com/baeldung/jsonpointer/JsonPointerCrud.java
+++ json/src/main/java/com/baeldung/jsonpointer/JsonPointerCrud.java
@@ -15,85 +15,82 @@ import javax.json.JsonString;
 import javax.json.JsonStructure;
 
 public class JsonPointerCrud {
-	
-	private JsonStructure jsonStructure = null;
-	
-	public JsonPointerCrud(String fileName) throws IOException {
-		
-		try (JsonReader reader = Json.createReader(Files.newBufferedReader(Paths.get(fileName)))){
-			jsonStructure = reader.read();
-		} catch (FileNotFoundException e) {
-			System.out.println("Error to open json file: " + e.getMessage());
-		}
-	    
-		
-	}
-	
-	public JsonPointerCrud(InputStream stream) {
-		
-		JsonReader reader = Json.createReader(stream);
-		jsonStructure = reader.read();
-	    reader.close();
-		
-	}
 
-	
-	public JsonStructure insert(String key, String value) {
-		
-		JsonPointer jsonPointer = Json.createPointer("/" + key);
-		JsonString jsonValue = Json.createValue(value);
-	    jsonStructure = jsonPointer.add(jsonStructure, jsonValue);
-	    
-	    return jsonStructure;
-		
-	}
-	
-	public JsonStructure update(String key, String newValue) {
-		
-		JsonPointer jsonPointer = Json.createPointer("/" + key);
-		JsonString jsonNewValue = Json.createValue(newValue);
-	    jsonStructure = jsonPointer.replace(jsonStructure, jsonNewValue);
-	    
-	    return jsonStructure;
-	}
-	
-	public JsonStructure delete(String key) {
-		
-		JsonPointer jsonPointer = Json.createPointer("/" + key);
-	    jsonPointer.getValue(jsonStructure);
-	    jsonStructure = jsonPointer.remove(jsonStructure);
-	    
+    private JsonStructure jsonStructure = null;
 
-	    return jsonStructure;
-		
-	}
-	
-	public String fetchValueFromKey(String key) {
-		JsonPointer jsonPointer = Json.createPointer("/" + key);
-	    JsonString jsonString = (JsonString) jsonPointer.getValue(jsonStructure);
-	    
-	    return jsonString.getString();
-	}
-	
-	public String fetchListValues(String key) {
-		JsonPointer jsonPointer = Json.createPointer("/" + key);
-	    JsonObject jsonObject = (JsonObject) jsonPointer.getValue(jsonStructure);
-	    
-	    return jsonObject.toString();
-	}
-	
-	public String fetchFullJSON() {
-		JsonPointer jsonPointer = Json.createPointer("");
-	    JsonObject jsonObject = (JsonObject) jsonPointer.getValue(jsonStructure);
-	    
-	    return jsonObject.toString();
-		
-	}
-	
-	public boolean check(String key) {
-		JsonPointer jsonPointer = Json.createPointer("/" + key);
-	    boolean found = jsonPointer.containsValue(jsonStructure);
-	    
-	    return found;
-	}
+    public JsonPointerCrud(String fileName) throws IOException {
+
+        try (JsonReader reader = Json.createReader(Files.newBufferedReader(Paths.get(fileName)))) {
+            jsonStructure = reader.read();
+        } catch (FileNotFoundException e) {
+            System.out.println("Error to open json file: " + e.getMessage());
+        }
+
+    }
+
+    public JsonPointerCrud(InputStream stream) {
+
+        JsonReader reader = Json.createReader(stream);
+        jsonStructure = reader.read();
+        reader.close();
+
+    }
+
+    public JsonStructure insert(String key, String value) {
+
+        JsonPointer jsonPointer = Json.createPointer("/" + key);
+        JsonString jsonValue = Json.createValue(value);
+        jsonStructure = jsonPointer.add(jsonStructure, jsonValue);
+
+        return jsonStructure;
+
+    }
+
+    public JsonStructure update(String key, String newValue) {
+
+        JsonPointer jsonPointer = Json.createPointer("/" + key);
+        JsonString jsonNewValue = Json.createValue(newValue);
+        jsonStructure = jsonPointer.replace(jsonStructure, jsonNewValue);
+
+        return jsonStructure;
+    }
+
+    public JsonStructure delete(String key) {
+
+        JsonPointer jsonPointer = Json.createPointer("/" + key);
+        jsonPointer.getValue(jsonStructure);
+        jsonStructure = jsonPointer.remove(jsonStructure);
+
+        return jsonStructure;
+
+    }
+
+    public String fetchValueFromKey(String key) {
+        JsonPointer jsonPointer = Json.createPointer("/" + key);
+        JsonString jsonString = (JsonString) jsonPointer.getValue(jsonStructure);
+
+        return jsonString.getString();
+    }
+
+    public String fetchListValues(String key) {
+        JsonPointer jsonPointer = Json.createPointer("/" + key);
+        JsonObject jsonObject = (JsonObject) jsonPointer.getValue(jsonStructure);
+
+        return jsonObject.toString();
+    }
+
+    public String fetchFullJSON() {
+        JsonPointer jsonPointer = Json.createPointer("");
+        JsonObject jsonObject = (JsonObject) jsonPointer.getValue(jsonStructure);
+
+        return jsonObject.toString();
+
+    }
+
+    public boolean check(String key) {
+        JsonPointer jsonPointer = Json.createPointer("/" + key);
+        boolean found = jsonPointer.containsValue(jsonStructure);
+
+        return found;
+    }
 }
