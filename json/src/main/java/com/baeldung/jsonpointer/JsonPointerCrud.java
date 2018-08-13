66524049899e508b9002ac522b62d44d93a57021
Darragh Pigott <darragh@acornsoftware.ie>
2018-08-13 21:32:40 +0100

    Fix imports

--- json/src/main/java/com/baeldung/jsonpointer/JsonPointerCrud.java
+++ json/src/main/java/com/baeldung/jsonpointer/JsonPointerCrud.java
@@ -1,7 +1,6 @@
 package com.baeldung.jsonpointer;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
