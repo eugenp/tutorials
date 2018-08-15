d29f186ab2d3a46b955b3c73f2e477bfd7db2dee
Andrey Shcherbakov <veontomo@gmail.com>
2018-08-15 19:07:23 +0200

    Uncomment the benchmarks and remove unused class

--- core-java-8/src/main/java/com/baeldung/primitive/BenchmarkRunner.java
+++ core-java-8/src/main/java/com/baeldung/primitive/BenchmarkRunner.java
@@ -3,18 +3,18 @@ package com.baeldung.primitive;
 public class BenchmarkRunner {
 
     public static void main(String[] args) throws Exception {
-//        new IntPrimitiveLookup().run();
-//        new IntegerWrapperLookup().run();
-//        new FloatWrapperLookup().run();
-//        new FloatPrimitiveLookup().run();
-//        new DoublePrimitiveLookup().run();
-//        new DoubleWrapperLookup().run();
-//        new ShortPrimitiveLookup().run();
-//        new ShortWrapperLookup().run();
-//        new BooleanPrimitiveLookup().run();
-//        new BooleanWrapperLookup().run();
-//        new CharPrimitiveLookup().run();
-//        new CharacterWrapperLookup().run();
+        new IntPrimitiveLookup().run();
+        new IntegerWrapperLookup().run();
+        new FloatWrapperLookup().run();
+        new FloatPrimitiveLookup().run();
+        new DoublePrimitiveLookup().run();
+        new DoubleWrapperLookup().run();
+        new ShortPrimitiveLookup().run();
+        new ShortWrapperLookup().run();
+        new BooleanPrimitiveLookup().run();
+        new BooleanWrapperLookup().run();
+        new CharPrimitiveLookup().run();
+        new CharacterWrapperLookup().run();
         new BytePrimitiveLookup().run();
         new ByteWrapperLookup().run();
     }
