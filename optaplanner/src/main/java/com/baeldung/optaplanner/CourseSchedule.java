88b03694481a7a05e730e1f1a83a636333a6d25a
Kyle Doyle <kdoyle8597@gmail.com>
2018-08-17 15:54:42 -0400

    BAEL-801 A Guide to OptaPlanner

---
+++ optaplanner/src/main/java/com/baeldung/optaplanner/CourseSchedule.java
@@ -0,0 +1,57 @@
+package com.baeldung.optaplanner;
+
+import org.optaplanner.core.api.domain.solution.PlanningEntityCollectionProperty;
+import org.optaplanner.core.api.domain.solution.PlanningScore;
+import org.optaplanner.core.api.domain.solution.PlanningSolution;
+import org.optaplanner.core.api.domain.solution.drools.ProblemFactCollectionProperty;
+import org.optaplanner.core.api.domain.valuerange.ValueRangeProvider;
+import org.optaplanner.core.api.score.buildin.hardsoft.HardSoftScore;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@PlanningSolution
+public class CourseSchedule {
+
+    private List<Integer> roomList;
+    private List<Integer> periodList;
+    private List<Lecture> lectureList;
+    private HardSoftScore score;
+
+    public CourseSchedule(){
+        roomList = new ArrayList<>();
+        periodList = new ArrayList<>();
+        lectureList = new ArrayList<>();
+    }
+
+    @ValueRangeProvider(id = "availableRooms")
+    @ProblemFactCollectionProperty
+    public List<Integer> getRoomList() {
+        return roomList;
+    }
+
+    @ValueRangeProvider(id = "availablePeriods")
+    @ProblemFactCollectionProperty
+    public List<Integer> getPeriodList() {
+        return periodList;
+    }
+
+    @PlanningEntityCollectionProperty
+    public List<Lecture> getLectureList() {
+        return lectureList;
+    }
+
+    @PlanningScore
+    public HardSoftScore getScore() {
+        return score;
+    }
+
+    public void setScore(HardSoftScore score) {
+        this.score = score;
+    }
+
+    public void printCourseSchedule() {
+        lectureList.stream().map(c -> "Lecture in Room " + c.getRoomNumber().toString() + " during Period " + c.getPeriod().toString()).forEach(System.out::println);
+    }
+
+}
---
+++ optaplanner/src/test/java/com/baeldung/optaplanner/test/OptaPlannerUnitTest.java
@@ -0,0 +1,55 @@
+package com.baeldung.optaplanner.test;
+
+import com.baeldung.optaplanner.CourseSchedule;
+import com.baeldung.optaplanner.Lecture;
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.optaplanner.core.api.solver.Solver;
+import org.optaplanner.core.api.solver.SolverFactory;
+import java.util.Arrays;
+
+public class OptaPlannerUnitTest {
+
+    static SolverFactory<CourseSchedule> solverFactory;
+    static CourseSchedule unsolvedCourseSchedule;
+    static CourseSchedule solvedCourseSchedule;
+    static Solver<CourseSchedule> solver;
+
+    @BeforeAll
+    public static void setUp() {
+
+        unsolvedCourseSchedule = new CourseSchedule();
+
+        for(int i = 0; i < 10; i++){
+            unsolvedCourseSchedule.getLectureList().add(new Lecture());
+        }
+
+        unsolvedCourseSchedule.getPeriodList().addAll(Arrays.asList(new Integer[] { 1, 2, 3 }));
+        unsolvedCourseSchedule.getRoomList().addAll(Arrays.asList(new Integer[] { 1, 2 }));
+    }
+
+    @Test
+    public void test_whenCustomJavaSolver() {
+
+        SolverFactory<CourseSchedule> solverFactory = SolverFactory.createFromXmlResource("courseScheduleSolverConfiguration.xml");
+        solver = solverFactory.buildSolver();
+        CourseSchedule solvedCourseSchedule = solver.solve(unsolvedCourseSchedule);
+
+        Assert.assertNotNull(solvedCourseSchedule.getScore());
+
+        solvedCourseSchedule.printCourseSchedule();
+    }
+
+    @Test
+    public void test_whenDroolsSolver() {
+
+        SolverFactory<CourseSchedule> solverFactory = SolverFactory.createFromXmlResource("courseScheduleSolverConfigDrools.xml");
+        solver = solverFactory.buildSolver();
+        CourseSchedule solvedCourseSchedule = solver.solve(unsolvedCourseSchedule);
+
+        Assert.assertNotNull(solvedCourseSchedule.getScore());
+
+        solvedCourseSchedule.printCourseSchedule();
+    }
+}
