3c334fdd2e8b6589a4b50d340024c84bdefd1a56
Nguyen Nam Thai <namthainguyen@gmail.com>
2018-08-18 14:08:52 +0700

    Remove Maven integration tests in the old module

--- testing-modules/maven-integration-test/src/main/java/com/baeldung/maven/integration/EndpointConfig.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.maven.integration;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class EndpointConfig extends ResourceConfig {
-    public EndpointConfig() {
-        register(RestEndpoint.class);
-    }
-}
--- testing-modules/maven-integration-test/src/main/java/com/baeldung/maven/integration/Integration.java
+++
@@ -1,4 +0,0 @@
-package com.baeldung.maven.integration;
-
-public interface Integration {
-}
