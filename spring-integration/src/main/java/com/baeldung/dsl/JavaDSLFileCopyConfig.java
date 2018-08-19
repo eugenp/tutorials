92363a7c1ed3d6127d997a4c297bb69f99f00671
Dhawal Kapil <dhawalkapil@gmail.com>
2018-08-19 11:03:31 +0530

    BAEL-1790 Spring Integration Java DSL
    -Updated bean configurations and names

--- spring-integration/src/main/java/com/baeldung/dsl/JavaDSLFileCopyConfig.java
+++ spring-integration/src/main/java/com/baeldung/dsl/JavaDSLFileCopyConfig.java
@@ -9,22 +9,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.integration.annotation.IntegrationComponentScan;
-import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.PriorityChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.core.GenericSelector;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
 import org.springframework.integration.dsl.Pollers;
-import org.springframework.integration.dsl.channel.MessageChannels;
 import org.springframework.integration.file.FileReadingMessageSource;
 import org.springframework.integration.file.FileWritingMessageHandler;
-import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
 /**
  * JavaDSLFileCopyConfig contains various Integration Flows created from various spring integration components.
  * Activate only one flow at a time by un-commenting @Bean annotation from IntegrationFlow beans. 
+ * <p>
+ * Different flows are :<br>
+ * - {@link #fileMover()} - default app - activated<br>
+ * - {@link #fileMoverWithLambda()} - app with file writing expressions as lambda<br>
+ * - {@link #fileMoverWithPriorityChannel()} - app with priority channel<br>
+ * - {@link #fileReader()}, {@link #fileWriter()}, {@link #anotherFileWriter()} - app with bridge
  */
 @Configuration
 @EnableIntegration
@@ -32,147 +36,97 @@ import org.springframework.messaging.MessageHandler;
 public class JavaDSLFileCopyConfig {
 
     public static final String INPUT_DIR = "source";
-    public static final String OUTPUT_DIR = "target_first";
-    public static final String OUTPUT_DIR2 = "target_second";
+    public static final String OUTPUT_DIR = "target";
+    public static final String OUTPUT_DIR2 = "target2";
 
     @Bean
-    public MessageSource<File> anInputSource() {
+    public MessageSource<File> sourceDirectory() {
         FileReadingMessageSource messageSource = new FileReadingMessageSource();
         messageSource.setDirectory(new File(INPUT_DIR));
         return messageSource;
     }
 
     @Bean
-    public GenericSelector<File> aFilter() {
+    public GenericSelector<File> onlyJpgs() {
         return new GenericSelector<File>() {
 
             @Override
             public boolean accept(File source) {
-                return source.getName().endsWith(".jpg");
+              return source.getName()
+                .endsWith(".jpg");
             }
         };
     }
 
     @Bean
-    public MessageHandler aServiceActivator() {
+    public MessageHandler targetDirectory() {
         FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(OUTPUT_DIR));
         handler.setExpectReply(false); // end of pipeline, reply not needed
         return handler;
     }
     
     @Bean
-    public IntegrationFlow flow() {
-        return IntegrationFlows.from(anInputSource(), configurer -> configurer.poller(Pollers.fixedDelay(10000)))
-            .filter(aFilter())
-            .handle(aServiceActivator())
+    public IntegrationFlow fileMover() {
+        return IntegrationFlows.from(sourceDirectory(), configurer -> configurer.poller(Pollers.fixedDelay(10000)))
+            .filter(onlyJpgs())
+            .handle(targetDirectory())
             .get();
     }
 
     // @Bean
-    public IntegrationFlow flowWithLambda() {
-        return IntegrationFlows.from(anInputSource(), configurer -> configurer.poller(Pollers.fixedDelay(10000)))
-            .filter(source -> ((File) source).getName()
+    public IntegrationFlow fileMoverWithLambda() {
+        return IntegrationFlows.from(sourceDirectory(), configurer -> configurer.poller(Pollers.fixedDelay(10000)))
+            .filter(message -> ((File) message).getName()
                 .endsWith(".jpg"))
-            .handle(aServiceActivator())
+            .handle(targetDirectory())
             .get();
+    }
+
+    @Bean
+    public PriorityChannel alphabetically() {
+        return new PriorityChannel(1000, (left, right) -> ((File) left.getPayload()).getName()
+            .compareTo(((File) right.getPayload()).getName()));
     }
 
     // @Bean
-    public IntegrationFlow flowWithPoller() {
-        return IntegrationFlows.from(anInputSource(), configurer -> configurer.poller(Pollers.fixedDelay(10000)))
-            .filter(aFilter())
-            .channel(aChannel())
-            .bridge()
-            .channel(destinationChannel1())
-            .channel(destinationChannel2())
-            .handle(aServiceActivator())
+    public IntegrationFlow fileMoverWithPriorityChannel() {
+        return IntegrationFlows.from(sourceDirectory())
+            .filter(onlyJpgs())
+            .channel("alphabetically")
+            .handle(targetDirectory())
             .get();
     }
 
     @Bean
-    public MessageChannel aChannel() {
-        return new DirectChannel();
-    }
-
-    // @Bean
-    public IntegrationFlow flowInitializeFromChannel() {
-        return IntegrationFlows.from(aChannel())
-            .filter(aFilter())
-            .handle(aServiceActivator())
-            .get();
-    }
-
-    // @Bean
-    public IntegrationFlow flowPipeChannel() {
-        return IntegrationFlows.from(anInputSource(), configurer -> configurer.poller(Pollers.fixedDelay(10000)))
-            .filter(aFilter())
-            .channel(aChannel())
-            .handle(aServiceActivator())
-            .get();
-    }
-
-    @Bean
-    public MessageChannel sourceChannel() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MessageChannel destinationChannel1() {
-        return new DirectChannel();
-    }
-
-    @Bean
-    public MessageChannel destinationChannel2() {
-        return new DirectChannel();
-    }
-
-    // @Bean
-    public IntegrationFlow flowWithBridge() {
-        return IntegrationFlows.from(anInputSource())
-            .filter(aFilter())
-            .channel(sourceChannel())
-            .bridge()
-            .channel(destinationChannel1())
-            .channel(destinationChannel2())
-            .handle(aServiceActivator())
-            .get();
-    }
-    
-    @Bean
-    public MessageHandler aServiceActivator2() {
+    public MessageHandler anotherTargetDirectory() {
         FileWritingMessageHandler handler = new FileWritingMessageHandler(new File(OUTPUT_DIR2));
         handler.setExpectReply(false); // end of pipeline, reply not needed
         return handler;
     }
 
-    //@Bean
-    public MessageChannel filesQueueChannel() {
-        return MessageChannels.queue().get();
+    // @Bean
+    public IntegrationFlow fileReader() {
+        return IntegrationFlows.from(sourceDirectory())
+            .filter(onlyJpgs())
+            .channel("holdingTank")
+            .get();
     }
-    
-	//@Bean
-	public IntegrationFlow flowToQueue() {
-		return IntegrationFlows.from(anInputSource(), configurer -> configurer.poller(Pollers.fixedDelay(10)))
-		    .filter(aFilter())
-		    .channel(filesQueueChannel())
-		    .get();
-	}
 
-	//@Bean
-	public IntegrationFlow flowCopyToFirstDirectory() {
-		return IntegrationFlows.from(filesQueueChannel())
-		    .bridge(e -> e.poller(Pollers.fixedRate(1, TimeUnit.SECONDS, 20)))
-		    .handle(aServiceActivator())
-		    .get();
-	}
+    // @Bean
+    public IntegrationFlow fileWriter() {
+        return IntegrationFlows.from("holdingTank")
+            .bridge(e -> e.poller(Pollers.fixedRate(1, TimeUnit.SECONDS, 20)))
+            .handle(targetDirectory())
+            .get();
+    }
 
-	@Bean
-	public IntegrationFlow flowCopyToSecondDirectory() {
-		return IntegrationFlows.from(filesQueueChannel())
-		    .bridge(e -> e.poller(Pollers.fixedRate(2, TimeUnit.SECONDS, 10)))
-		    .handle(aServiceActivator2())
-		    .get();
-	}
+    // @Bean
+    public IntegrationFlow anotherFileWriter() {
+        return IntegrationFlows.from("holdingTank")
+            .bridge(e -> e.poller(Pollers.fixedRate(2, TimeUnit.SECONDS, 10)))
+            .handle(anotherTargetDirectory())
+            .get();
+    }
 
     public static void main(final String... args) {
         final AbstractApplicationContext context = new AnnotationConfigApplicationContext(JavaDSLFileCopyConfig.class);
