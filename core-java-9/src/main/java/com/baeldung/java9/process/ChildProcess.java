a79d597be491f598ae670216ab179214f5ed947d
amitabhmandal <amitabhmandal@192.168.1.13>
2018-08-20 20:20:09 +0530

    dummy commit

--- core-java-9/src/main/java/com/baeldung/java9/process/ChildProcess.java
+++ core-java-9/src/main/java/com/baeldung/java9/process/ChildProcess.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ChildProcess {
 
     public static void main(String[] args) {
+        @SuppressWarnings("resource")
         Scanner input = new Scanner(System.in);
         System.out.println("Child received: " + input.nextLine());
     }
