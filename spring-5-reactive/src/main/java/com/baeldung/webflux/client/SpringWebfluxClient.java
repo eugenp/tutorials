da243134213188e8fe068aa3052a5fd9d0adac62
Jan Hauer <mail@Jan-Hauer.de>
2018-08-21 00:10:37 +0200

    EVAL-jan.hauer: Adding client

---
+++ spring-5-reactive/src/main/java/com/baeldung/webflux/client/SpringWebfluxClient.java
@@ -0,0 +1,29 @@
+package com.baeldung.webflux.client;
+
+import static org.apache.logging.log4j.LogManager.getLogger;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication(exclude = { ReactiveSecurityAutoConfiguration.class, ReactiveUserDetailsServiceAutoConfiguration.class })
+public class SpringWebfluxClient {
+
+    @Bean
+    InitializingBean logEvents() {
+        return () -> WebClient.create("http://localhost:9001")
+                .get().uri("/time")
+                .retrieve()
+                .bodyToFlux(Long.class)
+                .subscribe(value -> getLogger(this.getClass()).info("Received {}", value));
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SpringWebfluxClient.class).properties("server.port=9002").run(args);
+    }
+}
+
