d275ab77b2db2454df76e0b61d5ddc023241b55a
Kacper Koza <kkoza11@gmail.com>
2018-08-23 23:21:54 +0200

    BAEL-2122 | optional orelsethrow

--- core-java/src/test/java/com/baeldung/throwsexception/PersonRepositoryUnitTest.java
+++ core-java/src/test/java/com/baeldung/throwsexception/PersonRepositoryUnitTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.util.Optional;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
