3c4f97fbb3f28fc57b2983701e099d104b4daad1
amitabhmandal <amitabhmandal@192.168.1.13>
2018-08-24 18:28:38 +0530

    Replaced System.out.println with logging

--- core-java-9/src/main/java/com/baeldung/java9/process/OutputStreamExample.java
+++ core-java-9/src/main/java/com/baeldung/java9/process/OutputStreamExample.java
@@ -1,9 +1,13 @@
 package com.baeldung.java9.process;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class OutputStreamExample {
 
     public static void main(String[] args) {
-        System.out.println(sum(1, 2));
+        Logger log = Logger.getLogger(OutputStreamExample.class.getName());
+        log.log(Level.INFO, Integer.toString(sum(1,2)));
     }
 
     public static int sum(int a, int b) {
