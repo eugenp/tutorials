47c55294b7bbf419e44de4785a90de3c7175cfce
Kotharu <bkotharu@BANL1699b0a1d.local>
2018-08-26 21:52:54 +0530

    Added Spring Vault config with Environment

---
+++ spring-vault/src/main/java/org/baeldung/springvault/VaultEnvironmentConfig.java
@@ -0,0 +1,17 @@
+package org.baeldung.springvault;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.vault.authentication.ClientAuthentication;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
+import org.springframework.vault.config.AbstractVaultConfiguration;
+import org.springframework.vault.config.EnvironmentVaultConfiguration;
+
+@Configuration
+@PropertySource(value = { "vault-config.properties" })
+@Import(value = EnvironmentVaultConfiguration.class)
+public class VaultEnvironmentConfig {
+
+}
