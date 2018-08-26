30745738166d4553437b5b52ea229f4147aead0d
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-26 14:00:27 -0300

    Initial Commit

---
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.baeldung.application.repositories;
+
+import com.baeldung.application.entities.Customer;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    
+    List<Customer> findByLastName(String lastName);
+}
---
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/runners/CommandLineCrudRunner.java
@@ -0,0 +1,40 @@
+package com.baeldung.application.runners;
+
+import com.baeldung.application.entities.Customer;
+import com.baeldung.application.repositories.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommandLineCrudRunner implements CommandLineRunner {
+    
+    private static final Logger logger = LoggerFactory.getLogger(CommandLineCrudRunner.class);
+    private final CustomerRepository repository;
+    
+    @Autowired
+    public CommandLineCrudRunner(CustomerRepository repository) {
+        this.repository = repository;
+    }
+    
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Customer("John", "Doe"));
+        repository.save(new Customer("Jennifer", "Wilson"));
+        
+        logger.info("Customers found with findAll():");
+        repository.findAll().forEach(c -> logger.info(c.toString()));
+        
+        logger.info("Customer found with findById(1L):");
+        Customer customer = repository.findById(1L)
+                .orElseGet(() -> new Customer("Non-existing customer", ""));
+        logger.info(customer.toString());
+        
+        logger.info("Customer found with findByLastName('Wilson'):");
+        repository.findByLastName("Wilson").forEach(c -> {
+            logger.info(c.toString());
+        });
+    }
+}
