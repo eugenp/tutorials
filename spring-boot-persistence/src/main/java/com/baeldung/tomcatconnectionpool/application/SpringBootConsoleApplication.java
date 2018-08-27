8e59d23e2dfdd3c4c85e7afa5e9e79b6bc6ad779
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 04:56:23 -0300

    Initial Commit

---
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.tomcatconnectionpool.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConsoleApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsoleApplication.class);
+    }
+}
---
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/repositories/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.baeldung.tomcatconnectionpool.repositories;
+
+import com.baeldung.tomcatconnectionpool.entities.Customer;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    
+    List<Customer> findByLastName(String lastName);
+}
