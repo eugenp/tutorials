d81fdc58b9c9362e50850fcd5869d452b1258d66
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 01:09:44 -0300

    Update SpringBootConsoleApplication class

--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.baeldung.application"})
 public class SpringBootConsoleApplication {
     
     public static void main(String[] args) {
