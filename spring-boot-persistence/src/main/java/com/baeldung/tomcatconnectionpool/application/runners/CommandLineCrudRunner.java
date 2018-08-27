7c7edec88c41ddb396043a5fcb13c6ba62ca1935
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 02:52:05 -0300

    Update CommandLineCrudRunner.java

--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/runners/CommandLineCrudRunner.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/runners/CommandLineCrudRunner.java
@@ -1,7 +1,7 @@
 package com.baeldung.tomcatconnectionpool.application.runners;
 
-import com.baeldung.application.entities.Customer;
-import com.baeldung.application.repositories.CustomerRepository;
+import com.baeldung.tomcatconnectionpool.application.entities.Customer;
+import com.baeldung.tomcatconnectionpool.application.repositories.CustomerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
