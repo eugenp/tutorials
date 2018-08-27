cfd69193c99af29ff168a2e4b4cacf283126c978
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 01:38:13 -0300

    Update source files

--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
@@ -2,10 +2,8 @@ package com.baeldung.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.baeldung.application.entities"})
 public class SpringBootConsoleApplication {
     
     public static void main(String[] args) {
--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/repositories/CustomerRepository.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/repositories/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.baeldung.application.repositories;
 import com.baeldung.application.entities.Customer;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     
     List<Customer> findByLastName(String lastName);
