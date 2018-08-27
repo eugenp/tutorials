e280a36defdd5ebbaec5cd5167eb45ede5fc9971
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 06:06:17 -0300

    Update CustomerRepository.java

--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/repositories/CustomerRepository.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/repositories/CustomerRepository.java
@@ -3,9 +3,9 @@ package com.baeldung.tomcatconnectionpool.application.repositories;
 import com.baeldung.tomcatconnectionpool.application.entities.Customer;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
-@Repository
+@Component
 public interface CustomerRepository extends CrudRepository<Customer, Long> {
     
     List<Customer> findByLastName(String lastName);
