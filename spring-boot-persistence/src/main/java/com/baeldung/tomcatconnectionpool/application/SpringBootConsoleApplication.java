36b855e343f2b3e7df7fd6937c76de6c9b59db0c
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 02:02:08 -0300

    Update source files

--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/SpringBootConsoleApplication.java
@@ -2,9 +2,14 @@ package com.baeldung.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-public class SpringBootConsoleApplication {
+@ComponentScan({"com.baeldung.application"})
+@EntityScan("com.baeldung.application.entities")
+@EnableJpaRepositories("com.baeldung.application.repositories")public class SpringBootConsoleApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(SpringBootConsoleApplication.class);
