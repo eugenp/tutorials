ed77848ad89b6b01b65650dffce4b9709eb70321
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-08-27 03:13:25 -0300

    Update CommandLineCrudRunner.java

--- spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/runners/CommandLineCrudRunner.java
+++ spring-boot-persistence/src/main/java/com/baeldung/tomcatconnectionpool/application/runners/CommandLineCrudRunner.java
@@ -12,28 +12,28 @@ import org.springframework.stereotype.Component;
 public class CommandLineCrudRunner implements CommandLineRunner {
     
     private static final Logger logger = LoggerFactory.getLogger(CommandLineCrudRunner.class);
-    private final CustomerRepository repository;
+    private final CustomerRepository customerRepository;
     
     @Autowired
-    public CommandLineCrudRunner(CustomerRepository repository) {
-        this.repository = repository;
+    public CommandLineCrudRunner(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
     
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new Customer("John", "Doe"));
-        repository.save(new Customer("Jennifer", "Wilson"));
+        customerRepository.save(new Customer("John", "Doe"));
+        customerRepository.save(new Customer("Jennifer", "Wilson"));
         
         logger.info("Customers found with findAll():");
         repository.findAll().forEach(c -> logger.info(c.toString()));
         
         logger.info("Customer found with findById(1L):");
-        Customer customer = repository.findById(1L)
+        Customer customer = customerRepository.findById(1L)
                 .orElseGet(() -> new Customer("Non-existing customer", ""));
         logger.info(customer.toString());
         
         logger.info("Customer found with findByLastName('Wilson'):");
-        repository.findByLastName("Wilson").forEach(c -> {
+        customerRepository.findByLastName("Wilson").forEach(c -> {
             logger.info(c.toString());
         });
     }
