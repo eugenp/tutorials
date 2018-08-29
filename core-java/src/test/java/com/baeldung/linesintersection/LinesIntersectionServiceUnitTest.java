37518fb1392fb8cf95b1e65c3f2ac5a03ef38d93
Ekaterina Galkina <myluckagain@gmail.com>
2018-08-29 10:19:07 +0500

    cleaning

--- core-java/src/test/java/com/baeldung/linesintersection/LinesIntersectionServiceUnitTest.java
+++ core-java/src/test/java/com/baeldung/linesintersection/LinesIntersectionServiceUnitTest.java
@@ -3,10 +3,9 @@ package com.baeldung.linesintersection;
 import java.awt.Point;
 import java.util.Optional;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 public class LinesIntersectionServiceUnitTest {
     private LinesIntersectionService service = new LinesIntersectionService();
@@ -14,29 +13,27 @@ public class LinesIntersectionServiceUnitTest {
     @Test
     public void givenNotParallelLines_whenCalculatePoint_thenPresent() {
 
-        float m1 = 0f;
-        float b1 = 0f;
-        float m2 = 1f;
-        float b2 = -1f;
+        float m1 = 0;
+        float b1 = 0;
+        float m2 = 1;
+        float b2 = -1;
 
         Optional<Point> point = service.calculateIntersectionPoint(m1, b1, m2, b2);
 
-        assertThat(point.isPresent(), equalTo(true));
-        assertThat(point.get()
-            .getX(), closeTo(1, 0.001));
-        assertThat(point.get()
-            .getY(), closeTo(0, 0.001));
+        assertTrue(point.isPresent());
+        assertTrue(point.get().getX() == 1);
+        assertTrue(point.get().getY() == 0);
     }
 
     @Test
     public void givenParallelLines_whenCalculatePoint_thenEmpty() {
-        float m1 = 0.1999f;
-        float m2 = 0.1999f;
-        float b1 = 1.2f;
-        float b2 = 1.3f;
+        float m1 = 1;
+        float b1 = 0;
+        float m2 = 1;
+        float b2 = -1;
 
         Optional<Point> point = service.calculateIntersectionPoint(m1, b1, m2, b2);
 
-        assertThat(point.isPresent(), equalTo(false));
+        assertFalse(point.isPresent());
     }
 }
