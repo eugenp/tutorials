a111cd8b2159e1fc404aa892ab94b22cc30043a4
Mladen Rudic <Mladen.Rudic@crealogix.com>
2018-09-01 22:57:54 +0200

    A short example of real-time event streaming using Spring Webflux.

---
+++ spring-webflux-example/src/main/java/com/baeldung/model/Event.java
@@ -0,0 +1,15 @@
+package com.baeldung.model;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private long id;
+    private Date date;
+}
