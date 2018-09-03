601da62d7d19b22c9ef6504348a956a6b4bbdf1a
geroza <rozagerardo@gmail.com>
2018-09-02 23:30:00 -0300

    * added MVC example * added spring 5 webflux test

--- spring-5-reactive/src/main/java/com/baeldung/reactive/serversentevents/consumer/controller/ClientController.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/serversentevents/consumer/controller/ClientController.java
@@ -5,14 +5,13 @@ import java.time.LocalTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -24,48 +23,26 @@ public class ClientController {
     private static Logger logger = LoggerFactory.getLogger(ClientController.class);
     private WebClient client = WebClient.create("http://localhost:8081/sse-server");
 
-    @GetMapping("/launch-sse-from-sse-client")
+    @GetMapping("/launch-sse-client")
     public String launchSSEFromSSEWebClient() {
-        consumeSSEFromSSE();
+        consumeSSE();
         return "LAUNCHED EVENT CLIENT!!! Check the logs...";
     }
 
-    @GetMapping("/launch-flux-from-sse-client")
+    @GetMapping("/launch-flux-client")
     public String launchcFluxFromSSEWebClient() {
-        consumeFluxFromSSE();
-        return "LAUNCHED SSE CONTENT CLIENT!!! Check the logs...";
+        consumeFlux();
+        return "LAUNCHED EVENT CLIENT!!! Check the logs...";
     }
 
-    @GetMapping("/launch-flux-from-flux-client")
+    @GetMapping("/launch-sse-from-flux-endpoint-client")
     public String launchFluxFromFluxWebClient() {
-        consumeFluxFromFlux();
-        return "LAUNCHED EVENT CLIENT!!! Check the logs...";
-    }
-
-    @GetMapping("/launch-sse-from-flux-client")
-    public String launchSSEFromFluxWebClient() {
-        consumeSSEFromFlux();
-        return "LAUNCHED SSE CONTENT CLIENT!!! Check the logs...";
-    }
-
-    @GetMapping("/launch-mono-from-mono-client")
-    public String launchMonoFromMonoWebClient() {
-        consumeMonoFromMono();
+        consumeSSEFromFluxEndpoint();
         return "LAUNCHED EVENT CLIENT!!! Check the logs...";
     }
 
     @Async
-    public void consumeFluxFromSSE() {
-        Flux<String> stringStream = client.get()
-            .uri("/stream-sse")
-            .retrieve()
-            .bodyToFlux(String.class);
-
-        stringStream.subscribe(content -> logger.info("Current time: {} - Received content: {} ", LocalTime.now(), content), error -> logger.error("Error retrieving content: {}", error), () -> logger.info("Completed!!!"));
-    }
-
-    @Async
-    public void consumeSSEFromSSE() {
+    public void consumeSSE() {
         ParameterizedTypeReference<ServerSentEvent<String>> type = new ParameterizedTypeReference<ServerSentEvent<String>>() {
         };
 
@@ -79,7 +56,7 @@ public class ClientController {
     }
 
     @Async
-    public void consumeFluxFromFlux() {
+    public void consumeFlux() {
         Flux<String> stringStream = client.get()
             .uri("/stream-flux")
             .accept(MediaType.TEXT_EVENT_STREAM)
@@ -90,7 +67,7 @@ public class ClientController {
     }
 
     @Async
-    public void consumeSSEFromFlux() {
+    public void consumeSSEFromFluxEndpoint() {
         ParameterizedTypeReference<ServerSentEvent<String>> type = new ParameterizedTypeReference<ServerSentEvent<String>>() {
         };
 
@@ -102,16 +79,5 @@ public class ClientController {
 
         eventStream.subscribe(content -> logger.info("Current time: {} - Received SSE: name[{}], id [{}], content[{}] ", LocalTime.now(), content.event(), content.id(), content.data()), error -> logger.error("Error receiving SSE: {}", error),
             () -> logger.info("Completed!!!"));
-    }
-
-    @Async
-    public void consumeMonoFromMono() {
-        Mono<String> stringStream = client.get()
-            .uri("/stream-mono")
-            .accept(MediaType.TEXT_EVENT_STREAM)
-            .retrieve()
-            .bodyToMono(String.class);
-
-        stringStream.subscribe(content -> logger.info("Current time: {} - Received content: {} ", LocalTime.now(), content), error -> logger.error("Error retrieving content: {}", error), () -> logger.info("Completed!!!"));
     }
 }
--- spring-5-reactive/src/main/java/com/baeldung/reactive/serversentevents/server/controllers/ServerController.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/serversentevents/server/controllers/ServerController.java
@@ -3,13 +3,13 @@ package com.baeldung.reactive.serversentevents.server.controllers;
 import java.time.Duration;
 import java.time.LocalTime;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/sse-server")
@@ -26,14 +26,10 @@ public class ServerController {
                 .build());
     }
 
-    @GetMapping("/stream-flux")
+    @GetMapping(path = "/stream-flux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<String> streamFlux() {
         return Flux.interval(Duration.ofSeconds(1))
-            .map(sequence -> "Flux - " + LocalTime.now().toString());
-    }
-    
-    @GetMapping("/stream-mono")
-    public Mono<String> streamMono() {
-        return Mono.just("Mono - " + LocalTime.now().toString());
+            .map(sequence -> "Flux - " + LocalTime.now()
+                .toString());
     }
 }
