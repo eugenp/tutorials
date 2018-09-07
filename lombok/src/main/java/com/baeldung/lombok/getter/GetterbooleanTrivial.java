a5d30aba329805394f2149304d78a782214afa17
codehunter34 <codehunter34@github.com>
2018-09-06 20:48:39 -0400

    BAEL-2075: Lombok @Getter for boolean field

---
+++ lombok/src/main/java/com/baeldung/lombok/getter/GetterbooleanTrivial.java
@@ -0,0 +1,16 @@
+package com.baeldung.lombok.getter;
+
+
+import lombok.Getter;
+
+/**
+ * Related Article Sections:
+ * 3. Using @Getter on a boolean Field
+ *
+ */
+public class GetterbooleanTrivial {
+
+    @Getter
+    private boolean running;
+
+}
