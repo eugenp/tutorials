7ba5b99925d0cf16d34ac0b0e1618f4ba7528846
Mike Wojtyna <mike@buildmysoftware.pro>
2018-09-08 02:05:31 +0200

    Add BAEL-2067 DomainEvents examples

---
+++ spring-data-jpa/src/main/java/com/baeldung/ddd/event/Aggregate3.java
@@ -0,0 +1,23 @@
+/**
+ *
+ */
+package com.baeldung.ddd.event;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.data.domain.AbstractAggregateRoot;
+
+@Entity
+public class Aggregate3 extends AbstractAggregateRoot<Aggregate3> {
+    @Id
+    @GeneratedValue
+    private long id;
+
+    public void domainOperation() {
+        // some domain operation
+        registerEvent(new DomainEvent());
+    }
+
+}
