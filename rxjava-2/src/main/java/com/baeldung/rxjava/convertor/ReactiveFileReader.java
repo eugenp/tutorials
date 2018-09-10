3f0e91c3232e91dc7bf64ec63581a01400abc06e
Shanshan Wang <mmchsusan@gmail.com>
2018-09-10 11:07:16 -0400

    Revert "Imporve readability"
    This reverts commit 3352c0c27ba1967e7f7840adb4866a75f18bdfa8.

--- rxjava-2/src/main/java/com/baeldung/rxjava/convertor/ReactiveFileReader.java
+++ rxjava-2/src/main/java/com/baeldung/rxjava/convertor/ReactiveFileReader.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousFileChannel;
 import java.nio.channels.CompletionHandler;
@@ -38,6 +39,28 @@ public class ReactiveFileReader {
     }
 
     public Flowable<String> readFileConvertSyncToObservablesByGenerate() {
+
+        return Flowable.generate(() -> new BufferedReader(new FileReader(file)), (reader, emitter) -> {
+            final String line = reader.readLine();
+            if (line != null) {
+                emitter.onNext(line);
+            } else {
+                emitter.onComplete();
+            }
+        }, reader -> reader.close());
+    }
+
+    Flowable<String> readFileConvertSyncToObservablesFromIterable() {
+        return Flowable.fromIterable(() -> {
+            try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                return reader.lines().iterator();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        });
+    }
+
+    Flowable<String> readFileConvertSyncToObservablesByGenerate() {
         return Flowable.generate(() -> new BufferedReader(new FileReader(file)), (reader, emitter) -> {
             try {
                 final String line = reader.readLine();
@@ -51,51 +74,6 @@ public class ReactiveFileReader {
             }
 
         }, BufferedReader::close);
-    }
-
-    public Flowable<String> readFileConvertSyncToObservablesFromIterable() {
-        return Flowable.fromIterable(() -> {
-            try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
-                return reader.lines().iterator();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        });
-    }
-
-    public Flowable<String> readFileConvertAsyncToObservablesByCreate() {
-        return Single.<String> create(emitter -> {
-            AsynchronousFileChannel channel = AsynchronousFileChannel.open(file.toPath());
-            
-            channel.read(buffer, 0, buffer, new CompletionHandler<Integer, ByteBuffer>() {
-
-                @Override
-                public void completed(Integer result, ByteBuffer attachment) {
-                    try {
-                        channel.close();
-                    } catch (IOException e) {
-                        emitter.onError(e);
-                        return;
-                    }
-                    attachment.flip();
-                    byte[] data = new byte[attachment.limit()];
-                    attachment.get(data);
-                    emitter.onSuccess(new String(data));
-                    attachment.clear();
-                }
-
-                @Override
-                public void failed(Throwable error, ByteBuffer attachment) {
-                    try {
-                        channel.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    emitter.onError(error);
-                }
-            });
-        })
-            .toFlowable();
     }
 
     public Flowable<Integer> readFileConvertAsyncToObservablesFromFuture() throws IOException {
