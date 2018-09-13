1e23745b1f63dd665b48ba79558d27c38fc0440a
mprevisic <markoprevisic@gmail.com>
2018-09-14 00:15:29 +0200

    implement controller and client

--- spring-5-reactive/src/main/java/com/baeldung/reactive/client/CpuUsageClient.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/client/CpuUsageClient.java
@@ -1,10 +1,27 @@
 package com.baeldung.reactive.client;
 
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.baeldung.reactive.model.CpuUsage;
+
+import reactor.core.publisher.Flux;
+
 public class CpuUsageClient {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
+    public void runClient() {
+        WebClient client = WebClient.create("http://localhost:8080");
 
+        Flux<CpuUsage> cpuUsageFlux = client.get()
+            .uri("/cpu-usage")
+            .retrieve()
+            .bodyToFlux(CpuUsage.class);
+
+        cpuUsageFlux.subscribe(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        CpuUsageClient cpuClient = new CpuUsageClient();
+        cpuClient.runClient();
     }
 
 }
--- spring-5-reactive/src/main/java/com/baeldung/reactive/model/CpuUsage.java
+++ spring-5-reactive/src/main/java/com/baeldung/reactive/model/CpuUsage.java
@@ -1,5 +1,14 @@
 package com.baeldung.reactive.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
 public class CpuUsage {
+    
+    int usage;
+    
+    String severity;
 
 }
