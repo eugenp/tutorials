597602a47833af962dae2c32f5acc50ad6d9f6d2
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2018-09-13 22:37:40 +0200

    BAEL-2171 - java heap dumps

---
+++ core-java/src/main/java/com/baeldung/heapdump/HeapDump.java
@@ -0,0 +1,23 @@
+package com.baeldung.heapdump;
+
+import com.sun.management.HotSpotDiagnosticMXBean;
+
+import javax.management.MBeanServer;
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+
+public class HeapDump {
+
+    public static void dumpHeap(String filePath, boolean live) throws IOException {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        HotSpotDiagnosticMXBean mxBean = ManagementFactory.newPlatformMXBeanProxy(
+          server, "com.sun.management:type=HotSpotDiagnostic", HotSpotDiagnosticMXBean.class);
+        mxBean.dumpHeap(filePath, live);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final String file = "/tmp/dump.hprof";
+
+        dumpHeap(file, true);
+    }
+}
