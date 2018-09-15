3886936096e4dda4a61e6ffb2d61531d819d88ef
varahalarajup <43283567+varahalarajup@users.noreply.github.com>
2018-09-15 17:44:38 -0400

    Spring webflux client server hot stream demo

---
+++ spring-webflux-client-server/PricingPublisher/src/main/java/com/baledung/webflux/pricingpublisher/PricingPublisher/PricingPublisherApplication.java
@@ -0,0 +1,12 @@
+package com.baledung.webflux.pricingpublisher.PricingPublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingPublisherApplication.class, args);
+	}
+}
