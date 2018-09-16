889c52f1c276c6fa372a13ab1cae880b60733cd2
sachin29aug <sachin.sachdeva@gmail.com>
2018-09-15 22:19:20 -0400

    BAEL-8939: Adding a Spring Context test in all Spring Modules (changes for around 27 more modules.. work in progress..)

---
+++ spring-mvc-java/src/test/java/org/baeldung/SpringContextIntegrationTest.java
@@ -0,0 +1,18 @@
+package org.baeldung;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baeldung.app.Application;
+import com.baeldung.rss.RssFeedApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {Application.class, RssFeedApplication.class})
+public class SpringContextIntegrationTest {
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
+}
