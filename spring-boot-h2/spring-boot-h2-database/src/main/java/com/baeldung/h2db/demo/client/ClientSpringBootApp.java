1dea2b8cbda63df1ac57083e97847c80c66f1c26
micropatel <31759369+micropatel@users.noreply.github.com>
2018-09-20 22:57:08 -0300

    BAEL-1980 Access an in-memory h2 db from 2 Spring Boot applications (#5230)
    * Added Spring-Boot-H2 project
    
    * Optimized imports and formatted code
    
    * Code Formatting changes
    
    * Renamed the client applicaiton
    
    * Removed Name from pom.xml
    
    * formatting changes - applied formatting style
    
    * added souts
    
    * Update SpringBootApp.java
    
    * Update ClientSpringBootApp.java
    
    * Fixed Formatting issues
    
    * Fixed formatting and spelling mistakes
    
    * added H2 shared db example
    
    * a
    
    * patch
    
    * test
    
    * Revert "Fixed formatting and spelling mistakes"
    
    This reverts commit ed56ededec903e85c2d9f2b55e7730682455d5f5.
    
    * Revert "test"
    
    This reverts commit 4a7133dbd1d181cc304348f63998924a9db5acfe.
    
    * Revert "patch"
    
    This reverts commit 404b9ebe74aef4faa2ad62d9867652b62a5445b8.
    
    * Revert "a"
    
    This reverts commit af77f841114e19b0a0eafb15530dbaaaf34b5493.
    
    * Revert "added H2 shared db example"
    
    This reverts commit e16918cae3813fe20172c7e90e27bbec34b5fb5f.
    
    * Updated to have client and server in same directory
    
    * Deleted old files
    
    * removed unwanted directory
    
    * formating
    
    * removed unwanted module
    
    * commentted removed module
    
    * Revert "commentted removed module"
    
    This reverts commit 211ff1441f6bde920d7af3f8cd52c2a682ba3967.
    
    * Revert "removed unwanted module"
    
    This reverts commit 657f9ee45e02269bcfdbf380b9a74ff2370857fc.
    
    * fixed pom.xml issue
    
    * typo fixed
    
    * fixed multiple main class issue

--- spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/demo/client/ClientSpringBootApp.java
+++ spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/demo/client/ClientSpringBootApp.java
@@ -1,37 +1,35 @@
-package com.baeldung.h2db.demo;
+package com.baeldung.h2db.demo.client;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 import javax.annotation.PostConstruct;
-import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 @SpringBootApplication
-public class SpringBootApp {
+@ComponentScan("com.baeldung.h2db.demo.client")
+public class ClientSpringBootApp {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApp.class, args);
+        System.setProperty("spring.datasource.url","jdbc:h2:tcp://localhost:9091/mem:mydb");
+        SpringApplication.run(ClientSpringBootApp.class, args);
     }
 
     @PostConstruct
     private void initDb() {
-        System.out.println(String.format(
-            "****** Creating table: %s, and Inserting test data ******", "Employees"));
-
+        System.out.println("****** Inserting more sample data in the table: Employees ******");
         String sqlStatements[] = {
-            "drop table employees if exists",
-            "create table employees(id serial,first_name varchar(255),last_name varchar(255))",
-            "insert into employees(first_name, last_name) values('Eugen','Paraschiv')",
-            "insert into employees(first_name, last_name) values('Scott','Tiger')"
+            "insert into employees(first_name, last_name) values('Donald','Trump')",
+            "insert into employees(first_name, last_name) values('Barack','Obama')"
         };
 
         Arrays.asList(sqlStatements).stream().forEach(sql -> {
@@ -51,10 +49,5 @@ public class SpringBootApp {
                     return null;
                 }
             });
-    }
-
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
     }
 }
--- spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/demo/server/SpringBootApp.java
+++ spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/demo/server/SpringBootApp.java
@@ -1,31 +1,38 @@
-package com.baeldung.h2db.demo;
+package com.baeldung.h2db.demo.server;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
 import javax.annotation.PostConstruct;
+import org.h2.tools.Server;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 @SpringBootApplication
-public class ClientSpringBootApp {
+@ComponentScan("com.baeldung.h2db.demo.server")
+public class SpringBootApp {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public static void main(String[] args) {
-        SpringApplication.run(ClientSpringBootApp.class, args);
+        SpringApplication.run(SpringBootApp.class, args);
     }
 
     @PostConstruct
     private void initDb() {
-        System.out.println("****** Inserting more sample data in the table: Employees ******");
+        System.out.println(String.format("****** Creating table: %s, and Inserting test data ******", "Employees"));
+
         String sqlStatements[] = {
-            "insert into employees(first_name, last_name) values('Donald','Trump')",
-            "insert into employees(first_name, last_name) values('Barack','Obama')"
+            "drop table employees if exists",
+            "create table employees(id serial,first_name varchar(255),last_name varchar(255))",
+            "insert into employees(first_name, last_name) values('Eugen','Paraschiv')",
+            "insert into employees(first_name, last_name) values('Scott','Tiger')"
         };
 
         Arrays.asList(sqlStatements).stream().forEach(sql -> {
@@ -45,5 +52,10 @@ public class ClientSpringBootApp {
                     return null;
                 }
             });
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server inMemoryH2DatabaseServer() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9091");
     }
 }
