81b34c7628273b45e3966c11e64528dd6899409d
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2018-09-25 01:22:37 -0300

    Update UserNotBlankUnitTest.java

--- javaxval/src/test/java/org/baeldung/javabeanconstraints/test/UserNotBlankUnitTest.java
+++ javaxval/src/test/java/org/baeldung/javabeanconstraints/test/UserNotBlankUnitTest.java
@@ -21,34 +21,43 @@ public class UserNotBlankUnitTest {
     @Test
     public void whenNotBlankName_thenNoConstraintViolations() {
         UserNotBlank user = new UserNotBlank("John");
+        
         Set<ConstraintViolation<UserNotBlank>> violations = validator.validate(user);
+        
         assertThat(violations.size()).isEqualTo(0);
     }
     
     @Test
     public void whenBlankName_thenOneConstraintViolation() {
         UserNotBlank user = new UserNotBlank(" ");
+        
         Set<ConstraintViolation<UserNotBlank>> violations = validator.validate(user);
+        
         assertThat(violations.size()).isEqualTo(1);
     }
     
     @Test
     public void whenEmptyName_thenOneConstraintViolation() {
         UserNotBlank user = new UserNotBlank("");
+        
         Set<ConstraintViolation<UserNotBlank>> violations = validator.validate(user);
+        
         assertThat(violations.size()).isEqualTo(1);
     }
     
     @Test
     public void whenNullName_thenOneConstraintViolation() {
         UserNotBlank user = new UserNotBlank(null);
+        
         Set<ConstraintViolation<UserNotBlank>> violations = validator.validate(user);
+        
         assertThat(violations.size()).isEqualTo(1);
     }
     
     @Test
     public void whenToString_thenCorrect() {
         UserNotBlank user = new UserNotBlank("John");
+        
         assertThat(user.toString()).isEqualTo("User{name=John}");
     }
 }
