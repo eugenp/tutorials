9d8b90794e2ae6ed4c189e612d24fdae6277bc89
Dhawal Kapil <dhawalkapil@gmail.com>
2018-09-25 23:22:58 +0530

    BAEL-8962 Fix surefire configs of twitterhdfs, spring cloud projects
    - Surefire config fixes
    - pom fixes and standardization
    - test names standardization
    - added packaging pom at spring-cloud-bus and spring-cloud/spring-cloud-task level

--- spring-cloud-bus/spring-cloud-config-server/src/test/java/com/baeldung/SpringCloudConfigServerApplicationIntegrationTest.java
+++ spring-cloud-bus/spring-cloud-config-server/src/test/java/com/baeldung/SpringCloudConfigServerApplicationIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCloudConfigServerApplicationTests {
+public class SpringCloudConfigServerApplicationIntegrationTest {
 
 	@Test
 	public void contextLoads() {
