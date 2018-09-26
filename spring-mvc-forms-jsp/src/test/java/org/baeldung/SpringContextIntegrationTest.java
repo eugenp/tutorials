0edea763ca749975e1998c75434cae8e8682d73c
sachin29aug <sachin.sachdeva@gmail.com>
2018-09-25 22:52:38 -0400

    Fixed the travis build

--- spring-mvc-forms-jsp/src/test/java/org/baeldung/SpringContextIntegrationTest.java
+++ spring-mvc-forms-jsp/src/test/java/org/baeldung/SpringContextIntegrationTest.java
@@ -1,5 +1,7 @@
 package org.baeldung;
 
+import com.baeldung.springmvcforms.configuration.ApplicationConfiguration;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
