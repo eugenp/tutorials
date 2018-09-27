74535cd94527e565b87d99ca4fa34bda36ee692c
Jakub Fedoruk <kubafedoruk@gmail.com>
2018-09-27 22:37:45 +0200

    BAEL-2200 Sample application for auto-configuration report.

---
+++ spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/auto/configuration/AutoConfigurationDemo.java
@@ -0,0 +1,15 @@
+package com.baeldung.h2db.auto.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.baeldung.h2db.auto-configuration")
+public class AutoConfigurationDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoConfigurationDemo.class, args);
+	}
+
+}
