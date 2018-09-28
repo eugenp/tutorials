d3ad4dcfc2ad1fe818973fbca7d47da80f5e36be
sachin29aug <sachin.sachdeva@gmail.com>
2018-09-27 22:52:50 -0400

    BAEL-9051: Fix failing unit tests

--- persistence-modules/spring-data-redis/src/test/java/org/baeldung/SpringContextIntegrationTest.java
+++ persistence-modules/spring-data-redis/src/test/java/org/baeldung/SpringContextIntegrationTest.java
@@ -4,13 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.baeldung.spring.data.redis.config.RedisConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({ "/RedirectionWebSecurityConfig.xml", "/mvc-servlet.xml" })
-@WebAppConfiguration
+@ContextConfiguration(classes = RedisConfig.class)
 public class SpringContextIntegrationTest {
-	@Test
-	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
-	}
+
+    @Test
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    }
 }
