aeb17113973b9d216a9cf9c0eb40fb79bc48d957
amit2103 <amit.pandey2103@gmail.com>
2018-09-29 23:22:40 +0530

    [BAEL-9018] - Fixed malformed POMs and remaining PMD problems

--- apache-avro/src/test/java/com/baeldung/avro/util/serealization/AvroSerealizerDeSerealizerUnitTest.java
+++ apache-avro/src/test/java/com/baeldung/avro/util/serealization/AvroSerealizerDeSerealizerUnitTest.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 
 import static org.junit.Assert.*;
 
-public class AvroSerealizerDeSerealizerTest {
+public class AvroSerealizerDeSerealizerUnitTest {
 
     AvroSerealizer serealizer;
     AvroDeSerealizer deSerealizer;
