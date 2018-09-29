fc5adcc8de426ae4c54c2e7b456ce4088258d7c7
Larry Chung <codewarrior2000@users.noreply.github.com>
2018-09-29 15:20:19 -0400

    Branched code for BAEL-2142
    Refactored WordIndexer and added JUnit test class WordIndexerTest according to Kevin Gilmore.

--- java-strings/src/main/java/com/baeldung/string/searching/WordIndexerTest.java
+++ java-strings/src/main/java/com/baeldung/string/searching/WordIndexerTest.java
@@ -1,9 +1,10 @@
 package BAEL2142;
 
-import org.junit.Before;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 public class WordIndexerTest {
 
@@ -11,39 +12,36 @@ public class WordIndexerTest {
     String theExpected;
     WordIndexer wordIndexer;
 
-    @Before
+    @BeforeEach
     public void setUp() throws Exception {
         wordIndexer = new WordIndexer();
-        theString = "To be, or not to be: that is the question:\n"
-          + "Whether 'tis nobler in the mind to suffer\n"
-          + "The slings and arrows of outrageous fortune,\n"
-          + "Or to take arms against a sea of troubles,\n"
-          + "And by opposing end them? To die: to sleep;\n"
-          + "No more; and by a sleep to say we end\n"
-          + "The heart-ache and the thousand natural shocks\n"
-          + "That flesh is heir to, 'tis a consummation\n"
-          + "Devoutly to be wish'd. To die, to sleep;\n"
-          + "To sleep: perchance to dream: ay, there's the rub;\n";
 
-        theExpected = "0 To be, or not to \n" +
-                "14 to be: that is th\n" +
-                "75 to suffer\n" +
-                "The sli\n" +
-                "133 to take arms agai\n" +
-                "199 To die: to sleep;\n" +
-                "207 to sleep;\n" +
-                "No more\n" +
-                "241 to say we end\n" +
-                "The\n" +
-                "321 to, 'tis a consum\n" +
-                "354 to be wish'd. To \n" +
-                "368 To die, to sleep;\n" +
-                "376 to sleep;\n" +
-                "To slee\n" +
-                "386 To sleep: perchan\n" +
-                "406 to dream: ay, the\n" +
-                "\n" +
-                "The string 'to' was found 13 times.";
+        theString = "To be, or not to be: that is the question:"
+          + "Whether 'tis nobler in the mind to suffer"
+          + "The slings and arrows of outrageous fortune,"
+          + "Or to take arms against a sea of troubles,"
+          + "And by opposing end them? To die: to sleep;"
+          + "No more; and by a sleep to say we end"
+          + "The heart-ache and the thousand natural shocks"
+          + "That flesh is heir to, 'tis a consummation"
+          + "Devoutly to be wish'd. To die, to sleep;"
+          + "To sleep: perchance to dream: ay, there's the rub;";
+
+        theExpected = "0 To be, or not to \n"
+          + "14 to be: that is th\n"
+          + "74 to sufferThe slin\n"
+          + "130 to take arms agai\n"
+          + "195 To die: to sleep;\n"
+          + "203 to sleep;No more;\n"
+          + "236 to say we endThe \n"
+          + "314 to, 'tis a consum\n"
+          + "346 to be wish'd. To \n"
+          + "360 To die, to sleep;\n"
+          + "368 to sleep;To sleep\n"
+          + "377 To sleep: perchan\n"
+          + "397 to dream: ay, the\n"
+          + "\n"
+          + "The string 'to' was found 13 times.";
     }
 
     @Test
