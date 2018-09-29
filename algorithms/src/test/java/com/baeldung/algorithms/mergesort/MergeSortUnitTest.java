575dcc38409110819d7af51a8189d8bab475c506
Vaibhav Sahay <vsahay28@gmail.com>
2018-09-29 16:15:54 +0530

    Updated the imports

---
+++ algorithms/src/test/java/com/baeldung/algorithms/mergesort/MergeSortUnitTest.java
@@ -0,0 +1,26 @@
+package com.baeldung.algorithms.mergesort;
+
+import org.junit.Assert;
+
+import org.junit.Test;
+
+public class MergeSortUnitTest {
+
+    @Test
+    public void positiveTest() {
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+        int[] expected = { 1, 2, 3, 4, 5, 6 };
+        MergeSort.mergeSort(actual, actual.length);
+        Assert.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void negativeTest() {
+        int[] actual = { 5, 1, 6, 2, 3, 4 };
+        int[] expected = { 1, 2, 3, 4, 6, 5 };
+        MergeSort.mergeSort(actual, actual.length);
+        Assert.assertArrayEquals(expected, actual);
+
+    }
+
+}
