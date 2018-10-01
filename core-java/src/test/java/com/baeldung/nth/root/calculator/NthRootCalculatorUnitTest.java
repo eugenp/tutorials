2784b3de4c7e610081dc44b4c4fbee137821265a
Tom Hombergs <tom.hombergs@gmail.com>
2018-10-01 21:06:19 +0200

    removed unnecessary Mockito and refactored test method name

--- core-java/src/test/java/com/baeldung/nth/root/calculator/NthRootCalculatorUnitTest.java
+++ core-java/src/test/java/com/baeldung/nth/root/calculator/NthRootCalculatorUnitTest.java
@@ -7,14 +7,12 @@ import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(MockitoJUnitRunner.class)
 public class NthRootCalculatorUnitTest {
 
-    @InjectMocks
-    private NthRootCalculator nthRootCalculator;
+    private NthRootCalculator nthRootCalculator = new NthRootCalculator();
 
     @Test
-    public void giventThatTheBaseIs125_andTheExpIs3_whenCalculateIsCalled_thenTheResultIsTheCorrectOne() {
+    public void whenBaseIs125AndNIs3_thenNthRootIs5() {
        Double result = nthRootCalculator.calculate(125.0, 3.0);
        assertEquals(result, (Double) 5.0d);
     }
