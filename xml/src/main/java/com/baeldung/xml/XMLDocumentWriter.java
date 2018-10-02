f8f343b001d9ef5436a1affd641ef45d1c9ff734
Priyesh Mashelkar <mashelkar.priyesh@gmail.com>
2018-10-02 23:48:28 +0100

    Added implementation and test class

--- xml/src/main/java/com/baeldung/xml/XMLDocumentWriter.java
+++ xml/src/main/java/com/baeldung/xml/XMLDocumentWriter.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
@@ -14,20 +16,25 @@ import org.w3c.dom.Document;
 
 public class XMLDocumentWriter {
     
-    public void write(Document document, String fileName) {
-        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    public void write(Document document, String fileName, boolean excludeDeclaration, boolean prettyPrint) {
         try {
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
-        } catch (TransformerConfigurationException e) {
-            return;
-        }
-                
-        try {
+            if(excludeDeclaration) {
+                transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            }
+            if(prettyPrint) {
+                transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+            }
             DOMSource source = new DOMSource(document);
             FileWriter writer = new FileWriter(new File(fileName));
             StreamResult result = new StreamResult(writer);
+            transformer.transform(source, result);
         } catch (IOException e) {
-            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
             e.printStackTrace();
         }
     }
---
+++ xml/src/test/java/com/baeldung/xml/XMLDocumentWriterUnitTest.java
@@ -0,0 +1,22 @@
+package com.baeldung.xml;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.junit.Test;
+import org.w3c.dom.Document;
+
+public class XMLDocumentWriterUnitTest {
+
+    @Test
+    public void givenXMLDocumentWhenWriteIsCalledThenXMLIsWrittenToFile() throws Exception {
+        DocumentBuilder documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputStream stringInputStream = new ByteArrayInputStream("<Company><Department name=\"Sales\"><Employee name=\"John Smith\"/><Employee name=\"Tim Dellor\"/></Department></Company>".getBytes());
+        Document document = documentBuilder.parse(stringInputStream);
+        new XMLDocumentWriter().write(document, "company.xml", false, false);
+    }
+
+}
