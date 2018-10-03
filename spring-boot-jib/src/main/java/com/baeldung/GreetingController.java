ad0a1a3d28946cd110988bc609ff765482a29b81
shreyas <mahajan.shreyas@gmail.com>
2018-10-03 23:55:44 +0530

    BAEL-2000 - Shreyas Mahajan - Incorporating the changes

--- spring-boot-jib/src/main/java/com/baeldung/GreetingController.java
+++ spring-boot-jib/src/main/java/com/baeldung/GreetingController.java
@@ -1,6 +1,6 @@
 package com.baeldung;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +12,7 @@ public class GreetingController {
     private static final String template = "Hello Docker, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
           String.format(template, name));
