7b973b1a85f9e783ff8563e7c3e0e95ed78013fd
Krzysztof Majewski <majewski.kielce@gmail.com>
2018-10-03 11:02:47 +0200

    change file name

--- java-numbers/src/test/java/com/baeldung/maths/MathSinUnitTest.java
+++ java-numbers/src/test/java/com/baeldung/maths/MathSinUnitTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-public class BigDecimalDemoUnitTest {
+public class MathSinUnitTest {
 
     @Test
     public void givenAnAngleInDegrees_whenUsingToRadians_thenResultIsInRadians() {
