8ce5edf4345ecb7a2df81c5d654bc542c5bf7bcb
Aditya Kulkarni <adityapk@gmail.com>
2018-10-08 14:19:43 -0700

    Hexagonal Architecture

---
+++ hexagonalarchitecture/src/main/java/com/baeldung/hexagonal/HexagonalArchitecture.java
@@ -0,0 +1,54 @@
+package com.baeldung.hexagonal;
+
+public class HexagonalArchitecture {
+
+	public interface Printer {
+		void print(Cube cube);
+	};
+
+	public static class LengthPrinter implements Printer {
+//		@Override
+		public void print(Cube cube) {
+			System.out.println("Cube of side length = " + cube.getLength());
+		}
+	}
+
+	public static class VolumePrinter implements Printer {
+//		@Override
+		public void print(Cube cube) {
+			double volume = Math.pow(cube.getLength(), 3);
+			System.out.println("Cube of volume = " + volume);
+		}
+	}
+
+	public static class Cube {
+		private int sideLength;
+		private Printer printer;
+
+		public Cube() {
+			printer = new LengthPrinter();	// Set the default printer
+		}
+
+		public int getLength() {
+			return sideLength;
+		}
+
+		public void updateAndPrint(int newSideLength) {
+			sideLength = newSideLength;
+			printer.print(this);
+		}
+
+		public void updatePrinter(Printer newPrinter) {
+			this.printer = newPrinter;
+		}
+	}
+
+	public static void main(String[] args) {
+		Cube c = new Cube();
+		c.updateAndPrint(4);		// Prints "Cube of side length = 4"
+		
+		c.updatePrinter(new VolumePrinter());
+		c.updateAndPrint(6);		// Prints "Cube of volume = 216.0"
+	}
+
+}
