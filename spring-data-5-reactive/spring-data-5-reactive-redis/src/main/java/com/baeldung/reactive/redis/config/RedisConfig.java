31c0134c22753ac9b65f3d066d76de4e4c052783
shreyasht <shreyash.thakare@gmail.com>
2018-10-08 16:34:47 +0530

    Chnages as per suggestion from Jose.

--- spring-data-5-reactive/spring-data-5-reactive-redis/src/main/java/com/baeldung/reactive/redis/config/RedisConfig.java
+++ spring-data-5-reactive/spring-data-5-reactive-redis/src/main/java/com/baeldung/reactive/redis/config/RedisConfig.java
@@ -1,30 +1,26 @@
 package com.baeldung.reactive.redis.config;
 
-import com.baeldung.reactive.redis.model.Employee;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.redis.connection.*;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.ReactiveKeyCommands;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.ReactiveStringCommands;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-import javax.annotation.PreDestroy;
+import com.baeldung.reactive.redis.model.Employee;
 
 @Configuration
 public class RedisConfig {
 
     @Autowired
     RedisConnectionFactory factory;
-
-    @Bean
-    @Primary
-    public ReactiveRedisConnectionFactory reactiveRedisConnectionFactory() {
-        return new LettuceConnectionFactory();
-    }
 
     @Bean
     public ReactiveRedisTemplate<String, Employee> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
@@ -42,12 +38,14 @@ public class RedisConfig {
 
     @Bean
     public ReactiveKeyCommands keyCommands(final ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
-        return reactiveRedisConnectionFactory.getReactiveConnection().keyCommands();
+        return reactiveRedisConnectionFactory.getReactiveConnection()
+            .keyCommands();
     }
 
     @Bean
     public ReactiveStringCommands stringCommands(final ReactiveRedisConnectionFactory reactiveRedisConnectionFactory) {
-        return reactiveRedisConnectionFactory.getReactiveConnection().stringCommands();
+        return reactiveRedisConnectionFactory.getReactiveConnection()
+            .stringCommands();
     }
 
     @PreDestroy
