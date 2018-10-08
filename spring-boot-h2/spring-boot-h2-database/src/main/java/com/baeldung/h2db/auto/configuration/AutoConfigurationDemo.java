7ea359ee1d953a2adf94ac31391e8676951cdcd0
Jakub Fedoruk <jakub.fedoruk@pragmatists.com>
2018-10-08 20:59:38 +0200

    BAEL-2200 Sample application for auto-configuration report fixes.

--- spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/auto/configuration/AutoConfigurationDemo.java
+++ spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/h2db/auto/configuration/AutoConfigurationDemo.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("com.baeldung.h2db.auto-configuration")
+@SpringBootApplication(scanBasePackages = "com.baeldung.h2db.auto-configuration")
 public class AutoConfigurationDemo {
 
 	public static void main(String[] args) {
