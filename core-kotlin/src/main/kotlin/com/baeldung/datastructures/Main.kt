4096a3f1d91bea16ef6773e90d0ec05bb26e7618
Andrey Shcherbakov <veontomo@gmail.com>
2018-10-12 06:34:59 +0200

    Adjust the print statements

--- core-kotlin/src/main/kotlin/com/baeldung/datastructures/Main.kt
+++ core-kotlin/src/main/kotlin/com/baeldung/datastructures/Main.kt
@@ -6,12 +6,14 @@ package com.baeldung.datastructures
  */
 fun main(args: Array<String>) {
     val tree = Node(4)
-    val keys = arrayOf(1, 2, 5, 10, 2, 3, 4, 6, 11)
+    val keys = arrayOf(8, 15, 21, 3, 7, 2, 5, 10, 2, 3, 4, 6, 11)
     for (key in keys) {
         tree.insert(key)
     }
     val node = tree.find(4)!!
-    print("Node with value ${node.key} [left = ${node.left?.key}, right = ${node.right?.key}]")
+    println("Node with value ${node.key} [left = ${node.left?.key}, right = ${node.right?.key}]")
+    println("Delete node with key = 3")
     node.delete(3)
-    print(tree.visit().joinToString { it.toString() })
+    print("Tree content after the node elimination: ")
+    println(tree.visit().joinToString { it.toString() })
 }
