7d1866e94bf3435bde883934f5effa49d07d26f8
Larry Chung <codewarrior2000@users.noreply.github.com>
2018-10-18 14:09:35 -0400

    Updated WordIndexer in response to Kevin Gilmore
    Generally speaking, it's good practice to declare return types, parameters, variables using an interface rather than an implementation, if the interface is sufficient to satisfy the need. In this case, there's no reason you can't simply return a List of Integer. (Excellent point, fixed)
    
    The two methods are identical. Isn't the first one supposed to be the naive approach? (It was a copy and paste error, fixed)

--- java-strings/src/main/java/com/baeldung/string/searching/WordIndexer.java
+++ java-strings/src/main/java/com/baeldung/string/searching/WordIndexer.java
@@ -1,1 +1,45 @@
+package com.baeldung.string.searching;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordIndexer {
+
+    public List findWord(String textString, String word) {
+        int index = -1;
+        List<Integer> indexes = new ArrayList<Integer>();
+        boolean done = false;
+        String lowerCaseTextString = textString.toLowerCase();
+        String lowerCaseWord = word.toLowerCase();
+
+        while(!done){
+            index = lowerCaseTextString.indexOf(lowerCaseWord, index + 1);
+            if (index != -1) {
+                indexes.add(index);
+            }else{
+                done = true;
+            }
+        }
+        return indexes;
+    }
+
+    public List findWordUpgrade(String textString, String word) {
+        int index = -1;
+        List<Integer> indexes = new ArrayList<Integer>();
+        StringBuilder output = new StringBuilder();
+        boolean done = false;
+        String lowerCaseTextString = textString.toLowerCase();
+        String lowerCaseWord = word.toLowerCase();
+        int wordLength = word.length();
+
+        while(!done){
+            index = lowerCaseTextString.indexOf(lowerCaseWord, index + wordLength);  // Slight improvement
+            if (index != -1) {
+                indexes.add(index);
+            }else{
+                done = true;
+            }
+        }
+        return indexes;
+    }
+}
