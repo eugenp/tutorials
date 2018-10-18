4656d3814ba846bb337d3c9669cb82b9574a2b43
KevinGilmore <kpg102@gmail.com>
2018-10-18 13:39:59 -0400

    Update java-strings/src/main/java/com/baeldung/string/WordIndexer.java

--- java-strings/src/main/java/com/baeldung/string/WordIndexer.java
+++ java-strings/src/main/java/com/baeldung/string/WordIndexer.java
@@ -26,7 +26,7 @@ public class WordIndexer {
 
     public List<Integer> findWordUpgrade(String textString, String word) {
         int index = -1;
-        ArrayList<Integer> indexes = new ArrayList<Integer>();
+        List<Integer> indexes = new ArrayList<Integer>();
         StringBuilder output = new StringBuilder();
         boolean done = false;
         String lowerCaseTextString = textString.toLowerCase();
