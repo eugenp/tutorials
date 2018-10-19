fdcaff1d3bcdc517f72b6c4b85f2332b5a59e220
Dhawal Kapil <dhawalkapil@gmail.com>
2018-10-19 20:25:35 +0530

    BAEL-9683 Fix tutorials default-first build - ref# 156
    - Fix assertion error - loss of precision was happening because double was being divided by an integer

--- java-numbers/src/test/java/com/baeldung/maths/MathSinUnitTest.java
+++ java-numbers/src/test/java/com/baeldung/maths/MathSinUnitTest.java
@@ -1,20 +1,21 @@
 package com.baeldung.maths;
 
-import static org.junit.Assert.assertTrue;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 import org.junit.jupiter.api.Test;
 
 public class MathSinUnitTest {
 
-    @Test
-    public void givenAnAngleInDegrees_whenUsingToRadians_thenResultIsInRadians() {
-        double angleInDegrees = 30;
-        double sinForDegrees = Math.sin(Math.toRadians(angleInDegrees)); // 0.5
+	@Test
+	public void givenAnAngleInDegrees_whenUsingToRadians_thenResultIsInRadians() {
+		double angleInDegrees = 30;
+		double sinForDegrees = Math.sin(Math.toRadians(angleInDegrees)); // 0.5
 
-        double thirtyDegreesInRadians = 1/6 * Math.PI;
-        double sinForRadians = Math.sin(thirtyDegreesInRadians); // 0.5
+		double thirtyDegreesInRadians = (double) 1 / 6 * Math.PI;
+		double sinForRadians = Math.sin(thirtyDegreesInRadians); // 0.5
 
-        assertTrue(sinForDegrees == sinForRadians);
-    }
+		assertThat(sinForDegrees, is(sinForRadians));
+	}
 
 }
