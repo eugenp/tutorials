45226da3f6401140e2a6da929408b57e1ec234ca
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2018-10-24 17:27:19 -0300

    Update Application.java

--- persistence-modules/spring-boot-persistence/src/main/java/com/baeldung/springbootcrudapp/application/Application.java
+++ persistence-modules/spring-boot-persistence/src/main/java/com/baeldung/springbootcrudapp/application/Application.java
@@ -3,7 +3,12 @@ package com.baeldung.springbootcrudapp.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication 
+@EnableAutoConfiguration
+@ComponentScan(basePackages={"com.baeldung.springbootcrudapp.application"})
+@EnableJpaRepositories(basePackages="com.baeldung.springbootcrudapp.application.repositories")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.baeldung.springbootcrudapp.application.entities")
 public class Application {
 
     public static void main(String[] args) {
