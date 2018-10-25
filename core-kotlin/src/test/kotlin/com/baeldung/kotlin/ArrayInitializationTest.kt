7b68d07480d661f5be55efa5eace2d04d956642b
vizsoro <zsolt.vicze@gmail.com>
2018-10-25 15:03:13 +0200

    bael-2022 formatting with eclipse

--- core-kotlin/src/test/kotlin/com/baeldung/kotlin/ArrayInitializationTest.kt
+++ core-kotlin/src/test/kotlin/com/baeldung/kotlin/ArrayInitializationTest.kt
@@ -3,47 +3,46 @@ package com.baeldung.kotlin
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class ArrayInitializationTest{
+class ArrayInitializationTest {
 
-    @Test
-    fun givenArrayOfStrings_thenValuesPopulated(){
-        val strings = arrayOf("January", "February", "March")
+	@Test
+	fun givenArrayOfStrings_thenValuesPopulated() {
+		val strings = arrayOf("January", "February", "March")
+		assertEquals(3, strings.size)
+		assertEquals("March", strings[2]);
+	}
 
-        assertEquals(3, strings.size)
-        assertEquals("March", strings[2]);
-    }
+	@Test
+	fun givenArrayOfIntegers_thenValuesPopulated() {
+		val integers = intArrayOf(1, 2, 3, 4)
 
-    @Test
-    fun givenArrayOfIntegers_thenValuesPopulated(){
-        val integers = intArrayOf(1,2,3,4)
+		assertEquals(4, integers.size)
+		assertEquals(1, integers[0])
+	}
 
-        assertEquals(4, integers.size)
-        assertEquals(1, integers[0])
-    }
+	@Test
+	fun givenArrayOfNulls_whenPopulated_thenValuesPresent() {
+		val array = arrayOfNulls<Number>(5)
 
-    @Test
-    fun givenArrayOfNulls_whenPopulated_thenValuesPresent(){
-        val array = arrayOfNulls<Number>(5)
+		for (i in array.indices) {
+			array[i] = i * i;
+		}
 
-        for(i in array.indices){
-            array[i] = i*i;
-        }
+		assertEquals(16, array[4])
+	}
 
-        assertEquals(16, array[4])
-    }
+	@Test
+	fun whenGeneratorUsed_thenValuesPresent() {
+		val generatedArray = IntArray(10, { i -> i * i })
 
-    @Test
-    fun whenGeneratorUsed_thenValuesPresent(){
-        val generatedArray = IntArray(10,{i->i*i})
+		assertEquals(81, generatedArray[9]);
+	}
 
-        assertEquals(81, generatedArray[9]);
-    }
+	@Test
+	fun whenStringGenerated_thenValuesPresent() {
+		val generatedStringArray = Array(10, { i -> "Number of index: " + i })
 
-    @Test
-    fun whenStringGenerated_thenValuesPresent(){
-        val generatedStringArray = Array(10, {i -> "Number of index: " + i })
-
-        assertEquals("Number of index: 0", generatedStringArray[0])
-    }
+		assertEquals("Number of index: 0", generatedStringArray[0])
+	}
 
 }
