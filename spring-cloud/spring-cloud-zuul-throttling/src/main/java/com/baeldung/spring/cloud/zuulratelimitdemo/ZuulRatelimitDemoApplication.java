08d14d5dd47209dece0e9178c413b3a92e494b7c
Ganesh Pagade <ganesh.pagade@tesco.com>
2018-10-26 10:21:35 +0530

    fixes

--- spring-cloud/spring-cloud-zuul-throttling/src/main/java/com/baeldung/spring/cloud/zuulratelimitdemo/ZuulRatelimitDemoApplication.java
+++ spring-cloud/spring-cloud-zuul-throttling/src/main/java/com/baeldung/spring/cloud/zuulratelimitdemo/ZuulRatelimitDemoApplication.java
@@ -1,12 +1,53 @@
 package com.baeldung.spring.cloud.zuulratelimitdemo;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.context.annotation.Bean;
+
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.RateLimitKeyGenerator;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.RateLimitUtils;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.properties.RateLimitProperties;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.RateLimiterErrorHandler;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.config.repository.DefaultRateLimiterErrorHandler;
+import com.marcosbarbero.cloud.autoconfigure.zuul.ratelimit.support.DefaultRateLimitKeyGenerator;
 
 @SpringBootApplication
 public class ZuulRatelimitDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulRatelimitDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulRatelimitDemoApplication.class, args);
+    }
+
+    @Bean
+    public RateLimitKeyGenerator ratelimitKeyGenerator(RateLimitProperties properties, RateLimitUtils rateLimitUtils) {
+        return new DefaultRateLimitKeyGenerator(properties, rateLimitUtils) {
+            @Override
+            public String key(HttpServletRequest request, Route route, RateLimitProperties.Policy policy) {
+                return super.key(request, route, policy) + ":" + request.getMethod();
+            }
+        };
+    }
+
+    @Bean
+    public RateLimiterErrorHandler rateLimitErrorHandler() {
+        return new DefaultRateLimiterErrorHandler() {
+            @Override
+            public void handleSaveError(String key, Exception e) {
+                // custom code
+            }
+
+            @Override
+            public void handleFetchError(String key, Exception e) {
+                // custom code
+            }
+
+            @Override
+            public void handleError(String msg, Exception e) {
+                // custom code
+            }
+        };
+    }
 }
