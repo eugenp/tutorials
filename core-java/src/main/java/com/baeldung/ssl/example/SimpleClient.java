bbcdeec409774c51dada32908bfd29cd8d297981
Kumar Chandrakant <kumchand0@publicisgroupe.net>
2018-10-26 18:20:03 +0100

    BAEL-2250: Adding files for the article on SSL handshake failure.

---
+++ core-java/src/main/java/com/baeldung/ssl/example/SimpleClient.java
@@ -0,0 +1,27 @@
+package com.baeldung.ssl.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.URISyntaxException;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
+
+public class SimpleClient {
+    private static void startClient(String host, int port) throws IOException {
+
+        SocketFactory factory = SSLSocketFactory.getDefault();
+
+        try (Socket connection = factory.createSocket(host, port)) {
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            System.out.println(input.readLine());
+        }
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        startClient("localhost", 1234);
+    }
+}
---
+++ core-java/src/main/java/com/baeldung/ssl/example/SimpleServer.java
@@ -0,0 +1,30 @@
+package com.baeldung.ssl.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+
+public class SimpleServer {
+    private static void startServer(int port) throws IOException {
+        ServerSocketFactory factory = SSLServerSocketFactory.getDefault();
+        try (ServerSocket listener = factory.createServerSocket(port)) {
+            ((SSLServerSocket) listener).setNeedClientAuth(true);
+            while (true) {
+                try (Socket socket = listener.accept()) {
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    out.println(new Date().toString());
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {      
+        startServer(1234);
+    }
+}
