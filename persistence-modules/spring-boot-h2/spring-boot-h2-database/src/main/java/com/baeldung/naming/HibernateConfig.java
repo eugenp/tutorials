e54c16d5cdf924df8c577a847fc6201f83ebbfff
Juan Moreno <earth001@gmail.com>
2018-10-30 01:31:06 -0300

    Added sample code for BAEL-2098

---
+++ persistence-modules/spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/naming/HibernateConfig.java
@@ -0,0 +1,15 @@
+package com.baeldung.naming;
+
+import org.hibernate.jpa.boot.spi.IntegratorProvider;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class HibernateConfig implements HibernatePropertiesCustomizer {
+
+    @Override
+    public void customize(Map<String, Object> hibernateProperties) {
+        hibernateProperties.put("hibernate.integrator_provider", (IntegratorProvider) () -> Collections.singletonList(MetadataExtractorIntegrator.INSTANCE));
+    }
+}
---
+++ persistence-modules/spring-boot-h2/spring-boot-h2-database/src/main/java/com/baeldung/naming/SpringBootNamingApp.java
@@ -0,0 +1,12 @@
+package com.baeldung.naming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNamingApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNamingApp.class, args);
+    }
+}
