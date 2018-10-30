3221968f9e4e3fb58f5039c80c68b0092bcbc77a
Hai Nguyen <nn.hai@samsung.com>
2018-10-30 13:40:59 +0800

    BAEL-1913 kotline random string

--- core-kotlin/src/test/kotlin/com/baeldung/random/RandomStringTest.kt
+++ core-kotlin/src/test/kotlin/com/baeldung/random/RandomStringTest.kt
@@ -1,18 +1,50 @@
-
+import org.apache.commons.lang3.RandomStringUtils
 import org.junit.jupiter.api.Test
-import java.util.concurrent.ThreadLocalRandom
-import kotlin.test.assertTrue
+import kotlin.streams.asSequence
+import kotlin.test.assertEquals
 
-class RandomNumberTest {
+const val STRING_LENGTH = 10;
+const val ALPHANUMERIC_REGEX = "[a-zA-Z0-9]+";
+
+class RandomStringTest {
 
     @Test
-    fun whenRandomNumberWithJavaUtilMath_thenResultIsBetween0And1() {
-        val source = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        var test = Random().ints(outputStrLength, 0, source.length)
+    fun generateRandomString_useJava_returnString() {
+        val charPool = ArrayList<Char>();
+        charPool.addAll('a'..'z');
+        charPool.addAll('A'..'Z');
+        charPool.addAll('0'..'9');
+
+        var randomString = java.util.Random().ints(STRING_LENGTH.toLong(), 0, charPool.size)
                 .asSequence()
-                .map(source::get)
+                .map(charPool::get)
                 .joinToString("")
-        print("message")
+
+        assert(randomString.matches(Regex(ALPHANUMERIC_REGEX)));
+        assertEquals(STRING_LENGTH, randomString.length);
+    }
+
+    @Test
+    fun generateRandomString_useKotlin_returnString() {
+        val charPool = ArrayList<Char>();
+        charPool.addAll('a'..'z');
+        charPool.addAll('A'..'Z');
+        charPool.addAll('0'..'9');
+
+        var randomString = (1..STRING_LENGTH).map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+                .map(charPool::get)
+                .joinToString("");
+
+        assert(randomString.matches(Regex(ALPHANUMERIC_REGEX)));
+        assertEquals(STRING_LENGTH, randomString.length);
+    }
+
+    @Test
+    fun generateRandomString_useApacheCommon_returnString() {
+        var randomString = RandomStringUtils.randomAlphanumeric(STRING_LENGTH);
+
+        assert(randomString.matches(Regex(ALPHANUMERIC_REGEX)));
+        assertEquals(STRING_LENGTH, randomString.length);
     }
 
 }
