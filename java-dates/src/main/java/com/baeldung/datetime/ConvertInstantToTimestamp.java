7064a7e30960c51b7ed2b8aebee6465d6d4c3cb2
Yatendra Goel <yatendra.goel.cs@gmail.com>
2018-11-03 01:01:07 +0530

    Removed ZoneId code

--- java-dates/src/main/java/com/baeldung/datetime/ConvertInstantToTimestamp.java
+++ java-dates/src/main/java/com/baeldung/datetime/ConvertInstantToTimestamp.java
@@ -1,5 +1,7 @@
 package com.baeldung.datetime;
 
+import static org.junit.Assert.assertEquals;
+
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -16,22 +18,15 @@ public class ConvertInstantToTimestamp {
     public static void run() {
         Instant instant = Instant.now();
         Timestamp timestamp = Timestamp.from(instant); // same point on the time-line as Instant
+        assertEquals(instant.toEpochMilli(), timestamp.getTime());
+
         instant = timestamp.toInstant();
-
-        System.out.println("Instant (milliseconds from epoch): " + instant.toEpochMilli());
-        System.out.println("Timestamp (milliseconds from epoch): " + timestamp.getTime());
-        System.out.print("\n");
-
-        // Local TimeZone of the machine at the time of running this code is GMT +5:30 a.k.a IST
-        ZoneId zoneId = ZoneId.of(ZoneId.SHORT_IDS.get("IST"));
+        assertEquals(instant.toEpochMilli(), timestamp.getTime());
 
         DateFormat df = DateFormat.getDateTimeInstance();
-        df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SS'Z'");
+        df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
         df.setTimeZone(TimeZone.getTimeZone("UTC"));
 
-        System.out.println("Instant (in UTC): " + instant);
-        System.out.println("Timestamp (in UTC): " + df.format(timestamp));
-        System.out.println("Instant (in GMT +05:30): " + instant.atZone(zoneId));
-        System.out.println("Timestamp (in GMT +05:30): " + timestamp);
+        assertEquals(instant.toString(), df.format(timestamp).toString());
     }
 }
