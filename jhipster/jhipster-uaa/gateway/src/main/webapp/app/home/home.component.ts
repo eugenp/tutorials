13741c57970c63616f38c78d262aa755eae74624
psevestre <phil@lighthouse.com.br>
2018-11-03 13:00:06 -0300

    BAEL-1996 (#5507)
    * [BAEL-1996] WIP - Initial import
    
    * [BAEL-1996] Apply formatting rules
    
    * [BAEL-1996] Import UAA modules
    
    * [BAEL-1996] New directory structure

---
+++ jhipster/jhipster-uaa/gateway/src/main/webapp/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { LoginModalService, Principal, Account } from 'app/core';
+
+@Component({
+    selector: 'jhi-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['home.scss']
+})
+export class HomeComponent implements OnInit {
+    account: Account;
+    modalRef: NgbModalRef;
+
+    constructor(private principal: Principal, private loginModalService: LoginModalService, private eventManager: JhiEventManager) {}
+
+    ngOnInit() {
+        this.principal.identity().then(account => {
+            this.account = account;
+        });
+        this.registerAuthenticationSuccess();
+    }
+
+    registerAuthenticationSuccess() {
+        this.eventManager.subscribe('authenticationSuccess', message => {
+            this.principal.identity().then(account => {
+                this.account = account;
+            });
+        });
+    }
+
+    isAuthenticated() {
+        return this.principal.isAuthenticated();
+    }
+
+    login() {
+        this.modalRef = this.loginModalService.open();
+    }
+}
