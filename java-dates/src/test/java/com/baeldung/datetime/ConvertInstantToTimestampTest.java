5e1fc54ec2cfc589988af9f2b8cd4a3bf1ac7290
Yatendra Goel <yatendra.goel.cs@gmail.com>
2018-11-03 15:59:05 +0530

    Converted class to JUnit Test

--- java-dates/src/test/java/com/baeldung/datetime/ConvertInstantToTimestampTest.java
+++ java-dates/src/test/java/com/baeldung/datetime/ConvertInstantToTimestampTest.java
@@ -1,6 +1,6 @@
 package com.baeldung.datetime;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.sql.Timestamp;
 import java.text.DateFormat;
@@ -9,24 +9,22 @@ import java.time.Instant;
 import java.time.ZoneId;
 import java.util.TimeZone;
 
-public class ConvertInstantToTimestamp {
+import org.junit.Test;
 
-    public static void main(String[] args) {
-        run();
-    }
+public class ConvertInstantToTimestampTest {
 
-    public static void run() {
+    @Test
+    public void givenInstant_whenConvertedToTimestamp_thenGetTimestampWithSamePointOnTimeline() {
         Instant instant = Instant.now();
         Timestamp timestamp = Timestamp.from(instant); // same point on the time-line as Instant
-        assertEquals(instant.toEpochMilli(), timestamp.getTime());
+        assertThat(instant.toEpochMilli()).isEqualTo(timestamp.getTime());
 
         instant = timestamp.toInstant();
-        assertEquals(instant.toEpochMilli(), timestamp.getTime());
+        assertThat(instant.toEpochMilli()).isEqualTo(timestamp.getTime());
 
         DateFormat df = DateFormat.getDateTimeInstance();
         df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SS'Z'");
         df.setTimeZone(TimeZone.getTimeZone("UTC"));
-
-        assertEquals(instant.toString(), df.format(timestamp).toString());
+        assertThat(instant.toString()).isEqualTo(df.format(timestamp).toString());
     }
 }
