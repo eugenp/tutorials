6a352be9778f6220030f3b6c0163eba2374bb52b
cdjole <djordje.cvijetic@gmail.com>
2018-11-09 01:07:59 +0100

    Calculate circle area. (#5644)

---
+++ core-java/src/main/java/com/baeldung/area/circle/Circle.java
@@ -0,0 +1,30 @@
+package com.baeldung.area.circle;
+
+public class Circle {
+
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return calculateArea();
+    }
+
+    private double calculateArea() {
+        return radius * radius * Math.PI;
+    }
+
+    public String toString() {
+        return "The area of the circle [radius = " + radius + "]: " + calculateArea();
+    }
+}
