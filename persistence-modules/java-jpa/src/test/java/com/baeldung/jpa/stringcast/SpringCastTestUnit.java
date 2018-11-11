e7d145e4f9b6b8b1f0a656682d35ea7af63107fc
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2018-11-11 09:28:06 +0200

    Update and rename SpringCastTest.java to SpringCastTestUnit.java

--- persistence-modules/java-jpa/src/test/java/com/baeldung/jpa/stringcast/SpringCastTestUnit.java
+++ persistence-modules/java-jpa/src/test/java/com/baeldung/jpa/stringcast/SpringCastTestUnit.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
-public class SpringCastTest {
+public class SpringCastUnitTest {
 
     private static EntityManager em;
     private static EntityManagerFactory emFactory;
