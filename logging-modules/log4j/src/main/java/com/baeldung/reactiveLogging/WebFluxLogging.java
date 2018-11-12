bb04d504d201b1e8ab2cfb839fb2049a9ebc2565
Jakub Fedoruk <kubafedoruk@gmail.com>
2018-11-12 20:31:31 +0100

    Add all examples. Fix formatting.

--- logging-modules/log4j/src/main/java/com/baeldung/reactiveLogging/WebFluxLogging.java
+++ logging-modules/log4j/src/main/java/com/baeldung/reactiveLogging/WebFluxLogging.java
@@ -2,12 +2,23 @@ package com.baeldung.reactiveLogging;
 
 import reactor.core.publisher.Flux;
 
-public class ReactiveLogging {
-    
+public class WebFluxLogging {
+
     public static void main(String[] args) {
         Flux<Integer> reactiveStream = Flux.range(1, 5)
+            .log();
+
+        reactiveStream.subscribe();
+
+        reactiveStream = Flux.range(1, 5)
             .log()
             .take(3);
+
+        reactiveStream.subscribe();
+
+        reactiveStream = Flux.range(1, 5)
+            .take(3)
+            .log();
 
         reactiveStream.subscribe();
     }
