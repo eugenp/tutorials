e86f2a06c10fdd75e0120c2cf09be74c96df3480
grvgupta00 <45037454+grvgupta00@users.noreply.github.com>
2018-11-14 19:02:42 +0530

    JIRA-XXX: Corrected file name.

--- spring-5-reactive/src/main/java/com/baeldung/hexagonal/Game.java
+++ spring-5-reactive/src/main/java/com/baeldung/hexagonal/Game.java
@@ -4,14 +4,14 @@ import com.baeldung.hexagonal.adapters.ConsoleDisplay;
 import com.baeldung.hexagonal.adapters.KeyboardGuesses;
 import com.baeldung.hexagonal.domain.Display;
 import com.baeldung.hexagonal.domain.Guesses;
-import com.baeldung.hexagonal.domain.HexagonalGuessMyNumber;
+import com.baeldung.hexagonal.domain.GuessMyNumber;
 
-public class Main {
+public class Game {
 
 	public static void main(String [] args){
 		Display display = new ConsoleDisplay();
         Guesses guesses = new KeyboardGuesses();
 
-        new HexagonalGuessMyNumber(3, guesses, display).play();
+        new GuessMyNumber(3, guesses, display).play();
 	}
 }
