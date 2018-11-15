de3a7e4727aeb0b454d5ff7947fe19cb645017af
thoughtscript <adam.gerard@gmail.com>
2018-11-14 20:39:48 -0800

    support for swagger 2 boot 2.x

---
+++ spring-boot-mvc/src/main/java/com/baeldung/swaggerboot/Constants.java
@@ -0,0 +1,16 @@
+package com.baeldung.swaggerboot;
+
+public class Constants {
+
+    public static final String DEFAULT_GREETING = "Howdy Cosmic Spheroid!";
+    public static final String DEFAULT_ERROR = "Fail!";
+
+    /**
+     * API Endpoint.
+     */
+
+    public static final String REACTIVE_REST_URL = "/reactiverest";
+    public static final String FUNCTIONAL_URL = "/functional";
+    public static final String REGULAR_REST_URL = "/regularrest";
+
+}
