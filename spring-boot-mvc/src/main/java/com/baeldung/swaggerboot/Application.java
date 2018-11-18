9ec41d973b89f1542bf26c1b40c0f2daac0c142f
Loredana <lore.crusoveanu@gmail.com>
2018-11-18 17:12:59 +0200

    simplify swagger ex, fix start

--- spring-boot-mvc/src/main/java/com/baeldung/swaggerboot/Application.java
+++ spring-boot-mvc/src/main/java/com/baeldung/swaggerboot/Application.java
@@ -2,10 +2,8 @@ package com.baeldung.swaggerboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.baeldung"})
 public class Application {
 
     public static void main(String[] args) {
--- spring-boot-mvc/src/main/java/com/baeldung/swaggerboot/controller/RegularRestController.java
+++ spring-boot-mvc/src/main/java/com/baeldung/swaggerboot/controller/RegularRestController.java
@@ -1,22 +1,14 @@
 package com.baeldung.swaggerboot.controller;
 
-import com.baeldung.swaggerboot.services.RegularWebService;
-import com.baeldung.swaggerboot.transfer.CustomResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.baeldung.swaggerboot.Constants.REGULAR_REST_URL;
 
 @RestController
 public class RegularRestController {
 
-    @Autowired
-    RegularWebService regularWebService;
-
-    @GetMapping(REGULAR_REST_URL)
-    public CustomResponse getSession() {
-        return regularWebService.example();
+    @GetMapping("home")
+    public String getSession() {
+        return "Hello";
     }
 
 }
