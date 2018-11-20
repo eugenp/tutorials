4e71ac71613bd9d1a0c01bc2d996a4c16ce43833
MASHELKAR Priyesh <mashelkar.priyesh@gmail.com>
2018-11-20 16:23:42 +0000

    Added test class and one named query

--- hibernate5/src/test/java/com/baeldung/hibernate/NamedQueryIntegrationTest.java
+++ hibernate5/src/test/java/com/baeldung/hibernate/NamedQueryIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.baeldung.hibernate;
 
+<<<<<<< HEAD
 import java.io.IOException;
 
 import org.hibernate.Session;
@@ -40,12 +41,43 @@ public class NamedQueryIntegrationTest {
         session.persist(salesDepartment);
         session.persist(purchaseDepartment);
         purchaseDeptId = purchaseDepartment.getId();
+=======
+import com.baeldung.hibernate.entities.Department;
+import com.baeldung.hibernate.entities.DeptEmployee;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+
+public class NamedQueryIntegrationTest {
+    private Session session;
+
+    private Transaction transaction;
+
+    @Before
+    public void setUp() throws IOException {
+        session = HibernateUtil.getSessionFactory().openSession();
+        transaction = session.beginTransaction();
+        session.createNativeQuery("delete from department").executeUpdate();
+        session.createNativeQuery("delete from deptemployee").executeUpdate();
+        Department department = new Department("Sales");
+        DeptEmployee employee1 = new DeptEmployee("John Smith", "001", department);
+        DeptEmployee employee2 = new DeptEmployee("Sarah Vinton", "002", department);
+        DeptEmployee employee3 = new DeptEmployee("Lisa Carter", "003", department);
+        session.persist(department);
+>>>>>>> Added test class and one named query
         session.persist(employee1);
         session.persist(employee2);
         session.persist(employee3);
         transaction.commit();
         transaction = session.beginTransaction();
     }
+<<<<<<< HEAD
     
     @After
     public void tearDown() {
@@ -100,4 +132,15 @@ public class NamedQueryIntegrationTest {
         spQuery.executeUpdate();
         transaction.commit();        
     }       
+=======
+
+    @Test
+    public void givenMultipleEmployees_WhenFindByEmployeeNumberIsExecuted_ThenCorrectEmployeeIsReturned() {
+        Query<DeptEmployee> query = session.createNamedQuery("findByEmployeeNumber", DeptEmployee.class);
+        query.setParameter("employeeNo", "002");
+        DeptEmployee result = query.getSingleResult();
+        Assert.assertNotNull(result);
+        Assert.assertEquals("Sarah Vinton", result.getName());
+    }
+>>>>>>> Added test class and one named query
 }
