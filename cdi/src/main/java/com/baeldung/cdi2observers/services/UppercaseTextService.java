dd53fcf3b0bb6910a222790e39a09dad3a8e1e1d
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2018-11-24 16:15:38 -0300

    Update UppercaseTextService.java

--- cdi/src/main/java/com/baeldung/cdi2observers/services/UppercaseTextService.java
+++ cdi/src/main/java/com/baeldung/cdi2observers/services/UppercaseTextService.java
@@ -1,6 +1,6 @@
 package com.baeldung.cdi.cdi2observers.services;
 
-public class UppercaseTextService implements TextService {
+public class TextService {
 
     @Override
     public String parseText(String text) {
