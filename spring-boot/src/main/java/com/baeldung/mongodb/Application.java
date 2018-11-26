5bca3a132a4f3f4855cc907a40b3d6f47bc472cb
Seun Matt <smatt382@gmail.com>
2018-11-26 08:33:20 +0100

    example code for BAEL-1961

--- spring-boot/src/main/java/com/baeldung/mongodb/Application.java
+++ spring-boot/src/main/java/com/baeldung/mongodb/Application.java
@@ -1,11 +1,34 @@
 package com.baeldung.mongodb;
 
+import com.baeldung.mongodb.daos.UserRepository;
+import com.baeldung.mongodb.models.User;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(UserRepository userRepository) {
+        return args -> {
+
+            User user = new User();
+            user.setFirstName("John");
+            user.setLastName("Doe");
+            user.setEmail("john.doe@example.com");
+            userRepository.save(user);
+
+            List<User> storedUsers = userRepository.findAll();
+            storedUsers.forEach(System.out::println);
+
+        };
     }
 }
