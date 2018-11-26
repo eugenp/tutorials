b3bd86cdd3982d0a26eb161396f9b008c683dbbe
Joel Juarez <j7.juarezb@gmail.com>
2018-11-27 00:12:09 +0100

    CHANGED: Mock instead of implementations on Tests

--- hexagonal-architecture/src/test/java/com/baeldung/hexagonal/MyEtlServiceTest.java
+++ hexagonal-architecture/src/test/java/com/baeldung/hexagonal/MyEtlServiceTest.java
@@ -1,19 +1,38 @@
 package com.baeldung.hexagonal;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MyEtlServiceTest {
+
+    @Mock
+    private ExtractorPort extractorMock;
+
+    @Mock
+    private LoaderPort loaderMock;
 
     @Test
     public void givenInputData_thenLoadTransformedData() {
         String inputData = "This is an example Input with some numbers(1,2,3,4) and some symbols";
         String expectedLoadedData = "THIS IS AN EXAMPLE INPUT WITH SOME NUMBERS(1,2,3,4) AND SOME SYMBOLS";
 
-        TestExtractorAdapter extractorAdapter = new TestExtractorAdapter(inputData);
-        TestLoaderAdapter loaderAdapter = new TestLoaderAdapter();
+        when(extractorMock.extractData()).thenReturn(inputData.getBytes());
 
         MyEtlService tested = new MyEtlService();
-        tested.etl(extractorAdapter, loaderAdapter);
-        loaderAdapter.assertDataLoaded(expectedLoadedData);
+        tested.etl(extractorMock, loaderMock);
+
+        ArgumentCaptor<byte[]> loaderCaptor = ArgumentCaptor.forClass(byte[].class);
+
+        verify(loaderMock).loadData(loaderCaptor.capture());
+
+        Assert.assertEquals(expectedLoadedData, new String(loaderCaptor.getValue()));
     }
 }
