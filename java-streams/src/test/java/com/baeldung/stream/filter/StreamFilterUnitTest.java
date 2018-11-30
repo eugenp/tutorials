f06a9f4106cff01c7de0b05b6d980dfc98533e7d
Juan Moreno <earth001@gmail.com>
2018-11-30 01:30:19 -0300

    Sample code for BAEL-2333

---
+++ java-streams/src/test/java/com/baeldung/stream/filter/StreamFilterUnitTest.java
@@ -0,0 +1,115 @@
+package com.baeldung.stream.filter;
+
+import org.junit.jupiter.api.Test;
+import pl.touk.throwing.ThrowingPredicate;
+import pl.touk.throwing.exception.WrappedException;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+public class StreamFilterUnitTest {
+
+    @Test
+    public void givenListOfCustomers_whenFilterByLambda_thenGetTwo() {
+        List<Customer> customers = Arrays.asList(new Customer("John P.", 15), new Customer("Sarah M.", 200), new Customer("Charles B.", 150), new Customer("Mary T.", 1));
+
+        long customersWithMoreThan100Points = customers
+          .stream()
+          .filter(c -> c.getPoints() > 100)
+          .count();
+
+        assertThat(customersWithMoreThan100Points).isEqualTo(2);
+    }
+
+    @Test
+    public void givenListOfCustomers_whenFilterByMethodReference_thenGetTwo() {
+        List<Customer> customers = Arrays.asList(new Customer("John P.", 15), new Customer("Sarah M.", 200), new Customer("Charles B.", 150), new Customer("Mary T.", 1));
+
+        long customersWithMoreThan100Points = customers
+          .stream()
+          .filter(Customer::hasOverThousandPoints)
+          .count();
+
+        assertThat(customersWithMoreThan100Points).isEqualTo(2);
+    }
+
+    @Test
+    public void givenListOfCustomersWithOptional_whenFilterBy100Points_thenGetTwo() {
+        List<Optional<Customer>> customers = Arrays.asList(Optional.of(new Customer("John P.", 15)), Optional.of(new Customer("Sarah M.", 200)), Optional.empty(), Optional.of(new Customer("Mary T.", 300)), Optional.empty());
+
+        long customersWithMoreThan100Points = customers
+          .stream()
+          .flatMap(c -> c
+            .map(Stream::of)
+            .orElseGet(Stream::empty))
+          .filter(Customer::hasOverThousandPoints)
+          .count();
+
+        assertThat(customersWithMoreThan100Points).isEqualTo(2);
+    }
+
+    @Test
+    public void givenListOfCustomers_whenFilterWithCustomHandling_thenThrowException() {
+        List<Customer> customers = Arrays.asList(new Customer("John P.", 15, "https://images.unsplash.com/photo-1543320485-d0d5a49c2b2e"), new Customer("Sarah M.", 200), new Customer("Charles B.", 150),
+          new Customer("Mary T.", 1, "https://images.unsplash.com/photo-1543297057-25167dfc180e"));
+
+        assertThatThrownBy(() -> customers
+          .stream()
+          .filter(Customer::hasValidProfilePhotoWithoutCheckedException)
+          .count()).isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void givenListOfCustomers_whenFilterWithThrowingFunction_thenThrowException() {
+        List<Customer> customers = Arrays.asList(new Customer("John P.", 15, "https://images.unsplash.com/photo-1543320485-d0d5a49c2b2e"), new Customer("Sarah M.", 200), new Customer("Charles B.", 150),
+          new Customer("Mary T.", 1, "https://images.unsplash.com/photo-1543297057-25167dfc180e"));
+
+        assertThatThrownBy(() -> customers
+          .stream()
+          .filter((ThrowingPredicate.unchecked(Customer::hasValidProfilePhoto)))
+          .count()).isInstanceOf(WrappedException.class);
+    }
+
+    @Test
+    public void givenListOfCustomers_whenFilterWithTryCatch_thenGetTwo() {
+        List<Customer> customers = Arrays.asList(new Customer("John P.", 15, "https://images.unsplash.com/photo-1543320485-d0d5a49c2b2e"), new Customer("Sarah M.", 200), new Customer("Charles B.", 150),
+          new Customer("Mary T.", 1, "https://images.unsplash.com/photo-1543297057-25167dfc180e"));
+
+        long customersWithValidProfilePhoto = customers
+          .stream()
+          .filter(c -> {
+              try {
+                  return c.hasValidProfilePhoto();
+              } catch (IOException e) {
+                  //handle exception
+              }
+              return false;
+          })
+          .count();
+
+        assertThat(customersWithValidProfilePhoto).isEqualTo(2);
+    }
+
+    @Test
+    public void givenListOfCustomers_whenFilterWithTryCatchAndRuntime_thenThrowException() {
+        List<Customer> customers = Arrays.asList(new Customer("John P.", 15, "https://images.unsplash.com/photo-1543320485-d0d5a49c2b2e"), new Customer("Sarah M.", 200), new Customer("Charles B.", 150),
+          new Customer("Mary T.", 1, "https://images.unsplash.com/photo-1543297057-25167dfc180e"));
+
+        assertThatThrownBy(() -> customers
+          .stream()
+          .filter(c -> {
+              try {
+                  return c.hasValidProfilePhoto();
+              } catch (IOException e) {
+                  throw new RuntimeException(e);
+              }
+          })
+          .count()).isInstanceOf(RuntimeException.class);
+    }
+}
