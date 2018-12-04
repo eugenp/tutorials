452cb9be1019cc7c99336b3da4fa43d4248fbda9
Priyesh Mashelkar <mashelkar.priyesh@gmail.com>
2018-12-04 00:47:20 +0000

    Removed commented code

--- hibernate5/src/main/resources/init_database.sql
+++ hibernate5/src/main/resources/init_database.sql
@@ -5,10 +5,6 @@ import java.sql.SQLException;
 @CODE
 void updateEmployeeDesignation(final Connection conn, final String employeeNumber, final String designation) throws SQLException {
         CallableStatement updateStatement = conn.prepareCall("update deptemployee set designation = '" + designation + "' where employeeNumber = '" + employeeNumber + "'");
-//        updateStatement.setString(0, designation);
-//        updateStatement.setString(1, employeeNumber);
-		System.out.println("update deptemployee set designation = '" + designation + "' where employeeNumber = '" + employeeNumber + "'");
-        boolean result = updateStatement.execute();
-        System.out.println("Result "+result);
+        updateStatement.execute();
 }
 $$;
