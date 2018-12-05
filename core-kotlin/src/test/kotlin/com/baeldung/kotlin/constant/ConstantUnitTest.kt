a32421704e6964ffe199056eec0bc2c538e0e18d
nnhai1991@gmail.com <Hai Nguyen>
2018-12-05 22:43:51 +0800

    kotlin const

---
+++ core-kotlin/src/test/kotlin/com/baeldung/kotlin/constant/ConstantUnitTest.kt
@@ -0,0 +1,17 @@
+import com.baeldung.kotlin.constant.TestKotlinConstantClass
+import com.baeldung.kotlin.constant.TestKotlinConstantObject
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ConstantUnitTest {
+
+    @Test
+    fun givenConstant_whenCompareWithActualValue_thenReturnTrue() {
+        assertEquals(10, TestKotlinConstantObject.COMPILE_TIME_CONST)
+        assertEquals(30, TestKotlinConstantObject.RUN_TIME_CONST)
+        assertEquals(20, TestKotlinConstantObject.JAVA_STATIC_FINAL_FIELD)
+
+        assertEquals(40, TestKotlinConstantClass.COMPANION_OBJECT_NUMBER)
+    }
+}
+
