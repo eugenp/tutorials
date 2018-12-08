50fafbaf9ac9bca0732c69daa5cf2116922968ad
Krzysztof Majewski <kmajewski@berg.technology>
2018-12-08 15:32:08 +0100

    changes in PR

--- spring-all/src/main/java/org/baeldung/scheduling/shedlock/Application.java
+++ spring-all/src/main/java/org/baeldung/scheduling/shedlock/Application.java
@@ -1,4 +1,4 @@
-package com.baeldung.shedlock;
+package com.baeldung.scheduling.shedlock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-all/src/main/java/org/baeldung/scheduling/shedlock/TaskScheduler.java
+++ spring-all/src/main/java/org/baeldung/scheduling/shedlock/TaskScheduler.java
@@ -1,14 +1,14 @@
-package com.baeldung.shedlock;
+package com.baeldung.scheduling.shedlock;
 
 import net.javacrumbs.shedlock.core.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
-class ShedlockScheduler {
+class TaskScheduler {
 
-    @Scheduled(cron = "0 */30 * * * *")
-    @SchedulerLock(name = "TaskScheduler_scheduledTask", lockAtLeastForString = "PT5M", lockAtMostForString = "PT15M")
+    @Scheduled(cron = "0 */15 * * * *")
+    @SchedulerLock(name = "TaskScheduler_scheduledTask", lockAtLeastForString = "PT5M", lockAtMostForString = "PT14M")
     public void scheduledTask() {
         System.out.println("Running ShedLock task");
     }
