250245d78f5d7f884f8a3ac4fdf7f666adaa91b8
Jędrzej Frankowski <jfrankowski@pl.sii.eu>
2018-12-09 17:07:31 +0100

    Remove redundant @Repository annotation

--- persistence-modules/spring-data-jpa/src/main/java/com/baeldung/dao/repositories/impl/InsertRepositoryImpl.java
+++ persistence-modules/spring-data-jpa/src/main/java/com/baeldung/dao/repositories/impl/InsertRepositoryImpl.java
@@ -1,12 +1,10 @@
 package com.baeldung.dao.repositories.impl;
 
 import com.baeldung.dao.repositories.InsertRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 
-@Repository
 @Transactional
 public class InsertRepositoryImpl<T> implements InsertRepository<T> {
     private final EntityManager em;
