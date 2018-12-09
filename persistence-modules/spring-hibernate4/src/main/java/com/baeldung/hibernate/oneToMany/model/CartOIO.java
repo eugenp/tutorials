0470426fe4b48d9a5e3ecb296412a13ed3434a86
Ekaterina Galkina <myluckagain@gmail.com>
2018-12-09 21:00:13 +0500

    Many To One BAEL-2329

---
+++ persistence-modules/spring-hibernate4/src/main/java/com/baeldung/hibernate/oneToMany/model/CartOIO.java
@@ -0,0 +1,42 @@
+package com.baeldung.hibernate.oneToMany.model;
+
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "CARTOIO")
+public class CartOIO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany
+    @JoinColumn(name = "cart_id") // we need to duplicate the physical information
+    private Set<ItemsOIO> items;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Set<ItemsOIO> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<ItemsOIO> items) {
+        this.items = items;
+    }
+
+}
