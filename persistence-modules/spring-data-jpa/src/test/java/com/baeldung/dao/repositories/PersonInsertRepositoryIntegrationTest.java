18685e56210aac9dfff5837bb999137b9c8e0756
Jędrzej Frankowski <jfrankowski@pl.sii.eu>
2018-12-09 16:44:34 +0100

    Small refactor

--- persistence-modules/spring-data-jpa/src/test/java/com/baeldung/dao/repositories/PersonInsertRepositoryIntegrationTest.java
+++ persistence-modules/spring-data-jpa/src/test/java/com/baeldung/dao/repositories/PersonInsertRepositoryIntegrationTest.java
@@ -22,6 +22,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class PersonInsertRepositoryIntegrationTest {
 
     private static final Person PERSON = new Person(1l, "firstname", "lastname");
+
     @Autowired
     private PersonRepository personRepository;
     @Autowired
