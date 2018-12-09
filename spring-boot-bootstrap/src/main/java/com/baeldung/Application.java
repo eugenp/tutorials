95b1a9b1587b1c6acba15128cab0890170d8a50a
Loredana <lore.crusoveanu@gmail.com>
2018-12-09 20:36:56 +0200

    fix test names, missing templates

---
+++ spring-boot-bootstrap/src/main/java/com/baeldung/Application.java
@@ -0,0 +1,21 @@
+package com.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@ServletComponentScan
+@SpringBootApplication
+@ComponentScan("com.baeldung")
+@EnableJpaRepositories("com.baeldung.persistence.repo")
+@EntityScan("com.baeldung.persistence.model")
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
