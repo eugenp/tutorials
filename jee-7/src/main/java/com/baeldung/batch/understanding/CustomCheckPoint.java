f1126332950bd9a0eb979316895f98c1b8a69f0d
amitabhmandal <amitabhmandal@192.168.1.18>
2018-12-10 22:49:26 +0530

    Fixed public static count

--- jee-7/src/main/java/com/baeldung/batch/understanding/CustomCheckPoint.java
+++ jee-7/src/main/java/com/baeldung/batch/understanding/CustomCheckPoint.java
@@ -1,12 +1,23 @@
 package com.baeldung.batch.understanding;
 
 import javax.batch.api.chunk.AbstractCheckpointAlgorithm;
+import javax.batch.runtime.context.JobContext;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 @Named
 public class CustomCheckPoint extends AbstractCheckpointAlgorithm {
+    
+    @Inject
+    JobContext jobContext;
+    
+    private Integer counterRead = 0;
+    
+    
     @Override
     public boolean isReadyToCheckpoint() throws Exception {
-        return SimpleChunkItemReader.COUNT % 5 == 0;
+        counterRead = (Integer)jobContext.getTransientUserData();
+        System.out.println("counterRead : " + counterRead);
+        return counterRead % 5 == 0;
     }
 }
