43ad4ed3a4bff3be154662907d5ce671a68e2458
j-bennett <j.bennett@nexusglobal.com>
2018-12-10 16:27:23 -0500

    BAEL-2353: Persist a JSON object using Hibernate

---
+++ persistence-modules/hibernate5/src/main/java/com/baeldung/hibernate/persistjson/HashMapConverter.java
@@ -0,0 +1,47 @@
+package com.baeldung.hibernate.persistjson;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.persistence.AttributeConverter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.baeldung.hibernate.interceptors.CustomInterceptor;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class HashMapConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomInterceptor.class);
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> customerInfo) {
+
+        String customerInfoJson = null;
+        try {
+            customerInfoJson = objectMapper.writeValueAsString(customerInfo);
+        } catch (final JsonProcessingException e) {
+            logger.error("JSON writing error", e);
+        }
+
+        return customerInfoJson;
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String customerInfoJSON) {
+
+        Map<String, Object> customerInfo = null;
+        try {
+            customerInfo = objectMapper.readValue(customerInfoJSON, Map.class);
+        } catch (final IOException e) {
+            logger.error("JSON reading error", e);
+        }
+
+        return customerInfo;
+    }
+
+}
