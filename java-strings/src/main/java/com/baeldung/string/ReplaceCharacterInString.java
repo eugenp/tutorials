047a17a9c943a279abd858824a284f9272a1e472
Shashank Agarwal <shashank@fptechscience.com>
2018-12-11 21:41:15 +0530

    added test cases

--- java-strings/src/main/java/com/baeldung/string/ReplaceCharacterInString.java
+++ java-strings/src/main/java/com/baeldung/string/ReplaceCharacterInString.java
@@ -1,12 +1,12 @@
 package com.baeldung.string;
 
 public class ReplaceCharacterInString {
-    public String replaceCharSubstring(String str, char ch, int index) {
-        String myString = str.substring(0, index) + ch + str.substring(index);
+    public  String replaceCharSubstring(String str, char ch, int index) {
+        String myString = str.substring(0, index) + ch + str.substring(index+1);
         return myString;
     }
 
-    public String replaceCharStringBuilder(String str, char ch, int index) {
+    public  String replaceCharStringBuilder(String str, char ch, int index) {
         StringBuilder myString = new StringBuilder(str);
         myString.setCharAt(index, ch);
         return myString.toString();
---
+++ java-strings/src/test/java/com/baeldung/string/ReplaceCharInStringTest.java
@@ -0,0 +1,23 @@
+package com.baeldung.string;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ReplaceCharInStringTest {
+    private ReplaceCharacterInString characterInString = new ReplaceCharacterInString();
+
+    @Test
+    public void replaceCarAtIndex(){
+        assertEquals("abcme",characterInString.replaceCharSubstring("abcde",'m',3));
+    }
+
+    @Test
+    public void replaceCarAtIndexStringBuilder(){
+        assertEquals("abcme",characterInString.replaceCharStringBuilder("abcde",'m',3));
+    }
+
+    @Test
+    public void replaceCarAtIndexStringBuffer(){
+        assertEquals("abcme",characterInString.replaceCharStringBuffer("abcde",'m',3));
+    }
+}
