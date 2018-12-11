99dd82d2ff8f307fe4cf2bd7ae6b3c7c78b25e52
geroza <rozagerardo@gmail.com>
2018-12-11 12:17:29 -0200

    Migrated modules to parent-boot-2 usign spring-boot 2.1: spring-mvc-forms-thymeleaf spring-rest spring-rest-angular spring-rest-query-language spring-rest-shell spring-rest-simple spring-rest-template spring-resttemplate spring-security-mvc-boot spring-security-openid spring-security-sso spring-security-thymeleaf

--- spring-rest/src/main/java/com/baeldung/web/upload/app/UploadApplication.java
+++ spring-rest/src/main/java/com/baeldung/web/upload/app/UploadApplication.java
@@ -9,9 +9,9 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan("com.baeldung.web.upload")
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class UploadApplication extends SpringBootServletInitializer {
 
     public static void main(final String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(UploadApplication.class, args);
     }
 }
