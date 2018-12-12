01cdc8d4cdc2b73d72ae100c04ca811d7b7d7508
jose <larroquester@gmail.com>
2018-12-11 21:57:01 -0300

    hexagonal architecture - jlarroque - jlarroque at gmail dot com

---
+++ hexagonal/src/main/java/org/baeldung/hexagonal/HexagonalApplication.java
@@ -0,0 +1,12 @@
+package org.baeldung.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalApplication.class, args);
+	}
+}
