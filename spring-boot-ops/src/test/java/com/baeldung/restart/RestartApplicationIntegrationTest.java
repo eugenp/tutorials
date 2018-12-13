7a23ac4f5301a45b74e992b8e730ff7c7dab1d89
markoprevisic <m.previsic@reply.de>
2018-12-13 15:12:23 +0100

    BAEL-2351 fixed integration tests

--- spring-boot-ops/src/test/java/com/baeldung/restart/RestartApplicationIntegrationTest.java
+++ spring-boot-ops/src/test/java/com/baeldung/restart/RestartApplicationIntegrationTest.java
@@ -2,20 +2,25 @@ package com.baeldung.restart;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+import java.time.Duration;
+
 public class RestartApplicationIntegrationTest {
-    
-    private TestRestTemplate template = new TestRestTemplate();
+
+    private TestRestTemplate restTemplate = new TestRestTemplate();
 
     @Test
     public void givenBootApp_whenRestart_thenOk() throws Exception {
         Application.main(new String[0]);
-        
-        ResponseEntity response = template.exchange("http://localhost:8080/restart", 
+
+        ResponseEntity response = restTemplate.exchange("http://localhost:8080/restart",
            HttpMethod.POST, null, Object.class);
         
         assertEquals(200, response.getStatusCode().value());
@@ -24,8 +29,8 @@ public class RestartApplicationIntegrationTest {
     @Test
     public void givenBootApp_whenRestartUsingActuator_thenOk() throws Exception {
         Application.main(new String[] { "--server.port=8090" });
-        
-        ResponseEntity response = template.exchange("http://localhost:8090/restartApp", 
+
+        ResponseEntity response = restTemplate.exchange("http://localhost:8090/restartApp",
            HttpMethod.POST, null, Object.class);
         
         assertEquals(200, response.getStatusCode().value());
