3553fb750685f94af4f8dbdeaffb0a140497556a
Steven van Beelen <steven.vanbeelen@axoniq.io>
2018-12-18 14:49:30 +0100

    BAEL-2435 Update commands and events
    -Change TargetAggregateIdentifier import statement to the new location
    -Add equals/hashcode as best practice for commands and events
    -Add description
    -Change parent to parent-boot-2 to leverage a spring boot start up
    approach

--- axon/src/main/java/com/baeldung/axon/commands/CreateMessageCommand.java
+++ axon/src/main/java/com/baeldung/axon/commands/CreateMessageCommand.java
@@ -1,24 +1,42 @@
 package com.baeldung.axon.commands;
 
+import java.util.Objects;
 
-import org.axonframework.commandhandling.TargetAggregateIdentifier;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public class CreateMessageCommand {
- 
+
     @TargetAggregateIdentifier
     private final String id;
     private final String text;
- 
+
     public CreateMessageCommand(String id, String text) {
         this.id = id;
         this.text = text;
     }
- 
+
     public String getId() {
         return id;
     }
- 
+
     public String getText() {
         return text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CreateMessageCommand that = (CreateMessageCommand) o;
+        return Objects.equals(id, that.id) && Objects.equals(text, that.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, text);
     }
 }
--- axon/src/main/java/com/baeldung/axon/commands/MarkReadMessageCommand.java
+++ axon/src/main/java/com/baeldung/axon/commands/MarkReadMessageCommand.java
@@ -1,18 +1,36 @@
 package com.baeldung.axon.commands;
 
+import java.util.Objects;
 
-import org.axonframework.commandhandling.TargetAggregateIdentifier;
+import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 public class MarkReadMessageCommand {
- 
+
     @TargetAggregateIdentifier
     private final String id;
- 
+
     public MarkReadMessageCommand(String id) {
         this.id = id;
     }
- 
+
     public String getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MarkReadMessageCommand that = (MarkReadMessageCommand) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
