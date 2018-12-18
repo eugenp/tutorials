446fbd2c8b71c8289b87eb788c12d33e290831d2
Rajesh Bhojwani <rajesh.bhojwani@gmail.com>
2018-12-18 10:43:27 +0530

    removing everything to clean up

--- hexagonal/src/main/java/com/baeldung/port/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.port;
-import com.baeldung.domain.Employee;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
-}
