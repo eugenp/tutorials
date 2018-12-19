5996de6af1a681d8c9a6c9c930a57b498daab92b
EZZEDDINE.ELHAZATI <Ezzeddine.ELHAZATI.ext@Teal.ma>
2018-12-19 16:04:46 +0100

    rename test class.

--- persistence-modules/hibernate5/src/test/java/com/baeldung/hibernate/bootstrap/BootstrapAPIIntegrationTest.java
+++ persistence-modules/hibernate5/src/test/java/com/baeldung/hibernate/bootstrap/BootstrapAPIIntegrationTest.java
@@ -1,7 +1,6 @@
 package com.baeldung.hibernate.bootstrap;
 
 import com.baeldung.hibernate.pojo.Movie;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
@@ -10,22 +9,18 @@ import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class Hibernate5BootstrapAPITest {
+public class BootstrapAPIIntegrationTest {
 
     SessionFactory sessionFactory = null;
-    Session session = null;
 
-    @Before
-    public void setUp() throws IOException {
+    @Test
+    public void whenServiceRegistryAndMetadata_thenSessionFactory() throws IOException {
 
         BootstrapServiceRegistry bootstrapRegistry = new BootstrapServiceRegistryBuilder()
                 .build();
@@ -41,29 +36,12 @@ public class Hibernate5BootstrapAPITest {
         Metadata metadata = metadataSources.getMetadataBuilder().build();
 
         sessionFactory = metadata.buildSessionFactory();
-    }
-
-    @Test
-    public void testBuildSessionFactory() {
         assertNotNull(sessionFactory);
-        session = sessionFactory.openSession();
-        assertNotNull(session);
-
-        //Persist Movie
-        session.getTransaction().begin();
-        Movie movie = new Movie();
-        movie.setId(100L);
-        session.persist(movie);
-        session.getTransaction().commit();
-
-        List<Movie> movies = session.createQuery("FROM Movie").list();
-        assertNotNull(movies);
-        assertEquals(movies.size(), 1L);
+        sessionFactory.close();
     }
 
     @After
     public void clean() throws IOException {
-        session.close();
         sessionFactory.close();
     }
 }
