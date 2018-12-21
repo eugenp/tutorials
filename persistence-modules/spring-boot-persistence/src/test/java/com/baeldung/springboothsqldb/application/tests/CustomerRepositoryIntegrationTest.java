4f5b4552689c70727b922c41c1dc5cd18331d72d
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2018-12-21 19:49:35 -0300

    Add CustomerRepositoryIntegrationTest

--- persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryIntegrationTest.java
+++ persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryIntegrationTest.java
@@ -12,8 +12,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-public class CustomerRepositoryTest {
+@SpringBootTest
+public class CustomerRepositoryIntegrationTest {
     
     @Autowired
     private CustomerRepository customerRepository;
