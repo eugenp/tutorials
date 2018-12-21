9f3744890f9204b786fa6e3e6b3b493b3dabbe75
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2018-12-21 19:55:17 -0300

    Update CustomerRepositoryIntegrationTest.java

--- persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryIntegrationTest.java
+++ persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryIntegrationTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
