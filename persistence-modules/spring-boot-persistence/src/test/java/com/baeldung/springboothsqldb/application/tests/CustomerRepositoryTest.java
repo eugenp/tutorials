46b2b493a1f558817e697d2b47f1ee1c00939397
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2018-12-21 19:41:56 -0300

    Update CustomerRepositoryTest.java

--- persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryTest.java
+++ persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryTest.java
@@ -11,10 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@DataJpaTest
 public class CustomerRepositoryTest {
     
-    @Autowired
-    private CustomerRepository customerRepository;
+    
 }
