8ed731aadf81b297b2969e7f38f54953ba27f67c
Sandip Singh <sandip.03934@gmail.com>
2018-12-21 19:30:06 +0530

    BAEL-2417 Updated the code to make simple examples of passing parameter to Java Thread. Removed BlockingQueue implementation.

--- core-java-concurrency/src/main/java/com/baeldung/concurrent/parameter/ParameterizedThreadExample.java
+++ core-java-concurrency/src/main/java/com/baeldung/concurrent/parameter/ParameterizedThreadExample.java
@@ -1,10 +1,6 @@
 package com.baeldung.concurrent.parameter;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Callable;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class ParameterizedThreadExample {
@@ -23,39 +19,14 @@ public class ParameterizedThreadExample {
         parameterizedThread.start();
     }
 
-    public static Runnable numberGenerator(BlockingQueue<Integer> topic) {
-        return () -> {
-            IntStream.rangeClosed(1, 10)
-                .forEach(i -> {
-                    try {
-                        Thread.sleep(50);
-                        topic.put(i);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                });
-            try {
-                topic.put(-1); // This marks the end of the message.
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        };
+    public static Callable<Integer> sumCalculator(int... numbers) {
+        return () -> numbers != null ? IntStream.of(numbers)
+            .sum() : 0;
     }
 
-    public static Callable<Double> averageCalculator(BlockingQueue<Integer> topic) {
-        return () -> {
-            List<Integer> numbers = new ArrayList<>();
-            try {
-                int i;
-                while ((i = topic.take()) > 0) {
-                    numbers.add(i);
-                    Thread.sleep(50);
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            return numbers.stream()
-                .collect(Collectors.averagingInt(v -> v));
-        };
+    public static Callable<Double> averageCalculator(int... numbers) {
+        return () -> numbers != null ? IntStream.of(numbers)
+            .average()
+            .orElse(0d) : 0d;
     }
 }
