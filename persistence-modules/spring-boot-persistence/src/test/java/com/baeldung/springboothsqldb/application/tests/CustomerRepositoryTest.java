67544df6b989dbce321b0cf6000dd1929e52c9c1
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2018-12-21 19:19:12 -0300

    Update CustomerRepositoryTest.java

--- persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryTest.java
+++ persistence-modules/spring-boot-persistence/src/test/java/com/baeldung/springboothsqldb/application/tests/CustomerRepositoryTest.java
@@ -17,27 +17,4 @@ public class CustomerRepositoryTest {
     
     @Autowired
     private CustomerRepository customerRepository;
-    
-    @Test
-    public void whenFindingCustomerById_thenCorrect() {
-        customerRepository.save(new Customer("John", "john@domain.com"));
-        
-        assertThat(customerRepository.findById(1L)).isInstanceOf(Optional.class);
-    }
-    
-    @Test
-    public void whenFindingAllCustomers_thenCorrect() {
-        customerRepository.save(new Customer("John", "john@domain.com"));
-        customerRepository.save(new Customer("Julie", "julie@domain.com"));
-        
-        assertThat(customerRepository.findAll()).isInstanceOf(List.class);
-    }
-    
-    @Test
-    public void whenSavingCustomer_thenCorrect() {
-        customerRepository.save(new Customer("Bob", "bob@domain.com"));
-        Customer customer = customerRepository.findById(1L).orElseGet(() -> new Customer("john", "john@domain.com"));
-        
-        assertThat(customer.getName()).isEqualTo("Bob");
-    }
 }
