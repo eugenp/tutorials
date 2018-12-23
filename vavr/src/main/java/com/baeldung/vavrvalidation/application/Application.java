5deb87325bdbf3e330d38efbc6802b86b72b9b2f
amit2103 <amit.pandey2103@gmail.com>
2018-12-23 16:59:32 +0530

    [BAEL-10985] - Removed vavr-validation submodule

--- vavr/src/main/java/com/baeldung/vavrvalidation/application/Application.java
+++ vavr/src/main/java/com/baeldung/vavrvalidation/application/Application.java
@@ -8,10 +8,10 @@ import io.vavr.control.Validation;
 public class Application {
 
     public static void main(String[] args) {
-        
+
         UserValidator userValidator = new UserValidator();
         Validation<Seq<String>, User> validation = userValidator.validateUser("John", "john@domain.com");
-        
+
         // process validation results here
-	}	
+    }	
 }
