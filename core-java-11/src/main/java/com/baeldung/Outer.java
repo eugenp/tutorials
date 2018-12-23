338b0bc80cf15eb20e9f9201e8efafc3d2a65eab
Ganesh Pagade <ganesh.pagade@tesco.com>
2018-12-23 12:08:36 +0530

    format

--- core-java-11/src/main/java/com/baeldung/Outer.java
+++ core-java-11/src/main/java/com/baeldung/Outer.java
@@ -4,21 +4,21 @@ import java.lang.reflect.Method;
 
 public class Outer {
 
-  public void outerPublic() {
-  }
-
-  private void outerPrivate() {
-  }
-
-  class Inner {
-
-    public void innerPublic() {
-      outerPrivate();
+    public void outerPublic() {
     }
 
-  	public void innerPublicReflection(Outer ob) throws Exception {
-      Method method = ob.getClass().getDeclaredMethod("outerPrivate");
-      method.invoke(ob);
-  	}
-  }
+    private void outerPrivate() {
+    }
+
+    class Inner {
+
+        public void innerPublic() {
+            outerPrivate();
+        }
+
+        public void innerPublicReflection(Outer ob) throws Exception {
+            Method method = ob.getClass().getDeclaredMethod("outerPrivate");
+            method.invoke(ob);
+        }
+    }
 }
--- core-java-11/src/test/java/com/baeldung/OuterUnitTest.java
+++ core-java-11/src/test/java/com/baeldung/OuterUnitTest.java
@@ -9,42 +9,42 @@ import org.junit.Test;
 
 public class OuterUnitTest {
 
-  private static final String NEST_HOST_NAME = "com.baeldung.Outer";
+    private static final String NEST_HOST_NAME = "com.baeldung.Outer";
 
-  @Test
-  public void whenGetNestHostFromOuter_thenGetNestHost() {
-    is(Outer.class.getNestHost().getName()).equals(NEST_HOST_NAME);
-  }
+    @Test
+    public void whenGetNestHostFromOuter_thenGetNestHost() {
+        is(Outer.class.getNestHost().getName()).equals(NEST_HOST_NAME);
+    }
 
-  @Test
-  public void whenGetNestHostFromInner_thenGetNestHost() {
-    is(Outer.Inner.class.getNestHost().getName()).equals(NEST_HOST_NAME);
-  }
+    @Test
+    public void whenGetNestHostFromInner_thenGetNestHost() {
+        is(Outer.Inner.class.getNestHost().getName()).equals(NEST_HOST_NAME);
+    }
 
-  @Test
-  public void whenCheckNestmatesForNestedClasses_thenGetTrue() {
-    is(Outer.Inner.class.isNestmateOf(Outer.class)).equals(true);
-  }
+    @Test
+    public void whenCheckNestmatesForNestedClasses_thenGetTrue() {
+        is(Outer.Inner.class.isNestmateOf(Outer.class)).equals(true);
+    }
 
-  @Test
-  public void whenCheckNestmatesForUnrelatedClasses_thenGetFalse() {
-    is(Outer.Inner.class.isNestmateOf(Outer.class)).equals(false);
-  }
+    @Test
+    public void whenCheckNestmatesForUnrelatedClasses_thenGetFalse() {
+        is(Outer.Inner.class.isNestmateOf(Outer.class)).equals(false);
+    }
 
-  @Test
-  public void whenGetNestMembersForNestedClasses_thenGetAllNestedClasses() {
-    List<String> nestMembers = Arrays.stream(Outer.Inner.class.getNestMembers())
-        .map(Class::getName)
-        .collect(Collectors.toList());
+    @Test
+    public void whenGetNestMembersForNestedClasses_thenGetAllNestedClasses() {
+        List<String> nestMembers = Arrays.stream(Outer.Inner.class.getNestMembers())
+          .map(Class::getName)
+          .collect(Collectors.toList());
 
-    is(nestMembers.size()).equals(2);
+        is(nestMembers.size()).equals(2);
 
-    boolean containsOuter = nestMembers.stream()
-        .anyMatch("com.baeldung.Outer"::equals);
-    is(containsOuter).equals(true);
+        boolean containsOuter = nestMembers.stream()
+          .anyMatch("com.baeldung.Outer"::equals);
+        is(containsOuter).equals(true);
 
-    boolean containsInner = nestMembers.stream()
-        .anyMatch("com.baeldung.Outer$Inner"::equals);
-    is(containsInner).equals(true);
-  }
+        boolean containsInner = nestMembers.stream()
+          .anyMatch("com.baeldung.Outer$Inner"::equals);
+        is(containsInner).equals(true);
+    }
 }
