4c7303c2936706da2aa207eae4b41f0caf8f9d19
Steven van Beelen <steven.vanbeelen@axoniq.io>
2018-12-27 10:46:45 +0100

    BAEL-2435 Rename main file
    Rename main file from MessagesRunner to OrderApplication

--- axon/src/main/java/com/baeldung/axon/OrderApplication.java
+++ axon/src/main/java/com/baeldung/axon/OrderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessagesRunner {
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MessagesRunner.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 
 }
