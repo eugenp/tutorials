c5f5c9e006bb9b48184e7f896b178a7ba281e0ae
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2019-01-02 16:38:02 -0300

    Update Application class

--- core-java/src/main/java/com/baeldung/threadsafety/application/Application.java
+++ core-java/src/main/java/com/baeldung/threadsafety/application/Application.java
@@ -2,9 +2,15 @@ package com.baeldung.threadsafety.application;
 
 import com.baeldung.threadsafety.mathutils.MathUtils;
 import com.baeldung.threadsafety.callables.CounterCallable;
+import com.baeldung.threadsafety.callables.ExtrinsicLockCounterCallable;
 import com.baeldung.threadsafety.callables.MessageServiceCallable;
+import com.baeldung.threadsafety.callables.ReentranReadWriteLockCounterCallable;
+import com.baeldung.threadsafety.callables.ReentrantLockCounterCallable;
 import com.baeldung.threadsafety.services.Counter;
+import com.baeldung.threadsafety.services.ExtrinsicLockCounter;
 import com.baeldung.threadsafety.services.MessageService;
+import com.baeldung.threadsafety.services.ReentrantLockCounter;
+import com.baeldung.threadsafety.services.ReentrantReadWriteLockCounter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -27,7 +33,7 @@ public class Application {
             System.out.println(MathUtils.factorial(5));
         }).start();
         
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
         MessageService messageService = new MessageService("Welcome to Baeldung!");
         Future<String> future1 = (Future<String>) executorService.submit(new MessageServiceCallable(messageService));
         Future<String> future2 = (Future<String>) executorService.submit(new MessageServiceCallable(messageService));
@@ -40,11 +46,29 @@ public class Application {
         System.out.println(future3.get());
         System.out.println(future4.get());
         
+        ExtrinsicLockCounter extrinsicLockCounter = new ExtrinsicLockCounter();
+        Future<Integer> future5 = (Future<Integer>) executorService.submit(new ExtrinsicLockCounterCallable(extrinsicLockCounter));
+        Future<Integer> future6 = (Future<Integer>) executorService.submit(new ExtrinsicLockCounterCallable(extrinsicLockCounter));
+        System.out.println(future5.get());
+        System.out.println(future6.get());
+        
+        ReentrantLockCounter reentrantLockCounter = new ReentrantLockCounter();
+        Future<Integer> future7 = (Future<Integer>) executorService.submit(new ReentrantLockCounterCallable(reentrantLockCounter));
+        Future<Integer> future8 = (Future<Integer>) executorService.submit(new ReentrantLockCounterCallable(reentrantLockCounter));
+        System.out.println(future7.get());
+        System.out.println(future8.get());
+        
+        ReentrantReadWriteLockCounter reentrantReadWriteLockCounter = new ReentrantReadWriteLockCounter();
+        Future<Integer> future9 = (Future<Integer>) executorService.submit(new ReentranReadWriteLockCounterCallable(reentrantReadWriteLockCounter));
+        Future<Integer> future10 = (Future<Integer>) executorService.submit(new ReentranReadWriteLockCounterCallable(reentrantReadWriteLockCounter));
+        System.out.println(future9.get());
+        System.out.println(future10.get());
+        
         Collection<Integer> syncCollection = Collections.synchronizedCollection(new ArrayList<>());
-        Thread thread17 = new Thread(() -> syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        Thread thread18 = new Thread(() -> syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6)));
-        thread17.start();
-        thread18.start();
+        Thread thread11 = new Thread(() -> syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        Thread thread12 = new Thread(() -> syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        thread11.start();
+        thread12.start();
         
         Map<String,String> concurrentMap = new ConcurrentHashMap<>();
         concurrentMap.put("1", "one");
