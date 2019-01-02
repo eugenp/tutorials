f9e560b2a078727a5cc4bc59419cc80a96ac38e1
geroza <rozagerardo@gmail.com>
2019-01-02 15:22:50 -0200

    Added examples fow webclient + oauth2

--- spring-5-reactive-oauth/src/main/java/com/baeldung/webclient/clientcredentials/service/WebClientChonJob.java
+++ spring-5-reactive-oauth/src/main/java/com/baeldung/webclient/clientcredentials/service/WebClientChonJob.java
@@ -19,7 +19,7 @@ public class WebClientChonJob {
     @Autowired
     private WebClient webClient;
 
-    @Scheduled(fixedRate = 10000)
+    @Scheduled(fixedRate = 1000)
     public void logResourceServiceResponse() {
 
         webClient.get()
