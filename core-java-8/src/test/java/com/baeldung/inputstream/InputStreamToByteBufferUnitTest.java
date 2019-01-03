52c0433e4391a3f58f31910ac26d01453fe6b030
Leo Tolstoy <eaudet@jarics.com>
2019-01-03 15:34:07 -0500

    Formatting changes

--- core-java-8/src/test/java/com/baeldung/inputstream/InputStreamToByteBufferUnitTest.java
+++ core-java-8/src/test/java/com/baeldung/inputstream/InputStreamToByteBufferUnitTest.java
@@ -9,17 +9,23 @@ import java.nio.ByteBuffer;
 import org.junit.jupiter.api.Test;
 
 class InputStreamToByteBufferUnitTest {
+@Test
+void giventUsingCoreClasses_whenWritingAFileIntoAByteBuffer_thenBytesLengthMustMatch()
+  throws IOException {
 
+    ClassLoader classLoader = new InputStreamToByteBufferUnitTest().getClass().getClassLoader();
 
-    @Test
-    void when_ReadingInputFileInByteBuffer_then_InputFileAndByteBufferSizeMustMatch() throws IOException {
-        String fileName = "frontenac-2257154_960_720.jpg";
-        ClassLoader classLoader = new InputStreamToByteBufferUnitTest().getClass().getClassLoader();
-        File inputFile = new File(classLoader.getResource(fileName).getFile());
-        ByteBuffer bufferByte =
-            ByteBuffer.allocate((int) inputFile.length());
-        FileInputStream in = new FileInputStream(inputFile);
-        in.getChannel().read(bufferByte);
-        assertEquals(bufferByte.position(),inputFile.length());
-    }
+    String fileName = "frontenac-2257154_960_720.jpg";
+
+    File inputFile = new File(classLoader.getResource(fileName).getFile());
+
+    ByteBuffer bufferByte =
+      ByteBuffer.allocate((int) inputFile.length());
+
+    FileInputStream in = new FileInputStream(inputFile);
+
+    in.getChannel().read(bufferByte);
+
+    assertEquals(bufferByte.position(),inputFile.length());
+}
 }
