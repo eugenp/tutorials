88e928ac997eafd8cb2f81ec936ad49653cfb4d0
Emily Cheyne <emilylcheyne@gmail.com>
2019-01-04 10:27:48 -0700

    Fix formatting

--- java-strings/src/main/java/com/baeldung/string/tostring/CustomerReflectionToString.java
+++ java-strings/src/main/java/com/baeldung/string/tostring/CustomerReflectionToString.java
@@ -13,18 +13,23 @@ public class CustomerReflectionToString extends Customer{
     public Integer getScore() {
         return score;
     }
+    
     public void setScore(Integer score) {
         this.score = score;
     }
+    
     public List<String> getOrders() {
         return orders;
     }
+    
     public void setOrders(List<String> orders) {
         this.orders = orders;
     }
+    
     public StringBuffer getFullname() {
         return fullname;
     }
+    
     public void setFullname(StringBuffer fullname) {
         this.fullname = fullname;
     }
