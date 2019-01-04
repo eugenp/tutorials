78387abdf375089929a039a67231885688cd7784
Leo Tolstoy <eaudet@jarics.com>
2019-01-04 10:22:56 -0500

    Code reformat using intellij formatter.

--- core-java-8/src/test/java/com/baeldung/inputstream/InputStreamToByteBufferUnitTest.java
+++ core-java-8/src/test/java/com/baeldung/inputstream/InputStreamToByteBufferUnitTest.java
@@ -1,32 +1,37 @@
 package com.baeldung.inputstream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class InputStreamToByteBufferUnitTest {
 
-@Test
-void givenUsingCoreClasses_whenWritingAFileIntoAByteBuffer_thenBytesLengthMustMatch()
-  throws IOException {
+    @Test
+    void givenUsingCoreClasses_whenWritingAFileIntoAByteBuffer_thenBytesLengthMustMatch() throws IOException {
 
-    ClassLoader classLoader = new InputStreamToByteBufferUnitTest().getClass().getClassLoader();
+        ClassLoader classLoader = new InputStreamToByteBufferUnitTest()
+          .getClass()
+          .getClassLoader();
 
-    String fileName = "frontenac-2257154_960_720.jpg";
+        String fileName = "frontenac-2257154_960_720.jpg";
 
-    File inputFile = new File(classLoader.getResource(fileName).getFile());
+        File inputFile = new File(classLoader
+          .getResource(fileName)
+          .getFile());
 
-    ByteBuffer bufferByte =
-      ByteBuffer.allocate((int) inputFile.length());
+        ByteBuffer bufferByte = ByteBuffer.allocate((int) inputFile.length());
 
-    FileInputStream in = new FileInputStream(inputFile);
+        FileInputStream in = new FileInputStream(inputFile);
 
-    in.getChannel().read(bufferByte);
+        in
+          .getChannel()
+          .read(bufferByte);
 
-    assertEquals(bufferByte.position(), inputFile.length());
-}
+        assertEquals(bufferByte.position(), inputFile.length());
+    }
 }
