6b24f9801c01c18dc90d9dd47bc06c7ea90bfda9
AUTODATACORP\andarik <kandari@autodata.net>
2019-01-04 08:14:27 -0500

    Adding code to java core and deleting it from java-8

---
+++ core-java/src/main/java/com/baeldung/interfaces/Box.java
@@ -0,0 +1,6 @@
+package com.baeldung.interfaces;
+
+public interface Box extends HasColor {
+
+    int getHeight();
+}
--- core-java/src/main/java/com/baeldung/interfaces/Employee.java
+++ core-java/src/main/java/com/baeldung/interfaces/Employee.java
@@ -11,5 +11,4 @@ public class Employee {
     public void setSalary(double salary) {
         this.salary = salary;
     }
-
 }
