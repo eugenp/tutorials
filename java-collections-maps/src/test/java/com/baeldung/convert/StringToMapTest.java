c9e5fae676dd963e0d99b0cc8234e76efcca6d77
Laurentiu <delcea.laurentiu@gmail.com>
2019-01-05 13:35:33 +0200

    BAEL-2480 Java Map to String conversion

---
+++ java-collections-maps/src/test/java/com/baeldung/convert/StringToMapTest.java
@@ -0,0 +1,23 @@
+package com.baeldung.convert;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+public class StringToMapTest {
+
+    @Test
+    public void testConversionViaStream() {
+        Map<String, String> wordsByKey = StringToMap.convertWithStream("1=one,2=two,3=three,4=four");
+        Assert.assertEquals(4, wordsByKey.size());
+        Assert.assertEquals("one", wordsByKey.get("1"));
+    }
+
+    @Test
+    void testConversionViaGuava() {
+        Map<String, String> wordsByKey = StringToMap.convertWithGuava("1=one,2=two,3=three,4=four");
+        Assert.assertEquals(4, wordsByKey.size());
+        Assert.assertEquals("one", wordsByKey.get("1"));
+    }
+}
