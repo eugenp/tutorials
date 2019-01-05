9424e55904e58cd6a58fbd396d209154d7fffe2d
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2019-01-05 16:48:24 -0300

    Initial Commit

---
+++ core-java-concurrency-basic/src/main/java/com/baeldung/concurrent/threadsafety/application/Application.java
@@ -0,0 +1,54 @@
+package com.baeldung.concurrent.threadsafety.application;
+
+import com.baeldung.concurrent.threadsafety.mathutils.MathUtils;
+import com.baeldung.concurrent.threadsafety.callables.CounterCallable;
+import com.baeldung.concurrent.threadsafety.callables.MessageServiceCallable;
+import com.baeldung.concurrent.threadsafety.services.Counter;
+import com.baeldung.concurrent.threadsafety.services.MessageService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Application {
+    
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+        new Thread(() -> {
+            System.out.println(MathUtils.factorial(10));
+        }).start();
+        new Thread(() -> {
+            System.out.println(MathUtils.factorial(5));
+        }).start();
+        
+        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        MessageService messageService = new MessageService("Welcome to Baeldung!");
+        Future<String> future1 = (Future<String>) executorService.submit(new MessageServiceCallable(messageService));
+        Future<String> future2 = (Future<String>) executorService.submit(new MessageServiceCallable(messageService));
+        System.out.println(future1.get());
+        System.out.println(future2.get());
+        
+        Counter counter = new Counter();
+        Future<Integer> future3 = (Future<Integer>) executorService.submit(new CounterCallable(counter));
+        Future<Integer> future4 = (Future<Integer>) executorService.submit(new CounterCallable(counter));
+        System.out.println(future3.get());
+        System.out.println(future4.get());
+        
+        Collection<Integer> syncCollection = Collections.synchronizedCollection(new ArrayList<>());
+        Thread thread17 = new Thread(() -> syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        Thread thread18 = new Thread(() -> syncCollection.addAll(Arrays.asList(1, 2, 3, 4, 5, 6)));
+        thread17.start();
+        thread18.start();
+        
+        Map<String,String> concurrentMap = new ConcurrentHashMap<>();
+        concurrentMap.put("1", "one");
+        concurrentMap.put("2", "two");
+        concurrentMap.put("3", "three");
+    }
+}
