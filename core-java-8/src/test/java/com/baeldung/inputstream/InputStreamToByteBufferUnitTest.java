06204ad6552200ea7afa43d77614ba7ed663267e
Leo Tolstoy <eaudet@jarics.com>
2019-01-05 19:05:09 -0500

    guava implementation to be completed

--- core-java-8/src/test/java/com/baeldung/inputstream/InputStreamToByteBufferUnitTest.java
+++ core-java-8/src/test/java/com/baeldung/inputstream/InputStreamToByteBufferUnitTest.java
@@ -1,37 +1,68 @@
 package com.baeldung.inputstream;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class InputStreamToByteBufferUnitTest {
 
     @Test
     public void givenUsingCoreClasses_whenWritingAFileIntoAByteBuffer_thenBytesLengthMustMatch() throws IOException {
 
-        ClassLoader classLoader = new InputStreamToByteBufferUnitTest()
-          .getClass()
-          .getClassLoader();
-
-        String fileName = "frontenac-2257154_960_720.jpg";
-
-        File inputFile = new File(classLoader
-          .getResource(fileName)
-          .getFile());
+        File inputFile = getFile();
 
         ByteBuffer bufferByte = ByteBuffer.allocate((int) inputFile.length());
 
+        // read all bytes into the allocated ByteBuffer
         FileInputStream in = new FileInputStream(inputFile);
-
         in
           .getChannel()
           .read(bufferByte);
 
         assertEquals(bufferByte.position(), inputFile.length());
     }
+
+    @Test
+    public void givenUsingCommonsIo_whenWritingAFileIntoAByteBuffer_thenBytesLengthMustMatch() throws IOException {
+
+        File inputFile = getFile();
+
+        ByteBuffer bufferByte = ByteBuffer.allocateDirect((int) inputFile.length());
+
+        ReadableByteChannel readableByteChannel = new FileInputStream(inputFile).getChannel();
+
+        IOUtils.readFully(readableByteChannel, bufferByte);
+
+        assertEquals(bufferByte.position(), inputFile.length());
+    }
+
+    @Test
+    public void givenUsingGuava_whenWritingAFileIntoAByteBuffer_thenBytesLengthMustMatch() {
+
+        File inputFile = getFile();
+
+        assertTrue(false);
+
+    }
+
+    private File getFile() {
+        ClassLoader classLoader = new InputStreamToByteBufferUnitTest()
+          .getClass()
+          .getClassLoader();
+
+        String fileName = "frontenac-2257154_960_720.jpg";
+
+        return new File(classLoader
+          .getResource(fileName)
+          .getFile());
+    }
+
 }
