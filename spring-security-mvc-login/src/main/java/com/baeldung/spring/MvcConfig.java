74df0da304af2152a45ecf60a5f65ae77e0bc474
geroza <rozagerardo@gmail.com>
2019-01-09 12:28:21 -0200

    fixed removed import by mistake

--- spring-security-mvc-login/src/main/java/com/baeldung/spring/MvcConfig.java
+++ spring-security-mvc-login/src/main/java/com/baeldung/spring/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.baeldung.spring;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
