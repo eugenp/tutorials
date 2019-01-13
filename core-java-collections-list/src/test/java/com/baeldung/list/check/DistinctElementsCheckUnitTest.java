f2db39e8b29e75afb617d1948fca158a33f30f06
Lalitha Gottumukkula Mutnuru <jee.lalitha@gmail.com>
2019-01-13 18:47:41 +0530

    Added IterableUtils and Google api implementation

--- core-java-collections-list/src/test/java/com/baeldung/list/check/DistinctElementsCheckUnitTest.java
+++ core-java-collections-list/src/test/java/com/baeldung/list/check/DistinctElementsCheckUnitTest.java
@@ -5,9 +5,13 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 
+import org.apache.commons.collections4.IterableUtils;
 import org.junit.Test;
+
+import com.google.common.collect.Iterables;
 
 public class DistinctElementsCheckUnitTest {
 
@@ -24,19 +28,33 @@ public class DistinctElementsCheckUnitTest {
 
         assertTrue(distinctList.parallelStream()
             .allMatch(e -> e.equals(distinctList.get(0))));
-
-        // 2
+   
         assertTrue(distinctList.stream()
-            .allMatch(distinctList.get(0)::equals));
+            .allMatch(distinctList.get(0) :: equals));
 
         assertTrue(distinctList.parallelStream()
-            .allMatch(distinctList.get(0)::equals));
+            .allMatch(distinctList.get(0) :: equals));
+
+        // 2
+        assertTrue(Collections.frequency(distinctList, distinctList.get(0)) == distinctList.size());
 
         // 3
-        assertTrue(Collections.frequency(distinctList, distinctList.get(0)) == distinctList.size());
-       
+        assertTrue(new HashSet<>(distinctList).size() == 1);
+        
+        // 4
+        assertTrue(IterableUtils.countMatches(distinctList, e -> e.equals(distinctList.get(0))) == distinctList.size());
+        
+        assertTrue(IterableUtils.frequency(distinctList, distinctList.get(0)) == distinctList.size()) ;
+        
+        assertTrue(IterableUtils.matchesAll(distinctList,  e -> e.equals(distinctList.get(0))));
+        
+        // 5
+        assertTrue(Iterables.all(distinctList,  e -> e.equals(distinctList.get(0))));
+        
+        assertTrue(Iterables.frequency(distinctList, distinctList.get(0)) == distinctList.size());
+        
+        
     }
-    
 
     @Test
     public void whenNonDistinctList_thenAssertFalse() {
@@ -47,15 +65,29 @@ public class DistinctElementsCheckUnitTest {
 
         assertFalse(nonDistinctList.parallelStream()
             .allMatch(e -> e.equals(nonDistinctList.get(0))));
-
-        // 2
+        
         assertFalse(nonDistinctList.stream()
-            .allMatch(nonDistinctList.get(0)::equals));
+            .allMatch(nonDistinctList.get(0) :: equals));
 
         assertFalse(nonDistinctList.parallelStream()
-            .allMatch(nonDistinctList.get(0)::equals));
+            .allMatch(nonDistinctList.get(0) :: equals));
+
+        // 2
+        assertFalse(Collections.frequency(nonDistinctList, nonDistinctList.get(0)) == nonDistinctList.size());
 
         // 3
-        assertFalse(Collections.frequency(nonDistinctList, nonDistinctList.get(0)) == nonDistinctList.size());
+        assertFalse(new HashSet<>(nonDistinctList).size() == 1);
+        
+        // 4
+        assertFalse(IterableUtils.countMatches(nonDistinctList, e -> e.equals(nonDistinctList.get(0))) == nonDistinctList.size());
+        
+        assertFalse(IterableUtils.frequency(nonDistinctList, nonDistinctList.get(0)) == nonDistinctList.size());
+        
+        assertFalse(IterableUtils.matchesAll(nonDistinctList, e -> e.equals(nonDistinctList.get(0))));
+        
+        // 5
+        assertFalse(Iterables.all(nonDistinctList,  e -> e.equals(nonDistinctList.get(0))));
+        
+        assertFalse(Iterables.frequency(nonDistinctList, nonDistinctList.get(0)) == nonDistinctList.size());
     }
 }
