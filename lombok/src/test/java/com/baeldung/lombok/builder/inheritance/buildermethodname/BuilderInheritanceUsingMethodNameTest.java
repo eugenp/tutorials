609c748edd0834a8cc6c1a68796840868a97d5c3
Dave Crane <dave@sunwheeltech.com>
2019-01-18 23:32:32 +0000

    added second subpackage showing newer approach to inheritance

---
+++ lombok/src/test/java/com/baeldung/lombok/builder/inheritance/buildermethodname/BuilderInheritanceUsingMethodNameTest.java
@@ -0,0 +1,24 @@
+package com.baeldung.lombok.builder.inheritance.buildermethodname;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BuilderInheritanceUsingMethodNameTest {
+
+    @Test
+    public void givenBuilderAtMethodLevel_ChildInheritingParentIsBuilt() {
+        Child child = Child.childBuilder()
+                .parentName("Andrea")
+                .parentAge(38)
+                .childName("Emma")
+                .childAge(6)
+                .build();
+
+        assertThat(child.getChildName()).isEqualTo("Emma");
+        assertThat(child.getChildAge()).isEqualTo(6);
+        assertThat(child.getParentName()).isEqualTo("Andrea");
+        assertThat(child.getParentAge()).isEqualTo(38);
+    }
+
+}
