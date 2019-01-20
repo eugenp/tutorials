96e782768353dc99456762b730a57319225264e2
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2019-01-20 17:41:48 +0200

    Update and rename BuilderInheritanceUsingSuperBuilderTest.java to BuilderInheritanceUsingSuperBuilderUnitTest.java

--- lombok/src/test/java/com/baeldung/lombok/builder/inheritance/superbuilder/BuilderInheritanceUsingSuperBuilderUnitTest.java
+++ lombok/src/test/java/com/baeldung/lombok/builder/inheritance/superbuilder/BuilderInheritanceUsingSuperBuilderUnitTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class BuilderInheritanceUsingSuperBuilderTest {
+public class BuilderInheritanceUsingSuperBuilderUnitTest {
 
     @Test
     public void givenSuperBuilderOnParentAndOnChild_ChildInheritingParentIsBuilt() {
