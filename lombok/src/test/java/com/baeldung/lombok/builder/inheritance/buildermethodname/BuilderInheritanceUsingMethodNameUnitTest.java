43e59380cc7b6036448046367e321b3942c7811d
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2019-01-20 17:41:26 +0200

    Update and rename BuilderInheritanceUsingMethodNameTest.java to BuilderInheritanceUsingMethodNameUnitTest.java

--- lombok/src/test/java/com/baeldung/lombok/builder/inheritance/buildermethodname/BuilderInheritanceUsingMethodNameUnitTest.java
+++ lombok/src/test/java/com/baeldung/lombok/builder/inheritance/buildermethodname/BuilderInheritanceUsingMethodNameUnitTest.java
@@ -4,7 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-public class BuilderInheritanceUsingMethodNameTest {
+public class BuilderInheritanceUsingMethodNameUnitTest {
 
     @Test
     public void givenBuilderAtMethodLevel_ChildInheritingParentIsBuilt() {
