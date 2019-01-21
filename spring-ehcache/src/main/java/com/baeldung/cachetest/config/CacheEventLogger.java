369655aa02adf178b3d813edbc97ee603a963ffc
pcoates33 <pcoates33@users.noreply.github.com>
2019-01-21 08:14:06 +0000

    spring-boot ehcache example added (#6136)

--- spring-ehcache/src/main/java/com/baeldung/cachetest/config/CacheEventLogger.java
+++ spring-ehcache/src/main/java/com/baeldung/cachetest/config/CacheEventLogger.java
@@ -7,12 +7,11 @@ import org.slf4j.LoggerFactory;
 
 public class CacheEventLogger implements CacheEventListener<Object, Object> {
 
-	private static final Logger log = LoggerFactory.getLogger(CacheEventLogger.class);
+    private static final Logger log = LoggerFactory.getLogger(CacheEventLogger.class);
 
-	@Override
-	public void onEvent(CacheEvent<? extends Object, ? extends Object> cacheEvent) {
-		log.info("Cache event {} for item with key {}. Old value = {}, New value = {}", cacheEvent.getType(),
-				cacheEvent.getKey(), cacheEvent.getOldValue(), cacheEvent.getNewValue());
-	}
+    @Override
+    public void onEvent(CacheEvent<? extends Object, ? extends Object> cacheEvent) {
+        log.info("Cache event {} for item with key {}. Old value = {}, New value = {}", cacheEvent.getType(), cacheEvent.getKey(), cacheEvent.getOldValue(), cacheEvent.getNewValue());
+    }
 
 }
