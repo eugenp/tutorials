1a230f3ed021cf737bb82ebe38fcea6f9d85421c
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2019-01-22 18:19:40 -0300

    Initial Commit

---
+++ spring-boot-validation/src/main/java/com/baeldung/application/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.baeldung.application.repositories;
+
+import com.baeldung.application.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {}
