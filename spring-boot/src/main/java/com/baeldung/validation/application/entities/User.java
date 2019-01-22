d030efff16d813d792066d229670be692dbc9e55
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2019-01-22 17:27:29 -0300

    Update User.java

--- spring-boot/src/main/java/com/baeldung/validation/application/entities/User.java
+++ spring-boot/src/main/java/com/baeldung/validation/application/entities/User.java
@@ -1,10 +1,10 @@
 package com.baeldung.validation.application.entities;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 @Entity
