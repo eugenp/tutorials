63f1da74a68ba1f2805511d1326b923193bcf66a
alv21 <alv21>
2019-01-23 10:38:28 -0800

    first commit

---
+++ spring-soap/src/test/java/hello/ApplicationIntegrationTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hello;
+
+import io.spring.guides.gs_producing_web_service.GetCountryRequest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ClassUtils;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ApplicationIntegrationTests {
+
+    private Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+
+    @LocalServerPort private int port = 0;
+
+    @Before
+    public void init() throws Exception {
+        marshaller.setPackagesToScan(ClassUtils.getPackageName(GetCountryRequest.class));
+        marshaller.afterPropertiesSet();
+    }
+
+    @Test
+    public void testSendAndReceive() {
+        WebServiceTemplate ws = new WebServiceTemplate(marshaller);
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName("Spain");
+
+        assertThat(ws.marshalSendAndReceive("http://localhost:" + port + "/ws", request)).isNotNull();
+    }
+}
