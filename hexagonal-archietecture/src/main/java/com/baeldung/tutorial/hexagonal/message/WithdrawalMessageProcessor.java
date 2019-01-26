25c7510182b5c9ef1e7d0c42a7221ece6debf396
Sushant <sushant57@gmail.com>
2019-01-26 10:30:22 +0100

    Formatting

--- hexagonal-archietecture/src/main/java/com/baeldung/tutorial/hexagonal/message/WithdrawalMessageProcessor.java
+++ hexagonal-archietecture/src/main/java/com/baeldung/tutorial/hexagonal/message/WithdrawalMessageProcessor.java
@@ -2,5 +2,5 @@ package com.baeldung.tutorial.hexagonal.message;
 
 public interface WithdrawalMessageProcessor {
 
-        void onMessageReceived(WithdrawalRequestMessage withdrawalRequestMessage);
+    void onMessageReceived(WithdrawalRequestMessage withdrawalRequestMessage);
 }
