fd70f654b54e3ac59a9477809aed14c833ec15fa
mherbaghinyan <mbaghinyan@gmail.com>
2019-01-27 17:45:34 +0400

    primitive list collections

--- core-java-collections-list/src/main/java/com/baeldung/list/primitive/PrimitiveCollections.java
+++ core-java-collections-list/src/main/java/com/baeldung/list/primitive/PrimitiveCollections.java
@@ -2,6 +2,8 @@ package com.baeldung.list.primitive;
 
 import com.google.common.primitives.ImmutableIntArray;
 import com.google.common.primitives.Ints;
+import gnu.trove.list.array.TIntArrayList;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,6 +15,18 @@ public class PrimitiveCollections {
         int[] primitives = new int[] {5, 10, 0, 2};
 
         guavaPrimitives(primitives);
+
+        TIntArrayList tList = new TIntArrayList(primitives);
+
+        cern.colt.list.IntArrayList coltList = new cern.colt.list.IntArrayList(primitives);
+
+        IntArrayList fastUtilList = new IntArrayList(primitives);
+
+        System.out.println(tList);
+
+        System.out.println(coltList);
+
+        System.out.println(fastUtilList);
     }
 
 
