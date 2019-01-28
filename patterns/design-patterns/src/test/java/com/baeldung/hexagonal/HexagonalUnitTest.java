f4795089e61018a2aa34bed3d228940471422d45
amolpdongare <amol.p.dongare@gmail.com>
2019-01-29 01:35:50 +0530

    refactored HexagonalUnitTest

--- patterns/design-patterns/src/test/java/com/baeldung/hexagonal/HexagonalUnitTest.java
+++ patterns/design-patterns/src/test/java/com/baeldung/hexagonal/HexagonalUnitTest.java
@@ -12,12 +12,12 @@ import com.baeldung.hexagonal.adaptors.LoggingOperation;
 import com.baeldung.hexagonal.core.Customer;
 import com.baeldung.hexagonal.core.CustomerCore;
 
-public class HexagonalTest{
+public class HexagonalUnitTest{
     @Test
     public void AppTest(){
         CustomerCore cusCore =new CustomerCore(new EmailNotificationService(),new DatabaseOperation(),new LoggingOperation(),new CustomerInfoService());
         assertEquals(Boolean.TRUE,cusCore.updateCustomerInformation());  
-        assertEquals(Boolean.TRUE,cusCore.addCustomerData(new Customer(123, "Amol", "XXX", "abc@xx.com", 012344545455)));
+        assertEquals(Boolean.TRUE,cusCore.addCustomerData(new Customer(123, "TestCustomer", "XXX", "abc@xx.com", 012344545455)));
         assertEquals(Boolean.TRUE,cusCore.sendEmail(123));
     }
 
