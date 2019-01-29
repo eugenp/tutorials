0b988db96d1fa03768402b81360722d0606114b8
Ger Roza <rozagerardo@gmail.com>
2019-01-29 17:04:33 -0200

    Moved Building REST API article related code from spring-rest-full to spring-boot-rest module

--- spring-rest-full/src/main/java/org/baeldung/persistence/IOperations.java
+++ spring-rest-full/src/main/java/org/baeldung/persistence/IOperations.java
@@ -16,11 +16,7 @@ public interface IOperations<T extends Serializable> {
     // write
 
     T create(final T entity);
-
+    
     T update(final T entity);
-
-    void delete(final T entity);
-
-    void deleteById(final long entityId);
 
 }
