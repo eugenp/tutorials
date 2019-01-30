2ceef05c8959ef831526cd9f492b591552213eb4
soufiane-cheouati <46105138+soufiane-cheouati@users.noreply.github.com>
2019-01-30 22:37:02 +0000

    Adding marker interface classes

---
+++ core-java-lang-oop/src/main/java/com/baeldung/markerinterface/Rectangle.java
@@ -0,0 +1,22 @@
+package com.baeldung.markerinterface;
+
+public class Rectangle implements Deletable {
+
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return width * height;
+    }
+
+    @Override
+    public double getCircumference() {
+        return 2 * (width + height);
+    }
+}
