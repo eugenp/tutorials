ca98fbb7062c13129ee3193b679a64ad9da63f84
mherbaghinyan <mbaghinyan@gmail.com>
2019-01-31 16:18:59 +0400

    Added Stream Api

--- core-java-collections-list/src/main/java/com/baeldung/list/primitive/PrimitiveCollections.java
+++ core-java-collections-list/src/main/java/com/baeldung/list/primitive/PrimitiveCollections.java
@@ -7,14 +7,19 @@ import it.unimi.dsi.fastutil.ints.IntArrayList;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.OptionalDouble;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
 public class PrimitiveCollections {
 
     public static void main(String[] args) {
 
-        int[] primitives = new int[] {5, 10, 0, 2};
+        int[] primitives = new int[] {5, 10, 0, 2, -8};
 
         guavaPrimitives(primitives);
+
+        intStream(primitives);
 
         TIntArrayList tList = new TIntArrayList(primitives);
 
@@ -27,6 +32,15 @@ public class PrimitiveCollections {
         System.out.println(coltList);
 
         System.out.println(fastUtilList);
+    }
+
+    private static void intStream(int[] primitives) {
+
+        IntStream stream = IntStream.of(5, 10, 0, 2, -8);
+
+        IntStream newStream = IntStream.of(primitives);
+
+        OptionalDouble average = stream.filter(i -> i > 0).average();
     }
 
 
