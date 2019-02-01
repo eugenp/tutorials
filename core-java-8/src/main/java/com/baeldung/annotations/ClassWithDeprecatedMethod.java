a29c428d935452f258dbb3021a0920836e9296b8
jose <larroquester@gmail.com>
2019-01-31 22:46:32 -0300

    BAEL-2570: Added an example of @Deprecated with the optional attributes available in Java 9

--- core-java-8/src/main/java/com/baeldung/annotations/ClassWithDeprecatedMethod.java
+++ core-java-8/src/main/java/com/baeldung/annotations/ClassWithDeprecatedMethod.java
@@ -3,6 +3,8 @@ package com.baeldung.annotations;
 class ClassWithDeprecatedMethod {
 
     @Deprecated
+    // Java 9 example of @Deprecated
+    // @Deprecated(since = "4.5", forRemoval = true)
     static void deprecatedMethod() {
 
     }
