2983e1c26ae5a49dd41459d050b3b92cb30676ab
Andrey Shcherbakov <veontomo@gmail.com>
2019-02-02 20:54:09 +0100

    Add the live test to spring-exceptions module

--- spring-exceptions/src/test/java/org/baeldung/SpringContextLiveTest.java
+++ spring-exceptions/src/test/java/org/baeldung/SpringContextLiveTest.java
@@ -1,16 +1,15 @@
 package org.baeldung;
 
-import org.baeldung.spring.config.CoreConfig;
-import org.baeldung.spring.config.MvcConfig;
 import org.baeldung.spring.config.PersistenceConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { CoreConfig.class, MvcConfig.class, PersistenceConfig.class})
-public class SpringContextIntegrationTest {
+@ContextConfiguration(classes = { PersistenceConfig.class }, loader = AnnotationConfigContextLoader.class)
+public class SpringContextLiveTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
