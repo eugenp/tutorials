b5433fff9ccac45554c3bca3d6290428255b7e46
Fabio Borriello <borriello.fabio@gmail.com>
2019-02-06 06:32:01 +0100

    Defined a specific method for the definining the lambda functions required for special order field conversion

--- performance-tests/src/main/java/com/baeldung/performancetests/bull/BULLConverter.java
+++ performance-tests/src/main/java/com/baeldung/performancetests/bull/BULLConverter.java
@@ -16,23 +16,36 @@ public class BULLConverter implements Converter {
     private final BeanUtils beanUtils;
 
     public BULLConverter() {
-        BeanUtils beanUtils = new BeanUtils();
-        this.beanUtils = beanUtils;
+        this.beanUtils = new BeanUtils();
     }
 
     @Override
     public Order convert(SourceOrder sourceOrder) {
+        return beanUtils.getTransformer()
+                .withFieldMapping(new FieldMapping("status", "orderStatus"))
+                .withFieldTransformer(getOrderTransformerFunctions(sourceOrder))
+                .transform(sourceOrder, Order.class);
+    }
+
+    /**
+     * Initialized the lambda functions required for special order field conversion.
+     * @param sourceOrder the source object
+     * @return an array containing the required lambda functions required for special order field conversion.
+     */
+    private FieldTransformer[] getOrderTransformerFunctions(final SourceOrder sourceOrder) {
         FieldTransformer<AccountStatus, com.baeldung.performancetests.model.destination.AccountStatus> accountStatusTransformer =
                 new FieldTransformer<>("userAccountStatus", userAccountStatus ->
                         new com.baeldung.performancetests.model.destination.User().conversion(sourceOrder.getOrderingUser().getUserAccountStatus()));
+
         FieldTransformer<OrderStatus, com.baeldung.performancetests.model.destination.OrderStatus> orderStatusTransformer =
                 new FieldTransformer<>("orderStatus", orderStatus -> new Order().conversion(sourceOrder.getStatus()));
+
         FieldTransformer<PaymentType, com.baeldung.performancetests.model.destination.PaymentType> paymentTypeTransformer =
                 new FieldTransformer<>("paymentType", paymentType -> new Order().conversion(sourceOrder.getPaymentType()));
-        return beanUtils.getTransformer()
-                .withFieldMapping(new FieldMapping("status", "orderStatus"))
-                .withFieldTransformer(accountStatusTransformer, orderStatusTransformer, paymentTypeTransformer)
-                .transform(sourceOrder, Order.class);
+
+        return new FieldTransformer[] {
+                accountStatusTransformer, orderStatusTransformer, paymentTypeTransformer
+        };
     }
 
     @Override
