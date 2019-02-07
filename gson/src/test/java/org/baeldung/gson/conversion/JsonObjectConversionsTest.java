cb7cf999ca337097051146f08054ebb0df876187
David Calap Bayarri <dcalap@gmail.com>
2019-02-07 19:38:21 +0100

    BAEL-2738 - Convert String to JsonObject with GSON

---
+++ gson/src/test/java/org/baeldung/gson/conversion/JsonObjectConversionsTest.java
@@ -0,0 +1,27 @@
+package org.baeldung.gson.conversion;
+
+import com.google.gson.*;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class JsonObjectConversionsTest {
+
+    @Test
+    void givenAStringAndJsonParser_shouldConvertToJsonObject() throws Exception {
+        // Example 1: Using JsonParser
+        String json = "{ \"name\": \"Baeldung\", \"java\": true }";
+        JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        Assert.assertTrue(jsonObject.isJsonObject());
+        Assert.assertTrue(jsonObject.get("name").getAsString().equals("Baeldung"));
+        Assert.assertTrue(jsonObject.get("java").getAsBoolean() == true);
+    }
+
+    @Test
+    void givenAStringAndGsonInstance_shouldConvertToJsonObject() throws Exception {
+        // Example 2: Using fromJson
+        String json = "{ \"name\": \"Baeldung\", \"java\": true }";
+        JsonObject convertedObject = new Gson().fromJson(json, JsonObject.class);
+        Assert.assertTrue(convertedObject.isJsonObject());
+    }
+
+}
