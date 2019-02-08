3704450509574b98c33bf504090d514754e8e592
mherbaghinyan <mbaghinyan@gmail.com>
2019-02-08 14:55:23 +0400

    more tests

--- core-java-collections/src/main/java/com/baeldung/performance/ArraySortBenchmark.java
+++ core-java-collections/src/main/java/com/baeldung/performance/ArraySortBenchmark.java
@@ -22,28 +22,46 @@ public class ArraySortBenchmark {
 
         int iterations = 1000;
 
-        int[] array = new int[iterations];
-        List<Integer> list = new ArrayList<>();
+        String[] array = new String[iterations];
+        List<String> list = new ArrayList<>();
+
+        int[] intArray = new int[iterations];
+        List<Integer> integerList = new ArrayList<>();
 
         @Setup(Level.Trial)
         public void setUp() {
             for (int i = 0; i < iterations; i++) {
-                array[i] = i;
-                list.add(i);
+                array[i] = i + "";
+                list.add(i + "");
+
+                intArray[i] = i;
+                integerList.add(i);
             }
         }
     }
 
     @Benchmark
-    public int[] benchmarkArraysSort(ArraySortBenchmark.Initialize state) {
+    public String[] benchmarkArraysSort(ArraySortBenchmark.Initialize state) {
         Arrays.sort(state.array);
         return state.array;
     }
 
     @Benchmark
-    public List<Integer> benchmarkCollectionsSort(ArraySortBenchmark.Initialize state) {
+    public List<String> benchmarkCollectionsSort(ArraySortBenchmark.Initialize state) {
         Collections.sort(state.list);
         return state.list;
+    }
+
+    @Benchmark
+    public int[] benchmarkArraysSortInt(ArraySortBenchmark.Initialize state) {
+        Arrays.sort(state.intArray);
+        return state.intArray;
+    }
+
+    @Benchmark
+    public List<Integer> benchmarkCollectionsSortInteger(ArraySortBenchmark.Initialize state) {
+        Collections.sort(state.integerList);
+        return state.integerList;
     }
 
     public static void main(String[] args) throws Exception {
