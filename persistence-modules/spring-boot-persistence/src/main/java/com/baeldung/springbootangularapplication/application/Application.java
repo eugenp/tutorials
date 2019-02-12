a6d0e54bc0d17a3100426af4f34c531b53f66880
Alejandro Gervasio <electricalweb@yahoo.com.ar>
2019-02-12 18:47:35 -0300

    Initial Commit

---
+++ persistence-modules/spring-boot-persistence/src/main/java/com/baeldung/springbootangularapplication/application/Application.java
@@ -0,0 +1,28 @@
+package com.baeldung.springbootangularapplication.application;
+
+import com.baeldung.springbootangularapplication.application.entities.User;
+import com.baeldung.springbootangularapplicationn.application.repositories.UserRepository;
+import java.util.stream.Stream;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
