fc2541f317e9d31e5cc7273763e3138f70f63300
himanshusuri <himanshu.suri@gmail.com>
2019-02-12 21:36:36 +0100

    Create MemberOpsAdapter.java

---
+++ hexagonal-architecture/src/main/java/com/suri/blogic/MemberOpsAdapter.java
@@ -0,0 +1,15 @@
+package com.suri.blogic.adapters;
+  
+import com.suri.blogic.ports.MemberOpsPort;
+import com.suri.model.Member;
+
+public class MemberOpsAdapter implements MemberOpsPort {
+
+    public String addMember(Member member) {
+        return null;
+    }
+
+    public String removeMember(Member member) {
+        return null;
+    }
+}
