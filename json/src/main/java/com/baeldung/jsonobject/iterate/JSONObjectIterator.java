66b41f015117bb1569ebf1d800dd0f227aa0d6dc
saukedia1 <saukedia1@WKWIN2407161.mshome.net>
2019-02-13 16:38:56 +0530

    BAEL-2720: Iterating over an org.json.JSONObject

---
+++ json/src/main/java/com/baeldung/jsonobject/iterate/JSONObjectIterator.java
@@ -0,0 +1,59 @@
+package com.baeldung.jsonobject.iterate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class JSONObjectIterator {
+
+    private Map<String, Object> keyValuePairs;
+
+    public JSONObjectIterator() {
+        keyValuePairs = new HashMap<>();
+    }
+
+    public void handleJSONObject(JSONObject jsonObject) {
+        Iterator<String> jsonObjectIterator = jsonObject.keys();
+        jsonObjectIterator.forEachRemaining(key -> {
+            Object value = jsonObject.get(key);
+            boolean isJSONArray = value instanceof JSONArray;
+            boolean isJSONObject = value instanceof JSONObject;
+            if (isJSONArray) {
+                handleJSONArray(key, jsonObject.getJSONArray(key));
+            } else if (isJSONObject) {
+                handleJSONObject(jsonObject.getJSONObject(key));
+            } else {
+                keyValuePairs.put(key, value);
+            }
+        });
+    }
+
+    public void handleJSONArray(String key, JSONArray jsonArray) {
+        Iterator<Object> jsonArrayIterator = jsonArray.iterator();
+        List<Object> list = new ArrayList<>();
+        jsonArrayIterator.forEachRemaining(element -> {
+            if (element instanceof JSONObject) {
+                handleJSONObject((JSONObject) element);
+            } else if (element instanceof JSONArray) {
+                handleJSONArray(key, (JSONArray) element);
+            } else {
+                list.add(element);
+            }
+        });
+        keyValuePairs.put(key, list);
+    }
+
+    public Map<String, Object> getKeyValuePairs() {
+        return keyValuePairs;
+    }
+
+    public void setKeyValuePairs(Map<String, Object> keyValuePairs) {
+        this.keyValuePairs = keyValuePairs;
+    }
+
+}
