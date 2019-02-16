c4f47a709f28ff894306d8fd619d93a4f7bdfeb6
blockchain-trainer <blockchaintrainer.in@gmail.com>
2019-02-17 00:42:14 +0530

    [BAEL-2718] - Spring JPA Batch Inserts New PR

---
+++ persistence-modules/spring-data-jpa/src/test/java/com/baeldung/BatchInsertIntegrationTest.java
@@ -0,0 +1,44 @@
+package com.baeldung;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.baeldung.config.PersistenceConfiguration;
+import com.baeldung.config.PersistenceProductConfiguration;
+import com.baeldung.config.PersistenceUserConfiguration;
+import com.baeldung.controller.CustomerController;
+import com.baeldung.dao.repositories.CustomerRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(classes = { PersistenceConfiguration.class, PersistenceProductConfiguration.class, PersistenceUserConfiguration.class })
+public class BatchInsertIntegrationTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+    private MockMvc mockMvc;
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup( new CustomerController(customerRepository))
+            .build();
+    }
+
+    @Test
+    public void whenInsertingCustomers_thenCustomersAreCreated() throws Exception {
+        this.mockMvc.perform(post("/customers"))
+            .andExpect(status().isOk());
+    }    
+   
+}
