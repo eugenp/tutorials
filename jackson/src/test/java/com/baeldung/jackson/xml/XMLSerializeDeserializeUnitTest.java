fe289003faf09e7468882a5a11aed33d694c5cac
Urvy Agrawal <urvyagrawal@gmail.com>
2019-02-18 23:00:58 +0530

    Added files for BAEL-2494 (#6370)

--- jackson/src/test/java/com/baeldung/jackson/xml/XMLSerializeDeserializeUnitTest.java
+++ jackson/src/test/java/com/baeldung/jackson/xml/XMLSerializeDeserializeUnitTest.java
@@ -9,12 +9,16 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.baeldung.jackson.dtos.Address;
+import com.baeldung.jackson.dtos.Person;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class XMLSerializeDeserializeUnitTest {
 
@@ -49,7 +53,7 @@ public class XMLSerializeDeserializeUnitTest {
         assertTrue(value.getX() == 1 && value.getY() == 2);
     }
 
-    @Test 
+    @Test
     public void whenJavaGotFromXmlStrWithCapitalElem_thenCorrect() throws IOException {
         XmlMapper xmlMapper = new XmlMapper();
         SimpleBeanForCapitalizedFields value = xmlMapper.
@@ -65,6 +69,54 @@ public class XMLSerializeDeserializeUnitTest {
             new SimpleBeanForCapitalizedFields());
         File file = new File("target/simple_bean_capitalized.xml");
         assertNotNull(file);
+    }
+
+    @Test
+    public void whenJavaDeserializedFromXmlFile_thenCorrect() throws IOException {
+        XmlMapper xmlMapper = new XmlMapper();
+        Person value = xmlMapper.readValue(new File("src/test/resources/person.xml"), Person.class);
+
+        assertTrue(value.getAddress()
+            .get(0)
+            .getCity()
+            .equalsIgnoreCase("city1")
+            && value.getAddress()
+                .get(1)
+                .getCity()
+                .equalsIgnoreCase("city2"));
+    }
+
+    @Test
+    public void whenJavaSerializedToXmlFile_thenSuccess() throws IOException {
+        XmlMapper xmlMapper = new XmlMapper();
+
+        Person person = new Person();
+
+        person.setFirstName("Rohan");
+        person.setLastName("Daye");
+
+        List<String> ph = new ArrayList<>();
+        ph.add("9911778981");
+        ph.add("9991111111");
+        person.setPhoneNumbers(ph);
+
+        List<Address> addresses = new ArrayList<>();
+        
+        Address address1 = new Address();
+        address1.setStreetNumber("1");
+        address1.setStreetName("streetname1");
+        address1.setCity("city1");
+        
+        Address address2 = new Address();
+        address2.setStreetNumber("2");
+        address2.setStreetName("streetname2");
+        address2.setCity("city2");
+        
+        addresses.add(address1);
+        addresses.add(address2);
+        person.setAddress(addresses);
+
+        xmlMapper.writeValue(new File("src/test/resources/PersonGenerated.xml"), person);
     }
 
     private static String inputStreamToString(InputStream is) throws IOException {
@@ -103,10 +155,10 @@ class SimpleBean {
 
 }
 
-class SimpleBeanForCapitalizedFields { 
-    @JsonProperty("X") 
-    private int x = 1; 
-    private int y = 2; 
+class SimpleBeanForCapitalizedFields {
+    @JsonProperty("X")
+    private int x = 1;
+    private int y = 2;
 
     public int getX() {
         return x;
