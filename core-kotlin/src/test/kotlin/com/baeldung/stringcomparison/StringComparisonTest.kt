19b0b021024d0add9dda880d5be59ad72bc36ba6
Sushant <sushant57@gmail.com>
2019-02-18 19:39:36 +0100

    Change variable name

--- core-kotlin/src/test/kotlin/com/baeldung/stringcomparison/StringComparisonTest.kt
+++ core-kotlin/src/test/kotlin/com/baeldung/stringcomparison/StringComparisonTest.kt
@@ -6,42 +6,42 @@ import kotlin.test.assertTrue
 
 class StringComparisonUnitTest {
 
-	@Test
-	fun `compare using equals operator`() {
-		val first = "kotlin"
-		val second = "kotlin"
-		val firstCapitalized = "KOTLIN"
-		assertTrue { first == second }
-		assertFalse { first == firstCapitalized }
-	}
+    @Test
+    fun `compare using equals operator`() {
+        val first = "kotlin"
+        val second = "kotlin"
+        val firstCapitalized = "KOTLIN"
+        assertTrue { first == second }
+        assertFalse { first == firstCapitalized }
+    }
 
-	@Test
-	fun `compare using referential equals operator`() {
-		val first = "kotlin"
-		val second = "kotlin"
-		val firstBuildAgain = buildString { "kotlin" }
-		assertTrue { first === second }
-		assertFalse { first === firstBuildAgain }
-	}
+    @Test
+    fun `compare using referential equals operator`() {
+        val first = "kotlin"
+        val second = "kotlin"
+        val copyOfFirst = buildString { "kotlin" }
+        assertTrue { first === second }
+        assertFalse { first === copyOfFirst }
+    }
 
-	@Test
-	fun `compare using equals method`() {
-		val first = "kotlin"
-		val second = "kotlin"
-		val firstCapitalized = "KOTLIN"
-		assertTrue { first.equals(second) }
-		assertFalse { first.equals(firstCapitalized) }
-		assertTrue { first.equals(firstCapitalized, true) }
-	}
+    @Test
+    fun `compare using equals method`() {
+        val first = "kotlin"
+        val second = "kotlin"
+        val firstCapitalized = "KOTLIN"
+        assertTrue { first.equals(second) }
+        assertFalse { first.equals(firstCapitalized) }
+        assertTrue { first.equals(firstCapitalized, true) }
+    }
 
-	@Test
-	fun `compare using compare method`() {
-		val first = "kotlin"
-		val second = "kotlin"
-		val firstCapitalized = "KOTLIN"
-		assertTrue { first.compareTo(second) == 0 }
-		assertTrue { first.compareTo(firstCapitalized) == 32 }
-		assertTrue { firstCapitalized.compareTo(first) == -32 }
-		assertTrue { first.compareTo(firstCapitalized, true) == 0 }
-	}
+    @Test
+    fun `compare using compare method`() {
+        val first = "kotlin"
+        val second = "kotlin"
+        val firstCapitalized = "KOTLIN"
+        assertTrue { first.compareTo(second) == 0 }
+        assertTrue { first.compareTo(firstCapitalized) == 32 }
+        assertTrue { firstCapitalized.compareTo(first) == -32 }
+        assertTrue { first.compareTo(firstCapitalized, true) == 0 }
+    }
 }
