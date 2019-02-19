062b959d5a4d127a168d56045d589f0e45d8d5da
vsurapaneni <vsurapaneni@imedx.com>
2019-02-19 15:33:40 -0500

    BAEL-2575

---
+++ spring-boot-datasource-issue/spring-boot-datasource-exclude-yml/src/main/java/com/baeldung/datasourcedemo/DataSourceIssueSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.datasourcedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSourceIssueSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSourceIssueSolutionApplication.class, args);
+	}
+
+}
