0e1603839bcfd7cdc20dc66c7da3640db4a3173d
sumit.sg34 <sgajbhar@qualys.com>
2019-02-25 22:32:23 +0530

    BAEL-2520 updated code to use item entity

--- persistence-modules/spring-data-jpa/src/main/java/com/baeldung/dao/repositories/impl/CustomItemRepositoryImpl.java
+++ persistence-modules/spring-data-jpa/src/main/java/com/baeldung/dao/repositories/impl/CustomItemRepositoryImpl.java
@@ -1,6 +1,12 @@
 package com.baeldung.dao.repositories.impl;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -11,22 +17,70 @@ import com.baeldung.dao.repositories.CustomItemRepository;
 @Repository
 public class CustomItemRepositoryImpl implements CustomItemRepository {
 
-    @Autowired
-    private EntityManager entityManager;
+	@Autowired
+	private EntityManager entityManager;
 
-    @Override
-    public void deleteCustom(Item item) {
-        entityManager.remove(item);
-    }
+	@Override
+	public void deleteCustom(Item item) {
+		entityManager.remove(item);
+	}
 
-    @Override
-    public Item findItemById(Long id) {
-        return entityManager.find(Item.class, id);
-    }
+	@Override
+	public Item findItemById(Long id) {
+		return entityManager.find(Item.class, id);
+	}
 
-    @Override
-    public void findThenDelete(Long id) {
-        final Item item = entityManager.find(Item.class, id);
-        entityManager.remove(item);
-    }
+	@Override
+	public void findThenDelete(Long id) {
+		final Item item = entityManager.find(Item.class, id);
+		entityManager.remove(item);
+	}
+
+	@Override
+	public List<Item> findItemsByColorAndGrade() {
+
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+		Root<Item> itemRoot = criteriaQuery.from(Item.class);
+
+		Predicate predicateForBlueColor = criteriaBuilder.equal(itemRoot.get("color"), "blue");
+		Predicate predicateForRedColor = criteriaBuilder.equal(itemRoot.get("color"), "red");
+		Predicate predicateForColor = criteriaBuilder.or(predicateForBlueColor, predicateForRedColor);
+
+		Predicate predicateForGradeA = criteriaBuilder.equal(itemRoot.get("grade"), "A");
+		Predicate predicateForGradeB = criteriaBuilder.equal(itemRoot.get("grade"), "B");
+		Predicate predicateForGrade = criteriaBuilder.or(predicateForGradeA, predicateForGradeB);
+
+		// final search filter
+		Predicate finalPredicate = criteriaBuilder.and(predicateForColor, predicateForGrade);
+
+		criteriaQuery.where(finalPredicate);
+
+		List<Item> phones = entityManager.createQuery(criteriaQuery).getResultList();
+		return phones;
+	}
+
+	@Override
+	public List<Item> findItemByColorOrGrade() {
+		
+		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+		Root<Item> itemRoot = criteriaQuery.from(Item.class);
+
+		Predicate predicateForBlueColor = criteriaBuilder.equal(itemRoot.get("color"), "red");
+		Predicate predicateForGradeA = criteriaBuilder.equal(itemRoot.get("grade"), "D");
+		Predicate predicateForColor = criteriaBuilder.and(predicateForBlueColor, predicateForGradeA);
+
+		Predicate predicateForRedColor = criteriaBuilder.equal(itemRoot.get("color"), "blue");
+		Predicate predicateForGradeB = criteriaBuilder.equal(itemRoot.get("grade"), "B");
+		Predicate predicateForGrade = criteriaBuilder.and(predicateForRedColor, predicateForGradeB);
+
+		// final search filter
+		Predicate finalPredicate = criteriaBuilder.or(predicateForColor, predicateForGrade);
+
+		criteriaQuery.where(finalPredicate);
+
+		List<Item> phones = entityManager.createQuery(criteriaQuery).getResultList();
+		return phones;
+	}
 }
