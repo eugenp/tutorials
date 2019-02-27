19af38796110324c3da6174b6e148076ce3b1b6f
sumit.sg34 <sgajbhar@qualys.com>
2019-02-27 22:47:12 +0530

    BAEL-2520 code formatting corrected

--- persistence-modules/spring-data-jpa/src/main/java/com/baeldung/dao/repositories/impl/CustomItemRepositoryImpl.java
+++ persistence-modules/spring-data-jpa/src/main/java/com/baeldung/dao/repositories/impl/CustomItemRepositoryImpl.java
@@ -17,70 +17,72 @@ import com.baeldung.dao.repositories.CustomItemRepository;
 @Repository
 public class CustomItemRepositoryImpl implements CustomItemRepository {
 
-	@Autowired
-	private EntityManager entityManager;
+    @Autowired
+    private EntityManager entityManager;
 
-	@Override
-	public void deleteCustom(Item item) {
-		entityManager.remove(item);
-	}
+    @Override
+    public void deleteCustom(Item item) {
+        entityManager.remove(item);
+    }
 
-	@Override
-	public Item findItemById(Long id) {
-		return entityManager.find(Item.class, id);
-	}
+    @Override
+    public Item findItemById(Long id) {
+        return entityManager.find(Item.class, id);
+    }
 
-	@Override
-	public void findThenDelete(Long id) {
-		final Item item = entityManager.find(Item.class, id);
-		entityManager.remove(item);
-	}
+    @Override
+    public void findThenDelete(Long id) {
+        final Item item = entityManager.find(Item.class, id);
+        entityManager.remove(item);
+    }
 
-	@Override
-	public List<Item> findItemsByColorAndGrade() {
+    @Override
+    public List<Item> findItemsByColorAndGrade() {
 
-		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
-		Root<Item> itemRoot = criteriaQuery.from(Item.class);
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+        Root<Item> itemRoot = criteriaQuery.from(Item.class);
 
-		Predicate predicateForBlueColor = criteriaBuilder.equal(itemRoot.get("color"), "blue");
-		Predicate predicateForRedColor = criteriaBuilder.equal(itemRoot.get("color"), "red");
-		Predicate predicateForColor = criteriaBuilder.or(predicateForBlueColor, predicateForRedColor);
+        Predicate predicateForBlueColor = criteriaBuilder.equal(itemRoot.get("color"), "blue");
+        Predicate predicateForRedColor = criteriaBuilder.equal(itemRoot.get("color"), "red");
+        Predicate predicateForColor = criteriaBuilder.or(predicateForBlueColor, predicateForRedColor);
 
-		Predicate predicateForGradeA = criteriaBuilder.equal(itemRoot.get("grade"), "A");
-		Predicate predicateForGradeB = criteriaBuilder.equal(itemRoot.get("grade"), "B");
-		Predicate predicateForGrade = criteriaBuilder.or(predicateForGradeA, predicateForGradeB);
+        Predicate predicateForGradeA = criteriaBuilder.equal(itemRoot.get("grade"), "A");
+        Predicate predicateForGradeB = criteriaBuilder.equal(itemRoot.get("grade"), "B");
+        Predicate predicateForGrade = criteriaBuilder.or(predicateForGradeA, predicateForGradeB);
 
-		// final search filter
-		Predicate finalPredicate = criteriaBuilder.and(predicateForColor, predicateForGrade);
+        // final search filter
+        Predicate finalPredicate = criteriaBuilder.and(predicateForColor, predicateForGrade);
 
-		criteriaQuery.where(finalPredicate);
+        criteriaQuery.where(finalPredicate);
 
-		List<Item> items = entityManager.createQuery(criteriaQuery).getResultList();
-		return items;
-	}
+        List<Item> items = entityManager.createQuery(criteriaQuery)
+            .getResultList();
+        return items;
+    }
 
-	@Override
-	public List<Item> findItemByColorOrGrade() {
+    @Override
+    public List<Item> findItemByColorOrGrade() {
 
-		CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
-		Root<Item> itemRoot = criteriaQuery.from(Item.class);
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Item> criteriaQuery = criteriaBuilder.createQuery(Item.class);
+        Root<Item> itemRoot = criteriaQuery.from(Item.class);
 
-		Predicate predicateForBlueColor = criteriaBuilder.equal(itemRoot.get("color"), "red");
-		Predicate predicateForGradeA = criteriaBuilder.equal(itemRoot.get("grade"), "D");
-		Predicate predicateForColor = criteriaBuilder.and(predicateForBlueColor, predicateForGradeA);
+        Predicate predicateForBlueColor = criteriaBuilder.equal(itemRoot.get("color"), "red");
+        Predicate predicateForGradeA = criteriaBuilder.equal(itemRoot.get("grade"), "D");
+        Predicate predicateForBlueColorAndGradeA = criteriaBuilder.and(predicateForBlueColor, predicateForGradeA);
 
-		Predicate predicateForRedColor = criteriaBuilder.equal(itemRoot.get("color"), "blue");
-		Predicate predicateForGradeB = criteriaBuilder.equal(itemRoot.get("grade"), "B");
-		Predicate predicateForGrade = criteriaBuilder.and(predicateForRedColor, predicateForGradeB);
+        Predicate predicateForRedColor = criteriaBuilder.equal(itemRoot.get("color"), "blue");
+        Predicate predicateForGradeB = criteriaBuilder.equal(itemRoot.get("grade"), "B");
+        Predicate predicateForRedColorAndGradeB = criteriaBuilder.and(predicateForRedColor, predicateForGradeB);
 
-		// final search filter
-		Predicate finalPredicate = criteriaBuilder.or(predicateForColor, predicateForGrade);
+        // final search filter
+        Predicate finalPredicate = criteriaBuilder.or(predicateForBlueColorAndGradeA, predicateForRedColorAndGradeB);
 
-		criteriaQuery.where(finalPredicate);
+        criteriaQuery.where(finalPredicate);
 
-		List<Item> items = entityManager.createQuery(criteriaQuery).getResultList();
-		return items;
-	}
+        List<Item> items = entityManager.createQuery(criteriaQuery)
+            .getResultList();
+        return items;
+    }
 }
--- persistence-modules/spring-data-jpa/src/test/java/com/baeldung/dao/repositories/impl/CustomItemRepositoryIntegrationTest.java
+++ persistence-modules/spring-data-jpa/src/test/java/com/baeldung/dao/repositories/impl/CustomItemRepositoryIntegrationTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.dao.repositories.impl;
 
-
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -22,73 +21,73 @@ import com.baeldung.domain.Item;
 import org.springframework.util.CollectionUtils;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest(excludeAutoConfiguration = {PersistenceConfiguration.class, PersistenceUserConfiguration.class, PersistenceProductConfiguration.class})
+@DataJpaTest(excludeAutoConfiguration = { PersistenceConfiguration.class, PersistenceUserConfiguration.class, PersistenceProductConfiguration.class })
 public class CustomItemRepositoryIntegrationTest {
 
-	@Autowired
-	CustomItemRepository customItemRepositoryImpl;
+    @Autowired
+    CustomItemRepository customItemRepositoryImpl;
 
-	@Autowired
-	EntityManager entityManager;
+    @Autowired
+    EntityManager entityManager;
 
-	@Before
-	public void setUpForAndPredicate(){
+    @Before
+    public void setUpForAndPredicate() {
 
-		Item firstItem = new Item();
-		firstItem.setColor("blue");
-		firstItem.setGrade("C");
-		firstItem.setId(10l);
+        Item firstItem = new Item();
+        firstItem.setColor("blue");
+        firstItem.setGrade("C");
+        firstItem.setId(10l);
 
-		entityManager.persist(firstItem);
+        entityManager.persist(firstItem);
 
-		Item secondItem = new Item();
-		secondItem.setColor("red");
-		secondItem.setGrade("C");
-		secondItem.setId(11l);
+        Item secondItem = new Item();
+        secondItem.setColor("red");
+        secondItem.setGrade("C");
+        secondItem.setId(11l);
 
-		entityManager.persist(secondItem);
+        entityManager.persist(secondItem);
 
-		Item thirdItem = new Item();
-		thirdItem.setColor("blue");
-		thirdItem.setGrade("A");
-		thirdItem.setId(12l);
+        Item thirdItem = new Item();
+        thirdItem.setColor("blue");
+        thirdItem.setGrade("A");
+        thirdItem.setId(12l);
 
-		entityManager.persist(thirdItem);
+        entityManager.persist(thirdItem);
 
-		Item fourthItem = new Item();
-		fourthItem.setColor("red");
-		fourthItem.setGrade("D");
-		fourthItem.setId(13l);
+        Item fourthItem = new Item();
+        fourthItem.setColor("red");
+        fourthItem.setGrade("D");
+        fourthItem.setId(13l);
 
-		entityManager.persist(fourthItem);
-	}
+        entityManager.persist(fourthItem);
+    }
 
-	@Test
-	public void testFindItemsByColorAndGrade() {
+    @Test
+    public void testFindItemsByColorAndGrade() {
 
-		List<Item> items = customItemRepositoryImpl.findItemsByColorAndGrade();
+        List<Item> items = customItemRepositoryImpl.findItemsByColorAndGrade();
 
-		assertFalse("No items found", CollectionUtils.isEmpty(items));
-		assertEquals("There should be only one item", 1, items.size());
+        assertFalse("No items found", CollectionUtils.isEmpty(items));
+        assertEquals("There should be only one item", 1, items.size());
 
-		Item item = items.get(0);
+        Item item = items.get(0);
 
-		assertEquals("this item do not have blue color", "blue",item.getColor());
-		assertEquals("this item does not belong to A grade", "A",item.getGrade());
-	}
+        assertEquals("this item do not have blue color", "blue", item.getColor());
+        assertEquals("this item does not belong to A grade", "A", item.getGrade());
+    }
 
-	@Test
-	public void testFindItemsByColorOrGrade() {
+    @Test
+    public void testFindItemsByColorOrGrade() {
 
-		List<Item> items = customItemRepositoryImpl.findItemByColorOrGrade();
+        List<Item> items = customItemRepositoryImpl.findItemByColorOrGrade();
 
-		assertFalse("No items found", CollectionUtils.isEmpty(items));
-		assertEquals("There should be only one item", 1, items.size());
+        assertFalse("No items found", CollectionUtils.isEmpty(items));
+        assertEquals("There should be only one item", 1, items.size());
 
-		Item item = items.get(0);
+        Item item = items.get(0);
 
-		assertEquals("this item do not have red color", "red",item.getColor());
-		assertEquals("this item does not belong to D grade", "D",item.getGrade());
-	}
+        assertEquals("this item do not have red color", "red", item.getColor());
+        assertEquals("this item does not belong to D grade", "D", item.getGrade());
+    }
 
 }
