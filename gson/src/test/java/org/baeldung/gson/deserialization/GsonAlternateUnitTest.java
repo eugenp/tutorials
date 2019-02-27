46c012d43f97a23fa2e0fd4475936698c184f890
pcoates <pcoates33@gmail.com>
2019-02-27 23:14:08 +0000

    BAEL-2721 Update class and method names for JsonAlias and GsonAlternate

--- gson/src/test/java/org/baeldung/gson/deserialization/GsonAlternateUnitTest.java
+++ gson/src/test/java/org/baeldung/gson/deserialization/GsonAlternateUnitTest.java
@@ -8,20 +8,28 @@ import org.junit.Test;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class GsonAlternateTest {
+public class GsonAlternateUnitTest {
 
     @Test
-    public void testJsonAlias() throws Exception {
+    public void givenTwoJsonFormats_whenDeserialized_thenWeatherObjectsCreated() throws Exception {
 
         Gson gson = new GsonBuilder().create();
 
-        Weather weather = gson.fromJson("{\n" + "  \"location\": \"London\",\n" + "  \"temp\": 15,\n" + "  \"weather\": \"Cloudy\"\n" + "}", Weather.class);
+        Weather weather = gson.fromJson("{" + 
+            "\"location\": \"London\"," + 
+            "\"temp\": 15," + 
+            "\"weather\": \"Cloudy\"" + 
+            "}", Weather.class);
 
         assertEquals("London", weather.getLocation());
         assertEquals("Cloudy", weather.getOutlook());
         assertEquals(15, weather.getTemp());
 
-        weather = gson.fromJson("{\n" + "  \"place\": \"Lisbon\",\n" + "  \"temperature\": 35,\n" + "  \"outlook\": \"Sunny\"\n" + "}", Weather.class);
+        weather = gson.fromJson("{" + 
+            "\"place\": \"Lisbon\"," + 
+            "\"temperature\": 35," + 
+            "\"outlook\": \"Sunny\"" + 
+            "}", Weather.class);
 
         assertEquals("Lisbon", weather.getLocation());
         assertEquals("Sunny", weather.getOutlook());
--- jackson/src/test/java/com/baeldung/jackson/deserialization/jsonalias/JsonAliasUnitTest.java
+++ jackson/src/test/java/com/baeldung/jackson/deserialization/jsonalias/JsonAliasUnitTest.java
@@ -7,20 +7,28 @@ import org.junit.Test;
 import com.baeldung.jackson.entities.Weather;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class JsonAliasTest {
+public class JsonAliasUnitTest {
 
     @Test
-    public void testJsonAlias() throws Exception {
+    public void givenTwoJsonFormats_whenDeserialized_thenWeatherObjectsCreated() throws Exception {
 
         ObjectMapper mapper = new ObjectMapper();
 
-        Weather weather = mapper.readValue("{\n" + "  \"location\": \"London\",\n" + "  \"temp\": 15,\n" + "  \"weather\": \"Cloudy\"\n" + "}", Weather.class);
+        Weather weather = mapper.readValue("{" + 
+            "\"location\": \"London\"," + 
+            "\"temp\": 15," + 
+            "\"weather\": \"Cloudy\"" + 
+            "}", Weather.class);
 
         assertEquals("London", weather.getLocation());
         assertEquals("Cloudy", weather.getOutlook());
         assertEquals(15, weather.getTemp());
 
-        weather = mapper.readValue("{\n" + "  \"place\": \"Lisbon\",\n" + "  \"temperature\": 35,\n" + "  \"outlook\": \"Sunny\"\n" + "}", Weather.class);
+        weather = mapper.readValue("{" + 
+            "\"place\": \"Lisbon\"," + 
+            "\"temperature\": 35," + 
+            "\"outlook\": \"Sunny\"" + 
+            "}", Weather.class);
 
         assertEquals("Lisbon", weather.getLocation());
         assertEquals("Sunny", weather.getOutlook());
