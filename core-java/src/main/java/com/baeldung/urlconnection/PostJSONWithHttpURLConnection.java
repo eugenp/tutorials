4e4d0a55915a0acdf9b8a5ab9b8476db99f579aa
Upendra Chintala <upendra.chintala@gmail.com>
2019-03-01 00:02:12 +0530

    Made few changes for better readability

--- core-java/src/main/java/com/baeldung/urlconnection/PostJSONWithHttpURLConnection.java
+++ core-java/src/main/java/com/baeldung/urlconnection/PostJSONWithHttpURLConnection.java
@@ -9,44 +9,37 @@ import java.net.URL;
 
 public class PostJSONWithHttpURLConnection {
 	
-	public static void main (String []args) {
-		HttpURLConnection con = null;
-		try {
-			//Change the URL with any other publicly accessible POST resource, which accepts JSON request body
-			URL url = new URL ("https://reqres.in/api/users");
-			
-			con = (HttpURLConnection)url.openConnection();
-			con.setRequestMethod("POST");
-			
-			con.setRequestProperty("Content-Type", "application/json; utf-8");
-			con.setRequestProperty("Accept", "application/json");
-			
-			con.setDoOutput(true);
-			
-			//JSON String need to be constructed for the specific resource. 
-			//We may construct complex JSON using any third-party JSON libraries such as jackson or org.json
-			String jsonInputString = "{\"name\": \"Upendra\", \"job\": \"Programmer\"}";
-			
-			try(OutputStream os = con.getOutputStream()){
-				byte[] input = jsonInputString.getBytes("utf-8");
-				os.write(input, 0, input.length);			
-			}
+	public static void main (String []args) throws IOException{
+		//Change the URL with any other publicly accessible POST resource, which accepts JSON request body
+		URL url = new URL ("https://reqres.in/api/users");
+		
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		con.setRequestMethod("POST");
+		
+		con.setRequestProperty("Content-Type", "application/json; utf-8");
+		con.setRequestProperty("Accept", "application/json");
+		
+		con.setDoOutput(true);
+		
+		//JSON String need to be constructed for the specific resource. 
+		//We may construct complex JSON using any third-party JSON libraries such as jackson or org.json
+		String jsonInputString = "{\"name\": \"Upendra\", \"job\": \"Programmer\"}";
+		
+		try(OutputStream os = con.getOutputStream()){
+			byte[] input = jsonInputString.getBytes("utf-8");
+			os.write(input, 0, input.length);			
+		}
 
-			int code = con.getResponseCode();
-			System.out.println(code);
-			
-			try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))){
-				StringBuilder response = new StringBuilder();
-				String responseLine = null;
-				while ((responseLine = br.readLine()) != null) {
-					response.append(responseLine.trim());
-				}
-				System.out.println(response.toString());
+		int code = con.getResponseCode();
+		System.out.println(code);
+		
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"))){
+			StringBuilder response = new StringBuilder();
+			String responseLine = null;
+			while ((responseLine = br.readLine()) != null) {
+				response.append(responseLine.trim());
 			}
-		}catch (IOException ex) {
-			ex.printStackTrace();
-		}finally {
-			con = null;
+			System.out.println(response.toString());
 		}
 	}
 
