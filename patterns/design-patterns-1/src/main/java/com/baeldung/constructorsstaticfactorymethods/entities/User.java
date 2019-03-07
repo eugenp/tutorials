fea0f86f99e8150e8ae6c42665e149fc2b5f7d60
Krzysiek <krzysztofwoyke@gmail.com>
2019-03-07 20:23:10 +0100

    Splits design-patterns project

---
+++ patterns/design-patterns-1/src/main/java/com/baeldung/constructorsstaticfactorymethods/entities/User.java
@@ -0,0 +1,63 @@
+package com.baeldung.constructorsstaticfactorymethods.entities;
+
+import java.time.LocalTime;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class User {
+
+    private static volatile User instance = null;
+    private static final Logger LOGGER = Logger.getLogger(User.class.getName());
+    private final String name;
+    private final String email;
+    private final String country;
+    
+    public static User createWithDefaultCountry(String name, String email) {
+        return new User(name, email, "Argentina");
+    }
+
+    public static User createWithLoggedInstantiationTime(String name, String email, String country) {
+        setLoggerProperties();
+        LOGGER.log(Level.INFO, "Creating User instance at : {0}", LocalTime.now());
+        return new User(name, email, country);
+    }
+
+    public static User getSingletonInstance(String name, String email, String country) {
+        if (instance == null) {
+            synchronized (User.class) {
+                if (instance == null) {
+                    instance = new User(name, email, country);
+                }
+            }
+        }
+        return instance;
+
+    }
+
+    private User(String name, String email, String country) {
+        this.name = name;
+        this.email = email;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+    
+    private static void setLoggerProperties() {
+        ConsoleHandler handler = new ConsoleHandler();
+        handler.setLevel(Level.INFO);
+        handler.setFormatter(new SimpleFormatter());
+        LOGGER.addHandler(handler);
+    }
+}
---
+++ patterns/design-patterns-2/README.md
@@ -0,0 +1,2 @@
+### Relevant Articles:
+- [Introduction to the Null Object Pattern](https://www.baeldung.com/java-null-object-pattern)
