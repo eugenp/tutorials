da782c4b5cf095c455b50ab3120decedb30f0d96
Urvy Agrawal <urvagraw@publicisgroupe.net>
2019-03-07 12:43:34 +0530

    Adding files for BAEL-2196

---
+++ maven/compiler-plugin-java-9/src/main/java/com/baeldung/java9/mavendemo/MavenCompilerPlugin.java
@@ -0,0 +1,9 @@
+package com.baeldung.java9.mavendemo;
+
+import static javax.xml.XMLConstants.XML_NS_PREFIX;
+
+public class MavenCompilerPlugin {
+    public static void main(String[] args) {
+        System.out.println("The XML namespace prefix is: " + XML_NS_PREFIX);
+    }
+}
---
+++ maven/compiler-plugin-java-9/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.baeldung.java9.mavendemo {
+
+  requires java.xml;
+}
