9fae5f5a50f3cca4738b8ef0fd1346c8dc2be781
Antonio Moreno <anmoreno@theworkshop.com>
2019-03-10 17:40:04 +0000

    BAEL-2522 - Comments addressed.

--- java-dates/src/test/java/com/baeldung/xmlgregoriancalendar/XmlGregorianCalendarConverterTest.java
+++ java-dates/src/test/java/com/baeldung/xmlgregoriancalendar/XmlGregorianCalendarConverterTest.java
@@ -16,7 +16,7 @@ public class XmlGregorianCalendarConverterTest {
     @Test
     public void fromLocalDateToXMLGregorianCalendar() throws DatatypeConfigurationException {
         LocalDate localDate = LocalDate.of(2017, 4, 25);
-        XMLGregorianCalendar xmlGregorianCalendar = fromLocalDate(localDate);
+        XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar(localDate.toString());
 
         assertThat(xmlGregorianCalendar.getYear()).isEqualTo(localDate.getYear());
         assertThat(xmlGregorianCalendar.getMonth()).isEqualTo(localDate.getMonthValue());
@@ -26,19 +26,11 @@ public class XmlGregorianCalendarConverterTest {
     @Test
     public void fromXMLGregorianCalendarToLocalDate() throws DatatypeConfigurationException {
         XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar("2017-04-25");
-        LocalDate localDate = fromXMLGregorianCalendar(xmlGregorianCalendar);
+        LocalDate localDate = LocalDate.of(xmlGregorianCalendar.getYear(), xmlGregorianCalendar.getMonth(), xmlGregorianCalendar.getDay());
 
         assertThat(localDate.getYear()).isEqualTo(xmlGregorianCalendar.getYear());
         assertThat(localDate.getMonthValue()).isEqualTo(xmlGregorianCalendar.getMonth());
         assertThat(localDate.getDayOfMonth()).isEqualTo(xmlGregorianCalendar.getDay());
     }
-
-    @Test
-    public void compositionOfFunctionsIsIdentity() throws DatatypeConfigurationException { // Only if we don't consider time
-        LocalDate localDate = LocalDate.of(2017, 4, 25);
-        XMLGregorianCalendar xmlGregorianCalendar = fromLocalDate(localDate);
-        LocalDate resultDate = fromXMLGregorianCalendar(xmlGregorianCalendar);
-
-        assertThat(resultDate).isEqualTo(localDate);
-    }
+    
 }
