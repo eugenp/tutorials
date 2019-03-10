1b48e7943c2942feb1d49b8e4a8a352315a62fa4
PranayJain <pranay.jain@acellere.com>
2019-03-10 17:30:20 +0530

    BAEL-2719: Fixed the changes to load external file

--- spring-boot-ops/src/main/java/com/baeldung/properties/ExternalPropertyFileLoader.java
+++ spring-boot-ops/src/main/java/com/baeldung/properties/ExternalPropertyFileLoader.java
@@ -1,17 +1,18 @@
 package com.baeldung.properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ExternalPropertyFileLoader {
+
+    @Autowired
+    ConfProperties prop;
+
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(ExternalPropertyFileLoader.class).properties("spring.config.name:conf", "spring.config.location:file:src/main/resources/external/")
-            .build()
-            .run(args);
-        ConfigurableEnvironment environment = applicationContext.getEnvironment();
-        environment.getProperty("username");
+        new SpringApplicationBuilder(ExternalPropertyFileLoader.class).build()
+        .run(args);
     }
+
 }
