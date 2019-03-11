54ba95a48875ca3ed6daf0053afd8686f7ecac18
Rahul Srivastava <rahusr@gmail.com>
2019-03-12 03:03:13 +0530

    Adding test case

--- core-java-networking/src/test/java/com/baeldung/socket/read/SocketReadAllDataTest.java
+++ core-java-networking/src/test/java/com/baeldung/socket/read/SocketReadAllDataTest.java
@@ -1,38 +1,37 @@
 package com.baeldung.socket.read;
 
 import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.Test;
 
-public class TestSocketRead {
-
-    public static void main(String[] args) {
-        TestSocketRead t = new TestSocketRead();
+public class SocketReadAllDataTest {
+    
+    @Test
+    public void givenServerAndClient_whenClientSendsAndServerReceivesData_thenCorrect() {
         //Run server in new thread
-        Runnable runnable1 = () -> { t.runServer(); };
+        Runnable runnable1 = () -> { runServer(); };
         Thread thread1 = new Thread(runnable1);
         thread1.start();
         //Wait for 10 seconds 
         try {
             TimeUnit.SECONDS.sleep(10);
         } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         //Run client in a new thread
-        Runnable runnable2 = () -> { t.runClient(); };
+        Runnable runnable2 = () -> { runClient(); };
         Thread thread2 = new Thread(runnable2);
         thread2.start();
     }
     
-    void runServer() {
+    public static void runServer() {
         //Run Server
         Server server = new Server();
         server.runServer(5555);
     }
     
-    void runClient() {
+    public static void runClient() {
         //Run Client
         Client client = new Client();
         client.runClient("127.0.0.1", 5555);
     }
-
 }
