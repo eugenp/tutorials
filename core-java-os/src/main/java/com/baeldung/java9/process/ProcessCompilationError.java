63b652270afcd5a09a0d21ac4eb175df1424e5a8
Loredana <lore.crusoveanu@gmail.com>
2019-03-11 23:02:10 +0200

    add new core-java-os module

--- core-java-os/src/main/java/com/baeldung/java9/process/ProcessCompilationError.java
+++ core-java-os/src/main/java/com/baeldung/java9/process/ProcessCompilationError.java
@@ -3,5 +3,5 @@ package com.baeldung.java9.process;
 public class ProcessCompilationError {
     //This method has been written to generate error to display 
     //how process errorStream() can consume error
-    public static void();
+    //public static void();
 }
