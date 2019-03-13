ce8c28af9e506585eb470d69c0916badca1a97ed
Djordje Cvijetic <djordje.cvijetic@gmail.com>
2019-03-13 15:32:50 +0100

    Test added

--- core-java/src/main/java/com/baeldung/inlining/ConsecutiveNumbersSum.java
+++ core-java/src/main/java/com/baeldung/inlining/ConsecutiveNumbersSum.java
@@ -11,7 +11,7 @@ public class ConsecutiveNumbersSum {
 
     public long getTotalSum() {
         totalSum = 0;
-        for (int i = 0; i < totalNumbers; i++) {
+        for (int i = 1; i <= totalNumbers; i++) {
             totalSum += i;
         }
         return totalSum;
