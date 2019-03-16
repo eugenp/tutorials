a2f894bd76eefba6fd35a68cc8a2348cc8ec66c0
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-03-16 21:17:54 +0100

    workarounds when using non-final local variables in lambdas

--- core-java-8/src/main/java/com/baeldung/lambdas/LambdaVariables.java
+++ core-java-8/src/main/java/com/baeldung/lambdas/LambdaVariables.java
@@ -1,11 +1,14 @@
 package com.baeldung.lambdas;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class LambdaVariables {
 
     private String greeting = "Hello";
 
     public static void main(String[] args) {
-        new LambdaVariables().instanceVariableInLambda();
+        new LambdaVariables().workaroundMultithreading();
     }
 
     public void localVariableInLambda() {
@@ -26,6 +29,38 @@ public class LambdaVariables {
 
     public void parameterLambda(int counter) {
         Runnable runnable = () -> System.out.println(counter);
+    }
+
+    public void workaroundSingleThread() {
+        int[] holder = new int[] { 2 };
+        IntStream sums = IntStream
+          .of(1, 2, 3)
+          .map(val -> val + holder[0]);
+
+        holder[0] = 0;
+
+        System.out.println(sums.sum());
+    }
+
+    public void workaroundMultithreading() {
+        int[] holder = new int[] { 2 };
+        Runnable runnable = () -> {
+            System.out.println(IntStream
+              .of(1, 2, 3)
+              .map(val -> val + holder[0])
+              .sum());
+        };
+
+        new Thread(runnable).start();
+
+        // simulating some processing
+        try {
+            Thread.sleep(new Random().nextInt(3) * 1000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        holder[0] = 0;
     }
 
 }
