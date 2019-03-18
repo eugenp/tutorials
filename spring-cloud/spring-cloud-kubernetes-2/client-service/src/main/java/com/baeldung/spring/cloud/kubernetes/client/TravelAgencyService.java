f914b19bdbcfa5e4f2e3ddbd4e3bdefab7d94ec7
caroline <caroline@yobetit.com>
2019-03-18 14:57:31 +0100

    Formatting changes

--- spring-cloud/spring-cloud-kubernetes-2/client-service/src/main/java/com/baeldung/spring/cloud/kubernetes/client/TravelAgencyService.java
+++ spring-cloud/spring-cloud-kubernetes-2/client-service/src/main/java/com/baeldung/spring/cloud/kubernetes/client/TravelAgencyService.java
@@ -14,8 +14,8 @@ public class TravelAgencyService {
         this.restTemplate = restTemplate;
     }
 
-    @HystrixCommand(fallbackMethod = "getFallbackName", commandProperties =
-            { @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000") })
+    @HystrixCommand(fallbackMethod = "getFallbackName", commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000") })
     public String getDeals() {
         return this.restTemplate.getForObject("http://travel-agency-service:8080/deals", String.class);
     }
