2876df97d7f0b0376bbbc3bb03e27120336fa461
soufiane-cheouati <46105138+soufiane-cheouati@users.noreply.github.com>
2019-03-18 23:34:05 +0000

    Add files via upload

--- algorithms-miscellaneous-2/src/test/java/com/baeldung/algorithms/reversingtree/TreeReverserUnitTest.java
+++ algorithms-miscellaneous-2/src/test/java/com/baeldung/algorithms/reversingtree/TreeReverserUnitTest.java
@@ -10,7 +10,7 @@ public class TreeReverserUnitTest {
     public void givenTreeWhenReversingRecursivelyThenReversed() {
         TreeReverser reverser = new TreeReverser();
 
-        TreeNode treeNode = reverser.createBinaryTree();
+        TreeNode treeNode = createBinaryTree();
 
         reverser.reverseRecursive(treeNode);
 
@@ -22,12 +22,27 @@ public class TreeReverserUnitTest {
     public void givenTreeWhenReversingIterativelyThenReversed() {
         TreeReverser reverser = new TreeReverser();
 
-        TreeNode treeNode = reverser.createBinaryTree();
+        TreeNode treeNode = createBinaryTree();
 
         reverser.reverseIterative(treeNode);
 
         assertEquals("4 7 9 6 2 3 1", reverser.toString(treeNode)
             .trim());
+    }
+
+    private TreeNode createBinaryTree() {
+
+        TreeNode leaf1 = new TreeNode(1);
+        TreeNode leaf2 = new TreeNode(3);
+        TreeNode leaf3 = new TreeNode(6);
+        TreeNode leaf4 = new TreeNode(9);
+
+        TreeNode nodeRight = new TreeNode(7, leaf3, leaf4);
+        TreeNode nodeLeft = new TreeNode(2, leaf1, leaf2);
+
+        TreeNode root = new TreeNode(4, nodeLeft, nodeRight);
+
+        return root;
     }
 
 }
