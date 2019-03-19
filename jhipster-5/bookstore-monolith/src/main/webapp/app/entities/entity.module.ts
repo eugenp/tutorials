a03ec155b4231e1bb369baa194a63b5d40484cb9
Michael Pratt <prattm@gmail.com>
2019-03-19 06:31:54 -0600

    Initial application generated by JHipster-5.8.2

---
+++ jhipster-5/bookstore-monolith/src/main/webapp/app/entities/entity.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+    imports: [
+        RouterModule.forChild([
+            /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
+        ])
+    ],
+    declarations: [],
+    entryComponents: [],
+    providers: [],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class BookstoreEntityModule {}
