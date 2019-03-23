683fc563a6e2f999e49ea4410b1994d384c630c3
Loredana <lore.crusoveanu@gmail.com>
2019-03-23 12:30:06 +0200

    formatting

--- core-java-12/src/test/java/com/baeldung/collectors/CollectorsUnitTest.java
+++ core-java-12/src/test/java/com/baeldung/collectors/CollectorsUnitTest.java
@@ -16,62 +16,56 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class CollectorsUnitTest {
 
-	@Test
-	public void whenTeeing_ItShouldCombineTheResultsAsExpected() {
-		List<Integer> numbers = Arrays.asList(42, 4, 2, 24);
-		Range range = numbers.stream()
-				.collect(teeing(
-						minBy(Integer::compareTo),
-						maxBy(Integer::compareTo),
-						(min, max) -> new Range(min.orElse(null), max.orElse(null))
-				));
+    @Test
+    public void whenTeeing_ItShouldCombineTheResultsAsExpected() {
+        List<Integer> numbers = Arrays.asList(42, 4, 2, 24);
+        Range range = numbers.stream()
+            .collect(teeing(minBy(Integer::compareTo), maxBy(Integer::compareTo), (min, max) -> new Range(min.orElse(null), max.orElse(null))));
 
-		assertThat(range).isEqualTo(new Range(2, 42));
-	}
+        assertThat(range).isEqualTo(new Range(2, 42));
+    }
 
-	/**
-	 * Represents a closed range of numbers between {@link #min} and
-	 * {@link #max}, both inclusive.
-	 */
-	private static class Range {
+    /**
+     * Represents a closed range of numbers between {@link #min} and
+     * {@link #max}, both inclusive.
+     */
+    private static class Range {
 
-		private final Integer min;
+        private final Integer min;
 
-		private final Integer max;
+        private final Integer max;
 
-		Range(Integer min, Integer max) {
-			this.min = min;
-			this.max = max;
-		}
+        Range(Integer min, Integer max) {
+            this.min = min;
+            this.max = max;
+        }
 
-		Integer getMin() {
-			return min;
-		}
+        Integer getMin() {
+            return min;
+        }
 
-		Integer getMax() {
-			return max;
-		}
+        Integer getMax() {
+            return max;
+        }
 
-		@Override
-		public boolean equals(Object o) {
-			if (this == o) return true;
-			if (o == null || getClass() != o.getClass()) return false;
-			Range range = (Range) o;
-			return Objects.equals(getMin(), range.getMin()) &&
-					Objects.equals(getMax(), range.getMax());
-		}
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            if (o == null || getClass() != o.getClass())
+                return false;
+            Range range = (Range) o;
+            return Objects.equals(getMin(), range.getMin()) && Objects.equals(getMax(), range.getMax());
+        }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(getMin(), getMax());
-		}
+        @Override
+        public int hashCode() {
+            return Objects.hash(getMin(), getMax());
+        }
 
-		@Override
-		public String toString() {
-			return "Range{" +
-					"min=" + min +
-					", max=" + max +
-					'}';
-		}
-	}
+        @Override
+        public String toString() {
+            return "Range{" + "min=" + min + ", max=" + max + '}';
+        }
+    }
 }
