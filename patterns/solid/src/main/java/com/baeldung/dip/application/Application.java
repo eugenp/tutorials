963a9b7249258e88d707a0a85c9ea718412696aa
Alejandro Gervasio <alejandro.gervasio@gmail.com>
2019-03-23 18:32:50 -0300

    Update Application.java

--- patterns/solid/src/main/java/com/baeldung/dip/application/Application.java
+++ patterns/solid/src/main/java/com/baeldung/dip/application/Application.java
@@ -1,17 +1,17 @@
 package com.baeldung.dip.application;
 
-import com.baeldung.dip.daoimplementations.ListCustomerDao;
+import com.baeldung.dip.daoimplementations.MapCustomerDao;
 import com.baeldung.dip.entities.Customer;
-import com.baeldung.dip.daointerfaces.CustomerDao;
 import com.baeldung.dip.services.CustomerService;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Application {
 
     public static void main(String[] args) {
-        CustomerDao customerDao = new ListCustomerDao(Arrays.asList(new Customer(1, "John"), new Customer(2, "Susan")));
-        CustomerService customerService = new CustomerService(customerDao);
-        customerService.findAll().forEach(System.out::println);
-        System.out.println(customerService.findById(0).orElseGet(() -> new Customer(0, "Non- existing customer")));
+        var customers = new HashMap<Integer, Customer>();
+        customers.put(1, new Customer("John"));
+        customers.put(2, new Customer("Susan"));
+        CustomerService customerService = new CustomerService(new MapCustomerDao(customers));
+        customerService.findAll().values().forEach(System.out::println);
     }
 }
