6461816289bd27111d34e0dbca04d859f0df889e
Krzysiek <krzysztofwoyke@gmail.com>
2019-03-25 21:34:06 +0100

    BAEL-2836 jUnit test class name fix

--- patterns/design-patterns-2/src/test/java/com/baeldung/mediator/MediatorIntegrationTest.java
+++ patterns/design-patterns-2/src/test/java/com/baeldung/mediator/MediatorIntegrationTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-public class ButtonTest {
+public class MediatorIntegrationTest {
 
     private Button button;
     private Fan fan;
