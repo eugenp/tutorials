9a34fc3e20992cbf10e3c2867a3127a2e71251f8
Rodolfo Felipe <rodolforfq@gmail.com>
2019-03-25 23:25:15 -0400

    BAEL-2806
    Article companion code.

---
+++ persistence-modules/java-jpa/src/main/java/com/baeldung/jpa/querytypes/QueryTypesExamples.java
@@ -0,0 +1,58 @@
+package com.baeldung.jpa.querytypes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+/**
+ * JPA Query Types examples. All using the UserEntity class.
+ *
+ * @author Rodolfo Felipe
+ */
+public class QueryTypesExamples {
+
+    EntityManagerFactory emf; // Just an example. Should be replaced with actual implementation.
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public UserEntity getUserByIdWithPlainQuery(Long id) {
+        Query jpqlQuery = getEntityManager().createQuery("SELECT u FROM UserEntity u WHERE u.id=:id");
+        jpqlQuery.setParameter("id", id);
+        return (UserEntity) jpqlQuery.getSingleResult();
+    }
+
+    public UserEntity getUserByIdWithTypedQuery(Long id) {
+        TypedQuery<UserEntity> typedQuery = getEntityManager().createQuery("SELECT u FROM UserEntity u WHERE u.id=:id", UserEntity.class);
+        typedQuery.setParameter("id", id);
+        return typedQuery.getSingleResult();
+    }
+
+    public UserEntity getUserByIdWithNamedQuery(Long id) {
+        Query namedQuery = getEntityManager().createNamedQuery("UserEntity.findByUserId");
+        namedQuery.setParameter("userId", id);
+        return (UserEntity) namedQuery.getSingleResult();
+    }
+
+    public UserEntity getUserByIdWithNativeQuery(Long id) {
+        Query nativeQuery = getEntityManager().createNativeQuery("SELECT * FROM user WHERE id=:userId", UserEntity.class);
+        nativeQuery.setParameter("id", id);
+        return (UserEntity) nativeQuery.getSingleResult();
+    }
+
+    public UserEntity getUserByIdWithCriteriaQuery(Long id) {
+        CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+        CriteriaQuery<UserEntity> criteriaQuery = criteriaBuilder.createQuery(UserEntity.class);
+        Root<UserEntity> userRoot = criteriaQuery.from(UserEntity.class);
+        UserEntity queryResult = getEntityManager().createQuery(criteriaQuery.select(userRoot)
+            .where(criteriaBuilder.equal(userRoot.get("id"), criteriaBuilder.parameter(Long.class, "id"))))
+            .getSingleResult();
+        return queryResult;
+    }
+
+}
