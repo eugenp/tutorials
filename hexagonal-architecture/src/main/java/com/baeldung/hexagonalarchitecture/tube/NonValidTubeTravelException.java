f4cf9ff06af3b6628dbe5b77df0c2bcf26253ef4
Antonio Moreno <thedownhiller@gmail.com>
2019-03-27 18:48:58 +0100

    BAEL-2772 Relatively Prime code

--- hexagonal-architecture/src/main/java/com/baeldung/hexagonalarchitecture/tube/NonValidTubeTravelException.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.hexagonalarchitecture.tube;
-
-class NonValidTubeTravelException extends RuntimeException {
-    NonValidTubeTravelException(String msg) {
-        super(msg);
-    }
-}
