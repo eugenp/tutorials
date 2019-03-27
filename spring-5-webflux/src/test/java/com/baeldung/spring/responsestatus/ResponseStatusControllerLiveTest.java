d26f3ac49a176a193768830bcd2797739c42255a
isaolmez <isaolmez@gmail.com>
2019-03-27 21:35:54 +0300

    BAEL-2715: Moved reactor netty samples to an existing module

--- spring-5-webflux/src/test/java/com/baeldung/spring/responsestatus/ResponseStatusControllerLiveTest.java
+++ spring-5-webflux/src/test/java/com/baeldung/spring/responsestatus/ResponseStatusControllerLiveTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ResponseStatusControllerTests {
+public class ResponseStatusControllerLiveTest {
 
     @Autowired
     private WebTestClient testClient;
