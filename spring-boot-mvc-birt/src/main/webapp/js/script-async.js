d973b289c4076c0c2125f7bf24384d89ba966152
Gian Mario Contessa <gmcontessa@hedgingbeta.com>
2019-03-29 18:30:28 +0000

    BAEL-617: added dependencies and report generation code

--- spring-boot-mvc-birt/src/main/webapp/js/script-async.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-	var node1 = document.createTextNode("message = " + message);
-	var node2 = document.createTextNode("number = " + number);
-	document.getElementById('message-async').append(node1);
-	document.getElementById('number-async').append(node2);
-};
