9f8058a5603ced4177f5bc42ca7b8dbd408a5953
Matt Zhang <lihui.cheung@gmail.com>
2019-03-29 23:46:43 +1100

    BAEL-2569  refactoring the class PriceCalculationEnvironmentPostProcessor

--- spring-boot/src/main/java/com/baeldung/environmentpostprocessor/PriceCalculationEnvironmentPostProcessor.java
+++ spring-boot/src/main/java/com/baeldung/environmentpostprocessor/PriceCalculationEnvironmentPostProcessor.java
@@ -8,17 +8,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
 import org.springframework.core.env.StandardEnvironment;
 
-public class PriceCalculationEnvironmentPostProcessor implements EnvironmentPostProcessor, Ordered {
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class PriceCalculationEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private static final Logger logger = LoggerFactory.getLogger(PriceCalculationEnvironmentPostProcessor.class);
 
-    public static final int DEFAULT_ORDER = Ordered.LOWEST_PRECEDENCE;
-    private int order = DEFAULT_ORDER;
     private static final String PROPERTY_PREFIX = "com.baeldung.environmentpostprocessor.";
     private static final String OS_ENV_PROPERTY_CALCUATION_MODE = "calculation_mode";
     private static final String OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE = "gross_calculation_tax_rate";
@@ -28,24 +28,32 @@ public class PriceCalculationEnvironmentPostProcessor implements EnvironmentPost
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 
-        PropertySource<?> systemEnvironmentPropertySource = environment.getPropertySources()
+        PropertySource<?> systemEnvPropertySource = environment.getPropertySources()
             .get(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME);
 
-        Map<String, Object> priceCalculationConfiguration = new LinkedHashMap<>();
-        if (isActive(systemEnvironmentPropertySource)) {
-            priceCalculationConfiguration.put(key(OS_ENV_PROPERTY_CALCUATION_MODE), systemEnvironmentPropertySource.getProperty(OS_ENV_PROPERTY_CALCUATION_MODE));
-            priceCalculationConfiguration.put(key(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE), systemEnvironmentPropertySource.getProperty(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE));
+        Map<String, Object> mapPropertySource = new LinkedHashMap<>();
+        if (isActive(systemEnvPropertySource)) {
+            populatePropertySource(systemEnvPropertySource, mapPropertySource);
         } else {
             logger.warn("System environment variables [calculation_mode,gross_calculation_tax_rate] not detected, fallback to default value [calcuation_mode={},gross_calcuation_tax_rate={}]", OS_ENV_PROPERTY_CALCUATION_MODE_DEFAULT_VALUE,
                 OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
-            priceCalculationConfiguration.put(key(OS_ENV_PROPERTY_CALCUATION_MODE), OS_ENV_PROPERTY_CALCUATION_MODE_DEFAULT_VALUE);
-            priceCalculationConfiguration.put(key(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE), OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
+            populateDefaultPropertySource(mapPropertySource);
         }
 
-        PropertySource<?> priceCalcuationPropertySource = new MapPropertySource("priceCalcuationPS", priceCalculationConfiguration);
+        PropertySource<?> priceCalcuationPropertySource = new MapPropertySource("priceCalcuationPS", mapPropertySource);
         environment.getPropertySources()
             .addAfter(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, priceCalcuationPropertySource);
 
+    }
+
+    private void populatePropertySource(PropertySource<?> source, Map<String, Object> target) {
+        target.put(key(OS_ENV_PROPERTY_CALCUATION_MODE), source.getProperty(OS_ENV_PROPERTY_CALCUATION_MODE));
+        target.put(key(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE), source.getProperty(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE));
+    }
+
+    private void populateDefaultPropertySource(Map<String, Object> target) {
+        target.put(key(OS_ENV_PROPERTY_CALCUATION_MODE), OS_ENV_PROPERTY_CALCUATION_MODE_DEFAULT_VALUE);
+        target.put(key(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE), OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
     }
 
     private String key(String key) {
@@ -57,15 +65,6 @@ public class PriceCalculationEnvironmentPostProcessor implements EnvironmentPost
             return true;
         } else
             return false;
-    }
-
-    public void setOrder(int order) {
-        this.order = order;
-    }
-
-    @Override
-    public int getOrder() {
-        return order;
     }
 
 }
