99a3aa5bab5e066bd8470ac2619160aaab19e788
Matt Zhang <lihui.cheung@gmail.com>
2019-03-31 22:42:32 +1100

    BAEL-2569: changes to class PriceCalculationEnvironmentPostProcessor

--- spring-boot/src/main/java/com/baeldung/environmentpostprocessor/PriceCalculationEnvironmentPostProcessor.java
+++ spring-boot/src/main/java/com/baeldung/environmentpostprocessor/PriceCalculationEnvironmentPostProcessor.java
@@ -1,7 +1,12 @@
 package com.baeldung.environmentpostprocessor;
 
+import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
+
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,56 +17,62 @@ import org.springframework.core.annotation.Order;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
-import org.springframework.core.env.StandardEnvironment;
 
 @Order(Ordered.LOWEST_PRECEDENCE)
 public class PriceCalculationEnvironmentPostProcessor implements EnvironmentPostProcessor {
 
     private static final Logger logger = LoggerFactory.getLogger(PriceCalculationEnvironmentPostProcessor.class);
 
-    private static final String PROPERTY_PREFIX = "com.baeldung.environmentpostprocessor.";
-    private static final String OS_ENV_PROPERTY_CALCUATION_MODE = "calculation_mode";
-    private static final String OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE = "gross_calculation_tax_rate";
-    private static final String OS_ENV_PROPERTY_CALCUATION_MODE_DEFAULT_VALUE = "NET";
-    private static final double OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE = 0;
+    private static final String PREFIX = "com.baeldung.environmentpostprocessor.";
+    private static final String CALCUATION_MODE = "calculation_mode";
+    private static final String GROSS_CALCULATION_TAX_RATE = "gross_calculation_tax_rate";
+    private static final String CALCUATION_MODE_DEFAULT_VALUE = "NET";
+    private static final double GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE = 0;
+
+    List<String> names = Arrays.asList(CALCUATION_MODE, GROSS_CALCULATION_TAX_RATE);
+
+    private static Map<String, Object> defaults = new LinkedHashMap<>();
+    static {
+        defaults.put(CALCUATION_MODE, CALCUATION_MODE_DEFAULT_VALUE);
+        defaults.put(GROSS_CALCULATION_TAX_RATE, GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
+    }
 
     @Override
     public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 
-        PropertySource<?> systemEnvPropertySource = environment.getPropertySources()
-            .get(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME);
+        PropertySource<?> system = environment.getPropertySources()
+            .get(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME);
 
-        Map<String, Object> mapPropertySource = new LinkedHashMap<>();
-        if (isActive(systemEnvPropertySource)) {
-            populatePropertySource(systemEnvPropertySource, mapPropertySource);
-        } else {
-            logger.warn("System environment variables [calculation_mode,gross_calculation_tax_rate] not detected, fallback to default value [calcuation_mode={},gross_calcuation_tax_rate={}]", OS_ENV_PROPERTY_CALCUATION_MODE_DEFAULT_VALUE,
-                OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
-            populateDefaultPropertySource(mapPropertySource);
+        Map<String, Object> prefixed = new LinkedHashMap<>();
+
+        if (!hasOurPriceProperties(system)) {
+            // Baeldung-internal code so this doesn't break other examples
+            logger.warn("System environment variables [calculation_mode,gross_calculation_tax_rate] not detected, fallback to default value [calcuation_mode={},gross_calcuation_tax_rate={}]", CALCUATION_MODE_DEFAULT_VALUE,
+                GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
+            prefixed = names.stream()
+                .collect(Collectors.toMap(this::rename, this::getDefaultValue));
+            environment.getPropertySources()
+                .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource("prefixer", prefixed));
+            return;
         }
 
-        PropertySource<?> priceCalcuationPropertySource = new MapPropertySource("priceCalcuationPS", mapPropertySource);
+        prefixed = names.stream()
+            .collect(Collectors.toMap(this::rename, system::getProperty));
         environment.getPropertySources()
-            .addAfter(StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, priceCalcuationPropertySource);
+            .addAfter(SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME, new MapPropertySource("prefixer", prefixed));
 
     }
 
-    private void populatePropertySource(PropertySource<?> source, Map<String, Object> target) {
-        target.put(key(OS_ENV_PROPERTY_CALCUATION_MODE), source.getProperty(OS_ENV_PROPERTY_CALCUATION_MODE));
-        target.put(key(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE), source.getProperty(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE));
+    private Object getDefaultValue(String key) {
+        return defaults.get(key);
     }
 
-    private void populateDefaultPropertySource(Map<String, Object> target) {
-        target.put(key(OS_ENV_PROPERTY_CALCUATION_MODE), OS_ENV_PROPERTY_CALCUATION_MODE_DEFAULT_VALUE);
-        target.put(key(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE), OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE_DEFAULT_VALUE);
+    private String rename(String key) {
+        return PREFIX + key.replaceAll("\\_", ".");
     }
 
-    private String key(String key) {
-        return PROPERTY_PREFIX + key.replaceAll("\\_", ".");
-    }
-
-    private boolean isActive(PropertySource<?> systemEnvpropertySource) {
-        if (systemEnvpropertySource.containsProperty(OS_ENV_PROPERTY_CALCUATION_MODE) && systemEnvpropertySource.containsProperty(OS_ENV_PROPERTY_GROSS_CALCULATION_TAX_RATE)) {
+    private boolean hasOurPriceProperties(PropertySource<?> system) {
+        if (system.containsProperty(CALCUATION_MODE) && system.containsProperty(GROSS_CALCULATION_TAX_RATE)) {
             return true;
         } else
             return false;
