395b8ba5c33a5f9e1db2b603a786bdf16df4fd31
Vitalii Plagov <vitalii.plagov@gmail.com>
2019-03-31 15:32:14 +0300

    Fix issues with packages

--- patterns/design-patterns-2/src/main/java/com/baeldung/hexagonal/adapters/PurchaseByCash.java
+++ patterns/design-patterns-2/src/main/java/com/baeldung/hexagonal/adapters/PurchaseByCash.java
@@ -1,11 +1,11 @@
-package com.baeldung.adapters;
+package com.baeldung.hexagonal.adapters;
 
-import com.baeldung.domain.Product;
-import com.baeldung.domain.Purchase;
+import com.baeldung.hexagonal.domain.Product;
+import com.baeldung.hexagonal.domain.Purchase;
 
 public class PurchaseByCash implements Purchase {
 
-    public boolean purchaseProduct(Product product) {
-        return product.getPrice() <= 100;
-    }
+        public boolean purchaseProduct(Product product) {
+                return product.getPrice() <= 100;
+        }
 }
--- patterns/design-patterns-2/src/test/java/com/baeldung/hexagonal/PurchaseByCashTest.java
+++ patterns/design-patterns-2/src/test/java/com/baeldung/hexagonal/PurchaseByCashTest.java
@@ -1,26 +1,27 @@
-package com.baeldung.adapters;
+package com.baeldung.hexagonal;
 
-import com.baeldung.domain.Product;
+import com.baeldung.hexagonal.adapters.PurchaseByCash;
+import com.baeldung.hexagonal.domain.Product;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class PurchaseByCashTest {
 
-    @Test
-    void shouldPurchaseProductBelowLimit() {
-        Product product = new Product("book", 100);
-        PurchaseByCash purchase = new PurchaseByCash();
+        @Test
+        void shouldPurchaseProductBelowLimit() {
+                Product product = new Product("book", 100);
+                PurchaseByCash purchase = new PurchaseByCash();
 
-        assertTrue(purchase.purchaseProduct(product));
-    }
+                assertTrue(purchase.purchaseProduct(product));
+        }
 
-    @Test
-    void shouldNotPurchaseProductAboveLimit() {
-        Product product = new Product("car", 1000);
-        PurchaseByCash purchase = new PurchaseByCash();
+        @Test
+        void shouldNotPurchaseProductAboveLimit() {
+                Product product = new Product("car", 1000);
+                PurchaseByCash purchase = new PurchaseByCash();
 
-        assertFalse(purchase.purchaseProduct(product));
-    }
+                assertFalse(purchase.purchaseProduct(product));
+        }
 }
