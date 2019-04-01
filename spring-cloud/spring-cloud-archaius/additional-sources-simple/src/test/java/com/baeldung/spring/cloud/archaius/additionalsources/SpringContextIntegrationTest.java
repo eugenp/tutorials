b42febfd83c6797c9ee71847b61053b9818b31db
Ger Roza <rozagerardo@gmail.com>
2019-04-01 11:33:22 -0300

    Fixed or added Context Integration Tests for modules named in ticket

--- spring-cloud/spring-cloud-archaius/additional-sources-simple/src/test/java/com/baeldung/spring/cloud/archaius/additionalsources/SpringContextIntegrationTest.java
+++ spring-cloud/spring-cloud-archaius/additional-sources-simple/src/test/java/com/baeldung/spring/cloud/archaius/additionalsources/SpringContextIntegrationTest.java
@@ -1,11 +1,9 @@
-package org.baeldung;
+package com.baeldung.spring.cloud.archaius.additionalsources;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.baeldung.spring.cloud.archaius.additionalsources.AdditionalSourcesSimpleApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = AdditionalSourcesSimpleApplication.class)
--- spring-cloud/spring-cloud-eureka/spring-cloud-eureka-client/src/test/java/com/baeldung/spring/cloud/eureka/client/SpringContextIntegrationTest.java
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-client/src/test/java/com/baeldung/spring/cloud/eureka/client/SpringContextIntegrationTest.java
@@ -1,17 +1,16 @@
-package org.baeldung;
+package com.baeldung.spring.cloud.eureka.client;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.baeldung.spring.cloud.aws.InstanceProfileAwsApplication;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = InstanceProfileAwsApplication.class)
+@SpringBootTest
 public class SpringContextIntegrationTest {
-
+    
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
     }
+
 }
