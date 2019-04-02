551c3e06dabf3e0f4f7396f32d0e9024e05a31f4
eric-martin <emart.tigers@gmail.com>
2019-04-01 22:35:07 -0500

    BAEL-2829: Moving to spring-data-jpa-2

--- persistence-modules/spring-data-jpa-2/src/main/java/com/baeldung/repository/EmployeeRepository.java
+++ persistence-modules/spring-data-jpa-2/src/main/java/com/baeldung/repository/EmployeeRepository.java
@@ -1,8 +1,8 @@
-package com.baeldung.dao.repositories;
+package com.baeldung.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.baeldung.domain.Employee;
+import com.baeldung.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
