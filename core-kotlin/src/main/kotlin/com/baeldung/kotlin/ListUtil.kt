fc16b4238e748c551e66b0f038fe4e17df51bccf
Kacper Koza <kkoza11@gmail.com>
2019-04-03 09:47:58 +0200

    BAEL

--- core-kotlin/src/main/kotlin/com/baeldung/kotlin/ListUtil.kt
+++
@@ -1,9 +0,0 @@
-@file:JvmName("Lists")
-package com.baeldung.kotlin
-
-fun <T> MutableList<T>.swap(firstIndex: Int, secondIndex: Int): MutableList<T> {
-    val tmp = this[firstIndex]
-    this[firstIndex] = this[secondIndex]
-    this[secondIndex] = tmp
-    return this
-}
