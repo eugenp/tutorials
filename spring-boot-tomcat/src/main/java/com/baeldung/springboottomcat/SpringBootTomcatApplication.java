bac9fd273e2ddc1cb0a90a602979841a8e1c8e35
Krzysztof Majewski <kmajewski@berg.technology>
2019-04-04 10:21:07 +0200

    BAEL-2493

---
+++ spring-boot-tomcat/src/main/java/com/baeldung/springboottomcat/SpringBootTomcatApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.springboottomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTomcatApplication.class, args);
+	}
+
+}
