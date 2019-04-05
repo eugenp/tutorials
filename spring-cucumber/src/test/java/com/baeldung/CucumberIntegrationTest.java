b53be7994898cf4a504f8ec4053f1069802bf0ec
ABDERRAOUF MAKHLOUF <abderraouf.makhlouf@ext.adeo.com>
2019-04-05 12:01:00 +0200

    chore(BDD): publish cucumber-reports

--- spring-cucumber/src/test/java/com/baeldung/CucumberIntegrationTest.java
+++ spring-cucumber/src/test/java/com/baeldung/CucumberIntegrationTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/feature")
+@CucumberOptions(features = "src/test/resources/feature",
+        plugin = {"pretty", "html:target/cucumber-reports"})
 public class CucumberIntegrationTest {
 }
