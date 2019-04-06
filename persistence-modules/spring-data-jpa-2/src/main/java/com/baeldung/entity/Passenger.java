9276ba3c32fe575136778dfa457661720f99445d
shubhi22 <shubhi.srvstv@gmail.com>
2019-04-06 17:01:57 +0530

    BAEL-2826 : Override equals hashcode

--- persistence-modules/spring-data-jpa-2/src/main/java/com/baeldung/entity/Passenger.java
+++ persistence-modules/spring-data-jpa-2/src/main/java/com/baeldung/entity/Passenger.java
@@ -5,6 +5,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 public class Passenger {
@@ -58,5 +59,20 @@ public class Passenger {
     @Override
     public String toString() {
         return "Passenger{" + "id=" + id + ", firstName='" + firstName + '\'' + ", lastName='" + lastName + '\'' + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Passenger passenger = (Passenger) o;
+        return Objects.equals(firstName, passenger.firstName) && Objects.equals(lastName, passenger.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
     }
 }
