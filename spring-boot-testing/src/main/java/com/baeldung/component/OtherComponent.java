86355529110b51e30bdc4301f1208dadf711829a
mprevisic <markoprevisic@gmail.com>
2019-04-06 20:45:08 +0200

    BAEL-1970 minor log message correction

--- spring-boot-testing/src/main/java/com/baeldung/component/OtherComponent.java
+++ spring-boot-testing/src/main/java/com/baeldung/component/OtherComponent.java
@@ -10,10 +10,10 @@ public class OtherComponent {
     private static final Logger LOG = LoggerFactory.getLogger(OtherComponent.class);
 
     public void processData() {
-        LOG.trace("This is a TRACE log from OtherComponent");
-        LOG.debug("This is a DEBUG log from OtherComponent");
-        LOG.info("This is an INFO log from OtherComponent");
-        LOG.error("This is an ERROR log from OtherComponent");
+        LOG.trace("This is a TRACE log from another package");
+        LOG.debug("This is a DEBUG log from another package");
+        LOG.info("This is an INFO log from another package");
+        LOG.error("This is an ERROR log from another package");
     }
 
 }
