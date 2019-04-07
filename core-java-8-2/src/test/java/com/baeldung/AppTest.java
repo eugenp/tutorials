3c793a03247bd7fdfabc106bb8c9390462a635ec
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-07 19:37:05 +0200

    Add a code snippet about anonymous classes (BAEL-2833)
    Add a code snippet about anonymous classes (BAEL-2833)
    Create module core-java-2 and move 'anonymous' package there

---
+++ core-java-8-2/src/test/java/com/baeldung/AppTest.java
@@ -0,0 +1,20 @@
+package com.baeldung;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
