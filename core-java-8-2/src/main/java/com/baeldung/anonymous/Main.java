c0727cd90b9dd7bc027707a633c2aa3b9f9ed2e4
Andrew Shcherbakov <veontomo@gmail.com>
2019-04-08 20:13:15 +0200

    Create module core-java-2 and move 'anonymous' package there

--- core-java-8-2/src/main/java/com/baeldung/anonymous/Main.java
+++ core-java-8-2/src/main/java/com/baeldung/anonymous/Main.java
@@ -3,6 +3,15 @@ package com.baeldung.anonymous;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Code snippet that illustrates the usage of anonymous classes.
+ * 
+ * Note that use of Runnable instances in this example does not demonstrate their
+ * common use.
+ * 
+ * @author A. Shcherbakov
+ *
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -23,7 +32,7 @@ public class Main {
                 return "Famous GoF book.";
             }
         };
-        
+
         System.out.println(String.format("Title: %s, description: %s", book.title, book.description()));
 
         actions.add(new Runnable() {
@@ -35,7 +44,7 @@ public class Main {
         });
 
         int count = 1;
-        
+
         Runnable action2 = new Runnable() {
             static final int x = 0;
             // static int y = 0;
@@ -46,9 +55,9 @@ public class Main {
             }
         };
         actions.add(action2);
-        
+
         for (Runnable a : actions) {
-               a.run();
+            a.run();
         }
     }
 
