3f9af3a8f0dfff30c6d6b8362de38b5cda234612
Ger Roza <rozagerardo@gmail.com>
2019-04-10 17:58:34 -0300

    fixed spring-cloud context integration tests - part 2:

--- spring-cloud/spring-cloud-config/server/src/test/java/com/baeldung/spring/cloud/config/server/SpringContextIntegrationTest.java
+++ spring-cloud/spring-cloud-config/server/src/test/java/com/baeldung/spring/cloud/config/server/SpringContextIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.baeldung;
+package com.baeldung.spring.cloud.config.server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,13 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.baeldung.spring.cloud.config.client.ConfigClient;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ConfigClient.class)
+@SpringBootTest(classes = ConfigServer.class)
 @WebAppConfiguration
 public class SpringContextIntegrationTest {
     @Test
-    public void contextLoads() {
+    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
     }
 }
--- spring-cloud/spring-cloud-connectors-heroku/src/test/java/com/baeldung/spring/cloud/connectors/heroku/SpringContextIntegrationTest.java
+++ spring-cloud/spring-cloud-connectors-heroku/src/test/java/com/baeldung/spring/cloud/connectors/heroku/SpringContextIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.baeldung;
+package com.baeldung.spring.cloud.connectors.heroku;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
