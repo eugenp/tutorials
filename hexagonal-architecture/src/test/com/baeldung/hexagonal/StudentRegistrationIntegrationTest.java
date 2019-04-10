d0a00469afd9da58e3c41e57f6520157ddae2211
Pavan Shankar Koli <pskoli93@gmail.com>
2019-04-10 09:00:42 +0530

    Added static imports in test class

--- hexagonal-architecture/src/test/com/baeldung/hexagonal/StudentRegistrationIntegrationTest.java
+++ hexagonal-architecture/src/test/com/baeldung/hexagonal/StudentRegistrationIntegrationTest.java
@@ -1,34 +1,51 @@
 package com.baeldung.hexagonal;
 
-import com.baeldung.hexagonal.service.StudentService;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.baeldung.hexagonal.adapter.StudentInMemoryRepositoryAdapter;
 import com.baeldung.hexagonal.adapter.StudentInputAdapter;
+import com.baeldung.hexagonal.service.StudentService;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StudentRegistrationIntegrationTest {
 
-    StudentInMemoryRepositoryAdapter repositoryAdapter = new StudentInMemoryRepositoryAdapter();
-
-    StudentService studentService = new StudentService(repositoryAdapter);
-
-    StudentInputAdapter inputAdapter = new StudentInputAdapter(studentService);
-
     @Test
     public void givenStudentNameIsEntered_whenStudentIsCreated_thenStudentWillBePresentInRepository(){
+        StudentInMemoryRepositoryAdapter repositoryAdapter = new StudentInMemoryRepositoryAdapter();
+        StudentService studentService = new StudentService(repositoryAdapter);
+        StudentInputAdapter inputAdapter = new StudentInputAdapter(studentService);
+
         String studentId = inputAdapter.registerStudent("Tim");
 
-        Assert.assertTrue(repositoryAdapter.getStudent(studentId)!=null);
+        assertTrue(repositoryAdapter.getStudent(studentId)!=null);
     }
 
     @Test
-    public void givenStudentIdIsEnteredForRemoval_whenStudentIsRemoved_thenStudentWillBeAbsentInRepository(){
+    public void givenStudentIdIsEnteredForRemoval_whenStudentIsValid_thenStudentWillBeRemovedFromRepository(){
+        StudentInMemoryRepositoryAdapter repositoryAdapter = new StudentInMemoryRepositoryAdapter();
+        StudentService studentService = new StudentService(repositoryAdapter);
+        StudentInputAdapter inputAdapter = new StudentInputAdapter(studentService);
+
         String studentId = inputAdapter.registerStudent("Tom");
 
         boolean result = inputAdapter.deregisterStudent(studentId);
 
-        Assert.assertTrue(result && repositoryAdapter.getStudent(studentId)==null);
+        assertTrue(result && repositoryAdapter.getStudent(studentId)==null);
+    }
+
+    @Test
+    public void givenStudentIdIsEnteredForRemoval_whenStudentIsNotValid_thenStudentWillNotBeRemovedFromRepository(){
+        StudentInMemoryRepositoryAdapter repositoryAdapter = new StudentInMemoryRepositoryAdapter();
+        StudentService studentService = new StudentService(repositoryAdapter);
+        StudentInputAdapter inputAdapter = new StudentInputAdapter(studentService);
+
+        String studentId = inputAdapter.registerStudent("Tom");
+
+        boolean result = inputAdapter.deregisterStudent("random");
+
+        assertTrue(!result && repositoryAdapter.getStudent(studentId)!=null);
     }
 
 }
