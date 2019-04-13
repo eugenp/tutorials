beccf32be8bb458777f8c54b8e3aa4ad999e3301
Vivek <vivekrmd@gmail.com>
2019-04-13 14:18:55 +0530

    BAEL-2857 Defining a JPA Entity

---
+++ persistence-modules/java-jpa/src/test/java/com/baeldung/jpa/entity/StudentEntityIntegrationTest.java
@@ -0,0 +1,83 @@
+package com.baeldung.jpa.entity;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.baeldung.util.Gender;
+
+public class StudentEntityIntegrationTest {
+
+	private EntityManagerFactory emf;
+	private EntityManager em;
+
+	@Before
+	public void setup() {
+		emf = Persistence.createEntityManagerFactory("jpa-entity-definition");
+		em = emf.createEntityManager();
+	}
+
+	@Test
+	public void persistStudentThenRetrieveTheDetails() {
+		Student student = createStudentWithRelevantDetails();
+		persist(student);
+		clearThePersistenceContext();
+		List<Student> students = getStudentsFromTable();
+		checkAssertionsWith(students);
+	}
+
+	@After
+	public void destroy() {
+		if (em != null) {
+			em.close();
+		}
+		if (emf != null) {
+			emf.close();
+		}
+	}
+
+	private void clearThePersistenceContext() {
+		em.clear();
+	}
+
+	private void checkAssertionsWith(List<Student> students) {
+		assertEquals(1, students.size());
+		Student john = students.get(0);
+		assertEquals(1L, john.getId().longValue());
+		assertEquals(null, john.getAge());
+		assertEquals("John", john.getName());
+	}
+
+	private List<Student> getStudentsFromTable() {
+		String selectQuery = "SELECT student FROM Student student";
+		TypedQuery<Student> selectFromStudentTypedQuery = em.createQuery(selectQuery, Student.class);
+		List<Student> students = selectFromStudentTypedQuery.getResultList();
+		return students;
+	}
+
+	private void persist(Student student) {
+		em.getTransaction().begin();
+		em.persist(student);
+		em.getTransaction().commit();
+	}
+
+	private Student createStudentWithRelevantDetails() {
+		Student student = new Student();
+		student.setAge(20); // the 'age' field has been annotated with @Transient
+		student.setName("John");
+		student.setBirthDate(new Date());
+		student.setGender(Gender.MALE);
+		return student;
+	}
+
+}
