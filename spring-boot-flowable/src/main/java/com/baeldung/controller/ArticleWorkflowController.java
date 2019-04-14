919f20aba38a88ff806df2a2cb15a78098bcba96
Kumar Chandrakant <kchandrakant@gmail.com>
2019-04-14 17:01:49 +0530

    Made changes to the code for the review comments

--- spring-boot-flowable/src/main/java/com/baeldung/controller/ArticleWorkflowController.java
+++ spring-boot-flowable/src/main/java/com/baeldung/controller/ArticleWorkflowController.java
@@ -3,10 +3,9 @@ package com.baeldung.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,15 +17,15 @@ import com.baeldung.service.ArticleWorkflowService;
 public class ArticleWorkflowController {
     @Autowired
     private ArticleWorkflowService service;
-    @RequestMapping(value = "/submit", method = RequestMethod.POST)
+    @PostMapping("/submit")
     public void submit(@RequestBody Article article) {
         service.startProcess(article);
     }
-    @RequestMapping(value = "/tasks", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/tasks")
     public List<Article> getTasks(@RequestParam String assignee) {
         return service.getTasks(assignee);
     }
-    @RequestMapping(value = "/review", method = RequestMethod.POST)
+    @PostMapping("/review")
     public void review(@RequestBody Approval approval) {
         service.submitReview(approval);
     }
--- spring-boot-flowable/src/main/java/com/baeldung/service/ArticleWorkflowService.java
+++ spring-boot-flowable/src/main/java/com/baeldung/service/ArticleWorkflowService.java
@@ -1,9 +1,9 @@
 package com.baeldung.service;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.TaskService;
@@ -21,6 +21,7 @@ public class ArticleWorkflowService {
     private RuntimeService runtimeService;
     @Autowired
     private TaskService taskService;
+
     @Transactional
     public void startProcess(Article article) {
         Map<String, Object> variables = new HashMap<String, Object>();
@@ -28,18 +29,23 @@ public class ArticleWorkflowService {
         variables.put("url", article.getUrl());
         runtimeService.startProcessInstanceByKey("articleReview", variables);
     }
+
     @Transactional
     public List<Article> getTasks(String assignee) {
         List<Task> tasks = taskService.createTaskQuery()
-            .taskCandidateGroup(assignee)
-            .list();
-        List<Article> articles = new ArrayList<Article>();
-        for (Task task : tasks) {
-            Map<String, Object> variables = taskService.getVariables(task.getId());
-            articles.add(new Article(task.getId(), (String) variables.get("author"), (String) variables.get("url")));
-        }
+          .taskCandidateGroup(assignee)
+          .list();
+        
+        List<Article> articles = tasks.stream()
+          .map(task -> {
+              Map<String, Object> variables = taskService.getVariables(task.getId());
+              return new Article(
+                task.getId(), (String) variables.get("author"), (String) variables.get("url"));
+          })
+          .collect(Collectors.toList());
         return articles;
     }
+
     @Transactional
     public void submitReview(Approval approval) {
         Map<String, Object> variables = new HashMap<String, Object>();
