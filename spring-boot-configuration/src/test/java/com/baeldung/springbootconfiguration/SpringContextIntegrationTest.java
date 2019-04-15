0ae18bd657bbb8187303a4cb7590b33168f78d50
Krzysztof Majewski <kmajewski@berg.technology>
2019-04-15 13:14:39 +0200

    rename test

--- spring-boot-configuration/src/test/java/com/baeldung/springbootconfiguration/SpringContextIntegrationTest.java
+++ spring-boot-configuration/src/test/java/com/baeldung/springbootconfiguration/SpringContextIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootConfigurationApplicationTests {
+public class SpringContextIntegrationTest {
 
 	@Test
 	public void contextLoads() {
