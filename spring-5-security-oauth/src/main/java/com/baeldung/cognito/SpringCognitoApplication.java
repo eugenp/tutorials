3b37b67f460915d532b6487d44190ce26aa2a933
Adrian Precub <aprecub@ea.com>
2019-04-16 13:47:10 +0300

    BAEL-2587: Spring Security integration with Amazon Cognito - rename config file

--- spring-5-security-oauth/src/main/java/com/baeldung/cognito/SpringCognitoApplication.java
+++ spring-5-security-oauth/src/main/java/com/baeldung/cognito/SpringCognitoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("application.yml")
+@PropertySource("application_cognito.yml")
 public class SpringCognitoApplication {
 
     public static void main(String[] args) {
