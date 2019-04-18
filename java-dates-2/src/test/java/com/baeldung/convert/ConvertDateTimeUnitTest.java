637f2e945aaceb5c4b3162660bd45fc0cd6de1b2
mherbaghinyan <mbaghinyan@gmail.com>
2019-04-18 16:05:44 +0400

    added unit tests

--- java-dates-2/src/test/java/com/baeldung/convert/ConvertDateTimeUnitTest.java
+++ java-dates-2/src/test/java/com/baeldung/convert/ConvertDateTimeUnitTest.java
@@ -10,4 +10,33 @@ public class ConvertDateTimeUnitTest {
         System.out.println("LocalDateTime in milliseconds : " + millis);
     }
 
+    @Test
+    public void givenJava8Instant_WhenGetMillis() {
+        long millis = ConvertDateTime.java8Instant();
+        System.out.println("Java 8 Instant in milliseconds : " + millis);
+    }
+
+    @Test
+    public void givenDate_WhenGetMillis() {
+        long millis = ConvertDateTime.coreDate();
+        System.out.println("Core Date in milliseconds : " + millis);
+    }
+
+    @Test
+    public void givenCalendar_WhenGetMillis() {
+        long millis = ConvertDateTime.calendar();
+        System.out.println("Calendar in milliseconds : " + millis);
+    }
+
+    @Test
+    public void givenJodaInstant_WhenGetMillis() {
+        long millis = ConvertDateTime.jodaInstant();
+        System.out.println("JODA Instant in milliseconds : " + millis);
+    }
+
+    @Test
+    public void givenJODADateTime_WhenGetMillis() {
+        long millis = ConvertDateTime.jodaDateTime();
+        System.out.println("JODA DateTime in milliseconds : " + millis);
+    }
 }
