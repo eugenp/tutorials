5eb2a6e04eb393f3c9534c4f8a2f1f8b6af6327b
Liesheng Long <liesheng.long@gmail.com>
2019-04-19 19:08:52 -0400

    add test code for running jar with arguments

--- core-java/src/main/java/com/baeldung/jar/JarExample2.java
+++ core-java/src/main/java/com/baeldung/jar/JarExample2.java
@@ -1,9 +1,9 @@
 package com.baeldung.jar;
 
-public class JarExample {
+public class JarExample2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello Baeldung Reader in JarExample!");
+        System.out.println("Hello Baeldung Reader in JarExample2!");
         
         if(args == null) {
             System.out.println("You have not provided any arguments!");
