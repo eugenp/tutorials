4005794839c2b3320199d87d49dba56010192de2
David Calap <dcalap@gmail.com>
2019-04-20 14:36:10 +0200

    #BAEL-2828 - comments and Optional methods added

--- persistence-modules/spring-data-jpa/src/main/java/com/baeldung/passenger/PassengerRepository.java
+++ persistence-modules/spring-data-jpa/src/main/java/com/baeldung/passenger/PassengerRepository.java
@@ -4,12 +4,17 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+//import java.util.Optional;
 
 interface PassengerRepository extends JpaRepository<Passenger, Long>, CustomPassengerRepository {
 
     Passenger findFirstByOrderBySeatNumberAsc();
 
     Passenger findTopByOrderBySeatNumberAsc();
+
+    // As this wrapped Optional methods has the same name and params as above, we should use just one of them
+    //Optional<Passenger> findFirstByOrderBySeatNumberAsc();
+    //Optional<Passenger> findTopByOrderBySeatNumberAsc();
 
     List<Passenger> findByOrderBySeatNumberAsc();
     
