2eb83c12abaa09ffc9748d9df2cbdfccd76de7a8
alejandrogervasio <alejandro.gervasio@gmail.com>
2019-04-27 22:07:39 -0300

    Initial Commit

---
+++ core-java-modules/multimodulemavenproject/mainappmodule/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.baeldung.mainppmodule {
+    requires com.baeldung.entitymodule;
+    requires com.baeldung.daomodule;
+    requires com.baeldung.userdaomodule;
+    uses com.baeldung.userdaomodule.UserDao;
+    
+}
