e3a40972fe8c267a676d62ffdec5096c57bf59fc
amit2103 <amit.pandey2103@gmail.com>
2019-04-28 16:31:11 +0530

    [BAEL-13598] - Added old API, moved TimerTask Junit to httpclient-simple

--- httpclient-simple/src/test/java/org/baeldung/httpclient/HttpClientTimeoutLiveTest.java
+++ httpclient-simple/src/test/java/org/baeldung/httpclient/HttpClientTimeoutLiveTest.java
@@ -4,14 +4,21 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import org.apache.http.HttpResponse;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.ClientPNames;
 import org.apache.http.config.SocketConfig;
 import org.apache.http.conn.ConnectTimeoutException;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.params.CoreConnectionPNames;
+import org.apache.http.params.HttpParams;
 import org.junit.After;
 import org.junit.Test;
 
@@ -25,6 +32,20 @@ public class HttpClientTimeoutLiveTest {
     }
 
     // tests
+    @Test
+    public final void givenUsingOldApi_whenSettingTimeoutViaParameter_thenCorrect() throws IOException {
+        
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        int timeout = 5; // seconds
+        HttpParams httpParams = httpClient.getParams();
+        httpParams.setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, timeout * 1000);
+        httpParams.setParameter(CoreConnectionPNames.SO_TIMEOUT, timeout * 1000);
+        httpParams.setParameter(ClientPNames.CONN_MANAGER_TIMEOUT, new Long(timeout * 1000));
+        
+        final HttpGet request = new HttpGet("http://www.github.com");
+        HttpResponse execute = httpClient.execute(request);
+        assertThat(execute.getStatusLine().getStatusCode(), equalTo(200));
+    }
 
     @Test
     public final void givenUsingNewApi_whenSettingTimeoutViaRequestConfig_thenCorrect() throws IOException {
@@ -32,8 +53,6 @@ public class HttpClientTimeoutLiveTest {
         final RequestConfig config = RequestConfig.custom().setConnectTimeout(timeout * 1000).setConnectionRequestTimeout(timeout * 1000).setSocketTimeout(timeout * 1000).build();
         final CloseableHttpClient client = HttpClientBuilder.create().setDefaultRequestConfig(config).build();
         final HttpGet request = new HttpGet("http://www.github.com");
-
-        // httpParams.setLongParameter(ClientPNames.CONN_MANAGER_TIMEOUT, new Long(timeout * 1000)); // https://issues.apache.org/jira/browse/HTTPCLIENT-1418
 
         response = client.execute(request);
 
@@ -81,5 +100,28 @@ public class HttpClientTimeoutLiveTest {
         final HttpGet request = new HttpGet("http://www.google.com:81");
         client.execute(request);
     }
+    
+    @Test
+    public void whenSecuredRestApiIsConsumed_then200OK() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
 
+        int timeout = 20; // seconds
+        RequestConfig requestConfig = RequestConfig.custom().setConnectionRequestTimeout(timeout * 1000)
+          .setConnectTimeout(timeout * 1000).setSocketTimeout(timeout * 1000).build();
+        HttpGet getMethod = new HttpGet("http://localhost:8082/httpclient-simple/api/bars/1");
+        getMethod.setConfig(requestConfig);
+
+        int hardTimeout = 5; // seconds
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                getMethod.abort();
+            }
+        };
+        new Timer(true).schedule(task, hardTimeout * 1000);
+
+        HttpResponse response = httpClient.execute(getMethod);
+        System.out.println("HTTP Status of response: " + response.getStatusLine().getStatusCode());
+    }
+    
 }
