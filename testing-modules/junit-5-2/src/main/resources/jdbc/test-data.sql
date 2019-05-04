b15ba7ed4b5c8293fc136523f95f5297c7df2e88
Chirag Dewan <chirag.dewan@ericsson.com>
2019-05-04 16:14:14 +0530

    BAEL2848 JUnit Tagging and Filtering Tests

--- testing-modules/junit-5-2/src/main/resources/jdbc/test-data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO EMPLOYEE VALUES (1, 'James', 'Gosling', 'Canada');
-
-INSERT INTO EMPLOYEE VALUES (2, 'Donald', 'Knuth', 'USA');
-
-INSERT INTO EMPLOYEE VALUES (3, 'Linus', 'Torvalds', 'Finland');
-
-INSERT INTO EMPLOYEE VALUES (4, 'Dennis', 'Ritchie', 'USA');
