fc227bc128e4d931ae74e07d06f82cfcdb8b59a3
Ganesh Pagade <ganesh.pagade@tesco.com>
2019-05-07 10:58:11 +0530

    Java 12 String API Test

---
+++ core-java-12/src/test/java/com/baeldung/StringAPITest.java
@@ -0,0 +1,43 @@
+package com.baeldung;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+public class StringAPITest {
+
+    @Test
+    public void whenPositiveArgument_thenReturnIndentedString() {
+        String multilineStr = "This is\na multiline\nstring.";
+        String outputStr = "   This is\n   a multiline\n   string.\n";
+
+        String postIndent = multilineStr.indent(3);
+
+        assertThat(outputStr, equalTo(postIndent));
+    }
+
+    @Test
+    public void whenNegativeArgument_thenReturnReducedIndentedString() {
+        String multilineStr = "   This is\n   a multiline\n   string.";
+        String outputStr = " This is\n a multiline\n string.\n";
+
+        String postIndent = multilineStr.indent(-2);
+
+        assertThat(outputStr, equalTo(postIndent));
+    }
+
+    @Test
+    public void whenTransformUsingLamda_thenReturnTransformedString() {
+        String result = "hello".transform(input -> input + " world!");
+
+        assertThat("hello world!", equalTo(result));
+    }
+
+    @Test
+    public void whenTransformUsingParseInt_thenReturnInt() {
+        int result = "42".transform(Integer::parseInt);
+
+        assertThat(42, equalTo(result));
+    }
+}
