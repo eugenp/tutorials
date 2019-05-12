b83362799dbda6d758ba7277f0c8c37883b3fcee
Philippe <phil@lighthouse.com.br>
2019-05-12 17:09:09 -0300

    [BAEL-2894] remove Lombok

--- apache-olingo/olingo4/src/main/java/org/baeldung/examples/olingo4/ODataHttpHandlerFactoryImpl.java
+++ apache-olingo/olingo4/src/main/java/org/baeldung/examples/olingo4/ODataHttpHandlerFactoryImpl.java
@@ -9,17 +9,13 @@ import org.apache.olingo.server.api.ODataHttpHandler;
 import org.apache.olingo.server.api.ServiceMetadata;
 import org.apache.olingo.server.api.processor.Processor;
 
-import lombok.Builder;
-
-@Builder
 public class ODataHttpHandlerFactoryImpl implements ODataHttpHandlerFactory {
-    
-    
+
     private final ODataFactory odataFactory;
     private final CsdlEdmProvider edmProvider;
     private final List<Processor> processors;
 
-    public ODataHttpHandlerFactoryImpl(ODataFactory odataFactory,CsdlEdmProvider edmProvider, List<Processor> processors) {
+    public ODataHttpHandlerFactoryImpl(ODataFactory odataFactory, CsdlEdmProvider edmProvider, List<Processor> processors) {
         this.odataFactory = odataFactory;
         this.edmProvider = edmProvider;
         this.processors = processors;
@@ -27,16 +23,42 @@ public class ODataHttpHandlerFactoryImpl implements ODataHttpHandlerFactory {
 
     @Override
     public ODataHttpHandler newInstance() {
-        
+
         OData odata = odataFactory.newInstance();
         ServiceMetadata metadata = odata.createServiceMetadata(edmProvider, Collections.emptyList());
         ODataHttpHandler handler = odata.createHandler(metadata);
-        
+
         // Register all available processors
         processors.forEach(p -> handler.register(p));
-        
-        
+
         return handler;
+    }
+
+    public static class ODataHttpHandlerFactoryImplBuilder {
+
+        private ODataFactory odataFactory;
+        private CsdlEdmProvider edmProvider;
+        private List<Processor> processors;
+
+        public ODataHttpHandlerFactoryImplBuilder odataFactory(ODataFactory odataFactory) {
+            this.odataFactory = odataFactory;
+            return this;
+        }
+
+        public ODataHttpHandlerFactoryImplBuilder edmProvider(CsdlEdmProvider edmProvider) {
+            this.edmProvider = edmProvider;
+            return this;
+        }
+
+        public ODataHttpHandlerFactoryImplBuilder processors(List<Processor> processors) {
+            this.processors = processors;
+            return this;
+        }
+
+        public ODataHttpHandlerFactoryImpl build() {
+            return new ODataHttpHandlerFactoryImpl(odataFactory, edmProvider, processors);
+        }
+
     }
 
 }
