7f275f9b38f25b0a5b80c7315363c4addd45e190
dupirefr <dupire.francois+pro@gmail.com>
2019-05-15 08:19:30 +0200

    [BAEL-2932] Using Predicate.not to negate a method reference

---
+++ core-java-modules/core-java-11/src/main/java/com/baeldung/predicate/not/FindPeople.java
@@ -0,0 +1,31 @@
+package com.baeldung.predicate.not;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class FindPeople {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+          new Person(1),
+          new Person(18),
+          new Person(2)
+        );
+
+        people.stream()
+          .filter(Person::isAdult)
+          .collect(Collectors.toList());
+
+        people.stream()
+          .filter(person -> !person.isAdult())
+          .collect(Collectors.toList());
+
+        people.stream()
+          .filter(Person::isNotAdult)
+          .collect(Collectors.toList());
+
+        people.stream()
+          .filter(Predicate.not(Person::isAdult))
+          .collect(Collectors.toList());
+    }
+}
