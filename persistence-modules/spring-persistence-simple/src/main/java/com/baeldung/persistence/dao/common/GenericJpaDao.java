cbac5e3e765a706c5739d9ea66d69b695d7301ab
Loredana <lore.crusoveanu@gmail.com>
2019-05-18 15:34:01 +0300

    BAEL-12937

--- persistence-modules/spring-persistence-simple/src/main/java/com/baeldung/persistence/dao/common/GenericJpaDao.java
+++ persistence-modules/spring-persistence-simple/src/main/java/com/baeldung/persistence/dao/common/GenericJpaDao.java
@@ -2,10 +2,11 @@ package com.baeldung.persistence.dao.common;
 
 import java.io.Serializable;
 
-import org.baeldung.persistence.dao.AbstractJpaDAO;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Repository;
+
+import com.baeldung.jpa.dao.AbstractJpaDAO;
 
 @Repository
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
