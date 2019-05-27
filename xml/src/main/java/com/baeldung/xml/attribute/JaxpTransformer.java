295f56b9d5c9cdbf09e890bbec81a96707c9bca3
Juan Moreno <earth001@gmail.com>
2019-05-27 13:07:26 -0300

    UPDATE Added JAXP polish version

--- xml/src/main/java/com/baeldung/xml/attribute/JaxpTransformer.java
+++ xml/src/main/java/com/baeldung/xml/attribute/JaxpTransformer.java
@@ -1,66 +1,57 @@
 package com.baeldung.xml.attribute;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
-public class JaxpTransformer {
-    public String modifyAttribute(Document input) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
-        String xml = this.getClass()
-            .getClassLoader()
-            .getResource("xml/attribute.xml")
-            .getPath();
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
 
-        // 1- Build the doc from the XML file
-        Document doc = DocumentBuilderFactory.newInstance()
-            .newDocumentBuilder()
-            .parse(new InputSource(xml));
+public class JaxpTransformer {
+
+    public String setCustomerAttribute(String input, String oldValue, String newValue) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
+        // 1- Build the doc from the XML String
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        docFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        Document doc = docFactory.newDocumentBuilder()
+            .parse(new InputSource(new StringReader(input)));
 
         // 2- Locate the node(s) with xpath
         XPath xpath = XPathFactory.newInstance()
             .newXPath();
-        NodeList nodes = (NodeList) xpath.evaluate("//*[contains(@customer, 'true')]", doc, XPathConstants.NODESET);
+        NodeList nodes = (NodeList) xpath.evaluate(String.format("//*[contains(@customer, '%s')]", oldValue), doc, XPathConstants.NODESET);
 
         // 3- Make the change on the selected nodes
-        for (int idx = 0; idx < nodes.getLength(); idx++) {
-            Node value = nodes.item(idx)
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Node value = nodes.item(i)
                 .getAttributes()
                 .getNamedItem("customer");
             String val = value.getNodeValue();
-            value.setNodeValue(val.replaceAll("true", "false"));
+            value.setNodeValue(val.replaceAll(oldValue, newValue));
         }
 
         // 4- Save the result to a new XML doc
         TransformerFactory factory = TransformerFactory.newInstance();
         factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        Transformer xformer=factory.newTransformer();
-        xformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        Transformer transformer = factory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
         Writer output = new StringWriter();
-        xformer.transform(new DOMSource(doc), new StreamResult(output));
-        
+        transformer.transform(new DOMSource(doc), new StreamResult(output));
+
         return output.toString();
     }
 }
--- xml/src/test/java/com/baeldung/xml/attribute/JaxpProcessorUnitTest.java
+++ xml/src/test/java/com/baeldung/xml/attribute/JaxpProcessorUnitTest.java
@@ -1,65 +1,40 @@
 package com.baeldung.xml.attribute;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
-import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
+import static org.xmlunit.assertj.XmlAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.xpath.XPathExpressionException;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JaxpProcessorUnitTest {
 
     @Test
-    void dunno() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
-        String xml = this.getClass()
-            .getClassLoader()
-            .getResource("xml/attribute.xml")
-            .getPath();
+    public void givenXml_whenChangeAttributeWithJaxp_getXmlUpdated() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException, URISyntaxException {
+        String input = getContents("/xml/attribute.xml");
+        String outputExpected = getContents("/xml/attribute_expected.xml");
+        JaxpTransformer transformer = new JaxpTransformer();
 
-        // 1- Build the doc from the XML file
-        Document doc = DocumentBuilderFactory.newInstance()
-            .newDocumentBuilder()
-            .parse(new InputSource(xml));
+        String output = transformer.setCustomerAttribute(input,"true","false");
 
-        System.out.println(doc.getTextContent());
-        // 2- Locate the node(s) with xpath
-        XPath xpath = XPathFactory.newInstance()
-            .newXPath();
-        NodeList nodes = (NodeList) xpath.evaluate("//*[contains(@customer, 'true')]", doc, XPathConstants.NODESET);
+        assertThat(output).and(outputExpected).areIdentical();
+    }
 
-        // 3- Make the change on the selected nodes
-        for (int idx = 0; idx < nodes.getLength(); idx++) {
-            Node value = nodes.item(idx)
-                .getAttributes()
-                .getNamedItem("customer");
-            String val = value.getNodeValue();
-            value.setNodeValue(val.replaceAll("true", "false"));
-        }
-
-        // 4- Save the result to a new XML doc
-        Transformer xformer = TransformerFactory.newInstance()
-            .newTransformer();
-        Writer output = new StringWriter();
-        xformer.transform(new DOMSource(doc), new StreamResult(output));
-
-        System.out.println(output.toString());
+    String getContents(String fileName) throws IOException, URISyntaxException {
+        Path path = Paths.get(getClass()
+          .getResource(fileName)
+          .toURI());
+        return new String(Files.readAllBytes(path), StandardCharsets.UTF_8);
     }
 
 }
