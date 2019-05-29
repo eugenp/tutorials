9e470968e9f8b332e17493f1c6cb786bcf3e778c
amit2103 <amit.pandey2103@gmail.com>
2019-05-30 01:33:20 +0530

    [BAEL-14841] - Fixed tests in hibernate-mapping, hibernate5, java-jpa modules

--- persistence-modules/hibernate5/src/main/java/com/baeldung/hibernate/joincolumn/OfficeAddress.java
+++ persistence-modules/hibernate5/src/main/java/com/baeldung/hibernate/joincolumn/OfficeAddress.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Address {
+public class OfficeAddress {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
