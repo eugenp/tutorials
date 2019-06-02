9b591fada08d26b07c130398874f035f4b881cdf
balaji13us <Balaji>
2019-06-02 20:59:29 +0530

    Hexagonal Architecture implementation

---
+++ architecture-algorithms/hexagonal-architecture/src/main/java/org/java/spring/hexagonalarchitecture/inside/CommunicationPort.java
@@ -0,0 +1,6 @@
+package org.java.spring.hexagonalarchitecture.inside;
+
+public interface CommunicationPort {
+    
+    public boolean sendMessage(String channelType, String message, Person receiver);
+}
