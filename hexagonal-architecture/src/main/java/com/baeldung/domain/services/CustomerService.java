044701c84694ee5fb00d947890078b5c2eb5f18d
carloscano93 <carlcan.93@gmail.com>
2019-06-08 11:14:23 +0200

    refactor in one module

--- hexagonal-architecture/src/main/java/com/baeldung/domain/services/CustomerService.java
+++ hexagonal-architecture/src/main/java/com/baeldung/domain/services/CustomerService.java
@@ -3,8 +3,6 @@ package com.baeldung.domain.services;
 import com.baeldung.domain.entity.Customer;
 import com.baeldung.domain.port.CustomerPort;
 
-import java.util.Random;
-
 public class CustomerService {
     private CustomerPort customerPort;
 
@@ -12,14 +10,7 @@ public class CustomerService {
         this.customerPort = customerPort;
     }
 
-    public Customer getRandomCustomer() {
-        return customerPort.getCustomerById(newRandomFrom1To10());
-    }
-
-    private long newRandomFrom1To10() {
-        return new Random()
-                .ints(1, 10)
-                .findFirst()
-                .getAsInt();
+    public Customer getRandomCustomer(long id) {
+        return customerPort.getCustomerById(id);
     }
 }
