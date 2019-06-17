91419ec2ca05d00fbffb56e1ead7d0652712dd20
Gama Ogi Prayogo <ogi.prayogo@gmail.com>
2019-06-17 17:56:27 +0800

    BAEL-2076 use Classloader.getResourceAsStream

--- core-java-modules/core-resource-not-found/src/main/java/com/baeldung/core/resourcenotfound/Main.java
+++ core-java-modules/core-resource-not-found/src/main/java/com/baeldung/core/resourcenotfound/Main.java
@@ -2,6 +2,7 @@ package com.baeldung.core.resourcenotfound;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.net.URL;
 import java.util.Scanner;
 
@@ -9,27 +10,16 @@ public class Main {
 
     public static void main(String[] args) {
         String filePath = "code.csv";
-        try (Scanner scanner = new Scanner(getFile(filePath))) {
-            int count = 0;
-            while (scanner.hasNext()) {
-                System.out.println(scanner.nextLine());
-                count++;
-            }
-            System.out.printf("Reading %s lines from %s%n", count, filePath);
-        } catch (FileNotFoundException e) {
-            System.err.printf("File not found, %s", e.getMessage());
+        Scanner scanner = new Scanner(getInputStream(filePath));
+        int count = 0;
+        while (scanner.hasNext()) {
+            System.out.println(scanner.nextLine());
+            count++;
         }
+        System.out.printf("Reading %s lines from %s%n", count, filePath);
     }
 
-    private static File getFile(String filePath) throws FileNotFoundException {
-        URL resource = Main.class.getClassLoader().getResource(filePath);
-        if (resource == null) {
-            throw new FileNotFoundException();
-        }
-        return new File(resource.getFile());
-    }
-
-    private static File getFileIncorrectly(String filePath) {
-        return new File(filePath);
+    private static InputStream getInputStream(String filePath) {
+        return Main.class.getClassLoader().getResourceAsStream(filePath);
     }
 }
