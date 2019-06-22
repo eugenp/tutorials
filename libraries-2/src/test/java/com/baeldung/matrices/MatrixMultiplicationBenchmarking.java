bc8fbaf579054b968b9708bf8fa44b9e65c32cff
dupirefr <dupire.francois+pro@gmail.com>
2019-06-22 11:40:10 +0200

    Ã[BAEL-2996] Used qualified class for benchmarking

--- libraries-2/src/test/java/com/baeldung/matrices/MatrixMultiplicationBenchmarking.java
+++ libraries-2/src/test/java/com/baeldung/matrices/MatrixMultiplicationBenchmarking.java
@@ -1,11 +1,9 @@
 package com.baeldung.matrices;
 
-import org.openjdk.jmh.Main;
-
 public class MatrixMultiplicationBenchmarking {
 
     public static void main(String[] args) throws Exception {
-        Main.main(args);
+        org.openjdk.jmh.Main.main(args);
     }
 
 }
