d29ce18e7d8f422a25dc3ded23e6b4ccb3f0aa33
Crossmonk <evegaluna@gmail.com>
2019-06-21 20:53:20 -0400

    Adding printf and println examples for Print vs Logger article.

---
+++ core-java-system-println/src/com/baeldung/example/Printf.java
@@ -0,0 +1,10 @@
+package com.baeldung.example;
+
+public class Printf {
+    
+    public static void main(String[] args) {
+        System.out.printf("Hello, this is a number %d", 10);
+        System.out.printf("Now a String %s", "Baeldung");
+        System.out.printf("Even characters %c", 161);
+    }
+}
---
+++ core-java-system-println/src/com/baeldung/example/Println.java
@@ -0,0 +1,8 @@
+package com.baeldung.example;
+
+public class Println {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
