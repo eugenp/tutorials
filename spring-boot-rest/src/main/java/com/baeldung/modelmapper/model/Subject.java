f9e99a56d25c8ee55e3802aaa7e6e921879abd2f
amit2103 <amit.pandey2103@gmail.com>
2019-06-23 12:01:27 +0530

    [BAEL-14274] - Fixed article code for https://www.baeldung.com/rest-api-pagination-in-spring

---
+++ spring-boot-rest/src/main/java/com/baeldung/modelmapper/model/Subject.java
@@ -0,0 +1,37 @@
+package com.baeldung.modelmapper.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Subject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Subject() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
