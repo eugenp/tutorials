4026c6ae7a2677cd985a6f1f7bc11a9e0d1ca350
Lukasz Rys <>
2019-06-23 13:14:13 +0200

    [ BAEL-2515] : Rename tests

--- spring-boot-testing/src/test/java/com/baeldung/overrideproperties/ProfilePropertySourceResolverIntegrationTest.java
+++ spring-boot-testing/src/test/java/com/baeldung/overrideproperties/ProfilePropertySourceResolverIntegrationTest.java
@@ -5,22 +5,24 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestResourcePropertySourceResolverUnitTest {
+@ActiveProfiles("test")
+public class ProfilePropertySourceResolverIntegrationTest {
 
     @Autowired private PropertySourceResolver propertySourceResolver;
 
     @Test
-    public void shouldTestResourceFile_overridePropertyValues() {
+    public void shouldProfiledProperty_overridePropertyValues() {
         final String firstProperty = propertySourceResolver.getFirstProperty();
         final String secondProperty = propertySourceResolver.getSecondProperty();
 
-        assertEquals("file", firstProperty);
+        assertEquals("profile", firstProperty);
         assertEquals("file", secondProperty);
     }
 
--- spring-boot-testing/src/test/java/com/baeldung/overrideproperties/SpringBootPropertySourceResolverIntegrationTest.java
+++ spring-boot-testing/src/test/java/com/baeldung/overrideproperties/SpringBootPropertySourceResolverIntegrationTest.java
@@ -1,29 +1,26 @@
 package com.baeldung.overrideproperties;
 
 import com.baeldung.overrideproperties.resolver.PropertySourceResolver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertEquals;
-
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
-public class ProfilePropertySourceResolverUnitTest {
+@SpringBootTest(properties = { "example.firstProperty=annotation" })
+public class SpringBootPropertySourceResolverIntegrationTest {
 
     @Autowired private PropertySourceResolver propertySourceResolver;
 
     @Test
-    public void shouldProfiledProperty_overridePropertyValues() {
+    public void shouldSpringBootTestAnnotation_overridePropertyValues() {
         final String firstProperty = propertySourceResolver.getFirstProperty();
         final String secondProperty = propertySourceResolver.getSecondProperty();
 
-        assertEquals("profile", firstProperty);
-        assertEquals("file", secondProperty);
+        Assert.assertEquals("annotation", firstProperty);
+        Assert.assertEquals("file", secondProperty);
     }
 
 }
