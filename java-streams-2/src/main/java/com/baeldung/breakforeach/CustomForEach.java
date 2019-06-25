c08e20e129434d6f192221cdf2dbe7c00bbadc00
catalin-burcea <burcea.iulian.catalin@gmail.com>
2019-06-25 11:35:48 +0300

    added break mechanisms for Stream.forEach

---
+++ java-streams-2/src/main/java/com/baeldung/breakforeach/CustomForEach.java
@@ -0,0 +1,32 @@
+package com.baeldung.breakforeach;
+
+import java.util.Spliterator;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+public class CustomForEach {
+
+    public static class Breaker {
+        private boolean shouldBreak = false;
+
+        public void stop() {
+            shouldBreak = true;
+        }
+
+        boolean get() {
+            return shouldBreak;
+        }
+    }
+
+    public static <T> void forEach(Stream<T> stream, BiConsumer<T, Breaker> consumer) {
+        Spliterator<T> spliterator = stream.spliterator();
+        boolean hadNext = true;
+        Breaker breaker = new Breaker();
+
+        while (hadNext && !breaker.get()) {
+            hadNext = spliterator.tryAdvance(elem -> {
+                consumer.accept(elem, breaker);
+            });
+        }
+    }
+}
