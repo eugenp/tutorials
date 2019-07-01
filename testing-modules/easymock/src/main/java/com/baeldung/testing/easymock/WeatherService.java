1c913d4f118d667633e0de8c6ff376724dfe37a8
priyesh.mashelkar <mashelkar.priyesh@gmail.com>
2019-07-01 23:43:48 +0100

    Updated code as per review comments

--- testing-modules/easymock/src/main/java/com/baeldung/testing/easymock/WeatherService.java
+++ testing-modules/easymock/src/main/java/com/baeldung/testing/easymock/WeatherService.java
@@ -1,7 +1,5 @@
 package com.baeldung.testing.easymock;
 
-import java.util.List;
-
 public interface WeatherService {
-    void populateTemperature(List<Location> location);
+    void populateTemperature(Location location) throws ServiceUnavailableException;
 }
