b68808cf90669297130a66d9de56a7e7cafe885f
Juan Moreno <earth001@gmail.com>
2019-07-02 01:14:24 -0300

    FEAT Added complete JAXP Sample

--- xml/src/main/java/com/baeldung/xml/attribute/JaxpTransformer.java
+++ xml/src/main/java/com/baeldung/xml/attribute/JaxpTransformer.java
@@ -7,10 +7,8 @@ import java.io.Writer;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.stream.XMLInputFactory;
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
@@ -22,45 +20,39 @@ import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class JaxpTransformer {
-    public String modifyAttribute(Document input) throws SAXException, IOException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
-        String xml = this.getClass()
-            .getClassLoader()
-            .getResource("xml/attribute.xml")
-            .getPath();
 
+    private Document input;
+
+    public JaxpTransformer(String resourcePath) throws SAXException, IOException, ParserConfigurationException {
         // 1- Build the doc from the XML file
-        Document doc = DocumentBuilderFactory.newInstance()
-            .newDocumentBuilder()
-            .parse(new InputSource(xml));
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+        input = factory.newDocumentBuilder()
+            .parse(resourcePath);
+    }
 
+    public String modifyAttribute(String attribute, String oldValue, String newValue) throws XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
         // 2- Locate the node(s) with xpath
         XPath xpath = XPathFactory.newInstance()
             .newXPath();
-        NodeList nodes = (NodeList) xpath.evaluate("//*[contains(@customer, 'true')]", doc, XPathConstants.NODESET);
-
+        NodeList nodes = (NodeList) xpath.evaluate(String.format("//*[contains(@%s, '%s')]", attribute, oldValue), input, XPathConstants.NODESET);
         // 3- Make the change on the selected nodes
-        for (int idx = 0; idx < nodes.getLength(); idx++) {
-            Node value = nodes.item(idx)
-                .getAttributes()
-                .getNamedItem("customer");
-            String val = value.getNodeValue();
-            value.setNodeValue(val.replaceAll("true", "false"));
+        for (int i = 0; i < nodes.getLength(); i++) {
+            Element value = (Element) nodes.item(i);
+            value.setAttribute(attribute, newValue);
         }
-
         // 4- Save the result to a new XML doc
         TransformerFactory factory = TransformerFactory.newInstance();
         factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        Transformer xformer=factory.newTransformer();
+        Transformer xformer = factory.newTransformer();
         xformer.setOutputProperty(OutputKeys.INDENT, "yes");
         Writer output = new StringWriter();
-        xformer.transform(new DOMSource(doc), new StreamResult(output));
-        
+        xformer.transform(new DOMSource(input), new StreamResult(output));
         return output.toString();
     }
 }
--- xml/src/test/java/com/baeldung/xml/attribute/JaxpProcessorUnitTest.java
+++ xml/src/test/java/com/baeldung/xml/attribute/JaxpProcessorUnitTest.java
@@ -1,65 +1,31 @@
 package com.baeldung.xml.attribute;
 
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+import static org.xmlunit.assertj.XmlAssert.assertThat;
 
-import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.IOException;
+
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.TransformerFactoryConfigurationError;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.xpath.XPath;
-import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
-import javax.xml.xpath.XPathFactory;
 
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 public class JaxpProcessorUnitTest {
 
     @Test
-    void dunno() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
-        String xml = this.getClass()
-            .getClassLoader()
-            .getResource("xml/attribute.xml")
-            .getPath();
+    public void givenXmlWithAttributes_whenModifyAttribute_thenGetXmlUpdated() throws IOException, SAXException, ParserConfigurationException, XPathExpressionException, TransformerFactoryConfigurationError, TransformerException {
+        String path = getClass().getResource("/xml/attribute.xml")
+            .toString();
+        JaxpTransformer transformer = new JaxpTransformer(path);
+        String attribute = "customer";
+        String oldValue = "true";
+        String newValue = "false";
 
-        // 1- Build the doc from the XML file
-        Document doc = DocumentBuilderFactory.newInstance()
-            .newDocumentBuilder()
-            .parse(new InputSource(xml));
+        String result = transformer.modifyAttribute(attribute, oldValue, newValue);
 
-        System.out.println(doc.getTextContent());
-        // 2- Locate the node(s) with xpath
-        XPath xpath = XPathFactory.newInstance()
-            .newXPath();
-        NodeList nodes = (NodeList) xpath.evaluate("//*[contains(@customer, 'true')]", doc, XPathConstants.NODESET);
-
-        // 3- Make the change on the selected nodes
-        for (int idx = 0; idx < nodes.getLength(); idx++) {
-            Node value = nodes.item(idx)
-                .getAttributes()
-                .getNamedItem("customer");
-            String val = value.getNodeValue();
-            value.setNodeValue(val.replaceAll("true", "false"));
-        }
-
-        // 4- Save the result to a new XML doc
-        Transformer xformer = TransformerFactory.newInstance()
-            .newTransformer();
-        Writer output = new StringWriter();
-        xformer.transform(new DOMSource(doc), new StreamResult(output));
-
-        System.out.println(output.toString());
+        assertThat(result).hasXPath("//*[contains(@customer, 'false')]");
     }
 
 }
