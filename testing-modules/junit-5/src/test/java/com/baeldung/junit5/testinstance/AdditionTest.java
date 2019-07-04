3151f1c938db70bda61837d5dcc1514c8ab9e364
Vivek Balasubramaniam <vivekrmd@gmail.com>
2019-07-04 21:51:14 +0530

    BAEL-3040: @TestInstance annotation in JUnit 5 (renamed the tests)

--- testing-modules/junit-5/src/test/java/com/baeldung/junit5/testinstance/AdditionTest.java
+++ testing-modules/junit-5/src/test/java/com/baeldung/junit5/testinstance/AdditionTest.java
@@ -9,13 +9,13 @@ public class AdditionTest {
     private int sum = 1;
 
     @Test
-    void addTwoToSum() {
+    void addingTwoToSumReturnsThree() {
         sum += 2;
         assertEquals(3, sum);
     }
 
     @Test
-    void addThreeToSum() {
+    void addThreeToSumReturnsFour() {
         sum += 3;
         assertEquals(4, sum);
     }
