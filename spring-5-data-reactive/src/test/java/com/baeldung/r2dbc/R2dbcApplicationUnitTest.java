9a646be2c83b1b6d8192b9ca8df6f7e94ab56c41
Rodrigo Graciano <graciano@Graciano.local>
2019-07-05 18:07:05 -0400

    Method name incorrect

--- spring-5-data-reactive/src/test/java/com/baeldung/r2dbc/R2dbcApplicationUnitTest.java
+++ spring-5-data-reactive/src/test/java/com/baeldung/r2dbc/R2dbcApplicationUnitTest.java
@@ -56,7 +56,7 @@ public class R2dbcApplicationUnitTest {
     }
 
     @Test
-    public void whenInsertSix_then6AreExpected() {
+    public void whenInsert6_then6AreExpected() {
 
         insertPlayers();
 
