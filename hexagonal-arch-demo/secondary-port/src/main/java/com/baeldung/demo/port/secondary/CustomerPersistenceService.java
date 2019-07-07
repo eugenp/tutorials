b2d7f374ac191968afb20e98efdc3c922aec0335
p2740056 <tigPwd4030>
2019-07-07 12:38:50 -0500

    Demo Project with 6 modules to explain hexagonal architecture

--- hexagonal-arch-demo/secondary-port/src/main/java/com/baeldung/demo/port/secondary/CustomerPersistenceService.java
+++ hexagonal-arch-demo/secondary-port/src/main/java/com/baeldung/demo/port/secondary/CustomerPersistenceService.java
@@ -3,7 +3,7 @@ package com.baeldung.demo.port.secondary;
 import com.baeldung.demo.model.Customer;
 
 public interface CustomerPersistenceService {
-    void createCustomer(Customer customer);
+    Customer createCustomer(Customer customer);
     Customer retrieveCustomer(String customerId);
-    boolean updateCustomer(Customer customer);
+    Customer updateCustomer(Customer customer);
 }
