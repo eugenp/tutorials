001c9f45d9b16ed22da12d3406d791d8fed44622
npecanac <npecanac@bulbtech.com>
2019-07-09 14:22:55 +0200

    data class enhanced

--- jee-kotlin/src/main/kotlin/com/baeldung/jeekotlin/entity/Student.kt
+++ jee-kotlin/src/main/kotlin/com/baeldung/jeekotlin/entity/Student.kt
@@ -4,13 +4,19 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import javax.persistence.*
 
 @Entity
-data class Student (var firstName: String, var lastName: String) {
+data class Student constructor (
 
     @SequenceGenerator(name = "student_id_seq", sequenceName = "student_id_seq", allocationSize = 1)
     @GeneratedValue(generator = "student_id_seq", strategy = GenerationType.SEQUENCE)
     @Id
-    var id: Long? = null
+    var id: Long?,
 
-    constructor() : this("", "")
+    var firstName: String,
 
+    var lastName: String
+
+) {
+    constructor() : this(null, "", "")
+
+    constructor(firstName: String, lastName: String) : this(null, firstName, lastName)
 }
