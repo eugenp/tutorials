f43db12fcbdac8b0c47cede959066f7de53ea46d
vatsalgosar <vatsalgosar@gmail.com>
2019-07-16 01:55:08 +0530

    BAEL-2728 | vatsalgosar@gmail.com - Adding spring-boot-configuration project to spring-boot-bootstrap module

--- spring-boot-bootstrap/spring-boot-configuration/src/main/java/com/baeldung/Application.java
+++ spring-boot-bootstrap/spring-boot-configuration/src/main/java/com/baeldung/Application.java
@@ -1,20 +1,25 @@
 package com.baeldung;
 
+import com.baeldung.service.PersonService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.baeldung.*"})
 @SpringBootConfiguration
-@Import({SwaggerConfig.class})
 public class Application {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public PersonService personService() {
+		return new PersonService();
 	}
 }
 
--- spring-boot-bootstrap/spring-boot-configuration/src/main/java/com/baeldung/service/PersonService.java
+++ spring-boot-bootstrap/spring-boot-configuration/src/main/java/com/baeldung/service/PersonService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+//@Service
 public class PersonService {
 
     @Autowired
