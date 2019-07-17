b33ca2f0c4f8010cd9095b26f2c02a902ec489cb
rahulgul8 <rahulgul8@gmail.com>
2019-07-17 14:06:29 +0530

    Looking for constants instead of 200 and 404.

--- core-java-modules/core-java-io/src/test/java/com/baeldung/url/UrlCheckerUnitTest.java
+++ core-java-modules/core-java-io/src/test/java/com/baeldung/url/UrlCheckerUnitTest.java
@@ -3,7 +3,7 @@ package com.baeldung.url;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
-
+import java.net.HttpURLConnection;
 import org.junit.Test;
 
 public class UrlCheckerUnitTest {
@@ -12,28 +12,28 @@ public class UrlCheckerUnitTest {
     public void givenValidUrl_WhenUsingHEAD_ThenReturn200() throws IOException {
         UrlChecker tester = new UrlChecker();
         int responseCode = tester.getResponseCodeForURLUsingHead("http://www.example.com");
-        assertEquals(200, responseCode);
+        assertEquals(HttpURLConnection.HTTP_OK, responseCode);
     }
     
     @Test
     public void givenInvalidIUrl_WhenUsingHEAD_ThenReturn404() throws IOException {
         UrlChecker tester = new UrlChecker();
         int responseCode = tester.getResponseCodeForURLUsingHead("http://www.example.com/unkownurl");
-        assertEquals(404, responseCode);
+        assertEquals(HttpURLConnection.HTTP_NOT_FOUND, responseCode);
     }
     
     @Test
     public void givenValidUrl_WhenUsingGET_ThenReturn200() throws IOException {
         UrlChecker tester = new UrlChecker();
         int responseCode = tester.getResponseCodeForURL("http://www.example.com");
-        assertEquals(200, responseCode);
+        assertEquals(HttpURLConnection.HTTP_OK, responseCode);
     }
     
     @Test
     public void givenInvalidIUrl_WhenUsingGET_ThenReturn404() throws IOException {
         UrlChecker tester = new UrlChecker();
         int responseCode = tester.getResponseCodeForURL("http://www.example.com/unkownurl");
-        assertEquals(404, responseCode);
+        assertEquals(HttpURLConnection.HTTP_NOT_FOUND, responseCode);
     }
     
 }
