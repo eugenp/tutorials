da86b78fa5a54f850c5c24bac02cc830a99ca820
amit2103 <amit.pandey2103@gmail.com>
2019-07-19 00:10:31 +0530

    [BAEL-11603] - Moved MessageController to a child package and removed ExcelPOIHelper bean definition from TestConfig

--- spring-mvc-java/src/test/java/com/baeldung/htmlunit/TestConfig.java
+++ spring-mvc-java/src/test/java/com/baeldung/htmlunit/TestConfig.java
@@ -13,11 +13,9 @@ import org.thymeleaf.spring4.SpringTemplateEngine;
 import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
-import com.baeldung.excel.ExcelPOIHelper;
-
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = { "com.baeldung.web.controller" })
+@ComponentScan(basePackages = { "com.baeldung.web.controller.message" })
 public class TestConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -45,10 +43,5 @@ public class TestConfig implements WebMvcConfigurer {
         final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         templateEngine.setTemplateResolver(templateResolver());
         return templateEngine;
-    }
-    
-    @Bean
-    public ExcelPOIHelper excelPOIHelper() {
-        return new ExcelPOIHelper();
     }
 }
