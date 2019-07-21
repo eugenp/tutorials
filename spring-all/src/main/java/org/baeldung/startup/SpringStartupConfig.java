dbbd5254db078a37be57c52dc3a39e2c2a8905fa
amit2103 <amit.pandey2103@gmail.com>
2019-07-21 21:51:01 +0530

    [BAEL-16045] - Fixed article code for https://www.baeldung.com/running-setup-logic-on-startup-in-spring

--- spring-all/src/main/java/org/baeldung/startup/SpringStartupConfig.java
+++ spring-all/src/main/java/org/baeldung/startup/SpringStartupConfig.java
@@ -1,9 +1,15 @@
 package org.baeldung.startup;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("org.baeldung.startup")
 public class SpringStartupConfig {
+    
+    @Bean(initMethod="init")
+    public InitMethodExampleBean exBean() {
+        return new InitMethodExampleBean();
+    }
 }
