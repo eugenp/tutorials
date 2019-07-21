76e14a7418eea141066edc6c38b3889174072321
devender.kumar <devender.kumar@auto1.com>
2019-07-21 19:45:23 +0200

    Added BigDecimal validation classes

---
+++ javaxval/src/test/java/org/baeldung/javabeanconstraints/bigdecimal/InvoiceUnitTest.java
@@ -0,0 +1,48 @@
+package org.baeldung.javabeanconstraints.bigdecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class InvoiceUnitTest {
+	
+	private static Validator validator;
+
+    @BeforeClass
+    public static void setupValidatorInstance() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+    
+    @Test
+    public void whenPriceIntegerDigitLessThanThree_thenShouldGiveConstraintViolations() {
+    	Invoice invoice = new Invoice(new BigDecimal(10.20), "Book purchased");
+        Set<ConstraintViolation<Invoice>> violations = validator.validate(invoice);
+        assertThat(violations.size()).isEqualTo(1);
+        violations.forEach(action->System.out.println(action.getMessage()));
+    }
+    
+    @Test
+    public void whenPriceIsZero_thenShouldGiveConstraintViolations() {
+    	Invoice invoice = new Invoice(new BigDecimal(000.00), "Book purchased");
+        Set<ConstraintViolation<Invoice>> violations = validator.validate(invoice);
+        assertThat(violations.size()).isEqualTo(1);
+        violations.forEach(action->System.out.println(action.getMessage()));
+    }
+    
+    @Test
+    public void whenPriceIsGreaterThanZero_thenShouldNotGiveConstraintViolations() {
+    	Invoice invoice = new Invoice(new BigDecimal(100.50), "Book purchased");
+        Set<ConstraintViolation<Invoice>> violations = validator.validate(invoice);
+        assertThat(violations.size()).isEqualTo(0);
+        violations.forEach(action->System.out.println(action.getMessage()));
+    }
+
+}
