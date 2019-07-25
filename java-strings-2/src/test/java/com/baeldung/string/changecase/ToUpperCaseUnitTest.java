ffada1e2a749cf5903d28921e4697760f2b9569f
Justin Albano <albano.justin@gmail.com>
2019-07-25 07:52:18 -0400

    BAEL-3153: Updated code format to match standard Baeldung format
    Converted tabs into spaces

--- java-strings-2/src/test/java/com/baeldung/string/changecase/ToUpperCaseUnitTest.java
+++ java-strings-2/src/test/java/com/baeldung/string/changecase/ToUpperCaseUnitTest.java
@@ -8,22 +8,22 @@ import org.junit.Test;
 
 public class ToUpperCaseUnitTest {
 
-	private static final Locale TURKISH = new Locale("tr");
-	private String name = "John Doe";
-	private String foreignUppercase = "\u0069";
+    private static final Locale TURKISH = new Locale("tr");
+    private String name = "John Doe";
+    private String foreignUppercase = "\u0069";
 
-	@Test
-	public void givenMixedCaseString_WhenToUpperCase_ThenResultIsUpperCase() {
-		assertEquals("JOHN DOE", name.toUpperCase());
-	}
-	
-	@Test
-	public void givenForeignString_WhenToUpperCaseWithoutLocale_ThenResultIsUpperCase() {
-		assertEquals("\u0049", foreignUppercase.toUpperCase());
-	}
-	
-	@Test
-	public void givenForeignString_WhenToUpperCaseWithLocale_ThenResultIsUpperCase() {
-		assertEquals("\u0130", foreignUppercase.toUpperCase(TURKISH));
-	}
+    @Test
+    public void givenMixedCaseString_WhenToUpperCase_ThenResultIsUpperCase() {
+        assertEquals("JOHN DOE", name.toUpperCase());
+    }
+
+    @Test
+    public void givenForeignString_WhenToUpperCaseWithoutLocale_ThenResultIsUpperCase() {
+        assertEquals("\u0049", foreignUppercase.toUpperCase());
+    }
+
+    @Test
+    public void givenForeignString_WhenToUpperCaseWithLocale_ThenResultIsUpperCase() {
+        assertEquals("\u0130", foreignUppercase.toUpperCase(TURKISH));
+    }
 }
