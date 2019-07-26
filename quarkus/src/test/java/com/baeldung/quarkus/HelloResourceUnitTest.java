8358d5b2ecc5e9fe6889bfc593f31c8387d0028d
Rodrigo Graciano <graciano@Graciano.local>
2019-07-25 22:14:58 -0400

    Rename unit test to follow naming standards

--- quarkus/src/test/java/com/baeldung/quarkus/HelloResourceUnitTest.java
+++ quarkus/src/test/java/com/baeldung/quarkus/HelloResourceUnitTest.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class HelloResourceTest {
+public class HelloResourceUnitTest {
 
     @Test
     public void testHelloEndpoint() {
