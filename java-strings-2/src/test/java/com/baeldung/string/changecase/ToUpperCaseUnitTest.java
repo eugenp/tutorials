e67462412e8deb5b0a23ed162ce82c5b8ecaf097
Justin Albano <albanoj2@users.noreply.github.com>
2019-07-26 23:33:34 -0400

    BAEL-3153: Corrected name of foreign lower case variable in tests

--- java-strings-2/src/test/java/com/baeldung/string/changecase/ToUpperCaseUnitTest.java
+++ java-strings-2/src/test/java/com/baeldung/string/changecase/ToUpperCaseUnitTest.java
@@ -10,7 +10,7 @@ public class ToUpperCaseUnitTest {
 
     private static final Locale TURKISH = new Locale("tr");
     private String name = "John Doe";
-    private String foreignUppercase = "\u0069";
+    private String foreignLowercase = "\u0069";
 
     @Test
     public void givenMixedCaseString_WhenToUpperCase_ThenResultIsUpperCase() {
@@ -19,11 +19,11 @@ public class ToUpperCaseUnitTest {
 
     @Test
     public void givenForeignString_WhenToUpperCaseWithoutLocale_ThenResultIsUpperCase() {
-        assertEquals("\u0049", foreignUppercase.toUpperCase());
+        assertEquals("\u0049", foreignLowercase.toUpperCase());
     }
 
     @Test
     public void givenForeignString_WhenToUpperCaseWithLocale_ThenResultIsUpperCase() {
-        assertEquals("\u0130", foreignUppercase.toUpperCase(TURKISH));
+        assertEquals("\u0130", foreignLowercase.toUpperCase(TURKISH));
     }
 }
