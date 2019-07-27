8d62e17b8e05f07f14b9fdf3f9c0c86063d13046
maryarm <45322329+maryarm@users.noreply.github.com>
2019-07-27 16:50:52 +0430

    #BAEL-2888- Reloading Properties Files in Spring (#7392)
    Code samples for BAEL-2888

---
+++ spring-boot-properties/src/test/java/com/baeldung/properties/beans/ValueRefreshConfigBean.java
@@ -0,0 +1,13 @@
+package com.baeldung.properties.beans;
+
+public class ValueRefreshConfigBean {
+    private String color;
+
+    public ValueRefreshConfigBean(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
