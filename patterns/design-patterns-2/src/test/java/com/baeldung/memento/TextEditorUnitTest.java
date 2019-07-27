dcc8d524412637e00cb7ee23d9fe8af70c2e471f
dupirefr <dupire.francois+pro@gmail.com>
2019-07-27 12:01:37 +0200

    [BAEL-3090] Renamed test to match PMD

--- patterns/design-patterns-2/src/test/java/com/baeldung/memento/TextEditorUnitTest.java
+++ patterns/design-patterns-2/src/test/java/com/baeldung/memento/TextEditorUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class TextEditorTest {
+class TextEditorUnitTest {
 
     @Test
     void givenTextEditor_whenAddTextSaveAddMoreAndUndo_thenSavecStateRestored() {
