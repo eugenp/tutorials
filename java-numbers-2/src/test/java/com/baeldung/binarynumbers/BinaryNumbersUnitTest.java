6d130980404ac424e20f6a55921aaf6113bf5777
sumit.sg34 <sgajbhar@qualys.com>
2019-07-28 20:41:05 +0530

    added unit test cases for built-in java functions

--- java-numbers-2/src/test/java/com/baeldung/binarynumbers/BinaryNumbersUnitTest.java
+++ java-numbers-2/src/test/java/com/baeldung/binarynumbers/BinaryNumbersUnitTest.java
@@ -9,19 +9,31 @@ public class BinaryNumbersUnitTest {
     private BinaryNumbers binaryNumbers = new BinaryNumbers();
 
     @Test
-    public void Given_DecimalNumber_Then_ReturnBinayNumber() {
+    public void Given_DecimalNumber_Then_ReturnBinaryNumber() {
         assertEquals(Integer.valueOf(1000), binaryNumbers.convertDecimalToBinary(8));
         assertEquals(Integer.valueOf(10100), binaryNumbers.convertDecimalToBinary(20));
     }
 
     @Test
-    public void Given_BinayNumber_Then_ReturnDecimalNumber() {
+    public void Given_DecimalNumber_Then_ConvertToBinaryNumber() {
+        assertEquals("1000", Integer.toBinaryString(8));
+        assertEquals("10100", Integer.toBinaryString(20));
+    }
+
+    @Test
+    public void Given_BinaryNumber_Then_ConvertToDecimalNumber() {
+        assertEquals(8, Integer.parseInt("1000", 2));
+        assertEquals(20, Integer.parseInt("10100", 2));
+    }
+
+    @Test
+    public void Given_BinaryNumber_Then_ReturnDecimalNumber() {
         assertEquals(Integer.valueOf(8), binaryNumbers.convertBinaryToDecimal(1000));
         assertEquals(Integer.valueOf(20), binaryNumbers.convertBinaryToDecimal(10100));
     }
 
     @Test
-    public void Given_TwoBinayNumber_Then_ReturnAddition() {
+    public void Given_TwoBinaryNumber_Then_ReturnAddition() {
         // adding 4 and 10
         assertEquals(Integer.valueOf(1110), binaryNumbers.addBinaryNumber(100, 1010));
 
@@ -30,7 +42,7 @@ public class BinaryNumbersUnitTest {
     }
 
     @Test
-    public void Given_TwoBinayNumber_Then_ReturnSubtraction() {
+    public void Given_TwoBinaryNumber_Then_ReturnSubtraction() {
         // subtracting 16 from 25
         assertEquals(Integer.valueOf(1001), binaryNumbers.substractBinaryNumber(11001, 10000));
 
