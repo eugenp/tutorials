5aecdeb0211c9dc44e3e5138d4a30bf837d19204
Ger Roza <rozagerardo@gmail.com>
2019-07-29 02:56:21 -0300

    [BAEL-3066] Spring Security: Exploring JDBC Authentication (#7441)
    * created multi-module project from spring-security-mvc-boot
    
    * Added JDBC Authentication application to spring-security-mvc-boot-default
    
    * Added JDBC Authentication application to spring-security-mvc-boot-mysql
    
    * Added JDBC Authentication application to spring-security-mvc-boot-postgre
    
    * adding new modules to parent spring-security-mvc-boot module, reformatting sql scripts, and added form fields to H2 LiveTest

--- spring-security-mvc-boot/spring-security-mvc-boot-default/src/main/java/org/baeldung/custom/Application.java
+++ spring-security-mvc-boot/spring-security-mvc-boot-default/src/main/java/org/baeldung/custom/Application.java
@@ -1,12 +1,8 @@
 package org.baeldung.custom;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
--- spring-security-mvc-boot/spring-security-mvc-boot-default/src/main/java/org/baeldung/multiplelogin/MultipleLoginApplication.java
+++ spring-security-mvc-boot/spring-security-mvc-boot-default/src/main/java/org/baeldung/multiplelogin/MultipleLoginApplication.java
@@ -1,11 +1,8 @@
 package org.baeldung.multiplelogin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("org.baeldung.multiplelogin")
