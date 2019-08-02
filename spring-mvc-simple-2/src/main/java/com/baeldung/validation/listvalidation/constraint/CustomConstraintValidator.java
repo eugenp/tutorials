a5f06318502750a5593578761cdbd7f3176554a3
Pallavi Priyadarshani <50460471+pallavi-priyadarshani@users.noreply.github.com>
2019-08-02 16:42:36 +0530

    Update CustomConstraintValidator.java

--- spring-mvc-simple-2/src/main/java/com/baeldung/validation/listvalidation/constraint/CustomConstraintValidator.java
+++ spring-mvc-simple-2/src/main/java/com/baeldung/validation/listvalidation/constraint/CustomConstraintValidator.java
@@ -1,17 +1,32 @@
 package com.baeldung.validation.listvalidation.constraint;
 
+import java.util.List;
+import java.util.regex.Pattern;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
-public class CustomConstraintValidator implements ConstraintValidator<CustomConstraint, String> {
+import com.baeldung.validation.listvalidation.model.Movie;
+
+public class CustomConstraintValidator implements ConstraintValidator<CustomConstraint, List<Movie>> {
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null || value.isEmpty()) {
-            return false;
-        } else {
-            return true;
+    public boolean isValid(List<Movie> values, ConstraintValidatorContext context) {
+        boolean isValid = true;
+        if (values == null || values.isEmpty()) {
+            isValid = false;
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate("Movie list cannot be empty.")
+                .addConstraintViolation();
         }
+        String regex = "[^A-Za-z0-9].*";
+        for (Movie movie : values) {
+            if (Pattern.matches(regex, movie.getName())) {
+                isValid = false;
+                break;
+            }
+        }
+        return isValid;
     }
 
 }
