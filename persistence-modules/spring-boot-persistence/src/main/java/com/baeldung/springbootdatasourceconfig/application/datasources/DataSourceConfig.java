eea20faf08f24e201f33bc2667dfe0cf17207bc6
amit2103 <amit.pandey2103@gmail.com>
2019-08-03 23:24:53 +0530

    [BAEL-16045] - Check Article Code Matches GitHub for https://www.baeldung.com/spring-boot-configure-data-source-programmatic article

--- persistence-modules/spring-boot-persistence/src/main/java/com/baeldung/springbootdatasourceconfig/application/datasources/DataSourceConfig.java
+++ persistence-modules/spring-boot-persistence/src/main/java/com/baeldung/springbootdatasourceconfig/application/datasources/DataSourceConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DataSourceBean {
+public class DataSourceConfig {
     
     @Bean
     public DataSource getDataSource() {
