51dd87a4e759e7ef7c6198e0b90a7cda58cadb52
Johannes Lichtenberger <lichtenberger.johannes@gmail.com>
2019-08-04 10:19:04 +0200

    Change to integration tests

--- sirix/core-api-tutorial/src/main/java/io/sirix/tutorial/json/JsonDocumentCreator.java
+++ sirix/core-api-tutorial/src/main/java/io/sirix/tutorial/json/JsonDocumentCreator.java
@@ -1,26 +1,5 @@
 package io.sirix.tutorial.json;
 
-/**
- * Copyright (c) 2011, University of Konstanz, Distributed Systems Group All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification, are permitted
- * provided that the following conditions are met: * Redistributions of source code must retain the
- * above copyright notice, this list of conditions and the following disclaimer. * Redistributions
- * in binary form must reproduce the above copyright notice, this list of conditions and the
- * following disclaimer in the documentation and/or other materials provided with the distribution.
- * * Neither the name of the University of Konstanz nor the names of its contributors may be used to
- * endorse or promote products derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
- * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
- * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
- * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 import org.sirix.access.trx.node.json.objectvalue.ArrayValue;
 import org.sirix.access.trx.node.json.objectvalue.BooleanValue;
 import org.sirix.access.trx.node.json.objectvalue.ObjectValue;
@@ -94,5 +73,21 @@ public final class JsonDocumentCreator {
            .insertArrayAsRightSibling();
 
         wtx.moveToDocumentRoot();
+    }
+
+    public static void createVersioned(final JsonNodeTrx wtx) {
+        // Create sample document.
+        JsonDocumentCreator.create(wtx);
+        wtx.commit();
+
+        // Add changes and commit a second revision.
+        wtx.moveToDocumentRoot().trx().moveToFirstChild();
+        wtx.insertObjectRecordAsFirstChild("revision2", new StringValue("yes"));
+        wtx.commit();
+
+        // Add changes and commit a third revision.
+        wtx.moveToDocumentRoot().trx().moveToFirstChild().trx().moveToFirstChild();
+        wtx.insertObjectRecordAsRightSibling("revision3", new StringValue("yes"));
+        wtx.commit();
     }
 }
--- sirix/core-api-tutorial/src/main/java/io/sirix/tutorial/xml/MyXmlDiffObserver.java
+++ sirix/core-api-tutorial/src/main/java/io/sirix/tutorial/xml/MyXmlDiffObserver.java
@@ -1,82 +1,33 @@
 package io.sirix.tutorial.xml;
 
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-import org.sirix.api.xml.XmlNodeReadOnlyTrx;
 import org.sirix.diff.DiffDepth;
 import org.sirix.diff.DiffFactory.DiffType;
 import org.sirix.diff.DiffObserver;
+import org.sirix.diff.DiffTuple;
 
 public class MyXmlDiffObserver implements DiffObserver {
 
-    private final XmlNodeReadOnlyTrx firstRtx;
+    private final List<DiffTuple> diffTuples;
 
-    private final XmlNodeReadOnlyTrx secondRtx;
-
-    MyXmlDiffObserver(final XmlNodeReadOnlyTrx newRtx, final XmlNodeReadOnlyTrx oldRtx) {
-        this.firstRtx = Objects.requireNonNull(newRtx, "The first read only trx must not be null");
-        this.secondRtx = Objects.requireNonNull(oldRtx, "The second read only trx must not be null");
+    MyXmlDiffObserver() {
+        diffTuples = new ArrayList<>();
     }
 
     @Override
     public void diffListener(DiffType diffType, long newNodeKey, long oldNodeKey, DiffDepth depth) {
-        switch (diffType) {
-        case INSERTED:
-            System.out.println("DiffType: " + diffType);
-
-            firstRtx.moveTo(newNodeKey);
-            printNode(firstRtx);
-
-            break;
-        case DELETED:
-            System.out.println("DiffType: " + diffType);
-
-            secondRtx.moveTo(oldNodeKey);
-            printNode(secondRtx);
-
-            break;
-        case UPDATED:
-            System.out.println("DiffType: " + diffType);
-
-            secondRtx.moveTo(oldNodeKey);
-            System.out.print("Old: ");
-            printNode(secondRtx);
-
-            firstRtx.moveTo(newNodeKey);
-            System.out.print("New: ");
-            printNode(firstRtx);
-
-            break;
-        default:
-        }
-    }
-
-    private void printNode(final XmlNodeReadOnlyTrx rtx) {
-        switch (rtx.getKind()) {
-        case ELEMENT:
-            System.out.println(rtx.getName());
-            break;
-        case TEXT:
-            System.out.println(rtx.getValue());
-            break;
-        case COMMENT:
-            System.out.println(rtx.getValue());
-            break;
-        case PROCESSING_INSTRUCTION:
-            System.out.println(rtx.getName());
-            break;
-        case ATTRIBUTE:
-            System.out.println(rtx.getName());
-            break;
-        case NAMESPACE:
-            System.out.println(rtx.getName());
-            break;
-        default:
-            throw new IllegalStateException("Kind not known.");
-        }
+        if (diffType == DiffType.INSERTED || diffType == DiffType.DELETED || diffType == DiffType.UPDATED)
+            diffTuples.add(new DiffTuple(diffType, newNodeKey, oldNodeKey, depth));
     }
 
     @Override
     public void diffDone() {
+    }
+
+    public List<DiffTuple> getDiffTuples() {
+        return Collections.unmodifiableList(diffTuples);
     }
 }
