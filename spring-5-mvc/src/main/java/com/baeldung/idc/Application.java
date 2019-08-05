4b7d77eb1dfe70694031c54d0be3892e4dfc89c1
Andrew Shcherbakov <veontomo@gmail.com>
2019-08-05 19:57:12 +0200

    Get rid of custom.properties

--- spring-5-mvc/src/main/java/com/baeldung/idc/Application.java
+++ spring-5-mvc/src/main/java/com/baeldung/idc/Application.java
@@ -1,24 +1,13 @@
 package com.baeldung.idc;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.baeldung.idc")
-@PropertySource(value = { "classpath:/custom.properties" }, ignoreResourceNotFound = true)
 public class Application {
 
     public static void main(String[] args) {
-        final SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
-        final Map<String, Object> map = new HashMap<>();
-        map.put("spring.config.name", "custom.properties");
-        builder.properties(map).run(args);
-
+        SpringApplication.run(Application.class, args);
     }
 
 }
