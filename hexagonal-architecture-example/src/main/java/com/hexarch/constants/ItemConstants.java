8c3ddabd76abf515608aeb659e21ccd82d13447d
dilipsundarraj <dilip.bandlasundarraj@gmail.com>
2019-08-06 19:55:09 -0500

    removed the unwanted repo

--- hexagonal-architecture-example/src/main/java/com/hexarch/constants/ItemConstants.java
+++
@@ -1,9 +0,0 @@
-package com.hexarch.constants;
-
-public class ItemConstants {
-
-    public static final String ITEM_URL = "/v1/item";
-    public static final String SINGLE_ITEM = ITEM_URL+"/{id}";
-    public static final String ALL_ITEMS = "/v1/allitems";
-    public static final String ERROR_MESSAGE = "Please pass valid values for the following fields :" ;
-}
--- hexagonal-architecture-example/src/main/java/com/hexarch/jpa/ItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hexarch.jpa;
-
-import com.hexarch.domain.Item;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ItemRepository extends CrudRepository<Item, Integer> {
-}
