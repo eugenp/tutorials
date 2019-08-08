4277c2ecf6016597419c9c5bfa68b433eaa6a97f
Alex Tighe <alex.tighe@gmail.com>
2019-08-07 23:15:55 -0400

    Reformatting try-with-resources

--- core-java-modules/core-java-io2/src/main/java/com/baeldung/resource/MyResourceLoader.java
+++ core-java-modules/core-java-io2/src/main/java/com/baeldung/resource/MyResourceLoader.java
@@ -11,7 +11,8 @@ public class MyResourceLoader {
 
     private void loadFileWithReader() throws IOException {
 
-        try (FileReader fileReader = new FileReader("src/main/resources/input.txt"); BufferedReader reader = new BufferedReader(fileReader)) {
+        try (FileReader fileReader = new FileReader("src/main/resources/input.txt");
+             BufferedReader reader = new BufferedReader(fileReader)) {
             String contents = reader.lines()
                 .collect(Collectors.joining(System.lineSeparator()));
             System.out.println(contents);
@@ -21,7 +22,8 @@ public class MyResourceLoader {
 
     private void loadFileAsResource() throws IOException {
 
-        try (InputStream inputStream = getClass().getResourceAsStream("/input.txt"); BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+        try (InputStream inputStream = getClass().getResourceAsStream("/input.txt");
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
             String contents = reader.lines()
                 .collect(Collectors.joining(System.lineSeparator()));
             System.out.println(contents);
