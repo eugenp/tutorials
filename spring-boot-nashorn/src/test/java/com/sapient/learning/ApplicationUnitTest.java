2bb9dcfd6f51aa9b62e5f9f01bdc2b31a8674e19
Kumar Chandrakant <kchandrakant@gmail.com>
2019-08-09 14:58:24 +0530

    Adding source code for article tracked under BAEL-3172.

--- spring-boot-nashorn/src/test/java/com/sapient/learning/ApplicationUnitTest.java
+++ spring-boot-nashorn/src/test/java/com/sapient/learning/ApplicationUnitTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ApplicationTests {
+public class ApplicationUnitTest {
 
 	@Test
 	public void contextLoads() {
