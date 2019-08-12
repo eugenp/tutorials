8c70206e2add53367bbc8e710e10c591e8aa4f52
Gerardo Roza <rozagerardo@gmail.com>
2019-08-12 16:45:59 -0300

    Redone Spring-Security-Exploring-JDBC-Authentication code, now witout using submodules

--- spring-security-mvc-boot/src/main/java/org/baeldung/custom/Application.java
+++ spring-security-mvc-boot/src/main/java/org/baeldung/custom/Application.java
@@ -1,14 +1,12 @@
 package org.baeldung.custom;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-defaults.properties")
 public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- spring-security-mvc-boot/src/main/java/org/baeldung/jdbcauthentication/h2/H2JdbcAuthenticationApplication.java
+++ spring-security-mvc-boot/src/main/java/org/baeldung/jdbcauthentication/h2/H2JdbcAuthenticationApplication.java
@@ -1,14 +1,17 @@
-package org.baeldung.multipleauthproviders;
+package org.baeldung.jdbcauthentication.h2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 @PropertySource("classpath:application-defaults.properties")
-// @ImportResource({ "classpath*:spring-security-multiple-auth-providers.xml" })
-public class MultipleAuthProvidersApplication {
+public class H2JdbcAuthenticationApplication {
+
     public static void main(String[] args) {
-        SpringApplication.run(MultipleAuthProvidersApplication.class, args);
+        SpringApplication.run(H2JdbcAuthenticationApplication.class, args);
     }
+
 }
