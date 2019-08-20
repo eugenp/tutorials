05fe7fa7952e025ca398168688f02e17c6563e11
Deb <debadatta10pati@gmail.com>
2019-08-20 22:40:15 +0530

    Json2 project

---
+++ Json2/src/main/java/com/baeldung/brtojsonobject/BRtoJSONObject.java
@@ -0,0 +1,53 @@
+package com.baeldung.jsontobufferedreader;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+
+import java.net.URL;
+import java.util.stream.Collectors;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONObject;
+
+public class BRtoJSONObject {
+public static void main(String args[])
+{
+
+try {
+
+URL url = new URL("https://baeldung.com/api/getUser");
+HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream(), "utf-8"));
+
+// Java 7
+StringBuilder response = new StringBuilder();
+String responseLine = null;
+while ((responseLine = br.readLine()) != null) {
+response.append(responseLine.trim());
+}
+br.close();
+JSONObject json = new JSONObject(response.toString());
+
+String name = json.getString("name");
+
+//Java 8
+String line = br.lines().collect(Collectors.joining());
+JSONObject json2 = new JSONObject(line); 
+
+// Using IOUtils
+
+String responseNew= IOUtils.toString(br);
+
+JSONObject json3 = new JSONObject(responseNew.toString());
+
+}
+catch(Exception e)
+{
+e.printStackTrace();    
+}
+
+}
+
+
+}
