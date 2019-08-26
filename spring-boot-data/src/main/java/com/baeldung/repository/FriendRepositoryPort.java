a347ef6ba0461a6e0610d266656407452ccdacdb
yandajun <yandajun@vipkid.com.cn>
2019-08-26 11:21:51 +0800

    delete all newly added classes

--- spring-boot-data/src/main/java/com/baeldung/repository/FriendRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.repository;
-
-import com.baeldung.domain.Friend;
-
-import java.util.List;
-
-public interface FriendRepositoryPort {
-    List<Friend> list(Long id);
-}
--- spring-boot-data/src/main/java/com/baeldung/service/FriendServicePort.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.service;
-
-import com.baeldung.domain.Friend;
-
-import java.util.List;
-
-public interface FriendServicePort {
-    List<Friend> list(Long id);
-}
