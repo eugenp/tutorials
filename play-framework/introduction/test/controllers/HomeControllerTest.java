e4bc369a9aec1a2b25f901a109eda3a4fc4d39c7
alfred.samanga@gmail.com <alfred.samanga@gmail.com>
2019-08-28 22:19:00 +0200

    Implementation of Introduction to Play Framework in Java

---
+++ play-framework/introduction/test/controllers/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package controllers;
+
+import org.junit.Test;
+import play.Application;
+import play.inject.guice.GuiceApplicationBuilder;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.test.WithApplication;
+
+import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.route;
+
+public class HomeControllerTest extends WithApplication {
+
+    @Override
+    protected Application provideApplication() {
+        return new GuiceApplicationBuilder().build();
+    }
+
+    @Test
+    public void testIndex() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/");
+
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+    }
+
+}
