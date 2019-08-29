be9cb3055a2004c793e1d344a19a66a851c63ace
alfred.samanga@gmail.com <alfred.samanga@gmail.com>
2019-08-29 21:55:55 +0200

    Finished implementing student-api

--- play-framework/student-api/app/model/Student.java
+++ play-framework/student-api/app/model/Student.java
@@ -1,8 +1,6 @@
 package model;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
