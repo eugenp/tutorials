f4c226849cc7a6e5c797aa82d042ae18f4b7d0a1
alfred.samanga@gmail.com <alfred.samanga@gmail.com>
2019-08-29 12:16:04 +0200

    Implementation of the tutorial for Introduction to Play Framework.

--- play-framework/introduction/test/controllers/HomeControllerTest.java
+++ play-framework/introduction/test/controllers/HomeControllerTest.java
@@ -6,9 +6,14 @@ import play.inject.guice.GuiceApplicationBuilder;
 import play.mvc.Http;
 import play.mvc.Result;
 import play.test.WithApplication;
+import play.twirl.api.Html;
+
+import java.util.Optional;
+import java.util.concurrent.CompletionStage;
 
 import static org.junit.Assert.assertEquals;
-import static play.mvc.Http.Status.OK;
+import static org.junit.Assert.assertTrue;
+import static play.mvc.Http.Status.*;
 import static play.test.Helpers.GET;
 import static play.test.Helpers.route;
 
@@ -29,4 +34,66 @@ public class HomeControllerTest extends WithApplication {
         assertEquals(OK, result.status());
     }
 
+    @Test
+    public void testApplyHtml() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/baeldung/html");
+
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+    }
+
+    @Test
+    public void testBadRequestPage() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/baeldung/bad-req");
+
+        Result result = route(app, request);
+        assertEquals(BAD_REQUEST, result.status());
+    }
+
+    @Test
+    public void testNotFoundPage() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/baeldung/not-found");
+
+        Result result = route(app, request);
+        assertEquals(NOT_FOUND, result.status());
+    }
+
+    @Test
+    public void testCustomContentType() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/baeldung/custom-content-type");
+
+        Result result = route(app, request);
+        assertTrue(result.contentType().isPresent());
+        assertEquals("text/html", result.contentType().get());
+    }
+
+    @Test
+    public void testAsyncOperation() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/baeldung/async");
+
+        Result result = route(app, request);
+        assertEquals(OK, result.status());
+    }
+
+    @Test
+    public void testSetHeaders() {
+        Http.RequestBuilder request = new Http.RequestBuilder()
+                .method(GET)
+                .uri("/baeldung/headers");
+
+        Result result = route(app, request);
+        final Optional<String> headerOptional = result.header("Header-Key");
+        assertTrue(headerOptional.isPresent());
+        assertEquals("Some value", headerOptional.get());
+    }
 }
