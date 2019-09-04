02e8039e2c2bd57f65320fa8b874d1f605b16536
Gerardo Roza <rozagerardo@gmail.com>
2019-09-04 19:26:49 -0300

    fixed Spring Context Test in spring-data-rest-querydsl module

--- spring-data-rest-querydsl/src/test/java/com/baeldung/SpringContextIntegrationTest.java
+++ spring-data-rest-querydsl/src/test/java/com/baeldung/SpringContextIntegrationTest.java
@@ -1,4 +1,4 @@
-package org.baeldung;
+package com.baeldung;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +7,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.baeldung.Application;
 
+/**
+ * Note: In the IDE, remember to generate query type classes before running the Integration Test (e.g. in Eclipse right-click on the project > Run As > Maven generate sources)
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class SpringContextLiveTest {
+public class SpringContextIntegrationTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
