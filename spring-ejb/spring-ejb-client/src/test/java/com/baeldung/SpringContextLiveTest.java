bf8e736ed929c337ade92dc81e71a568951a055d
Gerardo Roza <rozagerardo@gmail.com>
2019-09-05 15:15:09 -0300

    fixed spring-ejb/spring-ejb-client Context tests, removed ContextIntegrationTest and added a note to the ContextLiveTest. Plus, redefined spring-ejb-remote url in pom, it was not working properly'

--- spring-ejb/spring-ejb-client/src/test/java/com/baeldung/SpringContextLiveTest.java
+++ spring-ejb/spring-ejb-client/src/test/java/com/baeldung/SpringContextLiveTest.java
@@ -1,4 +1,4 @@
-package org.baeldung;
+package com.baeldung;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +7,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.baeldung.springejbclient.SpringEjbClientApplication;
 
+/**
+ * This Live Test requires:
+ * * run the `spring-ejb-remote` module with the following command: `mvn clean package cargo:run -Pwildfly-standalone`
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringEjbClientApplication.class)
-public class SpringContextIntegrationTest {
+public class SpringContextLiveTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
