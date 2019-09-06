2d7cc4030cb03e04dff34ad3194b770472404812
Gerardo Roza <rozagerardo@gmail.com>
2019-09-06 14:20:24 -0300

    Fixed Context test in spring-remoting/remoting-rmi\remoting-rmi-server, added notes for Live Test

--- spring-remoting/remoting-rmi/remoting-rmi-client/src/test/java/com/baeldung/SpringContextLiveTest.java
+++ spring-remoting/remoting-rmi/remoting-rmi-client/src/test/java/com/baeldung/SpringContextLiveTest.java
@@ -1,4 +1,4 @@
-package org.baeldung;
+package com.baeldung;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +7,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.baeldung.client.RmiClient;
 
+/**
+ * This Live Test requires:
+ * * the `com.baeldung:remoting-http-api:jar:1.0-SNAPSHOT` artifact accessible. For that we can run `mvn clean install` in the 'spring-remoting/remoting-http/remoting-http-api' module.
+ * * the 'spring-remoting\remoting-rmi\remoting-rmi-server' service running
+ *
+ */
 @SpringBootTest(classes = RmiClient.class)
 @RunWith(SpringRunner.class)
 public class SpringContextLiveTest {
