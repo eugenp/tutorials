b6183e931519750033d28297113aecf2f564216c
Ger Roza <rozagerardo@gmail.com>
2019-09-09 08:16:40 -0300

    [BAEL-9498] Fix failing context tests - part 2 (#7738)
    * fixed Spring Context Test in spring-data-rest-querydsl module
    
    * fixed spring-ejb/spring-ejb-client Context tests, removed ContextIntegrationTest and added a note to the ContextLiveTest. Plus, redefined spring-ejb-remote url in pom, it was not working properly'
    
    * Fixed Context test in spring-remoting/remoting-jms/remoting-jms-server, added notes for Live Test
    
    * Fixed Context test in spring-remoting/remoting-rmi\remoting-rmi-server, added notes for Live Test
    
    * Fixed Context test in spring-rest-query-language, added note for ContextTest and deleter ContextIntegrationTest
    
    * renamed SpringContextIntegrationTest to SpringContextTest, as per comment in BAEL-14304

--- spring-data-rest-querydsl/src/test/java/com/baeldung/SpringContextTest.java
+++ spring-data-rest-querydsl/src/test/java/com/baeldung/SpringContextTest.java
@@ -5,11 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.baeldung.spring.Application;
+import com.baeldung.Application;
 
+/**
+ * Note: In the IDE, remember to generate query type classes before running the Integration Test (e.g. in Eclipse right-click on the project > Run As > Maven generate sources)
+ *
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
-public class SpringContextIntegrationTest {
+public class SpringContextTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
--- spring-ejb/spring-ejb-client/src/test/java/com/baeldung/SpringContextLiveTest.java
+++ spring-ejb/spring-ejb-client/src/test/java/com/baeldung/SpringContextLiveTest.java
@@ -1,14 +1,19 @@
-package org.baeldung;
+package com.baeldung;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.baeldung.client.RmiClient;
+import com.baeldung.springejbclient.SpringEjbClientApplication;
 
-@SpringBootTest(classes = RmiClient.class)
+/**
+ * This Live Test requires:
+ * * run the `spring-ejb-remote` module with the following command: `mvn clean package cargo:run -Pwildfly-standalone`
+ *
+ */
 @RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringEjbClientApplication.class)
 public class SpringContextLiveTest {
 
     @Test
