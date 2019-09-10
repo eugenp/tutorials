c113e324eaf8904ead13a0514faff30d9fe91c01
eoral <erayoral2001@yahoo.com>
2019-09-11 00:12:46 +0300

    PendingJobWebServiceImpl

---
+++ patterns/hexagonal-architecture/src/main/java/com/baeldung/patterns/hexagonalarchitecture/changerequest2rewrite/PendingJobWebServiceImpl.java
@@ -0,0 +1,13 @@
+package com.baeldung.patterns.hexagonalarchitecture.changerequest2rewrite;
+
+import java.util.Random;
+ 
+public class PendingJobWebServiceImpl implements PendingJobService {
+    @Override
+    public int getPendingJobCount() {
+        System.out.println("sending HTTP request...");
+        // send HTTP request: GET http://pending-job-web-service.com/count
+        // We are returning a random integer for convenience purposes.
+        return new Random().nextInt(9999);
+    }
+}
