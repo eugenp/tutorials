4672d46a6e91c2ff05ef982027ec53818094ce94
eoral <erayoral2001@yahoo.com>
2019-09-10 23:33:06 +0300

    MaintenanceService

---
+++ patterns/hexagonal-architecture/src/main/java/com/baeldung/patterns/hexagonalarchitecture/initial/MaintenanceService.java
@@ -0,0 +1,27 @@
+package com.baeldung.patterns.hexagonalarchitecture.initial;
+
+import java.util.Random;
+ 
+public class MaintenanceService {
+ 
+    public static final int MAX_PENDING_JOB_COUNT = 1000;
+ 
+    public void checkPendingJobsAndNotifyIfNecessary() {
+        int pendingJobCount = getPendingJobCountFromDatabase();
+        if (pendingJobCount > MAX_PENDING_JOB_COUNT) {
+            sendEmailNotification();
+        }
+    }
+ 
+    public int getPendingJobCountFromDatabase() {
+        System.out.println("executing SQL query...");
+        // execute SQL query: SELECT COUNT(*) FROM PENDING_JOB
+        // We are returning a random integer for convenience purposes.
+        return new Random().nextInt(9999);
+    }
+ 
+    public void sendEmailNotification() {
+        System.out.println("sending email...");
+        // send an email notification to the database team
+    }
+}
