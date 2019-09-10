0844dfd625ecaf191c21271aa887ac0fd3c19260
NickTononi <nico.tononi@gmail.com>
2019-09-10 15:01:46 +0200

    fixed import issues

--- spring-rest-simple/src/main/java/com/baeldung/requestmapping/FooMappingExamplesController.java
+++ spring-rest-simple/src/main/java/com/baeldung/requestmapping/FooMappingExamplesController.java
@@ -1,6 +1,8 @@
-package com.baeldung.requestmapping;
+package test;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
