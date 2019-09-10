f7bb88d26d2bb1f66da55a79c97bf0031fb0da4e
eoral <erayoral2001@yahoo.com>
2019-09-10 23:44:40 +0300

    MaintenanceService

---
+++ patterns/hexagonal-architecture/src/main/java/com/baeldung/patterns/hexagonalarchitecture/initialrewrite/MaintenanceService.java
@@ -0,0 +1,23 @@
+package com.baeldung.patterns.hexagonalarchitecture.initialrewrite;
+
+public class MaintenanceService {
+ 
+    public static final int MAX_PENDING_JOB_COUNT = 1000;
+ 
+    private PendingJobService pendingJobService;
+    private NotificationService notificationService;
+ 
+    public MaintenanceService(
+            PendingJobService pendingJobService,
+            NotificationService notificationService) {
+        this.pendingJobService = pendingJobService;
+        this.notificationService = notificationService;
+    }
+ 
+    public void checkPendingJobsAndNotifyIfNecessary() {
+        int pendingJobCount = pendingJobService.getPendingJobCount();
+        if (pendingJobCount > MAX_PENDING_JOB_COUNT) {
+            notificationService.sendNotification();
+        }
+    }
+}
