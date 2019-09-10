e4fc01767e41f6f4b14a6f0470ac5e6acb2d4fed
eoral <erayoral2001@yahoo.com>
2019-09-11 00:05:20 +0300

    PendingJobDatabaseImpl

---
+++ patterns/hexagonal-architecture/src/main/java/com/baeldung/patterns/hexagonalarchitecture/changerequest1rewrite/PendingJobDatabaseImpl.java
@@ -0,0 +1,13 @@
+package com.baeldung.patterns.hexagonalarchitecture.changerequest1rewrite;
+
+import java.util.Random;
+ 
+public class PendingJobDatabaseImpl implements PendingJobService {
+    @Override
+    public int getPendingJobCount() {
+        System.out.println("executing SQL query...");
+        // execute SQL query: SELECT COUNT(*) FROM PENDING_JOB
+        // We are returning a random integer for convenience purposes.
+        return new Random().nextInt(9999);
+    }
+}
