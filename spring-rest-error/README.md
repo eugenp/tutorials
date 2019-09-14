e7f093cf04418b99877d34ac9e24180b1538aae5
Justin Albano <albano.justin@gmail.com>
2019-09-14 11:45:56 -0400

    BAEL-3225: Simplified error handling ControllerAdvice
    Also added new module to root-level pom.xml; renamed test cases to follow Baeldung naming convention, and added default README.md.

---
+++ spring-rest-error/README.md
@@ -0,0 +1,1 @@
+## Relavent Articles
--- spring-rest-error/src/main/java/com/baeldung/spring/rest/error/controller/ApiExceptionHandler.java
+++ spring-rest-error/src/main/java/com/baeldung/spring/rest/error/controller/ApiExceptionHandler.java
@@ -1,25 +1,18 @@
 package com.baeldung.spring.rest.error.controller;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.baeldung.spring.rest.error.exception.ApiException;
 import com.baeldung.spring.rest.error.exception.BookNotFoundException;
 
-@ControllerAdvice
-public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
-
-    @ExceptionHandler(BookNotFoundException.class)
-    protected ResponseEntity<Object> handleNotFound(BookNotFoundException ex, WebRequest request) {
-        return handleApiException(ex, HttpStatus.NOT_FOUND, request);
-    }
+@RestControllerAdvice
+public class ApiExceptionHandler {
     
-    private ResponseEntity<Object> handleApiException(ApiException ex, HttpStatus status, WebRequest request) {
-        return handleExceptionInternal(ex, ex.getJson(), new HttpHeaders(), status, request);
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleApiException(BookNotFoundException ex) {
+        return ex.toJson();
     }
 }
