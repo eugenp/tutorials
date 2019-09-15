8e0edb91c300e2e1be31b62219e740581d30cf38
Ezzeddine EL HAZATI <eelhazati@gmail.com>
2019-09-15 19:25:22 +0100

    final String & just one way to get public.

--- digital-signature/src/main/java/com/baeldung/security/Utils.java
+++ digital-signature/src/main/java/com/baeldung/security/Utils.java
@@ -1,16 +1,10 @@
 package com.baeldung.security;
 
-import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.cert.Certificate;
-import java.security.cert.CertificateFactory;
-import java.security.spec.X509EncodedKeySpec;
 
 public class Utils {
 
@@ -19,7 +13,7 @@ public class Utils {
     private static final String SENDER_KEYSTORE = "sender_keystore.p12";
     private static final String SENDER_ALIAS = "senderKeyPair";
 
-    public static String signingAlgorithm = "SHA256withRSA";
+    public static final String signingAlgorithm = "SHA256withRSA";
 
     private static final String RECEIVER_KEYSTORE = "receiver_keystore.p12";
     private static final String RECEIVER_ALIAS = "receiverKeyPair";
@@ -35,21 +29,5 @@ public class Utils {
         keyStore.load(new FileInputStream(RECEIVER_KEYSTORE), PASSWORD);
         Certificate certificate = keyStore.getCertificate(RECEIVER_ALIAS);
         return certificate.getPublicKey();
-    }
-
-    public static PublicKey getPublicKey2() throws Exception {
-        byte[] publicKeyEncoded = Files.readAllBytes(Paths.get("public_key"));
-        X509EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyEncoded);
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        PublicKey publicKey = keyFactory.generatePublic(publicKeySpec);
-        return publicKey;
-    }
-
-    public static PublicKey getPublicKey3() throws Exception {
-        BufferedInputStream bis = null;//...
-        CertificateFactory cf = CertificateFactory.getInstance("X.509");
-        Certificate certificate = cf.generateCertificate(bis);
-        PublicKey publicKey = certificate.getPublicKey();
-        return publicKey;
     }
 }
