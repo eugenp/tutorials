dec27638778958523a90ca285746f9f9823d89ce
alfred.samanga@gmail.com <alfred.samanga@gmail.com>
2019-09-18 17:16:51 +0200

    Implemented Flogger logging examples

---
+++ logging-modules/flogger/src/main/java/com/baeldung/flogger/FloggerExamples.java
@@ -0,0 +1,20 @@
+package com.baeldung.flogger;
+
+import com.google.common.flogger.FluentLogger;
+import com.google.common.flogger.LoggerConfig;
+
+import java.util.logging.Level;
+
+/**
+ * Created by alfred on 18 September 2019
+ */
+public class FloggerExamples {
+
+    private static final FluentLogger logger = FluentLogger.forEnclosingClass();
+
+    public static void main(String[] args) {
+        LoggerConfig.of(logger).setLevel(Level.FINE);
+        Exception exception = new Exception("This is a test exception.");
+        logger.atInfo().withCause(exception).log("Log message with: %s", "Alfred");
+    }
+}
