bb71b1ec070ff5ec2d2edc4960166074825235b5
at508 <apt80@icloud.com>
2019-09-23 07:04:22 -0400

    [BAEL-2998] - Updating unit test name to match convention

--- testing-modules/spring-testing/src/test/java/com/baeldung/dirtiescontext/DirtiesContextIntegrationTest.java
+++ testing-modules/spring-testing/src/test/java/com/baeldung/dirtiescontext/DirtiesContextIntegrationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = SpringDataRestApplication.class)
-class DirtiesContextTest {
+class DirtiesContextIntegrationTest {
 
     @Autowired
     protected UserCache userCache;
