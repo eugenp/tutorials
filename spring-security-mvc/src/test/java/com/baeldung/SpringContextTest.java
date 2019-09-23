ce315b230d20a0c85514d988d3c9ab1299e11a25
Dhawal Kapil <dhawalkapil@gmail.com>
2019-09-23 05:43:55 +0530

    BAEL-9496 Fix failing context tests
    - Fixes for SpringContextIntegrationTest in several projects

--- spring-security-mvc/src/test/java/com/baeldung/SpringContextTest.java
+++ spring-security-mvc/src/test/java/com/baeldung/SpringContextTest.java
@@ -1,14 +1,15 @@
-package org.baeldung;
+package com.baeldung;
 
-import org.baeldung.springvault.SpringVaultApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.baeldung.session.SpringSessionApplication;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringVaultApplication.class)
-public class SpringContextIntegrationTest {
+@SpringBootTest(classes = SpringSessionApplication.class)
+public class SpringContextTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
