af75e04e5085a5e9a3ad4ede025275ebec43c750
Dhawal Kapil <dhawalkapil@gmail.com>
2019-09-25 18:55:50 +0530

    BAEL-9496 Fix failing context tests (#7855)
    * BAEL-9496 Fix failing context tests
    
    - Fixes for SpringContextIntegrationTest in several projects
    
    * BAEL-9496 Fix failing context tests
    
    -Fixed ContextTests for spring-jms and further fixes spring-session-mongodb
    
    * BAEL-9496 Fix failing context tests
    
    -Fixed context tests from spring-rest-angular project

--- spring-rest-angular/src/main/resources/data.sql
+++ spring-rest-angular/src/main/resources/data.sql
@@ -1,10 +1,3 @@
-CREATE TABLE student (
-  id   INTEGER PRIMARY KEY,
-  name VARCHAR(30),
-  gender  VARCHAR(10),
-  age INTEGER
-);
-
 INSERT INTO student (id,name,gender,age) 
 VALUES (1,'Bryan', 'Male',20);
 INSERT INTO student (id,name,gender,age) 
--- spring-session/spring-session-mongodb/src/test/java/org/baeldung/SpringContextTest.java
+++ spring-session/spring-session-mongodb/src/test/java/org/baeldung/SpringContextTest.java
@@ -1,14 +1,14 @@
 package org.baeldung;
 
-import org.baeldung.spring.amqp.SpringWebfluxAmqpApplication;
+import com.baeldung.springsessionmongodb.SpringSessionMongoDBApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SpringWebfluxAmqpApplication.class)
-public class SpringContextIntegrationTest {
+@SpringBootTest(classes = SpringSessionMongoDBApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpringContextTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
