499bb0162e13098ea6c47c07b646395619f688a2
devender.kumar <devender.kumar@auto1.com>
2019-09-28 11:43:21 +0200

    Update code

--- spring-boot-di/src/main/java/com/baeldung/componentscan/filter/custom/ComponentScanCustomFilterApp.java
+++ spring-boot-di/src/main/java/com/baeldung/componentscan/filter/custom/ComponentScanCustomFilterApp.java
@@ -1,21 +1,13 @@
 package com.baeldung.componentscan.filter.custom;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
-import java.util.Arrays;
-
 @Configuration
 @ComponentScan(includeFilters = @ComponentScan.Filter(type = FilterType.CUSTOM, classes = ComponentScanCustomFilter.class))
 public class ComponentScanCustomFilterApp {
-    private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        applicationContext = new AnnotationConfigApplicationContext(ComponentScanCustomFilterApp.class);
-        Arrays.stream(applicationContext.getBeanDefinitionNames())
-                .forEach(System.out::println);
     }
 }
