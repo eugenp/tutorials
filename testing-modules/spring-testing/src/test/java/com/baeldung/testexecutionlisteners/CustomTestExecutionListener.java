7cfcc44f5b5a96c41b28851b84246633f3414011
priyesh.mashelkar <mashelkar.priyesh@gmail.com>
2019-10-01 01:00:19 +0100

    First draft

---
+++ testing-modules/spring-testing/src/test/java/com/baeldung/testexecutionlisteners/CustomTestExecutionListener.java
@@ -0,0 +1,30 @@
+package com.baeldung.testexecutionlisteners;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListener;
+
+public class CustomTestExecutionListener implements TestExecutionListener {
+    private static final Logger logger = LoggerFactory.getLogger(CustomTestExecutionListener.class);
+    
+    public void beforeTestClass(TestContext testContext) throws Exception {
+        logger.info("beforeTestClass : {}", testContext.getTestClass());
+    }; 
+    
+    public void prepareTestInstance(TestContext testContext) throws Exception {
+        logger.info("prepareTestInstance : {}", testContext.getTestClass());
+    }; 
+    
+    public void beforeTestMethod(TestContext testContext) throws Exception {
+        logger.info("beforeTestMethod : {}", testContext.getTestMethod());
+    }; 
+    
+    public void afterTestMethod(TestContext testContext) throws Exception {
+        logger.info("afterTestMethod : {}", testContext.getTestMethod());
+    }; 
+    
+    public void afterTestClass(TestContext testContext) throws Exception {
+        logger.info("afterTestClass : {}", testContext.getTestClass());
+    };
+}
