cd0d79d8f64396ab831dfcbc8ffe3cf061cf01e6
Anshul BANSAL <smartdiscover17@gmail.com>
2019-10-08 15:40:45 +0300

    BAEL-1871 - Spring Data Geode

---
+++ persistence-modules/spring-data-geode/src/main/java/com/baeldung/springdatageode/repo/AuthorRepository.java
@@ -0,0 +1,12 @@
+package com.baeldung.springdatageode.repo;
+
+import com.baeldung.springdatageode.domain.Author;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthorRepository extends CrudRepository<Author, Long> {
+
+    Optional<Author> findById(Long id);
+}
