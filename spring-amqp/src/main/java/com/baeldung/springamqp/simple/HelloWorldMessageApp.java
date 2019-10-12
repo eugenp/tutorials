4bef98e9b8070971a2898976fd0099345a520ac8
PK <pradhyu.shrestha@gmail.com>
2019-10-12 00:42:37 -0400

    retriable with last line of defense. good for rabbit template

--- spring-amqp/src/main/java/com/baeldung/springamqp/simple/HelloWorldMessageApp.java
+++ spring-amqp/src/main/java/com/baeldung/springamqp/simple/HelloWorldMessageApp.java
@@ -1,25 +1,25 @@
 package com.baeldung.springamqp.simple;
 
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.impl.AMQImpl;
+import org.aopalliance.intercept.MethodInterceptor;
 import org.springframework.amqp.core.AcknowledgeMode;
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.amqp.rabbit.retry.MessageRecoverer;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
 
-import java.io.IOException;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
@@ -59,24 +59,53 @@ public class HelloWorldMessageApp {
     // Number of messages in one transaction if transacted
     // if not transacted but AcknowledgeMode.AUTO no of message after which ack is sent back
     // by default the size in one so ack is sent for each message
-    int batchSize=25;
+    int batchSize = 1;
+
     // consumer
-    int consumerCount=2;
+    int consumerCount = 1;
     // max concurrent consumer
-    int maxConsumerCount=3;
+    int maxConsumerCount = consumerCount;
+    // total prefetchCount considering the concurrent consumers
+    int totalPrefetchCount = consumerCount * prefetchCount;
+
+    // kill -9
+    // jps -v | grep HelloWorldMessageApp | cut -d " " -f1 | xargs kill -9
+    // Process finished with exit code 137 (interrupted by signal 9: SIGKILL)
+
+    @Bean
+    public MethodInterceptor retryAdvice() {
+        FixedBackOffPolicy backOffPolicy = new FixedBackOffPolicy();
+        backOffPolicy.setBackOffPeriod(1000);
+        return RetryInterceptorBuilder.stateless()
+                .backOffPolicy(backOffPolicy)
+                .maxAttempts(300)
+                .recoverer(new MessageRecoverer() {
+                    @Override
+                    public void recover(Message message, Throwable cause) {
+                        System.out.println("last line of defense");
+                        System.out.println("Write to file " + message + " throwing exception " + cause.getMessage());
+                    }
+                })
+                .build();
+    }
+
+    ;
 
     @Bean
     @Primary
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory());
-        factory.setPrefetchCount(prefetchCount);
+        factory.setPrefetchCount(totalPrefetchCount);
         factory.setAcknowledgeMode(AcknowledgeMode.AUTO);
         factory.setConcurrentConsumers(consumerCount);
+
         // this is really important when the acknowledgemode is Auto
         // but it's depricated in 2.2 amqp for batch size
         factory.setTxSize(batchSize);
         factory.setMaxConcurrentConsumers(maxConsumerCount);
+        // may be we don't need the advice any more
+        factory.setAdviceChain(retryAdvice());
         return factory;
     }
 
@@ -87,17 +116,20 @@ public class HelloWorldMessageApp {
         return myQueue;
     }
 
-    @RabbitListener(queues = MY_QUEUE_NAME,containerFactory = "rabbitListenerContainerFactory")
-    public void listen(String in, Channel channel) throws InterruptedException, IOException {
+    @RabbitListener(
+            queues = MY_QUEUE_NAME,
+            containerFactory = "rabbitListenerContainerFactory")
+    public void listen(String in, Channel channel) throws InterruptedException {
         System.out.println("Message read from myQueue : " + in);
-        Thread.sleep(10);
-        System.out.println("Message processing done :" + in);
         int msgNo = Integer.parseInt(in.split("@", 2)[1]);
-        if (msgNo ==batchSize-1) {
-            System.out.println("Prefetch " + prefetchCount + ", batch="+batchSize+" all messages would be put back ");
-            //  Thread.sleep(1000000);
-            System.exit(2);
+        if (msgNo == 2) {
+            throw new RuntimeException("Injected runtime exception " + msgNo);
         }
+        if (msgNo == totalPrefetchCount - 1) {
+            System.out.println("Prefetch " + totalPrefetchCount + ", batch=" + batchSize + " all messages would be put back ");
+            //   System.exit(2);
+        }
+        System.out.println("Message processing done :" + in);
     }
 
 }
