3e4c964e6ac53f62f06d4c364b74ed517bc66f9d
Devender Kumar <47500074+kumar-devender@users.noreply.github.com>
2019-10-15 04:24:18 +0200

    modifieds example to creates less cognitive load for a reader (#8018)

--- spring-boot-di/src/test/java/com/baeldung/componentscan/filter/custom/ComponentScanCustomFilterAppIntegrationTest.java
+++ spring-boot-di/src/test/java/com/baeldung/componentscan/filter/custom/ComponentScanCustomFilterAppIntegrationTest.java
@@ -1,18 +1,18 @@
 package com.baeldung.componentscan.filter.custom;
 
-import static org.junit.Assert.assertThat;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ComponentScanCustomFilterApp.class)
@@ -22,9 +22,10 @@ public class ComponentScanCustomFilterAppIntegrationTest {
     public void whenCustomFilterIsUsed_thenComponentScanShouldRegisterBeanMatchingCustomFilter() {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ComponentScanCustomFilterApp.class);
         List<String> beans = Arrays.stream(applicationContext.getBeanDefinitionNames())
-            .filter(bean -> !bean.contains("org.springframework") && !bean.contains("componentScanCustomFilterApp"))
-            .collect(Collectors.toList());
+                .filter(bean -> !bean.contains("org.springframework") && !bean.contains("componentScanCustomFilterApp")
+                        && !bean.contains("componentScanCustomFilter"))
+                .collect(Collectors.toList());
         assertThat(beans.size(), equalTo(1));
-        assertThat(beans.get(0), equalTo("cat"));
+        assertThat(beans.get(0), equalTo("elephant"));
     }
 }
