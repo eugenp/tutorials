30c1f5e1925471d6295928aa03c4c0c8a29d83ae
sumit-bhawsar <bhawsar.sumit08@gmail.com>
2019-10-20 01:51:06 +0100

    BAEL-3335 example of reading request multiple times

---
+++ spring-core-2/src/main/java/org/baeldung/cachedrequest/ContentCachingFilter.java
@@ -0,0 +1,23 @@
+package org.baeldung.cachedrequest;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Order(value = Ordered.HIGHEST_PRECEDENCE)
+@Component
+public class ContentCachingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        CachedBodyHttpServletRequest cachedBodyHttpServletRequest = new CachedBodyHttpServletRequest(httpServletRequest);
+        filterChain.doFilter(cachedBodyHttpServletRequest, httpServletResponse);
+    }
+}
---
+++ spring-core-2/src/main/java/org/baeldung/cachedrequest/PrintRequestContentFilter.java
@@ -0,0 +1,27 @@
+package org.baeldung.cachedrequest;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Order(Ordered.LOWEST_PRECEDENCE)
+@Component
+public class PrintRequestContentFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        InputStream inputStream = httpServletRequest.getInputStream();
+        byte[] body = StreamUtils.copyToByteArray(inputStream);
+        System.out.println("In PrintRequestContentFilter." + " Request body is: " + new String(body));
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
+}
