1de7b016a87b375e4573661f880cfcd24265c3e2
Martin van Wingerden <martin@martinvw.nl>
2019-10-23 11:04:40 +0200

    [BAEL-3315] Added missing code snippets from the Java 8 Date/Time article
    Also:
    - formatted changed files with Eclipse profile
    - corrected failing test: givenTwoDatesInJava8_whenDifferentiating_thenWeGetSix

--- java-dates/src/test/java/com/baeldung/datetime/UseZonedDateTimeUnitTest.java
+++ java-dates/src/test/java/com/baeldung/datetime/UseZonedDateTimeUnitTest.java
@@ -7,19 +7,27 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class UseZonedDateTimeUnitTest {
 
-    UseZonedDateTime zonedDateTime = new UseZonedDateTime();
+    private UseZonedDateTime zonedDateTime = new UseZonedDateTime();
 
     @Test
     public void givenZoneId_thenZonedDateTime() {
         ZoneId zoneId = ZoneId.of("Europe/Paris");
         ZonedDateTime zonedDatetime = zonedDateTime.getZonedDateTime(LocalDateTime.parse("2016-05-20T06:30"), zoneId);
         Assert.assertEquals(zoneId, ZoneId.from(zonedDatetime));
+    }
+
+    @Test
+    public void whenRequestingZones_thenAtLeastOneIsReturned() {
+        Set<String> allZoneIds = ZoneId.getAvailableZoneIds();
+
+        assertThat(allZoneIds.size()).isGreaterThan(1);
     }
 
     @Test
@@ -41,5 +49,16 @@ public class UseZonedDateTimeUnitTest {
         assertThat(startOfOfDayWithMethod.toLocalTime()).isEqualTo(LocalTime.MIDNIGHT);
         assertThat(startOfOfDayWithMethod.toLocalTime()
             .toString()).isEqualTo("00:00");
+    }
+
+    @Test
+    public void givenAStringWithTimeZone_whenParsing_thenEqualsExpected() {
+        ZonedDateTime resultFromString = zonedDateTime.getZonedDateTimeUsingParseMethod("2015-05-03T10:15:30+01:00[Europe/Paris]");
+        ZonedDateTime resultFromLocalDateTime = ZonedDateTime.of(2015, 5, 3, 11, 15, 30, 0, ZoneId.of("Europe/Paris"));
+
+        assertThat(resultFromString.getZone()).isEqualTo(ZoneId.of("Europe/Paris"));
+        assertThat(resultFromLocalDateTime.getZone()).isEqualTo(ZoneId.of("Europe/Paris"));
+
+        assertThat(resultFromString).isEqualTo(resultFromLocalDateTime);
     }
 }
