f2a06c7c34c7ae787b30af4af3332cdf93dd8a36
Yavuz Tas <yavuzslmtas@gmail.com>
2019-10-25 14:50:02 +0200

    Multpile issues resolved
    - fix formatting issues
    - add email field to Person
    - add email validation test

--- javaxval/src/test/java/org/baeldung/javaxval/messageinterpolator/ParameterMessageInterpolaterIntegrationTest.java
+++ javaxval/src/test/java/org/baeldung/javaxval/messageinterpolator/ParameterMessageInterpolaterIntegrationTest.java
@@ -15,44 +15,56 @@ import org.junit.Test;
 
 public class ParameterMessageInterpolaterIntegrationTest {
 
-	private static Validator validator;
+    private static Validator validator;
 
-	@BeforeClass
-	public static void beforeClass() {
-		ValidatorFactory validatorFactory = Validation.byDefaultProvider()
-			.configure()
-			.messageInterpolator(new ParameterMessageInterpolator())
-			.buildValidatorFactory();
-		
-		validator = validatorFactory.getValidator();
-	}
+    @BeforeClass
+    public static void beforeClass() {
+        ValidatorFactory validatorFactory = Validation.byDefaultProvider()
+          .configure()
+          .messageInterpolator(new ParameterMessageInterpolator())
+          .buildValidatorFactory();
 
-	@Test
-	public void ifNameLengthIsLess_nameValidationFails() {
-		Person person = new Person();
-		person.setName("John Doe");
-		person.setAge(18);
+        validator = validatorFactory.getValidator();
+    }
 
-		Set<ConstraintViolation<Person>> violations = validator.validate(person);
-		assertEquals(1, violations.size());
+    @Test
+    public void ifNameLengthIsLess_nameValidationFails() {
+        Person person = new Person();
+        person.setName("John Doe");
+        person.setAge(18);
 
-		ConstraintViolation<Person> violation = violations.iterator().next();
-		assertEquals("Name should be in-between 10 and 200 characters", violation.getMessage());
-	}
-	
-	@Test
-	public void ifAgeIsLess_ageMinValidationFails() {
-		Person person = new Person();
-		person.setName("John Stephaner Doe");
-		person.setAge(16);
+        Set<ConstraintViolation<Person>> violations = validator.validate(person);
+        assertEquals(1, violations.size());
 
-		Set<ConstraintViolation<Person>> violations = validator.validate(person);
-		assertEquals(1, violations.size());
+        ConstraintViolation<Person> violation = violations.iterator().next();
+        assertEquals("Name should be between 10 and 100 characters", violation.getMessage());
+    }
 
-		ConstraintViolation<Person> violation = violations.iterator().next();
-		assertEquals("Age should not be less than 18 but it is ${validatedValue}", violation.getMessage());
-	}
+    @Test
+    public void ifAgeIsLess_ageMinValidationFails() {
+        Person person = new Person();
+        person.setName("John Stephaner Doe");
+        person.setAge(16);
+
+        Set<ConstraintViolation<Person>> violations = validator.validate(person);
+        assertEquals(1, violations.size());
+
+        ConstraintViolation<Person> violation = violations.iterator().next();
+        assertEquals("Age should not be less than 18", violation.getMessage());
+    }
+    
+    @Test
+    public void ifEmailIsMalformed_emailFormatValidationFails() {
+        Person person = new Person();
+        person.setName("John Stephaner Doe");
+        person.setAge(18);
+        person.setEmail("johndoe.dev");
+        
+        Set<ConstraintViolation<Person>> violations = validator.validate(person);
+        assertEquals(1, violations.size());
+        
+        ConstraintViolation<Person> violation = violations.iterator().next();
+        assertEquals("Email address should be in a correct format: ${validatedValue}", violation.getMessage());
+    }
 
 }
-
-
