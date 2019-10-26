ed95ec46ee54184ff460213fe249aeb66a19f5ca
Ali Dehghani <ali.dehghani.g@gmail.com>
2019-10-26 23:23:30 +0330

    Refactored the packages.

--- persistence-modules/spring-data-jpa-3/src/main/java/com/baeldung/model/Payment.java
+++ persistence-modules/spring-data-jpa-3/src/main/java/com/baeldung/model/Payment.java
@@ -1,4 +1,4 @@
-package com.baeldung.tx;
+package com.baeldung.model;
 
 import javax.persistence.*;
 
--- persistence-modules/spring-data-jpa-3/src/test/java/com/baeldung/tx/ManualTransactionIntegrationTest.java
+++ persistence-modules/spring-data-jpa-3/src/test/java/com/baeldung/tx/ManualTransactionIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.baeldung.tx;
 
+import com.baeldung.model.Payment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
