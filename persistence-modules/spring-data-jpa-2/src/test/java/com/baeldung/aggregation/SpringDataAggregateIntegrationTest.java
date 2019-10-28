1c3a58cbb5a84f23506eaf7924048f1611578bfb
zepfred <zepfred@gmail.com>
2019-10-28 13:17:30 -0300

    *Adjust in the integration tests PR #7977

--- persistence-modules/spring-data-jpa-2/src/test/java/com/baeldung/aggregation/SpringDataAggregateIntegrationTest.java
+++ persistence-modules/spring-data-jpa-2/src/test/java/com/baeldung/aggregation/SpringDataAggregateIntegrationTest.java
@@ -17,6 +17,7 @@ import static org.junit.Assert.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+
 @Sql(scripts = "/test-aggregation-data.sql")
 public class SpringDataAggregateIntegrationTest {
 
@@ -26,13 +27,19 @@ public class SpringDataAggregateIntegrationTest {
     @Test
     public void whenQueryWithAggregation_thenReturnResult() {
         List<Object[]> commentCountsByYear = commentRepository.countTotalCommentsByYear();
+
         Object[] countYear2019 = commentCountsByYear.get(0);
+
         assertThat(countYear2019[0], is(Integer.valueOf(2019)));
         assertThat(countYear2019[1], is(1l));
+
         Object[] countYear2018 = commentCountsByYear.get(1);
+
         assertThat(countYear2018[0], is(Integer.valueOf(2018)));
         assertThat(countYear2018[1], is(2l));
+
         Object[] countYear2017 = commentCountsByYear.get(2);
+
         assertThat(countYear2017[0], is(Integer.valueOf(2017)));
         assertThat(countYear2017[1], is(1l));
     }
@@ -40,13 +47,19 @@ public class SpringDataAggregateIntegrationTest {
     @Test
     public void whenQueryWithAggregation_thenReturnCustomResult() {
         List<CommentCount> commentCountsByYear = commentRepository.countTotalCommentsByYearClass();
+
         CommentCount countYear2019 = commentCountsByYear.get(0);
+
         assertThat(countYear2019.getYear(), is(Integer.valueOf(2019)));
         assertThat(countYear2019.getTotal(), is(1l));
+
         CommentCount countYear2018 = commentCountsByYear.get(1);
+
         assertThat(countYear2018.getYear(), is(Integer.valueOf(2018)));
         assertThat(countYear2018.getTotal(), is(2l));
+
         CommentCount countYear2017 = commentCountsByYear.get(2);
+
         assertThat(countYear2017.getYear(), is(Integer.valueOf(2017)));
         assertThat(countYear2017.getTotal(), is(1l));
     }
@@ -54,13 +67,19 @@ public class SpringDataAggregateIntegrationTest {
     @Test
     public void whenQueryWithAggregation_thenReturnInterfaceResult() {
         List<ICommentCount> commentCountsByYear = commentRepository.countTotalCommentsByYearInterface();
+
         ICommentCount countYear2019 = commentCountsByYear.get(0);
+
         assertThat(countYear2019.getYearComment(), is(Integer.valueOf(2019)));
         assertThat(countYear2019.getTotalComment(), is(1l));
+
         ICommentCount countYear2018 = commentCountsByYear.get(1);
+
         assertThat(countYear2018.getYearComment(), is(Integer.valueOf(2018)));
         assertThat(countYear2018.getTotalComment(), is(2l));
+
         ICommentCount countYear2017 = commentCountsByYear.get(2);
+
         assertThat(countYear2017.getYearComment(), is(Integer.valueOf(2017)));
         assertThat(countYear2017.getTotalComment(), is(1l));
     }
@@ -68,13 +87,19 @@ public class SpringDataAggregateIntegrationTest {
     @Test
     public void whenNativeQueryWithAggregation_thenReturnInterfaceResult() {
         List<ICommentCount> commentCountsByYear = commentRepository.countTotalCommentsByYearNative();
+
         ICommentCount countYear2019 = commentCountsByYear.get(0);
+
         assertThat(countYear2019.getYearComment(), is(Integer.valueOf(2019)));
         assertThat(countYear2019.getTotalComment(), is(1l));
+
         ICommentCount countYear2018 = commentCountsByYear.get(1);
+
         assertThat(countYear2018.getYearComment(), is(Integer.valueOf(2018)));
         assertThat(countYear2018.getTotalComment(), is(2l));
+
         ICommentCount countYear2017 = commentCountsByYear.get(2);
+
         assertThat(countYear2017.getYearComment(), is(Integer.valueOf(2017)));
         assertThat(countYear2017.getTotalComment(), is(1l));
     }
