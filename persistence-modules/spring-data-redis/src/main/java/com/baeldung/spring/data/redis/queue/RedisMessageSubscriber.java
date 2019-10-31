03679107fd87e4ad8341ebe01b9875753c939491
Ali Dehghani <ali.dehghani.g@gmail.com>
2019-10-31 15:04:40 +0330

    Using blocking queue for pub-sub

--- persistence-modules/spring-data-redis/src/main/java/com/baeldung/spring/data/redis/queue/RedisMessageSubscriber.java
+++ persistence-modules/spring-data-redis/src/main/java/com/baeldung/spring/data/redis/queue/RedisMessageSubscriber.java
@@ -4,16 +4,16 @@ import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 @Service
 public class RedisMessageSubscriber implements MessageListener {
 
-    public static List<String> messageList = new ArrayList<String>();
+    public static BlockingQueue<String> messages = new ArrayBlockingQueue<String>(100);
 
     public void onMessage(final Message message, final byte[] pattern) {
-        messageList.add(message.toString());
+        messages.add(message.toString());
         System.out.println("Message received: " + new String(message.getBody()));
     }
 }
