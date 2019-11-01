af3b4b8a5f98db343b7fc05789aa9656e786d080
Martin van Wingerden <martin@martinvw.nl>
2019-11-01 09:05:12 +0100

    [BAEL-3392] Formatted code examples for [BAEL-3392]

--- core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/debug/Example1.java
+++ core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/debug/Example1.java
@@ -1,17 +1,17 @@
 package com.baeldung.streams.debug;
 
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.IntStream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
 
 public class Example1 {
     @Test
     public void whenDebugging_thenInformationIsShown() {
         int[] listOutputSorted = IntStream.of(-3, 10, -4, 1, 3)
-                .sorted()
-                .toArray();
+            .sorted()
+            .toArray();
 
         assertThat(listOutputSorted).isSorted();
     }
--- core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/debug/Example2.java
+++ core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/debug/Example2.java
@@ -1,35 +1,35 @@
 package com.baeldung.streams.debug;
 
-import com.baeldung.streams.debug.entity.Customer;
-import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+
+import com.baeldung.streams.debug.entity.Customer;
 
 public class Example2 {
     @Test
     public void whenDebugging_thenInformationIsShown() {
         List<Optional<Customer>> customers = Arrays.asList(
-                Optional.of(new Customer("John P.", 15)),
-                Optional.of(new Customer("Sarah M.", 78)),
-                Optional.empty(),
-                Optional.of(new Customer("Mary T.", 20)),
-                Optional.empty(),
-                Optional.of(new Customer("Florian G.", 89)),
-                Optional.empty());
+            Optional.of(new Customer("John P.", 15)),
+            Optional.of(new Customer("Sarah M.", 78)),
+            Optional.empty(),
+            Optional.of(new Customer("Mary T.", 20)),
+            Optional.empty(),
+            Optional.of(new Customer("Florian G.", 89)),
+            Optional.empty()
+        );
 
-        long numberOf65PlusCustomers = customers
-                .stream()
-                .flatMap(c -> c
-                        .map(Stream::of)
-                        .orElseGet(Stream::empty))
-                .mapToInt(Customer::getAge)
-                .filter(c -> c > 65)
-                .count();
+        long numberOf65PlusCustomers = customers.stream()
+            .flatMap(c -> c.map(Stream::of)
+                .orElseGet(Stream::empty))
+            .mapToInt(Customer::getAge)
+            .filter(c -> c > 65)
+            .count();
 
         assertThat(numberOf65PlusCustomers).isEqualTo(2);
     }
