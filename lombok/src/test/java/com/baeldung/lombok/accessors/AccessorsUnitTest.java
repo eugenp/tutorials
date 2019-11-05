2d511b537a17b9910911386fb5844dc97bd3ecdf
Roger Yates <roger42yates@gmail.com>
2019-11-06 06:44:10 +1000

    BAEL-3475 - Rename Test to UnitTest

--- lombok/src/test/java/com/baeldung/lombok/accessors/AccessorsUnitTest.java
+++ lombok/src/test/java/com/baeldung/lombok/accessors/AccessorsUnitTest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 
 import static org.junit.Assert.assertEquals;
 
-public class AccessorsTest {
+public class AccessorsUnitTest {
 
     @Test
     public void givenBasicAccount_thenUseBasicAccessors() {
