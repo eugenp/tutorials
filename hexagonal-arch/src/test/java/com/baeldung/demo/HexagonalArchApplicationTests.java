37aea837f134aaf87663bd570151836c654f9089
Chaudhary <rohit.chaudhary@dxc.com>
2019-11-06 21:38:09 +0530

    initial commit

---
+++ hexagonal-arch/src/test/java/com/baeldung/demo/HexagonalArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
