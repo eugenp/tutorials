c3ad62609cec99df286357b2eca5eb6b56f04bf4
vatsalgosar <vatsalgosar@gmail.com>
2019-11-09 02:33:50 +0530

    - Separated add and subtract days into two methods for simplification

--- core-java-modules/core-java-date-operations/src/main/java/com/baeldung/datetime/AddSubtractDaysSkippingWeekendsUtils.java
+++ core-java-modules/core-java-date-operations/src/main/java/com/baeldung/datetime/AddSubtractDaysSkippingWeekendsUtils.java
@@ -5,15 +5,25 @@ import java.time.LocalDate;
 
 public class AddSubtractDaysSkippingWeekendsUtils {
 
-    public static LocalDate addSubtractDaysSkippingWeekendsIterativeMethod(LocalDate date, int days) {
+    public static LocalDate addDaysSkippingWeekends(LocalDate date, int days) {
         LocalDate result = date;
         int addedDays = 0;
-        int absDays = Math.abs(days);
-        int day = (days > 0) ? 1 : -1;
-        while (addedDays < absDays) {
-            result = result.plusDays(day);
+        while (addedDays < days) {
+            result = result.plusDays(1);
             if (!(result.getDayOfWeek() == DayOfWeek.SATURDAY || result.getDayOfWeek() == DayOfWeek.SUNDAY)) {
                 ++addedDays;
+            }
+        }
+        return result;
+    }
+
+    public static LocalDate subtractDaysSkippingWeekends(LocalDate date, int days) {
+        LocalDate result = date;
+        int subtractedDays = 0;
+        while (subtractedDays < days) {
+            result = result.minusDays(1);
+            if (!(result.getDayOfWeek() == DayOfWeek.SATURDAY || result.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+                ++subtractedDays;
             }
         }
         return result;
--- core-java-modules/core-java-date-operations/src/test/java/com/baeldung/datetime/AddSubtractDaysSkippingWeekendsUtilsUnitTest.java
+++ core-java-modules/core-java-date-operations/src/test/java/com/baeldung/datetime/AddSubtractDaysSkippingWeekendsUtilsUnitTest.java
@@ -9,18 +9,18 @@ import java.time.LocalDate;
 public class AddSubtractDaysSkippingWeekendsUtilsUnitTest {
 
     @Test
-    public void givenLocalDateAndDaysToAdd_thenAddDaysSkippingWeekendsUsingIterativeMethod() {
+    public void givenLocalDateAndDaysToAdd_thenAddDaysSkippingWeekends() {
         LocalDate initialDate = LocalDate.of(2019, 11, 7);
         LocalDate expectedDate = LocalDate.of(2019, 11, 13);
-        LocalDate result = AddSubtractDaysSkippingWeekendsUtils.addSubtractDaysSkippingWeekendsIterativeMethod(initialDate, 4);
+        LocalDate result = AddSubtractDaysSkippingWeekendsUtils.addDaysSkippingWeekends(initialDate, 4);
         assertEquals(expectedDate, result);
     }
 
     @Test
-    public void givenLocalDateAndDaysToAdd_thenSubtractDaysSkippingWeekendsUsingIterativeMethod() {
+    public void givenLocalDateAndDaysToSubtract_thenSubtractDaysSkippingWeekends() {
         LocalDate initialDate = LocalDate.of(2019, 11, 7);
         LocalDate expectedDate = LocalDate.of(2019, 11, 1);
-        LocalDate result = AddSubtractDaysSkippingWeekendsUtils.addSubtractDaysSkippingWeekendsIterativeMethod(initialDate, -4);
+        LocalDate result = AddSubtractDaysSkippingWeekendsUtils.subtractDaysSkippingWeekends(initialDate, 4);
         assertEquals(expectedDate, result);
     }
 
