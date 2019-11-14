076ad430d5fb6326e25c5173a53d11ba1ac7d2ab
dupirefr <dupire.francois+pro@gmail.com>
2019-11-14 10:07:44 +0100

    [BAEL-3452] Added deciphering

--- algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/caesarcipher/CaesarCipherAlgorithm.java
+++ algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/caesarcipher/CaesarCipherAlgorithm.java
@@ -4,22 +4,24 @@ public class CaesarCipherAlgorithm {
     private static final char LETTER_A = 'a';
     private static final int LETTERS_COUNT = 26;
 
-    public String cipher(String message, int shift) {
-        if (shift < 0 || shift >= LETTERS_COUNT) {
-            throw new IllegalArgumentException("Shift must be between 0 and 25");
-        }
-
+    public String cipher(String message, int offset) {
         StringBuilder result = new StringBuilder();
 
         for (char character : message.toCharArray()) {
             if (character != ' ') {
-                int shiftFromA = (character + shift - LETTER_A) % LETTERS_COUNT;
-                result.append((char) (LETTER_A + shiftFromA));
+                int originalAlphabetPosition = character - LETTER_A;
+                int newAlphabetPosition = (originalAlphabetPosition + offset) % LETTERS_COUNT;
+                char newCharacter = (char) (LETTER_A + newAlphabetPosition);
+                result.append(newCharacter);
             } else {
                 result.append(character);
             }
         }
 
         return result.toString();
+    }
+
+    public String decipher(String message, int offset) {
+        return cipher(message, LETTERS_COUNT - (offset % LETTERS_COUNT));
     }
 }
--- algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/caesarcipher/CaesarCipherAlgorithmUnitTest.java
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/caesarcipher/CaesarCipherAlgorithmUnitTest.java
@@ -6,6 +6,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class CaesarCipherAlgorithmUnitTest {
     private static final String SENTENCE = "he told me i could never teach a lama to drive";
+    private static final String SENTENCE_SHIFTED_THREE = "kh wrog ph l frxog qhyhu whdfk d odpd wr gulyh";
+    private static final String SENTENCE_SHIFTED_TEN = "ro dyvn wo s myevn xofob dokmr k vkwk dy nbsfo";
 
     private CaesarCipherAlgorithm algorithm = new CaesarCipherAlgorithm();
 
@@ -14,7 +16,7 @@ class CaesarCipherAlgorithmUnitTest {
         String cipheredSentence = algorithm.cipher(SENTENCE, 3);
 
         assertThat(cipheredSentence)
-          .isEqualTo("kh wrog ph l frxog qhyhu whdfk d odpd wr gulyh");
+          .isEqualTo(SENTENCE_SHIFTED_THREE);
     }
 
     @Test
@@ -22,6 +24,38 @@ class CaesarCipherAlgorithmUnitTest {
         String cipheredSentence = algorithm.cipher(SENTENCE, 10);
 
         assertThat(cipheredSentence)
-          .isEqualTo("ro dyvn wo s myevn xofob dokmr k vkwk dy nbsfo");
+          .isEqualTo(SENTENCE_SHIFTED_TEN);
+    }
+
+    @Test
+    void givenSentenceAndShiftThirtySix_whenCipher_thenCipheredLikeTenMessageWithOverflow() {
+        String cipheredSentence = algorithm.cipher(SENTENCE, 36);
+
+        assertThat(cipheredSentence)
+          .isEqualTo(SENTENCE_SHIFTED_TEN);
+    }
+
+    @Test
+    void givenSentenceShiftedThreeAndShiftThree_whenDecipher_thenOriginalSentenceWithoutOverflow() {
+        String decipheredSentence = algorithm.decipher(SENTENCE_SHIFTED_THREE, 3);
+
+        assertThat(decipheredSentence)
+          .isEqualTo(SENTENCE);
+    }
+
+    @Test
+    void givenSentenceShiftedTenAndShitTen_whenDecipher_thenOriginalSentenceWithOverflow() {
+        String decipheredSentence = algorithm.decipher(SENTENCE_SHIFTED_TEN, 10);
+
+        assertThat(decipheredSentence)
+          .isEqualTo(SENTENCE);
+    }
+
+    @Test
+    void givenSentenceShiftedTenAndShiftThirtySix_whenDecipher_thenOriginalSentenceWithOverflow() {
+        String decipheredSentence = algorithm.decipher(SENTENCE_SHIFTED_TEN, 36);
+
+        assertThat(decipheredSentence)
+          .isEqualTo(SENTENCE);
     }
 }
