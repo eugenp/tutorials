473dc2df5b4d46f4f0c152f37cb476c713462167
Justin Albano <albanoj2@users.noreply.github.com>
2019-11-14 20:50:45 -0500

    BAEL-3444: Simplified example steps

--- testing-modules/testing-libraries/src/test/java/com/baeldung/cucumber/books/BookStoreIntegrationTest.java
+++ testing-modules/testing-libraries/src/test/java/com/baeldung/cucumber/books/BookStoreIntegrationTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "classpath:features/book-store.feature")
 public class BookStoreIntegrationTest {
