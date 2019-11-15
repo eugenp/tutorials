86c72ffd7b9e5a08295a559e5b1967a5a91b9f00
M-Abdelbaset <31687750+M-Abdelbaset@users.noreply.github.com>
2019-11-16 01:07:27 +0200

    Bael-3395: Spring optional path variables (#8193)
    * initial test cases
    
    * changes in @requestMapping
    
    * moving code to spring-mvc-basic-2 project

---
+++ spring-mvc-basics-2/src/main/java/com/baeldung/controller/optionalpathvars/ArticleViewerWithOptionalParamController.java
@@ -0,0 +1,28 @@
+package com.baeldung.controller.optionalpathvars;
+
+import static com.baeldung.model.Article.DEFAULT_ARTICLE;
+
+import java.util.Optional;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.model.Article;;
+
+@RestController
+@RequestMapping("/optionalParam")
+public class ArticleViewerWithOptionalParamController {
+
+    @RequestMapping(value = {"/article", "/article/{id}"})
+    public Article getArticle(@PathVariable(name = "id") Optional<Integer> optionalArticleId) {
+
+        if(optionalArticleId.isPresent()) {
+            Integer articleId = optionalArticleId.get();
+            return new Article(articleId);
+        }else {
+            return DEFAULT_ARTICLE;
+        }
+    }
+    
+}
