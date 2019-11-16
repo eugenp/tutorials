a2314c69d9dffc092e2d8ad081b2689dc8d56aed
matt.rossi <matt.rossi@reply.it>
2019-11-16 18:00:04 +0100

    Add Jenkinsfile & update gatling version

--- testing-modules/gatling/src/test/scala/Engine.scala
+++ testing-modules/gatling/src/test/scala/Engine.scala
@@ -3,11 +3,10 @@ import io.gatling.core.config.GatlingPropertiesBuilder
 
 object Engine extends App {
 
-    val props = new GatlingPropertiesBuilder
-    props.dataDirectory(IDEPathHelper.dataDirectory.toString)
-    props.resultsDirectory(IDEPathHelper.resultsDirectory.toString)
-    props.bodiesDirectory(IDEPathHelper.bodiesDirectory.toString)
-    props.binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString)
+  val props = new GatlingPropertiesBuilder()
+    .resourcesDirectory(IDEPathHelper.resourcesDirectory.toString)
+    .resultsDirectory(IDEPathHelper.resultsDirectory.toString)
+    .binariesDirectory(IDEPathHelper.mavenBinariesDirectory.toString)
 
-    Gatling.fromMap(props.build)
+  Gatling.fromMap(props.build)
 }
