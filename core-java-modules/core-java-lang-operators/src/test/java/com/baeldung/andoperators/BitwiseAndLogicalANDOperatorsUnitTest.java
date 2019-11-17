93f42e4b96e16f6eac5fff6a9e4c0e3497cfcfef
sumit-bhawsar <bhawsar.sumit08@gmail.com>
2019-11-17 15:25:18 +0000

    BAEL-3512 unit tests for Bitwise and Logical AND operators

---
+++ core-java-modules/core-java-lang-operators/src/test/java/com/baeldung/andoperators/BitwiseAndLogicalANDOperatorsUnitTest.java
@@ -0,0 +1,41 @@
+package com.baeldung.andoperators;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.*;
+
+public class BitwiseAndLogicalANDOperatorsUnitTest {
+
+    @Test
+    public void givenTwoIntegers_whenBitwiseAndOperator_thenNewDecimalNumber() {
+        int value1 = 6;
+        int value2 = 5;
+        int result = value1 & value2;
+        assertEquals(4, result);
+    }
+
+    @Test
+    public void givenTwoTrueBooleans_whenLogicalAndOperator_thenTrue() {
+        boolean a = true;
+        boolean b = true;
+        boolean result = a && b;
+        assertTrue(result);
+    }
+
+    @Test
+    public void givenOneFalseAndOneTrueBooleans_whenLogicalAndOperator_thenFalse() {
+        boolean a = false;
+        boolean b = true;
+        boolean result = a && b;
+        assertFalse(result);
+    }
+
+    @Test
+    public void givenTwoFalseBooleans_whenLogicalAndOperator_thenFalse() {
+        boolean a = false;
+        boolean b = false;
+        boolean result = a && b;
+        assertFalse(result);
+    }
+
+}
