10f220aea576d906a5929b3b1885f08b6cd5097a
rahul <rahulgul8@gmail.com>
2019-11-19 23:46:10 +0530

    Adding webrtc changes

---
+++ webrtc/src/main/java/com/baeldung/webrtc/WebSocketConfiguration.java
@@ -0,0 +1,16 @@
+package com.baeldung.webrtc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new SocketHandler(), "/socket").setAllowedOrigins("*");
+    }
+}
