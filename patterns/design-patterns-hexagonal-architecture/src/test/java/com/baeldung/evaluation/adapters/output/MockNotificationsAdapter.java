fc9e3213b4a188cb216134d67ad0104331bcafc4
boneacsu <bogdan.neacsu@hotmail.com>
2019-11-20 22:01:32 +0200

    For Eval Article

--- patterns/design-patterns-hexagonal-architecture/src/test/java/com/baeldung/evaluation/adapters/output/MockNotificationsAdapter.java
+++ patterns/design-patterns-hexagonal-architecture/src/test/java/com/baeldung/evaluation/adapters/output/MockNotificationsAdapter.java
@@ -3,7 +3,6 @@ package com.baeldung.evaluation.adapters.output;
 import com.baeldung.evaluation.hexagon.spi.INotificationsSpi;
 
 public class MockNotificationsAdapter implements INotificationsSpi {
-
     public boolean send() {
         System.out.println("Sending notifications from mocked adapter.");
         return true;
--- patterns/design-patterns-hexagonal-architecture/src/test/java/com/baeldung/evaluation/adapters/output/MockPersistenceAdapter.java
+++ patterns/design-patterns-hexagonal-architecture/src/test/java/com/baeldung/evaluation/adapters/output/MockPersistenceAdapter.java
@@ -3,7 +3,6 @@ package com.baeldung.evaluation.adapters.output;
 import com.baeldung.evaluation.hexagon.spi.IPersistenceSpi;
 
 public class MockPersistenceAdapter implements IPersistenceSpi {
-
     public boolean persist() {
         System.out.println("Persisting data through mocked adapter.");
         return true;
