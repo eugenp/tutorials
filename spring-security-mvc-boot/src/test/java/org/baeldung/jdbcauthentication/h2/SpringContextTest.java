bd8a6c0271f70a73c55cfde90ceda0025fc70d27
amit2103 <amit.pandey2103@gmail.com>
2019-11-20 11:11:07 +0530

    [BAEL-19420] - Removed the duplicate SpringContextIntegrationTest and spring-context profile

--- spring-security-mvc-boot/src/test/java/org/baeldung/jdbcauthentication/h2/SpringContextTest.java
+++ spring-security-mvc-boot/src/test/java/org/baeldung/jdbcauthentication/h2/SpringContextTest.java
@@ -1,4 +1,4 @@
-package org.baeldung;
+package org.baeldung.jdbcauthentication.h2;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = SampleLDAPApplication.class)
-public class SpringContextIntegrationTest {
+@SpringBootTest(classes = H2JdbcAuthenticationApplication.class)
+public class SpringContextTest {
 
     @Test
     public void whenSpringContextIsBootstrapped_thenNoExceptions() {
