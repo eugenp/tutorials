51e32cca925354e7821ef2e39984ceed1e8cf394
Sjmillington <sam@sbl-uk.org>
2019-11-23 13:06:06 +0000

    added SecurityConfig

--- spring-boot-runtime/src/test/java/com/baeldung/web/controller/TaxiFareControllerIntegrationTest.java
+++ spring-boot-runtime/src/test/java/com/baeldung/web/controller/TaxiFareControllerIntegrationTest.java
@@ -7,14 +7,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import com.baeldung.web.log.app.Application;
 import com.baeldung.web.log.data.TaxiRide;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = { Application.class, TaxiFareControllerIntegrationTest.SecurityConfig.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TaxiFareControllerIntegrationTest {
     
     @LocalServerPort
@@ -23,6 +26,7 @@ public class TaxiFareControllerIntegrationTest {
     @Test
     public void givenRequest_whenFetchTaxiFareRateCard_thanOK() {
 
+        System.out.println(port);
         String URL = "http://localhost:" + port + "/spring-rest";
         TestRestTemplate testRestTemplate = new TestRestTemplate();
         TaxiRide taxiRide = new TaxiRide(true, 10l);
@@ -30,6 +34,19 @@ public class TaxiFareControllerIntegrationTest {
           URL + "/taxifare/calculate/", 
           taxiRide, String.class);
       
-        //assertThat(fare, equalTo("200"));
+        assertThat(fare, equalTo("200"));
+    }
+
+    @Configuration
+    static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            System.out.println("security being set");
+            http
+                    .authorizeRequests()
+                    .anyRequest().permitAll()
+                    .and()
+                    .csrf().disable();
+        }
     }
 }
