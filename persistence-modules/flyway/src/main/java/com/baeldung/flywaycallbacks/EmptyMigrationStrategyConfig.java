e8a8be2e7a530d72da43a63956bc7dc7e2a90378
Krzysiek <krzysztofwoyke@gmail.com>
2019-11-23 22:58:54 +0100

    BAEL-3498: Change anonymous inner class to lambda

--- persistence-modules/flyway/src/main/java/com/baeldung/flywaycallbacks/EmptyMigrationStrategyConfig.java
+++ persistence-modules/flyway/src/main/java/com/baeldung/flywaycallbacks/EmptyMigrationStrategyConfig.java
@@ -2,7 +2,6 @@ package com.baeldung.flywaycallbacks;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,11 +13,8 @@ public class EmptyMigrationStrategyConfig {
 
     @Bean
     public FlywayMigrationStrategy flywayMigrationStrategy() {
-        return new FlywayMigrationStrategy() {
-            @Override
-            public void migrate(Flyway flyway) {
-                log.info("Skipping Flyway migration!");
-            }
+        return flyway -> {
+            log.info("Skipping Flyway migration!");
         };
     }
 }
