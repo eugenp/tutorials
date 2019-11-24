6080f679c762e3258f33eb13673dd9fb501c2287
fejera <attila@javadev.hu>
2019-11-24 12:42:59 +0100

    cleaned up and documented live testing script usage

--- persistence-modules/spring-data-mongodb/live-test.sh
+++ persistence-modules/spring-data-mongodb/live-test.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-docker image build -t spring-data-mongodb:live-test live-test/
-
-docker run -p 27017:27017 --name spring-data-mongodb-live-test -it spring-data-mongodb:live-test
-#wait
-
 mvn clean compile test -P live-all
-
-docker stop spring-data-mongodb-live-test
-docker rm spring-data-mongodb-live-test
