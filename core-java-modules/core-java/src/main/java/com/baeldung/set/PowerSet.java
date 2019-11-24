6ce9395a33de3eeeeac5dd37769df40ac7c580c7
Afshin <mnafshin@gmail.com>
2019-11-24 20:07:54 +0100

    some code refactoring is done

--- core-java-modules/core-java/src/main/java/com/baeldung/set/PowerSet.java
+++ core-java-modules/core-java/src/main/java/com/baeldung/set/PowerSet.java
@@ -11,16 +11,28 @@ public class PowerSet<T> {
             ret.add(new HashSet<>());
             return ret;
         }
+        //Extract one element and get set without that element
         T element = set.iterator().next();
-        Set<T> subset = getSetWithoutElement(set, element);
-        Set<Set<T>> powerSetSubSet = recursivePowerSet(subset);
-        Set<Set<T>> powerSet = new HashSet<>(powerSetSubSet);
-        for (Set<T> s : powerSetSubSet) {
+        Set<T> setWithoutElement = getSetWithoutElement(set, element);
+        //recursively calculate the powerSet of setWithoutElement
+        Set<Set<T>> powerSetSubSetWithoutElement = recursivePowerSet(setWithoutElement);
+        //add the extracted element to all sets in powerSetSubSetWithoutElement
+        Set<Set<T>> powerSetSubSetWithElement = addElementToAll(powerSetSubSetWithoutElement, element);
+        //union of powerSetSubSetWithElement and powerSetSubSetWithoutElement will be the result
+        Set<Set<T>> powerSet = new HashSet<>();
+        powerSet.addAll(powerSetSubSetWithoutElement);
+        powerSet.addAll(powerSetSubSetWithElement);
+        return powerSet;
+    }
+
+    private Set<Set<T>> addElementToAll(Set<Set<T>> ps, T element) {
+        Set<Set<T>> ret = new HashSet<>();
+        for (Set<T> s : ps) {
             Set<T> subsetWithElement = new HashSet<>(s);
             subsetWithElement.add(element);
-            powerSet.add(subsetWithElement);
+            ret.add(subsetWithElement);
         }
-        return powerSet;
+        return ret;
     }
 
     private Set<T> getSetWithoutElement(Set<T> set, T element) {
