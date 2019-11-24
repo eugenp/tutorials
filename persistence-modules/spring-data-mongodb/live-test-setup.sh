23fb4ae451fef29cb5a81d486542e32cb4d5b979
fejera <attila@javadev.hu>
2019-11-24 12:42:59 +0100

    cleaned up and documented live testing script usage

---
+++ persistence-modules/spring-data-mongodb/live-test-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker image build -t spring-data-mongodb:live-test live-test/
+
+docker run -p 27017:27017 --name spring-data-mongodb-live-test spring-data-mongodb:live-test
---
+++ persistence-modules/spring-data-mongodb/live-test-teardown.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker stop spring-data-mongodb-live-test
+docker rm spring-data-mongodb-live-test
