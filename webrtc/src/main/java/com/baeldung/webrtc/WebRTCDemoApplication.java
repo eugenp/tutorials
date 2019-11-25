1d811008fec75e6afb4225f4da51106f37fc4c0d
rahul <rahulgul8@gmail.com>
2019-11-25 21:48:13 +0530

    formatting changes

--- webrtc/src/main/java/com/baeldung/webrtc/WebRTCDemoApplication.java
+++ webrtc/src/main/java/com/baeldung/webrtc/WebRTCDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebRTCDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebRTCDemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebRTCDemoApplication.class, args);
+    }
 }
--- webrtc/src/main/java/com/baeldung/webrtc/WebSocketConfiguration.java
+++ webrtc/src/main/java/com/baeldung/webrtc/WebSocketConfiguration.java
@@ -11,6 +11,7 @@ public class WebSocketConfiguration implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new SocketHandler(), "/socket").setAllowedOrigins("*");
+        registry.addHandler(new SocketHandler(), "/socket")
+            .setAllowedOrigins("*");
     }
 }
