672ec2e74d725be77ab718a4d29cb2f367297742
Gian Mario Contessa <gmcontessa@hedgingbeta.com>
2019-11-25 17:19:24 +0000

    project creation and added ws library

---
+++ play-framework/async-http/app/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.mvc.*;
+
+/**
+ * This controller contains an action to handle HTTP requests
+ * to the application's home page.
+ */
+public class HomeController extends Controller {
+
+    /**
+     * An action that renders an HTML page with a welcome message.
+     * The configuration in the <code>routes</code> file means that
+     * this method will be called when the application receives a
+     * <code>GET</code> request with a path of <code>/</code>.
+     */
+    public Result index() {
+        return ok(views.html.index.render());
+    }
+
+}
---
+++ play-framework/async-http/build.sbt
@@ -0,0 +1,14 @@
+name := """async"""
+organization := "com.example"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayJava)
+
+scalaVersion := "2.13.1"
+
+// comment out the original line
+// libraryDependencies += guice
+
+// include multiple dependencies
+libraryDependencies ++= Seq(guice, javaWs)
