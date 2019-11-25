0b49d2ad6b19b7f919b7057cf46a7d3065aa5720
Ger Roza <rozagerardo@gmail.com>
2019-11-25 16:55:02 -0300

    [BAEL-3313] spring-cloud/spring-cloud-gateway | Writing custom Spring Cloud Gateway Filters -- address comments (#8251)
    * Addressed comments:
    * Removed Global filter bean with @Order annotation, now using class that implements Ordered
    * Added logic to actually mutate the request entity (and the exchange) for the Modify Request example
    * changed endpoint for languageServiceEndpoint in the chain requests example
    
    * removed order from first simple global filter, since well be covering that with other global filter

--- spring-cloud/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/customfilters/filters/global/FirstPreLastPostGlobalFilter.java
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/customfilters/filters/global/FirstPreLastPostGlobalFilter.java
@@ -4,19 +4,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 
 import reactor.core.publisher.Mono;
 
 @Component
-public class LoggingGlobalPreFilter implements GlobalFilter {
+public class FirstPreLastPostGlobalFilter implements GlobalFilter, Ordered {
 
-    final Logger logger = LoggerFactory.getLogger(LoggingGlobalPreFilter.class);
+    final Logger logger = LoggerFactory.getLogger(FirstPreLastPostGlobalFilter.class);
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("Global Pre Filter executed");
-        return chain.filter(exchange);
+        logger.info("First Pre Global Filter");
+        return chain.filter(exchange)
+            .then(Mono.fromRunnable(() -> {
+                logger.info("Last Post Global Filter");
+            }));
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
     }
 }
