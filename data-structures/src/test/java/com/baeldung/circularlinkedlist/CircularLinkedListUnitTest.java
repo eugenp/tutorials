fa28032d48d6cbe79a2b2586139b064659f4a773
Radhe Sravan <paturisravan@gmail.com>
2019-11-29 05:58:55 +0530

    Fixed the review comments

--- data-structures/src/test/java/com/baeldung/circularlinkedlist/CircularLinkedListUnitTest.java
+++ data-structures/src/test/java/com/baeldung/circularlinkedlist/CircularLinkedListUnitTest.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-import com.baeldung.circularlinkedlist.CircularLinkedList;
-
 public class CircularLinkedListUnitTest {
 
     @Test
