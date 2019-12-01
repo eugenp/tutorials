848abce39636e2e939ac4c798047935c215679d1
Anshul BANSAL <smartdiscover17@gmail.com>
2019-12-01 13:08:53 +0200

    Springfox - PR review fixes

--- springfox/src/main/java/com/baeldung/springfox/model/User.java
+++ springfox/src/main/java/com/baeldung/springfox/model/User.java
@@ -8,41 +8,41 @@ import javax.validation.constraints.NotNull;
 import javax.persistence.Entity;
 
 @Entity
-public class User{
-    
+public class User {
+
     @Id
     private Long id;
-    
+
     @NotNull(message = "First Name cannot be null")
     private String firstName;
-    
+
     @Min(value = 15, message = "Age should not be less than 15")
     @Max(value = 65, message = "Age should not be greater than 65")
     private int age;
-    
+
     @Email(regexp=".@.\\..*", message = "Email should be valid")
     private String email;
-    
+
     public Long getId() {
         return id;
     }
-    
+
     public void setId(Long id) {
         this.id = id;
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
-    
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
+
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
@@ -54,5 +54,5 @@ public class User{
     public void setAge(int age) {
         this.age = age;
     }
-    
-  }
+
+}
