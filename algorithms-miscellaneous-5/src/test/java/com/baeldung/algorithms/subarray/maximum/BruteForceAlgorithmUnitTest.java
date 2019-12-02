9a8aadf12556dcbe9d7a49a1e41f7aa2c6a4aa31
chirag d <chirag.dewan22@gmail.com>
2019-12-02 19:52:17 +0530

    BAEL3455 Maximum Subarray Problem

---
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/subarray/maximum/BruteForceAlgorithmUnitTest.java
@@ -0,0 +1,24 @@
+package com.baeldung.algorithms.subarray.maximum;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BruteForceAlgorithmUnitTest {
+
+    @Test
+    void givenArrayWithNegativeNumberWhenMaximumSubarrayThenReturns6() {
+        //given
+        int[] arr = new int[]{-3, 1, -8, 4, -1, 2, 1, -5, 5};
+
+        //when
+        BruteForceAlgorithm algorithm = new BruteForceAlgorithm();
+        int maximumSum = algorithm.maxSubArray(arr);
+
+        //then
+        assertEquals(6, maximumSum);
+
+    }
+
+}
