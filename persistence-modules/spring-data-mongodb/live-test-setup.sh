f3405e0daac9662355c31d66935db65515a29ce4
fejera <attila@javadev.hu>
2019-12-02 10:18:42 +0100

    moved live test resources to src/live-test/resources

--- persistence-modules/spring-data-mongodb/live-test-setup.sh
+++ persistence-modules/spring-data-mongodb/live-test-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker image build -t spring-data-mongodb:live-test live-test/
+docker image build -t spring-data-mongodb:live-test src/live-test/resources/
 
 docker run -p 27017:27017 --name spring-data-mongodb-live-test spring-data-mongodb:live-test
