5922f521900bf9c2e8785368ae49ab2103ac08fe
Cavero Barca <carlos.cavero@atos.net>
2019-12-03 01:02:39 +0100

    Create the Spring Boot project

---
+++ spring-boot-hexagon/src/main/java/com/baeldung/spring/hexagon/SpringBootHexagonApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.hexagon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHexagonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHexagonApplication.class, args);
+	}
+
+}
---
+++ spring-boot-hexagon/src/test/java/com/baeldung/spring/hexagon/SpringBootHexagonApplicationUnitTest.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.hexagon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHexagonApplicationUnitTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
