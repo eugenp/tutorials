47277fb85f2e6c2d1e8289c29ab7903d32337825
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2019-12-04 09:01:18 +0100

    BAEL-19790: Use GetMapping and rename SecurityConfigurer

--- spring-cloud/spring-cloud-config/server/src/main/java/com/baeldung/spring/cloud/config/server/SecurityConfiguration.java
+++ spring-cloud/spring-cloud-config/server/src/main/java/com/baeldung/spring/cloud/config/server/SecurityConfiguration.java
@@ -5,11 +5,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf()
-                .disable()
-                .httpBasic();
+                .ignoringAntMatchers("/encrypt/**")
+                .ignoringAntMatchers("/decrypt/**");
+
+        super.configure(http);
     }
 }
