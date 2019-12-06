3435ed1a12671b63b823a0bcfc958808ffea1acc
dupirefr <dupire.francois+pro@gmail.com>
2019-12-06 07:55:55 +0100

    [BAEL-3445] Added Background examples to Cucumber examples

--- testing-modules/testing-libraries/src/main/java/com/baeldung/cucumber/books/BookStore.java
+++ testing-modules/testing-libraries/src/main/java/com/baeldung/cucumber/books/BookStore.java
@@ -1,13 +1,9 @@
 package com.baeldung.cucumber.books;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class BookStore {
-
     private List<Book> books = new ArrayList<>();
     
     public void addBook(Book book) {
@@ -20,7 +16,13 @@ public class BookStore {
     
     public List<Book> booksByAuthor(String author) {
         return books.stream()
-            .filter(book -> Objects.equals(author, book.getAuthor()))
-            .collect(Collectors.toList());
+          .filter(book -> Objects.equals(author, book.getAuthor()))
+          .collect(Collectors.toList());
+    }
+
+    public Optional<Book> bookByTitle(String title) {
+        return books.stream()
+          .filter(book -> book.getTitle().equals(title))
+          .findFirst();
     }
 }
