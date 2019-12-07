6d1caf802679058c022ea6927dc11a3025e3f7a3
Lukasz Rys <lukaszrys@me.com>
2019-12-07 17:40:17 +0100

    BAEL-2275: Fill the test. Rename package.

--- ddd/src/main/java/com/baeldung/ddd/layers/infrastracture/configuration/MongoDBConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.ddd.layers.infrastracture.configuration;
-
-import com.baeldung.ddd.layers.infrastracture.repository.SpringDataOrderRepository;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@EnableMongoRepositories(basePackageClasses = SpringDataOrderRepository.class)
-public class MongoDBConfiguration {
-}
---
+++ ddd/src/main/java/com/baeldung/dddhexagonalspring/DomainLayerApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.dddhexagonalspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(value = { "classpath:ddd-layers.properties" })
+public class DomainLayerApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(DomainLayerApplication.class, args);
+    }
+}
