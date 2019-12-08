1960603812067cd75fa508b00ae4036fc53bc1ed
chirag d <chirag.dewan22@gmail.com>
2019-12-08 18:59:51 +0530

    BAEL3455 Max subarray problem

--- algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithmUnitTest.java
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithmUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class KadaneAlgorithmTest {
+class KadaneAlgorithmUnitTest {
 
     @Test
     void givenArrayWithNegativeNumberWhenMaximumSubarrayThenReturns6() {
