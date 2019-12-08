6f971744917e64e4fd439fe8d9b514b03a030a6c
chirag d <chirag.dewan22@gmail.com>
2019-12-08 18:59:51 +0530

    BAEL3455 Max subarray problem

--- algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/BruteForceAlgorithmUnitTest.java
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/BruteForceAlgorithmUnitTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BruteForceAlgorithmTest {
+class BruteForceAlgorithmUnitTest {
 
     @Test
     void givenArrayWithNegativeNumberWhenMaximumSubarrayThenReturns6() {
