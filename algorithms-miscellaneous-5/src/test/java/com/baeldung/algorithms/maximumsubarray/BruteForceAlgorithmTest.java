c2b71c2d33e68332f183c6633f01a8d7c49a0ad7
chirag d <chirag.dewan22@gmail.com>
2019-12-08 18:47:23 +0530

    BAEL3455 Max subarray problem

--- algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/BruteForceAlgorithmTest.java
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/BruteForceAlgorithmTest.java
@@ -1,24 +1,19 @@
-package com.baeldung.algorithms.subarray.maximum;
+package com.baeldung.algorithms.maximumsubarray;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BruteForceAlgorithmUnitTest {
+class BruteForceAlgorithmTest {
 
     @Test
     void givenArrayWithNegativeNumberWhenMaximumSubarrayThenReturns6() {
         //given
         int[] arr = new int[]{-3, 1, -8, 4, -1, 2, 1, -5, 5};
-
         //when
         BruteForceAlgorithm algorithm = new BruteForceAlgorithm();
         int maximumSum = algorithm.maxSubArray(arr);
-
         //then
         assertEquals(6, maximumSum);
-
     }
-
 }
--- algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithmTest.java
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithmTest.java
@@ -1,23 +1,19 @@
-package com.baeldung.algorithms.subarray.maximum;
+package com.baeldung.algorithms.maximumsubarray;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class KadaneAlgorithmUnitTest {
+class KadaneAlgorithmTest {
 
     @Test
     void givenArrayWithNegativeNumberWhenMaximumSubarrayThenReturns6() {
         //given
         int[] arr = new int[]{-3, 1, -8, 4, -1, 2, 1, -5, 5};
-
         //when
         KadaneAlgorithm algorithm = new KadaneAlgorithm();
         int maxSum = algorithm.maxSubArraySum(arr);
-
         //then
         assertEquals(6, maxSum);
-
     }
-
 }
