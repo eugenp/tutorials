c2a787eb44aba791c4bd8860d16f2c7b146488d4
Sorin Zamfir <sorin.zamfir@outlook.com>
2019-12-09 22:30:42 +0200

    BAEL-3370: Cleaned up examples

--- linux-bash/read/src/main/bash/read_inputs.sh
+++ linux-bash/read/src/main/bash/read_inputs.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+# section 2.1
+default_read() {
+    read input1 input2 input3
+    echo "[$input1] [$input2] [$input3]"
+}
+
+# section 2.2
+custom_ifs_no_array(){
+    IFS=";"
+    read input1 input2 input3
+    echo "[$input1] [$input2] [$input3]"
+}
+
 # Section 2.3
 prompt_read_password(){
     prompt="You shall not pass:"
@@ -17,19 +30,6 @@ array_read() {
         done
 }
 
-# section 2.2
-custom_ifs_no_array(){
-    IFS=";"
-    read input1 input2 input3
-    echo "[$input1] [$input2] [$input3]"
-}
-
-# section 2.1
-default_read() {
-    read input1 input2 input3
-    echo "[$input1] [$input2] [$input3]"
-}
-
 # section 3.1
 file_read(){
     exec {file_descriptor}<"./file.csv"
@@ -43,7 +43,7 @@ file_read(){
 }
 
 # section 3.2
-advanced_pipeing(){
+command_pipe(){
     ls -ll | { declare -a input
                read
                while read -a input; 
@@ -69,64 +69,3 @@ exactly_n_read(){
     echo -e "\ninput word1 [$input1]"
     echo "input word2 [$input2]"
 }
-
-
-advanced_pipeing_ps(){
-# process substitution
-    declare -a input
-    while read -a input
-    do
-        echo "${input[0]} ${input[8]}"
-    done < <(ls -ll)
-}
-
-custom_ifs_read(){
-    declare -a input_array
-    while IFS=";" read -a input_array -r
-        do
-            for element in ${input_array[@]}
-                do
-                    echo -n "$element.backup "
-                done
-            echo ""
-        done < "file.csv"
-}
-
-infinite_read(){
-    while read -d "?" -ep "Input:" input
-        do
-            echo "$input"
-        done
-}
-
-custom_ifs(){
-    declare -a input_array
-    IFS=$1 # whitespace 
-    read -a input_array -p "Enter something with delimiter <$1>:"
-    for input in ${input_array[@]}
-        do
-            echo "[$input]"
-        done
-}
-
-
-
-#default_read
-#array_read
-#special_delim
-file_read
-#prompt_read
-#default_input_read
-#advanced_pipeing
-#custom_ifs_read
-#infinite_read
-# custom_ifs " "
-# custom_ifs ";"
-#custom_ifs_no_array
-
-
-
-
-
-
-
