045bae290f16ddda39d67011e4c0f9175809b6f2
Vikas Ramsingh Rajput <vikas.rajput@crownconsult.com>
2019-12-10 16:37:15 +0300

    BAEL-3397: removed hexagonal module from the code

--- hexagonal/src/main/java/com/baeldung/architecture/repository/WeatherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.architecture.repository;
-
-import com.baeldung.architecture.model.Weather;
-
-public interface WeatherRepository {
-
-    public Weather getTodaysForecast();
-
-}
--- hexagonal/src/main/java/com/baeldung/architecture/service/WeatherService.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.architecture.service;
-
-import com.baeldung.architecture.model.Weather;
-
-public interface WeatherService {
-
-    public Weather getTodaysForecast();
-
-}
