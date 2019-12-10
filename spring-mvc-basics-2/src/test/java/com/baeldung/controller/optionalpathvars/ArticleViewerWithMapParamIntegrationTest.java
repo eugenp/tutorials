96d4c3c95b38329994c5c96544fb05f03c82ac08
M-Abdelbaset <31687750+M-Abdelbaset@users.noreply.github.com>
2019-12-10 19:13:07 +0200

    Bael 3395 - spring optional path var (#8341)
    * initial test cases
    
    * changes in @requestMapping
    
    * moving code to spring-mvc-basic-2 project
    
    * moving Article class

--- spring-mvc-basics-2/src/test/java/com/baeldung/controller/optionalpathvars/ArticleViewerWithMapParamIntegrationTest.java
+++ spring-mvc-basics-2/src/test/java/com/baeldung/controller/optionalpathvars/ArticleViewerWithMapParamIntegrationTest.java
@@ -12,8 +12,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import com.baeldung.model.Article;
 import org.baeldung.controller.config.WebConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
