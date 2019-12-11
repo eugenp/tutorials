cb568e33c4c034ebcc40c107079dd6a5eb664c7b
dupirefr <dupire.francois+pro@gmail.com>
2019-12-11 06:06:41 +0100

    [BAEL-3445] Removed code from other package

--- testing-modules/testing-libraries/src/main/java/com/baeldung/cucumber/books/BookStore.java
+++ testing-modules/testing-libraries/src/main/java/com/baeldung/cucumber/books/BookStore.java
@@ -1,6 +1,9 @@
 package com.baeldung.cucumber.books;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class BookStore {
@@ -18,11 +21,5 @@ public class BookStore {
         return books.stream()
           .filter(book -> Objects.equals(author, book.getAuthor()))
           .collect(Collectors.toList());
-    }
-
-    public Optional<Book> bookByTitle(String title) {
-        return books.stream()
-          .filter(book -> book.getTitle().equals(title))
-          .findFirst();
     }
 }
