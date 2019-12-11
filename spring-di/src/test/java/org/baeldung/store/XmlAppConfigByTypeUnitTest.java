9aff76fcf8b4caf0e3ee1f04df37f778094a06d1
Justin Albano <albano.justin@gmail.com>
2019-12-11 08:26:29 -0500

    BAEL-3290: Corrected formatting

--- spring-di/src/test/java/org/baeldung/store/XmlAppConfigByTypeUnitTest.java
+++ spring-di/src/test/java/org/baeldung/store/XmlAppConfigByTypeUnitTest.java
@@ -13,8 +13,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * Separate unit test class where only one Item object is available for 
  * autowiring. If the ioc-context.xml were used for autowiring by type, there 
  * would be multiple qualifying Item objects, causing a failure.
- * 
- * @author Justin Albano <albano.justin@gmail.com>
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/ioc-context-by-type.xml")
