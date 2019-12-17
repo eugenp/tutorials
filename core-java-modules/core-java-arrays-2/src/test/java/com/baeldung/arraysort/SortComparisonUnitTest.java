dcd00f7030a383ece735c860befa10ef47f61c6d
Chaudhary <rohit.chaudhary@dxc.com>
2019-12-17 07:23:41 +0530

    BAEL-3380 - updated tests and removed main class

--- core-java-modules/core-java-arrays-2/src/test/java/com/baeldung/arraysort/SortComparisonUnitTest.java
+++ core-java-modules/core-java-arrays-2/src/test/java/com/baeldung/arraysort/SortComparisonUnitTest.java
@@ -20,213 +20,166 @@ import org.junit.runners.MethodSorters;
  */
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class SortComparisonUnitTest  {
-    
-    private int[]  sizeOfArrays  = { 1000, 10000, 100000, 1000000, 10000000 };
-    
+public class SortComparisonTest {
+
+    private int[] sizeOfArrays = { 1000, 10000, 100000, 1000000 };
+
     private int[] _1000_elements_array;
     private int[] _10000_elements_array;
     private int[] _100000_elements_array;
     private int[] _1000000_elements_array;
-    private int[] _10000000_elements_array;
-    
+
     @Before
     public void setUp() throws Exception {
-        
+
         _1000_elements_array = new int[sizeOfArrays[0]];
         _10000_elements_array = new int[sizeOfArrays[1]];
         _100000_elements_array = new int[sizeOfArrays[2]];
         _1000000_elements_array = new int[sizeOfArrays[3]];
-        _10000000_elements_array = new int[sizeOfArrays[4]];
-        
+
         Random random = new Random();
         for (int i = 0; i < sizeOfArrays[0]; i++) {
             _1000_elements_array[i] = random.nextInt(sizeOfArrays[0]) + random.nextInt(sizeOfArrays[0]);
         }
-        
+
         for (int i = 0; i < sizeOfArrays[1]; i++) {
             _10000_elements_array[i] = random.nextInt(sizeOfArrays[1]) + random.nextInt(sizeOfArrays[1]);
         }
-        
+
         for (int i = 0; i < sizeOfArrays[2]; i++) {
             _100000_elements_array[i] = random.nextInt(sizeOfArrays[2]) + random.nextInt(sizeOfArrays[2]);
         }
-        
+
         for (int i = 0; i < sizeOfArrays[3]; i++) {
             _1000000_elements_array[i] = random.nextInt(sizeOfArrays[3]) + random.nextInt(sizeOfArrays[3]);
         }
-        
-        for (int i = 0; i < sizeOfArrays[4]; i++) {
-            _10000000_elements_array[i] = random.nextInt(sizeOfArrays[4]) + random.nextInt(sizeOfArrays[4]);
-        }
-        
+
     }
-    
-    
+
     @Test
-    public void testSort_fullArray() {
-        
-        int[] array = {10,4,6,2,1,9,7,8,3,5};
-        int[] expected = {1,2,3,4,5,6,7,8,9,10};
-        
+    public void sortFullArrayUsingSortMethod() {
+
+        int[] array = { 10, 4, 6, 2, 1, 9, 7, 8, 3, 5 };
+        int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
         Arrays.sort(array);
-        
+
         assertArrayEquals(expected, array);
-        
+
     }
-    
+
     @Test
-    public void testSort_partialArray() {
-        int[] array = {10,4,6,2,1,9,7,8,3,5};
-        int[] expected = {10,4,1,2,6,7,8,9,3,5};
-        
-        Arrays.sort(array,2,8);
-        
+    public void sortPartialArrayUsingSortMethod() {
+        int[] array = { 10, 4, 6, 2, 1, 9, 7, 8, 3, 5 };
+        int[] expected = { 10, 4, 1, 2, 6, 7, 8, 9, 3, 5 };
+
+        Arrays.sort(array, 2, 8);
+
         assertArrayEquals(expected, array);
     }
-    
+
     @Test
-    public void testParallelSort_fullArray() {
-        int[] array = {10,4,6,2,1,9,7,8,3,5};
-        int[] expected = {1,2,3,4,5,6,7,8,9,10};
-        
+    public void sortFullArrayUsingParallelSortMethod() {
+        int[] array = { 10, 4, 6, 2, 1, 9, 7, 8, 3, 5 };
+        int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
         Arrays.parallelSort(array);
-        
+
         assertArrayEquals(expected, array);
     }
-    
+
     @Test
-    public void testParallelSort_partialArray() {
-        int[] array = {10,4,6,2,1,9,7,8,3,5};
-        int[] expected = {10,4,1,2,6,7,8,9,3,5};
-        
-        Arrays.parallelSort(array,2,8);
-        
+    public void sortPartialArrayUsingParallelSortMethod() {
+        int[] array = { 10, 4, 6, 2, 1, 9, 7, 8, 3, 5 };
+        int[] expected = { 10, 4, 1, 2, 6, 7, 8, 9, 3, 5 };
+
+        Arrays.parallelSort(array, 2, 8);
+
         assertArrayEquals(expected, array);
     }
-    
-    
-    
+
     @Test
-    public void test_1000_elements_sort() {
-       
+    public void sortArrayof_1000Elements_UsingSortMethod() {
         int[] sequentialDataSet = Arrays.copyOf(_1000_elements_array, _1000_elements_array.length);
-        System.out.println(sequentialDataSet.length);
         Arrays.sort(sequentialDataSet);
-        
+
         assertNotNull(sequentialDataSet);
         assertNotSame(Arrays.copyOf(_1000_elements_array, _1000_elements_array.length), sequentialDataSet);
-        
+
     }
-    
-    
+
     @Test
-    public void test_1000_elements_parallelsort() {
-        
+    public void sortArrayof_1000Elements_UsingParallelSortMethod() {
         int[] parallelDataSet = Arrays.copyOf(_1000_elements_array, _1000_elements_array.length);
-        System.out.println(parallelDataSet.length);
         Arrays.parallelSort(parallelDataSet);
-        
+
         assertNotNull(parallelDataSet);
         assertNotSame(Arrays.copyOf(_1000_elements_array, _1000_elements_array.length), parallelDataSet);
-        
+
     }
-    
-    
+
     @Test
-    public void test_10000_elements_sort() {
+    public void sortArrayof_10000Elements_UsingSortMethod() {
         int[] sequentialDataSet = Arrays.copyOf(_10000_elements_array, _10000_elements_array.length);
-        System.out.println(sequentialDataSet.length);
         Arrays.sort(sequentialDataSet);
-        
+
         assertNotNull(sequentialDataSet);
         assertNotSame(Arrays.copyOf(_10000_elements_array, _10000_elements_array.length), sequentialDataSet);
     }
-    
-    
+
     @Test
-    public void test_10000_elements_parallelsort() {
+    public void sortArrayof_10000Elements_UsingParallelSortMethod() {
         int[] parallelDataSet = Arrays.copyOf(_10000_elements_array, _10000_elements_array.length);
-        System.out.println(parallelDataSet.length);
         Arrays.parallelSort(parallelDataSet);
-        
+
         assertNotNull(parallelDataSet);
         assertNotSame(Arrays.copyOf(_10000_elements_array, _10000_elements_array.length), parallelDataSet);
     }
-    
+
     @Test
-    public void test_100000_elements_sort() {
+    public void sortArrayof_100000Elements_UsingSortMethod() {
         int[] sequentialDataSet = Arrays.copyOf(_100000_elements_array, _100000_elements_array.length);
-        System.out.println(sequentialDataSet.length);
         Arrays.sort(sequentialDataSet);
-        
+
         assertNotNull(sequentialDataSet);
         assertNotSame(Arrays.copyOf(_100000_elements_array, _100000_elements_array.length), sequentialDataSet);
     }
-    
-    
+
     @Test
-    public void test_100000_elements_parallelsort() {
+    public void sortArrayof_100000Elements_UsingParallelSortMethod() {
         int[] parallelDataSet = Arrays.copyOf(_100000_elements_array, _100000_elements_array.length);
-        System.out.println(parallelDataSet.length);
         Arrays.parallelSort(parallelDataSet);
-        
+
         assertNotNull(parallelDataSet);
         assertNotSame(Arrays.copyOf(_100000_elements_array, _100000_elements_array.length), parallelDataSet);
     }
 
-    
     @Test
-    public void test_1000000_elements_sort() {
+    public void sortArrayof_1000000Elements_UsingSortMethod() {
         int[] sequentialDataSet = Arrays.copyOf(_1000000_elements_array, _1000000_elements_array.length);
-        System.out.println(sequentialDataSet.length);
         Arrays.sort(sequentialDataSet);
-        
+
         assertNotNull(sequentialDataSet);
         assertNotSame(Arrays.copyOf(_1000000_elements_array, _1000000_elements_array.length), sequentialDataSet);
     }
-    
-    
+
     @Test
-    public void test_1000000_elements_parallelsort() {
+    public void sortArrayof_1000000Elements_UsingParallelSortMethod() {
         int[] parallelDataSet = Arrays.copyOf(_1000000_elements_array, _1000000_elements_array.length);
-        System.out.println(parallelDataSet.length);
         Arrays.parallelSort(parallelDataSet);
-        
+
         assertNotNull(parallelDataSet);
         assertNotSame(Arrays.copyOf(_1000000_elements_array, _1000000_elements_array.length), parallelDataSet);
     }
-    
-    @Test
-    public void test_10000000_elements_sort() {
-        int[] sequentialDataSet = Arrays.copyOf(_10000000_elements_array, _10000000_elements_array.length);
-        System.out.println(sequentialDataSet.length);
-        Arrays.sort(sequentialDataSet);
-        
-        assertNotNull(sequentialDataSet);
-        assertNotSame(Arrays.copyOf(_10000000_elements_array, _10000000_elements_array.length), sequentialDataSet);
-    }
-    
-    
-    @Test
-    public void test_10000000_elements_parallelsort() {
-        int[] parallelDataSet = Arrays.copyOf(_10000000_elements_array, _10000000_elements_array.length);
-        System.out.println(parallelDataSet.length);
-        Arrays.parallelSort(parallelDataSet);
-        
-        assertNotNull(parallelDataSet);
-        assertNotSame(Arrays.copyOf(_10000000_elements_array, _10000000_elements_array.length), parallelDataSet);
-    }
-    
 
     @After
     public void tearDown() throws Exception {
         sizeOfArrays = null;
-       _1000_elements_array = null;
-       _10000_elements_array = null;
-       _100000_elements_array = null;
-       _1000000_elements_array = null;
-       _10000000_elements_array = null;
+        _1000_elements_array = null;
+        _10000_elements_array = null;
+        _100000_elements_array = null;
+        _1000000_elements_array = null;
     }
-
 }
+
+
