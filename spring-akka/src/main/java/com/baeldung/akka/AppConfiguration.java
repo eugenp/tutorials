9df9e5b068982d6069eae5eea7c827c6bcc8c69e
Krzysiek <krzysztofwoyke@gmail.com>
2019-12-17 21:26:22 +0100

    BAEL-19967: Migrate spring-akka module to the com.baeldung package

--- spring-akka/src/main/java/com/baeldung/akka/AppConfiguration.java
+++ spring-akka/src/main/java/com/baeldung/akka/AppConfiguration.java
@@ -1,4 +1,4 @@
-package org.baeldung.akka;
+package com.baeldung.akka;
 
 import akka.actor.ActorSystem;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -6,8 +6,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
-import static org.baeldung.akka.SpringExtension.SPRING_EXTENSION_PROVIDER;
 
 @Configuration
 @ComponentScan
@@ -19,7 +17,7 @@ public class AppConfiguration {
     @Bean
     public ActorSystem actorSystem() {
         ActorSystem system = ActorSystem.create("akka-spring-demo");
-        SPRING_EXTENSION_PROVIDER.get(system).initialize(applicationContext);
+        SpringExtension.SPRING_EXTENSION_PROVIDER.get(system).initialize(applicationContext);
         return system;
     }
 
