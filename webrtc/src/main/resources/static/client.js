93b79e272da1c5ec1c6f8689cb25df1673954ff2
rahulgul8 <rahulgul8@gmail.com>
2019-12-19 04:34:44 +0530

    Changes for WebRTC (BAEL-3198) (#8213)
    * Adding webrtc changes
    
    * formatting
    
    * formatting pom
    
    * formatting
    
    * Adding space instead of tabs for indentation
    
    * formatting changes
    
    * Updating tabs to spaces

---
+++ webrtc/src/main/resources/static/client.js
@@ -0,0 +1,116 @@
+//connecting to our signaling server 
+var conn = new WebSocket('ws://localhost:8080/socket');
+
+conn.onopen = function() {
+    console.log("Connected to the signaling server");
+    initialize();
+};
+
+conn.onmessage = function(msg) {
+    console.log("Got message", msg.data);
+    var content = JSON.parse(msg.data);
+    var data = content.data;
+    switch (content.event) {
+    // when somebody wants to call us
+    case "offer":
+        handleOffer(data);
+        break;
+    case "answer":
+        handleAnswer(data);
+        break;
+    // when a remote peer sends an ice candidate to us
+    case "candidate":
+        handleCandidate(data);
+        break;
+    default:
+        break;
+    }
+};
+
+function send(message) {
+    conn.send(JSON.stringify(message));
+}
+
+var peerConnection;
+var dataChannel;
+var input = document.getElementById("messageInput");
+
+function initialize() {
+    var configuration = null;
+
+    peerConnection = new RTCPeerConnection(configuration, {
+        optional : [ {
+            RtpDataChannels : true
+        } ]
+    });
+
+    // Setup ice handling
+    peerConnection.onicecandidate = function(event) {
+        if (event.candidate) {
+            send({
+                event : "candidate",
+                data : event.candidate
+            });
+        }
+    };
+
+    // creating data channel
+    dataChannel = peerConnection.createDataChannel("dataChannel", {
+        reliable : true
+    });
+
+    dataChannel.onerror = function(error) {
+        console.log("Error occured on datachannel:", error);
+    };
+
+    // when we receive a message from the other peer, printing it on the console
+    dataChannel.onmessage = function(event) {
+        console.log("message:", event.data);
+    };
+
+    dataChannel.onclose = function() {
+        console.log("data channel is closed");
+    };
+}
+
+function createOffer() {
+    peerConnection.createOffer(function(offer) {
+        send({
+            event : "offer",
+            data : offer
+        });
+        peerConnection.setLocalDescription(offer);
+    }, function(error) {
+        alert("Error creating an offer");
+    });
+}
+
+function handleOffer(offer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+
+    // create and send an answer to an offer
+    peerConnection.createAnswer(function(answer) {
+        peerConnection.setLocalDescription(answer);
+        send({
+            event : "answer",
+            data : answer
+        });
+    }, function(error) {
+        alert("Error creating an answer");
+    });
+
+};
+
+function handleCandidate(candidate) {
+    peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+};
+
+function handleAnswer(answer) {
+    peerConnection.setRemoteDescription(new RTCSessionDescription(answer));
+    console.log("connection established successfully!!");
+};
+
+function sendMessage() {
+    dataChannel.send(input.value);
+    input.value = "";
+}
