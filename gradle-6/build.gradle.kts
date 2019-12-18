94be519f16312172ca9e1c7f8bdb5863507c4b1c
Johnathan Gilday <me@johnathangilday.com>
2019-12-17 21:18:54 -0500

    :construction: Gradle 6 - Platforms

--- gradle-6/build.gradle.kts
+++ gradle-6/build.gradle.kts
@@ -1,42 +1,5 @@
-plugins {
-    `java-library`
-    `maven-publish`
-}
-
-group = "com.baeldung"
-version = "1.0.0"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    api("io.reactivex.rxjava2:rxjava:2.2.16")
-    implementation("com.google.guava:guava") {
-        version {
-            require("2.0")
-            prefer("28.1-jre")
-            because("Only uses ImmutableList type, so any version since 2.0 will do, but tested with 28.1-jre")
-        }
-    }
-
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
-}
-
-tasks.compileJava {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("mavenJava") {
-            from(components["java"])
-        }
+subprojects {
+    repositories {
+        mavenCentral()
     }
 }
--- gradle-6/settings.gradle.kts
+++ gradle-6/settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "gradle-6"
+
+include("dependency-constraints")
+include("httpclient-platform")
+include("module-metadata-publishing")
+include("person-rest-client")
+include("widget-rest-client")
