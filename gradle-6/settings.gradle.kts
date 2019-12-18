99e94be7d0e4462735eeda36ea3e44265fa0be3a
Johnathan Gilday <me@johnathangilday.com>
2019-12-17 22:09:33 -0500

    :construction: Gradle 6 - Test Fixtures

--- gradle-6/settings.gradle.kts
+++ gradle-6/settings.gradle.kts
@@ -1,6 +1,8 @@
 rootProject.name = "gradle-6"
 
 include("dependency-constraints")
+include("fibonacci-spi")
+include("fibonacci-recursive")
 include("httpclient-platform")
 include("module-metadata-publishing")
 include("person-rest-client")
