354b92c029fdabd26e4a8bc9a1febeea270ab96f
Unknown <michele.guarnaccia@gmail.com>
2019-12-24 10:07:21 +0100

    Formatting issue solved

--- algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/greedy/Follower.java
+++ algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/greedy/Follower.java
@@ -4,17 +4,17 @@ import lombok.Getter;
 
 public class Follower {
 
-	@Getter String username;
-	@Getter long count;
-	
-	public Follower(String username, long count) {
-		super();
-		this.username = username;
-		this.count = count;
-	}
-	
-	@Override
-	public String toString() {
-		return "User: " + username + ", Followers: " + count + "\n\r" ; 
-	}
+    @Getter String username;
+    @Getter long count;
+    
+    public Follower(String username, long count) {
+        super();
+        this.username = username;
+        this.count = count;
+    }
+    
+    @Override
+    public String toString() {
+        return "User: " + username + ", Followers: " + count + "\n\r" ; 
+    }
 }
--- algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/greedy/SocialUser.java
+++ algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/greedy/SocialUser.java
@@ -7,32 +7,32 @@ import lombok.Getter;
 
 public class SocialUser {
 
-	@Getter private String username;
-	@Getter private List<SocialUser> followers;
+    @Getter private String username;
+    @Getter private List<SocialUser> followers;
 
-	public SocialUser(String username) {
-		super();
-		this.username = username;
-		this.followers = new ArrayList<>();
-	}
-	
-	public SocialUser(String username, List<SocialUser> followers) {
-		super();
-		this.username = username;
-		this.followers = followers;
-	}
-	
-	public long getFollowersCount() {
-		return followers.size();
-	}
-	
-	public void addFollowers(List<SocialUser> followers) {
-		this.followers.addAll(followers);
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		return ((SocialUser) obj).getUsername().equals(username);
-	}
-	
+    public SocialUser(String username) {
+        super();
+        this.username = username;
+        this.followers = new ArrayList<>();
+    }
+    
+    public SocialUser(String username, List<SocialUser> followers) {
+        super();
+        this.username = username;
+        this.followers = followers;
+    }
+    
+    public long getFollowersCount() {
+        return followers.size();
+    }
+    
+    public void addFollowers(List<SocialUser> followers) {
+        this.followers.addAll(followers);
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        return ((SocialUser) obj).getUsername().equals(username);
+    }
+    
 }
