123a5cfd55e11ba1679cb46b6fc554f64c2cbf27
amit.pandey <amit.pandey2103@gmail.com>
2019-12-27 12:48:31 +0530

    renamed the package name

--- spring-boot-mvc/src/test/java/org/baeldung/SpringContextLiveTest.java
+++ spring-boot-mvc/src/test/java/org/baeldung/SpringContextLiveTest.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import com.baeldung.autoconfiguration.MySQLAutoconfiguration;
+import com.baeldung.springbootannotations.MySQLAutoconfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = MySQLAutoconfiguration.class)
