947e039ce4445aead0cea531a27301a93602d79e
amit.pandey <amit.pandey2103@gmail.com>
2019-12-27 13:32:29 +0530

    rename package name

--- spring-boot-rest/src/main/java/com/baeldung/springpagination/repository/SubjectRepository.java
+++ spring-boot-rest/src/main/java/com/baeldung/springpagination/repository/SubjectRepository.java
@@ -1,4 +1,4 @@
-package com.baeldung.modelmapper.repository;
+package com.baeldung.springpagination.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -6,7 +6,7 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import com.baeldung.modelmapper.model.Subject;
+import com.baeldung.springpagination.model.Subject;
 
 public interface SubjectRepository extends PagingAndSortingRepository<Subject, Long> {
 
