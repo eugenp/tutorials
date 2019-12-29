6d4727fe22dd6272b97257fa51e2e90db617c2a2
mikr <michael.krimgen@ximedes.com>
2019-12-29 15:40:04 +0100

    Cucumber hooks - http://jira.baeldung.com/browse/BAEL-3590

--- testing-modules/testing-libraries/src/test/java/com/baeldung/cucumberhooks/books/BookStoreWithHooksRunSteps.java
+++ testing-modules/testing-libraries/src/test/java/com/baeldung/cucumberhooks/books/BookStoreWithHooksRunSteps.java
@@ -1,29 +1,28 @@
-package com.baeldung.cucumberbackground.books;
+package com.baeldung.cucumberhooks.books;
 
 import io.cucumber.datatable.DataTable;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.cucumber.java8.En;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-public class BookStoreRunSteps {
+public class BookStoreWithHooksRunSteps implements En {
+
     private BookStore store;
     private List<Book> foundBooks;
     private Book foundBook;
-    
-    @Before
-    public void setUp() {
+
+    public BookStoreWithHooksRunSteps() {
         store = new BookStore();
         foundBooks = new ArrayList<>();
-        System.out.print("Book store steps run simple");
     }
-    
-    @Given("^I have the following books in the store$")
+
+    @Given("^The following books are available in the store$")
     public void haveBooksInTheStore(DataTable table) {
         List<List<String>> rows = table.asLists(String.class);
 
@@ -31,29 +30,15 @@ public class BookStoreRunSteps {
             store.addBook(new Book(columns.get(0), columns.get(1)));
         }
     }
-    
-    @When("^I search for books by author (.+)$")
+
+    @When("^I ask for a book by the author (.+)$")
     public void searchForBooksByAuthor(String author) {
         foundBooks = store.booksByAuthor(author);
     }
 
-    @When("^I search for a book titled (.+)$")
-    public void searchForBookByTitle(String title) {
-        foundBook = store.bookByTitle(title).orElse(null);
-    }
-    
-    @Then("^I find (\\d+) books$")
+    @Then("^The salesperson says that there are (\\d+) books$")
     public void findBooks(int count) {
         assertEquals(count, foundBooks.size());
     }
 
-    @Then("^I find a book$")
-    public void findABook() {
-        assertNotNull(foundBook);
-    }
-
-    @Then("^I find no book$")
-    public void findNoBook() {
-        assertNull(foundBook);
-    }
 }
