cd9b7f6cfc9aa6ec845fa4303ca4b3a44920f1f8
Joao Esperancinha <jofisaes@gmail.com>
2019-12-31 15:00:33 +0100

    [BAEL-3339] - Creates @CurrentSecurityContext example

--- spring-boot-security/src/main/java/com/baeldung/springbootsecurity/oauth2server/SpringBootAuthorizationServerApplication.java
+++ spring-boot-security/src/main/java/com/baeldung/springbootsecurity/oauth2server/SpringBootAuthorizationServerApplication.java
@@ -1,7 +1,11 @@
 package com.baeldung.springbootsecurity.oauth2server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +18,8 @@ import java.security.Principal;
 @SpringBootApplication(scanBasePackages = "com.baeldung.springbootsecurity.oauth2server")
 public class SpringBootAuthorizationServerApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootAuthorizationServerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringBootAuthorizationServerApplication.class, args);
     }
@@ -24,6 +30,12 @@ public class SpringBootAuthorizationServerApplication {
         @GetMapping("/user")
         public Principal user(Principal user) {
             return user;
+        }
+
+        @GetMapping("/authentication")
+        public String authentication(@CurrentSecurityContext(expression = "authentication") Authentication authentication) {
+            logger.info("authentication -> {}", authentication);
+            return authentication.getName();
         }
 
     }
--- spring-boot-security/src/test/java/com/baeldung/springbootsecurity/oauth2server/CustomConfigAuthorizationServerIntegrationTest.java
+++ spring-boot-security/src/test/java/com/baeldung/springbootsecurity/oauth2server/CustomConfigAuthorizationServerIntegrationTest.java
@@ -1,23 +1,51 @@
 package com.baeldung.springbootsecurity.oauth2server;
 
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.internal.util.io.IOUtil;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.resource.OAuth2AccessDeniedException;
 import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 
 import static java.util.Collections.singletonList;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT, classes = SpringBootAuthorizationServerApplication.class)
 @ActiveProfiles("authz")
 public class CustomConfigAuthorizationServerIntegrationTest extends OAuth2IntegrationTestSupport {
+
+    URL base;
+
+    @LocalServerPort
+    int port;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        base = new URL("http://localhost:" + port);
+    }
 
     @Test
     public void givenOAuth2Context_whenAccessTokenIsRequested_ThenAccessTokenValueIsNotNull() {
@@ -26,7 +54,11 @@ public class CustomConfigAuthorizationServerIntegrationTest extends OAuth2Integr
 
         OAuth2AccessToken accessToken = restTemplate.getAccessToken();
 
+        final String authentication  = restTemplate.execute(base.toString() + "/authentication", HttpMethod.GET, clientHttpRequest -> {
+        }, clientHttpResponse -> IOUtils.toString(clientHttpResponse.getBody(), Charset.defaultCharset()));
+
         assertNotNull(accessToken);
+        assertEquals(authentication, "baeldung");
 
     }
 
