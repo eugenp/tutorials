139664a5499ba7db5b0b07367d8401e3c8ec33ac
eric-martin <emart.tigers@gmail.com>
2020-01-01 16:59:11 -0600

    BAEL-3517: Code Review Feedback Applied

--- java-numbers-3/src/test/java/com/baeldung/doubletolong/DoubleToLongUnitTest.java
+++ java-numbers-3/src/test/java/com/baeldung/doubletolong/DoubleToLongUnitTest.java
@@ -9,13 +9,17 @@ public class DoubleToLongUnitTest {
 
     @Test
     public void givenDoubleValue_whenLongValueCalled_thenLongValueReturned() {
-        Assert.assertEquals(9999L, Double.valueOf(VALUE)
-            .longValue());
+        Assert.assertEquals(9999L, Double.valueOf(VALUE).longValue());
     }
 
     @Test
-    public void givenDoubleValue_whenMathRoundUseds_thenLongValueReturned() {
+    public void givenDoubleValue_whenMathRoundUsed_thenLongValueReturned() {
         Assert.assertEquals(10000L, Math.round(VALUE));
+    }
+
+    @Test
+    public void givenDoubleValue_whenMathRoundUsed_thenRoundDown() {
+        Assert.assertEquals(9999L, Math.round(9999.444));
     }
 
     @Test
@@ -24,13 +28,27 @@ public class DoubleToLongUnitTest {
     }
 
     @Test
+    public void givenDoubleValue_whenMathCeilUsed_thenSameValueReturned() {
+        Assert.assertEquals(9999L, Math.ceil(9999.0), 0);
+    }
+
+    @Test
+    public void givenDoubleValue_whenMathCeilUsed_thenDifferentThanRound() {
+        Assert.assertEquals(10000L, Math.ceil(9999.444), 0);
+    }
+
+    @Test
     public void givenDoubleValue_whenMathFloorUsed_thenLongValueReturned() {
         Assert.assertEquals(9999L, Math.floor(VALUE), 0);
+    }
+
+    @Test
+    public void givenDoubleValue_whenMathFloorUsed_thenSameValueReturned() {
+        Assert.assertEquals(9999L, Math.floor(9999.0), 0);
     }
 
     @Test
     public void givenDoubleValue_whenTypeCasted_thenLongValueReturned() {
         Assert.assertEquals(9999L, (long) VALUE);
     }
-
 }
