f145e70c5ba31dc2f5359015f1b7b7095e524af8
catalin-burcea <burcea.iulian.catalin@gmail.com>
2020-01-03 18:33:14 +0200

    [BAEL-19886] - moved related articles to the new core-kotlin-modules/core-kotlin module

---
+++ core-kotlin-modules/core-kotlin/src/test/kotlin/com/baeldung/interoperability/ArrayTest.kt
@@ -0,0 +1,37 @@
+package com.baeldung.interoperability
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ArrayTest {
+
+    @Test
+    fun givenArray_whenValidateArrayType_thenComplete () {
+        val ex = com.baeldung.interoperability.ArrayExample()
+        val numArray = intArrayOf(1, 2, 3)
+
+        assertEquals(ex.sumValues(numArray), 6)
+    }
+
+    @Test
+    fun givenCustomer_whenGetSuperType_thenComplete() {
+        val instance = com.baeldung.interoperability.Customer::class
+        val supertypes = instance.supertypes
+
+        assertEquals(supertypes[0].toString(), "kotlin.Any")
+    }
+
+    @Test
+    fun givenCustomer_whenGetConstructor_thenComplete() {
+        val instance = com.baeldung.interoperability.Customer::class.java
+        val constructors = instance.constructors
+
+        assertEquals(constructors.size, 1)
+        assertEquals(constructors[0].name, "com.baeldung.interoperability.Customer")
+    }
+
+    fun makeReadFile() {
+        val ax = com.baeldung.interoperability.ArrayExample()
+        ax.writeList()
+    }
+}
