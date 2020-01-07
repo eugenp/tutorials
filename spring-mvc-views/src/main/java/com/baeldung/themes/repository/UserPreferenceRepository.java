721c60966fbd96499d673cd9ef1f0ce428603e81
sandip singh <sandip.03934@gmail.com>
2020-01-07 09:25:52 +0530

    BAEL-3283 - Moved the code to a single package meant for the themes tutorial.

--- spring-mvc-views/src/main/java/com/baeldung/themes/repository/UserPreferenceRepository.java
+++ spring-mvc-views/src/main/java/com/baeldung/themes/repository/UserPreferenceRepository.java
@@ -1,6 +1,6 @@
-package com.baeldung.repository;
+package com.baeldung.themes.repository;
 
-import com.baeldung.domain.UserPreference;
+import com.baeldung.themes.domain.UserPreference;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface UserPreferenceRepository extends PagingAndSortingRepository<UserPreference, String> {
