edebe1dbe6dbda58c9c5df1ff6d8fd2026e8e424
pazis <pazis.x@gmail.com>
2020-01-08 09:30:41 +0000

    BAEL3513

---
+++ core-java-modules/core-java-exceptions-2/src/main/java/com/baeldung/exceptions/UnknownHostExceptionHandling.java
@@ -0,0 +1,21 @@
+package com.baeldung.exceptions;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.UnknownHostException;
+
+public class UnknownHostExceptionHandling {
+
+    public int getResponseCode(String hostname) throws IOException {
+        URL url = new URL(hostname.trim());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        int resCode = -1;
+        try {
+            resCode = con.getResponseCode();
+        } catch (UnknownHostException e){
+            con.disconnect();
+        }
+        return resCode;
+    }
+}
