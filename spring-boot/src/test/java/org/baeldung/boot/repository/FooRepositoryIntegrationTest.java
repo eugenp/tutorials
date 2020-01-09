42e086fecaee130da3894c6cdd404a6496bcbe20
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2020-01-09 08:46:48 +0100

    BAEL-3733: Fix integration tests

--- spring-boot/src/test/java/org/baeldung/boot/repository/FooRepositoryIntegrationTest.java
+++ spring-boot/src/test/java/org/baeldung/boot/repository/FooRepositoryIntegrationTest.java
@@ -1,18 +1,16 @@
 package org.baeldung.boot.repository;
 
-import static org.junit.Assert.assertThat;
-
 import org.baeldung.boot.DemoApplicationIntegrationTest;
 import org.baeldung.demo.model.Foo;
 import org.baeldung.demo.repository.FooRepository;
-
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.Matchers.is;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 @Transactional
 public class FooRepositoryIntegrationTest extends DemoApplicationIntegrationTest {
@@ -28,8 +26,10 @@ public class FooRepositoryIntegrationTest extends DemoApplicationIntegrationTest
     @Test
     public void testFindByName() {
         Foo foo = fooRepository.findByName("Bar");
+
         assertThat(foo, notNullValue());
-        assertThat(foo.getId(), is(2));
+        assertThat(foo.getId(), notNullValue());
+        assertThat(foo.getName(), is("Bar"));
     }
 
 }
