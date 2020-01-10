5d50eb77096062880db11eaa72b10aad44c14128
sampada <sampada.wagde@gmail.com>
2020-01-10 17:43:48 +0530

    BAEL-3602 : Java 13 New Features

---
+++ core-java-modules/core-java-13/src/test/java/com/baeldung/newfeatures/SwitchExpressionsWithYieldUnitTest.java
@@ -0,0 +1,27 @@
+package com.baeldung.newfeatures;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SwitchExpressionsWithYieldUnitTest {
+
+    @Test
+    @SuppressWarnings("preview")
+    public void whenSwitchingOnOperationSquareMe_thenWillReturnSquare() {
+        var me = 4;
+        var operation = "squareMe";
+        var result = switch (operation) {
+        case "doubleMe" -> {
+            yield me * 2;
+        }
+        case "squareMe" -> {
+            yield me * me;
+        }
+        default -> me;
+        };
+
+        assertEquals(result, 16);
+    }
+
+}
