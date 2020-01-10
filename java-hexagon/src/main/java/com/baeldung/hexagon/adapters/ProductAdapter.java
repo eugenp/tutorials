98dc9b5e3c4e4c72a37ce8a81f8350498938e9b7
Murtaza Ghadiali <solutions.fusion@gmail.com>
2020-01-10 17:29:25 +0530

    Changes done for Product Domain Logic.

--- java-hexagon/src/main/java/com/baeldung/hexagon/adapters/ProductAdapter.java
+++ java-hexagon/src/main/java/com/baeldung/hexagon/adapters/ProductAdapter.java
@@ -3,9 +3,12 @@
  */
 package com.baeldung.hexagon.adapters;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import com.baeldung.hexagon.domain.Product;
-import com.baeldung.hexagon.domain.ProductDomainLogic;
 import com.baeldung.hexagon.domain.ProductSummary;
+import com.baeldung.hexagon.factory.AdapterFactory;
 import com.baeldung.hexagon.ports.ProductPort;
 
 public class ProductAdapter implements ProductPort {
@@ -21,8 +24,26 @@ public class ProductAdapter implements ProductPort {
 
     @Override
     public ProductSummary viewSummary(String name) {
-        ProductDomainLogic prodDomainLogic = new ProductDomainLogic();
-        return prodDomainLogic.viewSummary(name);
+        ProductSummary summary = new ProductSummary();
+        try {
+            summary.setName(name);
+            summary.setTotalQuantity(getTotal(AdapterFactory.getProducts(), name));
+        } catch (Exception e) {
+            summary.setTotalQuantity(0);
+        }
+        return summary;
+    }
+
+    private int getTotal(List<Product> products, String name) {
+        int totalQuantity = 0;
+        List<Product> availableProduct = products.stream()
+            .filter(line -> line.getName()
+                .equals(name))
+            .collect(Collectors.toList());
+        for (Product product : availableProduct) {
+            totalQuantity = totalQuantity + product.getQuantity();
+        }
+        return totalQuantity;
     }
 
 }
