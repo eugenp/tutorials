920f83a2a6b6ae05a080315d0a3eab6adba7ae8f
Sorin Zamfir <sorin.zamfir@outlook.com>
2020-01-11 22:51:19 +0200

    BAEL-3451: More samples

--- linux-bash/functions/src/main/bash/functions.sh
+++ linux-bash/functions/src/main/bash/functions.sh
@@ -66,13 +66,31 @@ function simple_comparison()
 
 # command groups with subshells 
 # with the limitation of new enviornments
+sum=0
 function simple_subshell()
     (
-        echo "I'm a little tea pot"
-        simple_comparison 10
-        cd ..
-        global_variable="ipsum"
+        declare -n sum_ref=$3
+        sum_ref=$(($1+$2))
+        # sum=$(($1+$2))
     )
+
+# variable shadowing
+variable="baeldung"
+function variable_scope(){
+    local variable="lorem"
+    echo "Variable inside function variable_scope : [$variable]"
+    variable_scope2
+}
+
+function variable_scope2(){
+    echo "Variable inside function variable_scope2 : [$variable]"
+    local variable="ipsum"
+    variable_scope3
+}
+
+function variable_scope3(){
+    echo "Variable inside function variable_scope3 : [$variable]"
+}
 
 function fibonnaci_recursion() {
     argument=$1
@@ -85,18 +103,57 @@ function fibonnaci_recursion() {
     fi 
 }
 
+function redirection_in() {
+    # echo "$1"
+    while read input;
+        do
+            echo "$input"
+        done
+} < infile
+
+function redirection_in_ps() {
+    while read input;
+        do
+            echo "$input"
+        done
+} < <(ls -ll /)
+
+
+function redirection_out_ps(){
+    declare -a output=("baeldung" "lorem" "ipsum" "caracg")
+    for element in "${output[@]}"
+        do
+            echo "$element"
+        done
+} > >(grep "g")
+
+function redirection_out() {
+    declare -a output=("baeldung" "lorem" "ipsum")
+    for element in "${output[@]}"
+        do
+            echo "$element"
+        done
+} > outfile
 
 #simple_function
 # simple_inputs one 'two three'
 # sum=$(simple_outputs 1 2)
 # echo "Sum is $sum"
 # sum=0
-ref_outputs 1 9 sumt
-echo "Sum is $sumt"
+# ref_outputs 1 9 sumt
+# echo "Sum is $sumt"
 # simple_for_loop
 # simple_comparison 6
 # simple_comparison 4
-# simple_subshell
-# echo $global_variable
+simple_subshell 1 2 sum
+echo "Sum is $sum"
 
-#echo $(fibonnaci_recursion 7)
+#variable_scope
+# echo "Variable outside function variable_scope : [$variable]"
+# FUNCNEST=5
+# echo $(fibonnaci_recursion 7)
+# echo $(fibonnaci_recursion 15)
+# redirection_in 
+# redirection_in_ps
+# redirection_out
+# redirection_out_ps
