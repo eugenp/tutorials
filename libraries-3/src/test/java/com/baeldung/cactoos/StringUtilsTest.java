ad0b2e92c5b58cc682383064387dccb917222747
Radhe Sravan <paturisravan@gmail.com>
2020-01-12 19:54:20 +0530

    BAEL-3597 Cactoos

---
+++ libraries-3/src/test/java/com/baeldung/cactoos/StringUtilsTest.java
@@ -0,0 +1,54 @@
+package com.baeldung.cactoos;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class StringUtilsTest {
+
+	@Test
+	public void whenFormattedTextIsPassedWithArgs_thenFormattedStringIsReturned() throws IOException {
+
+		StringUtils obj = new StringUtils();
+
+		// when
+		String formattedString = obj.createdFormattedString("John");
+
+		// then
+		assertEquals("Hello John", formattedString);
+
+	}
+	
+	@Test
+	public void whenStringISPassesdToLoweredOrUpperClass_thenCorrespondingStringIsReturned() throws Exception {
+
+		StringUtils obj = new StringUtils();
+
+		// when
+		String lowerCaseString = obj.toLowerCase("TeSt StrIng");
+		String upperCaseString = obj.toUpperCase("TeSt StrIng");
+
+		// then
+		assertEquals("test string", lowerCaseString);
+		assertEquals("TEST STRING", upperCaseString);
+
+	}
+	
+	@Test
+	public void whenEmptyStringISPassesd_thenISBlankReturnsTrue() throws Exception {
+
+		StringUtils obj = new StringUtils();
+
+		// when
+		boolean isBlankEmptyString = obj.isBlank("");
+		boolean isBlankNull = obj.isBlank(null);
+
+		// then
+		assertEquals(true, isBlankEmptyString);
+		assertEquals(true, isBlankNull);
+
+	}
+
+}
