04e64eb3045b7578688e0a374e2ad5c021a3eae6
Anshul BANSAL <smartdiscover17@gmail.com>
2020-01-17 16:16:23 +0200

    BAEL-3660 - Open Liberty: code cleanup

--- open-liberty/src/main/java/com/baeldung/openliberty/rest/consumes/PersonClient.java
+++ open-liberty/src/main/java/com/baeldung/openliberty/rest/consumes/PersonClient.java
@@ -2,19 +2,16 @@ package com.baeldung.openliberty.rest.consumes;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.ProcessingException;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient(configKey = "personClient", baseUri = "http://localhost:9080/")
-@RegisterProvider(UriNotFoundExceptionMapper.class)
-@Path("/api/person/1")
-public interface PersonClient extends AutoCloseable {
+public interface PersonClient {
 
     @GET
+    @Path("/api/person")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getPerson() throws UriNotFoundException, ProcessingException;
+    public String getPerson();
 }
