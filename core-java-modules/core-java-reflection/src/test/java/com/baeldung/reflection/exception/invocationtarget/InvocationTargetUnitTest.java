9e2e9bd460ad28580fc73fecba3077f010371e8d
Vikas Rajput <vkhaas.rajput1993@gmail.com>
2020-01-19 10:52:37 +0300

    BAEL-3504: Update core-java-modules/core-java-reflection/src/test/java/com/baeldung/reflection/exception/invocationtarget/InvocationTargetUnitTest.java
    Co-Authored-By: KevinGilmore <kpg102@gmail.com>

--- core-java-modules/core-java-reflection/src/test/java/com/baeldung/reflection/exception/invocationtarget/InvocationTargetUnitTest.java
+++ core-java-modules/core-java-reflection/src/test/java/com/baeldung/reflection/exception/invocationtarget/InvocationTargetUnitTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 public class InvocationTargetUnitTest {
 
     @Test
-    public void whenCallingMethodThrowsException_thenAssertTrue() throws Exception {
+    public void whenCallingMethodThrowsException_thenAssertCauseOfInvocationTargetException() throws Exception {
         InvocationTargetExample targetExample = new InvocationTargetExample();
         Method method = InvocationTargetExample.class.getMethod("divideByZeroExample");
         Exception exception = assertThrows(InvocationTargetException.class, () -> method.invoke(targetExample));
