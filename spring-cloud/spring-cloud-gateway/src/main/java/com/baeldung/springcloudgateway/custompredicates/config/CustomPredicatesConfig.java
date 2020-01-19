a11821f04a13ed5f381c654cfaa3fa35f1628380
Philippe <phil@lighthouse.com.br>
2020-01-19 13:26:41 -0300

    [BAEL-3311] Spring Cloud Gateway Routing Predicate Factories

---
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/custompredicates/config/CustomPredicatesConfig.java
@@ -0,0 +1,38 @@
+package com.baeldung.springcloudgateway.custompredicates.config;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baeldung.springcloudgateway.custompredicates.factories.GoldenCustomerRoutePredicateFactory;
+import com.baeldung.springcloudgateway.custompredicates.factories.GoldenCustomerRoutePredicateFactory.Config;
+import com.baeldung.springcloudgateway.custompredicates.service.GoldenCustomerService;
+
+@Configuration
+public class CustomPredicatesConfig {
+    
+    
+    @Bean
+    public GoldenCustomerRoutePredicateFactory goldenCustomer(GoldenCustomerService goldenCustomerService) {
+        return new GoldenCustomerRoutePredicateFactory(goldenCustomerService);
+    }
+    
+
+    //@Bean
+    public RouteLocator routes(RouteLocatorBuilder builder, GoldenCustomerRoutePredicateFactory gf ) {
+
+        return builder.routes()
+            .route("dsl_golden_route", r -> r.path("/dsl_api/**")
+                .filters(f -> f.stripPrefix(1))
+                .uri("https://httpbin.org")
+                .predicate(gf.apply(new Config(true, "customerId"))))
+            .route("dsl_common_route", r -> r.path("/dsl_api/**")
+                .filters(f -> f.stripPrefix(1))
+                .uri("https://httpbin.org")
+                .predicate(gf.apply(new Config(false, "customerId"))))             
+            .build();
+    }
+    
+}
---
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/custompredicates/factories/GoldenCustomerRoutePredicateFactory.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package com.baeldung.springcloudgateway.custompredicates.factories;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.cloud.gateway.handler.predicate.AbstractRoutePredicateFactory;
+import org.springframework.http.HttpCookie;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.server.ServerWebExchange;
+
+import com.baeldung.springcloudgateway.custompredicates.service.GoldenCustomerService;
+
+/**
+ * @author Philippe
+ *
+ */
+public class GoldenCustomerRoutePredicateFactory extends AbstractRoutePredicateFactory<GoldenCustomerRoutePredicateFactory.Config> {
+
+    private final GoldenCustomerService goldenCustomerService;
+    
+    public GoldenCustomerRoutePredicateFactory(GoldenCustomerService goldenCustomerService ) {
+        super(Config.class);
+        this.goldenCustomerService = goldenCustomerService;
+    }
+
+
+    @Override
+    public List<String> shortcutFieldOrder() {
+        return Arrays.asList("isGolden","customerIdCookie");
+    }
+
+
+    @Override
+    public Predicate<ServerWebExchange> apply(Config config) {
+        
+        return (ServerWebExchange t) -> {
+            List<HttpCookie> cookies = t.getRequest()
+                .getCookies()
+                .get(config.getCustomerIdCookie());
+              
+              boolean isGolden; 
+              if ( cookies == null || cookies.isEmpty()) {
+                  isGolden = false;
+              }
+              else {                
+                  String customerId = cookies.get(0).getValue();                
+                  isGolden = goldenCustomerService.isGoldenCustomer(customerId);
+              }
+              
+              return config.isGolden()?isGolden:!isGolden;           
+        };
+        
+    }
+    
+    
+    @Validated
+    public static class Config {
+        
+        boolean isGolden = true;
+        
+        @NotEmpty
+        String customerIdCookie = "customerId";
+        
+        
+        public Config() {}
+        
+        public Config( boolean isGolden, String customerIdCookie) {
+            this.isGolden = isGolden;
+            this.customerIdCookie = customerIdCookie;
+        }
+        
+        public boolean isGolden() {
+            return isGolden;
+        }
+        
+        public void setGolden(boolean value) {
+            this.isGolden = value;
+        }
+
+        /**
+         * @return the customerIdCookie
+         */
+        public String getCustomerIdCookie() {
+            return customerIdCookie;
+        }
+
+        /**
+         * @param customerIdCookie the customerIdCookie to set
+         */
+        public void setCustomerIdCookie(String customerIdCookie) {
+            this.customerIdCookie = customerIdCookie;
+        }
+        
+        
+        
+    }
+    
+}
