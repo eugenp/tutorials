910ac808c8c079cd22efe1ef7a95e10fc73883a5
Vikas Ramsingh Rajput <vikas.rajput@crownconsult.com>
2020-01-19 11:56:33 +0300

    BAEL-3504: Added blank lines before when and then in unit test

--- core-java-modules/core-java-reflection/src/test/java/com/baeldung/reflection/exception/invocationtarget/InvocationTargetUnitTest.java
+++ core-java-modules/core-java-reflection/src/test/java/com/baeldung/reflection/exception/invocationtarget/InvocationTargetUnitTest.java
@@ -12,9 +12,12 @@ public class InvocationTargetUnitTest {
 
     @Test
     public void whenCallingMethodThrowsException_thenAssertCauseOfInvocationTargetException() throws Exception {
+
         InvocationTargetExample targetExample = new InvocationTargetExample();
         Method method = InvocationTargetExample.class.getMethod("divideByZeroExample");
+        
         Exception exception = assertThrows(InvocationTargetException.class, () -> method.invoke(targetExample));
+        
         assertEquals(ArithmeticException.class, exception.getCause().getClass());
     }
 }
