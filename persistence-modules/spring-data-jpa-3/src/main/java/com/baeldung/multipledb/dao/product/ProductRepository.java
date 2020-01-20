9403e9737340d5aa94758998496703cd509d0341
Krzysiek <krzysztofwoyke@gmail.com>
2020-01-20 22:17:07 +0100

    BAEL-3683: Rename ProductMultipleDB entity to Product

--- persistence-modules/spring-data-jpa-3/src/main/java/com/baeldung/multipledb/dao/product/ProductRepository.java
+++ persistence-modules/spring-data-jpa-3/src/main/java/com/baeldung/multipledb/dao/product/ProductRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import com.baeldung.multipledb.model.product.ProductMultipleDB;
+import com.baeldung.multipledb.model.product.Product;
 
-public interface ProductRepository extends PagingAndSortingRepository<ProductMultipleDB, Integer> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Integer> {
 
-    List<ProductMultipleDB> findAllByPrice(double price, Pageable pageable);
+    List<Product> findAllByPrice(double price, Pageable pageable);
 }
