6c3f4fe4123edb25f6e3924ff0bdf16bed0057fc
BudBak <budbakforgit@gmail.com>
2020-01-20 13:53:26 +0530

    BAEL-3481 - Fixed Formatting.

--- algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/balancedbrackets/BalancedBracketsUsingString.java
+++ algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/balancedbrackets/BalancedBracketsUsingString.java
@@ -2,26 +2,26 @@ package com.baeldung.algorithms.balancedbrackets;
 
 public class BalancedBracketsUsingString {
 
-        public boolean isBalanced(String str) {
-                if (null == str || ((str.length() % 2) != 0)) {
-                        return false;
-                } else {
-                        char[] ch = str.toCharArray();
-                        for(char c : ch) {
-                                if(!(c == '{' || c == '[' || c == '(' || c == '}' || c == ']' || c == ')')) {
-                                        return false;
-                                }
-
-                        }
+    public boolean isBalanced(String str) {
+        if (null == str || ((str.length() % 2) != 0)) {
+            return false;
+        } else {
+            char[] ch = str.toCharArray();
+            for (char c : ch) {
+                if (!(c == '{' || c == '[' || c == '(' || c == '}' || c == ']' || c == ')')) {
+                    return false;
                 }
 
-                while (str.contains("()") || str.contains("[]") || str.contains("{}")) {
-                        str = str.replaceAll("\\(\\)", "")
-                            .replaceAll("\\[\\]", "")
-                            .replaceAll("\\{\\}", "");
-                }
-                return (str.length() == 0);
-
+            }
         }
 
-} 
+        while (str.contains("()") || str.contains("[]") || str.contains("{}")) {
+            str = str.replaceAll("\\(\\)", "")
+                .replaceAll("\\[\\]", "")
+                .replaceAll("\\{\\}", "");
+        }
+        return (str.length() == 0);
+
+    }
+
+}
--- algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/balancedbrackets/BalancedBracketsUsingDequeUnitTest.java
+++ algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/balancedbrackets/BalancedBracketsUsingDequeUnitTest.java
@@ -6,75 +6,71 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BalancedBracketsUsingDequeUnitTest {
-        private BalancedBracketsUsingDeque balancedBracketsUsingDeque;
+    private BalancedBracketsUsingDeque balancedBracketsUsingDeque;
 
-        @Before
-        public void setup() {
-                balancedBracketsUsingDeque = new BalancedBracketsUsingDeque();
-        }
+    @Before
+    public void setup() {
+        balancedBracketsUsingDeque = new BalancedBracketsUsingDeque();
+    }
 
+    @Test
+    public void givenNullInput_whenCheckingForBalance_shouldReturnFalse() {
+        boolean result = balancedBracketsUsingDeque.isBalanced(null);
+        assertThat(result).isFalse();
+    }
 
-        @Test
-        public void givenNullInput_whenCheckingForBalance_shouldReturnFalse() {
-                boolean result = balancedBracketsUsingDeque.isBalanced(null);
-                assertThat(result).isFalse();
-        }
+    @Test
+    public void givenEmptyString_whenCheckingForBalance_shouldReturnTrue() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("");
+        assertThat(result).isTrue();
+    }
 
-        @Test
-        public void givenEmptyString_whenCheckingForBalance_shouldReturnTrue() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("");
-                assertThat(result).isTrue();
-        }
+    @Test
+    public void givenInvalidCharacterString_whenCheckingForBalance_shouldReturnFalse() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("abc[](){}");
+        assertThat(result).isFalse();
+    }
 
-        @Test
-        public void givenInvalidCharacterString_whenCheckingForBalance_shouldReturnFalse() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("abc[](){}");
-                assertThat(result).isFalse();
-        }
+    @Test
+    public void givenOddLengthString_whenCheckingForBalance_shouldReturnFalse() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{{[]()}}}");
+        assertThat(result).isFalse();
+    }
 
-        @Test
-        public void givenOddLengthString_whenCheckingForBalance_shouldReturnFalse() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{{[]()}}}");
-                assertThat(result).isFalse();
-        }
+    @Test
+    public void givenEvenLengthString_whenCheckingForBalance_shouldReturnFalse() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{{[]()}}}}");
+        assertThat(result).isFalse();
+    }
 
-        @Test
-        public void givenEvenLengthString_whenCheckingForBalance_shouldReturnFalse() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{{[]()}}}}");
-                assertThat(result).isFalse();
-        }
+    @Test
+    public void givenEvenLengthUnbalancedString_whenCheckingForBalance_shouldReturnFalse() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{[(])}");
+        assertThat(result).isFalse();
+    }
 
-        @Test
-        public void givenEvenLengthUnbalancedString_whenCheckingForBalance_shouldReturnFalse() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{[(])}");
-                assertThat(result).isFalse();
-        }
+    @Test
+    public void givenEvenLengthBalancedString_whenCheckingForBalance_shouldReturnTrue() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{[()]}");
+        assertThat(result).isTrue();
+    }
 
+    @Test
+    public void givenBalancedString_whenCheckingForBalance_shouldReturnTrue() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{{[[(())]]}}");
+        assertThat(result).isTrue();
+    }
 
-        @Test
-        public void givenEvenLengthBalancedString_whenCheckingForBalance_shouldReturnTrue() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{[()]}");
-                assertThat(result).isTrue();
-        }
+    @Test
+    public void givenAnotherBalancedString_whenCheckingForBalance_shouldReturnTrue() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{{([])}}");
+        assertThat(result).isTrue();
+    }
 
-        @Test
-        public void givenBalancedString_whenCheckingForBalance_shouldReturnTrue() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{{[[(())]]}}");
-                assertThat(result).isTrue();
-        }
+    @Test
+    public void givenUnBalancedString_whenCheckingForBalance_shouldReturnFalse() {
+        boolean result = balancedBracketsUsingDeque.isBalanced("{{)[](}}");
+        assertThat(result).isFalse();
+    }
 
-
-        @Test
-        public void givenAnotherBalancedString_whenCheckingForBalance_shouldReturnTrue() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{{([])}}");
-                assertThat(result).isTrue();
-        }
-
-
-        @Test
-        public void givenUnBalancedString_whenCheckingForBalance_shouldReturnFalse() {
-                boolean result = balancedBracketsUsingDeque.isBalanced("{{)[](}}");
-                assertThat(result).isFalse();
-        }
-
-} 
+}
