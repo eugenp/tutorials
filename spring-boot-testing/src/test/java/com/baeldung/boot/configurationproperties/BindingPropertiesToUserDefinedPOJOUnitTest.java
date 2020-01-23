5b245cde28d427c3b15c08d02dd181f606dea561
m.raheem <m.abdelbaset.dev@gmail.com>
2020-01-23 21:42:44 +0200

    Using java7 syntax for Map and Set

--- spring-boot-testing/src/test/java/com/baeldung/boot/configurationproperties/BindingPropertiesToUserDefinedPOJOUnitTest.java
+++ spring-boot-testing/src/test/java/com/baeldung/boot/configurationproperties/BindingPropertiesToUserDefinedPOJOUnitTest.java
@@ -1,6 +1,9 @@
 package com.baeldung.boot.configurationproperties;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
@@ -20,13 +23,17 @@ public class BindingPropertiesToUserDefinedPOJOUnitTest {
 
 		assertEquals("node1", serverConfig.getName());
 
-		assertEquals(Set.of("img1.jpg", "img2.jpg"),
-				serverConfig.getImgIds());
+		Set<String> expectedImgs = new HashSet<>();
+		expectedImgs.add("img1.jpg");
+		expectedImgs.add("img2.jpg");
+		assertEquals(expectedImgs, serverConfig.getImgIds());
 
 		assertEquals("192.168.0.1", serverConfig.getAddress().getIp());
 		assertEquals(8099, serverConfig.getAddress().getPort());
 
-		assertEquals(Map.of("imgs", "/root/imgs", "html", "/root/html"), 
-				serverConfig.getDirs());
+		Map<String, String> expectedDirs = new HashMap<>();
+		expectedDirs.put("imgs", "/root/imgs");
+		expectedDirs.put("html", "/root/html");
+		assertEquals(expectedDirs, serverConfig.getDirs());
 	}
 }
