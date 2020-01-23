04a36b0b0945a770ebb8284bd55f8cebbf7352aa
Gian Mario Contessa <gmcontessa@hedgingbeta.com>
2020-01-23 08:38:35 +0000

    BAEL-3353: updated test following feedback

--- play-framework/async-http/test/controllers/HomeControllerTest.java
+++ play-framework/async-http/test/controllers/HomeControllerTest.java
@@ -1,6 +1,18 @@
 package controllers;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+import static play.mvc.Http.Status.SERVICE_UNAVAILABLE;
+
+import java.time.Duration;
+import java.util.OptionalInt;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -14,22 +26,6 @@ import play.libs.ws.ahc.AhcCurlRequestLogger;
 import play.mvc.Result;
 import play.mvc.Results;
 import play.test.WithServer;
-
-import java.time.Duration;
-import java.util.OptionalInt;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.IntStream;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
-import static org.junit.Assert.assertEquals;
-import static play.mvc.Http.Status.SERVICE_UNAVAILABLE;
-
-import play.mvc.Http.MultipartFormData.*;
-import akka.stream.javadsl.*;
-import akka.util.ByteString;
 
 public class HomeControllerTest extends WithServer {
 
@@ -49,7 +45,8 @@ public class HomeControllerTest extends WithServer {
             port = optHttpsPort.getAsInt();
             url = "https://localhost:" + port;
         } else {
-            port = testServer.getRunningHttpPort().getAsInt();
+            port = testServer.getRunningHttpPort()
+                             .getAsInt();
             url = "http://localhost:" + port;
         }
     }
@@ -57,7 +54,6 @@ public class HomeControllerTest extends WithServer {
     @Test
     public void givenASingleGetRequestWhenResponseThenBlockWithCompletableAndLog()
       throws InterruptedException, ExecutionException {
-        AtomicInteger completedReqs = new AtomicInteger(0);
         WSClient ws = play.test.WSTestClient.newClient(port);
         WSResponse wsResponse = ws.url(url)
                                   .setRequestFilter(new AhcCurlRequestLogger())
@@ -75,8 +71,10 @@ public class HomeControllerTest extends WithServer {
     }
 
     @Test
-    public void givenASingleGetRequestWhenResponseThenLog() throws InterruptedException {
+    public void givenASingleGetRequestWhenResponseThenLog() throws Exception {
         AtomicInteger completedReqs = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(1);
+
         WSClient ws = play.test.WSTestClient.newClient(port);
         ws.url(url)
           .setRequestFilter(new AhcCurlRequestLogger())
@@ -88,20 +86,22 @@ public class HomeControllerTest extends WithServer {
                                           .getId() + " Request complete: Response code = "
                 + r.getStatus()
                 + " | Response: " + r.getBody() + " | Current Time:" + System.currentTimeMillis());
+              latch.countDown();
               completedReqs.incrementAndGet();
           });
 
         log.debug(
           "Waiting for requests to be completed. Current Time: " + System.currentTimeMillis());
-        while (completedReqs.get() != 1) {
-            Thread.sleep(100);
-        }
-        log.debug("All requests have been completed. Exiting test.");
+        latch.await();
+        assertEquals(1, completedReqs.get());
+        log.debug("All requests have1 been completed. Exiting test.");
     }
 
     @Test
-    public void givenASinglePostRequestWhenResponseThenLog() throws InterruptedException {
+    public void givenASinglePostRequestWhenResponseThenLog() throws Exception {
         AtomicInteger completedReqs = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(1);
+
         WSClient ws = play.test.WSTestClient.newClient(port);
         ws.url(url)
           .setContentType("application/x-www-form-urlencoded")
@@ -111,20 +111,22 @@ public class HomeControllerTest extends WithServer {
                                           .getId() + " Request complete: Response code = "
                 + r.getStatus()
                 + " | Response: " + r.getBody() + " | Current Time:" + System.currentTimeMillis());
+              latch.countDown();
               completedReqs.incrementAndGet();
           });
 
         log.debug(
           "Waiting for requests to be completed. Current Time: " + System.currentTimeMillis());
-        while (completedReqs.get() != 1) {
-            Thread.sleep(100);
-        }
+        latch.await();
+        assertEquals(1, completedReqs.get());
         log.debug("All requests have been completed. Exiting test.");
     }
 
     @Test
-    public void givenMultipleRequestsWhenResponseThenLog() throws InterruptedException {
+    public void givenMultipleRequestsWhenResponseThenLog() throws Exception {
         AtomicInteger completedReqs = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(100);
+
         WSClient ws = play.test.WSTestClient.newClient(port);
         IntStream.range(0, 100)
                  .parallel()
@@ -139,46 +141,58 @@ public class HomeControllerTest extends WithServer {
                            "Thread#" + num + " Request complete: Response code = " + r.getStatus()
                              + " | Response: " + r.getBody() + " | Current Time:"
                              + System.currentTimeMillis());
+                         latch.countDown();
                          completedReqs.incrementAndGet();
                      })
                  );
 
         log.debug(
           "Waiting for requests to be completed. Current Time: " + System.currentTimeMillis());
-        while (completedReqs.get() != 100) {
-            Thread.sleep(100);
-        }
+        latch.await();
+        assertEquals(100, completedReqs.get());
         log.debug("All requests have been completed. Exiting test.");
     }
 
     @Test
-    public void givenLongResponseWhenTimeoutThenHandle() throws InterruptedException {
+    public void givenLongResponseWhenTimeoutThenHandle() throws Exception {
+        AtomicInteger completedReqs = new AtomicInteger(0);
+        CountDownLatch latch = new CountDownLatch(1);
+
         WSClient ws = play.test.WSTestClient.newClient(port);
         Futures futures = app.injector()
                              .instanceOf(Futures.class);
-        CompletionStage<Result> f = futures.timeout(ws.url(url)
-                                                      .setRequestTimeout(Duration.of(1, SECONDS))
-                                                      .get()
-                                                      .thenApply(result -> {
-                                                          try {
-                                                              Thread.sleep(2000L);
-                                                              return Results.ok();
-                                                          } catch (InterruptedException e) {
-                                                              return Results.status(
-                                                                SERVICE_UNAVAILABLE);
-                                                          }
-                                                      }), 1L, TimeUnit.SECONDS);
+        CompletionStage<Result> f = futures.timeout(
+          ws.url(url)
+            .setRequestTimeout(Duration.of(1, SECONDS))
+            .get()
+            .thenApply(result -> {
+                try {
+                    Thread.sleep(2000L);
+                    return Results.ok();
+                } catch (InterruptedException e) {
+                    return Results.status(
+                      SERVICE_UNAVAILABLE);
+                }
+            }), 1L, TimeUnit.SECONDS
+        );
         CompletionStage<Object> res = f.handleAsync((result, e) -> {
             if (e != null) {
                 log.error("Exception thrown", e);
+                completedReqs.incrementAndGet();
+                latch.countDown();
                 return e.getCause();
             } else {
                 return result;
             }
         });
-        res.thenAccept(result ->
-          assertEquals(TimeoutException.class, result)
-        );
-        Thread.sleep(2500);
+        res.thenAccept(result -> {
+            assertEquals(TimeoutException.class, result);
+        });
+
+        log.debug(
+          "Waiting for requests to be completed. Current Time: " + System.currentTimeMillis());
+        latch.await();
+        assertEquals(1, completedReqs.get());
+        log.debug("All requests have been completed. Exiting test.");
     }
 }
