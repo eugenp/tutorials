d59bd5f066c3231039b566f5eed6b270ab81eae8
Martin van Wingerden <martin@martinvw.nl>
2020-01-26 15:14:18 +0100

    [BAEL-2397] Text Blocks
    Added code/configuration for the article about Text Blocks

---
+++ core-java-modules/core-java-14/src/main/java/com/baeldung/java14/textblocks/TextBlocks14.java
@@ -0,0 +1,16 @@
+package com.baeldung.java14.textblocks;
+
+public class TextBlocks14 {
+public String getIgnoredNewLines() {
+    return """
+            This is a long test which looks to \
+            have a newline but actually does not""";
+}
+
+public String getEscapedSpaces() {
+    return """
+            line 1
+            line 2       \s
+            """;
+}
+}
