8144040512b45445879570c4ac2b7a331e24b287
Sorin Zamfir <sorin.zamfir@outlook.com>
2020-01-27 21:14:55 +0200

    BAEL-3777: Fixed some spring init issues

--- ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/CassandraDbOrderRepository.java
+++ ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/CassandraDbOrderRepository.java
@@ -3,10 +3,21 @@ package com.baeldung.dddhexagonalspring.infrastracture.repository;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.baeldung.dddhexagonalspring.domain.Order;
 import com.baeldung.dddhexagonalspring.domain.repository.OrderRepository;
 
+@Component
 public class CassandraDbOrderRepository implements OrderRepository {
+
+    private final SpringDataCassandraOrderRepository orderRepository;
+    
+    @Autowired
+    public CassandraDbOrderRepository(SpringDataCassandraOrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
 
     @Override
     public Optional<Order> findById(UUID id) {
--- ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/MongoDbOrderRepository.java
+++ ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/MongoDbOrderRepository.java
@@ -3,12 +3,14 @@ package com.baeldung.dddhexagonalspring.infrastracture.repository;
 import com.baeldung.dddhexagonalspring.domain.Order;
 import com.baeldung.dddhexagonalspring.domain.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Component
+@Primary
 public class MongoDbOrderRepository implements OrderRepository {
 
     private final SpringDataMongoOrderRepository orderRepository;
