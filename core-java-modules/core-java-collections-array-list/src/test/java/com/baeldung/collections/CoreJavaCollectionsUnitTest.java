262261088cbb0ce4292418f698aa6a09aaac9311
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2020-01-27 13:21:52 +0100

    BAEL-3476: Align methods naming

--- core-java-modules/core-java-collections-array-list/src/test/java/com/baeldung/collections/CoreJavaCollectionsUnitTest.java
+++ core-java-modules/core-java-collections-array-list/src/test/java/com/baeldung/collections/CoreJavaCollectionsUnitTest.java
@@ -20,35 +20,42 @@ public class CoreJavaCollectionsUnitTest {
 
     @Test
     public final void givenUsingTheJdk_whenArrayListIsSynchronized_thenCorrect() {
-        final List<String> list = new ArrayList<String>(Arrays.asList("one", "two", "three"));
+        final List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
         final List<String> synchronizedList = Collections.synchronizedList(list);
         LOG.debug("Synchronized List is: " + synchronizedList);
     }
 
     @Test(expected = UnsupportedOperationException.class)
-    public final void givenUsingTheJdk_whenUnmodifiableListIsCreatedFromOriginal_thenNoLongerModifiable() {
-        final List<String> list = new ArrayList<String>(Arrays.asList("one", "two", "three"));
+    public final void givenUsingTheJdk_whenUnmodifiableListIsCreated_thenNotModifiable() {
+        final List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
         final List<String> unmodifiableList = Collections.unmodifiableList(list);
         unmodifiableList.add("four");
     }
 
     @Test(expected = UnsupportedOperationException.class)
-    public final void givenUsingGuava_whenUnmodifiableListIsCreatedFromOriginal_thenNoLongerModifiable() {
-        final List<String> list = new ArrayList<String>(Arrays.asList("one", "two", "three"));
+    public final void givenUsingTheJava9_whenUnmodifiableListIsCreated_thenNotModifiable() {
+        final List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
+        // final List<String> unmodifiableList = // Java 9 List.of code goes here
+        // unmodifiableList.add("four");
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public final void givenUsingGuava_whenUnmodifiableListIsCreated_thenNotModifiable() {
+        final List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
         final List<String> unmodifiableList = ImmutableList.copyOf(list);
         unmodifiableList.add("four");
     }
 
     @Test(expected = UnsupportedOperationException.class)
-    public final void givenUsingGuavaBuilder_whenUnmodifiableListIsCreatedFromOriginal_thenNoLongerModifiable() {
-        final List<String> list = new ArrayList<String>(Arrays.asList("one", "two", "three"));
+    public final void givenUsingGuavaBuilder_whenUnmodifiableListIsCreated_thenNoLongerModifiable() {
+        final List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
         final ImmutableList<String> unmodifiableList = ImmutableList.<String>builder().addAll(list).build();
         unmodifiableList.add("four");
     }
 
     @Test(expected = UnsupportedOperationException.class)
-    public final void givenUsingCommonsCollections_whenUnmodifiableListIsCreatedFromOriginal_thenNoLongerModifiable() {
-        final List<String> list = new ArrayList<String>(Arrays.asList("one", "two", "three"));
+    public final void givenUsingCommonsCollections_whenUnmodifiableListIsCreated_thenNotModifiable() {
+        final List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
         final List<String> unmodifiableList = ListUtils.unmodifiableList(list);
         unmodifiableList.add("four");
     }
