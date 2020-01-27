da869efa41d05bac5d05b0fee40e607a22127e84
Sorin Zamfir <sorin.zamfir@outlook.com>
2020-01-27 21:14:55 +0200

    BAEL-3777: Fixed some spring init issues

--- ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/SpringDataCassandraOrderRepository.java
+++ ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/SpringDataCassandraOrderRepository.java
@@ -3,9 +3,11 @@ package com.baeldung.dddhexagonalspring.infrastracture.repository;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
 import com.baeldung.dddhexagonalspring.domain.Order;
 
+@Repository
 public interface SpringDataCassandraOrderRepository extends CassandraRepository<Order, UUID>{
 
 }
