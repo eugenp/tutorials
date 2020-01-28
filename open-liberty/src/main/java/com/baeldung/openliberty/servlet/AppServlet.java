4477bcd0aca7d2523601754757577521e9d6338a
Anshul BANSAL <smartdiscover17@gmail.com>
2020-01-28 15:43:28 +0200

    review fixes

--- open-liberty/src/main/java/com/baeldung/openliberty/servlet/AppServlet.java
+++ open-liberty/src/main/java/com/baeldung/openliberty/servlet/AppServlet.java
@@ -13,11 +13,13 @@ public class AppServlet extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
 
+    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String htmlOutput = "<html><h2>Hello! Welcome to Open Liberty</h2></html>";
         response.getWriter().append(htmlOutput);
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         doGet(request, response);
