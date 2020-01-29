0d9033a6e7c561b4bfb0452bb6dd7635292374e2
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2020-01-29 11:38:46 -0800

    formatted the code as per guidelines

--- patterns/design-patterns-architectural/src/test/java/com/baeldung/hexagonpattern/test/LibraryProcessingUnitTest.java
+++ patterns/design-patterns-architectural/src/test/java/com/baeldung/hexagonpattern/test/LibraryProcessingUnitTest.java
@@ -12,7 +12,7 @@ import com.baeldung.hexagonpattern.factory.AdapterFactory;
 import com.baeldung.hexagonpattern.ports.ExtendMaterialPort;
 
 public class LibraryProcessingUnitTest {
-	
+
 	@Test
 	public void givenStatusOfBooks_whenProcessed_thenTwoAssertion() throws Exception {
 		ExtendMaterialPort itemProcessor = AdapterFactory.getOrderProcessor("MOBILE");
@@ -22,7 +22,7 @@ public class LibraryProcessingUnitTest {
 		assertThat(status.getMaterialStatus()).contains("extended").doesNotContain("failed");
 		assertThat(status.getDueDate()).isInstanceOf(LocalDate.class);
 	}
-	
+
 	@Test
 	public void givenStatusOfInvalidItem_whenProcessed_thenOneAssertion() throws Exception {
 		ExtendMaterialPort itemProcessor = AdapterFactory.getOrderProcessor("MOBILE");
@@ -32,12 +32,11 @@ public class LibraryProcessingUnitTest {
 		assertThat(status.getMaterialStatus()).contains("failed").doesNotContain("extended");
 	}
 
-	
 	@Test
 	public void givenInvalidEntryPoint_whenProcessed_thenOneAssertion() throws Exception {
 		assertThatThrownBy(() -> {
 			ExtendMaterialPort itemProcessor = AdapterFactory.getOrderProcessor("Counter");
 		}).isInstanceOf(Exception.class)
-		  .hasMessageContaining("Since it is a holiday, extending the book from  COUNTER is not possible");
+				.hasMessageContaining("Since it is a holiday, extending the book from  COUNTER is not possible");
 	}
 }
