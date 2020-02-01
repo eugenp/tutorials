c1099603f4664f3901a3c96206a30b64713fcbd3
Sorin Zamfir <sorin.zamfir@outlook.com>
2020-02-01 13:02:43 +0200

    BAEL-3777: Cleanup

--- ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/cassandra/SpringDataCassandraOrderRepository.java
+++ ddd/src/main/java/com/baeldung/dddhexagonalspring/infrastracture/repository/cassandra/SpringDataCassandraOrderRepository.java
@@ -5,8 +5,6 @@ import java.util.UUID;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
 
-import com.baeldung.dddhexagonalspring.domain.Order;
-
 @Repository
 public interface SpringDataCassandraOrderRepository extends CassandraRepository<OrderEntity, UUID>{
 
