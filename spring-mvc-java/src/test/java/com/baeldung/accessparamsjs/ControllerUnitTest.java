cd745e0b8d239d5321227c39db1edb1acb7c1143
dupirefr <dupire.francois+pro@gmail.com>
2020-02-09 11:20:42 +0100

    * Moved tests as well

--- spring-mvc-java/src/test/java/com/baeldung/accessparamsjs/ControllerUnitTest.java
+++ spring-mvc-java/src/test/java/com/baeldung/accessparamsjs/ControllerUnitTest.java
@@ -1,7 +1,5 @@
 package com.baeldung.accessparamsjs;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
