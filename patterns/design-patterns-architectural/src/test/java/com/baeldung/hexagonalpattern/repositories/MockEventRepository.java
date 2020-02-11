f0a8c18c3a5bac4d9421238f8d28095c89694d8e
Eduard Ardeleanu <eduard-andrei.ardeleanu@1and1.ro>
2020-02-11 14:41:46 +0200

    Practical example of Hexagonal Architecture in Java

---
+++ patterns/design-patterns-architectural/src/test/java/com/baeldung/hexagonalpattern/repositories/MockEventRepository.java
@@ -0,0 +1,22 @@
+package com.baeldung.hexagonalpattern.repositories;
+
+import com.baeldung.hexagonalpattern.entities.Event;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class MockEventRepository implements EventRepository {
+
+    private final List<Event> events = new ArrayList<>();
+
+    @Override
+    public void saveEvent(Event event) {
+        this.events.add(event);
+    }
+
+    @Override
+    public List<Event> getAllEvents() {
+        return Collections.unmodifiableList(this.events);
+    }
+}
---
+++ patterns/design-patterns-architectural/src/test/java/com/baeldung/hexagonalpattern/services/EventServiceUnitTest.java
@@ -0,0 +1,32 @@
+package com.baeldung.hexagonalpattern.services;
+
+import com.baeldung.hexagonalpattern.entities.Event;
+import com.baeldung.hexagonalpattern.repositories.EventRepository;
+import com.baeldung.hexagonalpattern.repositories.MockEventRepository;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventServiceUnitTest {
+
+    private static EventService eventService;
+
+    @BeforeClass
+    public static void setEventServiceInstance() {
+        EventRepository eventRepository = new MockEventRepository();
+        eventService = new EventService(eventRepository);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void givenEventService_whenNullEventIsSaved_thenThrowIllegalArgumentException() {
+        eventService.save(null);
+    }
+
+    @Test
+    public void givenEventService_whenEventIsSaved_thenSuccess() {
+        Event event = new Event(1, "Meeting");
+        eventService.save(event);
+        assertThat(eventService.getAllEvents().size()).isEqualTo(1);
+    }
+}
