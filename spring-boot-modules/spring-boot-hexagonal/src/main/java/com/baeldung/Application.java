91da58e880b411d0a99000d555359c41d5af59f2
ajay74984 <51405689+ajay74984@users.noreply.github.com>
2020-02-12 16:06:47 +0100

    initial commit of Hexagonal Architecture

---
+++ spring-boot-modules/spring-boot-hexagonal/src/main/java/com/baeldung/Application.java
@@ -0,0 +1,13 @@
+package com.ajay.documentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentserviceApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentserviceApplication.class, args);
+	}
+	
+}
---
+++ spring-boot-modules/spring-boot-hexagonal/src/main/java/com/baeldung/adapter/DocumentRestAdapter.java
@@ -0,0 +1,24 @@
+package com.ajay.documentservice.adapter;
+
+import com.ajay.documentservice.domain.Document;
+import com.ajay.documentservice.port.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/doc")
+public class DocumentRestAdapter {
+	@Autowired
+	private DocumentService documentService;
+	
+	@PostMapping
+	public void createDocument(@RequestBody Document document) {
+		documentService.createDocument(document);
+	}
+	
+	@GetMapping("/{id}")
+	public Document findById(@PathVariable String id) {
+		return documentService.findById(id);
+	}
+	
+}
