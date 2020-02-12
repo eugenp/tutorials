7feb7bf9d8d22dfc015b969eda13e5c76e3e6426
ajay74984 <51405689+ajay74984@users.noreply.github.com>
2020-02-12 17:15:44 +0100

    renamed packages.

--- spring-boot-modules/spring-boot-hexagonal/src/main/java/com/baeldung/Application.java
+++ spring-boot-modules/spring-boot-hexagonal/src/main/java/com/baeldung/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DocumentserviceApplication {
+public class Application {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(DocumentserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 	
 }
