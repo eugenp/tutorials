53183ba16689ba7a33eefd02179e7f22c443ebaf
Justin Albano <albano.justin@gmail.com>
2020-02-14 10:51:26 -0500

    BAEL-3744: Corrected test method names and added XML-based tests.

--- spring-core-3/src/test/java/com/baeldung/spring/patterns/factory/AnnotationConfigApplicationContextTest.java
+++ spring-core-3/src/test/java/com/baeldung/spring/patterns/factory/AnnotationConfigApplicationContextTest.java
@@ -7,12 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AnnotationConfigApplicationContextTest {
 
     @Test
     @SuppressWarnings("resource")
-    public void whenGetSimpleBean_ThenReturnConstructedBean() {
+    public void whenGetSimpleBean_thenReturnConstructedBean() {
         
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         
@@ -23,10 +24,34 @@ public class AnnotationConfigApplicationContextTest {
 
     @Test
     @SuppressWarnings("resource")
-    public void whenGetPrototypeBean_ThenReturnConstructedBean() {
+    public void whenGetPrototypeBean_thenReturnConstructedBean() {
         
         String expectedName = "Some name";
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        
+        Bar bar = context.getBean(Bar.class, expectedName);
+        
+        assertNotNull(bar);
+        assertThat(bar.getName(), is(expectedName));
+    }
+    
+    @Test
+    @SuppressWarnings("resource")
+    public void givenXmlConfiguration_whenGetSimpleBean_thenReturnConstructedBean() {
+        
+        ApplicationContext context = new ClassPathXmlApplicationContext("patterns-context.xml");
+        
+        Foo foo = context.getBean(Foo.class);
+        
+        assertNotNull(foo);
+    }
+
+    @Test
+    @SuppressWarnings("resource")
+    public void givenXmlConfiguration_whenGetPrototypeBean_thenReturnConstructedBean() {
+        
+        String expectedName = "Some name";
+        ApplicationContext context = new ClassPathXmlApplicationContext("patterns-context.xml");
         
         Bar bar = context.getBean(Bar.class, expectedName);
         
