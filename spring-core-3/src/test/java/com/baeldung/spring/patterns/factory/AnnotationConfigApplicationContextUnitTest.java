e3310d5ddb43cda71f0d1d71ac7a0abb044af4e5
Justin Albano <albano.justin@gmail.com>
2020-02-14 15:02:48 -0500

    BAEL-3744: Corrected unit test class names; split unit tests into two classes.

--- spring-core-3/src/test/java/com/baeldung/spring/patterns/factory/AnnotationConfigApplicationContextUnitTest.java
+++ spring-core-3/src/test/java/com/baeldung/spring/patterns/factory/AnnotationConfigApplicationContextUnitTest.java
@@ -7,9 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AnnotationConfigApplicationContextTest {
+public class AnnotationConfigApplicationContextUnitTest {
 
     @Test
     @SuppressWarnings("resource")
@@ -28,30 +27,6 @@ public class AnnotationConfigApplicationContextTest {
         
         String expectedName = "Some name";
         ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-        
-        Bar bar = context.getBean(Bar.class, expectedName);
-        
-        assertNotNull(bar);
-        assertThat(bar.getName(), is(expectedName));
-    }
-    
-    @Test
-    @SuppressWarnings("resource")
-    public void givenXmlConfiguration_whenGetSimpleBean_thenReturnConstructedBean() {
-        
-        ApplicationContext context = new ClassPathXmlApplicationContext("patterns-context.xml");
-        
-        Foo foo = context.getBean(Foo.class);
-        
-        assertNotNull(foo);
-    }
-
-    @Test
-    @SuppressWarnings("resource")
-    public void givenXmlConfiguration_whenGetPrototypeBean_thenReturnConstructedBean() {
-        
-        String expectedName = "Some name";
-        ApplicationContext context = new ClassPathXmlApplicationContext("patterns-context.xml");
         
         Bar bar = context.getBean(Bar.class, expectedName);
         
