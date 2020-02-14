7c99e13c05b981e6d8cc288fea5d1a34f5860fdd
Mona Mohamadinia <mona.mohamadinia@gmail.com>
2020-02-15 00:27:00 +0330

    Resotre Delete Tests

--- testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/conditional/ConditionalAnnotationsUnitTest.java
+++ testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/conditional/ConditionalAnnotationsUnitTest.java
@@ -1,5 +1,6 @@
 package com.baeldung.junit5.conditional;
 
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.*;
 
@@ -70,6 +71,42 @@ public class ConditionalAnnotationsUnitTest {
         System.out.println("will not run if environment variable LC_TIME is UTF-8");
     }
 
+    // Commented codes are going to work prior JUnit 5.5
+
+    @Test
+    // @EnabledIf("'FR' == systemProperty.get('user.country')")
+    public void onlyFrenchPeopleWillRunThisMethod() {
+        System.out.println("will run only if user.country is FR");
+    }
+
+    @Test
+    // @DisabledIf("java.lang.System.getProperty('os.name').toLowerCase().contains('mac')")
+    public void shouldNotRunOnMacOS() {
+        System.out.println("will not run if our os.name is mac");
+    }
+
+    @Test
+    /*@EnabledIf(value = {
+      "load('nashorn:mozilla_compat.js')",
+      "importPackage(java.time)",
+      "",
+      "var thisMonth = LocalDate.now().getMonth().name()",
+      "var february = Month.FEBRUARY.name()",
+      "thisMonth.equals(february)"
+    },
+      engine = "nashorn",
+      reason = "Self-fulfilling: {result}")*/
+    public void onlyRunsInFebruary() {
+        System.out.println("this test only runs in February");
+    }
+
+    @Test
+    /*@DisabledIf("systemEnvironment.get('XPC_SERVICE_NAME') != null " +
+      "&& systemEnvironment.get('XPC_SERVICE_NAME').contains('intellij')")*/
+    public void notValidForIntelliJ() {
+        System.out.println("this test will run if our ide is INTELLIJ");
+    }
+
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
     @Test
@@ -81,5 +118,17 @@ public class ConditionalAnnotationsUnitTest {
     @ThisTestWillOnlyRunAtLinuxAndMacWithJava9Or10Or11
     public void someSuperTestMethodHere() {
         System.out.println("this method will run with java9, 10, 11 and Linux or macOS.");
+    }
+
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.RUNTIME)
+    // @DisabledIf("Math.random() >= 0.5")
+    @interface CoinToss {
+    }
+
+    @RepeatedTest(2)
+    @CoinToss
+    public void gamble() {
+        System.out.println("This tests run status is a gamble with %50 rate");
     }
 }
