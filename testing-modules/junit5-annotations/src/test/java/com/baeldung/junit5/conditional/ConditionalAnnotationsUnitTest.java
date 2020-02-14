63702494d7fd9e950ab992397954af09645fff0f
Mona Mohamadinia <mona.mohamadinia@gmail.com>
2020-02-14 21:38:47 +0330

    Upgraded to Jupiter 5.6.0
    Also removed some deprecated stuff!

--- testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/conditional/ConditionalAnnotationsUnitTest.java
+++ testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/conditional/ConditionalAnnotationsUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.junit5.conditional;
 
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.*;
 
@@ -10,6 +9,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 public class ConditionalAnnotationsUnitTest {
+
     @Test
     @EnabledOnOs({OS.WINDOWS, OS.MAC})
     public void shouldRunBothWindowsAndMac() {
@@ -58,40 +58,6 @@ public class ConditionalAnnotationsUnitTest {
         System.out.println("will not run if environment variable LC_TIME is UTF-8");
     }
 
-    @Test
-    @EnabledIf("'FR' == systemProperty.get('user.country')")
-    public void onlyFrenchPeopleWillRunThisMethod() {
-        System.out.println("will run only if user.country is FR");
-    }
-
-    @Test
-    @DisabledIf("java.lang.System.getProperty('os.name').toLowerCase().contains('mac')")
-    public void shouldNotRunOnMacOS() {
-        System.out.println("will not run if our os.name is mac");
-    }
-
-    @Test
-    @EnabledIf(value = {
-            "load('nashorn:mozilla_compat.js')",
-            "importPackage(java.time)",
-            "",
-            "var thisMonth = LocalDate.now().getMonth().name()",
-            "var february = Month.FEBRUARY.name()",
-            "thisMonth.equals(february)"
-    },
-            engine = "nashorn",
-            reason = "Self-fulfilling: {result}")
-    public void onlyRunsInFebruary() {
-        System.out.println("this test only runs in February");
-    }
-
-    @Test
-    @DisabledIf("systemEnvironment.get('XPC_SERVICE_NAME') != null " +
-            "&& systemEnvironment.get('XPC_SERVICE_NAME').contains('intellij')")
-    public void notValidForIntelliJ() {
-        System.out.println("this test will run if our ide is INTELLIJ");
-    }
-
     @Target(ElementType.METHOD)
     @Retention(RetentionPolicy.RUNTIME)
     @Test
@@ -103,17 +69,5 @@ public class ConditionalAnnotationsUnitTest {
     @ThisTestWillOnlyRunAtLinuxAndMacWithJava9Or10Or11
     public void someSuperTestMethodHere() {
         System.out.println("this method will run with java9, 10, 11 and Linux or macOS.");
-    }
-
-    @Target(ElementType.METHOD)
-    @Retention(RetentionPolicy.RUNTIME)
-    @DisabledIf("Math.random() >= 0.5")
-    @interface CoinToss {
-    }
-
-    @RepeatedTest(2)
-    @CoinToss
-    public void gamble() {
-        System.out.println("This tests run status is a gamble with %50 rate");
     }
 }
