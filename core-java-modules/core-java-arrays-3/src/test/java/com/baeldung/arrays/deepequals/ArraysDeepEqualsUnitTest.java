9ba4bc4a5c1f9d31dc57a6afbc5630825cb9350a
Florin Hriscu <alexflorin.hriscu@gmail.com>
2020-02-16 17:34:31 +0200

    BAEL-3502 Apply correct formatter.

--- core-java-modules/core-java-arrays-3/src/test/java/com/baeldung/arrays/deepequals/ArraysDeepEqualsUnitTest.java
+++ core-java-modules/core-java-arrays-3/src/test/java/com/baeldung/arrays/deepequals/ArraysDeepEqualsUnitTest.java
@@ -9,92 +9,88 @@ import org.junit.jupiter.api.Test;
 
 public class ArraysDeepEqualsUnitTest {
 
-	class Person {
-		private int id;
-		private String name;
-		private int age;
+    class Person {
+        private int id;
+        private String name;
+        private int age;
 
-		Person(int id, String name, int age) {
-			this.id = id;
-			this.name = name;
-			this.age = age;
-		}
+        Person(int id, String name, int age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj) {
-				return true;
-			}
-			if (obj == null) {
-				return false;
-			}
-			if (!(obj instanceof Person))
-				return false;
-			Person person = (Person) obj;
-			return id == person.id && name.equals(person.name) && age == person.age;
-		}
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (!(obj instanceof Person))
+                return false;
+            Person person = (Person) obj;
+            return id == person.id && name.equals(person.name) && age == person.age;
+        }
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(id, name, age);
-		}
-	}
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, age);
+        }
+    }
 
-	@Test
-	void givenTwoUnidimensionalObjectTypeArrays_whenUsingEqualsAndDeepEquals_thenBothShouldReturnTrue() {
-		Object[] anArray = new Object[] { "string1", "string2", "string3" };
-		Object[] anotherArray = new Object[] { "string1", "string2", "string3" };
+    @Test
+    void givenTwoUnidimensionalObjectTypeArrays_whenUsingEqualsAndDeepEquals_thenBothShouldReturnTrue() {
+        Object[] anArray = new Object[] { "string1", "string2", "string3" };
+        Object[] anotherArray = new Object[] { "string1", "string2", "string3" };
 
-		assertTrue(Arrays.equals(anArray, anotherArray));
-		assertTrue(Arrays.deepEquals(anArray, anotherArray));
-	}
+        assertTrue(Arrays.equals(anArray, anotherArray));
+        assertTrue(Arrays.deepEquals(anArray, anotherArray));
+    }
 
-	@Test
-	void givenTwoUnidimensionalObjectTypeArraysWithNullElements_whenUsingEqualsAndDeepEquals_thenBothShouldReturnTrue() {
-		Object[] anArray = new Object[] { "string1", null, "string3" };
-		Object[] anotherArray = new Object[] { "string1", null, "string3" };
+    @Test
+    void givenTwoUnidimensionalObjectTypeArraysWithNullElements_whenUsingEqualsAndDeepEquals_thenBothShouldReturnTrue() {
+        Object[] anArray = new Object[] { "string1", null, "string3" };
+        Object[] anotherArray = new Object[] { "string1", null, "string3" };
 
-		assertTrue(Arrays.equals(anArray, anotherArray));
-		assertTrue(Arrays.deepEquals(anArray, anotherArray));
-	}
+        assertTrue(Arrays.equals(anArray, anotherArray));
+        assertTrue(Arrays.deepEquals(anArray, anotherArray));
+    }
 
-	@Test
-	void givenTwoUnidimensionalObjectTypeArraysWithNestedElements_whenUsingEqualsAndDeepEquals_thenShouldReturnDifferently() {
-		Object[] anArray = new Object[] { "string1", null, new String[] { "nestedString1", "nestedString2" } };
-		Object[] anotherArray = new Object[] { "string1", null, new String[] { "nestedString1", "nestedString2" } };
+    @Test
+    void givenTwoUnidimensionalObjectTypeArraysWithNestedElements_whenUsingEqualsAndDeepEquals_thenShouldReturnDifferently() {
+        Object[] anArray = new Object[] { "string1", null, new String[] { "nestedString1", "nestedString2" } };
+        Object[] anotherArray = new Object[] { "string1", null, new String[] { "nestedString1", "nestedString2" } };
 
-		assertFalse(Arrays.equals(anArray, anotherArray));
-		assertTrue(Arrays.deepEquals(anArray, anotherArray));
-	}
+        assertFalse(Arrays.equals(anArray, anotherArray));
+        assertTrue(Arrays.deepEquals(anArray, anotherArray));
+    }
 
-	@Test
-	void givenTwoMultidimensionalPrimitiveTypeArrays_whenUsingEqualsAndDeepEquals_thenBothShouldReturnDifferently() {
-		int[][] anArray = { { 1, 2, 3 }, { 4, 5, 6, 9 }, { 7 } };
-		int[][] anotherArray = { { 1, 2, 3 }, { 4, 5, 6, 9 }, { 7 } };
+    @Test
+    void givenTwoMultidimensionalPrimitiveTypeArrays_whenUsingEqualsAndDeepEquals_thenBothShouldReturnDifferently() {
+        int[][] anArray = { { 1, 2, 3 }, { 4, 5, 6, 9 }, { 7 } };
+        int[][] anotherArray = { { 1, 2, 3 }, { 4, 5, 6, 9 }, { 7 } };
 
-		assertFalse(Arrays.equals(anArray, anotherArray));
-		assertTrue(Arrays.deepEquals(anArray, anotherArray));
-	}
+        assertFalse(Arrays.equals(anArray, anotherArray));
+        assertTrue(Arrays.deepEquals(anArray, anotherArray));
+    }
 
-	@Test
-	void givenTwoMultidimensionalObjectTypeArrays_whenUsingEqualsAndDeepEquals_thenBothShouldReturnDifferently() {
-		Person personArray1[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) },
-				{ new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
-		Person personArray2[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) },
-				{ new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
+    @Test
+    void givenTwoMultidimensionalObjectTypeArrays_whenUsingEqualsAndDeepEquals_thenBothShouldReturnDifferently() {
+        Person personArray1[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) }, { new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
+        Person personArray2[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) }, { new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
 
-		assertFalse(Arrays.equals(personArray1, personArray2));
-		assertTrue(Arrays.deepEquals(personArray1, personArray2));
-	}
+        assertFalse(Arrays.equals(personArray1, personArray2));
+        assertTrue(Arrays.deepEquals(personArray1, personArray2));
+    }
 
-	@Test
-	void givenTwoMultidimensionalObjectTypeArrays_whenUsingDeepEqualsFromObjectsAndArraysClasses_thenBothShouldReturnTrue() {
-		Person personArray1[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) },
-				{ new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
-		Person personArray2[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) },
-				{ new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
+    @Test
+    void givenTwoMultidimensionalObjectTypeArrays_whenUsingDeepEqualsFromObjectsAndArraysClasses_thenBothShouldReturnTrue() {
+        Person personArray1[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) }, { new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
+        Person personArray2[][] = { { new Person(1, "John", 22), new Person(2, "Mike", 23) }, { new Person(3, "Steve", 27), new Person(4, "Gary", 28) } };
 
-		assertTrue(Objects.deepEquals(personArray1, personArray2));
-		assertTrue(Arrays.deepEquals(personArray1, personArray2));
-	}
+        assertTrue(Objects.deepEquals(personArray1, personArray2));
+        assertTrue(Arrays.deepEquals(personArray1, personArray2));
+    }
 }
