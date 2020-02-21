63b4db7f7a4253e486d4b13fffadd4f26f89c8f6
Unknown <michele.guarnaccia@gmail.com>
2020-02-21 14:17:46 +0100

    BAEL-3855
    identation, moved to new module, removed needless classes, better benchmark

--- lock-striping/src/com/baeldung/concurrent/lock/LockStriped.java
+++ lock-striping/src/com/baeldung/concurrent/lock/LockStriped.java
@@ -7,41 +7,41 @@ import com.google.common.base.Supplier;
 import com.google.common.util.concurrent.Striped;
 
 public class LockStriped extends ConcurrentAccessMap {
-	Striped<Lock> lock;
+    Striped<Lock> lock;
 
-	public LockStriped(Map<String, String> map) {
-		super(map);
-		lock = getStripedLock();
-	}
+    public LockStriped(Map<String, String> map) {
+        super(map);
+        lock = getStripedLock();
+    }
 
-	private Striped<Lock> getStripedLock() {
-		Striped<Lock> map = Striped.lock(BUCKETS);
-		return map;
-	}
-	
-	protected Supplier<?> putSupplier(int x) {
-		return (()-> {
-			Lock currentLock = lock.get("key" + x);
-			boolean done = false;
-			while(!done) {
-				done = currentLock.tryLock();
-        	}
-    		map.put("key" + x, "value" + x);
-    		currentLock.unlock();
-    		return null;
-		});
-	}
-	
-	protected Supplier<?> getSupplier(int x) {
-		return (()-> {
-			Lock currentLock = lock.get("key" + x);
-			boolean done = false;
-			while(!done) {
-				done = currentLock.tryLock();
-        	}
-    		map.get("key" + x);
-    		currentLock.unlock();
-    		return null;
-		});
-	}
+    private Striped<Lock> getStripedLock() {
+        Striped<Lock> map = Striped.lock(BUCKETS);
+        return map;
+    }
+
+    protected Supplier<?> putSupplier(int x) {
+        return (()-> {
+            Lock currentLock = lock.get("key" + x);
+            boolean done = false;
+            while(!done) {
+                done = currentLock.tryLock();
+            }
+            map.put("key" + x, "value" + x);
+            currentLock.unlock();
+            return null;
+        });
+    }
+
+    protected Supplier<?> getSupplier(int x) {
+        return (()-> {
+            Lock currentLock = lock.get("key" + x);
+            boolean done = false;
+            while(!done) {
+                done = currentLock.tryLock();
+            }
+            map.get("key" + x);
+            currentLock.unlock();
+            return null;
+        });
+    }
 }
