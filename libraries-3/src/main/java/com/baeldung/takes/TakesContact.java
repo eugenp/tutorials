ad1894d022cdae81e688dd22d60e1acd10dba1f0
Anshul BANSAL <smartdiscover17@gmail.com>
2020-02-24 15:17:18 +0200

    Takes - review changes

--- libraries-3/src/main/java/com/baeldung/takes/TakesContact.java
+++ libraries-3/src/main/java/com/baeldung/takes/TakesContact.java
@@ -10,10 +10,13 @@ import org.takes.rs.RsWithStatus;
 import org.takes.rs.RsWithType;
 
 public final class TakesContact implements Take {
-    
+
     @Override
     public Response act(Request req) throws IOException {
-        return new RsWithStatus(new RsWithType(new RsWithBody("Contact us at https://www.baeldung.com"), "text/html"), 200);
-    }
+        return new RsWithStatus(
+            new RsWithType(
+                new RsWithBody("Contact us at https://www.baeldung.com"), 
+                "text/html"), 200);
+    } 
 
 }
