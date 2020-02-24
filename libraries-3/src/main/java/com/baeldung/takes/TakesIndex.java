93e7e1a51cca6732d83c65443e22515bb180ec8c
Anshul BANSAL <smartdiscover17@gmail.com>
2020-02-24 15:17:18 +0200

    Takes - review changes

--- libraries-3/src/main/java/com/baeldung/takes/TakesIndex.java
+++ libraries-3/src/main/java/com/baeldung/takes/TakesIndex.java
@@ -10,12 +10,15 @@ import org.takes.rs.RsHtml;
 import org.takes.rs.RsVelocity;
 
 public final class TakesIndex implements Take {
-    
+
     @Override
     public Response act(final Request req) throws IOException {
         RqFormSmart form = new RqFormSmart(req);
         String username = form.single("username");
-        return new RsHtml(new RsVelocity(this.getClass().getResource("/templates/index.vm"), new RsVelocity.Pair("username", username)));
+        return new RsHtml(
+            new RsVelocity(this.getClass().getResource("/templates/index.vm"), 
+                new RsVelocity.Pair("username", username))
+            );
     }
 
 }
