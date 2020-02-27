7045c3f8b42d06cdcc69058e05532428f6270287
m.raheem <m.abdelbaset.dev@gmail.com>
2020-02-27 16:59:16 +0200

    Autowiring ServerConfig in @Bean method test class

--- spring-boot-modules/spring-boot-testing/src/main/java/com/baeldung/boot/configurationproperties/ServerConfigFactory.java
+++ spring-boot-modules/spring-boot-testing/src/main/java/com/baeldung/boot/configurationproperties/ServerConfigFactory.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServerConfigFactory {
 
-    @Bean
+    @Bean(name = "default_bean")
     @ConfigurationProperties(prefix = "server.default")
     public ServerConfig getDefaultConfigs() {
         return new ServerConfig();
