a6c090f66b75d61ca5cb1523d0c0d6ff072caef2
Anshul Bansal <smartdiscover17@gmail.com>
2020-03-01 15:07:50 +0200

    BAEL_3760-Introduction-to-Takes (#8767)
    * Initial commit
    
    * Intro to Takes
    
    * Takes - code
    
    * Code changes
    
    * Takes - unit and integration test
    
    * Takes - hit-refresh feature
    
    * Takes - renamed hit-refresh to reload
    
    * Takes - review changes

---
+++ libraries-3/src/main/java/com/baeldung/takes/TakesContact.java
@@ -0,0 +1,22 @@
+package com.baeldung.takes;
+
+import java.io.IOException;
+
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.rs.RsWithBody;
+import org.takes.rs.RsWithStatus;
+import org.takes.rs.RsWithType;
+
+public final class TakesContact implements Take {
+
+    @Override
+    public Response act(Request req) throws IOException {
+        return new RsWithStatus(
+            new RsWithType(
+                new RsWithBody("Contact us at https://www.baeldung.com"), 
+                "text/html"), 200);
+    } 
+
+}
