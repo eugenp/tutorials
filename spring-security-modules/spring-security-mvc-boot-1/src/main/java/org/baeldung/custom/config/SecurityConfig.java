77be69798059a66ea214f6d40bd715e015e67d2d
Mathieu Fortin <mathieufortin01@gmail.com>
2020-03-02 15:02:08 -0500

    BAEL-3900 Fixed ApplicationLiveTest.

--- spring-security-modules/spring-security-mvc-boot-1/src/main/java/org/baeldung/custom/config/SecurityConfig.java
+++ spring-security-modules/spring-security-mvc-boot-1/src/main/java/org/baeldung/custom/config/SecurityConfig.java
@@ -2,12 +2,25 @@ package org.baeldung.custom.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf()
+            .disable()
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .formLogin().permitAll();
+    }
+    
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(11);
