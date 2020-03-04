2731f094dc4af786224e521e8edec42f6720b5af
David Calap <dcalap@gmail.com>
2020-03-04 07:28:51 +0100

    BAEL-3461 Added an extra method to have all null warn cases. Steps to reproduce and fix in the code.

--- libraries/src/main/java/com/baeldung/nullaway/NullAwayExample.java
+++ libraries/src/main/java/com/baeldung/nullaway/NullAwayExample.java
@@ -2,12 +2,21 @@ package com.baeldung.nullaway;
 
 import com.baeldung.distinct.Person;
 
-import javax.annotation.Nullable;
-
 public class NullAwayExample {
 
-    static void printAge(@Nullable Person person) {
-        System.out.println(person.getAge());
+    /*
+     * 1- NullAway will warn about showPersonAge method
+     * 2- Uncomment @Nullable in printAge and NullAway will warn about this method
+     * 3- Add a standard null check to be NullAway compliant
+     * 4- Build will be SUCCESS
+     */
+
+    static Integer printAge(/*@Nullable*/ Person person) {
+        return person.getAge();
+    }
+
+    static void showPersonAge() {
+        printAge(null);
     }
 
 }
