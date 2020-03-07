e0bdb9810ee873d9cd6e253cdba8400546211d3a
David Calap <dcalap@gmail.com>
2020-03-07 20:55:57 +0100

    BAEL-3461 Renamed and modified example methods for the sake of the article.

--- libraries/src/main/java/com/baeldung/nullaway/NullAwayExample.java
+++ libraries/src/main/java/com/baeldung/nullaway/NullAwayExample.java
@@ -5,18 +5,20 @@ import com.baeldung.distinct.Person;
 public class NullAwayExample {
 
     /*
-     * 1- NullAway will warn about showPersonAge method
+     * 1- NullAway will warn about yearsToRetirement method
      * 2- Uncomment @Nullable in printAge and NullAway will warn about this method
      * 3- Add a standard null check to be NullAway compliant
      * 4- Build will be SUCCESS
      */
 
-    static Integer printAge(/*@Nullable*/ Person person) {
+    static Integer getAge(/*@Nullable*/ Person person) {
         return person.getAge();
     }
 
-    static void showPersonAge() {
-        printAge(null);
+    Integer yearsToRetirement() {
+        Person p = null;
+        // ... p never gets set correctly...
+        return 65 - getAge(p);
     }
 
 }
