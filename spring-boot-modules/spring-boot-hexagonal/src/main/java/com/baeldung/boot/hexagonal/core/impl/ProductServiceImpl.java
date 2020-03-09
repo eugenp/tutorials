b60578eb950ef9bddd44b4a96685d4826747178c
alex.peptan <alex07pep@gmail.com>
2020-03-09 09:31:03 +0200

    A quick and practical example of Hexagonal Architecture in Java

---
+++ spring-boot-modules/spring-boot-hexagonal/src/main/java/com/baeldung/boot/hexagonal/core/impl/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.baeldung.boot.hexagonal.core.impl;
+
+import com.baeldung.boot.hexagonal.core.domain.Product;
+import com.baeldung.boot.hexagonal.port.ProductRepo;
+import com.baeldung.boot.hexagonal.port.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepo productRepository;
+
+    public ProductServiceImpl(ProductRepo productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void addProduct(Product product) {
+        productRepository.addProduct(product);
+    }
+
+    @Override
+    public Product getProduct(Long productNumber) {
+        return productRepository.getProduct(productNumber);
+    }
+
+    @Override
+    public List<Product> allProducts() {
+        return productRepository.allProducts();
+    }
+}
