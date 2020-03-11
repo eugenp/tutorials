df9f9ad4b69e3772f706189203855493775acac4
Korek, Patrycjusz <Patrycjusz.Korek@finastra.com>
2020-03-11 20:45:14 +0100

    Revert "Example of Hexagonal pattern"
    This reverts commit f6360438c16c1197fc512c62799f848614bac95c.

--- patterns/hexagonal-pattern/src/main/java/com/baeldung/hexbookstore/infrastructure/DBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hexbookstore.infrastructure;
-
-import com.baeldung.hexbookstore.core.BookStore;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DBRepository extends CrudRepository<BookStore, Long> {
-}
