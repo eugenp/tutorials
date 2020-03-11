3b19ea08c1c1cd2ea306e16f1abd24a9bf9c4e3e
sampadawagde <sampada.wagde@gmail.com>
2020-03-11 18:18:27 +0530

    JAVA-4 : Split hibernate5 module to smaller parts

--- persistence-modules/hibernate5/src/test/java/com/baeldung/hibernate/multitenancy/schema/TenantIdNames.java
+++
@@ -1,6 +0,0 @@
-package com.baeldung.hibernate.multitenancy.schema;
-
-public class TenantIdNames {
-    public static final String MYDB1 = "mydb1";
-    public static final String MYDB2 = "mydb2";
-}
