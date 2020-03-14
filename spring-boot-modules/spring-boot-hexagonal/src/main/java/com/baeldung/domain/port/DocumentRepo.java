83ea1a11266876926ec2f1ee17fb39e183b6b0c2
ajay74984 <51405689+ajay74984@users.noreply.github.com>
2020-03-14 12:25:31 +0100

    REmoved files

--- spring-boot-modules/spring-boot-hexagonal/src/main/java/com/baeldung/domain/port/DocumentRepo.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.domain.port;
-
-import com.baeldung.domain.Document;
-
-public interface DocumentRepo {
-    void storeDocument(Document document);
-
-    Document findDocumentById(String id);
-}
