73d7f4f3acaa823757b65323db8aa9ad4dfef2aa
chirag d <chirag.dewan22@gmail.com>
2020-03-14 12:24:44 +0530

    BAEL-3901 Clear Site Data Header in Spring Security

---
+++ spring-security-modules/spring-security-clear-site-data/src/main/java/com/baeldung/WebConfig.java
@@ -0,0 +1,19 @@
+package com.baeldung;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = {"com.baeldung"})
+public class WebConfig implements WebMvcConfigurer {
+}
