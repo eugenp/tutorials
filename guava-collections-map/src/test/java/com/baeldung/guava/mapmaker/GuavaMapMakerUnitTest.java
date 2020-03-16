d3b351e27f4e21857ffa92d8acb5f5a5efba86eb
ramkumarvenkat <ramkumar.venkat@gmail.com>
2020-03-16 08:30:23 +0530

    fix formatting

--- guava-collections-map/src/test/java/com/baeldung/guava/mapmaker/GuavaMapMakerUnitTest.java
+++ guava-collections-map/src/test/java/com/baeldung/guava/mapmaker/GuavaMapMakerUnitTest.java
@@ -8,28 +8,33 @@ import java.util.concurrent.ConcurrentMap;
 import static org.junit.Assert.assertNotNull;
 
 public class GuavaMapMakerUnitTest {
-        @Test public void whenMakeMap_thenCreated() {
-                ConcurrentMap<String, String> concurrentMap = new MapMaker().makeMap();
-                assertNotNull(concurrentMap);
-        }
+    @Test
+    public void whenMakeMap_thenCreated() {
+        ConcurrentMap<String, String> concurrentMap = new MapMaker().makeMap();
+        assertNotNull(concurrentMap);
+    }
 
-        @Test public void whenMakeMapWithWeakKeys_thenCreated() {
-                ConcurrentMap<String, String> concurrentMap = new MapMaker().weakKeys().makeMap();
-                assertNotNull(concurrentMap);
-        }
+    @Test
+    public void whenMakeMapWithWeakKeys_thenCreated() {
+        ConcurrentMap<String, String> concurrentMap = new MapMaker().weakKeys().makeMap();
+        assertNotNull(concurrentMap);
+    }
 
-        @Test public void whenMakeMapWithWeakValues_thenCreated() {
-                ConcurrentMap<String, String> concurrentMap = new MapMaker().weakValues().makeMap();
-                assertNotNull(concurrentMap);
-        }
+    @Test
+    public void whenMakeMapWithWeakValues_thenCreated() {
+        ConcurrentMap<String, String> concurrentMap = new MapMaker().weakValues().makeMap();
+        assertNotNull(concurrentMap);
+    }
 
-        @Test public void whenMakeMapWithInitialCapacity_thenCreated() {
-                ConcurrentMap<String, String> concurrentMap = new MapMaker().initialCapacity(10).makeMap();
-                assertNotNull(concurrentMap);
-        }
+    @Test
+    public void whenMakeMapWithInitialCapacity_thenCreated() {
+        ConcurrentMap<String, String> concurrentMap = new MapMaker().initialCapacity(10).makeMap();
+        assertNotNull(concurrentMap);
+    }
 
-        @Test public void whenMakeMapWithConcurrencyLevel_thenCreated() {
-                ConcurrentMap<String, String> concurrentMap = new MapMaker().concurrencyLevel(10).makeMap();
-                assertNotNull(concurrentMap);
-        }
+    @Test
+    public void whenMakeMapWithConcurrencyLevel_thenCreated() {
+        ConcurrentMap<String, String> concurrentMap = new MapMaker().concurrencyLevel(10).makeMap();
+        assertNotNull(concurrentMap);
+    }
 }
