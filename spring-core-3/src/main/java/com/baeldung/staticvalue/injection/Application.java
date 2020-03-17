3d2df762daf4c88beae8925fef5eb77a507b0513
Georgios Charitakis <georgios.charitakis@prodyna.com>
2020-03-17 22:22:34 +0200

    BAEL-3835: integration of code into the src directory

--- spring-core-3/src/main/java/com/baeldung/staticvalue/injection/Application.java
+++ spring-core-3/src/main/java/com/baeldung/staticvalue/injection/Application.java
@@ -1,9 +1,12 @@
-package com.baeldung;
+package com.baeldung.staticvalue.injection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("/application.properties")
+
 public class Application {
 
 	public static void main(String[] args) {
--- spring-core-3/src/main/java/com/baeldung/staticvalue/injection/PropertyController.java
+++ spring-core-3/src/main/java/com/baeldung/staticvalue/injection/PropertyController.java
@@ -1,7 +1,6 @@
-package com.baeldung.controller;
+package com.baeldung.staticvalue.injection;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
