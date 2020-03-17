f55a86f19f742f20ff139095c7d24994953f2399
kkaravitis <kkaravitis@gmail.com>
2020-03-17 15:55:04 +0200

    Removed evaluation article code

--- patterns/hexagonal-architecture-example/src/main/java/com/baeldung/hexagonal/architecture/example/application/ports/in/GetCocktailsPort.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hexagonal.architecture.example.application.ports.in;
-
-import java.util.List;
-
-import com.baeldung.hexagonal.architecture.example.application.domain.Cocktail;
-
-public interface GetCocktailsPort {
-    List<Cocktail> getCocktails();
-}
