1a7fb4b9d0af65201514e600727ca10f1df3bf1a
Vikas Ramsingh Rajput <vikas.rajput@crownconsult.com>
2020-03-18 19:59:02 +0300

    BAEL-3832: Removed logger checks from the test cases

--- spring-core-3/src/main/java/com/baeldung/ioccontainer/bean/CustomBeanFactoryPostProcessor.java
+++ spring-core-3/src/main/java/com/baeldung/ioccontainer/bean/CustomBeanFactoryPostProcessor.java
@@ -1,16 +1,14 @@
 package com.baeldung.ioccontainer.bean;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 
 public class CustomBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
-    static final Logger LOGGER = LogManager.getLogger(CustomBeanPostProcessor.class.getName());
-
+    public static boolean isBeanInstantiated = false;
+    
     @Override
     public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
-        LOGGER.info("BeanFactoryPostProcessor is Registered");
+        isBeanInstantiated = true;
     }
 }
