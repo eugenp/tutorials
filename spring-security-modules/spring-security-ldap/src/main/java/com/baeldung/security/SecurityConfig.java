d6e6e5a8cad28041a88fc5ed8e49fb480430acb3
dupirefr <dupire.francois+pro@gmail.com>
2020-03-19 21:01:20 +0100

    [JAVA-961] Standardize packages in spring-security-modules: spring-security-ldap

--- spring-security-modules/spring-security-ldap/src/main/java/com/baeldung/security/SecurityConfig.java
+++ spring-security-modules/spring-security-ldap/src/main/java/com/baeldung/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package org.baeldung.security;
+package com.baeldung.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
