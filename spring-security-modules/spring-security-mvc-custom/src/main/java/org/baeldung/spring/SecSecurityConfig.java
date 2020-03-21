5484fb6cb548eb574619451cfda3b174751b90c2
mikr <michael.krimgen@ximedes.com>
2020-03-21 21:43:32 +0100

    JAVA-877 Update redirect after login

--- spring-security-modules/spring-security-mvc-custom/src/main/java/org/baeldung/spring/SecSecurityConfig.java
+++ spring-security-modules/spring-security-mvc-custom/src/main/java/org/baeldung/spring/SecSecurityConfig.java
@@ -1,11 +1,9 @@
 package org.baeldung.spring;
 
 import org.baeldung.security.MySimpleUrlAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,7 +13,7 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 //@ImportResource({ "classpath:webSecurityConfig.xml" })
 @EnableWebSecurity
 public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     public SecSecurityConfig() {
         super();
     }
@@ -26,43 +24,34 @@ public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
             return super.authenticationManagerBean();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // @formatter:off
-        auth.inMemoryAuthentication()
-            .withUser("user1").password("{noop}user1Pass").roles("USER")
-            .and()
-            .withUser("admin1").password("{noop}admin1Pass").roles("ADMIN");
-        // @formatter:on
-    }
-
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         // @formatter:off
-        http.authorizeRequests()
-            .antMatchers("/anonymous*").anonymous()
-            .antMatchers("/login*").permitAll()
-            .anyRequest().authenticated()
-            
+        http
+            .authorizeRequests()
+                .antMatchers("/anonymous*").anonymous()
+                .antMatchers("/login*").permitAll()
+                .anyRequest().authenticated()
+
             .and()
             .formLogin()
-            .loginPage("/login.html")
-            .loginProcessingUrl("/login")
-            .successHandler(myAuthenticationSuccessHandler())
-            .failureUrl("/login.html?error=true")
-            
+                .loginPage("/login.html")
+                .loginProcessingUrl("/login")
+                .successHandler(myAuthenticationSuccessHandler())
+                .failureUrl("/login.html?error=true")
+
             .and()
-            .logout().deleteCookies("JSESSIONID")
-            
+                .logout().deleteCookies("JSESSIONID")
+
             .and()
-            .rememberMe().key("uniqueAndSecret").tokenValiditySeconds(86400)
-            
+                .rememberMe().key("uniqueAndSecret").tokenValiditySeconds(86400)
+
             .and()
-            .csrf().disable()
+                .csrf().disable()
             ;
         // @formatter:on
     }
-    
+
     @Bean
     public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
         return new MySimpleUrlAuthenticationSuccessHandler();
