73981f73357002eab7619c445723dc459b4b07e4
dupirefr <dupire.francois+pro@gmail.com>
2020-03-22 09:51:32 +0100

    [JAVA-961] Standardize packages in spring-security-modules: spring-security-mvn-custom (finalized)

--- spring-security-modules/spring-security-mvc-custom/src/main/java/com/baeldung/spring/MyUserDetailsService.java
+++ spring-security-modules/spring-security-mvc-custom/src/main/java/com/baeldung/spring/MyUserDetailsService.java
@@ -1,4 +1,4 @@
-package org.baeldung.spring;
+package com.baeldung.spring;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
