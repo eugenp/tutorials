dd72c90ae46e29d3058524730effb2b836bec96a
sampadawagde <sampada.wagde@gmail.com>
2020-03-28 16:48:04 +0530

    JAVA-623: Moved 3 articles from core-java-security to security-2

--- core-java-modules/core-java-security/src/main/java/com/baeldung/hashing/DigestAlgorithms.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hashing;
-
-public class DigestAlgorithms {
-
-    public static final String SHA3_256 = "SHA3-256";
-    public static final String SHA_256 = "SHA-256";
-    public static final String KECCAK_256 = "Keccak-256";
-
-}
