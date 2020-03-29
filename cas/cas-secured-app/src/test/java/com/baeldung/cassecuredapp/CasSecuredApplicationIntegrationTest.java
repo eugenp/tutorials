65e21d3f6a394cbfc34f6c6d8b49d92a02a4aa9e
Seun Matt <smatt382@gmail.com>
2020-03-29 21:36:54 +0100

    example code for BAEL-3749 (#8815)
    * example code for BAEL-3749
    
    * added live test
    
    * added live test
    
    * improved exception handling in response log filter
    
    * propage exception in example code
    
    * updated repo with upstream
    
    * added example code for BAEL-3293
    
    * updated the example code for BAEL-3749
    
    * updated the example code for BAEL-3749
    
    * updated the example code for BAEL-3749
    
    * updated the example code for BAEL-3749

--- cas/cas-secured-app/src/test/java/com/baeldung/cassecuredapp/CasSecuredApplicationIntegrationTest.java
+++ cas/cas-secured-app/src/test/java/com/baeldung/cassecuredapp/CasSecuredApplicationIntegrationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CasSecuredAppApplicationIntegrationTest {
+public class CasSecuredApplicationIntegrationTest {
 
 	@Test
 	public void contextLoads() {
