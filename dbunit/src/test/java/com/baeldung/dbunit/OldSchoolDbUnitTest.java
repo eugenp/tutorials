e3ac902d6f6e085f002ea631897ae1995aab1394
Joao Esperancinha <joao.esperancinha@eonics.nl>
2020-03-29 21:47:00 +0200

    [BAEL-2749] Code refactoring

--- dbunit/src/test/java/com/baeldung/dbunit/OldSchoolDbUnitTest.java
+++ dbunit/src/test/java/com/baeldung/dbunit/OldSchoolDbUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.dbunit;
 
-import org.dbunit.Assertion;
 import org.dbunit.IDatabaseTester;
 import org.dbunit.JdbcDatabaseTester;
 import org.dbunit.dataset.IDataSet;
@@ -17,14 +16,14 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 
+import static com.baeldung.dbunit.ConnectionSettings.JDBC_DRIVER;
+import static com.baeldung.dbunit.ConnectionSettings.JDBC_URL;
+import static com.baeldung.dbunit.ConnectionSettings.PASSWORD;
+import static com.baeldung.dbunit.ConnectionSettings.USER;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.dbunit.Assertion.assertEquals;
 
 public class OldSchoolDbUnitTest {
-    private static final String JDBC_DRIVER = org.h2.Driver.class.getName();
-    private static final String JDBC_URL = "jdbc:h2:mem:default;DB_CLOSE_DELAY=-1;init=runscript from 'classpath:schema.sql'";
-    private static final String USER = "sa";
-    private static final String PASSWORD = "";
 
     private static IDatabaseTester tester = null;
 
@@ -99,20 +98,31 @@ public class OldSchoolDbUnitTest {
 
     @Test
     public void testUpdate() throws Exception {
-        // Arrange
         final Connection connection = tester.getConnection().getConnection();
 
         final InputStream is = OldSchoolDbUnitTest.class.getClassLoader().getResourceAsStream("items_exp_rename.xml");
-        ITable expectedTable = new FlatXmlDataSetBuilder().build(is).getTable("items");
-        //expectedTable = DefaultColumnFilter.excludedColumnsTable(expectedTable, new String[]{"produced"});
+        final ITable expectedTable = new FlatXmlDataSetBuilder().build(is).getTable("items");
 
-        // Act
         connection.createStatement().executeUpdate("update ITEMS set title='new name' where id = 1");
 
-        // Assert
         final IDataSet databaseDataSet = tester.getConnection().createDataSet();
         ITable actualTable = databaseDataSet.getTable("items");
-        //actualTable = DefaultColumnFilter.excludedColumnsTable(actualTable, new String[]{"produced"});
+
+        assertEquals(expectedTable, actualTable);
+    }
+
+    @Test
+    public void testUpdateWithExcludedColumns() throws Exception {
+        final Connection connection = tester.getConnection().getConnection();
+
+        final InputStream is = OldSchoolDbUnitTest.class.getClassLoader().getResourceAsStream("items_exp_rename_no_produced.xml");
+        ITable expectedTable = new FlatXmlDataSetBuilder().build(is).getTable("items");
+
+        connection.createStatement().executeUpdate("update ITEMS set title='new name' where id = 1");
+
+        final IDataSet databaseDataSet = tester.getConnection().createDataSet();
+        ITable actualTable = databaseDataSet.getTable("items");
+        actualTable = DefaultColumnFilter.excludedColumnsTable(actualTable, new String[]{"produced"});
 
         assertEquals(expectedTable, actualTable);
     }
