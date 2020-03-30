26b81069a29d7f10677b32c52fee56a1af443751
Maciej Glowka <maciejglowka.pl@gmail.com>
2020-03-30 22:23:36 +0200

    BAEL-3505: Added util class to check server connection

--- core-java-modules/core-java-networking-2/src/main/java/com/baeldung/connectexception/ConnectionChecker.java
+++ core-java-modules/core-java-networking-2/src/main/java/com/baeldung/connectexception/ConnectionChecker.java
@@ -10,11 +10,12 @@ public class ConnectionChecker {
 
         try {
             Socket clientSocket = new Socket(host, port);
-            System.out.println("is client connected to server? " + clientSocket.isConnected());
+
+            //successfully connected to host, do something with opened socket
 
             clientSocket.close();
         } catch (ConnectException e) {
-            System.err.println("could not connect to server " + host + ":" + port + ", check host and port combination");
+            //host and port combination not valid
         }
     }
 }
