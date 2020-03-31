e8d61ab79313d0fdcb39f103bb6e308c2cca173a
Amy DeGregorio <amy.m.degregorio@gmail.com>
2020-03-31 12:02:54 -0400

    BAEL-3827 Add CSS and JS to Thymeleaf (#8985)
    * BAEL-3827 Add CSS and JS to Thymeleaf
    
    * BAEL-3827 Add integration tests
    
    * BAEL-3827 Add new spring-thymeleaf-3 module to the parent pom.xml
    
    * BAEL-3827 Add new spring-thymeleaf-3 module to the parent pom.xml in both places

---
+++ spring-thymeleaf-3/src/main/java/com/baeldung/thymeleaf/cssandjs/CssAndJsApplication.java
@@ -0,0 +1,11 @@
+package com.baeldung.thymeleaf.cssandjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CssAndJsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CssAndJsApplication.class, args);
+    }
+}
