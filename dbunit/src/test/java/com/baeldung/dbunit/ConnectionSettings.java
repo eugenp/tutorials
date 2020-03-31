f6b9f67c16785eaae65bf80d4af177010b53f3af
Joao Esperancinha <joao.esperancinha@eonics.nl>
2020-03-31 20:19:11 +0200

    [BAEL-2749] Removes dbunit module

--- dbunit/src/test/java/com/baeldung/dbunit/ConnectionSettings.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.dbunit;
-
-public class ConnectionSettings {
-    public static final String JDBC_DRIVER = org.h2.Driver.class.getName();
-    public static final String JDBC_URL = "jdbc:h2:mem:default;DB_CLOSE_DELAY=-1;init=runscript from 'classpath:schema.sql'";
-    public static final String USER = "sa";
-    public static final String PASSWORD = "";
-}
