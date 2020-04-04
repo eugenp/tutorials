60a554b5274aeda5560da7177ab39ec6016eb4f0
Fabricio <fabripautasso@gmail.com>
2020-04-04 09:53:00 -0300

    BAEL-3918 - Updating test methods names to be compliant with Baeldung standards

--- core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/bigdecimals/AddNumbersUnitTest.java
+++ core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/bigdecimals/AddNumbersUnitTest.java
@@ -13,13 +13,13 @@ import org.junit.Test;
 public class AddNumbersUnitTest {
 
     @Test
-    public void addInts() {
+    public void givenIntStream_whenSum_thenEnsureSumResultIsCorrect() {
         IntStream intNumbers = IntStream.range(0, 3);
         assertEquals(3, intNumbers.sum());
     }
 
     @Test
-    public void addDoubleOnList() {
+    public void givenCollectionOfDouble_whenUsingMapToDoubleToSum_thenEnsureSumResultIsCorrect() {
         List<Double> doubleNumbers = Arrays.asList(23.48, 52.26, 13.5);
         double result = doubleNumbers.stream()
             .mapToDouble(Double::doubleValue)
@@ -27,13 +27,13 @@ public class AddNumbersUnitTest {
         assertEquals(89.24, result, .1);
     }
 
-    public void addNumbersWithReduce() {
-        Stream<Integer> intNumbers = Stream.of(5, 1, 100);
+    public void givenStreamOfIntegers_whenUsingReduceToSum_thenEnsureSumResultIsCorrect() {
+        Stream<Integer> intNumbers = Stream.of(0, 1, 2);
         int result = intNumbers.reduce(0, Integer::sum);
         assertEquals(106, result);
     }
 
-    public void addBigDecimals() {
+    public void givenStreamOfBigDecimals_whenUsingReduceToSum_thenEnsureSumResultIsCorrect() {
         Stream<BigDecimal> bigDecimalNumber = Stream.of(BigDecimal.ZERO, BigDecimal.ONE, BigDecimal.TEN);
         BigDecimal result = bigDecimalNumber.reduce(BigDecimal.ZERO, BigDecimal::add);
         assertEquals(11, result);
