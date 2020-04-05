f9a6ef176faec736ffe699134a2727bf959b2fe1
kwoyke <krzysztofwoyke@gmail.com>
2020-04-05 07:52:47 +0200

    BAEL-3978: Add code samples for Objects.equals() (#9033)

--- core-java-modules/core-java-lang-2/src/test/java/com/baeldung/comparelong/CompareLongUnitTest.java
+++ core-java-modules/core-java-lang-2/src/test/java/com/baeldung/comparelong/CompareLongUnitTest.java
@@ -1,7 +1,11 @@
 package com.baeldung.comparelong;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 import org.junit.Test;
+
+import java.util.Objects;
 
 public class CompareLongUnitTest {
 
@@ -30,6 +34,33 @@ public class CompareLongUnitTest {
         Long l2 = 128L;
 
         assertThat(l1.equals(l2)).isTrue();
+    }
+
+    @Test
+    public void givenLongValuesLessThan128_whenUsingObjectsEquals_thenSuccess() {
+
+        Long l1 = 127L;
+        Long l2 = 127L;
+
+        assertThat(Objects.equals(l1, l2)).isTrue();
+    }
+
+    @Test
+    public void givenLongValuesGreaterOrEqualsThan128_whenUsingObjectsEquals_thenSuccess() {
+
+        Long l1 = 128L;
+        Long l2 = 128L;
+
+        assertThat(Objects.equals(l1, l2)).isTrue();
+    }
+
+    @Test
+    public void givenNullReference_whenUsingObjectsEquals_thenNoException() {
+
+        Long l1 = null;
+        Long l2 = 128L;
+
+        assertThatCode(() -> Objects.equals(l1, l2)).doesNotThrowAnyException();
     }
 
     @Test
