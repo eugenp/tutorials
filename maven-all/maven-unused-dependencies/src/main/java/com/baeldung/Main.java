b2702878ee4fba5abebf18f81e2d2db763bc2a34
Tyrell Flurry <tyrellflurry@gmail.com>
2020-04-09 16:52:54 -0400

    BAEL-3929: modified java.

--- maven-all/maven-unused-dependencies/src/main/java/com/baeldung/Main.java
+++ maven-all/maven-unused-dependencies/src/main/java/com/baeldung/Main.java
@@ -6,15 +6,13 @@ import java.util.Map;
 import org.slf4j.LoggerFactory;
 
 public class Main {
-
     public static void main(String[] args) {
-        Map<String, String> teamMap = new HashMap<>();
-        teamMap.put("Dallas", "Cowboys");
-        teamMap.put("Green Bay", "Packers");
-        teamMap.put("Washington", "Redskins");
+        Map<String, String> map = new HashMap<>();
+        map.put("Dallas", "Texas");
+        map.put("Green Bay", "Wisconsin");
+        map.put("Seattle", "Washington");
 
         LoggerFactory.getLogger(Main.class)
-            .info("Team Map: {}", teamMap);
-
+            .info("Map: {}", map);
     }
 }
