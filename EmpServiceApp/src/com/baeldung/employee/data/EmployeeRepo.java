6205b4965841c366c97fcb62bfe9f2d26ceb17b9
Sasa m <sasam0320@gmail.com>
2020-04-09 20:56:42 +0200

    Remove unused commit

--- EmpServiceApp/src/com/baeldung/employee/data/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.employee.data;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.baeldung.employee.domain.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
-}
