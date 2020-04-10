314f86da2ad977f8007eac9c8b2498ed587de43b
Marc Guerrini <maguerrini@airfrance.fr>
2020-04-10 17:54:39 +0200

    switched to dedicated branch and removal of unwanted project

--- hexagonal-architecture/src/main/java/com/baeldung/springboot/hexagonal/infrastructure/FlightService.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.springboot.hexagonal.infrastructure;
-
-import java.util.List;
-
-import com.baeldung.springboot.hexagonal.domain.Flight;
-
-public interface FlightService {
-    public List<Flight> getRoute(String date, String origin, String destination);
-}
