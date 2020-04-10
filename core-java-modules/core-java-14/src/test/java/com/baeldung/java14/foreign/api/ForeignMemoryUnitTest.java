e7fa33ab4340e734b16e37f4e5ea596c0251bfd0
musibs <musib.somnath@gmail.com>
2020-04-10 13:56:53 +1000

    BAEL-3852 - Review changes of A Guide to Foreign Memory Access API in Java 14

--- core-java-modules/core-java-14/src/test/java/com/baeldung/java14/foreign/api/ForeignMemoryUnitTest.java
+++ core-java-modules/core-java-14/src/test/java/com/baeldung/java14/foreign/api/ForeignMemoryUnitTest.java
@@ -16,23 +16,25 @@ import static org.junit.Assert.assertThat;
 public class ForeignMemoryUnitTest {
 
     @Test
-    public void whenValueIsSetThenAccessTheValue(){
+    public void whenAValueIsSet_thenAccessTheValue() {
         long value = 10;
-        MemoryAddress memoryAddress = MemorySegment.allocateNative(8).baseAddress();
-        VarHandle varHandle = MemoryHandles.varHandle(long.class, ByteOrder.nativeOrder());
+        MemoryAddress memoryAddress = MemorySegment
+                .allocateNative(8).baseAddress();
+        VarHandle varHandle = MemoryHandles
+                .varHandle(long.class, ByteOrder.nativeOrder());
         varHandle.set(memoryAddress, value);
         assertThat(varHandle.get(memoryAddress), is(value));
     }
 
     @Test
-    public void whenMultipleValuesAreSetThenAccessAll(){
-        VarHandle varHandle = MemoryHandles.varHandle(int.class, ByteOrder.nativeOrder());
+    public void whenMultipleValuesAreSet_thenAccessAll() {
+        VarHandle varHandle = MemoryHandles
+                .varHandle(int.class, ByteOrder.nativeOrder());
         try(MemorySegment memorySegment = MemorySegment.allocateNative(100)){
             MemoryAddress base = memorySegment.baseAddress();
             for(int i=0; i<25; i++){
                 varHandle.set(base.addOffset((i*4)), i);
             }
-
             for(int i=0; i<25; i++){
                 assertThat(varHandle.get(base.addOffset((i*4))), is(i));
             }
@@ -40,29 +42,37 @@ public class ForeignMemoryUnitTest {
     }
 
     @Test
-    public void testMemoryLayout(){
-        SequenceLayout sequenceLayout = MemoryLayout.ofSequence(25, MemoryLayout.ofValueBits(64, ByteOrder.nativeOrder()));
-        VarHandle varHandle = sequenceLayout.varHandle(long.class, MemoryLayout.PathElement.sequenceElement());
-        try(MemorySegment memorySegment = MemorySegment.allocateNative(sequenceLayout)){
+    public void whenSetValuesWithMemoryLayout_thenTheyCanBeRetrieved() {
+        SequenceLayout sequenceLayout = MemoryLayout
+                .ofSequence(25, MemoryLayout
+                        .ofValueBits(64, ByteOrder.nativeOrder()));
+        VarHandle varHandle = sequenceLayout.varHandle(long.class, MemoryLayout
+                .PathElement.sequenceElement());
+        try(MemorySegment memorySegment = MemorySegment
+                .allocateNative(sequenceLayout)){
             MemoryAddress base = memorySegment.baseAddress();
-            for(long i=0; i<sequenceLayout.elementCount().getAsLong(); i++){
+            for(long i=0; i<sequenceLayout.elementCount().getAsLong(); i++) {
                 varHandle.set(base, i, i);
             }
-
-            for(long i=0; i<sequenceLayout.elementCount().getAsLong(); i++){
+            for(long i=0; i<sequenceLayout.elementCount().getAsLong(); i++) {
                 assertThat(varHandle.get(base, i), is(i));
             }
         }
     }
 
     @Test
-    public void testSlicingOfMemorySegment(){
-        MemoryAddress memoryAddress = MemorySegment.allocateNative(12).baseAddress();
-        MemoryAddress memoryAddress1 = memoryAddress.segment().asSlice(0,4).baseAddress();
-        MemoryAddress memoryAddress2 = memoryAddress.segment().asSlice(4,4).baseAddress();
-        MemoryAddress memoryAddress3 = memoryAddress.segment().asSlice(8,4).baseAddress();
+    public void whenSlicingMemorySegment_thenTheyCanBeAccessedIndividually() {
+        MemoryAddress memoryAddress = MemorySegment
+                .allocateNative(12).baseAddress();
+        MemoryAddress memoryAddress1 = memoryAddress
+                .segment().asSlice(0,4).baseAddress();
+        MemoryAddress memoryAddress2 = memoryAddress
+                .segment().asSlice(4,4).baseAddress();
+        MemoryAddress memoryAddress3 = memoryAddress
+                .segment().asSlice(8,4).baseAddress();
 
-        VarHandle intHandle = MemoryHandles.varHandle(int.class, ByteOrder.nativeOrder());
+        VarHandle intHandle = MemoryHandles
+                .varHandle(int.class, ByteOrder.nativeOrder());
         intHandle.set(memoryAddress1, Integer.MIN_VALUE);
         intHandle.set(memoryAddress2, 0);
         intHandle.set(memoryAddress3, Integer.MAX_VALUE);
