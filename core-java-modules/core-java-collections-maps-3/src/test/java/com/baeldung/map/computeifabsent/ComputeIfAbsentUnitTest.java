a4ebcbd0df48d827cc184b59d97764377f975db4
unknown <vatsalgosar@gmail.com>
2020-04-11 03:15:45 +0530

    reformatting

--- core-java-modules/core-java-collections-maps-3/src/test/java/com/baeldung/map/computeifabsent/ComputeIfAbsentUnitTest.java
+++ core-java-modules/core-java-collections-maps-3/src/test/java/com/baeldung/map/computeifabsent/ComputeIfAbsentUnitTest.java
@@ -4,22 +4,19 @@ import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.Map;
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertTrue;
 
 public class ComputeIfAbsentUnitTest {
+        @Test public void whenKeyIsPresent_thenFetchTheValue() {
+                Map<String, Integer> stringLength = new HashMap<>();
+                stringLength.put("John", 4);
+                assertTrue(stringLength.computeIfAbsent("John", s -> s.length()) == 4);
+        }
 
-@Test
-public void whenKeyIsPresent_thenFetchTheValue() {
-        Map<String, Integer> stringLength = new HashMap<>();
-        stringLength.put("John", 4);
-        assertTrue(stringLength.computeIfAbsent("John", s -> s.length()) == 4);
-}
-
-@Test
-public void whenKeyIsNotPresent_thenComputeTheValueUsingMappingFunctionAndStore() {
-        Map<String, Integer> stringLength = new HashMap<>();
-        assertTrue(stringLength.computeIfAbsent("John", s -> s.length()) == 4);
-        assertTrue(stringLength.get("John") == 4);
-}
-
+        @Test public void whenKeyIsNotPresent_thenComputeTheValueUsingMappingFunctionAndStore() {
+                Map<String, Integer> stringLength = new HashMap<>();
+                assertTrue(stringLength.computeIfAbsent("John", s -> s.length()) == 4);
+                assertTrue(stringLength.get("John") == 4);
+        }
 }
