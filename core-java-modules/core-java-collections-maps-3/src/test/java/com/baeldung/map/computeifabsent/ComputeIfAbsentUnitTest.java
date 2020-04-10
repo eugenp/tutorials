da6c57d0e32fbec84c8a07fa83cd38971c79cda6
unknown <vatsalgosar@gmail.com>
2020-04-11 03:11:47 +0530

    BAEL-3919 - computeIfAbsent method unit tests

---
+++ core-java-modules/core-java-collections-maps-3/src/test/java/com/baeldung/map/computeifabsent/ComputeIfAbsentUnitTest.java
@@ -0,0 +1,25 @@
+package com.baeldung.map.computeifabsent;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.Assert.*;
+
+public class ComputeIfAbsentUnitTest {
+
+@Test
+public void whenKeyIsPresent_thenFetchTheValue() {
+        Map<String, Integer> stringLength = new HashMap<>();
+        stringLength.put("John", 4);
+        assertTrue(stringLength.computeIfAbsent("John", s -> s.length()) == 4);
+}
+
+@Test
+public void whenKeyIsNotPresent_thenComputeTheValueUsingMappingFunctionAndStore() {
+        Map<String, Integer> stringLength = new HashMap<>();
+        assertTrue(stringLength.computeIfAbsent("John", s -> s.length()) == 4);
+        assertTrue(stringLength.get("John") == 4);
+}
+
+}
