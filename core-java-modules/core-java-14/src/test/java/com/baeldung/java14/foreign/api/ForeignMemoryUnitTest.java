373be297884202ab6bf1f2032db6aca492f0b8a6
musibs <musib.somnath@gmail.com>
2020-04-12 17:53:47 +1000

    Review changes for alignment and class removal

--- core-java-modules/core-java-14/src/test/java/com/baeldung/java14/foreign/api/ForeignMemoryUnitTest.java
+++ core-java-modules/core-java-14/src/test/java/com/baeldung/java14/foreign/api/ForeignMemoryUnitTest.java
@@ -18,18 +18,19 @@ public class ForeignMemoryUnitTest {
     @Test
     public void whenAValueIsSet_thenAccessTheValue() {
         long value = 10;
-        MemoryAddress memoryAddress = MemorySegment
-                .allocateNative(8).baseAddress();
-        VarHandle varHandle = MemoryHandles
-                .varHandle(long.class, ByteOrder.nativeOrder());
+        MemoryAddress memoryAddress =
+                MemorySegment.allocateNative(8)
+                        .baseAddress();
+        VarHandle varHandle = MemoryHandles.varHandle(long.class,
+                        ByteOrder.nativeOrder());
         varHandle.set(memoryAddress, value);
         assertThat(varHandle.get(memoryAddress), is(value));
     }
 
     @Test
     public void whenMultipleValuesAreSet_thenAccessAll() {
-        VarHandle varHandle = MemoryHandles
-                .varHandle(int.class, ByteOrder.nativeOrder());
+        VarHandle varHandle = MemoryHandles.varHandle(int.class,
+                ByteOrder.nativeOrder());
         try(MemorySegment memorySegment = MemorySegment.allocateNative(100)) {
             MemoryAddress base = memorySegment.baseAddress();
             for(int i=0; i<25; i++) {
@@ -43,13 +44,14 @@ public class ForeignMemoryUnitTest {
 
     @Test
     public void whenSetValuesWithMemoryLayout_thenTheyCanBeRetrieved() {
-        SequenceLayout sequenceLayout = MemoryLayout
-                .ofSequence(25, MemoryLayout
-                        .ofValueBits(64, ByteOrder.nativeOrder()));
-        VarHandle varHandle = sequenceLayout.varHandle(long.class, MemoryLayout
-                .PathElement.sequenceElement());
-        try(MemorySegment memorySegment = MemorySegment
-                .allocateNative(sequenceLayout)) {
+        SequenceLayout sequenceLayout =
+                MemoryLayout.ofSequence(25,
+                        MemoryLayout.ofValueBits(64, ByteOrder.nativeOrder()));
+        VarHandle varHandle =
+                sequenceLayout.varHandle(long.class,
+                        MemoryLayout.PathElement.sequenceElement());
+        try(MemorySegment memorySegment =
+                    MemorySegment.allocateNative(sequenceLayout)) {
             MemoryAddress base = memorySegment.baseAddress();
             for(long i=0; i<sequenceLayout.elementCount().getAsLong(); i++) {
                 varHandle.set(base, i, i);
@@ -62,17 +64,22 @@ public class ForeignMemoryUnitTest {
 
     @Test
     public void whenSlicingMemorySegment_thenTheyCanBeAccessedIndividually() {
-        MemoryAddress memoryAddress = MemorySegment
-                .allocateNative(12).baseAddress();
-        MemoryAddress memoryAddress1 = memoryAddress
-                .segment().asSlice(0,4).baseAddress();
-        MemoryAddress memoryAddress2 = memoryAddress
-                .segment().asSlice(4,4).baseAddress();
-        MemoryAddress memoryAddress3 = memoryAddress
-                .segment().asSlice(8,4).baseAddress();
+        MemoryAddress memoryAddress =
+                MemorySegment.allocateNative(12)
+                        .baseAddress();
+        MemoryAddress memoryAddress1 =
+                memoryAddress.segment()
+                        .asSlice(0,4).baseAddress();
+        MemoryAddress memoryAddress2 =
+                memoryAddress.segment()
+                        .asSlice(4,4).baseAddress();
+        MemoryAddress memoryAddress3 =
+                memoryAddress.segment()
+                        .asSlice(8,4).baseAddress();
 
-        VarHandle intHandle = MemoryHandles
-                .varHandle(int.class, ByteOrder.nativeOrder());
+        VarHandle intHandle =
+                MemoryHandles.varHandle(int.class,
+                        ByteOrder.nativeOrder());
         intHandle.set(memoryAddress1, Integer.MIN_VALUE);
         intHandle.set(memoryAddress2, 0);
         intHandle.set(memoryAddress3, Integer.MAX_VALUE);
