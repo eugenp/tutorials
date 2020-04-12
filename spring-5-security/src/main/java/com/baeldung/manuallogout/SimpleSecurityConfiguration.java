17aea50a53daa804a030d451689924c46d60b1d1
Abdelillah Hassam <ahassam@itpps.com>
2020-04-12 22:26:04 +0200

    Add intergration tests for manual logouts.

--- spring-5-security/src/main/java/com/baeldung/manuallogout/SimpleSecurityConfiguration.java
+++ spring-5-security/src/main/java/com/baeldung/manuallogout/SimpleSecurityConfiguration.java
@@ -1,8 +1,13 @@
 package com.baeldung.manuallogout;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
 public class SimpleSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
@@ -16,4 +21,19 @@ public class SimpleSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .failureUrl("/login?error");
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("manager")
+                .password("password")
+                .credentialsExpired(true)
+                .accountExpired(true)
+                .accountLocked(true)
+                .authorities("WRITE_PRIVILEGES", "READ_PRIVILEGES")
+                .roles("MANAGER");
+    }
 }
--- spring-5-security/src/test/java/com/baeldung/manuallogout/ManualLogoutIntegrationTest.java
+++ spring-5-security/src/test/java/com/baeldung/manuallogout/ManualLogoutIntegrationTest.java
@@ -1,25 +1,54 @@
 package com.baeldung.manuallogout;
 
-import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.*;
-import static org.springframework.security.web.header.writers.ClearSiteDataHeaderWriter.Directive.EXECUTION_CONTEXTS;
+import javax.servlet.http.Cookie;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-public class ManualLogoutTests {
+@WebMvcTest()
+public class ManualLogoutIntegrationTest {
 
     private static final String CLEAR_SITE_DATA_HEADER = "Clear-Site-Data";
 
-    private static final ClearSiteDataHeaderWriter.Directive[] SOURCE =
-            { CACHE, COOKIES, STORAGE, EXECUTION_CONTEXTS };
+    @Autowired
+    private MockMvc mockMvc;
 
-    private static final String HEADER_VALUE = "\"cache\", \"cookies\", \"storage\", \"executionContexts\"";
+    @WithMockUser(value = "spring")
+    @Test
+    public void logoutWhenRequestPost() throws Exception {
 
-    @Rule
-    public final SpringTestRule spring = new SpringTestRule();
+        MockHttpSession session = new MockHttpSession();
+        Cookie randomCookie = new Cookie("customerName", "myName");
 
+        session.setAttribute("att", "attvalue");
+        this.mockMvc.perform(post("/basiclogout").secure(true).with(csrf()).session(session).cookie(randomCookie))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(unauthenticated())
+                .andReturn();
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    public void logoutWithClearDataSiteHeaderWhenRequestPost() throws Exception {
+
+        this.mockMvc.perform(post("/csdlogout").secure(true).with(csrf()))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(header().exists(CLEAR_SITE_DATA_HEADER))
+                .andReturn();
+    }
 }
