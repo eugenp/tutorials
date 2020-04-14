c50f5893ed5ef2cfbae3cf23c2d173162b0adecc
mthomas <spatialguru.net@gmail.com>
2020-04-14 06:44:36 -0500

    Rename classes

--- core-java-modules/core-java-concurrency-advanced-3/src/main/java/com/baeldung/atomicstampedreference/StampedAccount.java
+++ core-java-modules/core-java-concurrency-advanced-3/src/main/java/com/baeldung/atomicstampedreference/StampedAccount.java
@@ -3,32 +3,32 @@ package com.baeldung.atomicstampedreference;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicStampedReference;
 
-public class SafeAccount {
+public class StampedAccount {
 
     private AtomicInteger stamp = new AtomicInteger(0);
-    private AtomicStampedReference<Integer> balance = new AtomicStampedReference<>(0, 0);
+    private AtomicStampedReference<Integer> account = new AtomicStampedReference<>(0, 0);
 
     public int getBalance() {
-        return this.balance.get(new int[1]);
+        return this.account.get(new int[1]);
     }
 
     public int getStamp() {
         int[] stamps = new int[1];
-        this.balance.get(stamps);
+        this.account.get(stamps);
         return stamps[0];
     }
 
     public boolean deposit(int funds) {
         int[] stamps = new int[1];
-        int current = this.balance.get(stamps);
+        int current = this.account.get(stamps);
         int newStamp = this.stamp.incrementAndGet();
-        return this.balance.compareAndSet(current, current + funds, stamps[0], newStamp);
+        return this.account.compareAndSet(current, current + funds, stamps[0], newStamp);
     }
 
     public boolean withdrawal(int funds) {
         int[] stamps = new int[1];
-        int current = this.balance.get(stamps);
+        int current = this.account.get(stamps);
         int newStamp = this.stamp.incrementAndGet();
-        return this.balance.compareAndSet(current, current - funds, stamps[0], newStamp);
+        return this.account.compareAndSet(current, current - funds, stamps[0], newStamp);
     }
 }
--- core-java-modules/core-java-concurrency-advanced-3/src/test/java/com/baeldung/atomicstampedreference/ThreadStampedAccountUnitTest.java
+++ core-java-modules/core-java-concurrency-advanced-3/src/test/java/com/baeldung/atomicstampedreference/ThreadStampedAccountUnitTest.java
@@ -3,11 +3,11 @@ package com.baeldung.atomicstampedreference;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ThreadSafeAccountUnitTest {
+public class ThreadStampedAccountUnitTest {
 
     @Test
-    public void givenMultiThread_whenSafeAccount_thenSetBalance() throws InterruptedException {
-        SafeAccount account = new SafeAccount();
+    public void givenMultiThread_whenStampedAccount_thenSetBalance() throws InterruptedException {
+        StampedAccount account = new StampedAccount();
         Thread t = new Thread(() -> {
             while (!account.withdrawal(100))
                 Thread.yield();
