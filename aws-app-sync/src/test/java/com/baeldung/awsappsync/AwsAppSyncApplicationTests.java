6e0e4ed7817a8d39687f4b5a17db0d75516478f6
Kyle Doyle <kdoyle8597@gmail.com>
2020-04-16 09:55:40 -0400

    Removed rest of project

---
+++ aws-app-sync/src/test/java/com/baeldung/awsappsync/AwsAppSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.awsappsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsAppSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
