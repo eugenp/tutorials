8748288025c15942d810e9a8941b05567bd4a451
Roque Santos <roquesantosdev@gmail.com>
2020-04-16 19:07:13 -0300

    BAEL-3927 : Coding styles adjustements

--- core-java-modules/core-java-string-operations-2/src/test/java/com/baeldung/encode/StringEncodeUnitTest.java
+++ core-java-modules/core-java-string-operations-2/src/test/java/com/baeldung/encode/StringEncodeUnitTest.java
@@ -15,18 +15,20 @@ public class StringEncodeUnitTest {
     public void givenGermanAsciiString_whenComparing_thenCompareNotEquals() {
         String germanString = "Entwickeln Sie mit Vergnügen";
         byte[] germanBytes = germanString.getBytes();
+
         String asciiEncodedString = new String(germanBytes, StandardCharsets.US_ASCII);
 
-        assertNotEquals(asciiEncodedString, germanString);
+        assertNotEquals(germanString, asciiEncodedString);
     }
 
     @Test
     public void givenUsAsciiString_whenComparing_thenCompareNotEquals() {
         String englishString = "Develop with pleasure";
         byte[] englishBytes = englishString.getBytes();
+
         String asciiEncondedEnglishString = new String(englishBytes, StandardCharsets.US_ASCII);
 
-        assertEquals(asciiEncondedEnglishString, englishString);
+        assertEquals(englishString, asciiEncondedEnglishString);
     }
 
     /*
@@ -36,6 +38,7 @@ public class StringEncodeUnitTest {
     public void givenSomeUnencodedString_whenApacheCommonsCodecEncode_thenCompareEquals() {
         String rawString = "Develop with pleasure";
         byte[] bytes = StringUtils.getBytesUtf8(rawString);
+
         String utf8EncodedString = StringUtils.newStringUtf8(bytes);
 
         assertEquals(rawString, utf8EncodedString);
@@ -48,6 +51,7 @@ public class StringEncodeUnitTest {
     public void givenSomeUnencodedString_whenCoreJavaEncode_thenCompareEquals() {
         String rawString = "Develop with pleasure";
         byte[] bytes = rawString.getBytes(StandardCharsets.UTF_8);
+
         String utf8EncodedString = new String(bytes, StandardCharsets.UTF_8);
 
         assertEquals(rawString, utf8EncodedString);
@@ -60,6 +64,7 @@ public class StringEncodeUnitTest {
     public void givenSomeUnencodedString_whenJava7StandardCharsetsEncode_thenCompareEquals() {
         String rawString = "Develop with pleasure";
         ByteBuffer buffer = StandardCharsets.UTF_8.encode(rawString);
+
         String utf8EncodedString = StandardCharsets.UTF_8.decode(buffer)
             .toString();
 
