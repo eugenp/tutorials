baffab762d9b5768a0078f24a49bfb0cb1f87b91
Arindum Roy <arindum.roy@gmail.com>
2020-04-18 04:19:25 +0530

    removed draft article

--- hexagonal-architecture/src/main/resources/db/migration/V1.0.0__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE employee (
-    id          IDENTITY NOT NULL PRIMARY KEY,
-    name        VARCHAR(60) NOT NULL,
-    age        	int NOT NULL,
-    department  VARCHAR(40) NOT NULL
-);
