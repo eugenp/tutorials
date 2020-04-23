ed74b2df41e8f3c837de6051c758916a7f0e26ae
mdabrowski-eu <firma@mdabrowski.eu>
2020-04-24 01:56:25 +0200

    BAEL-3863 Introduction to Finagle

---
+++ finagle/README.md
@@ -0,0 +1,7 @@
+## Finagle
+
+This module contains articles about Finagle and associated libraries.
+
+### Relevant articles
+
+- [Introduction to Finagle](https://www.baeldung.com/introduction-to-finagle)
---
+++ finagle/src/main/java/com/baeldung/finagle/LogFilter.java
@@ -0,0 +1,17 @@
+package com.baeldung.finagle;
+
+import com.twitter.finagle.Service;
+import com.twitter.finagle.SimpleFilter;
+import com.twitter.finagle.http.Request;
+import com.twitter.finagle.http.Response;
+import com.twitter.util.Future;
+
+public class LogFilter extends SimpleFilter<Request, Response> {
+    @Override
+    public Future<Response> apply(Request request, Service<Request, Response> service) {
+        System.out.println("Request host:" + request.host().getOrElse(() -> ""));
+        System.out.println("Request params:");
+        request.getParams().forEach(entry -> System.out.println("\t" + entry.getKey() + " : " + entry.getValue()));
+        return service.apply(request);
+    }
+}
