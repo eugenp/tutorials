acf4fa5af9ad36f7273c11ee4be36dbe95434056
Krzysiek <krzysztofwoyke@gmail.com>
2020-04-23 22:18:45 +0200

    JAVA-73: Move scripts under live-test-resources

--- persistence-modules/spring-data-mongodb/src/live-test/resources/live-test-setup.sh
+++ persistence-modules/spring-data-mongodb/src/live-test/resources/live-test-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker image build -t spring-data-mongodb:live-test src/live-test/resources/
+docker image build -t spring-data-mongodb:live-test .
 
 docker run -p 27017:27017 --name spring-data-mongodb-live-test spring-data-mongodb:live-test
