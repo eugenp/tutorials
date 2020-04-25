0aa1090ef6543022ef64a1ed643001bf027ad66a
andrebrowne <42154231+andrebrowne@users.noreply.github.com>
2020-04-25 13:41:47 -0400

    Revert "Add architecture module with Hexagonal example"
    This reverts commit 50efbf3dfb3a951d276c13ab3b8f1a0cadb114d4.

--- architecture/src/main/java/com/baeldung/architecture/commands/task/CreateTask.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.architecture.commands.task;
-
-import com.baeldung.architecture.domain.task.Task;
-
-public interface CreateTask {
-    public void create(Task newTask);
-};
--- architecture/src/main/java/com/baeldung/architecture/domain/task/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.architecture.domain.task;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<Task, Long> {};
