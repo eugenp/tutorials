9171c4f4fcb5882db2b98f1eda05cdb0932b7d36
sergio41 <sergiorr41@gmail.com>
2020-04-30 18:44:05 +0200

    [BAEL-3944] Minor changes on examples and identations

--- core-java-modules/core-java-regex/src/test/java/com/baeldung/regex/phonenumbers/PhoneNumbersRegexUnitTest.java
+++ core-java-modules/core-java-regex/src/test/java/com/baeldung/regex/phonenumbers/PhoneNumbersRegexUnitTest.java
@@ -15,37 +15,38 @@ public class PhoneNumbersRegexUnitTest {
         Matcher matcher = pattern.matcher("2055550125");
         assertTrue(matcher.matches());
     }
-    
+
     @Test
-    public void whenMatchesTenDigitsNumberWhitespacesHyphen_thenCorrect() {
-        Pattern pattern = Pattern.compile("^(\\d{3}[- ]?){2}\\d{4}$");
+    public void whenMatchesTenDigitsNumberWhitespacesDotHyphen_thenCorrect() {
+        Pattern pattern = Pattern.compile("^(\\d{3}[- .]?){2}\\d{4}$");
         Matcher matcher = pattern.matcher("202 555 0125");
         assertTrue(matcher.matches());
     }
-    
+
     @Test
     public void whenMatchesTenDigitsNumberParenthesis_thenCorrect() {
-        Pattern pattern = Pattern.compile("^\\(?\\d{3}\\)?[- ]?\\d{3}[- ]?\\d{4}$");
-        Matcher matcher = pattern.matcher("(202)-555-0125");
+        Pattern pattern = Pattern.compile("^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$");
+        Matcher matcher = pattern.matcher("(202) 555-0125");
         assertTrue(matcher.matches());
     }
-    
+
     @Test
     public void whenMatchesTenDigitsNumberPrefix_thenCorrect() {
-        Pattern pattern = Pattern.compile("^(\\+\\d{1,3}( )?)?\\(?\\d{3}\\)?[- ]?\\d{3}[- ]?\\d{4}$");
-        Matcher matcher = pattern.matcher("+111 (202)-555-0125");
+        Pattern pattern = Pattern.compile("^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$");
+        Matcher matcher = pattern.matcher("+111 (202) 555-0125");
         assertTrue(matcher.matches());
     }
-    
+
     @Test
     public void whenMatchesPhoneNumber_thenCorrect() {
-        String patterns = "^(\\+\\d{1,3}( )?)?\\(?\\d{3}\\)?[- ]?\\d{3}[- ]?\\d{4}$" + 
-                         "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$" + 
-                         "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
-        
-        String[] validPhoneNumbers = {"2055550125","202 555 0125", "(202)-555-0125", "+111 (202)-555-0125",
-                                      "636 856 789", "+111 636 856 789", "636 85 67 89", "+111 636 85 67 89"};
-        
+        String patterns 
+          = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+          + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$" 
+          + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+
+        String[] validPhoneNumbers 
+        = {"2055550125","202 555 0125", "(202) 555-0125", "+111 (202) 555-0125", "636 856 789", "+111 636 856 789", "636 85 67 89", "+111 636 85 67 89"};
+
         Pattern pattern = Pattern.compile(patterns);
         for(String phoneNumber : validPhoneNumbers) {
             Matcher matcher = pattern.matcher(phoneNumber);
