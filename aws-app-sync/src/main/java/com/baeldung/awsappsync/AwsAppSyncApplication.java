2e5cc4e1f19a5610541b27bf64581f295b58a50a
Kyle Doyle <kdoyle8597@gmail.com>
2020-05-01 19:48:38 -0400

    Bael 3893 - Code review updates

--- aws-app-sync/src/main/java/com/baeldung/awsappsync/AwsAppSyncApplication.java
+++ aws-app-sync/src/main/java/com/baeldung/awsappsync/AwsAppSyncApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsAppSyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AwsAppSyncApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AwsAppSyncApplication.class, args);
+    }
 
 }
