288eb4dcdcfb5580dcebb5388f4af3b9b6598f8f
Yavuz Tas <yavuzslmtas@gmail.com>
2020-05-03 02:48:35 +0200

    fix formatting

--- persistence-modules/spring-persistence-simple/src/main/java/com/baeldung/spring/data/persistence/repository/UserRepositoryCustom.java
+++ persistence-modules/spring-persistence-simple/src/main/java/com/baeldung/spring/data/persistence/repository/UserRepositoryCustom.java
@@ -8,7 +8,9 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 public interface UserRepositoryCustom {
+
     List<User> findUserByEmails(Set<String> emails);
     
     List<User> findAllUsersByPredicates(Collection<Predicate<User>> predicates);
+
 }
