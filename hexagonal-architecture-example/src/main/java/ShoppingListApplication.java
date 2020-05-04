f56864e46db92c4f981bdc38f78fbabade72156c
mbragilevsky <bragilev@gmail.com>
2020-05-04 16:56:21 +0200

    Revert "Delete ShoppingListApplication.java"
    This reverts commit 640bb544e6cd8eda6d1fe6a38607323871ddb957.

---
+++ hexagonal-architecture-example/src/main/java/ShoppingListApplication.java
@@ -0,0 +1,12 @@
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+	}
+
+}
