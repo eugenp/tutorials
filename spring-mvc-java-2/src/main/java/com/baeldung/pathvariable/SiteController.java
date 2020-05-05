990faedf2f785ad2a025865ec790951139942697
Krzysiek <krzysztofwoyke@gmail.com>
2020-05-06 01:05:52 +0200

    JAVA-1529: Add MVC config

--- spring-mvc-java-2/src/main/java/com/baeldung/pathvariable/SiteController.java
+++ spring-mvc-java-2/src/main/java/com/baeldung/pathvariable/SiteController.java
@@ -4,8 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/site")
 public class SiteController {
 
