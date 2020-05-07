81dfbe8aaee06583a022b665e3cc5cf904dab916
Yavuz Tas <12643010+yavuztas@users.noreply.github.com>
2020-05-07 04:22:23 +0200

    Changes for BAEL-2621 (#9217)
    * update dependency versions to latest
    
    * merge repository packages
    
    * remove obselete xml configuration
    
    * fix formatting
    
    * clean up
    
    * update package name
    
    * revert spring data jpa xml config and demonstrate how to import in PersistenceConfig by a commented @ImportResource annotation
    
    * rename jpa repositories xml to make it more clear
    
    * remove obselete @EnableJpaRepositories configs since we don't use spring data's jpa repositories for these congigurations

--- persistence-modules/spring-persistence-simple/src/main/java/com/baeldung/spring/data/persistence/repository/UserRepositoryCustom.java
+++ persistence-modules/spring-persistence-simple/src/main/java/com/baeldung/spring/data/persistence/repository/UserRepositoryCustom.java
@@ -1,14 +1,16 @@
-package com.baeldung.spring.data.persistence.jpaquery;
+package com.baeldung.spring.data.persistence.repository;
+
+import com.baeldung.spring.data.persistence.model.User;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Predicate;
 
-import com.baeldung.spring.data.persistence.model.User;
-
 public interface UserRepositoryCustom {
+
     List<User> findUserByEmails(Set<String> emails);
     
     List<User> findAllUsersByPredicates(Collection<Predicate<User>> predicates);
+
 }
