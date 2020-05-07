68dc88528e488db5aef951d8578bcfb33c46cd81
Gergo Petrik <petrikgergo@gmail.com>
2020-05-07 11:45:40 +0200

    added transaction recording

--- core-java-modules/core-java-concurrency-advanced-3/src/main/java/com/baeldung/abaproblem/Account.java
+++ core-java-modules/core-java-concurrency-advanced-3/src/main/java/com/baeldung/abaproblem/Account.java
@@ -1,14 +1,21 @@
 package com.baeldung.abaproblem;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Account {
 
     private AtomicInteger balance = new AtomicInteger(0);
+    private List<Long> transactionDates = new ArrayList<>();
 
     public int getBalance() {
         return balance.get();
+    }
+
+    public List<Long> getTransactionDates() {
+        return transactionDates;
     }
 
     public boolean withdraw(int amount) throws InterruptedException {
@@ -17,18 +24,26 @@ public class Account {
             throw new RuntimeException("Not sufficient balance");
         }
         precessBalance();
-        return balance.compareAndSet(current, current - amount);
+        boolean result = balance.compareAndSet(current, current - amount);
+        if (result) {
+            transactionDates.add(System.currentTimeMillis());
+        }
+        return result;
     }
 
     private void precessBalance() throws InterruptedException {
-        if ("thread 1".equals(Thread.currentThread().getName())) {
+        if ("thread1".equals(Thread.currentThread().getName())) {
             TimeUnit.SECONDS.sleep(2);
         }
     }
 
     public boolean deposit(int amount) {
         int current = balance.get();
-        return balance.compareAndSet(current, current + amount);
+        boolean result = balance.compareAndSet(current, current + amount);
+        if (result) {
+            transactionDates.add(System.currentTimeMillis());
+        }
+        return result;
     }
 
 }
