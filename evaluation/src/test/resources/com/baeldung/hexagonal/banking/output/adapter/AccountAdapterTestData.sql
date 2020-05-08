a5908a27dd8183744031e93d6f6ccbed5f62d4c4
Leonardo Mattos <leonardo.mattos@gmail.com>
2020-05-08 07:20:43 -0400

    Delete AccountAdapterTestData.sql

--- evaluation/src/test/resources/com/baeldung/hexagonal/banking/output/adapter/AccountAdapterTestData.sql
+++
@@ -1,5 +0,0 @@
-insert into account (account_number, pin, balance, account_holder_id_number, first_name, last_name) 
-values (1, 1234, 0, 156789, 'Joe', 'Doe');
-
-insert into account (account_number, pin, balance, account_holder_id_number, first_name, last_name) 
-values (2, 2222, 1000, 98765, 'Jenny', 'Lee');
