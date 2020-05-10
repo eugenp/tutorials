b224c21f5a0ea5b9a0dac5d0cc2517177365d59e
ajay74984 <51405689+ajay74984@users.noreply.github.com>
2020-05-10 15:16:42 +0200

    OCP change

---
+++ patterns/solid/src/main/java/com/baeldung/o/Addition.java
@@ -0,0 +1,41 @@
+package com.baeldung.o;
+
+public class Addition implements CalculatorOperation {
+    private double number1;
+    private double number2;
+    private double result = 0.0;
+
+    public Addition(double number1, double number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }
+
+    public double getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(double number1) {
+        this.number1 = number1;
+    }
+
+    public double getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    @Override
+    public void perform() {
+        result = number1 + number2;
+    }
+}
