fdc2a8ef0c161e5852ee6e3a17a2b707ded35afd
mikr <michael.krimgen@ximedes.com>
2020-05-10 23:18:49 +0200

    BAEL-1524 Fix tests

--- persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/boot/jdbi/domain/CarMaker.java
+++ persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/boot/jdbi/domain/CarMaker.java
@@ -11,4 +11,28 @@ public class CarMaker {
     private Long id;
     private String name;
     private List<CarModel> models;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<CarModel> getModels() {
+        return models;
+    }
+
+    public void setModels(List<CarModel> models) {
+        this.models = models;
+    }
 }
--- persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/boot/jdbi/domain/CarModel.java
+++ persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/boot/jdbi/domain/CarModel.java
@@ -11,4 +11,44 @@ public class CarModel {
     private Integer year;
     private String sku;
     private Long makerId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public Long getMakerId() {
+        return makerId;
+    }
+
+    public void setMakerId(Long makerId) {
+        this.makerId = makerId;
+    }
 }
