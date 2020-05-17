aca989958fb2143df787a11b0770b5a04dccba10
andrebrowne <42154231+andrebrowne@users.noreply.github.com>
2020-05-17 10:17:12 -0400

    Remove utf8.cnf

--- persistence-modules/hibernate-libraries/create-database.sh
+++ persistence-modules/hibernate-libraries/create-database.sh
@@ -3,6 +3,5 @@ docker run \
   -p 53306:3306 \
   --name=mysql57-hibernate-types \
   -e MYSQL_ALLOW_EMPTY_PASSWORD=true \
-  -v "${PWD}/docker/etc/mysql/conf.d":/etc/mysql/conf.d \
   -v "${PWD}/docker/docker-entrypoint-initdb.d":/docker-entrypoint-initdb.d \
   -d mysql:5.7
