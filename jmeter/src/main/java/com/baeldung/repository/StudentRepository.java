fee8656f04294b50965d019e1f968a3af9ef27a1
octavian.barbanta <octavian.barbanta@metrosystems.net>
2020-05-19 22:02:29 +0300

    BAEL-3845 - created multi-module project + added current article

--- jmeter/src/main/java/com/baeldung/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.baeldung.domain.Student;
-
-public interface StudentRepository extends MongoRepository<Student, String> {
-}
