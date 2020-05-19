4fecbde64b365228cd9895b58d4d91c1a5f57574
octavian.barbanta <octavian.barbanta@metrosystems.net>
2020-05-19 22:05:04 +0300

    BAEL-3845 - added rest of files

---
+++ jmeter/jmeter-java/src/test/java/com/baeldung/jmeter/java/JMeterFromJmxUnitTest.java
@@ -0,0 +1,33 @@
+package com.baeldung.jmeter.java;
+
+import org.apache.jmeter.engine.StandardJMeterEngine;
+import org.apache.jmeter.save.SaveService;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.collections.HashTree;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.net.URL;
+
+public class JMeterFromJmxUnitTest {
+
+    @Test
+    void startJMeterTest() throws Exception {
+
+        StandardJMeterEngine standardJMeterEngine = new StandardJMeterEngine();
+
+        JMeterUtils.loadJMeterProperties("path-to-jmeter-installation/bin/jmeter.properties");
+        JMeterUtils.setJMeterHome("path-to-jmeter-installation");
+        JMeterUtils.initLocale();
+
+        SaveService.loadProperties();
+
+        URL resource = getClass().getClassLoader().getResource("test_plan.jmx");
+        File file = new File(resource.toURI());
+        HashTree hashTree = SaveService.loadTree(file);
+
+        standardJMeterEngine.configure(hashTree);
+        standardJMeterEngine.run();
+    }
+
+}
---
+++ jmeter/jmeter-java/src/test/java/com/baeldung/jmeter/java/PureJavaJMeterUnitTest.java
@@ -0,0 +1,55 @@
+package com.baeldung.jmeter.java;
+
+import org.apache.jmeter.control.LoopController;
+import org.apache.jmeter.engine.StandardJMeterEngine;
+import org.apache.jmeter.protocol.http.sampler.HTTPSampler;
+import org.apache.jmeter.testelement.TestPlan;
+import org.apache.jmeter.threads.SetupThreadGroup;
+import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jorphan.collections.HashTree;
+import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+
+public class PureJavaJMeterUnitTest {
+
+    @Test
+    void pureJavaTest() {
+
+        StandardJMeterEngine standardJMeterEngine = new StandardJMeterEngine();
+
+        URL resource = getClass().getClassLoader().getResource("jmeter.properties");
+        JMeterUtils.loadJMeterProperties(resource.getPath());
+        JMeterUtils.initLocale();
+
+        HTTPSampler httpSampler = new HTTPSampler();
+        httpSampler.setDomain("localhost");
+        httpSampler.setPort(8080);
+        httpSampler.setPath("/login");
+        httpSampler.setMethod("GET");
+
+        LoopController loopController = new LoopController();
+        loopController.setLoops(100);
+        loopController.addTestElement(httpSampler);
+        loopController.setFirst(true);
+        loopController.initialize();
+
+        SetupThreadGroup setupThreadGroup = new SetupThreadGroup();
+        setupThreadGroup.setNumThreads(3);
+        setupThreadGroup.setRampUp(1);
+        setupThreadGroup.setSamplerController(loopController);
+
+        TestPlan testPlan = new TestPlan("My Test Plan");
+
+        HashTree testPlanHashTree = new HashTree();
+        testPlanHashTree.add(testPlan);
+
+        HashTree threadGroupHashTree = testPlanHashTree.add(testPlan, setupThreadGroup);
+        threadGroupHashTree.add(httpSampler);
+        threadGroupHashTree.add(loopController);
+
+        standardJMeterEngine.configure(testPlanHashTree);
+        standardJMeterEngine.run();
+
+    }
+}
