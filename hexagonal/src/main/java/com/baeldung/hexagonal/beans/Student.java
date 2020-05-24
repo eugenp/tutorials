dc0e916ba7a3388408ad971c419d4bdf2e5bb5fd
rajiv-guliani <65620559+rajiv-guliani@users.noreply.github.com>
2020-05-24 16:12:29 +0530

    Code formatting

--- hexagonal/src/main/java/com/baeldung/hexagonal/beans/Student.java
+++ hexagonal/src/main/java/com/baeldung/hexagonal/beans/Student.java
@@ -1,40 +1,40 @@
 package com.baeldung.hexagonal.beans;
 
 public class Student {
-	private long id;
-	private String firstName;
-	private String lastName;
-	private String address;
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String address;
 
-	public long getId() {
-		return id;
-	}
+    public long getId() {
+        return id;
+    }
 
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 
-	public String getFirstName() {
-		return firstName;
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	public String getLastName() {
-		return lastName;
-	}
+    public String getLastName() {
+        return lastName;
+    }
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
-	public String getAddress() {
-		return address;
-	}
+    public String getAddress() {
+        return address;
+    }
 
-	public void setAddress(String address) {
-		this.address = address;
-	}
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
