2ed04df220948ee85ceaf49d19edd53b16398486
rajiv-guliani <65620559+rajiv-guliani@users.noreply.github.com>
2020-05-24 16:13:16 +0530

    Code formatting

--- hexagonal/src/main/java/com/baeldung/hexagonal/interfaces/StudentDBPort.java
+++ hexagonal/src/main/java/com/baeldung/hexagonal/interfaces/StudentDBPort.java
@@ -4,9 +4,9 @@ import com.baeldung.hexagonal.beans.Student;
 
 public interface StudentDBPort {
 
-	public Object createStudent(Student pStudent);
+    public Object createStudent(Student pStudent);
 
-	public Object updateStudent(Student pStudent);
+    public Object updateStudent(Student pStudent);
 
-	public Object findById(long id);
+    public Object findById(long id);
 }
