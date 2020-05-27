47c35828d83b005e4ee6156166e12ca14d5fdda0
unknown <vatsalgosar@gmail.com>
2020-05-28 01:45:11 +0530

    bAEL-3987 - Fixed indentation

--- core-java-modules/core-java-lang-2/src/main/java/com/baeldung/inttoenum/PizzaStatus.java
+++ core-java-modules/core-java-lang-2/src/main/java/com/baeldung/inttoenum/PizzaStatus.java
@@ -4,32 +4,33 @@ import java.util.HashMap;
 import java.util.Map;
 
 public enum PizzaStatus {
-        ORDERED(5),
-        READY(2),
-        DELIVERED(0);
+    ORDERED(5),
+    READY(2),
+    DELIVERED(0);
 
-        private int timeToDelivery;
+    private int timeToDelivery;
 
-        PizzaStatus (int timeToDelivery) {
-                this.timeToDelivery = timeToDelivery;
+    PizzaStatus(int timeToDelivery) {
+        this.timeToDelivery = timeToDelivery;
+    }
+
+    public int getTimeToDelivery() {
+        return timeToDelivery;
+    }
+
+    private static Map<Integer, PizzaStatus> timeToDeliveryToEnumValuesMapping = new HashMap<>();
+
+    static {
+        PizzaStatus[] pizzaStatuses = PizzaStatus.values();
+        for (int pizzaStatusIndex = 0; pizzaStatusIndex < pizzaStatuses.length; pizzaStatusIndex++) {
+            timeToDeliveryToEnumValuesMapping.put(
+                pizzaStatuses[pizzaStatusIndex].getTimeToDelivery(),
+                pizzaStatuses[pizzaStatusIndex]
+            );
         }
+    }
 
-        public int getTimeToDelivery() {
-                return timeToDelivery;
-        }
-
-        private static Map<Integer, PizzaStatus> timeToDeliveryToEnumValuesMapping = new HashMap<>();
-        static {
-                PizzaStatus[] pizzaStatuses = PizzaStatus.values();
-                for(int pizzaStatusIndex = 0; pizzaStatusIndex < pizzaStatuses.length; pizzaStatusIndex++) {
-                        timeToDeliveryToEnumValuesMapping.put(
-                                pizzaStatuses[pizzaStatusIndex].getTimeToDelivery(),
-                                pizzaStatuses[pizzaStatusIndex]
-                        );
-                }
-        }
-
-        public static PizzaStatus castIntToEnum(int timeToDelivery) {
-                return timeToDeliveryToEnumValuesMapping.get(timeToDelivery);
-        }
+    public static PizzaStatus castIntToEnum(int timeToDelivery) {
+        return timeToDeliveryToEnumValuesMapping.get(timeToDelivery);
+    }
 }
