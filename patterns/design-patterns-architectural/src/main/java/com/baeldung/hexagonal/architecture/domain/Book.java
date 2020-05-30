7221f1444f6ab95be8ac98cce7f453216737a3f3
developerDiv <divyeshdabhi21@gmail.com>
2020-05-30 12:03:42 +0100

    Simplify code to highlight architecture

--- patterns/design-patterns-architectural/src/main/java/com/baeldung/hexagonal/architecture/domain/Book.java
+++ patterns/design-patterns-architectural/src/main/java/com/baeldung/hexagonal/architecture/domain/Book.java
@@ -1,22 +1,14 @@
 package com.baeldung.hexagonal.architecture.domain;
 
-import java.util.UUID;
-
 public class Book {
-    private UUID id;
     private String isbn;
     private String title;
     private String author;
 
-    public Book(UUID id, String isbn, String title, String author) {
-        this.id = id;
+    public Book(String isbn, String title, String author) {
         this.isbn = isbn;
         this.title = title;
         this.author = author;
-    }
-
-    public UUID getId() {
-        return id;
     }
 
     public String getIsbn() {
@@ -27,25 +19,7 @@ public class Book {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    @Override
-    public String toString() {
-        return "Book{" +
-                "id=" + id +
-                ", isbn='" + isbn + '\'' +
-                ", title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                '}';
     }
 }
--- patterns/design-patterns-architectural/src/test/java/com/baeldung/hexagonal/architecture/adapter/input/CliApiTest.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hexagonal.architecture.adapter.input;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class CliApiTest {
-
-
-
-}
