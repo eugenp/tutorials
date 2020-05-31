fe33100c073ac7755fd99ac746fcc3af53dc18f6
developerDiv <divyeshdabhi21@gmail.com>
2020-05-31 21:52:38 +0100

    Made code more compact, to reduce word count.

--- patterns/design-patterns-architectural/src/main/java/com/baeldung/hexagonal/architecture/Application.java
+++ patterns/design-patterns-architectural/src/main/java/com/baeldung/hexagonal/architecture/Application.java
@@ -2,7 +2,6 @@ package com.baeldung.hexagonal.architecture;
 
 import com.baeldung.hexagonal.architecture.adapter.incoming.Command;
 import com.baeldung.hexagonal.architecture.adapter.outgoing.InMemoryBookDatabase;
-import com.baeldung.hexagonal.architecture.adapter.outgoing.MultiLineConsoleWriter;
 import com.baeldung.hexagonal.architecture.adapter.outgoing.SingleLineConsoleWriter;
 import com.baeldung.hexagonal.architecture.domain.BookServiceDomain;
 import com.baeldung.hexagonal.architecture.port.incoming.BookService;
@@ -12,17 +11,10 @@ import com.baeldung.hexagonal.architecture.port.outgoing.BookWriter;
 public class Application {
 
     public static void main(String[] args) {
-        // MultiLineWriter
         BookRepository inMemoryBookRepository = new InMemoryBookDatabase();
-        BookWriter multiLineWriter = new MultiLineConsoleWriter();
-        BookService multiLineBookService = new BookServiceDomain(inMemoryBookRepository, multiLineWriter);
-        Command multiLineCommand = new Command(multiLineBookService);
-        multiLineCommand.run();
-
-        // comment out for singleLineConsoleWriter
-        // BookWriter singleLineWriter = new SingleLineConsoleWriter();
-        // BookService singleLineBookService = new BookServiceDomain(inMemoryBookRepository, singleLineWriter);
-        // Command singleLineCommand = new Command(singleLineBookService);
-        // singleLineCommand.run();
+        BookWriter singleLineWriter = new SingleLineConsoleWriter();
+        BookService singleLineBookService = new BookServiceDomain(inMemoryBookRepository, singleLineWriter);
+        Command singleLineCommand = new Command(singleLineBookService);
+        singleLineCommand.run();
     }
 }
