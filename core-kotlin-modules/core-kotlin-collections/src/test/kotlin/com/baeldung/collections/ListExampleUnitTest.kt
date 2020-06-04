db695999982446070b4cd0b10c9159358be2920b
Anirban Chatterjee <canirban99@gmail.com>
2020-06-04 18:07:25 +0200

    Added functions for list write and sort operations

--- core-kotlin-modules/core-kotlin-collections/src/test/kotlin/com/baeldung/collections/ListExampleUnitTest.kt
+++ core-kotlin-modules/core-kotlin-collections/src/test/kotlin/com/baeldung/collections/ListExampleUnitTest.kt
@@ -3,6 +3,7 @@ package com.baeldung.collections
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
@@ -29,5 +30,79 @@ class ListExampleUnitTest {
     @Test
     fun whenRetrieveElementsFirstAndLast_thenSuccess() {
         assertEquals("Australia", classUnderTest.retrieveElementsFirstAndLast())
+    }
+
+    @Test
+    fun whenRetrieveSubList_thenSuccess() {
+        assertEquals(3, classUnderTest.retrieveSubList().size)
+    }
+
+    @Test
+    fun whenRetrieveListSliceUsingIndices_thenSuccess() {
+        assertEquals(4, classUnderTest.retrieveListSliceUsingIndices().size)
+    }
+
+    @Test
+    fun whenRetrieveListSliceUsingIndicesList_thenSuccess() {
+        assertEquals(2, classUnderTest.retrieveListSliceUsingIndicesList().size)
+    }
+
+    @Test
+    fun whenCountList_thenSuccess() {
+        assertEquals(5, classUnderTest.countList())
+    }
+
+    @Test
+    fun whenCountListUsingPredicate_thenSuccess() {
+        assertEquals(3, classUnderTest.countListUsingPredicate())
+    }
+
+    @Test
+    fun whenCountListUsingProperty_thenSuccess() {
+        assertEquals(5, classUnderTest.countListUsingProperty())
+    }
+
+    @Test
+    fun whenAddToList_thenSuccess() {
+        assertEquals(11, classUnderTest.addToList().count())
+    }
+
+    @Test
+    fun whenRemoveFromList_thenSuccess() {
+        val list = classUnderTest.removeFromList()
+        assertEquals(3, list.size)
+        assertEquals("Sao Paulo", list[1])
+    }
+
+    @Test
+    fun whenReplaceFromList_thenSuccess() {
+        val list = classUnderTest.replaceFromList()
+        assertEquals(5, list.size)
+        assertEquals("Barcelona", list[1])
+    }
+
+    @Test
+    fun whenSortMutableList_thenSuccess() {
+        assertEquals("Sydney", classUnderTest.sortMutableList()[0])
+    }
+
+    @Test
+    fun whenSortList_thenSuccess() {
+        assertEquals("India", classUnderTest.sortList()[1])
+    }
+
+    @Test
+    fun whenCheckOneElementInList_thenSuccess() {
+        assertTrue(classUnderTest.checkOneElementInList())
+    }
+
+    @Test
+    fun whenCheckOneElementInListUsingOperator_thenSuccess() {
+        assertFalse(classUnderTest.checkOneElementInListUsingOperator())
+    }
+
+    @Test
+    fun whenCheckElementsInList_thenSuccess() {
+        assertTrue(classUnderTest.checkElementsInList())
     }
 }
