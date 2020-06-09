c2a7a971a5aa5cd7a0de5e991156946439a3fe80
Sasa M <sasam0320@gmail.com>
2020-06-09 13:10:48 +0200

    Add modified project modules in spring cloud module

---
+++ spring-cloud/spring-cloud-bootstrap/shared-dto/src/main/java/com/baeldung/shared/CustomerDTO.java
@@ -0,0 +1,17 @@
+package com.baeldung.shared;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDTO {
+
+    private String firstName;
+    private String lastName;
+    private String homeAddress;
+    private String cardNumber;
+
+}
