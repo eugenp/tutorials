b9befff7b5f100dbb313c3836e90585f7639be1e
azhwani <>
2020-06-10 18:46:08 +0100

    updating jndi.properties

--- persistence-modules/spring-persistence-simple-2/src/test/java/com/baeldung/jndi/datasource/SimpleJNDIUnitTest.java
+++ persistence-modules/spring-persistence-simple-2/src/test/java/com/baeldung/jndi/datasource/SimpleJNDIUnitTest.java
@@ -1,33 +1,31 @@
 package com.baeldung.jndi.datasource;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
+import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 
-@SuppressWarnings("deprecation")
-public class SimpleNamingContextBuilderUnitTest {
+public class SimpleJNDIUnitTest {
 
     private InitialContext initContext;
 
     @BeforeEach
-    public void init() throws Exception {
-        SimpleNamingContextBuilder.emptyActivatedContextBuilder();
+    public void setup() throws Exception {
         this.initContext = new InitialContext();
     }
 
     @Test
     public void whenMockJndiDataSource_thenReturnJndiDataSource() throws Exception {
-        this.initContext.bind("java:comp/env/jdbc/datasource", new DriverManagerDataSource("jdbc:h2:mem:testdb"));
-        DataSource ds = (DataSource) this.initContext.lookup("java:comp/env/jdbc/datasource");
+        String dsString = "org.h2.Driver::::jdbc:jdbc:h2:mem:testdb::::sa";
+        Context envContext = (Context) this.initContext.lookup("java:/comp/env");
+        DataSource ds = (DataSource) envContext.lookup("datasource/ds");
 
-        assertNotNull(ds.getConnection());
+        assertEquals(dsString, ds.toString());
     }
     
     @AfterEach
