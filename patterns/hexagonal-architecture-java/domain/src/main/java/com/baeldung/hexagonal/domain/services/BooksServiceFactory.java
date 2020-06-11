846f90d628e5b3518566c16004a279227e6983a7
Daniel Strmecki <daniel.strmecki@gmail.com>
2020-06-11 16:15:21 +0200

    strmecki - refactoring for hexagonal architecture in java

--- patterns/hexagonal-architecture-java/domain/src/main/java/com/baeldung/hexagonal/domain/services/BooksServiceFactory.java
+++ patterns/hexagonal-architecture-java/domain/src/main/java/com/baeldung/hexagonal/domain/services/BooksServiceFactory.java
@@ -3,7 +3,7 @@ package com.baeldung.hexagonal.domain.services;
 import com.baeldung.hexagonal.domain.repo.BooksRepository;
 import com.baeldung.hexagonal.domain.services.impl.BooksServiceImpl;
 
-public class ServiceFactory {
+public class BooksServiceFactory {
 
     public static BooksService getBooksService(BooksRepository booksRepository) {
         return new BooksServiceImpl(booksRepository);
