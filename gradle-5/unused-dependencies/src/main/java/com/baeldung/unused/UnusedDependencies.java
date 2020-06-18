bc13529859dbed9a3e7a3650d90304fe982ed5bc
Sorin Zamfir <sorin.zamfir@outlook.com>
2020-06-18 23:06:07 +0300

    BAEL-4090: Initial module structure

---
+++ gradle-5/unused-dependencies/src/main/java/com/baeldung/unused/UnusedDependencies.java
@@ -0,0 +1,12 @@
+package com.baeldung.unused;
+
+public class UnusedDependencies {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+//        List<String> list = ImmutableList.of("Baledung", "is", "cool");
+//        System.out.println(list.stream().collect(Collectors.joining(" ")));
+    }
+
+}
