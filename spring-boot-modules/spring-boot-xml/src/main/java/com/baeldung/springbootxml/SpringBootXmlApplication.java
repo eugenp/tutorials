3cb402ced71073a70875cc770e2abc47b0e9ef5e
marcodenisi <marcodenisi35@gmail.com>
2020-06-18 17:58:25 +0200

    BAEL-4083 - removed sample with xml context-config

--- spring-boot-modules/spring-boot-xml/src/main/java/com/baeldung/springbootxml/SpringBootXmlApplication.java
+++ spring-boot-modules/spring-boot-xml/src/main/java/com/baeldung/springbootxml/SpringBootXmlApplication.java
@@ -3,12 +3,10 @@ package com.baeldung.springbootxml;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @ImportResource("classpath:beans.xml")
 public class SpringBootXmlApplication implements CommandLineRunner {
 
