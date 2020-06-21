de4c260b72e416c556a6251275db67587ffc3f8f
Sarcode <sarkout.mahmoud@rabobank.nl>
2020-06-21 23:14:51 +0200

    rework

--- patterns/design-patterns-structural/src/main/java/com/baeldung/hexagonal/user/CarUserAdapter.java
+++ patterns/design-patterns-structural/src/main/java/com/baeldung/hexagonal/user/CarUserAdapter.java
@@ -5,28 +5,16 @@ import com.baeldung.hexagonal.domain.CarPortUserSide;
 import com.baeldung.hexagonal.domain.CarService;
 import com.baeldung.hexagonal.server.CarServerAdapter;
 
-import static com.baeldung.util.LoggerUtil.LOG;
-
 public class CarUserAdapter {
 
-    public static final String RETRIEVE_CONSTANT = "/all";
-    private static CarPortUserSide carPortUserSide = new CarService(new CarServerAdapter());
+    private CarPortUserSide carPortUserSide = new CarService(new CarServerAdapter());
 
-    private static void submit(Car car) {
+    public void submit(Car car) {
         carPortUserSide.submitCar(car);
     }
 
-    private static void retrieve() {
-        LOG.info(carPortUserSide.retrieveCars());
-    }
-
-    public static void main(String[] args) {
-        if (RETRIEVE_CONSTANT.equals(args[0]) && args[1] == null) {
-            retrieve();
-        } else {
-            Car car = new Car(args[0], args[1]);
-            submit(car);
-        }
+    public void retrieve() {
+        carPortUserSide.retrieveCars();
     }
 
 }
