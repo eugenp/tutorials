339ea43e617edd0be1b45d90620006ee2c7924c3
azhwani <>
2020-06-22 20:12:08 +0100

    Add MapFromYamlApplication.java

---
+++ spring-boot-modules/spring-boot-properties-2/src/main/java/com/baeldung/properties/yamlmap/MapFromYamlApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.properties.yamlmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapFromYamlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapFromYamlApplication.class, args);
+    }
+
+}
