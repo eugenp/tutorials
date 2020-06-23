a718b382d296d6d8649312d9d36567a65133fb7f
Nguyen Nam Thai <namthainguyen@gmail.com>
2020-06-23 18:01:17 +0700

    BAEL-4068 Remove AspectJ DI from module spring-di

--- spring-di/src/main/java/com/baeldung/di/aspectj/AspectJConfig.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.di.aspectj;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
-
-@ComponentScan
-@EnableSpringConfigured
-public class AspectJConfig {
-}
