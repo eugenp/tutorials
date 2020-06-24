2c385acf710a1db8f814ccf42559e1ada93223a5
Ali Dehghani <ali.dehghani.g@gmail.com>
2020-06-24 15:21:53 +0430

    Fixed a typo

--- core-java-modules/core-java-jvm-2/src/test/java/com/baeldung/memlayout/MemoryLayoutUnitTest.java
+++ core-java-modules/core-java-jvm-2/src/test/java/com/baeldung/memlayout/MemoryLayoutUnitTest.java
@@ -91,7 +91,7 @@ public class MemoryLayoutUnitTest {
         private boolean first;
         private char second;
         private double third;
-        private int forth;
+        private int fourth;
         private boolean fifth;
     }
 
