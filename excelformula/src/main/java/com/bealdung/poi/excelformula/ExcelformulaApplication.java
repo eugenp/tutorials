67e9dc8187222f9746889f090cef492425ab1fce
STS <STS>
2020-06-28 20:33:10 +0200

    add excel formula

---
+++ excelformula/src/main/java/com/bealdung/poi/excelformula/ExcelformulaApplication.java
@@ -0,0 +1,13 @@
+package com.bealdung.poi.excelformula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelformulaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelformulaApplication.class, args);
+    }
+
+}
