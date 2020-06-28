476f4f6971c4bb974e35a5a9dd888db377985717
Daniel Strmecki <daniel.strmecki@gmail.com>
2020-06-28 15:11:59 +0200

    BAEL-4280: Refactor

--- core-java-modules/core-java-collections-list-3/src/main/java/com/baeldung/list/difference/FindDifferencesBetweenLists.java
+++ core-java-modules/core-java-collections-list-3/src/main/java/com/baeldung/list/difference/FindDifferencesBetweenLists.java
@@ -6,38 +6,38 @@ import org.apache.commons.collections4.CollectionUtils;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class FindDifferenceBetweenLists {
+public class FindDifferencesBetweenLists {
 
-    public static <T> List<T> differenceUsingPlainJavaCollections(List<T> listOne, List<T> listTwo) {
-        List<T> differences = differenceUsingCollections(listOne, listTwo);
-        differences.addAll(differenceUsingCollections(listTwo, listOne));
+    public static <T> List<T> differencesUsingPlainJavaCollections(List<T> listOne, List<T> listTwo) {
+        List<T> differences = differencesUsingCollections(listOne, listTwo);
+        differences.addAll(differencesUsingCollections(listTwo, listOne));
         return differences;
     }
 
-    private static <T> List<T> differenceUsingCollections(List<T> listOne, List<T> listTwo) {
+    private static <T> List<T> differencesUsingCollections(List<T> listOne, List<T> listTwo) {
         Set<T> differences = new HashSet<>(listOne);
         differences.removeAll(listTwo);
         return new ArrayList<>(differences);
     }
 
-    public static <T> List<T> differenceUsingPlainJavaStream(List<T> listOne, List<T> listTwo) {
-        List<T> differences = differenceUsingStream(listOne, listTwo);
-        differences.addAll(differenceUsingStream(listTwo, listOne));
+    public static <T> List<T> differencesUsingPlainJavaStream(List<T> listOne, List<T> listTwo) {
+        List<T> differences = differencesUsingStream(listOne, listTwo);
+        differences.addAll(differencesUsingStream(listTwo, listOne));
         return differences;
     }
 
-    private static <T> List<T> differenceUsingStream(List<T> listOne, List<T> listTwo) {
+    private static <T> List<T> differencesUsingStream(List<T> listOne, List<T> listTwo) {
         return listOne.stream()
             .filter(element -> !listTwo.contains(element))
             .distinct()
             .collect(Collectors.toList());
     }
 
-    public static <T> List<T> differenceUsingApacheCommonsCollections(List<T> listOne, List<T> listTwo) {
+    public static <T> List<T> differencesUsingApacheCommonsCollections(List<T> listOne, List<T> listTwo) {
         return new ArrayList<>(new HashSet<>(CollectionUtils.disjunction(listOne, listTwo)));
     }
 
-    public static <T> List<T> differenceUsingGoogleGuava(List<T> listOne, List<T> listTwo) {
+    public static <T> List<T> differencesUsingGoogleGuava(List<T> listOne, List<T> listTwo) {
         List<T> differences = new ArrayList<>(Sets.difference(Sets.newHashSet(listOne), Sets.newHashSet(listTwo)));
         differences.addAll(new ArrayList<>(Sets.difference(Sets.newHashSet(listTwo), Sets.newHashSet(listOne))));
         return differences;
--- core-java-modules/core-java-collections-list-3/src/test/java/com/baeldung/list/difference/FindDifferencesBetweenListsUnitTest.java
+++ core-java-modules/core-java-collections-list-3/src/test/java/com/baeldung/list/difference/FindDifferencesBetweenListsUnitTest.java
@@ -7,40 +7,40 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class FindDifferenceBetweenListsUnitTest {
+public class FindDifferencesBetweenListsUnitTest {
 
     private static List<String> listOneWithDuplicates = Arrays.asList("Jack", "Tom", "Sam", "James", "Tom");
     private static List<String> listTwoWithDuplicates = Arrays.asList("Jack", "Daniel", "Sam", "Alan", "James", "Daniel");
-    private static List<String> expectedDifference = Arrays.asList("Tom", "Daniel", "Alan");
+    private static List<String> expectedDifferences = Arrays.asList("Tom", "Daniel", "Alan");
 
     @Test
     public void givenTwoListsWithDifferentElements_whenUsingPlainJavaListsImpl_thenExpectedDifferencesAreFound() {
-        List<String> differences = FindDifferenceBetweenLists.differenceUsingPlainJavaCollections(listOneWithDuplicates, listTwoWithDuplicates);
+        List<String> differences = FindDifferencesBetweenLists.differencesUsingPlainJavaCollections(listOneWithDuplicates, listTwoWithDuplicates);
         assertExpectedDifferencesAreFound(differences);
     }
 
     @Test
     public void givenTwoListsWithDifferentElements_whenUsingPlainJavaStreamsImpl_thenExpectedDifferencesAreFound() {
-        List<String> differences = FindDifferenceBetweenLists.differenceUsingPlainJavaStream(listOneWithDuplicates, listTwoWithDuplicates);
+        List<String> differences = FindDifferencesBetweenLists.differencesUsingPlainJavaStream(listOneWithDuplicates, listTwoWithDuplicates);
         assertExpectedDifferencesAreFound(differences);
     }
 
     @Test
     public void givenTwoListsWithDifferentElements_whenUsingGoogleGuavaImpl_thenExpectedDifferencesAreFound() {
-        List<String> differences = FindDifferenceBetweenLists.differenceUsingGoogleGuava(listOneWithDuplicates, listTwoWithDuplicates);
+        List<String> differences = FindDifferencesBetweenLists.differencesUsingGoogleGuava(listOneWithDuplicates, listTwoWithDuplicates);
         assertExpectedDifferencesAreFound(differences);
     }
 
     @Test
     public void givenTwoListsWithDifferentElements_whenUsingApacheCommonsCollectionsImpl_thenExpectedDifferencesAreFound() {
-        List<String> differences = FindDifferenceBetweenLists.differenceUsingApacheCommonsCollections(listOneWithDuplicates, listTwoWithDuplicates);
+        List<String> differences = FindDifferencesBetweenLists.differencesUsingApacheCommonsCollections(listOneWithDuplicates, listTwoWithDuplicates);
         assertExpectedDifferencesAreFound(differences);
     }
 
     private void assertExpectedDifferencesAreFound(List<String> differences) {
         assertAll(
                 () -> assertEquals(3, differences.size()),
-                () -> assertTrue(differences.containsAll(expectedDifference))
+                () -> assertTrue(differences.containsAll(expectedDifferences))
         );
     }
 
