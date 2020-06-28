8b8554eb84e9d02e73affde7b302b0a87ccbf1b5
akeshri <ashwini.nitdgp@gmail.com>
2020-06-28 22:32:39 +0530

    Add files via upload

---
+++ hexagonal-architecture/src/main/java/com/baeldung/hexagonal/architecture/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.baeldung.hexagonal.architecture.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.baeldung.hexagonal.architecture.model.Product;
+
+/**
+ * @author AshwiniKeshri
+ *
+ */
+
+public interface ProductRepository extends JpaRepository<Product,Long>{
+
+}
