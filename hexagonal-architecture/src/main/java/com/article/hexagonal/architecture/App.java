36d37894420227c3e56ff244822b239c268b6db0
akeshri <ashwini.nitdgp@gmail.com>
2020-07-03 22:32:44 +0530

    1.fixed code format 2. resolved build issue

---
+++ hexagonal-architecture/src/main/java/com/article/hexagonal/architecture/App.java
@@ -0,0 +1,16 @@
+package com.article.hexagonal.architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author AshwiniKeshri
+ *
+ */
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ hexagonal-architecture/src/test/java/com/article/hexagonal/architecture/service/ProductServiceTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.article.hexagonal.architecture.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.article.hexagonal.architecture.App;
+import com.article.hexagonal.architecture.model.Product;
+import com.article.hexagonal.architecture.service.ProductService;
+
+/**
+ * @author AshwiniKeshri
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(App.class)
+@ActiveProfiles(value = "test")
+public class ProductServiceTest {
+
+    @Autowired
+    private ProductService productService;
+
+    @Test
+    public void testCreateProduct() {
+        Product product = new Product();
+        product.setDescription("test product");
+        product.setName("Product1");
+        product.setPrice(10.0);
+        product.setQuantity(100l);
+        Long id = productService.create(product);
+        Assert.assertTrue(id > 0);
+    }
+
+}
