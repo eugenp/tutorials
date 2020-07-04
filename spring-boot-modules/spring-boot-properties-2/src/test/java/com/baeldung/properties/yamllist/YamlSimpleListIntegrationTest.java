decb1516e3dc434c7a7dd25dd765c4afca91fe72
azhwani <>
2020-07-04 11:58:20 +0100

    Create test classes

---
+++ spring-boot-modules/spring-boot-properties-2/src/test/java/com/baeldung/properties/yamllist/YamlSimpleListIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.baeldung.properties.yamllist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baeldung.properties.yamllist.pojo.ApplicationProps;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class YamlSimpleListIntegrationTest {
+
+    @Autowired
+    private ApplicationProps applicationProps;
+
+    @Test
+    public void whenYamlList_thenLoadSimpleList() {
+        assertThat(applicationProps.getProfiles()
+            .get(0)).isEqualTo("dev");
+        assertThat(applicationProps.getProfiles()
+            .get(4)
+            .getClass()
+            .toString()).isEqualTo("class java.lang.Integer");
+        assertThat(applicationProps.getProfiles()
+            .size()).isEqualTo(5);
+    }
+}
