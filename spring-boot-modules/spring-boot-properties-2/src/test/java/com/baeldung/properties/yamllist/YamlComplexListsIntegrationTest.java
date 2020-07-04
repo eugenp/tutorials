a1732d99a70942dcf95e315c5e46b1384f12dd85
azhwani <>
2020-07-04 11:58:20 +0100

    Create test classes

---
+++ spring-boot-modules/spring-boot-properties-2/src/test/java/com/baeldung/properties/yamllist/YamlComplexListsIntegrationTest.java
@@ -0,0 +1,30 @@
+package com.baeldung.properties.yamllist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baeldung.properties.yamllist.pojo.ApplicationProps;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class YamlComplexListsIntegrationTest {
+
+    @Autowired
+    private ApplicationProps applicationProps;
+
+    @Test
+    public void whenYamlNestedLists_thenLoadComplexLists() {
+        assertThat(applicationProps.getUsers()
+            .get(0)
+            .getPassword()).isEqualTo("admin@10@");
+        assertThat(applicationProps.getTeam()
+            .get(0)
+            .size()).isEqualTo(3);
+    }
+
+}
