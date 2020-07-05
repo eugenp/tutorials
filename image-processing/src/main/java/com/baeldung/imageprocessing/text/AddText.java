e15002be1a43c4a8eb71631c6825e61f8d68c8c4
sergio41 <sergiorr41@gmail.com>
2020-07-05 19:27:59 +0200

    [BAEL-4240] Adapting Text to the image

--- image-processing/src/main/java/com/baeldung/imageprocessing/text/AddText.java
+++ image-processing/src/main/java/com/baeldung/imageprocessing/text/AddText.java
@@ -5,6 +5,7 @@ import ij.ImagePlus;
 import ij.process.ImageProcessor;
 
 import java.awt.*;
+import java.awt.font.GlyphVector;
 import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -34,6 +35,9 @@ public class AddText {
 
         ImagePlus resultGraphicsTopLeft= new ImagePlus("", signImageTopLeft("www.baeldung.com", imagePath));
         resultGraphicsTopLeft.show();
+
+        ImagePlus resultGraphicsAdaptBasedOnImage= new ImagePlus("", signImageAdaptBasedOnImage("www.baeldung.com", imagePath));
+        resultGraphicsAdaptBasedOnImage.show();
     }
 
     private static ImagePlus signImageImageProcessor(String text, String path) {
@@ -159,5 +163,58 @@ public class AddText {
         g.drawString(attributedText.getIterator(), positionX, positionY);
                 
         return image;
+    }
+    
+    /**
+     * Draw a String centered in the middle of a Rectangle.
+     *
+     * @param g The Graphics instance.
+     * @param text The String to draw.
+     * @param rect The Rectangle to center the text in.
+     * @throws IOException 
+     */
+    public static BufferedImage signImageAdaptBasedOnImage(String text, String path) throws IOException {
+        
+        BufferedImage image = ImageIO.read(new File(path));
+        
+        Font font = createFontToFit(new Font("Arial", Font.BOLD, 80), text, image);
+        
+        AttributedString attributedText = new AttributedString(text);
+        attributedText.addAttribute(TextAttribute.FONT, font);
+        attributedText.addAttribute(TextAttribute.FOREGROUND, Color.GREEN);
+        
+        Graphics g = image.getGraphics();
+        
+        FontMetrics metrics = g.getFontMetrics(font);
+        int positionX = (image.getWidth() - metrics.stringWidth(text));
+        int positionY = (image.getHeight() - metrics.getHeight()) + metrics.getAscent();
+        
+        g.drawString(attributedText.getIterator(), positionX, positionY);
+                
+        return image;
+    }
+    
+    public static Font createFontToFit(Font baseFont, String text, BufferedImage image) throws IOException
+    {              
+        Font newFont = baseFont;
+        
+        FontMetrics ruler = image.getGraphics().getFontMetrics(baseFont);
+        GlyphVector vector = baseFont.createGlyphVector(ruler.getFontRenderContext(), text);
+        
+        Shape outline = vector.getOutline(0, 0);
+        
+        double expectedWidth = outline.getBounds().getWidth();
+        double expectedHeight = outline.getBounds().getHeight();
+        
+        boolean textFits = image.getWidth() >= expectedWidth && image.getHeight() >= expectedHeight;
+        
+        if(!textFits) {
+            double widthBasedFontSize = (baseFont.getSize2D()*image.getWidth())/expectedWidth;
+            double heightBasedFontSize = (baseFont.getSize2D()*image.getHeight())/expectedHeight;
+            
+            double newFontSize = widthBasedFontSize < heightBasedFontSize ? widthBasedFontSize : heightBasedFontSize;
+            newFont = baseFont.deriveFont(baseFont.getStyle(), (float)newFontSize);
+        }
+        return newFont;
     }
 }
