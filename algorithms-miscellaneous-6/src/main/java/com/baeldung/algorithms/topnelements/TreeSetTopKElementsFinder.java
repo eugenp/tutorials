4f6c33f935dfbbedbc693a76ac65130a1cd7ab8f
helga_sh <v.shyrayeva@sam-solutions.com>
2020-07-06 17:39:18 +0300

    Top k elements in an array

---
+++ algorithms-miscellaneous-6/src/main/java/com/baeldung/algorithms/topnelements/TreeSetTopKElementsFinder.java
@@ -0,0 +1,17 @@
+package com.baeldung.algorithms.topnelements;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+
+public class TreeSetTopKElementsFinder implements TopKElementsFinder<Integer> {
+
+    public List<Integer> findTopK(List<Integer> input, int k) {
+        Set<Integer> sortedSet = new TreeSet<>(Comparator.reverseOrder());
+        sortedSet.addAll(input);
+
+        return sortedSet.stream().limit(k).collect(Collectors.toList());
+    }
+}
