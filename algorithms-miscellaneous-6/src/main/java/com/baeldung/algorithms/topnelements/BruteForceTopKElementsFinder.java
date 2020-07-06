c792a4fdfe2a906066bc2b80c82d5b9c6f1f39d0
helga_sh <v.shyrayeva@sam-solutions.com>
2020-07-06 17:39:18 +0300

    Top k elements in an array

---
+++ algorithms-miscellaneous-6/src/main/java/com/baeldung/algorithms/topnelements/BruteForceTopKElementsFinder.java
@@ -0,0 +1,26 @@
+package com.baeldung.algorithms.topnelements;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BruteForceTopKElementsFinder implements TopKElementsFinder<Integer> {
+
+    public List<Integer> findTopK(List<Integer> input, int k) {
+        List<Integer> array = new ArrayList<>(input);
+        List<Integer> topKList = new ArrayList<>();
+
+        for (int i = 0; i < k; i++) {
+            int maxIndex = 0;
+
+            for (int j = 1; j < array.size(); j++) {
+                if (array.get(j) > array.get(maxIndex)) {
+                    maxIndex = j;
+                }
+            }
+
+            topKList.add(array.remove(maxIndex));
+        }
+
+        return topKList;
+    }
+}
