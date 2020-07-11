3e31cb49b16682206ba189db0233d1e5bc0b43fd
STS <STS>
2020-07-11 10:31:02 +0200

    changes after adding excel file

--- apache-poi/src/test/java/com/baeldung/poi/excel/setFormula/ExcelFormulaUnitTest.java
+++ apache-poi/src/test/java/com/baeldung/poi/excel/setFormula/ExcelFormulaUnitTest.java
@@ -2,28 +2,48 @@ package com.baeldung.poi.excel.setFormula;
 
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
 
 class ExcelFormulaUnitTest {
+    private static String FILE_NAME = "SetFormulaTest.xlsx";
+    private String fileLocation;
+    private ExcelFormula excelFormula;
+
+    @BeforeEach
+    public void setup() throws URISyntaxException {
+        fileLocation = Paths.get(ClassLoader.getSystemResource(FILE_NAME).toURI()).toString();
+        excelFormula = new ExcelFormula();
+    }
+
     @Test
     void givenExcelData_whenSetAndEvaluateFormula() throws IOException {
-        ExcelFormula excelFormula = new ExcelFormula();
-        List<Integer> data = Arrays.asList(2, 5, 10, 15, 7, 9);
-        XSSFSheet sheet = excelFormula.inserData(data);
-        int result = 0;
+        FileInputStream inputStream = new FileInputStream(new File(fileLocation));
+        XSSFWorkbook wb = new XSSFWorkbook(inputStream);
+        XSSFSheet sheet = wb.getSheetAt(0);
+        double resultColumnA = 0;
+        double resultColumnB = 0;
         for (int row = 0; row <= sheet.getLastRowNum(); row++) {
-            result += sheet.getRow(row).getCell(0).getNumericCellValue();
+            resultColumnA += sheet.getRow(row).getCell(0).getNumericCellValue();
+            resultColumnB += sheet.getRow(row).getCell(1).getNumericCellValue();
         }
-        String colName = CellReference.convertNumToColString(0);
-        String startCell = colName + 1;
-        String stopCell = colName + (sheet.getLastRowNum() + 1);
-        String sumFormula = String.format("SUM(%s:%s)", startCell, stopCell);
-        int resultValue = (int) excelFormula.setFormula(sumFormula);
-        Assert.assertEquals("The results are the same!", resultValue , result);
+        String colNameA = CellReference.convertNumToColString(0);
+        String colNameB = CellReference.convertNumToColString(1);
+        String startCellA = colNameA + 1;
+        String stopCellA = colNameA + (sheet.getLastRowNum() + 1);
+        String sumFormulaForColumnA = String.format("SUM(%s:%s)", startCellA, stopCellA);
+        String startCellB = colNameB + 1;
+        String stopCellB = colNameB + (sheet.getLastRowNum() + 1);
+        String sumFormulaForColumnB = String.format("SUM(%s:%s)", startCellB, stopCellB);
+        double resultValue = excelFormula.setFormula(fileLocation, wb, sumFormulaForColumnA + "-" + sumFormulaForColumnB);
+        Assert.assertEquals(resultValue, resultColumnA - resultColumnB, 0d);
     }
 }
