52501da4cfe33c062ee6f21c5a5a7e81e699a2a6
Sebastian Luna <sebastian.x.luna.-@disney.com>
2020-07-12 21:11:02 -0500

    Remove Extra line

--- ddd/src/main/java/com/baeldung/hexagonalarchitecture/HexagonalArchitectureApplication.java
+++ ddd/src/main/java/com/baeldung/hexagonalarchitecture/HexagonalArchitectureApplication.java
@@ -9,5 +9,4 @@ public class HexagonalArchitectureApplication {
     public static void main(String[] args) {
         SpringApplication.run(HexagonalArchitectureApplication.class, args);
     }
-
 }
