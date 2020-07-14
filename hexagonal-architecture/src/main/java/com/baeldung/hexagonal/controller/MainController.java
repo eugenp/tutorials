cbbd765f5e4f0404632c8cdd7ab39f3d825d977a
Raymond Chenon <raymond.chenon@gmail.com>
2020-07-14 20:49:51 +0200

    fix Maven source and test folder

--- hexagonal-architecture/src/main/java/com/baeldung/hexagonal/controller/MainController.java
+++ hexagonal-architecture/src/main/java/com/baeldung/hexagonal/controller/MainController.java
@@ -1,4 +1,4 @@
-package main.java.com.baeldung.hexagonal.controller;
+package com.baeldung.hexagonal.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
