47d457a15a02162260f9e3e50faa08b1d1a20baa
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2020-07-15 13:19:18 +0200

    BAEL-3917: Rename live tests to *LiveTest

--- ddd/src/test/java/com/baeldung/dddhexagonalspring/infrastracture/repository/MongoDbOrderRepositoryLiveTest.java
+++ ddd/src/test/java/com/baeldung/dddhexagonalspring/infrastracture/repository/MongoDbOrderRepositoryLiveTest.java
@@ -16,22 +16,27 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import com.baeldung.dddhexagonalspring.domain.Order;
 import com.baeldung.dddhexagonalspring.domain.Product;
 import com.baeldung.dddhexagonalspring.domain.repository.OrderRepository;
-import com.baeldung.dddhexagonalspring.infrastracture.repository.cassandra.SpringDataCassandraOrderRepository;
+import com.baeldung.dddhexagonalspring.infrastracture.repository.mongo.SpringDataMongoOrderRepository;
 
+/*
+ To run this test we need to run the databases first.
+ A dedicated docker-compose.yml file is located under the resources directory.
+ We can run it by simple executing `docker-compose up`.
+ */
 @SpringJUnitConfig
 @SpringBootTest
 @TestPropertySource("classpath:ddd-layers-test.properties")
-class CassandraDbOrderRepositoryIntegrationTest {
+class MongoDbOrderRepositoryLiveTest {
 
     @Autowired
-    private SpringDataCassandraOrderRepository cassandraOrderRepository;
+    private SpringDataMongoOrderRepository mongoOrderRepository;
 
     @Autowired
     private OrderRepository orderRepository;
 
     @AfterEach
     void cleanUp() {
-        cassandraOrderRepository.deleteAll();
+        mongoOrderRepository.deleteAll();
     }
 
     @Test
@@ -40,8 +45,6 @@ class CassandraDbOrderRepositoryIntegrationTest {
         // given
         final UUID id = UUID.randomUUID();
         final Order order = createOrder(id);
-        order.addOrder(new Product(UUID.randomUUID(), BigDecimal.TEN, "second"));
-        order.complete();
 
         // when
         orderRepository.save(order);
