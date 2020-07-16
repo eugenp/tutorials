ceeea4f1abf21407004a2ec24f492ddc30723983
Saurabh Bakshi <saurabhbak1983@gmail.com>
2020-07-16 16:05:16 +0530

    PR review changes: Added more scenarios to the static variable test
    Covered the following cases:
    1. Variable initialization in a static block
    2. Variable initialization in a static nested class

--- core-java-modules/core-java-lang-3/src/test/java/com/baeldung/staticvariables/StaticVariableUnitTest.java
+++ core-java-modules/core-java-lang-3/src/test/java/com/baeldung/staticvariables/StaticVariableUnitTest.java
@@ -33,4 +33,81 @@ public class StaticVariableUnitTest {
         }
 
     }
+
+    @Test
+    public void initializeStaticVariable_checkStaticBlock() {
+
+        try {
+            Class<?> staticVariableDemo = this.getClass()
+                .getClassLoader()
+                .loadClass("com.baeldung.staticvariables.StaticVariableDemo");
+
+            Field field1 = staticVariableDemo.getField("z");
+
+            assertThat(field1.getInt(staticVariableDemo)).isEqualTo(30);
+
+            Field field2 = staticVariableDemo.getField("a");
+
+            assertThat(field2.getInt(staticVariableDemo)).isEqualTo(50);
+
+        } catch (ClassNotFoundException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void initializeStaticVariable_checkFinalValues() {
+
+        try {
+            Class<?> staticVariableDemo = this.getClass()
+                .getClassLoader()
+                .loadClass("com.baeldung.staticvariables.StaticVariableDemo");
+
+            Field field1 = staticVariableDemo.getField("b");
+
+            assertThat(field1.getInt(staticVariableDemo)).isEqualTo(100);
+
+        } catch (ClassNotFoundException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void initializeStaticVariable_checkInnerClassValues() {
+
+        try {
+            Class<?> staticVariableDemo = this.getClass()
+                .getClassLoader()
+                .loadClass("com.baeldung.staticvariables.StaticVariableDemo");
+
+            Class<?>[] nestedClasses = staticVariableDemo.getClasses();
+
+            for (Class<?> nestedClass : nestedClasses) {
+                if (nestedClass.getName()
+                    .equals("Nested")) {
+
+                    Field field1 = nestedClass.getField("nestedClassStaticVariable");
+                    assertThat(field1.get(nestedClass)).isEqualTo("test");
+                }
+            }
+
+        } catch (ClassNotFoundException | NoSuchFieldException | SecurityException e) {
+            e.printStackTrace();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
