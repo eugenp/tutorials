3ccb59bf84c06e6c50757af8440ca9d2b16ace4a
kwoyke <krzysztofwoyke@gmail.com>
2020-07-16 09:15:40 +0200

    BAEL-3917: Fix the integrations tests in ddd (#9708)

--- ddd/src/test/java/com/baeldung/ddd/order/jpa/PersistOrderLiveTest.java
+++ ddd/src/test/java/com/baeldung/ddd/order/jpa/PersistOrderLiveTest.java
@@ -1,19 +1,24 @@
 package com.baeldung.ddd.order.jpa;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.math.BigDecimal;
-import java.util.Arrays;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/*
+ To run this test we need to run the databases first.
+ A dedicated docker-compose.yml file is located under the resources directory.
+ We can run it by simple executing `docker-compose up`.
+ */
 @SpringJUnitConfig
 @SpringBootTest
-public class PersistOrderIntegrationTest {
+public class PersistOrderLiveTest {
     @Autowired
     private JpaOrderRepository repository;
 
