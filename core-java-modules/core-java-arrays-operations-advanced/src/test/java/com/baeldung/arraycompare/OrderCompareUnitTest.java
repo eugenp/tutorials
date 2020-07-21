cffc591b5cd84ef056bc90d5e3a3d087d8882850
Joao Esperancinha <jofisaes@gmail.com>
2020-07-21 06:55:28 +0200

    [BAEL-4281] Naming convention and conversion to assertJ

--- core-java-modules/core-java-arrays-operations-advanced/src/test/java/com/baeldung/arraycompare/OrderCompareUnitTest.java
+++ core-java-modules/core-java-arrays-operations-advanced/src/test/java/com/baeldung/arraycompare/OrderCompareUnitTest.java
@@ -5,11 +5,12 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.Comparator;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 public class OrderCompareUnitTest {
     @Test
-    public void givenArray1andArray2_whenNotSameContent_thenNotDeepEquals() {
+    public void givenSameContentDifferentOrder_whenSortedAndDeepEquals_thenTrue() {
         final Plane[][] planes1 = new Plane[][] {
             new Plane[] { new Plane("Plane 1", "A320"), new Plane("Plane 2", "B738") } };
         final Plane[][] planes2 = new Plane[][] {
@@ -27,7 +28,6 @@ public class OrderCompareUnitTest {
         Arrays.sort(planes1[0], planeComparator);
         Arrays.sort(planes2[0], planeComparator);
 
-        boolean result = Arrays.deepEquals(planes1, planes2);
-        assertTrue("Result is false", result);
+        assertThat(Arrays.deepEquals(planes1, planes2)).isTrue();
     }
 }
--- core-java-modules/core-java-arrays-operations-advanced/src/test/java/com/baeldung/arraycompare/ReferenceCompareUnitTest.java
+++ core-java-modules/core-java-arrays-operations-advanced/src/test/java/com/baeldung/arraycompare/ReferenceCompareUnitTest.java
@@ -2,33 +2,29 @@ package com.baeldung.arraycompare;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertSame;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReferenceCompareUnitTest {
 
     @Test
-    public void givenArray1andArray2_whenEquals_thenEqual() {
+    public void givenSameReferences_whenSame_thenTrue() {
         final String[] planes1 = new String[] { "A320", "B738", "A321", "A319", "B77W", "B737", "A333", "A332" };
         final String[] planes2 = planes1;
 
-        assertSame("Objects are not equal!", planes1, planes2);
+        assertThat(planes1).isSameAs(planes2);
 
         planes2[0] = "747";
 
-        assertSame("Objects are not same!", planes1, planes2);
-        assertEquals("Objects are not equal!", "747", planes2[0]);
-        assertEquals("Objects are not equal!", "747", planes1[0]);
+        assertThat(planes1).isSameAs(planes2);
+        assertThat(planes2[0]).isEqualTo("747");
+        assertThat(planes1[0]).isEqualTo("747");
     }
 
     @Test
-    public void givenArray1andArray2_whenDifferentValues_thenNotEqual() {
+    public void givenSameContentDifferentReferences_whenSame_thenFalse() {
         final String[] planes1 = new String[] { "A320", "B738", "A321", "A319", "B77W", "B737", "A333", "A332" };
         final String[] planes2 = new String[] { "A320", "B738", "A321", "A319", "B77W", "B737", "A333", "A332" };
 
-        assertNotSame("Objects are the same!", planes1, planes2);
-        assertNotEquals("Objects are equal!", planes1, planes2);
+        assertThat(planes1).isNotSameAs(planes2);
     }
 }
