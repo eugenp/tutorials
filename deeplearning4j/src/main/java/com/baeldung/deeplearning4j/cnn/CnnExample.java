51f1fc9b1e07d5bd342b583b750b66bcabf13838
helga_sh <v.shyrayeva@sam-solutions.com>
2020-07-23 16:17:04 +0300

    CNN example with Deeplearning4j in Java: refactor

--- deeplearning4j/src/main/java/com/baeldung/deeplearning4j/cnn/CnnExample.java
+++ deeplearning4j/src/main/java/com/baeldung/deeplearning4j/cnn/CnnExample.java
@@ -1,14 +1,11 @@
 package com.baeldung.deeplearning4j.cnn;
 
 
-import com.baeldung.deeplearning4j.cnn.domain.network.CnnModel;
-import com.baeldung.deeplearning4j.cnn.domain.network.CnnModelProperties;
-import com.baeldung.deeplearning4j.cnn.service.dataset.CifarDataSetService;
 import lombok.extern.slf4j.Slf4j;
 import org.deeplearning4j.eval.Evaluation;
 
 @Slf4j
-public class CnnExample {
+class CnnExample {
 
     public static void main(String... args) {
         CnnModel network = new CnnModel(new CifarDataSetService(), new CnnModelProperties());
--- deeplearning4j/src/main/java/com/baeldung/deeplearning4j/cnn/CnnModelProperties.java
+++ deeplearning4j/src/main/java/com/baeldung/deeplearning4j/cnn/CnnModelProperties.java
@@ -1,10 +1,10 @@
-package com.baeldung.deeplearning4j.cnn.domain.network;
+package com.baeldung.deeplearning4j.cnn;
 
 import lombok.Value;
 import org.deeplearning4j.nn.conf.Updater;
 
 @Value
-public class CnnModelProperties {
+class CnnModelProperties {
     private final int epochsNum = 512;
 
     private final double learningRate = 0.001;
