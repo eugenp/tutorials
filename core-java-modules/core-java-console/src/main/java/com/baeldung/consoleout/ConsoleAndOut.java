e9e98ecc8e41fe02a68771448464ca982c81659e
Tarun Jain <tarunalways@gmail.com>
2020-07-26 02:21:02 +0530

    Created seaprate method for each functionality

--- core-java-modules/core-java-console/src/main/java/com/baeldung/consoleout/ConsoleAndOut.java
+++ core-java-modules/core-java-console/src/main/java/com/baeldung/consoleout/ConsoleAndOut.java
@@ -4,12 +4,28 @@ import java.io.Console;
 
 public class ConsoleAndOut {
     public static void main(String[] args) {
+        try {
+            printConsoleObject();
+            readPasswordFromConsole();
+        } catch (Exception ex) {
+            // Eating NullPointerExcpetion which will occur when this 
+            // program will be run from mediums other than console
+        }
+        printSysOut();    
+    }
+
+    static void printConsoleObject() {
         Console console = System.console();
         console.writer().print(console);
-        
-        char[] password = console.readPassword("Enter password:");
+    }
+
+    static void readPasswordFromConsole() {
+        Console console = System.console();
+        char[] password = console.readPassword("Enter password: ");
         console.printf(String.valueOf(password));
-        
+    }
+    
+    static void printSysOut() {
         System.out.println(System.out);
     }
 }
