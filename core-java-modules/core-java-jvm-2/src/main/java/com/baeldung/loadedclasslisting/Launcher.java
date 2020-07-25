40f6fef100b75690f54d8aaa3525b8a6707d70a6
root <anmoldeep0123@gmail.com>
2020-07-25 18:28:28 +0000

    Review - II Kevin Gilmore Changes

--- core-java-modules/core-java-jvm-2/src/main/java/com/baeldung/loadedclasslisting/Launcher.java
+++ core-java-modules/core-java-jvm-2/src/main/java/com/baeldung/loadedclasslisting/Launcher.java
@@ -1,48 +1,19 @@
 package com.baeldung.loadedclasslisting;
 
-import java.lang.reflect.Method;
 import java.util.Arrays;
-
-import com.baeldung.loadedclasslisting.ListLoadedClassesAgent.ClassLoaderType;
-import com.baeldung.loadedclasslisting.customLoader.ClassLoaderInfo;
-import com.baeldung.loadedclasslisting.customLoader.CustomClassLoader;
 
 public class Launcher {
 
-    private static ClassLoader customClassLoader;
-
     public static void main(String[] args) {
-        printClassesLoadedBy(ClassLoaderType.BOOTSTRAP);
-        printClassesLoadedBy(ClassLoaderType.SYSTEM);
-        printClassesLoadedBy(ClassLoaderType.EXTENSION);
-        printClassesLoadedBy(ClassLoaderType.CUSTOM);
+        printClassesLoadedBy("BOOTSTRAP");
+        printClassesLoadedBy("SYSTEM");
+        printClassesLoadedBy("EXTENSION");
     }
 
-    private static void printClassesLoadedBy(ClassLoaderType classLoaderType) {
-        Class<?>[] classes;
-        if (classLoaderType.equals(ClassLoaderType.CUSTOM)) {
-            customClassLoader = customClassLoading();
-            classes = ListLoadedClassesAgent.listLoadedClasses(customClassLoader);
-        } else {
-            classes = ListLoadedClassesAgent.listLoadedClasses(classLoaderType);
-        }
+    private static void printClassesLoadedBy(String classLoaderType) {
+        Class<?>[] classes = ListLoadedClassesAgent.listLoadedClasses(classLoaderType);
         Arrays.asList(classes)
             .forEach(clazz -> System.out.println(
               classLoaderType + " ClassLoader : " + clazz.getCanonicalName()));
-    }
-    
-    private static CustomClassLoader customClassLoading() {
-        CustomClassLoader customClassLoader = new CustomClassLoader();
-        Class<?> c;
-        try {
-            c = customClassLoader.findClass(ClassLoaderInfo.class.getName());
-            Object ob = c.getDeclaredConstructor()
-                .newInstance();
-            Method md = c.getMethod("printClassLoaders");
-            md.invoke(ob);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return customClassLoader;
     }
 }
