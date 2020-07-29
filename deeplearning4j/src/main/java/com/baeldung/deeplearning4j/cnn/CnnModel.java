768658cfc8ab0ad9a9df6fca9574f72ac1905576
helga_sh <v.shyrayeva@sam-solutions.com>
2020-07-29 11:36:09 +0300

    Fixed builder formatting

--- deeplearning4j/src/main/java/com/baeldung/deeplearning4j/cnn/CnnModel.java
+++ deeplearning4j/src/main/java/com/baeldung/deeplearning4j/cnn/CnnModel.java
@@ -30,23 +30,23 @@ class CnnModel {
         this.properties = properties;
 
         MultiLayerConfiguration configuration = new NeuralNetConfiguration.Builder()
-                .seed(1611)
-                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
-                .learningRate(properties.getLearningRate())
-                .regularization(true)
-                .updater(properties.getOptimizer())
-                .list()
-                .layer(0, conv5x5())
-                .layer(1, pooling2x2Stride2())
-                .layer(2, conv3x3Stride1Padding2())
-                .layer(3, pooling2x2Stride1())
-                .layer(4, conv3x3Stride1Padding1())
-                .layer(5, pooling2x2Stride1())
-                .layer(6, dense())
-                .pretrain(false)
-                .backprop(true)
-                .setInputType(dataSetService.inputType())
-                .build();
+          .seed(1611)
+          .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+          .learningRate(properties.getLearningRate())
+          .regularization(true)
+          .updater(properties.getOptimizer())
+          .list()
+          .layer(0, conv5x5())
+          .layer(1, pooling2x2Stride2())
+          .layer(2, conv3x3Stride1Padding2())
+          .layer(3, pooling2x2Stride1())
+          .layer(4, conv3x3Stride1Padding1())
+          .layer(5, pooling2x2Stride1())
+          .layer(6, dense())
+          .pretrain(false)
+          .backprop(true)
+          .setInputType(dataSetService.inputType())
+          .build();
 
         network = new MultiLayerNetwork(configuration);
     }
