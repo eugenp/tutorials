ae479954f48ae784e1c7e66a19b03e60cd0b31b7
joe zhang <joe.zhang@ringcentral.com>
2020-07-31 00:33:26 +0800

    refactor unit test name

--- core-java-modules/core-java-lang-oop-types/src/test/java/com/baeldung/primitivetype/PrimitiveTypeUtilUnitTest.java
+++ core-java-modules/core-java-lang-oop-types/src/test/java/com/baeldung/primitivetype/PrimitiveTypeUtilUnitTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 
-public class PrimitiveTypeUtilTest {
+public class PrimitiveTypeUtilUnitTest {
 
     private PrimitiveTypeUtil primitiveTypeUtil;
     private boolean booleanVal = false;
