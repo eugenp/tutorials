4957accaa05aba0a5bfefc9d4c9acf0563e8e1b9
azhwani <>
2020-08-03 12:57:37 +0100

    quick fix

--- spring-5-security/src/test/java/com/baeldung/logoutredirects/LogoutApplicationUnitTest.java
+++ spring-5-security/src/test/java/com/baeldung/logoutredirects/LogoutApplicationUnitTest.java
@@ -1,15 +1,12 @@
-package com.baeldung.authresolver.logoutredirects;
+package com.baeldung.logoutredirects;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-
-import com.baeldung.logoutredirects.securityconfig.SpringSecurityConfig;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
@@ -18,7 +15,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest()
-@ContextConfiguration(classes = { SpringSecurityConfig.class })
 public class LogoutApplicationUnitTest {
 
     @Autowired
