3e2ce28afbdeb558d8007a308ea3e6960430800c
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2020-08-16 01:15:24 +0530

    Incorporated the review comments on the pull request.

--- spring-webflux-threads/README.md
+++ spring-webflux-threads/README.md
@@ -1,7 +1,7 @@
 ## Spring WebFlux Concurrency
 
 This module contains articles about concurrency model in Spring WebFlux.
-Please note that this assumes Mongo and Kafka to be running on the local machine on default configurations.
+
 
 ### Relevant Articles: 
 
--- spring-webflux-threads/src/main/java/com/baeldung/webflux/Application.java
+++ spring-webflux-threads/src/main/java/com/baeldung/webflux/Application.java
@@ -3,6 +3,9 @@ package com.baeldung.webflux;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+* Please note that this assumes Mongo and Kafka to be running on the local machine on default configurations.
+*/
 @SpringBootApplication
 public class Application {
 
