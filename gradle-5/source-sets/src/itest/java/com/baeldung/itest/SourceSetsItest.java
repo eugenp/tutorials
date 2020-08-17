0df340f847429bac5e557da74078ac72a999179d
Sorin Zamfir <sorin.zamfir@outlook.com>
2020-08-17 23:55:43 +0300

    BAEL-4516: Refine examples

--- gradle-5/source-sets/src/itest/java/com/baeldung/itest/SourceSetsItest.java
+++ gradle-5/source-sets/src/itest/java/com/baeldung/itest/SourceSetsItest.java
@@ -3,17 +3,27 @@ package com.baeldung.itest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.util.List;
+//import java.util.List;
 
 import org.junit.Test;
 
-import com.google.common.collect.ImmutableList;
+import com.baeldung.main.SourceSetsObject;
+//import com.google.common.collect.ImmutableList;
 
 public class SourceSetsItest {
-
+    
     @Test
-    public void whenRunThenFail() {
-        List<String> someStrings = ImmutableList.of("Baeldung", "is", "cool");
-        assertThat(false, is(true));
+    public void whenRunThenSuccess() {
+        
+        SourceSetsObject underTest = new SourceSetsObject("lorem","ipsum");
+        
+        assertThat(underTest.getUser(), is("lorem"));
+        assertThat(underTest.getPassword(), is("ipsum"));
     }
+    
+//    @Test
+//    public void whenRunThenFail() {
+//        List<String> someStrings = ImmutableList.of("Baeldung", "is", "cool");
+//        assertThat(false, is(true));
+//    }
 }
