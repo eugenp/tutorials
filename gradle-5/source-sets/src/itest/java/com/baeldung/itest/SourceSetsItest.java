949502e831c9bf229f9d5ddf215aff8f4d298e02
Sorin Zamfir <zamfir.sorin@yahoo.com>
2020-08-18 21:52:30 +0300

    BAEL-4516: Cleanup sources

--- gradle-5/source-sets/src/itest/java/com/baeldung/itest/SourceSetsItest.java
+++ gradle-5/source-sets/src/itest/java/com/baeldung/itest/SourceSetsItest.java
@@ -3,27 +3,29 @@ package com.baeldung.itest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-//import java.util.List;
+import java.util.List;
 
 import org.junit.Test;
 
 import com.baeldung.main.SourceSetsObject;
-//import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableList;
 
 public class SourceSetsItest {
-    
+
     @Test
     public void whenRunThenSuccess() {
         
-        SourceSetsObject underTest = new SourceSetsObject("lorem","ipsum");
+        SourceSetsObject underTest = new SourceSetsObject("lorem", "ipsum");
         
         assertThat(underTest.getUser(), is("lorem"));
         assertThat(underTest.getPassword(), is("ipsum"));
     }
-    
-//    @Test
-//    public void whenRunThenFail() {
-//        List<String> someStrings = ImmutableList.of("Baeldung", "is", "cool");
-//        assertThat(false, is(true));
-//    }
+
+    @Test
+    public void givenImmutableListwhenRunThenSuccess() {
+        
+        List<String> someStrings = ImmutableList.of("Baeldung", "is", "cool");
+        
+        assertThat(someStrings.size(), is(3));
+    }
 }
