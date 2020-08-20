0b0e26c8d74508828d0271fb0c831aaac85c464e
bfontana <bruno.fontana@gmail.com>
2020-08-19 21:10:47 -0300

    Update ArrayToListConversion.java

--- java-collections-conversions-2/src/test/java/com/baeldung/arrayconversion/ArrayToListConversion.java
+++ java-collections-conversions-2/src/test/java/com/baeldung/arrayconversion/ArrayToListConversion.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class ArrayToListConversion {
+public class ArrayToListConversionUnitTest {
 
     @Test(expected = UnsupportedOperationException.class)
     public void givenAnArray_whenConvertingToList_returnUnmodifiableListUnitTest() {
