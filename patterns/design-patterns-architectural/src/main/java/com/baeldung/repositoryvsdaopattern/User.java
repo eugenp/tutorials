d549503224d420734308636eed901e7a3c006113
Anshul BANSAL <smartdiscover17@gmail.com>
2020-08-22 21:07:24 +0300

    BAEL-45087 - DAO vs Repository Pattern

---
+++ patterns/design-patterns-architectural/src/main/java/com/baeldung/repositoryvsdaopattern/User.java
@@ -0,0 +1,63 @@
+package com.baeldung.repositoryvsdaopattern;
+
+import java.util.List;
+
+public class User {
+    
+    private Long id;
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+    
+    private List<Tweet> tweets;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public void setTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+    
+}
