e1baa7c936fc003b2c8e43e4a8fc2be98ba31937
Juan Moreno <earth001@gmail.com>
2020-08-23 19:27:22 -0300

    UPDATE Simplify CyclicBarrier example

--- core-java-modules/core-java-concurrency-basic-2/src/main/java/com/baeldung/concurrent/competition/CyclicBarrierRace.java
+++ core-java-modules/core-java-concurrency-basic-2/src/main/java/com/baeldung/concurrent/competition/CyclicBarrierRace.java
@@ -7,24 +7,20 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-class CyclicBarrierRace implements Runnable {
+class CyclicBarrierRace {
     private static final Logger log = LoggerFactory.getLogger(CyclicBarrierRace.class.getName());
 
     public static void main(String[] args) {
         int maxPoolSize = 13000;
         ExecutorService executor = Executors.newFixedThreadPool(maxPoolSize);
-        CyclicBarrier barrier = new CyclicBarrier(maxPoolSize, new CyclicBarrierRace());
+        CyclicBarrier barrier = new CyclicBarrier(maxPoolSize);
         log.info("Waiting for runners");
         // The CycleBarrierRace 13000 runners; so wait for 13000 runners
         // (i.e., 13000 threads) to join to start the race
         for (int i = 1; i <= maxPoolSize; i++) {
             executor.execute(new CyclicBarrierRunner(i, barrier));
         }
-        executor.shutdown();
-    }
-
-    @Override
-    public void run() {
         log.info("Starting the race");
+        executor.shutdown();
     }
 }
