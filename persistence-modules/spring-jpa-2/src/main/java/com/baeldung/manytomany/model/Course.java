482357866eb215d3e1becabd7d06961e0d1a24f1
kwoyke <krzysztofwoyke@gmail.com>
2020-08-28 08:01:12 +0200

    BAEL-4577: Fix @MapsIs usage and add integration test (#9930)

--- persistence-modules/spring-jpa-2/src/main/java/com/baeldung/manytomany/model/Course.java
+++ persistence-modules/spring-jpa-2/src/main/java/com/baeldung/manytomany/model/Course.java
@@ -1,13 +1,8 @@
 package com.baeldung.manytomany.model;
 
+import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 
 @Entity
 @Table(name = "course")
@@ -18,29 +13,53 @@ public class Course {
     private Long id;
 
     @ManyToMany(mappedBy = "likedCourses")
-    private Set<Student> likes;
+    private Set<Student> likes = new HashSet<>();
 
     @OneToMany(mappedBy = "course")
-    private Set<CourseRating> ratings;
+    private Set<CourseRating> ratings = new HashSet<>();
 
     @OneToMany(mappedBy = "course")
-    private Set<CourseRegistration> registrations;
+    private Set<CourseRegistration> registrations = new HashSet<>();
 
     // additional properties
 
     public Course() {
     }
 
+    public Course(Long id) {
+        this.id = id;
+    }
+
     public Long getId() {
         return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Student> getLikes() {
+        return likes;
+    }
+
+    public void setLikes(Set<Student> likes) {
+        this.likes = likes;
     }
 
     public Set<CourseRating> getRatings() {
         return ratings;
     }
 
+    public void setRatings(Set<CourseRating> ratings) {
+        this.ratings = ratings;
+    }
+
     public Set<CourseRegistration> getRegistrations() {
         return registrations;
+    }
+
+    public void setRegistrations(Set<CourseRegistration> registrations) {
+        this.registrations = registrations;
     }
 
     @Override
