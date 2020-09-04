23d2db026ce51d4b6469fc38a76c6f2a46c9323e
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2020-09-04 09:33:39 +0200

    JAVA-2563: Use MongoClients factory instead of MongoClient directly

--- persistence-modules/spring-boot-persistence-mongodb/src/test/java/com/baeldung/mongodb/ManualEmbeddedMongoDbIntegrationTest.java
+++ persistence-modules/spring-boot-persistence-mongodb/src/test/java/com/baeldung/mongodb/ManualEmbeddedMongoDbIntegrationTest.java
@@ -1,18 +1,8 @@
 package com.baeldung.mongodb;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.util.SocketUtils;
-
 import com.mongodb.BasicDBObjectBuilder;
 import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-
+import com.mongodb.client.MongoClients;
 import de.flapdoodle.embed.mongo.MongodExecutable;
 import de.flapdoodle.embed.mongo.MongodStarter;
 import de.flapdoodle.embed.mongo.config.IMongodConfig;
@@ -20,6 +10,14 @@ import de.flapdoodle.embed.mongo.config.MongodConfigBuilder;
 import de.flapdoodle.embed.mongo.config.Net;
 import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.process.runtime.Network;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.util.SocketUtils;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ManualEmbeddedMongoDbIntegrationTest {
     private MongodExecutable mongodExecutable;
@@ -34,6 +32,7 @@ class ManualEmbeddedMongoDbIntegrationTest {
     void setup() throws Exception {
         String ip = "localhost";
         int randomPort = SocketUtils.findAvailableTcpPort();
+        String connectionString = ip + ":" + randomPort;
 
         IMongodConfig mongodConfig = new MongodConfigBuilder().version(Version.Main.PRODUCTION)
             .net(new Net(ip, randomPort, Network.localhostIsIPv6()))
@@ -42,7 +41,7 @@ class ManualEmbeddedMongoDbIntegrationTest {
         MongodStarter starter = MongodStarter.getDefaultInstance();
         mongodExecutable = starter.prepare(mongodConfig);
         mongodExecutable.start();
-        mongoTemplate = new MongoTemplate(new MongoClient(ip, randomPort), "test");
+        mongoTemplate = new MongoTemplate(MongoClients.create(connectionString), "test");
     }
 
     @DisplayName("Given object When save object using MongoDB template Then object can be found")
