c39f456de127173ae8f63e63965c755da6ea0eb6
Rutuja Joshi <67615932+rutujavjoshi@users.noreply.github.com>
2020-09-04 14:50:50 +0530

    BAEL-4201: Initial Commit

---
+++ spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/springvalidation/SpringvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.springvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringvalidationApplication.class, args);
+	}
+
+}
