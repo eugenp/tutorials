fb4041073bc3bd9e154f2def47b4bbcf70b9b2bd
Karsten Silz <karsten.silz@outlook.com>
2020-09-05 18:48:35 +0100

    BAEL-3326, "Optimizing JSON Schema for production use": Optimized imports and formatted source.

--- json/src/main/java/com/baeldung/jsonoptimization/CustomerSerializer.java
+++ json/src/main/java/com/baeldung/jsonoptimization/CustomerSerializer.java
@@ -6,21 +6,21 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.SerializerProvider;
 import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
-public class CustomerSerializer  extends StdSerializer<Customer> {
+public class CustomerSerializer extends StdSerializer<Customer> {
     private static final long serialVersionUID = 1L;
 
     public CustomerSerializer() {
         this(null);
     }
-    
+
     public CustomerSerializer(Class<Customer> t) {
         super(t);
     }
-    
+
     @Override
     public void serialize(Customer customer, JsonGenerator jsonGenerator, SerializerProvider serializer) throws IOException {
         jsonGenerator.writeStartArray();
-        jsonGenerator.writeNumber(customer.getId());        
+        jsonGenerator.writeNumber(customer.getId());
         jsonGenerator.writeString(customer.getFirstName());
         jsonGenerator.writeString(customer.getLastName());
         jsonGenerator.writeString(customer.getStreet());
