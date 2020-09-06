3ae5fc6035f7d1b9354b3ce5eb777df203ad1cc7
karan <karanpreet.singh@paytm.com>
2020-09-06 02:44:25 -0400

    fix verbose code

--- core-java-modules/core-java-lang-3/src/test/java/com/baeldung/comparators/PersonComparatorUnitTest.java
+++ core-java-modules/core-java-lang-3/src/test/java/com/baeldung/comparators/PersonComparatorUnitTest.java
@@ -1,7 +1,9 @@
 package com.baeldung.comparators;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -11,80 +13,40 @@ import com.baeldung.compareTo.comparators.PersonNameComparator;
 import com.baeldung.compareTo.model.Person;
 
 public class PersonComparatorUnitTest {
-	public static void sortIntegerArrayList() {
+	static List<Integer> intList = Arrays.asList(21, 12, 23, 31);
 
-		ArrayList<Integer> list = new ArrayList<>();
-		list.add(21);
-		list.add(12);
-		list.add(23);
-		list.add(31);
+	static List<Integer> sortedIntList = Arrays.asList(12, 21, 23, 31);
 
-		ArrayList<Integer> sortedList = new ArrayList<>();
-		sortedList.add(12);
-		sortedList.add(21);
-		sortedList.add(23);
-		sortedList.add(31);
+	static List<Person> personList = Arrays.asList(new Person(1, "John"), new Person(3, "Andy"),
+			new Person(2, "Kallis"), new Person(5, "Karen"), new Person(8, "Josh"));
 
-		Collections.sort(list);
-		Assert.assertArrayEquals(list.toArray(), sortedList.toArray());
+	static List<Person> idSortedPersonList = Arrays.asList(new Person(1, "John"), new Person(2, "Kallis"),
+			new Person(3, "Andy"), new Person(5, "Karen"), new Person(8, "Josh"));
+
+	static List<Person> nameSortedPersonList = Arrays.asList(new Person(3, "Andy"), new Person(1, "John"),
+			new Person(8, "Josh"), new Person(2, "Kallis"), new Person(5, "Karen"));
+
+	@Test
+	public void sortIntegerArrayList() {
+		Collections.sort(intList);
+		Assert.assertArrayEquals(intList.toArray(), sortedIntList.toArray());
 	}
 
 	@Test
-	public  void sortPersonWithComparable() {
-		ArrayList<Person> list = new ArrayList<Person>();
-		list.add(new Person(1, "John"));
-		list.add(new Person(3, "Andy"));
-		list.add(new Person(2, "Kallis"));
-		list.add(new Person(5, "Karen"));
-		list.add(new Person(8, "Josh"));
-
-		ArrayList<Person> idSortedList = new ArrayList<Person>();
-		idSortedList.add(new Person(1, "John"));
-		idSortedList.add(new Person(2, "Kallis"));
-		idSortedList.add(new Person(3, "Andy"));
-		idSortedList.add(new Person(5, "Karen"));
-		idSortedList.add(new Person(8, "Josh"));
-
-		Collections.sort(list);
-		Assert.assertArrayEquals(list.toArray(), idSortedList.toArray());
-
+	public void sortPersonWithComparable() {
+		Collections.sort(personList);
+		Assert.assertArrayEquals(personList.toArray(), idSortedPersonList.toArray());
 	}
 
 	@Test
-	public  void sortPersonWithComparator() {
-		ArrayList<Person> list = new ArrayList<Person>();
-		list.add(new Person(1, "John"));
-		list.add(new Person(3, "Andy"));
-		list.add(new Person(2, "Kallis"));
-		list.add(new Person(5, "Karen"));
-		list.add(new Person(8, "Josh"));
+	public void sortPersonWithComparator() {
 
-		ArrayList<Person> idSortedList = new ArrayList<Person>();
-		idSortedList.add(new Person(1, "John"));
-		idSortedList.add(new Person(2, "Kallis"));
-		idSortedList.add(new Person(3, "Andy"));
-		idSortedList.add(new Person(5, "Karen"));
-		idSortedList.add(new Person(8, "Josh"));
+		Collections.sort(personList, new PersonIdComparator());
+		Assert.assertArrayEquals(personList.toArray(), idSortedPersonList.toArray());
 
-		Collections.sort(list, new PersonIdComparator());
-		Assert.assertArrayEquals(list.toArray(), idSortedList.toArray());
-
-		ArrayList<Person> nameSortedList = new ArrayList<Person>();
-		nameSortedList.add(new Person(3, "Andy"));
-		nameSortedList.add(new Person(1, "John"));
-		nameSortedList.add(new Person(8, "Josh"));
-		nameSortedList.add(new Person(2, "Kallis"));
-		nameSortedList.add(new Person(5, "Karen"));
-
-		Collections.sort(list, new PersonNameComparator());
-		Assert.assertArrayEquals(list.toArray(), nameSortedList.toArray());
+		Collections.sort(personList, new PersonNameComparator());
+		Assert.assertArrayEquals(personList.toArray(), nameSortedPersonList.toArray());
 
 	}
-
-//	public  void main(String args[]) {
-//		sortIntegerArrayList();
-//		sortPersonWithComparable();
-//		sortPersonWithComparator();
-//	}
 
 }
