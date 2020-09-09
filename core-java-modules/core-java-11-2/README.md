b94df2cd33e1b0dd5aa47cd39b85ce1a1ce2ce98
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2020-09-09 12:55:16 +0200

    BAEL-4588: Move Guide To Java 8 Optional to core-java-11-2

--- core-java-modules/core-java-11-2/README.md
+++ core-java-modules/core-java-11-2/README.md
@@ -3,4 +3,5 @@
 This module contains articles about Java 11 core features
 
 ### Relevant articles
+- [Guide to Java 8 Optional](https://www.baeldung.com/java-optional)
 
--- core-java-modules/core-java-11-2/src/test/java/com/baeldung/optional/OptionalUnitTest.java
+++ core-java-modules/core-java-11-2/src/test/java/com/baeldung/optional/OptionalUnitTest.java
@@ -9,7 +9,9 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class OptionalUnitTest {
 
