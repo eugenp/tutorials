ca7b449fdaef8e07933bb8f592f42ac4e888a555
Rutuja Joshi <67615932+rutujavjoshi@users.noreply.github.com>
2020-09-12 20:40:20 +0530

    BAEL-4201 - review related updates

--- spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/springvalidation/ServletInitializer.java
+++ spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/springvalidation/ServletInitializer.java
@@ -5,9 +5,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringvalidationApplication.class);
-	}
-
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringValidationApplication.class);
+    }
 }
