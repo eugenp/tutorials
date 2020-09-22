0ef8e39af174f5c6acce09a0ed85578814c60367
Bruno Fontana <brunof.baeldung@gmail.com>
2020-09-22 20:54:25 -0300

    Fixing formatting issues.

--- testing-modules/junit-4/src/test/java/com/baeldung/assume/ConditionallyIgnoreTestsUnitTest.java
+++ testing-modules/junit-4/src/test/java/com/baeldung/assume/ConditionallyIgnoreTestsUnitTest.java
@@ -13,47 +13,51 @@ import org.junit.Test;
 
 public class ConditionallyIgnoreTestsUnitTest {
 
-  @Test public void whenAssumeThatAndOSIsLinux_thenRunTest() {
-    assumeThat(getOsName(), is("Linux"));
-    assertEquals("run", "RUN".toLowerCase());
-  }
-
-  @Test public void whenAssumeTrueAndOSIsLinux_thenRunTest() {
-    final int codeVersion = 1;
-    assumeTrue(isExpectedOS(getOsName()));
-    assertEquals("run", "RUN".toLowerCase());
-  }
-
-  @Test public void whenAssumeFalseAndOSIsLinux_thenIgnore() {
-    assumeFalse(isExpectedOS(getOsName()));
-    assertEquals("run", "RUN".toLowerCase());
-  }
-
-  @Test public void whenAssumeNotNullAndNotNullOSVersion_thenIgnore() {
-    assumeNotNull(getOsName());
-    assertEquals("run", "RUN".toLowerCase());
-  }
-
-  /**
-   * Let's use a different example here.
-   */
-  @Test public void whenAssumeNoExceptionAndExceptionThrown_thenIgnore() {
-    assertEquals("everything ok", "EVERYTHING OK".toLowerCase());
-    String t = null;
-    try {
-      t.charAt(0);
-    } catch (NullPointerException npe) {
-      assumeNoException(npe);
+    @Test
+    public void whenAssumeThatAndOSIsLinux_thenRunTest() {
+        assumeThat(getOsName(), is("Linux"));
+        assertEquals("run", "RUN".toLowerCase());
     }
-    assertEquals("run", "RUN".toLowerCase());
-  }
 
-  private boolean isExpectedOS(final String osName) {
-    return "Linux".equals(osName);
-  }
+    @Test
+    public void whenAssumeTrueAndOSIsLinux_thenRunTest() {
+        assumeTrue(isExpectedOS(getOsName()));
+        assertEquals("run", "RUN".toLowerCase());
+    }
 
-  // This should use System.getProperty("os.name") in a real test.
-  private String getOsName() {
-    return "Linux";
-  }
+    @Test
+    public void whenAssumeFalseAndOSIsLinux_thenIgnore() {
+        assumeFalse(isExpectedOS(getOsName()));
+        assertEquals("run", "RUN".toLowerCase());
+    }
+
+    @Test
+    public void whenAssumeNotNullAndNotNullOSVersion_thenRun() {
+        assumeNotNull(getOsName());
+        assertEquals("run", "RUN".toLowerCase());
+    }
+
+    /**
+     * Let's use a different example here.
+     */
+    @Test
+    public void whenAssumeNoExceptionAndExceptionThrown_thenIgnore() {
+        assertEquals("everything ok", "EVERYTHING OK".toLowerCase());
+        String t = null;
+        try {
+          t.charAt(0);
+        } catch (NullPointerException npe) {
+          assumeNoException(npe);
+        }
+        assertEquals("run", "RUN".toLowerCase());
+    }
+
+    private boolean isExpectedOS(final String osName) {
+        return "Linux".equals(osName);
+    }
+
+    // This should use System.getProperty("os.name") in a real test.
+    private String getOsName() {
+        return "Linux";
+    }
 }
