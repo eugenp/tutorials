3ec546e25e13765df01d859c52d6564a7be8073b
Trixi Turny <writetotrixi@gmail.com>
2020-09-24 07:25:40 +0100

    BAEL-4457 fix UserController

--- spring-resttemplate-2/src/main/java/com/baeldung/resttemplate/json/provider/controller/UserController.java
+++ spring-resttemplate-2/src/main/java/com/baeldung/resttemplate/json/provider/controller/UserController.java
@@ -3,7 +3,11 @@ package com.baeldung.resttemplate.json.provider.controller;
 import com.baeldung.resttemplate.json.model.User;
 import com.baeldung.resttemplate.json.service.UserService;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -14,9 +18,10 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
+
     @RequestMapping(value = "/users", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody
-    List<User> getUsers() {
+    @ResponseBody
+    public List<User> getUsers() {
         List<User> userObjects = userService.getUsers();
         return userObjects;
     }
