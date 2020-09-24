7638d61ecaaf56950514ed218493864496d8b8eb
Trixi Turny <writetotrixi@gmail.com>
2020-09-24 07:30:02 +0100

    BAEL-4457 delete provider service and controller

--- spring-resttemplate-2/src/main/java/com/baeldung/resttemplate/json/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.resttemplate.json.service;
-
-import com.baeldung.resttemplate.json.model.User;
-
-import java.util.List;
-
-public interface UserService {
-    List<User> getUsers() ;
-}
