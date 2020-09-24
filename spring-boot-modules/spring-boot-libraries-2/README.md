b7c67a99f0bf218849c34fdc00035f41f8164dc8
Ronald Dehuysser <ronald.dehuysser@gmail.com>
2020-09-24 10:54:35 +0200

    [BAEL-4639] Running background jobs in Spring with JobRunr

---
+++ spring-boot-modules/spring-boot-libraries-2/README.md
@@ -0,0 +1,7 @@
+## Spring Boot Libraries
+
+This module contains articles about various Spring Boot libraries
+
+### Relevant Articles:
+
+- [Guide to JobRunr with Spring](https://www.baeldung.com/java-jobrunr-spring)
---
+++ spring-boot-modules/spring-boot-libraries-2/src/main/java/com/baeldung/jobrunr/service/SampleJobService.java
@@ -0,0 +1,34 @@
+package com.baeldung.jobrunr.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Service
+public class SampleJobService {
+
+    public static final long EXECUTION_TIME = 5000L;
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private AtomicInteger count = new AtomicInteger();
+
+    public void executeSampleJob(String variable) {
+
+        logger.info("The sample job has begun. The variable you passed is {}", variable);
+        try {
+            Thread.sleep(EXECUTION_TIME);
+        } catch (InterruptedException e) {
+            logger.error("Error while executing sample job", e);
+        } finally {
+            count.incrementAndGet();
+            logger.info("Sample job has finished...");
+        }
+    }
+
+    public int getNumberOfInvocations() {
+        return count.get();
+    }
+}
