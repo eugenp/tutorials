515ceb3c186ef32e9b29ccd1b137a8417aa61b8d
Krzysztof Majewski <krzysztof.majewski.km1@contractors.roche.com>
2020-09-28 14:15:54 +0200

    BAEL-3997 Verify Working with Date Parameters in Spring app level config

--- spring-mvc-java-2/src/main/java/com/baeldung/datetime/DateTimeConfig.java
+++ spring-mvc-java-2/src/main/java/com/baeldung/datetime/DateTimeConfig.java
@@ -2,18 +2,22 @@ package com.baeldung.datetime;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
 import org.springframework.format.number.NumberFormatAnnotationFormatterFactory;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.time.format.DateTimeFormatter;
 
 @Configuration
-class DateTimeConfig {
+public class DateTimeConfig extends WebMvcConfigurationSupport {
 
     @Bean
-    public FormattingConversionService conversionService() {
+    @Override
+    public FormattingConversionService mvcConversionService() {
         DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService(false);
 
         conversionService.addFormatterForFieldAnnotation(new NumberFormatAnnotationFormatterFactory());
@@ -22,6 +26,10 @@ class DateTimeConfig {
         registrar.setDateFormatter(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         registrar.setDateTimeFormatter(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
         registrar.registerFormatters(conversionService);
+
+        DateFormatterRegistrar registrar2 = new DateFormatterRegistrar();
+        registrar2.setFormatter(new DateFormatter("dd.MM.yyyy"));
+        registrar2.registerFormatters(conversionService);
 
         return conversionService;
     }
