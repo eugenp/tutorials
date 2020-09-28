1facf4a578591b64cc41dda64ba3954cebb131ab
sampadawagde <sampada.wagde@gmail.com>
2020-09-28 23:10:40 +0530

    JAVA-2432: Moved articles from here to other modules

--- persistence-modules/spring-hibernate4/src/main/java/com/baeldung/persistence/service/IBarService.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.persistence.service;
-
-import com.baeldung.persistence.dao.common.IOperations;
-import com.baeldung.persistence.model.Bar;
-
-public interface IBarService extends IOperations<Bar> {
-    //
-}
