02b171942b84870b3589a933c2e7212fde1f258e
Maja Joksovic <joksovic.maja@gmail.com>
2020-10-01 21:03:31 +0200

    fixed formatting

--- persistence-modules/spring-boot-persistence-h2/src/test/java/com/baeldung/persistent/FilesLocationUnitTest.java
+++ persistence-modules/spring-boot-persistence-h2/src/test/java/com/baeldung/persistent/FilesLocationUnitTest.java
@@ -18,23 +18,23 @@ import com.baeldung.h2db.auto.configuration.AutoConfigurationDemo;
 @SpringBootTest(classes = AutoConfigurationDemo.class)
 public class FilesLocationUnitTest {
 
-	@Test(expected = Test.None.class)
+    @Test(expected = Test.None.class)
 	public void whenApplicationStarted_thenEmbeddedDbSubfolderCreated() {
-		File subdirectory = new File("./subdirectory");
-		assertTrue(subdirectory.exists());
-		assertTrue(subdirectory.isDirectory());
-	}
+        File subdirectory = new File("./subdirectory");
+        assertTrue(subdirectory.exists());
+        assertTrue(subdirectory.isDirectory());
+    }
 
-	@Test(expected = Test.None.class)
-	public void whenApplicationStarted_thenEmbeddedDbFilesCreated() {
-		File dbFile = new File("./subdirectory/demodb.mv.db");
-		assertTrue(dbFile.exists());
-		assertTrue(dbFile.isFile());
-	}
+    @Test(expected = Test.None.class)
+    public void whenApplicationStarted_thenEmbeddedDbFilesCreated() {
+        File dbFile = new File("./subdirectory/demodb.mv.db");
+        assertTrue(dbFile.exists());
+        assertTrue(dbFile.isFile());
+    }
 
-	@After
-	public void cleanUp() {
-		File file = new File("subdirectory");
-		file.deleteOnExit();
-	}
+    @After
+    public void cleanUp() {
+        File file = new File("subdirectory");
+        file.deleteOnExit();
+    }
 }
