5038b0acd2216066e2df768fad1568f58acf81d0
kwoyke <krzysztofwoyke@gmail.com>
2020-10-02 19:26:00 +0200

    BAEL-4658: Upgrade to Cucumber 6.8.0 (#10121)

--- spring-cucumber/src/main/java/com/baeldung/VersionController.java
+++ spring-cucumber/src/main/java/com/baeldung/VersionController.java
@@ -1,13 +1,12 @@
 package com.baeldung;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersionController {
 
-    @RequestMapping(method = { RequestMethod.GET }, value = { "/version" })
+    @GetMapping("/version")
     public String getVersion() {
         return "1.0";
     }
