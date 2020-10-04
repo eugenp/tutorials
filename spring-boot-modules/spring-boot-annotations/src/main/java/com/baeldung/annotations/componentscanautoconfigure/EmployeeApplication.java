14593ca02a9a316657a929c1cda6cf664d4beab8
Anirban Chatterjee <canirban99@gmail.com>
2020-10-04 12:55:22 +0200

    Refined bean methods

--- spring-boot-modules/spring-boot-annotations/src/main/java/com/baeldung/annotations/componentscanautoconfigure/EmployeeApplication.java
+++ spring-boot-modules/spring-boot-annotations/src/main/java/com/baeldung/annotations/componentscanautoconfigure/EmployeeApplication.java
@@ -8,18 +8,19 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan(basePackages = {"com.baeldung.annotations.componentscanautoconfigure.doctor", "com.baeldung.annotations.componentscanautoconfigure.employee"},
+//@Configuration
+@ComponentScan(basePackages = {"com.baeldung.annotations.componentscanautoconfigure.healthcare", "com.baeldung.annotations.componentscanautoconfigure.employee"},
         basePackageClasses = Teacher.class)
-@EnableAutoConfiguration(exclude={JdbcTemplateAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {JdbcTemplateAutoConfiguration.class})
 //@EnableAutoConfiguration(excludeName = {"org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration"})
 public class EmployeeApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(EmployeeApplication.class, args);
-        System.out.println("Configures Doctor: " + context.containsBeanDefinition("doctor"));
         System.out.println("Configures Employee: " + context.containsBeanDefinition("employee"));
         System.out.println("Configures Senior Employee: " + context.containsBeanDefinition("seniorEmployee"));
+        System.out.println("Configures Doctor: " + context.containsBeanDefinition("doctor"));
+        System.out.println("Configures Hospital: " + context.containsBeanDefinition("hospital"));
         System.out.println("Configures Student: " + context.containsBeanDefinition("student"));
         System.out.println("Configures Teacher: " + context.containsBeanDefinition("teacher"));
     }
