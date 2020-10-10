7890244d0a90043a3778c85273ac90f181f08d5d
Maiklins <kruemel@quantentunnel.de>
2020-10-10 09:17:35 +0200

    Java-82 Correctly configure Netty server and security (#10034)
    * Java-82 Correctly configure netty server and security
    
    * Java-82 Align WebSecurity with article
    
    * Java-82 Change port
    
    Co-authored-by: mikr <michael.krimgen@ximedes.com>

--- spring-5-reactive-security/src/main/java/com/baeldung/reactive/actuator/FeaturesEndpoint.java
+++ spring-5-reactive-security/src/main/java/com/baeldung/reactive/actuator/FeaturesEndpoint.java
@@ -7,7 +7,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
-@Endpoint(id = "features", enableByDefault = true)
+@Endpoint(id = "features")
 public class FeaturesEndpoint {
 
     private Map<String, Feature> features = new ConcurrentHashMap<>();
