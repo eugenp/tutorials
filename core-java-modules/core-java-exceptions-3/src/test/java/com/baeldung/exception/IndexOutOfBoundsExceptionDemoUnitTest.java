582bbdf32f24c6ca8b0990dc37250379cc910909
Vishal <vakkalkote@gmail.com>
2020-10-11 13:56:15 +0530

    rename test method.

--- core-java-modules/core-java-exceptions-3/src/test/java/com/baeldung/exception/IndexOutOfBoundsExceptionDemoUnitTest.java
+++ core-java-modules/core-java-exceptions-3/src/test/java/com/baeldung/exception/IndexOutOfBoundsExceptionDemoUnitTest.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import static com.baeldung.exception.IndexOutOfBoundsExceptionDemo.copyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -14,16 +13,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class IndexOutOfBoundsExceptionDemoUnitTest {
 
     @Test
-    void shouldReturnCopyOfSourceList() {
+    void givenDestinationArrayListSizeIsZero_whenCopySourceArrayListToDestination_thenShouldThrowIndexOutOfBoundsException() {
         List<Integer> source = Arrays.asList(1, 2, 3, 4, 5);
 
-        assertThrows(IndexOutOfBoundsException.class, () -> copyList(source));
+        assertThrows(IndexOutOfBoundsException.class, () -> IndexOutOfBoundsExceptionDemo.copyList(source));
     }
 
     @Test
-    void shouldCopyEmptyList() {
+    void givenSourceAndDestinationListSizeIsEqual_whenCopySourceArrayListToDestination_thenShouldNotThrowIndexOutOfBoundsException() {
         List<Integer> source = Collections.emptyList();
 
-        assertEquals(source, copyList(source));
+        assertEquals(source, IndexOutOfBoundsExceptionDemo.copyList(source));
     }
 }
