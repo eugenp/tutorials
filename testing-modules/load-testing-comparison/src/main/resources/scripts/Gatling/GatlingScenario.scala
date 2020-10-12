46b7a4f0ad7db9d8a6ff637ead439fc9973275a4
Stephane Landelle <slandelle@gatling.io>
2020-10-12 18:50:26 +0200

    Remove extra headers as the other tools don't set them and that's extra traffic

--- testing-modules/load-testing-comparison/src/main/resources/scripts/Gatling/GatlingScenario.scala
+++ testing-modules/load-testing-comparison/src/main/resources/scripts/Gatling/GatlingScenario.scala
@@ -10,11 +10,6 @@ class RewardsScenario extends Simulation {
   def randCustId() = java.util.concurrent.ThreadLocalRandom.current().nextInt(1, 10000)
   
   val httpProtocol = http.baseUrl("http://localhost:8080")
-					    .acceptHeader("text/html,application/json;q=0.9,*/*;q=0.8")
-						.doNotTrackHeader("1")
-						.acceptLanguageHeader("en-US,en;q=0.5")
-						.acceptEncodingHeader("gzip, deflate")
-						.userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
   
   val scn = scenario("RewardsScenario")
 	.repeat(100){
