0d3e1975179023c51cee38558686c9e788da37e4
Adrian Maghear <adrian.maghear@gmail.com>
2020-10-16 13:07:22 +0200

    [BAEL-3600] add integration test

--- netflix-modules/mantis/src/main/java/com/baeldung/netflix/mantis/model/LogAggregate.java
+++ netflix-modules/mantis/src/main/java/com/baeldung/netflix/mantis/model/LogAggregate.java
@@ -5,15 +5,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.mantisrx.runtime.codec.JsonType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LogAggregate implements JsonType {
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
-    private final Integer count;
-    private final String level;
+    private Integer count;
+    private String level;
 
     public String toJsonString() {
         try {
