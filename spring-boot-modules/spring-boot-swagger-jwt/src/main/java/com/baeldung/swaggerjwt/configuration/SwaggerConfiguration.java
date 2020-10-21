7588a1409766f6038aba706490ecc3193b369daf
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-10-21 11:15:16 +0100

    [BEAL-4173] Fixing identation issue

--- spring-boot-modules/spring-boot-swagger-jwt/src/main/java/com/baeldung/swaggerjwt/configuration/SwaggerConfiguration.java
+++ spring-boot-modules/spring-boot-swagger-jwt/src/main/java/com/baeldung/swaggerjwt/configuration/SwaggerConfiguration.java
@@ -16,47 +16,47 @@ import java.util.List;
 @Configuration
 public class SwaggerConfiguration {
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
+  public static final String AUTHORIZATION_HEADER = "Authorization";
 
-    private ApiInfo apiInfo() {
-        return new ApiInfo("My REST API",
-		  "Some custom description of API.",
-		  "1.0",
-		  "Terms of service",
-		  new Contact("Sallo Szrajbman", "www.baeldung.com", "salloszraj@gmail.com"),
-		  "License of API",
-		  "API license URL",
-		  Collections.emptyList());
-    }
+  private ApiInfo apiInfo() {
+    return new ApiInfo("My REST API",
+        "Some custom description of API.",
+        "1.0",
+        "Terms of service",
+        new Contact("Sallo Szrajbman", "www.baeldung.com", "salloszraj@gmail.com"),
+        "License of API",
+        "API license URL",
+        Collections.emptyList());
+  }
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-          .apiInfo(apiInfo())
-          .securityContexts(Arrays.asList(securityContext()))
-          .securitySchemes(Arrays.asList(apiKey()))
-          .select()
-          .apis(RequestHandlerSelectors.any())
-          .paths(PathSelectors.any())
-          .build();
-    }
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .apiInfo(apiInfo())
+        .securityContexts(Arrays.asList(securityContext()))
+        .securitySchemes(Arrays.asList(apiKey()))
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
 
-    private ApiKey apiKey() {
-        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
-    }
+  private ApiKey apiKey() {
+    return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
+  }
 
-    private SecurityContext securityContext() {
-        return SecurityContext.builder()
-          .securityReferences(defaultAuth())
-          .build();
-    }
+  private SecurityContext securityContext() {
+    return SecurityContext.builder()
+        .securityReferences(defaultAuth())
+        .build();
+  }
 
-    List<SecurityReference> defaultAuth() {
-        AuthorizationScope authorizationScope
-          = new AuthorizationScope("global", "accessEverything");
-        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
-        authorizationScopes[0] = authorizationScope;
-        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
-    }
+  List<SecurityReference> defaultAuth() {
+    AuthorizationScope authorizationScope
+        = new AuthorizationScope("global", "accessEverything");
+    AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+    authorizationScopes[0] = authorizationScope;
+    return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+  }
 
 }
