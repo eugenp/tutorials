7d745497095a53237bf4fb00775ac5d29c0561be
Vishal <vakkalkote@gmail.com>
2020-10-23 14:02:14 +0530

    Revert "Add srouce and test for Array Size vs ArrayList Capacity"
    This reverts commit 6933ffaf50697789e470d3dcb37b92e3ddc001a5.

--- core-java-modules/core-java-collections-array-list/src/main/java/com/baeldung/arraylistcapacityvsarraysize/ArrayCreator.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.arraylistcapacityvsarraysize;
-
-public class ArrayCreator {
-
-    static Integer[] createIntegerArrayOf(int size) {
-        return new Integer[size];
-    }
-}
