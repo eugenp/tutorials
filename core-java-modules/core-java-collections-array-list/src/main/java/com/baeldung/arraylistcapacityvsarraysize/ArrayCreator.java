6933ffaf50697789e470d3dcb37b92e3ddc001a5
Vishal <vakkalkote@gmail.com>
2020-10-23 13:37:24 +0530

    Add srouce and test for Array Size vs ArrayList Capacity

---
+++ core-java-modules/core-java-collections-array-list/src/main/java/com/baeldung/arraylistcapacityvsarraysize/ArrayCreator.java
@@ -0,0 +1,8 @@
+package com.baeldung.arraylistcapacityvsarraysize;
+
+public class ArrayCreator {
+
+    static Integer[] createIntegerArrayOf(int size) {
+        return new Integer[size];
+    }
+}
