dd98392dbe80e319aa61ca747a3d7f376d7c354b
mateusz.szablak <mateusz.szablak@accenture.com>
2020-10-26 16:35:41 +0100

    added after review

--- core-java-modules/core-java-string-operations-3/src/main/java/com/baeldung/tostring/StringCastUtils.java
+++ core-java-modules/core-java-string-operations-3/src/main/java/com/baeldung/tostring/StringCastUtils.java
@@ -2,9 +2,14 @@ package com.baeldung.tostring;
 
 public class StringCastUtils {
     public static String castToString(Object object) {
-        if (object instanceof String) {
+        if (object instanceof String)
             return (String) object;
-        }
+        return null;
+    }
+
+    public static String getToString(Object object) {
+        if (object != null)
+            return object.toString();
         return null;
     }
 }
