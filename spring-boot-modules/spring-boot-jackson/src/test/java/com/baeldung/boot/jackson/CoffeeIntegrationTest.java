9b8455e82992dff01cae00a1560f0d4ed750841c
Adina Rolea <adina.fometescu@elan-solutions.eu>
2020-10-27 15:47:40 +0200

    BAEL-4687: added spring boot jackson configuration example

---
+++ spring-boot-modules/spring-boot-jackson/src/test/java/com/baeldung/boot/jackson/CoffeeIntegrationTest.java
@@ -0,0 +1,32 @@
+package com.baeldung.boot.jackson;
+
+import com.baeldung.boot.jackson.config.CoffeeConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CoffeeIntegrationTest {
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    public void whenQueryCoffeeWithoutParam_thenNullIsNotInserted() {
+        String formattedDate = DateTimeFormatter.ofPattern(CoffeeConfiguration.dateTimeFormat)
+                .format(LocalDateTime.now());
+        String brand = "Lavazza";
+
+        String url = "/coffee?brand=" + brand;
+        String response = restTemplate.getForObject(url, String.class);
+
+        assertThat(response).isEqualTo(
+                "{\"date\":\"" + formattedDate + "\",\"body\":{\"brand\":\"" + brand + "\"}}");
+    }
+}
