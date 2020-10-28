06c800c526cf8f40906c02214cfffed687108c46
Emmanuel Yasa <emyasa.emmanuel.yasa@gmail.com>
2020-10-29 00:16:53 +0800

    BAEL-4643: JPA CascadeType.REMOVE vs orphanRemoval * Defines the Domain: ShipmentInfo, LineItem and OrderRequest entity * Adds CascadeTypeRemoveIntegrationTest * Adds OrphanRemovalIntegrationTest

---
+++ persistence-modules/java-jpa-3/src/main/java/com/baeldung/jpa/removal/OrderRequest.java
@@ -0,0 +1,38 @@
+package com.baeldung.jpa.removal;
+
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class OrderRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne(cascade = { CascadeType.REMOVE, CascadeType.PERSIST })
+    private ShipmentInfo shipmentInfo;
+
+    @OneToMany(orphanRemoval = true, cascade = CascadeType.PERSIST, mappedBy = "orderRequest")
+    private List<LineItem> lineItems;
+
+    public OrderRequest(ShipmentInfo shipmentInfo) {
+        this.shipmentInfo = shipmentInfo;
+    }
+
+    public OrderRequest(List<LineItem> lineItems) {
+        this.lineItems = lineItems;
+    }
+
+    public void removeLineItem(LineItem lineItem) {
+        lineItems.remove(lineItem);
+    }
+
+    protected OrderRequest() {}
+}
