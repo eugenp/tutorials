01baf5c7525383eff7deba29780de274af51d5d4
sharifi <h_sharifi@modernisc.com>
2020-10-31 17:41:37 +0330

    add aes main source

---
+++ core-java-modules/core-java-security-2/src/main/java/com/baeldung/aes/Student.java
@@ -0,0 +1,29 @@
+package com.baeldung.crypto;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    private String name;
+    private int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
