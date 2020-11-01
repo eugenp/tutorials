d4a8a336043cee35266c3f81fe4bf4214f5304cd
GilvanOrnelas <gilvanornelasff@gmail.com>
2020-11-01 11:46:15 -0300

    Storing files indexed by a database (#10174)
    Creating the FileLocationService to link the FileSystemRepository to the ImageDbRepository. Removing test order
    
    Changing to BDDMockito
    
    Changing Long wrapper for @id. Changing the test names to given-when-then pattern
    
    Co-authored-by: Gilvan Ornelas Fernandes Filho <gilvan.fernandes@bairesdev.com>

---
+++ persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/db/indexing/Image.java
@@ -0,0 +1,62 @@
+package com.baeldung.db.indexing;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+class Image {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+    String name;
+
+    String location;
+
+    @Lob
+    byte[] content;
+
+    public Image() {
+    }
+
+    public Image(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+
+    public byte[] getContent() {
+        return content;
+    }
+
+    public void setContent(byte[] content) {
+        this.content = content;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+}
