4db192497939682efbdcb322f2d42e026c569fef
Daniel Strmecki <daniel.strmecki@gmail.com>
2020-11-01 18:09:04 +0100

    BAEL-4662: Rename test classes

--- core-java-modules/core-java-15/src/test/java/com/baeldung/sealed/classes/VehicleUnitTest.java
+++ core-java-modules/core-java-15/src/test/java/com/baeldung/sealed/classes/VehicleUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.sealed.records;
+package com.baeldung.sealed.classes;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -6,51 +6,51 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.constant.ClassDesc;
 
-public class VehicleTest {
+public class VehicleUnitTest {
 
     private static Vehicle car;
     private static Vehicle truck;
 
     @BeforeAll
     public static void createInstances() {
-        car = new Car(4, "VZ500DA");
-        truck = new Truck(16000, "VZ600TA");
+        car = new Car(5, "VZ500DA");
+        truck = new Truck(19000, "VZ600TA");
     }
 
     @Test
-    public void givenCar_whenUsingReflectionAPI_thenInterfaceIsSealed() {
+    public void givenCar_whenUsingReflectionAPI_thenSuperClassIsSealed() {
         Assertions.assertThat(car.getClass().isSealed()).isEqualTo(false);
-        Assertions.assertThat(car.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
-        Assertions.assertThat(car.getClass().getInterfaces()[0].permittedSubclasses())
+        Assertions.assertThat(car.getClass().getSuperclass().isSealed()).isEqualTo(true);
+        Assertions.assertThat(car.getClass().getSuperclass().permittedSubclasses())
                 .contains(ClassDesc.of(car.getClass().getCanonicalName()));
     }
 
     @Test
-    public void givenTruck_whenUsingReflectionAPI_thenInterfaceIsSealed() {
+    public void givenTruck_whenUsingReflectionAPI_thenSuperClassIsSealed() {
         Assertions.assertThat(truck.getClass().isSealed()).isEqualTo(false);
-        Assertions.assertThat(truck.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
-        Assertions.assertThat(truck.getClass().getInterfaces()[0].permittedSubclasses())
+        Assertions.assertThat(truck.getClass().getSuperclass().isSealed()).isEqualTo(true);
+        Assertions.assertThat(truck.getClass().getSuperclass().permittedSubclasses())
                 .contains(ClassDesc.of(truck.getClass().getCanonicalName()));
     }
 
     @Test
     public void givenCar_whenGettingPropertyTraditionalWay_thenNumberOfSeatsPropertyIsReturned() {
-        Assertions.assertThat(getPropertyTraditionalWay(car)).isEqualTo(4);
+        Assertions.assertThat(getPropertyTraditionalWay(car)).isEqualTo(5);
     }
 
     @Test
     public void givenCar_whenGettingPropertyViaPatternMatching_thenNumberOfSeatsPropertyIsReturned() {
-        Assertions.assertThat(getPropertyViaPatternMatching(car)).isEqualTo(4);
+        Assertions.assertThat(getPropertyViaPatternMatching(car)).isEqualTo(5);
     }
 
     @Test
     public void givenTruck_whenGettingPropertyTraditionalWay_thenLoadCapacityIsReturned() {
-        Assertions.assertThat(getPropertyTraditionalWay(truck)).isEqualTo(16000);
+        Assertions.assertThat(getPropertyTraditionalWay(truck)).isEqualTo(19000);
     }
 
     @Test
     public void givenTruck_whenGettingPropertyViaPatternMatching_thenLoadCapacityIsReturned() {
-        Assertions.assertThat(getPropertyViaPatternMatching(truck)).isEqualTo(16000);
+        Assertions.assertThat(getPropertyViaPatternMatching(truck)).isEqualTo(19000);
     }
 
     private int getPropertyTraditionalWay(Vehicle vehicle) {
--- core-java-modules/core-java-15/src/test/java/com/baeldung/sealed/records/VehicleUnitTest.java
+++ core-java-modules/core-java-15/src/test/java/com/baeldung/sealed/records/VehicleUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.sealed.classes;
+package com.baeldung.sealed.records;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
@@ -6,51 +6,51 @@ import org.junit.jupiter.api.Test;
 
 import java.lang.constant.ClassDesc;
 
-public class VehicleTest {
+public class VehicleUnitTest {
 
     private static Vehicle car;
     private static Vehicle truck;
 
     @BeforeAll
     public static void createInstances() {
-        car = new Car(5, "VZ500DA");
-        truck = new Truck(19000, "VZ600TA");
+        car = new Car(4, "VZ500DA");
+        truck = new Truck(16000, "VZ600TA");
     }
 
     @Test
-    public void givenCar_whenUsingReflectionAPI_thenSuperClassIsSealed() {
+    public void givenCar_whenUsingReflectionAPI_thenInterfaceIsSealed() {
         Assertions.assertThat(car.getClass().isSealed()).isEqualTo(false);
-        Assertions.assertThat(car.getClass().getSuperclass().isSealed()).isEqualTo(true);
-        Assertions.assertThat(car.getClass().getSuperclass().permittedSubclasses())
+        Assertions.assertThat(car.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
+        Assertions.assertThat(car.getClass().getInterfaces()[0].permittedSubclasses())
                 .contains(ClassDesc.of(car.getClass().getCanonicalName()));
     }
 
     @Test
-    public void givenTruck_whenUsingReflectionAPI_thenSuperClassIsSealed() {
+    public void givenTruck_whenUsingReflectionAPI_thenInterfaceIsSealed() {
         Assertions.assertThat(truck.getClass().isSealed()).isEqualTo(false);
-        Assertions.assertThat(truck.getClass().getSuperclass().isSealed()).isEqualTo(true);
-        Assertions.assertThat(truck.getClass().getSuperclass().permittedSubclasses())
+        Assertions.assertThat(truck.getClass().getInterfaces()[0].isSealed()).isEqualTo(true);
+        Assertions.assertThat(truck.getClass().getInterfaces()[0].permittedSubclasses())
                 .contains(ClassDesc.of(truck.getClass().getCanonicalName()));
     }
 
     @Test
     public void givenCar_whenGettingPropertyTraditionalWay_thenNumberOfSeatsPropertyIsReturned() {
-        Assertions.assertThat(getPropertyTraditionalWay(car)).isEqualTo(5);
+        Assertions.assertThat(getPropertyTraditionalWay(car)).isEqualTo(4);
     }
 
     @Test
     public void givenCar_whenGettingPropertyViaPatternMatching_thenNumberOfSeatsPropertyIsReturned() {
-        Assertions.assertThat(getPropertyViaPatternMatching(car)).isEqualTo(5);
+        Assertions.assertThat(getPropertyViaPatternMatching(car)).isEqualTo(4);
     }
 
     @Test
     public void givenTruck_whenGettingPropertyTraditionalWay_thenLoadCapacityIsReturned() {
-        Assertions.assertThat(getPropertyTraditionalWay(truck)).isEqualTo(19000);
+        Assertions.assertThat(getPropertyTraditionalWay(truck)).isEqualTo(16000);
     }
 
     @Test
     public void givenTruck_whenGettingPropertyViaPatternMatching_thenLoadCapacityIsReturned() {
-        Assertions.assertThat(getPropertyViaPatternMatching(truck)).isEqualTo(19000);
+        Assertions.assertThat(getPropertyViaPatternMatching(truck)).isEqualTo(16000);
     }
 
     private int getPropertyTraditionalWay(Vehicle vehicle) {
