44ff0f03f6e674dea7261d51b7004497b667ff22
AbdallahSawan <abdallahsawan.e@gmail.com>
2020-11-02 10:31:44 +0200

    The Value of 0xFF Number and Its Uses With &amp; Operation in Java Article by Abdallah Sawan

--- java-numbers-4/src/test/java/com/baeldung/number_0xff/Number0xffUnitTest.java
+++ java-numbers-4/src/test/java/com/baeldung/number_0xff/Number0xffUnitTest.java
@@ -9,41 +9,41 @@ public class Number0xffUnitTest {
     public void test0xFFAssignedToInteger() {
         int x = 0xff;
         int expectedValue = 255;
-        assertEquals(x, expectedValue);
+        assertEquals(expectedValue, x);
     }
 
     @Test
     public void test0xFFAssignedToByte() {
         byte y = (byte) 0xff;
         int expectedValue = -1;
-        assertEquals(y, expectedValue);
+        assertEquals(expectedValue, y);
     }
 
     @Test
     public void givenColor_whenGetRedColor_thenExtractRedColor() {
         int rgba = 272214023;
         int expectedValue = 16;
-        assertEquals(Number0xff.getRedColor(rgba), expectedValue);
+        assertEquals(expectedValue, Number0xff.getRedColor(rgba));
     }
 
     @Test
     public void givenColor_whenGetGreenColor_thenExtractGreenColor() {
         int rgba = 272214023;
         int expectedValue = 57;
-        assertEquals(Number0xff.getGreenColor(rgba), expectedValue);
+        assertEquals(expectedValue, Number0xff.getGreenColor(rgba));
     }
 
     @Test
     public void givenColor_whenGetBlueColor_thenExtractBlueColor() {
         int rgba = 272214023;
         int expectedValue = 168;
-        assertEquals(Number0xff.getBlueColor(rgba), expectedValue);
+        assertEquals(expectedValue, Number0xff.getBlueColor(rgba));
     }
 
     @Test
     public void givenColor_whenGetAlfa_thenExtractAlfa() {
         int rgba = 272214023;
         int expectedValue = 7;
-        assertEquals(Number0xff.getAlfa(rgba), expectedValue);
+        assertEquals(expectedValue, Number0xff.getAlfa(rgba));
     }
 }
