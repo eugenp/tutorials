f09f44163b7d9a9f51641660dd0f065569fa4e69
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2020-11-03 14:07:23 +0530

    BAEL-4399: Added changes as per review comment

--- core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/Dependent.java
+++ core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/Dependent.java
@@ -1,6 +1,6 @@
 package com.baeldung.exceptions.nosuchfielderror;
 
 public class Dependent {
-	// This needed to be commented post compilation of NoSuchFielDError and Compile
-	public static String message = "Hello Baeldung!!";
+    // This needed to be commented post compilation of NoSuchFielDError and Compile
+    public static String message = "Hello Baeldung!!";
 }
