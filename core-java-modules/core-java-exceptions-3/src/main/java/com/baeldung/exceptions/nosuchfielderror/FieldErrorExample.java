74a8e9f163058904a6762e91cac6bf66e6ee1080
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2020-11-03 19:13:59 +0530

    Corrected the file names

---
+++ core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/FieldErrorExample.java
@@ -0,0 +1,16 @@
+package com.baeldung.exceptions.nosuchfielderror;
+
+public class FieldErrorExample {
+
+    public static void main(String... args) {
+        fetchAndPrint();
+    }
+
+    public static String getDependentMessage() {
+        return Dependent.message;
+    }
+
+    public static void fetchAndPrint() {
+        System.out.println(getDependentMessage());
+    }
+}
