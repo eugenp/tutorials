f40ed2cef50926ebc7c5b13413a84d1238c8a4b8
Harihar Das <harihar.246@gmail.com>
2020-11-03 19:13:36 +0100

    Channels examples (#10225)
    Co-authored-by: Harihar Das <harihar.das@revolut.com>

---
+++ core-kotlin-modules/core-kotlin-concurrency/src/main/kotlin/com/baeldung/channles/ProducerConsumer.kt
@@ -0,0 +1,22 @@
+package com.baeldung.channles
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.runBlocking
+
+@ExperimentalCoroutinesApi
+fun CoroutineScope.produceFruits(): ReceiveChannel<String> = produce {
+    val fruits = listOf("Apple", "Orange", "Apple")
+    for (fruit in fruits) send(fruit)
+}
+
+@ExperimentalCoroutinesApi
+fun main() = runBlocking {
+    val fruitChannel = produceFruits()
+    for (fruit in fruitChannel) {
+        println(fruit)
+    }
+    println("End!")
+}
