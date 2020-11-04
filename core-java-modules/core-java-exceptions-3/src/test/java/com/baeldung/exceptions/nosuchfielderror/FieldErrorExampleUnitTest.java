bef0ecee7211f6e7a758668dac2040e08eb49b97
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2020-11-04 19:34:50 +0530

    BAEL-4399: Changes for removing tab spaces

--- core-java-modules/core-java-exceptions-3/src/test/java/com/baeldung/exceptions/nosuchfielderror/FieldErrorExampleUnitTest.java
+++ core-java-modules/core-java-exceptions-3/src/test/java/com/baeldung/exceptions/nosuchfielderror/FieldErrorExampleUnitTest.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class FieldErrorExampleUnitTest {
-
+    
     @Test
     public void whenDependentMessage_returnMessage() {
+        
         String dependentMessage = FieldErrorExample.getDependentMessage();
         assertTrue("Hello Baeldung!!".equals(dependentMessage));
     }
+    
 }
