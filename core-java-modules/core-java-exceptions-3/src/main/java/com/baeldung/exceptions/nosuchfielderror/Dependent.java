e13f7952b13463ad6f274425e0b9e9d52826b1b2
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2020-11-04 19:34:50 +0530

    BAEL-4399: Changes for removing tab spaces

--- core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/Dependent.java
+++ core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/Dependent.java
@@ -1,6 +1,8 @@
 package com.baeldung.exceptions.nosuchfielderror;
 
 public class Dependent {
+    
     // This needed to be commented post compilation of NoSuchFielDError and Compile
     public static String message = "Hello Baeldung!!";
+    
 }
