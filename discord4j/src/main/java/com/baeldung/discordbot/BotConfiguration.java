3aac4f96e4b419d88e03b60bdc8bbd512ecac8b7
Jordan Simpson <jordan.simpson.dev@gmail.com>
2020-11-04 00:23:35 -0600

    Added code examples from the article.

---
+++ discord4j/src/main/java/com/baeldung/discordbot/BotConfiguration.java
@@ -0,0 +1,37 @@
+package com.baeldung.discordbot;
+
+import com.baeldung.discordbot.events.EventListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class BotConfiguration {
+
+    @Value("${token}")
+    private String token;
+
+    @Bean
+    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
+        GatewayDiscordClient client = DiscordClientBuilder.create(token)
+                .build()
+                .login()
+                .block();
+
+        for(EventListener<T> listener : eventListeners) {
+            client.on(listener.getEventType())
+                  .flatMap(listener::execute)
+                  .onErrorResume(listener::handleError)
+                  .subscribe();
+        }
+
+        client.onDisconnect().block();
+
+        return client;
+    }
+}
---
+++ discord4j/src/test/java/com/baeldung/discordbot/DiscordBotApplicationTests.java
@@ -0,0 +1,12 @@
+package com.baeldung.discordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
