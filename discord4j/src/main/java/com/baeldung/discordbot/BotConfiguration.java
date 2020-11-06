4710fe7bc6d66f7dea7f22d7b606746fdb438453
Jordan Simpson <jordan.simpson.dev@gmail.com>
2020-11-05 23:12:57 -0600

    Replace test class and add error handling for invalid tokens.

--- discord4j/src/main/java/com/baeldung/discordbot/BotConfiguration.java
+++ discord4j/src/main/java/com/baeldung/discordbot/BotConfiguration.java
@@ -4,6 +4,8 @@ import com.baeldung.discordbot.events.EventListener;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.Event;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,24 +15,33 @@ import java.util.List;
 @Configuration
 public class BotConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger( BotConfiguration.class );
+
     @Value("${token}")
     private String token;
 
     @Bean
     public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
-        GatewayDiscordClient client = DiscordClientBuilder.create(token)
-          .build()
-          .login()
-          .block();
+        GatewayDiscordClient client = null;
 
-        for(EventListener<T> listener : eventListeners) {
-            client.on(listener.getEventType())
-              .flatMap(listener::execute)
-              .onErrorResume(listener::handleError)
-              .subscribe();
+        try {
+            client = DiscordClientBuilder.create(token)
+              .build()
+              .login()
+              .block();
+
+            for(EventListener<T> listener : eventListeners) {
+                client.on(listener.getEventType())
+                  .flatMap(listener::execute)
+                  .onErrorResume(listener::handleError)
+                  .subscribe();
+            }
+
+            client.onDisconnect().block();
         }
-
-        client.onDisconnect().block();
+        catch ( Exception exception ) {
+            log.error( "Be sure to use a valid bot token!", exception );
+        }
 
         return client;
     }
