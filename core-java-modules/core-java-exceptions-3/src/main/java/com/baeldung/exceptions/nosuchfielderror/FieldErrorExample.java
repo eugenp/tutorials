d24a59c23613d9790ef601663c3c2377097793f2
Amitabh.Tiwari <amitabh.tiwari@maersk.com>
2020-11-06 16:02:36 +0530

    Added changes to remove unwanted spaces

--- core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/FieldErrorExample.java
+++ core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/FieldErrorExample.java
@@ -1,20 +1,20 @@
 package com.baeldung.exceptions.nosuchfielderror;
 
 public class FieldErrorExample {
-    
+
     public static void main(String... args) {
-        
+
         fetchAndPrint();
     }
-    
+
     public static String getDependentMessage() {
-        
+
         return Dependent.message;
     }
-    
+
     public static void fetchAndPrint() {
-        
+
         System.out.println(getDependentMessage());
     }
-    
+
 }
