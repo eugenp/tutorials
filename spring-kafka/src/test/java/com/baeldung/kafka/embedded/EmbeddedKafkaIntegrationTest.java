eb3631f36f8e6ab3728b3faffe4169988f84c914
Jonathan Cook <jcook@sciops.esa.int>
2020-11-08 13:23:31 +0100

    BAEL-4687 Testing Kafka and Spring Boot

---
+++ spring-kafka/src/test/java/com/baeldung/kafka/embedded/EmbeddedKafkaIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.baeldung.kafka.embedded;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
+class EmbeddedKafkaIntegrationTest {
+
+    @Autowired
+    public KafkaTemplate<String, String> template;
+
+    @Autowired
+    private KafkaConsumer consumer;
+
+    @Autowired
+    private KafkaProducer producer;
+
+    @Value("${test.topic}")
+    private String topic;
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingtoDefaultTemplate_thenMessageReceived() throws Exception {
+        template.send(topic, "Sending with default template");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        assertThat(consumer.getLatch().getCount(), equalTo(0L));
+        
+        assertThat(consumer.getPayload(), containsString("embedded-test-topic"));
+    }
+
+    @Test
+    public void givenEmbeddedKafkaBroker_whenSendingtoSimpleProducer_thenMessageReceived() throws Exception {
+        producer.send(topic, "Sending with own controller");
+        consumer.getLatch().await(10000, TimeUnit.MILLISECONDS);
+        
+        assertThat(consumer.getLatch().getCount(), equalTo(0L));
+        assertThat(consumer.getPayload(), containsString("embedded-test-topic"));
+    }
+
+}
