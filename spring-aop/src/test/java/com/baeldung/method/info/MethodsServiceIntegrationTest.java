fed995c645f4d013e216af67e57939fcb13b44c3
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-11-11 09:45:53 +0000

    BAEL-4071 - Fixing test class name

--- spring-aop/src/test/java/com/baeldung/method/info/MethodsServiceIntegrationTest.java
+++ spring-aop/src/test/java/com/baeldung/method/info/MethodsServiceIntegrationTest.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MethodsServiceTest {
+class MethodsServiceIntegrationTest {
 
   @Autowired
   MethodsService methodsService;
