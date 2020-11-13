cfb424f9d10b550856e1588def0d0e2c4af1b2d3
Jonathan Cook <jonathan.cook@erars.plus.com>
2020-11-13 19:31:59 +0100

    BAEL-4609 - Testing Kafka and Spring Boot (#10249)
    * BAEL-4437 - System Rules
    
    * BAEL-4687 Testing Kafka and Spring Boot
    
    * BAEL-4609 - Testing Kafka and Spring Boot
    
    Co-authored-by: Jonathan Cook <jcook@sciops.esa.int>

---
+++ spring-kafka/src/main/java/com/baeldung/kafka/embedded/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.baeldung.kafka.embedded;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaProducer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String payload) {
+        LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
+        kafkaTemplate.send(topic, payload);
+    }
+
+}
