861d25cbf7343df8d949bf4ce017e3d8c246563c
Karsten Silz <karsten.silz@outlook.com>
2020-11-14 18:21:48 +0000

    Fixed application start-up.

--- docker/docker-spring-boot-postgres/src/main/java/com/baeldung/docker/DemoApplication.java
+++ docker/docker-spring-boot-postgres/src/main/java/com/baeldung/docker/DemoApplication.java
@@ -1,13 +1,23 @@
 package com.baeldung.docker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class DemoApplication {
+    private final Logger logger = LoggerFactory.getLogger(DemoApplication.class);    
+    
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    @EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        logger.info("Here be dragons!");
+    }
+    
 }
