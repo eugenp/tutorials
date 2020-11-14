3ec8758de3dc89fca6c1077c01e67e514d634038
Karsten Silz <karsten.silz@outlook.com>
2020-11-14 15:30:38 +0000

    Shortened class names.

--- docker/docker-spring-boot-postgres/src/main/java/com/baeldung/docker/DemoApplication.java
+++ docker/docker-spring-boot-postgres/src/main/java/com/baeldung/docker/DemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootPostgresDockerApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPostgresDockerApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
