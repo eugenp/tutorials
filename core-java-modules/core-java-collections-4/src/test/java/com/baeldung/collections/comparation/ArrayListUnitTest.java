bd8aa6a8a4f38025de30ab9dc0c0caa43df7ce16
Daniel Strmecki <daniel.strmecki@gmail.com>
2020-11-15 11:47:08 +0100

    BAEL-4717: Added ArrayList examples

---
+++ core-java-modules/core-java-collections-4/src/test/java/com/baeldung/collections/comparation/ArrayListUnitTest.java
@@ -0,0 +1,29 @@
+package com.baeldung.collections.comparation;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArrayListUnitTest {
+
+    @Test
+    void givenList_whenItemAddedToSpecificIndex_thenItCanBeRetrieved() {
+        List<String> list = new ArrayList<>();
+        list.add("Daniel");
+        list.add(1, "Marko");
+        assertThat(list).hasSize(2);
+        assertThat(list.get(1)).isEqualTo("Marko");
+    }
+
+    @Test
+    void givenList_whenItemRemovedViaIndex_thenListSizeIsReduced() {
+        List<String> list = new ArrayList<>(Arrays.asList("Daniel", "Marko"));
+        list.remove(1);
+        assertThat(list).hasSize(1);
+    }
+
+}
