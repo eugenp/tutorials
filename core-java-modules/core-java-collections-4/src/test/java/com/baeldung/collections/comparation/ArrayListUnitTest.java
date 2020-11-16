9dd0aaf61b96caa027d5ed251c80684aabb6c07e
Daniel Strmecki <daniel.strmecki@gmail.com>
2020-11-16 16:02:18 +0100

    BAEL-4717: Added HashMap and LinkedList examples

--- core-java-modules/core-java-collections-4/src/test/java/com/baeldung/collections/comparation/ArrayListUnitTest.java
+++ core-java-modules/core-java-collections-4/src/test/java/com/baeldung/collections/comparation/ArrayListUnitTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArrayListUnitTest {
 
     @Test
-    void givenList_whenItemAddedToSpecificIndex_thenItCanBeRetrieved() {
+    void givenArrayList_whenItemAddedToSpecificIndex_thenItCanBeRetrieved() {
         List<String> list = new ArrayList<>();
         list.add("Daniel");
         list.add(1, "Marko");
@@ -20,10 +20,11 @@ public class ArrayListUnitTest {
     }
 
     @Test
-    void givenList_whenItemRemovedViaIndex_thenListSizeIsReduced() {
+    void givenArrayList_whenItemRemovedViaIndex_thenListSizeIsReduced() {
         List<String> list = new ArrayList<>(Arrays.asList("Daniel", "Marko"));
         list.remove(1);
         assertThat(list).hasSize(1);
+        assertThat(list).doesNotContain("Marko");
     }
 
 }
