567a63cb7da115bfa85690dc1c94dcbb67b8e3f5
Michael Pratt <prattm@gmail.com>
2020-11-20 07:49:50 -0700

    Update formatting

--- core-java-modules/core-java-15/src/main/java/com/baeldung/whatsnew/sealedclasses/Person.java
+++ core-java-modules/core-java-15/src/main/java/com/baeldung/whatsnew/sealedclasses/Person.java
@@ -2,23 +2,19 @@ package com.baeldung.whatsnew.sealedclasses;
 
 import java.util.Date;
 
-public sealed class Person permits Employee, Manager
-{
+public sealed class Person permits Employee, Manager {
     /**
      * Demonstration of pattern matching for instanceof
      *
      * @param person A Person object
      * @return
      */
-    public static void patternMatchingDemo(Person person)
-    {
-        if(person instanceof Employee employee)
-        {
+    public static void patternMatchingDemo(Person person) {
+        if(person instanceof Employee employee) {
             Date hiredDate = employee.getHiredDate();
         }
 
-        if(person instanceof Employee employee && employee.getHiredDate() != null)
-        {
+        if(person instanceof Employee employee && employee.getHiredDate() != null) {
             Date hiredDate = employee.getHiredDate();
         }
     }
