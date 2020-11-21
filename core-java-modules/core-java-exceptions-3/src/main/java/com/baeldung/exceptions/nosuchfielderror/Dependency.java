761e38f0a1965dcc5752bd8e622b91fcaaf3e683
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2020-11-21 15:35:14 +0530

    Corrected the changes based on review comment

--- core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/Dependency.java
+++ core-java-modules/core-java-exceptions-3/src/main/java/com/baeldung/exceptions/nosuchfielderror/Dependency.java
@@ -1,6 +1,6 @@
 package com.baeldung.exceptions.nosuchfielderror;
 
-public class Dependent {
+public class Dependency {
     
     // This needed to be commented post compilation of NoSuchFielDError and Compile
     public static String message = "Hello Baeldung!!";
