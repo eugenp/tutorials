83930cad49d1cd343aa1ced8754c76a1fa575a02
amdegregorio <amy.m.degregorio@gmail.com>
2020-11-28 12:46:54 -0500

    BAEL-4721 Requested edits

--- core-java-modules/core-java-io-3/src/test/java/com/baeldung/writebytearray/WriteByteArrayUnitTest.java
+++ core-java-modules/core-java-io-3/src/test/java/com/baeldung/writebytearray/WriteByteArrayUnitTest.java
@@ -10,15 +10,19 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 import org.apache.commons.io.FileUtils;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import com.google.common.io.ByteSink;
 import com.google.common.io.MoreFiles;
 
 public class WriteByteArrayUnitTest {
     private static byte[] dataForWriting;
+    
+    @Rule
+    public TemporaryFolder tempFolder = new TemporaryFolder();
 
     @BeforeClass
     public static void setup() throws IOException {
@@ -27,57 +31,47 @@ public class WriteByteArrayUnitTest {
 
     @Test
     public void whenUsingFileOutputStream_thenByteArrayIsWritten() throws IOException {
-        try (FileOutputStream outputStream = new FileOutputStream("src/test/resources/example-fos.jpg")) {
+    	File outputFile = tempFolder.newFile("example-fos.jpg");
+        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
             outputStream.write(dataForWriting);
-            assertThat(new File("src/test/resources/example-fos.jpg")).hasBinaryContent(dataForWriting);
-        } catch (IOException ioe) {
-            throw ioe;
+            assertThat(outputFile).hasBinaryContent(dataForWriting);
         }
     }
 
     @Test
     public void whenUsingNioFiles_thenByteArrayIsWritten() throws IOException {
-        // Takes Optional OpenOptions, if not present defaults to CREATE, TRUNCATE_EXISTING (will truncate an existing file to size of 0), and WRITE
-        // https://docs.oracle.com/javase/8/docs/api/java/nio/file/Files.html#write-java.nio.file.Path-byte:A-java.nio.file.OpenOption...-
-        Files.write(Paths.get("src/test/resources/example-nio-files.jpg"), dataForWriting);
-        assertThat(new File("src/test/resources/example-nio-files.jpg")).hasBinaryContent(dataForWriting);
+        File outputFile =  tempFolder.newFile("example-nio-files.jpg");
+        Files.write(outputFile.toPath(), dataForWriting);
+        assertThat(outputFile).hasBinaryContent(dataForWriting);
     }
 
     @Test
     public void whenUsingGuavaFiles_thenByteArrayIsWritten() throws IOException {
-        com.google.common.io.Files.write(dataForWriting, new File("src/test/resources/example-guava-files.jpg"));
-        assertThat(new File("src/test/resources/example-guava-files.jpg")).hasBinaryContent(dataForWriting);
+    	File outputFile = tempFolder.newFile("example-guava-files.jpg");
+        com.google.common.io.Files.write(dataForWriting, outputFile);
+        assertThat(outputFile).hasBinaryContent(dataForWriting);
     }
 
     @Test
     public void whenUsingGuavaByteSink_thenByteArrayIsWritten() throws IOException {
-        // Also discuss - https://guava.dev/releases/snapshot-jre/api/docs/com/google/common/io/MoreFiles.html
-        ByteSink byteSink = com.google.common.io.Files.asByteSink(new File("src/test/resources/example-guava-bs.jpg"));
+    	File outputFile = tempFolder.newFile("example-guava-bs.jpg");
+        ByteSink byteSink = com.google.common.io.Files.asByteSink(outputFile);
         byteSink.write(dataForWriting);
-        assertThat(new File("src/test/resources/example-guava-bs.jpg")).hasBinaryContent(dataForWriting);
+        assertThat(outputFile).hasBinaryContent(dataForWriting);
     }
 
     @Test
     public void whenUsingGuavaByteSinkMoreFiles_thenByteArrayIsWritten() throws IOException {
-        ByteSink byteSink = MoreFiles.asByteSink(Paths.get("src/test/resources/example-guava-bs-mf.jpg"), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+    	File outputFile = tempFolder.newFile("example-guava-bs.jpg");
+        ByteSink byteSink = MoreFiles.asByteSink(outputFile.toPath(), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
         byteSink.write(dataForWriting);
-        assertThat(new File("src/test/resources/example-guava-bs-mf.jpg")).hasBinaryContent(dataForWriting);
+        assertThat(outputFile).hasBinaryContent(dataForWriting);
     }
 
     @Test
     public void whenUserCommonsIo_thenByteArrayIsWritten() throws IOException {
-        // http://commons.apache.org/proper/commons-io/apidocs/org/apache/commons/io/FileUtils.html#writeByteArrayToFile%28java.io.File,%20byte%5B%5D%29
-        FileUtils.writeByteArrayToFile(new File("src/test/resources/example-file-utils.jpg"), dataForWriting);
-        assertThat(new File("src/test/resources/example-file-utils.jpg")).hasBinaryContent(dataForWriting);
-    }
-
-    @AfterClass
-    public static void teardown() throws IOException {
-        Files.deleteIfExists(Paths.get("src/test/resources/example-fos.jpg"));
-        Files.deleteIfExists(Paths.get("src/test/resources/example-nio-files.jpg"));
-        Files.deleteIfExists(Paths.get("src/test/resources/example-guava-files.jpg"));
-        Files.deleteIfExists(Paths.get("src/test/resources/example-guava-bs.jpg"));
-        Files.deleteIfExists(Paths.get("src/test/resources/example-guava-bs-mf.jpg"));
-        Files.deleteIfExists(Paths.get("src/test/resources/example-file-utils.jpg"));
+    	File outputFile = tempFolder.newFile("example-file-utils.jpg");
+        FileUtils.writeByteArrayToFile(outputFile, dataForWriting);
+        assertThat(outputFile).hasBinaryContent(dataForWriting);
     }
 }
