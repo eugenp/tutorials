3181d109627497aa7b1fccc90357fada54f4842c
Adrian Maghear <adrian.maghear@gmail.com>
2020-12-14 19:24:02 +0100

    update indentation

--- spring-cloud/spring-cloud-eureka/spring-cloud-eureka-feign-client-integration-test/src/test/java/com/baeldung/spring/cloud/client/BookMocks.java
+++ spring-cloud/spring-cloud-eureka/spring-cloud-eureka-feign-client-integration-test/src/test/java/com/baeldung/spring/cloud/client/BookMocks.java
@@ -14,13 +14,14 @@ public class BookMocks {
 
     public static void setupMockBooksResponse(WireMockServer mockService) throws IOException {
         mockService.stubFor(WireMock.get(WireMock.urlEqualTo("/books"))
-          .willReturn(WireMock.aResponse()
-            .withStatus(HttpStatus.OK.value())
-            .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-            .withBody(
-              copyToString(
-                BookMocks.class.getClassLoader().getResourceAsStream("payload/get-books-response.json"),
-                defaultCharset()))));
+          .willReturn(
+            WireMock.aResponse()
+              .withStatus(HttpStatus.OK.value())
+              .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+              .withBody(
+                copyToString(
+                  BookMocks.class.getClassLoader().getResourceAsStream("payload/get-books-response.json"),
+                  defaultCharset()))));
     }
 
 }
