d4b22c74e3ab06291ba92585f136342c347b83e2
Adina Rolea <adina.fometescu@elan-solutions.eu>
2020-12-17 14:02:59 +0200

    BAEL-4687: updated formatting

--- spring-boot-modules/spring-boot-data-2/src/main/java/com/baeldung/boot/jackson/controller/CoffeeController.java
+++ spring-boot-modules/spring-boot-data-2/src/main/java/com/baeldung/boot/jackson/controller/CoffeeController.java
@@ -1,20 +1,21 @@
 package com.baeldung.boot.jackson.controller;
 
-import java.time.LocalDateTime;
-
+import com.baeldung.boot.jackson.model.Coffee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.baeldung.boot.jackson.model.Coffee;
+import java.time.LocalDateTime;
 
 @RestController
 public class CoffeeController {
 
     @GetMapping("/coffee")
-    public Coffee getCoffee(@RequestParam(required = false) String brand, @RequestParam(required = false) String name) {
+    public Coffee getCoffee(
+            @RequestParam(required = false) String brand,
+            @RequestParam(required = false) String name) {
         return new Coffee().setBrand(brand)
-            .setDate(LocalDateTime.now())
-            .setName(name);
+                .setDate(LocalDateTime.now())
+                .setName(name);
     }
 }
