0c926e74413c8e2b011358b4db1e32deb1730717
Sallo Szrajbman <salloszrajbman@hotmail.com>
2020-12-19 12:58:00 +0000

    BAEL-4071 - Get advised method info in Spring AOP Fixing identation

--- spring-aop/src/main/java/com/baeldung/method/info/WithdrawLimitException.java
+++ spring-aop/src/main/java/com/baeldung/method/info/WithdrawLimitException.java
@@ -1,7 +1,7 @@
 package com.baeldung.method.info;
 
-public class WithdrawLimitException  extends RuntimeException {
-  public WithdrawLimitException(String message) {
-    super(message);
-  }
+public class WithdrawLimitException extends RuntimeException {
+    public WithdrawLimitException(String message) {
+        super(message);
+    }
 }
