6e4b940d7e499fe20eedf122e307659c49be0573
Gilvan Ornelas Fernandes Filho <gilvan.fernandes@bairesdev.com>
2020-12-20 21:29:30 -0300

    Clean architecture initial commit

---
+++ patterns/clean-architecture/src/main/java/com/baeldung/pattern/cleanarchitecture/usercreation/CommonUser.java
@@ -0,0 +1,30 @@
+package com.baeldung.pattern.cleanarchitecture.usercreation;
+
+class CommonUser implements User {
+
+    String name;
+    String password;
+
+    CommonUser(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    CommonUser() {
+    }
+
+    @Override
+    public boolean passwordIsValid() {
+        return password == null || password.length() > 5;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ patterns/clean-architecture/src/test/java/com/baeldung/pattern/cleanarchitecture/usercreation/UserResponseFormatterTests.java
@@ -0,0 +1,29 @@
+package com.baeldung.pattern.cleanarchitecture.usercreation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.baeldung.pattern.cleanarchitecture.usercreation.UserResponseFormatter;
+import com.baeldung.pattern.cleanarchitecture.usercreation.UserResponseModel;
+
+class UserResponseFormatterTests {
+
+    UserResponseFormatter userResponseFormatter = new UserResponseFormatter();
+
+    @Test
+    void givenDateAnd3HourTime_whenPrepareSuccessView_thenReturnOnly3HourTime() {
+        UserResponseModel modelResponse = new UserResponseModel("baeldung", "2020-12-20T03:00:00.000");
+        UserResponseModel formattedResponse = userResponseFormatter.prepareSuccessView(modelResponse);
+
+        assertThat(formattedResponse.getCreationTime()).isEqualTo("03:00:00");
+    }
+
+    @Test
+    void whenPrepareFailView_thenThrowHttpConflictException() {
+        assertThatThrownBy(() -> userResponseFormatter.prepareFailView("Invalid password"))
+            .isInstanceOf(ResponseStatusException.class);
+    }
+}
