c6e380d798ed362a754754a73f82a7553f66dd0d
krzysztof <kmajewski@berg.technology>
2020-12-23 12:46:18 +0100

    BAEL-4750 Java 12 New Features

---
+++ core-java-modules/core-java-12/src/test/java/com/baeldung/newfeatures/TeeingCollectorUnitTest.java
@@ -0,0 +1,18 @@
+package java.com.baeldung.newfeatures;
+
+import org.junit.Test;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+
+public class TeeingCollectorUnitTest {
+
+    @Test
+    public void givenSetOfNumbers_thenCalculateAverage() {
+        double mean = Stream.of(1, 2, 3, 4, 5)
+                .collect(Collectors.teeing(Collectors.summingDouble(i -> i), Collectors.counting(), (sum, count) -> sum / count));
+        assertEquals(3.0, mean);
+    }
+}
