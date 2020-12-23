59b79b1db61bdee270640b39be2d892f9f07bd8a
Krzysztof Woyke <krzysztofwoyke@gmail.com>
2020-12-23 11:05:24 +0100

    JAVA-3496: Add integration tests for UserRepository

---
+++ persistence-modules/spring-data-jpa-query-2/src/main/java/com/baeldung/spring/data/jpa/query/UserApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.spring.data.jpa.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
