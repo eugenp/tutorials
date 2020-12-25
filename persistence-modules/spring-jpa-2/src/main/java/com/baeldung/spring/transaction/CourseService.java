9f76afc4404d4018f44abfd72c2b5a1a688dd634
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2020-12-25 09:06:51 +0530

    Added changes to show transaction roll back

---
+++ persistence-modules/spring-jpa-2/src/main/java/com/baeldung/spring/transaction/CourseService.java
@@ -0,0 +1,38 @@
+package com.baeldung.spring.transaction;
+
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.TransactionAspectSupport;
+
+@Service
+public class CourseService {
+    
+    @Autowired
+    private CourseDao courseDao;
+    
+    @Transactional
+    public void createCourseDeclarativeWithRuntimeException(Course course) {
+        courseDao.createWithRuntimeException(course);
+    }
+    
+    @Transactional(rollbackFor = { SQLException.class })
+    public void createCourseDeclarativeWithCheckedException(Course course) throws SQLException {
+        courseDao.createWithCheckedException(course);
+    }
+    
+    public void createCourseDefaultRatingProgramatic(Course course) {
+        try {
+            courseDao.createWithRuntimeException(course);
+        } catch (Exception e) {
+            TransactionAspectSupport.currentTransactionStatus().setRollbackOnly();
+        }
+    }
+    
+    public Course findById(Long id) {
+        return courseDao.findOne(id);
+    }
+    
+}
