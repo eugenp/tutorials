1b4e5782ec11316b254c8faec298dc66d433dfdc
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2021-01-04 09:05:55 +0530

    BAEL-4624: Review comments

--- persistence-modules/spring-jpa-2/src/main/java/com/baeldung/spring/transaction/CourseDao.java
+++ persistence-modules/spring-jpa-2/src/main/java/com/baeldung/spring/transaction/CourseDao.java
@@ -1,8 +1,5 @@
 package com.baeldung.spring.transaction;
 
-import java.sql.SQLException;
-
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Repository;
 
 import com.baeldung.spring.hibernate.AbstractHibernateDao;
@@ -12,13 +9,5 @@ public class CourseDao extends AbstractHibernateDao<Course> {
     public CourseDao() {
         super();
         setClazz(Course.class);
-    }
-    
-    public Course createWithRuntimeException(final Course entity) {
-        throw new DataIntegrityViolationException("Throwing exception for demoing Rollback!!!");
-    }
-    
-    public Course createWithCheckedException(final Course entity) throws SQLException {
-        throw new SQLException("Throwing exception for demoing Rollback!!!");
     }
 }
