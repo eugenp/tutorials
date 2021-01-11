0de32373657be04f4d28ed50bc558a263fd0cb00
Gerardo Roza <rozagerardo@gmail.com>
2021-01-11 14:23:27 -0300

    improved and updated spring-boot-yaml-vs-properties article code

--- spring-boot-modules/spring-boot-properties-3/src/test/java/com/baeldung/boot/properties/multidocument/DevMultidocumentFilesIntegrationTest.java
+++ spring-boot-modules/spring-boot-properties-3/src/test/java/com/baeldung/boot/properties/multidocument/DevMultidocumentFilesIntegrationTest.java
@@ -11,14 +11,18 @@ import org.springframework.test.context.ActiveProfiles;
 import com.baeldung.boot.properties.DemoApplication;
 
 @SpringBootTest(classes = { DemoApplication.class }, webEnvironment = WebEnvironment.MOCK)
-@ActiveProfiles("prod")
-public class ProdMultidocumentPropertiesFileIntegrationTest {
+@ActiveProfiles("multidocument-dev")
+public class DevMultidocumentFilesIntegrationTest {
 
     @Value("${bael.property}")
     private String baelCustomProperty;
+    
+    @Value("${bael.root-level-property}")
+    private String baelRootProperty;
 
     @Test
-    public void givenProductionProfileActive_whenApplicationStarts_thenDefaultPropertiesUser() {
-        assertThat(baelCustomProperty).isEqualTo("prodValue");
+    public void givenDefaultProfileActive_whenApplicationStarts_thenDefaultPropertiesUser() {
+        assertThat(baelCustomProperty).isEqualTo("devValue");
+        assertThat(baelRootProperty).isEqualTo("defaultRootLevelValue");
     }
 }
--- spring-boot-modules/spring-boot-properties-3/src/test/java/com/baeldung/boot/properties/multidocument/StagingMultidocumentFilesIntegrationTest.java
+++ spring-boot-modules/spring-boot-properties-3/src/test/java/com/baeldung/boot/properties/multidocument/StagingMultidocumentFilesIntegrationTest.java
@@ -6,17 +6,23 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.baeldung.boot.properties.DemoApplication;
 
 @SpringBootTest(classes = { DemoApplication.class }, webEnvironment = WebEnvironment.MOCK)
-public class DefaultMultidocumentPropertiesFileIntegrationTest {
+@ActiveProfiles("multidocument-staging")
+public class StagingMultidocumentFilesIntegrationTest {
 
     @Value("${bael.property}")
     private String baelCustomProperty;
+    
+    @Value("${bael.root-level-property}")
+    private String baelRootProperty;
 
     @Test
-    public void givenDefaultProfileActive_whenApplicationStarts_thenDefaultPropertiesUser() {
-        assertThat(baelCustomProperty).isEqualTo("defaultValue");
+    public void givenProductionProfileActive_whenApplicationStarts_thenDefaultPropertiesUser() {
+        assertThat(baelCustomProperty).isEqualTo("stagingValue");
+        assertThat(baelRootProperty).isEqualTo("defaultRootLevelValue");
     }
 }
