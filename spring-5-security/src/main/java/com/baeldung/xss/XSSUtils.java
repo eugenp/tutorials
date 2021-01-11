bd0c47a5603dc31d6032debfc45d202487a30666
sharifi <h_sharifi@modernisc.com>
2021-01-11 16:07:11 +0330

    change value check

--- spring-5-security/src/main/java/com/baeldung/xss/XSSUtils.java
+++ spring-5-security/src/main/java/com/baeldung/xss/XSSUtils.java
@@ -5,16 +5,15 @@ import org.jsoup.safety.Whitelist;
 import org.owasp.esapi.ESAPI;
 
 public class XSSUtils {
-    private XSSUtils() {
-    }
 
     public static String stripXSS(String value) {
-        if (value != null) {
-            value = ESAPI.encoder()
-                .canonicalize(value);
-            value = value.replaceAll("\0", "");
-            value = Jsoup.clean(value, Whitelist.none());
+        if (value == null) {
+            return null;
         }
+        value = ESAPI.encoder()
+            .canonicalize(value);
+        value = value.replaceAll("\0", "");
+        value = Jsoup.clean(value, Whitelist.none());
         return value;
     }
 
