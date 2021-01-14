7e51af38d962b0f26f630bc0db1527778a7c4f87
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2021-01-14 15:18:33 +0530

    Corrected the name of unit test to mark them as manual.

--- core-java-modules/core-java-concurrency-2/src/test/java/com/baeldung/concurrent/MyCounterJCStressManualTest.java
+++ core-java-modules/core-java-concurrency-2/src/test/java/com/baeldung/concurrent/MyCounterJCStressManualTest.java
@@ -10,11 +10,17 @@ import org.openjdk.jcstress.annotations.Outcome;
 import org.openjdk.jcstress.annotations.State;
 import org.openjdk.jcstress.infra.results.I_Result;
 
+/**
+ * This is defined as a manual test because it tries to simulate the race conditions
+ * in a concurrent program that is poorly designed and hence may fail nondeterministically.
+ * This will help the CI jobs to ignore these tests and a developer to run them manually.
+ *
+ */
 @JCStressTest
 @Outcome(id = "1", expect = ACCEPTABLE_INTERESTING, desc = "One update lost.")
 @Outcome(id = "2", expect = ACCEPTABLE, desc = "Both updates.")
 @State
-public class MyCounterJCStressUnitTest {
+public class MyCounterJCStressManualTest {
 
     private MyCounter counter;
 
