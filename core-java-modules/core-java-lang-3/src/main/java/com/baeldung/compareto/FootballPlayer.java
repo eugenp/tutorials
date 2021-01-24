268fe2dfe65da6315c59b1e84bcb7d88e357f3cd
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-01-24 12:33:05 +0100

    BASE-4535: Example for overriding compareTo

---
+++ core-java-modules/core-java-lang-3/src/main/java/com/baeldung/compareto/FootballPlayer.java
@@ -0,0 +1,28 @@
+package com.baeldung.compareto;
+
+public class FootballPlayer implements Comparable<FootballPlayer> {
+
+    private final String name;
+    private final int goalsScored;
+
+    public FootballPlayer(String name, int goalsScored) {
+        this.name = name;
+        this.goalsScored = goalsScored;
+    }
+
+    @Override
+    public int compareTo(FootballPlayer anotherPlayer) {
+        return this.goalsScored - anotherPlayer.goalsScored;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (object == null || getClass() != object.getClass())
+            return false;
+        FootballPlayer player = (FootballPlayer) object;
+        return name.equals(player.name);
+    }
+
+}
