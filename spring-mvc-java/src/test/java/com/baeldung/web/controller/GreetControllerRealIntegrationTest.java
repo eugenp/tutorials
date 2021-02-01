48abacef11d6970740c85fae237c19dbf2a35606
vishal <vishal.akkalkote@spark.net>
2021-02-01 23:19:23 +0100

    BAEL-2620 | Update test with latest version of Spring 5 and JUnit 5

--- spring-mvc-java/src/test/java/com/baeldung/web/controller/GreetControllerRealIntegrationTest.java
+++ spring-mvc-java/src/test/java/com/baeldung/web/controller/GreetControllerRealIntegrationTest.java
@@ -2,33 +2,27 @@ package com.baeldung.web.controller;
 
 import io.restassured.RestAssured;
 import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
+import static io.restassured.RestAssured.DEFAULT_PORT;
 import static io.restassured.RestAssured.given;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = RANDOM_PORT)
+@SpringBootTest(webEnvironment = DEFINED_PORT)
 @TestPropertySource(properties = {"spring.main.allow-bean-definition-overriding=true", "server.servlet.context-path=/"})
 public class GreetControllerRealIntegrationTest {
 
-    @LocalServerPort
-    private int port;
-
     @Before
     public void setUp() {
-        RestAssured.port = port;
+        RestAssured.port = DEFAULT_PORT;
     }
 
     @Test
     public void givenGreetURI_whenSendingReq_thenVerifyResponse() {
         given().get("/greet")
-          .then()
-          .statusCode(200);
+                .then()
+                .statusCode(200);
     }
 }
