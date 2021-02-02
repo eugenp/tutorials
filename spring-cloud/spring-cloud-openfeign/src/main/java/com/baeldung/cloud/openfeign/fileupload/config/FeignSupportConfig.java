5a510cfbaa15e430623710c8d80719535eb36772
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2021-02-02 18:29:42 +0530

    BAEL-3595: Initial change for Multipart file upload

---
+++ spring-cloud/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/fileupload/config/FeignSupportConfig.java
@@ -0,0 +1,28 @@
+package com.baeldung.cloud.openfeign.fileupload.config;
+
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+import org.springframework.web.client.RestTemplate;
+
+import feign.codec.Encoder;
+import feign.form.spring.SpringFormEncoder;
+
+@Configuration
+public class FeignSupportConfig {
+    @Bean
+    @Primary
+    @Scope("prototype")
+    public Encoder multipartFormEncoder() {
+        return new SpringFormEncoder(new SpringEncoder(new ObjectFactory<HttpMessageConverters>() {
+            @Override
+            public HttpMessageConverters getObject() {
+                return new HttpMessageConverters(new RestTemplate().getMessageConverters());
+            }
+        }));
+    }
+}
---
+++ spring-cloud/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/fileupload/controller/FileController.java
@@ -0,0 +1,27 @@
+package com.baeldung.cloud.openfeign.fileupload.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.baeldung.cloud.openfeign.fileupload.service.UploadService;
+
+@RestController
+public class FileController {
+    
+    @Autowired
+    private UploadService service;
+    
+    @PostMapping(value = "/upload")
+    public String handleFileUpload(@RequestPart(value = "file") MultipartFile file) {
+        return service.uploadFile(file);
+    }
+    
+    @PostMapping(value = "/upload-wo-client")
+    public boolean handleFileUploadWithoutClient(@RequestPart(value = "file") MultipartFile file) {
+        return service.uploadFileWithoutClient(file);
+    }
+    
+}
