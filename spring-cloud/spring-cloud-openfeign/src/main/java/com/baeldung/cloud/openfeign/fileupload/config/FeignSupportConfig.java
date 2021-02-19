d77f3147e77c77c9a2dc9e1d15f3c6a29125b0e1
Amitabh Tiwari <Amitabh.Tiwari@maersk.com>
2021-02-19 20:58:16 +0530

    Removed @confiuration

--- spring-cloud/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/fileupload/config/FeignSupportConfig.java
+++ spring-cloud/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/fileupload/config/FeignSupportConfig.java
@@ -4,19 +4,13 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.web.client.RestTemplate;
 
 import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 
-@Configuration
 public class FeignSupportConfig {
     @Bean
-    @Primary
-    @Scope("prototype")
     public Encoder multipartFormEncoder() {
         return new SpringFormEncoder(new SpringEncoder(new ObjectFactory<HttpMessageConverters>() {
             @Override
