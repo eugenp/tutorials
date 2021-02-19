e2e416552384ccf115eacacab8159583e0240096
Ali Dehghani <ali.dehghani.g@gmail.com>
2021-02-19 17:06:57 +0330

    Fixed a typo

--- core-java-modules/core-java-annotations/src/main/java/com/baeldung/annotations/AnnotatedClass.java
+++ core-java-modules/core-java-annotations/src/main/java/com/baeldung/annotations/AnnotatedClass.java
@@ -3,6 +3,6 @@ package com.baeldung.annotations;
 import javax.annotation.Generated;
 
 @RetentionAnnotation
-@Generated("Avilable only on source code")
+@Generated("Available only on source code")
 public class AnnotatedClass {
 }
