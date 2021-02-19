ed326fd521adbd3b67a71d563cfed0758f2a3952
philippesoares <psoares@tracfone.com>
2021-02-19 09:03:08 -0500

    Fixing PR comments.

--- docker/heap-sizing/src/main/java/com/baeldung/docker/heapsizing/PrintXmxXms.java
+++ docker/heap-sizing/src/main/java/com/baeldung/docker/heapsizing/PrintXmxXms.java
@@ -14,11 +14,16 @@ public class PrintXmxXms {
         logMemory(logger);
     }
 
+    /**
+     * We're reusing this method in HeapSizingApplication, therefore this method was extracted
+     * to avoid repetition.
+     */
     static void logMemory(Logger logger) {
         float mb = 1024f * 1024f;
         MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
 
-        // xmx controls the
+        // xmx controls the maximum size of the memory allocation pool,
+        // which includes the heap, the garbage collector's survivor space, and other pools.
         float xmx = memoryBean.getHeapMemoryUsage().getMax() / mb;
         float xms = memoryBean.getHeapMemoryUsage().getInit() / mb;
         logger.log(Level.INFO, "Initial Memory (xms) : {0}mb", xms);
