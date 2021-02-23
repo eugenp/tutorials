e0889531e31f44fefcf12ff5196b286f985f92ae
bhandy <brad.handy@gmail.com>
2021-02-22 22:13:56 -0500

    Forgot to make the test class and method package-private.

--- core-java-modules/core-java-reflection-2/src/test/java/com/baeldung/reflection/access/privatemethods/InvokePrivateMethodsUnitTest.java
+++ core-java-modules/core-java-reflection-2/src/test/java/com/baeldung/reflection/access/privatemethods/InvokePrivateMethodsUnitTest.java
@@ -6,16 +6,16 @@ import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class InvokePrivateMethodsUnitTest {
+class InvokePrivateMethodsUnitTest {
 
     private final long[] someLongArray = new long[] { 1L, 2L, 1L, 4L, 2L };
 
     @Test
-    public void whenSearchingForLongValueInSubsequence_theCorrectIndexOfTheValueIsReturned() throws Exception {
-        Method indexOfNonWhitespaceMethod = LongArrayUtil.class.getDeclaredMethod("indexOf", long[].class, long.class, int.class, int.class);
-        indexOfNonWhitespaceMethod.setAccessible(true);
+    void whenSearchingForLongValueInSubsequence_thenTheCorrectIndexOfTheValueIsReturned() throws Exception {
+        Method indexOfMethod = LongArrayUtil.class.getDeclaredMethod("indexOf", long[].class, long.class, int.class, int.class);
+        indexOfMethod.setAccessible(true);
 
-        assertEquals(2, indexOfNonWhitespaceMethod.invoke(LongArrayUtil.class, someLongArray, 1L, 1, someLongArray.length), "The index should be 2.");
+        assertEquals(2, indexOfMethod.invoke(LongArrayUtil.class, someLongArray, 1L, 1, someLongArray.length), "The index should be 2.");
     }
 
 }
