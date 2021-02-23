c8592130fb734e8cbc3ead9f9f9428a725f1d2a3
AbdallahSawan <abdallahsawan.e@gmail.com>
2021-02-23 15:56:18 +0200

    Usage of the Hibernate @LazyCollection Annotation Article by Abdallah Sawan

--- persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/lazycollection/model/Employee.java
+++ persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/lazycollection/model/Employee.java
@@ -4,7 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OrderColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Entity;
 
 @Entity
