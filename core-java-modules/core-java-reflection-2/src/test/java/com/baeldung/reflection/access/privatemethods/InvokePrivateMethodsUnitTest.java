5b61b7e56e15b61fb6df64c04afab6bee5d3d6fc
bhandy <brad.handy@gmail.com>
2021-02-22 22:09:26 -0500

    Made the code example more useful so we don't look like we're hacking another framework.

--- core-java-modules/core-java-reflection-2/src/test/java/com/baeldung/reflection/access/privatemethods/InvokePrivateMethodsUnitTest.java
+++ core-java-modules/core-java-reflection-2/src/test/java/com/baeldung/reflection/access/privatemethods/InvokePrivateMethodsUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.reflection.access.privatemethods;
 
-import com.google.common.primitives.Longs;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
@@ -9,14 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InvokePrivateMethodsUnitTest {
 
-    private final long[] someLongArray = new long[] { 1L, 2L, 3L, 4L, 5L };
+    private final long[] someLongArray = new long[] { 1L, 2L, 1L, 4L, 2L };
 
     @Test
-    public void whenSearchingForLongValue_theCorrectIndexOfTheValueIsReturned() throws Exception {
-        Method indexOfNonWhitespace = Longs.class.getDeclaredMethod("indexOf", long[].class, long.class, int.class, int.class);
-        indexOfNonWhitespace.setAccessible(true);
+    public void whenSearchingForLongValueInSubsequence_theCorrectIndexOfTheValueIsReturned() throws Exception {
+        Method indexOfNonWhitespaceMethod = LongArrayUtil.class.getDeclaredMethod("indexOf", long[].class, long.class, int.class, int.class);
+        indexOfNonWhitespaceMethod.setAccessible(true);
 
-        assertEquals(1, indexOfNonWhitespace.invoke(Longs.class, someLongArray, 2L, 0, someLongArray.length), "The index should be 1.");
+        assertEquals(2, indexOfNonWhitespaceMethod.invoke(LongArrayUtil.class, someLongArray, 1L, 1, someLongArray.length), "The index should be 2.");
     }
 
 }
