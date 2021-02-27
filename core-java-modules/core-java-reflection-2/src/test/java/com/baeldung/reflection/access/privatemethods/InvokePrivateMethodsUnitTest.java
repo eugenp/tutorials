9b7ec50d7f729b57c2b4533a8bbbb568194f4af5
bhandy <brad.handy@gmail.com>
2021-02-27 09:17:38 -0500

    Incorporated Spring ReflectionTestUtils into the code sample.

--- core-java-modules/core-java-reflection-2/src/test/java/com/baeldung/reflection/access/privatemethods/InvokePrivateMethodsUnitTest.java
+++ core-java-modules/core-java-reflection-2/src/test/java/com/baeldung/reflection/access/privatemethods/InvokePrivateMethodsUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.reflection.access.privatemethods;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.lang.reflect.Method;
 
@@ -11,11 +12,17 @@ class InvokePrivateMethodsUnitTest {
     private final long[] someLongArray = new long[] { 1L, 2L, 1L, 4L, 2L };
 
     @Test
-    void whenSearchingForLongValueInSubsequence_thenTheCorrectIndexOfTheValueIsReturned() throws Exception {
+    void whenSearchingForLongValueInSubsequenceUsingReflection_thenTheCorrectIndexOfTheValueIsReturned() throws Exception {
         Method indexOfMethod = LongArrayUtil.class.getDeclaredMethod("indexOf", long[].class, long.class, int.class, int.class);
         indexOfMethod.setAccessible(true);
 
         assertEquals(2, indexOfMethod.invoke(LongArrayUtil.class, someLongArray, 1L, 1, someLongArray.length), "The index should be 2.");
+    }
+
+    @Test
+    void whenSearchingForLongValueInSubsequenceUsingSpring_thenTheCorrectIndexOfTheValueIsReturned() throws Exception {
+        int indexOfSearchTarget = ReflectionTestUtils.invokeMethod(LongArrayUtil.class, "indexOf", someLongArray, 1L, 1, someLongArray.length);
+        assertEquals(2, indexOfSearchTarget, "The index should be 2.");
     }
 
 }
