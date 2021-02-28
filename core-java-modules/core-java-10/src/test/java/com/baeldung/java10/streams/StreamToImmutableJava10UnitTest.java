0af0fadd2dc862325ede2ca10598c69e9261471c
mdhtr <mdhtr@users.noreply.github.com>
2021-02-28 14:15:55 +0100

    Add java10 examples in the style of the existing examples
    The existing examples in core-java-streams-2 module still had system out in them instead of assertions, so I kept that style to keep the article consistent.

---
+++ core-java-modules/core-java-10/src/test/java/com/baeldung/java10/streams/StreamToImmutableJava10UnitTest.java
@@ -0,0 +1,32 @@
+package com.baeldung.java10.streams;
+
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.Collectors.toUnmodifiableList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+public class StreamToImmutableJava10UnitTest {
+
+    @Test
+    public void whenUsingCollectorsToUnmodifiableList_thenSuccess() {
+        List<String> givenList = Arrays.asList("a", "b", "c");
+        List<String> result = givenList.stream()
+            .collect(toUnmodifiableList());
+
+        System.out.println(result.getClass());
+    }
+
+    @Test
+    public void whenUsingCollectingToJavaListCopyOf_thenSuccess() {
+        List<String> givenList = new ArrayList<>(Arrays.asList("a", "b", "c"));
+        List<String> result = givenList.stream()
+            .collect(collectingAndThen(toList(), List::copyOf));
+
+        System.out.println(result.getClass());
+    }
+}
