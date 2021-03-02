47044bfd0d59977391b7ca5cff241ad0644938da
Abdoul Karim SAVADOGO <savadogoak@gmail.com>
2021-03-02 11:28:01 +0000

    Update that ignore DataSourceAutoConfiguration.class

--- hexagonal/src/main/java/com/baeldung/hexagonal/HexagonalApplication.java
+++ hexagonal/src/main/java/com/baeldung/hexagonal/HexagonalApplication.java
@@ -2,8 +2,9 @@ package com.baeldung.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class HexagonalApplication {
 
     public static void main(String[] args) {
