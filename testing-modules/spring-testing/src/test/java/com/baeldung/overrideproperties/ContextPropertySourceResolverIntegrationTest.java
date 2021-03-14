33e05c80f4f3dd47f3b56de681a8736055995749
mikr <michael.krimgen@ximedes.com>
2021-03-14 16:15:23 +0100

    JAVA-4278 Update "Override Properties" article

--- testing-modules/spring-testing/src/test/java/com/baeldung/overrideproperties/ContextPropertySourceResolverIntegrationTest.java
+++ testing-modules/spring-testing/src/test/java/com/baeldung/overrideproperties/ContextPropertySourceResolverIntegrationTest.java
@@ -1,15 +1,14 @@
 package com.baeldung.overrideproperties;
 
 import com.baeldung.overrideproperties.resolver.PropertySourceResolver;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.Assert.assertEquals;
-
-@RunWith(SpringRunner.class)
+import static org.junit.jupiter.api.Assertions.assertEquals;
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(initializers = PropertyOverrideContextInitializer.class, classes = Application.class)
 public class ContextPropertySourceResolverIntegrationTest {
 
