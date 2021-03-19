7f879019cf0a9b38c32414c5bffaa5c118a99023
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-03-19 10:55:15 +0100

    Update Command API
    - Adjust PlaceOrderCommand to only construct the order
    - Introduce AddProductCommand to be able to add several products to an
    order
    - Introduce IncrementProductCountCommand to increase the number of
    product instances for a given Order
    - Introduce DecrementProductCountCommand to increase the number of
    product instances for a given Order
    
    #BAEL-4767

--- axon/src/main/java/com/baeldung/axon/coreapi/commands/AddProductCommand.java
+++ axon/src/main/java/com/baeldung/axon/coreapi/commands/AddProductCommand.java
@@ -4,17 +4,23 @@ import org.axonframework.modelling.command.TargetAggregateIdentifier;
 
 import java.util.Objects;
 
-public class PlaceOrderCommand {
+public class AddProductCommand {
 
     @TargetAggregateIdentifier
     private final String orderId;
+    private final String productId;
 
-    public PlaceOrderCommand(String orderId) {
+    public AddProductCommand(String orderId, String productId) {
         this.orderId = orderId;
+        this.productId = productId;
     }
 
     public String getOrderId() {
         return orderId;
+    }
+
+    public String getProductId() {
+        return productId;
     }
 
     @Override
@@ -25,19 +31,20 @@ public class PlaceOrderCommand {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        PlaceOrderCommand that = (PlaceOrderCommand) o;
-        return Objects.equals(orderId, that.orderId);
+        AddProductCommand that = (AddProductCommand) o;
+        return Objects.equals(orderId, that.orderId) && Objects.equals(productId, that.productId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(orderId);
+        return Objects.hash(orderId, productId);
     }
 
     @Override
     public String toString() {
-        return "PlaceOrderCommand{" +
+        return "AddProductCommand{" +
                 "orderId='" + orderId + '\'' +
+                ", productId='" + productId + '\'' +
                 '}';
     }
 }
