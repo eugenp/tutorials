86d75ea3945031d36ad02e0d538fad3c49d6e6c4
Sallo Szrajbman <salloszrajbman@hotmail.com>
2021-04-03 13:51:08 +0100

    BAEL-4842 - Use React and Spring Boot to Build a Simple CRUD App Updating PR to reduce complexity of react components Fixing react test

--- spring-boot-modules/spring-boot-react/frontend/src/App.test.js
+++ spring-boot-modules/spring-boot-react/frontend/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Clients/i);
   expect(linkElement).toBeInTheDocument();
 });
