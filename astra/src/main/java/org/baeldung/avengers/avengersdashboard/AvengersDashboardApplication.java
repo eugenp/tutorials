ac372d811f4d6add0f66de7e93364d7d82afe5bf
Graham Cox <graham@grahamcox.co.uk>
2021-04-07 07:26:47 +0100

    Import from Spring Start

---
+++ astra/src/main/java/org/baeldung/avengers/avengersdashboard/AvengersDashboardApplication.java
@@ -0,0 +1,13 @@
+package org.baeldung.avengers.avengersdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersDashboardApplication.class, args);
+	}
+
+}
---
+++ astra/src/test/java/org/baeldung/avengers/avengersdashboard/AvengersDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baeldung.avengers.avengersdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
