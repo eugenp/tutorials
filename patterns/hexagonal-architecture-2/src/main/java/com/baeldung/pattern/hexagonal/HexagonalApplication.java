35d2679756420d68744e10a901d5b29496979fae
Jacek Polom <polomos@gmail.com>
2021-04-14 20:54:12 +0200

    Remove unnecessary ComponentScan annoatation

--- patterns/hexagonal-architecture-2/src/main/java/com/baeldung/pattern/hexagonal/HexagonalApplication.java
+++ patterns/hexagonal-architecture-2/src/main/java/com/baeldung/pattern/hexagonal/HexagonalApplication.java
@@ -2,13 +2,10 @@ package com.baeldung.pattern.hexagonal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.baeldung.pattern.hexagonal")
 public class HexagonalApplication {
     public static void main(String[] args) {
         SpringApplication.run(HexagonalApplication.class, args);
     }
-
 }
