312f44eb53ed864652f150e3dca1fbed756ef9b8
Haroon Khan <haroonkhan79@gmail.com>
2021-04-20 15:50:21 +0100

    [BAEL-4096] Integration test cleanup

--- spring-5-webflux/src/test/java/com/baeldung/spring/retry/ExternalConnectorIntegrationTest.java
+++ spring-5-webflux/src/test/java/com/baeldung/spring/retry/ExternalConnectorIntegrationTest.java
@@ -6,15 +6,11 @@ import okhttp3.mockwebserver.RecordedRequest;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.test.StepVerifier;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.util.stream.IntStream;
 
 import static io.netty.handler.codec.http.HttpResponseStatus.SERVICE_UNAVAILABLE;
 import static io.netty.handler.codec.http.HttpResponseStatus.UNAUTHORIZED;
@@ -41,12 +37,16 @@ class ExternalConnectorIntegrationTest {
     }
 
     @Test
-    void givenExternalServiceReturnsError_whenGettingData_thenRetryAndReturnResponse() {
+    void givenExternalServiceReturnsError_whenGettingData_thenRetryAndReturnResponse() throws Exception {
 
-        mockExternalService.enqueue(new MockResponse().setResponseCode(SERVICE_UNAVAILABLE.code()));
-        mockExternalService.enqueue(new MockResponse().setResponseCode(SERVICE_UNAVAILABLE.code()));
-        mockExternalService.enqueue(new MockResponse().setResponseCode(SERVICE_UNAVAILABLE.code()));
-        mockExternalService.enqueue(new MockResponse().setBody("stock data"));
+        mockExternalService.enqueue(new MockResponse()
+                .setResponseCode(SERVICE_UNAVAILABLE.code()));
+        mockExternalService.enqueue(new MockResponse()
+                .setResponseCode(SERVICE_UNAVAILABLE.code()));
+        mockExternalService.enqueue(new MockResponse()
+                .setResponseCode(SERVICE_UNAVAILABLE.code()));
+        mockExternalService.enqueue(new MockResponse()
+                .setBody("stock data"));
 
         StepVerifier.create(externalConnector.getData("ABC"))
                 .expectNextMatches(response -> response.equals("stock data"))
@@ -56,7 +56,7 @@ class ExternalConnectorIntegrationTest {
     }
 
     @Test
-    void givenExternalServiceReturnsClientError_whenGettingData_thenNoRetry() {
+    void givenExternalServiceReturnsClientError_whenGettingData_thenNoRetry() throws Exception {
 
         mockExternalService.enqueue(new MockResponse().setResponseCode(UNAUTHORIZED.code()));
 
@@ -68,7 +68,7 @@ class ExternalConnectorIntegrationTest {
     }
 
     @Test
-    void givenExternalServiceRetryAttemptsExhausted_whenGettingData_thenRetryAndReturnError() {
+    void givenExternalServiceRetryAttemptsExhausted_whenGettingData_thenRetryAndReturnError() throws Exception {
 
         mockExternalService.enqueue(new MockResponse().setResponseCode(SERVICE_UNAVAILABLE.code()));
         mockExternalService.enqueue(new MockResponse().setResponseCode(SERVICE_UNAVAILABLE.code()));
@@ -82,19 +82,12 @@ class ExternalConnectorIntegrationTest {
         verifyNumberOfGetRequests(4);
     }
 
-    private void verifyNumberOfGetRequests(int times) {
-        IntStream.range(0, times).forEach(i -> {
-            RecordedRequest recordedRequest = getRecordedRequest();
+    private void verifyNumberOfGetRequests(int times) throws Exception {
+        for (int i = 0 ; i < times; i++) {
+            RecordedRequest recordedRequest = mockExternalService.takeRequest();
             assertThat(recordedRequest.getMethod()).isEqualTo("GET");
             assertThat(recordedRequest.getPath()).isEqualTo("/data/ABC");
-        });
-    }
-
-    private RecordedRequest getRecordedRequest() {
-        try {
-            return mockExternalService.takeRequest();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
+
 }
