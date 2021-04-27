af5c172e378579917e84685358c7adfe234b1d23
Krzysztof Woyke <krzysztof.woyke.sp@lhsystems.com>
2021-04-27 16:38:29 +0200

    BAEL-4948: Rename JsonSerializerUnitTest to the ObjectToJsonConverterUnitTest

--- core-java-modules/core-java-annotations/src/test/java/com/baeldung/customannotations/ObjectToJsonConverterUnitTest.java
+++ core-java-modules/core-java-annotations/src/test/java/com/baeldung/customannotations/ObjectToJsonConverterUnitTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class JsonSerializerUnitTest {
+public class ObjectToJsonConverterUnitTest {
 
     @Test
     public void givenObjectNotSerializedThenExceptionThrown() throws JsonSerializationException {
