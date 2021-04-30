30ebf26c96ab6ca4e53af3da2e39882153f2d13c
amdegregorio <amy.m.degregorio@gmail.com>
2021-04-30 18:45:41 -0400

    BAEL-4925

---
+++ core-java-modules/core-java-lang-4/src/test/java/com/baeldung/hash/PlayerUnitTest.java
@@ -0,0 +1,11 @@
+package com.baeldung.hash;
+
+import org.junit.Test;
+
+public class PlayerUnitTest {
+
+    @Test
+    public void whenCallingCodeOnIdenticalValue_thenSameHashCodeReturned() {
+        
+    }
+}
