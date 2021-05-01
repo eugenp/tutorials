ba24a6104fc13b89a3266818b8a743ab91621f65
akhil90s <46933461+akhil90s@users.noreply.github.com>
2021-05-01 20:16:22 +0530

    SonarQube And JaCoCo

--- testing-modules/sonarqubeandjacoco/src/test/java/com/baeldung/codecoverage/sonarqubeandjacoco/AppTest.java
+++ testing-modules/sonarqubeandjacoco/src/test/java/com/baeldung/codecoverage/sonarqubeandjacoco/AppTest.java
@@ -4,15 +4,12 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
- /**
-  * Rigorous Test :-)
-  */
- @Test
- public void shouldAnswerWithTrue() {
-  assertTrue(true);
- }
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
 }
--- testing-modules/sonarqubeandjacoco/src/test/java/com/baeldung/codecoverage/sonarqubeandjacoco/product/ProductTest.java
+++ testing-modules/sonarqubeandjacoco/src/test/java/com/baeldung/codecoverage/sonarqubeandjacoco/product/ProductTest.java
@@ -7,18 +7,18 @@ import org.junit.Test;
 
 public class ProductTest {
 
- @Test
- public void test() {
-  Product product = new Product();
-  product.setId(1);
-  assertNull(product.getName());
-  assert (product.getId() == 1);
- }
+    @Test
+    public void test() {
+        Product product = new Product();
+        product.setId(1);
+        assertNull(product.getName());
+        assert (product.getId() == 1);
+    }
 
- @Test
- public void testProduct() {
-  Product product = new Product(1, "product", 1, 2.0);
-  assertNotNull(product.getName());
- }
+    @Test
+    public void testProduct() {
+        Product product = new Product(1, "product", 1, 2.0);
+        assertNotNull(product.getName());
+    }
 
 }
