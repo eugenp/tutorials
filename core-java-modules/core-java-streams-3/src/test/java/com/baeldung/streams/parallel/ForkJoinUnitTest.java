4f350873c066d9cb7671e48dc0e1081747c43513
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-05-03 11:52:32 +0200

    BAEL-4876: Use Java 8 API

--- core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/parallel/ForkJoinUnitTest.java
+++ core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/parallel/ForkJoinUnitTest.java
@@ -2,6 +2,7 @@ package com.baeldung.streams.parallel;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
@@ -12,21 +13,21 @@ class ForkJoinUnitTest {
 
     @Test
     void givenSequentialStreamOfNumbers_whenReducingSumWithIdentityFive_thenResultIsCorrect() {
-        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4);
         int sum = listOfNumbers.stream().reduce(5, Integer::sum);
         assertThat(sum).isEqualTo(15);
     }
 
     @Test
     void givenParallelStreamOfNumbers_whenReducingSumWithIdentityFive_thenResultIsNotCorrect() {
-        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4);
         int sum = listOfNumbers.parallelStream().reduce(5, Integer::sum);
         assertThat(sum).isNotEqualTo(15);
     }
 
     @Test
     void givenParallelStreamOfNumbers_whenReducingSumWithIdentityZero_thenResultIsCorrect() {
-        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4);
         int sum = listOfNumbers.parallelStream().reduce(0, Integer::sum) + 5;
         assertThat(sum).isEqualTo(15);
     }
@@ -34,7 +35,7 @@ class ForkJoinUnitTest {
     @Test
     public void givenParallelStreamOfNumbers_whenUsingCustomThreadPool_thenResultIsCorrect()
       throws InterruptedException, ExecutionException {
-        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4);
         ForkJoinPool customThreadPool = new ForkJoinPool(4);
         int sum = customThreadPool.submit(
           () -> listOfNumbers.parallelStream().reduce(0, Integer::sum)).get();
