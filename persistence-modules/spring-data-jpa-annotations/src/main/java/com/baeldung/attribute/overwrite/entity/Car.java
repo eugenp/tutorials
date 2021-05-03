0586a4110218d9e4134567aa4a2100aad24a64ce
Jacek Polom <polomos@gmail.com>
2021-05-03 12:08:57 +0200

    BAEL-4900 Code formatting fixed

--- persistence-modules/spring-data-jpa-annotations/src/main/java/com/baeldung/attribute/overwrite/entity/Car.java
+++ persistence-modules/spring-data-jpa-annotations/src/main/java/com/baeldung/attribute/overwrite/entity/Car.java
@@ -1,8 +1,8 @@
 package com.baeldung.attribute.overwrite.entity;
 
+import java.util.Map;
 
 import javax.persistence.*;
-import java.util.Map;
 
 @Entity
 @AttributeOverride(name = "identifier", column = @Column(name = "VIN"))
@@ -12,18 +12,17 @@ public class Car extends Vehicle {
     private String name;
     @Embedded
     @AttributeOverrides({
-            @AttributeOverride(name = "name", column = @Column(name = "BRAND_NAME", length = 5)),
-            @AttributeOverride(name = "address.name", column = @Column(name = "ADDRESS_NAME"))
+      @AttributeOverride(name = "name", column = @Column(name = "BRAND_NAME", length = 5)),
+      @AttributeOverride(name = "address.name", column = @Column(name = "ADDRESS_NAME"))
     })
     private Brand brand;
     @ElementCollection
     @AttributeOverrides({
-            @AttributeOverride(name = "key.name", column = @Column(name = "OWNER_NAME")),
-            @AttributeOverride(name = "key.surname", column = @Column(name = "OWNER_SURNAME")),
-            @AttributeOverride(name = "value.name", column = @Column(name = "ADDRESS_NAME")),
+      @AttributeOverride(name = "key.name", column = @Column(name = "OWNER_NAME")),
+      @AttributeOverride(name = "key.surname", column = @Column(name = "OWNER_SURNAME")),
+      @AttributeOverride(name = "value.name", column = @Column(name = "ADDRESS_NAME")),
     })
     Map<Owner, Address> owners;
-
 
     public String getModel() {
         return model;
