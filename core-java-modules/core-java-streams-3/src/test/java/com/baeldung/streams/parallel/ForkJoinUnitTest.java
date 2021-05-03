d160a09b014bd2204c26bcaac8752784e9c87423
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-05-03 11:18:02 +0200

    BAEL-4876: Parallel stream examples added

---
+++ core-java-modules/core-java-streams-3/src/test/java/com/baeldung/streams/parallel/ForkJoinUnitTest.java
@@ -0,0 +1,45 @@
+package com.baeldung.streams.parallel;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ForkJoinUnitTest {
+
+    @Test
+    void givenSequentialStreamOfNumbers_whenReducingSumWithIdentityFive_thenResultIsCorrect() {
+        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        int sum = listOfNumbers.stream().reduce(5, Integer::sum);
+        assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    void givenParallelStreamOfNumbers_whenReducingSumWithIdentityFive_thenResultIsNotCorrect() {
+        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        int sum = listOfNumbers.parallelStream().reduce(5, Integer::sum);
+        assertThat(sum).isNotEqualTo(15);
+    }
+
+    @Test
+    void givenParallelStreamOfNumbers_whenReducingSumWithIdentityZero_thenResultIsCorrect() {
+        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        int sum = listOfNumbers.parallelStream().reduce(0, Integer::sum) + 5;
+        assertThat(sum).isEqualTo(15);
+    }
+
+    @Test
+    public void givenParallelStreamOfNumbers_whenUsingCustomThreadPool_thenResultIsCorrect()
+      throws InterruptedException, ExecutionException {
+        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        ForkJoinPool customThreadPool = new ForkJoinPool(4);
+        int sum = customThreadPool.submit(
+          () -> listOfNumbers.parallelStream().reduce(0, Integer::sum)).get();
+        customThreadPool.shutdown();
+        assertThat(sum).isEqualTo(10);
+    }
+
+}
