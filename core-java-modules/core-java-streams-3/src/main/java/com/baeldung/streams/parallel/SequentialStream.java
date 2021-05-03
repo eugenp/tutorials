b0ade9d58b6741362a0ce4676df7c1a2c1033922
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-05-03 11:52:32 +0200

    BAEL-4876: Use Java 8 API

--- core-java-modules/core-java-streams-3/src/main/java/com/baeldung/streams/parallel/SequentialStream.java
+++ core-java-modules/core-java-streams-3/src/main/java/com/baeldung/streams/parallel/SequentialStream.java
@@ -1,11 +1,12 @@
 package com.baeldung.streams.parallel;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SequentialStream {
 
     public static void main(String[] args) {
-        List<Integer> listOfNumbers = List.of(1, 2, 3, 4);
+        List<Integer> listOfNumbers = Arrays.asList(1, 2, 3, 4);
         listOfNumbers.stream().forEach(number ->
           System.out.println(number + " " + Thread.currentThread().getName())
         );
