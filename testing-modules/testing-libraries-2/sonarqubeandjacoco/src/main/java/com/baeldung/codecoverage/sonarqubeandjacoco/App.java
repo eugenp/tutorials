7bd88a153cec7700845c941dc2c71d41e2c98949
akhil90s <46933461+akhil90s@users.noreply.github.com>
2021-05-04 21:38:41 +0530

    BAEL-4636 : Code Coverage with SonarQube and Jacoco

---
+++ testing-modules/testing-libraries-2/sonarqubeandjacoco/src/main/java/com/baeldung/codecoverage/sonarqubeandjacoco/App.java
@@ -0,0 +1,7 @@
+package com.baeldung.codecoverage.sonarqubeandjacoco;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ testing-modules/testing-libraries-2/sonarqubeandjacoco/src/test/java/com/baeldung/codecoverage/sonarqubeandjacoco/AppTest.java
@@ -0,0 +1,15 @@
+package com.baeldung.codecoverage.sonarqubeandjacoco;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
