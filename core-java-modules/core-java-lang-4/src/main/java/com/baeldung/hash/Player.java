a10d361cfb8105de3ea53131c38f4d850f226f7f
amdegregorio <amy.m.degregorio@gmail.com>
2021-05-08 12:22:55 -0400

    BAEL-4925

--- core-java-modules/core-java-lang-4/src/main/java/com/baeldung/hash/Player.java
+++ core-java-modules/core-java-lang-4/src/main/java/com/baeldung/hash/Player.java
@@ -7,6 +7,16 @@ public class Player {
     private String lastName;
     private String position;
 
+    public Player() {
+
+    }
+
+    public Player(String firstName, String lastName, String position) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.position = position;
+    }
+
     public String getFirstName() {
         return firstName;
     }
@@ -41,31 +51,37 @@ public class Player {
         if (this == obj) {
             return true;
         }
-        
+
         if (obj == null) {
             return false;
         }
-        
+
         if (getClass() != obj.getClass()) {
             return false;
         }
-        
+
         Player other = (Player) obj;
-        
-        if (firstName == null && other.firstName != null) {
-            return false;
+
+        if (firstName == null) {
+            if (other.firstName != null) {
+                return false;
+            }
         } else if (!firstName.equals(other.firstName)) {
             return false;
         }
-        
-        if (lastName == null && other.lastName != null) {
-            return false;
+
+        if (lastName == null) {
+            if (other.lastName != null) {
+                return false;
+            }
         } else if (!lastName.equals(other.lastName)) {
             return false;
         }
-        
-        if (position == null && other.position != null) {
-            return false;
+
+        if (position == null) {
+            if (other.position != null) {
+                return false;
+            }
         } else if (!position.equals(other.position)) {
             return false;
         }
