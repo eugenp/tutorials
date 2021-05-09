86c36061c98ec35d012f35c8a1612b61d96c1fd7
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-05-09 11:41:35 +0200

    BAEL-4876: Add more benchmarks

--- core-java-modules/core-java-streams-3/src/main/java/com/baeldung/streams/parallel/MergingCosts.java
+++ core-java-modules/core-java-streams-3/src/main/java/com/baeldung/streams/parallel/MergingCosts.java
@@ -6,49 +6,47 @@ import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class DifferentSourceSplitting {
+public class MergingCosts {
 
     private static final List<Integer> arrayListOfNumbers = new ArrayList<>();
-    private static final List<Integer> linkedListOfNumbers = new LinkedList<>();
 
     static {
         IntStream.rangeClosed(1, 100_000).forEach(i -> {
             arrayListOfNumbers.add(i);
-            linkedListOfNumbers.add(i);
         });
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public static void differentSourceArrayListSequential() {
+    public static void mergingCostsSumSequential() {
         arrayListOfNumbers.stream().reduce(0, Integer::sum);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public static void differentSourceArrayListParallel() {
-        arrayListOfNumbers.parallelStream().reduce(0, Integer::sum);
+    public static void mergingCostsSumParallel() {
+        arrayListOfNumbers.stream().parallel().reduce(0, Integer::sum);
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public static void differentSourceLinkedListSequential() {
-        linkedListOfNumbers.stream().reduce(0, Integer::sum);
+    public static void mergingCostsGroupingSequential() {
+        arrayListOfNumbers.stream().collect(Collectors.groupingBy(i -> i % 2 == 0));
     }
 
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     @OutputTimeUnit(TimeUnit.NANOSECONDS)
-    public static void differentSourceLinkedListParallel() {
-        linkedListOfNumbers.parallelStream().reduce(0, Integer::sum);
+    public static void mergingCostsGroupingParallel() {
+        arrayListOfNumbers.stream().parallel().collect(Collectors.groupingBy(i -> i % 2 == 0));
     }
 
 }
