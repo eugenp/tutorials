bec30e1fef73571c7f5b0ab20122189b0937241e
Thomas Duxbury <thomas.duxbury90@gmail.com>
2021-05-12 22:40:06 +0100

    BAEL-4801: Copying files with Maven

---
+++ maven-modules/maven-copy-files/copy-rename-maven-plugin/src/main/java/org/baeldung/App.java
@@ -0,0 +1,13 @@
+package org.baeldung;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
