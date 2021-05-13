1c11400bf7131194775fcd1535c77d8f1965e326
Liam Garvie <liamgarvie@Liams-MacBook-Pro.local>
2021-05-13 20:48:03 +0100

    BAEL-4946 added in code for java deserialization vulnerabilities article

---
+++ core-java-modules/core-java/src/test/java/com/baeldung/deserialization/vulnerabilities/BadThingTest.java
@@ -0,0 +1,38 @@
+package com.baeldung.deserialization.vulnerabilities;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class BadThingTest {
+
+    @Test
+    public void testCodeExecution() throws Exception {
+        BadThing bt = new BadThing();
+
+        bt.looselyDefinedThing = new MyCustomAttackObject();
+        bt.methodName = "methodThatTriggersAttack";
+
+        byte[] serializedObject = serialize(bt);
+
+        try (InputStream bis = new ByteArrayInputStream(serializedObject);
+             ObjectInputStream ois = new ObjectInputStream(bis)) {
+
+            ois.readObject(); // malicious code is run
+        }
+    }
+
+    private static byte[] serialize(Object object) throws Exception {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+
+            oos.writeObject(object);
+            oos.flush();
+            return bos.toByteArray();
+        }
+    }
+}
