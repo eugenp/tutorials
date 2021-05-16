4d08dcace0982ab0747b1a5937a932dd08ee320a
majewsk6 <krzysztof.majewski.km1@contractors.roche.com>
2021-05-16 20:16:33 +0200

    BAEL-4871 Spring Conditional Annotations

--- spring-boot-modules/spring-boot-annotations/src/main/java/com/baeldung/annotations/conditional/Java8Condition.java
+++ spring-boot-modules/spring-boot-annotations/src/main/java/com/baeldung/annotations/conditional/Java8Condition.java
@@ -1,6 +1,6 @@
-package java.com.baeldung.annotations.conditional;
+package com.baeldung.annotations.conditional;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnJava;
+import org.springframework.boot.system.JavaVersion;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -9,6 +9,6 @@ public class Java8Condition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        return ConditionalOnJava.JavaVersion.getJavaVersion().equals(ConditionalOnJava.JavaVersion.EIGHT);
+        return JavaVersion.getJavaVersion().equals(JavaVersion.EIGHT);
     }
 }
