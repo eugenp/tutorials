748d2022d8e82fadf0e3f5c68f048b4de85adc25
Karthick S <karthick.sridhar.1998@gmail.com>
2021-05-23 21:08:26 +0530

    BAEL-4254  JavaDoc method reference example classes

---
+++ core-java-modules/core-java-15/src/main/java/com/baeldung/javadocmemberreference/Person.java
@@ -0,0 +1,48 @@
+package com.baeldung.javadocmemberreference;
+
+import com.baeldung.sealed.records.Car;
+
+public class Person {
+    Person() {
+
+    }
+
+    /**
+     * Also, check the {@link #move() Move} method for more movement details.
+     */
+    public void walk() {
+
+    }
+
+    /**
+     * Check this {@link #move(String) Move} method for direction oriented movement.
+     */
+    public void move() {
+
+    }
+
+    public void move(String direction) {
+
+    }
+
+    /**
+     * Additionally, check this {@link Animal#run(String) Run} method for direction based run.
+     */
+    public void run() {
+
+    }
+
+    /**
+     * Also consider checking {@link com.baeldung.sealed.classes.Vehicle#Vehicle()} constructor to initialize vehicle object.
+     */
+    public void goToWork() {
+
+    }
+
+    /**
+     * Have a look at {@link Car#getNumberOfSeats() SeatsAvailability} method for checking the available seats needed for driving.
+     */
+    public void drive() {
+
+    }
+}
