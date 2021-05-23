27a79748cfc3044afacdd5605961d1b17fb48d09
Vikas Ramsingh Rajput <vikas.rajput@crownconsult.com>
2021-05-23 16:14:00 +0530

    BAEL-4936: Removed similar methods as per review comments

--- core-java-modules/core-java-8-2/src/test/java/com/baeldung/interfaceVsAbstractClass/InterfaceVsAbstractClassUnitTest.java
+++ core-java-modules/core-java-8-2/src/test/java/com/baeldung/interfaceVsAbstractClass/InterfaceVsAbstractClassUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.interfaceVsAbstractClass;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -14,23 +13,9 @@ public class InterfaceVsAbstractClassUnitTest {
     }
 
     @Test
-    public void givenAbstractClass_whenInvalidCircleUsedThenFail() {
-        CircleClass redCircle = new ChildCircleClass();
-        redCircle.setColor("WHITE");
-        assertFalse(redCircle.isValid());
-    }
-
-    @Test
     public void givenInterface_whenValidCircleWithoutStateUsedThenPass() {
         ChidlCircleInterfaceImpl redCircleWithoutState = new ChidlCircleInterfaceImpl();
         redCircleWithoutState.setColor("RED");
         assertTrue(redCircleWithoutState.isValid(redCircleWithoutState.getColor()));
-    }
-
-    @Test
-    public void givenInterface_whenInvalidCircleWithoutStateUsedThenFail() {
-        ChidlCircleInterfaceImpl redCircleWithoutState = new ChidlCircleInterfaceImpl();
-        redCircleWithoutState.setColor("WHITE");
-        assertFalse(redCircleWithoutState.isValid(redCircleWithoutState.getColor()));
     }
 }
