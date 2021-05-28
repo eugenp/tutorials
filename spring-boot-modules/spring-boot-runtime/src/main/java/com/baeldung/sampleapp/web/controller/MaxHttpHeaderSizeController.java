eefc0b2bc7dd31a53c9f47a36acf73e883f2c9aa
Bhabani Prasad Patel <patelbhabani@gmail.com>
2021-05-29 02:08:15 +0530

    code commit for BAEL-4869 (#10812)
    * Code commit for "Converting String to BigDecimal in Java" - Article
    
    * modified the assert param for comparing actual and expected values
    
    * removed the conflict change
    
    * Code commit for Secret Key to String and vice versa in java
    
    * renaming the junit class name to match coding standard
    
    * code commit for BAEL-4869
    
    * Adding new module with Secret key to String conversion in Java [BAEL-4489]
    
    * changed code indent by space instead of tab

---
+++ spring-boot-modules/spring-boot-runtime/src/main/java/com/baeldung/sampleapp/web/controller/MaxHttpHeaderSizeController.java
@@ -0,0 +1,17 @@
+package com.baeldung.sampleapp.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/request-header-test")
+public class MaxHttpHeaderSizeController {
+
+    @GetMapping
+    public boolean testMaxHTTPHeaderSize(@RequestHeader(value = "token") String token) {
+        return true;
+    }
+
+}
---
+++ spring-boot-modules/spring-boot-runtime/src/test/java/com/baeldung/web/controller/MaxHttpHeaderSizeControllerIntegrationTest.java
@@ -0,0 +1,48 @@
+package com.baeldung.web.controller;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.baeldung.sampleapp.config.WebConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebConfig.class)
+@WebAppConfiguration
+public class MaxHttpHeaderSizeControllerIntegrationTest {
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void givenTokenWithLessThan8KBLegth_whenSendGetRequest_thenReturnsOK() throws Exception {
+        mockMvc.perform(get("/request-header-test").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .with(httpBasic("user", "password")).header("token", "token")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenTokenIsMissingInHeade_whenSendGetRequest_thenThrowsBadRequest() throws Exception {
+        mockMvc.perform(get("/request-header-test").contentType(MediaType.APPLICATION_JSON_VALUE)
+                .with(httpBasic("user", "password"))).andExpect(status().isBadRequest());
+    }
+
+}
