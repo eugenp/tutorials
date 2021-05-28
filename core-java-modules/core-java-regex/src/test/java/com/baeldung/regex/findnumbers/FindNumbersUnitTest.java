24c3e05f7d259a9a99167357f84d75f7dd022a8a
gjohnson <gjohnson@192.168.1.64>
2021-05-28 21:19:10 +0100

    BAEL-4875 - Changed assertThat.contains(...) to containsExactly(...)
    Changed all instances of assertThat.contains(...) to
    containsExactly(...) in FindNumbersUnitTest

--- core-java-modules/core-java-regex/src/test/java/com/baeldung/regex/findnumbers/FindNumbersUnitTest.java
+++ core-java-modules/core-java-regex/src/test/java/com/baeldung/regex/findnumbers/FindNumbersUnitTest.java
@@ -44,42 +44,42 @@ class FindNumbersUnitTest {
     void givenStrOfAllDigits_whenRegexMatchByInt_thenWholeStrMatchedAsOneInt() {     
         List<String> integersFound = findIntegers("970987678607608");
 
-        assertThat(integersFound).contains("970987678607608");
+        assertThat(integersFound).containsExactly("970987678607608");
     }
 
     @Test
     void givenStrWithIntegersSepByPeriods_whenRegexMatchByInt_thenExpectedIntsFound() {
         List<String> integersFound = findIntegers("3453..5.-23532...32432.-2363.3454......345.-34.");
 
-        assertThat(integersFound).contains("3453", "5", "-23532", "32432", "-2363", "3454", "345", "-34");
+        assertThat(integersFound).containsExactly("3453", "5", "-23532", "32432", "-2363", "3454", "345", "-34");
     }
 
     @Test
     void givenStrWithIntegersSepByNonDigits_whenRegexMatchByInt_thenExpectedIntsFound() {
         List<String> integersFound = findIntegers("646lkl~4-53l-k34.fdsf.-ds-35.45f9wg3868*()(k;-95786fsd79-86");
 
-        assertThat(integersFound).contains("646", "4", "-53", "34", "-35", "45", "9", "3868", "-95786", "79", "-86");
+        assertThat(integersFound).containsExactly("646", "4", "-53", "34", "-35", "45", "9", "3868", "-95786", "79", "-86");
     }
     
     @Test
     void givenStrOfAllDigits_whenRegexMatchByDecNum_thenWholeStrMatchedAsOneDecimalNumber() {
         List<String> decimalNumsFound = findDecimalNums("970987678607608");
         
-        assertThat(decimalNumsFound).contains("970987678607608");
+        assertThat(decimalNumsFound).containsExactly("970987678607608");
     }
     
     @Test
     void givenStrOfDecNumsSepByNonDigits_whenRegexMatchByDecNum_thenExpectedNumsFound() {
         List<String> decimalNumsFound = findDecimalNums(".7854.455wo.rdy(do.g)-3.-553.00.53;good^night%o3456sdcardR%3567.4%£cat");
         
-        assertThat(decimalNumsFound).contains("7854.455", "-3", "-553.00", "53", "3456", "3567.4");
+        assertThat(decimalNumsFound).containsExactly("7854.455", "-3", "-553.00", "53", "3456", "3567.4");
     }
     
     @Test
     void givenStrWithRandomDigitsDashesAndPeriods_whenRegexMatchByDecNum_thenExpectedNumsFound() {
         List<String> decimalNumsFound = findDecimalNums(".-..90834.345.--493-..-85.-875.345-.-.-355.345...345.-.636-5.6-3.");
         
-        assertThat(decimalNumsFound).contains("90834.345", "-493", "-85", "-875.345", "-355.345", "345", "636", "-5.6", "-3");
+        assertThat(decimalNumsFound).containsExactly("90834.345", "-493", "-85", "-875.345", "-355.345", "345", "636", "-5.6", "-3");
     }
     
     @Test
@@ -87,7 +87,7 @@ class FindNumbersUnitTest {
         LongStream integerValuesFound = findIntegers(".7854.455wo.rdy(do.g)-3.ght%o34.56")
           .stream().mapToLong(Long::valueOf);
         
-        assertThat(integerValuesFound).contains(7854L, 455L, -3L, 34L, 56L);
+        assertThat(integerValuesFound).containsExactly(7854L, 455L, -3L, 34L, 56L);
     }    
     
     @Test
@@ -95,7 +95,7 @@ class FindNumbersUnitTest {
         DoubleStream decimalNumValuesFound = findDecimalNums(".7854.455wo.rdy(do.g)-3.ght%o34.56")
           .stream().mapToDouble(Double::valueOf);
         
-        assertThat(decimalNumValuesFound).contains(7854.455, -3.0, 34.56);
+        assertThat(decimalNumValuesFound).containsExactly(7854.455, -3.0, 34.56);
     }
     
     @Test
@@ -109,7 +109,7 @@ class FindNumbersUnitTest {
             sciNotationNums.add(matcher.group());
         }
         
-        assertThat(sciNotationNums).contains("1.25E-3", "2e109", "-70.96E+105", "-8.7312E-102", "919.3822e+31");
+        assertThat(sciNotationNums).containsExactly("1.25E-3", "2e109", "-70.96E+105", "-8.7312E-102", "919.3822e+31");
     }
     
     @Test
@@ -123,6 +123,6 @@ class FindNumbersUnitTest {
             hexNums.add(matcher.group());
         }
         
-        assertThat(hexNums).contains("aF851B", "-3f6C", "-2Ad9eE", "70ae19");
+        assertThat(hexNums).containsExactly("aF851B", "-3f6C", "-2Ad9eE", "70ae19");
     }
 }
