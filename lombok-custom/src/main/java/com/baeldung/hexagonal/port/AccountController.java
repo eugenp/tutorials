2ad7612b0c3d20a65ccd45a1674ea83c60806f3d
Vimit Dhawan <Vimit.Dhawan@agoda.com>
2021-05-31 00:00:06 +0700

    remove duplicate

--- lombok-custom/src/main/java/com/baeldung/hexagonal/port/AccountController.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.hexagonal.port;
-
-import com.baeldung.hexagonal.domain.Account;
-
-public interface AccountController {
-    void createAccount(Account account);
-}
--- lombok-custom/src/main/java/com/baeldung/hexagonal/port/AccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.hexagonal.port;
-
-import com.baeldung.hexagonal.domain.Account;
-
-public interface AccountRepository {
-    void createAccount(Account account);
-}
