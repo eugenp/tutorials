c51cbe11c62974b528c67c0b8aacffc673fc3a22
sunainag <sunaina89.goyal@gmail.com>
2021-05-31 21:41:58 +0530

    formatting

--- patterns/enterprise-patterns/wire-tap/src/main/java/com/baeldung/AmqApplication.java
+++ patterns/enterprise-patterns/wire-tap/src/main/java/com/baeldung/AmqApplication.java
@@ -13,58 +13,59 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AmqApplication {
 
-	public static void main(String[] args) throws Exception {
-		SpringApplication.run(AmqApplication.class, args);
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AmqApplication.class, args);
 
-		try (CamelContext context = new DefaultCamelContext()) {
-			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(
-					"vm://localhost?broker.persistent=false");
-			connectionFactory.setTrustAllPackages(true);
-			context.addComponent("direct", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-			addRoute(context);
+        try (CamelContext context = new DefaultCamelContext()) {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+            connectionFactory.setTrustAllPackages(true);
+            context.addComponent("direct", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+            addRoute(context);
 
-			try (ProducerTemplate template = context.createProducerTemplate()) {
-				context.start();
+            try (ProducerTemplate template = context.createProducerTemplate()) {
+                context.start();
 
-				MyPayload payload = new MyPayload("One");
-				template.sendBody("direct:source", payload);
-				Thread.sleep(10000);
-			} finally {
-				context.stop();
-			}
-		}
-	}
+                MyPayload payload = new MyPayload("One");
+                template.sendBody("direct:source", payload);
+                Thread.sleep(10000);
+            } finally {
+                context.stop();
+            }
+        }
+    }
 
-	private static void addRoute(CamelContext context) throws Exception {
-		context.addRoutes(newExchangeRoute());
-	}
+    private static void addRoute(CamelContext context) throws Exception {
+        context.addRoutes(newExchangeRoute());
+    }
 
-	static RoutesBuilder traditionalWireTapRoute() {
-		return new RouteBuilder() {
-			public void configure() {
+    static RoutesBuilder traditionalWireTapRoute() {
+        return new RouteBuilder() {
+            public void configure() {
 
-				from("direct:source").log("Main route: Send '${body}' to tap router").wireTap("direct:tap").delay(1000)
-						.log("Main route: Add 'two' to '${body}'").bean(MyBean.class, "addTwo").to("direct:destination")
-						.log("Main route: Output '${body}'");
+                from("direct:source").wireTap("direct:tap")
+                    .delay(1000)
+                    .bean(MyBean.class, "addTwo")
+                    .to("direct:destination");
 
-				from("direct:tap").log("Tap Wire route: received '${body}'")
-						.log("Tap Wire route: Add 'three' to '${body}'").bean(MyBean.class, "addThree")
-						.log("Tap Wire route: Output '${body}'");
+                from("direct:tap").log("Tap Wire route: received");
 
-				from("direct:destination").log("Output at destination: '${body}'");
-			}
-		};
-	}
+                from("direct:destination").log("Output at destination: '${body}'");
+            }
+        };
+    }
 
-	static RoutesBuilder newExchangeRoute() throws Exception {
-		return new RouteBuilder() {
-			public void configure() throws Exception {
+    static RoutesBuilder newExchangeRoute() throws Exception {
+        return new RouteBuilder() {
+            public void configure() throws Exception {
 
-				from("direct:source").wireTap("direct:tap").onPrepare(new MyPayloadClonePrepare()).end().delay(1000);
+                from("direct:source").wireTap("direct:tap")
+                    .onPrepare(new MyPayloadClonePrepare())
+                    .end()
+                    .delay(1000);
 
-				from("direct:tap").bean(MyBean.class, "addThree");
-			}
-		};
-	}
+                from("direct:tap").bean(MyBean.class, "addThree");
+            }
+        };
+    }
 
 }
