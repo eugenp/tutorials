236820291517806273efc7a4ff7358033e98961d
Philippe <phil@lighthouse.com.br>
2021-05-31 20:39:10 -0300

    [BAEL-4968] Article code

--- kubernetes/k8s-intro/src/main/java/com/baeldung/kubernetes/intro/RunJob.java
+++ kubernetes/k8s-intro/src/main/java/com/baeldung/kubernetes/intro/RunJob.java
@@ -3,22 +3,28 @@
  */
 package com.baeldung.kubernetes.intro;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.kubernetes.client.custom.V1Patch;
+import io.kubernetes.client.custom.V1Patch.V1PatchAdapter;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.apis.BatchV1Api;
 import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1DeleteOptions;
+import io.kubernetes.client.openapi.models.V1DeleteOptionsBuilder;
 import io.kubernetes.client.openapi.models.V1Job;
 import io.kubernetes.client.openapi.models.V1JobBuilder;
 import io.kubernetes.client.openapi.models.V1JobSpec;
 import io.kubernetes.client.openapi.models.V1JobSpecBuilder;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1ObjectMetaBuilder;
+import io.kubernetes.client.openapi.models.V1Status;
 import io.kubernetes.client.util.Config;
+import io.kubernetes.client.util.PatchUtils;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 
@@ -32,6 +38,7 @@ public class RunJob {
     
     public static void main(String[] args) throws Exception {
         
+        // Create client with logginginterceptor
         ApiClient client  = Config.defaultClient();
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(message -> log.info(message));
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -40,20 +47,23 @@ public class RunJob {
           .addInterceptor(interceptor)
           .readTimeout(0, TimeUnit.SECONDS)
           .build();
-        
         client.setHttpClient(newClient);
+        
+        // Create Job Spec
         BatchV1Api api = new BatchV1Api(client);
         String ns = "report-jobs";
         V1Job body = new V1JobBuilder()
           .withNewMetadata()
             .withNamespace(ns)
             .withName("payroll-report-job")
-            .and()
+            .endMetadata()
           .withNewSpec()
+            .withCompletions(2)
+            .withParallelism(1)
             .withNewTemplate()
               .withNewMetadata()
                 .addToLabels("name", "payroll-report")
-                .and()
+                .endMetadata()
               .editOrNewSpec()
                 .addNewContainer()
                   .withName("main")
@@ -61,20 +71,61 @@ public class RunJob {
                   .addNewCommand("/bin/sh")
                   .addNewArg("-c")
                   .addNewArg("sleep 10")
-                  .and()
+                  .endContainer()
                 .withRestartPolicy("Never")
-                .and()
-              .and()
-            .and()
+                .endSpec()
+              .endTemplate()
+            .endSpec()
           .build();
         
+        // Send to K8S 
         V1Job createdJob = api.createNamespacedJob(ns, body, null, null, null);
         log.info("job: uid={}", createdJob.getMetadata().getUid());
-
+        
+        // Let's change its parallelism value
+        V1Job patchedJob = new V1JobBuilder(createdJob)
+          .withNewMetadata()
+            .withName(createdJob.getMetadata().getName())
+            .withNamespace(createdJob.getMetadata().getNamespace())
+            .endMetadata()
+          .editSpec()
+            .withParallelism(2)
+          .endSpec()
+          .build();
+        
+        String patchedJobJSON = client.getJSON().serialize(patchedJob);
+        V1Patch patch = new V1Patch(patchedJobJSON);
+            
+        PatchUtils.patch(
+          V1Job.class, 
+          () -> api.patchNamespacedJobCall(
+            createdJob.getMetadata().getName(), 
+            createdJob.getMetadata().getNamespace(), 
+            patch, 
+            null, 
+            null, 
+            "acme", 
+            true, 
+            null),
+          V1Patch.PATCH_FORMAT_APPLY_YAML,
+          api.getApiClient());
+          
         while(!jobCompleted(api,createdJob)) {
             log.info("[I75] still running...");
             Thread.sleep(1000);
         }
+        
+        V1Status response = api.deleteNamespacedJob(
+          createdJob.getMetadata().getName(), 
+          createdJob.getMetadata().getNamespace(), 
+          null, 
+          null, 
+          null, 
+          null, 
+          null, 
+          null ) ;
+        
+        log.info("[I122] response={}", response);
     }
 
     private static boolean jobCompleted(BatchV1Api api, V1Job createdJob) throws Exception {
