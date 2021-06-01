20a329a603beb1e8bc6940fb362c4f30cf9354bd
uzma khan <uzma.khan@nominet.uk>
2021-06-01 11:16:59 +0100

    [BAEL-4406] Fix typo

--- maven-modules/maven-plugins/maven-jacoco/src/main/java/com/baeldung/generated/Customer.java
+++ maven-modules/maven-plugins/maven-jacoco/src/main/java/com/baeldung/generated/Customer.java
@@ -2,7 +2,7 @@ package com.baeldung.generated;
 
 @Generated
 public class Customer {
-    // every thing in this class will be excluded from jacoco report because of @Generated
+    // everything in this class will be excluded from jacoco report because of @Generated
 
     @Override
     public String toString() {
