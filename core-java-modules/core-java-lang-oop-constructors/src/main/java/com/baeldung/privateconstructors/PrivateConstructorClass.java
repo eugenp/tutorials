96a05c14601f2e422897fdb66b1901f14ffd43c0
Thomas Duxbury <thomas.duxbury90@gmail.com>
2021-06-01 19:41:51 +0100

    BAEL-4986: Private constructors in Java

---
+++ core-java-modules/core-java-lang-oop-constructors/src/main/java/com/baeldung/privateconstructors/PrivateConstructorClass.java
@@ -0,0 +1,8 @@
+package com.baeldung.privateconstructors;
+
+public class PrivateConstructorClass {
+
+    private PrivateConstructorClass() {
+        System.out.println("In the private constructor");
+    }
+}
