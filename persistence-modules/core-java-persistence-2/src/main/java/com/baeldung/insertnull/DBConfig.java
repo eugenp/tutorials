77ce7acb00ebe0aa05935fb894e0a1eb43c96ed0
Mladen Savic <mladensavic94@gmail.com>
2021-06-03 12:26:02 +0200

    insert null into Integer column using JDBC - code improvements

--- persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/insertnull/DBConfig.java
+++ persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/insertnull/DBConfig.java
@@ -9,22 +9,18 @@ public class DBConfig {
 
     private static Connection INSTANCE;
 
-    public static Connection getConnection() throws Exception {
+    public static Connection getConnection() throws SQLException {
         if (INSTANCE == null) {
-            Class.forName("org.h2.Driver");
             INSTANCE = DriverManager.getConnection("jdbc:h2:mem:insertnull", "user", "password");
             createPersonTable();
         }
         return INSTANCE;
     }
 
-    private static void createPersonTable() {
-        try {
-            Statement statement = INSTANCE.createStatement();
+    private static void createPersonTable() throws SQLException {
+        try(Statement statement = INSTANCE.createStatement()) {
             String sql = "CREATE TABLE Person (id INTEGER not null, name VARCHAR(50), lastName VARCHAR(50), age INTEGER, PRIMARY KEY (id))";
             statement.executeUpdate(sql);
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 }
