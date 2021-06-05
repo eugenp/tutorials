0cd0c87d8fcb0adbbbd0868ec41a118983e06c73
kaushal <kaushal.raj01@gmail.com>
2021-06-05 13:27:16 +0530

    BAEL-4126

--- reactor-core/src/test/java/com/baeldung/mono/MonoUnitTest.java
+++ reactor-core/src/test/java/com/baeldung/mono/MonoUnitTest.java
@@ -6,7 +6,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import java.awt.image.SampleModel;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
