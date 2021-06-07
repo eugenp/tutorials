08e659ded9e72b1f9ea11ab705c6745ddb3778a1
Arash Ariani <arash.sw90@gmail.com>
2021-06-07 08:03:31 +0430

    Update EmployeeRepositoryImpl.java
    query fixed

--- hexagonal/java-modules/infrastructure/src/main/java/com/baeldung/hexgonal/repo/EmployeeRepositoryImpl.java
+++ hexagonal/java-modules/infrastructure/src/main/java/com/baeldung/hexgonal/repo/EmployeeRepositoryImpl.java
@@ -12,12 +12,12 @@ import java.sql.Statement;
 @Slf4j
 public class EmployeeRepositoryImpl implements EmployeeRepository {
 
-    private static final String INSERT_BOOKS_SQL = "INSERT INTO employees" + " (id, firstName, lastName, code) VALUES " + " (?, ?, ?, ?);";
-    private static final String DELETE_BOOKS_SQL = "DELETE FROM employees WHERE id = '%s'";
+    private static final String INSERT_EMPLOYEE_SQL = "INSERT INTO employees" + " (id, firstName, lastName, code) VALUES " + " (?, ?, ?, ?);";
+    private static final String DELETE_EMPLOYEE_SQL = "DELETE FROM employees WHERE id = '%s'";
 
     @Override
     public void save(Employee employee) {
-        try (Connection connection = MySQLConnectionFactory.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_BOOKS_SQL)) {
+        try (Connection connection = MySQLConnectionFactory.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_EMPLOYEE_SQL)) {
             preparedStatement.setString(1, employee.getId()
                 .toString());
             preparedStatement.setString(2, employee.getFirstName());
@@ -36,7 +36,7 @@ public class EmployeeRepositoryImpl implements EmployeeRepository {
     @Override
     public void delete(Long id) {
         try (Connection connection = MySQLConnectionFactory.getConnection(); Statement statement = connection.createStatement();) {
-            statement.execute(String.format(DELETE_BOOKS_SQL, id));
+            statement.execute(String.format(DELETE_EMPLOYEE_SQL, id));
             log.debug("Employee {} deleted from database", id);
         } catch (SQLException e) {
             log.error("SQL exception during delete", e);
