0ad2b4d2f041f063b41e0aedf7533a28e1193d63
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2021-06-08 12:16:37 +0300

    rename project, format

--- spring-boot-modules/spring-boot-validation/src/main/java/com/baeldung/spring/servicevalidation/domain/UserAddress.java
+++ spring-boot-modules/spring-boot-validation/src/main/java/com/baeldung/spring/servicevalidation/domain/UserAddress.java
@@ -1,11 +1,12 @@
 package com.baeldung.spring.servicevalidation.domain;
 
 import javax.validation.constraints.NotBlank;
+
 public class UserAddress {
-    
+
     @NotBlank
     private String countryCode;
-    
+
     public String getCountryCode() {
         return countryCode;
     }
@@ -13,5 +14,5 @@ public class UserAddress {
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
     }
-   
+
 }
