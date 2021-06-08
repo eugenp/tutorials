c7326c23caa0f00fd46ffa0305a5923d637bcb9b
bhandy <brad.handy@gmail.com>
2021-06-07 22:41:29 -0400

    Updated the fluent API calls for the AssertJ assertions.

--- core-java-modules/core-java-regex/src/test/java/com/baeldung/regex/noncapturinggroups/NonCapturingGroupUnitTest.java
+++ core-java-modules/core-java-regex/src/test/java/com/baeldung/regex/noncapturinggroups/NonCapturingGroupUnitTest.java
@@ -21,115 +21,99 @@ public class NonCapturingGroupUnitTest {
     void givenSimpleUrlPattern_whenValidUrlProvided_thenMatches() {
         Matcher urlMatcher = SIMPLE_URL_PATTERN.matcher("http://www.microsoft.com/");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
-        Assertions.assertThatThrownBy(() -> urlMatcher.group(1))
-            .isInstanceOf(IndexOutOfBoundsException.class);
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
+        Assertions.assertThatThrownBy(() -> urlMatcher.group(1)).isInstanceOf(IndexOutOfBoundsException.class);
     }
 
     @Test
     void whenSimpleUrlProvidedWithPathProvided_thenMatches() {
         Matcher urlMatcher = SIMPLE_URL_PATTERN.matcher("http://www.microsoft.com/live");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
     }
 
     @Test
     void whenSimpleUrlProvidedWithPathEndingWithSlashProvided_thenMatches() {
         Matcher urlMatcher = SIMPLE_URL_PATTERN.matcher("http://www.microsoft.com/live/");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
     }
 
     @Test
     void givenSimpleUrlPattern_whenUrlWithMultiplePathSegmentsProvided_thenMatches() {
         Matcher urlMatcher = SIMPLE_URL_PATTERN.matcher("http://www.microsoft.com/some/other/url/path");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
     }
 
     @Test
     void whenUrlWithUppercaseCharactersProvided_thenDoesNotMatch() {
         Matcher urlMatcher = SIMPLE_URL_PATTERN.matcher("http://www.Microsoft.com/");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isFalse();
+        Assertions.assertThat(urlMatcher.matches()).isFalse();
     }
 
     @Test
     void givenPatternWithCaseInsensitiveGroup_whenUrlHasUppercaseCharactersInsideOfScope_thenMatches() {
         Matcher urlMatcher = SCOPED_CASE_INSENSITIVE_URL_PATTERN.matcher("http://www.Microsoft.com/");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
     }
 
     @Test
     void givenCaseInsensitivePattern_whenUrlHasUppercaseCharacters_thenMatches() {
         Matcher urlMatcher = CASE_INSENSITIVE_URL_PATTERN.matcher("http://www.Microsoft.com/Ending-path");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
-        Assertions.assertThat(urlMatcher.group(1))
-            .isEqualTo("/Ending-path");
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
+        Assertions.assertThat(urlMatcher.group(1)).isEqualTo("/Ending-path");
     }
 
     @Test
     void givenPatternWithCaseInsensitiveGroup_whenUrlHasUppercaseCharactersOutsideOfScope_thenMatchFails() {
         Matcher urlMatcher = SCOPED_CASE_INSENSTIIVE_URL_PATTERN_WITH_ENDING_PATH.matcher("http://www.Microsoft.com/Ending-path");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isFalse();
+        Assertions.assertThat(urlMatcher.matches()).isFalse();
     }
 
     @Test
     void givenPatternAllowingBacktracking_whenUrlWithEndingPathCausingBacktrackingProvided_thenMatches() {
         Matcher urlMatcher = SIMPLE_URL_PATTERN_WITH_SPECIFIC_ENDING_PATH.matcher("http://www.microsoft.com/ending-path");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
     }
 
     @Test
     void givenPatternWithIndependentNonCapturingGroup_whenBacktrackingOccurs_thenDoesNotMatch() {
         Matcher independentMatcher = INDEPENDENT_URL_PATTERN_WITH_ENDING_PATH.matcher("http://www.microsoft.com/ending-path");
         
-        Assertions.assertThat(independentMatcher.matches())
-            .isFalse();
+        Assertions.assertThat(independentMatcher.matches()).isFalse();
     }
 
     @Test
     void givenPatternWithIndependentNonCapturingGroup_whenBacktrackingDoesNotOccur_thenMatches() {
         Matcher independentMatcher = INDEPENDENT_URL_PATTERN_WITH_ENDING_PATH.matcher("http://www.microsoft.com//ending-path");
         
-        Assertions.assertThat(independentMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(independentMatcher.matches()).isTrue();
     }
 
     @Test
     void givenPatternWithIndependentNonCapturingGroup_whenBacktrackingOccursInsideGroup_thenMatches() {
         Matcher independentMatcher = INDEPENDENT_URL_PATTERN_WITH_ENDING_PATH_AND_BACKTRACKING.matcher("http://www.microsoft.com/ending-path");
         
-        Assertions.assertThat(independentMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(independentMatcher.matches()).isTrue();
     }
 
     @Test
     void givenCaseInsensitivePatternWithCaseSensitivieSubPattern_whenUrlWithUppercaseCharactersOutsideOfScopeProvided_thenMatches() {
         Matcher urlMatcher = SCOPED_CASE_SENSITIVE_URL_PATTERN.matcher("http://www.microsoft.com/ENDING-PATH");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isTrue();
+        Assertions.assertThat(urlMatcher.matches()).isTrue();
     }
 
     @Test
     void givenCaseInsensitivePatternWithCaseSensitivieSubPattern_whenUrlWithUppercaseCharactersInsideOfScopeProvided_thenDoesNotMatch() {
         Matcher urlMatcher = SCOPED_CASE_SENSITIVE_URL_PATTERN.matcher("http://www.Microsoft.com/ending-path");
         
-        Assertions.assertThat(urlMatcher.matches())
-            .isFalse();
+        Assertions.assertThat(urlMatcher.matches()).isFalse();
     }
 }
