f777c86fcf3aa91eef3d558b12f6d00514fe0fdc
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2021-06-08 12:16:37 +0300

    rename project, format

--- spring-boot-modules/spring-boot-validation/src/main/java/com/baeldung/spring/servicevalidation/SpringServiceLayerValidationApp.java
+++ spring-boot-modules/spring-boot-validation/src/main/java/com/baeldung/spring/servicevalidation/SpringServiceLayerValidationApp.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringServiceLayerValidationApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringServiceLayerValidationApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServiceLayerValidationApp.class, args);
+    }
 
 }
