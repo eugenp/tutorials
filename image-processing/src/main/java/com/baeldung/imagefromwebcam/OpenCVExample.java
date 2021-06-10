4440c3e9c5bed01ef34f8d340f11ecd28982d296
Anshul BANSAL <smartdiscover17@gmail.com>
2021-06-10 16:44:49 +0300

    BAEL-4329 - Capturing image from webcam in java

---
+++ image-processing/src/main/java/com/baeldung/imagefromwebcam/OpenCVExample.java
@@ -0,0 +1,31 @@
+package com.baeldung.imagefromwebcam;
+
+import org.bytedeco.javacv.*;
+import org.bytedeco.opencv.opencv_core.IplImage;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import static org.bytedeco.opencv.helper.opencv_imgcodecs.cvSaveImage;
+
+public class OpenCVExample {
+
+    public static void main(String[] args) throws Exception {
+        CanvasFrame canvas = new CanvasFrame("Web Cam");
+        canvas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FrameGrabber grabber = new OpenCVFrameGrabber(0);
+        OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
+
+        grabber.start();
+        Frame frame = grabber.grab();
+
+        IplImage img = converter.convert(frame);
+        cvSaveImage("selfie.jpg", img);
+
+        canvas.showImage(converter.convert(img));
+
+        Thread.sleep(2000);
+
+        canvas.dispatchEvent(new WindowEvent(canvas, WindowEvent.WINDOW_CLOSING));
+    }
+
+}
