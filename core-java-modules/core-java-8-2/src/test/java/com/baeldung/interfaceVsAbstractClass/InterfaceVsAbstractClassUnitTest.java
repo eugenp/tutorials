c3563566f805ede7ad5da0973b45767cc812c70f
Vikas Ramsingh Rajput <vikas.rajput@crownconsult.com>
2021-06-11 19:55:03 +0530

    BAEL-4936: converted multiline code to one liner, also changed names of unit test methods

--- core-java-modules/core-java-8-2/src/test/java/com/baeldung/interfaceVsAbstractClass/InterfaceVsAbstractClassUnitTest.java
+++ core-java-modules/core-java-8-2/src/test/java/com/baeldung/interfaceVsAbstractClass/InterfaceVsAbstractClassUnitTest.java
@@ -6,14 +6,14 @@ import org.junit.Test;
 
 public class InterfaceVsAbstractClassUnitTest {
     @Test
-    public void givenAbstractClass_whenValidCircleUsedThenPass() {
+    public void givenAbstractClass_whenValidCircleUsed_thenPass() {
         CircleClass redCircle = new ChildCircleClass();
         redCircle.setColor("RED");
         assertTrue(redCircle.isValid());
     }
 
     @Test
-    public void givenInterface_whenValidCircleWithoutStateUsedThenPass() {
+    public void givenInterface_whenValidCircleWithoutStateUsed_thenPass() {
         ChidlCircleInterfaceImpl redCircleWithoutState = new ChidlCircleInterfaceImpl();
         redCircleWithoutState.setColor("RED");
         assertTrue(redCircleWithoutState.isValid());
