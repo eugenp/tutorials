257ba7551f796cf512e50961819cc464bef00798
ravipandey-dev <ravipan31.1991@gmail.com>
2021-06-14 16:32:13 +0530

    Added new module for hexagonal arch

---
+++ hexagonal/README.md
@@ -0,0 +1,1 @@
+An example for Hexagonal Architecture in Java.
---
+++ hexagonal/src/main/java/com/baeldung/hexagonal/adapter/BookController.java
@@ -0,0 +1,24 @@
+package com.baeldung.hexagonal.adapter;
+
+import com.baeldung.hexagonal.core.domain.Book;
+import com.baeldung.hexagonal.port.BookService;
+import com.baeldung.hexagonal.web.BookUI;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
+public class BookController implements BookUI {
+
+    @Autowired
+    BookService bookService;
+    @PostMapping
+    public void addBook(@RequestBody Book book) {
+        bookService.addBook(book);
+    }
+
+    @GetMapping("/{title}")
+    public Book getBook(@PathVariable String title) {
+        return bookService.getBook(title);
+    }
+}
