5b50a93d0b4d6fc39b0e257b90ee70e883d7b113
dev-chirag <chirag.dewan22@gmail.com>
2021-06-15 23:51:57 +0530

    BAEL4235 FindNumberOfThread class

--- core-java-modules/core-java-concurrency-basic-2/src/main/java/com/baeldung/concurrent/threads/number/FindNumberOfThreads.java
+++ core-java-modules/core-java-concurrency-basic-2/src/main/java/com/baeldung/concurrent/threads/number/FindNumberOfThreads.java
@@ -6,7 +6,9 @@ public class FindNumberOfThreads {
 
     public static void main(String[] args) {
         System.out.println("Number of threads " + Thread.activeCount());
-        System.out.println("Current Thread Group - " + Thread.currentThread().getThreadGroup().getName());
-        System.out.println("Total Number of threads " + ManagementFactory.getThreadMXBean().getThreadCount());
+        System.out.println("Current Thread Group - "
+            + Thread.currentThread().getThreadGroup().getName());
+        System.out.println("Total Number of threads "
+            + ManagementFactory.getThreadMXBean().getThreadCount());
     }
 }
