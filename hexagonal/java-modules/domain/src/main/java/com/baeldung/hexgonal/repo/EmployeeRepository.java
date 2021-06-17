3a3f431ca3d8b989541fc992473533fccc190381
arash.sw90@gmail.com <arash.sw90@gmail.com>
2021-06-18 03:55:21 +0430

    hexagonal has been removed , minmaxheap has been added to data-structure module

--- hexagonal/java-modules/domain/src/main/java/com/baeldung/hexgonal/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hexgonal.repo;
-
-
-import com.baeldung.hexgonal.data.Employee;
-
-public interface EmployeeRepository {
-    void save(Employee employee);
-    void delete(Long id);
-}
