f63f14679ced8168c73a8d556c16bceb404e9a02
Anshul BANSAL <smartdiscover17@gmail.com>
2021-06-27 22:35:43 +0300

    BAEL-4913 - review commit

--- spring-native/src/main/java/com/baeldung/springnativeintro/SpringNativeApp.java
+++ spring-native/src/main/java/com/baeldung/springnativeintro/SpringNativeApp.java
@@ -3,7 +3,7 @@ package com.baeldung.springnativeintro;
 public class SpringNativeApp {
     
     public static void main(String[] args) {
-        System.out.println("Hello World!, This is Baledung Spring Native Application");
+        System.out.println("Hello, World! This is a Baledung Spring Native Application");
     }
 
 }
