38429af7a215092339e6ba99f474db74abbfe783
uzma khan <uzma.khan@nominet.uk>
2021-06-27 11:00:05 +0100

    [BAEL-4264] Surefire and Failsafe comparison

---
+++ maven-modules/maven-plugins/surefire-failsafe-comparison/src/test/java/com/baeldung/ExampleIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.baeldung;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExampleIntegrationTest {
+
+    @Test
+    public void whenTestExecutes_thenTestPasses() {
+        assertTrue(true);
+    }
+}
---
+++ maven-modules/maven-plugins/surefire-failsafe-comparison/src/test/java/com/baeldung/ExampleUnitTest.java
@@ -0,0 +1,14 @@
+package com.baeldung;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ExampleUnitTest {
+
+    @Test
+    void whenTestExecutes_thenTestPasses() {
+        assertTrue(true);
+    }
+
+}
