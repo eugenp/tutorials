fe660b97a2d79060ad78d563f406bfca2e20c32b
Haroon Khan <haroonkhan79@gmail.com>
2021-06-27 13:17:22 +0100

    [BAEL-4823] Minor code cleanup

--- maven-modules/plugin-management/submodule-1/src/test/java/com/baeldung/CopiesAdditionalResourcesUnitTest.java
+++ maven-modules/plugin-management/submodule-1/src/test/java/com/baeldung/CopiesAdditionalResourcesUnitTest.java
@@ -4,16 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.net.URL;
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class CopiesAdditionalResourcesUnitTest {
+class CopiesAdditionalResourcesUnitTest {
 
     @Test
-    public void givenAdditionalResource_whenCopyingFromSourceToDestination_thenShouldBeInDestination() {
+    void givenAdditionalResource_whenCopyingFromSourceToDestination_thenShouldBeInDestination() {
         URL resource = getClass().getClassLoader().getResource("json/include.json");
-        File destinationFile = new File(Objects.requireNonNull(resource).getFile());
+        File destinationFile = new File(resource.getFile());
 
         assertTrue(destinationFile.exists());
     }
