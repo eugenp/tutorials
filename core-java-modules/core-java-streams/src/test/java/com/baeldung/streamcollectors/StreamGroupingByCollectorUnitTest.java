cc03e75561e5ee0679dc8a582244084292ab4e8f
Devansh <devansh.trivedi5@gmail.com>
2021-07-02 00:44:08 +0530

    Added tests for groupingBy Collector

--- core-java-modules/core-java-streams/src/test/java/com/baeldung/streamcollectors/StreamGroupingByCollectorUnitTest.java
+++ core-java-modules/core-java-streams/src/test/java/com/baeldung/streamcollectors/StreamGroupingByCollectorUnitTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 
 public class StreamGroupingByCollectorUnitTest
 {
-
+h
         static class Employee {
 
                 Integer employeeId;
--- spring-boot-modules/spring-boot-hexarchitecture/hex-architecture/src/main/java/com/baeldung/hexarchitecture/productstore/ports/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hexarchitecture.productstore.ports;
-
-import com.baeldung.hexarchitecture.productstore.core.domain.model.Product;
-
-public interface ProductService {
-        void createProduct(Product product);
-
-        Product getProduct(String id);
-}
