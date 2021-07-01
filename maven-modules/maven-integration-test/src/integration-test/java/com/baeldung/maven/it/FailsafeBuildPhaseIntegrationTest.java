772632c5ce4999991c2720c424a5997eaf3fabd3
uzma khan <uzma.khan@nominet.uk>
2021-07-01 18:51:02 +0100

    [BAEL-4264] Rename test

--- maven-modules/maven-integration-test/src/integration-test/java/com/baeldung/maven/it/FailsafeBuildPhaseIntegrationTest.java
+++ maven-modules/maven-integration-test/src/integration-test/java/com/baeldung/maven/it/FailsafeBuildPhaseIntegrationTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class FailsafeBuildPhaseIT {
+class FailsafeBuildPhaseIntegrationTest {
 
     @Test
     public void whenTestExecutes_thenPreAndPostIntegrationBuildPhasesAreExecuted() {
