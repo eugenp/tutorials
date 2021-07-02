a38af5dcae879ece09657cef93449ad708f0aac9
Sunil Garhwal <sunil.garhwal@girnarsoft.com>
2021-07-02 22:23:15 +0530

    hexagonal architecture overview

--- patterns/design-patterns-architectural/src/main/java/com/baeldung/hexagonal/domain/User.java
+++ patterns/design-patterns-architectural/src/main/java/com/baeldung/hexagonal/domain/User.java
@@ -1,5 +1,8 @@
 package com.baeldung.hexagonal.domain;
 
+import lombok.Data;
+
+@Data
 public class User {
 
     private int userId;
@@ -7,44 +10,4 @@ public class User {
     private String lastName;
     private MemeberStatus status;
     private int rewardedpoints;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public MemeberStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(MemeberStatus status) {
-        this.status = status;
-    }
-
-    public int getRewardedpoints() {
-        return rewardedpoints;
-    }
-
-    public void setRewardedpoints(int rewardedpoints) {
-        this.rewardedpoints = rewardedpoints;
-    }
 }
