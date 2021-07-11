218f67ef4c8146a3c98ef3f5f306ffb748154669
Amitabh.Tiwari <amitabh.tiwari@maersk.com>
2021-07-11 12:15:12 +0530

    Added changes as per review

--- core-java-modules/core-java-string-conversions-2/src/test/java/com/baeldung/stringtobybiginteger/StringToBigIntegerUnitTest.java
+++ core-java-modules/core-java-string-conversions-2/src/test/java/com/baeldung/stringtobybiginteger/StringToBigIntegerUnitTest.java
@@ -10,33 +10,33 @@ public class StringToBigIntegerUnitTest {
 
     @Test
     public void whenGetStringWithOutRadix_thenOK() {
-        final String inputString = "878";
+        String inputString = "878";
         BigInteger result = new BigInteger(inputString);
         assertEquals("878", result.toString());
     }
 
     @Test
     public void whenGetStringWithRadix_thenOK() {
-        final String inputString = "290f98";
+        String inputString = "290f98";
         BigInteger result = new BigInteger(inputString, 16);
         assertEquals("2690968", result.toString());
     }
 
     @Test(expected = NumberFormatException.class)
     public void whenGetStringWithOutRadix_thenThrowError() {
-        final String inputString = "290f98";
+        String inputString = "290f98";
         new BigInteger(inputString);
     }
 
     @Test(expected = NumberFormatException.class)
     public void whenGetStringWithRadix_thenThrowError() {
-        final String inputString = "290f98";
+        String inputString = "290f98";
         new BigInteger(inputString, 7);
     }
 
     @Test
     public void whenGetStringUsingByte_thenOk() {
-        final String inputString = "290f98";
+        String inputString = "290f98";
         byte[] inputStringBytes = inputString.getBytes();
         BigInteger result = new BigInteger(inputStringBytes);
         assertEquals("290f98", new String(result.toByteArray()));
