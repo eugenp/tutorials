01d102ecddbc2802abc7f930bed8334e92f44f96
Philippe <phil@lighthouse.com.br>
2021-07-17 17:44:20 -0300

    [BAEL-4968] Article code

---
+++ kubernetes/k8s-admission-controller/src/main/java/com/baeldung/kubernetes/admission/config/AdmissionControllerProperties.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.baeldung.kubernetes.admission.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+/**
+ * @author lighthouse.psevestre
+ *
+ */
+@ConfigurationProperties(prefix = "admission-controller")
+@Data
+public class AdmissionControllerProperties {
+
+    private boolean disabled;
+    private String annotation = "com.baeldung/wait-for-it";
+    private String waitForItImage = "willwill/wait-for-it";
+
+}
