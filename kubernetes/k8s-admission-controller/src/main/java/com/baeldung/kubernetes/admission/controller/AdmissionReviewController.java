b8a05bcc9d63638de7a3bffc985aca643331b14b
psevestre <phil@lighthouse.com.br>
2021-07-18 12:19:13 -0300

    [BAEL-5014] Kubernetes Admission Controller (#11044)
    * [BAEL-4849] Article code
    
    * [BAEL-4968] Article code
    
    * [BAEL-4968] Article code
    
    * [BAEL-4968] Article code

---
+++ kubernetes/k8s-admission-controller/src/main/java/com/baeldung/kubernetes/admission/controller/AdmissionReviewController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.baeldung.kubernetes.admission.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.kubernetes.admission.dto.AdmissionReviewResponse;
+import com.baeldung.kubernetes.admission.service.AdmissionService;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
+
+/**
+ *
+ */
+@RestController
+@RequiredArgsConstructor
+public class AdmissionReviewController {
+
+    private final AdmissionService admissionService;
+
+    @PostMapping(path = "/mutate")
+    public Mono<AdmissionReviewResponse> processAdmissionReviewRequest(@RequestBody Mono<ObjectNode> request) {
+        return request.map((body) -> admissionService.processAdmission(body));
+    }
+}
