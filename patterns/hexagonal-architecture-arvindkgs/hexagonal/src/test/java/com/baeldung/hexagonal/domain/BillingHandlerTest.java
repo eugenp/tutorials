3b3a5e6b39c4705581a82d7e5b8eb05714a31b3e
arvindkgs <arvindkgs@nile-global.com>
2021-07-18 16:05:15 +0530

    arvindkgs: Modified Test

--- patterns/hexagonal-architecture-arvindkgs/hexagonal/src/test/java/com/baeldung/hexagonal/domain/BillingHandlerTest.java
+++ patterns/hexagonal-architecture-arvindkgs/hexagonal/src/test/java/com/baeldung/hexagonal/domain/BillingHandlerTest.java
@@ -10,6 +10,7 @@ import com.stripe.exception.StripeException;
 public class BillingHandlerTest {
 	public void payTest() throws PaymentException, StripeException {
 		PaymentService mockPay = Mockito.mock(PaymentService.class);
+		Mockito.when(mockPay.debit(Mockito.any())).then(x-> 100);
 		BillingHandler billing = new BillingHandler(mockPay);
 		Order singleOrder = new Order();
 		singleOrder.getItems().add(new Item(100,1));
