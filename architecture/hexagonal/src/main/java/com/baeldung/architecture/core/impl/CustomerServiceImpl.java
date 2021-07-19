8ad136c360066bd247375d9bf1e2d8a8d44a9933
hajk1 <k1.tehrani@gmail.com>
2021-07-20 01:21:46 +0430

    adding repo to the service

--- architecture/hexagonal/src/main/java/com/baeldung/architecture/core/impl/CustomerServiceImpl.java
+++ architecture/hexagonal/src/main/java/com/baeldung/architecture/core/impl/CustomerServiceImpl.java
@@ -1,26 +1,30 @@
 package com.baeldung.architecture.core.impl;
 
 import com.baeldung.architecture.core.domain.Customer;
+import com.baeldung.architecture.port.CustomerRepository;
 import com.baeldung.architecture.port.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    CustomerRepository repository;
 
     @Override
     public List<Customer> getAll() {
-        return null;
+        return repository.getAll();
     }
 
     @Override
     public void create(Customer customer) {
-
+        repository.create(customer);
     }
 
     @Override
-    public Customer getByName(String id) {
-        return null;
+    public Customer getByName(String name) {
+        return repository.getByName(name);
     }
 }
