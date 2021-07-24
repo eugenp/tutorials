104f28d64bc9b150b200f0f57511133eeaf8047c
tovij <tovij@DESKTOP-IJ1PS23>
2021-07-24 08:53:14 +0530

    Added Test Case.

--- patterns/hexagonal-example-java/src/main/java/com/baeldung/movie/domain/MovieDto.java
+++ patterns/hexagonal-example-java/src/main/java/com/baeldung/movie/domain/MovieDto.java
@@ -15,5 +15,30 @@ public class MovieDto {
     private String title;
     private String description;
     private Double price;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 
+    
 }
---
+++ patterns/hexagonal-example-java/src/test/java/com/baeldung/movie/application/MovieServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.baeldung.movie.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.baeldung.movie.application.port.in.MovieServicePort;
+import com.baeldung.movie.application.port.out.MoviePersistencePort;
+import com.baeldung.movie.domain.MovieDto;
+
+
+public class MovieServiceImplTest {
+    
+    private MoviePersistencePort moviePersistencePort;
+    private MovieServicePort movieServicePort;
+    private MovieDto movieDto;
+    
+    @BeforeEach
+    void setUp() {
+        moviePersistencePort = mock(MoviePersistencePort.class);
+        movieServicePort = new MovieServiceImpl(moviePersistencePort);
+        movieDto = new MovieDto();
+        movieDto.setId(Long.valueOf(2));
+        movieDto.setDescription("Action");
+        movieDto.setTitle("Terminator");
+        movieDto.setPrice(Double.valueOf(40));
+    }
+    
+    @Test
+    void addMovie() {
+        when(moviePersistencePort.addMovie(any(MovieDto.class))).thenReturn(movieDto);
+        MovieDto testResponse = movieServicePort.addMovie(movieDto);
+        assertEquals(movieDto, testResponse);
+    }
+
+    @Test
+    void getMovie() {
+        when(moviePersistencePort.getMovieById(Long.valueOf(2))).thenReturn(movieDto);
+        MovieDto testResponse = movieServicePort.getMovieById(Long.valueOf(2));
+        assertEquals(movieDto, testResponse);
+    }
+
+}
