e5aa902abb31bac67afc1561648805e3ea22c385
Suresh Raghavan <contactnrsuresh@gmail.com>
2021-07-28 23:03:38 -0500

    removed ddd-hexagonal-arch module

--- ddd-hexagonal-arch/src/main/java/com/baeldung/ddd/hexagonal/arch/adapters/driven/db/UserDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.ddd.hexagonal.arch.adapters.driven.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserDBRepository extends JpaRepository<UserEntity, String> {
-    UserEntity findByEmail(String email);
-}
--- ddd-hexagonal-arch/src/main/java/com/baeldung/ddd/hexagonal/arch/core/ports/drivers/UserSignup.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.ddd.hexagonal.arch.core.ports.drivers;
-
-import com.baeldung.ddd.hexagonal.arch.core.domain.User;
-
-public interface UserSignup {
-    void signup(User user);
-}
