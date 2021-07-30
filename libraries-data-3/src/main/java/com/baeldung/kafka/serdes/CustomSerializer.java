7f0c38b1c5485f72db55c3b9005cc13fa3a67cc1
Carlos Cavero <cavero.carlos@gmail.com>
2021-07-30 18:21:23 +0200

    [BAEL-4854] - Add the dto (including the lombok dependency) and the custom serializers and deserializers

---
+++ libraries-data-3/src/main/java/com/baeldung/kafka/serdes/CustomSerializer.java
@@ -0,0 +1,37 @@
+package com.baeldung.kafka.serdes;
+
+import com.baeldung.kafka.dto.MessageDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+@Slf4j
+public class CustomSerializer implements Serializer<MessageDto> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+
+    @Override
+    public byte[] serialize(String topic, MessageDto data) {
+        try {
+            if (data == null){
+                System.out.println("Null received at serializing");
+                return null;
+            }
+            System.out.println("Serializing...");
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error when serializing MessageDto to byte[]");
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
