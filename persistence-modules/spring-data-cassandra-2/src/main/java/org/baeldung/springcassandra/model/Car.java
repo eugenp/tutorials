1fcc7b6dfeb37deea62e59b66a8a1eca4575b0e7
Daniel Strmecki <daniel.strmecki@gmail.com>
2021-08-01 10:41:51 +0200

    BAEL-5040: update test example

--- persistence-modules/spring-data-cassandra-2/src/main/java/org/baeldung/springcassandra/model/Car.java
+++ persistence-modules/spring-data-cassandra-2/src/main/java/org/baeldung/springcassandra/model/Car.java
@@ -1,15 +1,11 @@
 package org.baeldung.springcassandra.model;
 
-import lombok.Builder;
-import lombok.Data;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
-import java.time.LocalDate;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
-@Builder
 @Table
 public class Car {
 
@@ -21,5 +17,59 @@ public class Car {
     private String model;
 
     private int year;
+
+    public Car(UUID id, String make, String model, int year) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Car car = (Car) o;
+        return year == car.year && Objects.equals(id, car.id) && Objects.equals(make, car.make) && Objects.equals(model, car.model);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, make, model, year);
+    }
 
 }
