68886ddcfb578d2696419bb03591ee040a4bc601
ine12363914 <saikat.chakraborty@tesco.com>
2021-08-03 22:39:51 +0530

    BAEL-4216 Corrected Formatting and Removed final and Super

--- core-java-modules/core-java-lang-4/src/main/java/com/baeldung/bookstore/Book.java
+++ core-java-modules/core-java-lang-4/src/main/java/com/baeldung/bookstore/Book.java
@@ -3,15 +3,13 @@ package com.baeldung.bookstore;
 
 public class Book {
 
-	private String title;
-	private String author;
-	private long isbn;
-
-	public Book(final String title, final String author, final long isbn) {
-		super();
-		this.title = title;
-		this.author = author;
-		this.isbn = isbn;
-	}
-
+    private String title;
+    private String author;
+    private long isbn;
+    
+    public Book(String title, String author, long isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
 }
--- core-java-modules/core-java-lang-4/src/main/java/com/baeldung/bookstore/LibraryAdmin.java
+++ core-java-modules/core-java-lang-4/src/main/java/com/baeldung/bookstore/LibraryAdmin.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class LibraryAdmin {
 
-	public Book createBook(String title, String author) {
+    public Book createBook(String title, String author) {
 
-		final long isbn = new Random().nextLong();
+        final long isbn = new Random().nextLong();
 
-		return new Book(title, author, isbn);
-	}
-	
+        return new Book(title, author, isbn);
+    }
+    
 }
