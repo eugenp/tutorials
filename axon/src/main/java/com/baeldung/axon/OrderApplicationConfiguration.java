eef0c1e5ec1f5129f2487f4c49767b9f7da7e91d
Steven van Beelen <steven.vanbeelen@axoniq.io>
2021-08-04 11:35:23 +0200

    Add SnapshotTriggerDefinition bean creation method
    Add SnapshotTriggerDefinition bean creation method
    
    #BAEL-4768

--- axon/src/main/java/com/baeldung/axon/OrderApplicationConfiguration.java
+++ axon/src/main/java/com/baeldung/axon/OrderApplicationConfiguration.java
@@ -1,8 +1,16 @@
 package com.baeldung.axon;
 
+import org.axonframework.eventsourcing.EventCountSnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.SnapshotTriggerDefinition;
+import org.axonframework.eventsourcing.Snapshotter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OrderApplicationConfiguration {
 
+    @Bean
+    public SnapshotTriggerDefinition orderAggregateSnapshotTriggerDefinition(Snapshotter snapshotter) {
+        return new EventCountSnapshotTriggerDefinition(snapshotter, 50);
+    }
 }
