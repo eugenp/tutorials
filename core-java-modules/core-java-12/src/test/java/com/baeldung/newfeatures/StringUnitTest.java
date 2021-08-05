b623297829c113a80b571e25e5680ff5e9bcfaf6
mbarriola <85458535+mbarriola@users.noreply.github.com>
2021-08-05 15:50:37 -0400

    Core java 12 improvements (#11090)
    * Commit source code to branch
    
    * Fix to BAEL-5072
    Change package definition and implementation of compactValues junit
    
    * BAEL-5053 Compare file contents

--- core-java-modules/core-java-12/src/test/java/com/baeldung/newfeatures/StringUnitTest.java
+++ core-java-modules/core-java-12/src/test/java/com/baeldung/newfeatures/StringUnitTest.java
@@ -1,6 +1,6 @@
-package java.com.baeldung.newfeatures;
+package com.baeldung.newfeatures;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
