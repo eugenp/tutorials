e1c7c0ef481e47c0be970bfd42c83661380a8c89
Gerald Boersma <gboersma@users.noreply.github.com>
2021-08-07 17:55:35 -0700

    BAEL-5049 spring-cloud-dapr (#11063)
    * BAEL-5049 Added tutorial code for article.
    
    * BAEL-5049 Added readme.
    
    * Initial code for 2-javasdk to properly include as part of Spring Boot project.
    
    * Added binding with rabbitmq.
    
    * Removed 1-intro module.
    
    * Updates from comments.
    
    * Added .gitignore back in.
    
    * Cleaned up POMs.
    
    * Get rid of .gitignore.
    
    * Change controller to use GetMapping.
    
    Co-authored-by: Gerald Boersma <gerald.boersma@icsynergy.com>

---
+++ spring-cloud/spring-cloud-dapr/greeting/src/main/java/org/example/hello/GreetingApp.java
@@ -0,0 +1,11 @@
+package org.example.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingApp.class, args);
+    }
+}
