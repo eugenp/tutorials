0ba320ada11f804f12ae6dfc95a3c08db28a2ef9
hajk1 <k1.tehrani@gmail.com>
2021-08-17 08:45:08 +0430

    adding log4j2 dependency

--- maven-dependency-management/src/main/java/com/baeldung/Main.java
+++ maven-dependency-management/src/main/java/com/baeldung/Main.java
@@ -1,7 +1,12 @@
 package com.baeldung;
 
-public class Main {
-    public static void main(String[] args) {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+public class Main {
+    private static final Logger logger = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger.info("This is a sample class");
     }
 }
