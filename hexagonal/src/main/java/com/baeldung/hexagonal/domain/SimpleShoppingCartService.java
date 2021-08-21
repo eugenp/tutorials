5f93ff5b3be1e8658913fc9dedbdadd918cdf1be
asia <joannakrzeklubowiecka@protonmail.com>
2021-08-21 15:42:18 +0200

    Refactoring

--- hexagonal/src/main/java/com/baeldung/hexagonal/domain/SimpleShoppingCartService.java
+++ hexagonal/src/main/java/com/baeldung/hexagonal/domain/SimpleShoppingCartService.java
@@ -5,7 +5,6 @@ import java.util.HashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.baeldung.hexagonal.ShoppingCartService;
 import com.baeldung.hexagonal.adapter.out.Product;
 import com.baeldung.hexagonal.domain.ports.ProductRepository;
 
