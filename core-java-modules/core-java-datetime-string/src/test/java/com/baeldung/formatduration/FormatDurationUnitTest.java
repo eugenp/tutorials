665f9e97daf2035bd48ae014739cf0eaa67b092c
Remy Ohajinwa <remy.ohajinwa@interswitchgroup.com>
2021-08-22 17:50:17 -0500

    BAEL-5013 format duration

--- core-java-modules/core-java-datetime-string/src/test/java/com/baeldung/formatduration/FormatDurationUnitTest.java
+++ core-java-modules/core-java-datetime-string/src/test/java/com/baeldung/formatduration/FormatDurationUnitTest.java
@@ -1,41 +1,54 @@
 package com.baeldung.formatduration;
 
 import org.apache.commons.lang3.time.DurationFormatUtils;
+import org.joda.time.Period;
 import org.junit.Test;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FormatDurationUnitTest {
 
-    private FormatDuration formatDuration = new FormatDuration();
-
-    @Test
-    public void givenSeconds_WhenUsingFormatter_FormatDuration() {
-        long s = 36000000;
-        assertThat(String.format("%2d:%02d:%02d", s / 3600, (s % 3600) / 60, (s % 60))).isEqualTo("10000:00:00");
-    }
 
     @Test
     public void givenInterval_WhenFormatInterval_formatDuration() {
-        assertThat(formatDuration.formatDurationUsingTimeUnit(36000000)).isEqualTo("10:00:00");
+        long HH = TimeUnit.MILLISECONDS.toHours(38114000);
+        long MM = TimeUnit.MILLISECONDS.toMinutes(38114000) % 60;
+        long SS = TimeUnit.MILLISECONDS.toSeconds(38114000) % 60;
+        String timeInHHMMSS = String.format("%02d:%02d:%02d", HH, MM, SS);
+
+        assertThat(timeInHHMMSS).isEqualTo("10:35:14");
     }
 
     @Test
     public void givenInterval_WhenFormatUsingDuration_formatDuration() {
-        assertThat(formatDuration.formatUsingDuration(Duration.ofMillis(36000000))).isEqualTo("10:00:00");
+        Duration duration = Duration.ofMillis(38114000);
+        long seconds = duration.getSeconds();
+        long HH = seconds / 3600;
+        long MM = (seconds % 3600) / 60;
+        long SS = seconds % 60;
+        String timeInHHMMSS =  String.format("%02d:%02d:%02d", HH, MM, SS);
+        assertThat(timeInHHMMSS).isEqualTo("10:35:14");
     }
 
 
     @Test
     public void givenInterval_WhenFormatDurationUsingApacheCommons_formatDuration() {
-        assertThat(DurationFormatUtils.formatDuration(36000000, "HH:MM:SS", true))
-          .isEqualTo("10:00:00");
+        assertThat(DurationFormatUtils.formatDuration(38114000, "HH:mm:ss"))
+          .isEqualTo("10:35:14");
     }
 
     @Test
     public void givenInterval_WhenFormatDurationUsingJodaTime_formatDuration() {
-        assertThat(formatDuration.formatDurationUsingJodaTime((36000000))).isEqualTo("10:00:00");
+        org.joda.time.Duration duration = new org.joda.time.Duration(38114000);
+        Period period = duration.toPeriod();
+        long HH = period.getHours();
+        long MM = period.getMinutes();
+        long SS = period.getSeconds();
+
+        String timeInHHMMSS = String.format("%02d:%02d:%02d", HH, MM, SS);
+        assertThat(timeInHHMMSS).isEqualTo("10:35:14");
     }
 }
