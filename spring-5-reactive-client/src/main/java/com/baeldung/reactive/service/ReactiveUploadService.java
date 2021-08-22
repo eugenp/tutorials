f3469367ed8db9c9950f85ab4df6a695d1d782bd
Trixi Turny <writetotrixi@gmail.com>
2021-08-22 22:08:17 +0100

    BAEL-5054 throw ServiceException when not OK
    BAEL-5054 fix test name and tidy indent
    
    BAEL-5054 remove unnecessary changes
    
    BAEL-5054 restore main class name
    
    BAEL-5054 rename test to end with UnitTest

--- spring-5-reactive-client/src/main/java/com/baeldung/reactive/service/ReactiveUploadService.java
+++ spring-5-reactive-client/src/main/java/com/baeldung/reactive/service/ReactiveUploadService.java
@@ -1,6 +1,7 @@
 package com.baeldung.reactive.service;
 
 
+import com.baeldung.reactive.exception.ServiceException;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -20,50 +21,46 @@ public class ReactiveUploadService {
     private final WebClient webClient;
     private static final String EXTERNAL_UPLOAD_URL = "http://localhost:8080/external/upload";
 
-    public ReactiveUploadService() {
-        this.webClient = WebClient.create();
+    public ReactiveUploadService(final WebClient webClient) {
+        this.webClient = webClient;
     }
 
 
-    public Mono<HttpStatus> uploadPdf(final Resource resource){
+    public Mono<HttpStatus> uploadPdf(final Resource resource) {
 
         final URI url = UriComponentsBuilder.fromHttpUrl(EXTERNAL_UPLOAD_URL).build().toUri();
         Mono<HttpStatus> httpStatusMono = webClient.post()
-                .uri(url)
-                .contentType(MediaType.APPLICATION_PDF)
-                .body(BodyInserters.fromResource(resource))
-                .exchangeToMono(response -> {
-                    if (response.statusCode().equals(HttpStatus.OK)) {
-                        return response.bodyToMono(HttpStatus.class).thenReturn(response.statusCode());
-                    } else {
-                        System.out.println("Failed to upload pdf. " + response.statusCode());
-                    }
-                    return null;
-                });
+          .uri(url)
+          .contentType(MediaType.APPLICATION_PDF)
+          .body(BodyInserters.fromResource(resource))
+          .exchangeToMono(response -> {
+            if (response.statusCode().equals(HttpStatus.OK)) {
+              return response.bodyToMono(HttpStatus.class).thenReturn(response.statusCode());
+            } else {
+              throw new ServiceException("Error uploading file");
+            }
+          });
         return httpStatusMono;
     }
 
 
-    public Mono<HttpStatus> uploadMultipart(final MultipartFile multipartFile){
+    public Mono<HttpStatus> uploadMultipart(final MultipartFile multipartFile) {
         final URI url = UriComponentsBuilder.fromHttpUrl(EXTERNAL_UPLOAD_URL).build().toUri();
 
         final MultipartBodyBuilder builder = new MultipartBodyBuilder();
         builder.part("file", multipartFile.getResource());
 
         Mono<HttpStatus> httpStatusMono = webClient.post()
-                .uri(url)
-                .contentType(MediaType.MULTIPART_FORM_DATA)
-                .body(BodyInserters.fromMultipartData(builder.build()))
-                .exchangeToMono(response -> {
-                    if (response.statusCode().equals(HttpStatus.OK)) {
-                        return response.bodyToMono(HttpStatus.class).thenReturn(response.statusCode());
-                    } else {
-                        System.out.println("Failed to upload pdf. " + response.statusCode());
-                    }
-                    return null;
-                });
+          .uri(url)
+          .contentType(MediaType.MULTIPART_FORM_DATA)
+          .body(BodyInserters.fromMultipartData(builder.build()))
+          .exchangeToMono(response -> {
+            if (response.statusCode().equals(HttpStatus.OK)) {
+              return response.bodyToMono(HttpStatus.class).thenReturn(response.statusCode());
+            } else {
+                throw new ServiceException("Error uploading file");
+            }
+          });
         return httpStatusMono;
     }
-
-
 }
