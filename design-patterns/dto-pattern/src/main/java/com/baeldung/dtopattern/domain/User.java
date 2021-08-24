9311d5aa1315293746d676f8cd584b1cb857bfbc
Thiago dos Santos Hora <thiago.santos.hora@gmail.com>
2021-08-24 22:00:49 +0200

    Fix encrypt logic

--- design-patterns/dto-pattern/src/main/java/com/baeldung/dtopattern/domain/User.java
+++ design-patterns/dto-pattern/src/main/java/com/baeldung/dtopattern/domain/User.java
@@ -1,28 +1,25 @@
 package com.baeldung.dtopattern.domain;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.*;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
-import java.util.Random;
 
 public class User {
 
-    private static final IvParameterSpec ivSpec;
+    private static SecretKeySpec KEY = initKey();
 
-    static  {
-        byte[] iv = new byte[128/8];
-        new Random().nextBytes(iv);
-        ivSpec = new IvParameterSpec(iv);
+    static SecretKeySpec initKey(){
+        try {
+            SecretKey secretKey = KeyGenerator.getInstance("AES").generateKey();
+            return new SecretKeySpec(secretKey.getEncoded(), "AES");
+        } catch (NoSuchAlgorithmException ex) {
+            return null;
+        }
     }
 
     private String id;
@@ -71,14 +68,11 @@ public class User {
     String encrypt(String password) {
         Objects.requireNonNull(password);
         try {
-            final byte[] secret = "secret".getBytes(StandardCharsets.UTF_8);
-            SecretKeySpec key = new SecretKeySpec(secret, "DES");
-            Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-            cipher.init(Cipher.ENCRYPT_MODE, key, ivSpec);
-            final byte[] encriptedBytes = cipher.doFinal(password.getBytes(StandardCharsets.UTF_8));
-            return new String(encriptedBytes, StandardCharsets.UTF_8);
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidAlgorithmParameterException
-                | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, KEY);
+            final byte[] encryptedBytes = cipher.doFinal(password.getBytes(StandardCharsets.UTF_8));
+            return new String(encryptedBytes, StandardCharsets.UTF_8);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
             // do nothing
             return "";
         }
