34bb110b5cf12530327a3db7e12cfc81d208ba47
vaibhav007jain@gmail.com <vaibhav007jain@gmail.com>
2021-08-26 15:40:56 +0530

    removed hexagonal-architecture code from last commit

--- hexagonal-architecture/README.md
+++
@@ -1,1 +0,0 @@
-This module contains article about A Quick and Practical Example of Hexagonal Architecture in Java.
--- hexagonal-architecture/src/main/java/com/baeldung/hexagonalarchitecture/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.hexagonalarchitecture.dao;
-
-import com.baeldung.hexagonalarchitecture.entity.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, Long> {
-}
