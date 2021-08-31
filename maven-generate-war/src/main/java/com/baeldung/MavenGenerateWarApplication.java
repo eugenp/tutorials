c530879cc17ae048a35bb7be284dca48a9add0a8
anuradha <anuradhap@intervest.lk>
2021-08-31 21:03:43 +0530

    Maven - Generate War File

---
+++ maven-generate-war/src/main/java/com/baeldung/MavenGenerateWarApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenGenerateWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenGenerateWarApplication.class, args);
+	}
+
+}
