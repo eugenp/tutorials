80f9c20edf3a25d5e92682c3ada354b4fdaac022
asia <joannakrzeklubowiecka@protonmail.com>
2021-09-05 11:16:30 +0200

    Refactoring based on the feedback

--- core-java-modules/core-java-arrays-operations-advanced/src/main/java/com/baeldung/copyArrayMethodsPerformance/BenchmarkRunner.java
+++ core-java-modules/core-java-arrays-operations-advanced/src/main/java/com/baeldung/copyArrayMethodsPerformance/BenchmarkRunner.java
@@ -1,8 +1,9 @@
-package com.baeldung.jmh;
+package com.baeldung.copyArrayMethodsPerformance;
 
 public class BenchmarkRunner {
 
     public static void main(String[] args) throws Exception {
+
         org.openjdk.jmh.Main.main(args);
 
     }
