323646b549ea6e16735402875778effa01dac9ac
saikatcse03 <40471715+saikatcse03@users.noreply.github.com>
2021-09-06 23:14:54 +0530

    Update Book.java
    Formatted comments with spaces

--- core-java-modules/core-java-lang-4/src/main/java/com/baeldung/bookstore/Book.java
+++ core-java-modules/core-java-lang-4/src/main/java/com/baeldung/bookstore/Book.java
@@ -1,9 +1,10 @@
-//The next line is commented out to avoid the code to fail the build.
-//package com.baeldung;
+// The next line is commented out to avoid the code to fail the build.
+// package com.baeldung;
 
-/* If the below package declaration is commented out and the above incorrect package declaration 
- * is uncommented, then the problem will replicate.
- */
+/**
+* If the below package declaration is commented out and the above incorrect package 
+* declaration is uncommented, then the problem will replicate.
+*/
 package com.baeldung.bookstore;
 
 public class Book {
