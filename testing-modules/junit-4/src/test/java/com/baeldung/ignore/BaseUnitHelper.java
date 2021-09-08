ed77864c663769d96b65f74ac7c5fceceee5ab76
LUCA <lucacambi77@gmail.com>
2021-09-08 21:58:34 +0200

    add ignore package

---
+++ testing-modules/junit-4/src/test/java/com/baeldung/ignore/BaseUnitHelper.java
@@ -0,0 +1,9 @@
+package com.baeldung.ignore;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public abstract class BaseUnitHelper {
+
+}
---
+++ testing-modules/junit-4/src/test/java/com/baeldung/ignore/IgnoreBaseUnitTest.java
@@ -0,0 +1,11 @@
+package com.baeldung.ignore;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@Ignore
+public abstract class IgnoreBaseUnitTest {
+
+}
