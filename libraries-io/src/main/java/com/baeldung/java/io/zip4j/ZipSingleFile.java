8c7827bebd4d977f2fd6f5cae3bf76513058f432
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2021-09-16 15:41:15 +0300

    BAEL-5033 close resources, create file

--- libraries-io/src/main/java/com/baeldung/java/io/zip4j/ZipSingleFile.java
+++ libraries-io/src/main/java/com/baeldung/java/io/zip4j/ZipSingleFile.java
@@ -1,21 +1,22 @@
 package com.baeldung.java.io.zip4j;
 
 import net.lingala.zip4j.ZipFile;
-import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.model.enums.CompressionLevel;
 import net.lingala.zip4j.model.enums.EncryptionMethod;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ZipSingleFile {
 
-    public static void main(String[] args) throws ZipException {
+    public static void main(String[] args) throws IOException {
         ZipParameters zipParameters = new ZipParameters();
         zipParameters.setEncryptFiles(true);
         zipParameters.setCompressionLevel(CompressionLevel.HIGHER);
         zipParameters.setEncryptionMethod(EncryptionMethod.AES);
         ZipFile zipFile = new ZipFile("compressed.zip", "password".toCharArray());
         zipFile.addFile(new File("aFile.txt"));
+        zipFile.close();
     }
 }
