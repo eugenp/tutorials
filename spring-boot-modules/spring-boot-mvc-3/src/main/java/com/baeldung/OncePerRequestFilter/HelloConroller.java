0094784cd7f3eab7dcfaa81876bca42621e0dab6
nishitgoyal17 <nits.gyl@gmail.com>
2021-09-22 23:14:39 +0530

    OncePerReqFilter

--- spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/OncePerRequestFilter/HelloConroller.java
+++ spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/OncePerRequestFilter/HelloConroller.java
@@ -2,18 +2,43 @@ package com.baeldung.OncePerRequestFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller
-public class HelloConroller {
+public class HelloConroller implements AutoCloseable {
+
+    private final ExecutorService executorService = Executors
+            .newCachedThreadPool();
 
     private Logger logger = LoggerFactory.getLogger(HelloConroller.class);
 
-    @GetMapping("/hello")
-    public String hello() {
-        logger.info("Hello from the controller");
-        return "hello";
+    @GetMapping(path = "/greeting1")
+    public DeferredResult<String> hello1(HttpServletResponse response) throws Exception{
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        executorService.submit(() -> perform(deferredResult));
+        return deferredResult;
     }
 
+    private void perform(DeferredResult<String> dr) {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        dr.setResult("OK");
+    }
+
+    @Override
+    public void close() throws Exception {
+        executorService.shutdownNow();
+    }
 }
--- spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/OncePerRequestFilter/MyOncePerRequestFilter.java
+++ spring-boot-modules/spring-boot-mvc-3/src/main/java/com/baeldung/OncePerRequestFilter/MyOncePerRequestFilter.java
@@ -2,32 +2,26 @@ package com.baeldung.OncePerRequestFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-public class MyFilter extends OncePerRequestFilter {
-    private Logger logger = LoggerFactory.getLogger(MyFilter.class);
+@Component
+public class MyOncePerRequestFilter extends OncePerRequestFilter {
+    private Logger logger = LoggerFactory.getLogger(MyOncePerRequestFilter.class);
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        logger.info("Inside Filter");
+        logger.info("Inside OncePer Request Filter originated by request {}",request.getRequestURI());
         filterChain.doFilter(request, response);
     }
+    protected boolean shouldNotFilterAsyncDispatch() { return false; }
 
 
-    //This piece of code is commented out.SHould be uncommented if we are using OncePerRequestFilter
-   /* @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        logger.info("Inside Filter");
-        filterChain.doFilter(servletRequest, servletResponse);
-    }*/
 }
 
