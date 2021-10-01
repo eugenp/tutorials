82d0c5d90e581e2129265f91ca97cdf43eeb4083
0swald <andrey.bichkevskiy@gmail.com>
2021-10-01 14:10:21 -0500

    BAEL-5143

---
+++ rule-engines/evrete/src/main/java/com/baeldung/evrete/introduction/IntroductionInline.java
@@ -0,0 +1,61 @@
+package com.baeldung.evrete.introduction;
+
+import org.evrete.KnowledgeService;
+import org.evrete.api.Knowledge;
+import com.baeldung.evrete.introduction.model.*;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
+public class IntroductionInline {
+    public static void main(String[] args) {
+        KnowledgeService service = new KnowledgeService();
+        Knowledge knowledge = service
+                .newKnowledge()
+                .newRule("Clear total sales")
+                .forEach("$c", Customer.class)
+                .execute(ctx -> {
+                    Customer c = ctx.get("$c");
+                    c.setTotal(0.0);
+                })
+                .newRule("Compute totals")
+                .forEach(
+                        "$c", Customer.class,
+                        "$i", Invoice.class
+                )
+                .where("$i.customer == $c")
+                .execute(ctx -> {
+                    Customer c = ctx.get("$c");
+                    Invoice i = ctx.get("$i");
+                    c.addToTotal(i.getAmount());
+                });
+
+
+        List<Customer> customers = Arrays.asList(
+                new Customer("Customer A"),
+                new Customer("Customer B"),
+                new Customer("Customer C")
+        );
+
+        Random random = new Random();
+        List<Object> sessionData = new LinkedList<>(customers);
+        for (int i = 0; i < 100_000; i++) {
+            Customer randomCustomer = customers.get(random.nextInt(customers.size()));
+            Invoice invoice = new Invoice(randomCustomer, 100 * random.nextDouble());
+            sessionData.add(invoice);
+        }
+
+        knowledge
+                .newStatelessSession()
+                .insert(sessionData)
+                .fire();
+
+        for (Customer c : customers) {
+            System.out.printf("%s:\t$%,.2f%n", c.getName(), c.getTotal());
+        }
+
+        service.shutdown();
+    }
+}
