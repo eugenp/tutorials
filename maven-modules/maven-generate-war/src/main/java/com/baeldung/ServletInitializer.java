fc42d4ad1bcce6d79c8fe4a277841fbb65f57573
anuradha <anuradhap@intervest.lk>
2021-10-01 23:55:41 +0530

    Refactored code

--- maven-modules/maven-generate-war/src/main/java/com/baeldung/ServletInitializer.java
+++ maven-modules/maven-generate-war/src/main/java/com/baeldung/ServletInitializer.java
@@ -5,9 +5,9 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MavenGenerateWarApplication.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MavenGenerateWarApplication.class);
+    }
 
 }
