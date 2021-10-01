eae33a7730b0ddb1e353f83e990adc4990987631
anuradha <anuradhap@intervest.lk>
2021-10-01 23:55:41 +0530

    Refactored code

--- maven-modules/maven-generate-war/src/main/java/com/baeldung/MavenGenerateWarApplication.java
+++ maven-modules/maven-generate-war/src/main/java/com/baeldung/MavenGenerateWarApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MavenGenerateWarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MavenGenerateWarApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MavenGenerateWarApplication.class, args);
+    }
 
 }
