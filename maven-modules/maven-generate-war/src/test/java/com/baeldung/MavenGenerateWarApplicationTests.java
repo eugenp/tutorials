79963c7cf0e5c0ec8bacf80c66540d499e6b2699
anuradha <anuradhap@intervest.lk>
2021-10-05 09:19:36 +0530

    Added space indents

--- maven-modules/maven-generate-war/src/test/java/com/baeldung/MavenGenerateWarApplicationTests.java
+++ maven-modules/maven-generate-war/src/test/java/com/baeldung/MavenGenerateWarApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MavenGenerateWarApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
