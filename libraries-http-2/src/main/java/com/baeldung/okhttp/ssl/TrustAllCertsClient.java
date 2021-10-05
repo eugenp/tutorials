d57455f3ab543e64f8d5059e0621ff167f0d3e86
tienvn4 <tienvn4@ghtk.co>
2021-10-05 12:01:02 +0700

    refactor

--- libraries-http-2/src/main/java/com/baeldung/okhttp/ssl/TrustAllCertsClient.java
+++ libraries-http-2/src/main/java/com/baeldung/okhttp/ssl/TrustAllCertsClient.java
@@ -14,20 +14,20 @@ public class TrustAllCertsClient {
 
     public static OkHttpClient getTrustAllCertsClient() throws NoSuchAlgorithmException, KeyManagementException {
         TrustManager[] trustAllCerts = new TrustManager[]{
-                new X509TrustManager() {
-                    @Override
-                    public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
-                    }
-
-                    @Override
-                    public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
-                    }
-
-                    @Override
-                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                        return new java.security.cert.X509Certificate[]{};
-                    }
+            new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) {
                 }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) {
+                }
+
+                @Override
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return new java.security.cert.X509Certificate[]{};
+                }
+            }
         };
 
         SSLContext sslContext = SSLContext.getInstance("SSL");
