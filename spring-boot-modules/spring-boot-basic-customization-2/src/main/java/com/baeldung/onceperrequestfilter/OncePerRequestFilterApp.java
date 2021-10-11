2291f8023be9ee16af2a42fd84084f9460056b17
nishitgoyal17 <nits.gyl@gmail.com>
2021-10-11 12:32:31 +0530

    OncePerRequest changes

--- spring-boot-modules/spring-boot-basic-customization-2/src/main/java/com/baeldung/onceperrequestfilter/OncePerRequestFilterApp.java
+++ spring-boot-modules/spring-boot-basic-customization-2/src/main/java/com/baeldung/onceperrequestfilter/OncePerRequestFilterApp.java
@@ -1,10 +1,10 @@
-package com.baeldung.OncePerRequestFilter;
+package com.baeldung.onceperrequestfilter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication(scanBasePackages = "com.baeldung.OncePerRequestFilter")
+@SpringBootApplication(scanBasePackages = "com.baeldung.onceperrequestfilter")
 public class OncePerRequestFilterApp {
     public static void main(String[] args) {
         SpringApplication.run(OncePerRequestFilterApp.class, args);
