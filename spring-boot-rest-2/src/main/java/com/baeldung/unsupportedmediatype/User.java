1c38ab94a1d3c67c2fc47789401ee8c9d127a9ad
Muhammad Abdullah Azam Khan <abdullahazamkhan@gmail.com>
2021-10-14 01:15:13 +0400

    415 Unsupported Media Type
    1. Basic user controller added
    
    2. POST API explains the how to support differnet content-type formats

---
+++ spring-boot-rest-2/src/main/java/com/baeldung/unsupportedmediatype/User.java
@@ -0,0 +1,61 @@
+package com.baeldung.unsupportedmediatype;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement
+public class User implements Serializable {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String address;
+
+    public User(Integer id, String name, Integer age, String address){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
