96fb3150e7058db82d03208e05b930d1474bfd1d
Muhammad Abdullah Azam Khan <abdullahazamkhan@gmail.com>
2021-10-14 01:15:13 +0400

    415 Unsupported Media Type
    1. Basic user controller added
    
    2. POST API explains the how to support differnet content-type formats

---
+++ spring-boot-rest-2/src/main/java/com/baeldung/unsupportedmediatype/UnsupportedMediaTypeApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.unsupportedmediatype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsupportedMediaTypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsupportedMediaTypeApplication.class, args);
+	}
+
+}
