a045ed05d6a3c2ab39e6c72b49f1c15d5a1273da
arash.sw90@gmail.com <arash.sw90@gmail.com>
2021-10-15 16:00:12 +0330

    BAEL-5058 : apache-derby added to dir

---
+++ persistence-modules/apache-derby/src/test/java/com/baeldung/derby/DerbyApplicationDemoUnitTest.java
@@ -0,0 +1,15 @@
+package com.baeldung.derby;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.*;
+
+
+class DerbyApplicationDemoUnitTest {
+
+    @Before
+    public void init(){
+
+    }
+}
