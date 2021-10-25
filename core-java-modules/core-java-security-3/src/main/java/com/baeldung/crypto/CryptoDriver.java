06e948db2f4063865c486b19aaf3c35de01fbb2b
Trying something new <50325284+tancafa@users.noreply.github.com>
2021-10-25 11:43:40 +0530

    JAVA-4486, unit tests
    Added Unit Test
    Corrected the formatting using formatter.xml in eclipse

--- core-java-modules/core-java-security-3/src/main/java/com/baeldung/crypto/CryptoDriver.java
+++ core-java-modules/core-java-security-3/src/main/java/com/baeldung/crypto/CryptoDriver.java
@@ -9,135 +9,81 @@ import java.security.GeneralSecurityException;
 import com.baeldung.crypto.utils.CryptoUtils;
 
 public class CryptoDriver {
-	
-	private byte[] ecbEncrypt(SecretKey key, byte[] data) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, key);
-		return cipher.doFinal(data);
-	}
 
-	private byte[] ecbDecrypt(SecretKey key, byte[] cipherText) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, key);
-		return cipher.doFinal(cipherText);
-	}
+    public byte[] ecbEncrypt(SecretKey key, byte[] data) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.doFinal(data);
+    }
 
-	public void encryptAesEcb() throws GeneralSecurityException {
-		SecretKey key = CryptoUtils.generateKey();
-		byte[] plaintext = ("Encrypt this").getBytes();
-		byte[] ciphertext = ecbEncrypt(key, plaintext);
-		byte[] decryptedtext = ecbDecrypt(key, ciphertext);
-	}
+    public byte[] ecbDecrypt(SecretKey key, byte[] cipherText) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        return cipher.doFinal(cipherText);
+    }
 
-	private byte[] cbcEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    public byte[] cbcEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
-		cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-		return cipher.doFinal(data);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        return cipher.doFinal(data);
+    }
 
-	}
+    public byte[] cbcDecrypt(SecretKey key, IvParameterSpec iv, byte[] cipherText) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return cipher.doFinal(cipherText);
+    }
 
-	private byte[] cbcDecrypt(SecretKey key, IvParameterSpec iv, byte[] cipherText) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, key, iv);
-		return cipher.doFinal(cipherText);
-	}
+    public byte[] cfbEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        return cipher.doFinal(data);
+    }
 
-	public void aesCbc() throws GeneralSecurityException {
-		SecretKey key = CryptoUtils.generateKey();
-		IvParameterSpec iv = CryptoUtils.getIVSecureRandom("AES");
-		byte[] plaintext = ("Encrypt this block for testing").getBytes();
+    public byte[] cfbDecrypt(SecretKey key, IvParameterSpec iv, byte[] cipherText) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return cipher.doFinal(cipherText);
+    }
 
-		byte[] ciphertext = cbcEncrypt(key, iv, plaintext);
-		byte[] decryptedtext = cbcDecrypt(key, iv, ciphertext);
-	}
+    public byte[] ofbEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/OFB32/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        return cipher.doFinal(data);
+    }
 
-	private byte[] cfbEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding");
-		cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-		return cipher.doFinal(data);
-	}
+    public byte[] ofbDecrypt(SecretKey key, IvParameterSpec iv, byte[] cipherText) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/OFB32/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return cipher.doFinal(cipherText);
+    }
 
-	private byte[] cfbDecrypt(SecretKey key, IvParameterSpec iv, byte[] cipherText) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/CFB/NoPadding");
-		cipher.init(Cipher.DECRYPT_MODE, key, iv);
-		return cipher.doFinal(cipherText);
-	}
+    public byte[][] ctrEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        return new byte[][] { cipher.getIV(), cipher.doFinal(data) };
+    }
 
-	public void aesCfb() throws GeneralSecurityException {
-		SecretKey key = CryptoUtils.generateKey();
-		IvParameterSpec iv = CryptoUtils.getIVSecureRandom("AES/CFB/NoPadding");
-		byte[] plaintext = ("Encrypt this for testing").getBytes();
+    public byte[] ctrDecrypt(SecretKey key, byte[] iv, byte[] cipherText) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+        return cipher.doFinal(cipherText);
+    }
 
-		byte[] ciphertext = cfbEncrypt(key, iv, plaintext);
-		byte[] decryptedtext = cfbDecrypt(key, iv, ciphertext);
-	}
+    public byte[][] gcmEncrypt(SecretKey key, byte[] iv, byte[] data) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(128, iv));
+        byte[] ciphertext = cipher.doFinal(data);
+        return new byte[][] { iv, ciphertext };
+    }
 
-	private byte[] ofbEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/OFB32/PKCS5Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-		return cipher.doFinal(data);
-	}
+    public byte[] gcmDecrypt(SecretKey key, byte[] iv, byte[] ciphertext) throws GeneralSecurityException {
 
-	private byte[] ofbDecrypt(SecretKey key, IvParameterSpec iv, byte[] cipherText) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/OFB32/PKCS5Padding");
-		cipher.init(Cipher.DECRYPT_MODE, key, iv);
-		return cipher.doFinal(cipherText);
-	}
-
-	public void aesOfb() throws GeneralSecurityException {
-		SecretKey key = CryptoUtils.generateKey();
-		IvParameterSpec iv = CryptoUtils.getIVSecureRandom("AES/OFB32/PKCS5Padding");
-		byte[] plaintext = ("Encrypt this for testing").getBytes();
-
-		byte[] ciphertext = ofbEncrypt(key, iv, plaintext);
-		byte[] decryptedtext = ofbDecrypt(key, iv, ciphertext);
-	}
-
-	public static byte[][] ctrEncrypt(SecretKey key, IvParameterSpec iv, byte[] data) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-		cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-		return new byte[][] { cipher.getIV(), cipher.doFinal(data) };
-	}
-
-	public static byte[] ctrDecrypt(SecretKey key, byte[] iv, byte[] cipherText) throws GeneralSecurityException {
-		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-		cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
-		return cipher.doFinal(cipherText);
-	}
-	
-	public void aesCtr() throws GeneralSecurityException {
-		SecretKey key = CryptoUtils.generateKey();
-		IvParameterSpec iv = CryptoUtils.getIVSecureRandom("AES/CTR/NoPadding");
-		byte[] plaintext = ("Encrypt this for testing").getBytes();
-
-		byte[][] ciphertext = ctrEncrypt(key, iv, plaintext);
-		byte[] decryptedtext = ctrDecrypt(key, ciphertext[0], ciphertext[1]);
-	}
-	
-	public static byte[][] gcmEncrypt(SecretKey key, byte[] iv,  byte[] data) throws GeneralSecurityException {
-	       Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-	       cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(128, iv));
-	        byte[] ciphertext = cipher.doFinal(data);
-	        return new byte[][] {
-	        	iv, ciphertext
-	        };	
-	}
-
-	public static byte[] gcmDecrypt(SecretKey key, byte[] iv, byte[] ciphertext) throws GeneralSecurityException {
-	
-		Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
-		cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv));
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv));
         byte[] plaintext = cipher.doFinal(ciphertext);
-        return plaintext;	
-	}
-	
-	public void aesGcm() throws GeneralSecurityException {
-		SecretKey key = CryptoUtils.generateKey();
-		byte[] iv = CryptoUtils.getRandomIVWithSize(12);
-		byte[] plaintext = ("Encrypt this for testing").getBytes();
+        return plaintext;
+    }
 
-		byte[][] ciphertext = gcmEncrypt(key, iv, plaintext);
-		byte[] decryptedtext = gcmDecrypt(key, ciphertext[0], ciphertext[1]);
-	}
 }
--- core-java-modules/core-java-security-3/src/main/java/com/baeldung/crypto/utils/CryptoUtils.java
+++ core-java-modules/core-java-security-3/src/main/java/com/baeldung/crypto/utils/CryptoUtils.java
@@ -14,36 +14,40 @@ import javax.crypto.spec.IvParameterSpec;
 
 public class CryptoUtils {
 
-	public static SecretKey generateKey() throws GeneralSecurityException {
-		KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-		keyGenerator.init(256);
-		return keyGenerator.generateKey();
-	}
-	public static IvParameterSpec getIV() {
-		SecureRandom secureRandom = new SecureRandom();
-		byte[] iv = new byte[128 / 8];
-		byte[] nonce = new byte[96 / 8];
-		secureRandom.nextBytes(nonce);
-		System.arraycopy(nonce, 0, iv, 0, nonce.length);
-		return new IvParameterSpec(nonce);
-	}
+    public static SecretKey generateKey() throws GeneralSecurityException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(256);
+        return keyGenerator.generateKey();
+    }
 
-	public static IvParameterSpec getIVSecureRandom(String algorithm) throws NoSuchAlgorithmException, NoSuchPaddingException {
-		SecureRandom random = SecureRandom.getInstanceStrong();
-		byte[] iv = new byte[Cipher.getInstance(algorithm).getBlockSize()];
-		random.nextBytes(iv);
-	    return new IvParameterSpec(iv);
-	}
-	public static IvParameterSpec getIVInternal(Cipher cipher) throws InvalidParameterSpecException {
-		AlgorithmParameters params = cipher.getParameters();
-		byte[] iv = params.getParameterSpec(IvParameterSpec.class).getIV();
-		return new IvParameterSpec(iv);
-	}
-	
-	public static byte[] getRandomIVWithSize(int size) {
-	        byte[] nonce = new byte[size];
-	        new SecureRandom().nextBytes(nonce);
-	        return nonce;
-	  }
-	
+    public static IvParameterSpec getIV() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[128 / 8];
+        byte[] nonce = new byte[96 / 8];
+        secureRandom.nextBytes(nonce);
+        System.arraycopy(nonce, 0, iv, 0, nonce.length);
+        return new IvParameterSpec(nonce);
+    }
+
+    public static IvParameterSpec getIVSecureRandom(String algorithm) throws NoSuchAlgorithmException, NoSuchPaddingException {
+        SecureRandom random = SecureRandom.getInstanceStrong();
+        byte[] iv = new byte[Cipher.getInstance(algorithm)
+            .getBlockSize()];
+        random.nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    public static IvParameterSpec getIVInternal(Cipher cipher) throws InvalidParameterSpecException {
+        AlgorithmParameters params = cipher.getParameters();
+        byte[] iv = params.getParameterSpec(IvParameterSpec.class)
+            .getIV();
+        return new IvParameterSpec(iv);
+    }
+
+    public static byte[] getRandomIVWithSize(int size) {
+        byte[] nonce = new byte[size];
+        new SecureRandom().nextBytes(nonce);
+        return nonce;
+    }
+
 }
