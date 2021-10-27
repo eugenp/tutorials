2b28fffb24ea90ff92da461cd01a9418e9050b4a
Vlad Fernoaga <vlad.fernoaga-external@getyourguide.com>
2021-10-27 13:35:09 +0300

    initial project structure

---
+++ hexagonal-architecture-in-java/src/main/java/com/baeldung/hexagonalarchitectureinjava/HexagonalArchitectureInJavaApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.hexagonalarchitectureinjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureInJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalArchitectureInJavaApplication.class, args);
+	}
+
+}
