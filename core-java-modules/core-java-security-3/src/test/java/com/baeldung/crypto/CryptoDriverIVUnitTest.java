96450043a7700dabdde619b3f2417c0be56fdc09
Trying something new <50325284+tancafa@users.noreply.github.com>
2021-10-28 10:08:59 +0530

    Update CryptoDriverIVUnitTest.java
    Resubmitting CryptoDriverIVUnitTest

--- core-java-modules/core-java-security-3/src/test/java/com/baeldung/crypto/CryptoDriverIVUnitTest.java
+++ core-java-modules/core-java-security-3/src/test/java/com/baeldung/crypto/CryptoDriverIVUnitTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import com.baeldung.crypto.utils.CryptoUtils;
 
-public class CrytoDriverIVUnitTest {
+public class CryptoDriverIVUnitTest{
     private CryptoDriver driver = new CryptoDriver();
     private String TEST_DATA = "Encrypt this for testing";
 
