e29cfda933ba0ecbc16d969dba0bb53b9ccd8683
Swapan Pramanick <swapan.pramanick@zalando.de>
2021-10-30 02:01:47 +0200

    BAEL-4546: Reverting evaluation article code

--- ddd/src/main/java/com/baeldung/hexagonal/port/BookingPersistencePort.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.hexagonal.port;
-
-import com.baeldung.hexagonal.domain.Booking;
-
-public interface BookingPersistencePort {
-    boolean persist(Booking booking);
-    public boolean updateStatus(String bookingId, String status);
-}
