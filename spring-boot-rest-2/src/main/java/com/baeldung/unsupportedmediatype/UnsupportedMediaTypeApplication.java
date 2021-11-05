df4c4a3f8d5a055598a3d1651ca022d8e5c0af4a
Muhammad Abdullah Azam Khan <abdullahazamkhan@gmail.com>
2021-11-05 18:32:19 +0400

    Refactoring for better Indentation and Formatting

--- spring-boot-rest-2/src/main/java/com/baeldung/unsupportedmediatype/UnsupportedMediaTypeApplication.java
+++ spring-boot-rest-2/src/main/java/com/baeldung/unsupportedmediatype/UnsupportedMediaTypeApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UnsupportedMediaTypeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UnsupportedMediaTypeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UnsupportedMediaTypeApplication.class, args);
+    }
 
 }
