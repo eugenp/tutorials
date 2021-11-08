0ea2f13c3436eee85793074b09df428048b67377
LUCA <lucacambi77@gmail.com>
2021-11-08 11:15:30 +0100

    fix: review comments

--- testing-modules/testing-assertions/src/test/java/com/baeldung/optional/OptionalUnitTest.java
+++ testing-modules/testing-assertions/src/test/java/com/baeldung/optional/OptionalUnitTest.java
@@ -9,61 +9,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class OptionalUnitTest {
+class OptionalUnitTest {
 
-    private final String stringToTest = "testString";
+    private static final String optionalReturnString = "optionalReturnString";
 
     @Test
     public void givenOptionalWithValue_whenAssertThatIsNotEmpty_thenOk() {
-        assertThat(optionalWithValueMethodToTest()).isNotEmpty();
+        assertThat(Optional.of(optionalReturnString)).isNotEmpty();
     }
 
     @Test
     public void givenOptionalWithValue_whenAssertThatHasValue_thenOk() {
-        assertThat(optionalWithValueMethodToTest()).hasValue(stringToTest);
+        assertThat(Optional.of(optionalReturnString)).hasValue(optionalReturnString);
     }
 
     @Test
     public void givenOptionalWithValue_whenAssertEqualsOptionalObject_thenOk() {
-        assertEquals(Optional.of(stringToTest), optionalWithValueMethodToTest());
+        assertEquals(Optional.of(optionalReturnString), Optional.of(optionalReturnString));
     }
 
     @Test
     public void givenOptionalWithValue_whenAssertEqualsGet_thenOk() {
-        Optional<String> optional = optionalWithValueMethodToTest();
-        assertEquals(stringToTest, optional.get());
-    }
-
-    @Test
-    public void givenOptionalWithValue_whenAssertTrueIsPresentAndGet_thenOk() {
-        Optional<String> optional = optionalWithValueMethodToTest();
-        assertTrue(optional.isPresent() && stringToTest.equals(optional.get()));
+        Optional<String> optional = Optional.of(optionalReturnString);
+        assertEquals(optionalReturnString, optional.get());
     }
 
     @Test
     public void givenOptionalWithValue_whenIsPresentAndGetSplit_thenOk() {
-        Optional<String> optional = optionalWithValueMethodToTest();
+        Optional<String> optional = Optional.of(optionalReturnString);
 
         assertTrue(optional.isPresent());
-        assertEquals(stringToTest, optional.get());
+        assertEquals(optionalReturnString, optional.get());
     }
 
     @Test
     public void givenEmptyOptional_whenAssertThatIsEmpty_thenOk() {
-        assertThat(optionalIsEmptyMethodToTest()).isEmpty();
+        Optional<String> emptyOptional = Optional.empty();
+        assertThat(emptyOptional).isEmpty();
     }
 
     @Test
     public void givenEmptyOptional_whenAssertIsNotPresent_thenOk() {
-        Optional<String> optional = optionalIsEmptyMethodToTest();
-        assertFalse(optional.isPresent());
-    }
-
-    private Optional<String> optionalWithValueMethodToTest() {
-        return Optional.of(stringToTest);
-    }
-
-    private Optional<String> optionalIsEmptyMethodToTest() {
-        return Optional.empty();
+        Optional<String> emptyOptional = Optional.empty();
+        assertFalse(emptyOptional.isPresent());
     }
 }
