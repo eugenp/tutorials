c5f676c6ccc9fb78245193555a0a024640e9aeb4
Mladen Savic <mladensavic94@gmail.com>
2021-11-08 17:06:17 +0100

    Spring Webflux and @Cacheable Annotation

---
+++ spring-webflux-caching/src/main/java/com/baeldung/caching/ItemRepository.java
@@ -0,0 +1,8 @@
+package com.baeldung.caching;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends ReactiveMongoRepository<Item, String> {
+}
