cdfd90b0b856d21e0e61cc554f2ccfc88cd2e2a6
ioanadinuit <ioana.dinu.it@gmail.com>
2021-11-09 17:17:14 +0200

    BAEL-5080 Custom validation with Swagger Codegen
    Formatting

--- spring-swagger-codegen/custom-validations-opeanpi-codegen/src/main/java/com/baeldung/openapi/petstore/controller/PetController.java
+++ spring-swagger-codegen/custom-validations-opeanpi-codegen/src/main/java/com/baeldung/openapi/petstore/controller/PetController.java
@@ -1,17 +1,17 @@
 package com.baeldung.openapi.petstore.controller;
 
-import com.baeldung.openapi.api.PetApi;
+import com.baeldung.openapi.api.PetsApi;
 import com.baeldung.openapi.model.Pet;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
-public class PetController implements PetApi {
+public class PetController implements PetsApi {
 
     public ResponseEntity<List<Pet>> findPetsByName(String name) {
-        return new ResponseEntity<>(HttpStatus.OK);
+        return ResponseEntity.ok(Arrays.asList(new Pet().id(1L).name(name)));
     }
 }
