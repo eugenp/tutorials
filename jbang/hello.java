48be3e092aa79b35b8116c7fe3d5a0ce6ce7eb8e
Max Rydahl Andersen <max@xam.dk>
2021-11-14 11:47:06 +0100

    initial jbang example

---
+++ jbang/hello.java
@@ -0,0 +1,11 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+// //DEPS <dependency1> <dependency2>
+
+import static java.lang.System.*;
+
+public class hello {
+
+    public static void main(String... args) {
+        out.println("Hello World");
+    }
+}
