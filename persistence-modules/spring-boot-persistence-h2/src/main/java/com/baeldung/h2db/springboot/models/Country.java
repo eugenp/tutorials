b4836ce7d9fff1a70b5260a03985e6a316368f5c
Haroon Khan <haroonkhan79@gmail.com>
2021-11-18 13:23:25 +0000

    [JAVA-6455] Remove Lombok

--- persistence-modules/spring-boot-persistence-h2/src/main/java/com/baeldung/h2db/springboot/models/Country.java
+++ persistence-modules/spring-boot-persistence-h2/src/main/java/com/baeldung/h2db/springboot/models/Country.java
@@ -1,15 +1,13 @@
 package com.baeldung.h2db.springboot.models;
 
-import lombok.Data;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 @Table(name = "countries")
 @Entity
-@Data
 public class Country {
 
     @Id
@@ -18,4 +16,42 @@ public class Country {
 
     private String name;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Country country = (Country) o;
+        return id == country.id && name.equals(country.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Country{" +
+          "id=" + id +
+          ", name='" + name + '\'' +
+          '}';
+    }
 }
