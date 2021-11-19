5994b9f06a45a1cea2f666f76a2df60c8b829ee9
Vaibhav Jain <vaibhav007jain@gmail.com>
2021-11-19 11:18:54 +0530

    BAEL-5228: renamed UT

--- core-java-modules/core-java-string-operations-4/src/test/java/com/baeldung/concatenation/ConcatenatingNullUnitTest.java
+++ core-java-modules/core-java-string-operations-4/src/test/java/com/baeldung/concatenation/ConcatenatingNullUnitTest.java
@@ -11,48 +11,48 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class ConcatenatingNullTest {
+public class ConcatenatingNullUnitTest {
 
     String[] values = { "Java ", null, "", "is ", "great!" };
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingPlus_thenNullIsRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingPlus_thenNullIsIgnored() {
         String result = concatenateUsingPlusOperator(values);
         assertEquals("Java is great!", result);
     }
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingHelperMethod_thenNullIsRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingHelperMethod_thenNullIsIgnored() {
         String result = concatenateUsingHelperMethod(values);
         assertEquals("Java is great!", result);
     }
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingStringBuilder_thenNullIsRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingStringBuilder_thenNullIsIgnored() {
         String result = concatenateUsingStringBuilder(values);
         assertEquals("Java is great!", result);
     }
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingJoin_thenNullIsNotRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingJoin_thenNullIsNotIgnored() {
         String result = concatenateUsingJoin(values);
         assertEquals("Java nullis great!", result);
     }
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingStringJoiner_thenNullIsRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingStringJoiner_thenNullIsIgnored() {
         String result = concatenateUsingStringJoiner(values);
         assertEquals("Java is great!", result);
     }
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingCollectorsJoining_thenNullIsRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingCollectorsJoining_thenNullIsIgnored() {
         String result = concatenateUsingCollectorsJoining(values);
         assertEquals("Java is great!", result);
     }
-    
+
     @Test
-    public void givenStringElementsWithNull_whenConcatenatedUsingStringConcat_thenNullIsRemoved() {
+    public void givenStringElementsWithNull_whenConcatenatedUsingStringConcat_thenNullIsIgnored() {
         String result = concatenateUsingStringConcat(values);
         assertEquals("Java is great!", result);
     }
