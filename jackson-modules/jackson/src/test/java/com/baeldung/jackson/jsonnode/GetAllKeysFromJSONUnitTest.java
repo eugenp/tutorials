ecbb78d1eb294ab6bc7b61e12b00fea0bab27258
priya-soni <erpsoni7@gmail.com>
2021-11-21 10:42:40 +0530

    BAEL - 5191 - Updated exception handling

--- jackson-modules/jackson/src/test/java/com/baeldung/jackson/jsonnode/GetAllKeysFromJSONUnitTest.java
+++ jackson-modules/jackson/src/test/java/com/baeldung/jackson/jsonnode/GetAllKeysFromJSONUnitTest.java
@@ -2,10 +2,12 @@ package com.baeldung.jackson.jsonnode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class GetAllKeysFromJSONUnitTest {
@@ -16,41 +18,68 @@ public class GetAllKeysFromJSONUnitTest {
         + "         }\r\n" + "      ]\r\n" + "   }\r\n" + "}";
 
     private static ObjectMapper mapper = new ObjectMapper();
+    private static GetAllKeysFromJSON getAllKeysFromJSONUtil = new GetAllKeysFromJSON();
 
     // Top level keys : [Name, Age, BookInterests, FoodInterests]
     // All keys: [Name, Age, BookInterests, Book, Author, Book, Author, FoodInterests, Breakfast, Bread, Beverage, Sandwich, Beverage]
 
     @Test
     public void givenAJsonNode_whenUsingFieldNamesMethod_thenWeGetTopFieldNames() {
-        List<String> keys = GetAllKeysFromJSON.getKeysInJsonUsingJsonNodeFieldNames(json, mapper);
-        assertEquals(4, keys.size());
+        List<String> keys;
+        try {
+            keys = getAllKeysFromJSONUtil.getKeysInJsonUsingJsonNodeFieldNames(json, mapper);
+            assertEquals(4, keys.size());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void givenAJsonNode_whenUsingFieldNamesMethodForAllNodes_thenWeGetAllFieldNames() {
-        List<String> keys = GetAllKeysFromJSON.getAllKeysInJsonUsingJsonNodeFieldNames(json, mapper);
-        assertEquals(13, keys.size());
+        List<String> keys;
+        try {
+            keys = getAllKeysFromJSONUtil.getAllKeysInJsonUsingJsonNodeFieldNames(json, mapper);
+            assertEquals(13, keys.size());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void givenAJsonNode_whenUsingFieldsMethod_thenWeGetAllFieldNames() {
-        List<String> keys = GetAllKeysFromJSON.getAllKeysInJsonUsingJsonNodeFields(json, mapper);
-        assertEquals(13, keys.size());
+        List<String> keys;
+        try {
+            keys = getAllKeysFromJSONUtil.getAllKeysInJsonUsingJsonNodeFields(json, mapper);
+            assertEquals(13, keys.size());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void givenAJsonNode_whenUsingJsonParserMethod_thenWeGetAllFieldNames() {
-        List<String> keys = GetAllKeysFromJSON.getKeysInJsonUsingJsonParser(json, mapper);
-        assertEquals(13, keys.size());
+        List<String> keys;
+        try {
+            keys = getAllKeysFromJSONUtil.getKeysInJsonUsingJsonParser(json, mapper);
+            assertEquals(13, keys.size());
 
-        keys = GetAllKeysFromJSON.getKeysInJsonUsingJsonParser(json);
-        assertEquals(13, keys.size());
+            keys = getAllKeysFromJSONUtil.getKeysInJsonUsingJsonParser(json);
+            assertEquals(13, keys.size());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Test
     public void givenAJsonNode_whenUsingMaps_thenWeGetAllFieldNames() {
-        List<String> keys = GetAllKeysFromJSON.getKeysInJsonUsingMaps(json, mapper);
-        assertEquals(13, keys.size());
+        List<String> keys;
+        try {
+            keys = getAllKeysFromJSONUtil.getKeysInJsonUsingMaps(json, mapper);
+            assertEquals(13, keys.size());
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
     }
 
 }
