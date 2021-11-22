3ec32e3ff8ee83ee84c2ef09a2f4efeb50706b3a
Jason <jasonsaparamadu@yahoo.com>
2021-11-22 08:56:43 +0530

    BAEL-5138: Implementation code for Lombok @With Methods

---
+++ lombok/src/main/java/com/baeldung/lombok/with/User.java
@@ -0,0 +1,20 @@
+package com.baeldung.lombok.with;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.With;
+
+@Getter
+@AllArgsConstructor
+public class User {
+    private final String username;
+    private final String emailAddress;
+    @With
+    private final boolean isAuthenticated;
+
+    public User(String username, String emailAddress) {
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.isAuthenticated = false;
+    }
+}
