203f7a16bb0c50cb2359e4703153d780cd3a25b9
Krzysiek <krzysztofwoyke@gmail.com>
2021-11-24 22:52:41 +0100

    JAVA-8362: Copy batchscheduler code to spring-batch-2

---
+++ spring-batch-2/src/main/java/com/baeldung/batchscheduler/SpringBatchSchedulerApplication.java
@@ -0,0 +1,14 @@
+package com.baeldung.batchscheduler;
+
+import com.baeldung.batch.SpringBootBatchProcessingApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchSchedulerApplication.class, args);
+    }
+
+}
