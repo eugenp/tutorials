5500caa38084dc19436a06b9b9a75821f5886ff8
Krzysiek <krzysztofwoyke@gmail.com>
2021-11-24 23:20:22 +0100

    JAVA-8362: Cleanup

--- spring-batch-2/src/main/java/com/baeldung/batchscheduler/SpringBatchSchedulerApplication.java
+++ spring-batch-2/src/main/java/com/baeldung/batchscheduler/SpringBatchSchedulerApplication.java
@@ -1,6 +1,5 @@
 package com.baeldung.batchscheduler;
 
-import com.baeldung.batch.SpringBootBatchProcessingApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
