af150e03017b4539aeb4550039726ae774045194
Krzysiek <krzysztofwoyke@gmail.com>
2021-11-24 09:47:43 +0100

    JAVA-8664: Change ports numbers and cleanup the code

--- spring-boot-modules/spring-boot-ctx-fluent/src/main/java/com/baeldung/ctx2/Ctx2Controller.java
+++ spring-boot-modules/spring-boot-ctx-fluent/src/main/java/com/baeldung/ctx2/Ctx2Controller.java
@@ -1,18 +1,17 @@
 package com.baeldung.ctx2;
 
+import com.baeldung.parent.IHomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.baeldung.parent.IHomeService;
 
 @RestController
 public class Ctx2Controller {
 
     @Autowired
-    IHomeService homeService;
+    private IHomeService homeService;
 
-    @GetMapping(value = "/greeting", produces = "application/json")
+    @GetMapping(value = "/greeting")
     public String getGreeting() {
         return homeService.getGreeting();
     }
