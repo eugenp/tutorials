fab1638eebbdfab352c662410114e11f875f1355
Krzysiek <krzysztofwoyke@gmail.com>
2021-11-25 13:10:58 +0100

    JAVA-8363: Move etag code to spring-boot-mvc-3

---
+++ spring-boot-modules/spring-boot-mvc-2/src/main/java/com/baeldung/mime/WebConfig.java
@@ -0,0 +1,28 @@
+package com.baeldung.mime;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class WebConfig {
+
+    // Etags
+
+    // If we're not using Spring Boot we can make use of
+    // AbstractAnnotationConfigDispatcherServletInitializer#getServletFilters
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        FilterRegistrationBean<ShallowEtagHeaderFilter> filterRegistrationBean = new FilterRegistrationBean<>( new ShallowEtagHeaderFilter());
+        filterRegistrationBean.addUrlPatterns("/foos/*");
+        filterRegistrationBean.setName("etagFilter");
+        return filterRegistrationBean;
+    }
+    
+    // We can also just declare the filter directly
+    // @Bean
+    // public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+    // return new ShallowEtagHeaderFilter();
+    // }
+}
