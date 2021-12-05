7f17253b07adc3852967f8ada1c49f51d680a66f
sharifi <h_sharifi@modernisc.com>
2021-12-05 14:28:10 +0330

    bael-4197: fix bug

--- spring-boot-modules/spring-boot-testing/src/test/java/com/baeldung/xmlapplicationcontext/EmployeeServiceTestContextIntegrationTest.java
+++ spring-boot-modules/spring-boot-testing/src/test/java/com/baeldung/xmlapplicationcontext/EmployeeServiceTestContextIntegrationTest.java
@@ -2,14 +2,17 @@ package com.baeldung.xmlapplicationcontext;
 
 import com.baeldung.xmlapplicationcontext.service.EmployeeService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = XmlBeanApplication.class)
 @ContextConfiguration(locations = "/test-context.xml")
 public class EmployeeServiceTestContextIntegrationTest {
 
