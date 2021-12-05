63843c008c055e7d9da3efef2d5773e5316f3722
sharifi <h_sharifi@modernisc.com>
2021-12-05 13:58:13 +0330

    bael-4197: add main source

---
+++ spring-boot-modules/spring-boot-testing/src/main/java/com/baeldung/xmlapplicationcontext/domain/Employee.java
@@ -0,0 +1,32 @@
+package com.baeldung.xmlapplicationcontext.domain;
+
+public class Employee {
+
+    private String name;
+    private String role;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
