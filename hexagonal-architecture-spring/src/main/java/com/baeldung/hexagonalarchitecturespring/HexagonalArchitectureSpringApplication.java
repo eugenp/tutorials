c41789ca897a151bfb99baf9092b761ce048a10f
josephdsbr@gmail.com <josephdsbr@gmail.com>
2021-12-05 18:20:33 -0300

    - feat: Adding an example for Hexagonal Architecture using spring.

---
+++ hexagonal-architecture-spring/src/main/java/com/baeldung/hexagonalarchitecturespring/HexagonalArchitectureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.hexagonalarchitecturespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalArchitectureSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HexagonalArchitectureSpringApplication.class, args);
+    }
+
+}
