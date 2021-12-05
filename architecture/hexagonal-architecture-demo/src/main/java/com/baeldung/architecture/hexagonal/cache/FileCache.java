0cc7df159f566766a4a8152151b2a37f111d3411
Sameer <d7495b5879fca1ec1367476a5395a60a8e9baec9>
2021-12-05 20:37:07 +0530

    Using Slf4j directly instead of via lombok

--- architecture/hexagonal-architecture-demo/src/main/java/com/baeldung/architecture/hexagonal/cache/FileCache.java
+++ architecture/hexagonal-architecture-demo/src/main/java/com/baeldung/architecture/hexagonal/cache/FileCache.java
@@ -1,6 +1,7 @@
 package com.baeldung.architecture.hexagonal.cache;
 
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -11,8 +12,9 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Optional;
 
-@Slf4j
 public class FileCache implements Cache {
+    private static final Logger logger = LoggerFactory.getLogger(FileCache.class);
+
     private static final String baseCacheLocation = "/tmp/cache";
     private static final String cacheLocationFormat = "/tmp/cache/%s.cache";
 
@@ -20,9 +22,9 @@ public class FileCache implements Cache {
         try {
             Files.createDirectory(Paths.get(baseCacheLocation));
         } catch (FileAlreadyExistsException e) {
-            log.info("Cache location {} already exists.", baseCacheLocation);
+            logger.info("Cache location {} already exists.", baseCacheLocation);
         } catch (IOException e) {
-            log.warn("Unable to create base cache location. FileCache won't be available. Error Message: {}", e.getMessage());
+            logger.warn("Unable to create base cache location. FileCache won't be available. Error Message: {}", e.getMessage());
         }
     }
 
@@ -33,7 +35,7 @@ public class FileCache implements Cache {
         try {
             Files.writeString(cacheLocation, value, StandardCharsets.UTF_16, StandardOpenOption.CREATE_NEW);
         } catch (IOException e) {
-            log.warn("Unable to write to cache. Error Message: {}", e.getMessage());
+            logger.warn("Unable to write to cache. Error Message: {}", e.getMessage());
         }
     }
 
@@ -44,7 +46,7 @@ public class FileCache implements Cache {
         try {
             return Optional.of(Files.readString(cacheLocation, StandardCharsets.UTF_16));
         } catch (IOException e) {
-            log.warn("Unable to read from cache. Error Message: {}", e.getMessage());
+            logger.warn("Unable to read from cache. Error Message: {}", e.getMessage());
             return Optional.empty();
         }
     }
