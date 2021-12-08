1e9cc725707c2f39b8011999f884d5e59ca182e7
anuragkumawat <anuragkumawat7@gmail.com>
2021-12-08 18:27:42 +0530

    Removed the used code

--- spring-web-modules/spring-web-url/src/main/java/com/baeldung/exclude_urls_filter/Application.java
+++ spring-web-modules/spring-web-url/src/main/java/com/baeldung/exclude_urls_filter/Application.java
@@ -2,11 +2,8 @@ package com.baeldung.exclude_urls_filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@ComponentScan(basePackages = "com.baeldung.exclude_urls_filter")
-@Configuration
+
 @SpringBootApplication
 public class Application {
 
