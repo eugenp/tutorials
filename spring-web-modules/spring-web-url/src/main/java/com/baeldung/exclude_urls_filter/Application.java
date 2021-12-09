aa0207e2d236197b1b2717172d7277b26910b4cc
anuragkumawat <anuragkumawat7@gmail.com>
2021-12-09 17:32:11 +0530

    Remove unused libraries and code

--- spring-web-modules/spring-web-url/src/main/java/com/baeldung/exclude_urls_filter/Application.java
+++ spring-web-modules/spring-web-url/src/main/java/com/baeldung/exclude_urls_filter/Application.java
@@ -2,9 +2,15 @@ package com.baeldung.exclude_urls_filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    DataSourceAutoConfiguration.class,
+	    DataSourceTransactionManagerAutoConfiguration.class,
+	    HibernateJpaAutoConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
--- spring-web-modules/spring-web-url/src/main/java/com/baeldung/form_submission/Application.java
+++ spring-web-modules/spring-web-url/src/main/java/com/baeldung/form_submission/Application.java
@@ -2,9 +2,15 @@ package com.baeldung.form_submission;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	    DataSourceAutoConfiguration.class,
+	    DataSourceTransactionManagerAutoConfiguration.class,
+	    HibernateJpaAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
