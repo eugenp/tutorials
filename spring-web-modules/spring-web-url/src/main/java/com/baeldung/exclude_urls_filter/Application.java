768078f3531fe563d178cbf8ca04ad0d40713fb7
anuragkumawat <anuragkumawat7@gmail.com>
2021-12-09 18:07:56 +0530

    Removed unused annotations

--- spring-web-modules/spring-web-url/src/main/java/com/baeldung/exclude_urls_filter/Application.java
+++ spring-web-modules/spring-web-url/src/main/java/com/baeldung/exclude_urls_filter/Application.java
@@ -2,15 +2,9 @@ package com.baeldung.exclude_urls_filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 
-@SpringBootApplication(exclude = {
-	    DataSourceAutoConfiguration.class,
-	    DataSourceTransactionManagerAutoConfiguration.class,
-	    HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
--- spring-web-modules/spring-web-url/src/main/java/com/baeldung/spring/slash/Application.java
+++ spring-web-modules/spring-web-url/src/main/java/com/baeldung/spring/slash/Application.java
@@ -2,15 +2,9 @@ package com.baeldung.spring.slash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication(exclude = {
-	    DataSourceAutoConfiguration.class,
-	    DataSourceTransactionManagerAutoConfiguration.class,
-	    HibernateJpaAutoConfiguration.class})
+@SpringBootApplication
 public class Application implements WebMvcConfigurer {
 
     public static void main(String[] args) {
