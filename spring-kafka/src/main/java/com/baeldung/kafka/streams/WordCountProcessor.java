cdf6eb92f422e3acb1a5baab16aeabd25d9adb90
ukhan1980 <82222767+ukhan1980@users.noreply.github.com>
2021-12-11 04:58:11 +0000

    [BAEL-5223] Add code for Spring Boot Streams article (#11568)
    Co-authored-by: uzma khan <uzma.khan@nominet.uk>

---
+++ spring-kafka/src/main/java/com/baeldung/kafka/streams/WordCountProcessor.java
@@ -0,0 +1,36 @@
+package com.baeldung.kafka.streams;
+
+import java.util.Arrays;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.ValueMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WordCountProcessor {
+
+    private static final Serde<String> STRING_SERDE = Serdes.String();
+
+    @Autowired
+    void buildPipeline(StreamsBuilder streamsBuilder) {
+        KStream<String, String> messageStream = streamsBuilder
+            .stream("input-topic", Consumed.with(STRING_SERDE, STRING_SERDE));
+
+        KTable<String, Long> wordCounts = messageStream
+            .mapValues((ValueMapper<String, String>) String::toLowerCase)
+            .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+            .groupBy((key, word) -> word, Grouped.with(STRING_SERDE, STRING_SERDE))
+            .count(Materialized.as("counts"));
+
+        wordCounts.toStream().to("output-topic");
+    }
+
+}
