f3983e8cbaec40a70e2fb55dd1811e8c702d86c9
Haroon Khan <haroonkhan79@gmail.com>
2021-12-13 20:26:29 +0000

    [JAVA-8295] Split spring-mvc-xml module

--- spring-web-modules/spring-mvc-xml-2/src/main/java/com/baeldung/spring/paramsvalidation/RequestAndPathVariableValidationController.java
+++ spring-web-modules/spring-mvc-xml-2/src/main/java/com/baeldung/spring/paramsvalidation/RequestAndPathVariableValidationController.java
@@ -1,12 +1,17 @@
-package com.baeldung.spring.controller;
+package com.baeldung.spring.paramsvalidation;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.validation.annotation.Validated;
-import javax.validation.constraints.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Controller
 @RequestMapping("/public/api/1")
