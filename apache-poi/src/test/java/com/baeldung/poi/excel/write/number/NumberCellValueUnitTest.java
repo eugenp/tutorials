a141904d39e35f336cd4de998b879ec41fb99e82
Amitabh Tiwari <amitiwarimech@gmail.com>
2021-12-16 20:06:16 +0530

    Update NumberCellValueUnitTest.java

--- apache-poi/src/test/java/com/baeldung/poi/excel/write/number/NumberCellValueUnitTest.java
+++ apache-poi/src/test/java/com/baeldung/poi/excel/write/number/NumberCellValueUnitTest.java
@@ -18,27 +18,22 @@ import org.junit.jupiter.api.Assertions;
 import com.baeldung.poi.excel.setnumberic.ExcelNumericFormat;
 
 public class NumberCellValueUnitTest {
-    private static final String NEW_FILE_NAME = "number_test.xlsx";
-    private static final String NEW_FILE_NAME_XLS = "number_test.xls";
 
     @Test
     public void decimalDisplay_whenAddedInteger_thenNumericCellCreated() throws IOException {
-        File file;
+        File file = new File("number_test.xlsx");
         try (Workbook outWorkbook = new XSSFWorkbook()) {
             Sheet sheet = outWorkbook.createSheet("Numeric Sheet");
             Row row = sheet.createRow(0);
             Cell cell = row.createCell(0);
             ExcelNumericFormat.applyNumbericFormat(outWorkbook, row, cell, 10.251, "0.00");
-            file = new File(NEW_FILE_NAME);
             FileOutputStream fileOut = new FileOutputStream(file);
             outWorkbook.write(fileOut);
             fileOut.close();
         }
-        try (Workbook inWorkbook = new XSSFWorkbook(NEW_FILE_NAME)) {
+        try (Workbook inWorkbook = new XSSFWorkbook("number_test.xlsx")) {
             Sheet sheet = inWorkbook.cloneSheet(0);
             Row row = sheet.getRow(0);
-            Assertions.assertEquals(10.251, row.getCell(0)
-                .getNumericCellValue());
             Assertions.assertEquals(10.251, row.getCell(0)
                 .getNumericCellValue());
             file.delete();
@@ -47,18 +42,17 @@ public class NumberCellValueUnitTest {
 
     @Test
     public void desimalRoundedDisplay_whenAddedDuble_thenNumericCellCreated() throws IOException {
-        File file;
+        File file = new File("number_test.xlsx");
         try (Workbook outWorkbook = new XSSFWorkbook()) {
             Sheet sheet = outWorkbook.createSheet("Numeric Sheet");
             Row row = sheet.createRow(0);
             Cell cell = row.createCell(0);
             ExcelNumericFormat.applyNumbericFormat(outWorkbook, row, cell, 10.251123, "#,##0.0000");
-            file = new File(NEW_FILE_NAME);
             FileOutputStream fileOut = new FileOutputStream(file);
             outWorkbook.write(fileOut);
             fileOut.close();
         }
-        try (Workbook inWorkbook = new XSSFWorkbook(NEW_FILE_NAME)) {
+        try (Workbook inWorkbook = new XSSFWorkbook("number_test.xlsx")) {
             Sheet sheet = inWorkbook.cloneSheet(0);
             Row row = sheet.getRow(0);
             Assertions.assertEquals(10.251123, row.getCell(0)
@@ -69,13 +63,12 @@ public class NumberCellValueUnitTest {
 
     @Test
     public void decimalDisplayInXLS_whenAddedInteger_thenNumericCellCreated() throws IOException {
-        File file;
+        File file = new File("number_test.xls");
         try (Workbook outWorkbook = new HSSFWorkbook()) {
             Sheet sheet = outWorkbook.createSheet("Numeric Sheet");
             Row row = sheet.createRow(0);
             Cell cell = row.createCell(0);
             ExcelNumericFormat.applyNumbericFormat(outWorkbook, row, cell, 10.251, "0.00");
-            file = new File(NEW_FILE_NAME_XLS);
             FileOutputStream fileOut = new FileOutputStream(file);
             outWorkbook.write(fileOut);
             fileOut.close();
@@ -91,14 +84,14 @@ public class NumberCellValueUnitTest {
 
     @Test
     public void decimalValue_whenAddedInteger_thenNumericCellCreated() throws IOException {
-        File file;
+        File file = new File("number_test.xlsx");
         try (Workbook outWorkbook = new HSSFWorkbook()) {
             Sheet sheet = outWorkbook.createSheet("Numeric Sheet");
             Row row = sheet.createRow(0);
             Cell cell = row.createCell(0);
             DecimalFormat df = new DecimalFormat("#,###.##");
             ExcelNumericFormat.applyNumbericFormat(outWorkbook, row, cell, Double.valueOf(df.format(10.251)), "#,###.##");
-            file = new File(NEW_FILE_NAME_XLS);
+
             FileOutputStream fileOut = new FileOutputStream(file);
             outWorkbook.write(fileOut);
             fileOut.close();
