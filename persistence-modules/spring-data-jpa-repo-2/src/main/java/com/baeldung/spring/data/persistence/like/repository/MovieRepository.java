38e32634ecf00b326b536cd6f901de533288e8e2
Haroon Khan <haroonkhan79@gmail.com>
2021-12-23 22:20:05 +0000

    [JAVA-8367] Split spring-data-jpa-repo module

--- persistence-modules/spring-data-jpa-repo-2/src/main/java/com/baeldung/spring/data/persistence/like/repository/MovieRepository.java
+++ persistence-modules/spring-data-jpa-repo-2/src/main/java/com/baeldung/spring/data/persistence/like/repository/MovieRepository.java
@@ -1,17 +1,16 @@
-package com.baeldung.like.repository;
+package com.baeldung.spring.data.persistence.like.repository;
 
-import java.util.List;
-
+import com.baeldung.spring.data.persistence.like.model.Movie;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
-import com.baeldung.like.model.Movie;
+import java.util.List;
 
 public interface MovieRepository extends CrudRepository<Movie, Long> {
 
     List<Movie> findByTitleContaining(String title);
-    
+
     List<Movie> findByTitleLike(String title);
 
     List<Movie> findByTitleContains(String title);
@@ -23,17 +22,17 @@ public interface MovieRepository extends CrudRepository<Movie, Long> {
     List<Movie> findByDirectorEndsWith(String director);
 
     List<Movie> findByTitleContainingIgnoreCase(String title);
-    
+
     List<Movie> findByRatingNotContaining(String rating);
-    
+
     List<Movie> findByDirectorNotLike(String director);
-    
+
     @Query("SELECT m FROM Movie m WHERE m.title LIKE %:title%")
     List<Movie> searchByTitleLike(@Param("title") String title);
-    
+
     @Query("SELECT m FROM Movie m WHERE m.rating LIKE ?1%")
     List<Movie> searchByRatingStartsWith(String rating);
-    
+
     //Escaping works in SpringBoot >= 2.4.1
     //@Query("SELECT m FROM Movie m WHERE m.director LIKE %?#{escape([0])} escape ?#{escapeCharacter()}")
     @Query("SELECT m FROM Movie m WHERE m.director LIKE %:#{[0]}")
