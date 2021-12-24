afdb1988249efc07e1b0307325953a32ffa01e69
Matea Pejčinović <matea.pejcinovic@intellexi.hr>
2021-12-24 17:47:00 +0100

    removed public from class and methods, removed unnecessary test examples, extracted contants

--- core-java-modules/core-java-string-algorithms-3/src/test/java/com/baeldung/repeatedcharstring/RepeatedCharacterStringUnitTest.java
+++ core-java-modules/core-java-string-algorithms-3/src/test/java/com/baeldung/repeatedcharstring/RepeatedCharacterStringUnitTest.java
@@ -2,7 +2,6 @@ package com.baeldung.repeatedcharstring;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
@@ -10,127 +9,83 @@ import java.nio.CharBuffer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.Strings;
 
 import static ch.qos.logback.core.CoreConstants.EMPTY_STRING;
 import static java.util.stream.Stream.generate;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class RepeatedCharacterStringUnitTest {
+class RepeatedCharacterStringUnitTest {
 
+    private static final String EXAMPLE_STRING = "aaaaaaa";
+    private static final int LENGTH = 7;
+    
     @Test
-    public void givenString_whenStringBufferUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenStringBufferUsed_thenStringCreated() {
         char charToAppend = 'a';
-        StringBuilder outputBuffer = new StringBuilder(length);
-        for (int i = 0; i < length; i++) {
-            outputBuffer.append(charToAppend);
-        }
-        Assertions.assertEquals(exampleString, outputBuffer.toString());
+        StringBuilder outputBuilder = new StringBuilder(LENGTH);
+        outputBuilder.append(String.valueOf(charToAppend).repeat(LENGTH));
+        assertEquals(EXAMPLE_STRING, outputBuilder.toString());
     }
 
     @Test
-    public void givenString_whenEmptyArrayUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenCharBufferUsed_thenStringCreated() {
         char charToAppend = 'a';
-        String newString = new String(new char[length]).replace('\0', charToAppend);
-        Assertions.assertEquals(exampleString, newString);
+        String newString = CharBuffer.allocate(LENGTH).toString().replace('\0', charToAppend);
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenCharBufferUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenApacheStringUtilsUsed_thenStringCreated() {
         char charToAppend = 'a';
-        String newString = CharBuffer.allocate(length).toString().replace('\0', charToAppend);
-        Assertions.assertEquals(exampleString, newString);
+        String newString = StringUtils.repeat(charToAppend, LENGTH);
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenApacheStringUtilsUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
-        char charToAppend = 'a';
-        String newString = StringUtils.repeat(charToAppend, length);
-        Assertions.assertEquals(exampleString, newString);
-    }
-
-    @Test
-    public void givenString_whenGuavaRepeatUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenGuavaRepeatUsed_thenStringCreated() {
         String charToAppend = "a";
-        String newString = Strings.repeat(charToAppend, length);
-        Assertions.assertEquals(exampleString, newString);
+        String newString = Strings.repeat(charToAppend, LENGTH);
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenArraysFillUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenArraysFillUsed_thenStringCreated() {
         char charToAppend = 'a';
-        char[] charArray = new char[length];
+        char[] charArray = new char[LENGTH];
         Arrays.fill(charArray, charToAppend);
         String newString = new String(charArray);
-        Assertions.assertEquals(exampleString, newString);
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenGenerateAndJoiningUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenGenerateAndJoiningUsed_thenStringCreated() {
         String charToAppend = "a";
-        String newString = generate(() -> charToAppend).limit(length).collect(Collectors.joining());
-        Assertions.assertEquals(exampleString, newString);
+        String newString = generate(() -> charToAppend).limit(LENGTH).collect(Collectors.joining());
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenStringJoinUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenStringJoinUsed_thenStringCreated() {
         String charToAppend = "a";
-        String newString = String.join(EMPTY_STRING, Collections.nCopies(length, charToAppend));
-        Assertions.assertEquals(exampleString, newString);
+        String newString = String.join(EMPTY_STRING, Collections.nCopies(LENGTH, charToAppend));
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenGuavaJoinerUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenGuavaJoinerUsed_thenStringCreated() {
         String charToAppend = "a";
-        String newString = Joiner.on(EMPTY_STRING).join(Collections.nCopies(length, charToAppend));
-        Assertions.assertEquals(exampleString, newString);
+        String newString = Joiner.on(EMPTY_STRING).join(Collections.nCopies(LENGTH, charToAppend));
+        assertEquals(EXAMPLE_STRING, newString);
     }
 
     @Test
-    public void givenString_whenStringFormatUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
+    void givenString_whenRandomStringUtilsUsed_thenStringCreated() {
         String charToAppend = "a";
-        String newString = String.format("%0" + length + "d", 0).replace("0", charToAppend);
-        Assertions.assertEquals(exampleString, newString);
-    }
-
-    @Test
-    public void givenString_whenCollectUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
-        String charToAppend = "a";
-        String newString = IntStream.range(0, length).mapToObj(i -> charToAppend).collect(Collectors.joining(EMPTY_STRING));
-        Assertions.assertEquals(exampleString, newString);
-    }
-
-    @Test
-    public void givenString_whenRandomStringUtilsUsed_thenStringCreated() {
-        String exampleString = "aaaaaaa";
-        int length = 7;
-        String charToAppend = "a";
-        String newString = RandomStringUtils.random(length, charToAppend);
-        Assertions.assertEquals(exampleString, newString);
+        String newString = RandomStringUtils.random(LENGTH, charToAppend);
+        assertEquals(EXAMPLE_STRING, newString);
     }
 }
