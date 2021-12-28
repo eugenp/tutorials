f18503e3b113a875e59bec157d2997e74c8e1241
Philippe <phil@lighthouse.com.br>
2021-12-27 21:31:59 -0300

    WIP:Update to latest Boot

--- spring-cloud/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/custompredicates/config/CustomPredicatesConfig.java
+++ spring-cloud/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/custompredicates/config/CustomPredicatesConfig.java
@@ -1,6 +1,5 @@
 package com.baeldung.springcloudgateway.custompredicates.config;
 
-import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -24,14 +23,20 @@ public class CustomPredicatesConfig {
     public RouteLocator routes(RouteLocatorBuilder builder, GoldenCustomerRoutePredicateFactory gf ) {
 
         return builder.routes()
-          .route("dsl_golden_route", r -> r.path("/dsl_api/**")
-            .filters(f -> f.stripPrefix(1))
-            .uri("https://httpbin.org")
-            .predicate(gf.apply(new Config(true, "customerId"))))
-          .route("dsl_common_route", r -> r.path("/dsl_api/**")
-            .filters(f -> f.stripPrefix(1))
-            .uri("https://httpbin.org")
-            .predicate(gf.apply(new Config(false, "customerId"))))             
+          .route("dsl_golden_route", r -> 
+            r.predicate(gf.apply(new Config(true, "customerId")))
+             .and()
+             .path("/dsl_api/**")
+             .filters(f -> f.stripPrefix(1))
+             .uri("https://httpbin.org")
+          )
+          .route("dsl_common_route", r -> 
+             r.predicate(gf.apply(new Config(false, "customerId")))
+              .and()
+              .path("/dsl_api/**")
+              .filters(f -> f.stripPrefix(1))
+              .uri("https://httpbin.org")
+              )             
           .build();
     }
     
