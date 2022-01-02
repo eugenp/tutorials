522fbb0a409a3bfb8f5093352c34c5e368a09ca0
Haris <harishashim@gmail.com>
2022-01-03 01:10:17 +0800

    BAEL-5204: fix indent

--- apache-poi/src/test/java/com/baeldung/poi/excel/cellstyle/CellStylerUnitTest.java
+++ apache-poi/src/test/java/com/baeldung/poi/excel/cellstyle/CellStylerUnitTest.java
@@ -15,38 +15,38 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CellStylerUnitTest {
-	private static String FILE_NAME = "com/baeldung/poi/excel/cellstyle/CellStyle.xlsx";
-	private static final String NEW_FILE_NAME = "CellStyleTest_output.xlsx";
-	private String fileLocation;
+    private static String FILE_NAME = "com/baeldung/poi/excel/cellstyle/CellStyle.xlsx";
+    private static final String NEW_FILE_NAME = "CellStyleTest_output.xlsx";
+    private String fileLocation;
 
-	@Before
-	public void setup() throws IOException, URISyntaxException {
+    @Before
+    public void setup() throws IOException, URISyntaxException {
         fileLocation = Paths.get(ClassLoader.getSystemResource(FILE_NAME)
-                .toURI())
-                .toString();
-	}
+            .toURI())
+            .toString();
+    }
 
-	@Test
-	public void testApplyWarningColor() throws IOException {
-		Workbook workbook = new XSSFWorkbook(fileLocation);
-		Sheet sheet = workbook.getSheetAt(0);
-		Row row1 = sheet.createRow(0);
-		row1.setHeightInPoints((short) 40);
-		
-		CellStyler styler = new CellStyler();
-		CellStyle style = styler.createWarningColor(workbook);
+    @Test
+    public void testApplyWarningColor() throws IOException {
+        Workbook workbook = new XSSFWorkbook(fileLocation);
+        Sheet sheet = workbook.getSheetAt(0);
+        Row row1 = sheet.createRow(0);
+        row1.setHeightInPoints((short) 40);
 
-		Cell cell1 = row1.createCell(0);
-		cell1.setCellStyle(style);
-		cell1.setCellValue("Hello");
+        CellStyler styler = new CellStyler();
+        CellStyle style = styler.createWarningColor(workbook);
 
-		Cell cell2 = row1.createCell(1);
-		cell2.setCellStyle(style);
-		cell2.setCellValue("world!");
+        Cell cell1 = row1.createCell(0);
+        cell1.setCellStyle(style);
+        cell1.setCellValue("Hello");
 
-		FileOutputStream outputStream = new FileOutputStream(NEW_FILE_NAME);
-		workbook.write(outputStream);
-		outputStream.close();
-		workbook.close();
-	}
+        Cell cell2 = row1.createCell(1);
+        cell2.setCellStyle(style);
+        cell2.setCellValue("world!");
+
+        FileOutputStream outputStream = new FileOutputStream(NEW_FILE_NAME);
+        workbook.write(outputStream);
+        outputStream.close();
+        workbook.close();
+    }
 }
