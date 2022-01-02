3c9831f32334c91b69392136a448f2ef90dceecf
sharifi <h_sharifi@modernisc.com>
2022-01-02 13:09:06 +0330

    bael-4197: add @ContextConfiguration for loading application context

--- spring-boot-modules/spring-boot-testing/src/test/java/com/baeldung/xmlapplicationcontext/EmployeeServiceAppContextIntegrationTest.java
+++ spring-boot-modules/spring-boot-testing/src/test/java/com/baeldung/xmlapplicationcontext/EmployeeServiceAppContextIntegrationTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = XmlBeanApplication.class)
+//@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/application-context.xml")
 public class EmployeeServiceAppContextIntegrationTest {
 
     @Autowired
