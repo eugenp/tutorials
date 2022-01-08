784c0e11bdd55475e8829e7f2e8ce42af70b13d5
sharifi <h_sharifi@modernisc.com>
2022-01-08 12:48:48 +0330

    bael-4197: indent with spaces instead of tab

--- spring-boot-modules/spring-boot-testing/src/main/java/com/baeldung/xmlapplicationcontext/XmlBeanApplication.java
+++ spring-boot-modules/spring-boot-testing/src/main/java/com/baeldung/xmlapplicationcontext/XmlBeanApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ImportResource;
 public class XmlBeanApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(XmlBeanApplication.class, args);
+        SpringApplication.run(XmlBeanApplication.class, args);
 	}
 
 }
