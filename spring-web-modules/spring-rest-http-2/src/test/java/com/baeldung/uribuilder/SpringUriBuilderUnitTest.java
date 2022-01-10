5521870d6498930287da8fc92dcc8bf88279bae6
Haroon Khan <haroonkhan79@gmail.com>
2022-01-10 08:57:45 +0000

    [JAVA-9347] Split spring-rest-http module

--- spring-web-modules/spring-rest-http-2/src/test/java/com/baeldung/uribuilder/SpringUriBuilderUnitTest.java
+++ spring-web-modules/spring-rest-http-2/src/test/java/com/baeldung/uribuilder/SpringUriBuilderUnitTest.java
@@ -1,39 +1,47 @@
 package com.baeldung.uribuilder;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.Collections;
-
 import org.junit.Test;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-public class SpringUriBuilderIntegrationTest {
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+
+public class SpringUriBuilderUnitTest {
 
     @Test
     public void constructUri() {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("www.baeldung.com").path("/junit-5").build();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+          .scheme("http").host("www.baeldung.com").path("/junit-5")
+          .build();
 
         assertEquals("http://www.baeldung.com/junit-5", uriComponents.toUriString());
     }
 
     @Test
     public void constructUriEncoded() {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("www.baeldung.com").path("/junit 5").build().encode();
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+          .scheme("http").host("www.baeldung.com").path("/junit 5")
+          .build().encode();
 
         assertEquals("http://www.baeldung.com/junit%205", uriComponents.toUriString());
     }
 
     @Test
     public void constructUriFromTemplate() {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("www.baeldung.com").path("/{article-name}").buildAndExpand("junit-5");
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+          .scheme("http").host("www.baeldung.com").path("/{article-name}")
+          .buildAndExpand("junit-5");
 
         assertEquals("http://www.baeldung.com/junit-5", uriComponents.toUriString());
     }
 
     @Test
     public void constructUriWithQueryParameter() {
-        UriComponents uriComponents = UriComponentsBuilder.newInstance().scheme("http").host("www.google.com").path("/").query("q={keyword}").buildAndExpand("baeldung");
+        UriComponents uriComponents = UriComponentsBuilder.newInstance()
+          .scheme("http").host("www.google.com").path("/").query("q={keyword}")
+          .buildAndExpand("baeldung");
 
         assertEquals("http://www.google.com/?q=baeldung", uriComponents.toUriString());
     }
