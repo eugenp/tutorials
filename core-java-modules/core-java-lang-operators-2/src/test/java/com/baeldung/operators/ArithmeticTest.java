c58885a6652fac9a8e2da55aaad8dd68c74b8db2
Silvio Gomes <43036208+silviorazec@users.noreply.github.com>
2022-01-12 17:02:10 -0300

    Update core-java-modules/core-java-lang-operators-2/src/test/java/com/baeldung/operators/ArithmeticTest.java
    Co-authored-by: andrzejsydor <andrzejsydor@users.noreply.github.com>

--- core-java-modules/core-java-lang-operators-2/src/test/java/com/baeldung/operators/ArithmeticTest.java
+++ core-java-modules/core-java-lang-operators-2/src/test/java/com/baeldung/operators/ArithmeticTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 public class ArithmeticTest {
 
     @Test
-    public void whenUseTheOperatorArithmeticAdditionToConcatenate() {
+    void whenUseTheOperatorArithmeticAdditionToConcatenate() {
 
         String sentence = "Hello world!";
         String wordsConcatenated = "Hello " + "world" + "!";
