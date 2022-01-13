cfd566c8e5c33b7ee54addad9fb128c1e018c365
Haroon Khan <haroonkhan79@gmail.com>
2022-01-13 09:01:20 +0000

    [JAVA-9425] Fix failing integration tests in Sirix module

--- persistence-modules/sirix/src/test/java/io/sirix/tutorial/json/SerializeVersionedJsonResourceIntegrationTest.java
+++ persistence-modules/sirix/src/test/java/io/sirix/tutorial/json/SerializeVersionedJsonResourceIntegrationTest.java
@@ -1,6 +1,13 @@
 package io.sirix.tutorial.json;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.sirix.access.DatabaseConfiguration;
+import org.sirix.access.Databases;
+import org.sirix.api.json.JsonResourceManager;
+import org.sirix.service.json.serialize.JsonSerializer;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -10,39 +17,27 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.sirix.access.DatabaseConfiguration;
-import org.sirix.access.Databases;
-import org.sirix.api.json.JsonResourceManager;
-import org.sirix.service.json.serialize.JsonSerializer;
+import static org.junit.Assert.assertEquals;
 
 public class SerializeVersionedJsonResourceIntegrationTest {
 
     private static final Path JSON_TEST_RESULT_DIRECTORY = Paths.get("src", "test", "resources", "json-test-result-strings");
 
-    private static final String TMP_DIRECTORY = System.getProperty("java.io.tmpdir");
+    @Rule
+    public TemporaryFolder tempDirectory = new TemporaryFolder();
 
-    private static final Path DATABASE_PATH = Paths.get(TMP_DIRECTORY, "sirix", "json-database");
+    private Path databasePath;
 
     @Before
-    public void setUp() throws Exception {
-        if (Files.exists(DATABASE_PATH))
-            Databases.removeDatabase(DATABASE_PATH);
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        if (Files.exists(DATABASE_PATH))
-            Databases.removeDatabase(DATABASE_PATH);
+    public void setUp() {
+        databasePath = Paths.get(tempDirectory.getRoot().getPath(), "sirix", "json-database");
     }
 
     @Test
     public void createVersionedResourceAndTestSerializations() throws IOException {
-        Databases.createJsonDatabase(new DatabaseConfiguration(DATABASE_PATH));
+        Databases.createJsonDatabase(new DatabaseConfiguration(databasePath));
 
-        try (final var database = Databases.openJsonDatabase(DATABASE_PATH)) {
+        try (final var database = Databases.openJsonDatabase(databasePath)) {
             VersionedJsonDocumentCreator.create(database);
 
             try (final var manager = database.openResourceManager("resource")) {
