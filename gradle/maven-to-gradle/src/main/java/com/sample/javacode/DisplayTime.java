b4620da02ce2a61b29a683181930655a1843babe
Ankit Anurag <46675158+ankit0706@users.noreply.github.com>
2022-01-14 11:36:59 +0530

    formatted code

--- gradle/maven-to-gradle/src/main/java/com/sample/javacode/DisplayTime.java
+++ gradle/maven-to-gradle/src/main/java/com/sample/javacode/DisplayTime.java
@@ -6,16 +6,16 @@ import org.apache.commons.lang3.StringUtils;
 
 public class DisplayTime {
 
-	public static void main(String[] args) {
-		Date date = new java.util.Date();
-		System.out.println(date);
-		String gmt = date.toString();
-		boolean flag = StringUtils.isEmpty(gmt);
-		if(flag) {
-			System.out.println("No time found");
-		}else {
-			System.out.println(gmt);
-		}
-	}
+    public static void main(String[] args) {
+        Date date = new java.util.Date();
+        System.out.println(date);
+        String gmt = date.toString();
+        boolean flag = StringUtils.isEmpty(gmt);
+        if (flag) {
+            System.out.println("No time found");
+        } else {
+            System.out.println(gmt);
+        }
+    }
 
 }
