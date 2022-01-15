ff205844593138406776245643c91326e6a2e4d8
Daniel Strmecki <daniel.strmecki@gmail.com>
2022-01-15 11:59:23 +0100

    BAEL-5307: Create new module

---
+++ core-java-modules/core-java-graphql/src/main/java/com/baeldung/graphql/Demo.java
@@ -0,0 +1,9 @@
+package com.baeldung.graphql;
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+
+}
---
+++ core-java-modules/core-java-graphql/src/test/java/com/baeldung/graphql/DemoUnitTest.java
@@ -0,0 +1,13 @@
+package com.baeldung.graphql;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class DemoUnitTest {
+
+    @Test
+    public void given_when_then() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
