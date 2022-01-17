d8cb70194dc1dc82e12b1d1050505c9f8d2ced76
Haroon Khan <haroonkhan79@gmail.com>
2022-01-17 14:04:55 +0000

    [JAVA-9421] Cleanup code present in stackify repo (#11709)

--- guest/webservices/rest-server/src/main/java/com/stackify/ApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.stackify;
-
-import org.glassfish.jersey.server.ResourceConfig;
-
-public class ApplicationInitializer extends ResourceConfig {
-    public ApplicationInitializer() {
-        packages("com.stackify.services");
-    }
-}
