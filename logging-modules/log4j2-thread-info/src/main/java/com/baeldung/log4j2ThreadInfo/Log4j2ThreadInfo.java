e183a6bffa23470bde973764cb19ffa934192876
Pajé <josephdsbr@gmail.com>
2022-01-18 21:12:39 -0300

    - fix: Changing module name as suggested to log4j2-thread-info. - fix: Fixing log path in log4j2 configuration

--- logging-modules/log4j2-thread-info/src/main/java/com/baeldung/log4j2ThreadInfo/Log4j2ThreadInfo.java
+++ logging-modules/log4j2-thread-info/src/main/java/com/baeldung/log4j2ThreadInfo/Log4j2ThreadInfo.java
@@ -1,12 +1,12 @@
-package com.baeldung.threadinfolog4j2example;
+package com.baeldung.log4j2ThreadInfo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.IntStream;
 
-public class ThreadInfoLog4j2ExampleApplication {
-    private static final Logger LOGGER = LogManager.getLogger(ThreadInfoLog4j2ExampleApplication.class);
+public class Log4j2ThreadInfo {
+    private static final Logger LOGGER = LogManager.getLogger(Log4j2ThreadInfo.class);
     
     public static void main(String[] args) {
         IntStream.range(0, 5).forEach(i -> {
