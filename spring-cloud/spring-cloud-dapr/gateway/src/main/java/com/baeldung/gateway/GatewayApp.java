92f784484457d45d1fd726ad6a522f64103b5a51
kwoyke <krzysztofwoyke@gmail.com>
2022-01-22 10:00:31 +0100

    JAVA-9511: Fix spring-cloud-dapr module (#11726)

--- spring-cloud/spring-cloud-dapr/gateway/src/main/java/com/baeldung/gateway/GatewayApp.java
+++ spring-cloud/spring-cloud-dapr/gateway/src/main/java/com/baeldung/gateway/GatewayApp.java
@@ -1,4 +1,4 @@
-package org.example.gateway;
+package com.baeldung.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
