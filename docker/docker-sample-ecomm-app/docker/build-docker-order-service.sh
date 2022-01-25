7f1a9357aa89849616bdbd9c59178b05bffd4df5
Swapan Pramanick <swapan.pramanick@zalando.de>
2022-01-25 02:41:40 +0100

    BAEL-4316 change example

--- docker/docker-sample-ecomm-app/docker/build-docker-order-service.sh
+++
@@ -1,6 +0,0 @@
-mkdir tmp-context
-cp -R ../scripts tmp-context/
-cp -R ../configs tmp-context/
-cp ../order-service/target/order-service-0.0.1.jar tmp-context/service.jar
-docker build -f Dockerfile-order-service-from-script -t ecomm-order-service:0.0.1 tmp-context
-rm -rf tmp-context
--- docker/docker-sample-ecomm-app/scripts/start.sh
+++
@@ -1,1 +0,0 @@
-java -jar service.jar
