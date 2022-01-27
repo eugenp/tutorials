0782dc0ac808e5cacebd4b8c3e9ac61780739349
eugene.kovko <eugene.kovko@tensquaregames.com>
2022-01-27 20:45:15 +0100

    Main comment

--- ddd/src/main/java/com/baeldung/hexagonal/App.java
+++ ddd/src/main/java/com/baeldung/hexagonal/App.java
@@ -3,7 +3,9 @@ package com.baeldung.hexagonal;
 public class App {
 
     public static void main(String[] args) {
-
+        // Here should be logic that constructs application
+        // Some framework as Spring or Guice can be used here
+        // At the same time it is possible to set the application manually
     }
 
 }
