57f91f9948a4350e851e42eba42638ccb8139daa
Krzysiek <krzysztofwoyke@gmail.com>
2022-01-27 10:15:16 +0100

    JAVA-9605: Disable security for the integration tests

--- spring-cloud/spring-cloud-gateway/src/test/java/com/baeldung/springcloudgateway/customfilters/secondservice/SecondServiceIntegrationTest.java
+++ spring-cloud/spring-cloud-gateway/src/test/java/com/baeldung/springcloudgateway/customfilters/secondservice/SecondServiceIntegrationTest.java
@@ -2,12 +2,14 @@ package com.baeldung.springcloudgateway.customfilters.secondservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import com.baeldung.springcloudgateway.customfilters.secondservice.web.SecondServiceRestController;
 
-@WebFluxTest(SecondServiceRestController.class)
+@WebFluxTest(controllers = SecondServiceRestController.class,
+        excludeAutoConfiguration = ReactiveSecurityAutoConfiguration.class)
 public class SecondServiceIntegrationTest {
 
     @Autowired
