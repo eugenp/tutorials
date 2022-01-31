d093b4b0cf802b6eb73e06957c3c1b727c9ce84e
Attila Uhrin <uhrin.attila1@gmail.com>
2022-01-31 22:10:09 +0100

    Remove empty test case.

--- spring-sleuth/src/test/java/com/baeldung/SpringContextTest.java
+++ spring-sleuth/src/test/java/com/baeldung/SpringContextTest.java
@@ -1,9 +1,11 @@
-package com.baeldung.spring.session;
+package com.baeldung;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baeldung.spring.session.SleuthWebApp;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SleuthWebApp.class)
