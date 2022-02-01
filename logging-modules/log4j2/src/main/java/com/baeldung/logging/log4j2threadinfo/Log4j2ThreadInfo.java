a7c7ae16f6a3ba2d1c6af8d1675302f06a2c3b22
Pajé <josephdsbr@gmail.com>
2022-02-01 01:33:53 -0300

    lowercasing package name and variable name

--- logging-modules/log4j2/src/main/java/com/baeldung/logging/log4j2threadinfo/Log4j2ThreadInfo.java
+++ logging-modules/log4j2/src/main/java/com/baeldung/logging/log4j2threadinfo/Log4j2ThreadInfo.java
@@ -1,4 +1,4 @@
-package com.baeldung.logging.log4j2ThreadInfo;
+package com.baeldung.logging.log4j2threadinfo;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -6,11 +6,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.IntStream;
 
 public class Log4j2ThreadInfo {
-    private static final Logger LOGGER = LogManager.getLogger(Log4j2ThreadInfo.class);
+    private static final Logger logger = LogManager.getLogger(Log4j2ThreadInfo.class);
     
     public static void main(String[] args) {
         IntStream.range(0, 5).forEach(i -> {
-            Runnable runnable = () -> LOGGER.info("Logging info");
+            Runnable runnable = () -> logger.info("Logging info");
             Thread thread = new Thread(runnable);
             thread.start();
         });
