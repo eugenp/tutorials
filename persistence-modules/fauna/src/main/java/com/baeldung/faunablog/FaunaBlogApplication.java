9cb6ff1227750aeb25db44183ce945f6bcf9835e
Graham Cox <graham@grahamcox.co.uk>
2022-02-03 07:47:26 +0000

    BAEL-5229: Code for Fauna article

---
+++ persistence-modules/fauna/src/main/java/com/baeldung/faunablog/FaunaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.faunablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaunaBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaunaBlogApplication.class, args);
+	}
+
+}
