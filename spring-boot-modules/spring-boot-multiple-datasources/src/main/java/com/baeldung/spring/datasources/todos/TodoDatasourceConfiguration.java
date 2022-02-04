0c992fc9c962177ce219c6494bb1c94713112be7
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2022-02-04 07:09:28 +0100

    BAEL-5323: Refactor configuration classes

--- spring-boot-modules/spring-boot-multiple-datasources/src/main/java/com/baeldung/spring/datasources/todos/TodoDatasourceConfiguration.java
+++ spring-boot-modules/spring-boot-multiple-datasources/src/main/java/com/baeldung/spring/datasources/todos/TodoDatasourceConfiguration.java
@@ -1,28 +1,14 @@
 package com.baeldung.spring.datasources.todos;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.Objects;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-  basePackageClasses = Todo.class,
-  entityManagerFactoryRef = "todosEntityManagerFactory",
-  transactionManagerRef = "todosTransactionManager"
-)
 public class TodoDatasourceConfiguration {
 
     @Bean
@@ -39,19 +25,4 @@ public class TodoDatasourceConfiguration {
           .build();
     }
 
-    @Bean(name = "todosEntityManagerFactory")
-    @Primary
-    public LocalContainerEntityManagerFactoryBean todosEntityManagerFactory(EntityManagerFactoryBuilder builder) {
-        return builder
-          .dataSource(todosDataSource())
-          .packages(Todo.class)
-          .build();
-    }
-
-    @Bean(name = "todosTransactionManager")
-    @Primary
-    public PlatformTransactionManager todosTransactionManager(
-      final @Qualifier("todosEntityManagerFactory") LocalContainerEntityManagerFactoryBean todosEntityManagerFactory) {
-        return new JpaTransactionManager(Objects.requireNonNull(todosEntityManagerFactory.getObject()));
-    }
 }
--- spring-boot-modules/spring-boot-multiple-datasources/src/main/java/com/baeldung/spring/datasources/topics/TopicDatasourceConfiguration.java
+++ spring-boot-modules/spring-boot-multiple-datasources/src/main/java/com/baeldung/spring/datasources/topics/TopicDatasourceConfiguration.java
@@ -1,27 +1,13 @@
 package com.baeldung.spring.datasources.topics;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.Objects;
 
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories(
-  basePackageClasses = Topic.class,
-  entityManagerFactoryRef = "topicsEntityManagerFactory",
-  transactionManagerRef = "topicsTransactionManager"
-)
 public class TopicDatasourceConfiguration {
 
     @Bean
@@ -37,17 +23,4 @@ public class TopicDatasourceConfiguration {
           .build();
     }
 
-    @Bean(name = "topicsEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean topicsEntityManagerFactory(EntityManagerFactoryBuilder builder) {
-        return builder
-          .dataSource(topicsDataSource())
-          .packages(Topic.class)
-          .build();
-    }
-
-    @Bean(name = "topicsTransactionManager")
-    public PlatformTransactionManager topicsTransactionManager(
-      final @Qualifier("topicsEntityManagerFactory") LocalContainerEntityManagerFactoryBean topicsEntityManagerFactory) {
-        return new JpaTransactionManager(Objects.requireNonNull(topicsEntityManagerFactory.getObject()));
-    }
 }
