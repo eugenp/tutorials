b2230fec5d03d653867f182236ba02c6cd484ff7
Pajé <josephdsbr@gmail.com>
2022-02-05 11:58:56 -0300

    - fix: formatting imports

--- logging-modules/log4j2/src/main/java/com/baeldung/logging/log4j2threadinfo/Log4j2ThreadInfo.java
+++ logging-modules/log4j2/src/main/java/com/baeldung/logging/log4j2threadinfo/Log4j2ThreadInfo.java
@@ -1,9 +1,9 @@
 package com.baeldung.logging.log4j2threadinfo;
 
+import java.util.stream.IntStream;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.IntStream;
 
 public class Log4j2ThreadInfo {
     private static final Logger logger = LogManager.getLogger(Log4j2ThreadInfo.class);
