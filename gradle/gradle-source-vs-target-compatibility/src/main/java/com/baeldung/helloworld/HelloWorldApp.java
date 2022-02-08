694a947c4b7c284d2017c1dbf30894fe553415e6
kpentaris <kpentaris@gmail.com>
2022-02-08 12:30:05 +0200

    Fix code format

--- gradle/gradle-source-vs-target-compatibility/src/main/java/com/baeldung/helloworld/HelloWorldApp.java
+++ gradle/gradle-source-vs-target-compatibility/src/main/java/com/baeldung/helloworld/HelloWorldApp.java
@@ -2,8 +2,8 @@ package com.baeldung.helloworld;
 
 public class HelloWorldApp {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 
 }
