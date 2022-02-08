7fc6e3f02fa3f4ff820b4c4ba6234b0b94c74712
Vaibhav Jain <vaibhav007jain@gmail.com>
2022-02-08 11:59:18 +0530

    BAEL-5280: Fixed indentation in ArrayIndexOutOfBoundsExceptionDemoUnitTest.java.

--- core-java-modules/core-java-exceptions-4/src/test/java/com/baeldung/exception/arrayindexoutofbounds/ArrayIndexOutOfBoundsExceptionDemoUnitTest.java
+++ core-java-modules/core-java-exceptions-4/src/test/java/com/baeldung/exception/arrayindexoutofbounds/ArrayIndexOutOfBoundsExceptionDemoUnitTest.java
@@ -8,27 +8,27 @@ import org.junit.jupiter.api.Test;
 public class ArrayIndexOutOfBoundsExceptionDemoUnitTest {
 
     private static int[] numbers;
-    
+
     @BeforeAll
     public static void setUp() {
-        numbers = new int[] { 1, 2, 3, 4, 5 };        
+        numbers = new int[] { 1, 2, 3, 4, 5 };
     }
 
     @Test
     void givenAnArrayOfSizeFive_whenAccessedElementBeyondRange_thenShouldThrowArrayIndexOutOfBoundsException() {
         assertThrows(ArrayIndexOutOfBoundsException.class,
-                () -> ArrayIndexOutOfBoundsExceptionDemo.addArrayElementsUsingLoop(numbers));
+          () -> ArrayIndexOutOfBoundsExceptionDemo.addArrayElementsUsingLoop(numbers));
     }
 
     @Test
     void givenAnArrayOfSizeFive_whenAccessedAnElementAtIndexEqualToSize_thenShouldThrowArrayIndexOutOfBoundsException() {
         assertThrows(ArrayIndexOutOfBoundsException.class,
-                () -> ArrayIndexOutOfBoundsExceptionDemo.getArrayElementAtIndex(numbers, 5));
+          () -> ArrayIndexOutOfBoundsExceptionDemo.getArrayElementAtIndex(numbers, 5));
     }
 
     @Test
     void givenAListReturnedByArraysAsListMethod_whenAccessedAnElementAtIndexEqualToSize_thenShouldThrowArrayIndexOutOfBoundsException() {
         assertThrows(ArrayIndexOutOfBoundsException.class,
-                () -> ArrayIndexOutOfBoundsExceptionDemo.getListElementAtIndex(5));
+          () -> ArrayIndexOutOfBoundsExceptionDemo.getListElementAtIndex(5));
     }
 }
