5da21d6bba964c02f0a3936149ff5b0674f5282f
Palaniappan Arunachalam <palaniappan.ar@gmail.com>
2022-02-09 13:21:48 +0530

    Revert "Implementation for Simple Hexagonal Architecture"
    This reverts commit ad893be8ac4d6fea0f1e2799cad6b3c8c3ba40c1.

--- ddd/src/main/java/com/baeldung/simplehexagonal/infrastructure/repositories/SessionJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.simplehexagonal.infrastructure.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SessionJpaRepository extends JpaRepository<SessionEntity, Long> {
-
-}
