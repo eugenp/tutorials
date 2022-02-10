8a7ee4a1fcaf9641da5fb34f6fb5733f5aaf1486
Graham Cox <graham@grahamcox.co.uk>
2022-02-10 07:32:33 +0000

    Replaced tabs with spaces in auto-generated files

--- persistence-modules/fauna/src/main/java/com/baeldung/faunablog/FaunaBlogApplication.java
+++ persistence-modules/fauna/src/main/java/com/baeldung/faunablog/FaunaBlogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FaunaBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FaunaBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FaunaBlogApplication.class, args);
+    }
 
 }
