771097c0861818e7d6f70afdb9f5e0b3c8f8154c
Silvio Gomes <silviorazec@gmail.com>
2022-02-10 10:38:05 -0300

    examples of the fail added

--- core-java-modules/core-java-lang-operators-2/src/test/java/com/baeldung/operators/ConditionalUnitTest.java
+++ core-java-modules/core-java-lang-operators-2/src/test/java/com/baeldung/operators/ConditionalUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.operators;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -28,6 +29,27 @@ class ConditionalUnitTest {
     }
 
     @Test
+    void whenUseTheOperatorConditionalIncluive_thenFail() {
+        int years = 25;
+        boolean driverLicence = false;
+        String result = "";
+        if (years >= 21 && driverLicence) {
+            result = "Successful Candidate.";
+        } else {
+            result = "Failed Candidate.";
+        }
+        assertNotEquals(result, "Successful Candidate.");
+    }
+
+    @Test
+    void whenUseTheOperatorConditionalInclusiveWithTernary_thenFail() {
+        int years = 25;
+        boolean driverLicence = false;
+        String result = (years >= 21 && driverLicence) ? "Successful Candidate." : "Successful Candidate.";
+        assertEquals(result, "Successful Candidate.");
+    }
+
+    @Test
     void whenUseTheOperatorConditionalAlternative_thenSuccess() {
         int years = 25;
         boolean driverLicence = false;
@@ -47,4 +69,24 @@ class ConditionalUnitTest {
         String result = (years >= 21 || driverLicence) ? "Successful Candidate." : "Successful Candidate.";
         assertEquals(result, "Successful Candidate.");
     }
+    
+    void whenUseTheOperatorConditionalAlternative_thenFail() {
+        int years = 20;
+        boolean driverLicence = false;
+        String result = "";
+        if (years >= 21 || driverLicence) {
+            result = "Successful Candidate.";
+        } else {
+            result = "Failed Candidate.";
+        }
+        assertEquals(result, "Successful Candidate.");
+    }
+
+    @Test
+    void whenUseTheOperatorConditionalAlternativeWithTernary_thenFail() {
+        int years = 25;
+        boolean driverLicence = false;
+        String result = (years >= 21 || driverLicence) ? "Successful Candidate." : "Successful Candidate.";
+        assertEquals(result, "Successful Candidate.");
+    }    
 }
