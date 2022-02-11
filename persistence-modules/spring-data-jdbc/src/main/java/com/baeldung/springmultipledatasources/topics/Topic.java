7f84edf0f8885a5159f65c229b079a422c2b1da1
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2022-02-11 20:49:43 +0100

    BAEL-5323: Merge sources into existing persistence module (#11804)
    * BAEL-5323: Spring Boot with multiple data sources
    
    * BAEL-5323: small fixes
    
    * BAEL-5323: fix test class
    
    * BAEL-5323: Refactor configuration classes
    
    * BAEL-5323: Set proeprty to private
    
    * BAEL-5323: rename test class to follow naming conventions
    
    * BAEL-5323: merge sources into existing persistence module
    
    * BAEL-5323: merge sources into existing persistence module

---
+++ persistence-modules/spring-data-jdbc/src/main/java/com/baeldung/springmultipledatasources/topics/Topic.java
@@ -0,0 +1,39 @@
+package com.baeldung.springmultipledatasources.topics;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+
+    public Topic() {
+    }
+
+    public Topic(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
