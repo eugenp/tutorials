723de4964fde1560d18f15fc8a6c97a7be3d93c5
Parikshit Murria <pmurria91@gmail.com>
2022-02-12 20:04:05 -0700

    BAEL-5326 - Documenting Enum in swagger using swagger maven plugin

---
+++ swagger-enum-documentation/src/test/java/com/baeldung/swaggerenums/SwaggerEnumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.swaggerenums;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SwaggerEnumsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
