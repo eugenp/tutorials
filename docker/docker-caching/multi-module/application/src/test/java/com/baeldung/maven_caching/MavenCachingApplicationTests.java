185266113c7d623faa8bee79aeb8de4578719cc1
msavic <mladensavic94@gmail.com>
2022-02-13 23:56:05 +0100

    Caching Maven Dependencies with Docker

---
+++ docker/docker-caching/multi-module/application/src/test/java/com/baeldung/maven_caching/MavenCachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.maven_caching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenCachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
