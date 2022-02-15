8abb95a1b1078e8cdfa80470daad4e09de009061
Iulian Manda <julian.manda_ext@autoscout24.com>
2022-02-15 17:33:38 +0200

    Code review

--- jackson-modules/jackson-annotations/src/test/java/com/baeldung/jackson/deduction_based_polymorphism/SimpleInferenceUnitTest.java
+++ jackson-modules/jackson-annotations/src/test/java/com/baeldung/jackson/deduction_based_polymorphism/SimpleInferenceUnitTest.java
@@ -10,14 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SimpleInferenceUnitTest {
+class SimpleInferenceUnitTest {
 
     private final ObjectMapper objectMapper = JsonMapper.builder().build();
 
     @Test
     void givenAKnight_whenMapping_thenExpectAKnightType() throws Exception {
         String knightJson = formatJson("{'name':'Ostrava, of Boletaria', 'weapon':'Rune Sword'}");
+
         Character character = objectMapper.readValue(knightJson, Character.class);
+
         assertTrue(character instanceof Knight);
         assertSame(character.getClass(), Knight.class);
         Knight king = (Knight) character;
@@ -28,7 +30,9 @@ public class SimpleInferenceUnitTest {
     @Test
     void givenAKing_whenMapping_thenExpectAKingType() throws Exception {
         String kingJson = formatJson("{'name':'Old King Allant', 'land':'Boletaria'}");
+
         Character character = objectMapper.readValue(kingJson, Character.class);
+
         assertTrue(character instanceof King);
         assertSame(character.getClass(), King.class);
         King king = (King) character;
@@ -39,13 +43,16 @@ public class SimpleInferenceUnitTest {
     @Test
     void givenAnEmptyObject_whenMapping_thenExpectAnImperialSpy() throws Exception {
         String imperialSpyJson = "{}";
+
         Character character = objectMapper.readValue(imperialSpyJson, Character.class);
+
         assertTrue(character instanceof ImperialSpy);
     }
 
     @Test
     void givenANullObject_whenMapping_thenExpectANullObject() throws Exception {
         Character character = objectMapper.readValue("null", Character.class);
+
         assertNull(character);
     }
 
