3efc1e5514b652434c739203b76180d71c65879b
LUCA <lucacambi77@gmail.com>
2022-02-15 09:51:23 +0100

    fix: apply formatter

--- spring-security-modules/spring-security-web-boot-3/src/main/java/com/baeldung/logging/SecurityConfig.java
+++ spring-security-modules/spring-security-web-boot-3/src/main/java/com/baeldung/logging/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**")
-                .permitAll();
+            .antMatchers("/**")
+            .permitAll();
     }
 }
