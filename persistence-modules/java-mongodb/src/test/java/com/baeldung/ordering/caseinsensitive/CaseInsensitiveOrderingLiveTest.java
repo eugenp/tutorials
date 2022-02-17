db70508d9611f2b2062ad73bf2ecbb8a18bfd6b4
Gaetano Piazzolla <gae.piaz@gmail.com>
2022-02-17 16:33:09 +0100

    [BAEL-5358] - using junit5 and testcontainers

--- persistence-modules/java-mongodb/src/test/java/com/baeldung/ordering/caseinsensitive/CaseInsensitiveOrderingLiveTest.java
+++ persistence-modules/java-mongodb/src/test/java/com/baeldung/ordering/caseinsensitive/CaseInsensitiveOrderingLiveTest.java
@@ -7,9 +7,11 @@ import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Sorts;
 import org.bson.Document;
 import org.bson.conversions.Bson;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -22,44 +24,45 @@ import static org.junit.Assert.assertEquals;
 
 public class CaseInsensitiveOrderingLiveTest {
 
-    private MongoClient mongoClient;
-    private MongoCollection<Document> collection;
+    private static MongoCollection<Document> userCollections;
 
-    @Before
-    public void setup() {
-        if (mongoClient == null) {
-            MongoClient mongoClient = new MongoClient();
-            MongoDatabase db = mongoClient.getDatabase("sorting");
-            MongoCollection<Document> collection = db.getCollection("users");
+    private static MongoDBContainer mongoDBContainer;
 
-            List<Document> list = new ArrayList<>();
+    @BeforeAll
+    public static void setup() {
+        mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
+        mongoDBContainer.start();
 
-            list.add(Document.parse("{'name': 'ben', surname: 'ThisField' }"));
-            list.add(Document.parse("{'name': 'aen', surname: 'Does' }"));
-            list.add(Document.parse("{'name': 'Ben', surname: 'Not' }"));
-            list.add(Document.parse("{'name': 'cen', surname: 'Matter' }"));
-            list.add(Document.parse("{'name': 'Aen', surname: 'Really' }"));
-            list.add(Document.parse("{'name': 'Cen', surname: 'TrustMe' }"));
+        MongoClient mongoClient = new MongoClient(mongoDBContainer.getContainerIpAddress(), mongoDBContainer.getMappedPort(27017));
+        MongoDatabase database = mongoClient.getDatabase("test");
+        userCollections = database.getCollection("users");
 
-            collection.insertMany(list);
-        }
+        List<Document> list = new ArrayList<>();
+        list.add(Document.parse("{'name': 'ben', surname: 'ThisField' }"));
+        list.add(Document.parse("{'name': 'aen', surname: 'Does' }"));
+        list.add(Document.parse("{'name': 'Ben', surname: 'Not' }"));
+        list.add(Document.parse("{'name': 'cen', surname: 'Matter' }"));
+        list.add(Document.parse("{'name': 'Aen', surname: 'Really' }"));
+        list.add(Document.parse("{'name': 'Cen', surname: 'TrustMe' }"));
+
+        userCollections.insertMany(list);
     }
 
-    @After
-    public void tearDown() {
-        collection.deleteMany(Document.parse("{}"));
+    @AfterAll
+    public static void tearDown() {
+        mongoDBContainer.stop();
     }
 
     @Test
     public void givenMongoCollection_whenUsingFindWithSort_caseIsConsideredByDefault() {
-        FindIterable<Document> nameDoc = collection.find().sort(ascending("name"));
+        FindIterable<Document> nameDoc = userCollections.find().sort(ascending("name"));
         MongoCursor<Document> cursor = nameDoc.cursor();
         assertOrdering(cursor, Arrays.asList("Aen", "Ben", "Cen", "aen", "ben", "cen"));
     }
 
     @Test
     public void givenMongoCollection_whenUsingFindWithSortAndCollation_caseIsNotConsidered() {
-        FindIterable<Document> nameDoc = collection.find().sort(ascending("name"))
+        FindIterable<Document> nameDoc = userCollections.find().sort(ascending("name"))
           .collation(Collation.builder().locale("en").build());
         MongoCursor<Document> cursor = nameDoc.cursor();
         assertOrdering(cursor, Arrays.asList("aen", "Aen", "ben", "Ben", "cen", "Cen"));
@@ -73,7 +76,7 @@ public class CaseInsensitiveOrderingLiveTest {
             Projections.include("name", "surname"),
             Projections.computed("lowerName", Projections.computed("$toLower", "$name"))));
 
-        AggregateIterable<Document> nameDoc = collection.aggregate(
+        AggregateIterable<Document> nameDoc = userCollections.aggregate(
           Arrays.asList(projectBson,
             sort(Sorts.ascending("lowerName"))));
 
