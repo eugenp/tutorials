ab6b4d345b4ee9d2f06fa800cea139d10110f136
s9m33r <no-reply>
2022-02-18 10:10:30 +0530

    static imports

--- maven-modules/maven-classifier/maven-classifier-example-provider/src/test/java/com/baeldung/maven/dependency/classifier/provider/factory/CarFactoryUnitTest.java
+++ maven-modules/maven-classifier/maven-classifier-example-provider/src/test/java/com/baeldung/maven/dependency/classifier/provider/factory/CarFactoryUnitTest.java
@@ -5,21 +5,21 @@ import com.baeldung.maven.dependency.classifier.provider.model.Car;
 import com.baeldung.maven.dependency.classifier.provider.model.Car.Type;
 import com.baeldung.maven.dependency.classifier.provider.model.PowerSource;
 import com.baeldung.maven.dependency.classifier.provider.stub.CarStub;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class CarFactoryUnitTest {
-    CarFactoryUnitTest() {
-    }
 
     @Test
     @DisplayName("Given Car type When CarFactory manufacture is called Then create a Car of the given type")
     public void givenCarType_whenCarFactoryManufactureCalled_thenCreateCarOfGivenType() {
         Car car = CarFactory.manufacture(Type.ELECTRIC);
 
-        Assertions.assertNotNull(car, "CarFactory didn't manufacture a car. Car is null");
-        Assertions.assertEquals(Type.ELECTRIC, car.getType());
+        assertNotNull(car, "CarFactory didn't manufacture a car. Car is null");
+        assertEquals(Type.ELECTRIC, car.getType());
     }
 
     @Test
@@ -27,6 +27,6 @@ class CarFactoryUnitTest {
     public void givenElectricCar_whenAskedForFuelType_thenReturnBattery() {
         Car car = CarStub.ELECTRIC_CAR;
 
-        Assertions.assertEquals(PowerSource.BATTERY, car.getPowerSource());
+        assertEquals(PowerSource.BATTERY, car.getPowerSource());
     }
 }
