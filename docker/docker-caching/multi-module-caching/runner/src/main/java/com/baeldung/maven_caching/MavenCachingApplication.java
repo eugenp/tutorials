fb851d088c4bf707d89725ab4abd4c53b9b7701c
Mladen Savic <mladensavic94@gmail.com>
2022-02-20 22:51:08 +0100

    Caching Maven Dependencies with Docker - improvements

--- docker/docker-caching/multi-module-caching/runner/src/main/java/com/baeldung/maven_caching/MavenCachingApplication.java
+++ docker/docker-caching/multi-module-caching/runner/src/main/java/com/baeldung/maven_caching/MavenCachingApplication.java
@@ -1,15 +1,11 @@
 package com.baeldung.maven_caching;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class MavenCachingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MavenCachingApplication.class, args);
         CoreClass cc = new CoreClass();
         System.out.println(cc.method());
+        System.out.println(cc.dependencyMethod());
     }
 
 }
