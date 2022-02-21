e7572d691e92915c46d486cb4c7c023ae618f475
sebx59 <sebastien.hardeman@gmail.com>
2022-02-21 15:56:27 +0100

    BAEL-4674 - Basic Authentication in JMeter
    Test plans & resources for article Basic Authentication in JMeter
    Server classes to test basic auth against local server

---
+++ jmeter/src/main/java/com/baeldung/controller/SecuredUuidController.java
@@ -0,0 +1,26 @@
+package com.baeldung.controller;
+
+import static java.lang.String.format;
+
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.baeldung.model.Response;
+
+@RestController
+public class SecuredUuidController {
+      
+    private Logger logger = LoggerFactory.getLogger(SecuredUuidController.class);
+
+    @GetMapping("/secured/uuid")
+    public Response uuid() {
+        
+        logger.info("Returning response");
+        
+        return new Response(format("Secured test message... %s.", UUID.randomUUID()));
+    }
+}
