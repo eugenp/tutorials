545fcb678042e86c81d0f05d3f490675511233dc
anuragkumawat <anuragkumawat7@gmail.com>
2022-02-23 19:54:43 +0530

    JAVA-10081: Removed unused files and updated pom.xml

---
+++ persistence-modules/hibernate-mapping-2/src/main/java/com/baeldung/manytomany/dao/IEmployeeDao.java
@@ -0,0 +1,8 @@
+package com.baeldung.manytomany.dao;
+
+import com.baeldung.manytomany.dao.common.IOperations;
+import com.baeldung.manytomany.model.Employee;
+
+public interface IEmployeeDao extends IOperations<Employee>{
+
+}
