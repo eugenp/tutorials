7fd5c78f1cb9beb098c39f91c4550ef8750abbdd
mladensavic94 <mladensavic94@gmail.com>
2022-02-25 09:54:42 +0100

    Caching Maven Dependencies with Docker - fix

--- docker/docker-caching/single-module-caching/src/main/java/com/baeldung/maven_caching/MavenCachingMain.java
+++ docker/docker-caching/single-module-caching/src/main/java/com/baeldung/maven_caching/MavenCachingMain.java
@@ -5,7 +5,7 @@ import com.google.common.io.Files;
 public class MavenCachingMain {
 
     public static void main(String[] args) {
-		System.out.println("Hello from maven_caching app!!!");
+        System.out.println("Hello from maven_caching app!!!");
         System.out.println(Files.simplifyPath("/home/app/test"));
     }
 }
