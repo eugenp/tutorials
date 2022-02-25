de6d367e413f3ec302ac19030030e20c11fabe06
sanitaso <thesataso@gmail.com>
2022-02-25 12:54:20 +0100

    add files to springdoc

---
+++ spring-boot-modules/spring-boot-springdoc/swaggerResponseAPI/src/main/java/com/baeldung/swaggerresponseapi/model/Product.java
@@ -0,0 +1,15 @@
+package com.baeldung.swaggerresponseapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    String code;
+    String name;
+}
