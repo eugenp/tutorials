440f4e476c0584a079478fbe1108c29b6d650d83
Kapil Khandelwal <kapilbk1996@gmail.com>
2022-02-28 01:51:17 +0530

    BAEL-5359: Updating Multiple Fields in a MongoDB Document (#11861)
    * BAEL-5359: Updating Multiple Fields in a MongoDB Document
    
    * BAEL-5359: Add test class file
    
    * BAEL-5359: Rename UpdateMultipleFieldsUnitTest.java to UpdateMultipleFieldsLiveTest.java

---
+++ persistence-modules/java-mongodb/src/main/java/com/baeldung/mongo/update/MultipleFieldsExample.java
@@ -0,0 +1,44 @@
+package com.baeldung.mongo.update;
+
+import org.bson.Document;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.UpdateResult;
+
+public class MultipleFieldsExample {
+
+	public static void main(String[] args) {
+
+	    //
+	    // Connect to cluster (default is localhost:27017)
+	    //
+
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		MongoDatabase database = mongoClient.getDatabase("baeldung");
+		MongoCollection<Document> collection = database.getCollection("employee");
+
+		//
+		// Filter on the basis of employee_id
+		//
+
+		BasicDBObject searchQuery = new BasicDBObject("employee_id", 794875);
+
+		//
+		// Update the fields in Document
+		//
+
+		BasicDBObject updateFields = new BasicDBObject();
+		updateFields.append("department_id", 3);
+		updateFields.append("job", "Sales Manager");
+		BasicDBObject setQuery = new BasicDBObject();
+		setQuery.append("$set", updateFields);
+		UpdateResult updateResult = collection.updateMany(searchQuery, setQuery);
+
+		System.out.println("updateResult:- " + updateResult);
+		System.out.println("updateResult:- " + updateResult.getModifiedCount());
+
+	}
+}
