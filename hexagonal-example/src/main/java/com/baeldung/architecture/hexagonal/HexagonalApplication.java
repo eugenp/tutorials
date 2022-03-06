e19fb548e2f4e3e1a1ebf878c7e7f9eb46707901
harry9656 <singharpal9656@gmail.com>
2022-03-06 18:26:51 +0100

    First commit for hexagonal Architecture

---
+++ hexagonal-example/src/main/java/com/baeldung/architecture/hexagonal/HexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.architecture.hexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalApplication.class, args);
+	}
+
+}
---
+++ hexagonal-example/src/test/java/com/baeldung/architecture/hexagonal/HexagonalApplicationIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.baeldung.architecture.hexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalApplicationIntegrationTest {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
