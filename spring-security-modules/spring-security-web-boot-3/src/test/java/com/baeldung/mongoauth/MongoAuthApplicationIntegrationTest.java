8b8e3913e3361afcfe39cd58a91be5164d355ded
LUCA <lucacambi77@gmail.com>
2022-03-07 12:13:51 +0100

    fix: static finale case

--- spring-security-modules/spring-security-web-boot-3/src/test/java/com/baeldung/mongoauth/MongoAuthApplicationIntegrationTest.java
+++ spring-security-modules/spring-security-web-boot-3/src/test/java/com/baeldung/mongoauth/MongoAuthApplicationIntegrationTest.java
@@ -40,9 +40,9 @@ class MongoAuthApplicationIntegrationTest {
 
     private MockMvc mvc;
 
-    private static final String userName = "user@gmail.com";
-    private static final String adminName = "admin@gmail.com";
-    private static final String password = "password";
+    private static final String USER_NAME = "user@gmail.com";
+    private static final String ADMIN_NAME = "admin@gmail.com";
+    private static final String PASSWORD = "password";
 
     @BeforeEach
     public void setup() {
@@ -60,8 +60,8 @@ class MongoAuthApplicationIntegrationTest {
         mongoTemplate.save(roleUser);
 
         User user = new User();
-        user.setUsername(userName);
-        user.setPassword(bCryptPasswordEncoder.encode(password));
+        user.setUsername(USER_NAME);
+        user.setPassword(bCryptPasswordEncoder.encode(PASSWORD));
 
         UserRole userRole = new UserRole();
         userRole.setRole(roleUser);
@@ -69,8 +69,8 @@ class MongoAuthApplicationIntegrationTest {
         mongoTemplate.save(user);
 
         User admin = new User();
-        admin.setUsername(adminName);
-        admin.setPassword(bCryptPasswordEncoder.encode(password));
+        admin.setUsername(ADMIN_NAME);
+        admin.setPassword(bCryptPasswordEncoder.encode(PASSWORD));
 
         Role roleAdmin = new Role();
         roleAdmin.setName("ROLE_ADMIN");
@@ -84,7 +84,7 @@ class MongoAuthApplicationIntegrationTest {
 
     @Test
     void givenUserCredentials_whenInvokeUserAuthorizedEndPoint_thenReturn200() throws Exception {
-        mvc.perform(get("/user").with(httpBasic(userName, password)))
+        mvc.perform(get("/user").with(httpBasic(USER_NAME, PASSWORD)))
           .andExpect(status().isOk());
     }
 
@@ -96,22 +96,22 @@ class MongoAuthApplicationIntegrationTest {
 
     @Test
     void givenUserExistsAndWrongPassword_whenInvokeEndPoint_thenReturn401() throws Exception {
-        mvc.perform(get("/user").with(httpBasic(userName, "wrong_password")))
+        mvc.perform(get("/user").with(httpBasic(USER_NAME, "wrong_password")))
           .andExpect(status().isUnauthorized());
     }
 
     @Test
     void givenUserCredentials_whenInvokeAdminAuthorizedEndPoint_thenReturn403() throws Exception {
-        mvc.perform(get("/admin").with(httpBasic(userName, password)))
+        mvc.perform(get("/admin").with(httpBasic(USER_NAME, PASSWORD)))
           .andExpect(status().isForbidden());
     }
 
     @Test
     void givenAdminCredentials_whenInvokeAdminAuthorizedEndPoint_thenReturn200() throws Exception {
-        mvc.perform(get("/admin").with(httpBasic(adminName, password)))
+        mvc.perform(get("/admin").with(httpBasic(ADMIN_NAME, PASSWORD)))
           .andExpect(status().isOk());
 
-        mvc.perform(get("/user").with(httpBasic(adminName, password)))
+        mvc.perform(get("/user").with(httpBasic(ADMIN_NAME, PASSWORD)))
           .andExpect(status().isOk());
     }
 
