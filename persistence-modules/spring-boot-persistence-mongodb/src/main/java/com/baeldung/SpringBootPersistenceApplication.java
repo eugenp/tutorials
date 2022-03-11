b617e5a11b9386f6d5a207f37ef618e2c5150b09
Ulisses Lima <ulisses@dvlcube.com>
2022-03-11 20:11:54 -0300

    BAEL-5403 - Import Data to MongoDB from Json File using Java
    * Project
    * Integration tests

--- persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/SpringBootPersistenceApplication.java
+++ persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/SpringBootPersistenceApplication.java
@@ -1,13 +1,59 @@
 package com.baeldung;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.baeldung.boot.json.convertfile.ImportUtils;
+import com.baeldung.boot.json.convertfile.service.ImportJsonService;
 
 @SpringBootApplication
-public class SpringBootPersistenceApplication {
+public class SpringBootPersistenceApplication implements ApplicationRunner {
+    private Logger log = LogManager.getLogger(this.getClass());
+    private static final String RESOURCE_PREFIX = "classpath:";
+
+    @Autowired
+    private ApplicationContext context;
 
     public static void main(String ... args) {
         SpringApplication.run(SpringBootPersistenceApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (args.containsOption("import")) {
+            if (!args.containsOption("collection"))
+                throw new IllegalArgumentException("required option: --collection with collection name when using --import");
+
+            String collection = args.getOptionValues("collection")
+                .get(0);
+
+            List<String> sources = args.getOptionValues("import");
+            for (String source : sources) {
+                List<String> jsonLines = new ArrayList<>();
+                if (source.startsWith(RESOURCE_PREFIX)) {
+                    String resource = source.substring(RESOURCE_PREFIX.length());
+                    jsonLines = ImportUtils.linesFromResource(resource);
+                } else {
+                    jsonLines = ImportUtils.lines(new File(source));
+                }
+
+                if (jsonLines == null || jsonLines.isEmpty())
+                    throw new IllegalArgumentException("no input to import");
+
+                ImportJsonService importService = context.getBean(ImportJsonService.class);
+                String result = importService.importTo(collection, jsonLines);
+                log.info(source + " - result: " + result);
+            }
+        }
+    }
 }
