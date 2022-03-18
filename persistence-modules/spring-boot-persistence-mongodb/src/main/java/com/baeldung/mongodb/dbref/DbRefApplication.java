db4a9cc761fa5784802f5dd7280b38993fd4ec15
AttilaUhrin <uhrin.attila1@gmail.com>
2022-03-18 04:16:47 +0100

    [BAEL-5373] A Guide to @DBRef in MongoDB (#11909)
    * Add implementation for BAEL-5373.
    
    * Fix test class name.
    
    * Change collection names to be aligned with article.

---
+++ persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/mongodb/dbref/DbRefApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.mongodb.dbref;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbRefApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(DbRefApplication.class, args);
+    }
+
+}
