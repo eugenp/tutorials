131f1356462ae73c263a51f747dec1a34ef995c0
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2022-03-20 17:11:44 +0100

    rename the unittest class

--- core-java-modules/core-java-io-4/src/test/java/com/baeldung/userinput/UserInputHandlerUnitTest.java
+++ core-java-modules/core-java-io-4/src/test/java/com/baeldung/userinput/UserInputHandlerUnitTest.java
@@ -11,7 +11,7 @@ import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UserInputHandlerTest {
+public class UserInputHandlerUnitTest {
 
     @Test
     public void givenDataInSystemIn_whenCallingReadUserInputMethod_thenHaveUserInputData() {
