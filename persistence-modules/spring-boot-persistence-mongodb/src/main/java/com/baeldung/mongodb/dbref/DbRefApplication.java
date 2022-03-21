abb131dd54a44e991ddb7a0174258994798c3da3
Attila Uhrin <uhrin.attila1@gmail.com>
2022-03-21 17:43:35 +0100

    Fix integration tests.

--- persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/mongodb/dbref/DbRefApplication.java
+++ persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/mongodb/dbref/DbRefApplication.java
@@ -2,8 +2,10 @@ package com.baeldung.mongodb.dbref;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = { "com.baeldung" })
 public class DbRefApplication {
 
     public static void main(String... args) {
