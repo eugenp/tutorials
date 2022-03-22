f7420526f4a2f93f6d7405887f5d0cba52fee304
Krzysiek <krzysztofwoyke@gmail.com>
2022-03-22 10:01:46 +0100

    JAVA-10236: Fix a typo

--- spring-native/src/main/java/com/baeldung/springnativeintro/SpringNativeApp.java
+++ spring-native/src/main/java/com/baeldung/springnativeintro/SpringNativeApp.java
@@ -3,7 +3,7 @@ package com.baeldung.springnativeintro;
 public class SpringNativeApp {
     
     public static void main(String[] args) {
-        System.out.println("Hello, World! This is a Baledung Spring Native Application");
+        System.out.println("Hello, World! This is a Baeldung Spring Native Application");
     }
 
 }
