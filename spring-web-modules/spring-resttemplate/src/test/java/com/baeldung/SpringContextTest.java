598ed10d60df8bdcbe617a94b01f3535b6c2e10d
Dhawal Kapil <dhawalkapil@gmail.com>
2022-03-23 08:39:37 +0530

    JAVA-10377 Removed @RunWith as there is no specific configuration class needed to run this test

--- spring-web-modules/spring-resttemplate/src/test/java/com/baeldung/SpringContextTest.java
+++ spring-web-modules/spring-resttemplate/src/test/java/com/baeldung/SpringContextTest.java
@@ -1,11 +1,8 @@
 package com.baeldung;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringContextTest {
 
