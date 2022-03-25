5383fc155f4da7f8a1225fede6831f0e16ef9c60
Bhaskara Navuluri <bhaskara.navuluri@hpe.com>
2022-03-26 00:39:24 +0530

    Changed the Parent to parent-boot-2

--- feign/src/test/java/com/baeldung/feign/clients/BookClientLiveTest.java
+++ feign/src/test/java/com/baeldung/feign/clients/BookClientLiveTest.java
@@ -21,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Consumes https://github.com/Baeldung/spring-hypermedia-api
  */
 @Slf4j
-class BookClientIntegrationTest {
+class BookClientLiveTest {
     private BookClient bookClient;
 
     @BeforeEach
--- feign/src/test/java/com/baeldung/feign/soap/FeignLiveTest.java
+++ feign/src/test/java/com/baeldung/feign/soap/FeignLiveTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
-class FeignIntegrationTest {
+class FeignLiveTest {
     @Test
     void givenSOAPPayload_whenStringRequest_thenReturnSOAPResponse() {
         //@formatter:off
