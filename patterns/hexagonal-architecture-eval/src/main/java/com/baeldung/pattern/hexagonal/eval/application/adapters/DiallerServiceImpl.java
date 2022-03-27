13979ab844ba5e8d58e92e521f025bf158b16b3a
Shaun Phillips <61982125+ShaPhi7@users.noreply.github.com>
2022-03-27 13:39:18 +0100

    Add logger

--- patterns/hexagonal-architecture-eval/src/main/java/com/baeldung/pattern/hexagonal/eval/application/adapters/DiallerServiceImpl.java
+++ patterns/hexagonal-architecture-eval/src/main/java/com/baeldung/pattern/hexagonal/eval/application/adapters/DiallerServiceImpl.java
@@ -1,24 +1,42 @@
-package main.java.com.baeldung.pattern.hexagonal.eval.application.adapters;
+package com.baeldung.pattern.hexagonal.eval.application.adapters;
 
-import main.java.com.baeldung.pattern.hexagonal.eval.domain.model.Contact;
-import main.java.com.baeldung.pattern.hexagonal.eval.domain.ports.DiallerService;
+import java.util.StringJoiner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.baeldung.pattern.hexagonal.eval.domain.model.Contact;
+import com.baeldung.pattern.hexagonal.eval.domain.ports.DiallerService;
 
 public class DiallerServiceImpl implements DiallerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DiallerServiceImpl.class);
 
     private AddressBookRepositoryImpl addressBookImpl;
 
     public DiallerServiceImpl(AddressBookRepositoryImpl addressBookImpl) {
-        
+
         this.addressBookImpl = addressBookImpl;
     }
 
     @Override
-    public boolean dial(String nickame) {
+    public boolean dial(String nickname) {
 
-        Contact contactToDial = addressBookImpl.retrieveContact(nickame);
+        Contact contactToDial = addressBookImpl.retrieveContact(nickname);
 
-        System.out.println("Dialling " + contactToDial.getNumber());
-        
+        // we only log the call details rather than actually dialling the contact
+        LOGGER.info(getDialInfoStr(nickname, contactToDial));
+
         return true;
+    }
+
+    private String getDialInfoStr(String nickname, Contact contactToDial) {
+        return new StringJoiner(" ").add("Dialling")
+            .add(contactToDial.getName())
+            .add("aka")
+            .add(nickname)
+            .add("on")
+            .add(contactToDial.getNumber())
+            .toString();
     }
 }
