1d1791c90d989b111c21474ec9332685e12d61dd
soni.sk@1969 <erpsoni7@gmail.com>
2022-03-30 11:08:29 +0530

    BAEL-5329 Hide a Request Field in Swagger UI

---
+++ spring-boot-modules/spring-boot-mvc/src/main/java/com/baeldung/springboot/swagger/ArticleApplication.java
@@ -0,0 +1,31 @@
+package com.baeldung.springboot.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class ArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleApplication.class, args);
+		
+	}
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
+
+}
