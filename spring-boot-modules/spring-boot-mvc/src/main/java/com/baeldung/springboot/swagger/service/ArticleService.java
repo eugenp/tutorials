dbbc6a6a1df0a3d9d1fc5a32324bc403f2d08acd
priya-soni <erpsoni7@gmail.com>
2022-04-05 00:14:28 +0530

    BAEL-5329 Hide a Request Field in Swagger UI (#11992)
    * BAEL-5329 Hide a Request Field in Swagger UI
    
    * Updated indentation

---
+++ spring-boot-modules/spring-boot-mvc/src/main/java/com/baeldung/springboot/swagger/service/ArticleService.java
@@ -0,0 +1,25 @@
+package com.baeldung.springboot.swagger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.baeldung.springboot.swagger.model.Article;
+
+@Service
+public class ArticleService {
+
+    private List<Article> articles = new ArrayList<>();
+
+    public List<Article> getAllArticles() {
+        return articles;
+    }
+
+    public void addArticle(Article article) {
+        article.setId(articles.size() + 1);
+        articles.add(article);
+    }
+	
+
+}
