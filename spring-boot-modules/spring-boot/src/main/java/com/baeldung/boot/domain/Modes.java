e24171d732656ac786886857b095f0e51b63792d
sampadawagde <sampada.aws@gmail.com>
2022-04-09 17:51:42 +0530

    JAVA-10516: Revisit spring-boot module inside spring-boot-modules folder

--- spring-boot-modules/spring-boot/src/main/java/com/baeldung/boot/domain/Modes.java
+++
@@ -1,6 +0,0 @@
-package com.baeldung.boot.domain;
-
-public enum Modes {
-
-    ALPHA, BETA;
-}
--- spring-boot-modules/spring-boot/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists USERS;
-
-create table USERS(
-  ID int not null AUTO_INCREMENT,
-  NAME varchar(100) not null,
-  STATUS int,
-  PRIMARY KEY ( ID )
-);
