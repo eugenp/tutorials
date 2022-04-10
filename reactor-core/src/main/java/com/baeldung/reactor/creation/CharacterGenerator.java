2655c90b0720b7e0a221c29b478c391c9092b2e9
Somnath Musib <7885767+musibs@users.noreply.github.com>
2022-04-10 08:12:09 +0530

    BAEL-4886 Flux generate vs create

---
+++ reactor-core/src/main/java/com/baeldung/reactor/creation/CharacterGenerator.java
@@ -0,0 +1,18 @@
+package com.baeldung.reactor.creation;
+
+import reactor.core.publisher.Flux;
+
+public class CharacterGenerator {
+    
+    public Flux<Character> generateCharacters() {
+        
+        return Flux.generate(() -> 97, (state, sink) -> {
+            char value = (char) state.intValue();
+            sink.next(value);
+            if (value == 'z') {
+                sink.complete();
+            }
+            return state + 1;
+        });
+    }
+}
