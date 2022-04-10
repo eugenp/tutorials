859e4bac776d7b522549c2d4a8b452f5179ce1d6
Somnath Musib <7885767+musibs@users.noreply.github.com>
2022-04-10 08:12:09 +0530

    BAEL-4886 Flux generate vs create

---
+++ reactor-core/src/test/java/com/baeldung/reactor/creation/CharacterUnitTest.java
@@ -0,0 +1,50 @@
+package com.baeldung.reactor.creation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class CharacterUnitTest {
+
+    @Test
+    public void whenGeneratingCharacters_thenCharactersAreProduced() {
+        CharacterGenerator characterGenerator = new CharacterGenerator();
+        Flux<Character> characterFlux = characterGenerator.generateCharacters().take(3);
+
+        StepVerifier.create(characterFlux)
+                .expectNext('a', 'b', 'c')
+                .expectComplete()
+                .verify();
+    }
+    
+    @Test
+    public void whenCreatingCharactersWithMultipleThreads_thenSequenceIsProducedAsynchronously() throws InterruptedException {
+        CharacterGenerator characterGenerator = new CharacterGenerator();
+        List<Character> sequence1 = characterGenerator.generateCharacters().take(3).collectList().block();
+        List<Character> sequence2 = characterGenerator.generateCharacters().take(2).collectList().block();
+
+        CharacterCreator characterCreator = new CharacterCreator();
+        Thread producingThread1 = new Thread(
+                () -> characterCreator.consumer.accept(sequence1)
+        );
+        Thread producingThread2 = new Thread(
+                () -> characterCreator.consumer.accept(sequence2)
+        );
+
+        List<Character> consolidated = new ArrayList<>();
+        characterCreator.createCharacterSequence().subscribe(consolidated::add);
+
+        producingThread1.start();
+        producingThread2.start();
+        producingThread1.join();
+        producingThread2.join();
+
+        assertThat(consolidated).containsExactlyInAnyOrder('a', 'b', 'c', 'a', 'b');
+    }
+}
