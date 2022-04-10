7e4f28150c14a4926082713a52edb7182bec18ae
lucaCambi77 <lucacambi77@gmail.com>
2022-04-10 16:19:13 +0200

    BAEL-5400 - Return Only Specific Fields for a Query in Spring Data MongoDB (#11991)
    * feat: field projection spring data mongodb
    
    * fix: use available ports
    
    * fix: typo
    
    * fix: add abstraction
    
    * fix: avoid blank lines

---
+++ persistence-modules/spring-boot-persistence-mongodb/src/test/java/com/baeldung/projection/MongoTemplateProjectionUnitTest.java
@@ -0,0 +1,216 @@
+package com.baeldung.projection;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.query.Query;
+
+import com.baeldung.projection.model.InStock;
+import com.baeldung.projection.model.Inventory;
+import com.baeldung.projection.model.Size;
+
+@SpringBootTest
+public class MongoTemplateProjectionUnitTest extends AbstractTestProjection {
+
+    @BeforeEach
+    void setup() throws Exception {
+        super.setUp();
+
+        List<Inventory> inventoryList = getInventories();
+
+        mongoTemplate.insert(inventoryList, Inventory.class);
+    }
+
+    @Test
+    void whenIncludeFields_thenOnlyIncludedFieldsAreNotNull() {
+        Query query = new Query();
+        query.fields()
+          .include("item")
+          .include("status");
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getId());
+            assertNotNull(i.getItem());
+            assertNotNull(i.getStatus());
+            assertNull(i.getSize());
+            assertNull(i.getInStock());
+        });
+    }
+
+    @Test
+    void whenIncludeFieldsAndExcludeOtherFields_thenOnlyExcludedFieldsAreNull() {
+        Query query = new Query();
+        query.fields()
+          .include("item")
+          .include("status")
+          .exclude("_id");
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getItem());
+            assertNotNull(i.getStatus());
+            assertNull(i.getId());
+            assertNull(i.getSize());
+            assertNull(i.getInStock());
+        });
+    }
+
+    @Test
+    void whenIncludeAllButExcludeSomeFields_thenOnlyExcludedFieldsAreNull() {
+        Query query = new Query();
+        query.fields()
+          .exclude("status")
+          .exclude("inStock");
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getItem());
+            assertNotNull(i.getId());
+            assertNotNull(i.getSize());
+            assertNull(i.getInStock());
+            assertNull(i.getStatus());
+        });
+    }
+
+    @Test
+    void whenIncludeEmbeddedFields_thenEmbeddedFieldsAreNotNull() {
+        Query query = new Query();
+        query.fields()
+          .include("item")
+          .include("status")
+          .include("size.uom");
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getItem());
+            assertNotNull(i.getStatus());
+            assertNotNull(i.getId());
+            assertNotNull(i.getSize());
+            assertNotNull(i.getSize()
+              .getUom());
+            assertNull(i.getSize()
+              .getHeight());
+            assertNull(i.getSize()
+              .getWidth());
+            assertNull(i.getInStock());
+        });
+    }
+
+    @Test
+    void whenExcludeEmbeddedFields_thenEmbeddedFieldsAreNull() {
+        Query query = new Query();
+        query.fields()
+          .exclude("size.uom");
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getItem());
+            assertNotNull(i.getStatus());
+            assertNotNull(i.getId());
+            assertNotNull(i.getSize());
+            assertNull(i.getSize()
+              .getUom());
+            assertNotNull(i.getSize()
+              .getHeight());
+            assertNotNull(i.getSize()
+              .getWidth());
+            assertNotNull(i.getInStock());
+        });
+    }
+
+    @Test
+    void whenIncludeEmbeddedFieldsInArray_thenEmbeddedFieldsInArrayAreNotNull() {
+        Query query = new Query();
+        query.fields()
+          .include("item")
+          .include("status")
+          .include("inStock.quantity");
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getItem());
+            assertNotNull(i.getStatus());
+            assertNotNull(i.getId());
+            assertNotNull(i.getInStock());
+            i.getInStock()
+              .forEach(stock -> {
+                  assertNull(stock.getWareHouse());
+                  assertNotNull(stock.getQuantity());
+              });
+            assertNull(i.getSize());
+        });
+    }
+
+    @Test
+    void whenIncludeEmbeddedFieldsSliceInArray_thenArrayLengthEqualToSlice() {
+        Inventory postcard = new Inventory();
+        postcard.setItem("postcard");
+        postcard.setStatus("A");
+        postcard.setSize(new Size(10.0, 15.25, "cm"));
+
+        InStock firstInStock = new InStock("B", 15);
+        InStock lastInStock = new InStock("C", 35);
+
+        postcard.setInStock(Arrays.asList(firstInStock, lastInStock));
+
+        mongoTemplate.save(postcard);
+
+        Query query = new Query();
+        query.fields()
+          .include("item")
+          .include("status")
+          .slice("inStock", -1);
+
+        List<Inventory> inventoryList = mongoTemplate.find(query, Inventory.class);
+
+        assertTrue(inventoryList.size() > 0);
+
+        inventoryList.forEach(i -> {
+            assertNotNull(i.getItem());
+            assertNotNull(i.getStatus());
+            assertNotNull(i.getId());
+            assertNotNull(i.getInStock());
+            assertEquals(1, i.getInStock()
+              .size());
+            assertNull(i.getSize());
+        });
+
+        InStock stock = inventoryList.stream()
+          .filter(i -> i.getItem()
+            .equals("postcard"))
+          .map(i -> i.getInStock()
+            .get(0))
+          .findFirst()
+          .orElse(null);
+
+        assertEquals(lastInStock, stock);
+    }
+
+}
