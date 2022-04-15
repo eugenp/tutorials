bf1970ce99d2cf9a579df3ee097d6b528be751dc
Sameer <f0jppwy40@mozmail.com>
2022-04-15 15:24:04 +0530

    Article/bael 5446 (#2)
    * updated demo properties
    
    * formatting
    
    Co-authored-by: s9m33r <no-reply>

--- spring-boot-modules/spring-boot-properties-migrator-demo/src/main/java/com/baeldung/spring/boot/properties/migrator/Main.java
+++ spring-boot-modules/spring-boot-properties-migrator-demo/src/main/java/com/baeldung/spring/boot/properties/migrator/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
-	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
