bf9255bdf8def3889b5efb66c21df2be57b05e78
Daniel Strmecki <daniel.strmecki@gmail.com>
2022-04-17 10:23:22 +0200

    BAEL-5481: Concurrent example

--- core-java-modules/core-java-httpclient/src/main/java/com/baeldung/httpclient/HttpClientPost.java
+++ core-java-modules/core-java-httpclient/src/main/java/com/baeldung/httpclient/HttpClientPost.java
@@ -10,44 +10,56 @@ import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HttpClientPost {
 
     public static HttpResponse<String> sendSynchronousPost(String serviceUrl) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
           .uri(URI.create(serviceUrl))
           .POST(HttpRequest.BodyPublishers.noBody())
           .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        HttpResponse<String> response = client
+          .send(request, HttpResponse.BodyHandlers.ofString());
+
         return response;
     }
 
     public static CompletableFuture<HttpResponse<String>> sendAsynchronousPost(String serviceUrl) {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
           .uri(URI.create(serviceUrl))
           .POST(HttpRequest.BodyPublishers.noBody())
           .build();
-        CompletableFuture<HttpResponse<String>> futureResponse = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        CompletableFuture<HttpResponse<String>> futureResponse = client
+          .sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
         return futureResponse;
     }
 
-    public static CompletableFuture<HttpResponse<String>> sendConcurrentPost(List<String> serviceUrls) {
+    public static CompletableFuture<List<HttpResponse<String>>> sendConcurrentPost(List<String> serviceUrls) {
         HttpClient client = HttpClient.newHttpClient();
 
-        List<HttpRequest> requests = serviceUrls.stream()
+        List<CompletableFuture<HttpResponse<String>>> completableFutures = serviceUrls.stream()
           .map(URI::create)
           .map(HttpRequest::newBuilder)
           .map(HttpRequest.Builder::build)
+          .map(request -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
           .collect(Collectors.toList());
 
-        CompletableFuture.allOf(requests.stream()
-            .map(request -> client.sendAsync(request, HttpResponse.BodyHandlers.ofString()))
-            .toArray(CompletableFuture<?>[]::new))
-          .join();
+        CompletableFuture<List<HttpResponse<String>>> combinedFutures = CompletableFuture
+          .allOf(completableFutures.toArray(new CompletableFuture[0]))
+          .thenApply(future ->
+            completableFutures.stream()
+              .map(CompletableFuture::join)
+              .collect(Collectors.toList()));
 
-        throw new NotImplementedException();
+        return combinedFutures;
     }
 
 }
