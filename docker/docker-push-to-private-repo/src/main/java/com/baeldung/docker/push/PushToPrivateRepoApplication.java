103520d6334ffb1b9c7ed1165c709eed0dbf70b1
Denis Holdeew <hold.denis@gmail.com>
2022-04-18 21:50:00 +0200

    Format

--- docker/docker-push-to-private-repo/src/main/java/com/baeldung/docker/push/PushToPrivateRepoApplication.java
+++ docker/docker-push-to-private-repo/src/main/java/com/baeldung/docker/push/PushToPrivateRepoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PushToPrivateRepoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PushToPrivateRepoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PushToPrivateRepoApplication.class, args);
+    }
 
 }
