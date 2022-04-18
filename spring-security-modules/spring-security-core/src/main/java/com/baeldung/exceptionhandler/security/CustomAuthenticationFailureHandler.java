1a49398d795d193994aeaaf598b59f1b8f1e1844
Pablo <pal19@alu.ua.es>
2022-04-18 22:20:24 +0200

    Exception Handler implemented for Spring Security Resource Server

---
+++ spring-security-modules/spring-security-core/src/main/java/com/baeldung/exceptionhandler/security/CustomAuthenticationFailureHandler.java
@@ -0,0 +1,16 @@
+package com.baeldung.exceptionhandler.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
+        httpServletResponse.sendRedirect("/customError");
+    }
+}
