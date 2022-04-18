8c3f6b85e69cfda468c28bc851f846dfa27eb2cd
Pablo <pal19@alu.ua.es>
2022-04-18 22:27:14 +0200

    Renamed test class name to solve PMD Failure

--- spring-security-modules/spring-security-core/src/test/java/com/baeldung/exceptionhandler/SecurityConfigUnitTest.java
+++ spring-security-modules/spring-security-core/src/test/java/com/baeldung/exceptionhandler/SecurityConfigUnitTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(SecurityConfig.class)
-class SecurityConfigTest {
+class SecurityConfigUnitTest {
     @Autowired
     private MockMvc mvc;
 
