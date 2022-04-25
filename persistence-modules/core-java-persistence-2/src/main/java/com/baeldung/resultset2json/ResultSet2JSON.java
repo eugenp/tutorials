6f18493186e1a9a53d46199b7c71a318c8744cf5
Alireza Ghasemi <alireza.ghasemi1@swisscom.com>
2022-04-25 22:02:12 +0200

    Apply formatting from Eclipse

--- persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/resultset2json/ResultSet2JSON.java
+++ persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/resultset2json/ResultSet2JSON.java
@@ -19,119 +19,119 @@ import org.json.JSONObject;
 
 public class ResultSet2JSON {
 
-  public static void main(String... args) throws ClassNotFoundException, SQLException {
+    public static void main(String... args) throws ClassNotFoundException, SQLException {
 
-    ResultSet2JSON testClass = new ResultSet2JSON();
-    testClass.testMethod1();
-  }
-
-  public void testMethod1() throws ClassNotFoundException, SQLException {
-    Class.forName("org.h2.Driver");
-    Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:rs2jdbc", "user", "password");
-
-    // Create a table
-    Statement stmt = dbConnection.createStatement();
-    stmt.execute("CREATE TABLE words AS SELECT * FROM CSVREAD('./example.csv')");
-    ResultSet resultSet = stmt.executeQuery("SELECT * FROM words");
-
-    JSONArray result1 = resultSet2JdbcMethod1(resultSet);
-    System.out.println(result1);
-
-    resultSet.close();
-  }
-
-  public void testMethod2() throws ClassNotFoundException, SQLException {
-    Class.forName("org.h2.Driver");
-    Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:rs2jdbc", "user", "password");
-    // Create a table
-    Statement stmt = dbConnection.createStatement();
-    stmt.execute("CREATE TABLE words AS SELECT * FROM CSVREAD('./example.csv')");
-    ResultSet resultSet = stmt.executeQuery("SELECT * FROM words");
-
-    JSONObject result1 = resultSet2JdbcMethod2(resultSet, dbConnection);
-    System.out.println(result1);
-
-    resultSet.close();
-  }
-
-  public void testMethod3() throws ClassNotFoundException, SQLException {
-    Class.forName("org.h2.Driver");
-    Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:rs2jdbc", "user", "password");
-    // Create a table
-    Statement stmt = dbConnection.createStatement();
-    stmt.execute("CREATE TABLE words AS SELECT * FROM CSVREAD('./example.csv')");
-    ResultSet resultSet = stmt.executeQuery("SELECT * FROM words");
-
-    JSONArray result1 = resultSet2JdbcMethod3(resultSet, dbConnection);
-    System.out.println(result1);
-
-    resultSet.close();
-  }
-
-  private static JSONArray resultSet2JdbcMethod1(ResultSet resultSet) throws SQLException {
-    ResultSetMetaData md = resultSet.getMetaData();
-    int numCols = md.getColumnCount();
-    List<String> colNames = IntStream.range(0, numCols)
-      .mapToObj(i -> {
-        try {
-          return md.getColumnName(i + 1);
-        } catch (SQLException e) {
-
-          e.printStackTrace();
-          return "?";
-        }
-      })
-      .collect(Collectors.toList());
-
-    JSONArray result = new JSONArray();
-    while (resultSet.next()) {
-      JSONObject row = new JSONObject();
-      colNames.forEach(cn -> {
-        try {
-          row.put(cn, resultSet.getObject(cn));
-        } catch (JSONException | SQLException e) {
-
-          e.printStackTrace();
-        }
-      });
-      result.add(row);
+        ResultSet2JSON testClass = new ResultSet2JSON();
+        testClass.testMethod1();
     }
-    return result;
-  }
 
-  private static JSONObject resultSet2JdbcMethod2(ResultSet resultSet, Connection dbConnection) throws SQLException {
-    JSONObject result = new JSONObject(DSL.using(dbConnection)
-      .fetch(resultSet)
-      .formatJSON());
-    return result;
-  }
+    public void testMethod1() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:rs2jdbc", "user", "password");
 
-  private static JSONArray resultSet2JdbcMethod3(ResultSet resultSet, Connection dbConnection) throws SQLException {
-    ResultSetMetaData md = resultSet.getMetaData();
-    int numCols = md.getColumnCount();
-    List<String> colNames = IntStream.range(0, numCols)
-      .mapToObj(i -> {
-        try {
-          return md.getColumnName(i + 1);
-        } catch (SQLException e) {
+        // Create a table
+        Statement stmt = dbConnection.createStatement();
+        stmt.execute("CREATE TABLE words AS SELECT * FROM CSVREAD('./example.csv')");
+        ResultSet resultSet = stmt.executeQuery("SELECT * FROM words");
 
-          e.printStackTrace();
-          return "?";
+        JSONArray result1 = resultSet2JdbcMethod1(resultSet);
+        System.out.println(result1);
+
+        resultSet.close();
+    }
+
+    public void testMethod2() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:rs2jdbc", "user", "password");
+        // Create a table
+        Statement stmt = dbConnection.createStatement();
+        stmt.execute("CREATE TABLE words AS SELECT * FROM CSVREAD('./example.csv')");
+        ResultSet resultSet = stmt.executeQuery("SELECT * FROM words");
+
+        JSONObject result1 = resultSet2JdbcMethod2(resultSet, dbConnection);
+        System.out.println(result1);
+
+        resultSet.close();
+    }
+
+    public void testMethod3() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:rs2jdbc", "user", "password");
+        // Create a table
+        Statement stmt = dbConnection.createStatement();
+        stmt.execute("CREATE TABLE words AS SELECT * FROM CSVREAD('./example.csv')");
+        ResultSet resultSet = stmt.executeQuery("SELECT * FROM words");
+
+        JSONArray result1 = resultSet2JdbcMethod3(resultSet, dbConnection);
+        System.out.println(result1);
+
+        resultSet.close();
+    }
+
+    private static JSONArray resultSet2JdbcMethod1(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData md = resultSet.getMetaData();
+        int numCols = md.getColumnCount();
+        List<String> colNames = IntStream.range(0, numCols)
+            .mapToObj(i -> {
+                try {
+                    return md.getColumnName(i + 1);
+                } catch (SQLException e) {
+
+                    e.printStackTrace();
+                    return "?";
+                }
+            })
+            .collect(Collectors.toList());
+
+        JSONArray result = new JSONArray();
+        while (resultSet.next()) {
+            JSONObject row = new JSONObject();
+            colNames.forEach(cn -> {
+                try {
+                    row.put(cn, resultSet.getObject(cn));
+                } catch (JSONException | SQLException e) {
+
+                    e.printStackTrace();
+                }
+            });
+            result.add(row);
         }
-      })
-      .collect(Collectors.toList());
+        return result;
+    }
 
-    List<JSONObject> json = DSL.using(dbConnection)
-      .fetch(resultSet)
-      .map(new RecordMapper<Record, JSONObject>() {
+    private static JSONObject resultSet2JdbcMethod2(ResultSet resultSet, Connection dbConnection) throws SQLException {
+        JSONObject result = new JSONObject(DSL.using(dbConnection)
+            .fetch(resultSet)
+            .formatJSON());
+        return result;
+    }
 
-        @Override
-        public JSONObject map(Record r) {
-          JSONObject obj = new JSONObject();
-          colNames.forEach(cn -> obj.put(cn, r.get(cn)));
-          return obj;
-        }
-      });
-    return new JSONArray(json);
-  }
+    private static JSONArray resultSet2JdbcMethod3(ResultSet resultSet, Connection dbConnection) throws SQLException {
+        ResultSetMetaData md = resultSet.getMetaData();
+        int numCols = md.getColumnCount();
+        List<String> colNames = IntStream.range(0, numCols)
+            .mapToObj(i -> {
+                try {
+                    return md.getColumnName(i + 1);
+                } catch (SQLException e) {
+
+                    e.printStackTrace();
+                    return "?";
+                }
+            })
+            .collect(Collectors.toList());
+
+        List<JSONObject> json = DSL.using(dbConnection)
+            .fetch(resultSet)
+            .map(new RecordMapper<Record, JSONObject>() {
+
+                @Override
+                public JSONObject map(Record r) {
+                    JSONObject obj = new JSONObject();
+                    colNames.forEach(cn -> obj.put(cn, r.get(cn)));
+                    return obj;
+                }
+            });
+        return new JSONArray(json);
+    }
 }
