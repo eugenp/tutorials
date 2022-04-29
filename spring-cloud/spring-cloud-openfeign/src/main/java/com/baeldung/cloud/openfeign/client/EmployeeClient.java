ec0b628fb40376285315f55be22458a2a68b9feb
Amitabh Tiwari <amitiwarimech@gmail.com>
2022-04-29 18:42:53 +0530

    Initial Changes for RequestLine

---
+++ spring-cloud/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/client/EmployeeClient.java
@@ -0,0 +1,13 @@
+package com.baeldung.cloud.openfeign.client;
+
+import com.baeldung.cloud.openfeign.model.Employee;
+import feign.Headers;
+import feign.Param;
+import feign.RequestLine;
+
+public interface EmployeeClient {
+
+	@RequestLine("GET /empployee/{id}?active={isActive}")
+	@Headers("Content-Type: application/json")
+	Employee getEmployee(@Param long id, @Param boolean isActive);
+}
