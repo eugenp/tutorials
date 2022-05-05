70fc78e5090d05aabae500d9a6d6fd1d227293f7
temecom <csmith@temecom.net>
2022-05-04 21:29:36 -0400

    BAEL-5516 How to Convert InputStream to base64 String  - Junit test demonstrating the code

---
+++ core-java-modules/core-java-io-conversions-2/src/test/java/com/baeldung/inputstreamtobase64/InputStreamToBase64Test.java
@@ -0,0 +1,45 @@
+package com.baeldung.inputstreamtobase64;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.InputStream;
+import java.util.Base64;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+/**
+ * 
+ */
+public class InputStreamToBase64Test {
+    
+    /**
+     * Test stream to base64 conversion
+     * @throws Exception 
+     */
+    @Test
+    public void testWhenAnInputStreamIsConvertedToBase64_thenItCanBeDecoded() throws Exception {
+        InputStream sourceStream  = getClass().getClassLoader().getResourceAsStream("logo.png");
+        byte[] sourceBytes = IOUtils.toByteArray(sourceStream);
+        String encodedString = Base64.getEncoder().encodeToString(sourceBytes); 
+        assertNotNull(encodedString);
+        System.out.println(encodedString); 
+        byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
+        assertNotNull(decodedBytes);
+        assertTrue(decodedBytes.length == sourceBytes.length);
+        assertTrue(calculateChecksum(decodedBytes) == calculateChecksum(sourceBytes)); 
+     }
+
+     /**
+      * Calculate a checksum
+      * @param bytes array of bytes to check
+      * @return the total sum of all bytes
+      */
+     private int calculateChecksum(byte[] bytes) {
+        int checksum = 0; 
+        for(int index=0; index < bytes.length; index++) {
+            checksum+=bytes[index]; 
+        }
+        return checksum; 
+     }
+}
