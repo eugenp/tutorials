1ce88958918aa490c1643bf80cc416b7ade79eef
Mayank Aggarwal <aggmayank12@gmail.com>
2022-05-07 17:52:36 +0530

    Bael 5438 (#12176)
    * [BAEL-5438] Added Criteria Queries for Employee
    
    * [BAEL-5438] Added tests and entities for named queries and criteria queries
    
    * [BAEL-5438] Removed unused sorting files
    
    * [BAEL-5438] Ignored spring context test
    
    * BAEL-5438 Indented with 4 spaces
    
    Co-authored-by: Mayank Agarwal <mayankaggarwal@zeta.tech>

--- persistence-modules/hibernate-queries/src/main/java/com/baeldung/hibernate/criteria/view/EmployeeCriteriaQueries.java
+++ persistence-modules/hibernate-queries/src/main/java/com/baeldung/hibernate/criteria/view/EmployeeCriteriaQueries.java
@@ -11,35 +11,34 @@ import org.hibernate.query.Query;
 
 public class EmployeeCriteriaQueries {
 
-  public List<Employee> getAllEmployees() {
-    final Session session = HibernateUtil.getHibernateSession();
-    final CriteriaBuilder cb = session.getCriteriaBuilder();
-    final CriteriaQuery<Employee> cr = cb.createQuery(Employee.class);
-    final Root<Employee> root = cr.from(Employee.class);
-    cr.select(root);
-    Query<Employee> query = session.createQuery(cr);
-    List<Employee> results = query.getResultList();
-    session.close();
-    return results;
-  }
-
-  // To get items having salary more than 50000
-  public String[] greaterThanCriteria() {
-    final Session session = HibernateUtil.getHibernateSession();
-    final CriteriaBuilder cb = session.getCriteriaBuilder();
-    final CriteriaQuery<Employee> cr = cb.createQuery(Employee.class);
-    final Root<Employee> root = cr.from(Employee.class);
-    cr.select(root)
-      .where(cb.gt(root.get("salary"), 50000));
-    Query<Employee> query = session.createQuery(cr);
-    final List<Employee> greaterThanEmployeeList = query.getResultList();
-    final String employeeWithGreaterSalary[] = new String[greaterThanEmployeeList.size()];
-    for (int i = 0; i < greaterThanEmployeeList.size(); i++) {
-      employeeWithGreaterSalary[i] = greaterThanEmployeeList.get(i)
-        .getName();
+    public List<Employee> getAllEmployees() {
+        final Session session = HibernateUtil.getHibernateSession();
+        final CriteriaBuilder cb = session.getCriteriaBuilder();
+        final CriteriaQuery<Employee> cr = cb.createQuery(Employee.class);
+        final Root<Employee> root = cr.from(Employee.class);
+        cr.select(root);
+        Query<Employee> query = session.createQuery(cr);
+        List<Employee> results = query.getResultList();
+        session.close();
+        return results;
     }
-    session.close();
-    return employeeWithGreaterSalary;
-  }
 
+    // To get items having salary more than 50000
+    public String[] greaterThanCriteria() {
+        final Session session = HibernateUtil.getHibernateSession();
+        final CriteriaBuilder cb = session.getCriteriaBuilder();
+        final CriteriaQuery<Employee> cr = cb.createQuery(Employee.class);
+        final Root<Employee> root = cr.from(Employee.class);
+        cr.select(root)
+            .where(cb.gt(root.get("salary"), 50000));
+        Query<Employee> query = session.createQuery(cr);
+        final List<Employee> greaterThanEmployeeList = query.getResultList();
+        final String employeeWithGreaterSalary[] = new String[greaterThanEmployeeList.size()];
+        for (int i = 0; i < greaterThanEmployeeList.size(); i++) {
+            employeeWithGreaterSalary[i] = greaterThanEmployeeList.get(i)
+                .getName();
+        }
+        session.close();
+        return employeeWithGreaterSalary;
+    }
 }
