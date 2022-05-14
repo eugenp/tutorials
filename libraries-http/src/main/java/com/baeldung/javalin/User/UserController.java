52b7ea691c194ad7b3db0922f59e6dd60477e44a
Mark West <66728126+MWest2020@users.noreply.github.com>
2022-05-14 15:43:18 +0200

    Update UserController.java
    updated for 2022 . Code in tutorial wasn't working

--- libraries-http/src/main/java/com/baeldung/javalin/User/UserController.java
+++ libraries-http/src/main/java/com/baeldung/javalin/User/UserController.java
@@ -1,24 +1,27 @@
 package com.baeldung.javalin.User;
 
-import io.javalin.Handler;
+import io.javalin.http.Handler;
 
 import java.util.Objects;
+import java.util.Optional;
 
 public class UserController {
-    public static Handler fetchAllUsernames = ctx -> {
+   public static Handler fetchAllUsernames = ctx -> {
         UserDao dao = UserDao.instance();
-        Iterable<String> allUsers = dao.getAllUsernames();
+        Iterable<String> allUsers = dao.getAllUserNames();
         ctx.json(allUsers);
     };
 
-    public static Handler fetchById = ctx -> {
-        int id = Integer.parseInt(Objects.requireNonNull(ctx.param("id")));
+    public static Handler fetchById = ctx ->{
+        int id = Integer.parseInt(Objects.requireNonNull(ctx.pathParam("id")));
         UserDao dao = UserDao.instance();
-        User user = dao.getUserById(id).get();
-        if (user == null) {
-            ctx.html("Not Found");
+
+        Optional<User> user = dao.getUserById(id);
+        if(user == null){
+            ctx.html("User Not Found");
         } else {
             ctx.json(user);
         }
     };
-}
+};
+
