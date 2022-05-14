10b8d437cdb80f8c5d97a8b1a66771477ff7f4b5
Mark West <66728126+MWest2020@users.noreply.github.com>
2022-05-14 15:52:47 +0200

    Update JavalinApp.java
    most importantly... :id changed to {id}.
    
    exmaple above no post, in the tutorial, pint 6, there's a post.

--- libraries-http/src/main/java/com/baeldung/javalin/JavalinApp.java
+++ libraries-http/src/main/java/com/baeldung/javalin/JavalinApp.java
@@ -3,14 +3,15 @@ package com.baeldung.javalin;
 import com.baeldung.javalin.User.UserController;
 import io.javalin.Javalin;
 
-public class JavalinApp {
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Javalin app = Javalin.create()
-                .port(7000)
-                .start();
-
+                .start(7000);
         app.get("/hello", ctx -> ctx.html("Hello, Javalin!"));
+
         app.get("/users", UserController.fetchAllUsernames);
-        app.get("/users/:id", UserController.fetchById);
+        app.get("/users/{id}", UserController.fetchById);
+        app.post("/", ctx -> {
+            User user = ctx.bodyAsClass(User.class);
+        });
     }
 }
