17f0ac5929606808459c94f4853244d5760f1741
Ulisses Lima <ulisses@dvlcube.com>
2022-05-16 23:23:26 -0300

    removing from the old module

--- persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/boot/collection/name/dao/MusicTrackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.boot.collection.name.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.baeldung.boot.collection.name.data.MusicTrack;
-
-public interface MusicTrackRepository extends MongoRepository<MusicTrack, String> {
-
-}
--- persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/boot/collection/name/dao/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.boot.collection.name.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.baeldung.boot.collection.name.data.Store;
-
-public interface StoreRepository extends MongoRepository<Store, String> {
-
-}
