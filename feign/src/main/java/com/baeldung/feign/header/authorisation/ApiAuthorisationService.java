722a890c7aa38d09771df7c1ab6f2fbe1c929255
saikatcse03 <saikatcse03@gmail.com>
2022-05-18 19:14:24 +0530

    @Override included

--- feign/src/main/java/com/baeldung/feign/header/authorisation/ApiAuthorisationService.java
+++ feign/src/main/java/com/baeldung/feign/header/authorisation/ApiAuthorisationService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 public class ApiAuthorisationService implements AuthorisationService {
 
+    @Override
     public String getAuthToken() {
         return "Bearer " + UUID.randomUUID();
     }
