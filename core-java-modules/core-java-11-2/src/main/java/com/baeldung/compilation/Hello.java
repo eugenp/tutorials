5eb9bbdd7a9d2a6e3e670653d7a47d5a53c2d2f3
thibault.faure <thibault.faure@mimacom.com>
2022-05-27 22:02:08 +0200

    BAEL-5426 Code for the Compile Java Programs Using the java Command in Java 11 article

---
+++ core-java-modules/core-java-11-2/src/main/java/com/baeldung/compilation/Hello.java
@@ -0,0 +1,12 @@
+package com.baeldung.compilation;
+
+// NB: if you rename this file WrongName.java instead, without changing the public class name
+// You can still run it through java command
+// But won't be able to compile it with javac
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
---
+++ core-java-modules/core-java-11-2/src/main/java/com/baeldung/compilation/Multiplication.java
@@ -0,0 +1,9 @@
+package com.baeldung.compilation;
+
+public class Multiplication {
+
+    public static void main(String args[]) {
+        System.out.println(Integer.valueOf(args[0]) * Integer.valueOf(args[1]));
+    }
+
+}
