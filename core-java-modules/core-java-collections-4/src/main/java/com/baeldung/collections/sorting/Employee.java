e6c268ce9260af284ec3872d61bebdf4259d35e9
Mayank Aggarwal <aggmayank12@gmail.com>
2022-05-27 20:37:59 +0530

    BAEL-5558 (#12222)
    * [BAEL-5438] Added Criteria Queries for Employee
    
    * [BAEL-5558] Sorting By Date in Java
    
    * BAEL-5558: Added and refactored tests name
    
    * [BAEL-5558] Refactored the code.
    
    * [BAEL-5558] Resolved PMD violation.
    
    * [BAEL-5558] Indented with 4 spaces
    
    * Refactored the Employee class
    
    Co-authored-by: Mayank Agarwal <mayankaggarwal@zeta.tech>

--- core-java-modules/core-java-collections-4/src/main/java/com/baeldung/collections/sorting/Employee.java
+++ core-java-modules/core-java-collections-4/src/main/java/com/baeldung/collections/sorting/Employee.java
@@ -4,69 +4,48 @@ import java.util.Date;
 
 public class Employee implements Comparable<Employee>{
 
-  private String name;
-  private int age;
-  private double salary;
-  private Date joiningDate;
+    private String name;
+    private Date joiningDate;
 
-  public Employee(String name, int age, double salary, Date joiningDate) {
-    this.name = name;
-    this.age = age;
-    this.salary = salary;
-    this.joiningDate = joiningDate;
-  }
+    public Employee(String name, Date joiningDate) {
+        this.name = name;
+        this.joiningDate = joiningDate;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
-  public int getAge() {
-    return age;
-  }
+    public Date getJoiningDate() {
+        return joiningDate;
+    }
 
-  public void setAge(int age) {
-    this.age = age;
-  }
+    public void setJoiningDate(Date joiningDate) {
+        this.joiningDate = joiningDate;
+    }
 
-  public double getSalary() {
-    return salary;
-  }
+    @Override
+    public boolean equals(Object obj) {
+        return ((Employee) obj).getName()
+            .equals(getName());
+    }
 
-  public void setSalary(double salary) {
-    this.salary = salary;
-  }
+    @Override
+    public String toString() {
+        return new StringBuffer().append("(")
+            .append(getName())
+            .append(",")
+            .append(getJoiningDate())
+            .append(")")
+            .toString();
+    }
 
-  public Date getJoiningDate() {
-    return joiningDate;
-  }
-
-  public void setJoiningDate(Date joiningDate) {
-    this.joiningDate = joiningDate;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    return ((Employee) obj).getName()
-      .equals(getName());
-  }
-
-  @Override
-  public String toString() {
-    return new StringBuffer().append("(")
-      .append(getName()).append(",")
-      .append(getAge())
-      .append(",")
-      .append(getSalary()).append(",").append(getJoiningDate())
-      .append(")")
-      .toString();
-  }
-
-  @Override
-  public int compareTo(Employee employee) {
-    return getJoiningDate().compareTo(employee.getJoiningDate());
-  }
+    @Override
+    public int compareTo(Employee employee) {
+        return getJoiningDate().compareTo(employee.getJoiningDate());
+    }
 }
