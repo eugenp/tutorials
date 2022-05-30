b14e8332f821e64121e17904b0c7706f0c37ce92
sampadawagde <sampada.aws@gmail.com>
2022-05-30 18:21:38 +0530

    JAVA-8149: Update Introduction to the Functional Web Framework in Spring 5

--- spring-reactive/src/main/java/com/baeldung/reactive/webflux/functional/EmployeeFunctionalConfig.java
+++ spring-reactive/src/main/java/com/baeldung/reactive/webflux/functional/EmployeeFunctionalConfig.java
@@ -25,49 +25,39 @@ public class EmployeeFunctionalConfig {
 
     @Bean
     RouterFunction<ServerResponse> getAllEmployeesRoute() {
-        return route(GET("/employees"),
-          req -> ok().body(
-            employeeRepository().findAllEmployees(), Employee.class));
+        return route(GET("/employees"), req -> ok().body(employeeRepository().findAllEmployees(), Employee.class));
     }
 
     @Bean
     RouterFunction<ServerResponse> getEmployeeByIdRoute() {
-        return route(GET("/employees/{id}"),
-          req -> ok().body(
-            employeeRepository().findEmployeeById(req.pathVariable("id")), Employee.class));
+        return route(GET("/employees/{id}"), req -> ok().body(employeeRepository().findEmployeeById(req.pathVariable("id")), Employee.class));
     }
 
     @Bean
     RouterFunction<ServerResponse> updateEmployeeRoute() {
-        return route(POST("/employees/update"),
-          req -> req.body(toMono(Employee.class))
+        return route(POST("/employees/update"), req -> req.body(toMono(Employee.class))
             .doOnNext(employeeRepository()::updateEmployee)
             .then(ok().build()));
     }
 
     @Bean
     RouterFunction<ServerResponse> composedRoutes() {
-        return
-          route(GET("/employees"),
-            req -> ok().body(
-              employeeRepository().findAllEmployees(), Employee.class))
+        return route(GET("/employees"), req -> ok().body(employeeRepository().findAllEmployees(), Employee.class))
 
-            .and(route(GET("/employees/{id}"),
-              req -> ok().body(
-                employeeRepository().findEmployeeById(req.pathVariable("id")), Employee.class)))
+            .and(route(GET("/employees/{id}"), req -> ok().body(employeeRepository().findEmployeeById(req.pathVariable("id")), Employee.class)))
 
-            .and(route(POST("/employees/update"),
-              req -> req.body(toMono(Employee.class))
+            .and(route(POST("/employees/update"), req -> req.body(toMono(Employee.class))
                 .doOnNext(employeeRepository()::updateEmployee)
                 .then(ok().build())));
     }
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http
-          .csrf().disable()
-          .authorizeExchange()
-            .anyExchange().permitAll();
+        http.csrf()
+            .disable()
+            .authorizeExchange()
+            .anyExchange()
+            .permitAll();
         return http.build();
     }
 }
