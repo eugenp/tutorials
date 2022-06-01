921f2928df65a2f7be47c7da543fd7f65e1eeb53
phuongthunee <thulp.bi11-258@st.usth.edu.vn>
2022-06-01 21:54:33 +0700

    Create new test

---
+++ Test/test.java
@@ -0,0 +1,19 @@
+package com.baeldung.pow;
+
+import java.text.DecimalFormat;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPowerExample {
+
+    @Test
+    public static void main(String[] args) {
+
+        assertEquals(8, PowerExample.intResult);
+
+        assertEquals(74, PowerExample.dblResult);
+
+        assertEquals(74.09, PowerExample.df);
+
+    }
+}
