bc25fd686dbf911b1e20a9b814ec85e60582b0e5
phuongthunee <thulp.bi11-258@st.usth.edu.vn>
2022-06-02 15:05:55 +0700

    Update test case

--- java-numbers-2/src/main/java/com/baeldung/pow/TestPowerExample.java
+++ java-numbers-2/src/main/java/com/baeldung/pow/TestPowerExample.java
@@ -8,11 +8,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class TestPowerExample {
 
     @Test
-    public static void main(String[] args) {
+    public static void testPowerExample1(String[] args) {
 
         assertEquals(8, PowerExample.intResult);
 
+    }
+    @Test
+    public static void testPowerExample2(String[] args) {
+
         assertEquals(74, PowerExample.dblResult);
+
+    }
+    @Test
+    public static void testPowerExample3(String[] args) {
 
         assertEquals(74.09, PowerExample.df);
 
