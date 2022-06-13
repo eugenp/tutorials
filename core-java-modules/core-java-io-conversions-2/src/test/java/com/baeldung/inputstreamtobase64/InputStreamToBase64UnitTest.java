09b5656dee36098509e38b17201f7edda8d306a3
temecom <csmith@temecom.net>
2022-06-13 10:20:42 -0400

    BAEL-5516 - How-to-Convert-InputStream-to-base64-String  - rename and formatting to match Baeldung standards

--- core-java-modules/core-java-io-conversions-2/src/test/java/com/baeldung/inputstreamtobase64/InputStreamToBase64UnitTest.java
+++ core-java-modules/core-java-io-conversions-2/src/test/java/com/baeldung/inputstreamtobase64/InputStreamToBase64UnitTest.java
@@ -9,20 +9,25 @@ import java.util.Base64;
 import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 /**
- * 
+ * Test the Stream to base64 conversion
  */
-public class InputStreamToBase64Test {
+public class InputStreamToBase64UnitTest {
     
     /**
      * Test stream to base64 conversion
      * @throws Exception 
      */
     @Test
-    public void testWhenAnInputStreamIsConvertedToBase64_thenItCanBeDecoded() throws Exception {
+    public void givenABinaryInputStream_whenItIsConvertedToBase64_thenItCanBeDecoded() throws Exception {
+        // given a binary input stream
         InputStream sourceStream  = getClass().getClassLoader().getResourceAsStream("logo.png");
         byte[] sourceBytes = IOUtils.toByteArray(sourceStream);
+
+        // when it is converted to base64
         String encodedString = Base64.getEncoder().encodeToString(sourceBytes); 
         assertNotNull(encodedString);
+
+        // then it can be decoded
         byte[] decodedBytes = Base64.getDecoder().decode(encodedString);
         assertNotNull(decodedBytes);
         assertTrue(decodedBytes.length == sourceBytes.length);
