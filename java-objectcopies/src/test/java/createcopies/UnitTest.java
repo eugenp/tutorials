a5d37bb86d24aa9bd35d941465a0efcb117dce48
jhart3000 <j.hart4000@gmail.com>
2022-06-15 08:46:38 +0100

    edited test names

--- java-objectcopies/src/test/java/createcopies/UnitTest.java
+++ java-objectcopies/src/test/java/createcopies/UnitTest.java
@@ -9,13 +9,13 @@ import computermodel.ComputerShallow;
 class UnitTest {
 
     @Test
-    void givenAComputerObject_whenCreateDeepCopyMethodIsCalled_thenDeepCopyShouldBeCreated() {
+    void whenCreateDeepCopyMethodIsCalled_thenDeepCopyShouldBeCreated() {
         ComputerDeep newComputer = CreateCopies.createDeepCopy();
         Assertions.assertEquals("Intel", newComputer.getCpu().getBrandName());
     }
 
     @Test
-    void givenAComputerObject_whenCreateShallowCopyMethodIsCalled_thenShallowCopyShouldBeCreated() {
+    void whenCreateShallowCopyMethodIsCalled_thenShallowCopyShouldBeCreated() {
         ComputerShallow newComputer = CreateCopies.createShallowCopy();
         Assertions.assertEquals("AMD", newComputer.getCpu().getBrandName());
     }
