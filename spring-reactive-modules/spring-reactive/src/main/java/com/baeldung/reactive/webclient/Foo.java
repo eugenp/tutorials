fa84c3c64f2f3ca2a90047641db199d5bf328871
panagiotiskakos <panagiotis.kakos@libra-is.com>
2022-06-18 17:30:16 +0300

    moved spring-reactive (submodule) to spring-reactive-modules (parent)

---
+++ spring-reactive-modules/spring-reactive/src/main/java/com/baeldung/reactive/webclient/Foo.java
@@ -0,0 +1,14 @@
+package com.baeldung.reactive.webclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Foo {
+
+    private String name;
+
+}
---
+++ spring-reactive-modules/spring-reactive/src/main/java/com/baeldung/reactive/webflux/annotation/EmployeeWebClient.java
@@ -0,0 +1,32 @@
+package com.baeldung.reactive.webflux.annotation;
+
+import com.baeldung.reactive.webflux.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class EmployeeWebClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeWebClient.class);
+
+    WebClient client = WebClient.create("http://localhost:8080");
+
+    public void consume() {
+
+        Mono<Employee> employeeMono = client.get()
+          .uri("/employees/{id}", "1")
+          .retrieve()
+          .bodyToMono(Employee.class);
+
+        employeeMono.subscribe(employee -> LOGGER.info("Employee: {}", employee));
+
+        Flux<Employee> employeeFlux = client.get()
+          .uri("/employees")
+          .retrieve()
+          .bodyToFlux(Employee.class);
+
+        employeeFlux.subscribe(employee -> LOGGER.info("Employee: {}", employee));
+    }
+}
