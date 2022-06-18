6f91bccac00b32fb8a789a2a23c0c0f75a22ec3e
panagiotiskakos <panagiotis.kakos@libra-is.com>
2022-06-18 16:44:40 +0300

    moved spring-5-reactive (submodule) to spring-reactive-modules (parent)

---
+++ spring-reactive-modules/spring-5-reactive/src/main/java/com/baeldung/websession/transfer/CustomResponse.java
@@ -0,0 +1,31 @@
+package com.baeldung.websession.transfer;
+
+public class CustomResponse {
+
+    private int id;
+    private String note;
+
+    public CustomResponse() {}
+
+    public CustomResponse(int id, String note) {
+        this.id = id;
+        this.note = note;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+}
