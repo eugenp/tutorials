3ae97db187f253b5773d120b89601e34b57e9630
Ulisses Lima <ulisses@dvlcube.com>
2022-06-19 16:35:36 -0300

    removing Customer2

--- persistence-modules/spring-boot-persistence-mongodb-2/src/main/java/com/baeldung/boot/unique/field/dao/Customer2Repository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.boot.unique.field.dao;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.baeldung.boot.unique.field.data.Customer2;
-
-public interface Customer2Repository extends MongoRepository<Customer2, Long> {
-}
