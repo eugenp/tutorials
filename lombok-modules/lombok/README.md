cb0c526d737d54ccc6d3bb3f035fbe04ccd0ba85
panagiotiskakos <panagiotis.kakos@libra-is.com>
2022-06-19 18:01:59 +0300

    added lombok submodule to parent lombok-modules

---
+++ lombok-modules/lombok/README.md
@@ -0,0 +1,17 @@
+## Project Lombok
+
+This module contains articles about Project Lombok.
+
+### Relevant Articles:
+
+- [Introduction to Project Lombok](https://www.baeldung.com/intro-to-project-lombok)
+- [Using Lombok’s @Builder Annotation](https://www.baeldung.com/lombok-builder)
+- [Using Lombok’s @Getter for Boolean Fields](https://www.baeldung.com/lombok-getter-boolean)
+- [Lombok @Builder with Inheritance](https://www.baeldung.com/lombok-builder-inheritance)
+- [Lombok Builder with Default Value](https://www.baeldung.com/lombok-builder-default-value)
+- [Lombok Builder with Custom Setter](https://www.baeldung.com/lombok-builder-custom-setter)
+- [Setting up Lombok with Eclipse and Intellij](https://www.baeldung.com/lombok-ide)
+- [Using the @Singular Annotation with Lombok Builders](https://www.baeldung.com/lombok-builder-singular)
+- [Omitting Getter or Setter in Lombok](https://www.baeldung.com/lombok-omit-getter-setter)
+- [Lombok Configuration System](https://www.baeldung.com/lombok-configuration-system)
+- More articles: [[next -->]](../lombok-2)
---
+++ lombok-modules/lombok/src/test/java/com/baeldung/lombok/builder/BuilderUnitTest.java
@@ -0,0 +1,43 @@
+package com.baeldung.lombok.builder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BuilderUnitTest {
+
+    @Test
+    public void givenBuilder_WidgetIsBuilt() {
+        Widget testWidget = Widget.builder()
+            .name("foo")
+            .id(1)
+            .build();
+        assertThat(testWidget.getName()).isEqualTo("foo");
+        assertThat(testWidget.getId()).isEqualTo(1);
+    }
+
+    @Test
+    public void givenToBuilder_whenToBuilder_BuilderIsCreated() {
+
+        Widget testWidget = Widget.builder()
+            .name("foo")
+            .id(1)
+            .build();
+        Widget.WidgetBuilder widgetBuilder = testWidget.toBuilder();
+
+        Widget newWidget = widgetBuilder.id(2)
+            .build();
+        assertThat(newWidget.getName()).isEqualTo("foo");
+        assertThat(newWidget.getId()).isEqualTo(2);
+    }
+
+    @Test
+    public void givenBuilderMethod_ClientIsBuilt() {
+        ImmutableClient testImmutableClient = ClientBuilder.builder()
+            .name("foo")
+            .id(1)
+            .build();
+        assertThat(testImmutableClient.getName()).isEqualTo("foo");
+        assertThat(testImmutableClient.getId()).isEqualTo(1);
+    }
+}
