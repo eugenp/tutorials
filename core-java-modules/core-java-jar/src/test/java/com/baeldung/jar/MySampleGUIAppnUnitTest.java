2cb4fdbbc525a139c8a900c88a57709f32051a92
opokharel <opokharel@gmail.com>
2022-06-25 11:00:26 -0600

    [BAEL-5421] by @opokharel
    [BAEL-5421] by @opokharel

---
+++ core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
@@ -0,0 +1,14 @@
+package com.baeldung.jar;
+
+import org.junit.jupiter.api.Test;
+
+class MySampleGUIAppnUnitTest extends MySampleGUIAppn {
+
+    @Test
+    void testMain() {
+        MySampleGUIAppn instance = new MySampleGUIAppn();
+        String [] args = null;
+        System.exit(DO_NOTHING_ON_CLOSE);
+        main(args);
+    }
+}
