ded7b0af8f215075e586a128a13caafba2aeb015
opokharel <opokharel@gmail.com>
2022-06-25 11:35:39 -0600

    BAEL-5421

--- core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
+++ core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
@@ -1,14 +1,16 @@
 package com.baeldung.jar;
 
+import java.io.IOException;
+
 import org.junit.jupiter.api.Test;
 
-class MySampleGUIAppnUnitTest throws IOException {
+class MySampleGUIAppnUnitTest {
 
     @Test
     void testMain() throws IOException {
         MySampleGUIAppn instance = new MySampleGUIAppn();
         String [] args = null;
-        System.exit(DO_NOTHING_ON_CLOSE);
-        main(args);
+        System.exit(0);
+        MySampleGUIAppn.main(args);
     }
 }
