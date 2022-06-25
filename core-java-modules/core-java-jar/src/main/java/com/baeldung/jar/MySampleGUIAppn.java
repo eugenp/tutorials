1b29276c17a2810a2bcd8d4b49f09f916531a878
opokharel <opokharel@gmail.com>
2022-06-25 11:00:26 -0600

    [BAEL-5421] by @opokharel
    [BAEL-5421] by @opokharel

---
+++ core-java-modules/core-java-jar/src/main/java/com/baeldung/jar/MySampleGUIAppn.java
@@ -0,0 +1,26 @@
+package com.baeldung.jar;
+
+import java.awt.event.*;
+import java.awt.*;
+
+import javax.swing.*;
+
+public class MySampleGUIAppn extends JFrame{
+    public  MySampleGUIAppn() {
+        setSize(300,300);
+        setTitle("MySampleGUIAppn");
+        Button b = new Button("Click Me!");
+        b.setBounds(30,100,80,30);
+        add(b);
+        setVisible(true);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                dispose();
+                System.exit(0);
+            }
+        });
+    }
+    public static void main(String[] args) {
+        MySampleGUIAppn app=new  MySampleGUIAppn();
+    }
+}
