7249030acd6a7a2e7c7fad4b115f888512dd8a1e
opokharel <opokharel@gmail.com>
2022-06-25 11:46:51 -0600

    BAEL-5421

--- core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
+++ core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
@@ -8,7 +8,7 @@ class MySampleGUIAppnUnitTest {
 
     @Test
     void testMain() throws IOException {
-        System.setProperty("java.awt.headless", "false");
+        System.setProperty("java.awt.headless", "true");
         System.out.println(java.awt.GraphicsEnvironment.isHeadless());
         MySampleGUIAppn instance = new MySampleGUIAppn();
         String [] args = null;
