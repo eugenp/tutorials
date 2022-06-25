c2bfe407de76d8f76208fd28435790305ca7c312
opokharel <opokharel@gmail.com>
2022-06-25 11:09:55 -0600

    BAEL-5421

--- core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
+++ core-java-modules/core-java-jar/src/test/java/com/baeldung/jar/MySampleGUIAppnUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 class MySampleGUIAppnUnitTest extends MySampleGUIAppn {
 
     @Test
-    void testMain() {
+    void testMain() throws IOException {
         MySampleGUIAppn instance = new MySampleGUIAppn();
         String [] args = null;
         System.exit(DO_NOTHING_ON_CLOSE);
