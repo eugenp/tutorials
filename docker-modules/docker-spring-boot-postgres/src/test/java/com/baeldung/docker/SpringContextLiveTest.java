f46636529b1cc8e85248f26c86f18c7f9a104052
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2022-07-01 17:50:57 +0300

    set java 8 version

--- docker-modules/docker-spring-boot-postgres/src/test/java/com/baeldung/docker/SpringContextLiveTest.java
+++ docker-modules/docker-spring-boot-postgres/src/test/java/com/baeldung/docker/SpringContextLiveTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringContextLiveTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
