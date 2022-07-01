f02a63fc0aa44a62da4c998c30ab7afed83a3aa2
Abdul Wahab <abdul.wahab@monese.com>
2022-07-01 11:24:00 +0300

    BAEL-5616: Added example code for java wildcard imports

---
+++ patterns/wildcard-imports/src/main/java/com/baeldung/imports/Main.java
@@ -0,0 +1,7 @@
+package com.baeldung.imports;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ patterns/wildcard-imports/src/main/java/com/baeldung/imports/examples/specific/Book.java
@@ -0,0 +1,17 @@
+package com.baeldung.imports.examples.specific;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+public class Book {
+
+    private UUID id;
+
+    private String name;
+
+    private Date datePublished;
+
+    private List<String> authors;
+
+}
