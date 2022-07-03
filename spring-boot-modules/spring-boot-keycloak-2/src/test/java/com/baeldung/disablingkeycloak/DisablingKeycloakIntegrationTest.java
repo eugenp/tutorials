0fdd115becc7f13899ad4c7be49f6bfa82e755c1
Christian GERMAN <christian.german@pm.me>
2022-07-03 18:09:53 +0200

    BAEL-5252-new-spring-boot-keycloak-2-module

--- spring-boot-modules/spring-boot-keycloak-2/src/test/java/com/baeldung/disablingkeycloak/DisablingKeycloakIntegrationTest.java
+++ spring-boot-modules/spring-boot-keycloak-2/src/test/java/com/baeldung/disablingkeycloak/DisablingKeycloakIntegrationTest.java
@@ -1,28 +1,25 @@
 package com.baeldung.disablingkeycloak;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.apache.http.HttpStatus;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest(classes = App.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@RunWith(SpringRunner.class)
 @ActiveProfiles("disablingkeycloak")
-public class DisablingKeycloakIntegrationTest {
+class DisablingKeycloakIntegrationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void givenUnauthenticated_whenGettingUser_shouldReturnUser() {
+    void givenUnauthenticated_whenGettingUser_shouldReturnUser() {
         ResponseEntity<User> responseEntity = restTemplate.getForEntity("/users/1", User.class);
 
         assertEquals(HttpStatus.SC_OK, responseEntity.getStatusCodeValue());
