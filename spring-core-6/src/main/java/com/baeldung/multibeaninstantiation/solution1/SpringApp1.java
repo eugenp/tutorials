2fe4ccbead7e7c5c20099115c912d10343e40b19
Sumit Kumar <mail.sumitkumar30@gmail.com>
2022-07-08 09:14:24 +0530

    BAEL-5449 Instantiating Multiple Beans of the Same Class with Spring Annotations (#12446)
    * added multiple bean instantiation
    
    * added module /spring-core-6 and formatting changes
    
    * resolved formatting issues as suggested
    
    * resolved formatting issues and changed package names
    
    * formatting changes in XML files
    
    * Reformatted XML files as per the instructions
    
    * FIXED Example in Section 3 and some changes in Pom.xml
    
    * changed xml config to Java config in solution 3
    
    * Project updated to spring boot application
    
    * Removed unwanted spring dependencies from pom.xml
    
    * moved the project under new module spring-core-6
    
    * added spring-core-6 module in parent's pom.xml
    
    * resolved formatting issue in parent's pom.xml
    
    * new module entry in parent's pom
    
    * final commit
    
    * Added spring-core-6 in parent's pom.xml 
    
    * fixed some merging issues.
    
    * added spring-core-6 module entry under jdk9-and-above profile in main pom
    
    * removed App.java from multibeaninstantiation directory

---
+++ spring-core-6/src/main/java/com/baeldung/multibeaninstantiation/solution1/SpringApp1.java
@@ -0,0 +1,11 @@
+package com.baeldung.multibeaninstantiation.solution1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApp1 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApp1.class, args);
+    }
+}
