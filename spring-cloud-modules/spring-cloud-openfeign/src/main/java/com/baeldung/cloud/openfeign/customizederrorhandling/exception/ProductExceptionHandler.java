794f56b377be5eca1ef2e3b248782f03ba95dc8c
Saikat Chakraborty <40471715+saikatcse03@users.noreply.github.com>
2022-07-11 09:05:27 +0530

    [Saikat]| Removed unused import

--- spring-cloud-modules/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/customizederrorhandling/exception/ProductExceptionHandler.java
+++ spring-cloud-modules/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/customizederrorhandling/exception/ProductExceptionHandler.java
@@ -6,10 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ProductExceptionHandler extends ResponseEntityExceptionHandler {
+public class ProductExceptionHandler {
 
     @ExceptionHandler({ProductServiceNotAvailableException.class})
     public ResponseEntity<ErrorResponse> handleProductServiceNotAvailableException(ProductServiceNotAvailableException exception, WebRequest request) {
@@ -38,4 +37,3 @@ public class ProductExceptionHandler extends ResponseEntityExceptionHandler {
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
-
