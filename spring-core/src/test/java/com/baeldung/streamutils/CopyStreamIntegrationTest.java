4b9a4f88c1574041785c87d872e71a51cde411bc
Alex Golub <a12thu@gmail.com>
2022-07-11 21:54:46 +0300

    Keep consistency using Files and Paths to get Input/Output Streams and files respectively

--- spring-core/src/test/java/com/baeldung/streamutils/CopyStreamIntegrationTest.java
+++ spring-core/src/test/java/com/baeldung/streamutils/CopyStreamIntegrationTest.java
@@ -1,52 +1,51 @@
 package com.baeldung.streamutils;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.springframework.util.StreamUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
-
-import static org.junit.Assert.assertEquals;
-import org.junit.Test;
-import org.springframework.util.StreamUtils;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import static com.baeldung.streamutils.CopyStream.getStringFromInputStream;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CopyStreamIntegrationTest {
 
     @Test
     public void whenCopyInputStreamToOutputStream_thenCorrect() throws IOException {
-        String inputFileName = "src/test/resources/input.txt";
-        String outputFileName = "src/test/resources/output.txt";
-        File outputFile = new File(outputFileName);
-        InputStream in = new FileInputStream(inputFileName);
-        OutputStream out = new FileOutputStream(outputFileName);
+        Path inputFile = Paths.get("src/test/resources/input.txt");
+        Path outputFile = Paths.get("src/test/resources/output.txt");
+        InputStream in = Files.newInputStream(inputFile);
+        OutputStream out = Files.newOutputStream(outputFile);
 
         StreamUtils.copy(in, out);
 
-        assertTrue(outputFile.exists());
-        String inputFileContent = getStringFromInputStream(new FileInputStream(inputFileName));
-        String outputFileContent = getStringFromInputStream(new FileInputStream(outputFileName));
+        assertTrue(Files.exists(outputFile));
+
+        String inputFileContent = getStringFromInputStream(Files.newInputStream(inputFile));
+        String outputFileContent = getStringFromInputStream(Files.newInputStream(outputFile));
         assertEquals(inputFileContent, outputFileContent);
     }
 
     @Test
     public void whenCopyRangeOfInputStreamToOutputStream_thenCorrect() throws IOException {
-        String inputFileName = "src/test/resources/input.txt";
-        String outputFileName = "src/test/resources/output.txt";
-        File outputFile = new File(outputFileName);
-        InputStream in = new FileInputStream(inputFileName);
-        OutputStream out = new FileOutputStream(outputFileName);
+        Path inputFile = Paths.get("src/test/resources/input.txt");
+        Path outputFile = Paths.get("src/test/resources/output.txt");
+        InputStream in = Files.newInputStream(inputFile);
+        OutputStream out = Files.newOutputStream(outputFile);
 
         StreamUtils.copyRange(in, out, 1, 10);
 
-        assertTrue(outputFile.exists());
-        String inputFileContent = getStringFromInputStream(new FileInputStream(inputFileName));
-        String outputFileContent = getStringFromInputStream(new FileInputStream(outputFileName));
+        assertTrue(Files.exists(outputFile));
+
+        String inputFileContent = getStringFromInputStream(Files.newInputStream(inputFile));
+        String outputFileContent = getStringFromInputStream(Files.newInputStream(outputFile));
         assertEquals(inputFileContent.substring(1, 11), outputFileContent);
     }
 
@@ -54,46 +53,50 @@ public class CopyStreamIntegrationTest {
     public void whenCopyStringToOutputStream_thenCorrect() throws IOException {
         String string = "Should be copied to OutputStream.";
         String outputFileName = "src/test/resources/output.txt";
-        File outputFile = new File(outputFileName);
-        OutputStream out = new FileOutputStream("src/test/resources/output.txt");
+        Path outputFile = Paths.get("src/test/resources/output.txt");
+        OutputStream outputStream = Files.newOutputStream(outputFile);
 
-        StreamUtils.copy(string, StandardCharsets.UTF_8, out);
+        StreamUtils.copy(string, StandardCharsets.UTF_8, outputStream);
 
-        assertTrue(outputFile.exists());
-        String outputFileContent = getStringFromInputStream(new FileInputStream(outputFileName));
+        assertTrue(Files.exists(outputFile));
+
+        String outputFileContent = getStringFromInputStream(Files.newInputStream(Paths.get(outputFileName)));
         assertEquals(outputFileContent, string);
     }
 
     @Test
     public void whenCopyInputStreamToString_thenCorrect() throws IOException {
-        String inputFileName = "src/test/resources/input.txt";
-        InputStream is = new FileInputStream(inputFileName);
-        String content = StreamUtils.copyToString(is, StandardCharsets.UTF_8);
+        Path inputFile = Paths.get("src/test/resources/input.txt");
+        InputStream inputStream = Files.newInputStream(inputFile);
 
-        String inputFileContent = getStringFromInputStream(new FileInputStream(inputFileName));
+        String content = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        String inputFileContent = getStringFromInputStream(Files.newInputStream(inputFile));
         assertEquals(inputFileContent, content);
     }
 
     @Test
     public void whenCopyByteArrayToOutputStream_thenCorrect() throws IOException {
-        String outputFileName = "src/test/resources/output.txt";
+        Path outputFile = Paths.get("src/test/resources/output.txt");
         String string = "Should be copied to OutputStream.";
         byte[] byteArray = string.getBytes();
-        OutputStream out = new FileOutputStream("src/test/resources/output.txt");
+        OutputStream out = Files.newOutputStream(outputFile);
 
         StreamUtils.copy(byteArray, out);
-        String outputFileContent = getStringFromInputStream(new FileInputStream(outputFileName));
+
+        String outputFileContent = getStringFromInputStream(Files.newInputStream(outputFile));
         assertEquals(outputFileContent, string);
     }
 
     @Test
     public void whenCopyInputStreamToByteArray_thenCorrect() throws IOException {
-        String inputFileName = "src/test/resources/input.txt";
-        InputStream in = new FileInputStream(inputFileName);
+        Path inputFile = Paths.get("src/test/resources/input.txt");
+        InputStream in = Files.newInputStream(inputFile);
+
         byte[] out = StreamUtils.copyToByteArray(in);
 
         String content = new String(out);
-        String inputFileContent = getStringFromInputStream(new FileInputStream(inputFileName));
+        String inputFileContent = getStringFromInputStream(Files.newInputStream(inputFile));
         assertEquals(inputFileContent, content);
     }
 
