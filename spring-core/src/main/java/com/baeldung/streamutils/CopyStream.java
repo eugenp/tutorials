3ce164a63f2f8de3672ec7eb63cc19fcfd27d026
Alex Golub <a12thu@gmail.com>
2022-07-11 20:39:40 +0300

    'InputStream' can be constructed using 'Files.newInputStream'

--- spring-core/src/main/java/com/baeldung/streamutils/CopyStream.java
+++ spring-core/src/main/java/com/baeldung/streamutils/CopyStream.java
@@ -3,11 +3,12 @@ package com.baeldung.streamutils;
 import org.apache.commons.io.IOUtils;
 import org.springframework.util.StreamUtils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class CopyStream {
     public static String getStringFromInputStream(InputStream input) throws IOException {
@@ -18,7 +19,8 @@ public class CopyStream {
     }
 
     public InputStream getNonClosingInputStream() throws IOException {
-        InputStream in = new FileInputStream("src/test/resources/input.txt");
+        InputStream in = Files.newInputStream(Paths.get("src/test/resources/input.txt"));
+
         return StreamUtils.nonClosing(in);
     }
 }
