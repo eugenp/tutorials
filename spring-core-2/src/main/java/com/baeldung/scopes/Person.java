39fc9df2fe340616f0bd9b60d8d77fa4b7d82e0c
Alex Golub <a12thu@gmail.com>
2022-07-16 13:02:27 +0300

    Remove redundant `age` field

--- spring-core-2/src/main/java/com/baeldung/scopes/Person.java
+++ spring-core-2/src/main/java/com/baeldung/scopes/Person.java
@@ -2,14 +2,6 @@ package com.baeldung.scopes;
 
 public class Person {
     private String name;
-    private int age;
-
-    public Person() {
-    }
-
-    public Person(final String name, final int age) {
-        this.name = name;
-    }
 
     public String getName() {
         return name;
