9075b1ce8ca1b26dc6c0042d5274154a3daa450f
h_sharifi <h_sharifi@modernisc.com>
2022-07-17 12:12:03 +0430

    BAEL-5407: add tenant filter

---
+++ persistence-modules/spring-jpa-2/src/main/java/com/baeldung/multitenant/config/TenantFilter.java
@@ -0,0 +1,28 @@
+package com.baeldung.multitenant.config;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+@Order(1)
+class TenantFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String tenantName = req.getHeader("X-TenantID");
+        TenantContext.setCurrentTenant(tenantName);
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            TenantContext.setCurrentTenant("");
+        }
+
+    }
+}
