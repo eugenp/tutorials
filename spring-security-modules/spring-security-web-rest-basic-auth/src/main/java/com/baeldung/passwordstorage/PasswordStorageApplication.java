39108198348282dae09337f70990df62d211021c
sampadawagde <sampada.aws@gmail.com>
2022-07-18 20:27:20 +0530

    JAVA-12754: Moved 2 articles to spring-security-web-rest-basic-auth

---
+++ spring-security-modules/spring-security-web-rest-basic-auth/src/main/java/com/baeldung/passwordstorage/PasswordStorageApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.passwordstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordStorageApplication.class, args);
+    }
+
+}
