e1183cb7e72703e2d3866865b60cfbf720304039
achraftt <achraf.taitai1@gmail.com>
2022-07-22 21:23:38 +0200

    Load Image With Spring Boot and Thymeleaf

--- spring-web-modules/spring-thymeleaf-5/src/main/java/com/baeldung/thymeleaf/imageupload/UploadController.java
+++ spring-web-modules/spring-thymeleaf-5/src/main/java/com/baeldung/thymeleaf/imageupload/UploadController.java
@@ -14,21 +14,17 @@ import java.nio.file.Paths;
 
 @Controller public class UploadController {
 
-    public static String uploadDirectory = System.getProperty("user.dir") + "/uploads";
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir") + "/uploads";
 
     @GetMapping("/uploadimage") public String displayUploadForm() {
         return "imageupload/index";
     }
 
-    @PostMapping("/upload") public String uploadImage(Model model, @RequestParam("image") MultipartFile file) {
+    @PostMapping("/upload") public String uploadImage(Model model, @RequestParam("image") MultipartFile file) throws IOException {
         StringBuilder fileNames = new StringBuilder();
-        try {
-            Path fileNameAndPath = Paths.get(uploadDirectory, file.getOriginalFilename());
-            fileNames.append(file.getOriginalFilename());
-            Files.write(fileNameAndPath, file.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
         model.addAttribute("msg", "Uploaded images: " + fileNames.toString());
         return "imageupload/index";
     }
