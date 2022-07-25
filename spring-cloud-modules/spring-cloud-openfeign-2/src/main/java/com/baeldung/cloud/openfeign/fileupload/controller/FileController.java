b79cee7a45a38bf498f6e1dfe933f40438011f1c
Amitabh Tiwari <amitiwarimech@gmail.com>
2022-07-25 07:15:20 +0530

    Changes for fallback option

--- spring-cloud-modules/spring-cloud-openfeign-2/src/main/java/com/baeldung/cloud/openfeign/fileupload/controller/FileController.java
+++ spring-cloud-modules/spring-cloud-openfeign-2/src/main/java/com/baeldung/cloud/openfeign/fileupload/controller/FileController.java
@@ -6,34 +6,31 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.baeldung.cloud.openfeign.exception.NotFoundException;
 import com.baeldung.cloud.openfeign.fileupload.service.UploadService;
 
 @RestController
 public class FileController {
-    
+
     @Autowired
     private UploadService service;
-    
+
     @PostMapping(value = "/upload-mannual-client")
-    public boolean handleFileUploadWithManualClient(
-            @RequestPart(value = "file") MultipartFile file) {
+    public boolean handleFileUploadWithManualClient(@RequestPart(value = "file") MultipartFile file) {
         return service.uploadFileWithManualClient(file);
     }
-    
+
     @PostMapping(value = "/upload-file")
     public boolean handleFileUpload(@RequestPart(value = "file") MultipartFile file) {
         return service.uploadFileWithManualClient(file);
     }
 
     @PostMapping(value = "/upload-with-fallbackfactory")
-    public String uploadFileWithFallbackFactory(@RequestPart(value = "file") MultipartFile file)
-            throws NotFoundException {
+    public String uploadFileWithFallbackFactory(@RequestPart(value = "file") MultipartFile file) {
         return service.uploadFileWithFallbackFactory(file);
     }
 
     @PostMapping(value = "/upload-with-fallback")
-    public String uploadFileWithFallback(@RequestPart(value = "file") MultipartFile file) throws NotFoundException {
+    public String uploadFileWithFallback(@RequestPart(value = "file") MultipartFile file) {
         return service.uploadFileWithFallback(file);
     }
 
