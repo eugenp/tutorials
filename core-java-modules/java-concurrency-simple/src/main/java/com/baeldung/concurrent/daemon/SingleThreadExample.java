23f8f5ee676044c8b0f63602cc129587f0a5b949
keerthigadde <keerthigadde@gmail.com>
2022-08-07 00:04:12 +1000

    JAVA-12045: removed duplicate module after renaming java-simple-module to core-java-simple-module

--- core-java-modules/java-concurrency-simple/src/main/java/com/baeldung/concurrent/daemon/SingleThreadExample.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.daemon;
-
-public class SingleThreadExample {
-    public static void main(String[] args) {
-        NewThread t = new NewThread();
-        t.start();
-    }
-}
