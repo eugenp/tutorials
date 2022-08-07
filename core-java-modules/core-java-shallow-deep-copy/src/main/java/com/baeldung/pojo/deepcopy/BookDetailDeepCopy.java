2ef10c082032f83bc5ebf2cd0c58545d3adb7efe
JyothiSusarla <benjyo82@gmail.com>
2022-08-07 18:55:09 +0530

    changed some method and class names. Removed comments as well

--- core-java-modules/core-java-shallow-deep-copy/src/main/java/com/baeldung/pojo/deepcopy/BookDetailDeepCopy.java
+++ core-java-modules/core-java-shallow-deep-copy/src/main/java/com/baeldung/pojo/deepcopy/BookDetailDeepCopy.java
@@ -1,30 +1,12 @@
 package com.baeldung.pojo.deepcopy;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class BookDetailDeepCopy implements Cloneable, Serializable {
 
     private static final long serialVersionUID = 1L;
-    private String authorName;
-    private String publicationName;
     private int yearOfPublication;
-    private int editionNumber;
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public String getPublicationName() {
-        return publicationName;
-    }
-
-    public void setPublicationName(String publicationName) {
-        this.publicationName = publicationName;
-    }
 
     public int getYearOfPublication() {
         return yearOfPublication;
@@ -34,30 +16,16 @@ public class BookDetailDeepCopy implements Cloneable, Serializable {
         this.yearOfPublication = yearOfPublication;
     }
 
-    public int getEditionNumber() {
-        return editionNumber;
-    }
-
-    public void setEditionNumber(int editionNumber) {
-        this.editionNumber = editionNumber;
-    }
-
     public BookDetailDeepCopy() {
         super();
 
     }
 
-    public BookDetailDeepCopy(String authorName, String publicationName, int yearOfPublication, int editionNumber) {
+    public BookDetailDeepCopy(int yearOfPublication) {
         super();
-        this.authorName = authorName;
-        this.publicationName = publicationName;
-        this.yearOfPublication = yearOfPublication;
-        this.editionNumber = editionNumber;
-    }
 
-    @Override
-    public String toString() {
-        return "BookDetailDeepCopy [authorName=" + authorName + ", publicationName=" + publicationName + ", yearOfPublication=" + yearOfPublication + ", editionNumber=" + editionNumber + "]";
+        this.yearOfPublication = yearOfPublication;
+
     }
 
     @Override
@@ -65,15 +33,19 @@ public class BookDetailDeepCopy implements Cloneable, Serializable {
         return super.clone();
     }
 
+    public BookDetailDeepCopy(BookDetailDeepCopy bookDetail) {
+
+        this.yearOfPublication = bookDetail.getYearOfPublication();
+    }
+
+    @Override
+    public String toString() {
+        return "BookDetailDeepCopy [yearOfPublication=" + yearOfPublication + "]";
+    }
+
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((authorName == null) ? 0 : authorName.hashCode());
-        result = prime * result + editionNumber;
-        result = prime * result + ((publicationName == null) ? 0 : publicationName.hashCode());
-        result = prime * result + yearOfPublication;
-        return result;
+        return Objects.hash(yearOfPublication);
     }
 
     @Override
@@ -85,28 +57,7 @@ public class BookDetailDeepCopy implements Cloneable, Serializable {
         if (getClass() != obj.getClass())
             return false;
         BookDetailDeepCopy other = (BookDetailDeepCopy) obj;
-        if (authorName == null) {
-            if (other.authorName != null)
-                return false;
-        } else if (!authorName.equals(other.authorName))
-            return false;
-        if (editionNumber != other.editionNumber)
-            return false;
-        if (publicationName == null) {
-            if (other.publicationName != null)
-                return false;
-        } else if (!publicationName.equals(other.publicationName))
-            return false;
-        if (yearOfPublication != other.yearOfPublication)
-            return false;
-        return true;
-    }
-
-    public BookDetailDeepCopy(BookDetailDeepCopy bookDetail) {
-        this.authorName = bookDetail.getAuthorName();
-        this.editionNumber = bookDetail.getEditionNumber();
-        this.publicationName = bookDetail.getPublicationName();
-        this.yearOfPublication = bookDetail.getYearOfPublication();
+        return yearOfPublication == other.yearOfPublication;
     }
 
 }
--- core-java-modules/core-java-shallow-deep-copy/src/test/java/com/baeldung/shallowdeepcopy/DeepCopyUnitTest.java
+++ core-java-modules/core-java-shallow-deep-copy/src/test/java/com/baeldung/shallowdeepcopy/DeepCopyUnitTest.java
@@ -5,8 +5,8 @@ import static org.junit.Assert.assertNotEquals;
 import org.apache.commons.lang.SerializationUtils;
 import org.junit.Test;
 
+import com.baeldung.pojo.deepcopy.BookDeepCopy;
 import com.baeldung.pojo.deepcopy.BookDetailDeepCopy;
-import com.baeldung.pojo.deepcopy.BookShelfInfoDeepCopy;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
@@ -14,81 +14,75 @@ import com.google.gson.Gson;
 public class DeepCopyUnitTest {
 
     @Test
-    public void whenCreatingDeepCopyWithCloneable_andChangeCloneDate_thenCloneShouldNotBeSameAsOriginalObject() throws CloneNotSupportedException {
+    public void whenCloneChanges_thenOriginalObjectShouldNotChange() throws CloneNotSupportedException {
 
-        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy("Lynn Beighley", "SHROFF", 2021, 2);
-        BookShelfInfoDeepCopy originalObj = new BookShelfInfoDeepCopy(1111, "Head First SQL", "A12", bookDetail);
-        // deep copy happens as all the objects with COMPOSITION RELATIONSHIP with this class also implement Cloneable interface
-        // and override clone method
-        BookShelfInfoDeepCopy deepCopy = (BookShelfInfoDeepCopy) originalObj.clone();
-        // 'deepCopy' and 'originalObj' reference completely different memory locations.
-        // Thus changing one does not change the other.
-        deepCopy.getBookDetail()
+        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy(2021);
+        BookDeepCopy originalObj = new BookDeepCopy("Head First SQL", bookDetail);
+
+        BookDeepCopy deepCopyObj = (BookDeepCopy) originalObj.clone();
+
+        deepCopyObj.getBookDetail()
             .setYearOfPublication(2022);
 
-        assertNotEquals(originalObj, deepCopy);
+        assertNotEquals(originalObj, deepCopyObj);
     }
 
     @Test
-    public void whenCreatingDeepcopyWithCopyConstructor_andChangeCopiedObject_thenOnlyCopiedObjectShouldChange() {
+    public void whenCopyChanges_thenOriginalObjectShouldNotChange() {
 
-        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy("Lynn Beighley", "SHROFF", 2021, 2);
-        BookShelfInfoDeepCopy originalObj = new BookShelfInfoDeepCopy(1111, "Head First SQL", "A12", bookDetail);
-        // create a deep copy through copy constructor
-        // Objects having COMPOSITION RELATIONSHIP with this class also implement copy constructor.
-        // Hence, actual values are copied.
-        BookShelfInfoDeepCopy deepCopy = new BookShelfInfoDeepCopy(originalObj);// triggers
-                                                                                // deep
-                                                                                // copy
-        deepCopy.getBookDetail()
+        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy(2021);
+        BookDeepCopy originalObj = new BookDeepCopy("Head First SQL", bookDetail);
+
+        BookDeepCopy deepCopyObj = new BookDeepCopy(originalObj);
+
+        deepCopyObj.getBookDetail()
             .setYearOfPublication(2022);
 
-        assertNotEquals(originalObj, deepCopy);
+        assertNotEquals(originalObj, deepCopyObj);
     }
 
     @Test
-    public void whenCreatingDeepCopyWithSerializableUsingJacksonLib_andChangeJacksonCopy_thenOnlyJacksonCopyShouldChange() throws JsonProcessingException {
+    public void whenCreatingCopyWithSerializableUsingJacksonLib_thenOriginalObjectShouldNotChange() throws JsonProcessingException {
 
-        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy("Lynn Beighley", "SHROFF", 2021, 2);
-        BookShelfInfoDeepCopy originalObj = new BookShelfInfoDeepCopy(1111, "Head First SQL", "A12", bookDetail);
+        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy(2021);
+        BookDeepCopy originalObj = new BookDeepCopy("Head First SQL", bookDetail);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        // Completely separate instance of the type with same values as original is created.
-        // All objects having COMPOSITION RELATIONSHIP with this class also have to implement Serializable interface.
-        BookShelfInfoDeepCopy deepCopy = objectMapper.readValue(objectMapper.writeValueAsString(originalObj), BookShelfInfoDeepCopy.class);
-        // Hence, change in 'deepCopy' does not change 'originalObj' in any way.
-        deepCopy.getBookDetail()
+
+        BookDeepCopy deepCopyObj = objectMapper.readValue(objectMapper.writeValueAsString(originalObj), BookDeepCopy.class);
+
+        deepCopyObj.getBookDetail()
             .setYearOfPublication(2022);
 
-        assertNotEquals(originalObj, deepCopy);
+        assertNotEquals(originalObj, deepCopyObj);
 
     }
 
     @Test
-    public void whenCreatingDeepCopyWithSerializableUsingGsonLib_andChangeGsonCopy_thenOnlyGsonCopyShouldChange() {
+    public void whenCreatingCopyWithSerializableUsingGsonLib_thenOriginalObjectShouldNotChange() {
 
-        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy("Lynn Beighley", "SHROFF", 2021, 2);
-        BookShelfInfoDeepCopy originalObj = new BookShelfInfoDeepCopy(1111, "Head First SQL", "A12", bookDetail);
+        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy(2021);
+        BookDeepCopy originalObj = new BookDeepCopy("Head First SQL", bookDetail);
 
         Gson gson = new Gson();
-        BookShelfInfoDeepCopy deepCopy = gson.fromJson(gson.toJson(originalObj), BookShelfInfoDeepCopy.class);
-        deepCopy.getBookDetail()
+        BookDeepCopy deepCopyObj = gson.fromJson(gson.toJson(originalObj), BookDeepCopy.class);
+        deepCopyObj.getBookDetail()
             .setYearOfPublication(2022);
 
-        assertNotEquals(originalObj, deepCopy);
+        assertNotEquals(originalObj, deepCopyObj);
 
     }
 
     @Test
-    public void whenCreatingDeepCopyWithSerializableUsingApacheCommonsLang_andChangeCommonsLangCopy_thenOnlyCommonsLangCopyShouldChange() {
+    public void whenCreatingCopyWithSerializableUsingApacheCommonsLang_thenOriginalObjectShouldNotChange() {
 
-        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy("Lynn Beighley", "SHROFF", 2021, 2);
-        BookShelfInfoDeepCopy originalObj = new BookShelfInfoDeepCopy(1111, "Head First SQL", "A12", bookDetail);
-        BookShelfInfoDeepCopy deepCopy = (BookShelfInfoDeepCopy) SerializationUtils.clone(originalObj);
-        deepCopy.getBookDetail()
+        BookDetailDeepCopy bookDetail = new BookDetailDeepCopy(2021);
+        BookDeepCopy originalObj = new BookDeepCopy("Head First SQL", bookDetail);
+        BookDeepCopy deepCopyObj = (BookDeepCopy) SerializationUtils.clone(originalObj);
+        deepCopyObj.getBookDetail()
             .setYearOfPublication(2022);
 
-        assertNotEquals(originalObj, deepCopy);
+        assertNotEquals(originalObj, deepCopyObj);
 
     }
 
