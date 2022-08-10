aa99d3ca153d71611c29c0b3e845a1019c0fc0ff
h_sharifi <h_sharifi@modernisc.com>
2022-08-10 13:13:34 +0430

    BAEL-5407: improve indent

--- persistence-modules/spring-jpa-2/src/main/java/com/baeldung/multitenant/MultiTenantApplication.java
+++ persistence-modules/spring-jpa-2/src/main/java/com/baeldung/multitenant/MultiTenantApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class MultiTenantApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 	    SpringApplication.run(MultiTenantApplication.class, args);
-	}
+    }
 
 }
