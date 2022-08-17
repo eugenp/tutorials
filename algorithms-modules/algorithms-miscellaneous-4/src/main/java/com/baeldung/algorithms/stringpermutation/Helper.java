3a0c628d3ff1fb91d3449cfadb9824a662df0cfa
Eugene Kovko <37694937+eukovko@users.noreply.github.com>
2022-08-17 22:14:47 +0200

    BAEL-5646: String permutation improvements (#12613)

--- algorithms-modules/algorithms-miscellaneous-4/src/main/java/com/baeldung/algorithms/stringpermutation/Helper.java
+++ algorithms-modules/algorithms-miscellaneous-4/src/main/java/com/baeldung/algorithms/stringpermutation/Helper.java
@@ -1,14 +1,20 @@
 package com.baeldung.algorithms.stringpermutation;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ArrayHelper {
+public class Helper {
 
-    private ArrayHelper() {
+    private Helper() {
     }
 
     static List<Character> toCharacterList(final String string) {
         return string.chars().mapToObj(s -> ((char) s)).collect(Collectors.toList());
     }
+
+    static String toString(Collection<Character> collection) {
+        return collection.stream().map(Object::toString).collect(Collectors.joining());
+    }
+
 }
