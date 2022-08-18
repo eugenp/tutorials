6fe3c4eb9d7dd5436ef743d922a16d36fbcfa626
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2022-08-18 10:06:04 +0200

    reformat single line if/else statements

--- core-java-modules/core-java-numbers-5/src/main/java/com/baeldung/positivenegative/PositiveOrNegative.java
+++ core-java-modules/core-java-numbers-5/src/main/java/com/baeldung/positivenegative/PositiveOrNegative.java
@@ -6,33 +6,38 @@ public class PositiveOrNegative {
     }
 
     public static Result byOperator(Integer integer) {
-        if (integer > 0)
+        if (integer > 0) {
             return Result.POSITIVE;
-        else if (integer < 0)
+        } else if (integer < 0) {
             return Result.NEGATIVE;
+        }
         return Result.ZERO;
     }
 
     public static Result bySignum(Integer integer) {
         int result = Integer.signum(integer);
-        if (result == 0)
+        if (result == 0) {
             return Result.ZERO;
+        }
 
-        if (result == 1)
+        if (result == 1) {
             return Result.POSITIVE;
-        else
+        } else {
             return Result.NEGATIVE;
+        }
     }
 
     public static Result bySignum(Float floatNumber) {
         float result = Math.signum(floatNumber);
 
-        if (result == 0)
+        if (result == 0) {
             return Result.ZERO;
+        }
 
-        if (result == 1.0f)
+        if (result == 1.0f) {
             return Result.POSITIVE;
-        else
+        } else {
             return Result.NEGATIVE;
+        }
     }
 }
