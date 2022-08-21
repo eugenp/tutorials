fe6384b546df3b635e4883e2d0fbb60beadfb95b
PentaKon <kpentaris@gmail.com>
2022-08-22 00:39:39 +0300

    refactor application snippet

--- core-java-modules/core-java-concurrency-advanced-4/src/main/java/com/baeldung/ownablesynchronizers/Application.java
+++ core-java-modules/core-java-concurrency-advanced-4/src/main/java/com/baeldung/ownablesynchronizers/Application.java
@@ -4,58 +4,37 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Application {
 
-    static ReentrantLock firstLock = new ReentrantLock(true);
-    static ReentrantLock secondLock = new ReentrantLock(true);
+    public static Thread createThread(ReentrantLock mainLock, ReentrantLock otherLock, String threadName) {
+        return new Thread(() -> {
+            try {
+                mainLock.lock();
+
+                // synchronized block here allows us to wait for other thread to lock,
+                // in order to simulate a deadlock
+                synchronized (Application.class) {
+                    Application.class.notify();
+                    if (!otherLock.isLocked()) {
+                        Application.class.wait();
+                    }
+                }
+
+                // thread will hang here
+                otherLock.lock();
+
+                System.out.println(threadName + ": Finished");
+                mainLock.unlock();
+                otherLock.unlock();
+            } catch (InterruptedException e) {
+                System.out.println(threadName + ": " + e.getMessage());
+            }
+        });
+    }
 
     public static void main(String[] args) throws Exception {
-        // Create first thread that will lock on 'firstLock'
-        Thread first = new Thread(() -> {
-            try {
-                firstLock.lock();
-
-                // synchronized block here allows us to wait for other thread to lock,
-                // in order to simulate a deadlock
-                synchronized (Application.class) {
-                    Application.class.notify();
-                    if (!secondLock.isLocked()) {
-                        Application.class.wait();
-                    }
-                }
-
-                // thread will hang here
-                secondLock.lock();
-
-                System.out.println("Thread 1: Finished");
-                firstLock.unlock();
-                secondLock.unlock();
-            } catch (Exception e) {
-
-            }
-        });
-
-        // Second thread will lock on 'secondLock'
-        Thread second = new Thread(() -> {
-            try {
-                secondLock.lock();
-
-                // synchronized block here allows us to wait for other thread to lock,
-                // in order to simulate a deadlock
-                synchronized (Application.class) {
-                    Application.class.notify();
-                    if (!firstLock.isLocked()) {
-                        Application.class.wait();
-                    }
-                }
-
-                // thread will hang here
-                firstLock.lock();
-
-                System.out.println("Thread 2: Finished");
-                firstLock.unlock();
-                secondLock.unlock();
-            } catch (Exception e) {
-            }
-        });
+        ReentrantLock firstLock = new ReentrantLock(true);
+        ReentrantLock secondLock = new ReentrantLock(true);
+        Thread first = createThread(firstLock, secondLock, "Thread-0");
+        Thread second = createThread(secondLock, firstLock, "Thread-1");
 
         first.start();
         second.start();
