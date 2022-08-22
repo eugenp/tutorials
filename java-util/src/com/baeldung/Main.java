26f73d1e113dab3027f45e97db3870ce3c8ce9c6
andrei-mandris <andrei.mandris.ext@deliveryhero.com>
2022-08-22 10:31:07 +0300

    BAEL-5692 Validate UUID String in Java

---
+++ java-util/src/com/baeldung/Main.java
@@ -0,0 +1,9 @@
+package com.baeldung;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RegexUUIDValidator.isUUID("1234-1234-53214-1234");
+        UUIDCustomValidator.isUUID("1234-1234-53214-1234");
+    }
+}
