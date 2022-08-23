95ba0cec234ca6b7956ac363b2b9ea90bbe3379e
andrei-mandris <andrei.mandris.ext@deliveryhero.com>
2022-08-23 23:07:52 +0300

    cleaned up the Main method

--- java-util/src/com/baeldung/Main.java
+++ java-util/src/com/baeldung/Main.java
@@ -3,7 +3,5 @@ package com.baeldung;
 public class Main {
 
     public static void main(String[] args) {
-        RegexUUIDValidator.isUUID("1234-1234-53214-1234");
-        UUIDCustomValidator.isUUID("1234-1234-53214-1234");
     }
 }
