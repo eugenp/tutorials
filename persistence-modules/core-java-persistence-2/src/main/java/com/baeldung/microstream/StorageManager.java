36646ef48201ff2b5e95ca405a4faaa53583d8c6
Daniel Strmecki <daniel.strmecki@gmail.com>
2022-08-27 11:57:31 +0200

    BAEL-5653: Lazy loading

--- persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/microstream/StorageManager.java
+++ persistence-modules/core-java-persistence-2/src/main/java/com/baeldung/microstream/StorageManager.java
@@ -3,27 +3,56 @@ package com.baeldung.microstream;
 import one.microstream.storage.embedded.types.EmbeddedStorage;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
 
+import java.nio.file.Paths;
+import java.util.List;
+
 public class StorageManager {
 
+    public static final String STORAGE_ROOT_FOLDER = "storage";
+
     public static EmbeddedStorageManager initializeStorageWithStringAsRoot(String root) {
-        EmbeddedStorageManager storageManager = EmbeddedStorage.start();
+        EmbeddedStorageManager storageManager = EmbeddedStorage.start(Paths.get(STORAGE_ROOT_FOLDER, root));
         storageManager.setRoot(root);
         storageManager.storeRoot();
         return storageManager;
     }
 
     public static EmbeddedStorageManager initializeStorageWithCustomTypeAsRoot(String root) {
-        EmbeddedStorageManager storageManager = EmbeddedStorage.start();
+        EmbeddedStorageManager storageManager = EmbeddedStorage.start(Paths.get(STORAGE_ROOT_FOLDER, root));
         storageManager.setRoot(new RootInstance(root));
         storageManager.storeRoot();
         return storageManager;
     }
 
+    public static EmbeddedStorageManager initializeStorageWithCustomTypeAsRoot(String root, List<Book> booksToStore) {
+        EmbeddedStorageManager storageManager = EmbeddedStorage.start(Paths.get(STORAGE_ROOT_FOLDER, root));
+        RootInstance rootInstance = new RootInstance(root);
+        storageManager.setRoot(rootInstance);
+        storageManager.storeRoot();
+
+        List<Book> books = rootInstance.getBooks();
+        books.addAll(booksToStore);
+        storageManager.store(books);
+        return storageManager;
+    }
+
     public static EmbeddedStorageManager loadOrCreateStorage(String root) {
-        EmbeddedStorageManager storageManager = EmbeddedStorage.start();
+        EmbeddedStorageManager storageManager = EmbeddedStorage.start(Paths.get(STORAGE_ROOT_FOLDER, root));
         if (storageManager.root() == null) {
             RootInstance rootInstance = new RootInstance(root);
             storageManager.setRoot(rootInstance);
+            storageManager.storeRoot();
+        }
+        return storageManager;
+    }
+
+    public static EmbeddedStorageManager lazyLoadOrCreateStorage(String root, List<Book> booksToStore) {
+        EmbeddedStorageManager storageManager = EmbeddedStorage.start(Paths.get(STORAGE_ROOT_FOLDER, root));
+        if (storageManager.root() == null) {
+            RootInstanceLazy rootInstance = new RootInstanceLazy(root);
+            rootInstance.getBooks().addAll(booksToStore);
+            storageManager.setRoot(rootInstance);
+            storageManager.storeRoot();
         }
         return storageManager;
     }
--- persistence-modules/core-java-persistence-2/src/test/java/com/baeldung/microstream/StorageManagerUnitTest.java
+++ persistence-modules/core-java-persistence-2/src/test/java/com/baeldung/microstream/StorageManagerUnitTest.java
@@ -3,11 +3,16 @@ package com.baeldung.microstream;
 import one.microstream.storage.embedded.types.EmbeddedStorageManager;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class StorageManagerUnitTest {
+
+    private static final Author author = new Author("Joanne", "Rowling");
+    private static final Book bookOne = new Book("Harry Potter and the Philosopher's Stone", author, 1997);
+    private static final Book bookTwo = new Book("Harry Potter and the Chamber of Secrets", author, 1998);
 
     @Test
     void givenStorageWithStringAsRoot_whenFetchingRoot_thenExpectedStringIsReturned() {
@@ -26,9 +31,31 @@ class StorageManagerUnitTest {
     }
 
     @Test
-    void givenStorageWithCustomTypeAsRoot_whenLoadingOrCreateingStorage_thenNonNullRootIsReturned() {
-        EmbeddedStorageManager storageManager = StorageManager.loadOrCreateStorage("baeldung-demo-3");
+    void givenStorageWithCustomTypeAsRoot_whenStoringAdditionalObjects_thenAdditionalObjectsAreSuccessfullyStored() {
+        EmbeddedStorageManager storageManager = StorageManager.initializeStorageWithCustomTypeAsRoot("baeldung-demo-3", Arrays.asList(bookOne, bookTwo));
+        RootInstance rootInstance = (RootInstance) storageManager.root();
+
+        assertThat(rootInstance.getName()).isEqualTo("baeldung-demo-3");
+        assertThat(rootInstance.getBooks()).hasSize(2);
+        assertThat(rootInstance.getBooks().get(0)).isEqualTo(bookOne);
+        assertThat(rootInstance.getBooks().get(1)).isEqualTo(bookTwo);
+    }
+
+    @Test
+    void givenStorageWithCustomTypeAsRoot_whenLoadingOrCreatingStorage_thenNonNullRootIsReturned() {
+        EmbeddedStorageManager storageManager = StorageManager.loadOrCreateStorage("baeldung-demo-4");
         assertThat(storageManager.root()).isNotNull();
+        storageManager.shutdown();
+    }
+
+    @Test
+    void givenStorageWithCustomTypeAsRoot_whenLazyLoadingAdditionalObjects_thenAdditionalObjectsAreSuccessfullyLoadedOnDemand() {
+        EmbeddedStorageManager storageManager = StorageManager.lazyLoadOrCreateStorage("baeldung-demo-5", Arrays.asList(bookOne, bookTwo));
+        RootInstanceLazy rootInstance = (RootInstanceLazy) storageManager.root();
+        assertThat(rootInstance.getName()).isEqualTo("baeldung-demo-5");
+        assertThat(rootInstance.getBooks()).hasSize(2);
+        assertThat(rootInstance.getBooks().get(0)).isEqualTo(bookOne);
+        assertThat(rootInstance.getBooks().get(1)).isEqualTo(bookTwo);
         storageManager.shutdown();
     }
 
