0615cb0c788a60db203691005b2f8f05a2372e14
Mahn00rMalik <malikmah23@gmail.com>
2022-09-06 10:52:53 +0500

    Create a BMI Caclualtor in Java

--- bmicalculator/src/test/java/com/baeldung/bmicalculator/BMICalculatorUnitTest.java
+++ bmicalculator/src/test/java/com/baeldung/bmicalculator/BMICalculatorUnitTest.java
@@ -8,7 +8,7 @@ public class BMICalculatorUnitTest {
     @Test
     public void whenBMIIsGreaterThanThirty_thenObese() {
 
-        double weight = 50;
+        double weight = 100;
         double height = 1.524;
         String actual = BMICalculator.calculateBMI(weight, height);
         String expected = "Obese";
