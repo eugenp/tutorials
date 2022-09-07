a33f4b132fb740e71c70d9dbade628ae83b75926
Amitabh Tiwari <amitiwarimech@gmail.com>
2022-09-07 10:15:38 +0530

    Changes for JSON to Protobuf

--- spring-protobuf/src/test/java/com/baeldung/protobuf/convert/ProtobufUtilUnitTest.java
+++ spring-protobuf/src/test/java/com/baeldung/protobuf/convert/ProtobufUtilUnitTest.java
@@ -2,13 +2,6 @@ package com.baeldung.protobuf.convert;
 
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
-import java.io.BufferedRead
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;er;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -23,34 +16,28 @@ import com.google.protobuf.Message;
 
 public class ProtobufUtilUnitTest {
 
-	public static String jsonInput = "{\r\n"
-			+ "  \"boolean\": true,\r\n"
-			+ "  \"color\": \"gold\",\r\n"
-			+ "  \"object\": {\r\n"
-			+ "    \"a\": \"b\",\r\n"
-			+ "    \"c\": \"d\"\r\n"
-			+ "  },\r\n"
-			+ "  \"string\": \"Hello World\"\r\n"
-			+ "}";
-	
-	@Test
-	public void givenProtobuf_convertToJson() throws IOException {
-		Message fromJson = ProtobuffUtil.fromJson(jsonInput);
+    public static String jsonInput = "{\r\n" + "  \"boolean\": true,\r\n" + "  \"color\": \"gold\",\r\n"
+            + "  \"object\": {\r\n" + "    \"a\": \"b\",\r\n" + "    \"c\": \"d\"\r\n" + "  },\r\n"
+            + "  \"string\": \"Hello World\"\r\n" + "}";
 
-		InputStream inputStream = new ByteArrayInputStream(fromJson.toByteArray());
+    @Test
+    public void givenProtobuf_convertToJson() throws IOException {
+        Message fromJson = ProtobuffUtil.fromJson(jsonInput);
 
-		StringBuilder textBuilder = new StringBuilder();
-		try (Reader reader = new BufferedReader(
-				new InputStreamReader(inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
-			int c = 0;
-			while ((c = reader.read()) != -1) {
-				textBuilder.append((char) c);
-			}
-		}
-		String json = ProtobuffUtil.toJson(fromJson);
-		Assert.assertTrue(json.contains("\"boolean\": true"));
-		Assert.assertTrue(json.contains("\"string\": \"Hello World\""));
-		Assert.assertTrue(json.contains("\"color\": \"gold\""));
-	}
-	
+        InputStream inputStream = new ByteArrayInputStream(fromJson.toByteArray());
+
+        StringBuilder textBuilder = new StringBuilder();
+        try (Reader reader = new BufferedReader(
+                new InputStreamReader(inputStream, Charset.forName(StandardCharsets.UTF_8.name())))) {
+            int c = 0;
+            while ((c = reader.read()) != -1) {
+                textBuilder.append((char) c);
+            }
+        }
+        String json = ProtobuffUtil.toJson(fromJson);
+        Assert.assertTrue(json.contains("\"boolean\": true"));
+        Assert.assertTrue(json.contains("\"string\": \"Hello World\""));
+        Assert.assertTrue(json.contains("\"color\": \"gold\""));
+    }
+
 }
