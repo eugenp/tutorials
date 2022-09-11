79bbf68fdfcf36c4aa019d999e4b4713858fa87e
andreimandris <andrei.mandris.ext@deliveryhero.com>
2022-09-11 11:52:56 +0300

    segregated unit tests by validator type.

--- java-util/src/test/java/validator/uuid/UUIDValidatorTest.java
+++ java-util/src/test/java/validator/uuid/UUIDValidatorTest.java
@@ -9,6 +9,10 @@ public class UUIDValidatorTest {
   public void whenValidUUIDStringIsValidated_thenValidationSucceeds() {
     Assertions.assertTrue(
         UUIDCustomValidator.isUUID("26929514-237c-11ed-861d-0242ac120002"));
+  }
+
+  @Test
+  public void whenUUIDIsValidatedUsingRegex_thenValidationSucceeds() {
     Assertions.assertTrue(RegexUUIDValidator.isUUID("26929514-237c-11ed-861d-0242ac120002"));
   }
 }
