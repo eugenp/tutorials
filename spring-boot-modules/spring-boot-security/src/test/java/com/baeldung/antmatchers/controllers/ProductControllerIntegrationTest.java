c260ad3c476fd7abe79933bd26df297ee1f7d484
anuragkumawat <anuragkumawat7@gmail.com>
2022-09-17 23:50:10 +0530

    JAVA-14676 Update remaining code

--- spring-boot-modules/spring-boot-security/src/test/java/com/baeldung/antmatchers/controllers/ProductControllerIntegrationTest.java
+++ spring-boot-modules/spring-boot-security/src/test/java/com/baeldung/antmatchers/controllers/ProductControllerIntegrationTest.java
@@ -1,24 +1,29 @@
 package com.baeldung.antmatchers.controllers;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.baeldung.antmatchers.AntMatchersExampleApplication;
+import com.baeldung.antmatchers.config.SecurityConfiguration;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = ProductController.class)
-class ProductControllerIntegrationTest {
+@ContextConfiguration(classes = { AntMatchersExampleApplication.class, SecurityConfiguration.class })
+public class ProductControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void getProducts() throws Exception {
+    public void getProducts() throws Exception {
         mockMvc.perform(get("/products"))
           .andExpect(status().isOk());
     }
