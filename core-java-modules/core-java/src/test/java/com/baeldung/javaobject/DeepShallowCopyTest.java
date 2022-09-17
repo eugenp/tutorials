d09024805b8a8ea083cb4fc11dd91568de1f3a5c
Ritu Priya <priya.ritu89@gmail.com>
2022-09-17 10:32:33 +0530

    Deep and shallow copy code

---
+++ core-java-modules/core-java/src/test/java/com/baeldung/javaobject/DeepShallowCopyTest.java
@@ -0,0 +1,29 @@
+package com.baeldung.javaobject;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+public class DeepShallowCopyTest {
+    @Test
+    public void deepCopyTest(){
+        ObjectExample obj1 = new ObjectExample();
+        ObjectExample deepObj= new ObjectExample();
+        obj1.name="Advanced Java";
+        deepObj.name="Core Java";
+
+        assertNotEquals(obj1.name,deepObj.name);
+        
+    }
+    @Test
+    public void shallowCopyTest(){
+        ObjectExample obj1 = new ObjectExample();
+        ObjectExample shallowpObj= obj1;
+        obj1.name="Advanced Java";
+        shallowpObj.name="Core Java";
+        assertEquals(obj1.name,shallowpObj.name);
+
+    }
+
+}
