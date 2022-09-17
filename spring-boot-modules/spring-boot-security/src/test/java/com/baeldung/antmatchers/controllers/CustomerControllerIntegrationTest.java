a22e5483647b65080ace21c03afef8af6106ba22
anuragkumawat <anuragkumawat7@gmail.com>
2022-09-17 23:50:10 +0530

    JAVA-14676 Update remaining code

--- spring-boot-modules/spring-boot-security/src/test/java/com/baeldung/antmatchers/controllers/CustomerControllerIntegrationTest.java
+++ spring-boot-modules/spring-boot-security/src/test/java/com/baeldung/antmatchers/controllers/CustomerControllerIntegrationTest.java
@@ -1,36 +1,41 @@
 package com.baeldung.antmatchers.controllers;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.baeldung.antmatchers.AntMatchersExampleApplication;
+import com.baeldung.antmatchers.config.SecurityConfiguration;
+
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = CustomerController.class)
-class CustomerControllerIntegrationTest {
+@ContextConfiguration(classes = { AntMatchersExampleApplication.class, SecurityConfiguration.class })
+public class CustomerControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    void getCustomerByIdUnauthorized() throws Exception {
+    public void getCustomerByIdUnauthorized() throws Exception {
         mockMvc.perform(get("/customers/1")).andExpect(status().isUnauthorized());
     }
 
     @Test
-    void getCustomerByIdForbidden() throws Exception {
+    public void getCustomerByIdForbidden() throws Exception {
         mockMvc.perform(get("/customers/1").with(user("user").roles("USER")))
           .andExpect(status().isForbidden());
     }
 
     @Test
-    void getCustomerByIdOk() throws Exception {
+    public void getCustomerByIdOk() throws Exception {
         mockMvc.perform(get("/customers/1").with(user("admin").roles("ADMIN")))
           .andExpect(status().isOk());
     }
