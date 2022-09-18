cb30fcb12d99ea82dd9b3ff96c777ddffcf05464
Prabhash <kumarprabhash1995@gmail.com>
2022-09-18 15:09:29 +0200

    BAEL-5765 Adding scan a char unit tests

---
+++ core-java-modules/core-java-io-apis-2/src/test/java/com/baeldung/scanner/ScanACharacterTest.java
@@ -0,0 +1,37 @@
+package com.baeldung.scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+public class ScanACharacterTest {
+
+    String INPUT = new StringBuilder().append("abc\n")
+      .append("mno\n")
+      .append("xyz\n")
+      .toString();
+
+    @Test
+    public void testReadCharUsingNext() {
+        Scanner sc = new Scanner(INPUT);
+        char c = sc.next().charAt(0);
+        assertEquals('a', c);
+    }
+
+    @Test
+    public void testReadCharUsingFindInLine() {
+        Scanner sc = new Scanner(INPUT);
+        char c = sc.findInLine(".").charAt(0);
+        assertEquals('a', c);
+    }
+
+    @Test
+    public void testReadCharUsingUseDelimiter() {
+        Scanner sc = new Scanner(INPUT);
+        String c = sc.useDelimiter("").next();
+        assertEquals("a", c);
+    }
+
+}
