b4b0658ec601c83162b4c85be8b1faf77b9c0dfc
Prabhash <kumarprabhash1995@gmail.com>
2022-09-18 20:36:41 +0200

    BAEL-5765 updated test cases naming strategy

--- core-java-modules/core-java-io-apis-2/src/test/java/com/baeldung/scanner/ScanACharacterUnitTest.java
+++ core-java-modules/core-java-io-apis-2/src/test/java/com/baeldung/scanner/ScanACharacterUnitTest.java
@@ -8,28 +8,29 @@ import org.junit.jupiter.api.Test;
 
 public class ScanACharacterUnitTest {
 
-    String INPUT = new StringBuilder().append("abc\n")
+    // given - input scanner source, no need to scan from console
+    String Input = new StringBuilder().append("abc\n")
       .append("mno\n")
       .append("xyz\n")
       .toString();
 
     @Test
-    public void testReadCharUsingNext() {
-        Scanner sc = new Scanner(INPUT);
+    public void givenInputSource_whenScanCharUsingNext_thenOneCharIsRead() {
+        Scanner sc = new Scanner(Input);
         char c = sc.next().charAt(0);
         assertEquals('a', c);
     }
 
     @Test
-    public void testReadCharUsingFindInLine() {
-        Scanner sc = new Scanner(INPUT);
+    public void givenInputSource_whenScanCharUsingFindInLine_thenOneCharIsRead() {
+        Scanner sc = new Scanner(Input);
         char c = sc.findInLine(".").charAt(0);
         assertEquals('a', c);
     }
 
     @Test
-    public void testReadCharUsingUseDelimiter() {
-        Scanner sc = new Scanner(INPUT);
+    public void givenInputSource_whenScanCharUsingUseDelimiter_thenOneCharIsRead() {
+        Scanner sc = new Scanner(Input);
         String c = sc.useDelimiter("").next();
         assertEquals("a", c);
     }
