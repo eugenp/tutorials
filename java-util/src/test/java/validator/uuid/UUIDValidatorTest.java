2ee27313e4240cd6a07c72e2b5cb8e636351d703
andreimandris <andrei.mandris.ext@deliveryhero.com>
2022-09-19 12:22:09 +0300

    inlined the validating logic inside the unit tests

--- java-util/src/test/java/validator/uuid/UUIDValidatorTest.java
+++ java-util/src/test/java/validator/uuid/UUIDValidatorTest.java
@@ -3,16 +3,27 @@ package validator.uuid;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.UUID;
+import java.util.regex.Pattern;
+
 public class UUIDValidatorTest {
 
   @Test
   public void whenValidUUIDStringIsValidated_thenValidationSucceeds() {
-    Assertions.assertTrue(
-        UUIDCustomValidator.isUUID("26929514-237c-11ed-861d-0242ac120002"));
+    String validUUID = "26929514-237c-11ed-861d-0242ac120002";
+    Assertions.assertEquals(UUID.fromString(validUUID).toString(), validUUID);
+
+    String invalidUUID = "invalid-uuid";
+    Assertions.assertThrows(IllegalArgumentException.class, () -> UUID.fromString(invalidUUID));
   }
 
   @Test
   public void whenUUIDIsValidatedUsingRegex_thenValidationSucceeds() {
-    Assertions.assertTrue(RegexUUIDValidator.isUUID("26929514-237c-11ed-861d-0242ac120002"));
+    Pattern UUID_REGEX =
+        Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+
+    Assertions.assertTrue(UUID_REGEX.matcher("26929514-237c-11ed-861d-0242ac120002").matches());
+
+    Assertions.assertFalse(UUID_REGEX.matcher("invalid-uuid").matches());
   }
 }
