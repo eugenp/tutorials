a3306da9be6ac53ece6da5842fcbf05a506ff9fb
andreimandris <andrei.mandris.ext@deliveryhero.com>
2022-09-20 23:57:14 +0300

    moved the UUIDValidatorTest.java to core-java-uuid module

--- core-java-modules/core-java-uuid/src/test/java/com/baeldung/uuid/UUIDValidatorTest.java
+++ core-java-modules/core-java-uuid/src/test/java/com/baeldung/uuid/UUIDValidatorTest.java
@@ -1,4 +1,4 @@
-package validator.uuid;
+package com.baeldung.uuid;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
--- java-util/README.md
+++
@@ -1,3 +0,0 @@
-## Java UTIL
-
-This module contains articles about the core classes in Java SE.
