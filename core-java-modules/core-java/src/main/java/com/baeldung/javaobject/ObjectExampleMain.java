e31d3e4f646fb67661ff6e2968a299ad6c06126e
Ritu Priya <priya.ritu89@gmail.com>
2022-09-22 19:57:04 +0530

    Deep and shallow copy code - Review Comments

--- core-java-modules/core-java/src/main/java/com/baeldung/javaobject/ObjectExampleMain.java
+++ core-java-modules/core-java/src/main/java/com/baeldung/javaobject/ObjectExampleMain.java
@@ -1,29 +1,27 @@
 package com.baeldung.javaobject;
 
-        public class ObjectExampleMain {
-            public static void main(String args[]){
+public class ObjectExampleMain {
+    public static void main(String args[]) {
 
-                ObjectExample example= new ObjectExample();
-                //Creating a new reference and a new instance of the class Object Example
-                ObjectExample deepCopyExample=new ObjectExample();
-                //Changing the value of name
-                deepCopyExample.name="Core Java";
+        ObjectExample example = new ObjectExample();
+        //Creating a new reference and a new instance of the class Object Example
+        ObjectExample deepCopyExample = new ObjectExample();
+        //Changing the value of name
+        deepCopyExample.name = "Core Java";
 
-                //The output is "Name: Java"
-                System.out.println("Name: "+example.name);
-                //The output is "Name: Core Java"
-                System.out.println("Name: "+deepCopyExample.name);
+        //The output is "Name: Java"
+        System.out.println("Name: " + example.name);
+        //The output is "Name: Core Java"
+        System.out.println("Name: " + deepCopyExample.name);
 
-
-
-                ObjectExample example2= new ObjectExample();
-                //Creating a new object with the same reference
-                ObjectExample shallowCopyExample=example2;
-                shallowCopyExample.name="Core Java";
-                //Output is "Name: Core Java"
-                System.out.println("Name: "+example2.name);
-                //Output is "Name: Core Java"
-                System.out.println("Name: "+shallowCopyExample.name);
-            }
-        }
+        ObjectExample example2 = new ObjectExample();
+        //Creating a new object with the same reference
+        ObjectExample shallowCopyExample = example2;
+        shallowCopyExample.name = "Core Java";
+        //Output is "Name: Core Java"
+        System.out.println("Name: " + example2.name);
+        //Output is "Name: Core Java"
+        System.out.println("Name: " + shallowCopyExample.name);
+    }
+}
 
--- core-java-modules/core-java/src/test/java/com/baeldung/javaobject/DeepShallowCopyTest.java
+++ core-java-modules/core-java/src/test/java/com/baeldung/javaobject/DeepShallowCopyTest.java
@@ -7,22 +7,23 @@ import org.junit.Test;
 
 public class DeepShallowCopyTest {
     @Test
-    public void deepCopyTest(){
+    public void givenObject_whenDeepCopied_thenNotEquals() {
         ObjectExample obj1 = new ObjectExample();
-        ObjectExample deepObj= new ObjectExample();
-        obj1.name="Advanced Java";
-        deepObj.name="Core Java";
+        ObjectExample deepObj = new ObjectExample();
+        obj1.name = "Advanced Java";
+        deepObj.name = "Core Java";
 
-        assertNotEquals(obj1.name,deepObj.name);
-        
+        assertNotEquals(obj1.name, deepObj.name);
+
     }
+
     @Test
-    public void shallowCopyTest(){
+    public void givenObject_whenShallowCopied_thenEquals() {
         ObjectExample obj1 = new ObjectExample();
-        ObjectExample shallowObj= obj1;
-        obj1.name="Advanced Java";
-        shallowObj.name="Core Java";
-        assertEquals(obj1.name,shallowObj.name);
+        ObjectExample shallowObj = obj1;
+        obj1.name = "Advanced Java";
+        shallowObj.name = "Core Java";
+        assertEquals(obj1.name, shallowObj.name);
 
     }
 
