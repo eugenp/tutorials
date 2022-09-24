399db473e0ea7695ccb0e56416dd4a21f5257aca
anuragkumawat <anuragkumawat7@gmail.com>
2022-09-24 14:49:27 +0530

    JAVA-14879 Update spring-security-web-boot-1 under spring-security-modules to remove usage of deprecated WebSecurityConfigurerAdapter

--- spring-security-modules/spring-security-web-boot-1/src/main/java/com/baeldung/roles/ip/config/SecurityConfig.java
+++ spring-security-modules/spring-security-web-boot-1/src/main/java/com/baeldung/roles/ip/config/SecurityConfig.java
@@ -1,33 +1,47 @@
 package com.baeldung.roles.ip.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
     @Autowired
     private CustomIpAuthenticationProvider authenticationProvider;
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-       auth.inMemoryAuthentication().withUser("john").password("{noop}123").authorities("ROLE_USER");
-     //   auth.authenticationProvider(authenticationProvider);
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("john")
+            .password("{noop}123")
+            .authorities("ROLE_USER")
+            .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/login").permitAll()
-//            .antMatchers("/foos/**").hasIpAddress("11.11.11.11")
-            .antMatchers("/foos/**").access("isAuthenticated() and hasIpAddress('11.11.11.11')")
-            .anyRequest().authenticated()
-            .and().formLogin().permitAll()
-            .and().csrf().disable();
+            .antMatchers("/login")
+            .permitAll()
+            // .antMatchers("/foos/**").hasIpAddress("11.11.11.11")
+            .antMatchers("/foos/**")
+            .access("isAuthenticated() and hasIpAddress('11.11.11.11')")
+            .anyRequest()
+            .authenticated()
+            .and()
+            .formLogin()
+            .permitAll()
+            .and()
+            .csrf()
+            .disable();
+        return http.build();
     }
 }
