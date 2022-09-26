86783aa29fd02f0e0c3c9f5e450003c1ae5b4ca5
achraftt <achraf.taitai1@gmail.com>
2022-09-26 21:02:06 +0200

    Port Scanning with Java

--- core-java-modules/core-java-networking-3/src/main/java/com/baeldung/portscanner/PortScanner.java
+++ core-java-modules/core-java-networking-3/src/main/java/com/baeldung/portscanner/PortScanner.java
@@ -43,7 +43,7 @@ public class PortScanner {
         try {
             executorService.awaitTermination(10, TimeUnit.MINUTES);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Thread.currentThread().interrupt();
         }
 
         List openPortList = new ArrayList<>();
