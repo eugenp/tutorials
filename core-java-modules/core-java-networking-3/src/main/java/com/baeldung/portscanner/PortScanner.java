50a855040d38cc856a4e1414bf21b7f49bac0639
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2022-09-30 14:45:08 +0200

    Update PortScanner.java

--- core-java-modules/core-java-networking-3/src/main/java/com/baeldung/portscanner/PortScanner.java
+++ core-java-modules/core-java-networking-3/src/main/java/com/baeldung/portscanner/PortScanner.java
@@ -31,7 +31,6 @@ public class PortScanner {
                 } catch (IOException e) {
                     System.err.println(e);
                 }
-
             });
         }
         executorService.shutdown();
