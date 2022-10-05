d3edffaab17f7a5238712a38cead1db9bd17e026
Lamsa <9578233+lamsa@users.noreply.github.com>
2022-10-05 22:49:04 +0700

    LNX-267 - PC Speaker "beep" in Linux

---
+++ linux-bash/melodies/README.md
@@ -0,0 +1,2 @@
+### Relevant Articles:
+- [PC Speaker "beep" in Linux](https://www.baeldung.com/linux/)
---
+++ linux-bash/melodies/src/main/bash/pachelbel-canon.sh
@@ -0,0 +1,79 @@
+#!/bin/sh
+# https://www.baeldung.com/linux/
+play -n synth \
+pl F4  pl A4  pl C5  pl F5  pl C4  pl G4  pl C5  pl E5  pl D4  pl F4  \
+pl A4  pl D5  pl A3  pl F4  pl A4  pl C5  pl F5  pl C4  pl G4  pl C5  \
+pl E5  pl D4  pl F4  pl A4  pl D5  pl A3  pl E4  pl A4  pl C5  pl Bb3 \
+pl D4  pl F4  pl Bb4 pl F3  pl C4  pl F4  pl A4  pl Bb3 pl D4  pl F4  \
+pl Bb4 pl C4  pl E4  pl G4  pl C5  pl F4  pl A4  pl C5  pl F5  pl C4  \
+pl G4  pl C5  pl E5  pl D4  pl F4  pl A4  pl D5  pl A3  pl E4  pl A4  \
+pl C5  pl Bb3 pl D4  pl F4  pl Bb4 pl F3  pl C4  pl F4  pl A4  pl Bb3 \
+pl D4  pl F4  pl Bb4 pl C4  pl E4  pl G4  pl C5 \
+pl A4  pl G4  pl F4  pl E4  pl D4  pl C4  pl D4  pl E4 \
+pl A4  pl C4  pl F4  pl C4  pl G4  pl G3  pl E4  pl G3  pl F4  pl A3  \
+pl D4  pl A3  pl E4  pl E3  pl C4  pl E3  pl D4  pl F3  pl Bb3 pl F3  \
+pl C4  pl F3  pl A3  pl F3  pl D4  pl F3  pl Bb  pl F3  pl E4  pl G3  \
+pl C4  pl G3  pl F5  pl E5  pl F5  pl D5  pl C5  pl Bb4 pl A4  pl G4  \
+pl F4  pl G4  pl A4  pl Bb4 pl C5  pl D5  pl E5  pl F5  pl Bb5 pl A5  \
+pl G5  pl Bb5 pl A5  pl G5  pl F5  pl E5  pl D5  pl C5  pl Bb4 pl A4  \
+pl G4  pl Bb4 pl A4  pl G4  pl F4  pl G4  pl A4  pl Bb4 pl C5  pl G4  \
+pl C5  pl Bb4 pl A4  pl F5  pl E5  pl D5  pl E5  pl D5  pl C5  pl A4  \
+pl Bb4 pl G4  pl A4  pl Bb4 pl C5  pl D5  pl E5  pl F5  pl D5  pl E5  \
+pl F5  pl G5  pl A5  pl Bb5 pl C6  pl D6 \
+pl C6  pl A5  pl Bb5 pl C6  pl A5  pl Bb5 pl C6  pl C5  pl D5  pl E5  \
+pl F5  pl G5  pl A5  pl Bb5 pl A5  pl F5  pl G5  pl A5  pl A4  pl Bb4 \
+pl C5  pl D5  pl C5  pl Bb4 pl C5  pl A4  pl Bb4 pl C5  pl Bb4 pl D5  \
+pl C5  pl Bb4 pl A4  pl G4  pl A4  pl G4  pl F4  pl G4  pl A4  pl Bb4 \
+pl C5  pl D5  pl Bb4 pl D5  pl C5  pl D5  pl E5  pl F5  pl C5  pl D5  \
+pl E5  pl F5  pl G5  pl A5  pl Bb5 pl C6  \
+pl A5  pl G5  pl F5  pl E5  pl D5  pl C5  pl D5  pl E5  \
+pl A5  pl C4  pl F5  pl C4  pl G5  pl G3  pl E5  pl G3  pl F5  pl A3  \
+pl D5  pl A3  pl E5  pl E3  pl C5  pl E3  pl D5  pl F3  pl Bb4 pl F3  \
+pl C5  pl F3  pl A4  pl F3  \
+pl D5  pl E5  pl F4  pl A4  pl C5  pl F5  pl C4  pl G4  pl C5  pl E5  \
+pl D4  pl F4  pl A4  pl D5  pl A3  pl E4  pl A4  pl C5  pl Bb3 pl D4  \
+pl F4  pl Bb4 pl F3  pl C4  pl F4  pl A4  pl Bb3 pl D4  pl F4  pl Bb4 \
+pl C4  pl E4  pl G4  pl C5  pl F4  pl A4  pl C5  pl F5  pl C4  pl G4  \
+pl C5  pl E5  pl D4  pl F4  pl A4  pl D5  pl A3  pl E4  pl A4  pl C5  \
+pl Bb3 pl D4  pl F4  pl Bb4 pl F3  pl C4  pl F4  pl A4  pl Bb3 pl D4  \
+pl F4  pl Bb4 pl C4  pl E4  pl D4  pl G4  pl C5  pl A4  \
+delay \
+0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.700 +0.700 +0.700 +0.700 +0.700 +0.700 +0.700 +0.700 \
++0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 \
++0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 \
++0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 \
++0.350 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.350 +0.175 +0.175 +0.350 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.350 +0.175 +0.175 +0.350 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.350 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.350 +0.175 +0.175 +0.350 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.350 +0.350 +0.350 +0.350 +0.350 +0.350 +0.350 +0.350 \
++0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 \
++0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 +0.350 +0.175 \
++0.350 +0.175 +0.350 +0.175 \
++0.700 +0.700 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 +0.175 \
++0.175 +0.175 +0.350 +0.175 +0.175 +0.175 +0.175 +1.400 \
+\
+remix - fade 0 76 .1 norm -1
+
