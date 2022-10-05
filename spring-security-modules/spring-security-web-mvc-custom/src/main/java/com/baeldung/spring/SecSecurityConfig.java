8dab1f7363b02711a41f5eae124dcb33ec72b815
anuragkumawat <anuragkumawat7@gmail.com>
2022-10-05 23:37:59 +0530

    JAVA-14876 Update spring-security-web-mvc-custom under spring-security-modules to remove usage of deprecated WebSecurityConfigurerAdapter (#12820)

--- spring-security-modules/spring-security-web-mvc-custom/src/main/java/com/baeldung/spring/SecSecurityConfig.java
+++ spring-security-modules/spring-security-web-mvc-custom/src/main/java/com/baeldung/spring/SecSecurityConfig.java
@@ -1,56 +1,69 @@
 package com.baeldung.spring;
 
-import com.baeldung.security.MySimpleUrlAuthenticationSuccessHandler;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import com.baeldung.security.MySimpleUrlAuthenticationSuccessHandler;
 
 @Configuration
 //@ImportResource({ "classpath:webSecurityConfig.xml" })
 @EnableWebSecurity
-public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecSecurityConfig {
 
-    public SecSecurityConfig() {
-        super();
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+            .build();
     }
 
-    @Bean("authenticationManager")
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-            return super.authenticationManagerBean();
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("user1")
+            .password("{noop}user1Pass")
+            .authorities("ROLE_USER")
+            .build();
+        UserDetails admin = User.withUsername("admin1")
+            .password("{noop}admin1Pass")
+            .authorities("ROLE_ADMIN")
+            .build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
-        // @formatter:off
-        http
-            .authorizeRequests()
-                .antMatchers("/anonymous*").anonymous()
-                .antMatchers("/login*").permitAll()
-                .anyRequest().authenticated()
-
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+            .antMatchers("/anonymous*")
+            .anonymous()
+            .antMatchers("/login*")
+            .permitAll()
+            .anyRequest()
+            .authenticated()
             .and()
             .formLogin()
-                .loginPage("/login.html")
-                .loginProcessingUrl("/login")
-                .successHandler(myAuthenticationSuccessHandler())
-                .failureUrl("/login.html?error=true")
-
+            .loginPage("/login.html")
+            .loginProcessingUrl("/login")
+            .successHandler(myAuthenticationSuccessHandler())
+            .failureUrl("/login.html?error=true")
             .and()
-                .logout().deleteCookies("JSESSIONID")
-
+            .logout()
+            .deleteCookies("JSESSIONID")
             .and()
-                .rememberMe().key("uniqueAndSecret").tokenValiditySeconds(86400)
-
+            .rememberMe()
+            .key("uniqueAndSecret")
+            .tokenValiditySeconds(86400)
             .and()
-                .csrf().disable()
-            ;
-        // @formatter:on
+            .csrf()
+            .disable();
+        return http.build();
     }
 
     @Bean
