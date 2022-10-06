5327a74548f65b29b0fcb5d92fe183ed2e0e337e
Abdul <abdul.wahab@monese.com>
2022-10-06 20:08:15 +0300

    BAEL-5696: Refactors spring caching TTL tutorial

--- spring-caching-2/src/main/java/com/baeldung/caching/ttl/CachingTTLApplication.java
+++ spring-caching-2/src/main/java/com/baeldung/caching/ttl/CachingTTLApplication.java
@@ -2,7 +2,6 @@ package com.baeldung.caching.ttl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
