471af803432534c4d700e232053085b63db03485
Carlos Chacin <cchacin@gmail.com>
2022-10-07 00:52:41 -0700

    article: Shallow Copy vs Deep Copy on Records
    Added examples and unit tests for the article
    related to shallow vs. deep copy.

--- core-java-modules/core-java-16/src/main/java/com/baeldung/features/record/deepcopy/Project.java
+++ core-java-modules/core-java-16/src/main/java/com/baeldung/features/record/deepcopy/Project.java
@@ -7,11 +7,8 @@ public record Project(List<Task> tasks) {
     public Project {
         tasks = List.copyOf(tasks);
     }
-<<<<<<< HEAD
-=======
 
     public Project copy() {
         return new Project(this.tasks());
     }
->>>>>>> 987d59b3ba (article: Shallow Copy vs Deep Copy on Records)
 }
--- core-java-modules/core-java-16/src/main/java/com/baeldung/features/record/shallowcopy/Project.java
+++ core-java-modules/core-java-16/src/main/java/com/baeldung/features/record/shallowcopy/Project.java
@@ -3,11 +3,8 @@ package com.baeldung.features.record.shallowcopy;
 import java.util.List;
 
 public record Project(List<Task> tasks) {
-<<<<<<< HEAD
-=======
 
     public Project copy() {
         return new Project(this.tasks());
     }
->>>>>>> 987d59b3ba (article: Shallow Copy vs Deep Copy on Records)
 }
