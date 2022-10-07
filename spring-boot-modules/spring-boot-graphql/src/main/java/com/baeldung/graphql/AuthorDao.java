fafadd5474c5b17b2462a0867ae5f1a515af94b1
Haroon Khan <haroonkhan79@gmail.com>
2022-10-07 18:21:41 +0100

    [JAVA-14663] Update Graphql code for Boot 2.7.x changes and move to new module (#12729)

---
+++ spring-boot-modules/spring-boot-graphql/src/main/java/com/baeldung/graphql/AuthorDao.java
@@ -0,0 +1,18 @@
+package com.baeldung.graphql;
+
+import java.util.List;
+
+public class AuthorDao {
+    private final List<Author> authors;
+
+    public AuthorDao(List<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Author getAuthor(String id) {
+        return authors.stream()
+          .filter(author -> id.equals(author.getId()))
+          .findFirst()
+          .orElseThrow(RuntimeException::new);
+    }
+}
