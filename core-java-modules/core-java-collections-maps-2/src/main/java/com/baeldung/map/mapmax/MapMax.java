5eab831bb568a895ce056a627079be30cda5e6a7
press0@gmail.com <press0@gmail.com>
2022-10-08 22:30:21 -0500

    .

--- core-java-modules/core-java-collections-maps-2/src/main/java/com/baeldung/map/mapmax/MapMax.java
+++ core-java-modules/core-java-collections-maps-2/src/main/java/com/baeldung/map/mapmax/MapMax.java
@@ -1,6 +1,7 @@
 package com.baeldung.map.mapmax;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -25,48 +26,48 @@ public class MapMax {
 
     public <K, V extends Comparable<V>> V maxUsingCollectionsMax(Map<K, V> map) {
 
-        Entry<K, V> maxEntry = Collections.max(map.entrySet(), Entry.comparingByValue());
-
+        Entry<K, V> maxEntry = Collections.max(map.entrySet(), new Comparator<Entry<K, V>>() {
+            public int compare(Entry<K, V> e1, Entry<K, V> e2) {
+                return e1.getValue()
+                        .compareTo(e2.getValue());
+            }
+        });
         return maxEntry.getValue();
     }
 
     public <K, V extends Comparable<V>> V maxUsingCollectionsMaxAndLambda(Map<K, V> map) {
-
-        Entry<K, V> maxEntry = Collections.max(map.entrySet(), Entry.comparingByValue());
-
+        Entry<K, V> maxEntry = Collections.max(map.entrySet(), (Entry<K, V> e1, Entry<K, V> e2) -> e1.getValue()
+                .compareTo(e2.getValue()));
         return maxEntry.getValue();
     }
 
     public <K, V extends Comparable<V>> V maxUsingCollectionsMaxAndMethodReference(Map<K, V> map) {
 
-        Entry<K, V> maxEntry = Collections.max(map.entrySet(), Entry.comparingByValue());
+        Entry<K, V> maxEntry = Collections.max(map.entrySet(), Comparator.comparing(Map.Entry::getValue));
 
         return maxEntry.getValue();
     }
 
     public <K, V extends Comparable<V>> V maxUsingStreamAndLambda(Map<K, V> map) {
-
         Optional<Entry<K, V>> maxEntry = map.entrySet()
-            .stream()
-            .max(Entry.comparingByValue());
+                .stream()
+                .max((Entry<K, V> e1, Entry<K, V> e2) -> e1.getValue()
+                .compareTo(e2.getValue()));
 
-        return maxEntry.get()
-            .getValue();
+        return maxEntry.get().getValue();
     }
 
     public <K, V extends Comparable<V>> V maxUsingStreamAndMethodReference(Map<K, V> map) {
-
         Optional<Entry<K, V>> maxEntry = map.entrySet()
-            .stream()
-            .max(Entry.comparingByValue());
-
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue));
         return maxEntry.get()
-            .getValue();
+                .getValue();
     }
 
     public static void main(String[] args) {
 
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         map.put(1, 3);
         map.put(2, 4);
