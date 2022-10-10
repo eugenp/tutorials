02f8f60d53f435c3c1d6e8bf6b5551468fbf836b
anuragkumawat <anuragkumawat7@gmail.com>
2022-10-10 23:24:23 +0530

    JAVA-14677 Update spring-boot-keycloak module under spring-boot-modules to remove usage of deprecated WebSecurityConfigurerAdapter (#12842)

--- spring-boot-modules/spring-boot-keycloak/src/main/java/com/baeldung/keycloak/SecurityConfig.java
+++ spring-boot-modules/spring-boot-keycloak/src/main/java/com/baeldung/keycloak/SecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig extends WebSecurityConfigurerAdapter {
+class SecurityConfig {
 
     private final KeycloakLogoutHandler keycloakLogoutHandler;
 
@@ -24,18 +24,18 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-          .antMatchers("/customers*", "/users*")
-          .hasRole("USER")
-          .anyRequest()
-          .permitAll();
+            .antMatchers("/customers*", "/users*")
+            .hasRole("USER")
+            .anyRequest()
+            .permitAll();
         http.oauth2Login()
-           .and()
-           .logout()
-           .addLogoutHandler(keycloakLogoutHandler)
-           .logoutSuccessUrl("/");
+            .and()
+            .logout()
+            .addLogoutHandler(keycloakLogoutHandler)
+            .logoutSuccessUrl("/");
+        return http.build();
     }
-
 }
