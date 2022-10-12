19db1498e87c701246c6662b08418b5b2b280e81
Philippe <phil@lighthouse.com.br>
2022-10-13 01:40:42 +0200

    [BAEL-5698] Article code

--- rabbitmq/src/test/java/com/baeldung/benchmark/SingleConnectionPublisherLiveTest.java
+++ rabbitmq/src/test/java/com/baeldung/benchmark/SingleConnectionPublisherLiveTest.java
@@ -1,8 +1,6 @@
 package com.baeldung.benchmark;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.List;
+import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
 
@@ -10,8 +8,8 @@ class SingleConnectionPublisherLiveTest {
 
     @Test
     void whenSingleChannel_thenRunBenchmark() throws Exception {
-        // host, workerCount, iterations, payloadSize
-        List.of(1,5,10,20,50,100,150).stream()
+        // host, workerCount, iterations, payloadSize        
+        Arrays.asList(1,5,10,20,50,100,150).stream()
           .forEach(workers -> {
               SingleConnectionPublisher.main(new String[]{"192.168.99.100", Integer.toString(workers), "1000", "4096"});              
           });
