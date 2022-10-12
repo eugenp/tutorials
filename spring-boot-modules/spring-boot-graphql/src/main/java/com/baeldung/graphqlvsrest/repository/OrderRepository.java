b622c4241407a404d46b79ce923067fcb0b2333a
Haroon Khan <haroonkhan79@gmail.com>
2022-10-12 16:38:39 +0100

    [JAVA-15353] Update code for GraphQL vs REST article (#12849)

--- spring-boot-modules/spring-boot-graphql/src/main/java/com/baeldung/graphqlvsrest/repository/OrderRepository.java
+++ spring-boot-modules/spring-boot-graphql/src/main/java/com/baeldung/graphqlvsrest/repository/OrderRepository.java
@@ -1,8 +1,6 @@
 package com.baeldung.graphqlvsrest.repository;
 
 import com.baeldung.graphqlvsrest.entity.Order;
-import com.baeldung.graphqlvsrest.entity.Product;
-import com.baeldung.graphqlvsrest.model.ProductModel;
 
 import java.util.List;
 
