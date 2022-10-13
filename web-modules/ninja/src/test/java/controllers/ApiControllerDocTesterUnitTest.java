34e665f2ebbcdab9d61d012daed8125b10f66d66
panagiotiskakos <panagiotis.kakos@libra-is.com>
2022-10-13 07:45:10 +0300

    [JAVA-13966] Fixed naming convention for pmd

--- web-modules/ninja/src/test/java/controllers/ApiControllerDocTesterUnitTest.java
+++ web-modules/ninja/src/test/java/controllers/ApiControllerDocTesterUnitTest.java
@@ -7,7 +7,7 @@ import org.doctester.testbrowser.Response;
 import org.junit.Test;
 import ninja.NinjaDocTester;
 
-public class ApiControllerDocTesterTest extends NinjaDocTester {
+public class ApiControllerDocTesterUnitTest extends NinjaDocTester {
     
     String URL_INDEX = "/";
     String URL_HELLO = "/hello";
