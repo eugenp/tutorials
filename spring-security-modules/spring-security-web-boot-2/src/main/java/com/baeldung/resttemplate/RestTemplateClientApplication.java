1f5585cf71f5d2b208e90a773f7c752bf7fb081a
Ajay Veluru <ajayveluru@gmail.com>
2022-10-15 23:12:05 +0530

    Customized RestTemplate to access HTTPS Rest Service

---
+++ spring-security-modules/spring-security-web-boot-2/src/main/java/com/baeldung/resttemplate/RestTemplateClientApplication.java
@@ -0,0 +1,15 @@
+package com.baeldung.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource("classpath:application-defaults.properties")
+public class RestTemplateClientApplication {
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication(RestTemplateClientApplication.class);
+        application.setAdditionalProfiles("ssl-client");
+        application.run(args);
+	}
+}
