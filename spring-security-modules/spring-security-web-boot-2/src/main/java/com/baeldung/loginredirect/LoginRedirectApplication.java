343fe3b89ab2e2f3d2435bc65293e27da0fd1e88
Ajay Veluru <ajayveluru@gmail.com>
2022-10-15 23:21:52 +0530

    Customized RestTemplate to access HTTPS Rest Service

--- spring-security-modules/spring-security-web-boot-2/src/main/java/com/baeldung/loginredirect/LoginRedirectApplication.java
+++ spring-security-modules/spring-security-web-boot-2/src/main/java/com/baeldung/loginredirect/LoginRedirectApplication.java
@@ -2,9 +2,12 @@ package com.baeldung.loginredirect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class
+})
 @ImportResource({"classpath*:spring-security-login-redirect.xml"})
 class LoginRedirectApplication {
     public static void main(String[] args) {
--- spring-security-modules/spring-security-web-boot-2/src/main/java/com/baeldung/ssl/SecurityConfig.java
+++ spring-security-modules/spring-security-web-boot-2/src/main/java/com/baeldung/ssl/SecurityConfig.java
@@ -1,11 +1,12 @@
 package com.baeldung.ssl;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
 
     @Bean
