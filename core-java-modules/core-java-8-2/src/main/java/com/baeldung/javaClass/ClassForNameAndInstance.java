8c35d38c7bebaacf818417d6a9c4a01ce93aff53
Ritzy-NCode <priya.ritu89@gmail.com>
2022-10-16 23:04:48 +0530

    BAEL-5726: Difference between Class.forName() and Class.forName().newInstance()

---
+++ core-java-modules/core-java-8-2/src/main/java/com/baeldung/javaClass/ClassForNameAndInstance.java
@@ -0,0 +1,12 @@
+package com.baeldung.javaClass;
+
+public class ClassForNameAndInstance {
+    
+    public Class createClassForName(String className) throws ClassNotFoundException {
+        return Class.forName(className);
+    }
+    
+    public Object createNewInstance(String className) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        return Class.forName(className).newInstance();
+    }
+}
