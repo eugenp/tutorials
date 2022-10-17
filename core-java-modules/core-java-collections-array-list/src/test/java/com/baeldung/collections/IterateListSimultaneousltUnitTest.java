298228bb4fadadd0f8b8a136a8c2b5a472130e14
Prabhash <kumarprabhash1995@gmail.com>
2022-10-17 23:00:24 +0200

    BAEL-5801 - adding iterate list unit test

---
+++ core-java-modules/core-java-collections-array-list/src/test/java/com/baeldung/collections/IterateListSimultaneousltUnitTest.java
@@ -0,0 +1,74 @@
+package com.baeldung.collections;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.util.StreamUtils;
+
+public class IterateListSimultaneousltUnitTest {
+
+    List<String> names = List.of("John", "Nik", "Tom");
+    List<Integer> numbers = List.of(1, 2, 3);
+
+    @Test
+    public void givenTwoLists_whenProcessByZipping_thenProcessedSimultaneously() {
+        Stream<String> nameStream = names.stream();
+        Stream<Integer> numberStream = numbers.stream();
+        List<String> processedList = StreamUtils.zip(numberStream, nameStream,
+            (numStr, nameStr) -> String.format("[Using Zip] Processing ArrayList Simultaneously : %d, %s", numStr, nameStr))
+            .collect(Collectors.toList());
+        Assertions.assertTrue(processedList.containsAll(
+          List.of("[Using Zip] Processing ArrayList Simultaneously : 1, John",
+                "[Using Zip] Processing ArrayList Simultaneously : 2, Nik",
+                "[Using Zip] Processing ArrayList Simultaneously : 3, Tom")));
+    }
+
+    @Test
+    public void givenTwoLists_whenIterateUsingIterator_thenProcessedSimultaneously() {
+        Iterator nameIterator = names.iterator();
+        Iterator numberIterator = numbers.iterator();
+        List<String> processedList = new ArrayList<>();
+        while (nameIterator.hasNext() && numberIterator.hasNext()) {
+            String processedData = String.format("[Using Iterator] Processing ArrayList Simultaneously : %d, %s", numberIterator.next(), nameIterator.next());
+            processedList.add(processedData);
+        }
+        Assertions.assertTrue(processedList.containsAll(
+          List.of("[Using Iterator] Processing ArrayList Simultaneously : 1, John",
+                "[Using Iterator] Processing ArrayList Simultaneously : 2, Nik",
+                "[Using Iterator] Processing ArrayList Simultaneously : 3, Tom")));
+    }
+
+    @Test
+    public void givenTwoLists_whenIterateUsingLoop_thenProcessedSimultaneously() {
+        List<String> processedList = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++) {
+            String processedData = String.format("[Using Loop] Processing ArrayList Simultaneously : %d, %s", numbers.get(i), names.get(i));
+            processedList.add(processedData);
+        }
+        Assertions.assertTrue(processedList.containsAll(
+          List.of("[Using Loop] Processing ArrayList Simultaneously : 1, John",
+                "[Using Loop] Processing ArrayList Simultaneously : 2, Nik",
+                "[Using Loop] Processing ArrayList Simultaneously : 3, Tom")));
+    }
+
+    @Test
+    public void givenRecordList_whenIteratre_thenProcessedSimultaneously() {
+        List<EncapData> list = List.of(new EncapData("John", 1),
+                                new EncapData("Nik", 2),
+                                new EncapData("Tom", 3));
+        List<String> processedList = list.stream().map(l -> String.format("[Using Encapsulated Record] Processing ArrayList Simultaneously : %d, %s", l.number(), l.name()))
+          .collect(Collectors.toList());
+        Assertions.assertTrue(processedList.containsAll(
+          List.of("[Using Encapsulated Record] Processing ArrayList Simultaneously : 1, John",
+            "[Using Encapsulated Record] Processing ArrayList Simultaneously : 2, Nik",
+            "[Using Encapsulated Record] Processing ArrayList Simultaneously : 3, Tom")));
+    }
+
+}
+
+record EncapData (String name, int number) {}
