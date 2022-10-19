eb17d0e2edecca9ce57874870041aeac877b28a9
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2022-10-19 19:47:05 +0100

    [JAVA-13966] Upgraded h2 database to 2.1.214 + clean up (#12711)
    * [JAVA-13966] Upgraded h2 database to 2.1.214
    
    * [JAVA-13966] fixed tests for libraries-apache-commons
    
    * [JAVA-13966] fixed tests
    
    * [JAVA-13966] fixed tests
    
    * [JAVA-13966] fixed tests JAVA-JPA MODULE
    
    * [JAVA-13966] Made ninja submodule part of web-modules(parent)
    
    * [JAVA-13966] Fixed naming convention for pmd
    
    * [JAVA-13966] Moved tests of ninja-module to ManualTest category
    
    * [JAVA-13966] Fix tests
    
    Co-authored-by: panagiotiskakos <panagiotis.kakos@libra-is.com>

--- web-modules/ninja/src/test/java/controllers/ApiControllerMockManualTest.java
+++ web-modules/ninja/src/test/java/controllers/ApiControllerMockManualTest.java
@@ -10,7 +10,7 @@ import ninja.Result;
 import services.UserService;
 
 @RunWith(NinjaRunner.class)
-public class ApiControllerMockUnitTest {
+public class ApiControllerMockManualTest {
 
     @Inject private UserService userService;
 
