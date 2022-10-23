aa5563509ede10ee7245e6b573d81e00c5b4cde2
anuragkumawat <anuragkumawat7@gmail.com>
2022-10-23 14:39:06 +0530

    JAVA-14887 Update spring-security-cognito module under spring-security-modules to remove usage of deprecated WebSecurityConfigurerAdapter

--- spring-security-modules/spring-security-cognito/src/main/java/com/baeldung/cognito/SecurityConfiguration.java
+++ spring-security-modules/spring-security-cognito/src/main/java/com/baeldung/cognito/SecurityConfiguration.java
@@ -1,14 +1,15 @@
 package com.baeldung.cognito;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf()
             .and()
             .authorizeRequests(authz -> authz.mvcMatchers("/")
@@ -19,5 +20,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
             .and()
             .logout()
             .logoutSuccessUrl("/");
+        return http.build();
     }
 }
