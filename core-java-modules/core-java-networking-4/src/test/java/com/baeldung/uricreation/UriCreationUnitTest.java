e920b6f819efc43d319c0fc0d57b194219427883
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2022-10-27 00:37:01 +0200

    [URI-Creation] Difference Between URI.create() and new URI()

---
+++ core-java-modules/core-java-networking-4/src/test/java/com/baeldung/uricreation/UriCreationUnitTest.java
@@ -0,0 +1,31 @@
+package com.baeldung.uricreation;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UriCreationUnitTest {
+    @Test
+    void givenInvalidUriString_whenUsingConstructor_shouldGetExpectedResult() {
+        assertThrows(URISyntaxException.class, () -> new URI("I am an invalid URI string."));
+
+        try {
+            URI myUri = new URI("https://www.baeldung.com/articles");
+            assertNotNull(myUri);
+        } catch (URISyntaxException e) {
+            fail();
+        }
+
+    }
+
+    @Test
+    void givenInvalidUriString_whenUsingCreateMethod_shouldGetExpectedResult() {
+        assertThrows(IllegalArgumentException.class, () -> URI.create("I am an invalid URI string."));
+
+        URI myUri = URI.create("https://www.baeldung.com/articles");
+        assertNotNull(myUri);
+    }
+}
