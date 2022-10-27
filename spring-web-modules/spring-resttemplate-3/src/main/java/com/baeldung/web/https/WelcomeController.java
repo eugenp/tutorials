049a155a4f7741babb5f3d5a829dd7ec815a8940
ajayveluru <ajayveluru@gmail.com>
2022-10-27 09:00:19 +0530

    Update WelcomeController.java

--- spring-web-modules/spring-resttemplate-3/src/main/java/com/baeldung/web/https/WelcomeController.java
+++ spring-web-modules/spring-resttemplate-3/src/main/java/com/baeldung/web/https/WelcomeController.java
@@ -5,9 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-
-	@GetMapping(value = "/welcome")
-	public String welcome() {
-		return "Welcome To Secured REST Service";
-	}
+    @GetMapping(value = "/welcome")
+    public String welcome() {
+        return "Welcome To Secured REST Service";
+    }
 }
