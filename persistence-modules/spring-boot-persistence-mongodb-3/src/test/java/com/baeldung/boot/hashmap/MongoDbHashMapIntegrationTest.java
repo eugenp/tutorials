5b7ae6dd282678e0c6152a2d903901eebe5a27ee
Ulisses Lima <ulisses@dvlcube.com>
2022-10-27 01:07:33 -0300

    bael-5848 - tests

--- persistence-modules/spring-boot-persistence-mongodb-3/src/test/java/com/baeldung/boot/hashmap/MongoDbHashMapIntegrationTest.java
+++ persistence-modules/spring-boot-persistence-mongodb-3/src/test/java/com/baeldung/boot/hashmap/MongoDbHashMapIntegrationTest.java
@@ -1,11 +1,16 @@
 package com.baeldung.boot.hashmap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.bson.Document;
 import org.junit.jupiter.api.BeforeAll;
@@ -23,54 +28,93 @@ import com.mongodb.BasicDBObject;
 @TestPropertySource("/embedded.properties")
 class MongoDbHashMapIntegrationTest {
 
-    private static Map<String, Object> map = new HashMap<>();
-    private static Collection<Map<String, Object>> mapList = new ArrayList<>();
+    private static final Map<String, Object> MAP = new HashMap<>();
+    private static final Set<Map<String, Object>> MAP_SET = new HashSet<>();
 
     @Autowired
     private MongoTemplate mongo;
 
+    private void assertHasMongoId(Map<String, Object> saved) {
+        assertNotNull(saved);
+        assertNotNull(saved.get("_id"));
+    }
+
     @BeforeAll
     static void init() {
-        map.put("prop1", "val");
-        map.put("prop2", 2);
+        MAP.put("name", "Document A");
+        MAP.put("number", 2);
+        MAP.put("dynamic", true);
 
-        Map<String, Object> map2 = new HashMap<>();
-        map2.put("prop1", "val2");
-        map2.put("prop2", 22);
+        Map<String, Object> otherMap = new HashMap<>();
+        otherMap.put("name", "Other Document");
+        otherMap.put("number", 22);
 
-        mapList.add(map);
-        mapList.add(map2);
+        MAP_SET.add(MAP);
+        MAP_SET.add(otherMap);
     }
 
     @Test
     void whenUsingMap_thenInsertSucceeds() {
-        Map<String, Object> insert = mongo.insert(map, "map-collection");
+        Map<String, Object> saved = mongo.insert(MAP, "map-collection");
 
-        assertNotNull(insert);
+        assertHasMongoId(saved);
+    }
+
+    @Test
+    void whenMapSet_thenInsertSucceeds() {
+        Collection<Map<String, Object>> saved = mongo.insert(MAP_SET, "map-set");
+
+        saved.forEach(this::assertHasMongoId);
+        assertEquals(2, saved.size());
     }
 
     @Test
     void givenMap_whenDocumentConstructed_thenInsertSucceeds() {
-        Document document = new Document(map);
+        Document document = new Document(MAP);
 
-        Document insert = mongo.insert(document, "doc-collection");
+        Document saved = mongo.insert(document, "doc-collection");
 
-        assertNotNull(insert);
+        assertHasMongoId(saved);
     }
 
     @Test
     void givenMap_whenBasicDbObjectConstructed_thenInsertSucceeds() {
-        BasicDBObject dbObject = new BasicDBObject(map);
+        BasicDBObject dbObject = new BasicDBObject(MAP);
 
-        BasicDBObject insert = mongo.insert(dbObject, "db-collection");
+        BasicDBObject saved = mongo.insert(dbObject, "db-collection");
 
-        assertNotNull(insert);
+        assertHasMongoId(saved);
     }
 
     @Test
-    void whenMapCollection_thenInsertSucceeds() {
-        Collection<Map<String, Object>> insert = mongo.insert(mapList, "collection-map");
+    void givenObjectList_whenDocumentSetConstructed_thenInsertSucceeds() {
+        Map<String, List<Object>> input = new HashMap<>();
+        List<Object> listOne = new ArrayList<>();
+        listOne.add("Doc A");
+        listOne.add(1);
 
-        assertNotNull(insert);
+        List<Object> listTwo = new ArrayList<>();
+        listTwo.add("Doc B");
+        listTwo.add(2);
+
+        input.put("a", listOne);
+        input.put("b", listTwo);
+
+        Set<Document> docs = input.entrySet()
+            .stream()
+            .collect(HashSet<Document>::new, (set, entry) -> {
+                Document document = new Document();
+
+                document.put("_id", entry.getKey());
+                Iterator<Object> iterator = entry.getValue()
+                    .iterator();
+                document.put("name", iterator.next());
+                document.put("number", iterator.next());
+
+                set.add(document);
+            }, Set::addAll);
+
+        Collection<Document> saved = mongo.insert(docs, "custom-set");
+        saved.forEach(this::assertHasMongoId);
     }
 }
