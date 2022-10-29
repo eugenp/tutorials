7686ea3d086e293855fca510ddd08c0320d5c111
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2022-10-29 23:04:55 +0200

    [URI-Creation] split test methods for invalid & valid inputs

--- core-java-modules/core-java-networking-4/src/test/java/com/baeldung/uricreation/UriCreationUnitTest.java
+++ core-java-modules/core-java-networking-4/src/test/java/com/baeldung/uricreation/UriCreationUnitTest.java
@@ -9,23 +9,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class UriCreationUnitTest {
     @Test
-    void givenInvalidUriString_whenUsingConstructor_shouldGetExpectedResult() {
-        assertThrows(URISyntaxException.class, () -> new URI("I am an invalid URI string."));
-
+    void givenValidUriString_whenUsingConstructor_shouldGetExpectedResult() {
         try {
             URI myUri = new URI("https://www.baeldung.com/articles");
             assertNotNull(myUri);
         } catch (URISyntaxException e) {
             fail();
         }
+    }
 
+    @Test
+    void givenInvalidUriString_whenUsingConstructor_shouldGetExpectedResult() {
+        assertThrows(URISyntaxException.class, () -> new URI("I am an invalid URI string."));
+    }
+
+    @Test
+    void givenValidUriString_whenUsingCreateMethod_shouldGetExpectedResult() {
+        URI myUri = URI.create("https://www.baeldung.com/articles");
+        assertNotNull(myUri);
     }
 
     @Test
     void givenInvalidUriString_whenUsingCreateMethod_shouldGetExpectedResult() {
         assertThrows(IllegalArgumentException.class, () -> URI.create("I am an invalid URI string."));
-
-        URI myUri = URI.create("https://www.baeldung.com/articles");
-        assertNotNull(myUri);
     }
 }
