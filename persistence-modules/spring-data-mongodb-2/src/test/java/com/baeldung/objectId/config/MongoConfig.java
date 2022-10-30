b982c25bcde0bec70f76370da73cff85e3c70c79
Łukasz Włodkowski <lukaswlodkowski@gmail.com>
2022-10-30 16:54:15 +0100

    [BAEL-5355] try different Mongo configuration, previous one failed on Jenkins.

--- persistence-modules/spring-data-mongodb-2/src/test/java/com/baeldung/objectId/config/MongoConfig.java
+++ persistence-modules/spring-data-mongodb-2/src/test/java/com/baeldung/objectId/config/MongoConfig.java
@@ -1,17 +1,28 @@
 package com.baeldung.objectId.config;
 
+import org.bson.UuidRepresentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
 @Configuration
 public class MongoConfig {
     @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(MongoClients.create(MongoClientSettings.builder()
-          .build()), "databaseName");
+    public MongoClient mongo() throws Exception {
+        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
+        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+          .uuidRepresentation(UuidRepresentation.STANDARD)
+          .applyConnectionString(connectionString).build();
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "databaseName");
     }
 }
