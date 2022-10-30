7694311d49fce44eeff8b10ee47a930d36b52bff
Łukasz Włodkowski <lukaswlodkowski@gmail.com>
2022-10-30 17:07:34 +0100

    [BAEL-5355] try different Mongo configuration, previous one failed on Jenkins.

--- persistence-modules/spring-data-mongodb-2/src/test/java/com/baeldung/objectId/config/MongoConfig.java
+++ persistence-modules/spring-data-mongodb-2/src/test/java/com/baeldung/objectId/config/MongoConfig.java
@@ -4,25 +4,39 @@ import org.bson.UuidRepresentation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.util.SocketUtils;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
+import de.flapdoodle.embed.mongo.MongodExecutable;
+import de.flapdoodle.embed.mongo.MongodStarter;
+import de.flapdoodle.embed.mongo.config.ImmutableMongodConfig;
+import de.flapdoodle.embed.mongo.config.MongodConfig;
+import de.flapdoodle.embed.mongo.config.Net;
+import de.flapdoodle.embed.mongo.distribution.Version;
+import de.flapdoodle.embed.process.runtime.Network;
+
 @Configuration
 public class MongoConfig {
-    @Bean
-    public MongoClient mongo() throws Exception {
-        final ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/test");
-        final MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-          .uuidRepresentation(UuidRepresentation.STANDARD)
-          .applyConnectionString(connectionString).build();
-        return MongoClients.create(mongoClientSettings);
-    }
+    private static final String CONNECTION_STRING = "mongodb://%s:%d";
+    private static final String HOST = "localhost";
 
     @Bean
     public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "test");
+        int randomPort = SocketUtils.findAvailableTcpPort();
+
+        ImmutableMongodConfig mongoDbConfig = MongodConfig.builder()
+          .version(Version.Main.PRODUCTION)
+          .net(new Net(HOST, randomPort, Network.localhostIsIPv6()))
+          .build();
+
+        MongodStarter starter = MongodStarter.getDefaultInstance();
+        MongodExecutable mongodExecutable = starter.prepare(mongoDbConfig);
+        mongodExecutable.start();
+
+        return new MongoTemplate(MongoClients.create(String.format(CONNECTION_STRING, HOST, randomPort)), "mongo_auth");
     }
 }
