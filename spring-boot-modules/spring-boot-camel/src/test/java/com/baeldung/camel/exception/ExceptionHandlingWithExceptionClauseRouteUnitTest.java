d0dd334510fa1dcf1edab499c5777296aae9c817
Jonathan Cook <jonathan.cook@erars.plus.com>
2022-11-03 17:40:26 +0100

    BAEL-5236 - Apache Camel Exception Handling (#12973)
    * BAEL-4706 - Spring Boot with Spring Batch
    
    * BAEL-3948 - Fix test(s) in spring-batch which leaves repository.sqlite
    changed
    
    * BAEL-4736 - Convert JSONArray to List of Object using camel-jackson
    
    * BAEL-4756 - Mockito MockSettings
    
    * BAEL-4756 - Mockito MockSettings - fix spelling
    
    * BAEL-2674 - Upgrade the Okhttp article
    
    * BAEL-4204 - Adding Interceptors in OkHTTP
    
    * BAEL-4836 - Mocking Static Methods with Mockito
    
    * BAEL-4205 - A Guide to Events in OkHTTP
    
    * BAEL-5408 - Update Camel version in spring-boot-camel module
    
    * BAEL-5234 - Apache Camel Routes Testing in Spring Boot
    
    * BAEL-5234 - Apache Camel Routes Testing in Spring Boot
    
    * BAEL-5237 - Apache Camel Conditional Routing
    
    * BAEL-5236 - Apache Camel Exception Handling
    
    Co-authored-by: Jonathan Cook <jcook@sciops.esa.int>

---
+++ spring-boot-modules/spring-boot-camel/src/test/java/com/baeldung/camel/exception/ExceptionHandlingWithExceptionClauseRouteUnitTest.java
@@ -0,0 +1,30 @@
+package com.baeldung.camel.exception;
+
+import org.apache.camel.EndpointInject;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.spring.junit5.CamelSpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@CamelSpringBootTest
+class ExceptionHandlingWithExceptionClauseRouteUnitTest {
+
+    @Autowired
+    private ProducerTemplate template;
+    
+    @EndpointInject("mock:handled")
+    private MockEndpoint mock;
+    
+    @Test
+    void whenSendHeaders_thenExceptionRaisedAndHandledSuccessfully() throws Exception {
+        mock.expectedMessageCount(1);
+        
+        template.sendBodyAndHeader("direct:start-exception-clause", null, "fruit", "Banana");
+        
+        mock.assertIsSatisfied();
+    }
+
+}
