14e943102bc2ad22a7186624c6f90dbca33f8469
luca <lucacambi77@gmail.com>
2022-11-05 15:04:41 +0100

    fix: pmd violation

--- spring-security-modules/spring-security-web-boot-4/src/test/java/com/baeldung/enablemethodsecurity/EnableMethodSecurityIntegrationTest.java
+++ spring-security-modules/spring-security-web-boot-4/src/test/java/com/baeldung/enablemethodsecurity/EnableMethodSecurityIntegrationTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(classes = EnableMethodSecurityApplication.class)
-public class EnableMethodSecurityTest {
+public class EnableMethodSecurityIntegrationTest {
     @Autowired
     private WebApplicationContext context;
 
