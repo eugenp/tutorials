f39a1036d6cdb57f138867c460deba96096b9a78
Abhinav Pandey <abhinavpandey312@gmail.com>
2022-11-07 10:17:03 +0530

    BAEL-5436 - Uncommenting controller

--- spring-web-modules/spring-web-url/src/main/java/com/baeldung/rootmapping/controller/RootController.java
+++ spring-web-modules/spring-web-url/src/main/java/com/baeldung/rootmapping/controller/RootController.java
@@ -3,7 +3,7 @@ package com.baeldung.rootmapping.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-//@Controller
+@Controller
 public class RootController {
     @GetMapping("/")
     public String index() {
