80f3b095c4101588916d74ab833de862c545dac9
xustive <xustive@gmail.com>
2022-11-09 22:58:38 +0800

    Use Nonnull annotation

--- core-java-modules/core-java-collections-4/src/main/java/com/baeldung/collections/sorting/multiple/CheckFieldsOneByOne.java
+++ core-java-modules/core-java-collections-4/src/main/java/com/baeldung/collections/sorting/multiple/CheckFieldsOneByOne.java
@@ -6,12 +6,7 @@ import java.util.Comparator;
 public class CheckFieldsOneByOne implements Comparator<Person> {
     @Override
     public int compare(Person o1, Person o2) {
-        int nameCompare;
-        if(o1.getName() == null) {
-            nameCompare = o2.getName() == null ? 0 : -1;
-        } else {
-            nameCompare = o2.getName() == null ? 1 : o1.getName().compareTo(o2.getName());
-        }
+        int nameCompare = o1.getName().compareTo(o2.getName());
         if(nameCompare != 0) {
             return nameCompare;
         }
