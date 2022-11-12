c5cd44ece74f547baca48dda7f3fbd194c81160b
António Raposo <56557575+Antonio-Raposo@users.noreply.github.com>
2022-11-12 23:53:43 +0000

    New simpler version

--- core-java-modules/core-java-clone/src/main/java/com/baeldung/clone/ValueObject.java
+++ core-java-modules/core-java-clone/src/main/java/com/baeldung/clone/ValueObject.java
@@ -1,5 +1,7 @@
 package com.baeldung.clone;
 
+import java.util.Objects;
+
 public class ValueObject {
 
     private String value;
@@ -14,6 +16,18 @@ public class ValueObject {
 
     public void setValue(String value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ValueObject other = (ValueObject) obj;
+        return Objects.equals(value, other.value);
     }
 
 }
--- core-java-modules/core-java-clone/src/test/java/com/baeldung/clone/DeepCopyUnitTest.java
+++ core-java-modules/core-java-clone/src/test/java/com/baeldung/clone/DeepCopyUnitTest.java
@@ -1,63 +1,35 @@
 package com.baeldung.clone;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.*;
-
-import java.util.ArrayList;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.Test;
 
 public class DeepCopyUnitTest {
 
     @Test
-    public void whenDeepCopy_thenListsAreEqual() {
-        ValueObject object1 = new ValueObject("first");
-        ValueObject object2 = new ValueObject("second");
-        ValueObject object3 = new ValueObject("third");
+    public void whenDeepCopy_thenObjectsAreEqualAndNotTheSame() {
+        ValueObject original = new ValueObject("original");
+        ValueObject shallow = new ValueObject(original.getValue());
 
-        ArrayList<ValueObject> original = new ArrayList<>();
-        original.add(object1);
-        original.add(object2);
-        original.add(object3);
-
-        ArrayList<ValueObject> deep = new ArrayList<>();
-        for (int index = 0; index < original.size(); index++) {
-            deep.add(new ValueObject(original.get(index).getValue()));
-        }
-
-        assertEquals(original.size(), deep.size());
-        assertEquals(original.get(0).getValue(), deep.get(0).getValue());
-        assertEquals(original.get(1).getValue(), deep.get(1).getValue());
-        assertEquals(original.get(2).getValue(), deep.get(2).getValue());
-        assertNotSame(original.get(0), deep.get(0));
-        assertNotSame(original.get(1), deep.get(1));
-        assertNotSame(original.get(2), deep.get(2));
+        assertEquals(original, shallow);
+        assertNotSame(original, shallow);
+        assertEquals("original", original.getValue());
+        assertEquals("original", shallow.getValue());
     }
 
     @Test
-    public void givenDeepCopy_whenElementChanges_thenOriginalListIsNotChanged() {
-        ValueObject object1 = new ValueObject("first");
-        ValueObject object2 = new ValueObject("second");
-        ValueObject object3 = new ValueObject("third");
+    public void givenDeepCopy_whenShadowChanges_thenOriginalIsNotChanged() {
+        ValueObject original = new ValueObject("original");
+        ValueObject shallow = new ValueObject(original.getValue());
 
-        ArrayList<ValueObject> original = new ArrayList<>();
-        original.add(object1);
-        original.add(object2);
-        original.add(object3);
+        shallow.setValue("shallow");
 
-        ArrayList<ValueObject> deep = new ArrayList<>();
-        for (int index = 0; index < original.size(); index++) {
-            deep.add(new ValueObject(original.get(index).getValue()));
-        }
-
-        deep.add(deep.remove(0));
-        deep.get(0).setValue("new first");
-        deep.get(1).setValue("new second");
-        deep.get(2).setValue("new third");
-
-        assertEquals("first", object1.getValue());
-        assertEquals("second", object2.getValue());
-        assertEquals("third", object3.getValue());
+        assertNotEquals(original, shallow);
+        assertNotSame(original, shallow);
+        assertEquals("original", original.getValue());
+        assertEquals("shallow", shallow.getValue());
     }
 
 }
