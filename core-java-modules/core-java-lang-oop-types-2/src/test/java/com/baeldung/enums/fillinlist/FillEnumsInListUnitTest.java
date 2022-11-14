134692e95890c45d4172bf2ae496578906cdb31f
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2022-11-14 03:37:05 +0100

    [fillEnumInList] change the return type in the util method

--- core-java-modules/core-java-lang-oop-types-2/src/test/java/com/baeldung/enums/fillinlist/FillEnumsInListUnitTest.java
+++ core-java-modules/core-java-lang-oop-types-2/src/test/java/com/baeldung/enums/fillinlist/FillEnumsInListUnitTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.EnumSet;
 import java.util.List;
 
 import static com.baeldung.enums.fillinlist.MagicNumber.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 enum MagicNumber {
     ONE, TWO, THREE, FOUR, FIVE
@@ -20,7 +20,7 @@ public class FillEnumsInListUnitTest {
 
     static <T> List<T> enumValuesInList(Class<T> enumCls) {
         T[] arr = enumCls.getEnumConstants();
-        return arr == null ? null : Arrays.asList(arr);
+        return arr == null ? Collections.emptyList() : Arrays.asList(arr);
     }
 
     @Test
@@ -41,7 +41,7 @@ public class FillEnumsInListUnitTest {
         assertEquals(EXPECTED_LIST, result1);
 
         List<Integer> result2 = enumValuesInList(Integer.class);
-        assertNull(result2);
+        assertTrue(result2.isEmpty());
     }
 
     @Test
