1f427291931e0f98246ec5f30adf6de2a5810b3d
drafii <saintreprobus@gmail.com>
2022-11-15 00:02:42 +0300

    Added the code for the crawlers.

---
+++ libraries-2/src/main/java/com/baeldung/webmagic/GithubRepoAnnotations.java
@@ -0,0 +1,30 @@
+package us.codecraft.webmagic.samples;
+
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.model.ConsolePageModelPipeline;
+import us.codecraft.webmagic.model.OOSpider;
+import us.codecraft.webmagic.model.annotation.ExtractBy;
+import us.codecraft.webmagic.model.annotation.ExtractByUrl;
+import us.codecraft.webmagic.model.annotation.HelpUrl;
+import us.codecraft.webmagic.model.annotation.TargetUrl;
+import us.codecraft.webmagic.pipeline.FilePageModelPipeline;
+
+@TargetUrl("https://github.com/\\w+/\\w+")
+@HelpUrl("https://github.com/\\w+")
+public class GithubRepoAnnotations {
+
+    @ExtractBy(value = "//h1[@class='public']/strong/a/text()", notNull = true)
+    private String name;
+
+    @ExtractByUrl("https://github\\.com/(\\w+)/.*")
+    private String author;
+
+    @ExtractBy("//div[@id='readme']/tidyText()")
+    private String readme;
+
+    public static void main(String[] args) {
+        OOSpider.create(Site.me().setSleepTime(1000)
+                        , new ConsolePageModelPipeline(), GithubRepo.class)
+                .addUrl("https://github.com/code4craft").thread(5).run();
+    }
+}
