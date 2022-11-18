3cf065e54f5822f2c8152941ade9fccfc9343e06
drafii <saintreprobus@gmail.com>
2022-11-18 00:37:21 +0000

    More modifications. Added log4j.xml.

--- libraries-2/src/main/java/com/baeldung/webmagic/GithubRepoPageProcessor.java
+++ libraries-2/src/main/java/com/baeldung/webmagic/GithubRepoPageProcessor.java
@@ -1,26 +1,24 @@
 package com.baeldung.webmagic;
 
-import us.codecraft.webmagic.*;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
 public class GithubRepoPageProcessor implements PageProcessor {
 
-    private Site site = Site.me().setRetryTimes(3).setSleepTime(0);
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
 
     @Override
     public void process(Page page) {
         page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
-        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+)").all());
-        GithubRepo githubRepo = new GithubRepo();
-        githubRepo.setAuthor(page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-        githubRepo.setName(page.getHtml().xpath("//h1[contains(@class, 'entry-title') and contains(@class, 'public')]/strong/a/text()").toString());
-        githubRepo.setReadme(page.getHtml().xpath("//div[@id='readme']/tidyText()").toString());
-        if (githubRepo.getName() == null) {
+        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
+        page.putField("name", page.getHtml().xpath("//h1[@class='public']/strong/a/text()").toString());
+        if (page.getResultItems().get("name")==null){
             //skip this page
             page.setSkip(true);
-        } else {
-            page.putField("repo", githubRepo);
         }
+        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
     }
 
     @Override
@@ -29,6 +27,6 @@ public class GithubRepoPageProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft/webmagic").thread(5).run();
+        Spider.create(new GithubRepoPageProcessor()).addUrl("https://github.com/code4craft").thread(5).run();
     }
 }
