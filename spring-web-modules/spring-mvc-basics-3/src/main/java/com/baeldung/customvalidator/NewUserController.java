bd340f1dfd4e52fea7f478f974046fc454bb9d61
panagiotiskakos <panagiotis.kakos@libra-is.com>
2022-11-20 20:46:13 +0200

    [JAVA-16216] Moved customvalidator from spring-mvc-basic-5 to spring-mvc-basic-3

--- spring-web-modules/spring-mvc-basics-3/src/main/java/com/baeldung/customvalidator/NewUserController.java
+++ spring-web-modules/spring-mvc-basics-3/src/main/java/com/baeldung/customvalidator/NewUserController.java
@@ -1,12 +1,12 @@
 package com.baeldung.customvalidator;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.validation.Valid;
 
 @Controller
 public class NewUserController {
