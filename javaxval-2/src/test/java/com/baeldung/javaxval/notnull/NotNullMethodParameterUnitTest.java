0c7836bccdf7fd85a65f548c66ff27438f1f450a
panagiotiskakos <panagiotis.kakos@libra-is.com>
2022-11-21 16:39:21 +0200

    [JAVA-16218] Split javaxval module

--- javaxval-2/src/test/java/com/baeldung/javaxval/notnull/NotNullMethodParameterUnitTest.java
+++ javaxval-2/src/test/java/com/baeldung/javaxval/notnull/NotNullMethodParameterUnitTest.java
@@ -1,8 +1,8 @@
 package com.baeldung.javaxval.notnull;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
 
 class NotNullMethodParameterUnitTest {
 
