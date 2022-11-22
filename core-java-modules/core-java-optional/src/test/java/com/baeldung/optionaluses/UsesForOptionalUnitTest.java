7cbd700f777508eee639fc54801d67397a1c9548
pedrolopes9-7 <pedroinf97@gmail.com>
2022-11-22 08:57:51 -0300

    copy to optional module

--- core-java-modules/core-java-optional/src/test/java/com/baeldung/optionaluses/UsesForOptionalUnitTest.java
+++ core-java-modules/core-java-optional/src/test/java/com/baeldung/optionaluses/UsesForOptionalUnitTest.java
@@ -1,9 +1,8 @@
 package com.baeldung.optionaluses;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UsesForOptionalUnitTest {
 
