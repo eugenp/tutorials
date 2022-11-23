3dbe5dece27d57176b650dc87e1534657af31c9d
hemanth0903 <118755008+hemanth0903@users.noreply.github.com>
2022-11-23 20:40:42 +0530

    Create ShallowCopyUnitTest.java

---
+++ objectcopy/Simple Sample/shallowcopy/ShallowCopyUnitTest.java
@@ -0,0 +1,21 @@
+package com.baeldung.java.shallowcopy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShallowCopyUnitTest1 {
+
+    @Test
+    public void whenCreatingShallowCopy_thenValuesShouldBeSame() {
+            
+            Sample s1 = new Sample();
+            Sample s2 = s1;
+            
+            s2.a = 20;
+            
+            assertSame(s1.a, s2.a);
+            
+    }
+
+}
