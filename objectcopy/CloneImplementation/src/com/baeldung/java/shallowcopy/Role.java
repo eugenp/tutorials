bdcd8271e50d9864b98ef9bdc55d39bfdcd432fa
hemanth0903 <118755008+hemanth0903@users.noreply.github.com>
2022-11-23 20:25:45 +0530

    Create Role.java

---
+++ objectcopy/CloneImplementation/src/com/baeldung/java/shallowcopy/Role.java
@@ -0,0 +1,16 @@
+package com.baeldung.java.shallowcopy;
+
+public class Role{
+	
+	public int level;
+	public String designation;
+	public int grossPay;
+	
+	Role(int level, String designation, int grossPay) {
+		
+		this.level = level;
+		this.designation = designation;
+		this.grossPay = grossPay;
+	}
+	
+}
