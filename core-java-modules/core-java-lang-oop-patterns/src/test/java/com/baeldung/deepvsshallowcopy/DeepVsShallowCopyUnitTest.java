3b393c78d162f64c4c88df9e0e3ba5f0a384cdb4
IfThen2 <christopher.jamescooke@gmail.com>
2022-11-23 12:52:22 -0700

    fix unit test file name

--- core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/deepvsshallowcopy/DeepVsShallowCopyUnitTest.java
+++ core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/deepvsshallowcopy/DeepVsShallowCopyUnitTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class ObjectCopyUnitTest {
+public class DeepVsShallowCopyUnitTest {
 
     DeepObject subject;
 
