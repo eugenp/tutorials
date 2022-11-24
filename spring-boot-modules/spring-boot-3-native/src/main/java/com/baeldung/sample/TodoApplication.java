4ea66b59ce73d9b79db070e4f24075807b7ca782
Ralf Ueberfuhr <40685729+ueberfuhr@users.noreply.github.com>
2022-11-24 09:47:54 +0100

    BAEL-5951: Spring Boot 3 Sample for Native Image builds incl. Runtime Hints (#13047)
    * Update Spring Boot, Spring Native and Native Maven Plugin versions
    
    * BAEL-5951: Spring Boot 3 Sample for Native Image builds incl. Runtime Hints
    
    * BAEL-5951: Configure POMs and add Swagger UI runtime hints
    
    * BAEL-5951: Remove Swagger UI runtime hints
    
    * BAEL-5951: Remove Spring Boot3 parent POM from profiles because of JDK17 dependency during build (building the parent POM is even not necessary)
    
    * BAEL-5951: Add tests
    
    * BAEL-5951: Fix tests (PMD naming conventions)

---
+++ spring-boot-modules/spring-boot-3-native/src/main/java/com/baeldung/sample/TodoApplication.java
@@ -0,0 +1,11 @@
+package com.baeldung.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class TodoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TodoApplication.class, args);
+    }
+
+}
