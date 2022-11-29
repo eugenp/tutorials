4ad5f2375bcbc516ecabcaf8d053e1ddb35291aa
apeterlic <apeterlic@gmail.com>
2022-11-29 12:34:07 +0100

    Optimize imports

--- persistence-modules/blaze-persistence/src/test/java/com/baeldung/AbstractTest.java
+++ persistence-modules/blaze-persistence/src/test/java/com/baeldung/AbstractTest.java
@@ -1,15 +1,9 @@
 package com.baeldung;
 
-import javax.persistence.EntityManager;
-import com.baeldung.model.Post;
-import com.baeldung.model.Person;
 import com.blazebit.persistence.integration.view.spring.EnableEntityViews;
 import com.blazebit.persistence.spring.data.repository.config.EnableBlazeRepositories;
-import org.junit.Before;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.Transactional;
 
 abstract class AbstractTest {
 
