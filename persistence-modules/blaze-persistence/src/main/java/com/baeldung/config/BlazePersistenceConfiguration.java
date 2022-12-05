717004427b57af9fe5e8e44c1101fad8ac112c89
Ana Peterlic <apeterlic@gmail.com>
2022-12-05 14:43:48 +0100

    Update Configuration class

--- persistence-modules/blaze-persistence/src/main/java/com/baeldung/config/BlazePersistenceConfiguration.java
+++ persistence-modules/blaze-persistence/src/main/java/com/baeldung/config/BlazePersistenceConfiguration.java
@@ -7,18 +7,18 @@ import com.blazebit.persistence.spi.CriteriaBuilderConfiguration;
 import com.blazebit.persistence.spring.data.repository.config.EnableBlazeRepositories;
 import com.blazebit.persistence.view.EntityViewManager;
 import com.blazebit.persistence.view.spi.EntityViewConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
 
 @Configuration
 @EnableEntityViews(basePackages = {"com.baeldung.view"})
 @EnableBlazeRepositories(basePackages = "com.baeldung.repository")
 public class BlazePersistenceConfiguration {
 
-    @PersistenceUnit
+    @Autowired
     private EntityManagerFactory entityManagerFactory;
 
     @Bean
