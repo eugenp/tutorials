9732f9ffd37d11651d7436024dccbc9d2d97f2ce
achraftt <achraf.taitai1@gmail.com>
2022-12-08 22:36:19 +0100

    BAEL-5927: Sorting a HashSet in Java (Refacto)

--- core-java-modules/core-java-collections-4/src/test/java/com/baeldung/collections/sorting/HashSetUnitTest.java
+++ core-java-modules/core-java-collections-4/src/test/java/com/baeldung/collections/sorting/HashSetUnitTest.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 
@@ -41,13 +43,13 @@ public class HashSetUnitTest {
     @Test
     void givenHashSet_whenUsingStream_thenHashSetSorted() {
         HashSet<Integer> numberHashSet = new HashSet<>();
-        numberHashSet.add(2);
-        numberHashSet.add(1);
-        numberHashSet.add(4);
-        numberHashSet.add(3);
-        // Sort the HashSet using stream()
-        numberHashSet.stream().sorted();
-        assertThat(numberHashSet).containsExactly(1, 2, 3, 4);
+        numberHashSet.add(200);
+        numberHashSet.add(100);
+        numberHashSet.add(400);
+        numberHashSet.add(300);
+        HashSet<Integer> sortedHashSet = numberHashSet.stream().sorted()
+          .collect(Collectors.toCollection(LinkedHashSet::new));
+        assertThat(sortedHashSet).containsExactly(100, 200, 300, 400);
     }
 
 }
