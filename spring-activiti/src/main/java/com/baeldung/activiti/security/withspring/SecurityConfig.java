4564d046257f02da03699f0f6652f87580984588
anuragkumawat <anuragkumawat7@gmail.com>
2022-12-08 19:18:02 +0530

    JAVA-16250 Update spring-activiti module to use parent-boot-2 module as parent (#13120)

--- spring-activiti/src/main/java/com/baeldung/activiti/security/withspring/SecurityConfig.java
+++ spring-activiti/src/main/java/com/baeldung/activiti/security/withspring/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.baeldung.activiti.security.withspring;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Autowired
-    private DataSource dataSource;
 
     protected void configure(HttpSecurity http) throws Exception {
         http.antMatcher("/**")
@@ -36,15 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public JdbcUserDetailsManager userDetailsManager() {
-        JdbcUserDetailsManager manager = new JdbcUserDetailsManager();
-        manager.setDataSource(dataSource);
-        return manager;
-    }
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user")
+            .password("{noop}pass")
+            .authorities("ROLE_ACTIVITI_USER")
+            .build();
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsManager());
+        return new InMemoryUserDetailsManager(user);
     }
-
 }
