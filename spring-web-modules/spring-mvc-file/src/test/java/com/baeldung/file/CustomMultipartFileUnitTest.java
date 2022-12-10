0250bdc1c5ca1fc1a6806a2368ebd0c6761ffd6b
amar wadhwani <brokenhardisk@gmail.com>
2022-12-10 23:20:18 +0100

    BAEL-5942 Code changes

---
+++ spring-web-modules/spring-mvc-file/src/test/java/com/baeldung/file/CustomMultipartFileUnitTest.java
@@ -0,0 +1,24 @@
+package com.baeldung.file;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+public class CustomMultipartFileUnitTest {
+
+    @Test
+    public void givenByteArray_thenMultipartFileCreated() throws IOException {
+        byte[] inputArr = "Test String".getBytes();
+        CustomMultipartFile customMultipartFile = new CustomMultipartFile(inputArr);
+        Assertions.assertEquals(inputArr, customMultipartFile.getBytes());
+    }
+
+    @Test
+    public void givenByteArray_thenMockMultipartFileCreated() throws IOException {
+        byte[] inputArr = "Test String".getBytes();
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("tempFileName", inputArr);
+        Assertions.assertEquals(inputArr, mockMultipartFile.getBytes());
+    }
+}
