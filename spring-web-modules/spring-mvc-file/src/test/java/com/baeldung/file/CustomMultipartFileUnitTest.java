a2ad327278451f4590bea313f7c4952d148b8033
amar wadhwani <brokenhardisk@gmail.com>
2022-12-15 23:11:57 +0100

    BAEL-5942 Code updates from Review

--- spring-web-modules/spring-mvc-file/src/test/java/com/baeldung/file/CustomMultipartFileUnitTest.java
+++ spring-web-modules/spring-mvc-file/src/test/java/com/baeldung/file/CustomMultipartFileUnitTest.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 
 public class CustomMultipartFileUnitTest {
 
@@ -14,22 +12,13 @@ public class CustomMultipartFileUnitTest {
     public void givenByteArray_thenMultipartFileCreated() throws IOException {
         byte[] inputArr = "Test String".getBytes();
         CustomMultipartFile customMultipartFile = new CustomMultipartFile(inputArr);
-        Assertions.assertEquals(inputArr, customMultipartFile.getBytes());
+        Assertions.assertArrayEquals(inputArr, customMultipartFile.getBytes());
     }
 
     @Test
     public void givenByteArray_thenMockMultipartFileCreated() throws IOException {
         byte[] inputArr = "Test String".getBytes();
         MockMultipartFile mockMultipartFile = new MockMultipartFile("tempFileName", inputArr);
-        Assertions.assertEquals(inputArr, mockMultipartFile.getBytes());
-    }
-
-    @Test
-    public void givenByteArray_thenByteArrayMultipartFileEditorCreated() throws IOException{
-        byte[] inputArr = "Test String".getBytes();
-        ByteArrayMultipartFileEditor editor = new ByteArrayMultipartFileEditor();
-        editor.setValue(inputArr);
-        MultipartFile multipartFile = (MultipartFile) editor.getValue();
-        Assertions.assertEquals(inputArr,multipartFile.getBytes());
+        Assertions.assertArrayEquals(inputArr, mockMultipartFile.getBytes());
     }
 }
