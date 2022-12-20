c1a9150a31c9fbdae97103cd4849338f604cd606
2406ankita <24ankitapal@gmail.com>
2022-12-21 00:59:46 +0530

    formatter

--- core-java-modules/core-java-lang-5/src/main/java/com/baeldung/shallowdeep/Car.java
+++ core-java-modules/core-java-lang-5/src/main/java/com/baeldung/shallowdeep/Car.java
@@ -1,50 +1,57 @@
 package com.baeldung.shallowdeep;
 
-public class Car implements Cloneable{
+public class Car implements Cloneable {
 
     private int modelNumber;
-	private String modelName;
-	private Engine engine;
-	public Car(int modelNumber, Engine engine, String modelName) {
-		super();
-		this.modelNumber = modelNumber;
-		this.engine = engine;
-		this.modelName = modelName;
-	}
-	public Car(Car car) {
-		this.modelNumber=car.modelNumber;
-		this.modelName=car.modelName;
-		this.engine=new Engine(car.getEngine());
-	}
-	
-	public int getModelNumber() {
-		return modelNumber;
-	}
-	public void setModelNumber(int modelNumber) {
-		this.modelNumber = modelNumber;
-	}
-	public Engine getEngine() {
-		return engine;
-	}
-	public void setEngine(Engine engine) {
-		this.engine = engine;
-	}
-	public String getModelName() {
-		return modelName;
-	}
-	public void setModelName(String modelName) {
-		this.modelName = modelName;
-	}
-	@Override
-	public Object clone() {
-		Car car=null;
-		try {
-			car = (Car)super.clone();
-		}
-		catch(CloneNotSupportedException ex) {
-			car = new Car(this.modelNumber,this.engine,this.modelName);
-		}
-		car.setEngine((Engine)this.engine.clone());
-		return car;
-	}
+    private String modelName;
+    private Engine engine;
+
+    public Car(int modelNumber, Engine engine, String modelName) {
+        super();
+        this.modelNumber = modelNumber;
+        this.engine = engine;
+        this.modelName = modelName;
+    }
+
+    public Car(Car car) {
+        this.modelNumber = car.modelNumber;
+        this.modelName = car.modelName;
+        this.engine = new Engine(car.getEngine());
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    @Override
+    public Object clone() {
+        Car car = null;
+        try {
+            car = (Car) super.clone();
+        } catch (CloneNotSupportedException ex) {
+            car = new Car(this.modelNumber, this.engine, this.modelName);
+        }
+        car.setEngine((Engine) this.engine.clone());
+        return car;
+    }
 }
--- core-java-modules/core-java-lang-5/src/main/java/com/baeldung/shallowdeep/Engine.java
+++ core-java-modules/core-java-lang-5/src/main/java/com/baeldung/shallowdeep/Engine.java
@@ -1,37 +1,42 @@
 package com.baeldung.shallowdeep;
 
-public class Engine implements Cloneable{
-	private String type;
-	private double horsePower;
-	
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public double getHorsePower() {
-		return horsePower;
-	}
-	public void setHorsePower(double horsePower) {
-		this.horsePower = horsePower;
-	}
-	public Engine(String type, double horsePower) {
-		super();
-		this.type = type;
-		this.horsePower = horsePower;
-	}
-	public Engine(Engine engine) {
-		this.type=engine.type;
-		this.horsePower=engine.horsePower;
-	}
-	@Override
-	public Object clone() {
-		try {
-			return (Engine)super.clone();
-		}
-		catch(CloneNotSupportedException ex) {
-			return new Engine(this.getType(),this.getHorsePower());
-		}
-	}
+public class Engine implements Cloneable {
+    private String type;
+    private double horsePower;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(double horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public Engine(String type, double horsePower) {
+        super();
+        this.type = type;
+        this.horsePower = horsePower;
+    }
+
+    public Engine(Engine engine) {
+        this.type = engine.type;
+        this.horsePower = engine.horsePower;
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return (Engine) super.clone();
+        } catch (CloneNotSupportedException ex) {
+            return new Engine(this.getType(), this.getHorsePower());
+        }
+    }
 }
