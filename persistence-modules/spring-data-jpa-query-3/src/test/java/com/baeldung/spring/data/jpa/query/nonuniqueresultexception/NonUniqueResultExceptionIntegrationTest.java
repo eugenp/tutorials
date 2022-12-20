6dedf08b2f4c81d9f2898493fa0c67dabb27c42a
Basler182 <showdialog@web.de>
2022-12-20 17:07:20 +0100

    update using AssertJ Exception

--- persistence-modules/spring-data-jpa-query-3/src/test/java/com/baeldung/spring/data/jpa/query/nonuniqueresultexception/NonUniqueResultExceptionIntegrationTest.java
+++ persistence-modules/spring-data-jpa-query-3/src/test/java/com/baeldung/spring/data/jpa/query/nonuniqueresultexception/NonUniqueResultExceptionIntegrationTest.java
@@ -8,8 +8,11 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.NonUniqueResultException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest(properties = "spring.sql.init.data-locations=classpath:import_entities.sql", showSql = false)
@@ -18,19 +21,15 @@ public class NonUniqueResultExceptionIntegrationTest {
     @Autowired
     private ArticleRepository repository;
 
-    @Test(expected = IncorrectResultSizeDataAccessException.class)
-    public void givenImportedArticlesWhenFindByPublicationTimeBetweenThenNIncorrectResultSizeDataAccessExceptionThrown() throws Exception {
-        repository.findByPublicationTimeBetween(
-                new SimpleDateFormat("HH:mm").parse("15:15"),
-                new SimpleDateFormat("HH:mm").parse("16:30")
-        );
+    @Test
+    public void givenImportedArticlesWhenFindByPublicationTimeBetweenThenNIncorrectResultSizeDataAccessExceptionThrown() {
+        assertThatThrownBy(() -> repository.findByPublicationTimeBetween(new SimpleDateFormat("HH:mm").parse("15:15"), new SimpleDateFormat("HH:mm").parse("16:30")))
+                .isInstanceOf(IncorrectResultSizeDataAccessException.class)
+                .hasCauseInstanceOf(NonUniqueResultException.class);
     }
 
     @Test
     public void givenImportedArticlesWhenfindAllByPublicationTimeBetweenThenSuccess() throws ParseException {
-        repository.findAllByPublicationTimeBetween(
-                new SimpleDateFormat("HH:mm").parse("15:15"),
-                new SimpleDateFormat("HH:mm").parse("16:30")
-        );
+        repository.findAllByPublicationTimeBetween(new SimpleDateFormat("HH:mm").parse("15:15"), new SimpleDateFormat("HH:mm").parse("16:30"));
     }
 }
