56668d4eaa7ef7b26f0faf953a38619243c1f8f1
Ralf Ueberfuhr <ralf.ueberfuhr@yahoo.com>
2022-12-22 11:25:08 +0100

    BAEL-5867: Add tracing

--- spring-boot-modules/spring-boot-3-observation/src/test/java/com/baeldung/samples/domain/EnableTestObservation.java
+++ spring-boot-modules/spring-boot-3-observation/src/test/java/com/baeldung/samples/domain/EnableTestObservation.java
@@ -1,6 +1,9 @@
 package com.baeldung.samples.domain;
 
+import com.baeldung.samples.config.ObservedAspectConfiguration;
 import io.micrometer.observation.tck.TestObservationRegistry;
+import io.micrometer.tracing.test.simple.SimpleTracer;
+import org.springframework.boot.test.autoconfigure.actuate.observability.AutoConfigureObservability;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
@@ -16,8 +19,12 @@ import java.lang.annotation.Target;
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import(EnableTestObservationRegistry.ObservationTestConfiguration.class)
-public @interface EnableTestObservationRegistry {
+@AutoConfigureObservability
+@Import({
+  ObservedAspectConfiguration.class,
+  EnableTestObservation.ObservationTestConfiguration.class
+})
+public @interface EnableTestObservation {
 
     @TestConfiguration
     class ObservationTestConfiguration {
@@ -25,6 +32,11 @@ public @interface EnableTestObservationRegistry {
         @Bean
         TestObservationRegistry observationRegistry() {
             return TestObservationRegistry.create();
+        }
+
+        @Bean
+        SimpleTracer simpleTracer() {
+            return new SimpleTracer();
         }
 
     }
--- spring-boot-modules/spring-boot-3-observation/src/test/java/com/baeldung/samples/domain/GreetingServiceTracingTest.java
+++ spring-boot-modules/spring-boot-3-observation/src/test/java/com/baeldung/samples/domain/GreetingServiceTracingTest.java
@@ -1,35 +1,42 @@
 package com.baeldung.samples.domain;
 
-import io.micrometer.observation.tck.TestObservationRegistry;
+import io.micrometer.tracing.test.simple.SimpleTracer;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static io.micrometer.observation.tck.TestObservationRegistryAssert.assertThat;
+import static io.micrometer.tracing.test.simple.TracerAssert.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ComponentScan(basePackageClasses = GreetingService.class)
 @EnableAutoConfiguration
 @EnableTestObservation
-class GreetingServiceObservationTest {
+class GreetingServiceTracingTest {
 
     @Autowired
     GreetingService service;
+    @Value("${management.tracing.enabled:true}")
+    boolean tracingEnabled;
     @Autowired
-    TestObservationRegistry registry;
+    SimpleTracer tracer;
 
     @Test
-    void testObservation() {
-        // invoke service
+    void testEnabledTracing() {
+        Assertions.assertThat(tracingEnabled).isTrue();
+    }
+
+    @Test
+    void testTracingForGreeting() {
         service.sayHello();
-        assertThat(registry)
-          .hasObservationWithNameEqualTo("greetingService")
-          .that()
-          .hasBeenStarted()
-          .hasBeenStopped();
+        assertThat(tracer)
+          .onlySpan()
+          .hasNameEqualTo("greeting-service#say-hello")
+          .isEnded();
     }
 
 }
