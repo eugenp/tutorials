f75fec64c7b6242d15d41917bc947d34912426d8
Maciej Glowka <maciejglowka.pl@gmail.com>
2022-12-22 23:26:54 +0100

    BAEL-5957: Creaet prime number finder for Kubernetes heap dump

---
+++ kubernetes-modules/k8s-java-heap-dump/src/main/java/com/baeldung/kubernetes/heap/dump/PrimeNumberFinder.java
@@ -0,0 +1,20 @@
+package com.baeldung.kubernetes.heap.dump;
+
+import java.util.stream.IntStream;
+
+public class PrimeNumberFinder {
+    public static void main(String[] args) {
+        int maxNumber = Integer.MAX_VALUE; // set to huge to make it run a long time
+
+        for (int i = 2; i < maxNumber; i++) {
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean isPrime(int number) {
+        return IntStream.rangeClosed(2, (int) (Math.sqrt(number)))
+                .allMatch(n -> number % n != 0);
+    }
+}
