d82c77c591e95c032ae2f8923312ce76f3e5da70
Neha Puraswani <neha.298@hotmail.com>
2022-12-26 23:49:07 +0530

    WIP: Initial commit

---
+++ gradle-modules/gradle-7/multiple-repositories/src/test/java/com/baeldung/gradle/multiplerepositories/MultipleRepositoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.gradle.multiplerepositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleRepositoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
