bcea8970e982b92123834d1375d4436078c6ae01
Dhawal Kapil <dhawalkapil@gmail.com>
2022-12-28 00:27:48 +0530

    Task/java 16779 (#13211)
    * JAVA-16779 Removed kubernetes-spring from the kubernetes-modules
    
    - Removed kubernete-spring from the parent pom of kubernetes-modules
    since it is a JDK11 module
    
    * JAVA-16779 Converted KinesisApplicationIntegrationTest to ManualTest
    
    - Converted KinesisApplicationIntegrationTest to ManualTest since it
    needs working AWS credentials to work
    - Added missing properties

--- spring-cloud-modules/spring-cloud-stream/spring-cloud-stream-kinesis/src/test/java/com/baeldung/KinesisApplicationManualTest.java
+++ spring-cloud-modules/spring-cloud-stream/spring-cloud-stream-kinesis/src/test/java/com/baeldung/KinesisApplicationManualTest.java
@@ -5,11 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * Manual Test - this test needs correct AWS Access Key and Secret to build the Amazon Kinesis and complete successfully
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = KinesisApplication.class)
-public class KinesisApplicationIntegrationTest {
-    @Test
-    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+public class KinesisApplicationManualTest {
+	@Test
+	public void whenSpringContextIsBootstrapped_thenNoExceptions() {
 
-    }
+	}
 }
