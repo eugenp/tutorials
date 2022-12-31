874045b392481d95a76cced2214b58b9c061980f
anuragkumawat <anuragkumawat7@gmail.com>
2022-12-31 22:35:39 +0530

    JAVA-14471 Removing unnecessary code related to Article - Securing SOAP Web Services With Keycloak

--- spring-boot-modules/spring-boot-keycloak/src/main/java/com/baeldung/keycloaksoap/KeycloakSecurityConfig.java
+++ spring-boot-modules/spring-boot-keycloak/src/main/java/com/baeldung/keycloaksoap/KeycloakSecurityConfig.java
@@ -3,47 +3,23 @@ package com.baeldung.keycloaksoap;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
 @ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true")
-@EnableGlobalMethodSecurity(jsr250Enabled = true)
 public class KeycloakSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //@formatter:off
-        http
-          .csrf()
-          .disable()
-          .authorizeRequests()
-          .anyRequest()
-          .permitAll();
-        //@formatter:on
-        http.oauth2Login();
-        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+        http.csrf()
+            .disable()
+            .authorizeHttpRequests(auth -> auth.anyRequest()
+                .authenticated())
+            .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
         return http.build();
     }
-
-    @Bean
-    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        return http.getSharedObject(AuthenticationManagerBuilder.class)
-            .build();
-    }
-
 }
