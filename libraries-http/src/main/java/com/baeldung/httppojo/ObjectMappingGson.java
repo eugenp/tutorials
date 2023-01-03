a517bec7ff504fbd02258575ec3b53d946a3876d
Michael Olayemi <michaelolayemi28@gmail.com>
2023-01-03 08:42:32 +0000

    Java HttpClient - Map Json Response to Java Class

---
+++ libraries-http/src/main/java/com/baeldung/httppojo/ObjectMappingGson.java
@@ -0,0 +1,16 @@
+package com.baeldung.httppojo;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import java.util.List;
+
+public class ObjectMappingGson {
+    List<TodoApp> readValue(String content) {
+        Gson gson = new GsonBuilder().create();
+        return gson.fromJson(content, new TypeToken<List<TodoApp>>() {
+        }.getType());
+
+    }
+
+}
