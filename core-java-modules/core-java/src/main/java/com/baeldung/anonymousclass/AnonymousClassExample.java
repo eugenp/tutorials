bcdb2b2b8248b84a22173181566084e59ae4a631
Vartika_Nigam <Vartika_Nigam@DellTeam.com>
2023-01-05 23:20:54 +0530

    added an example of anonymous class and lambda expression

---
+++ core-java-modules/core-java/src/main/java/com/baeldung/anonymousclass/AnonymousClassExample.java
@@ -0,0 +1,13 @@
+package com.baeldung.anonymousclass;
+
+public class AnonymousClassExample{
+
+    public static void main(String[] args){
+        Thread t1 = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                System.out.println("Thread: "+Thread.currentThread().getName()+" started");
+            }
+        });
+    }
+}
