88b3f08fdf29821dcfd78cf4b6dfeda7619f5fbc
Vali Tuguran <valituguran@gmail.com>
2023-01-08 17:54:25 +0200

    BAEL-6040 Added List interface tests.

--- core-java-modules/core-java-collections-4/src/test/java/com/baeldung/java/list/ListUnitTest.java
+++ core-java-modules/core-java-collections-4/src/test/java/com/baeldung/java/list/ListUnitTest.java
@@ -1,31 +1,126 @@
 package com.baeldung.java.list;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
 
 public class ListUnitTest {
-
+    
     @Test
-    public void givenAFruitList_whenBasicOperations_thenSuccess(){
+    public void givenAFruitList_whenAddNewFruit_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        assertEquals("Unexpected number of fruits in the list, should have been 0", 0, fruits.size());
+        
+        fruits.add("Apple");
+        assertEquals("Unexpected number of fruits in the list, should have been 1", 1, fruits.size());
+    }
+    
+    @Test
+    public void givenAFruitList_whenContainsFruit_thenSuccess(){
         List<String> fruits = new ArrayList<>();
         
         fruits.add("Apple");
+        assertTrue("Apple should be in the fruit list", fruits.contains("Apple"));
+        assertFalse("Banana should not be in the fruit list", fruits.contains("Banana"));
+    }
+    
+    @Test
+    public void givenAFruitList_whenIsEmpty_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        assertTrue("Fruit list should be empty", fruits.isEmpty());
+        
+        fruits.add("Apple");
+        assertFalse("Fruit list should not be empty", fruits.isEmpty());
+    }
+    
+    @Test
+    public void givenAFruitList_whenIterateOverIt_thenFruitsAreInOrder(){
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apple"); // fruit at index 0
+        fruits.add("Orange");// fruit at index 1
+        fruits.add("Banana");// fruit at index 2
+        int index = 0;
+        for (Iterator<String> it = fruits.listIterator(); it.hasNext(); ) {
+            String fruit = it.next();
+            assertEquals("Fruits should be in order", fruits.get(index++), fruit);
+        }
+    }
+    
+    @Test
+    public void givenAFruitList_whenRemoveElement_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apple"); 
+        fruits.add("Orange");
+        assertEquals("Unexpected number of fruits in the list, should have been 2", 2, fruits.size());
+        
+        fruits.remove("Apple");
+        assertEquals("Unexpected number of fruits in the list, should have been 1", 1, fruits.size());
+    }
+    
+    @Test
+    public void givenAFruitList_whenSetElement_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apple"); 
+        fruits.add("Orange");
+        
+        fruits.set(0, "Banana");
+        assertEquals("Fruit at index 0 should be Banana", "Banana", fruits.get(0));
+    }
+    
+    @Test
+    public void givenAFruitList_whenSort_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apple"); 
         fruits.add("Orange");
         fruits.add("Banana");
         
-        assertEquals("Unexpected number of fruits in the list", 3, fruits.size());
+        fruits.sort(Comparator.naturalOrder());
         
-        fruits.remove("Banana");
-        
-        assertEquals("Unexpected number of fruits in the list", 2, fruits.size());
-        
-        for (Iterator<String> it = fruits.listIterator(); it.hasNext(); ) {
-            System.out.println(it.next());
-        }
+        assertEquals("Fruit at index 0 should be Apple", "Apple", fruits.get(0));
+        assertEquals("Fruit at index 1 should be Banana", "Banana", fruits.get(1));
+        assertEquals("Fruit at index 2 should be Orange", "Orange", fruits.get(2));
     }
+    
+    @Test
+    public void givenAFruitList_whenSublist_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apple"); 
+        fruits.add("Orange");
+        fruits.add("Banana");
+        
+        List<String> fruitsSublist = fruits.subList(0, 2);
+        assertEquals("Unexpected number of fruits in the sublist, should have been 2", 2, fruitsSublist.size());
+        
+        assertEquals("Fruit at index 0 should be Apple", "Apple", fruitsSublist.get(0));
+        assertEquals("Fruit at index 1 should be Orange", "Orange", fruitsSublist.get(1));
+    }
+    
+    @Test
+    public void givenAFruitList_whenToArray_thenSuccess(){
+        List<String> fruits = new ArrayList<>();
+        
+        fruits.add("Apple"); 
+        fruits.add("Orange");
+        fruits.add("Banana");
+        
+        String[] fruitsArray = fruits.toArray(new String[0]);
+        assertEquals("Unexpected number of fruits in the array, should have been 3", 3, fruitsArray.length);
+        
+        assertEquals("Fruit at index 0 should be Apple", "Apple", fruitsArray[0]);
+        assertEquals("Fruit at index 1 should be Orange", "Orange", fruitsArray[1]);
+        assertEquals("Fruit at index 2 should be Banana", "Banana", fruitsArray[2]);
+    }
+    
 }
