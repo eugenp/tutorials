12db46fa106ca7ea3f36b2ae0b392918c27748a2
amar wadhwani <brokenhardisk@gmail.com>
2023-01-11 19:08:26 +0100

    BAEL-5942 Test updates from Review

--- spring-web-modules/spring-mvc-file/src/test/java/com/baeldung/file/CustomMultipartFileUnitTest.java
+++ spring-web-modules/spring-mvc-file/src/test/java/com/baeldung/file/CustomMultipartFileUnitTest.java
@@ -12,13 +12,17 @@ class CustomMultipartFileUnitTest {
     void whenProvidingByteArray_thenMultipartFileCreated() throws IOException {
         byte[] inputArray = "Test String".getBytes();
         CustomMultipartFile customMultipartFile = new CustomMultipartFile(inputArray);
+        Assertions.assertFalse(customMultipartFile.isEmpty());
         Assertions.assertArrayEquals(inputArray, customMultipartFile.getBytes());
+        Assertions.assertEquals(inputArray.length,customMultipartFile.getSize());
     }
 
     @Test
     void whenProvidingByteArray_thenMockMultipartFileCreated() throws IOException {
         byte[] inputArray = "Test String".getBytes();
         MockMultipartFile mockMultipartFile = new MockMultipartFile("tempFileName", inputArray);
+        Assertions.assertFalse(mockMultipartFile.isEmpty());
         Assertions.assertArrayEquals(inputArray, mockMultipartFile.getBytes());
+        Assertions.assertEquals(inputArray.length,mockMultipartFile.getSize());
     }
 }
