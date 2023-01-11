f396ff0341117d7020234541840bda3a599e3f8e
cesarevalenti90 <50798547+cesarevalenti90@users.noreply.github.com>
2023-01-11 22:25:43 +0100

    BAEL-6057 - Implementing Retry In Kafka Consumer (#13274)
    * BAEL-6057 - Implementing Retry In Kafka Consumer
    
    (moved code example to new module)
    
    * BAEL-6057 - Implementing Retry In Kafka Consumer
    
    (fix on README.md)
    
    Co-authored-by: Cesare <cesare.valenti@hotmail.com>

---
+++ spring-kafka-2/src/main/java/com/baeldung/spring/kafka/Farewell.java
@@ -0,0 +1,37 @@
+package com.baeldung.spring.kafka;
+
+public class Farewell {
+
+    private String message;
+    private Integer remainingMinutes;
+
+    public Farewell() {
+    }
+
+    public Farewell(String message, Integer remainingMinutes) {
+        this.message = message;
+        this.remainingMinutes = remainingMinutes;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getRemainingMinutes() {
+        return remainingMinutes;
+    }
+
+    public void setRemainingMinutes(Integer remainingMinutes) {
+        this.remainingMinutes = remainingMinutes;
+    }
+
+    @Override
+    public String toString() {
+        return message + ". In " + remainingMinutes + "!";
+    }
+
+}
