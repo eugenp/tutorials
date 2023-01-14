1e9d7724e4df17e159723eceaf8c639bc7789ac4
2406ankita <24ankitapal@gmail.com>
2023-01-15 00:31:21 +0530

    Unit test changes

--- core-java-modules/core-java-lang-5/src/test/java/com/baeldung/listduplicate/ListDuplicateUnitTest.java
+++ core-java-modules/core-java-lang-5/src/test/java/com/baeldung/listduplicate/ListDuplicateUnitTest.java
@@ -2,65 +2,54 @@ package com.baeldung.listduplicate;
 
 import java.util.Arrays;
 import java.util.List;
-
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ListDuplicateUnitTest {
+    private ListDuplicate listDuplicate;
+    private List<Integer> list;
+
+    @Before
+    public void setup_tests() {
+        listDuplicate = new ListDuplicate();
+        list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
+    }
 
     @Test
     public void givenList_whenUsingSet_thenReturnDuplicateElements() {
-        ListDuplicate listDuplicate = new ListDuplicate();
-        List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
         List<Integer> duplicates = listDuplicate.listDuplicateUsingSet(list);
-        Assert.assertEquals(duplicates.size(), 2);
-        Assert.assertEquals(duplicates.contains(3), true);
-        Assert.assertEquals(duplicates.contains(4), true);
-        Assert.assertEquals(duplicates.contains(1), false);
+        basicExpectations(duplicates);
     }
 
     @Test
     public void givenList_whenUsingFrequencyMap_thenReturnDuplicateElements() {
-        ListDuplicate listDuplicate = new ListDuplicate();
-        List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
         List<Integer> duplicates = listDuplicate.listDuplicateUsingMap(list);
-        Assert.assertEquals(duplicates.size(), 2);
-        Assert.assertEquals(duplicates.contains(3), true);
-        Assert.assertEquals(duplicates.contains(4), true);
-        Assert.assertEquals(duplicates.contains(1), false);
+        basicExpectations(duplicates);
     }
 
     @Test
     public void givenList_whenUsingFilterAndSetAdd_thenReturnDuplicateElements() {
-        ListDuplicate listDuplicate = new ListDuplicate();
-        List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
         List<Integer> duplicates = listDuplicate.listDuplicateUsingFilterAndSetAdd(list);
-        Assert.assertEquals(duplicates.size(), 2);
-        Assert.assertEquals(duplicates.contains(3), true);
-        Assert.assertEquals(duplicates.contains(4), true);
-        Assert.assertEquals(duplicates.contains(1), false);
+        basicExpectations(duplicates);
     }
 
     @Test
     public void givenList_whenUsingCollectionsFrequency_thenReturnDuplicateElements() {
-        ListDuplicate listDuplicate = new ListDuplicate();
-        List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
         List<Integer> duplicates = listDuplicate.listDuplicateUsingCollectionsFrequency(list);
-        Assert.assertEquals(duplicates.size(), 2);
-        Assert.assertEquals(duplicates.contains(3), true);
-        Assert.assertEquals(duplicates.contains(4), true);
-        Assert.assertEquals(duplicates.contains(1), false);
+        basicExpectations(duplicates);
     }
 
     @Test
     public void givenList_whenUsingMapAndCollectorsGroupingBy_thenReturnDuplicateElements() {
-        ListDuplicate listDuplicate = new ListDuplicate();
-        List<Integer> list = Arrays.asList(1, 2, 3, 3, 4, 4, 5);
         List<Integer> duplicates = listDuplicate.listDuplicateUsingCollectionsFrequency(list);
+        basicExpectations(duplicates);
+    }
+
+    private void basicExpectations(List<Integer> duplicates) {
         Assert.assertEquals(duplicates.size(), 2);
         Assert.assertEquals(duplicates.contains(3), true);
         Assert.assertEquals(duplicates.contains(4), true);
         Assert.assertEquals(duplicates.contains(1), false);
     }
-
 }
