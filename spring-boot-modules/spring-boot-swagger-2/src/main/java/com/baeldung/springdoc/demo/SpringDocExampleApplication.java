dbf4c1406b968faffdca0a68c650d6b4e830d0a3
Sudarshan Hiray <sdhiray7@gmail.com>
2023-01-14 17:48:50 +0530

    BAEL-5321 Initial commit

---
+++ spring-boot-modules/spring-boot-swagger-2/src/main/java/com/baeldung/springdoc/demo/SpringDocExampleApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung.springdoc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocExampleApplication.class, args);
+    }
+
+}
