4e46b1158421d51afc901a5a561a9eb70fca9ad8
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-01-15 11:46:50 +0200

    JAVA-15980 Upgrade AWS Kinesis

--- spring-cloud-modules/spring-cloud-stream/spring-cloud-stream-kinesis/src/main/java/com/baeldung/kclkpl/KinesisKCLApplication.java
+++ spring-cloud-modules/spring-cloud-stream/spring-cloud-stream-kinesis/src/main/java/com/baeldung/kclkpl/KinesisKCLApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonaws.ClientConfiguration;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
@@ -17,32 +18,31 @@ public class KinesisKCLApplication implements ApplicationRunner {
 
     @Value("${aws.access.key}")
     private String accessKey;
-    
+
     @Value("${aws.secret.key}")
     private String secretKey;
-    
+
     @Value("${ips.stream}")
     private String IPS_STREAM;
-    
+
     public static void main(String[] args) {
         SpringApplication.run(KinesisKCLApplication.class, args);
     }
 
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-		KinesisClientLibConfiguration consumerConfig = new KinesisClientLibConfiguration(
-		  "KinesisKCLConsumer", 
-		  IPS_STREAM,
-		  new AWSStaticCredentialsProvider(awsCredentials), 
-		  "KinesisKCLConsumer")
-		    .withRegionName(Regions.EU_CENTRAL_1.getName());
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 
-		new Worker.Builder()
-		  .recordProcessorFactory(new IpProcessorFactory())
-		  .config(consumerConfig)
-		  .build()
-		  .run();
-	}
-    
+        KinesisClientLibConfiguration consumerConfig = new KinesisClientLibConfiguration("KinesisKCLConsumer", IPS_STREAM, "", "", KinesisClientLibConfiguration.DEFAULT_INITIAL_POSITION_IN_STREAM, new AWSStaticCredentialsProvider(awsCredentials),
+          new AWSStaticCredentialsProvider(awsCredentials), new AWSStaticCredentialsProvider(awsCredentials), KinesisClientLibConfiguration.DEFAULT_FAILOVER_TIME_MILLIS, "KinesisKCLConsumer", KinesisClientLibConfiguration.DEFAULT_MAX_RECORDS,
+          KinesisClientLibConfiguration.DEFAULT_IDLETIME_BETWEEN_READS_MILLIS, KinesisClientLibConfiguration.DEFAULT_DONT_CALL_PROCESS_RECORDS_FOR_EMPTY_RECORD_LIST, KinesisClientLibConfiguration.DEFAULT_PARENT_SHARD_POLL_INTERVAL_MILLIS,
+          KinesisClientLibConfiguration.DEFAULT_SHARD_SYNC_INTERVAL_MILLIS, KinesisClientLibConfiguration.DEFAULT_CLEANUP_LEASES_UPON_SHARDS_COMPLETION, new ClientConfiguration(), new ClientConfiguration(), new ClientConfiguration(),
+          KinesisClientLibConfiguration.DEFAULT_TASK_BACKOFF_TIME_MILLIS, KinesisClientLibConfiguration.DEFAULT_METRICS_BUFFER_TIME_MILLIS, KinesisClientLibConfiguration.DEFAULT_METRICS_MAX_QUEUE_SIZE,
+          KinesisClientLibConfiguration.DEFAULT_VALIDATE_SEQUENCE_NUMBER_BEFORE_CHECKPOINTING, Regions.EU_CENTRAL_1.getName(), KinesisClientLibConfiguration.DEFAULT_SHUTDOWN_GRACE_MILLIS, KinesisClientLibConfiguration.DEFAULT_DDB_BILLING_MODE, null, 0, 0, 0);
+
+        new Worker.Builder().recordProcessorFactory(new IpProcessorFactory())
+          .config(consumerConfig)
+          .build()
+          .run();
+    }
 }
