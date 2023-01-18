09ac2f08da6ca480fe02528bf316b8b7fbe3e6ed
IfThen2 <57305322+IfThen2@users.noreply.github.com>
2023-01-18 05:54:00 -0700

    code for BAEL-5999 Converting String Object to Java Compilable Code (#13179)
    * code for BAEL-5999 Converting String Object to Java Compilable Code
    
    * move code from core-java-jvm-2 to core-java-jvm-3
    fix unit test name
    fix core-java-jvm-3 test directory structure
    disable non-working unit tests from core-java-jvm-3

--- core-java-modules/core-java-jvm-3/src/test/java/com/baeldung/resource/ClassGetResourceUnitTest.java
+++ core-java-modules/core-java-jvm-3/src/test/java/com/baeldung/resource/ClassGetResourceUnitTest.java
@@ -1,10 +1,12 @@
 package com.baeldung.resource;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.net.URL;
 
+@Disabled
 class ClassGetResourceUnitTest {
 
     @Test
