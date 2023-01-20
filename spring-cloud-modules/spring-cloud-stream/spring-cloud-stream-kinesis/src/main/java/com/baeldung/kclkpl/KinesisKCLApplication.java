c543cb5cc22364507e27c897faff2d8f4ebd8483
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-01-20 10:23:01 +0200

    JAVA-15980 Minor readability improvement

--- spring-cloud-modules/spring-cloud-stream/spring-cloud-stream-kinesis/src/main/java/com/baeldung/kclkpl/KinesisKCLApplication.java
+++ spring-cloud-modules/spring-cloud-stream/spring-cloud-stream-kinesis/src/main/java/com/baeldung/kclkpl/KinesisKCLApplication.java
@@ -1,5 +1,7 @@
 package com.baeldung.kclkpl;
 
+import static com.amazonaws.services.kinesis.clientlibrary.lib.worker.KinesisClientLibConfiguration.*;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -33,12 +35,11 @@ public class KinesisKCLApplication implements ApplicationRunner {
     public void run(ApplicationArguments args) throws Exception {
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
 
-        KinesisClientLibConfiguration consumerConfig = new KinesisClientLibConfiguration("KinesisKCLConsumer", IPS_STREAM, "", "", KinesisClientLibConfiguration.DEFAULT_INITIAL_POSITION_IN_STREAM, new AWSStaticCredentialsProvider(awsCredentials),
-          new AWSStaticCredentialsProvider(awsCredentials), new AWSStaticCredentialsProvider(awsCredentials), KinesisClientLibConfiguration.DEFAULT_FAILOVER_TIME_MILLIS, "KinesisKCLConsumer", KinesisClientLibConfiguration.DEFAULT_MAX_RECORDS,
-          KinesisClientLibConfiguration.DEFAULT_IDLETIME_BETWEEN_READS_MILLIS, KinesisClientLibConfiguration.DEFAULT_DONT_CALL_PROCESS_RECORDS_FOR_EMPTY_RECORD_LIST, KinesisClientLibConfiguration.DEFAULT_PARENT_SHARD_POLL_INTERVAL_MILLIS,
-          KinesisClientLibConfiguration.DEFAULT_SHARD_SYNC_INTERVAL_MILLIS, KinesisClientLibConfiguration.DEFAULT_CLEANUP_LEASES_UPON_SHARDS_COMPLETION, new ClientConfiguration(), new ClientConfiguration(), new ClientConfiguration(),
-          KinesisClientLibConfiguration.DEFAULT_TASK_BACKOFF_TIME_MILLIS, KinesisClientLibConfiguration.DEFAULT_METRICS_BUFFER_TIME_MILLIS, KinesisClientLibConfiguration.DEFAULT_METRICS_MAX_QUEUE_SIZE,
-          KinesisClientLibConfiguration.DEFAULT_VALIDATE_SEQUENCE_NUMBER_BEFORE_CHECKPOINTING, Regions.EU_CENTRAL_1.getName(), KinesisClientLibConfiguration.DEFAULT_SHUTDOWN_GRACE_MILLIS, KinesisClientLibConfiguration.DEFAULT_DDB_BILLING_MODE, null, 0, 0, 0);
+        KinesisClientLibConfiguration consumerConfig = new KinesisClientLibConfiguration("KinesisKCLConsumer", IPS_STREAM, "", "", DEFAULT_INITIAL_POSITION_IN_STREAM, new AWSStaticCredentialsProvider(awsCredentials),
+          new AWSStaticCredentialsProvider(awsCredentials), new AWSStaticCredentialsProvider(awsCredentials), DEFAULT_FAILOVER_TIME_MILLIS, "KinesisKCLConsumer", DEFAULT_MAX_RECORDS, DEFAULT_IDLETIME_BETWEEN_READS_MILLIS,
+          DEFAULT_DONT_CALL_PROCESS_RECORDS_FOR_EMPTY_RECORD_LIST, DEFAULT_PARENT_SHARD_POLL_INTERVAL_MILLIS, DEFAULT_SHARD_SYNC_INTERVAL_MILLIS, DEFAULT_CLEANUP_LEASES_UPON_SHARDS_COMPLETION, new ClientConfiguration(), new ClientConfiguration(),
+          new ClientConfiguration(), DEFAULT_TASK_BACKOFF_TIME_MILLIS, DEFAULT_METRICS_BUFFER_TIME_MILLIS, DEFAULT_METRICS_MAX_QUEUE_SIZE, DEFAULT_VALIDATE_SEQUENCE_NUMBER_BEFORE_CHECKPOINTING, Regions.EU_CENTRAL_1.getName(), DEFAULT_SHUTDOWN_GRACE_MILLIS,
+          DEFAULT_DDB_BILLING_MODE, null, 0, 0, 0);
 
         new Worker.Builder().recordProcessorFactory(new IpProcessorFactory())
           .config(consumerConfig)
