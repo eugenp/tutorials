231c5e26d1261fa3770337617e29f0cbc168d2a8
pedrolopes9-7 <pedroinf97@gmail.com>
2023-01-23 19:23:26 -0300

    player entity and repository. test structure

---
+++ persistence-modules/spring-data-jpa-repo-2/src/main/java/com/baeldung/spring/data/persistence/findbyvsfindallby/FindByVsFindAllByApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.spring.data.persistence.findbyvsfindallby;
+
+import com.baeldung.spring.data.persistence.like.LikeApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindByVsFindAllByApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FindByVsFindAllByApplication.class, args);
+    }
+}
