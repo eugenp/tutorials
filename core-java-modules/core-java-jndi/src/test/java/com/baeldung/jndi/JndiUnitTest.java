fe7e45244be1c50582062a9ccff0e8507c249ec9
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-01-27 19:48:50 +0200

    JAVA-16710 Fix failed unit test

--- core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
+++ core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
@@ -3,9 +3,11 @@ package com.baeldung.jndi;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.annotation.DirtiesContext;
 
 import javax.naming.*;
 import javax.sql.DataSource;
@@ -14,6 +16,8 @@ import java.util.Enumeration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Isolated
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class JndiUnitTest {
 
     private static InitialContext ctx;
@@ -57,11 +61,10 @@ class JndiUnitTest {
         assertNotNull(ds);
         assertNotNull(ds.getConnection());
     }
-    
+
     @AfterAll
     static void tearDown() throws Exception {
        ctx.close();
+       ctx = null;
     }
-
-
 }
