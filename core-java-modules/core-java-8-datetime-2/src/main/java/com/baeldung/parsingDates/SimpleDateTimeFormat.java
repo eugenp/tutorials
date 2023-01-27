a27be8d908d0491e9acc53f1818c808485a0e3a0
tgvLt <mantaspm@gmail.com>
2023-01-27 09:36:49 +0200

    Update SimpleDateTimeFormat.java

--- core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleDateTimeFormat.java
+++ core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleDateTimeFormat.java
@@ -1,26 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.parsingDates;
 
 import java.util.Arrays;
 import java.util.List;
+import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 
 public class SimpleDateTimeFormat {
 
-    public boolean parseDate(String date) {
+    public LocalDate parseDate(String date) {
         List<String> patternList = Arrays.asList("MM/dd/yyyy", "dd.MM.yyyy", "yyyy-MM-dd");
         for (String pattern : patternList) {
             try {
-                DateTimeFormat.forPattern(pattern).parseLocalDate(date);
-                return true;
+                return DateTimeFormat.forPattern(pattern).parseLocalDate(date);
             } catch (Exception e) {
             }
         }
-        return false;
+        return null;
     }
-
 }
