f7a885675750de334e48872eb246ab436f743dee
tgvLt <mantaspm@gmail.com>
2023-01-27 09:44:47 +0200

    Update SimpleParseDate.java

--- core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleParseDate.java
+++ core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleParseDate.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.baeldung.parsingDates;
 
 import java.text.ParseException;
@@ -11,7 +6,6 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Pattern;
-
 
 public class SimpleParseDate {
 
@@ -30,9 +24,6 @@ public class SimpleParseDate {
             + "|^((0[1-9]|1[0-9]|2[0-8])/02/((19|2[0-9])[0-9]{2}))$"
             + "|^((0[1-9]|[12][0-9]|3[01])/(0[13578]|10|12)/((19|2[0-9])[0-9]{2}))$"
             + "|^((0[1-9]|[12][0-9]|30)/(0[469]|11)/((19|2[0-9])[0-9]{2}))$");
-
-    public SimpleParseDate() {
-    }
 
     public Date parseDateWithMatcher(String date) {
 
