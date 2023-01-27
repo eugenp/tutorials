8e902bada1fa8ad8880ba688e191f685f1b3afd8
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-01-27 19:48:50 +0200

    JAVA-16710 Fix failed unit test

--- core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
+++ core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
@@ -3,9 +3,11 @@ package com.baeldung.jndi;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.annotation.DirtiesContext;
 
 import javax.naming.*;
 import javax.sql.DataSource;
@@ -31,6 +33,8 @@ class JndiUnitTest {
 
     @Test
     void givenACompositeName_whenAddingAnElement_thenNameIncludesIt() throws Exception {
+//        JndiTemplate jndiTemplate = new JndiTemplate();
+//        ctx = (InitialContext) jndiTemplate.getContext();
         Name objectName = new CompositeName("java:comp/env/jdbc");
 
         Enumeration<String> elements = objectName.getAll();
@@ -46,22 +50,28 @@ class JndiUnitTest {
 
     @Test
     void givenADataSource_whenAddingDriver_thenBind() throws Exception {
+//        JndiTemplate jndiTemplate = new JndiTemplate();
+//        ctx = (InitialContext) jndiTemplate.getContext();
+
         ds.setDriverClassName("org.h2.Driver");
         ctx.bind("java:comp/env/jdbc/datasource", ds);
     }
 
     @Test
     void givenContext_whenLookupByName_thenValidDataSource() throws Exception {
+
+//        JndiTemplate jndiTemplate = new JndiTemplate();
+//        ctx = (InitialContext) jndiTemplate.getContext();
+
         DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/datasource");
 
         assertNotNull(ds);
         assertNotNull(ds.getConnection());
     }
-    
+
     @AfterAll
     static void tearDown() throws Exception {
        ctx.close();
+       ctx = null;
     }
-
-
 }
