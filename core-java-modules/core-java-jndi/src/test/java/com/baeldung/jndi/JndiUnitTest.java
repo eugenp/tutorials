631a1e55280d7ed234e6d90c9e3d222ef834c806
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-01-27 19:48:50 +0200

    JΑVA-16710 Fix failed unit test

--- core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
+++ core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
@@ -3,9 +3,12 @@ package com.baeldung.jndi;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.annotation.DirtiesContext;
 
 import javax.naming.*;
 import javax.sql.DataSource;
@@ -14,23 +17,34 @@ import java.util.Enumeration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Isolated
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class JndiUnitTest {
 
-    private static InitialContext ctx;
-    private static DriverManagerDataSource ds;
+    private InitialContext ctx;
+    private DriverManagerDataSource ds;
 
-    @BeforeAll
-    static void setUp() throws Exception {
-        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
-        ds = new DriverManagerDataSource("jdbc:h2:mem:mydb");
-        builder.activate();
+//    SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
 
-        JndiTemplate jndiTemplate = new JndiTemplate();
-        ctx = (InitialContext) jndiTemplate.getContext();
-    }
+//    @BeforeAll
+//    void setUp() throws Exception {
+//        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+//        ds = new DriverManagerDataSource("jdbc:h2:mem:mydb");
+//        builder.activate();
+//
+//                JndiTemplate jndiTemplate = new JndiTemplate();
+//                ctx = (InitialContext) jndiTemplate.getContext();
+//    }
 
     @Test
     void givenACompositeName_whenAddingAnElement_thenNameIncludesIt() throws Exception {
+        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+        ds = new DriverManagerDataSource("jdbc:h2:mem:mydb");
+        builder.activate();
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        ctx = (InitialContext) jndiTemplate.getContext();
+
         Name objectName = new CompositeName("java:comp/env/jdbc");
 
         Enumeration<String> elements = objectName.getAll();
@@ -42,26 +56,48 @@ class JndiUnitTest {
 
         assertEquals("env", objectName.get(1));
         assertEquals("example", objectName.get(objectName.size() - 1));
+
+        builder.deactivate();
+        ctx.close();
     }
 
     @Test
     void givenADataSource_whenAddingDriver_thenBind() throws Exception {
+        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+        ds = new DriverManagerDataSource("jdbc:h2:mem:mydb");
+        builder.activate();
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        ctx = (InitialContext) jndiTemplate.getContext();
+
         ds.setDriverClassName("org.h2.Driver");
         ctx.bind("java:comp/env/jdbc/datasource", ds);
+
+        builder.deactivate();
+        ctx.close();
     }
 
     @Test
     void givenContext_whenLookupByName_thenValidDataSource() throws Exception {
+
+        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+        ds = new DriverManagerDataSource("jdbc:h2:mem:mydb");
+        builder.activate();
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        ctx = (InitialContext) jndiTemplate.getContext();
+
         DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/datasource");
 
         assertNotNull(ds);
         assertNotNull(ds.getConnection());
-    }
-    
-    @AfterAll
-    static void tearDown() throws Exception {
-       ctx.close();
+
+        builder.deactivate();
+        ctx.close();
     }
 
-
+//    @AfterAll
+//    void tearDown() throws Exception {
+//        builder.deactivate();
+//        ctx.close();
+//        ctx = null;
+//    }
 }
--- core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/exceptions/JndiExceptionsUnitTest.java
+++ core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/exceptions/JndiExceptionsUnitTest.java
@@ -6,28 +6,35 @@ import javax.naming.InitialContext;
 import javax.naming.NameNotFoundException;
 import javax.naming.NoInitialContextException;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.annotation.DirtiesContext;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Isolated
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 public class JndiExceptionsUnitTest {
-    
-    InitialContext ctx;
+
+    InitialContext ctx = null;
+
+    SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
 
     @Test
     @Order(1)
-    @Disabled
     void givenNoContext_whenLookupObject_thenThrowNoInitialContext() {
         assertThrows(NoInitialContextException.class, () -> {
+            builder.deactivate();
+            ctx = null;
+
             JndiTemplate jndiTemplate = new JndiTemplate();
-            ctx = (InitialContext) jndiTemplate.getContext();
+            InitialContext ctx = (InitialContext) jndiTemplate.getContext();
             ctx.lookup("java:comp/env/jdbc/datasource");
-            ctx.close();            
+            ctx.close();
         }).printStackTrace();
     }
 
@@ -35,7 +42,7 @@ public class JndiExceptionsUnitTest {
     @Order(2)
     void givenEmptyContext_whenLookupNotBounds_thenThrowNameNotFound() {
         assertThrows(NameNotFoundException.class, () -> {
-            SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+            //            SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
             builder.activate();
 
             JndiTemplate jndiTemplate = new JndiTemplate();
