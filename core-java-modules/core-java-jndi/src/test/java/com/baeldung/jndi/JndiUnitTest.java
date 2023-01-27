9b24989e812946b159b5a6151ed4d64f806e2c7a
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-01-27 19:48:50 +0200

    JAVA-16710 Fix failed unit test

--- core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
+++ core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/JndiUnitTest.java
@@ -3,9 +3,11 @@ package com.baeldung.jndi;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Isolated;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jndi.JndiTemplate;
 import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.annotation.DirtiesContext;
 
 import javax.naming.*;
 import javax.sql.DataSource;
@@ -14,6 +16,8 @@ import java.util.Enumeration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@Isolated
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class JndiUnitTest {
 
     private static InitialContext ctx;
@@ -25,12 +29,14 @@ class JndiUnitTest {
         ds = new DriverManagerDataSource("jdbc:h2:mem:mydb");
         builder.activate();
 
-        JndiTemplate jndiTemplate = new JndiTemplate();
-        ctx = (InitialContext) jndiTemplate.getContext();
+//        JndiTemplate jndiTemplate = new JndiTemplate();
+//        ctx = (InitialContext) jndiTemplate.getContext();
     }
 
     @Test
     void givenACompositeName_whenAddingAnElement_thenNameIncludesIt() throws Exception {
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        ctx = (InitialContext) jndiTemplate.getContext();
         Name objectName = new CompositeName("java:comp/env/jdbc");
 
         Enumeration<String> elements = objectName.getAll();
@@ -46,22 +52,28 @@ class JndiUnitTest {
 
     @Test
     void givenADataSource_whenAddingDriver_thenBind() throws Exception {
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        ctx = (InitialContext) jndiTemplate.getContext();
+
         ds.setDriverClassName("org.h2.Driver");
         ctx.bind("java:comp/env/jdbc/datasource", ds);
     }
 
     @Test
     void givenContext_whenLookupByName_thenValidDataSource() throws Exception {
+
+        JndiTemplate jndiTemplate = new JndiTemplate();
+        ctx = (InitialContext) jndiTemplate.getContext();
+
         DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/datasource");
 
         assertNotNull(ds);
         assertNotNull(ds.getConnection());
     }
-    
+
     @AfterAll
     static void tearDown() throws Exception {
        ctx.close();
+       ctx = null;
     }
-
-
 }
