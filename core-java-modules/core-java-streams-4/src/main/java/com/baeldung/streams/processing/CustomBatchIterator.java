0767e3525522deff8f3df4f6b3673311783d9a6f
alemoles <alemolerio@gmail.com>
2023-01-28 14:50:57 -0300

    BAEL-5924 Java 8 Stream with Batch Processing Support

--- core-java-modules/core-java-streams-4/src/main/java/com/baeldung/streams/processing/CustomBatchIterator.java
+++ core-java-modules/core-java-streams-4/src/main/java/com/baeldung/streams/processing/CustomBatchIterator.java
@@ -9,39 +9,47 @@ import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public class CustomBatchIterator<T> implements Iterator<List<T>> {
-    private final int batchSize;
-    private List<T> currentBatch;
-    private final Iterator<T> iterator;
+public class CustomBatchIterator {
 
-    public CustomBatchIterator(Iterator<T> sourceIterator, int batchSize) {
-        this.batchSize = batchSize;
-        this.iterator = sourceIterator;
-    }
-
-    @Override
-    public List<T> next() {
-        return currentBatch;
-    }
-
-    @Override
-    public boolean hasNext() {
-        prepareNextBatch();
-        return currentBatch != null && !currentBatch.isEmpty();
+    private CustomBatchIterator() {
+        // Do nothing
     }
 
     public static <T> Stream<List<T>> batchStreamOf(Stream<T> stream, int batchSize) {
-        return stream(new CustomBatchIterator<>(stream.iterator(), batchSize));
+        return stream(new InnerCustomBatchIterator<>(stream.iterator(), batchSize));
     }
 
     private static <T> Stream<T> stream(Iterator<T> iterator) {
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(iterator, ORDERED), false);
     }
 
-    private void prepareNextBatch() {
-        currentBatch = new ArrayList<>(batchSize);
-        while (iterator.hasNext() && currentBatch.size() < batchSize) {
-            currentBatch.add(iterator.next());
+    private static class InnerCustomBatchIterator<T> implements Iterator<List<T>> {
+
+        private final int batchSize;
+        private List<T> currentBatch;
+        private final Iterator<T> iterator;
+
+        public InnerCustomBatchIterator(Iterator<T> sourceIterator, int batchSize) {
+            this.batchSize = batchSize;
+            this.iterator = sourceIterator;
+        }
+
+        @Override
+        public List<T> next() {
+            prepareNextBatch();
+            return currentBatch;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iterator.hasNext();
+        }
+
+        private void prepareNextBatch() {
+            currentBatch = new ArrayList<>(batchSize);
+            while (iterator.hasNext() && currentBatch.size() < batchSize) {
+                currentBatch.add(iterator.next());
+            }
         }
     }
 }
