e613fb8563bc6e31e8eb8227199ef9c7e277f1d6
panagiotiskakos <panagiotis.kakos@libra-is.com>
2023-01-29 09:49:39 +0200

    [JAVA-15035] Upgraded to spring-boot-3 and java 17

--- httpclient-simple/src/test/java/com/baeldung/client/ClientLiveTest.java
+++ httpclient-simple/src/test/java/com/baeldung/client/ClientLiveTest.java
@@ -1,12 +1,16 @@
 package com.baeldung.client;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.net.ssl.SSLPeerUnverifiedException;
 
 import com.baeldung.client.spring.ClientConfig;
 import com.baeldung.web.dto.Foo;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -27,21 +31,25 @@ public class ClientLiveTest {
     // tests
 
     @Test
-    public final void whenContextIsBootstrapped_thenNoExceptions() {
+    void whenContextIsBootstrapped_thenNoExceptions() {
         //
     }
 
     @Test
-    public final void whenSecuredRestApiIsConsumed_then200OK() {
+    void whenSecuredRestApiIsConsumed_then200OK() {
         final ResponseEntity<Foo> responseEntity = secureRestTemplate.exchange("http://localhost:8082/httpclient-simple/api/foos/1", HttpMethod.GET, null, Foo.class);
         assertThat(responseEntity.getStatusCode().value(), is(200));
     }
 
-    @Test(expected = ResourceAccessException.class)
-    public final void whenHttpsUrlIsConsumed_thenException() {
+    @Test
+    void whenHttpsUrlIsConsumed_thenException() {
         final String urlOverHttps = "https://localhost:8443/httpclient-simple/api/bars/1";
-        final ResponseEntity<String> response = new RestTemplate().exchange(urlOverHttps, HttpMethod.GET, null, String.class);
-        assertThat(response.getStatusCode().value(), equalTo(200));
+
+        assertThrows(ResourceAccessException.class, () -> {
+            final ResponseEntity<String> response = new RestTemplate()
+                .exchange(urlOverHttps, HttpMethod.GET, null, String.class);
+            assertThat(response.getStatusCode().value(), equalTo(200));
+        });
     }
 
 }
