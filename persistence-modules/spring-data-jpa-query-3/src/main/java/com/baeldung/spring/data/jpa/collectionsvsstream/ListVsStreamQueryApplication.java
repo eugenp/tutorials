bc446e25d40278683b7107ca4a9a9913aa325693
Amit Kumatr <amit.kumar@fyndna.com>
2023-01-31 18:31:35 +0530

    removed unwanted code

--- persistence-modules/spring-data-jpa-query-3/src/main/java/com/baeldung/spring/data/jpa/collectionsvsstream/ListVsStreamQueryApplication.java
+++ persistence-modules/spring-data-jpa-query-3/src/main/java/com/baeldung/spring/data/jpa/collectionsvsstream/ListVsStreamQueryApplication.java
@@ -1,15 +1,12 @@
 package com.baeldung.spring.data.jpa.collectionsvsstream;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ListVsStreamQueryApplication {
 
-  @Autowired private UserRepository userRepository;
-
-  public static void main(String[] args) {
-    SpringApplication.run(ListVsStreamQueryApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ListVsStreamQueryApplication.class, args);
+    }
 }
