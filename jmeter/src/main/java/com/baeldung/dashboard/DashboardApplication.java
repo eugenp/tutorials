87c22e5be4d63cfcd6898bf4943c28ce1337803d
Muhammad Asif <muasif80@gmail.com>
2023-02-04 21:25:23 +0500

    BAEL-5990 is implemented to generate JMeter dashboard reports (#13345)
    * BAEL-5990 is implemented to generate JMeter dashboard reports
    
    * BAEL-5990 made a fix to use Random nextInt() method with single arg
    
    * BAEL-5990 updated code to use Arrays.asList rather than List.of
    
    * BAEL-5990 Replaced tabs with space
    
    * BAEL-5990 Removed unnecessary exception in the method declaration
    
    * BAEL-5990 Updated class name

---
+++ jmeter/src/main/java/com/baeldung/dashboard/DashboardApplication.java
@@ -0,0 +1,15 @@
+package com.baeldung.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, MongoAutoConfiguration.class, MongoRepositoriesAutoConfiguration.class, MongoDataAutoConfiguration.class })
+public class DashboardApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
