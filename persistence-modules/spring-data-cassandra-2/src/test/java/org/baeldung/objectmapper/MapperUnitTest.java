a17c7f730ad7ebe2eedb6baa94cf8926d1e7f61f
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-02-05 22:10:12 +0530

    BAEL-4971 - Object mapping with Cassandra

---
+++ persistence-modules/spring-data-cassandra-2/src/test/java/org/baeldung/objectmapper/MapperUnitTest.java
@@ -0,0 +1,69 @@
+package org.baeldung.objectmapper;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
+import com.datastax.oss.driver.api.core.CqlSession;
+import org.baeldung.objectmapper.dao.CounterDao;
+import org.baeldung.objectmapper.dao.UserDao;
+import org.baeldung.objectmapper.entity.Counter;
+import org.baeldung.objectmapper.entity.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class MapperUnitTest {
+
+    static UserDao userDao;
+    static CounterDao counterDao;
+
+    @BeforeAll
+    static void setup() {
+        CqlSession session = CqlSession.builder().build();
+
+        String createKeyspace = "CREATE KEYSPACE IF NOT EXISTS baeldung WITH replication = {'class':'SimpleStrategy', 'replication_factor':1};";
+        String useKeyspace = "USE baeldung;";
+        String createUserTable = "CREATE TABLE IF NOT EXISTS user_profile (id int, username text, user_age int, writetime bigint, PRIMARY KEY (id, user_age)) WITH CLUSTERING ORDER BY (user_age DESC);";
+        String createAdminTable = "CREATE TABLE IF NOT EXISTS admin_profile (id int, username text, user_age int, role text, writetime bigint, department text, PRIMARY KEY (id, user_age)) WITH CLUSTERING ORDER BY (user_age DESC);";
+        String createCounter = "CREATE TABLE IF NOT EXISTS counter (id text, count counter, PRIMARY KEY (id));";
+
+        session.execute(createKeyspace);
+        session.execute(useKeyspace);
+        session.execute(createUserTable);
+        session.execute(createAdminTable);
+        session.execute(createCounter);
+
+        DaoMapper mapper = new DaoMapperBuilder(session).build();
+        userDao = mapper.getUserDao(CqlIdentifier.fromCql("baeldung"));
+        counterDao = mapper.getUserCounterDao(CqlIdentifier.fromCql("baeldung"));
+    }
+
+    @Test
+    void givenUser_whenInsert_thenRetrievedDuringGet() {
+        User user = new User(1, "JohnDoe", 31);
+        userDao.insertUser(user);
+        User retrievedUser = userDao.getUserById(1);
+        assert retrievedUser.getUserName().equals(user.getUserName());
+    }
+
+    @Test
+    void givenCounter_whenIncrement_thenIncremented() {
+        Counter users = counterDao.getCounterById("users");
+        long initialCount = users != null ? users.getCount(): 0;
+
+        counterDao.incrementCounter("users", 1);
+
+        users = counterDao.getCounterById("users");
+        long finalCount = users != null ? users.getCount(): 0;
+
+        assert finalCount - initialCount == 1;
+    }
+
+    @Test
+    void givenUser_whenGetUsersOlderThan_thenRetrieved() {
+        User user = new User(2, "JaneDoe", 20);
+        userDao.insertUser(user);
+        List<User> retrievedUsers = userDao.getUsersOlderThanAge(30).all();
+        assert retrievedUsers.size() == 1;
+    }
+
+}
