9cc6f315d9af70e61c96148a26eee08c5ee7afcf
Prabhash <kumarprabhash1995@gmail.com>
2023-02-07 19:18:58 +0100

    BAEL-6155 removed unused files

--- quarkus-modules/quarkus-funqy/src/test/java/com/baeldung/quarkus/MyFunctionsIT.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.quarkus;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class MyFunctionsIT extends MyFunctionsTest {
-    // Run the same tests in native
-}
