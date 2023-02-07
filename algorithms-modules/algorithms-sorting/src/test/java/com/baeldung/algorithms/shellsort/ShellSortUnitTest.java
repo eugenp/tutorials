d47969ce2d3a9c225bc5b751baf248b4639f6a4a
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2023-02-07 14:42:21 +0000

    [JAVA-18123] Upgraded algorithms-modules to java 11 + Upgraded unit t… (#13437)
    * [JAVA-18123] Upgraded algorithms-modules to java 11 + Upgraded unit tests to junit5
    
    * [JAVA-18123] Clean up properties
    
    ---------
    
    Co-authored-by: panagiotiskakos <panagiotis.kakos@libra-is.com>

--- algorithms-modules/algorithms-sorting/src/test/java/com/baeldung/algorithms/shellsort/ShellSortUnitTest.java
+++ algorithms-modules/algorithms-sorting/src/test/java/com/baeldung/algorithms/shellsort/ShellSortUnitTest.java
@@ -1,17 +1,17 @@
 package com.baeldung.algorithms.shellsort;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertArrayEquals;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
-public class ShellSortUnitTest {
+import org.junit.jupiter.api.Test;
+
+class ShellSortUnitTest {
 
     @Test
-    public void givenUnsortedArray_whenShellSort_thenSortedAsc() {
+    void givenUnsortedArray_whenShellSort_thenSortedAsc() {
         int[] input = {41, 15, 82, 5, 65, 19, 32, 43, 8};
         ShellSort.sort(input);
         int[] expected = {5, 8, 15, 19, 32, 41, 43, 65, 82};
-        assertArrayEquals("the two arrays are not equal", expected, input);
+        assertArrayEquals( expected, input, "the two arrays are not equal");
     }
 }
