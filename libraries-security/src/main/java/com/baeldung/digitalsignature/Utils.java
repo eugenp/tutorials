8ce202b18606897ad68d64ef4c8d662e006420e2
h_sharifi <h_sharifi@modernisc.com>
2023-02-07 17:29:06 +0330

    #BAEL-5190: add keystore address

--- libraries-security/src/main/java/com/baeldung/digitalsignature/Utils.java
+++ libraries-security/src/main/java/com/baeldung/digitalsignature/Utils.java
@@ -13,6 +13,7 @@ public class Utils {
     private static final String SENDER_KEYSTORE = "sender_keystore.p12";
     private static final String SENDER_ALIAS = "senderKeyPair";
 
+    public static final String HASHING_ALGORITHM = "SHA-256";
     public static final String SIGNING_ALGORITHM = "SHA256withRSA";
 
     private static final String RECEIVER_KEYSTORE = "receiver_keystore.p12";
@@ -20,13 +21,13 @@ public class Utils {
 
     public static PrivateKey getPrivateKey() throws Exception {
         KeyStore keyStore = KeyStore.getInstance(STORE_TYPE);
-        keyStore.load(new FileInputStream(SENDER_KEYSTORE), PASSWORD);
+        keyStore.load(new FileInputStream("src/main/resources/digitalsignature/" + SENDER_KEYSTORE), PASSWORD);
         return (PrivateKey) keyStore.getKey(SENDER_ALIAS, PASSWORD);
     }
 
     public static PublicKey getPublicKey() throws Exception {
         KeyStore keyStore = KeyStore.getInstance(STORE_TYPE);
-        keyStore.load(new FileInputStream(RECEIVER_KEYSTORE), PASSWORD);
+        keyStore.load(new FileInputStream("src/main/resources/digitalsignature/" + RECEIVER_KEYSTORE), PASSWORD);
         Certificate certificate = keyStore.getCertificate(RECEIVER_ALIAS);
         return certificate.getPublicKey();
     }
