3c41afcd72a5f3fd40463bf80b206803fb0f7bcf
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-02-14 14:03:47 +0200

    JAVA-15014 Upgrade HttpClient 5

--- apache-httpclient-2/src/test/java/com/baeldung/httpclient/httpclient/ApacheHttpClientUnitTest.java
+++ apache-httpclient-2/src/test/java/com/baeldung/httpclient/httpclient/ApacheHttpClientUnitTest.java
@@ -1,5 +1,9 @@
 package com.baeldung.httpclient.httpclient;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+
 import org.apache.hc.client5.http.classic.HttpClient;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
@@ -7,14 +11,9 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.HttpEntity;
-import org.apache.hc.core5.http.HttpResponse;
 import org.apache.hc.core5.http.HttpStatus;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class ApacheHttpClientUnitTest extends GetRequestMockServer {
 
@@ -22,16 +21,20 @@ class ApacheHttpClientUnitTest extends GetRequestMockServer {
     void givenDeveloperUsedHttpClient_whenExecutingGetRequest_thenStatusIsOkButSonarReportsAnIssue() throws IOException {
         HttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(serviceOneUrl);
-        HttpResponse response = httpClient.execute(httpGet);
-        assertThat(response.getCode()).isEqualTo(HttpStatus.SC_OK);
+        httpClient.execute(httpGet, response -> {
+                assertThat(response.getCode()).isEqualTo(HttpStatus.SC_OK);
+                return null;
+        });
     }
 
     @Test
     void givenDeveloperUsedCloseableHttpClient_whenExecutingGetRequest_thenStatusIsOk() throws IOException {
         try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
             HttpGet httpGet = new HttpGet(serviceOneUrl);
-            HttpResponse response = httpClient.execute(httpGet);
-            assertThat(response.getCode()).isEqualTo(HttpStatus.SC_OK);
+            httpClient.execute(httpGet, response -> {
+                assertThat(response.getCode()).isEqualTo(HttpStatus.SC_OK);
+                return null;
+            });
         }
     }
 
@@ -39,8 +42,10 @@ class ApacheHttpClientUnitTest extends GetRequestMockServer {
     void givenDeveloperUsedHttpClientBuilder_whenExecutingGetRequest_thenStatusIsOk() throws IOException {
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
             HttpGet httpGet = new HttpGet(serviceOneUrl);
-            HttpResponse response = httpClient.execute(httpGet);
-            assertThat(response.getCode()).isEqualTo(HttpStatus.SC_OK);
+            httpClient.execute(httpGet, response -> {
+                assertThat(response.getCode()).isEqualTo(HttpStatus.SC_OK);
+                return null;
+            });
         }
     }
 
@@ -60,18 +65,20 @@ class ApacheHttpClientUnitTest extends GetRequestMockServer {
     void givenDeveloperUsedSingleClient_whenExecutingTwoGetRequest_thenStatusIsOk() throws IOException {
         try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
             HttpGet httpGetOne = new HttpGet(serviceOneUrl);
-            try (CloseableHttpResponse responseOne = httpClient.execute(httpGetOne)) {
+            httpClient.execute(httpGetOne, responseOne -> {
                 HttpEntity entityOne = responseOne.getEntity();
                 EntityUtils.consume(entityOne);
                 assertThat(responseOne.getCode()).isEqualTo(HttpStatus.SC_OK);
-            }
+                return null;
+            });
 
             HttpGet httpGetTwo = new HttpGet(serviceTwoUrl);
-            try (CloseableHttpResponse responseTwo = httpClient.execute(httpGetTwo)) {
-                HttpEntity entityTwo = responseTwo.getEntity();
+            httpClient.execute(httpGetTwo, responseTwo -> {
+                HttpEntity entityTwo = httpGetTwo.getEntity();
                 EntityUtils.consume(entityTwo);
                 assertThat(responseTwo.getCode()).isEqualTo(HttpStatus.SC_OK);
-            }
+                return null;
+            });
         }
     }
 
