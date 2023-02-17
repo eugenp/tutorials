65d0bcebf4f6988399351a422318029e7023aa91
timis1 <noreplay@yahoo.com>
2023-02-17 22:09:14 +0200

    JAVA-18133 Upgrade drools module to JDK 11

--- drools/src/main/java/com/baeldung/drools/config/DroolsBeanFactory.java
+++ drools/src/main/java/com/baeldung/drools/config/DroolsBeanFactory.java
@@ -10,26 +10,25 @@ import org.kie.internal.builder.DecisionTableConfiguration;
 import org.kie.internal.builder.DecisionTableInputType;
 import org.kie.internal.builder.KnowledgeBuilderFactory;
 import org.kie.internal.io.ResourceFactory;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
 public class DroolsBeanFactory {
 
     private static final String RULES_PATH = "com/baeldung/drools/rules/";
-    private KieServices kieServices=KieServices.Factory.get();
+    private KieServices kieServices = KieServices.Factory.get();
 
-    private  KieFileSystem getKieFileSystem() throws IOException{
+    private  KieFileSystem getKieFileSystem() {
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
-        List<String> rules=Arrays.asList("BackwardChaining.drl","SuggestApplicant.drl","Product_rules.xls");
-        for(String rule:rules){
+        List<String> rules = Arrays.asList("BackwardChaining.drl", "SuggestApplicant.drl", "Product_rules.xls");
+        for(String rule:rules) {
             kieFileSystem.write(ResourceFactory.newClassPathResource(rule));
         }
         return kieFileSystem;
 
     }
 
-    public KieContainer getKieContainer() throws IOException {
+    public KieContainer getKieContainer() {
         getKieRepository();
 
         KieBuilder kb = kieServices.newKieBuilder(getKieFileSystem());
@@ -44,21 +43,16 @@ public class DroolsBeanFactory {
 
     private void getKieRepository() {
         final KieRepository kieRepository = kieServices.getRepository();
-        kieRepository.addKieModule(new KieModule() {
-                        public ReleaseId getReleaseId() {
-                return kieRepository.getDefaultReleaseId();
-            }
-        });
+        kieRepository.addKieModule(kieRepository::getDefaultReleaseId);
     }
 
-    public KieSession getKieSession(){
+    public KieSession getKieSession() {
         getKieRepository();
         KieFileSystem kieFileSystem = kieServices.newKieFileSystem();
 
         kieFileSystem.write(ResourceFactory.newClassPathResource("com/baeldung/drools/rules/BackwardChaining.drl"));
         kieFileSystem.write(ResourceFactory.newClassPathResource("com/baeldung/drools/rules/SuggestApplicant.drl"));
         kieFileSystem.write(ResourceFactory.newClassPathResource("com/baeldung/drools/rules/Product_rules.xls"));
-        
         
         KieBuilder kb = kieServices.newKieBuilder(kieFileSystem);
         kb.buildAll();
@@ -67,7 +61,6 @@ public class DroolsBeanFactory {
         KieContainer kContainer = kieServices.newKieContainer(kieModule.getReleaseId());
 
         return kContainer.newKieSession();
-
     }
 
     public KieSession getKieSession(Resource dt) {
