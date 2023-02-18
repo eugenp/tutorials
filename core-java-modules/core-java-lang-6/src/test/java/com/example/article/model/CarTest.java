ef4fcf431d172a961f23609c1eb3a9bcb34731e2
Macbook Pro <macbookpro@mbp-de-macbook.lan>
2023-02-18 00:06:30 +0000

    code refactor

--- core-java-modules/core-java-lang-6/src/test/java/com/example/article/model/CarTest.java
+++ core-java-modules/core-java-lang-6/src/test/java/com/example/article/model/CarTest.java
@@ -10,7 +10,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class CarTest {
 
     @Test
-    public void given_three_copies_and_origin_changed_should_reflect_on_clones() throws CloneNotSupportedException {
+    public void testShallowCopyObject() throws CloneNotSupportedException {
+        List<String> accessories = new ArrayList<>();
+        accessories.add("item1");
+        accessories.add("item2");
+
+        Car car = new Car(accessories);
+        Car clone1 = (Car) car.clone();
+        Car clone2 = (Car) car.clone();
+
+        assertTrue(clone1.getAccessories() == car.getAccessories());
+        assertTrue(clone2.getAccessories() == car.getAccessories());
+        assertFalse(clone1 == car);
+        assertFalse(clone2 == car);
+        assertFalse(clone1 == clone2);
+    }
+
+    @Test
+    public void testShallowCopyChangingOrigin() throws CloneNotSupportedException {
         List<String> accessories = new ArrayList<>();
         accessories.add("item1");
         accessories.add("item2");
@@ -30,28 +47,22 @@ public class CarTest {
         assertEquals(clone1.getAccessories().size(), car.getAccessories().size(), "Clone 1 and car accessories list must have the same size after accessories list be changed.");
         assertEquals(clone2.getAccessories().size(), car.getAccessories().size(), "Clone 1 and car accessories list must have the same size after accessories list be changed.");
         assertEquals(clone3.getAccessories().size(), car.getAccessories().size(), "Clone 1 and car accessories list must have the same size after accessories list be changed.");
-
     }
 
     @Test
-    public void given_one_copy_and_origin_attribute_should_point_to_same_object() throws CloneNotSupportedException {
+    public void testDeepCopy(){
         List<String> accessories = new ArrayList<>();
         accessories.add("item1");
         accessories.add("item2");
 
         Car car = new Car(accessories);
-        Car clone1 = (Car) car.clone();
-        Car clone2 = (Car) car.clone();
+        Car clone1 = car.deepCopy();
 
-        assertTrue(clone1.getAccessories() == car.getAccessories());
-        assertTrue(clone2.getAccessories() == car.getAccessories());
-        assertFalse(clone1 == car);
-        assertFalse(clone2 == car);
-        assertFalse(clone1 == clone2);
+        assertFalse(clone1.getAccessories() == car.getAccessories());
     }
 
     @Test
-    public void given_deep_copy_done_clone_attribute_should_not_be_affect_by_changes_on_origin(){
+    public void testDeepCopyChangingOrigin(){
         List<String> accessories = new ArrayList<>();
         accessories.add("item1");
         accessories.add("item2");
@@ -66,17 +77,5 @@ public class CarTest {
         car.getAccessories().remove(1);
 
         assertNotEquals(clone1.getAccessories().size(), car.getAccessories().size(), "Clone 1 and car accessories list must have the same size.");
-    }
-
-    @Test
-    public void given_deep_copy_done_clone_attribute_should_point_to_different_reference(){
-        List<String> accessories = new ArrayList<>();
-        accessories.add("item1");
-        accessories.add("item2");
-
-        Car car = new Car(accessories);
-        Car clone1 = car.deepCopy();
-
-        assertFalse(clone1.getAccessories() == car.getAccessories());
     }
 }
