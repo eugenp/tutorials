c23de028b2439c51fb8274aaa6d292fab15ec2cf
Muhammad Asif <muasif80@gmail.com>
2023-02-18 20:15:09 +0500

    BAEL-6035 Initial Code Implementation

---
+++ spring-security-modules/spring-security-saml-2/src/test/java/com/baeldung/saml/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.saml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
