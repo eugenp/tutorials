95026e6b88ca79b2a1f54dcd6c9da98d0527848d
timis1 <12120641+timis1@users.noreply.github.com>
2023-02-20 20:15:47 +0200

    JAVA-14723 Convert spring-mvc-basics-4 to Spring Boot project and upd… (#13399)
    * JAVA-14723 Convert spring-mvc-basics-4 to Spring Boot project and update articles
    
    * JAVA-14723 Refactoring and remove unused files
    
    * JAVA-14723 remove the WEB-INF and migrate jsp to html
    
    * JAVA-14723 Add back the removed test and rename the test into: ArticleViewerControllerWithRequiredAttributeIntegrationTest
    
    ---------
    
    Co-authored-by: timis1 <noreplay@yahoo.com>

--- spring-web-modules/spring-mvc-basics-4/src/test/java/com/baeldung/jsonparams/JsonParamsIntegrationTest.java
+++ spring-web-modules/spring-mvc-basics-4/src/test/java/com/baeldung/jsonparams/JsonParamsIntegrationTest.java
@@ -1,26 +1,25 @@
 package com.baeldung.jsonparams;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import com.baeldung.validation.listvalidation.SpringListValidationApplication;
 
-import com.baeldung.jsonparams.config.JsonParamsConfig;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = { JsonParamsConfig.class }, loader = AnnotationConfigWebContextLoader.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringListValidationApplication.class)
 public class JsonParamsIntegrationTest {
 
     private MockMvc mockMvc;
