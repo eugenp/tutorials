675c24e16612e09da25fafbb4be446d3f13c372d
Lucas Silva <44280182+Daiven75@users.noreply.github.com>
2023-02-22 00:31:50 -0300

    rename class according to the entity

--- spring-cloud-modules/spring-cloud-open-telemetry/spring-cloud-open-telemetry2/src/main/java/com/baeldung/opentelemetry/exception/ProductControllerAdvice.java
+++ spring-cloud-modules/spring-cloud-open-telemetry/spring-cloud-open-telemetry2/src/main/java/com/baeldung/opentelemetry/exception/ProductControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ProductControllerAdvice {
+public class PriceControllerAdvice {
 
     @ExceptionHandler(PriceNotFoundException.class)
     public ResponseEntity<Object> handlePriceNotFoundException(PriceNotFoundException exception) {
