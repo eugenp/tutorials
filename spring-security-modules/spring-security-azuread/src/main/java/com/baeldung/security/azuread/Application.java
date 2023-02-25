a713d96d4da5056c2ebf2881941b26d10c5254ac
psevestre <phil@lighthouse.com.br>
2023-02-25 16:46:17 -0300

    BAEL 6160 (#13519)
    * [BAEL-4849] Article code
    
    * [BAEL-4968] Article code
    
    * [BAEL-4968] Article code
    
    * [BAEL-4968] Article code
    
    * [BAEL-4968] Remove extra comments
    
    * [BAEL-5258] Article Code
    
    * [BAEL-2765] PKCE Support for Secret Clients
    
    * [BAEL-5698] Article code
    
    * [BAEL-5698] Article code
    
    * [BAEL-5900] Initial commit
    
    * [BAEL-5974] Article Code
    
    * [BAEL-5974] Article Code
    
    * [BAEL-6160] Article code
    
    * [BAEL-6160] Tests
    
    ---------
    
    Co-authored-by: Philippe Sevestre <psevestre@gmail.com>

---
+++ spring-security-modules/spring-security-azuread/src/main/java/com/baeldung/security/azuread/Application.java
@@ -0,0 +1,14 @@
+package com.baeldung.security.azuread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
