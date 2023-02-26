aa0c9c7db590144021fa951cded2ca8068f51804
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2023-02-26 18:25:24 +0200

    [JAVA-18177]  Moved server modules to jdk9-and-above profile (#13523)
    * [JAVA-18177]  Moved server modules to jdk9-and-above profile
    
    * [JAVA-18177]  Upgraded maven-war-plugin version
    
    * [JAVA-18146]  Moved json-modules to jdk9-and-above profile
    
    * [JAVA-18151]  Moved libraries-data module to jdk9-and-above profile
    
    * [JAVA-18176] Moved saas-modules to jdk9-and-above profile
    
    * [JAVA-18165] Moved patterns-modules to jdk9-and-above profile
    
    * [JAVA-18146] Fixed tests at gson module
    
    * [JAVA-18165] Upgraded maven-war-plugin version
    
    * [JAVA-16377] Moved apache-cxf-module to jdk9-and-above profile + comment out cxf spring
    
    * [JAVA-18151] Testing maven compiler plugin
    
    * [JAVA-18151] Ignored 2 test cases
    
    * [JAVA-18151] import javassist dependency manually
    
    * [JAVA-16377] Upgraded spring version + uncomment cxf-spring module

--- apache-cxf-modules/cxf-jaxrs-implementation/src/main/java/com/baeldung/cxf/jaxrs/implementation/CourseRepository.java
+++ apache-cxf-modules/cxf-jaxrs-implementation/src/main/java/com/baeldung/cxf/jaxrs/implementation/CourseRepository.java
@@ -1,7 +1,7 @@
 package com.baeldung.cxf.jaxrs.implementation;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
