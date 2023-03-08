4fcb7acbb23877cdfc7a6b476867050bd46c04ec
Bala <balamurugan.radhakrishnan@yapily.com>
2023-03-08 16:51:53 +0000

    BAEL-5988: Removed unnecessary import

--- persistence-modules/r2dbc/src/main/java/com/baeldung/examples/r2dbc/flyway/config/DatabaseConfig.java
+++ persistence-modules/r2dbc/src/main/java/com/baeldung/examples/r2dbc/flyway/config/DatabaseConfig.java
@@ -2,16 +2,13 @@ package com.baeldung.examples.r2dbc.flyway.config;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
-import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @EnableConfigurationProperties({ R2dbcProperties.class, FlywayProperties.class })
-@Import(R2dbcAutoConfiguration.class)
 class DatabaseConfig {
     @Bean(initMethod = "migrate")
     public Flyway flyway(FlywayProperties flywayProperties, R2dbcProperties r2dbcProperties) {
