9c12f5f3bd948f1a9327c0ff009567587d2d30fe
timis1 <noreplay@yahoo.com>
2023-03-11 16:58:55 +0200

    JAVA-18609 Adding the property value for start class

--- spring-batch/src/test/java/com/baeldung/batch/SpringBatchRetryIntegrationTest.java
+++ spring-batch/src/test/java/com/baeldung/batch/SpringBatchRetryIntegrationTest.java
@@ -17,10 +17,8 @@ import org.springframework.batch.test.JobRepositoryTestUtils;
 import org.springframework.batch.test.context.SpringBatchTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.hamcrest.CoreMatchers.containsString;
