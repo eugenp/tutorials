8a8a1c98d314b9877fd00d09c261ad2b1782f1f1
Cesare <cesare.valenti@hotmail.com>
2023-03-13 18:51:33 +0100

    removed log from test

--- spring-boot-modules/spring-boot-aws/src/test/java/com/baeldung/ProfileIntegrationTest.java
+++ spring-boot-modules/spring-boot-aws/src/test/java/com/baeldung/ProfileIntegrationTest.java
@@ -19,9 +19,7 @@ public class ProfileIntegrationTest {
 
     @Test
     void whenTheUsersPathIsInvokedViaLambda_thenShouldReturnAList() throws IOException {
-        System.out.println("Hey Spring is not started yet");
         LambdaHandler lambdaHandler = new LambdaHandler();
-        System.out.println("Voila, Spring now is started");
         AwsProxyRequest req = new AwsProxyRequestBuilder("/api/v1/users", "GET").build();
         AwsProxyResponse resp = lambdaHandler.handleRequest(req, lambdaContext);
         System.out.println(resp.getBody());
@@ -31,9 +29,7 @@ public class ProfileIntegrationTest {
 
     @Test
     void whenWrongPathPathIsInvokedViaLambda_thenShouldNotFound() throws IOException {
-        System.out.println("Hey Spring is not started yet");
         LambdaHandler lambdaHandler = new LambdaHandler();
-        System.out.println("Voila, Spring now is started");
         AwsProxyRequest req = new AwsProxyRequestBuilder("/api/v1/users/plus-one-level", "GET").build();
         AwsProxyResponse resp = lambdaHandler.handleRequest(req, lambdaContext);
         Assertions.assertEquals(resp.getStatusCode(), 404);
