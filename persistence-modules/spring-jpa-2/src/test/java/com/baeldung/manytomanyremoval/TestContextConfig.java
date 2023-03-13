6dd09b5c1ed1f7eae6cf87311512b242e39b76a6
Ana Peterlic <apeterlic@gmail.com>
2023-03-13 09:16:44 +0100

    BAEL-6189 - Remove Entity with ManyToMany Relationship in JPA

---
+++ persistence-modules/spring-jpa-2/src/test/java/com/baeldung/manytomanyremoval/TestContextConfig.java
@@ -0,0 +1,10 @@
+package com.baeldung.manytomanyremoval;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.baeldung.manytomanyremoval")
+public class TestContextConfig {
+
+}
