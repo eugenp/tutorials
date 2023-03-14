7d02e485ddebb69d2daf20e082bc139cfd03d37b
Sudarshan Hiray <sdhiray7@gmail.com>
2023-03-14 23:55:41 +0530

    [BAEL-5321] constructor

--- spring-boot-modules/spring-boot-swagger-2/src/main/java/com/baeldung/springdoc/demo/controller/TopicsController.java
+++ spring-boot-modules/spring-boot-swagger-2/src/main/java/com/baeldung/springdoc/demo/controller/TopicsController.java
@@ -1,8 +1,8 @@
 package com.baeldung.springdoc.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,21 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baeldung.springdoc.demo.model.Topic;
+import com.baeldung.springdoc.demo.service.TopicService;
 
 
 @RestController
 @RequestMapping(path = "/")
 public class TopicsController {
 	
-	List<Topic> topics = new ArrayList<Topic>() {{
-		add(new Topic(1, "Topic1"));
-		add(new Topic(2, "Topic2"));
-		add(new Topic(3, "Topic3"));
-	}};
-	
+	@Autowired
+	TopicService topicService;
 	
 	@GetMapping(value = "/topics")
 	public ResponseEntity<List<Topic>> getAllTopics() {
-		return new ResponseEntity<>(topics, HttpStatus.OK);
+		ResponseEntity<List<Topic>> response = new ResponseEntity<>(topicService.getAlllTopics(), HttpStatus.OK);
+		return response;
 	}
 }
