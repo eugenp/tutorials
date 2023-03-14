d129cf7eee74af01239a0d0a0ad9cc7f477f3e5c
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-03-14 20:59:10 +0530

    BAEL-6157 - Changing test class name

--- spring-boot-modules/spring-boot-3/src/test/java/com/baeldung/recordswithjpa/repository/CustomBookRepositoryIntegrationTest.java
+++ spring-boot-modules/spring-boot-3/src/test/java/com/baeldung/recordswithjpa/repository/CustomBookRepositoryIntegrationTest.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CustomBookRepositoryImplIntegrationTest extends RecordsAsJpaIntegrationTest {
+class CustomBookRepositoryIntegrationTest extends RecordsAsJpaIntegrationTest {
 
     @Autowired
     private CustomBookRepository customBookRepository;
