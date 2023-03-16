db6484d4e1a1a5f6c3221a461bbe68d8cb8ab76c
timis1 <noreplay@yahoo.com>
2023-03-16 09:52:26 +0200

    JAVA-18601 Update "Java Bean Validation Basics" article

--- javaxval-2/src/main/java/com/baeldung/javaxval/hibernate/validator/ap/Message.java
+++ javaxval-2/src/main/java/com/baeldung/javaxval/hibernate/validator/ap/Message.java
@@ -3,9 +3,9 @@ package com.baeldung.javaxval.hibernate.validator.ap;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 
 public class Message {
 
