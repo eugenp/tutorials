b27e46334ba9333ff7192a566c3fa8a4c4dab14e
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-03-20 22:17:03 +0530

    BAEL-5936 - Extracting methods and adding tests

---
+++ jackson-modules/jackson-custom-conversions/src/test/java/com/baeldung/offsetdatetime/MainTest.java
@@ -0,0 +1,25 @@
+package com.baeldung.offsetdatetime;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void serializeUser() throws JsonProcessingException {
+        Assertions.assertEquals("{\"createdAt\":\"2021-09-30T15:30:00+01:00\"}", Main.serializeUser());
+    }
+
+    @Test
+    void customSerialize() throws JsonProcessingException {
+        Assertions.assertEquals("{\"createdAt\":\"30-09-2021 15:30:00 +01:00\"}", Main.customSerialize());
+    }
+
+    @Test
+    void customDeserialize() throws JsonProcessingException {
+        Assertions.assertEquals("2021-09-30T15:30+01:00", Main.customDeserialize());
+    }
+}
