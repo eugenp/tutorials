30cd0b6bf41249e308e8696e5f45a5294b8cb9b7
Deboshree <deboshree.banerjee@cred.club>
2023-03-22 01:15:19 +0530

    https://jira.baeldung.com/browse/BAEL-5874 1. changing test to use constructor instead of lombok plugin 2. reverting lombok plugin addition from pom.xml

--- core-java-modules/core-java-date-operations-1/src/test/java/com/baeldung/date/DayOfTheWeekUnitTest.java
+++ core-java-modules/core-java-date-operations-1/src/test/java/com/baeldung/date/DayOfTheWeekUnitTest.java
@@ -6,26 +6,22 @@ import java.util.List;
 import java.util.Locale;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import lombok.Builder;
-import lombok.Setter;
 
-class DayOfTheWeekUnitTest {
+public class DayOfTheWeekUnitTest {
 
     DayOfTheWeek dayOfTheWeek = new DayOfTheWeek();
 
     @Test
-    void whenInputDateIsMonday_ThenFirstDayOfWeekIsSameAsInputDay() {
-        TestData testData =
-                TestData.builder().inputDate(LocalDate.of(2023, 2, 20)).expectedOutput(LocalDate.of(2023,2,20)).build();
+    public void whenInputDateIsMonday_ThenFirstDayOfWeekIsSameAsInputDay() {
+        TestData testData = new TestData(LocalDate.of(2023, 2, 20),LocalDate.of(2023,2,20) );
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingISODayOfWeek(testData.inputDate));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingCalendar(testData.inputDate));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingDayOfWeek(testData.inputDate));
     }
 
     @Test
-    void whenInputDateIsSunday_ThenFirstDayOfWeekIsPreviousWeekMonday() {
-        TestData testData =
-                TestData.builder().inputDate(LocalDate.of(2023, 2, 19)).expectedOutput(LocalDate.of(2023,2,13)).build();
+    public void whenInputDateIsSunday_ThenFirstDayOfWeekIsPreviousWeekMonday() {
+        TestData testData = new TestData(LocalDate.of(2023, 2, 19), LocalDate.of(2023,2,13));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingISODayOfWeek(testData.inputDate));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingCalendar(testData.inputDate));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingDayOfWeek(testData.inputDate));
@@ -33,8 +29,7 @@ class DayOfTheWeekUnitTest {
 
     @Test
     void whenInputDateIsNewYearMidWeek_ThenFirstDayOfWeekIsPreviousYearLastMonday() {
-        TestData testData =
-                TestData.builder().inputDate(LocalDate.of(2022, 1, 1)).expectedOutput(LocalDate.of(2021,12,27)).build();
+        TestData testData = new TestData(LocalDate.of(2022, 1, 1), LocalDate.of(2021,12,27));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingISODayOfWeek(testData.inputDate));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingCalendar(testData.inputDate));
         Assertions.assertEquals(testData.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingDayOfWeek(testData.inputDate));
@@ -43,31 +38,31 @@ class DayOfTheWeekUnitTest {
     @Test
     void whenInputDataHasLocale_ThenFirstDayOfWeekIsMondayOrSundayInDifferentLocale() {
         List<TestData> testData = Arrays.asList(
-                TestData.builder().inputLocale(Locale.FRANCE).
-                        inputDate(LocalDate.of(2023, 2, 20)).expectedOutput(LocalDate.of(2023,2,20)).build(),
-                TestData.builder().inputLocale(Locale.FRANCE)
-                        .inputDate(LocalDate.of(2023, 2, 19)).expectedOutput(LocalDate.of(2023,2,13)).build(),
-                //Note that the first week may start in the previous calendar year.
-                TestData.builder().inputLocale(Locale.FRANCE)
-                        .inputDate(LocalDate.of(2022, 1, 1)).expectedOutput(LocalDate.of(2021,12,27)).build(),
-
-                // week starts from Sunday in the US
-                TestData.builder().inputLocale(Locale.US).
-                        inputDate(LocalDate.of(2023, 2, 20)).expectedOutput(LocalDate.of(2023,2,19)).build(),
-                TestData.builder().inputLocale(Locale.US)
-                        .inputDate(LocalDate.of(2023, 2, 19)).expectedOutput(LocalDate.of(2023,2,19)).build(),
-                //Note that the first week may start in the previous calendar year.
-                TestData.builder().inputLocale(Locale.US)
-                        .inputDate(LocalDate.of(2022, 1, 1)).expectedOutput(LocalDate.of(2021,12,26)).build()
+                new TestData(LocalDate.of(2023, 2, 20),Locale.FRANCE,LocalDate.of(2023,2,20)),
+                new TestData(LocalDate.of(2023, 2, 19),Locale.FRANCE,LocalDate.of(2023,2,13)),
+                new TestData(LocalDate.of(2022, 1, 1),Locale.FRANCE,LocalDate.of(2021,12,27)),
+                // day of week starts from sunday in USA
+                new TestData(LocalDate.of(2023, 2, 20),Locale.US,LocalDate.of(2023,2,19)),
+                new TestData(LocalDate.of(2023, 2, 19),Locale.US,LocalDate.of(2023,2,19)),
+                new TestData(LocalDate.of(2022, 1, 1),Locale.US,LocalDate.of(2021,12,26))
         );
         testData.forEach(data -> Assertions.assertEquals(data.expectedOutput, dayOfTheWeek.getDateOfFirstDayOfTheWeek_UsingLocale(data.inputDate,data.inputLocale)));
     }
 
-    @Builder
-    @Setter
     private static class TestData{
-        LocalDate inputDate;
-        Locale inputLocale;
-        LocalDate expectedOutput;
+        private final LocalDate inputDate;
+        private Locale inputLocale;
+        private final LocalDate expectedOutput;
+
+        private TestData(LocalDate inputDate, LocalDate expectedOutput){
+            this.inputDate = inputDate;
+            this.expectedOutput = expectedOutput;
+        }
+
+        private TestData(LocalDate inputDate, Locale inputLocale, LocalDate expectedOutput){
+            this.inputDate = inputDate;
+            this.inputLocale = inputLocale;
+            this.expectedOutput = expectedOutput;
+        }
     }
 }
