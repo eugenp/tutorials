0375f77c63169eaeb94eb922f3ec33f79b245d8e
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-03-25 12:53:23 +0530

    BAEL-5936 - Changing test method names

--- jackson-modules/jackson-custom-conversions/src/test/java/com/baeldung/offsetdatetime/MainUnitTest.java
+++ jackson-modules/jackson-custom-conversions/src/test/java/com/baeldung/offsetdatetime/MainUnitTest.java
@@ -7,17 +7,17 @@ import org.junit.jupiter.api.Test;
 class MainUnitTest {
 
     @Test
-    void serializeUser() throws JsonProcessingException {
+    void givenUser_whenSerialized_thenCreatedDateIsSerialized() throws JsonProcessingException {
         Assertions.assertEquals("{\"createdAt\":\"2021-09-30T15:30:00+01:00\"}", Main.serializeUser());
     }
 
     @Test
-    void customSerialize() throws JsonProcessingException {
+    void givenUser_whenCustomSerialized_thenCreatedDateIsSerialized() throws JsonProcessingException {
         Assertions.assertEquals("{\"createdAt\":\"30-09-2021 15:30:00 +01:00\"}", Main.customSerialize());
     }
 
     @Test
-    void customDeserialize() throws JsonProcessingException {
+    void givenUser_whenCustomDeserialized_thenCreatedDateIsDeserialized() throws JsonProcessingException {
         Assertions.assertEquals("2021-09-30T15:30+01:00", Main.customDeserialize());
     }
 }
