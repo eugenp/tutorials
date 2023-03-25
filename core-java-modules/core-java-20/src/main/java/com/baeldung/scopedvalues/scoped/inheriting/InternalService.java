d8e645ef5edfef8e42c3d5488edac9b08e63ce96
Daniel Strmecki <daniel.strmecki@gmail.com>
2023-03-25 16:08:30 +0100

    BAEL-6254: Refactor

--- core-java-modules/core-java-20/src/main/java/com/baeldung/scopedvalues/scoped/inheriting/InternalService.java
+++ core-java-modules/core-java-20/src/main/java/com/baeldung/scopedvalues/scoped/inheriting/InternalService.java
@@ -1,7 +1,6 @@
 package com.baeldung.scopedvalues.scoped.inheriting;
 
 import com.baeldung.scopedvalues.data.Data;
-import com.baeldung.scopedvalues.data.User;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.Optional;
