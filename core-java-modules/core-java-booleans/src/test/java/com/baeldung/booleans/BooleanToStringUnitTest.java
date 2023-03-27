de3e3015ce961558ed0f74ed76db2bce20bcd4f0
Kent@lhind.hp.g5 <kent.yuan@Gmail.com>
2023-03-27 22:58:37 +0200

    [BAEL-6297_boolean-to-str] convert boolean to java article

---
+++ core-java-modules/core-java-booleans/src/test/java/com/baeldung/booleans/BooleanToStringUnitTest.java
@@ -0,0 +1,48 @@
+package com.baeldung.booleans;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class BooleanToStringUnitTest {
+    String optionToString(String optionName, boolean optionValue) {
+        return String.format("The option '%s' is %s.", optionName, optionValue ? "Enabled" : "Disabled");
+    }
+
+    @Test
+    void whenConvertingPrimitiveBoolean_shouldTernaryOperatorWork() {
+        boolean primitiveBoolean = true;
+        assertEquals("true", primitiveBoolean ? "true" : "false");
+
+        primitiveBoolean = false;
+        assertEquals("false", primitiveBoolean ? "true" : "false");
+
+        assertEquals("The option 'IgnoreWarnings' is Enabled.", optionToString("IgnoreWarnings", true));
+    }
+
+
+    @Test
+    void whenBooleanObject_shouldBooleanToStringWork() {
+        Boolean myBoolean = Boolean.TRUE;
+        assertEquals("true", myBoolean.toString());
+
+        myBoolean = false;
+        assertEquals("false", myBoolean.toString());
+
+        Boolean nullBoolean = null;
+        assertThrows(NullPointerException.class, () -> nullBoolean.toString());
+    }
+
+    @Test
+    void whenBooleanObject_shouldStringValueOfWork() {
+        Boolean myBoolean = Boolean.TRUE;
+        assertEquals("true", String.valueOf(myBoolean));
+
+        myBoolean = false;
+        assertEquals("false", String.valueOf(myBoolean));
+
+        Boolean nullBoolean = null;
+        assertEquals("null", String.valueOf(nullBoolean));
+    }
+}
