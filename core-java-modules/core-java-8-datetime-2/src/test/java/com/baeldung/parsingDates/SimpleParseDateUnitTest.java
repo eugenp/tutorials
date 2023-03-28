3834403b9c7e96c073634971d85a23fb9785c54c
tgvLt <mantaspm@gmail.com>
2023-03-28 16:43:59 +0300

    Update SimpleParseDateUnitTest.java

--- core-java-modules/core-java-8-datetime-2/src/test/java/com/baeldung/parsingDates/SimpleParseDateUnitTest.java
+++ core-java-modules/core-java-8-datetime-2/src/test/java/com/baeldung/parsingDates/SimpleParseDateUnitTest.java
@@ -12,31 +12,31 @@ import static org.junit.Assert.*;
 public class SimpleParseDateUnitTest {
 
     @Test
-    public void testDateParse() {
+    public void whenInvalidInput_thenGettingUnexpectedResult() {
         SimpleParseDate simpleParseDate = new SimpleParseDate();
         String date = "2022-40-40";
-        assertEquals(simpleParseDate.parseDate(date, Arrays.asList("MM/dd/yyyy", "dd.MM.yyyy", "yyyy-MM-dd")), "Sat May 10 00:00:00 UTC 2025");
+        assertEquals("Sat May 10 00:00:00 UTC 2025", simpleParseDate.parseDate(date, Arrays.asList("MM/dd/yyyy", "dd.MM.yyyy", "yyyy-MM-dd")).toString());
     }
 
     @Test
-    public void testSimpleDateTimeParse() {
+    public void whenInvalidDate_thenAssertThrows() {
         SimpleDateTimeFormater simpleDateTimeFormater = new SimpleDateTimeFormater();
-        assertEquals(simpleDateTimeFormater.parseDate("2022-12-04", "2022-12-04"));
+        assertEquals("2022-12-04", simpleDateTimeFormater.parseDate("2022-12-04"));
         assertThrows(DateTimeParseException.class, () -> simpleDateTimeFormater.parseDate("2022-13-04"));
     }
 
     @Test
-    public void testDateUtils() {
+    public void whenDateIsCorrect_thenParseCorrect() {
         SimpleDateUtils simpleDateUtils = new SimpleDateUtils();
         assertNull(simpleDateUtils.parseDate("53/10/2014"));
-        assertEquals(simpleDateUtils.parseDate("10/09/2014","10/09/2014"));
+        assertEquals("10/09/2014", simpleDateUtils.parseDate("10/09/2014"));
     }
 
     @Test
-    public void testJodaTime() {
+    public void whenDateIsCorrect_thenResultCorrect() {
         SimpleDateTimeFormat simpleDateUtils = new SimpleDateTimeFormat();
         assertNull(simpleDateUtils.parseDate("53/10/2014"));
-        assertNotNull(simpleDateUtils.parseDate("10/10/2014"));
+        assertEquals(LocalDate.parse("2014-10-10"), simpleDateUtils.parseDate("2014-10-10"));
     }
 
 }
