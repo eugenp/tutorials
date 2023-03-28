035b3d9d5ed885d4a2460978754cc7f144e0943d
tgvLt <mantaspm@gmail.com>
2023-03-28 16:46:16 +0300

    Update SimpleParseDate.java

--- core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleParseDate.java
+++ core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleParseDate.java
@@ -2,17 +2,15 @@ package com.baeldung.parsingDates;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class SimpleParseDate {
 
-    public static String parseDate(String dateString, List<String> formatStrings) {
+    public Date parseDate(String dateString, List<String> formatStrings) {
         for (String formatString : formatStrings) {
             try {
-                return new SimpleDateFormat(formatString).parse(dateString).toString();
+                return new SimpleDateFormat(formatString).parse(dateString);
             } catch (ParseException e) {
             }
         }
