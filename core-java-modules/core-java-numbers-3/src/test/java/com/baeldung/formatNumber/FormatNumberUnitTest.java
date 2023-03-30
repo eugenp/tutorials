11f740192c10b608b2d471ea151fe144d2a8d061
Anastasios Ioannidis <121166333+anastasiosioannidis@users.noreply.github.com>
2023-03-30 23:09:12 +0300

    JAVA-18131 Upgrade core-java-modules to JDK 11 (#13462)
    * JAVA-18131 Upgrade core-java-modules to JDK 11
    
    * JAVA-18131 Fixed pom conflicts
    
    * JAVA-18131 Upgraded ASM and updated expected test results
    
    * JAVA-18131 Fixed FormatNumberUnitTest and removed core-java-security from upgrading to 17
    
    * JAVA-18131 Removed core-java-sun from upgrading
    
    * JAVA-18131 Removed core-java-jvm-2, fixed number test
    
    * JAVA-18131 Removed core-java-sun from upgrading to 11
    
    * JAVA-18131 Removed all edited submodules from pre-jdk9
    
    * JAVA-18131 removed pre-jpms from pre-jdk9
    
    * JAVA-18131 Removed core-java-numbers-3 from pre-jdk9 profiles
    
    * JAVA-18131 Cleanup
    
    * JAVA-18131 Cleanup 2 - Removed upgraded submodules from core-java-modules
    
    * JAVA-18131 Cleanup completed
    
    * JAVA-18131 Replaced core-java-modules submodules in main pom with core-java-modules
    
    * JAVA-18131 Replaced core-java-modules submodules in main pom, continued
    
    * JAVA-18131 Core-java-nio-2, core-java-serialization back to pre-jdk9
    
    * JAVA-18131 Try removing math-3 from core-java-modules and explicitly refer in parent pom
    
    * JAVA-18131 Removed -lang, -nio, -streams-2, -math-3, -conversions-2 from jdk9 profiles
    
    * JAVA-18131 Corrected conversions-2 module
    
    * JAVA-18131 Review fixes
    
    * JAVA-18131 Fixed -nio, re-upgraded -lang
    
    * JAVA-18131 Adapted -nio for JDK 11, returned -lang to JDK 8
    
    * JAVA-18131 Final corrections

--- core-java-modules/core-java-numbers-3/src/test/java/com/baeldung/formatNumber/FormatNumberUnitTest.java
+++ core-java-modules/core-java-numbers-3/src/test/java/com/baeldung/formatNumber/FormatNumberUnitTest.java
@@ -75,8 +75,8 @@ public class FormatNumberUnitTest {
     public void givenCurrency_whenFormatNumberCurrencyWithChosenLocalisation_thenGetExpectedResult() {
         double value = 23_500;
         assertThat(currencyWithChosenLocalisation(value, new Locale("en", "US"))).isEqualTo("$23,500.00");
-        assertThat(currencyWithChosenLocalisation(value, new Locale("zh", "CN"))).isEqualTo("￥23,500.00");
-        assertThat(currencyWithChosenLocalisation(value, new Locale("pl", "PL"))).isEqualTo("23 500 zł");
+        assertThat(currencyWithChosenLocalisation(value, new Locale("zh", "CN"))).isEqualTo("¥23,500.00");
+        assertThat(currencyWithChosenLocalisation(value, new Locale("pl", "PL"))).isEqualTo("23 500,00 zł");
     }
 
 }
