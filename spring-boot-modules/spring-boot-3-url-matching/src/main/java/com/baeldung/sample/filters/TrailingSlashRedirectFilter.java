c85846ea8196c7f10226d507800a128bf7546d4b
Viktor Ardelean <viktor.ardelean.dev@gmail.com>
2023-03-31 13:20:30 +0300

    BAEL-6093 add spring boot 3 url matching code sample

---
+++ spring-boot-modules/spring-boot-3-url-matching/src/main/java/com/baeldung/sample/filters/TrailingSlashRedirectFilter.java
@@ -0,0 +1,52 @@
+package com.baeldung.sample.filters;
+
+import java.io.IOException;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+import org.springframework.stereotype.Component;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class TrailingSlashRedirectFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String path = httpRequest.getRequestURI();
+
+        if (path.endsWith("/")) {
+            String newPath = path.substring(0, path.length() - 1);
+            HttpServletRequest newRequest = new CustomHttpServletRequestWrapper(httpRequest, newPath);
+            chain.doFilter(newRequest, response);
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+
+    private static class CustomHttpServletRequestWrapper extends HttpServletRequestWrapper {
+
+        private final String newPath;
+
+        public CustomHttpServletRequestWrapper(HttpServletRequest request, String newPath) {
+            super(request);
+            this.newPath = newPath;
+        }
+
+        @Override
+        public String getRequestURI() {
+            return newPath;
+        }
+
+        @Override
+        public StringBuffer getRequestURL() {
+            StringBuffer url = new StringBuffer();
+            url.append(getScheme()).append("://").append(getServerName()).append(":").append(getServerPort())
+                    .append(newPath);
+            return url;
+        }
+    }
+}
