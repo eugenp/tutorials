ce48b2aa1e007a600c581d88bc6c58e4a903a389
cesarevalenti90 <cesarevalenti90@gmail.com>
2023-04-05 12:33:44 +0200

    BAEL-6277 - A Guide To Spring Cloud Azure Key Vault
    Revert for new pr

--- spring-cloud-modules/spring-cloud-azure/src/main/java/com/baeldung/spring/cloud/azure/keyvault/Application.java
+++ spring-cloud-modules/spring-cloud-azure/src/main/java/com/baeldung/spring/cloud/azure/keyvault/Application.java
@@ -29,8 +29,8 @@ public class Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        KeyVaultSecret keyVaultSecret = keyVaultClient.getSecret("my-secret");
-        System.out.println("Hey, our secret is here ->" + keyVaultSecret.getValue());
-        System.out.println("Hey, our secret is here from application properties file ->" + mySecret);
+        //KeyVaultSecret keyVaultSecret = keyVaultClient.getSecret("my-secret");
+        //System.out.println("Hey, our secret is here ->" + keyVaultSecret.getValue());
+        //System.out.println("Hey, our secret is here from application properties file ->" + mySecret);
     }
 }
