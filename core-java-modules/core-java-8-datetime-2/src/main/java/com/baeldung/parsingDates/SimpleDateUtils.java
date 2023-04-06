effa4b855a659f383b9dba43f0eae307ea80293a
tgvLt <mantaspm@gmail.com>
2023-04-06 15:24:12 +0300

    Update SimpleDateUtils.java

--- core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleDateUtils.java
+++ core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/parsingDates/SimpleDateUtils.java
@@ -1,18 +1,15 @@
 package com.baeldung.parsingDates;
 
-import java.text.DateFormat;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.Date;
 import org.apache.commons.lang3.time.DateUtils;
 
 public class SimpleDateUtils {
 
-    public static String parseDate(String date) {
-
-        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    public static Date parseDate(String date) {
         try {
-            return df.format(DateUtils.parseDateStrictly(date,
-                    new String[]{"yyyy/MM/dd", "dd/MM/yyyy", "yyyy-MM-dd"}));
+            return DateUtils.parseDateStrictly(date,
+                    new String[]{"yyyy/MM/dd", "dd/MM/yyyy", "yyyy-MM-dd"});
         } catch (ParseException ex) {
             return null;
         }
