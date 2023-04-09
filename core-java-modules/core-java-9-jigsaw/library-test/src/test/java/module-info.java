5effc55384b11961a1112ee97e7b4e6f12e7c4e2
Eugene Kovko <eu.kovko@gmail.com>
2023-04-09 14:00:19 +0200

    feat: Renamed test module

--- core-java-modules/core-java-9-jigsaw/library-test/src/test/java/module-info.java
+++ core-java-modules/core-java-9-jigsaw/library-test/src/test/java/module-info.java
@@ -1,4 +1,4 @@
-module test {
+module com.baeldung.library.test {
     requires com.baeldung.library.core;
     requires org.junit.jupiter.api;
     opens com.baeldung.library.test to org.junit.platform.commons;
