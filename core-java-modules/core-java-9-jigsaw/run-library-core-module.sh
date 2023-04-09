cb60b5051b5b11f69fa94f16b3698cd7a9406da2
Eugene Kovko <eu.kovko@gmail.com>
2023-04-09 14:33:25 +0200

    feat: Added scripts for running core module

---
+++ core-java-modules/core-java-9-jigsaw/run-library-core-module.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+java -p  mods/com.baeldung.library.core:\
+/Users/yauhenikauko/.m2/repository/org/junit/jupiter/junit-jupiter-engine/5.9.2/junit-jupiter-engine-5.9.2.jar:\
+/Users/yauhenikauko/.m2/repository/org/junit/platform/junit-platform-engine/1.9.2/junit-platform-engine-1.9.2.jar:\
+/Users/yauhenikauko/.m2/repository/org/apiguardian/apiguardian-api/1.1.2/apiguardian-api-1.1.2.jar:\
+/Users/yauhenikauko/.m2/repository/org/junit/jupiter/junit-jupiter-params/5.9.2/junit-jupiter-params-5.9.2.jar:\
+/Users/yauhenikauko/.m2/repository/org/junit/jupiter/junit-jupiter-api/5.9.2/junit-jupiter-api-5.9.2.jar:\
+/Users/yauhenikauko/.m2/repository/org/opentest4j/opentest4j/1.2.0/opentest4j-1.2.0.jar:\
+/Users/yauhenikauko/.m2/repository/org/junit/platform/junit-platform-commons/1.9.2/junit-platform-commons-1.9.2.jar \
+-m com.baeldung.library.core/com.baeldung.library.core.Main
