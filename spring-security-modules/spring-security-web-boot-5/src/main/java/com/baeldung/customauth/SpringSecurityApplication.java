560c68e82ff4aca56169dce2da04d24990eeab77
Saikat <saikatcse03@gmail.com>
2023-04-09 19:17:14 +0530

    exclude auto configuration

--- spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/customauth/SpringSecurityApplication.java
+++ spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/customauth/SpringSecurityApplication.java
@@ -2,8 +2,9 @@ package com.baeldung.customauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
