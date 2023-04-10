94ed7fd56c5b5f737693ce629ad9e39a142fd228
Ulisses Lima <ulisses@dvlcube.com>
2023-04-10 12:42:55 -0300

    auto encryption config

--- persistence-modules/spring-boot-persistence-mongodb-3/src/main/java/com/baeldung/boot/csfle/config/MongoClientConfig.java
+++ persistence-modules/spring-boot-persistence-mongodb-3/src/main/java/com/baeldung/boot/csfle/config/MongoClientConfig.java
@@ -1,14 +1,14 @@
 package com.baeldung.boot.csfle.config;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.bson.BsonBinary;
 import org.bson.BsonDocument;
-import org.bson.Document;
-import org.bson.conversions.Bson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,13 +20,8 @@ import com.mongodb.ClientEncryptionSettings;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoClientSettings.Builder;
-import com.mongodb.MongoNamespace;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.model.Filters;
-import com.mongodb.client.model.IndexOptions;
-import com.mongodb.client.model.Indexes;
 import com.mongodb.client.model.vault.DataKeyOptions;
 import com.mongodb.client.vault.ClientEncryption;
 import com.mongodb.client.vault.ClientEncryptions;
@@ -53,10 +48,9 @@ public class MongoClientConfig extends AbstractMongoClientConfiguration {
     public MongoClient mongoClient() {
         MongoClient client;
         try {
-            client = MongoClients.create(clientSettings());
-
             ClientEncryption encryption = clientEncryption();
-            encryptionConfig.setDataKeyId(createOrRetrieveDataKey(client, encryption));
+            encryptionConfig.setDataKeyId(createOrRetrieveDataKey(encryption));
+            client = MongoClients.create(clientSettings());
 
             return client;
         } catch (IOException e) {
@@ -77,20 +71,9 @@ public class MongoClientConfig extends AbstractMongoClientConfiguration {
         return ClientEncryptions.create(encryptionSettings);
     }
 
-    private BsonBinary createOrRetrieveDataKey(MongoClient client, ClientEncryption encryption) {
-        MongoNamespace namespace = new MongoNamespace(encryptionConfig.getKeyVaultNamespace());
-        MongoCollection<Document> keyVault = client.getDatabase(namespace.getDatabaseName())
-            .getCollection(namespace.getCollectionName());
-
-        Bson query = Filters.in("keyAltNames", encryptionConfig.getKeyVaultAlias());
-        BsonDocument key = keyVault.withDocumentClass(BsonDocument.class)
-            .find(query)
-            .first();
-
+    private BsonBinary createOrRetrieveDataKey(ClientEncryption encryption) {
+        BsonDocument key = encryption.getKeyByAltName(encryptionConfig.getKeyVaultAlias());
         if (key == null) {
-            keyVault.createIndex(Indexes.ascending("keyAltNames"), new IndexOptions().unique(true)
-                .partialFilterExpression(Filters.exists("keyAltNames")));
-
             DataKeyOptions options = new DataKeyOptions();
             options.keyAltNames(Arrays.asList(encryptionConfig.getKeyVaultAlias()));
             return encryption.createDataKey("local", options);
@@ -103,12 +86,52 @@ public class MongoClientConfig extends AbstractMongoClientConfiguration {
         Builder settings = MongoClientSettings.builder()
             .applyConnectionString(new ConnectionString(uri));
 
-        if (encryptionConfig.getAutoDecryption()) {
-            settings.autoEncryptionSettings(AutoEncryptionSettings.builder()
+        if (encryptionConfig.isAutoDecryption()) {
+            AutoEncryptionSettings.Builder builder = AutoEncryptionSettings.builder()
                 .keyVaultNamespace(encryptionConfig.getKeyVaultNamespace())
-                .kmsProviders(LocalKmsUtils.providersMap(encryptionConfig.getMasterKeyPath()))
-                .bypassAutoEncryption(true)
-                .build());
+                .kmsProviders(LocalKmsUtils.providersMap(encryptionConfig.getMasterKeyPath()));
+
+            if (encryptionConfig.isAutoEncryption() && encryptionConfig.getDataKeyId() != null) {
+                File autoEncryptionLib = encryptionConfig.getAutoEncryptionLib();
+                if (!autoEncryptionLib.isFile()) {
+                    throw new IllegalArgumentException("encryption lib must be an existing file");
+                }
+
+                Map<String, Object> map = new HashMap<>();
+                map.put("cryptSharedLibRequired", true);
+                map.put("cryptSharedLibPath", autoEncryptionLib.toString());
+
+                builder.extraOptions(map);
+                
+                String keyUuid = encryptionConfig.dataKeyIdUuid();
+                HashMap<String, BsonDocument> schemaMap = new HashMap<>();
+                schemaMap.put(getDatabaseName() + ".citizens",
+                    BsonDocument.parse("{"
+                        + "  bsonType: \"object\","
+                        + "  encryptMetadata: {"
+                        + "    keyId: [UUID(\"" + keyUuid + "\")]"
+                        + "  },"
+                        + "  properties: {"
+                        + "    email: {"
+                        + "      encrypt: {"
+                        + "        bsonType: \"string\","
+                        + "        algorithm: \"AEAD_AES_256_CBC_HMAC_SHA_512-Deterministic\""
+                        + "      }"
+                        + "    },"
+                        + "    birthYear: {"
+                        + "      encrypt: {"
+                        + "        bsonType: \"int\","
+                        + "        algorithm: \"AEAD_AES_256_CBC_HMAC_SHA_512-Random\""
+                        + "      }"
+                        + "    }"
+                        + "  }"
+                        + "}"));
+                builder.schemaMap(schemaMap);
+            } else {
+                builder.bypassAutoEncryption(true);
+            }
+
+            settings.autoEncryptionSettings(builder.build());
         }
 
         return settings.build();
