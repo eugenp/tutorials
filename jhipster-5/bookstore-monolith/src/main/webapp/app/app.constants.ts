71f8c95e65d9f6926695317a5a89fbbe06328d9e
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-04-10 20:41:11 +0300

    JAVA-18142 Removed jhipster-5

--- jhipster-5/bookstore-monolith/src/main/webapp/app/app.constants.ts
+++
@@ -1,8 +0,0 @@
-// These constants are injected via webpack environment variables.
-// You can add more variables in webpack.common.js or in profile specific webpack.<dev|prod>.js files.
-// If you change the values in the webpack config files, you need to re run webpack to update the application
-
-export const VERSION = process.env.VERSION;
-export const DEBUG_INFO_ENABLED: boolean = !!process.env.DEBUG_INFO_ENABLED;
-export const SERVER_API_URL = process.env.SERVER_API_URL;
-export const BUILD_TIMESTAMP = process.env.BUILD_TIMESTAMP;
