c8677ba06cbe217e879eb60dbba22896f79872d9
Anastasios Ioannidis <121166333+anastasiosioannidis@users.noreply.github.com>
2023-04-12 05:01:28 +0300

    JAVA-18142 Removed jhipster-5 (#13813)

--- jhipster-5/bookstore-monolith/src/main/webapp/app/admin/docs/docs.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'jhi-docs',
-    templateUrl: './docs.component.html'
-})
-export class JhiDocsComponent {
-    constructor() {}
-}
