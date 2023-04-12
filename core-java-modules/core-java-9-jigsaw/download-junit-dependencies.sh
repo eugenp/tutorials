d58e2ed896ff6ee9039c9fc1a39c1f04c7ff13dc
Eugene Kovko <eu.kovko@gmail.com>
2023-04-12 13:12:05 +0200

    fix: Fix a script

--- core-java-modules/core-java-9-jigsaw/download-junit-dependencies.sh
+++ core-java-modules/core-java-9-jigsaw/download-junit-dependencies.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 wget -P libs/ https://repo1.maven.org/maven2/org/apiguardian/apiguardian-api/1.1.2/apiguardian-api-1.1.2.jar /
 wget -P libs/ https://repo1.maven.org/maven2/org/opentest4j/opentest4j/1.2.0/opentest4j-1.2.0.jar /
 wget -P libs/ https://repo1.maven.org/maven2/org/junit/platform/junit-platform-commons/1.9.2/junit-platform-commons-1.9.2.jar /
