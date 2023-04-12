71b441b21e4882708cd1f669d6e0df426c9992c1
Dhawal Kapil <dhawalkapil@gmail.com>
2023-04-12 09:47:23 +0530

    JAVA-20220 Renamed test to proper convention

--- aws-modules/aws-lambda-modules/todo-reminder/ToDoFunction/src/test/java/com/baeldung/lambda/todo/AppUnitTest.java
+++ aws-modules/aws-lambda-modules/todo-reminder/ToDoFunction/src/test/java/com/baeldung/lambda/todo/AppUnitTest.java
@@ -21,7 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 
 @RunWith(MockitoJUnitRunner.class)
-public class AppTest {
+public class AppUnitTest {
 
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private Context mockContext;
