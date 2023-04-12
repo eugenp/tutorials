d5c3f01318c26e52fd700228eef5d80412f37730
Deboshree <deboshree.banerjee@cred.club>
2023-04-12 22:30:02 +0530

    remove AllArgsConstructor examples

--- lombok-modules/lombok-2/src/test/java/com/baeldung/lombok/constructor/LombokConstructorUnitTest.java
+++ lombok-modules/lombok-2/src/test/java/com/baeldung/lombok/constructor/LombokConstructorUnitTest.java
@@ -54,61 +54,7 @@ class LombokConstructorUnitTest {
         // when
         assertThrows(NullPointerException.class, () -> new LombokConstructorWithRequiredArgsAndFinal(name, age));
     }
-
-    @Test
-    void whenUsingAllArgsWithNonNullAnnotationAndNullableFieldIsNull_ThenCreateObject() {
-        // given
-        String name = "John";
-        int age = 30;
-
-        // when
-        LombokConstructorWithAllArgsAndNonNull lombokConstructorWithAllArgsAndNonNull = new LombokConstructorWithAllArgsAndNonNull(name, age, null);
-
-        // then
-        assertEquals(name, lombokConstructorWithAllArgsAndNonNull.getFirstName());
-        assertEquals(age, lombokConstructorWithAllArgsAndNonNull.getAge());
-        assertNull(lombokConstructorWithAllArgsAndNonNull.getEmail());
-    }
-
-    @Test
-    void whenUsingAllArgsWithNonNullAnnotationAndNonNullFieldIsNull_ThenThrowError() {
-        // given
-        String name = "John";
-        Integer age = null;
-        String email = "hello@dummyEmail.com";
-
-        // when
-        assertThrows(NullPointerException.class, () -> new LombokConstructorWithAllArgsAndNonNull(name, age, email));
-    }
-
-    @Test
-    void whenUsingAllArgsWithFinalAnnotationAndFinalFieldIsNotPassedInConstructor_ThenCreateObject() {
-        String email = "dummy_email@dummy.com";
-        String firstName = "John";
-        int age = 30;
-
-        LombokConstructorWithAllArgsAndFinal lombokConstructorWithAllArgsAndFinal = new LombokConstructorWithAllArgsAndFinal(email);
-
-        assertEquals(firstName, lombokConstructorWithAllArgsAndFinal.getFirstName());
-        assertEquals(age, lombokConstructorWithAllArgsAndFinal.getAge());
-        assertEquals(email, lombokConstructorWithAllArgsAndFinal.getEmail());
-    }
-
-    @Test
-    void whenUsingAllArgsWithFinalAnnotationAndNonFinalFieldIsNullInConstructor_ThenCreateObject() {
-        // given
-        String firstName = "John";
-        int age = 30;
-
-        // when
-        LombokConstructorWithAllArgsAndFinal lombokConstructorWithAllArgsAndFinal = new LombokConstructorWithAllArgsAndFinal(null);
-
-        // then
-        assertEquals(firstName, lombokConstructorWithAllArgsAndFinal.getFirstName());
-        assertEquals(age, lombokConstructorWithAllArgsAndFinal.getAge());
-        assertNull(lombokConstructorWithAllArgsAndFinal.getEmail());
-    }
-
+    
     @Test
     void whenUsingBuilderAnnotationAndSomeFieldIsNull_ThenCreateObject() {
         // given
@@ -122,5 +68,22 @@ class LombokConstructorUnitTest {
         assertEquals(name, lombokWithBuilder.getName());
         assertEquals(age, lombokWithBuilder.getAge());
         assertNull(lombokWithBuilder.getEmail());
+    }
+
+    @Test
+    void whenUsingNoArgsAnnotationAndSomeFieldHasNoSetter_ThenCreateObject() {
+        // given
+        String name = "John";
+        int age = 30;
+
+        // when
+        LombokConstructorWithNoArgsAndSetter lombokConstructorWithNoArgsAndSetter = new LombokConstructorWithNoArgsAndSetter();
+        lombokConstructorWithNoArgsAndSetter.setAge(age);
+        lombokConstructorWithNoArgsAndSetter.setName(name);
+
+        // then
+        assertEquals(name, lombokConstructorWithNoArgsAndSetter.getName());
+        assertEquals(age, lombokConstructorWithNoArgsAndSetter.getAge());
+        assertNull(lombokConstructorWithNoArgsAndSetter.getEmail());
     }
 }
