400b1732279176533aa50174ef2d8c235c74f84b
Mariusz Kaczmarczyk <to@mariu.sh>
2023-04-17 18:43:27 +0200

    BAEL-6319 Test

---
+++ persistence-modules/hibernate-jpa/src/test/java/com/baeldung/hibernate/transaction/SomeIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.baeldung.hibernate.transaction;
+
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+public class SomeIntegrationTest {
+
+    @Test
+    public void givenNull_thenNull() {
+        assertNull(null);
+    }
+}
