3b172fabd85f8c6c4e921e27475aa2293d7caba6
Ulisses Lima <ulisses@dvlcube.com>
2023-04-18 01:06:01 -0300

    createKeyUniqueIndex() and updated dependencies.

--- persistence-modules/spring-boot-persistence-mongodb-3/src/main/java/com/baeldung/boot/csfle/config/MongoClientConfig.java
+++ persistence-modules/spring-boot-persistence-mongodb-3/src/main/java/com/baeldung/boot/csfle/config/MongoClientConfig.java
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import org.bson.BsonBinary;
 import org.bson.BsonDocument;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -20,8 +21,13 @@ import com.mongodb.ClientEncryptionSettings;
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoClientSettings.Builder;
+import com.mongodb.MongoNamespace;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.IndexOptions;
+import com.mongodb.client.model.Indexes;
 import com.mongodb.client.model.vault.DataKeyOptions;
 import com.mongodb.client.vault.ClientEncryption;
 import com.mongodb.client.vault.ClientEncryptions;
@@ -46,13 +52,10 @@ public class MongoClientConfig extends AbstractMongoClientConfiguration {
     @Bean
     @Override
     public MongoClient mongoClient() {
-        MongoClient client;
         try {
             ClientEncryption encryption = clientEncryption();
             encryptionConfig.setDataKeyId(createOrRetrieveDataKey(encryption));
-            client = MongoClients.create(clientSettings());
-
-            return client;
+            return MongoClients.create(clientSettings());
         } catch (IOException e) {
             throw new IllegalStateException("unable to create client", e);
         }
@@ -74,11 +77,26 @@ public class MongoClientConfig extends AbstractMongoClientConfiguration {
     private BsonBinary createOrRetrieveDataKey(ClientEncryption encryption) {
         BsonDocument key = encryption.getKeyByAltName(encryptionConfig.getKeyVaultAlias());
         if (key == null) {
+            createKeyUniqueIndex();
+
             DataKeyOptions options = new DataKeyOptions();
             options.keyAltNames(Arrays.asList(encryptionConfig.getKeyVaultAlias()));
             return encryption.createDataKey("local", options);
         } else {
             return (BsonBinary) key.get("_id");
+        }
+    }
+
+    private void createKeyUniqueIndex() {
+        try (MongoClient client = MongoClients.create(MongoClientSettings.builder()
+            .applyConnectionString(new ConnectionString(uri))
+            .build())) {
+            MongoNamespace namespace = new MongoNamespace(encryptionConfig.getKeyVaultNamespace());
+            MongoCollection<Document> keyVault = client.getDatabase(namespace.getDatabaseName())
+                .getCollection(namespace.getCollectionName());
+
+            keyVault.createIndex(Indexes.ascending("keyAltNames"), new IndexOptions().unique(true)
+                .partialFilterExpression(Filters.exists("keyAltNames")));
         }
     }
 
@@ -100,7 +118,6 @@ public class MongoClientConfig extends AbstractMongoClientConfiguration {
                 Map<String, Object> map = new HashMap<>();
                 map.put("cryptSharedLibRequired", true);
                 map.put("cryptSharedLibPath", autoEncryptionLib.toString());
-
                 builder.extraOptions(map);
                 
                 String keyUuid = encryptionConfig.dataKeyIdUuid();
