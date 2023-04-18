be84968f60b0ea1381702722aae6dcdd83e6d783
Saikat <saikatcse03@gmail.com>
2023-04-18 23:23:53 +0530

    Remove unused code

--- spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/customauth/SpringSecurityApplication.java
+++ spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/customauth/SpringSecurityApplication.java
@@ -2,9 +2,8 @@ package com.baeldung.customauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
+@SpringBootApplication
 public class SpringSecurityApplication {
 
     public static void main(String[] args) {
