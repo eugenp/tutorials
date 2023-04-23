9227902c5ba9519d0b55068d2cd686f859345bf8
Mariusz Kaczmarczyk <to@mariu.sh>
2023-04-23 16:59:34 +0200

    BAEL-6319 Hibernate 6 Boolean Converters

--- persistence-modules/hibernate-mapping-2/src/main/java/com/baeldung/hibernate/HibernateUtil.java
+++ persistence-modules/hibernate-mapping-2/src/main/java/com/baeldung/hibernate/HibernateUtil.java
@@ -1,4 +1,6 @@
-package com.baeldung;
+package com.baeldung.hibernate;
+
+import static org.hibernate.boot.registry.StandardServiceRegistryBuilder.DEFAULT_CFG_RESOURCE_NAME;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
@@ -7,19 +9,20 @@ import org.hibernate.service.ServiceRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.baeldung.manytomany.model.Employee;
-import com.baeldung.manytomany.model.Project;
-import com.baeldung.uuids.WebSiteUser;
-import com.baeldung.uuids.Element;
-import com.baeldung.uuids.Reservation;
-import com.baeldung.uuids.Sale;
+import com.baeldung.hibernate.booleanconverters.model.Question;
+import com.baeldung.hibernate.manytomany.model.Employee;
+import com.baeldung.hibernate.manytomany.model.Project;
+import com.baeldung.hibernate.uuids.WebSiteUser;
+import com.baeldung.hibernate.uuids.Element;
+import com.baeldung.hibernate.uuids.Reservation;
+import com.baeldung.hibernate.uuids.Sale;
 
 public class HibernateUtil {
 
+    private static final String DEFAULT_RESOURCE = "manytomany.cfg.xml";
     private static final Logger LOGGER = LoggerFactory.getLogger(HibernateUtil.class);
-    private static SessionFactory sessionFactory;
 
-    private static SessionFactory buildSessionFactory() {
+    private static SessionFactory buildSessionFactory(String resource) {
         try {
             // Create the SessionFactory from hibernate-annotation.cfg.xml
             Configuration configuration = new Configuration();
@@ -29,16 +32,16 @@ public class HibernateUtil {
             configuration.addAnnotatedClass(Element.class);
             configuration.addAnnotatedClass(Reservation.class);
             configuration.addAnnotatedClass(Sale.class);
-            configuration.configure("manytomany.cfg.xml");
+            configuration.addAnnotatedClass(Question.class);
+            configuration.addPackage(Question.class.getPackageName());
+            configuration.configure(resource);
             LOGGER.debug("Hibernate Annotation Configuration loaded");
 
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
                     .build();
             LOGGER.debug("Hibernate Annotation serviceRegistry created");
 
-            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-
-            return sessionFactory;
+            return configuration.buildSessionFactory(serviceRegistry);
         } catch (Throwable ex) {
             LOGGER.error("Initial SessionFactory creation failed.", ex);
             throw new ExceptionInInitializerError(ex);
@@ -46,9 +49,10 @@ public class HibernateUtil {
     }
 
     public static SessionFactory getSessionFactory() {
-        if (sessionFactory == null) {
-            sessionFactory = buildSessionFactory();
-        }
-        return sessionFactory;
+        return buildSessionFactory(DEFAULT_RESOURCE);
+    }
+
+    public static SessionFactory getSessionFactory(String resource) {
+        return buildSessionFactory(resource);
     }
 }
---
+++ persistence-modules/hibernate-mapping-2/src/main/java/com/baeldung/hibernate/manytomany/dao/IProjectDao.java
@@ -0,0 +1,8 @@
+package com.baeldung.hibernate.manytomany.dao;
+
+import com.baeldung.hibernate.manytomany.dao.common.IOperations;
+import com.baeldung.hibernate.manytomany.model.Project;
+
+public interface IProjectDao extends IOperations<Project>{
+
+}
