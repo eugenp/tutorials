b96535e05637c1bf83ceb457ce9cb6389fe28c8f
panos-kakos <kakos.programmer@gmail.com>
2023-04-25 09:47:51 +0300

    [JAVA-15034] Clean up

--- apache-httpclient-2/src/test/java/com/baeldung/httpclient/expandUrl/HttpClientExpandUrlLiveTest.java
+++ apache-httpclient-2/src/test/java/com/baeldung/httpclient/expandUrl/HttpClientExpandUrlLiveTest.java
@@ -4,8 +4,10 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Lists;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -17,9 +19,27 @@ import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpHeaders;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class HttpClientExpandUrlLiveTest {
+
+    private static CloseableHttpClient httpClient;
+
+    @BeforeAll
+    static void setup() {
+        httpClient = HttpClientBuilder.create()
+            .disableRedirectHandling()
+            .build();
+    }
+
+    @AfterAll
+    static void tearDown() throws IOException {
+        if (httpClient != null) {
+            httpClient.close();
+        }
+    }
 
     @Test
     void givenShortenedOnce_whenUrlIsExpanded_thenCorrectResult() throws IOException {
@@ -46,12 +66,26 @@ class HttpClientExpandUrlLiveTest {
         return newUrl;
     }
 
+    final String expandSafe(final String urlArg) throws IOException {
+        String originalUrl = urlArg;
+        String newUrl = expandSingleLevelSafe(originalUrl).getRight();
+        final List<String> alreadyVisited = Lists.newArrayList(originalUrl, newUrl);
+        while (!originalUrl.equals(newUrl)) {
+            originalUrl = newUrl;
+            final Pair<Integer, String> statusAndUrl = expandSingleLevelSafe(originalUrl);
+            newUrl = statusAndUrl.getRight();
+            final boolean isRedirect = statusAndUrl.getLeft() == 301 || statusAndUrl.getLeft() == 302;
+            if (isRedirect && alreadyVisited.contains(newUrl)) {
+                throw new IllegalStateException("Likely a redirect loop");
+            }
+            alreadyVisited.add(newUrl);
+        }
+
+        return newUrl;
+    }
 
     private Pair<Integer, String> expandSingleLevelSafe(final String url) throws IOException {
-
-        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
-            .disableRedirectHandling()
-            .build()) {
+        try {
             HttpHead request = new HttpHead(url);
             Pair<Integer, String> resp = httpClient.execute(request, response -> {
                 final int statusCode = response.getCode();
@@ -65,14 +99,13 @@ class HttpClientExpandUrlLiveTest {
                 return new ImmutablePair<>(statusCode, newUrl);
             });
             return resp;
+        } catch (final IllegalArgumentException uriEx) {
+            return new ImmutablePair<>(500, url);
         }
     }
 
     private String expandSingleLevel(final String url) throws IOException {
-
-        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
-            .disableRedirectHandling()
-            .build()) {
+        try {
             HttpHead request = new HttpHead(url);
             String expandedUrl = httpClient.execute(request, response -> {
                 final int statusCode = response.getCode();
@@ -85,6 +118,8 @@ class HttpClientExpandUrlLiveTest {
                 return headers[0].getValue();
             });
             return expandedUrl;
+        } catch (final IllegalArgumentException uriEx) {
+            return url;
         }
     }
 
