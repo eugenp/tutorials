6c541c2904bfaf45b7b6181c9b27aab871e7a57b
Bipinkumar27 <sayy2bipin@gmail.com>
2023-04-25 18:58:52 +0530

    JAVA-18299: Changes made for removing extra boiler plate code

--- spring-boot-modules/spring-boot-mvc-4/src/main/java/com/baeldung/springboot/swagger/model/Author.java
+++ spring-boot-modules/spring-boot-mvc-4/src/main/java/com/baeldung/springboot/swagger/model/Author.java
@@ -25,12 +25,4 @@ public class Author {
     public void setId(Integer id) {
         this.id = id;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
