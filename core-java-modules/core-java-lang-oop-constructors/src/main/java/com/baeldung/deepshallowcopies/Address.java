27b7ac37881e918a136b0fef2640b8b11012b5b4
Tom <tomsy0123@gmail.com>
2023-04-25 15:38:58 +0300

    Added base classes - person and address.

---
+++ core-java-modules/core-java-lang-oop-constructors/src/main/java/com/baeldung/deepshallowcopies/Address.java
@@ -0,0 +1,9 @@
+class Address {
+    String street;
+    int number;
+
+    public Address(String street, int number) {
+        this.street = street;
+        this.number = number;
+    }
+}
---
+++ core-java-modules/core-java-lang-oop-constructors/src/main/java/com/baeldung/deepshallowcopies/Person.java
@@ -0,0 +1,9 @@
+class Person {
+    String name;
+    Address address;
+
+    public Person(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+}
