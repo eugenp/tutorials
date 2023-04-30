d650f7ace7d112794c36f0a4c638bbe023158277
Kingsley Amankwah <kuamiklassic149@gmail.com>
2023-04-30 15:53:49 +0530

    getters and setters removed from code

--- persistence-modules/hibernate-mapping-2/src/main/java/com/baeldung/associations/biredirectional/Employee.java
+++ persistence-modules/hibernate-mapping-2/src/main/java/com/baeldung/associations/biredirectional/Employee.java
@@ -15,5 +15,28 @@ public class Employee {
     @JoinColumn(name = "department_id")
     private Department department;
  
-    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
 }
