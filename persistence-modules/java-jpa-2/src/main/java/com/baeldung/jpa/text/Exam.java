75d5f8f14bf06620a8af75182022cbf32b3c29c6
timis1 <noreplay@yahoo.com>
2023-05-01 23:24:17 +0300

    JAVA-20164 Fixing test JPATextUnitTest#givenExam_whenSaveExam_thenReturnExpectedExam

--- persistence-modules/java-jpa-2/src/main/java/com/baeldung/jpa/text/Exam.java
+++ persistence-modules/java-jpa-2/src/main/java/com/baeldung/jpa/text/Exam.java
@@ -6,7 +6,7 @@ import jakarta.persistence.*;
 public class Exam {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Lob
     private String description;
