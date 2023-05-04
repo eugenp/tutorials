8f4b14a6695507bbced2eafe3e62fd36327f6599
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2023-05-04 18:04:15 +0200

    BAEL-6320: added code support for the @NaturalId article

---
+++ persistence-modules/spring-data-jpa-repo-3/src/main/java/com/baeldung/spring/data/jpa/naturalid/repository/GuestRoomJpaRepo.java
@@ -0,0 +1,9 @@
+package com.baeldung.spring.data.jpa.naturalid.repository;
+
+import com.baeldung.spring.data.jpa.naturalid.entity.GuestRoom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRoomJpaRepo extends JpaRepository<GuestRoom, Long> {
+}
