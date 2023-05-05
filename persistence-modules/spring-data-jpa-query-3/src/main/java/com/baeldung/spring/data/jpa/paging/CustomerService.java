555662907ab9a98f2914366cd8e4b940ccf38610
uzma <uzma.nasir@gmail.com>
2023-05-05 11:07:33 +0100

    [BAEL-6105] code for list to page article

---
+++ persistence-modules/spring-data-jpa-query-3/src/main/java/com/baeldung/spring/data/jpa/paging/CustomerService.java
@@ -0,0 +1,37 @@
+package com.baeldung.spring.data.jpa.paging;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+     private final CustomerRepository customerRepository;
+
+    public Page<Customer> getCustomers(int page, int size) {
+
+        Pageable pageRequest = createPageRequestUsing(page, size);
+
+        List<Customer> allCustomers = customerRepository.findAll();
+        int start = (int) pageRequest.getOffset();
+        int end = Math.min((start + pageRequest.getPageSize()), allCustomers.size());
+
+
+        return new PageImpl<>(allCustomers.subList(start, end), pageRequest, allCustomers.size());
+
+    }
+
+    private Pageable createPageRequestUsing(int page, int size) {
+        return PageRequest.of(page, size);
+
+    }
+}
