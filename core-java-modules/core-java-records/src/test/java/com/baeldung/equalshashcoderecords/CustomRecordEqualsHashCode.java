30276e03ff8916bc894b524bc34d78e61dd3dafe
Bhaskar <bhaskar.dastidar@freshworks.com>
2023-05-06 00:15:13 +0530

    [BAEL-6253] records equals and hashcode test case

--- core-java-modules/core-java-records/src/test/java/com/baeldung/equalshashcoderecords/CustomRecordEqualsHashCode.java
+++ core-java-modules/core-java-records/src/test/java/com/baeldung/equalshashcoderecords/CustomRecordEqualsHashCode.java
@@ -7,21 +7,21 @@ import org.junit.Test;
 
 public class CustomRecordEqualsHashCode {
     @Test
-    public void testDefaultEqualsOfRecords() {
+    public void givenTwoRecords_whenDefaultEquals_thenCompareEquality() {
         Person robert = new Person("Robert", "Frost", "HDHDB223", "2000-01-02");
-        Person mike = new Person("Mike", "ADAMS", "ABJDJ2883", "2001-01-02");
+        Person mike = new Person("Mike", "Adams", "ABJDJ2883", "2001-01-02");
         assertNotEquals(robert, mike);
     }
 
     @Test
-    public void testHashCodesOfRecords() {
+    public void givenTwoRecords_hashCodesShouldBeSame() {
         Person robert = new Person("Robert", "Frost", "HDHDB223", "2000-01-02");
         Person robertCopy = new Person("Robert", "Frost", "HDHDB223", "2000-01-02");
         assertEquals(robert.hashCode(), robertCopy.hashCode());
     }
 
     @Test
-    public void testMovieCustomEquality() {
+    public void givenTwoRecords_whenCustomImplementation_thenCompareEquality() {
         Movie movie1 = new Movie("The Batman", 2022, "WB");
         Movie movie2 = new Movie("The Batman", 2022, "Dreamworks");
         assertEquals(movie1, movie2);
