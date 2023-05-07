3aa158b8728e4fb8f3c7b2b6d594ef60c70135af
Zahid Khan <Zahid.khan@globalpay.com>
2023-05-07 22:48:49 +0530

    Remove Comments and update prefix.

--- spring-boot-modules/spring-boot-properties-3/src/main/java/com/baeldung/error/configuration/MainConfiguration.java
+++ spring-boot-modules/spring-boot-properties-3/src/main/java/com/baeldung/error/configuration/MainConfiguration.java
@@ -3,16 +3,8 @@ package com.baeldung.error.configuration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * <h2>This class is for Configuration.</h2>
- * <p>
- * Project Name: New folder
- *
- * @author Zahid Khan
- * @version 4/1/2023
- */
 @Configuration
-@ConfigurationProperties(prefix = "profile.application-properties")
+@ConfigurationProperties(prefix = "profileProperties")
 public class MainConfiguration {
 	private String name;
 	
