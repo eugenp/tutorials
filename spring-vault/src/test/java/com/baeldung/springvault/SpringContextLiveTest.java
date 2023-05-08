abcb0f86a5ce1530da62e603816728611d1b4303
achraftt <achraf.taitai1@gmail.com>
2023-05-08 20:39:32 +0200

    Upgrade spring.vault.core from 2.3.2 to 3.0.2

--- spring-vault/src/test/java/com/baeldung/springvault/SpringContextLiveTest.java
+++ spring-vault/src/test/java/com/baeldung/springvault/SpringContextLiveTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.springvault;
 
-import com.baeldung.springvault.SpringVaultApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
