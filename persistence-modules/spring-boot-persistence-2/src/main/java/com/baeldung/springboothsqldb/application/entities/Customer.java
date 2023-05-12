bb9e5b65b4044f430aa59e23113cc8f2f2676d7b
timis1 <12120641+timis1@users.noreply.github.com>
2023-05-12 16:44:03 +0300

    JAVA-20165 Upgrade spring-boot hibernate specific modules to JDK 11 (#13952)
    * JAVA-20165 Upgrade spring-boot hibernate specific modules to JDK 11
    
    * JAVA-20165 Migrating spring-boot-persistence-mongodb
    
    * JAVA-20165 Migrating spring-boot-persistence-mongodb-2
    
    * JAVA-20165 Migrating spring-boot-persistence-mongodb-3
    
    * JAVA-20165 Adding latest version for jdbi in spring-boot-persistence-2 module
    
    ---------
    
    Co-authored-by: timis1 <noreplay@yahoo.com>

--- persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/springboothsqldb/application/entities/Customer.java
+++ persistence-modules/spring-boot-persistence-2/src/main/java/com/baeldung/springboothsqldb/application/entities/Customer.java
@@ -1,10 +1,10 @@
 package com.baeldung.springboothsqldb.application.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "customers")
