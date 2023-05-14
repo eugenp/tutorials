b52c5ed2bd4c7e5e2f08fe82faf99badfdca03c4
technoddy <mail.technoddy@gmail.com>
2023-05-14 12:09:07 -0400

    JPA Counting Row Implementation

---
+++ persistence-modules/spring-boot-persistence-3/src/main/java/com/baeldung/countrows/entity/Permission.java
@@ -0,0 +1,39 @@
+package com.baeldung.countrows.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="PERMISSIONS")
+public class Permission {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator = "permissions_id_sq")
+    @SequenceGenerator(name = "permissions_id_sq", sequenceName = "permissions_id_sq", allocationSize = 1)
+    private int id;
+
+    private String type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Permission{" +
+            "id=" + id +
+            ", type='" + type + '\'' +
+            '}';
+    }
+}
