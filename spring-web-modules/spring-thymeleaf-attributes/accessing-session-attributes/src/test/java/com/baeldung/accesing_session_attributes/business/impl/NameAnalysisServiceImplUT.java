2e164ec759d54c23960087ec13919941728c2b52
Víctor <victor.sempere1@gmail.com>
2023-05-15 11:27:31 +0200

    BAEL-5575
    Removed unused import

--- spring-web-modules/spring-thymeleaf-attributes/accessing-session-attributes/src/test/java/com/baeldung/accesing_session_attributes/business/impl/NameAnalysisServiceImplUT.java
+++ spring-web-modules/spring-thymeleaf-attributes/accessing-session-attributes/src/test/java/com/baeldung/accesing_session_attributes/business/impl/NameAnalysisServiceImplUT.java
@@ -12,7 +12,6 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
