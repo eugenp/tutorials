51277bfeb35efa18d362d2f9afed92b9e1883e2d
Dhawal Kapil <dhawalkapil@gmail.com>
2023-05-16 23:06:58 +0530

    JAVA-13721 Further formatting of files

--- testing-modules/mockito-simple/src/main/java/com/baeldung/mockito/argumentmatchers/service/FlowerService.java
+++ testing-modules/mockito-simple/src/main/java/com/baeldung/mockito/argumentmatchers/service/FlowerService.java
@@ -11,15 +11,15 @@ public class FlowerService {
     private List<String> flowers = Arrays.asList("Poppy", "Ageratum", "Carnation", "Diascia", "Lantana");
 
     public String analyze(String name) {
-        if(flowers.contains(name)) {
+        if (flowers.contains(name)) {
             return "flower";
         }
         return null;
     }
 
     public boolean isABigFlower(String name, int petals) {
-        if(flowers.contains(name)) {
-            if(petals > 10) {
+        if (flowers.contains(name)) {
+            if (petals > 10) {
                 return true;
             }
         }
