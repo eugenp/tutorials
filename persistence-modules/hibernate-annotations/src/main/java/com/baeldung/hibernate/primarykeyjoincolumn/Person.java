42a626d260d8583365aec817c750554e7093f977
Seremba Patrick <pserembae@gmail.com>
2023-05-18 07:41:57 +0300

    ident department and person files

--- persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Person.java
+++ persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Person.java
@@ -12,38 +12,38 @@ import javax.persistence.Table;
 @Table(name = "person")
 public class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String name;
+  private String name;
 
-    @OneToOne
-    @PrimaryKeyJoinColumn
-    private Department department;
+  @OneToOne
+  @PrimaryKeyJoinColumn
+  private Department department;
 
-    public Long getId() {
-        return id;
-    }
+  public Long getId() {
+    return id;
+  }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+  public void setId(Long id) {
+    this.id = id;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public Department getDepartment() {
-        return department;
-    }
+  public Department getDepartment() {
+    return department;
+  }
 
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
+  public void setDepartment(Department department) {
+    this.department = department;
+  }
 
 }
