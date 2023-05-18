d54fd06e0aa89e9d1096346bbb29cb2791f62a2a
Muhammad Asif <muasif80@gmail.com>
2023-05-18 16:33:56 +0500

    Inital Commit for BAEL-6094

---
+++ spring-boot-modules/spring-boot-gradle-2/src/test/java/com/baeldung/gradle/SpringBootGradle2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGradle2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
