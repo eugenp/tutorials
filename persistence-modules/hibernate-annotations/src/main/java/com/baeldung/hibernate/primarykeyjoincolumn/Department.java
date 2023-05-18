aafa5792c804285664d274b39e3568fcd80ce8f7
Seremba Patrick <pserembae@gmail.com>
2023-05-18 08:49:00 +0300

    Revert "ident department and person files"
    This reverts commit 42a626d260d8583365aec817c750554e7093f977.

--- persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Department.java
+++ persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Department.java
@@ -8,25 +8,25 @@ import javax.persistence.Table;
 @Table(name = "department")
 public class Department {
 
-  @Id
-  private Long id;
+    @Id
+    private Long id;
 
-  private String name;
+    private String name;
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public String getName() {
+        return name;
+    }
 
-  public void setName(String name) {
-    this.name = name;
-  }
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
