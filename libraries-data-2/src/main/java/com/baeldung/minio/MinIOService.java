83971a3332d2b12ca44852b54541102122b1dd71
Michael Pratt <prattm@gmail.com>
2023-05-18 20:18:25 -0600

    BAEL-6417: MinIO Java SDK examples

---
+++ libraries-data-2/src/main/java/com/baeldung/minio/MinIOService.java
@@ -0,0 +1,28 @@
+package com.baeldung.minio;
+
+import io.minio.MakeBucketArgs;
+import io.minio.MinioClient;
+import io.minio.UploadObjectArgs;
+
+public class MinIOService {
+    private MinioClient minioClient =
+        MinioClient.builder()
+            .endpoint("http://127.0.0.1:9000")
+            .credentials("minioadmin", "minioadmin")
+            .build();
+
+    public void doSomething() throws Exception {
+        minioClient.makeBucket(
+            MakeBucketArgs
+                .builder()
+                .bucket("user2")
+                .build());
+
+        minioClient.uploadObject(
+            UploadObjectArgs
+                .builder()
+                .bucket("user2")
+                .filename("/tmp/Resume.pdf")
+                .build());
+    }
+}
