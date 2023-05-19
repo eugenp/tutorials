e993db2c23d559d503b8bf8bc27aab0847224593
timis1 <12120641+timis1@users.noreply.github.com>
2023-05-19 11:37:45 +0300

    JAVA-20164 Upgrade java hibernate specific modules to JDK 11 (#13900)
    * JAVA-20164 Upgrade java hibernate specific modules to JDK 11
    
    * JAVA-20164 Migrating java-jpa-2
    
    * JAVA-20164 Migrating jnosql-artemis
    
    * JAVA-20164 Migrating querydsl
    
    * JAVA-20164 Migrating r2dbc
    
    * JAVA-20164 Migrating redis
    
    * JAVA-20164 Fixing test JPATextUnitTest#givenExam_whenSaveExam_thenReturnExpectedExam
    
    * JAVA-20164 Fixing givenIdentityStrategy_whenCommitTransction_thenReturnPrimaryKey
    
    * JAVA-20164 Changes after review
    
    * JAVA-20164 Migrating java-cassandra to jdk8
    
    * JAVA-20164 Fix legacy mode error
    
    * Update pom.xml
    
    ---------
    
    Co-authored-by: timis1 <noreplay@yahoo.com>
    Co-authored-by: Dhawal Kapil <dhawalkapil@gmail.com>

--- persistence-modules/java-jpa-2/src/main/java/com/baeldung/jpa/unrelated/entities/Recipe.java
+++ persistence-modules/java-jpa-2/src/main/java/com/baeldung/jpa/unrelated/entities/Recipe.java
@@ -1,9 +1,9 @@
 package com.baeldung.jpa.unrelated.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import java.util.Objects;
 
 @Entity
