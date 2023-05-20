7430180967bc4d5ab5d3555fbccb1c4be2f5b9ce
Michael Pratt <prattm@gmail.com>
2023-05-20 08:40:54 -0600

    BAEL-6417 Rename live test class

--- libraries-data-2/src/test/java/com/baeldung/minio/MinIOServiceLiveTest.java
+++ libraries-data-2/src/test/java/com/baeldung/minio/MinIOServiceLiveTest.java
@@ -8,7 +8,7 @@ import java.io.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MinIOServiceTest {
+public class MinIOServiceLiveTest {
 
     private MinioClient minioClient;
 
