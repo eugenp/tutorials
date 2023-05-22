802b4ed08e13fecaff7ff5dab8585204c8d54305
RupaDosapati <dosapatirupa5@email.com>
2023-05-22 21:39:09 +0530

    shallow copy vs deep copy - updated changes for Copy constructor and clone method

--- core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/copyobjects/ShallowCopyUnitTest.java
+++ core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/copyobjects/ShallowCopyUnitTest.java
@@ -11,41 +11,42 @@ public class ShallowCopyUnitTest {
 		Address address = new Address("123", "Main Street", "New York", "state");
 		Employee original = new Employee("Dave", address);
 		Employee shallowCopy = new Employee(original);
-		
+
 		assertEquals(original.getName(), shallowCopy.getName());
-		
-		assertEquals(original.getAddress().hashCode(), shallowCopy.getAddress().hashCode());		
-		
-		original.getAddress().setStreet("Second St");		
-		
+
+		assertEquals(original.getAddress().hashCode(), shallowCopy.getAddress()
+				.hashCode());
+
+		original.getAddress().setStreet("Second St");
+
 		assertEquals("Second St", original.getAddress().getStreet());
 
 	}
-	
+
 	@Test
 	public void whenCreatingShallowCopyWithCloneMethod_ObjectShouldBeSameAsOriginal() {
-		
+
 		try {
-			 Person original = new Person();
-	         original.setName("John");
-	         
-	         Address address = new Address("1234", "Main Street", "USA", "state");        
-	         original.setAddress(address);         
-	    
-	         Person shallowCopy = (Person) original.clone();
-	         
-	         assertEquals(original.getName(), shallowCopy.getName());
-	 		
-	 		 assertEquals(original.getAddress().hashCode(), shallowCopy.getAddress().hashCode());
-	 		 
-	 		 original.getAddress().setStreet("Second St");
-	 		
-	 		 assertEquals("Second St", original.getAddress().getStreet());	 
-			
+			Person original = new Person();
+			original.setName("John");
+
+			Address address = new Address("1234", "Main Street", "USA", "state");
+			original.setAddress(address);
+
+			Person shallowCopy = (Person) original.clone();
+
+			assertEquals(original.getName(), shallowCopy.getName());
+
+			assertEquals(original.getAddress().hashCode(), shallowCopy
+					.getAddress().hashCode());
+
+			original.getAddress().setStreet("Second St");
+
+			assertEquals("Second St", original.getAddress().getStreet());
+
 		} catch (CloneNotSupportedException e) {
-            e.printStackTrace();            
-        }
-		 
- 		 
+			e.printStackTrace();
+		}
+
 	}
 }
