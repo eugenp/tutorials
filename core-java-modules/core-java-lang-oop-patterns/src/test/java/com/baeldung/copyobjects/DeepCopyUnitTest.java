c2247ce2ed96c83fcc5f8323a577086484adc72d
RupaDosapati <dosapatirupa5@email.com>
2023-05-22 21:36:40 +0530

    shallow copy vs deep copy - updated changes for Copy constructor and clone method

--- core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/copyobjects/DeepCopyUnitTest.java
+++ core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/copyobjects/DeepCopyUnitTest.java
@@ -12,41 +12,42 @@ public class DeepCopyUnitTest {
 		Address address = new Address("123", "Main Street", "Pune", "CA");
 		Student original = new Student(1, "Dave", address);
 		Student deepCopy = new Student(original);
-		
+
 		assertEquals(original.getName(), deepCopy.getName());
-	
-		assertNotEquals(original.getAddress().hashCode(), deepCopy.getAddress().hashCode());		
-		
-		original.getAddress().setStreet("Second St");	
-		
+
+		assertNotEquals(original.getAddress().hashCode(), deepCopy.getAddress()
+				.hashCode());
+
+		original.getAddress().setStreet("Second St");
+
 		assertEquals("Main Street", deepCopy.getAddress().getStreet());
 	}
-	
+
 	@Test
-	public void whenCreatingDeepCopyWithCloneMethod_ObjectShouldNotBeSameAsOriginal(){		
-		
+	public void whenCreatingDeepCopyWithCloneMethod_ObjectShouldNotBeSameAsOriginal() {
+
 		try {
-			 Person original = new Person();
-	         original.setName("John");
-	         
-	         Address address = new Address("1234", "Main Street", "USA", "state");        
-	         original.setAddress(address);         
-	    
-	         Person deepCopy = (Person) original.clone();
-	         
-	         deepCopy.setAddress((Address)original.getAddress().clone());
-	         
-	         
+			Person original = new Person();
+			original.setName("John");
+
+			Address address = new Address("1234", "Main Street", "USA", "state");
+			original.setAddress(address);
+
+			Person deepCopy = (Person) original.clone();
+
+			deepCopy.setAddress((Address) original.getAddress().clone());
+
 			assertEquals(original.getName(), deepCopy.getName());
-		
-			assertNotEquals(original.getAddress().hashCode(), deepCopy.getAddress().hashCode());		
-			
-			original.getAddress().setStreet("Second St");	
-			
+
+			assertNotEquals(original.getAddress().hashCode(), deepCopy
+					.getAddress().hashCode());
+
+			original.getAddress().setStreet("Second St");
+
 			assertEquals("Main Street", deepCopy.getAddress().getStreet());
-			
+
 		} catch (CloneNotSupportedException e) {
-            e.printStackTrace();            
-        }		 
+			e.printStackTrace();
+		}
 	}
 }
