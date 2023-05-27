755c554d3df24f20402cacc1841c024f19d0df40
kasramp <kasra_mp@yahoo.com>
2023-05-27 23:10:18 +0200

    JAVA-17164 remove unnecessary Redis config

--- spring-cloud-modules/spring-cloud-bootstrap/gateway/src/main/java/com/baeldung/spring/cloud/bootstrap/gateway/SessionConfig.java
+++ spring-cloud-modules/spring-cloud-bootstrap/gateway/src/main/java/com/baeldung/spring/cloud/bootstrap/gateway/SessionConfig.java
@@ -1,25 +1,10 @@
 package com.baeldung.spring.cloud.bootstrap.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.server.EnableRedisWebSession;
 
 @Configuration
 @EnableRedisWebSession
 public class SessionConfig {
 
-    @Autowired
-    private Environment properties;
-
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration();
-        redisConfiguration.setHostName(properties.getProperty("spring.redis.host", "localhost"));
-        redisConfiguration.setPort(properties.getProperty("spring.redis.port", Integer.TYPE, 6379));
-        return new LettuceConnectionFactory(redisConfiguration);
-    }
 }
