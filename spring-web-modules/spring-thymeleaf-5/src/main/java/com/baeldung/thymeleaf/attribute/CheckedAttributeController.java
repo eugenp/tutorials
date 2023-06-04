70388c0853c9d5f29a08d0bb764c742c919b249d
achraftt <achraf.taitai1@gmail.com>
2023-06-04 23:40:30 +0200

    BAEL-5648: Thymeleaf - How to add checked attribute to input conditionally

--- spring-web-modules/spring-thymeleaf-5/src/main/java/com/baeldung/thymeleaf/attribute/CheckedAttributeController.java
+++ spring-web-modules/spring-thymeleaf-5/src/main/java/com/baeldung/thymeleaf/attribute/CheckedAttributeController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AttributeController {
+public class CheckedAttributeController {
 
     @GetMapping("/checked")
     public String displayCheckboxForm(Model model) {
