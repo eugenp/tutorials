45544c41b0398d2bdae5c7f52e59ce2c07f05128
Seremba Patrick <pserembae@gmail.com>
2023-06-07 12:32:51 +0300

    indent Department Perspn and PrimaryKeyJoinColumnIntegrationTest classes

--- persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Department.java
+++ persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Department.java
@@ -7,26 +7,26 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "department")
 public class Department {
-
-    @Id
-    private Long id;
-
-    private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+	
+	@Id
+	private Long id;
+	
+	private String name;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
