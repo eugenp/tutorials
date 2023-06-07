3b0e64c6b8c6bb33ce069e15a6510cc34eabd93b
kasramp <kasra_mp@yahoo.com>
2023-06-07 22:03:42 +0200

    JAVA-22075 move Blaze persistence module to Spring Boot parent two

--- persistence-modules/blaze-persistence/src/test/java/com/baeldung/TestContextConfig.java
+++ persistence-modules/blaze-persistence/src/test/java/com/baeldung/TestContextConfig.java
@@ -1,15 +1,12 @@
 package com.baeldung;
 
-import com.blazebit.persistence.integration.view.spring.EnableEntityViews;
-import com.blazebit.persistence.spring.data.repository.config.EnableBlazeRepositories;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-@Configuration
-@ComponentScan("com.baeldung")
-@EnableEntityViews(basePackages = {"com.baeldung.view"})
-@EnableBlazeRepositories(basePackages = "com.baeldung.repository")
+@SpringBootTest
 public class TestContextConfig {
 
-
+    @Test
+    void contextLoads() {
+    }
 }
