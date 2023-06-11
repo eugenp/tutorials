849035a0d10bccfdcdf93743155ce32f50a5678b
Dhawal Kapil <dhawalkapil@gmail.com>
2023-06-11 13:09:16 +0530

    JAVA-8924 Cleanup for spliterator article (#14221)

--- core-java-modules/core-java-8/src/test/java/com/baeldung/spliterator/ExecutorUnitTest.java
+++ core-java-modules/core-java-8/src/test/java/com/baeldung/spliterator/ExecutorUnitTest.java
@@ -1,5 +1,7 @@
 package com.baeldung.spliterator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,33 +10,23 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-import static org.assertj.core.api.Assertions.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.baeldung.spliterator.Article;
-import com.baeldung.spliterator.Author;
-import com.baeldung.spliterator.Executor;
-import com.baeldung.spliterator.RelatedAuthorSpliterator;
-import com.baeldung.spliterator.Task;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ExecutorUnitTest {
     Article article;
     Stream<Author> stream;
     Spliterator<Author> spliterator;
-    Spliterator<Article> split1;
-    Spliterator<Article> split2;
 
     @Before
     public void init() {
         article = new Article(Arrays.asList(new Author("Ahmad", 0), new Author("Eugen", 0), new Author("Alice", 1), new Author("Alice", 1), new Author("Mike", 0), new Author("Alice", 1), new Author("Mike", 0), new Author("Alice", 1), new Author("Mike", 0),
             new Author("Alice", 1), new Author("Mike", 0), new Author("Mike", 0), new Author("Alice", 1), new Author("Mike", 0), new Author("Alice", 1), new Author("Mike", 0), new Author("Alice", 1), new Author("Mike", 0), new Author("Alice", 1),
             new Author("Mike", 0), new Author("Michał", 0), new Author("Loredana", 1)), 0);
-        stream = article.getListOfAuthors()
-            .stream();
-        split1 = Executor.generateElements()
-            .spliterator();
-        split2 = split1.trySplit();
+
         spliterator = new RelatedAuthorSpliterator(article.getListOfAuthors());
     }
 
@@ -45,36 +37,39 @@ public class ExecutorUnitTest {
     }
 
     @Test
-    public void givenSpliterator_whenAppliedToAListOfArticle_thenSplittedInHalf() {
-        assertThat(new Task(split1).call()).containsSequence(Executor.generateElements()
-            .size() / 2 + "");
-        assertThat(new Task(split2).call()).containsSequence(Executor.generateElements()
-            .size() / 2 + "");
-    }
-
-    @Test
-    public void givenAstreamOfArticles_whenProcessedInSequentiallyWithSpliterator_ProducessRightOutput() {
+    public void givenAStreamOfArticles_whenProcessedSequentiallyWithSpliterator_ProducessRightOutput() {
         List<Article> articles = Stream.generate(() -> new Article("Java"))
             .limit(35000)
             .collect(Collectors.toList());
 
         Spliterator<Article> spliterator = articles.spliterator();
         while (spliterator.tryAdvance(article -> article.setName(article.getName()
-            .concat("- published by Baeldung"))));
+            .concat("- published by Baeldung"))))
+            ;
 
         articles.forEach(article -> assertThat(article.getName()).isEqualTo("Java- published by Baeldung"));
     }
 
     @Test
-    public void givenSpliterator_whenAppliedToListOfArticles_thenSplitIntoEqualHalf() {
+    public void givenAStreamOfArticle_whenProcessedUsingTrySplit_thenSplitIntoEqualHalf() {
+        List<Article> articles = Stream.generate(() -> new Article("Java"))
+            .limit(35000)
+            .collect(Collectors.toList());
+
+        Spliterator<Article> split1 = articles.spliterator();
+        Spliterator<Article> split2 = split1.trySplit();
+
+        log.info("Size: " + split1.estimateSize());
+        log.info("Characteristics: " + split1.characteristics());
+
         List<Article> articlesListOne = new ArrayList<>();
         List<Article> articlesListTwo = new ArrayList<>();
 
         split1.forEachRemaining(articlesListOne::add);
         split2.forEachRemaining(articlesListTwo::add);
 
-        System.out.println(articlesListOne.size());
-        System.out.println(articlesListTwo.size());
+        assertThat(articlesListOne.size()).isEqualTo(17500);
+        assertThat(articlesListTwo.size()).isEqualTo(17500);
 
         assertThat(articlesListOne).doesNotContainAnyElementsOf(articlesListTwo);
     }
