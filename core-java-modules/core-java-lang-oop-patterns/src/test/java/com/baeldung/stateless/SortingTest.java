a70bdc7a09e6843cef1b5461e0258d0e08baae33
Vali Tuguran <valituguran@gmail.com>
2023-06-11 13:23:18 +0300

    BAEL-6225 Move code to core-java-lang-oop-patterns.

---
+++ core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/stateless/SortingTest.java
@@ -0,0 +1,29 @@
+package com.baeldung.stateless;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SortingTest {
+    
+    @Test
+    void givenArray_whenBubbleSorting_thenSorted() {
+        int[] arrayToSort = {17, 6, 11, 41, 5, 3, 4, -9};
+        int[] sortedArray = {-9, 3, 4, 5, 6, 11, 17, 41};
+        
+        IntArraySorter sorter = new IntArraySorter(new BubbleSort());
+        sorter.sort(arrayToSort);
+        assertArrayEquals(sortedArray, arrayToSort);
+    }
+    
+    @Test
+    void givenArray_whenQuickSortSorting_thenSorted() {
+        int[] arrayToSort = {17, 6, 11, 41, 5, 3, 4, -9};
+        int[] sortedArray = {-9, 3, 4, 5, 6, 11, 17, 41};
+        
+        IntArraySorter sorter = new IntArraySorter(new QuickSort());
+        sorter.sort(arrayToSort);
+        assertArrayEquals(sortedArray, arrayToSort);
+    }
+
+}
