d902e26e85dacb383ac1135746e524f90f9a7681
tudor.marc <marc.tudor.andrei@gmail.com>
2023-06-13 01:00:38 +0300

    BAEL-6310 - guide to JacksonJr

---
+++ jackson-jr/src/main/java/com/baeldung/jacksonjr/MyHandlerProvider.java
@@ -0,0 +1,28 @@
+package com.baeldung.jacksonjr;
+
+import com.fasterxml.jackson.jr.ob.api.ReaderWriterProvider;
+import com.fasterxml.jackson.jr.ob.api.ValueReader;
+import com.fasterxml.jackson.jr.ob.api.ValueWriter;
+import com.fasterxml.jackson.jr.ob.impl.JSONReader;
+import com.fasterxml.jackson.jr.ob.impl.JSONWriter;
+
+import java.time.LocalDate;
+
+public class MyHandlerProvider extends ReaderWriterProvider {
+
+    public ValueWriter findValueWriter (JSONWriter writeContext,
+                                        Class<?> type) {
+        if (type == LocalDate.class) {
+            return new CustomDateSerializer();
+        }
+        return null;
+    }
+
+    @Override
+    public ValueReader findValueReader (JSONReader readContext, Class<?> type) {
+        if (type.equals(LocalDate.class)) {
+            return new CustomDateDeserializer();
+        }
+        return null;
+    }
+}
