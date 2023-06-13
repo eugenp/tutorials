bde1195599126c39a02198eea4242f29c0d1a3dc
Seremba Patrick <pserembae@gmail.com>
2023-06-13 14:12:20 +0300

    format properly

--- persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Person.java
+++ persistence-modules/hibernate-annotations/src/main/java/com/baeldung/hibernate/primarykeyjoincolumn/Person.java
@@ -11,39 +11,39 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "person")
 public class Person {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	private String name;
-	
-	@OneToOne
-	@PrimaryKeyJoinColumn
-	private Department department;
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public Department getDepartment() {
-		return department;
-	}
-	
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
-	
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToOne
+    @PrimaryKeyJoinColumn
+    private Department department;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
 }
