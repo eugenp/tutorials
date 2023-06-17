6ab2be5a4e70e17ffcc0fc29ef21e0a1a4271ff8
timis1 <noreplay@yahoo.com>
2023-06-17 18:53:57 +0300

    JAVA-20168 Migrating spring-jpa

--- persistence-modules/spring-jpa/src/main/java/com/baeldung/sqlfiles/Country.java
+++ persistence-modules/spring-jpa/src/main/java/com/baeldung/sqlfiles/Country.java
@@ -1,11 +1,11 @@
 package com.baeldung.sqlfiles;
 
-import static javax.persistence.GenerationType.IDENTITY;
+import static jakarta.persistence.GenerationType.IDENTITY;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Country {
