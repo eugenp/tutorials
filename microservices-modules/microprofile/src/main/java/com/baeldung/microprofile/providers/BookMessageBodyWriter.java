da35ef7cc215300c7b891919bb9224d4783e651e
GaetanoPiazzolla <gae.piaz@gmail.com>
2023-06-18 12:26:39 +0200

    JAVA-22212 | upgraded microprofile version

--- microservices-modules/microprofile/src/main/java/com/baeldung/microprofile/providers/BookMessageBodyWriter.java
+++ microservices-modules/microprofile/src/main/java/com/baeldung/microprofile/providers/BookMessageBodyWriter.java
@@ -2,16 +2,16 @@ package com.baeldung.microprofile.providers;
 
 import com.baeldung.microprofile.model.Book;
 import com.baeldung.microprofile.util.BookMapper;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.json.JsonWriter;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.ext.MessageBodyWriter;
+import jakarta.ws.rs.ext.Provider;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.JsonWriter;
-import javax.ws.rs.Produces;
-import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.ext.MessageBodyWriter;
-import javax.ws.rs.ext.Provider;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.annotation.Annotation;
@@ -47,7 +47,8 @@ public class BookMessageBodyWriter implements MessageBodyWriter<Book> {
      * @throws WebApplicationException
      */
     @Override
-    public void writeTo(Book book, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+    public void writeTo(Book book, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType,
+                        MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
         JsonWriter jsonWriter = Json.createWriter(entityStream);
         JsonObject jsonObject = BookMapper.map(book);
         jsonWriter.writeObject(jsonObject);
