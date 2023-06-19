da23f3bc24fa3dbe620db9e0b84a642f294ec5e0
timis1 <noreplay@yahoo.com>
2023-06-19 21:57:34 +0300

    JAVA-18148 Cleanup the project to contain just the python related language interop

--- language-interop/src/main/resources/js/typed_arrays.js
+++
@@ -1,9 +0,0 @@
-function arrays(arr) {
-
-    var javaIntArray = Java.to(arr, "int[]");
-    print(javaIntArray[0]);
-    print(javaIntArray[1]);
-    print(javaIntArray[2]);
-}
-
-arrays([100, "1654", true]);
