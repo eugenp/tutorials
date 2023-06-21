ddcad29ca2a3380cd0105eceefd3c1de12f653e3
Bipinkumar27 <sayy2bipin@gmail.com>
2023-06-21 20:28:35 +0530

    Java-15020:  Changes made for upgrading DNS round robin test case to httpclient5

--- apache-httpclient4/src/test/java/com/baeldung/httpclient/HttpAsyncClientV5LiveTest.java
+++ apache-httpclient4/src/test/java/com/baeldung/httpclient/HttpAsyncClientV5LiveTest.java
@@ -1,5 +1,9 @@
 package com.baeldung.httpclient;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.net.SocketTimeoutException;
+
 import org.apache.hc.client5.http.config.RequestConfig;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
@@ -8,26 +12,22 @@ import org.apache.hc.core5.http.io.support.ClassicRequestBuilder;
 import org.apache.hc.core5.util.Timeout;
 import org.junit.jupiter.api.Test;
 
-import java.net.SocketTimeoutException;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
 public class HttpAsyncClientV5LiveTest {
 
     @Test
     void givenTimeoutIsConfigured_whenTimeOut_thenTimeoutException() {
         final int timeout = 3;
 
-        final RequestConfig config = RequestConfig.custom()
+        RequestConfig config = RequestConfig.custom()
             .setConnectTimeout(Timeout.ofMicroseconds(timeout * 1000))
             .setConnectionRequestTimeout(Timeout.ofMicroseconds(timeout * 1000))
             .setResponseTimeout(Timeout.ofMicroseconds(timeout * 1000))
             .build();
-        final CloseableHttpClient client = HttpClientBuilder.create()
+        CloseableHttpClient client = HttpClientBuilder.create()
             .setDefaultRequestConfig(config)
             .build();
 
-        final ClassicHttpRequest request = ClassicRequestBuilder.get("http://www.google.com:81")
+        ClassicHttpRequest request = ClassicRequestBuilder.get("http://www.google.com:81")
             .build();
 
         assertThrows(SocketTimeoutException.class, () -> {
