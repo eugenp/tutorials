0a08fb1190e1827a52d0f1c9cce5273b62fd2569
MohamedHelmyKassab <137485958+MohamedHelmyKassab@users.noreply.github.com>
2023-06-24 13:31:01 +0300

    BAEL-6418
    This commit aims to add a test class for the article BAEL-6418

---
+++ core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/integerclassintegertypeintclass/IntegerClassIntegerTYPEIntClassUnitTest.java
@@ -0,0 +1,37 @@
+package com.baeldung.integerclassintegertypeintclass;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class IntegerClassIntegerTYPEIntClassUnitTest {
+    
+    @Test
+    public void givenIntegerClass_whenGetName_thenVerifyClassName() {
+        Class<Integer> integerClass = Integer.class;
+        Assertions.assertEquals("java.lang.Integer", integerClass.getName());
+        Assertions.assertEquals(Number.class, integerClass.getSuperclass());
+        Assertions.assertFalse(integerClass.isPrimitive());
+    }
+    public void processInteger(Integer value) {
+        Assertions.assertEquals("Integer", value.getClass().getSimpleName());
+    }
+    public void processInt(int value) {
+        Assertions.assertEquals("int", Integer.TYPE.getName());
+    }
+    @Test
+    public void givenIntegerType_whenProcessIntegerAndProcessInt_thenVerifyClassNames() {
+        int intValue = 42;
+        Integer integerValue = 42;
+        processInteger(intValue);
+        processInteger(integerValue);
+        processInt(intValue);
+    }
+    @Test
+    public void givenIntValue_whenUsingIntClass_thenVerifyIntClassProperties() {
+        int intValue = 42;
+        Class<?> intClass = int.class;
+        Assertions.assertEquals("int", intClass.getName());
+        Assertions.assertTrue(intClass.isPrimitive());
+        Assertions.assertEquals(int.class, intClass);
+    }
+}
