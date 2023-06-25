55e8023fae8319ce93259f0b5fe06dea908c19de
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-06-25 15:25:29 +0530

    BAEL-6577 - Paralellize for loops in Java - adding JMH

--- core-java-modules/core-java-concurrency-2/src/main/java/com/baeldung/concurrent/parallel/Processor.java
+++ core-java-modules/core-java-concurrency-2/src/main/java/com/baeldung/concurrent/parallel/Processor.java
@@ -1,20 +1,28 @@
-package com.baeldung.parallel;
+package com.baeldung.concurrent.parallel;
+
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Mode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Processor {
 
+    @BenchmarkMode(Mode.AverageTime)
     public void processSerially() throws InterruptedException {
         for (int i = 0; i < 100; i++) {
             Thread.sleep(10);
         }
     }
 
+    @BenchmarkMode(Mode.AverageTime)
     public void processParallelyWithExecutor() throws InterruptedException, ExecutionException {
         // create a thread pool of 10 threads
         ExecutorService executorService = Executors.newFixedThreadPool(10);
@@ -41,18 +49,20 @@ public class Processor {
         executorService.shutdown();
     }
 
+    @BenchmarkMode(Mode.AverageTime)
     public void processParallelyWithStream() {
         IntStream.range(0, 100)
-                .parallel()
-                .forEach(i -> {
-                    try {
-                        Thread.sleep(10);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                });
+          .parallel()
+          .forEach(i -> {
+              try {
+                  Thread.sleep(10);
+              } catch (InterruptedException e) {
+                  e.printStackTrace();
+              }
+          });
     }
 
+    @BenchmarkMode(Mode.AverageTime)
     public void processParallelyWithStreamSupport() {
         Iterable<Integer> iterable = () -> IntStream.range(0, 100).iterator();
         Stream<Integer> stream = StreamSupport.stream(iterable.spliterator(), true);
