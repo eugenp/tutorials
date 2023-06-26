6da9592798de255ea59b5408af32a5d4c30f2337
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2023-06-26 19:35:01 +0300

    Removing JSON elements with Jackson [BAEL-6511]
    This PR adds a test class for removing JSON elements using the Jackson library.

--- jackson-modules/jackson-core/src/test/java/com/baeldung/jackson/jsonnode/RemoveJsonElementsUnitTest.java
+++ jackson-modules/jackson-core/src/test/java/com/baeldung/jackson/jsonnode/RemoveJsonElementsUnitTest.java
@@ -1,4 +1,5 @@
 package com.baeldung.jackson.jsonnode;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
