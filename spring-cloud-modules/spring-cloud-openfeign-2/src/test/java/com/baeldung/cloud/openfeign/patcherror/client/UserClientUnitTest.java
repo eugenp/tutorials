ce24546434be9932db5a337d402600647d268ffe
saikat <saikatcse03@gmail.com>
2023-06-27 23:23:17 +0530

    refactor the code

--- spring-cloud-modules/spring-cloud-openfeign-2/src/test/java/com/baeldung/cloud/openfeign/patcherror/client/UserClientUnitTest.java
+++ spring-cloud-modules/spring-cloud-openfeign-2/src/test/java/com/baeldung/cloud/openfeign/patcherror/client/UserClientUnitTest.java
@@ -1,11 +1,11 @@
-package com.baeldung.cloud.openfeign.patcherror.withdefaulthttpclient;
+package com.baeldung.cloud.openfeign.patcherror.client;
 
 import com.baeldung.cloud.openfeign.ExampleApplication;
-import com.baeldung.cloud.openfeign.patcherror.client.UserClient;
 import com.baeldung.cloud.openfeign.patcherror.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import feign.FeignException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,30 +13,30 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.File;
 import java.io.IOException;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = ExampleApplication.class)
-@TestPropertySource(locations = "/test1.properties")
 public class UserClientUnitTest {
 
     @Autowired
     private UserClient userClient;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     private WireMockServer wireMockServer;
 
     @BeforeEach
     public void startWireMockServer() {
-        wireMockServer = new WireMockServer(8081);
-        configureFor("localhost", 8081);
+        wireMockServer = new WireMockServer(8082);
+        configureFor("localhost", 8082);
         wireMockServer.start();
     }
 
@@ -46,8 +46,8 @@ public class UserClientUnitTest {
     }
 
     @Test
-    void givenUserIsValid_whenUpdateUserCalled_thenThrowFeignException() throws IOException {
-        User user = new ObjectMapper().readValue(new File("src/test/resources/existing-user.json"), User.class);
+    void givenUserExistsAndIsValid_whenUpdateUserCalled_thenReturnSuccess() throws IOException {
+        User user = objectMapper.readValue(new File("src/test/resources/existing-user.json"), User.class);
         user.setEmail("updated-email@mail.in");
 
         String updatedUserResponse = "{\n" +
@@ -61,14 +61,16 @@ public class UserClientUnitTest {
                         .withHeader("Content-Type", "application/json")
                         .withBody(updatedUserResponse)));
 
+        User updatedUser = userClient.updateUser("100001", user);
 
-        //assertDoesNotThrow(() -> userClient.updateUser("100001", user));
-        assertThrows(FeignException.class, () -> userClient.updateUser("100001", user));
+        assertEquals(user.getUserId(), updatedUser.getUserId());
+        assertEquals("updated-name", updatedUser.getUserName());
+        assertEquals("updated-email@mail.in", updatedUser.getEmail());
     }
 
     @Test
-    void givenUserIsNotValid_whenUpdateUserCalled_thenReturnNotFoundErrorAndFeignException() throws IOException {
-        User user = new ObjectMapper().readValue(new File("src/test/resources/existing-user.json"), User.class);
+    void givenUserNotFound_whenUpdateUserCalled_thenReturnNotFoundErrorAndFeignException() throws IOException {
+        User user = objectMapper.readValue(new File("src/test/resources/existing-user.json"), User.class);
         user.setEmail("updated-email@mail.in");
 
         stubFor(patch(urlEqualTo("/api/user/".concat("100002")))
