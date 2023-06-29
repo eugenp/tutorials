d0e96d7eb1c089817663178f3195f34a8e104f84
Bipinkumar27 <sayy2bipin@gmail.com>
2023-06-29 15:35:45 +0530

    JAVA-22625: Changes made for formatting the pom.xml

--- core-java-modules/core-java-8/src/main/java/com/baeldung/spliterator/RelatedAuthorSpliterator.java
+++ core-java-modules/core-java-8/src/main/java/com/baeldung/spliterator/RelatedAuthorSpliterator.java
@@ -7,22 +7,34 @@ import java.util.function.Consumer;
 
 public class RelatedAuthorSpliterator implements Spliterator<Author> {
 	private final List<Author> list;
-	AtomicInteger current = new AtomicInteger();
+	int current;
+
+	private int end;
 
 	public RelatedAuthorSpliterator(List<Author> list) {
 		this.list = list;
+		end =  list.size()-1;
+	}
+
+	public RelatedAuthorSpliterator(List<Author> list, int start, int end) {
+		this.list = list;
+		this.current=start;
+		this.end= end;
 	}
 
 	@Override
 	public boolean tryAdvance(Consumer<? super Author> action) {
-
-		action.accept(list.get(current.getAndIncrement()));
-		return current.get() < list.size();
+		if (current <= end) {
+			action.accept(list.get(current));
+			current++;
+			return true;
+		}
+		return false;
 	}
 
 	@Override
 	public Spliterator<Author> trySplit() {
-		int currentSize = list.size() - current.get();
+		/*int currentSize = list.size() - current.get();
 		if (currentSize < 10) {
 			return null;
 		}
@@ -33,12 +45,30 @@ public class RelatedAuthorSpliterator implements Spliterator<Author> {
 				return spliterator;
 			}
 		}
-		return null;
+		return null;*/
+		if((end - current) < 100){
+			return null;
+		}
+
+		int splitPos = current + (end -current)/2;
+
+		int relatedArticleBeforePos = list.get(splitPos-1).getRelatedArticleId();
+		int relatedArticleAfterPos = list.get(splitPos).getRelatedArticleId();
+
+		while(relatedArticleBeforePos == relatedArticleAfterPos){
+			splitPos++;
+			relatedArticleBeforePos = relatedArticleAfterPos;
+			relatedArticleAfterPos = list.get(splitPos).getRelatedArticleId();
+		}
+
+		RelatedAuthorSpliterator secondHlf = new RelatedAuthorSpliterator(list,splitPos,end);
+		end = splitPos -1;
+		return secondHlf;
 	}
 
 	@Override
 	public long estimateSize() {
-		return list.size() - current.get();
+		return end - current;
 	}
 
 	@Override
