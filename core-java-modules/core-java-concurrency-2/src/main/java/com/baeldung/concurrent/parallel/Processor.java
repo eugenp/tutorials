0b7314ef335d862ba84c881c2e5d476c5b46b9c1
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-07-01 10:35:04 +0530

    BAEL-6577 - Paralellize for loops in Java - using CompletableFuture

--- core-java-modules/core-java-concurrency-2/src/main/java/com/baeldung/concurrent/parallel/Processor.java
+++ core-java-modules/core-java-concurrency-2/src/main/java/com/baeldung/concurrent/parallel/Processor.java
@@ -1,20 +1,22 @@
 package com.baeldung.concurrent.parallel;
 
+import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Mode;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class Processor {
 
+    @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void processSerially() throws InterruptedException {
         for (int i = 0; i < 100; i++) {
@@ -22,33 +24,26 @@ public class Processor {
         }
     }
 
+    @Benchmark
     @BenchmarkMode(Mode.AverageTime)
-    public void processParallelyWithExecutor() throws InterruptedException, ExecutionException {
-        // create a thread pool of 10 threads
+    public void processParallelyWithExecutorService() throws InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(10);
-
-        List<Future<?>> futures = new ArrayList<>();
-        // submit tasks to the thread pool
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            Future<?> future = executorService.submit(() -> {
+            CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
                 try {
                     Thread.sleep(10);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            });
+            }, executorService);
             futures.add(future);
         }
-
-        // wait for all tasks to complete
-        for (Future<?> future : futures) {
-            future.get();
-        }
-
-        // shut down the executor service - free all threads
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
         executorService.shutdown();
     }
 
+    @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void processParallelyWithStream() {
         IntStream.range(0, 100)
@@ -62,6 +57,7 @@ public class Processor {
           });
     }
 
+    @Benchmark
     @BenchmarkMode(Mode.AverageTime)
     public void processParallelyWithStreamSupport() {
         Iterable<Integer> iterable = () -> IntStream.range(0, 100).iterator();
