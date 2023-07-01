a0502f7a426f27d36f8a3af7486619c2f218b93e
neha298 <32795246+neha298@users.noreply.github.com>
2023-07-01 19:46:50 +0530

    Add customizers in message producers

--- spring-pulsar/src/main/java/com/baeldung/springpulsar/PulsarProducer.java
+++ spring-pulsar/src/main/java/com/baeldung/springpulsar/PulsarProducer.java
@@ -14,10 +14,18 @@ public class PulsarProducer {
     private PulsarTemplate<String> stringTemplate;
 
     private static final String USER_TOPIC = "user-topic";
-    private static final String USER_TOPIC_STR = "user-topic-str";
+    private static final String USER_TOPIC_STR = "string-topic";
 
     public void sendMessageToPulsarTopic(User user) throws PulsarClientException {
-        template.send(USER_TOPIC, user);
+        template.newMessage(user)
+                .withProducerCustomizer(pc -> {
+                    pc.accessMode(ProducerAccessMode.Shared);
+                })
+                .withMessageCustomizer(mc -> {
+                    mc.deliverAfter(10L, TimeUnit.SECONDS);
+                })
+                .withTopic(USER_TOPIC)
+                .send();
     }
 
     public void sendStringMessageToPulsarTopic(String str) throws PulsarClientException {
