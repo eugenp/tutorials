aa44a8945ef5c26ed6c0642241d7d3cba754f379
Anastasios Ioannidis <anastasios.ioannidisa@gmail.com>
2023-07-06 13:42:39 +0300

    JAVA-22208 Spring-cloud-eureka upgrade to JDK 17

--- spring-cloud-modules/spring-cloud-eureka/spring-cloud-eureka-feign-client-integration-test/src/main/java/com/baeldung/spring/cloud/client/BooksClient.java
+++ spring-cloud-modules/spring-cloud-eureka/spring-cloud-eureka-feign-client-integration-test/src/main/java/com/baeldung/spring/cloud/client/BooksClient.java
@@ -1,16 +1,15 @@
 package com.baeldung.spring.cloud.client;
 
-import com.baeldung.spring.cloud.model.Book;
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
+import com.baeldung.spring.cloud.model.Book;
 
-@FeignClient("books-service")
-//@FeignClient(value="simple-books-client", url="${book.service.url}")
+@FeignClient(name = "books-service")
 public interface BooksClient {
 
     @RequestMapping("/books")
     List<Book> getBooks();
-
 }
