2c816f666ea5cf87c4b7b6ad2f306edb1f7b5b23
uzma <uzma.nasir@gmail.com>
2023-07-07 00:36:48 +0100

    [BAEL-6697] code for jacoco fail build

--- testing-modules/testing-libraries-2/src/test/java/com/baeldung/jacocoexclusions/service/ProductServiceUnitTest.java
+++ testing-modules/testing-libraries-2/src/test/java/com/baeldung/jacocoexclusions/service/ProductServiceUnitTest.java
@@ -9,7 +9,13 @@ class ProductServiceUnitTest {
     @Test
     public void givenOriginalPrice_whenGetSalePrice_thenReturnsDiscountedPrice() {
         ProductService productService = new ProductService();
-        double salePrice = productService.getSalePrice(100);
+        double salePrice = productService.getSalePrice(100, true);
         assertEquals(salePrice, 75);
+    }
+    @Test
+    public void givenOriginalPrice_whenGetSalePriceWithFlagFalse_thenReturnsDiscountedPrice() {
+        ProductService productService = new ProductService();
+        double salePrice = productService.getSalePrice(100, false);
+        assertEquals(salePrice, 100);
     }
 }
