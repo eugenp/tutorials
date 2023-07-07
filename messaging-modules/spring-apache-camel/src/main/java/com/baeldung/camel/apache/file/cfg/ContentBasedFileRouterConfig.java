91e3f76788dd48f9a0888ffe77386d4c46c709a9
timis1 <noreplay@yahoo.com>
2023-07-07 19:02:31 +0300

    JAVA-22210 Changes after review

--- messaging-modules/spring-apache-camel/src/main/java/com/baeldung/camel/apache/file/cfg/ContentBasedFileRouterConfig.java
+++ messaging-modules/spring-apache-camel/src/main/java/com/baeldung/camel/apache/file/cfg/ContentBasedFileRouterConfig.java
@@ -4,21 +4,19 @@ import java.util.Collections;
 import java.util.List;
 
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.baeldung.camel.apache.file.ContentBasedFileRouter;
 
 @Configuration
-public class ContentBasedFileRouterConfig extends CamelConfiguration {
+public class ContentBasedFileRouterConfig {
 
 	@Bean
 	ContentBasedFileRouter getContentBasedFileRouter() {
 		return new ContentBasedFileRouter();
 	}
 
-	@Override
 	public List<RouteBuilder> routes() {
 		return Collections.singletonList(getContentBasedFileRouter());
 	}
