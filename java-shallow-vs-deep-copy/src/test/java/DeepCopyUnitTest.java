79dce03a034cae0518fe1590b2f21a8a19e36846
Alammarcr7 <ahmadalammar@airasia.com>
2023-07-07 08:16:54 +0800

    remove redundant unit tests

--- java-shallow-vs-deep-copy/src/test/java/DeepCopyUnitTest.java
+++ java-shallow-vs-deep-copy/src/test/java/DeepCopyUnitTest.java
@@ -7,28 +7,7 @@ import com.baeldung.model.Employee;
 
 public class DeepCopyUnitTest {
     @Test
-    public void whenCreatingDeepCopy_thenObjectsShouldNotBeSame() {
-        // Create an original Employee object
-        Employee originalEmployee = new Employee();
-        originalEmployee.setId(1);
-        originalEmployee.setName("John Doe");
-
-        Address originalAddress = new Address();
-        originalAddress.setCity("New York");
-        originalAddress.setZipCode("12345");
-
-        originalEmployee.setAddress(originalAddress);
-
-        // Create a deep copy of the original Employee object
-        Employee deepCopy = originalEmployee.deepCopy();
-
-        // Assert that the original employee is not the same object as the deep copy employee
-        assertNotSame(originalEmployee, deepCopy);
-    }
-
-    @Test
     public void whenModifyingOriginalObject_thenCopyShouldNotChange() {
-        // Create an original Employee object
         Employee originalEmployee = new Employee();
         originalEmployee.setId(1);
         originalEmployee.setName("John Doe");
@@ -39,16 +18,13 @@ public class DeepCopyUnitTest {
 
         originalEmployee.setAddress(originalAddress);
 
-        // Create a deep copy of the original Employee object
         Employee deepCopy = originalEmployee.deepCopy();
 
-        // Modify the original copy
         originalEmployee.getAddress()
             .setCity("Los Angeles");
         originalEmployee.getAddress()
             .setZipCode("56789");
 
-        // Assert that the changes made to the original employee's address is different from deepCopy employee's address
         assertNotSame(originalEmployee.getAddress()
             .getCity(), deepCopy.getAddress()
             .getCity());
--- java-shallow-vs-deep-copy/src/test/java/ShallowCopyUnitTest.java
+++ java-shallow-vs-deep-copy/src/test/java/ShallowCopyUnitTest.java
@@ -9,32 +9,7 @@ import com.baeldung.model.Employee;
 
 public class ShallowCopyUnitTest {
     @Test
-    public void whenCreatingShallowCopy_thenObjectsShouldNotBeSame() {
-        // Create an original Employee object
-        Employee originalEmployee = new Employee();
-        originalEmployee.setId(1);
-        originalEmployee.setName("John Doe");
-
-        Address originalAddress = new Address();
-        originalAddress.setCity("New York");
-        originalAddress.setZipCode("12345");
-
-        originalEmployee.setAddress(originalAddress);
-
-        try {
-            // Perform a shallow copy of the original Employee object
-            Employee shallowCopy = (Employee) originalEmployee.clone();
-
-            // Assert that the original employee is not the same object as the shallow copy employee
-            assertNotSame(originalEmployee, shallowCopy);
-        } catch (CloneNotSupportedException e) {
-            fail("Cloning not supported");
-        }
-    }
-
-    @Test
     public void whenModifyingOriginalObject_thenCopyShouldChange() {
-        // Create an original Employee object
         Employee originalEmployee = new Employee();
         originalEmployee.setId(1);
         originalEmployee.setName("John Doe");
@@ -46,16 +21,13 @@ public class ShallowCopyUnitTest {
         originalEmployee.setAddress(originalAddress);
 
         try {
-            // Perform a shallow copy of the original Employee object
             Employee shallowCopy = (Employee) originalEmployee.clone();
 
-            // Modify the cloned employee's address
             originalEmployee.getAddress()
                 .setCity("Los Angeles");
             originalEmployee.getAddress()
                 .setZipCode("56789");
 
-            // Assert that the changes made to the original employee's address also reflect in the shallow copy employee's address
             assertEquals(originalEmployee.getAddress()
                 .getCity(), shallowCopy.getAddress()
                 .getCity());
@@ -67,3 +39,4 @@ public class ShallowCopyUnitTest {
         }
     }
 }
+
