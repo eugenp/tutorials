712e8b89e23f565b4d1c54d6db60f3d632ed0d45
Sam Gardner <samgardner909@gmail.com>
2023-07-13 19:07:35 +0100

    BAEL-6721 Add example code for notification of a complete task

---
+++ core-java-modules/core-java-concurrency-simple/src/main/java/com/baeldung/concurrent/notificationForCompleteTask/RunnableImpl.java
@@ -0,0 +1,22 @@
+package com.baeldung.concurrent.notificationForCompleteTask;
+
+public class RunnableImpl implements Runnable {
+
+    private final Runnable task;
+
+    private final CallbackInterface callback;
+
+    private final String taskDoneMessage;
+
+    public RunnableImpl(Runnable task, CallbackInterface callback, String taskDoneMessage) {
+        this.task = task;
+        this.callback = callback;
+        this.taskDoneMessage = taskDoneMessage;
+    }
+
+    public void run() {
+        task.run();
+        callback.taskDone(taskDoneMessage);
+    }
+
+}
