0cebdae48d15d0f2dc76ce767511d2f173eb4660
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2023-07-15 22:27:43 +0300

    Add files via upload

---
+++ core-java-modules/core-java-io-conversions-2/src/test/java/com/baeldung/stringtoinputstream/MapClearVsNewMapTest.java
@@ -0,0 +1,41 @@
+package com.baeldung.stringtoinputstream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapClearVsNewMapTest {
+
+    @Test
+    public void given_EmptyMap_whenUsingMapClear_thenMapIsEmpty() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+        map.clear();
+        Assertions.assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void given_NonEmptyMap_whenCreatingNewMapInstance_thenMapIsEmpty() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+        map = new HashMap<>();
+        Assertions.assertTrue(map.isEmpty());
+    }
+
+    @Test
+    public void given_OriginalMap_whenUsingMapClear_thenOtherReferencesStillPointToClearedMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("A", 1);
+        map.put("B", 2);
+        map.put("C", 3);
+        Map<String, Integer> originalMap = map;
+        map.clear();
+        Assertions.assertTrue(originalMap.isEmpty());
+    }
+}
