294afbbda8f97c7b636710563bf69cce53ad5b0a
Dhawal Kapil <dhawalkapil@gmail.com>
2023-07-15 16:55:03 +0530

    JAVA-20617 Migrated spring-soap to JDK 17
    - Upgraded plugins
    - Removed committed generated files

--- spring-soap/src/main/java/com/baeldung/springsoap/client/gen/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.baeldung.com/springsoap/gen", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.baeldung.springsoap.client.gen;
