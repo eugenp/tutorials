09e416abd8aaa5257e11fa3e9120d9336480d259
uzma <uzma.nasir@gmail.com>
2023-07-17 14:02:35 +0100

    [BAEL-6318] fix indentation

--- persistence-modules/spring-data-rest-2/src/main/java/com/baeldung/pageentityresponse/CustomPageImpl.java
+++ persistence-modules/spring-data-rest-2/src/main/java/com/baeldung/pageentityresponse/CustomPageImpl.java
@@ -14,12 +14,13 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CustomPageImpl<T> extends PageImpl<T> {
-
     @JsonCreator(mode = JsonCreator.Mode.PROPERTIES)
-    public CustomPageImpl(@JsonProperty("content") List<T> content, @JsonProperty("number") int number, @JsonProperty("size") int size, @JsonProperty("totalElements") Long totalElements, @JsonProperty("pageable") JsonNode pageable,
-      @JsonProperty("last") boolean last, @JsonProperty("totalPages") int totalPages, @JsonProperty("sort") JsonNode sort, @JsonProperty("numberOfElements") int numberOfElements) {
+    public CustomPageImpl(@JsonProperty("content") List<T> content, @JsonProperty("number") int number,
+      @JsonProperty("size") int size, @JsonProperty("totalElements") Long totalElements,
+      @JsonProperty("pageable") JsonNode pageable, @JsonProperty("last") boolean last,
+      @JsonProperty("totalPages") int totalPages, @JsonProperty("sort") JsonNode sort,
+      @JsonProperty("numberOfElements") int numberOfElements) {
         super(content, PageRequest.of(number, 1), 10);
-
     }
 
     public CustomPageImpl(List<T> content, Pageable pageable, long total) {
