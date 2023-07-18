f295258f98c23dd3e040f25bf855e338e995aa7e
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2023-07-19 01:19:49 +0300

    Update MapClearVsNewMapTest.java

--- core-java-modules/core-java-collections-maps-6/src/test/java/com/baeldung/mapclear/MapClearVsNewMapTest.java
+++ core-java-modules/core-java-collections-maps-6/src/test/java/com/baeldung/mapclear/MapClearVsNewMapTest.java
@@ -3,9 +3,6 @@ package com.baeldung.mapclear;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MapClearVsNewMapTest {
 
     @Test
