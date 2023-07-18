9d872661c799f3e67fe2863a15a1a2b4634afa2a
Philippe Sevestre <psevestre@gmail.com>
2023-07-18 01:03:47 -0300

    BAEL-6138

---
+++ spring-integration/src/main/java/com/baeldung/subflows/postgresqlnotify/PostgresqlPubSubExample.java
@@ -0,0 +1,10 @@
+package com.baeldung.subflows.postgresqlnotify;
+
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
+
+@EnableIntegration
+@IntegrationComponentScan
+public class PostgresqlPubSubExample {
+
+}
