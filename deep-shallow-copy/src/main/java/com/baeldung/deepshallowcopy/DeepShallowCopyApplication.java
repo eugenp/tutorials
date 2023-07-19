c738045da3d44cb0088e2cdd7fd1a8671e700d86
Amol Gote <aamolgote@hotmail.com>
2023-07-18 22:31:20 -0400

    Incorporate review comments

--- deep-shallow-copy/src/main/java/com/baeldung/deepshallowcopy/DeepShallowCopyApplication.java
+++ deep-shallow-copy/src/main/java/com/baeldung/deepshallowcopy/DeepShallowCopyApplication.java
@@ -8,25 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeepShallowCopyApplication {
 
 	public static void main(String[] args) {
-		Category electronics = new Category("Electronics");
-		Product originalProduct = new Product("TV", electronics);
 
-		// Shallow copy
-		Product shallowCopiedProduct = originalProduct.shallowCopy();
-		shallowCopiedProduct.getCategory().setName("Appliances");
-		System.out.println("After shallow copy");
-		System.out.println("Original product category: " + originalProduct.getCategory().getName()); // Outputs: Appliances
-		System.out.println("Copied product category: " + shallowCopiedProduct.getCategory().getName()); // Outputs: Appliances
-
-		// Reset the original product category
-		originalProduct.getCategory().setName("Electronics");
-
-		// Deep copy
-		Product deepCopiedProduct = originalProduct.deepCopy();
-		deepCopiedProduct.getCategory().setName("Appliances");
-		System.out.println("\nAfter deep copy");
-		System.out.println("Original product category: " + originalProduct.getCategory().getName()); // Outputs: Electronics
-		System.out.println("Copied product category: " + deepCopiedProduct.getCategory().getName()); // Outputs: Appliances
 	}
-
 }
