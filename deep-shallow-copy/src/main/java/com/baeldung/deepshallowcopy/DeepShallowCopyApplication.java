2338b070f5ea2a3dfc2ce586d3939fa091d29787
Amol Gote <aamolgote@hotmail.com>
2023-07-21 17:36:37 -0400

    incorporated review comments

--- deep-shallow-copy/src/main/java/com/baeldung/deepshallowcopy/DeepShallowCopyApplication.java
+++ deep-shallow-copy/src/main/java/com/baeldung/deepshallowcopy/DeepShallowCopyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DeepShallowCopyApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-	}
+    }
 }
