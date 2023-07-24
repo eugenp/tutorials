2ada5244d6132a77b8ec9ba452815c343beeaeb3
Alex Tighe <599747+atighe@users.noreply.github.com>
2023-07-23 23:32:31 -0400

    BAEL-6458: convert checks to tests

--- core-java-modules/core-java-conditionals/src/test/java/com/baeldung/conditionals/YieldTest.java
+++ core-java-modules/core-java-conditionals/src/test/java/com/baeldung/conditionals/YieldTest.java
@@ -1,23 +1,25 @@
 package com.baeldung.conditionals;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class YieldTest {
 
     @Test
     void testAnalyze() {
-        Assertions.assertEquals("Got a 1", YieldExamples.analyze(YieldExamples.Number.ONE));
+        assertEquals("Got a 1", YieldExamples.analyze(YieldExamples.Number.ONE));
     }
 
     @Test
     void testAnalyzeNumberArrow() {
-        Assertions.assertEquals("Got a 2", YieldExamples.analyzeNumberArrow(YieldExamples.Number.TWO));
+        assertEquals("Got a 2", YieldExamples.analyzeNumberArrow(YieldExamples.Number.TWO));
     }
 
     @Test
     void testAnalyzeNumberColon() {
-        Assertions.assertEquals("More than 2", YieldExamples.analyzeNumberColon(YieldExamples.Number.THREE));
+        assertEquals("More than 2", YieldExamples.analyzeNumberColon(YieldExamples.Number.THREE));
     }
 
 }
