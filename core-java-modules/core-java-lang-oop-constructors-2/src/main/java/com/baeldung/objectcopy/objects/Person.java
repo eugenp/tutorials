c9cb9261d31f17fde0bdd2127baed7e3b10bc03a
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-07-25 03:10:44 +0100

    Update Person.java

--- core-java-modules/core-java-lang-oop-constructors-2/src/main/java/com/baeldung/objectcopy/objects/Person.java
+++ core-java-modules/core-java-lang-oop-constructors-2/src/main/java/com/baeldung/objectcopy/objects/Person.java
@@ -1,27 +1,45 @@
 package com.baeldung.objectcopy.objects;
 
-public class Person {
-    private String name;
-    private int score;
-
-    public Person(String name, int score) {
-        this.name = name;
-        this.score = score;
-    }
-
-    // Deep copy constructor
-    public Person(Person other) {
-        this.name = other.name;
-        this.score = other.score;
-    }
-
-    // Getters and setters
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+public class Person implements Cloneable { 
+    private String name; 
+    private int score; 
+    private List<String> hobbies; 
+    public Person(String name, int score, List<String> hobbies) { 
+        this.name = name; 
+        this.score = score; 
+        this.hobbies = hobbies; 
+    } 
+    public Person(Person other) { 
+        this.name = other.name; 
+        this.score = other.score; 
+        this.hobbies = new ArrayList<>(other.hobbies); 
+    } 
+    public void setName(String name) { 
+        this.name = name; 
+    } 
+    public String getName() { 
+        return name; 
+    } 
+    public void setScore(int score) {
+        this.score = score; 
+    } 
+    public int getScore() { 
+        return score; 
+    } 
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies; 
+    } 
+    public List<String> getHobbies() {
+        return hobbies; 
+    } 
+    @Override protected Object clone() {
+        try { 
+            Person cloned = (Person) super.clone(); 
+            cloned.hobbies = new ArrayList<>(hobbies); 
+            return cloned; 
+        } 
+        catch (CloneNotSupportedException e) {
+            throw new AssertionError(e); 
+        } 
+    } 
 }
