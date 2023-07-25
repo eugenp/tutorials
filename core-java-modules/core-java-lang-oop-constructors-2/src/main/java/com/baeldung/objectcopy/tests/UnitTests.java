4ba6bb9aa539852c55a7dc4c5a658ce7b80d888d
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-07-25 03:14:03 +0100

    Create UnitTests.java

---
+++ core-java-modules/core-java-lang-oop-constructors-2/src/main/java/com/baeldung/objectcopy/tests/UnitTests.java
@@ -0,0 +1,33 @@
+public class PersonTest {
+
+    private List<String> hobbies;
+
+    @BeforeEach
+    public void setUp() {
+        hobbies = new ArrayList<>();
+        hobbies.add("Reading");
+        hobbies.add("Swimming");
+    }
+
+    @Test
+    public void testShallowCopy() {
+        Person original = new Person("John Bull", 69, hobbies);
+
+        Person shallowCopy = original;
+
+        original.getHobbies().add("Cycling");
+
+        assertEquals(original.getHobbies(), shallowCopy.getHobbies());
+    }
+
+    @Test
+    public void testDeepCopy() {
+        Person original = new Person("John Bull", 69, hobbies);
+
+        Person deepCopy = (Person) original.clone();
+
+        original.getHobbies().add("Cycling");
+
+        assertNotEquals(original.getHobbies(), deepCopy.getHobbies());
+    }
+}
