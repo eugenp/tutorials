17e56bbce744606da8853be7619c392d31dfebc5
Balamurugan <balamurugan.radhakrishnan@gmail.com>
2023-07-27 10:57:06 +0100

    BAEL-6584: Added spring data reactive pagination along with tests (#14478)
    * BAEL-6584: Added spring data reactive pagination along with tests
    
    * BAEL-6584: removed unnecessary spaces
    
    ---------
    
    Co-authored-by: balasr3 <balamurugan.radhakrishnan@imgarena.com>

---
+++ spring-reactive-modules/spring-5-data-reactive-2/src/main/java/com/baeldung/pagination/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.baeldung.pagination.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+
+import io.r2dbc.spi.ConnectionFactory;
+
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
+        ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+        initializer.setConnectionFactory(connectionFactory);
+
+        CompositeDatabasePopulator populator = new CompositeDatabasePopulator();
+        populator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("init.sql")));
+        initializer.setDatabasePopulator(populator);
+
+        return initializer;
+    }
+
+}
