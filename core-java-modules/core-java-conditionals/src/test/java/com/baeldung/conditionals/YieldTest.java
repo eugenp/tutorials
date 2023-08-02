9468d851e5467f774712c9aa5cd8fa6ddb1c9c8c
Alex Tighe <599747+atighe@users.noreply.github.com>
2023-08-01 23:35:30 -0400

    BAEL-6458: move examples to just YieldTest and removed YieldExamples class. Also added assigning the value of the switch expression to a variable instead of returning from method. Also added a non-default exhaustive test.

--- core-java-modules/core-java-conditionals/src/test/java/com/baeldung/conditionals/YieldTest.java
+++ core-java-modules/core-java-conditionals/src/test/java/com/baeldung/conditionals/YieldTest.java
@@ -7,19 +7,77 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class YieldTest {
 
-    @Test
-    void testAnalyze() {
-        assertEquals("Got a 1", YieldExamples.analyze(YieldExamples.Number.ONE));
+    public enum Number {
+        ONE, TWO, THREE, FOUR
     }
 
     @Test
-    void testAnalyzeNumberArrow() {
-        assertEquals("Got a 2", YieldExamples.analyzeNumberArrow(YieldExamples.Number.TWO));
+    void whenSwitchingOnNumberOne_thenWillReturnString() {
+        Number number = Number.ONE;
+        String message;
+        switch (number) {
+            case ONE:
+                message = "Got a 1";
+                break;
+            case TWO:
+                message = "Got a 2";
+                break;
+            default:
+                message = "More than 2";
+        }
+
+        assertEquals("Got a 1", message);
     }
 
     @Test
-    void testAnalyzeNumberColon() {
-        assertEquals("More than 2", YieldExamples.analyzeNumberColon(YieldExamples.Number.THREE));
+    void whenSwitchingWithArrowOnNumberTwo_thenWillReturnString() {
+        Number number = Number.TWO;
+        String message = switch (number) {
+            case ONE -> {
+                yield "Got a 1";
+            }
+            case TWO -> {
+                yield "Got a 2";
+            }
+            default -> {
+                yield "More than 2";
+            }
+        };
+
+        assertEquals("Got a 2", message);
+    }
+
+    @Test
+    void whenSwitchingWithArrowNoDefaultOnNumberTwo_thenWillReturnString() {
+        Number number = Number.TWO;
+        String message = switch (number) {
+            case ONE -> {
+                yield "Got a 1";
+            }
+            case TWO -> {
+                yield "Got a 2";
+            }
+            case THREE, FOUR -> {
+                yield "More than 2";
+            }
+        };
+
+        assertEquals("Got a 2", message);
+    }
+
+    @Test
+    void whenSwitchingWithColonOnNumberTwo_thenWillReturnString() {
+        Number number = Number.TWO;
+        String message = switch (number) {
+            case ONE:
+                yield "Got a 1";
+            case TWO:
+                yield "Got a 2";
+            default:
+                yield "More than 2";
+        };
+
+        assertEquals("Got a 2", message);
     }
 
 }
