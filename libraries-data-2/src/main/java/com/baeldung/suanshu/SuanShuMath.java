2d02afc6260e28ef7a11dceb920f643a523911fd
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2023-08-05 21:24:21 +0300

    comment suanshu library - see JAVA-24004

--- libraries-data-2/src/main/java/com/baeldung/suanshu/SuanShuMath.java
+++ libraries-data-2/src/main/java/com/baeldung/suanshu/SuanShuMath.java
@@ -1,5 +1,6 @@
 package com.baeldung.suanshu;
 
+/*
 import com.numericalmethod.suanshu.algebra.linear.matrix.doubles.Matrix;
 import com.numericalmethod.suanshu.algebra.linear.matrix.doubles.matrixtype.dense.DenseMatrix;
 import com.numericalmethod.suanshu.algebra.linear.matrix.doubles.operation.Inverse;
@@ -9,6 +10,7 @@ import com.numericalmethod.suanshu.analysis.function.polynomial.Polynomial;
 import com.numericalmethod.suanshu.analysis.function.polynomial.root.PolyRoot;
 import com.numericalmethod.suanshu.analysis.function.polynomial.root.PolyRootSolver;
 import com.numericalmethod.suanshu.number.complex.Complex;
+*/
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -137,5 +139,5 @@ class SuanShuMath {
         List<? extends Number> roots = solver.solve(p);
         log.info("Finding polynomial roots: {}", roots);
     }
-*//
+*/
 }
