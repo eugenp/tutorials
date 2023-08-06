c0dcba25386312884ff81200283d8c5cd94a07c0
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2023-08-06 06:25:26 -0700

    BAEL-6801 Validate string length with Java validation annotations

---
+++ spring-boot-modules/spring-boot-validation/src/main/java/com/baeldung/listvalidation/SpringJobApplicationDemoApp.java
@@ -0,0 +1,12 @@
+package com.baeldung.listvalidation;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJobApplicationDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJobApplicationDemoApp.class, args);
+    }
+}
