0612a17bc94fbc3c1de150a25210156d958eb4f8
anuragkumawat <anuragkumawat7@gmail.com>
2023-08-06 13:19:45 +0530

    JAVA-19717 Correct formatting changes

--- algorithms-modules/algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithm.java
+++ algorithms-modules/algorithms-miscellaneous-5/src/main/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithm.java
@@ -7,28 +7,28 @@ public class KadaneAlgorithm {
 
     private Logger logger = LoggerFactory.getLogger(KadaneAlgorithm.class.getName());
 
-	public int maxSubArraySum(int[] arr) {
+    public int maxSubArraySum(int[] arr) {
 
-		int size = arr.length;
-		int start = 0;
-		int end = 0;
-		
-		int maxSoFar = arr[0], maxEndingHere = arr[0];
+        int size = arr.length;
+        int start = 0;
+        int end = 0;
 
-		for (int i = 1; i < size; i++) {
-			maxEndingHere = maxEndingHere + arr[i];
-			if (arr[i] > maxEndingHere) {
-				maxEndingHere = arr[i];
-				if (maxSoFar < maxEndingHere) {
-					start = i;
-				}
-			}
-			if (maxSoFar < maxEndingHere) {
-				maxSoFar = maxEndingHere;
-				end = i;
-			}
-		}
-		logger.info("Found Maximum Subarray between {} and {}", Math.min(start, end), end);
-		return maxSoFar;
-	}
+        int maxSoFar = arr[0], maxEndingHere = arr[0];
+
+        for (int i = 1; i < size; i++) {
+            maxEndingHere = maxEndingHere + arr[i];
+            if (arr[i] > maxEndingHere) {
+                maxEndingHere = arr[i];
+                if (maxSoFar < maxEndingHere) {
+                    start = i;
+                }
+            }
+            if (maxSoFar < maxEndingHere) {
+                maxSoFar = maxEndingHere;
+                end = i;
+            }
+        }
+        logger.info("Found Maximum Subarray between {} and {}", Math.min(start, end), end);
+        return maxSoFar;
+    }
 }
--- algorithms-modules/algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithmUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-5/src/test/java/com/baeldung/algorithms/maximumsubarray/KadaneAlgorithmUnitTest.java
@@ -27,7 +27,7 @@ class KadaneAlgorithmUnitTest {
         //then
         assertEquals(-1, maxSum);
     }
-    
+
     @Test
     void givenArrayWithAllPosiitveNumbersWhenMaximumSubarrayThenReturnsExpectedResult() {
         //given
@@ -38,16 +38,16 @@ class KadaneAlgorithmUnitTest {
         //then
         assertEquals(10, maxSum);
     }
-    
+
     @Test
-	void givenArrayToTestStartIndexWhenMaximumSubarrayThenReturnsExpectedResult() {
-		// given
-		int[] arr = new int[] { 1, 2, -1, 3, -6, -2 };
-		// when
-		KadaneAlgorithm algorithm = new KadaneAlgorithm();
-		int maxSum = algorithm.maxSubArraySum(arr);
-		// then
-		assertEquals(5, maxSum);
-	}
+    void givenArrayToTestStartIndexWhenMaximumSubarrayThenReturnsExpectedResult() {
+        //given
+        int[] arr = new int[] { 1, 2, -1, 3, -6, -2 };
+        //when
+        KadaneAlgorithm algorithm = new KadaneAlgorithm();
+        int maxSum = algorithm.maxSubArraySum(arr);
+        //then
+        assertEquals(5, maxSum);
+    }
 
 }
