9a4df9e8f1aa6ce0d15543baf1e85bd6ec5e02d2
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-08-11 10:11:05 +0100

    Create SumArraysUsingStreams.java

---
+++ core-java-modules/core-java-arrays-operations-advanced/src/main/java/com/baeldung/arraysums/SumArraysUsingStreams.java
@@ -0,0 +1,16 @@
+package com.example.arraysums;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SumArraysUsingStreams {
+
+    public static int[] sumOfTwoArrays(int[] arr1, int[] arr2) {
+        IntStream stream1 = Arrays.stream(arr1);
+        IntStream stream2 = Arrays.stream(arr2);
+        IntStream stream3 = IntStream.zip(stream1, stream2);
+        IntStream stream4 = stream3.map(pair -> pair[0] + pair[1]);
+        int[] arr3 = stream4.toArray();
+        return arr3;
+    }
+}
