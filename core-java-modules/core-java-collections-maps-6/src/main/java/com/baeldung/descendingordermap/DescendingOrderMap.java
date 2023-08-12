5ab9cbc41588c31573aafa22d73a0ed8e6907253
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2023-08-12 13:20:43 +0300

    Update DescendingOrderMap.java

--- core-java-modules/core-java-collections-maps-6/src/main/java/com/baeldung/descendingordermap/DescendingOrderMap.java
+++ core-java-modules/core-java-collections-maps-6/src/main/java/com/baeldung/descendingordermap/DescendingOrderMap.java
@@ -3,14 +3,13 @@ package com.baeldung.descendingordermap;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DescendingOrderMap {
     public static <K, V extends Comparable<? super V>> Map<K, V> sortMapByValueDescending(Map<K, V> map) {
-        Comparator<Map.Entry<K, V>> valueComparator = (entry1, entry2) -> entry2.getValue().compareTo(entry1.getValue());
-        Map<K, V> sortedMap = new LinkedHashMap<>();
-        map.entrySet().stream()
-                .sorted(valueComparator)
-                .forEach(entry -> sortedMap.put(entry.getKey(), entry.getValue()));
-        return sortedMap;
+        return map.entrySet()
+                .stream()
+                .sorted(Map.Entry.<K, V>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
     }
 }
