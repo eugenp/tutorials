2abbc019fcf767ad940014a656ffc26f0bba44d4
Abhinav Pandey <abhinavpandey312@gmail.com>
2023-08-13 09:52:02 +0530

    Bael 6625 new (#14476)
    * BAEL-6625 - spring data neo4j example
    
    * BAEL-6625 - spring data neo4j example
    
    * BAEL-6625 - optimized imports
    
    * BAEL-6625 - moving project to jdk9 and above
    
    * BAEL-6625 - changing test names

---
+++ persistence-modules/spring-data-neo4j/src/main/java/com/baeldung/spring/data/neo4j/domain/Author.java
@@ -0,0 +1,48 @@
+package com.baeldung.spring.data.neo4j.domain;
+
+
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.schema.Relationship;
+
+import java.util.List;
+
+@Node("Author")
+public class Author {
+    @Id
+    private Long id;
+
+    private String name;
+
+    @Relationship(type = "WRITTEN_BY", direction = Relationship.Direction.INCOMING)
+    private List<Book> books;
+
+    public Author(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
