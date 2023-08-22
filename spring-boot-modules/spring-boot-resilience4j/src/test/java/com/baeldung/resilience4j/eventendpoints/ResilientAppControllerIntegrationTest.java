287c8c41bdf7a060a2c8e68eb979756a1a6118c2
panos-kakos <kakos.programmer@gmail.com>
2023-08-22 07:37:35 +0300

    [JAVA-22589] Converted integration test to manual

--- spring-boot-modules/spring-boot-resilience4j/src/test/java/com/baeldung/resilience4j/eventendpoints/ResilientAppControllerIntegrationTest.java
+++ spring-boot-modules/spring-boot-resilience4j/src/test/java/com/baeldung/resilience4j/eventendpoints/ResilientAppControllerIntegrationTest.java
@@ -33,7 +33,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class ResilientAppControllerIntegrationTest {
+class ResilientAppControllerManualTest {
 
   private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
