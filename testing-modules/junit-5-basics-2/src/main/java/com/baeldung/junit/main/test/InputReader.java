fd6b6aa6d530213a5aedf19ca6c3988346b34dab
Tetiana Okhotnik <tanyaohotnik3003@gmail.com>
2023-08-25 20:12:50 +0300

    BAEL-6812 Test Main Method with JUnit - added sample to test main method with different architecture

---
+++ testing-modules/junit-5-basics-2/src/main/java/com/baeldung/junit/main/test/InputReader.java
@@ -0,0 +1,40 @@
+package com.baeldung.junit.main.test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class InputReader {
+
+    public String read(InputType inputType, String fileName) {
+        switch (inputType) {
+        case FILE:
+            return readFromFile(fileName);
+        case CONSOLE:
+            return readFromConsole();
+        default:
+            return null;
+        }
+    }
+
+    private String readFromConsole() {
+        System.out.println("Enter values for calculation: \n");
+        String str = new Scanner(System.in).nextLine();
+
+        return str;
+    }
+
+    private String readFromFile(String fileName) {
+        String readString = null;
+        try {
+            readString = Files.readString(Path.of(URI.create(fileName)));
+            System.out.println(readString);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+}
