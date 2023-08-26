a40497a476f762a3c85177a6811ae77dbfdf3ecb
gaepi <gae.piaz@gmail.com>
2023-08-26 20:31:52 +0200

    JAVA-71 | added custom dockerfile to run preconfigured couchbase instance. Removed unused classes.

---
+++ persistence-modules/spring-data-couchbase-2/src/test/docker/dockerbuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# setup
+set -ex
+
+# main
+docker build -t couchbase_image .
+
+# cleanup
+set +ex
+
+# run
+docker run -d --name couchbase_container -p 8091-8096:8091-8096 -p 11210-11211:11210-11211 couchbase_image
