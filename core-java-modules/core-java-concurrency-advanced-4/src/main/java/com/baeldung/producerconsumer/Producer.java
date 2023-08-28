67750f826cc6bd36a7b507fb6c1f958d8e1e3788
Gaetano Piazzolla <gae.piaz@gmail.com>
2023-08-28 19:38:00 +0200

    JAVA-23377 | fixing sync code block (#14574)

--- core-java-modules/core-java-concurrency-advanced-4/src/main/java/com/baeldung/producerconsumer/Producer.java
+++ core-java-modules/core-java-concurrency-advanced-4/src/main/java/com/baeldung/producerconsumer/Producer.java
@@ -1,12 +1,13 @@
 package com.baeldung.producerconsumer;
 
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Logger;
 
 public class Producer implements Runnable {
     private static final Logger log = Logger.getLogger(Producer.class.getCanonicalName());
     private final DataQueue dataQueue;
-
     private static int idSequence = 0;
+    final ReentrantLock lock = new ReentrantLock();
 
     public Producer(DataQueue dataQueue) {
         this.dataQueue = dataQueue;
@@ -19,22 +20,38 @@ public class Producer implements Runnable {
 
     public void produce() {
         while (dataQueue.runFlag) {
-            while (dataQueue.isFull() && dataQueue.runFlag) {
-                try {
-                    dataQueue.waitOnFull();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+
+            try {
+                lock.lock();
+
+                while (dataQueue.isFull() && dataQueue.runFlag) {
+                    try {
+                        dataQueue.waitOnFull();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                        break;
+                    }
+                }
+
+                if (!dataQueue.runFlag) {
                     break;
                 }
-            }
-            if (!dataQueue.runFlag) {
-                break;
-            }
-            Message message = generateMessage();
-            dataQueue.add(message);
-            dataQueue.notifyAllForEmpty();
 
+                Message message = generateMessage();
+                dataQueue.add(message);
+                dataQueue.notifyAllForEmpty();
+
+                log.info("Size of the queue is: " + dataQueue.getSize());
+
+            }
+            finally{
+                lock.unlock();
+            }
+
+            //Sleeping on random time to make it realistic
+            ThreadUtil.sleep((long) (Math.random() * 100));
         }
+
         log.info("Producer Stopped");
     }
 
@@ -42,9 +59,6 @@ public class Producer implements Runnable {
         Message message = new Message(incrementAndGetId(), Math.random());
         log.info(String.format("[%s] Generated Message. Id: %d, Data: %f%n",
                 Thread.currentThread().getName(), message.getId(), message.getData()));
-
-        //Sleeping on random time to make it realistic
-        ThreadUtil.sleep((long) (message.getData() * 100));
 
         return message;
     }
