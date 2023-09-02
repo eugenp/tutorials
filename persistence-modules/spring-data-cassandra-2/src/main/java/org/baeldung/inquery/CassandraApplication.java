fc10cc073a4ec212fb23ccc6d1904ca360ee349e
Saikat <saikatcse03@gmail.com>
2023-09-02 18:01:44 +0530

    Add Cassandra IN Query and update pom file

---
+++ persistence-modules/spring-data-cassandra-2/src/main/java/org/baeldung/inquery/CassandraApplication.java
@@ -0,0 +1,15 @@
+package org.baeldung.inquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+@SpringBootApplication
+@EnableCassandraRepositories(basePackages = "org.baeldung.inquery.repository")
+public class CassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraApplication.class, args);
+	}
+
+}
