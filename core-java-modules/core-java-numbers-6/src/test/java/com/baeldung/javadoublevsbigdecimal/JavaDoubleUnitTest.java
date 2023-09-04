0924c5ede44c2fb756959d81129df5754b9baa1a
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-09-04 21:03:21 +0100

    Update JavaDoubleUnitTest.java

--- core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/JavaDoubleUnitTest.java
+++ core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/JavaDoubleUnitTest.java
@@ -8,7 +8,6 @@ public class JavaDoubleUnitTest {
     public void givenDoubleLiteral_whenAssigningToDoubleVariable_thenValueIsNotExactlyEqual() {
         double d = 0.1;
         double epsilon = 0.0000000000000001;
-        assertNotEquals(0.1, d);
         assertEquals(0.1, d, epsilon);
     }
 }
