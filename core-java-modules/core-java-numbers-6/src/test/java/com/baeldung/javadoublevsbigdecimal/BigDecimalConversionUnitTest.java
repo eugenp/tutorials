b6ec0b1e14bb9494b6e04aa458aaeee6911a043d
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-09-04 20:21:41 +0100

    Create BigDecimalConversionUnitTest.java

---
+++ core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/BigDecimalConversionUnitTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BigDecimalConversionUnitTest {
+    @Test
+    void whenConvertingDoubleToBigDecimal_thenConversionIsCorrect() {
+        double doubleValue = 123.456;
+        BigDecimal bigDecimalValue = BigDecimal.valueOf(doubleValue);
+        BigDecimal expected = new BigDecimal("123.456").setScale(3, RoundingMode.HALF_UP);
+        assertEquals(expected, bigDecimalValue.setScale(3, RoundingMode.HALF_UP));
+    }
+
+    @Test
+    void whenConvertingBigDecimalToDouble_thenConversionIsCorrect() {
+        BigDecimal bigDecimalValue = new BigDecimal("789.123456789").setScale(9, RoundingMode.HALF_UP);
+        double doubleValue = bigDecimalValue.doubleValue();
+        double expected = new BigDecimal("789.123456789").setScale(9, RoundingMode.HALF_UP).doubleValue();
+        assertEquals(expected, doubleValue);
+    }
+}
