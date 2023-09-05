5ad2e62a923a82362d69a584d9f0cd031ba0ca71
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-09-05 22:25:33 +0100

    Update JavaDoubleUnitTest.java
    change variable name from d to doubleValue

--- core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/JavaDoubleUnitTest.java
+++ core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/JavaDoubleUnitTest.java
@@ -6,8 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class JavaDoubleUnitTest {
     @Test
     public void givenDoubleLiteral_whenAssigningToDoubleVariable_thenValueIsNotExactlyEqual() {
-        double d = 0.1;
+        double doubleValue = 0.1;
         double epsilon = 0.0000000000000001;
-        assertEquals(0.1, d, epsilon);
+        assertEquals(0.1, doubleValue, epsilon);
     }
 }
