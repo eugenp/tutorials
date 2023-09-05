d45ed09d02b63b13ad8153631648c102ff7a6853
Michael Olayemi <michaelolayemi28@gmail.com>
2023-09-05 08:50:25 +0000

    Differences between Heap Dump, thread dump and core dump

---
+++ core-java-modules/core-java-perf-2/src/main/java/com/baeldung/dumps/CoreDump.c
@@ -0,0 +1,16 @@
+#include <jni.h>
+#include "CoreDump.h"
+
+void core() {
+  int *p = NULL;
+  *p = 0;
+}
+
+JNIEXPORT void JNICALL Java_CoreDump_core
+  (JNIEnv *env, jobject obj) {
+  core();
+};
+
+void main() {
+}
+
