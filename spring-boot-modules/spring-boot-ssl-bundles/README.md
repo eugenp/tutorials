4d0a29faf3e0df6a0eb919a9668e900bd45ddb28
Oleksandr Molochko <alexander@crosp.net>
2023-09-06 07:26:53 +0300

    BAEL-6866: Securing Spring Boot 3 Applications with SSL Bundles

---
+++ spring-boot-modules/spring-boot-ssl-bundles/README.md
@@ -0,0 +1,2 @@
+## Relevant Articles
+- [HTTPS using Self-Signed Certificate in Spring Boot](https://www.baeldung.com/spring-boot-https-self-signed-certificate)
---
+++ spring-boot-modules/spring-boot-ssl-bundles/src/test/java/com/baeldung/springbootsslbundles/SSLBundleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baeldung.springbootsslbundles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SSLBundleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
