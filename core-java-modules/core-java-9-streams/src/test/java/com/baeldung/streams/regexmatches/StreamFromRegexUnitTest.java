d140fb31bc2a6674bef654a4e4f0f30413e5efab
tienvn <tienvn@>
2023-09-07 21:27:26 +0700

    add unit test

--- core-java-modules/core-java-9-streams/src/test/java/com/baeldung/streams/regexmatches/StreamFromRegexUnitTest.java
+++ core-java-modules/core-java-9-streams/src/test/java/com/baeldung/streams/regexmatches/StreamFromRegexUnitTest.java
@@ -11,10 +11,17 @@ import static org.junit.Assert.assertEquals;
 public class StreamFromRegexUnitTest {
 
     @Test
-    public void whenInputStringIncludeLettersAndNumbers_ThenReturnStreamOfNumbers() {
+    public void whenInputStringIncludeLettersAndNumbersAndRegex_ThenReturnStreamOfNumbers() {
         List<String> result = StreamFromRegexUtil.getStream("There are 3 apples and 2 bananas on the table.", "\\d+")
                 .collect(Collectors.toList());
         assertEquals(asList("3", "2"), result);
+    }
+
+    @Test
+    public void whenInputStringsAndRegex_ThenReturnStreamOfJavaWords() {
+        List<String> result = StreamFromRegexUtil.getStream("sample sentence with some words Java Java", "\\bJava\\b")
+                .collect(Collectors.toList());
+        assertEquals(asList("Java", "Java"), result);
     }
 
 }
