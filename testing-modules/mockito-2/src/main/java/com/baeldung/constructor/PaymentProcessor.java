6d28a4ee0172c58d4299a66808e30008f7ee7800
shahulbasha <shhl.basha333@gmail.com>
2023-09-09 22:49:21 +0530

    BAEL-6830 example changed based on editor review comments

---
+++ testing-modules/mockito-2/src/main/java/com/baeldung/constructor/PaymentProcessor.java
@@ -0,0 +1,18 @@
+package com.baeldung.constructor;
+
+public class PaymentProcessor {
+
+    private final PaymentService paymentService;
+
+    public PaymentProcessor() {
+        this.paymentService = new PaymentService();
+    }
+
+    public PaymentProcessor(String paymentMode) {
+        this.paymentService = new PaymentService(paymentMode);
+    }
+
+    public String processPayment(){
+        return paymentService.processPayment();
+    }
+}
