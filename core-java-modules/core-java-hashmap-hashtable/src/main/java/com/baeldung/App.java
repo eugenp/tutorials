0d7e0ea13b89133b2518a74738eaab534e097811
Amol Gote <aamolgote@hotmail.com>
2023-09-09 15:51:22 -0400

    BAEL-6791 - Unit tets

--- core-java-modules/core-java-hashmap-hashtable/src/main/java/com/baeldung/App.java
+++ core-java-modules/core-java-hashmap-hashtable/src/main/java/com/baeldung/App.java
@@ -4,14 +4,16 @@ import com.baeldung.concurrenthashmap.ConcurrentHashMapExample;
 import com.baeldung.hashtable.HashTableExample;
 
 public class App {
-    public static void main(String[] args) {
-        HashTableExample.simpleHashTable();
-        ConcurrentHashMapExample.simpleConcurrentHashMap();
-
-        HashTableExample.hashTableThreadSafety();
-        ConcurrentHashMapExample.concurrentHashMapThreadSafety();
-
+    public static void main(String[] args) throws Exception {
+//        HashTableExample.simpleHashTable();
+//        ConcurrentHashMapExample.simpleConcurrentHashMap();
+//
+//        HashTableExample.hashTableThreadSafety();
+//        ConcurrentHashMapExample.concurrentHashMapThreadSafety();
+//
         HashTableExample.hashTableFailFast();
-        ConcurrentHashMapExample.concurrentHashMapWeaklyConsistent();
+//        ConcurrentHashMapExample.concurrentHashMapWeaklyConsistent();
+
+        //org.openjdk.jmh.Main.main(args);
     }
 }
---
+++ core-java-modules/core-java-hashmap-hashtable/src/main/java/com/baeldung/PerformanceBenchMark.java
@@ -0,0 +1,163 @@
+package com.baeldung;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 1)
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
+@Threads(10) // 10 threads for the test
+public class PerformanceBenchMark {
+    private Hashtable<String, Integer> hashTable;
+    private ConcurrentHashMap<String, Integer> concurrentHashMap;
+
+    @Setup(Level.Trial)
+    public void setup() {
+        hashTable = new Hashtable<>();
+        concurrentHashMap = new ConcurrentHashMap<>();
+    }
+
+    @Benchmark
+    @Group("hashtable")
+    public void benchmarkHashtablePut() {
+        for (int i = 0; i < 10000; i++) {
+            hashTable.put(String.valueOf(i), i);
+        }
+    }
+
+    @Benchmark
+    @Group("hashtable")
+    public void benchmarkHashtableGet() {
+        for (int i = 0; i < 10000; i++) {
+            hashTable.get(String.valueOf(i));
+        }
+    }
+
+    @Benchmark
+    @Group("concurrentHashMap")
+    public void benchmarkConcurrentHashMapPut() {
+        for (int i = 0; i < 10000; i++) {
+            concurrentHashMap.put(String.valueOf(i), i);
+        }
+    }
+
+    @Benchmark
+    @Group("concurrentHashMap")
+    public void benchmarkConcurrentHashMapGet() {
+        for (int i = 0; i < 10000; i++) {
+            concurrentHashMap.get(String.valueOf(i));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Options options = new OptionsBuilder()
+                .include(PerformanceBenchMark.class.getSimpleName())
+                .shouldFailOnError(true)
+                .shouldDoGC(true)
+                .jvmArgs("-server")
+                .build();
+        new Runner(options).run();
+    }
+}
+
+
+//@BenchmarkMode(Mode.AverageTime)
+//@OutputTimeUnit(TimeUnit.MILLISECONDS)
+//@State(Scope.Thread)
+//public class PerformanceBenchMark {
+//
+//    private Hashtable<String, Integer> hashTable;
+//    private ConcurrentHashMap<String, Integer> concurrentHashMap;
+//
+//    @Setup
+//    public void setup() {
+//        hashTable = new Hashtable<>();
+//        concurrentHashMap = new ConcurrentHashMap<>();
+//    }
+//
+//    @Benchmark
+//    @Group("hashtable")
+//    public void benchmarkHashtablePut() {
+//        for (int i = 0; i < 10000; i++) {
+//            hashTable.put(String.valueOf(i), i);
+//        }
+//    }
+//
+//    @Benchmark
+//    @Group("hashtable")
+//    public void benchmarkHashtableGet() {
+//        for (int i = 0; i < 10000; i++) {
+//            hashTable.get(String.valueOf(i));
+//        }
+//    }
+//
+//    @Benchmark
+//    @Group("concurrentHashMap")
+//    public void benchmarkConcurrentHashMapPut() {
+//        for (int i = 0; i < 10000; i++) {
+//            concurrentHashMap.put(String.valueOf(i), i);
+//        }
+//    }
+//
+//    @Benchmark
+//    @Group("concurrentHashMap")
+//    public void benchmarkConcurrentHashMapGet() {
+//        for (int i = 0; i < 10000; i++) {
+//            concurrentHashMap.get(String.valueOf(i));
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        Options options = new OptionsBuilder()
+//                .include(PerformanceBenchMark.class.getSimpleName())
+//                .threads(10)
+//                .forks(1)
+//                .shouldFailOnError(true)
+//                .shouldDoGC(true)
+//                .jvmArgs("-server")
+//                .build();
+//
+//        new Runner(options).run();
+//    }
+//}
+
+//@BenchmarkMode(Mode.AverageTime)
+//@OutputTimeUnit(TimeUnit.MILLISECONDS)
+//@Measurement(batchSize = 100000, iterations = 10)
+//@Warmup(batchSize = 100000, iterations = 10)
+//public class PerformanceBenchMark {
+//    @Benchmark
+//    public void benchmarkHashtable() {
+//        Hashtable<String, Integer> hashTable = new Hashtable<>();
+//        for (int i = 0; i < 10000; i++) {
+//            hashTable.put(String.valueOf(i), i);
+//        }
+//    }
+//    @Benchmark
+//    public void benchmarkConcurrentHashMap() {
+//        ConcurrentHashMap<String, Integer> concurrentHashMap = new ConcurrentHashMap<>();
+//        for (int i = 0; i < 10000; i++) {
+//            concurrentHashMap.put(String.valueOf(i), i);
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        Options options = new OptionsBuilder()
+//                .include(PerformanceBenchMark.class.getSimpleName()).threads(10)
+//                .forks(1).shouldFailOnError(true)
+//                .shouldDoGC(true)
+//                .jvmArgs("-server").build();
+//        new Runner(options).run();
+//    }
+//}
+
+
