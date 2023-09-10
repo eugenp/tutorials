0bd664369c29a6dac2710eee866b3033c1f075ab
danielmcnally285 <144589379+danielmcnally285@users.noreply.github.com>
2023-09-10 10:51:16 +0000

    Recipe class and shallow copy unit test

---
+++ core-java-modules/core-java-lang-oop-patterns/src/main/java/com/baeldung/shallowvsdeepcopy/Recipe.java
@@ -0,0 +1,40 @@
+package com.baeldung.shallowvsdeepcopy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recipe {
+	
+	private String name;
+	private List<String> ingredients;
+	
+	public Recipe(String name) {
+		this.name = name;
+		this.ingredients = new ArrayList<String>();
+	}
+	
+	public Recipe(String name, List<String> ingredients) {
+		this.name = name;
+		this.ingredients = ingredients;
+	}
+	
+	public static Recipe shallow(Recipe recipe) {
+		return new Recipe(recipe.name, recipe.ingredients);
+	}
+	
+	public static Recipe deep(Recipe recipe) {
+		return new Recipe(recipe.name, new ArrayList<String>(recipe.ingredients));
+	}
+	
+	public void addIngredient(String ingredient) {
+		this.ingredients.add(ingredient);
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public List<String> getIngredients() {
+		return ingredients;
+	}
+}
---
+++ core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/shallowvsdeepcopy/ShallowCopyUnitTest.java
@@ -0,0 +1,21 @@
+package com.baeldung.shallowvsdeepcopy;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ShallowCopyUnitTest {
+
+	@Test
+	public void whenOriginalModifiedAfterCopy_thenCopyAlsoModified() {
+		Recipe original = new Recipe("Smoothie");
+		original.addIngredient("Yoghurt");
+		original.addIngredient("Milk");
+		original.addIngredient("Banana");
+		Recipe copy = Recipe.shallowCopy(original);
+		copy.addIngredient("Strawberries");
+		original.addIngredient("Peanut Butter");
+		
+		assertTrue(copy.getIngredients().contains("Peanut Butter"));
+	}
+}
