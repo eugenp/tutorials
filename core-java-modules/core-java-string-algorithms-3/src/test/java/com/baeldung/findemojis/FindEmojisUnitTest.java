affbba39abf1a91683d244f842ae4d4a70760d48
Sam Gardner <samgardner909@gmail.com>
2023-09-13 15:39:17 +0100

    BAEL-6811 Check if letter is emoji example code

---
+++ core-java-modules/core-java-string-algorithms-3/src/test/java/com/baeldung/findemojis/FindEmojisUnitTest.java
@@ -0,0 +1,44 @@
+package com.baeldung.findemojis;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.vdurmont.emoji.EmojiManager;
+
+import emoji4j.EmojiUtils;
+
+public class FindEmojisUnitTest {
+
+    @Test
+    public void givenAWord_whenUsingEmoji4J_thenDetectEmoji() {
+        boolean emoji = EmojiUtils.isEmoji("\uD83D\uDC3B");
+        assertTrue(emoji);
+
+        boolean notEmoji = EmojiUtils.isEmoji("w");
+        assertFalse(notEmoji);
+    }
+
+    @Test
+    public void givenAWord_whenUsingEmojiJava_thenDetectEmoji() {
+        boolean emoji = EmojiManager.isEmoji("\uD83D\uDC3B");
+        assertTrue(emoji);
+
+        boolean notEmoji = EmojiManager.isEmoji("w");
+        assertFalse(notEmoji);
+    }
+
+    @Test
+    public void givenAWord_whenUsingRegex_thenDetectEmoji() {
+        String regexPattern = "[\uD800-\uDBFF\uDC00-\uDFFF]+";
+        String emojiString = "\uD83D\uDC3B";
+        boolean emoji = emojiString.matches(regexPattern);
+        assertTrue(emoji);
+
+        String notEmojiString = "w";
+        boolean notEmoji = notEmojiString.matches(regexPattern);
+        assertFalse(notEmoji);
+    }
+
+}
