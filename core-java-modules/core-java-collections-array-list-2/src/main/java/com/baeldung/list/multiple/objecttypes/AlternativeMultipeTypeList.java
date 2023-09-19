50515d3070194f66ad493a7b560b89f50bafb6ca
Niket Agrawal <niket.agrawal90@gmail.com>
2023-09-19 23:39:38 +0530

    Additional Approach Changes

---
+++ core-java-modules/core-java-collections-array-list-2/src/main/java/com/baeldung/list/multiple/objecttypes/AlternativeMultipeTypeList.java
@@ -0,0 +1,46 @@
+package com.baeldung.list.multiple.objecttypes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Predicate;
+
+public class AlternativeMultipeTypeList {
+
+    public static void main(String[] args) {
+        // List of Numbers
+        ArrayList<Number> myList = new ArrayList<>();
+        myList.add(1.2);
+        myList.add(2);
+        myList.add(-3.5);
+
+        // List of Map
+        ArrayList<Map> diffMapList = new ArrayList<>();
+        diffMapList.add(new HashMap<>());
+        diffMapList.add(new TreeMap<>());
+        diffMapList.add(new LinkedHashMap<>());
+
+        ArrayList<CustomObject> objList = new ArrayList<>();
+        objList.add(new CustomObject("obj1", 1));
+        objList.add(new CustomObject("obj2", 2));
+
+        List<Object> dataList = new ArrayList<>();
+
+        Predicate<Object> myPricate = inputData -> (inputData instanceof String || inputData instanceof Integer);
+
+        UserFunctionalInterface myInterface = (listObj, data) -> {
+            if (myPricate.test(data))
+                listObj.add(data);
+            return listObj;
+        };
+
+        myInterface.addToList(dataList, Integer.valueOf(2));
+        myInterface.addToList(dataList, Double.valueOf(3.33));
+        myInterface.addToList(dataList, "String Value");
+        myInterface.printList(dataList);
+    }
+
+}
