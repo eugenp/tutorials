db1d9552ec36c38c49ff3dc0215598ad76e1bcb8
rezaganjis <Ganji@tosan.com>
2023-09-19 18:01:46 +0330

    Rsocket in spring framework6.

---
+++ spring-6-rsocket/src/main/java/com/bealdung/rsocket/requester/MessageClient.java
@@ -0,0 +1,20 @@
+package com.bealdung.rsocket.requester;
+
+import org.springframework.messaging.rsocket.service.RSocketExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface MessageClient {
+
+    @RSocketExchange("MyMessagesDestination")
+    Mono<String> sendMessage(Mono<String> input);
+
+    @RSocketExchange("Counter")
+    Flux<String> Counter();
+
+    @RSocketExchange("Warning")
+    Mono<Void> Warning(Mono<String> warning);
+
+    @RSocketExchange("channel")
+    Flux<String> channel(Flux<String> input);
+}
---
+++ spring-6-rsocket/src/test/java/com/bealdung/rsocket/RSocketRequestResponseIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.bealdung.rsocket;
+
+import com.bealdung.rsocket.requester.MessageClient;
+import com.bealdung.rsocket.responder.RSocketApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.service.RSocketServiceProxyFactory;
+import reactor.core.publisher.Mono;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+    @SpringBootTest(classes = RSocketApplication.class)
+    public class RSocketRequestResponseIntegrationTest {
+
+    MessageClient client;
+
+    public RSocketRequestResponseIntegrationTest() {
+        RSocketRequester.Builder requesterBuilder = RSocketRequester.builder();
+        RSocketRequester requester = requesterBuilder.tcp("localhost", 7000);
+        RSocketServiceProxyFactory factory = RSocketServiceProxyFactory.builder(requester).build();
+        client = factory.createClient(MessageClient.class);
+    }
+
+    @Test
+    public void whenSendingStream_thenReceiveTheSameStream() {
+        String message = "test message";
+        assertEquals(message, client.sendMessage(Mono.just(message)).block());
+    }
+
+}
