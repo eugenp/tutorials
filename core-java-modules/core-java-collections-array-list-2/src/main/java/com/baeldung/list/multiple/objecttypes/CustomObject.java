b48fd86f30d5eac291ea02dcd094d7eb568123f0
Niket Agrawal <niket.agrawal90@gmail.com>
2023-09-19 23:39:38 +0530

    Additional Approach Changes

--- core-java-modules/core-java-collections-array-list-2/src/main/java/com/baeldung/list/multiple/objecttypes/CustomObject.java
+++ core-java-modules/core-java-collections-array-list-2/src/main/java/com/baeldung/list/multiple/objecttypes/CustomObject.java
@@ -2,12 +2,22 @@ package com.baeldung.list.multiple.objecttypes;
 
 public class CustomObject {
     String classData;
+    Integer intData;
 
     CustomObject(String classData) {
         this.classData = classData;
     }
 
+    CustomObject(String classData, Integer intData) {
+        this.classData = classData;
+        this.intData = intData;
+    }
+
     public String getClassData() {
         return this.classData;
+    }
+
+    public Integer getIntData() {
+        return this.intData;
     }
 }
