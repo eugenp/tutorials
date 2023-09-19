da0beaeee0af95bec423a6979169e523ead13e4a
Niket Agrawal <niket.agrawal90@gmail.com>
2023-09-19 21:25:59 +0530

    Restructuring to new module
    Restructuring to new module

--- core-java-modules/core-java-collections-array-list-2/src/main/java/com/baeldung/list/multiple/objecttypes/MultipleObjectTypeArrayList.java
+++ core-java-modules/core-java-collections-array-list-2/src/main/java/com/baeldung/list/multiple/objecttypes/MultipleObjectTypeArrayList.java
@@ -1,5 +1,7 @@
 package com.baeldung.list.multiple.objecttypes;
 
+import java.math.BigInteger;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,27 +9,32 @@ import java.util.List;
 public class MultipleObjectTypeArrayList {
 
     public static void main(String[] args) {
-        
+
         ArrayList<Object> multiTypeList = new ArrayList<>();
-        
+
         multiTypeList.add(Integer.valueOf(10));
         multiTypeList.add(Double.valueOf(11.5));
         multiTypeList.add("String Data");
         multiTypeList.add(Arrays.asList(1, 2, 3));
         multiTypeList.add(new CustomObject("Class Data"));
+        multiTypeList.add(BigInteger.valueOf(123456789));
+        multiTypeList.add(LocalDate.of(2023, 9, 19));
 
         for (Object dataObj : multiTypeList) {
             if (dataObj instanceof Integer intData)
                 System.out.println("Integer Data : " + intData);
-            if (dataObj instanceof Double doubleData)
+            else if (dataObj instanceof Double doubleData)
                 System.out.println("Double Data : " + doubleData);
-            if (dataObj instanceof String stringData)
+            else if (dataObj instanceof String stringData)
                 System.out.println("String Data : " + stringData);
-            if (dataObj instanceof List<?> intList)
+            else if (dataObj instanceof List<?> intList)
                 System.out.println("List Data : " + intList);
-            if (dataObj instanceof CustomObject customObj)
+            else if (dataObj instanceof CustomObject customObj)
                 System.out.println("CustomObject Data : " + customObj.getClassData());
+            else if (dataObj instanceof BigInteger bigIntData)
+                System.out.println("BigInteger Data : " + bigIntData);
+            else if (dataObj instanceof LocalDate localDate)
+                System.out.println("LocalDate Data : " + localDate.toString());
         }
     }
-
 }
