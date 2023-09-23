6ad085c95b02a2c506fd65725825248f31fda1ff
Niket Agrawal <niket.agrawal90@gmail.com>
2023-09-24 01:20:56 +0530

    Review comments

--- spring-boot-modules/spring-boot-validations/src/main/java/com/baeldung/deserializer/BooleanDeserializer.java
+++ spring-boot-modules/spring-boot-validations/src/main/java/com/baeldung/deserializer/BooleanDeserializer.java
@@ -8,11 +8,14 @@ import com.fasterxml.jackson.databind.JsonDeserializer;
 
 public class BooleanDeserializer extends JsonDeserializer<Boolean> {
     @Override
-    public Boolean deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
-        String value = p.getText();
-        if (value != null && !value.equals("TRUE") && !value.equals("FALSE")) {
-            throw new IllegalArgumentException("Boolean value must come as Capital TRUE or FALSE");
+    public Boolean deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        String value = parser.getText();
+        if (value != null && (value.equals("TRUE") || value.equals("1") || value.equals("+"))) {
+            return Boolean.TRUE;
+        } else if (value != null && (value.equals("FALSE") || value.equals("0") || value.equals("-"))) {
+            return Boolean.FALSE;
+        } else {
+            throw new IllegalArgumentException("Only values accepted as Boolean are TRUE, FALSE, +, -, 1 , 0");
         }
-        return Boolean.valueOf(value);
     }
 }
