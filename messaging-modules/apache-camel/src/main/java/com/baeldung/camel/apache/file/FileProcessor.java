8a2fd4b4baee0c3900fa80105f334888ff9d65bc
hmdrzsharifi <hmdrzsharifi@gmail.com>
2023-09-26 09:10:13 +0330

    #BAEL-6916: change timestamp to date for test assertion

--- messaging-modules/apache-camel/src/main/java/com/baeldung/camel/apache/file/FileProcessor.java
+++ messaging-modules/apache-camel/src/main/java/com/baeldung/camel/apache/file/FileProcessor.java
@@ -14,7 +14,7 @@ public class FileProcessor implements Processor {
         String originalFileName = (String) exchange.getIn().getHeader(Exchange.FILE_NAME, String.class);
 
         Date date = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         String changedFileName = dateFormat.format(date) + originalFileName;
         exchange.getIn().setHeader(Exchange.FILE_NAME, changedFileName);
     }
