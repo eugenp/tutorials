418d7c3043cdd80683c7f061719d7940dc479dd1
Nadia <62909670+nadiahylary@users.noreply.github.com>
2023-09-26 15:05:11 +0100

    Update CharStreams.java

--- core-java-modules/core-java-io-5/src/main/java/com/baeldung/charstreams/CharStreams.java
+++ core-java-modules/core-java-io-5/src/main/java/com/baeldung/charstreams/CharStreams.java
@@ -3,48 +3,61 @@ package com.baeldung.charstreams;
 import java.io.*;
 public class CharStreams {
 
-    public static void writingStringToFilePrintWriter(String fileName, String content) throws FileNotFoundException{
+    public static File writingStringToFilePrintWriter(String fileName, String content) throws FileNotFoundException{
+        File fileObj = new File(fileName);
         PrintWriter printWriter = new PrintWriter(fileName);
         printWriter.print(content);
         printWriter.flush();
         printWriter.close();
+        return fileObj;
     }
 
-    public static void writingStringToFileFileWriter(String fileName, String content) throws IOException {
-        FileWriter fileWriter = new FileWriter(fileName);
+    public static File writingStringToFileFileWriter(String fileName, String content) throws IOException {
+        File fileObj = new File(fileName);
+        FileWriter fileWriter = new FileWriter(fileObj);
         fileWriter.write(content);
         fileWriter.flush();
         fileWriter.close();
+        return fileObj;
 
     }
 
-    public static void writingNonStringToFilePrintWriter(String fileName, DailyTodo dailyTodo) throws FileNotFoundException, UnsupportedEncodingException {
-        PrintWriter printWriter = new PrintWriter(fileName, "utf-8");
+    public static File writingNonStringToFilePrintWriter(String fileName, DailyTodo dailyTodo) throws FileNotFoundException, UnsupportedEncodingException {
+        File fileObj = new File(fileName);
+        PrintWriter printWriter = new PrintWriter(fileObj, "utf-8");
         printWriter.print(dailyTodo);
         printWriter.flush();
         printWriter.close();
+        return fileObj;
+
     }
 
-    public static void writingNonStringToFileFileWriter(String fileName, DailyTodo dailyTodo) throws IOException {
-        FileWriter fileWriter = new FileWriter(fileName);
+    public static File writingNonStringToFileFileWriter(String fileName, DailyTodo dailyTodo) throws IOException {
+        File fileObj = new File(fileName);
+        FileWriter fileWriter = new FileWriter(fileObj);
         fileWriter.write(dailyTodo.toString());
         fileWriter.flush();
         fileWriter.close();
+        return fileObj;
     }
 
-    public static void appendingToFilePrintWriter(String fileName, DailyTodo content) throws FileNotFoundException, UnsupportedEncodingException {
+    public static File appendingToFilePrintWriter(String fileName, DailyTodo content) throws FileNotFoundException, UnsupportedEncodingException {
         File fileObj = new File(fileName);
-        PrintWriter printWriter = new PrintWriter(fileName, "utf-8");
+        PrintWriter printWriter = new PrintWriter(fileObj, "utf-8");
         printWriter.append(content.toString());
         printWriter.flush();
         printWriter.close();
+        return fileObj;
     }
 
-    public static void appendingToFileFileWriter(String fileName, DailyTodo content) throws IOException {
-        FileWriter fileWriter = new FileWriter(fileName, true);
+    public static File appendingToFileFileWriter(String fileName, DailyTodo content) throws IOException {
+        File fileObj = new File(fileName);
+        FileWriter fileWriter = new FileWriter(fileObj, true);
         fileWriter.write(content.toString());
         fileWriter.flush();
         fileWriter.close();
+        return fileObj;
+
     }
 
     public static void autoFlushPrintWriter(String fileName, DailyTodo content) throws FileNotFoundException {
