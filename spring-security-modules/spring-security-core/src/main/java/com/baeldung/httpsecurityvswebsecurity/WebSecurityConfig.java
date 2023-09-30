92ee41eb65f1a1b1b0d702b5b412fbb30b818da8
dlazydev <146261002+dlazydev@users.noreply.github.com>
2023-09-30 09:09:36 +0100

    Update WebSecurityConfig.java

--- spring-security-modules/spring-security-core/src/main/java/com/baeldung/httpsecurityvswebsecurity/WebSecurityConfig.java
+++ spring-security-modules/spring-security-core/src/main/java/com/baeldung/httpsecurityvswebsecurity/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.baeldung.httpsecurityvswebsecurity;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
