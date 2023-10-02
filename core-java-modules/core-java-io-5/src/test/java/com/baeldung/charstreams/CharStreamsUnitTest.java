97012f832c829977d262e97d6c5772e71f86c2f4
Nadia <62909670+nadiahylary@users.noreply.github.com>
2023-10-02 23:50:03 +0100

    Update CharStreamsUnitTest.java

--- core-java-modules/core-java-io-5/src/test/java/com/baeldung/charstreams/CharStreamsUnitTest.java
+++ core-java-modules/core-java-io-5/src/test/java/com/baeldung/charstreams/CharStreamsUnitTest.java
@@ -67,6 +67,8 @@ public class CharStreamsUnitTest {
 
     @Test
     public void whenWritingToExistingFile_thenPrintWriterAppendsToFile() throws IOException {
+        String existingText = "existing text";
+        Files.write(Paths.get(TODO_LIST_PW_FILE), existingText.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
         try {
             CharStreams.appendingToFilePrintWriter(TODO_LIST_PW_FILE, TODO);
             assertThat(new File(TODO_LIST_PW_FILE)).hasContent(TODO.toString());
@@ -77,6 +79,8 @@ public class CharStreamsUnitTest {
 
     @Test
     public void whenWritingToExistingFile_thenFileWriterAppendsToFile() throws IOException {
+        String existingText = "some text";
+        Files.write(Paths.get(TODO_LIST_FW_FILE), existingText.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
         try {
             CharStreams.appendingToFileFileWriter(TODO_LIST_FW_FILE, TODO);
             assertThat(new File(TODO_LIST_FW_FILE)).hasContent(TODO.toString());
