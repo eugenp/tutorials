40c5ed0c6f6ff9e8f0588a6b31e2e9239a0b612c
anuragkumawat <anuragkumawat7@gmail.com>
2023-10-02 23:07:36 +0530

    JAVA-24378 Fix references to parents - Week 35 - 2023 (#14852)

--- spring-boot-modules/spring-boot-ssl-bundles/src/test/java/com/baeldung/springbootsslbundles/SpringContextTest.java
+++ spring-boot-modules/spring-boot-ssl-bundles/src/test/java/com/baeldung/springbootsslbundles/SpringContextTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SSLBundleApplicationTests {
+class SpringContextTest {
     @Test
     void contextLoads() {
 
