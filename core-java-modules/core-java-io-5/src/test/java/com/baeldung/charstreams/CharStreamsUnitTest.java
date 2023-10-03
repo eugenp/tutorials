0d61f426fed072ac9aa0b500618aacda57bc4c6a
Nadia <62909670+nadiahylary@users.noreply.github.com>
2023-10-03 09:38:46 +0100

    Update CharStreamsUnitTest.java

--- core-java-modules/core-java-io-5/src/test/java/com/baeldung/charstreams/CharStreamsUnitTest.java
+++ core-java-modules/core-java-io-5/src/test/java/com/baeldung/charstreams/CharStreamsUnitTest.java
@@ -21,40 +21,45 @@ public class CharStreamsUnitTest {
     @BeforeEach
     public void createFiles() throws IOException {
         Files.createDirectories(Paths.get(TODO_LIST_PW_FILE).getParent());
+        Files.deleteIfExists(Paths.get(TODO_LIST_PW_FILE));
         Files.createDirectories(Paths.get(TODO_LIST_FW_FILE).getParent());
+        Files.deleteIfExists(Paths.get(TODO_LIST_FW_FILE));
     }
 
     @Test
     public void whenUsingTextData_thenPrintWriterWritesToFile() throws IOException {
-        PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), );
-        printWriter.print(TODO.toString());
-        printWriter.close();
-        assertThat(new File(TODO_LIST_PW_FILE)).hasContent(TODO.toString());
+        try (PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), StandardCharsets.UTF_8)) {
+            printWriter.print(TODO.toString());
+            printWriter.close();
+            assertThat(new File(TODO_LIST_PW_FILE)).hasContent(TODO.toString());
+        }
     }
 
     @Test
     public void whenUsingTextData_thenFileWriterWritesToFile() throws IOException {
-        FileWriter fileWriter = new FileWriter(new File(TODO_LIST_FW_FILE));
-        fileWriter.write(TODO);
-        fileWriter.close();
-        assertThat(new File(TODO_LIST_FW_FILE)).hasContent(TODO.toString());
-        
+        try (FileWriter fileWriter = new FileWriter(new File(TODO_LIST_FW_FILE))) {
+            fileWriter.write(TODO.toString());
+            fileWriter.close();
+            assertThat(new File(TODO_LIST_FW_FILE)).hasContent(TODO.toString());
+        }
     }
 
     @Test
     public void whenUsingFormattedData_thenPrintWriterWritesToFile() throws IOException {
-        PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), StandardCharsets.UTF_8);
-        printWriter.print(TODO);
-        printWriter.close();
-       assertThat(new File(TODO_LIST_PW_FILE)).hasContent(TODO.toString());
+        try (PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), StandardCharsets.UTF_8)) {
+            printWriter.print(TODO);
+            printWriter.close();
+            assertThat(new File(TODO_LIST_PW_FILE)).hasContent(TODO.toString());
+        }
     }
 
     @Test
     public void whenUsingFormattedData_thenFileWriterWritesToFile() throws IOException {
-        FileWriter fileWriter = new FileWriter(new File(TODO_LIST_FW_FILE));
-        fileWriter.write(TODO.toString());
-        fileWriter.close();
-        assertThat(new File(TODO_LIST_FW_FILE)).hasContent(TODO.toString());
+        try (FileWriter fileWriter = new FileWriter(new File(TODO_LIST_FW_FILE))){
+            fileWriter.write(TODO.toString());
+            fileWriter.close();
+            assertThat(new File(TODO_LIST_FW_FILE)).hasContent(TODO.toString());
+        }
     }
 
     @Test
@@ -62,10 +67,9 @@ public class CharStreamsUnitTest {
         String existingText = "existing text";
         Files.write(Paths.get(TODO_LIST_PW_FILE), existingText.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
 
-        try (PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), StandardCharsets.UTF_8)) {
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(TODO_LIST_PW_FILE, true), false, StandardCharsets.UTF_8)) {
             printWriter.append(TODO.toString());
         }
-
         assertThat(new File(TODO_LIST_PW_FILE)).hasContent(existingText + TODO.toString());
     }
 
@@ -77,23 +81,23 @@ public class CharStreamsUnitTest {
         try (FileWriter fileWriter = new FileWriter(new File(TODO_LIST_FW_FILE), StandardCharsets.UTF_8, true)) {
             fileWriter.append(TODO.toString());
         }
-
         assertThat(new File(TODO_LIST_FW_FILE)).hasContent(existingText + TODO.toString());
     }
 
     @Test
     public void whenCheckingError_thenPrintWriterReturnsFalse() throws IOException {
-        PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), StandardCharsets.UTF_8);
-        printWriter.print(TODO);
-        boolean result = printWriter.checkError();
-        printWriter.close();
-        assertFalse(result);
+        try (PrintWriter printWriter = new PrintWriter(new File(TODO_LIST_PW_FILE), StandardCharsets.UTF_8)) {
+            printWriter.print(TODO);
+            boolean result = printWriter.checkError();
+            printWriter.close();
+            assertFalse(result);
+        }
     }
 
     @Test
     public void whenHandlingError_thenFileWriterHandlesError() {
         try (FileWriter fileWriter = new FileWriter(TODO_LIST_FW_FILE, true)) {
-        fileWriter.write(TODO.toString());
+            fileWriter.write(TODO.toString());
         } catch (IOException e) {
             fail("error not expected", e);
         }
@@ -102,16 +106,9 @@ public class CharStreamsUnitTest {
 
     @Test
     public static void whenAutoFlushEqualTrue_thenPrintWriterFlushesAutomatically() throws FileNotFoundException {
-        PrintWriter printWriter = new PrintWriter(new FileOutputStream(TODO_LIST_PW_FILE), true);
-        printWriter.println(TODO);
-        printWriter.close();
-        assertTrue(printWriter == null);
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(TODO_LIST_PW_FILE), true)) {
+            printWriter.println(TODO);
+            assertThat(new File(TODO_LIST_PW_FILE)).hasContent(TODO.toString() + System.lineSeparator());
+        }
     }
-    
-    @AfterEach
-    public void clean() throws IOException {
-        Files.deleteIfExists(Paths.get(TODO_LIST_PW_FILE));
-        Files.deleteIfExists(Paths.get(TODO_LIST_FW_FILE));
-    }
-
 }
