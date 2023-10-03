067ab3f6c2a931805f19d9c39d0b724f0888be74
timis1 <noreplay@yahoo.com>
2023-10-03 10:46:18 +0300

    JAVA-25517 Upgrade spring-boot-bootstrap to Spring Boot 3

--- spring-boot-modules/spring-boot-bootstrap/src/main/java/com/baeldung/config/SecurityConfig.java
+++ spring-boot-modules/spring-boot-bootstrap/src/main/java/com/baeldung/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,12 +13,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-            .anyRequest()
-            .permitAll()
-            .and()
-            .csrf()
-            .disable();
+        http.authorizeHttpRequests(expressionInterceptUrlRegistry ->
+                        expressionInterceptUrlRegistry
+                                .anyRequest()
+                                .permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
