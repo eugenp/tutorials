e007350ba63e60fa9836095cfb09db03bdb579e8
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-10-04 14:54:44 +0100

    Update BigDecimalConversionUnitTest.java

--- core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/BigDecimalConversionUnitTest.java
+++ core-java-modules/core-java-numbers-6/src/test/java/com/baeldung/javadoublevsbigdecimal/BigDecimalConversionUnitTest.java
@@ -19,7 +19,7 @@ public class BigDecimalConversionUnitTest {
     }
 
     @Test
-    void givenDecimalPlacesGreaterThan15_whenConvertingBigDecimalToDouble_thenPrecisionisLost() {
+    void givenDecimalPlacesGreaterThan15_whenConvertingBigDecimalToDouble_thenPrecisionIsLost() {
         BigDecimal bigDecimalValue = new BigDecimal("789.1234567890123456");
         double doubleValue = bigDecimalValue.doubleValue();
         BigDecimal convertedBackToBigDecimal = BigDecimal.valueOf(doubleValue);
