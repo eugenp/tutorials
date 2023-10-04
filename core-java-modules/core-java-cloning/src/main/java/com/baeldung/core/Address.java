85ccacc1c2bc537e38d14cd8cacffeebd6398262
anujgaud <146576725+anujgaud@users.noreply.github.com>
2023-10-04 22:59:18 +0530

    Added Address class

---
+++ core-java-modules/core-java-cloning/src/main/java/com/baeldung/core/Address.java
@@ -0,0 +1,22 @@
+package core;
+
+class Address implements Cloneable{
+    private String city;
+
+    public Address(String city) {
+        this.city = city;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
