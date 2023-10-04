de32cc1f087536b1aa7ee0e9719f2593dc556889
anujgaud <146576725+anujgaud@users.noreply.github.com>
2023-10-04 22:58:07 +0530

    Modified Person class

--- core-java-modules/core-java-cloning/src/main/java/com/baeldung/core/Person.java
+++ core-java-modules/core-java-cloning/src/main/java/com/baeldung/core/Person.java
@@ -1,16 +1,44 @@
 package core;
 
-class Person implements Cloneable {
-    String name;
-    int age;
+import core.Address;
 
-    Person(String name, int age) {
+public class Person implements Cloneable {
+    String name;
+    Address address;
+
+    Person(String name, Address address) {
         this.name = name;
-        this.age = age;
+        this.address = address;
     }
+
     @Override
     public Object clone() throws CloneNotSupportedException {
+        // Shallow copy
         Person clonedPerson = (Person) super.clone();
+        // Since Address also implements Cloneable, we can clone it as well
+        clonedPerson.address = (Address) address.clone();
         return clonedPerson;
+    }
+
+    // Shallow copy implementation
+    public Person shallowCopy() {
+        return new Person(this.name, this.address);
+    }
+
+    // Standard Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
     }
 }
