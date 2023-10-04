721b9261a87ee4a992e924b3d08af05a02ed1ae7
panos-kakos <kakos.programmer@gmail.com>
2023-10-04 17:06:28 +0300

    [JAVA-25516] Fixed issue related to actuator security

--- spring-boot-modules/spring-boot-actuator/src/main/java/com/baeldung/endpoints/enabling/SecurityConfiguration.java
+++ spring-boot-modules/spring-boot-actuator/src/main/java/com/baeldung/endpoints/enabling/SecurityConfiguration.java
@@ -1,41 +1,52 @@
 package com.baeldung.endpoints.enabling;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-@Configuration
 @EnableWebSecurity
+@Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.securityMatcher(EndpointRequest.toAnyEndpoint());
         http.authorizeHttpRequests(authz -> {
-            authz.requestMatchers("/actuator/**")
-                .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated();
-        });
+                authz.requestMatchers(mvc.pattern("/actuator/**"))
+                    .hasRole("ADMIN")
+                    .anyRequest()
+                    .authenticated();
+            });
 
         return http.build();
     }
 
+
+
     @Bean
-    public UserDetailsService users() {
-        // The builder will ensure the passwords are encoded before saving in memory
-        User.UserBuilder users = User.withDefaultPasswordEncoder();
-        UserDetails user = users
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user =  User.withDefaultPasswordEncoder()
             .username("user")
             .password("password")
             .roles("USER")
             .build();
-        UserDetails admin = users
+        UserDetails admin =  User.withDefaultPasswordEncoder()
             .username("admin")
             .password("password")
             .roles("USER", "ADMIN")
