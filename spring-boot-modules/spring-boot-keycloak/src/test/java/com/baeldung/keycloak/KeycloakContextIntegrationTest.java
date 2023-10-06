27e66291a33de4f78cf54f424b65109b1d36dc00
gaepi <gae.piaz@gmail.com>
2023-10-06 10:50:47 +0200

    JAVA-25157 | minor fixes

--- spring-boot-modules/spring-boot-keycloak/src/test/java/com/baeldung/keycloak/KeycloakContextIntegrationTest.java
+++ spring-boot-modules/spring-boot-keycloak/src/test/java/com/baeldung/keycloak/KeycloakContextIntegrationTest.java
@@ -4,10 +4,9 @@ import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import com.baeldung.keycloak.SpringBoot;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = { SpringBoot.class })
+@SpringBootTest(classes = { SpringBootKeycloakApp.class })
 public class KeycloakContextIntegrationTest {
 
     @Test
