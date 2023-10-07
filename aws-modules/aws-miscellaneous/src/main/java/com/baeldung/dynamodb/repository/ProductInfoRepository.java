35be82141f5a61cea29a3dfe9cf0b541fa173065
panos-kakos <kakos.programmer@gmail.com>
2023-10-07 18:58:23 +0300

    [JAVA-23452] Clean up

--- aws-modules/aws-miscellaneous/src/main/java/com/baeldung/dynamodb/repository/ProductInfoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.baeldung.dynamodb.repository;
-
-import com.baeldung.dynamodb.entity.ProductInfo;
-
-public class ProductInfoRepository extends AbstractRepository<ProductInfo, String> {
-}
