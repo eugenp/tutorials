7fbdad9c5ba2a0c1733cad7dc7b595ce5b8b5465
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2023-10-14 13:06:31 +0200

    BAEL-7001: retry RetryCompletableFuture

---
+++ core-java-modules/core-java-concurrency-basic-3/src/test/java/com/baeldung/concurrent/completablefuture/retry/RetryCompletableFutureUnitTest.java
@@ -0,0 +1,125 @@
+package com.baeldung.concurrent.completablefuture.retry;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+import static com.baeldung.concurrent.completablefuture.retry.RetryCompletableFuture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class RetryCompletableFutureUnitTest {
+	private AtomicInteger retriesCounter = new AtomicInteger(0);
+
+	@BeforeEach
+	void beforeEach() {
+		retriesCounter.set(0);
+	}
+
+	@Test
+	void whenRetryingTask_thenReturnsCorrectlyAfterFourInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryTask(codeToRun, 10);
+
+		assertThat(result.join())
+		  .isEqualTo(100);
+		assertThat(retriesCounter)
+		  .hasValue(4);
+	}
+
+	@Test
+	void whenRetryingTask_thenThrowsExceptionAfterThreeInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryTask(codeToRun, 3);
+
+		assertThatThrownBy(result::join)
+		  .isInstanceOf(CompletionException.class)
+		  .hasMessageContaining("IllegalStateException: Task failed after 3 attempts");
+	}
+
+	@Test
+	void whenRetryingExceptionally_thenReturnsCorrectlyAfterFourInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryUnsafe(codeToRun, 10);
+
+		assertThat(result.join())
+		  .isEqualTo(100);
+		assertThat(retriesCounter)
+		  .hasValue(4);
+	}
+
+	@Test
+	void whenRetryingExceptionally_thenThrowsExceptionAfterThreeInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryUnsafe(codeToRun, 3);
+
+		assertThatThrownBy(result::join)
+		  .isInstanceOf(CompletionException.class)
+		  .hasMessageContaining("RuntimeException: task failed for 3 time(s)");
+	}
+
+	@Test
+	void whenRetryingExceptionallyAsync_thenReturnsCorrectlyAfterFourInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryExceptionallyAsync(codeToRun, 10);
+
+		assertThat(result.join())
+		  .isEqualTo(100);
+		assertThat(retriesCounter)
+		  .hasValue(4);
+	}
+
+	@Test
+	void whenRetryingExceptionallyAsync_thenThrowsExceptionAfterThreeInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryExceptionallyAsync(codeToRun, 3);
+
+		assertThatThrownBy(result::join)
+		  .isInstanceOf(CompletionException.class)
+		  .hasMessageContaining("RuntimeException: task failed for 3 time(s)");
+	}
+
+	@Test
+	void whenRetryingNesting_thenReturnsCorrectlyAfterFourInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryNesting(codeToRun, 10);
+
+		assertThat(result.join())
+		  .isEqualTo(100);
+		assertThat(retriesCounter)
+		  .hasValue(4);
+	}
+
+	@Test
+	void whenRetryingNesting_thenThrowsExceptionAfterThreeInvocations() {
+		Supplier<Integer> codeToRun = () -> failFourTimesThenReturn(100);
+
+		CompletableFuture<Integer> result = retryNesting(codeToRun, 3);
+
+		assertThatThrownBy(result::join)
+		  .isInstanceOf(CompletionException.class)
+		  .hasMessageContaining("RuntimeException: task failed for 3 time(s)");
+	}
+
+	int failFourTimesThenReturn(int returnValue) {
+		int retryNr = retriesCounter.get();
+		System.out.println(String.format("invocation: %s, thread: %s", retryNr, Thread.currentThread().getName()));
+		if (retryNr < 4) {
+			retriesCounter.set(retryNr + 1);
+			throw new RuntimeException(String.format("task failed for %s time(s)", retryNr));
+		}
+		return returnValue;
+	}
+
+}
