d850d6c30cbad40b5245b58f06ac0173ecfb3546
Amol Gote <aamolgote@hotmail.com>
2023-10-15 16:08:27 -0400

    Update apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/ProducerConfigurations.java
    Co-authored-by: Liam Williams <liam.williams@zoho.com>

--- apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/ProducerConfigurations.java
+++ apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/ProducerConfigurations.java
@@ -10,7 +10,7 @@ public class ProducerConfigurations {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1");
         props.put(ProducerConfig.BATCH_SIZE_CONFIG, "16384");
