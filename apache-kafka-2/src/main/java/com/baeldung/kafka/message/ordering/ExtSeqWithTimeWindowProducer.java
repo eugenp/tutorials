9eed20e483f00dde53393c3d0ae541a107a7696c
Amol Gote <aamolgote@hotmail.com>
2023-10-15 16:03:11 -0400

    Update apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/ExtSeqWithTimeWindowProducer.java
    Co-authored-by: Liam Williams <liam.williams@zoho.com>

--- apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/ExtSeqWithTimeWindowProducer.java
+++ apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/ExtSeqWithTimeWindowProducer.java
@@ -13,7 +13,7 @@ public class ExtSeqWithTimeWindowProducer {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "com.baeldung.kafka.message.ordering.serialization.JacksonSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "com.baeldung.kafka.message.ordering.serialization.JacksonSerializer");
 
         KafkaProducer<String, Message> producer = new KafkaProducer<>(props);
         for (long insertPosition = 1; insertPosition <= 10 ; insertPosition++) {
