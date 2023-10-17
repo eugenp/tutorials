6b286fcac5aaafbf0f997a6e016345f65980ac16
MohamedHelmyKassab <137485958+MohamedHelmyKassab@users.noreply.github.com>
2023-10-17 08:22:47 +0300

    This commit is related to the article BAEL-7023
    This commit aims to modify the class "CenteringTextUnitTest" to meet the review process requests.

--- core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/centertext/CenteringTextUnitTest.java
+++ core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/centertext/CenteringTextUnitTest.java
@@ -3,6 +3,7 @@ package com.baeldung.centertext;
 import liquibase.repackaged.org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class CenteringTextUnitTest {
@@ -10,17 +11,16 @@ public class CenteringTextUnitTest {
     @Test
     public void givenTextAndTotalWidth_whenUsingStringFormat_thenTextIsCentered() {
         String text = "Centered Text";
-        int totalWidth = 20;
+        int totalWidth = 15;
         int padding = (totalWidth - text.length()) / 2;
         String centeredText = String.format("%" + padding + "s%s%" + padding + "s", "", text, "");
-        String expectedCenteredText = "   Centered Text   ";
-        Assert.assertEquals("Centered Text", expectedCenteredText, centeredText);
+        Assert.assertEquals(" Centered Text ", centeredText);
     }
 
     @Test
     public void givenTextAndTotalWidth_whenCenterUsingStringBuilder_thenTextIsCentered() {
         String text = "Centered Text";
-        int width = 20;
+        int width = 15;
         int padding = (width - text.length()) / 2;
         StringBuilder centeredText = new StringBuilder();
         for (int i = 0; i < padding; i++) {
@@ -31,17 +31,15 @@ public class CenteringTextUnitTest {
             centeredText.append(" ");
         }
         String centeredTextString = centeredText.toString();
-        String expectedCenteredText = "   Centered Text   ";
-        Assert.assertEquals("Centered Text", expectedCenteredText, centeredTextString);
+        Assert.assertEquals(" Centered Text ", centeredTextString);
     }
 
     @Test
     public void givenTextAndTotalWidth_whenUsingStringUtilsCenterMethod_thenTextIsCentered() {
         String text = "Centered Text";
-        int width = 20;
+        int width = 15;
         String centeredText = StringUtils.center(text, width);
-        String expectedCenteredText = StringUtils.center("Centered Text", width);
-        assertEquals("Centered Text", expectedCenteredText, centeredText);
+        assertEquals(" Centered Text ", centeredText);
     }
 
 }
