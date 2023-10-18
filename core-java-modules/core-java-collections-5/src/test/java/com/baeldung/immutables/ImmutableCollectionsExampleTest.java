c1fc1d894595493e9925e453c04067b951eee94b
Muhammad Asif <muasif80@gmail.com>
2023-10-19 01:31:44 +0500

    Refactor code for BAEL-7030

--- core-java-modules/core-java-collections-5/src/test/java/com/baeldung/immutables/ImmutableCollectionsExampleTest.java
+++ core-java-modules/core-java-collections-5/src/test/java/com/baeldung/immutables/ImmutableCollectionsExampleTest.java
@@ -1,83 +1,55 @@
 package com.baeldung.immutables;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 public class ImmutableCollectionsExampleTest {
 
-	@Test
-    public void givenUnmodifiableCollection_whenAddNewItem_thenThrowUnsupportedOperationException() {
-		List<String> list = new ArrayList<>();
-		list.add("One");
-		list.add("Two");
-		list.add("Three");
-		
-		ImmutableCollectionsExample ice = new ImmutableCollectionsExample();
-        List<String> unmodifiableList = ice.createUnmodifiableList(list);
+    @Test
+    public void givenUnmodifiableMap_whenPutNewEntry_thenThrowsUnsupportedOperationException() {
+        Map<String, String> modifiableMap = new HashMap<>();
+        modifiableMap.put("name1", "Michael");
+        modifiableMap.put("name2", "Harry");
         
-        try {
-        	unmodifiableList.add("Four");
-        }catch(Exception e) {
-        	assertEquals(e.getClass().getName(), UnsupportedOperationException.class.getName());
-        }
+        Map<String, String> unmodifiableMap = Collections.unmodifiableMap(modifiableMap);
+
+        assertThrows(UnsupportedOperationException.class, () -> unmodifiableMap.put("name3", "Micky"));
     }
-	
-	@Test
-    public void givenUnmodifiableCollection_whenAddNewItemUsingOrigianReference_thenAddSuccessful() {
-		List<String> list = new ArrayList<>();
-		list.add("One");
-		list.add("Two");
-		list.add("Three");
-		
-		ImmutableCollectionsExample ice = new ImmutableCollectionsExample();
-        List<String> unmodifiableList = ice.createUnmodifiableList(list);
+
+    @Test
+    public void givenUnmodifiableMap_whenPutNewEntryUsingOriginalReference_thenSuccessful() {
+        Map<String, String> modifiableMap = new HashMap<>();
+        modifiableMap.put("name1", "Michael");
+        modifiableMap.put("name2", "Harry");
         
-        try {
-        	unmodifiableList.add("Four");
-        }catch(Exception e) {
-        	assertEquals(e.getClass().getName(), UnsupportedOperationException.class.getName());
-        }
+        Map<String, String> unmodifiableMap = Collections.unmodifiableMap(modifiableMap);
+        modifiableMap.put("name3", "Micky");
         
-        list.add("Four");
-        assertEquals(unmodifiableList, list);
-        Assert.assertTrue(unmodifiableList.contains("Four"));
+        assertEquals(modifiableMap, unmodifiableMap);
+        Assert.assertTrue(unmodifiableMap.containsKey("name3"));
     }
-	
-	@Test
-    public void givenImmutableCollection_whenAddNewItem_thenThrowUnsupportedOperationException() {
-		ImmutableCollectionsExample ice = new ImmutableCollectionsExample();
-        List<String> immutableList = ice.createImmuteableList();
-        
-        try {
-        	immutableList.add("Four");
-        }catch(Exception e) {
-        	assertEquals(e.getClass().getName(), UnsupportedOperationException.class.getName());
-        }
+
+    @Test
+    public void givenImmutableMap_whenPutNewEntry_thenThrowsUnsupportedOperationException() {
+        Map<String, String> immutableMap = Map.of("name1", "Michael", "name2", "Harry");
+
+        assertThrows(UnsupportedOperationException.class, () -> immutableMap.put("name3", "Micky"));
     }
-	
-	@Test
-    public void givenUnmodifiableCollection_whenAddNewItemUsingOrigianlReference_thenThrowUnsupportedOperationException() {
-		List<String> list = new ArrayList<>();
-		list.add("One");
-		list.add("Two");
-		list.add("Three");
-		
-		ImmutableCollectionsExample ice = new ImmutableCollectionsExample();
-        List<String> immutableList = ice.createImmuteableList(list);
-        
-        try {
-        	immutableList.add("Four");
-        }catch(Exception e) {
-        	assertEquals(e.getClass().getName(), UnsupportedOperationException.class.getName());
-        }
-        
-        list.add("Four");
-        Assert.assertNotEquals(immutableList, list);
-        Assert.assertFalse(immutableList.contains("Four"));
+
+    @Test
+    public void givenImmutableMap_whenUseCopyOf_thenPutOnCopyAlsoThrowsUnsupportedOperationException() {
+        Map<String, String> immutableMap = Map.of("name1", "Michael", "name2", "Harry");
+        Map<String, String> copyOfImmutableMap = Map.copyOf(immutableMap);
+
+        assertThrows(UnsupportedOperationException.class, () -> copyOfImmutableMap.put("name3", "Micky"));
     }
 }
