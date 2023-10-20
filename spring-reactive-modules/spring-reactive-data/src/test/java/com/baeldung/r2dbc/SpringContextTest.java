20c2fc3bb5d2300bd4cc1bb7cf4e6adc62848122
timis1 <12120641+timis1@users.noreply.github.com>
2023-10-20 18:23:11 +0300

    JAVA-24463 Coalesce spring-reactive-data and spring-reactive-data-2 into a single module (#15026)
    Co-authored-by: timis1 <noreplay@yahoo.com>

--- spring-reactive-modules/spring-reactive-data/src/test/java/com/baeldung/r2dbc/SpringContextTest.java
+++ spring-reactive-modules/spring-reactive-data/src/test/java/com/baeldung/r2dbc/SpringContextTest.java
@@ -1,11 +1,9 @@
-package com.baeldung;
+package com.baeldung.r2dbc;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.baeldung.r2dbc.R2dbcApplication;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = R2dbcApplication.class)
