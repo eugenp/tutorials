27a227c0c48e8e73032c54e07808d49582617c6d
pedrolopes9-7 <pedroinf97@gmail.com>
2023-10-19 22:49:09 -0300

    fix main class

--- spring-kafka-2/src/main/java/com/baeldung/spring/kafka/managingkafkaconsumergroups/ManagingConsumerGroupsApplicationKafkaApp.java
+++ spring-kafka-2/src/main/java/com/baeldung/spring/kafka/managingkafkaconsumergroups/ManagingConsumerGroupsApplicationKafkaApp.java
@@ -1,14 +1,11 @@
 package com.baeldung.spring.kafka.managingkafkaconsumergroups;
 
-import com.baeldung.spring.kafka.topicsandpartitions.KafkaConsumerConfig;
-import com.baeldung.spring.kafka.topicsandpartitions.KafkaProducerConfig;
-import com.baeldung.spring.kafka.topicsandpartitions.KafkaTopicConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import(value = {KafkaTopicConfig.class, KafkaProducerConfig.class, KafkaConsumerConfig.class})
+@Import(value = {KafkaConsumerConfiguration.class, KafkaProducerConfiguration.class, KafkaTopicConfiguration.class})
 public class ManagingConsumerGroupsApplicationKafkaApp {
     public static void main(String[] args) {
         SpringApplication.run(ManagingConsumerGroupsApplicationKafkaApp.class, args);
