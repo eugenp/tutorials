5e01076534e3ed6118c9373d3260f2292b556c40
Gaetano Piazzolla <gae.piaz@gmail.com>
2023-10-21 10:36:49 +0200

    JAVA-220644 | Upgrade spring-boot-persistence-h2 to Spring Boot 3 (#14998)

--- persistence-modules/spring-boot-persistence-h2/src/main/java/com/baeldung/h2db/demo/client/ClientSpringBootApp.java
+++ persistence-modules/spring-boot-persistence-h2/src/main/java/com/baeldung/h2db/demo/client/ClientSpringBootApp.java
@@ -3,14 +3,14 @@ package com.baeldung.h2db.demo.client;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @ComponentScan("com.baeldung.h2db.demo.client")
