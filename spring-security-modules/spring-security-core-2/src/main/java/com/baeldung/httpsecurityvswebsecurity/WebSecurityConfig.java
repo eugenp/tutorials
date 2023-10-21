ae676478e4e2b5b21d541dc0f27b519e32eedc4b
Rufina Uche <102101377+Finniki@users.noreply.github.com>
2023-10-21 19:17:05 +0100

    Update WebSecurityConfig.java

--- spring-security-modules/spring-security-core-2/src/main/java/com/baeldung/httpsecurityvswebsecurity/WebSecurityConfig.java
+++ spring-security-modules/spring-security-core-2/src/main/java/com/baeldung/httpsecurityvswebsecurity/WebSecurityConfig.java
@@ -25,11 +25,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .formLogin();
+          .antMatchers("/")
+          .permitAll()
+          .anyRequest()
+          .authenticated()
+          .and()
+          .formLogin();
     }
 }
