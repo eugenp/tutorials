106067388dd18e6ae03fbda03798c83ecab9cd13
Gaetano Piazzolla <gae.piaz@gmail.com>
2023-10-21 10:36:49 +0200

    JAVA-220644 | Upgrade spring-boot-persistence-h2 to Spring Boot 3 (#14998)

--- persistence-modules/spring-boot-persistence-h2/src/main/java/com/baeldung/h2/exceptions/models/User.java
+++ persistence-modules/spring-boot-persistence-h2/src/main/java/com/baeldung/h2/exceptions/models/User.java
@@ -1,7 +1,7 @@
 package com.baeldung.h2.exceptions.models;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class User {
