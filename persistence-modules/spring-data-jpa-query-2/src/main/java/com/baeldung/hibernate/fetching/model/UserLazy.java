193fa95e8c59bdb0dcba55b0319bc1a05f1bcc54
Gaetano Piazzolla <gae.piaz@gmail.com>
2023-10-22 14:40:23 +0200

    JAVA-26042 | updated to spring-boot-3 (#14983)

--- persistence-modules/spring-data-jpa-query-2/src/main/java/com/baeldung/hibernate/fetching/model/UserLazy.java
+++ persistence-modules/spring-data-jpa-query-2/src/main/java/com/baeldung/hibernate/fetching/model/UserLazy.java
@@ -1,6 +1,6 @@
 package com.baeldung.hibernate.fetching.model;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
