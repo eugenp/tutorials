d45cc86d5a65d8978c8d25cf98a68047cac85f0e
rajatgarg <rajatgarg@adobe.com>
2023-10-23 19:12:59 +0530

    Pass list as a query parameter in Jersey

---
+++ jersey/src/main/java/com/baeldung/jersey/client/listdemo/ListDemoApp.java
@@ -0,0 +1,8 @@
+package com.baeldung.jersey.client.listdemo;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class ListDemoApp extends ResourceConfig {
+    public ListDemoApp() {
+        packages("com.baeldung.jersey.client.listdemo");
+    }
+}
