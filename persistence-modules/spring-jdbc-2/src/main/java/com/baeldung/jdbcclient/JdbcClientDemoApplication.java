78f5072f7c93b46ccb6acc62fa0210be2e8d7ccd
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2023-10-25 15:10:31 +0530

    BAEL-7135 updated component scan

--- persistence-modules/spring-jdbc-2/src/main/java/com/baeldung/jdbcclient/JdbcClientDemoApplication.java
+++ persistence-modules/spring-jdbc-2/src/main/java/com/baeldung/jdbcclient/JdbcClientDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "jdbcclient")
+@ComponentScan(basePackages = "com.baledung.jdbcclient")
 public class JdbcClientDemoApplication {
 
     public static void main(String[] args) {
