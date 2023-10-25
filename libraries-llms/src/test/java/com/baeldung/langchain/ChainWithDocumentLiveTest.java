03c9dce8cf0a141e8dfd705d03aaaef06579dd30
CHANDRAKANT Kumar <kumar.chandrakant@soprabanking.com>
2023-10-25 10:40:32 +0530

    Incorporated some review comments on the code.

--- libraries-llms/src/test/java/com/baeldung/langchain/ChainWithDocumentLiveTest.java
+++ libraries-llms/src/test/java/com/baeldung/langchain/ChainWithDocumentLiveTest.java
@@ -1,9 +1,7 @@
 package com.baeldung.langchain;
 
 import static dev.langchain4j.data.document.FileSystemDocumentLoader.loadDocument;
-import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO;
 import static java.time.Duration.ofSeconds;
-import static java.util.stream.Collectors.joining;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -11,75 +9,64 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Logger;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import dev.langchain4j.chain.ConversationalRetrievalChain;
 import dev.langchain4j.data.document.Document;
-import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
-import dev.langchain4j.data.embedding.Embedding;
-import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.model.openai.OpenAiTokenizer;
-import dev.langchain4j.store.embedding.EmbeddingMatch;
+import dev.langchain4j.retriever.EmbeddingStoreRetriever;
 import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
 public class ChainWithDocumentLiveTest {
+    
+    Logger logger = LoggerFactory.getLogger(ChainWithDocumentLiveTest.class);
 
     @Test
-    public void givenChainWithDocument_whenPrompted_thenValidResponse() {
-
-        Document document = loadDocument(toPath("src/test/resources/example-files/simpson's_adventures.txt"));
-        DocumentSplitter splitter = DocumentSplitters.recursive(100, 0, new OpenAiTokenizer(GPT_3_5_TURBO));
-        List<TextSegment> segments = splitter.split(document);
+    public void givenDocument_whenPrompted_thenValidResponse() {
 
         EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
-        List<Embedding> embeddings = embeddingModel.embedAll(segments)
-            .content();
+
         EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-        embeddingStore.addAll(embeddings, segments);
 
-        String question = "Who is Simpson?";
-        Embedding questionEmbedding = embeddingModel.embed(question)
-            .content();
-        int maxResults = 3;
-        double minScore = 0.7;
-        List<EmbeddingMatch<TextSegment>> relevantEmbeddings = embeddingStore.findRelevant(questionEmbedding, maxResults, minScore);
+        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
+            .documentSplitter(DocumentSplitters.recursive(500, 0))
+            .embeddingModel(embeddingModel)
+            .embeddingStore(embeddingStore)
+            .build();
 
-        PromptTemplate promptTemplate = PromptTemplate.from("Answer the following question to the best of your ability:\n" + "\n" + "Question:\n" + "{{question}}\n" + "\n" + "Base your answer on the following information:\n" + "{{information}}");
+        Document document = loadDocument(toPath("src/test/resources/example-files/simpson's_adventures.txt"));
+        ingestor.ingest(document);
 
-        String information = relevantEmbeddings.stream()
-            .map(match -> match.embedded()
-                .text())
-            .collect(joining("\n\n"));
-
-        Map<String, Object> variables = new HashMap<>();
-        variables.put("question", question);
-        variables.put("information", information);
-
-        Prompt prompt = promptTemplate.apply(variables);
         ChatLanguageModel chatModel = OpenAiChatModel.builder()
-            .apiKey(Constants.OPEN_API_KEY)
+            .apiKey(Constants.OPEN_AI_KEY)
             .timeout(ofSeconds(60))
             .build();
-        AiMessage aiMessage = chatModel.generate(prompt.toUserMessage())
-            .content();
 
-        Logger.getGlobal()
-            .info(aiMessage.text());
-        Assert.assertNotNull(aiMessage.text());
+        ConversationalRetrievalChain chain = ConversationalRetrievalChain.builder()
+            .chatLanguageModel(chatModel)
+            .retriever(EmbeddingStoreRetriever.from(embeddingStore, embeddingModel))
+            .chatMemory(MessageWindowChatMemory.withMaxMessages(10))
+            .promptTemplate(PromptTemplate
+                .from("Answer the following question to the best of your ability: {{question}}\n\nBase your answer on the following information:\n{{information}}"))
+            .build();
+
+        String answer = chain.execute("Who is Simpson?");
+
+        logger.info(answer);
+        Assert.assertNotNull(answer);
 
     }
 
--- libraries-llms/src/test/java/com/baeldung/langchain/ChatWithDocumentLiveTest.java
+++ libraries-llms/src/test/java/com/baeldung/langchain/ChatWithDocumentLiveTest.java
@@ -1,7 +1,9 @@
 package com.baeldung.langchain;
 
 import static dev.langchain4j.data.document.FileSystemDocumentLoader.loadDocument;
+import static dev.langchain4j.model.openai.OpenAiModelName.GPT_3_5_TURBO;
 import static java.time.Duration.ofSeconds;
+import static java.util.stream.Collectors.joining;
 
 import java.io.File;
 import java.net.MalformedURLException;
@@ -9,59 +11,77 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.logging.Logger;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import dev.langchain4j.chain.ConversationalRetrievalChain;
 import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentSplitter;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.embedding.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.input.Prompt;
+import dev.langchain4j.model.input.PromptTemplate;
 import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.retriever.EmbeddingStoreRetriever;
+import dev.langchain4j.model.openai.OpenAiTokenizer;
+import dev.langchain4j.store.embedding.EmbeddingMatch;
 import dev.langchain4j.store.embedding.EmbeddingStore;
-import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
 public class ChatWithDocumentLiveTest {
+    
+    Logger logger = LoggerFactory.getLogger(ChatWithDocumentLiveTest.class);
 
     @Test
-    public void givenDocument_whenPrompted_thenValidResponse() {
-
-        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
-
-        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
-
-        EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-            .documentSplitter(DocumentSplitters.recursive(500, 0))
-            .embeddingModel(embeddingModel)
-            .embeddingStore(embeddingStore)
-            .build();
+    public void givenChainWithDocument_whenPrompted_thenValidResponse() {
 
         Document document = loadDocument(toPath("src/test/resources/example-files/simpson's_adventures.txt"));
-        ingestor.ingest(document);
+        DocumentSplitter splitter = DocumentSplitters.recursive(100, 0, new OpenAiTokenizer(GPT_3_5_TURBO));
+        List<TextSegment> segments = splitter.split(document);
 
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+        List<Embedding> embeddings = embeddingModel.embedAll(segments)
+            .content();
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, segments);
+
+        String question = "Who is Simpson?";
+        Embedding questionEmbedding = embeddingModel.embed(question)
+            .content();
+        int maxResults = 3;
+        double minScore = 0.7;
+        List<EmbeddingMatch<TextSegment>> relevantEmbeddings = embeddingStore.findRelevant(questionEmbedding, maxResults, minScore);
+
+        PromptTemplate promptTemplate = PromptTemplate.from("Answer the following question to the best of your ability:\n" + "\n" + "Question:\n" + "{{question}}\n" + "\n" + "Base your answer on the following information:\n" + "{{information}}");
+
+        String information = relevantEmbeddings.stream()
+            .map(match -> match.embedded()
+                .text())
+            .collect(joining("\n\n"));
+
+        Map<String, Object> variables = new HashMap<>();
+        variables.put("question", question);
+        variables.put("information", information);
+
+        Prompt prompt = promptTemplate.apply(variables);
         ChatLanguageModel chatModel = OpenAiChatModel.builder()
-            .apiKey(Constants.OPEN_API_KEY)
+            .apiKey(Constants.OPEN_AI_KEY)
             .timeout(ofSeconds(60))
             .build();
+        AiMessage aiMessage = chatModel.generate(prompt.toUserMessage())
+            .content();
 
-        ConversationalRetrievalChain chain = ConversationalRetrievalChain.builder()
-            .chatLanguageModel(chatModel)
-            .retriever(EmbeddingStoreRetriever.from(embeddingStore, embeddingModel))
-            // .chatMemory() // you can override default chat memory
-            // .promptTemplate() // you can override default prompt template
-            .build();
-
-        String answer = chain.execute("Who is Simpson?");
-
-        Logger.getGlobal()
-            .info(answer);
-        Assert.assertNotNull(answer);
+        logger.info(aiMessage.text());
+        Assert.assertNotNull(aiMessage.text());
 
     }
 
