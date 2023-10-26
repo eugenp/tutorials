aa7ad247e1c7d8e53c0fcb3554d60ff8807fa82b
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2023-10-27 00:02:38 +0530

    Update StringIteratorTest.java

--- core-java-modules/core-java-strings/src/test/java/com/baeldung/stringIterator/StringIteratorTest.java
+++ core-java-modules/core-java-strings/src/test/java/com/baeldung/stringIterator/StringIteratorTest.java
@@ -1,1 +1,47 @@
-test
+package com.baeldung.stringIterator;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StringIteratorTest {
+  
+    @Test
+    public void testJavaCharArray() {
+        String input = "Hello, Baeldung!";
+        String expectedOutput = "Hello, Baeldung!";
+        String result = StringIterator.javaCharArray(input);
+        assertEquals(expectedOutput, result);
+    }
+  
+    @Test
+    public void testJavaForLoop() {
+        String input = "Hello, Baeldung!";
+        String expectedOutput = "Hello, Baeldung!";
+        String result = StringIterator.javaForLoop(input);
+        assertEquals(expectedOutput, result);
+    }
+  
+    @Test
+    public void testJava8ForEach() {
+        String input = "Hello, Baeldung!";
+        String expectedOutput = "Hello, Baeldung!";
+        String result = StringIterator.java8ForEach(input);
+        assertEquals(expectedOutput, result);
+    }
+  
+    @Test
+    public void testJavaRegExp() {
+        String input = "Hello, Baeldung!";
+        String expectedOutput = "Hello, Baeldung!";
+        String result = StringIterator.javaRegExp(input);
+        assertEquals(expectedOutput, result);
+    }
+  
+    @Test
+    public void testJavaCharacterIterator() {
+        String input = "Hello, Baeldung!";
+        String expectedOutput = "Hello, Baeldung!";
+        String result = StringIterator.javaCharacterIterator(input);
+        assertEquals(expectedOutput, result);
+    }
+}
