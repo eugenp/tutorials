2c9b3ff3689664360fd1dc4f2d63f007dd0999c5
SGWebFreelancer <sgwebfreelancer@gmail.com>
2023-10-31 20:28:45 +0800

    Fix the test method

--- core-java-modules/core-java-8-datetime-2/src/test/java/com/baeldung/maxdate/MaxDateDisplayUnitTest.java
+++ core-java-modules/core-java-8-datetime-2/src/test/java/com/baeldung/maxdate/MaxDateDisplayUnitTest.java
@@ -1,7 +1,7 @@
 package com.baeldung.maxdate;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MaxDateDisplayUnitTest {
 
