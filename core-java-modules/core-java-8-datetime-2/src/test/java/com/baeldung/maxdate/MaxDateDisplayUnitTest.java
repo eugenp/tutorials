35eea1ef7ea2c7a5ab8b816b8118a88884994d5e
SGWebFreelancer <sgwebfreelancer@gmail.com>
2023-10-31 20:20:04 +0800

    Fix the test method

--- core-java-modules/core-java-8-datetime-2/src/test/java/com/baeldung/maxdate/MaxDateDisplayUnitTest.java
+++ core-java-modules/core-java-8-datetime-2/src/test/java/com/baeldung/maxdate/MaxDateDisplayUnitTest.java
@@ -9,6 +9,6 @@ public class MaxDateDisplayUnitTest {
     public void whenGetMaxDate_thenCorrectResult() {
         MaxDateDisplay display = new MaxDateDisplay();
         String result = display.getMaxDateValue();
-        assertTrue(result.startsWith("The maximum date value in Java is: 292278994-08-17"));
+        assertEquals(result, "The maximum date value in Java is: 292278994-08-17");
     }
 }
