dc20d0d77f870e953e7c1b88af3f6b3edcd8724d
vaibhav007jain <72961247+vaibhav007jain@users.noreply.github.com>
2023-11-01 12:19:50 +0530

    Update VectorOperationsUnitTest.java

--- core-java-modules/core-java-collections-5/src/test/java/com/baeldung/vectors/VectorOperationsUnitTest.java
+++ core-java-modules/core-java-collections-5/src/test/java/com/baeldung/vectors/VectorOperationsUnitTest.java
@@ -20,49 +20,56 @@ public class VectorOperationsUnitTest {
     }
 
     @Test
-    public void givenAVector_addMethodAddsTheElements() {
+    public void givenAVector_whenAddElementsUsingAddMethod_thenElementsGetAddedAtEnd() {
         Vector<String> vector = getVector();
         vector.add("Hello");
         assertEquals(6, vector.size());
     }
 
     @Test
-    public void givenAVector_updateMethodUpdatesTheVector() {
+    public void givenAVector_whenUpdateElementAtIndex_thenElementAtIndexGetsUpdated() {
         Vector<String> vector = getVector();
         assertEquals(5, vector.size());
+        assertEquals("great", vector.get(3));
         vector.set(3, "good");
         assertEquals("good", vector.get(3));
     }
 
     @Test
-    public void givenAVector_removeMethodRemovesAnElementFromTheVector() {
+    public void givenAVector_whenRemoveAnElement_thenElementGetsRemovedFromTheVector() {
         Vector<String> vector = getVector();
         assertEquals(5, vector.size());
 
+        // remove a specific element
         vector.remove("a");
         assertEquals(4, vector.size());
 
+        // remove at specific index
         vector.remove(2);
         assertEquals("day!", vector.get(2));
         assertEquals(3, vector.size());
+        
+        assertEquals(false, vector.remove("SomethingThatDoesn'tExist"));
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void givenAVector_removeMethodThrowsArrayIndexOutOfBoundsException_IfIndexIsBeyondRange() {
+    public void givenAVector_whenIndexIsBeyondRange_thenRemoveMethodThrowsArrayIndexOutOfBoundsException() {
         Vector<String> vector = getVector();
+        assertEquals(5, vector.size());
         vector.remove(10);
     }
 
     @Test
-    public void givenAVector_getMethodGetsAnElementFromTheVector() {
+    public void givenAVector_whenGetElementWithinARange_thenGetMethodGetsAnElementFromTheVector() {
         Vector<String> vector = getVector();
         String fourthElement = vector.get(3);
         assertEquals("great", fourthElement);
     }
 
     @Test(expected = ArrayIndexOutOfBoundsException.class)
-    public void givenAVector_getMethodThrowsArrayIndexOutOfBoundsException_IfIndexIsBeyondRange() {
+    public void givenAVector_whenGetElementBeyondARange_thenGetMethodThrowsArrayIndexOutOfBoundsException() {
         Vector<String> vector = getVector();
+        assertEquals(5, vector.size());
         vector.get(10);
     }
 }
