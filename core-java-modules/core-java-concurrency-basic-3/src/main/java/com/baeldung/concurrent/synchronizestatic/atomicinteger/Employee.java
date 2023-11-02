02e02f928f4ff4e4bad974b1e3e30e17b2fbc8ed
Michael Pratt <prattm@gmail.com>
2023-11-02 17:47:02 -0600

    [BAEL-7074] Synchronization for static data examples

---
+++ core-java-modules/core-java-concurrency-basic-3/src/main/java/com/baeldung/concurrent/synchronizestatic/atomicinteger/Employee.java
@@ -0,0 +1,32 @@
+package com.baeldung.concurrent.synchronizestatic.atomicinteger;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Synchronizing static variable with AtomicInteger.
+ */
+public class Employee {
+
+    private static final AtomicInteger count = new AtomicInteger(0);
+
+    int id;
+    String name;
+    String title;
+
+    public Employee(int id, String name, String title)
+    {
+        incrementCount();
+        this.id = id;
+        this.name = name;
+        this.title = title;
+    }
+
+    private static void incrementCount() {
+        count.incrementAndGet();
+        System.out.println("Count = " + count.get());
+    }
+
+    public static int getCount() {
+        return count.get();
+    }
+}
