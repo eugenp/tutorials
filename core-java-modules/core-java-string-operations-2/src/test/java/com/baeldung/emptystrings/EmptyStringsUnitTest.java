049770733ccef4e7e364c2cd6768f66b66e85e77
anuragkumawat <anuragkumawat7@gmail.com>
2023-11-04 12:24:35 +0530

    JAVA-27176 Upgrade hibernate validator version in core-java-string-operations-2 module

--- core-java-modules/core-java-string-operations-2/src/test/java/com/baeldung/emptystrings/EmptyStringsUnitTest.java
+++ core-java-modules/core-java-string-operations-2/src/test/java/com/baeldung/emptystrings/EmptyStringsUnitTest.java
@@ -5,10 +5,10 @@ import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.util.ObjectUtils;
 
-import javax.validation.ConstraintViolation;
-import javax.validation.Validation;
-import javax.validation.Validator;
-import javax.validation.ValidatorFactory;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
 import java.util.Set;
 
 import static org.hamcrest.Matchers.iterableWithSize;
