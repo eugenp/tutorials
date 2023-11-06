5d3f08d0e915ca8b077666253fa4b3899475155c
Amol Gote <aamolgote@hotmail.com>
2023-11-05 21:32:49 -0500

    Fix the whitespace and newline issues - wuth code formatter

--- apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/serialization/JacksonDeserializer.java
+++ apache-kafka-2/src/main/java/com/baeldung/kafka/message/ordering/serialization/JacksonDeserializer.java
@@ -1,6 +1,8 @@
 package com.baeldung.kafka.message.ordering.serialization;
+
 import com.baeldung.kafka.message.ordering.Config;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.apache.kafka.common.serialization.Deserializer;
 
 import java.util.Map;
@@ -11,7 +13,6 @@ import java.util.Map;
 public class JacksonDeserializer<T> implements Deserializer<T> {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private Class<T> type;
-
 
     @Override
     public void configure(Map<String, ?> configs, boolean isKey) {
