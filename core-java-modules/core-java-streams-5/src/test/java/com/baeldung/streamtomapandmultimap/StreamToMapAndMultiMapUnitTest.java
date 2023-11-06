875721538afe197904acf33679b6b98c1c90b2ba
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2023-11-06 12:30:07 +0200

    Update StreamToMapAndMultiMapUnitTest.java

--- core-java-modules/core-java-streams-5/src/test/java/com/baeldung/streamtomapandmultimap/StreamToMapAndMultiMapUnitTest.java
+++ core-java-modules/core-java-streams-5/src/test/java/com/baeldung/streamtomapandmultimap/StreamToMapAndMultiMapUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.streamtomapandmultimap;
 
-import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
 import org.junit.Test;
 
 import java.util.*;
@@ -32,15 +33,15 @@ public class StreamToMapAndMultiMapUnitTest {
     public void givenStringStream_whenConvertingToMultimap_thenExpectedMultimapIsGenerated() {
         Stream<String> stringStream = Stream.of("one", "two", "three", "two");
 
-        LinkedHashMultimap<Object, Object> multimap = LinkedHashMultimap.create();
+        ListMultimap<String, String> multimap = stringStream.collect(
+                ArrayListMultimap::create,
+                (map, element) -> map.put(element, element),
+                ArrayListMultimap::putAll
+        );
 
-        stringStream.collect(Collectors.groupingBy(
-                s -> s,
-                Collectors.mapping(s -> s, Collectors.toList())
-        )).forEach((key, value) -> multimap.putAll(key, value));
-
-        LinkedHashMultimap<Object, Object> expectedMultimap = LinkedHashMultimap.create();
+        ListMultimap<String, String> expectedMultimap = ArrayListMultimap.create();
         expectedMultimap.put("one", "one");
+        expectedMultimap.put("two", "two");
         expectedMultimap.put("two", "two");
         expectedMultimap.put("three", "three");
 
