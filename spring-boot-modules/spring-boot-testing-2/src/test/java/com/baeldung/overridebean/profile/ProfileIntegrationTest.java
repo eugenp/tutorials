1b28d393e84c9b86470b5ab779fc685d3bd600e1
luca <lucacambi77@gmail.com>
2023-11-10 14:30:22 +0100

    feat: override spring bean

---
+++ spring-boot-modules/spring-boot-testing-2/src/test/java/com/baeldung/overridebean/profile/ProfileIntegrationTest.java
@@ -0,0 +1,29 @@
+package com.baeldung.overridebean.profile;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.baeldung.overridebean.Endpoint;
+
+@SpringBootTest(classes = { Application.class, ProfileConfig.class, Endpoint.class, ProfileTestConfig.class })
+@AutoConfigureMockMvc
+class ProfileIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void givenConfigurationWithProfile_whenNoProductionProfileIsActive_thenStubOk() throws Exception {
+        this.mockMvc.perform(get("/hello"))
+          .andExpect(status().isOk())
+          .andExpect(content().string(containsString("hello profile stub")));
+    }
+}
