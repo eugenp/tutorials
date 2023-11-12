f8fea5bbd8a87cd1d7514d4159f704281d74d96a
dlazydev <146261002+dlazydev@users.noreply.github.com>
2023-11-12 15:48:09 +0100

    Create MultipartRelatedExampleUnitTest.java

---
+++ xml-2/src/test/java/com/baeldung/xml/binary/MultipartRelatedExampleUnitTest.java
@@ -0,0 +1,20 @@
+package com.baeldung.xml.binary;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class MultipartRelatedExampleUnitTest {
+
+    @Test
+    public void testEncodedData() {
+        // Your assertions for encoded data
+        assertNotNull("Your encoded data should not be null");
+    }
+
+    @Test
+    public void testXmlContent() {
+        // Your assertions for XML content
+        assertNotNull("Your XML content should not be null");
+    }
+}
