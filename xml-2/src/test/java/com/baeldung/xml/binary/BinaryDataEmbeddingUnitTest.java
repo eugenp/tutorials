5269910564a8fc490fa8235ccbdea2f8f202c51e
dlazydev <146261002+dlazydev@users.noreply.github.com>
2023-11-12 14:25:50 +0100

    Update BinaryDataEmbeddingUnitTest.java

--- xml-2/src/test/java/com/baeldung/xml/binary/BinaryDataEmbeddingUnitTest.java
+++ xml-2/src/test/java/com/baeldung/xml/binary/BinaryDataEmbeddingUnitTest.java
@@ -1,34 +1,30 @@
-package com.baeldung.xml.binary;
+package com.baeldung.xml.binary
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class BinaryDataEmbedderTest {
+public class BinaryDataEmbeddingUnitTest {
 
     @Test
-    void BinaryDataEmbeddingUnitTest() {
+    void testBinaryDataEmbedding() {
+        // Define test data
         byte[] binaryData = new byte[] { /* Your binary data here */ };
-
-        // Encode the binary data into Base64
-        String encodedData = Base64.getEncoder().encodeToString(binaryData);
-
-        // Assuming your BinaryDataEmbedder class has a method to generate the XML
-        String xml = BinaryDataEmbedder.generateXml(encodedData);
-
-        // Define the expected XML structure
         String expectedXml = "<image>" +
-            "<metadata>" +
-            "<format>JPEG</format>" +
-            "<width>800</width>" +
-            "<height>600</height>" +
-            "</metadata>" +
-            "<data>" +
-            "<encoding>base64</encoding>" +
-            "<content>" + encodedData + "</content>" +
-            "</data>" +
-            "</image>";
+                "<metadata>" +
+                "<format>JPEG</format>" +
+                "<width>800</width>" +
+                "<height>600</height>" +
+                "</metadata>" +
+                "<data>" +
+                "<encoding>base64</encoding>" +
+                "<content>" + Base64.getEncoder().encodeToString(binaryData) + "</content>" +
+                "</data>" +
+                "</image>";
 
-        // Check if the generated XML matches the expected XML
-        assertEquals(expectedXml, xml);
+        // Generate XML from binary data
+        String generatedXml = BinaryDataEmbedder.generateXml(binaryData);
+
+        // Assert that generated XML matches expected XML
+        assertEquals(expectedXml, generatedXml);
     }
 }
