43497cbd691ced4651224a2b17138b23003e60eb
dlazydev <146261002+dlazydev@users.noreply.github.com>
2023-11-12 15:45:22 +0100

    Create MultipartRelatedExample.java

---
+++ xml-2/src/main/java/com/baeldung/xml/binary/MultipartRelatedExample.java
@@ -0,0 +1,35 @@
+package com.baeldung.xml.binary;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+
+public class MultipartRelatedExample {
+
+    public static void main(String[] args) throws IOException {
+        // Read binary data from a file
+        byte[] binaryData = Files.readAllBytes(Path.of("your_binary_file.jpg"));
+
+        // Encode the binary data into Base64
+        String encodedData = Base64.getEncoder().encodeToString(binaryData);
+
+        // Create an XML document with inline references using Multipart/Related
+        String xmlContent = """
+                <multipart_related>
+                    <metadata>
+                        <format>JPEG</format>
+                        <width>800</width>
+                        <height>600</height>
+                    </metadata>
+                    <data>
+                        <encoding>multipart/related</encoding>
+                        <content_id>cid:binary_part</content_id>
+                        <xml_reference>...inline reference to binary part...</xml_reference>
+                    </data>
+                </multipart_related>
+                """;
+
+        // Your application logic here
+    }
+}
