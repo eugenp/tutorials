bdd72a25a53a69d69626d2a607a2623260c141bf
mcasari <mario.casari@gmail.com>
2023-11-13 23:39:30 +0100

    BAEL-6600: override put instead of additional method

--- core-java-modules/core-java-collections-maps-7/src/test/java/com/baeldung/map/LimitMaxSizeHashMapByCustomHashMapUnitTest.java
+++ core-java-modules/core-java-collections-maps-7/src/test/java/com/baeldung/map/LimitMaxSizeHashMapByCustomHashMapUnitTest.java
@@ -1,8 +1,9 @@
 package com.baeldung.map;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.util.LinkedHashMap;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -11,33 +12,27 @@ import org.junit.jupiter.api.Test;
 class LimitMaxSizeHashMapByCustomHashMapUnitTest {
 
     private final int MAX_SIZE = 4;
-    private LinkedHashMap<Integer, String> linkedHashMap;
+    private HashMapWithMaxSizeLimit<Integer, String> hashMapWithMaxSizeLimit;
 
     @BeforeEach
     void setUp() {
-        linkedHashMap = new LinkedHashMap<Integer, String>() {
-            private static final long serialVersionUID = 1L;
-
-            protected boolean removeEldestEntry(Map.Entry<Integer, String> eldest) {
-                return size() > MAX_SIZE;
-            }
-        };
-        linkedHashMap.put(1, "One");
-        linkedHashMap.put(2, "Two");
-        linkedHashMap.put(3, "Three");
-        linkedHashMap.put(4, "Four");
+        hashMapWithMaxSizeLimit = new HashMapWithMaxSizeLimit<Integer, String>(MAX_SIZE);
     }
 
     @Test
-    void givenLinkedHashMapObject_whenAddingNewEntry_thenEldestEntryIsRemoved() {
-        linkedHashMap.put(5, "Five");
-        String[] expectedArrayAfterFive = { "Two", "Three", "Four", "Five" };
-        assertArrayEquals(expectedArrayAfterFive, linkedHashMap.values()
-            .toArray());
-        linkedHashMap.put(6, "Six");
-        String[] expectedArrayAfterSix = { "Three", "Four", "Five", "Six" };
-        assertArrayEquals(expectedArrayAfterSix, linkedHashMap.values()
-            .toArray());
+    void givenCustomHashMapObject_whenAddingNewEntryAndLimitExceeded_thenThrowsException() {
+        Exception exception = assertThrows(RuntimeException.class, () -> {
+            hashMapWithMaxSizeLimit.put(1, "One");
+            hashMapWithMaxSizeLimit.put(2, "Two");
+            hashMapWithMaxSizeLimit.put(3, "Three");
+            hashMapWithMaxSizeLimit.put(4, "Four");
+            hashMapWithMaxSizeLimit.put(5, "Five");
+        });
+
+        String messageThrownWhenSizeExceedsLimit = "Max size exceeded!";
+        String actualMessage = exception.getMessage();
+
+        assertTrue(actualMessage.equals(messageThrownWhenSizeExceedsLimit));
     }
 
 }
