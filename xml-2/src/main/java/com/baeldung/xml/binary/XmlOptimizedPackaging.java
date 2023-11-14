97fe2ba8ceaa3f7136d8001af8953b8ee33c2cd7
dlazydev <146261002+dlazydev@users.noreply.github.com>
2023-11-14 15:19:33 +0100

    Update XmlOptimizedPackaging.java

--- xml-2/src/main/java/com/baeldung/xml/binary/XmlOptimizedPackaging.java
+++ xml-2/src/main/java/com/baeldung/xml/binary/XmlOptimizedPackaging.java
@@ -6,7 +6,14 @@ import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+
 
 public class XmlOptimizedPackaging {
 
