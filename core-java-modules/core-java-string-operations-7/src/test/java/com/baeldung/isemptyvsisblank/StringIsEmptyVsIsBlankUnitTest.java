1323c248253259b9581af7aec337b467c9049c8d
pedrolopes9-7 <pedroinf97@gmail.com>
2023-11-13 23:33:21 -0300

    final code

--- core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/isemptyvsisblank/StringIsEmptyVsIsBlankUnitTest.java
+++ core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/isemptyvsisblank/StringIsEmptyVsIsBlankUnitTest.java
@@ -2,46 +2,24 @@ package com.baeldung.isemptyvsisblank;
 
 import org.junit.jupiter.api.Test;
 
-import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringIsEmptyVsIsBlankUnitTest {
 
     @Test
-    public void givenString_whenCallIsEmpty_thenReturnCorrectValues(){
-        String fullText = "Example text";
-        String emptyText = "";
-        String blankText = "  ";
-        String specialCharText = "\t\n";
-
-        assertFalse(fullText.isEmpty());
-        assertTrue(emptyText.isEmpty());
-        assertFalse(blankText.isEmpty());
-        assertFalse(specialCharText.isEmpty());
+    public void givenString_whenCallIsEmpty_thenReturnCorrectValues() {
+        assertFalse("Example text".isEmpty());
+        assertTrue("".isEmpty());
+        assertFalse("  ".isEmpty());
+        assertFalse("\t\n\r\f".isEmpty());
     }
 
     @Test
-    public void givenString_whenCallStringIsBlank_thenReturnCorrectValues(){
-        String fullText = "Example text";
-        String emptyText = "";
-        String blankText = "  ";
-        String whitespaceEscapeSequences = "\t\n\r\f ";
-
-        assertFalse(fullText.isBlank());
-        assertTrue(emptyText.isBlank());
-        assertTrue(blankText.isBlank());
-        assertTrue(whitespaceEscapeSequences.isBlank());
-    }
-
-    @Test
-    public void givenString_whenCallStringUtilsIsBlank_thenReturnCorrectValues(){
-        String fullText = "Example text";
-        String emptyText = "";
-        String blankText = "  ";
-
-        assertFalse(isBlank(fullText));
-        assertTrue(isBlank(emptyText));
-        assertTrue(isBlank(blankText));
+    public void givenString_whenCallStringIsBlank_thenReturnCorrectValues() {
+        assertFalse("Example text".isBlank());
+        assertTrue("".isBlank());
+        assertTrue("  ".isBlank());
+        assertTrue("\t\n\r\f ".isBlank());
     }
 }
