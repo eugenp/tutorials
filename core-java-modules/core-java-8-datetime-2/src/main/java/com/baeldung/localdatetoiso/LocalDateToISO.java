c0ec3516535c74d1814bc724881d0282da42b038
anujgaud <146576725+anujgaud@users.noreply.github.com>
2023-11-18 00:58:52 +0530

    Add Joda library support

--- core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/localdatetoiso/LocalDateToISO.java
+++ core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/localdatetoiso/LocalDateToISO.java
@@ -9,6 +9,10 @@ import java.util.Date;
 
 import org.apache.commons.lang3.time.DateFormatUtils;
 
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+
 public class LocalDateToISO {
     public String formatUsingDateTimeFormatter(LocalDate localDate) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssX");
@@ -20,6 +24,13 @@ public class LocalDateToISO {
         Date utilDate = Date.from(date.atStartOfDay(ZoneOffset.UTC).toInstant());
         DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
         String formattedDate = dateFormat.format(utilDate);
+        return formattedDate;
+    }
+
+    public String formatUsingJodaTime(org.joda.time.LocalDate localDate) {
+        DateTime dateTime = localDate.toDateTimeAtStartOfDay(DateTimeZone.UTC);
+        org.joda.time.format.DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        String formattedDate = formatter.print(localDate);
         return formattedDate;
     }
 }
