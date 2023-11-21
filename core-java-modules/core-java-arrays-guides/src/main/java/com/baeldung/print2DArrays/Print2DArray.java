fc81cb058592f5ad8cd96a4c11319dfb7d548eef
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2023-11-21 16:31:48 +0530

    Create Print2DArray.java

---
+++ core-java-modules/core-java-arrays-guides/src/main/java/com/baeldung/print2DArrays/Print2DArray.java
@@ -0,0 +1,23 @@
+package com.baeldung.print2DArrays;
+
+import java.util.*;
+
+public class Print2DArray{
+
+    public static void print2DNested(int[][] myArray){
+        for (int i = 0; i < myArray.length; i++) { 
+            for (int j = 0; j < myArray[i].length; j++) { 
+                System.out.print(myArray[i][j] + " "); 
+            }
+            System.out.println(); 
+        }
+    }
+
+    public static void print2DStream(int[][] myArray){ 
+        Arrays.stream(myArray).flatMapToInt(Arrays::stream).forEach(num -> System.out.print(num + " "));
+    }
+  
+    public static void print2DDeepToString(int[][] myArray){ 
+        System.out.println(Arrays.deepToString(myArray));
+    }
+}
