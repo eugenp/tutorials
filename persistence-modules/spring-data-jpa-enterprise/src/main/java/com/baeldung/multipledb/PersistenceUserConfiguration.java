da0df4cf8a448033eab1236211842b0bdaa63d05
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2023-11-22 10:09:43 +0000

    [JAVA-26374-spring-data-jpa-enterprise] Moved articles from spring-data-jpa-enterprise-2 to spring-data-jpa-enterprise (#15103)

--- persistence-modules/spring-data-jpa-enterprise/src/main/java/com/baeldung/multipledb/PersistenceUserConfiguration.java
+++ persistence-modules/spring-data-jpa-enterprise/src/main/java/com/baeldung/multipledb/PersistenceUserConfiguration.java
@@ -1,8 +1,15 @@
 package com.baeldung.multipledb;
 
-import com.google.common.base.Preconditions;
+import java.util.HashMap;
+
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -11,8 +18,7 @@ import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import javax.sql.DataSource;
-import java.util.HashMap;
+import com.google.common.base.Preconditions;
 
 @Configuration
 @PropertySource({"classpath:persistence-multiple-db.properties"})
