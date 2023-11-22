b64a40f6f4f11c517c95dcdde684d3dec55a0f44
macasari <macasari@MACASARIW.mshome.net>
2023-11-22 10:40:38 +0100

    BAEL-6600 - fix various reported issues

--- core-java-modules/core-java-collections-maps-7/src/test/java/com/baeldung/map/LimitMaxSizeHashMapByLinkedHashMapUnitTest.java
+++ core-java-modules/core-java-collections-maps-7/src/test/java/com/baeldung/map/LimitMaxSizeHashMapByLinkedHashMapUnitTest.java
@@ -5,19 +5,18 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LimitMaxSizeHashMapByLinkedHashMapUnitTest {
 
-    private final int MAX_SIZE = 4;
-    private LinkedHashMap<Integer, String> linkedHashMap;
-
-    @BeforeEach
-    void setUp() {
+    @Test
+    void givenLinkedHashMapObject_whenAddingNewEntry_thenEldestEntryIsRemoved() {
+        final int MAX_SIZE = 4;
+        LinkedHashMap<Integer, String> linkedHashMap;
         linkedHashMap = new LinkedHashMap<Integer, String>() {
             private static final long serialVersionUID = 1L;
-
+            
+            @Override
             protected boolean removeEldestEntry(Map.Entry<Integer, String> eldest) {
                 return size() > MAX_SIZE;
             }
@@ -26,10 +25,6 @@ class LimitMaxSizeHashMapByLinkedHashMapUnitTest {
         linkedHashMap.put(2, "Two");
         linkedHashMap.put(3, "Three");
         linkedHashMap.put(4, "Four");
-    }
-
-    @Test
-    void givenLinkedHashMapObject_whenAddingNewEntry_thenEldestEntryIsRemoved() {
         linkedHashMap.put(5, "Five");
         String[] expectedArrayAfterFive = { "Two", "Three", "Four", "Five" };
         assertArrayEquals(expectedArrayAfterFive, linkedHashMap.values()
