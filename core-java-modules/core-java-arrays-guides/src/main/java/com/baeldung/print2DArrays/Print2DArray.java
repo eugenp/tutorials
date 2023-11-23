20e84dee3d887e16c0023b314bbf5b7d0a8f3f0b
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2023-11-24 00:28:48 +0530

    Update Print2DArray.java

--- core-java-modules/core-java-arrays-guides/src/main/java/com/baeldung/print2DArrays/Print2DArray.java
+++ core-java-modules/core-java-arrays-guides/src/main/java/com/baeldung/print2DArrays/Print2DArray.java
@@ -4,26 +4,34 @@ import java.util.*;
 
 public class Print2DArray{
 
-    public static void print2DNested(int[][] myArray){
+    public static String print2DNested(int[][] myArray){
+        StringBuilder result = new StringBuilder();
         for (int i = 0; i < myArray.length; i++) { 
             for (int j = 0; j < myArray[i].length; j++) { 
-                System.out.print(myArray[i][j] + " "); 
+                result.append(myArray[i][j]).append(" "); 
             }
-            System.out.println(); 
+            result.append("\n"); 
         }
+        return result.toString();
     }
 
-    public static void print2DStream(int[][] myArray){ 
-        Arrays.stream(myArray).flatMapToInt(Arrays::stream).forEach(num -> System.out.print(num + " "));
+    public static String print2DStream(int[][] myArray){ 
+        StringBuilder result = new StringBuilder();
+        Arrays.stream(myArray).flatMapToInt(Arrays::stream).forEach(num -> result.append(num).append(" "));
+        return result.toString();
     }
   
-    public static void print2DDeepToString(int[][] myArray){ 
-        System.out.println(Arrays.deepToString(myArray));
+    public static String print2DDeepToString(int[][] myArray){ 
+        StringBuilder result = new StringBuilder(); 
+        result.append(Arrays.deepToString(myArray)).append("\n");
+        return result.toString();
     }
     
-    public static void printArrayString(int[][] myArray) {
+    public static String printArrayString(int[][] myArray) {
+        StringBuilder result = new StringBuilder(); 
         for (int[] row : myArray) {
-            System.out.println(Arrays.toString(row));
+            result.append(Arrays.toString(row)).append("\n");
         }
+        return result.toString();
      }
 }
