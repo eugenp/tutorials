596c10de0acbd550c61b374f03cd78241b7a01ae
uzma <uzma.nasir@gmail.com>
2023-12-03 22:23:51 +0000

    [BAEL-7073] fix code style

--- core-java-modules/core-java-concurrency-simple/src/main/java/com/baeldung/concurrent/executorservice/ExecuteExample.java
+++ core-java-modules/core-java-concurrency-simple/src/main/java/com/baeldung/concurrent/executorservice/ExecuteExample.java
@@ -4,22 +4,21 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ExecuteExample {
-  public static void main(String[] args)
-  {
-    ExecutorService executorService = Executors.newFixedThreadPool(2);
-    // Task using Runnable
-     Runnable task = () ->
-     {
-       int[] numbers = {1, 2, 3, 4, 5};
-       int sum = 0;
-       for (int num : numbers) {
-         sum += num;
-       }
-       System.out.println("Sum calculated using execute:" + sum);
-     };
-     // Submit the task using execute
-     executorService.execute(task); executorService.shutdown();
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        // Task using Runnable
+        Runnable task = () -> {
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            int sum = 0;
+            for (int num : numbers) {
+                sum += num;
+            }
+            System.out.println("Sum calculated using execute:" + sum);
+        };
+        // Submit the task using execute
+        executorService.execute(task);
+        executorService.shutdown();
 
-  }
+    }
 
 }
