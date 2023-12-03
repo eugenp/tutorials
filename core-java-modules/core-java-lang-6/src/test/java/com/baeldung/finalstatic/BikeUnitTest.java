692f3b201a8a081664963b039a27ea41f3e5a318
michaelin007 <michaelolayemi28@gmail.com>
2023-12-03 07:19:26 +0000

    Final Static Variables in Java

---
+++ core-java-modules/core-java-lang-6/src/test/java/com/baeldung/finalstatic/BikeUnitTest.java
@@ -0,0 +1,28 @@
+package com.baeldung.finalstatic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BikeUnitTest {
+
+    @Test
+    void givenTireConstantSetUponDeclaration_whenGetTire_thenReturnTwo() {
+        assertEquals(2, Bike.TIRE);
+    }
+
+    @Test
+    void givenPedalConstantSetByStaticBlock_whenGetPedal_thenReturnFive() {
+        assertEquals(5, Bike.PEDAL);
+    }
+
+    @Test
+    void givenPartConstantObject_whenObjectStateChanged_thenCorrect() {
+        Bike.PART.put("seat", 1);
+        assertEquals(1, Bike.PART.get("seat"));
+
+        Bike.PART.put("seat", 5);
+        assertEquals(5, Bike.PART.get("seat"));
+    }
+
+}
