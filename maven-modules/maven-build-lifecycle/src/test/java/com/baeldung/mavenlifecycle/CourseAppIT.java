276e008b18b05e4d5f34d868c3a577a74add0f47
shahulbasha <shhl.basha333@gmail.com>
2023-12-10 13:59:05 -0500

    BAEL-7206 initial commit

---
+++ maven-modules/maven-build-lifecycle/src/test/java/com/baeldung/mavenlifecycle/CourseAppIT.java
@@ -0,0 +1,16 @@
+package com.baeldung.mavenlifecycle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.baeldung.mavenlifecycle.CourseApp;
+
+class CourseAppIT {
+
+    @Test
+    void givenIntegrationTest_whenGetCourse_ThenCourseShouldBePresent() {
+        CourseApp courseApp = new CourseApp();
+        assertEquals("Baeldung Spring Masterclass", courseApp.getCourse());
+    }
+}
---
+++ maven-modules/maven-build-lifecycle/src/test/java/com/baeldung/mavenlifecycle/CourseAppUnitTest.java
@@ -0,0 +1,24 @@
+package com.baeldung.mavenlifecycle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.baeldung.mavenlifecycle.CourseApp;
+
+@ExtendWith(MockitoExtension.class)
+class CourseAppUnitTest {
+
+    @Mock
+    CourseApp courseApp;
+
+    @Test
+    void whenGetCourse_ThenCourseShouldBePresent() {
+        when(courseApp.getCourse()).thenReturn("Baeldung Spring Masterclass");
+        assertEquals("Baeldung Spring Masterclass", courseApp.getCourse());
+    }
+}
