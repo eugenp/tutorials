31defb87888a634c2ee5342ee7108f80ef35f087
anujgaud <146576725+anujgaud@users.noreply.github.com>
2023-12-12 17:42:49 +0530

    Add unit tests for Enable Debug method

---
+++ core-java-modules/core-java-security-4/src/test/java/com/baeldung/enablessldebug/SSLDebugLoggerUnitTest.java
@@ -0,0 +1,31 @@
+package com.baeldung.enablessldebug;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import org.junit.Test;
+
+public class SSLDebugLoggerUnitTest {
+    @Test
+    public void givenDebugging_whenSystemProperties_thenEnableSSL() {
+        SSLDebugLogger.enableSSLDebugUsingSystemProperties();
+        assertEquals("ssl:debug", System.getProperty("javax.net.debug"));
+    }
+
+    @Test
+    public void givenDebugging_whenConfigurationFile_thenEnableSSL() throws IOException {
+        InputStream configFile = SSLDebugLoggerUnitTest.class.getClassLoader().getResourceAsStream("logging.properties");
+        LogManager.getLogManager().readConfiguration(configFile);
+
+        Logger sslLogger = Logger.getLogger("javax.net.ssl");
+        ConsoleHandler consoleHandler = (ConsoleHandler) sslLogger.getHandlers()[0];
+        Level consoleHandlerLevel = consoleHandler.getLevel();
+        assertEquals(Level.ALL, consoleHandlerLevel, "SSL ConsoleHandler level should be ALL");
+    }
+}
