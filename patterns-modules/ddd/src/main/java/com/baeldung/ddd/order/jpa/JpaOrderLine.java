9f7ec43036959e659ac17897426f38b3ac90de94
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2023-12-13 12:41:51 +0200

    [JAVA-27822] Upgrade ddd to Spring Boot 3 (#15399)

--- patterns-modules/ddd/src/main/java/com/baeldung/ddd/order/jpa/JpaOrderLine.java
+++ patterns-modules/ddd/src/main/java/com/baeldung/ddd/order/jpa/JpaOrderLine.java
@@ -1,7 +1,7 @@
 package com.baeldung.ddd.order.jpa;
 
-import javax.persistence.Embeddable;
-import javax.persistence.Embedded;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
 
 @Embeddable
 class JpaOrderLine {
