22d4bca34223012548db3b0ae238a14b69f722e3
anujgaud <146576725+anujgaud@users.noreply.github.com>
2023-12-14 13:58:02 +0530

    Update SSLDebugLoggerUnitTest.java
    Updated test method names

--- core-java-modules/core-java-security-4/src/test/java/com/baeldung/enablessldebug/SSLDebugLoggerUnitTest.java
+++ core-java-modules/core-java-security-4/src/test/java/com/baeldung/enablessldebug/SSLDebugLoggerUnitTest.java
@@ -12,20 +12,23 @@ import java.util.logging.Logger;
 import org.junit.Test;
 
 public class SSLDebugLoggerUnitTest {
+    
     @Test
-    public void givenDebugging_whenSystemProperties_thenEnableSSL() {
+    public void givenSSLDebuggingEnabled_whenUsingSystemProperties_thenEnableSSL() {
         SSLDebugLogger.enableSSLDebugUsingSystemProperties();
+        
         assertEquals("ssl:debug", System.getProperty("javax.net.debug"));
     }
 
     @Test
-    public void givenDebugging_whenConfigurationFile_thenEnableSSL() throws IOException {
+    public void givenSSLDebuggingEnabled_whenUsingConfigurationFile_thenEnableSSL() throws IOException {
         InputStream configFile = SSLDebugLoggerUnitTest.class.getClassLoader().getResourceAsStream("logging.properties");
         LogManager.getLogManager().readConfiguration(configFile);
 
         Logger sslLogger = Logger.getLogger("javax.net.ssl");
         ConsoleHandler consoleHandler = (ConsoleHandler) sslLogger.getHandlers()[0];
         Level consoleHandlerLevel = consoleHandler.getLevel();
+        
         assertEquals(Level.ALL, consoleHandlerLevel, "SSL ConsoleHandler level should be ALL");
     }
 }
