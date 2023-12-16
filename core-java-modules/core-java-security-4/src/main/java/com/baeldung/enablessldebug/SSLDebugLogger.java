9ddc60c995192f206ad59a62a368f37d3f7d5de1
anujgaud <146576725+anujgaud@users.noreply.github.com>
2023-12-16 14:34:48 +0530

    Add a method to make Https Request

--- core-java-modules/core-java-security-4/src/main/java/com/baeldung/enablessldebug/SSLDebugLogger.java
+++ core-java-modules/core-java-security-4/src/main/java/com/baeldung/enablessldebug/SSLDebugLogger.java
@@ -2,6 +2,21 @@ package com.baeldung.enablessldebug;
 
 public class SSLDebugLogger {
     public static void enableSSLDebugUsingSystemProperties() {
-        System.setProperty("javax.net.debug", "ssl:debug");
+        System.setProperty("javax.net.debug", "ssl");
+    }
+
+    public static void makeHttpsRequest() throws Exception {
+        String url = "https://github.com/eugenp/tutorials";
+        URL httpsUrl = new URL(url);
+        HttpsURLConnection connection = (HttpsURLConnection) httpsUrl.openConnection();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String line;
+            System.out.println("Response from " + url + ":");
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            System.out.println();
+        }
     }
 }
