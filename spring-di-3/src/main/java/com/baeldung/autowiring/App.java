2e274ef9bcad6a978a6271a9d91d059db0778a42
timis1 <12120641+timis1@users.noreply.github.com>
2023-12-16 11:03:41 +0200

    JAVA-27019 Simplify spring-di modules' poms (#15330)

--- spring-di-3/src/main/java/com/baeldung/autowiring/App.java
+++ spring-di-3/src/main/java/com/baeldung/autowiring/App.java
@@ -2,7 +2,6 @@ package com.baeldung.autowiring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class App {
