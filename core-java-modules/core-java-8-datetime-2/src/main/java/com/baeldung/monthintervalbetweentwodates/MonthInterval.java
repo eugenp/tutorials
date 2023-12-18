c35c13d5d8890cacd04cf7009d3673f7d98e8c5f
michaelin007 <michaelolayemi28@gmail.com>
2023-12-18 04:59:42 +0000

    https://jira.baeldung.com/browse/BAEL-7333

---
+++ core-java-modules/core-java-8-datetime-2/src/main/java/com/baeldung/monthintervalbetweentwodates/MonthInterval.java
@@ -0,0 +1,37 @@
+package com.baeldung.monthintervalbetweentwodates;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class MonthInterval {
+
+    public int monthsBetween(Date startDate, Date endDate) {
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("Both startDate and endDate must be provided");
+        }
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(startDate);
+        int startDateTotalMonths = 12 * startCalendar.get(Calendar.YEAR) + startCalendar.get(Calendar.MONTH);
+        Calendar endCalendar = Calendar.getInstance();
+        endCalendar.setTime(endDate);
+        int endDateTotalMonths = 12 * endCalendar.get(Calendar.YEAR) + endCalendar.get(Calendar.MONTH);
+        return endDateTotalMonths - startDateTotalMonths;
+    }
+
+    public int monthsBetweenWithDayValue(Date startDate, Date endDate) {
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("Both startDate and endDate must be provided");
+        }
+        Calendar startCalendar = Calendar.getInstance();
+        startCalendar.setTime(startDate);
+        int startDayOfMonth = startCalendar.get(Calendar.DAY_OF_MONTH);
+        int startDateTotalMonths = 12 * startCalendar.get(Calendar.YEAR) + startCalendar.get(Calendar.MONTH);
+        Calendar endCalendar = Calendar.getInstance();
+        endCalendar.setTime(endDate);
+        int endDayOfMonth = endCalendar.get(Calendar.DAY_OF_MONTH);
+        int endDateTotalMonths = 12 * endCalendar.get(Calendar.YEAR) + endCalendar.get(Calendar.MONTH);
+
+        return (startDayOfMonth > endDayOfMonth) ? (endDateTotalMonths - startDateTotalMonths) - 1 : (endDateTotalMonths - startDateTotalMonths);
+    }
+
+}
