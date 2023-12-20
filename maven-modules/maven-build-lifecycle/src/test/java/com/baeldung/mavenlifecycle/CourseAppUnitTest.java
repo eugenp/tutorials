ae4c534c5282e6e60d285422032d9160de9c3ace
shahulbasha <shhl.basha333@gmail.com>
2023-12-19 19:48:07 -0500

    BAEL-7206 removed mocks as per review

--- maven-modules/maven-build-lifecycle/src/test/java/com/baeldung/mavenlifecycle/CourseAppUnitTest.java
+++ maven-modules/maven-build-lifecycle/src/test/java/com/baeldung/mavenlifecycle/CourseAppUnitTest.java
@@ -1,22 +1,15 @@
 package com.baeldung.mavenlifecycle;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
-@ExtendWith(MockitoExtension.class)
+
 class CourseAppUnitTest {
-
-    @Mock
-    CourseApp courseApp;
 
     @Test
     void whenGetCourse_ThenCourseShouldBePresent() {
-        when(courseApp.getCourse()).thenReturn("Baeldung Spring Masterclass");
+        CourseApp courseApp = new CourseApp();
 
         assertEquals("Baeldung Spring Masterclass", courseApp.getCourse());
     }
