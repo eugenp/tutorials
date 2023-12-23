5dcfd12346ac35aff34bd52a135bbe53851bde42
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2023-12-23 13:00:19 +0530

    BAEL-7319, Introduction to Apache Calcite

---
+++ persistence-modules/java-calcite/src/test/java/com/baeldung/calcite/CalciteUnitTest.java
@@ -0,0 +1,48 @@
+package com.baeldung.calcite;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalciteUnitTest {
+
+    Logger logger = LoggerFactory.getLogger(CalciteUnitTest.class);
+    @Test
+    void whenCsvSchema_thenQuerySuccess() throws SQLException {
+        Properties info = new Properties();
+        info.put("model", getPath("model.json"));
+        try(Connection connection = DriverManager.getConnection("jdbc:calcite:", info)) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from trades.trade");
+
+            assertEquals(3, resultSet.getMetaData().getColumnCount());
+
+            List<Integer> tradeIds = new ArrayList<>();
+            while(resultSet.next()) {
+                tradeIds.add(resultSet.getInt("tradeid"));
+            }
+
+            assertEquals(3, tradeIds.size());
+        }
+    }
+
+    private String getPath(String model) {
+        URL url = ClassLoader.getSystemClassLoader().getResource(model);
+        logger.info("path fetched :" + url.getPath());
+        return url.getPath();
+    }
+
+
+}
