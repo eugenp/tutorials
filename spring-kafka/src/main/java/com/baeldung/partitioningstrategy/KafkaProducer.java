c3b97034ed99705a982490d476a90bd739696291
SGWebFreelancer <sgwebfreelancer@gmail.com>
2023-12-24 09:43:23 +0800

    First version

---
+++ spring-kafka/src/main/java/com/baeldung/partitioningstrategy/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.baeldung.partitioningstrategy;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void send(String topic, String key, String message) {
+        kafkaTemplate.send(topic, key, message);
+    }
+
+    public void send(String topic,  String message) {
+        kafkaTemplate.send(topic, message);
+    }
+}
