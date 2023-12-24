a5b9e8fde25e6025d74daa19e58a0a434f9399f7
timis1 <12120641+timis1@users.noreply.github.com>
2023-12-24 17:07:57 +0200

    JAVA-29329 Upgrade spring-security-web-rest-basic-auth (#15457)
    Co-authored-by: timis1 <noreplay@yahoo.com>

--- spring-security-modules/spring-security-web-rest-basic-auth/src/main/java/com/baeldung/basic/MyBasicAuthenticationEntryPoint.java
+++ spring-security-modules/spring-security-web-rest-basic-auth/src/main/java/com/baeldung/basic/MyBasicAuthenticationEntryPoint.java
@@ -4,9 +4,8 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
