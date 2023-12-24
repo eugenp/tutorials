750cc9b7486e32350e8bfe14da15cc888cc7c461
Gaetano Piazzolla <gae.piaz@gmail.com>
2023-12-24 15:02:39 +0100

    JAVA-29283 | added missing code for article (#15459)

--- persistence-modules/jooq/src/test/java/com/baeldung/jooq/CrudIntegrationTest.java
+++ persistence-modules/jooq/src/test/java/com/baeldung/jooq/CrudIntegrationTest.java
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
-public class CrudLiveTest {
+public class CrudIntegrationTest {
 
     static DSLContext context;
 
