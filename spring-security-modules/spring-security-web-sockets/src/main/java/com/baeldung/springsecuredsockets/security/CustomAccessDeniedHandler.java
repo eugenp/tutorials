37bddcdfced3c7ea09c45ec3287c68ff6462ada7
timis1 <12120641+timis1@users.noreply.github.com>
2023-12-24 15:06:05 +0200

    JAVA-29331 Upgrade spring-security-web-sockets (#15468)
    Co-authored-by: timis1 <noreplay@yahoo.com>

--- spring-security-modules/spring-security-web-sockets/src/main/java/com/baeldung/springsecuredsockets/security/CustomAccessDeniedHandler.java
+++ spring-security-modules/spring-security-web-sockets/src/main/java/com/baeldung/springsecuredsockets/security/CustomAccessDeniedHandler.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
