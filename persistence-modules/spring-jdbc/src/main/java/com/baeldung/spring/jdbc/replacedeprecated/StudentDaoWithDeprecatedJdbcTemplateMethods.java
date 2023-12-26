92bcd1f6eed485743af2464a153b6ab71dcbd13d
Amit Pandey <amit.pandey2103@gmail.com>
2023-12-26 17:59:02 +0530

    Revert StudentDaoWithDeprecatedJdbcTemplateMethods as its purpose is to demo deprecated methods.

--- persistence-modules/spring-jdbc/src/main/java/com/baeldung/spring/jdbc/replacedeprecated/StudentDaoWithDeprecatedJdbcTemplateMethods.java
+++ persistence-modules/spring-jdbc/src/main/java/com/baeldung/spring/jdbc/replacedeprecated/StudentDaoWithDeprecatedJdbcTemplateMethods.java
@@ -21,28 +21,34 @@ public class StudentDaoWithDeprecatedJdbcTemplateMethods {
 
     public List<Student> getStudentsOfAgeAndGender(Integer age, String gender) {
         String sql = "select student_id, student_name, age, gender, grade, state from student where age= ? and gender = ?";
-        return jdbcTemplate.query(sql, new StudentRowMapper(), age, gender);
+        Object[] args = {age, gender};
+        return jdbcTemplate.query(sql, args, new StudentRowMapper());
     }
 
     public List<Student> getStudentsOfAgeGenderAndGrade(Integer age, String gender, Integer grade) {
         String sql = "select student_id, student_name, age, gender, grade, state from student where age= ? and gender = ? and grade = ?";
-        return jdbcTemplate.query(sql, new StudentRowMapper(), age, gender, grade);
+        Object[] args = {age, gender, grade};
+        return jdbcTemplate.query(sql, args, new StudentRowMapper());
     }
 
     public List<Student> getStudentsOfGradeAndState(Integer grade, String state) {
         String sql = "select student_id, student_name, age, gender, grade, state from student where grade = ? and state = ?";
-        return jdbcTemplate.query(sql, new StudentResultExtractor(), grade, state);
+        Object[] args = {grade, state};
+        return jdbcTemplate.query(sql, args, new StudentResultExtractor());
     }
 
     public Student getStudentOfStudentIDAndGrade(Integer studentID, Integer grade) {
         String sql = "select student_id, student_name, age, gender, grade, state from student where student_id = ? and grade = ?";
-        return jdbcTemplate.queryForObject(sql,new StudentRowMapper(), studentID, grade);
+        Object[] args = {studentID, grade};
+
+        return jdbcTemplate.queryForObject(sql, args, new StudentRowMapper());
     }
 
     public Integer getCountOfStudentsInAGradeFromAState(Integer grade, String state) {
         String sql = "select student_id, student_name, age, gender, grade, state from student where grade = ? and state = ?";
+        Object[] args = {grade, state};
         RowCountCallbackHandler countCallbackHandler = new RowCountCallbackHandler();
-        jdbcTemplate.query(sql, countCallbackHandler, grade, state);
+        jdbcTemplate.query(sql, args, countCallbackHandler);
         return countCallbackHandler.getRowCount();
     }
 }
