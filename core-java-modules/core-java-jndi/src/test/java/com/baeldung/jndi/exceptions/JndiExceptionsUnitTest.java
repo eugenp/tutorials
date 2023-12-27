09a5bea86fe649a3500ce075f802d1361495ff47
panos-kakos <kakos.programmer@gmail.com>
2023-12-27 19:01:47 +0200

    [JAVA-27683]

--- core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/exceptions/JndiExceptionsUnitTest.java
+++ core-java-modules/core-java-jndi/src/test/java/com/baeldung/jndi/exceptions/JndiExceptionsUnitTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.jndi.JndiTemplate;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.mock.jndi.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JndiExceptionsUnitTest {
