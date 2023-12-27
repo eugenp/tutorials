622411f560b4dfc073ea3005cdb67fcd3473409e
panos-kakos <kakos.programmer@gmail.com>
2023-12-27 10:28:57 +0200

    [JAVA-29168]

--- spring-boot-modules/spring-boot-basic-customization-2/src/main/java/com/baeldung/dispatchservlet/servlet/CustomServlet.java
+++ spring-boot-modules/spring-boot-basic-customization-2/src/main/java/com/baeldung/dispatchservlet/servlet/CustomServlet.java
@@ -1,14 +1,12 @@
 package com.baeldung.dispatchservlet.servlet;
 
-import com.baeldung.dispatchservlet.filter.CustomFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class CustomServlet extends HttpServlet {
