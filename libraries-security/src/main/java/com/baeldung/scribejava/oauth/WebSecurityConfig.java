d267a008e606b2f3f58578fb42e504eee3e0182e
gaepi <gae.piaz@gmail.com>
2023-12-27 12:47:25 +0100

    JAVA-28925 | sec-libraries to boot3

--- libraries-security/src/main/java/com/baeldung/scribejava/oauth/WebSecurityConfig.java
+++ libraries-security/src/main/java/com/baeldung/scribejava/oauth/WebSecurityConfig.java
@@ -2,39 +2,36 @@ package com.baeldung.scribejava.oauth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableResourceServer
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .headers().frameOptions().disable()
-            .and()
-            .csrf().disable();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("baeldung")
-                .password("scribejava")
-                .roles("USER");
-    }
-
-    @Override
     @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
+    public SecurityFilterChain securityFilter(HttpSecurity http) throws Exception {
+        http.headers( it -> it.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+            .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withUsername("baeldung")
+            .password("scribejava")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 
     @EnableResourceServer
     @Configuration
@@ -42,11 +39,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http
-                .authorizeRequests()
-                .antMatchers("/user/me").authenticated()
-                .and()
-                .csrf().disable();
+            http.authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/user/me")
+                        .authenticated();
+                }
+            );
+            http.csrf(AbstractHttpConfigurer::disable);
         }
     }
 
