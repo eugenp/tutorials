cef792bc1094da3da4eba6e86338ff8c40e01b6b
Eugene Kovko <eu.kovko@gmail.com>
2023-12-28 16:53:14 +0100

    BAEL-7212: Added negative tests

--- spring-boot-modules/spring-boot-properties-4/src/test/java/com/baeldung/caseinsensitiveenum/StrictStringToEnumConverterNegativeUnitTest.java
+++ spring-boot-modules/spring-boot-properties-4/src/test/java/com/baeldung/caseinsensitiveenum/StrictStringToEnumConverterNegativeUnitTest.java
@@ -2,7 +2,7 @@ package com.baeldung.caseinsensitiveenum;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import com.baeldung.caseinsensitiveenum.StrictStringToEnumConverterUnitTest.WeekDayConverterConfiguration;
+import com.baeldung.caseinsensitiveenum.StrictStringToEnumConverterNegativeUnitTest.WeekDayConverterConfiguration;
 import com.baeldung.caseinsensitiveenum.converter.StrictNullableWeekDayConverter;
 import com.baeldung.caseinsensitiveenum.week.WeekDays;
 import com.baeldung.caseinsensitiveenum.week.WeekDaysHolder;
@@ -24,7 +24,7 @@ import org.springframework.core.convert.support.DefaultConversionService;
     "saturday=saturday",
     "sunday=sunday",
 }, classes = {WeekDaysHolder.class, WeekDayConverterConfiguration.class})
-class StrictStringToEnumConverterUnitTest {
+class StrictStringToEnumConverterNegativeUnitTest {
 
     public static class WeekDayConverterConfiguration {
         @Bean
---
+++ spring-boot-modules/spring-boot-properties-4/src/test/java/com/baeldung/caseinsensitiveenum/StrictStringToEnumConverterPositiveUnitTest.java
@@ -0,0 +1,48 @@
+package com.baeldung.caseinsensitiveenum;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.baeldung.caseinsensitiveenum.StrictStringToEnumConverterNegativeUnitTest.WeekDayConverterConfiguration;
+import com.baeldung.caseinsensitiveenum.converter.StrictNullableWeekDayConverter;
+import com.baeldung.caseinsensitiveenum.week.WeekDays;
+import com.baeldung.caseinsensitiveenum.week.WeekDaysHolder;
+import java.util.function.Function;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+@SpringBootTest(properties = {
+    "monday=MONDAY",
+    "tuesday=TUESDAY",
+    "wednesday=WEDNESDAY",
+    "thursday=THURSDAY",
+    "friday=FRIDAY",
+    "saturday=SATURDAY",
+    "sunday=SUNDAY",
+}, classes = {WeekDaysHolder.class, WeekDayConverterConfiguration.class})
+class StrictStringToEnumConverterPositiveUnitTest {
+
+    public static class WeekDayConverterConfiguration {
+        @Bean
+        public ConversionService conversionService() {
+            final DefaultConversionService defaultConversionService = new DefaultConversionService();
+            defaultConversionService.addConverter(new StrictNullableWeekDayConverter());
+            return defaultConversionService;
+        }
+    }
+
+    @Autowired
+    private WeekDaysHolder lenientHolder;
+
+    @ParameterizedTest
+    @ArgumentsSource(WeekDayHolderArgumentsProvider.class)
+    void givenPropertiesWhenInjectEnumThenValueIsNull(
+        Function<WeekDaysHolder, WeekDays> methodReference, WeekDays expected) {
+        WeekDays actual = methodReference.apply(lenientHolder);
+        assertThat(actual).isEqualTo(expected);
+    }
+}
