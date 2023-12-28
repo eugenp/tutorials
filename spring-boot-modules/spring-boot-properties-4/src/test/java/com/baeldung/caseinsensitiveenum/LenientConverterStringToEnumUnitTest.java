9c3ff8708d0a5ed1e5933b8cd13b0127520b6ad0
Eugene Kovko <eu.kovko@gmail.com>
2023-12-28 13:08:33 +0100

    BAEL-7212: Include only one holder

--- spring-boot-modules/spring-boot-properties-4/src/test/java/com/baeldung/caseinsensitiveenum/LenientConverterStringToEnumUnitTest.java
+++ spring-boot-modules/spring-boot-properties-4/src/test/java/com/baeldung/caseinsensitiveenum/LenientConverterStringToEnumUnitTest.java
@@ -1,20 +1,10 @@
 package com.baeldung.caseinsensitiveenum;
 
-import static com.baeldung.caseinsensitiveenum.WeekDays.FRIDAY;
-import static com.baeldung.caseinsensitiveenum.WeekDays.MONDAY;
-import static com.baeldung.caseinsensitiveenum.WeekDays.SATURDAY;
-import static com.baeldung.caseinsensitiveenum.WeekDays.SUNDAY;
-import static com.baeldung.caseinsensitiveenum.WeekDays.THURSDAY;
-import static com.baeldung.caseinsensitiveenum.WeekDays.TUESDAY;
-import static com.baeldung.caseinsensitiveenum.WeekDays.WEDNESDAY;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.function.Function;
-import java.util.stream.Stream;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -26,7 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
     "lenient.friday=Fri:Day_%",
     "lenient.saturday=Satur_DAY*",
     "lenient.sunday=Sun+Day",
-})
+}, classes = LenientWeekDaysHolder.class)
 class LenientConverterStringToEnumUnitTest {
 
     @Autowired
