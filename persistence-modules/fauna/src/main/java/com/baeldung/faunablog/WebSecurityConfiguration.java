f9c99f2de2859672b3d263c98ce2418d5e1b7868
Amit Pandey <amit.pandey2103@gmail.com>
2023-12-30 00:22:56 +0530

    Made changes to upgrade to Spring Boot 3

--- persistence-modules/fauna/src/main/java/com/baeldung/faunablog/WebSecurityConfiguration.java
+++ persistence-modules/fauna/src/main/java/com/baeldung/faunablog/WebSecurityConfiguration.java
@@ -5,9 +5,11 @@ import com.faunadb.client.FaunaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -21,13 +23,11 @@ public class WebSecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-            .disable();
-        http.authorizeRequests()
-            .antMatchers("/**")
-            .permitAll()
-            .and()
-            .httpBasic();
+        http.csrf(CsrfConfigurer::disable)
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
