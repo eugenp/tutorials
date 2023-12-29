d19f5c7aa3211076b63ddf5a6608219983057885
Eugene Kovko <eu.kovko@gmail.com>
2023-12-29 14:43:18 +0100

    BAEL-7066: Extract a base test class

--- spring-boot-modules/spring-boot-data-3/src/test/java/com/baeldung/jsonignore/AbstractEmployeeEchoControllerBaseTest.java
+++ spring-boot-modules/spring-boot-data-3/src/test/java/com/baeldung/jsonignore/AbstractEmployeeEchoControllerBaseTest.java
@@ -24,68 +24,41 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 @WebMvcTest(EmployeeEchoController.class)
-class EmployeeEchoControllerIntegrationTest {
+abstract class AbstractEmployeeEchoControllerBaseTest {
 
-    private static final ObjectMapper JSON_MAPPER = new ObjectMapper();
+    protected static final ObjectMapper JSON_MAPPER = new ObjectMapper();
 
     @Autowired
-    private MockMvc mockMvc;
+    protected MockMvc mockMvc;
 
-    @ParameterizedTest
-    @CsvSource(value = {
-        "1,John,Doe",
-        "1,,Doe",
-        "1,John,",
-        "1,,"
-    })
-    void giveEndpointWhenSendEmployeeThanReceiveThatUserBackIgnoringNullValues(
-        final long id, final String firstName, final String secondName) throws Exception {
-        final Employee expected = new Employee(id, firstName, secondName);
-        List<String> nullFields = getNullFields(expected);
-        List<String> nonNullFields = getNonNullFields(expected);
-        final String payload = JSON_MAPPER.writeValueAsString(expected);
-        final MvcResult result = mockMvc.perform(post(USERS)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
-                .content(payload))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
 
-        final String response = result.getResponse().getContentAsString();
-        final JsonNode jsonNode = JSON_MAPPER.readTree(response);
-
-        nullFieldsShouldBeMissing(nullFields, jsonNode);
-        nonNullFieldsShouldNonBeMissing(nonNullFields, jsonNode);
-    }
-
-    private static void nonNullFieldsShouldNonBeMissing(final List<String> nonNullFields, final JsonNode jsonNode) {
+    protected static void nonNullFieldsShouldNonBeMissing(final List<String> nonNullFields, final JsonNode jsonNode) {
         nonNullFields.forEach(nullField -> {
             final JsonNode nameNode = jsonNode.path(nullField);
             assertThat(nameNode.isMissingNode()).isFalse();
         });
     }
 
-    private static void nullFieldsShouldBeMissing(final List<String> nullFields, final JsonNode jsonNode) {
+    protected static void nullFieldsShouldBeMissing(final List<String> nullFields, final JsonNode jsonNode) {
         nullFields.forEach(nullField -> {
             final JsonNode nameNode = jsonNode.path(nullField);
             assertThat(nameNode.isMissingNode()).isTrue();
         });
     }
 
-    private static <T> List<String> getNullFields(final T object) {
+    protected static <T> List<String> getNullFields(final T object) {
         Predicate<Field> predicate = s -> isFieldNull(object, s);
         return ReflectionUtils.findFields(object.getClass(), predicate, HierarchyTraversalMode.BOTTOM_UP)
             .stream().map(Field::getName).collect(Collectors.toList());
     }
 
-    private static <T> List<String> getNonNullFields(final T object) {
+    protected static <T> List<String> getNonNullFields(final T object) {
         Predicate<Field> predicate = s -> !isFieldNull(object, s);
         return ReflectionUtils.findFields(object.getClass(), predicate, HierarchyTraversalMode.BOTTOM_UP)
             .stream().map(Field::getName).collect(Collectors.toList());
     }
 
-    private static <T> boolean isFieldNull(final T object, final Field s) {
+    protected static <T> boolean isFieldNull(final T object, final Field s) {
         try {
             if (Object.class.isAssignableFrom(s.getType())) {
                 s.setAccessible(true);
---
+++ spring-boot-modules/spring-boot-data-3/src/test/java/com/baeldung/jsonignore/NonNullEmployeeFieldsEchoControllerIntegrationTest.java
@@ -0,0 +1,47 @@
+package com.baeldung.jsonignore;
+
+import static com.baeldung.jsonignore.EmployeeEchoController.USERS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.baeldung.jsonignore.nullfields.Employee;
+import com.fasterxml.jackson.databind.JsonNode;
+import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+
+class NonNullEmployeeFieldsEchoControllerIntegrationTest extends AbstractEmployeeEchoControllerBaseTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "1,John,Doe",
+        "1,,Doe",
+        "1,John,",
+        "1,,"
+    })
+    void giveEndpointWhenSendEmployeeThanReceiveThatUserBackIgnoringNullValues(
+        final long id, final String firstName, final String secondName) throws Exception {
+        final Employee expected = new Employee(id, firstName, secondName);
+        List<String> nullFields = getNullFields(expected);
+        List<String> nonNullFields = getNonNullFields(expected);
+        final String payload = JSON_MAPPER.writeValueAsString(expected);
+        final MvcResult result = mockMvc.perform(post(USERS)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(payload))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn();
+
+        final String response = result.getResponse().getContentAsString();
+        final JsonNode jsonNode = JSON_MAPPER.readTree(response);
+
+        nullFieldsShouldBeMissing(nullFields, jsonNode);
+        nonNullFieldsShouldNonBeMissing(nonNullFields, jsonNode);
+    }
+
+}
