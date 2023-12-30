b61f9d07990b245fcd8b8ded13c17fb7f860c42a
Kai Yuan <kent.yuan@gmail.com>
2023-12-30 15:08:35 +0100

    [testBuild] test build

---
+++ core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/foo/FooUnitTest.java
@@ -0,0 +1,13 @@
+package com.baeldung.foo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FooUnitTest {
+    @Test
+    void whenRunThisTest_thenPass() {
+        assertTrue(true);
+    }
+
+}
