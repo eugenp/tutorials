164ef673259867206792299197a1250e66a96649
Oscar Mauricio Forero Carrillo <o.forero@icloud.com>
2023-12-30 09:42:21 +0100

    Bael 7317 (#15415)
    * BAEL-7226: Code for the improvements of the Percentages article.
    
    * BAEL-7226: Rename Test to follow the convention
    
    * BAEL-7317: Update the code to the latest versions of Spock, Groovy and the maven matching configuration
    
    * BAEL-7317: Add code for the article improvements

---
+++ testing-modules/groovy-spock/src/main/java/mocks/Book.java
@@ -0,0 +1,11 @@
+package mocks;
+
+public class Book {
+    private String title;
+    private String author;
+
+    Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+}
