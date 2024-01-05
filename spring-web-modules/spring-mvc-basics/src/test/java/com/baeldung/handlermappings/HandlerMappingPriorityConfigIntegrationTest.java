641f09e287eaab61cbc2799cc4bd57eacbddd853
Amit Pandey <amit.pandey2103@gmail.com>
2024-01-05 23:29:53 +0530

    JAVA-29172 :- Upgrade spring-mvc-basics to Spring Boot 3

--- spring-web-modules/spring-mvc-basics/src/test/java/com/baeldung/handlermappings/HandlerMappingPriorityConfigIntegrationTest.java
+++ spring-web-modules/spring-mvc-basics/src/test/java/com/baeldung/handlermappings/HandlerMappingPriorityConfigIntegrationTest.java
@@ -29,7 +29,7 @@ public class HandlerMappingPriorityConfigIntegrationTest {
 
     @Before
     public void setup() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
         mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
     }
 
