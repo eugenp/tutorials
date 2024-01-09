9c68a7b62cca92a2f423e9fc13518a6cee0c5a96
SGWebFreelancer <sgwebfreelancer@gmail.com>
2024-01-09 11:28:54 +0800

    First draft of priorityqueue iterator

--- core-java-modules/core-java-collections-5/src/main/java/com/baeldung/priorityqueueiterator/Person.java
+++ core-java-modules/core-java-collections-5/src/main/java/com/baeldung/priorityqueueiterator/Person.java
@@ -10,6 +10,22 @@ public class Person {
         this.name = name;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
 
 class PersonAgeComparator implements Comparator<Person> {
--- core-java-modules/core-java-collections-5/src/main/java/com/baeldung/priorityqueueiterator/PriorityQueueMain.java
+++ core-java-modules/core-java-collections-5/src/main/java/com/baeldung/priorityqueueiterator/PriorityQueueMain.java
@@ -1,40 +1,20 @@
 package com.baeldung.priorityqueueiterator;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
 public class PriorityQueueMain {
 
     public static void main(String[] args) {
-
-        // Creating a PriorityQueue with a custom comparator
-        PriorityQueue<Person> priorityQueue = new PriorityQueue<>(new PersonAgeComparator());
-
-        // Adding persons to the PriorityQueue
-        priorityQueue.add(new Person("Alice", 25));
-        priorityQueue.add(new Person("Bob", 30));
-        priorityQueue.add(new Person("Charlie", 22));
-
-        // Getting an iterator
-        Iterator<Person> iterator = priorityQueue.iterator();
-
-        // Iterating over the elements using the iterator
-        System.out.println("PriorityQueue elements using custom comparator:");
-        while (iterator.hasNext()) {
-            Person person = iterator.next();
-            System.out.println("Name: " + person.name + ", Age: " + person.age);
-        }
-//        System.out.println("People in ascending order of age:");
-//        while (!priorityQueue.isEmpty()) {
-//            Person person = priorityQueue.poll(); // Retrieves and removes the first (lowest-age) person
-//            System.out.println("Name: " + person.name + ", Age: " + person.age);
-//        }
+        showPriorityQueueWithIterator();
+        showCustomOrderingWithIterator();
+        showCustomOrderingWithPoll();
+        useIteratorToCapitalizeNames();
+        showFailFastIteratorBehavior();
     }
 
-    public static void iterateAndPrintPriorityQueue() {
+    public static void showPriorityQueueWithIterator() {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-
         queue.add(3);
         queue.add(1);
         queue.add(2);
@@ -44,6 +24,69 @@ public class PriorityQueueMain {
         while (iterator.hasNext()) {
             Integer element = iterator.next();
             System.out.println(element);
+        }
+    }
+
+    public static void showFailFastIteratorBehavior() {
+        PriorityQueue<Integer> numbers = new PriorityQueue<>();
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(2);
+
+        Iterator<Integer> iterator = numbers.iterator();
+        numbers.add(4);
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    public static void showCustomOrderingWithIterator() {
+        // Creating a PriorityQueue with a custom comparator
+        PriorityQueue<Person> priorityQueue = new PriorityQueue<>(new PersonAgeComparator());
+
+        // Adding persons to the PriorityQueue
+        priorityQueue.add(new Person("Alice", 25));
+        priorityQueue.add(new Person("Bob", 30));
+        priorityQueue.add(new Person("Charlie", 22));
+
+        Iterator<Person> iterator = priorityQueue.iterator();git ad
+
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            System.out.println("Name: " + person.name + ", Age: " + person.age);
+        }
+    }
+
+    public static void showCustomOrderingWithPoll() {
+        PriorityQueue<Person> priorityQueue = new PriorityQueue<>(new PersonAgeComparator());
+
+        priorityQueue.add(new Person("Alice", 25));
+        priorityQueue.add(new Person("Bob", 30));
+        priorityQueue.add(new Person("Charlie", 22));
+
+        Iterator<Person> iterator = priorityQueue.iterator();
+
+        while (!priorityQueue.isEmpty()) {
+            Person person = priorityQueue.poll();
+            System.out.println("Name: " + person.name + ", Age: " + person.age);
+        }
+    }
+
+    public static void useIteratorToCapitalizeNames() {
+        PriorityQueue<Person> priorityQueue = new PriorityQueue<>(new PersonAgeComparator());
+
+        priorityQueue.add(new Person("Alice", 25));
+        priorityQueue.add(new Person("Bob", 30));
+        priorityQueue.add(new Person("Charlie", 22));
+
+        Iterator<Person> iterator = priorityQueue.iterator();
+
+        while (iterator.hasNext()) {
+            Person person = iterator.next();
+            person.setName(person.getName()
+                .toUpperCase());
+            System.out.println("Name: " + person.name + ", Age: " + person.age);
         }
     }
 
