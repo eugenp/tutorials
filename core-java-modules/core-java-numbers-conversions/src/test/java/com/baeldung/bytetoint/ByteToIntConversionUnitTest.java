7c7cd2b4707e4c5e77a7721b00bfab8d68c58234
anujgaud <146576725+anujgaud@users.noreply.github.com>
2024-01-10 19:29:50 +0530

    Add unit tests for Byte to Int Conversion

---
+++ core-java-modules/core-java-numbers-conversions/src/test/java/com/baeldung/bytetoint/ByteToIntConversionUnitTest.java
@@ -0,0 +1,49 @@
+package com.baeldung.bytetoint;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ByteToIntConversionUnitTest {
+    @Test
+    void givenByte_whenUsingTypeCasting_thenConvertToInt() {
+        byte b = -51;
+        int result = ByteToIntConversion.usingTypeCasting(b);
+        assertEquals(-51, result);
+    }
+
+    @Test
+    void givenByte_whenUsingIntegerValueOf_thenConvertToInt() {
+        byte b = -51;
+        int result = ByteToIntConversion.usingIntegerValueOf(b);
+        assertEquals(-51, result);
+    }
+
+    @Test
+    void givenByte_whenUsingByteIntValue_thenConvertToInt() {
+        byte b = -51;
+        int result = ByteToIntConversion.usingByteIntValue(b);
+        assertEquals(-51, result);
+    }
+
+    @Test
+    void givenByte_whenUsingMathToIntExact_thenConvertToInt() {
+        byte b = -51;
+        int result = ByteToIntConversion.usingMathToIntExact(b);
+        assertEquals(-51, result);
+    }
+
+    @Test
+    void givenByte_whenUsingBitwiseOperator_thenConvertToInt() {
+        byte b = -51;
+        int result = ByteToIntConversion.usingBitwiseOperator(b);
+        assertEquals(-51, result);
+    }
+
+    @Test
+    void givenByte_whenUsingByteUnsignedInt_thenConvertToInt() {
+        byte b = -51;
+        int result = ByteToIntConversion.usingByteUnsignedInt(b);
+        assertEquals(205, result);
+    }
+}
