c9dced0ec51dac4b154371574d69cbfa1218a1f9
Amit Pandey <amit.pandey2103@gmail.com>
2024-01-12 15:56:52 +0530

    JAVA-28961 :- Fix semantics of hasLength check to mimic isBlank

--- spring-boot-modules/spring-boot-data-2/src/main/java/com/baeldung/propertyeditor/creditcard/CreditCardEditor.java
+++ spring-boot-modules/spring-boot-data-2/src/main/java/com/baeldung/propertyeditor/creditcard/CreditCardEditor.java
@@ -15,7 +15,7 @@ public class CreditCardEditor extends PropertyEditorSupport {
     
     @Override
     public void setAsText(String text) throws IllegalArgumentException {
-        if (StringUtils.hasLength(text)) {
+        if (!StringUtils.hasLength(text)) {
             setValue(null);
         } else {
             CreditCard creditCard = new CreditCard();
