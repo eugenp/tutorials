17fd1e35cc70a9b142506ef9e6c3bfd8a5a915cd
Eugene Kovko <eu.kovko@gmail.com>
2024-01-16 15:31:03 +0100

    BAEL-6887: Remove lazy examples

--- persistence-modules/spring-boot-persistence-4/src/main/java/com/baeldung/listvsset/lazy/list/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.listvsset.lazy.list;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Long> {
-
-}
