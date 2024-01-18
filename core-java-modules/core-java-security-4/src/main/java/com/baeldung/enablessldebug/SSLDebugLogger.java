4b5a9dfbad59d1f9bf0b787ffba0bc83bec80dfd
anujgaud <146576725+anujgaud@users.noreply.github.com>
2024-01-18 22:13:33 +0530

    Add code for enabling SSL Debug logs

---
+++ core-java-modules/core-java-security-4/src/main/java/com/baeldung/enablessldebug/SSLDebugLogger.java
@@ -0,0 +1,32 @@
+package com.baeldung.enablessldebug;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SSLDebugLogger {
+    private static final Logger logger = LoggerFactory.getLogger(SSLDebugLogger.class);
+
+    public static void enableSSLDebugUsingSystemProperties() {
+        System.setProperty("javax.net.debug", "ssl");
+    }
+
+    public static void makeHttpsRequest() throws Exception {
+        String url = "https://github.com/eugenp/tutorials";
+        URL httpsUrl = new URL(url);
+        HttpsURLConnection connection = (HttpsURLConnection) httpsUrl.openConnection();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            String line;
+            logger.info("Response from " + url + ":");
+            while ((line = reader.readLine()) != null) {
+                logger.info(line);
+            }
+        }
+    }
+}
