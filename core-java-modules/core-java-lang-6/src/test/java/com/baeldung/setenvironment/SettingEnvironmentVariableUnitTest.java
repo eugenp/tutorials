b5208092b6917f275e78ed1937026d8b332c2bb0
Eugene Kovko <eu.kovko@gmail.com>
2024-01-19 12:22:44 +0100

    feat: Simple test for PATH env variable

--- core-java-modules/core-java-lang-6/src/test/java/com/baeldung/setenvironment/SettingEnvironmentVariableUnitTest.java
+++ core-java-modules/core-java-lang-6/src/test/java/com/baeldung/setenvironment/SettingEnvironmentVariableUnitTest.java
@@ -8,6 +8,12 @@ import org.junitpioneer.jupiter.SetEnvironmentVariable;
 class SettingEnvironmentVariableUnitTest {
 
     @Test
+    void givenOS_whenGetPath_thenVariableIsPresent() {
+        String classPath = System.getenv("PATH");
+        assertThat(classPath).isNotNull();
+    }
+
+    @Test
     @SetEnvironmentVariable(key = "test", value = "Hello World")
     void givenVariableSet_whenGetenv_thenGetCorrectValue() {
         String expected = "Hello World";
