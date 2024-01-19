3e7419667a5887de1e0bcd9e621dd057a9073fa8
Eugene Kovko <eu.kovko@gmail.com>
2024-01-19 12:49:36 +0100

    feat: Guardrails for an environment test

--- core-java-modules/core-java-lang-6/src/test/java/com/baeldung/setenvironment/SettingEnvironmentVariableUnitTest.java
+++ core-java-modules/core-java-lang-6/src/test/java/com/baeldung/setenvironment/SettingEnvironmentVariableUnitTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
 class SettingEnvironmentVariableUnitTest {
 
     @Test
+    @EnabledIfEnvironmentVariable(named = "PATH", matches = "*",
+      disabledReason = "The test relies on the presence of PATH variable")
     void givenOS_whenGetPath_thenVariableIsPresent() {
         String classPath = System.getenv("PATH");
         assertThat(classPath).isNotNull();
