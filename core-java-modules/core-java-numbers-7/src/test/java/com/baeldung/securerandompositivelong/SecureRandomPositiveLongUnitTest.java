b8d8087262b81490b3717304d6cd8020c40105f3
@hangga <bazeniancode@gmail.com>
2024-01-21 13:19:52 +0700

    Double to double

--- core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/securerandompositivelong/SecureRandomPositiveLongUnitTest.java
+++ core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/securerandompositivelong/SecureRandomPositiveLongUnitTest.java
@@ -16,7 +16,7 @@ public class SecureRandomPositiveLongUnitTest {
         assertThat(randomPositiveLong).isPositive();
 
         //Double pc = 1.0 / Math.pow(2, 63);
-        Double pc = 1.0 / Long.MAX_VALUE;
+        double pc = 1.0 / Long.MAX_VALUE;
         System.out.printf("%.40f", pc);
 
         assertThat(pc).isLessThan(0.00000000000000001);
