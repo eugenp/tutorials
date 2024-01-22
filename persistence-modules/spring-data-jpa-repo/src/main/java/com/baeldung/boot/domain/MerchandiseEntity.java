3aa6d757c1506cb11299445f3a0fe4779ff14179
Bipin kumar <sayy2bipin@gmail.com>
2024-01-23 04:16:19 +0530

    [JAVA-28956] Upgrade spring-data-jpa-repo module to Spring Boot 3 (#15665)

--- persistence-modules/spring-data-jpa-repo/src/main/java/com/baeldung/boot/domain/MerchandiseEntity.java
+++ persistence-modules/spring-data-jpa-repo/src/main/java/com/baeldung/boot/domain/MerchandiseEntity.java
@@ -1,10 +1,11 @@
 package com.baeldung.boot.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class MerchandiseEntity {
