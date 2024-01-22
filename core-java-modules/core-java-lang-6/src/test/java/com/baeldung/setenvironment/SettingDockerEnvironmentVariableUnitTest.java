c71289c4f6e2a524b00a20d990af9c6240a90a2a
Eugene Kovko <eu.kovko@gmail.com>
2024-01-22 13:14:09 +0100

    feat: Prevent Docker test from running

--- core-java-modules/core-java-lang-6/src/test/java/com/baeldung/setenvironment/SettingDockerEnvironmentVariableUnitTest.java
+++ core-java-modules/core-java-lang-6/src/test/java/com/baeldung/setenvironment/SettingDockerEnvironmentVariableUnitTest.java
@@ -3,6 +3,7 @@ package com.baeldung.setenvironment;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
 class SettingDockerEnvironmentVariableUnitTest {
 
@@ -10,8 +11,9 @@ class SettingDockerEnvironmentVariableUnitTest {
     public static final String ENV_VARIABLE_VALUE = "CUSTOM_DOCKER_ENV_VARIABLE";
 
     @Test
-   void givenDockerEnvironment_whenGetEnvironmentVariable_thenReturnsCorrectValue() {
+    @EnabledIfEnvironmentVariable(named = ENV_VARIABLE_NAME, matches = ENV_VARIABLE_VALUE)
+    void givenDockerEnvironment_whenGetEnvironmentVariable_thenReturnsCorrectValue() {
         String actual = System.getenv(ENV_VARIABLE_NAME);
-       assertThat(actual).isEqualTo(ENV_VARIABLE_VALUE);
-   }
+        assertThat(actual).isEqualTo(ENV_VARIABLE_VALUE);
+    }
 }
