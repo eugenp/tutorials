fd4fb494262e60307c283cf60dca8bb2411b67e2
@hangga <bazeniancode@gmail.com>
2024-01-22 15:02:12 +0700

    simplify

--- core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/securerandompositivelong/SecureRandomPositiveLongUnitTest.java
+++ core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/securerandompositivelong/SecureRandomPositiveLongUnitTest.java
@@ -14,11 +14,5 @@ public class SecureRandomPositiveLongUnitTest {
         long randomPositiveLong = Math.abs(secureRandom.nextLong());
 
         assertThat(randomPositiveLong).isPositive();
-
-        //Double pc = 1.0 / Math.pow(2, 63);
-        double pc = 1.0 / Long.MAX_VALUE;
-        System.out.printf("%.40f", pc);
-
-        assertThat(pc).isLessThan(0.00000000000000001);
     }
 }
