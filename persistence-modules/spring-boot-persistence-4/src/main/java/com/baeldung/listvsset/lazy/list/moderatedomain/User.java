15de11de9aafafe787ccb01b4001e5f657fada5c
Eugene Kovko <37694937+eukovko@users.noreply.github.com>
2024-01-23 19:23:39 +0100

    Bael 7452 updated (#15714)
    * BAEL-7452: Lazy loaded lists
    
    * BAEL-7452: Example with Lazy Lists
    
    * BAEL-7452: Test for lazy groups

---
+++ persistence-modules/spring-boot-persistence-4/src/main/java/com/baeldung/listvsset/lazy/list/moderatedomain/User.java
@@ -0,0 +1,45 @@
+package com.baeldung.listvsset.lazy.list.moderatedomain;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.Objects;
+import lombok.Data;
+
+@Data
+@Entity(name = "simple_user")
+@Table(name = "simple_user")
+public class User {
+
+    @Id
+    private Long id;
+    private String username;
+    private String email;
+
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "author")
+    protected List<Post> posts;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
