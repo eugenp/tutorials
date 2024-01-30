b21bcae839f2d67fc6d8b2c619775ea4375db30d
kasramp <kasra_mp@yahoo.com>
2024-01-30 09:10:36 +0100

    JAVA-30445 test improvement

--- spring-web-modules/spring-mvc-basics-4/src/test/java/com/baeldung/controller/ControllerAnnotationIntegrationTest.java
+++ spring-web-modules/spring-mvc-basics-4/src/test/java/com/baeldung/controller/ControllerAnnotationIntegrationTest.java
@@ -1,6 +1,8 @@
 package com.baeldung.controller;
 
-import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,50 +30,53 @@ public class ControllerAnnotationIntegrationTest {
 
     private Student selectedStudent;
 
+    private ObjectMapper objectMapper;
+
     @Before
     public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+            .build();
         selectedStudent = new Student();
         selectedStudent.setId(1);
         selectedStudent.setName("Peter");
+        objectMapper = new ObjectMapper();
     }
 
     @Test
     public void testTestController() throws Exception {
-
-        ModelAndView mv = this.mockMvc.perform(MockMvcRequestBuilders.get("/test")).andReturn().getModelAndView();
+        ModelAndView mv = mockMvc.perform(MockMvcRequestBuilders.get("/test"))
+            .andReturn()
+            .getModelAndView();
 
         // validate modal data
-        Assert.assertSame(mv.getModelMap().get("data").toString(), "Welcome home man");
-
+        assertSame(mv.getModelMap()
+            .get("data")
+            .toString(), "Welcome home man");
         // validate view name
-        Assert.assertSame(mv.getViewName(), "welcome");
+        assertSame(mv.getViewName(), "welcome");
     }
 
     @Test
     public void testRestController() throws Exception {
+        String responseBody = mockMvc.perform(MockMvcRequestBuilders.get("/student/{studentId}", 1))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        Student studentDetails = objectMapper.readValue(responseBody, Student.class);
 
-        String responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/student/{studentId}", 1)).andReturn().getResponse().getContentAsString();
-
-        ObjectMapper reader = new ObjectMapper();
-
-        Student studentDetails = reader.readValue(responseBody, Student.class);
-
-        Assert.assertEquals(selectedStudent, studentDetails);
+        assertEquals(selectedStudent, studentDetails);
 
     }
 
     @Test
     public void testRestAnnotatedController() throws Exception {
+        String responseBody = mockMvc.perform(MockMvcRequestBuilders.get("/annotated/student/{studentId}", 1))
+            .andReturn()
+            .getResponse()
+            .getContentAsString();
+        Student studentDetails = objectMapper.readValue(responseBody, Student.class);
 
-        String responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/annotated/student/{studentId}", 1)).andReturn().getResponse().getContentAsString();
-
-        ObjectMapper reader = new ObjectMapper();
-
-        Student studentDetails = reader.readValue(responseBody, Student.class);
-
-        Assert.assertEquals(selectedStudent, studentDetails);
+        assertEquals(selectedStudent, studentDetails);
     }
 
 }
