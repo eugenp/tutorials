306107fcab54ecebd6c47dca92d7b95e68186f79
Amit Pandey <amit2103@users.noreply.github.com>
2024-01-30 19:30:17 +0530

    JAVA-30676 :- Upgrade messagingmodules/rabbitmq to latest Spring Cloud version. (#15750)

--- messaging-modules/rabbitmq/src/test/java/com/baeldung/benchmark/ConnectionPerChannelPublisherLiveTest.java
+++ messaging-modules/rabbitmq/src/test/java/com/baeldung/benchmark/ConnectionPerChannelPublisherLiveTest.java
@@ -2,6 +2,7 @@ package com.baeldung.benchmark;
 
 import java.util.Arrays;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 
 class ConnectionPerChannelPublisherLiveTest {
@@ -9,7 +10,7 @@ class ConnectionPerChannelPublisherLiveTest {
     @Test
     void whenConnectionPerChannel_thenRunBenchmark() throws Exception {
         // host, workerCount, iterations, payloadSize
-        Arrays.asList(1,5,10,20,50,100,150).stream()
+        Stream.of(1,5,10,20,50,100,150)
           .forEach(workers -> {
               ConnectionPerChannelPublisher.main(new String[]{"192.168.99.100", Integer.toString(workers), "1000", "4096"});
           });
