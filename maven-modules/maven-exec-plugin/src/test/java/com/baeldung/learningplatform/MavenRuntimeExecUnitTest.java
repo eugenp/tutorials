f39f138336566bc57a180e5129588788672219ac
Eugene Kovko <eu.kovko@gmail.com>
2024-02-01 19:40:06 +0100

    BAEL-7201: Disable the tests

--- maven-modules/maven-exec-plugin/src/test/java/com/baeldung/learningplatform/MavenRuntimeExecUnitTest.java
+++ maven-modules/maven-exec-plugin/src/test/java/com/baeldung/learningplatform/MavenRuntimeExecUnitTest.java
@@ -9,9 +9,11 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+@Disabled
 class MavenRuntimeExecUnitTest {
 
     @BeforeEach
