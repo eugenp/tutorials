5cd6020adb87dabcbd17fd121e7a62c2a1729a20
Eugene Kovko <eu.kovko@gmail.com>
2024-02-02 12:21:11 +0100

    BAEL-7201: Created MavenAdapter and moved repeated code

--- maven-modules/maven-exec-plugin/src/main/java/com/baeldung/learningplatform/MavenProcessBuilder.java
+++ maven-modules/maven-exec-plugin/src/main/java/com/baeldung/learningplatform/MavenProcessBuilder.java
@@ -3,27 +3,13 @@ package com.baeldung.learningplatform;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public class MavenProcessBuilder implements Maven {
-
+public class MavenProcessBuilder extends MavenExecutorAdapter {
     private static final ProcessBuilder PROCESS_BUILDER = new ProcessBuilder();
 
-    @Override
-    public void compile(Path projectFolder) {
-        int exitCode;
-        try {
-            Process process = PROCESS_BUILDER
-              .command(MVN, USE_CUSTOM_POM, projectFolder.resolve(POM_XML).toString(), COMPILE_GOAL)
-              .start();
-            exitCode = process.waitFor();
-        } catch (InterruptedException e) {
-            throw new MavenCompilationException("Interrupted during compilation", e);
-        } catch (IOException e) {
-            throw new MavenCompilationException("Incorrect execution", e);
-        }
-        if (exitCode != OK) {
-            throw new MavenCompilationException("Failure during compilation: " + exitCode);
-        }
-
-
+    protected int execute(Path projectFolder, String compileGoal) throws IOException, InterruptedException {
+        Process process = PROCESS_BUILDER
+          .command(MVN, USE_CUSTOM_POM, projectFolder.resolve(POM_XML).toString(), compileGoal)
+          .start();
+        return process.waitFor();
     }
 }
--- maven-modules/maven-exec-plugin/src/main/java/com/baeldung/learningplatform/MavenRuntimeExec.java
+++ maven-modules/maven-exec-plugin/src/main/java/com/baeldung/learningplatform/MavenRuntimeExec.java
@@ -3,24 +3,11 @@ package com.baeldung.learningplatform;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public class MavenRuntimeExec implements Maven {
-
-    public static final String MVN_COMPILE = "mvn -f %s compile";
-
+public class MavenRuntimeExec extends MavenExecutorAdapter {
     @Override
-    public void compile(Path projectFolder) {
-        int exitCode;
-        try {
-            String[] arguments = {MVN, USE_CUSTOM_POM, projectFolder.resolve(POM_XML).toString(), COMPILE_GOAL};
-            Process process = Runtime.getRuntime().exec(arguments);
-            exitCode = process.waitFor();
-        } catch (InterruptedException e) {
-            throw new MavenCompilationException("Interrupted during compilation", e);
-        } catch (IOException e) {
-            throw new MavenCompilationException("Incorrect execution", e);
-        }
-        if (exitCode != OK) {
-            throw new MavenCompilationException("Failure during compilation: " + exitCode);
-        }
+    protected int execute(Path projectFolder, String compileGoal) throws InterruptedException, IOException {
+        String[] arguments = {MVN, USE_CUSTOM_POM, projectFolder.resolve(POM_XML).toString(), COMPILE_GOAL};
+        Process process = Runtime.getRuntime().exec(arguments);
+        return process.waitFor();
     }
 }
