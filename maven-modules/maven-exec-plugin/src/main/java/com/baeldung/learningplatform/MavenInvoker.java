f2f979a43357fff84da54358827ab706265fb95a
Eugene Kovko <eu.kovko@gmail.com>
2024-02-02 13:13:59 +0100

    BAEL-7201: Project creation and tests

--- maven-modules/maven-exec-plugin/src/main/java/com/baeldung/learningplatform/MavenInvoker.java
+++ maven-modules/maven-exec-plugin/src/main/java/com/baeldung/learningplatform/MavenInvoker.java
@@ -1,6 +1,5 @@
 package com.baeldung.learningplatform;
 
-import java.io.File;
 import java.nio.file.Path;
 import java.util.Collections;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
@@ -12,19 +11,12 @@ import org.apache.maven.shared.invoker.MavenInvocationException;
 
 public class MavenInvoker implements Maven {
 
-    private final String mavenHome;
-
-    public MavenInvoker(String mavenHome) {
-        this.mavenHome = mavenHome;
-    }
-
     @Override
     public void compile(Path projectFolder) {
         InvocationRequest request = new DefaultInvocationRequest();
         request.setPomFile(projectFolder.resolve(POM_XML).toFile());
         request.setGoals(Collections.singletonList(Maven.COMPILE_GOAL));
         Invoker invoker = new DefaultInvoker();
-        invoker.setMavenHome(new File(mavenHome));
         try {
             InvocationResult result = invoker.execute(request);
             if (result.getExitCode() != 0) {
