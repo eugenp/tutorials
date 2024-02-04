5441cc139c56fe2e715c46da9c6013fa346af4ab
Eugene Kovko <eu.kovko@gmail.com>
2024-02-04 19:39:54 +0100

    feat: Working tests for WebMvc and WebTestClient

--- spring-5/src/test/java/com/baeldung/queryparamdoc/BookControllerReactiveIntegrationTest.java
+++ spring-5/src/test/java/com/baeldung/queryparamdoc/BookControllerReactiveIntegrationTest.java
@@ -1,18 +1,38 @@
 package com.baeldung.queryparamdoc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@WebFluxTest(controllers = BookController.class)
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@WebFluxTest
 class BookControllerReactiveIntegrationTest {
 
     @Autowired
     private WebTestClient webTestClient;
+
+    @BeforeEach
+    public void setUp(ApplicationContext webApplicationContext, RestDocumentationContextProvider restDocumentation) {
+        this.webTestClient = WebTestClient.bindToApplicationContext(webApplicationContext)
+          .configureClient()
+          .filter(documentationConfiguration(restDocumentation))
+          .build();
+    }
+
     @Test
     void smokeTest() {
         assertThat(webTestClient).isNotNull();
@@ -21,7 +41,9 @@ class BookControllerReactiveIntegrationTest {
     @Test
     @WithMockUser
     void giveEndpoint_whenSendGetRequest_thenSuccessfulResponse() {
-        webTestClient.get().uri("/books")
-          .exchange().expectStatus().isOk();
+        webTestClient.get().uri("/books?page=2")
+          .exchange().expectStatus().isOk().expectBody()
+          .consumeWith(document("books",
+            requestParameters(parameterWithName("page").description("The page to retrieve"))));
     }
 }
