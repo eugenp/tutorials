9e39aa4a7cb3f89de7d3dba39686d0774acaf640
Eugene Kovko <eu.kovko@gmail.com>
2024-02-04 19:02:53 +0100

    feat: Removed unused import

--- spring-5/src/test/java/com/baeldung/queryparamdoc/BookControllerReactiveIntegrationTest.java
+++ spring-5/src/test/java/com/baeldung/queryparamdoc/BookControllerReactiveIntegrationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.reactive.server.WebTestClient;
