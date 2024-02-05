1732af872137b5c84319cce5977533ea53fec9f4
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2024-02-05 15:29:01 +0530

    Update ImmutablePersonUnitTest.java

--- core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/objectmutability/ImmutablePersonUnitTest.java
+++ core-java-modules/core-java-lang-oop-patterns/src/test/java/com/baeldung/objectmutability/ImmutablePersonUnitTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ImmutablePersonUnitTest {
 
-
     @Test
     public void givenImmutablePerson_whenAccessFields_thenCorrectValues() {
         ImmutablePerson person = new ImmutablePerson("John", 30);
