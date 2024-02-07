20d1bd86ac1cc9e2c520ccfaa8ed41314dff4222
Amit Pandey <amit.pandey2103@gmail.com>
2024-02-07 21:31:52 +0530

    JAVA-31185 :- Upgrade to Hibernate latest version for persistence modules

--- persistence-modules/atomikos/src/main/java/com/baeldung/atomikos/spring/jpa/order/Order.java
+++ persistence-modules/atomikos/src/main/java/com/baeldung/atomikos/spring/jpa/order/Order.java
@@ -1,8 +1,8 @@
 package com.baeldung.atomikos.spring.jpa.order;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 
 @Entity
