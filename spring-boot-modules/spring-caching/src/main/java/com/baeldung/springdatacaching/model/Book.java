67712a89179f4cbe76f713e0107b59ae38bfced0
Bipinkumar27 <sayy2bipin@gmail.com>
2024-02-12 20:34:51 +0530

    JAVA-29166: Changes made for Upgrade spring-caching to Spring Boot 3

--- spring-boot-modules/spring-caching/src/main/java/com/baeldung/springdatacaching/model/Book.java
+++ spring-boot-modules/spring-caching/src/main/java/com/baeldung/springdatacaching/model/Book.java
@@ -1,11 +1,10 @@
 package com.baeldung.springdatacaching.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Entity;
-import javax.persistence.Id;
 
 import java.io.Serializable;
 import java.util.UUID;
