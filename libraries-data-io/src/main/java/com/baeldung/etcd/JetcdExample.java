018f5d9b6f6b3f6e8dfdf75a2287e2db53490058
vaibhav007jain <72961247+vaibhav007jain@users.noreply.github.com>
2024-02-15 22:18:07 +0530

    Update JetcdExample.java

--- libraries-data-io/src/main/java/com/baeldung/etcd/JetcdExample.java
+++ libraries-data-io/src/main/java/com/baeldung/etcd/JetcdExample.java
@@ -4,6 +4,7 @@ import io.etcd.jetcd.api.DeleteRangeRequest;
 import io.etcd.jetcd.api.DeleteRangeResponse;
 import io.etcd.jetcd.api.PutResponse;
 import io.etcd.jetcd.api.RangeResponse;
+import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.KV;
 import io.etcd.jetcd.Client;
 
@@ -15,17 +16,19 @@ public class JetcdExample {
 
         try (Client client = Client.builder().endpoints(etcdEndpoint).build()) {
             KV kvClient = client.getKVClient();
-
+            ByteSequence keyBytes = ByteSequence.from(key.getBytes());
+            ByteSequence valueBytes = ByteSequence.from(value.getBytes());
+            
             // Put a key-value pair
-            PutResponse response = kvClient.put(key.getBytes(), value.getBytes()).get();
+            PutResponse response = kvClient.put(keyBytes, valueBytes).get();
 
             // Retrieve the value
-            RangeResponse getResponse = kvClient.get(key.getBytes()).get();
+            RangeResponse getResponse = kvClient.get(keyBytes).get();
             String retrievedValue = getResponse.getKvs(0).getValue().toStringUtf8();
 
             // Delete the key
-            DeleteRangeResponse deleteResponse = 
-                kvClient.delete(DeleteRangeRequest.newBuilder().setKey(key.getBytes()).build()).get();
+            // DeleteRangeResponse deleteResponse = 
+            //     kvClient.delete(DeleteRangeRequest.newBuilder().setKey(key.getBytes()).build()).get();
         } catch (Exception e) {
             e.printStackTrace();
         }
