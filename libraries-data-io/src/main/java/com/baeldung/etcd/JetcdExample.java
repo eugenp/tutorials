707a5625c1fb7338c0d8b744ab8e68441e84ec0f
vaibhav007jain <72961247+vaibhav007jain@users.noreply.github.com>
2024-02-16 14:07:24 +0530

    Update JetcdExample.java

--- libraries-data-io/src/main/java/com/baeldung/etcd/JetcdExample.java
+++ libraries-data-io/src/main/java/com/baeldung/etcd/JetcdExample.java
@@ -2,9 +2,7 @@ package com.baeldung.etcd;
 
 import java.util.concurrent.CompletableFuture;
 
-import io.etcd.jetcd.kv.DeleteResponse;
 import io.etcd.jetcd.kv.GetResponse;
-import io.etcd.jetcd.kv.PutResponse;
 import io.etcd.jetcd.ByteSequence;
 import io.etcd.jetcd.KV;
 import io.etcd.jetcd.Client;
@@ -12,24 +10,20 @@ import io.etcd.jetcd.Client;
 public class JetcdExample {
     public static void main(String[] args) {
         String etcdEndpoint = "http://localhost:2379";
-        // String key = "/mykey";
-        // String value = "Hello, etcd!";
+        ByteSequence key = ByteSequence.from("/mykey".getBytes());
+        ByteSequence value = ByteSequence.from("Hello, etcd!".getBytes());
 
         try (Client client = Client.builder().endpoints(etcdEndpoint).build()) {
             KV kvClient = client.getKVClient();
-            ByteSequence key = ByteSequence.from("test_key".getBytes());
-            ByteSequence value = ByteSequence.from("test_value".getBytes());
             
-            // put the key-value
+            // Put a key-value pair
             kvClient.put(key, value).get();
             
-            // get the CompletableFuture
+            // Retrieve the value using CompletableFuture
             CompletableFuture<GetResponse> getFuture = kvClient.get(key);
-            
-            // get the value from CompletableFuture
             GetResponse response = getFuture.get();
             
-            // delete the key
+            // Delete the key
             kvClient.delete(key).get();
         } catch (Exception e) {
             e.printStackTrace();
