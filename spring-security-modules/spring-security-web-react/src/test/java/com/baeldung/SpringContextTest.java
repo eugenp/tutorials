bd36eddecaa79e19316fd270790822eec7de7731
Harry9656 <singharpal9656@gmail.com>
2024-02-20 01:19:05 +0100

    JAVA-29315: Upgrade spring-security-web-react to parent-spring-6 (#15920)

--- spring-security-modules/spring-security-web-react/src/test/java/com/baeldung/SpringContextTest.java
+++ spring-security-modules/spring-security-web-react/src/test/java/com/baeldung/SpringContextTest.java
@@ -1,19 +1,22 @@
 package com.baeldung;
 
-import com.baeldung.spring.MvcConfig;
-import com.baeldung.spring.SecSecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+import com.baeldung.spring.MvcConfig;
+import com.baeldung.spring.SecSecurityConfig;
+
 @WebAppConfiguration
 @ContextConfiguration(classes = { MvcConfig.class, SecSecurityConfig.class })
-public class SpringContextTest {
+@ExtendWith(SpringExtension.class)
+class SpringContextTest {
 
     @Test
-    public void whenSpringContextIsBootstrapped_thenNoExceptions() {
+    void whenSpringContextIsBootstrapped_thenNoExceptions() {
     }
 }
+
+
