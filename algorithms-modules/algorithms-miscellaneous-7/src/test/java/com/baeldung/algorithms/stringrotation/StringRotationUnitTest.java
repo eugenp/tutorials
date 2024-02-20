2b6c807f74fef268b531697dcd23af4fd0b38dec
luca <lucacambi77@gmail.com>
2024-02-20 11:29:57 +0100

    fix: test names

--- algorithms-modules/algorithms-miscellaneous-7/src/test/java/com/baeldung/algorithms/stringrotation/StringRotationUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-7/src/test/java/com/baeldung/algorithms/stringrotation/StringRotationUnitTest.java
@@ -12,49 +12,49 @@ import org.junit.jupiter.api.Test;
 class StringRotationUnitTest {
 
     @Test
-    void givenOriginAndRotationInput_whenCheckIfOriginContainsRotation_thenOk() {
+    void givenOriginAndRotationInput_whenCheckIfOriginContainsRotation_thenIsRotation() {
         assertTrue(doubledOriginContainsRotation("abcd", "cdab"));
         assertTrue(doubledOriginContainsRotation("abcd", "abcd"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckIfOriginContainsRotation_thenKo() {
+    void givenOriginAndRotationInput_whenCheckIfOriginContainsRotation_thenNoRotation() {
         assertFalse(doubledOriginContainsRotation("abcd", "bbbb"));
         assertFalse(doubledOriginContainsRotation("abcd", "abcde"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckingCommonStartWithOrigin_thenOk() {
+    void givenOriginAndRotationInput_whenCheckingCommonStartWithOrigin_thenIsRotation() {
         assertTrue(isRotationUsingCommonStartWithOrigin("abcd", "cdab"));
         assertTrue(isRotationUsingCommonStartWithOrigin("abcd", "abcd"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckingCommonStartWithOrigin_thenKo() {
+    void givenOriginAndRotationInput_whenCheckingCommonStartWithOrigin_thenNoRotation() {
         assertFalse(isRotationUsingCommonStartWithOrigin("abcd", "bbbb"));
         assertFalse(isRotationUsingCommonStartWithOrigin("abcd", "abcde"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckingUsingQueues_thenOk() {
+    void givenOriginAndRotationInput_whenCheckingUsingQueues_thenIsRotation() {
         assertTrue(isRotationUsingQueue("abcd", "cdab"));
         assertTrue(isRotationUsingQueue("abcd", "abcd"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckingUsingQueues_thenKo() {
+    void givenOriginAndRotationInput_whenCheckingUsingQueues_thenNoRotation() {
         assertFalse(isRotationUsingQueue("abcd", "bbbb"));
         assertFalse(isRotationUsingQueue("abcd", "abcde"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckingUsingSuffixAndPrefix_thenOk() {
+    void givenOriginAndRotationInput_whenCheckingUsingSuffixAndPrefix_thenIsRotation() {
         assertTrue(isRotationUsingSuffixAndPrefix("abcd", "cdab"));
         assertTrue(isRotationUsingSuffixAndPrefix("abcd", "abcd"));
     }
 
     @Test
-    void givenOriginAndRotationInput_whenCheckingUsingSuffixAndPrefix_thenKo() {
+    void givenOriginAndRotationInput_whenCheckingUsingSuffixAndPrefix_thenNoRotation() {
         assertFalse(isRotationUsingSuffixAndPrefix("abcd", "bbbb"));
         assertFalse(isRotationUsingSuffixAndPrefix("abcd", "abcde"));
     }
