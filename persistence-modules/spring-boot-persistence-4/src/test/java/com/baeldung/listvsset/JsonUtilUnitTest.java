dafdbb8dd61c48fd043e04b7e416ea0c923f6f54
anuragkumawat <anuragkumawat7@gmail.com>
2024-02-25 13:11:09 +0530

    JAVA-31111 Fix references to parent in spring-boot-persistence-4 spring-6-rsocket spring-cloud-azure modules

--- persistence-modules/spring-boot-persistence-4/src/test/java/com/baeldung/listvsset/JsonUtilUnitTest.java
+++ persistence-modules/spring-boot-persistence-4/src/test/java/com/baeldung/listvsset/JsonUtilUnitTest.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = {Application.class, TestConfig.class})
-class JsonUtilTest {
+class JsonUtilUnitTest {
 
     @Autowired
     private JsonUtils jsonUtils;
