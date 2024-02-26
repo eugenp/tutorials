e9c06e73397fbde1819bc3431e9c681f8843e61f
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-02-26 20:59:07 +0100

    BAEL-7523: article improvements

--- spring-boot-modules/spring-boot-libraries-3/src/main/java/com/baeldung/springmodulith/events/externalization/EventExternalizationConfig.java
+++ spring-boot-modules/spring-boot-libraries-3/src/main/java/com/baeldung/springmodulith/events/externalization/EventExternalizationConfig.java
@@ -4,12 +4,13 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.core.KafkaOperations;
 import org.springframework.modulith.events.EventExternalizationConfiguration;
-import org.springframework.modulith.events.Externalized;
 import org.springframework.modulith.events.RoutingTarget;
+
+import java.util.Objects;
 
 @Configuration
 class EventExternalizationConfig {
@@ -44,6 +45,9 @@ class EventExternalizationConfig {
     }
 
     record PostPublishedKafkaEvent(String slug, String title) {
+        PostPublishedKafkaEvent {
+            Objects.requireNonNull(slug, "Article Slug must not be null!");
+        }
     }
 
 }
