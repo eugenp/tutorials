82c8237b2647d7d454d9cda1fb3c7be7573a7667
Eugene Kovko <eu.kovko@gmail.com>
2024-02-29 12:19:41 +0100

    BAEL-7509: Formatting fix

--- persistence-modules/spring-data-jpa-repo-4/src/main/java/com/baeldung/spring/data/persistence/findvsget/entity/Group.java
+++ persistence-modules/spring-data-jpa-repo-4/src/main/java/com/baeldung/spring/data/persistence/findvsget/entity/Group.java
@@ -13,51 +13,38 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "group")
 public class Group {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
     @OneToOne
     private User administrator;
-
     @OneToMany(mappedBy = "id")
     private Set<User> users = new HashSet<>();
-
-
     public void addUser(User user) {
         users.add(user);
     }
     public Set<User> getUsers() {
         return users;
     }
-
     public void setUsers(Set<User> users) {
         this.users = users;
     }
-
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public User getAdministrator() {
         return administrator;
     }
-
     public void setAdministrator(User administrator) {
         this.administrator = administrator;
     }
