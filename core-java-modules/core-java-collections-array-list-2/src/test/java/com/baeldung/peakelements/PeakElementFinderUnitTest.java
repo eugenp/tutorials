1155a6d8e842e901ce2606d05348569156546c3b
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2024-03-01 13:19:42 +0530

    Update PeakElementFinderUnitTest.java

--- core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/peakelements/PeakElementFinderUnitTest.java
+++ core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/peakelements/PeakElementFinderUnitTest.java
@@ -9,18 +9,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PeakElementFinderUnitTest {
 
     @Test
-    void findPeakElement_givenArrayOfIntegers_whenValidInput_thenReturnsCorrectPeakIndex() {
-        PeakElementFinder finder = new PeakElementFinder();
-        int[] array = {5, 8, 7, 6, 12};
-        List<Integer> peaks = finder.findPeakElement(array);
-        assertEquals(2, peaks.size() );
-    }
-
-    @Test
     void findPeakElement_givenArrayOfIntegers_whenNoPeaks_thenReturnsMinusOne() {
         PeakElementFinder finder = new PeakElementFinder();
         int[] array = {5, 8, 7, 6, 12};
-        List<Integer> peaks = finder.findPeakElement(array);
+        List<Integer> peaks = finder.findPeakElements(array);
         assertEquals(2, peaks.size());
     }
 
@@ -28,7 +20,7 @@ public class PeakElementFinderUnitTest {
     void findPeakElement_givenArrayOfIntegers_whenPeaksAtExtremes_thenReturnsCorrectPeakIndex() {
         PeakElementFinder finder = new PeakElementFinder();
         int[] array = {5, 2, 1, 3, 4};
-        List<Integer> peaks = finder.findPeakElement(array);
+        List<Integer> peaks = finder.findPeakElements(array);
         assertEquals(2, peaks.size());
     }
 
@@ -36,7 +28,7 @@ public class PeakElementFinderUnitTest {
     void findPeakElement_givenArrayOfIntegers_whenPlateaus_thenReturnsCorrectPeakIndex() {
         PeakElementFinder finder = new PeakElementFinder();
         int[] array = {1, 2, 2, 2, 3, 4, 5};
-        List<Integer> peaks = finder.findPeakElement(array);
-        assertEquals(2, peaks.size());
+        List<Integer> peaks = finder.findPeakElements(array);
+        assertEquals(1, peaks.size());
     }
 }
