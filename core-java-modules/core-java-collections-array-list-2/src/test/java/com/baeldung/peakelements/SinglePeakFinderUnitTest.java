16b8708d25efc7c23962d143a5ce78f091621a6c
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2024-03-01 13:20:31 +0530

    Update SinglePeakFinderUnitTest.java

--- core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/peakelements/SinglePeakFinderUnitTest.java
+++ core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/peakelements/SinglePeakFinderUnitTest.java
@@ -2,12 +2,23 @@ package com.baeldung.peakelements;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.OptionalInt;
 
 public class SinglePeakFinderUnitTest {
 
     @Test
     void findSinglePeak_givenArrayOfIntegers_whenValidInput_thenReturnsCorrectPeak() {
         int[] arr = {0, 10, 2, 4, 5, 1};
-        assertEquals(10, SinglePeakFinder.findSinglePeak(arr));
+        OptionalInt peak = SinglePeakFinder.findSinglePeak(arr);
+        assertTrue(peak.isPresent());
+        assertEquals(10, peak.getAsInt());
+    }
+
+    @Test
+    void findSinglePeak_givenEmptyArray_thenReturnsEmptyOptional() {
+        int[] arr = {};
+        OptionalInt peak = SinglePeakFinder.findSinglePeak(arr);
+        assertTrue(peak.isEmpty());
     }
 }
