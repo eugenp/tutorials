cccccaf3e40d9ecb8b9798471075e85a3d81fae0
shahulbasha <shhl.basha333@gmail.com>
2024-03-03 20:17:50 -0500

    BAEL-7470 new module

--- spring-boot-modules/spring-boot-basic-customization-3/src/main/java/com/baeldung/filter/AppConfig.java
+++ spring-boot-modules/spring-boot-basic-customization-3/src/main/java/com/baeldung/filter/AppConfig.java
@@ -1,5 +1,6 @@
 package com.baeldung.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,10 +8,20 @@ import org.springframework.web.filter.DelegatingFilterProxy;
 
 @Configuration
 public class AppConfig {
+
     @Bean
-    public FilterRegistrationBean<DelegatingFilterProxy> loggingFilterRegistration() {
+    public FilterRegistrationBean<DelegatingFilterProxy> loggingFilterDelegatingProxy() {
         FilterRegistrationBean<DelegatingFilterProxy> registrationBean = new FilterRegistrationBean<>();
         registrationBean.setFilter(new DelegatingFilterProxy("loggingFilterDelegateProxy"));
+        registrationBean.addUrlPatterns("/*");
+        return registrationBean;
+    }
+
+    @Bean
+    @Autowired
+    public FilterRegistrationBean<LoggingFilterRegistrationBean> loggingFilterRegistration(LoggingService loggingService) {
+        FilterRegistrationBean<LoggingFilterRegistrationBean> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new LoggingFilterRegistrationBean(loggingService));
         registrationBean.addUrlPatterns("/*");
         return registrationBean;
     }
