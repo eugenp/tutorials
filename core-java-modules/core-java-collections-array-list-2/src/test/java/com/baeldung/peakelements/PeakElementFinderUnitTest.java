b09f501333e6d193226cb564521ee85aaaddf741
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2024-03-06 12:20:13 +0530

    Update PeakElementFinderUnitTest.java

--- core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/peakelements/PeakElementFinderUnitTest.java
+++ core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/peakelements/PeakElementFinderUnitTest.java
@@ -9,17 +9,24 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PeakElementFinderUnitTest {
 
     @Test
-    void findPeakElement_givenArrayOfIntegers_whenNoPeaks_thenReturnsMinusOne() {
+    void findPeakElement_givenArrayOfIntegers_whenValidInput_thenReturnsCorrectPeak() {
         PeakElementFinder finder = new PeakElementFinder();
-        int[] array = {5, 8, 7, 6, 12};
+        int[] array = {1, 2, 3, 2, 1};
         List<Integer> peaks = finder.findPeakElements(array);
-        assertEquals(2, peaks.size());
-        assertTrue(peaks.contains(8));
-        assertTrue(peaks.contains(12));
+        assertEquals(1, peaks.size());
+        assertTrue(peaks.contains(3));
     }
 
     @Test
-    void findPeakElement_givenArrayOfIntegers_whenPeaksAtExtremes_thenReturnsCorrectPeakIndex() {
+    void findPeakElement_givenArrayOfIntegers_whenNoPeaks_thenReturnsEmptyList() {
+        PeakElementFinder finder = new PeakElementFinder();
+        int[] array = {};
+        List<Integer> peaks = finder.findPeakElements(array);
+        assertEquals(0, peaks.size());
+    }
+
+    @Test
+    void findPeakElement_givenArrayOfIntegers_whenPeaksAtExtremes_thenReturnsCorrectPeak() {
         PeakElementFinder finder = new PeakElementFinder();
         int[] array = {5, 2, 1, 3, 4};
         List<Integer> peaks = finder.findPeakElements(array);
@@ -29,7 +36,7 @@ public class PeakElementFinderUnitTest {
     }
 
     @Test
-    void findPeakElement_givenArrayOfIntegers_whenPlateaus_thenReturnsCorrectPeakIndex() {
+    void findPeakElement_givenArrayOfIntegers_whenPlateaus_thenReturnsCorrectPeak() {
         PeakElementFinder finder = new PeakElementFinder();
         int[] array = {1, 2, 2, 2, 3, 4, 5};
         List<Integer> peaks = finder.findPeakElements(array);
