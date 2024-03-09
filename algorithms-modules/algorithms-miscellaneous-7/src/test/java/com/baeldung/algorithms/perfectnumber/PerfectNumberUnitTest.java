3313097e330d7f092f5af055e8a89d3352b8b679
alexyang <alexyang0623@gmail.com>
2024-03-09 17:13:56 +0800

    refactor UT using bdd style

--- algorithms-modules/algorithms-miscellaneous-7/src/test/java/com/baeldung/algorithms/perfectnumber/PerfectNumberUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-7/src/test/java/com/baeldung/algorithms/perfectnumber/PerfectNumberUnitTest.java
@@ -5,52 +5,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PerfectNumberUnitTest {
+class PerfectNumberUnitTest {
     @Test
-    void testIsPerfectBruteForce() {
-        // Test a perfect number
-        givenNumberIsPerfect(6);
-
-        // Test a non-perfect number
-        givenNumberIsNotPerfect(10);
-
-        // Test a negative number
-        givenNegativeNumberIsNotPerfect(-28);
+    void givenPerfectNumber_whenCheckingIsPerfectBruteForce_thenReturnTrue() {
+        assertTrue(PerfectNumber.isPerfectBruteForce(6));
     }
 
     @Test
-    void testIsPerfectStream() {
-        // Test a perfect number
-        givenNumberIsPerfect(28);
-
-        // Test a non-perfect number
-        givenNumberIsNotPerfect(10);
-
-        // Test a negative number
-        givenNegativeNumberIsNotPerfect(-6);
+    void givenNonPerfectNumber_whenCheckingIsPerfectBruteForce_thenReturnFalse() {
+        assertFalse(PerfectNumber.isPerfectBruteForce(10));
     }
 
     @Test
-    void testIsPerfectEuclidEuler() {
-        // Test a perfect number
-        givenNumberIsPerfect(28);
-
-        // Test a non-perfect number
-        givenNumberIsNotPerfect(10);
-
-        // Test a negative number
-        givenNegativeNumberIsNotPerfect(-6);
+    void givenNegativeNumber_whenCheckingIsPerfectBruteForce_thenReturnFalse() {
+        assertFalse(PerfectNumber.isPerfectBruteForce(-28));
     }
 
-    private void givenNumberIsPerfect(int number) {
-        assertTrue(PerfectNumber.isPerfectBruteForce(number));
+    @Test
+    void givenPerfectNumber_whenCheckingIsPerfectStream_thenReturnTrue() {
+        assertTrue(PerfectNumber.isPerfectStream(28));
     }
 
-    private void givenNumberIsNotPerfect(int number) {
-        assertFalse(PerfectNumber.isPerfectBruteForce(number));
+    @Test
+    void givenNonPerfectNumber_whenCheckingIsPerfectStream_thenReturnFalse() {
+        assertFalse(PerfectNumber.isPerfectStream(10));
     }
 
-    private void givenNegativeNumberIsNotPerfect(int number) {
-        assertFalse(PerfectNumber.isPerfectBruteForce(number));
+    @Test
+    void givenNegativeNumber_whenCheckingIsPerfectStream_thenReturnFalse() {
+        assertFalse(PerfectNumber.isPerfectStream(-6));
+    }
+
+    @Test
+    void givenPerfectNumber_whenCheckingIsPerfectEuclidEuler_thenReturnTrue() {
+        assertTrue(PerfectNumber.isPerfectEuclidEuler(28));
+    }
+
+    @Test
+    void givenNonPerfectNumber_whenCheckingIsPerfectEuclidEuler_thenReturnFalse() {
+        assertFalse(PerfectNumber.isPerfectEuclidEuler(10));
+    }
+
+    @Test
+    void givenNegativeNumber_whenCheckingIsPerfectEuclidEuler_thenReturnFalse() {
+        assertFalse(PerfectNumber.isPerfectEuclidEuler(-6));
     }
 }
