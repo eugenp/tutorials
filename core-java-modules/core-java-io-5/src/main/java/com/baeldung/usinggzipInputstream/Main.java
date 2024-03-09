31effb022e4fbe6ac1465f6a37be32ee94da0c8d
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-03-09 18:38:37 +0200

    Update Main.java

--- core-java-modules/core-java-io-5/src/main/java/com/baeldung/usinggzipInputstream/Main.java
+++ core-java-modules/core-java-io-5/src/main/java/com/baeldung/usinggzipInputstream/Main.java
@@ -1,39 +1,57 @@
 package com.baeldung.usinggzipInputstream;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String filePath = Objects.requireNonNull(Main.class.getClassLoader().getResource("myFile.gz")).getFile();
-        processGZipFile(filePath, lines -> lines.forEach(System.out::println));
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream("myFile.gz")) {
+            if (inputStream == null) {
+                throw new FileNotFoundException("File not found: myFile.gz");
+            }
+
+            processGZipFile(inputStream, lines -> lines.forEach(System.out::println));
+        }
     }
 
-    public static Stream<String> readGZipFile(String filePath) throws IOException {
-        InputStream inputStream = new FileInputStream(filePath);
-        GZIPInputStream gzip = new GZIPInputStream(inputStream);
-        BufferedReader br = new BufferedReader(new InputStreamReader(gzip));
+    public static List<String> readGZipFile(String filePath) throws IOException {
+        List<String> lines = new ArrayList<>();
 
-        Stream<String> lines = br.lines();
+        try (FileInputStream fileInputStream = new FileInputStream(filePath);
+             GZIPInputStream gzipInputStream = new GZIPInputStream(fileInputStream);
+             InputStreamReader inputStreamReader = new InputStreamReader(gzipInputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
 
-        lines.onClose(() -> {
-            try {
-                br.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
             }
-        });
+        }
 
         return lines;
     }
 
-    public static void processGZipFile(String filePath, Consumer<Stream<String>> lineProcessor) throws IOException {
-        try (Stream<String> lines = readGZipFile(filePath)) {
-            lineProcessor.accept(lines);
+    public static void processGZipFile(InputStream inputStream, LineProcessor lineProcessor) throws IOException {
+        try (Stream<String> lines = new BufferedReader(new InputStreamReader(new GZIPInputStream(inputStream)))
+                .lines()
+                .map(String::trim)) {
+            lineProcessor.process(lines);
         }
+    }
+
+    @FunctionalInterface
+    public interface LineProcessor {
+        void process(Stream<String> lines);
     }
 }
