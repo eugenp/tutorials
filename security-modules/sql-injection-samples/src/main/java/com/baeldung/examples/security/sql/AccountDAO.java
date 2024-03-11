06abed4ac3b991caec1b1cc74213053a913c71b1
sam-gardner <samgardner909@gmail.com>
2024-03-11 10:54:48 +0000

    JAVA-28946 Upgrade sql injection samples to Spring Boot 3

--- security-modules/sql-injection-samples/src/main/java/com/baeldung/examples/security/sql/AccountDAO.java
+++ security-modules/sql-injection-samples/src/main/java/com/baeldung/examples/security/sql/AccountDAO.java
@@ -17,14 +17,14 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Order;
-import javax.persistence.criteria.Root;
-import javax.persistence.metamodel.SingularAttribute;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Order;
+import jakarta.persistence.criteria.Root;
+import jakarta.persistence.metamodel.SingularAttribute;
 import javax.sql.DataSource;
 
 import org.springframework.stereotype.Component;
