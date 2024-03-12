7a9fa0add5f389f340576e563f8ad2228365dca0
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-03-12 22:48:04 +0200

    Update Main.java

--- core-java-modules/core-java-io-5/src/main/java/com/baeldung/usinggzipInputstream/Main.java
+++ core-java-modules/core-java-io-5/src/main/java/com/baeldung/usinggzipInputstream/Main.java
@@ -5,19 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import java.util.zip.GZIPInputStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
     static String filePath = Objects.requireNonNull(Main.class.getClassLoader().getResource("myFile.gz")).getFile();
 
     public static void main(String[] args) throws IOException {
+        // Test readGZipFile method
+        List<String> fileContents = readGZipFile(filePath);
+        System.out.println("Contents of GZIP file:");
+        fileContents.forEach(System.out::println);
 
-        try {
-            readGZipFileWithFilter(filePath, lines -> lines.forEach(System.out::println));
-        } catch (FileNotFoundException e) {
-            System.err.println(e.getMessage());
-        }
+        // Test findInZipFile method
+        String searchTerm = "Line 1 content";
+        List<String> foundLines = findInZipFile(filePath, searchTerm);
+        System.out.println("Lines containing '" + searchTerm + "' in GZIP file:");
+        foundLines.forEach(System.out::println);
+
+        // Test streamGZipFile method
+        System.out.println("Streaming contents of GZIP file:");
+        streamGZipFile(filePath).forEach(System.out::println);
+
+        // Test useContentsOfZipFile method
+        System.out.println("Using contents of GZIP file with consumer:");
+        useContentsOfZipFile(filePath, linesStream -> {
+            linesStream.filter(line -> line.length() > 10).forEach(System.out::println);
+        });
     }
+
 
     public static List<String> readGZipFile(String filePath) throws IOException {
         List<String> lines = new ArrayList<>();
@@ -35,7 +54,37 @@ public class Main {
         return lines;
     }
 
-    public static void readGZipFileWithFilter(String filePath, Consumer<List<String>> lineConsumer) throws IOException {
-        lineConsumer.accept(readGZipFile(filePath));
+    public static List<String> findInZipFile(String filePath, String toFind) throws IOException {
+        try (InputStream inputStream = new FileInputStream(filePath);
+             GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
+             InputStreamReader inputStreamReader = new InputStreamReader(gzipInputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+            return bufferedReader.lines().filter(line -> line.contains(toFind)).collect(toList());
+        }
+    }
+
+    public static Stream<String> streamGZipFile(String filePath) throws IOException {
+        InputStream inputStream = new FileInputStream(filePath);
+        GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
+        InputStreamReader inputStreamReader = new InputStreamReader(gzipInputStream);
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        return bufferedReader.lines();
+    }
+
+    public static List<String> useContentsOfZipFile(String filePath, Consumer<Stream<String>> consumer) throws IOException {
+        List<String> linesList;
+        try (InputStream inputStream = new FileInputStream(filePath);
+             GZIPInputStream gzipInputStream = new GZIPInputStream(inputStream);
+             InputStreamReader inputStreamReader = new InputStreamReader(gzipInputStream);
+             BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+
+            Stream<String> linesStream = bufferedReader.lines();
+            linesList = linesStream.collect(Collectors.toList());
+            consumer.accept(linesList.stream());
+        }
+
+        return linesList;
     }
 }
