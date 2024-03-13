ef55934563da3fd651fd1156d1e87724664ed6c6
Harry9656 <singharpal9656@gmail.com>
2024-03-13 17:56:24 +0100

    Minor change.

--- gradle-modules/gradle-5/cmd-line-args/src/main/java/com/baeldung/cmd/MainClass.java
+++ gradle-modules/gradle-5/cmd-line-args/src/main/java/com/baeldung/cmd/MainClass.java
@@ -1,6 +1,7 @@
 package com.baeldung.cmd;
 
 public class MainClass {
+
     public static void main(String[] args) {
         System.out.println("Gradle command line arguments example");
         for (String arg : args) {
