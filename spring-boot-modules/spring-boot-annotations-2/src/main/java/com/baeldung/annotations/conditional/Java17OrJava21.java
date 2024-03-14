9ad79c95d0a526bf1e1b259c1a5ffd0fdb596653
panos-kakos <kakos.programmer@gmail.com>
2024-03-14 23:14:26 +0200

    [JAVA-30987] Upgraded spring-annotations-2 to spring boot 3

--- spring-boot-modules/spring-boot-annotations-2/src/main/java/com/baeldung/annotations/conditional/Java17OrJava21.java
+++ spring-boot-modules/spring-boot-annotations-2/src/main/java/com/baeldung/annotations/conditional/Java17OrJava21.java
@@ -3,14 +3,14 @@ package com.baeldung.annotations.conditional;
 import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
 import org.springframework.context.annotation.Conditional;
 
-public class Java8OrJava9 extends AnyNestedCondition  {
-    Java8OrJava9() {
+public class Java17OrJava21 extends AnyNestedCondition  {
+    Java17OrJava21() {
         super(ConfigurationPhase.REGISTER_BEAN);
     }
 
-    @Conditional(Java8Condition.class)
-    static class Java8 { }
+    @Conditional(Java17Condition.class)
+    static class Java17 { }
 
-    @Conditional(Java9Condition.class)
-    static class Java9 { }
+    @Conditional(Java21Condition.class)
+    static class Java21 { }
 }
