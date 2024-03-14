ecbb3a705f5b5f48977f09ace8ab6f2ed3cf7bcb
pedrolopes9-7 <pedroinf97@gmail.com>
2024-03-13 21:56:29 -0300

    wrapping up logic]

--- persistence-modules/spring-boot-persistence-4/src/main/java/com/baeldung/transactionalandasync/AccountService.java
+++ persistence-modules/spring-boot-persistence-4/src/main/java/com/baeldung/transactionalandasync/AccountService.java
@@ -2,30 +2,45 @@ package com.baeldung.transactionalandasync;
 
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 
 @Service
 @AllArgsConstructor
+@Transactional
 public class AccountService {
 
     private final AccountRepository accountRepository;
 
-    @Transactional
-    public void transfer(Long fromId, Long toId, BigDecimal amount) {
-        Account fromAccount = getAccountInfo(fromId);
-        Account toAccount = getAccountInfo(toId);
+    @Async
+    public void transferAsync(Long depositorId, Long favoredId, BigDecimal amount) {
+        transfer(depositorId, favoredId, amount);
 
-        fromAccount.setBalance(fromAccount.getBalance().subtract(amount));
-        toAccount.setBalance(toAccount.getBalance().add(amount));
-
-        accountRepository.save(fromAccount);
-        accountRepository.save(toAccount);
+        printReceipt();
     }
 
-    public Account getAccountInfo(Long id) {
-        return accountRepository.findById(id)
-                .orElseThrow(() -> new IllegalStateException("Account not found"));
+    @Transactional
+    public void transfer(Long depositorId, Long favoredId, BigDecimal amount) {
+        Account depositorAccount = accountRepository.findById(depositorId).get();
+        Account favoredAccount = accountRepository.findById(favoredId).get();
+
+        depositorAccount.setBalance(depositorAccount.getBalance().subtract(amount));
+        favoredAccount.setBalance(favoredAccount.getBalance().add(amount));
+
+        auditTransferAttempt();
+
+        accountRepository.save(depositorAccount);
+        accountRepository.save(favoredAccount);
+    }
+
+    @Transactional(Transactional.TxType.REQUIRES_NEW)
+    public void auditTransferAttempt() {
+        // logic to save the transfer attempt
+    }
+
+    public void printReceipt() {
+        // logic to print the receipt
     }
 }
