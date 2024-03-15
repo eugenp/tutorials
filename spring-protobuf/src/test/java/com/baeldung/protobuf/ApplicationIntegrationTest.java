bc84fc35a44be9ee23b3c0b2681d07d324021ae1
sam-gardner <53271849+sam-gardner@users.noreply.github.com>
2024-03-15 15:39:33 +0000

    JAVA-27542 Upgrade spring protobuf module to spring boot 3 (#16059)

--- spring-protobuf/src/test/java/com/baeldung/protobuf/ApplicationIntegrationTest.java
+++ spring-protobuf/src/test/java/com/baeldung/protobuf/ApplicationIntegrationTest.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
