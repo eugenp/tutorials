8e529f241f5bffd94d3271f64474928e38b4d8b8
Wynn <sgwebfreelancer@gmail.com>
2024-03-15 09:03:16 +0800

    Update the test variable name

--- core-java-modules/core-java-date-operations-4/src/test/java/com/baeldung/compareyesterday/CompareYesterdayUnitTest.java
+++ core-java-modules/core-java-date-operations-4/src/test/java/com/baeldung/compareyesterday/CompareYesterdayUnitTest.java
@@ -18,11 +18,11 @@ public class CompareYesterdayUnitTest {
     void givenYesterdayDate_whenCompareWithCalendar_returnTrue() {
         // To simulate yesterday
         Date date = new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24));
-        Calendar actualCalendar = Calendar.getInstance();
-        actualCalendar.setTime(date);
-
         Calendar expectedCalendar = Calendar.getInstance();
-        expectedCalendar.add(Calendar.DATE, -1);
+        expectedCalendar.setTime(date);
+
+        Calendar actualCalendar = Calendar.getInstance();
+        actualCalendar.add(Calendar.DATE, -1);
 
         boolean isEqualToYesterday = expectedCalendar.get(Calendar.YEAR) == actualCalendar.get(Calendar.YEAR) &&
             expectedCalendar.get(Calendar.MONTH) == actualCalendar.get(Calendar.MONTH) &&
@@ -32,10 +32,10 @@ public class CompareYesterdayUnitTest {
 
     @Test
     void givenTodayDate_whenCompareWithCalendar_returnFalse() {
-        Calendar actualCalendar = Calendar.getInstance();
-
         Calendar expectedCalendar = Calendar.getInstance();
-        expectedCalendar.add(Calendar.DATE, -1);
+
+        Calendar actualCalendar = Calendar.getInstance();
+        actualCalendar.add(Calendar.DATE, -1);
 
         boolean isEqualToYesterday = expectedCalendar.get(Calendar.YEAR) == actualCalendar.get(Calendar.YEAR) &&
             expectedCalendar.get(Calendar.MONTH) == actualCalendar.get(Calendar.MONTH) &&
@@ -46,24 +46,24 @@ public class CompareYesterdayUnitTest {
     @Test
     void givenYesterdayDate_whenCompareWithDateMilliseconds_returnTrue() {
         // Create a Date object representing yesterday
-        Date actualDate = new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24));
+        Date expectedDate = new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24));
         long yesterdayMidnightMillis = Instant.now()
             .minus(1, ChronoUnit.DAYS)
             .toEpochMilli();
 
-        boolean isEqualToYesterday = actualDate.getTime() >= yesterdayMidnightMillis && actualDate.getTime() < yesterdayMidnightMillis + 86_400_000;
+        boolean isEqualToYesterday = expectedDate.getTime() >= yesterdayMidnightMillis && expectedDate.getTime() < yesterdayMidnightMillis + 86_400_000;
         assertTrue(isEqualToYesterday);
     }
 
     @Test
     void givenTodayDate_whenCompareWithDateMilliseconds_returnFalse() {
         // Create a Date object representing yesterday
-        Date actualDate = new Date();
+        Date expectedDate = new Date();
         long yesterdayMidnightMillis = Instant.now()
             .minus(1, ChronoUnit.DAYS)
             .toEpochMilli();
 
-        boolean isEqualToYesterday = actualDate.getTime() >= yesterdayMidnightMillis && actualDate.getTime() < yesterdayMidnightMillis + 86_400_000;
+        boolean isEqualToYesterday = expectedDate.getTime() >= yesterdayMidnightMillis && expectedDate.getTime() < yesterdayMidnightMillis + 86_400_000;
         assertFalse(isEqualToYesterday);
     }
 
@@ -71,20 +71,20 @@ public class CompareYesterdayUnitTest {
     void givenYesterdayDate_whenCompareWithLocalDate_returnTrue() {
         // To simulate yesterday
         Date date = new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24));
-        LocalDate actualLocalDate = LocalDate.of(date.getYear() + 1900, date.getMonth() + 1, date.getDate());
+        LocalDate expectedLocalDate = LocalDate.of(date.getYear() + 1900, date.getMonth() + 1, date.getDate());
 
-        LocalDate expectedLocalDate = LocalDate.now()
+        LocalDate actualLocalDate = LocalDate.now()
             .minusDays(1);
-        boolean isEqualToYesterday = actualLocalDate.equals(expectedLocalDate);
+        boolean isEqualToYesterday = expectedLocalDate.equals(actualLocalDate);
         assertTrue(isEqualToYesterday);
     }
 
     @Test
     void givenTodayDate_whenCompareWithLocalDate_returnFalse() {
-        LocalDate actualLocalDate = LocalDate.now();
-        LocalDate expectedLocalDate = actualLocalDate.minusDays(1);
-
-        boolean isEqualToYesterday = actualLocalDate.equals(expectedLocalDate);
+        LocalDate expectedLocalDate = LocalDate.now();
+        LocalDate actualLocalDate = LocalDate.now()
+            .minusDays(1);
+        boolean isEqualToYesterday = expectedLocalDate.equals(actualLocalDate);
         assertFalse(isEqualToYesterday);
     }
 
@@ -92,24 +92,25 @@ public class CompareYesterdayUnitTest {
     void givenYesterdayDate_whenCompareWithJodaTime_returnTrue() {
         // To simulate yesterday
         Date date = new Date(System.currentTimeMillis() - (1000 * 60 * 60 * 24));
-        DateTime actualDateTime = new DateTime(date).withTimeAtStartOfDay();
+        DateTime expectedDateTime = new DateTime(date).withTimeAtStartOfDay();
 
-        DateTime expectedDateTime = DateTime.now()
+        DateTime actualDateTime = DateTime.now()
             .minusDays(1)
             .withTimeAtStartOfDay();
 
-        boolean isEqualToYesterday = actualDateTime.equals(expectedDateTime);
+        boolean isEqualToYesterday = expectedDateTime.equals(actualDateTime);
         assertTrue(isEqualToYesterday);
     }
 
     @Test
     void givenTodayDate_whenCompareWithJodaTime_returnFalse() {
-        DateTime actualDateTime = DateTime.now()
+        DateTime expectedDateTime = DateTime.now()
             .withTimeAtStartOfDay();
-        DateTime expectedDateTime = actualDateTime.minusDays(1)
+        DateTime actualDateTime = DateTime.now()
+            .minusDays(1)
             .withTimeAtStartOfDay();
 
-        boolean isEqualToYesterday = actualDateTime.equals(expectedDateTime);
+        boolean isEqualToYesterday = expectedDateTime.equals(actualDateTime);
         assertFalse(isEqualToYesterday);
     }
 }
