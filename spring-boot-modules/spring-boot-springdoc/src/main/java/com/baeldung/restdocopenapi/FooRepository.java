ef96037176eb3045f762a934bb81d477c0d30302
Amit Pandey <amit2103@users.noreply.github.com>
2024-03-17 19:36:36 +0530

    JAVA-31001: Upgrade spring-boot-springdoc to boot 3 (#16139)
    * JAVA-31001: Upgrade spring-boot-springdoc
    
    * JAVA-31001: Upgrade spring-boot-springdoc
    
    * JAVA-31001: Upgrade spring-boot-springdoc.
    
    * Uncomment in Parent module POM.
    
    * JAVA-31001: Upgrade spring-boot-springdoc parent.

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/restdocopenapi/FooRepository.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/restdocopenapi/FooRepository.java
@@ -1,9 +1,10 @@
 package com.baeldung.restdocopenapi;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FooRepository extends PagingAndSortingRepository<Foo, Long>{
+public interface FooRepository extends JpaRepository<Foo, Long> {
 
 }
