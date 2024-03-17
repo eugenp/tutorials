046616179696a403fb4238723b260ff2cd7ad488
Kai.Yuan <kent.yuan@Gmail.com>
2024-03-17 21:54:52 +0100

    [improvement-merging-maps] handle null values

--- core-java-modules/core-java-streams-simple/src/test/java/com/baeldung/streams/flatmap/map/Java8MapAndFlatMapUnitTest.java
+++ core-java-modules/core-java-streams-simple/src/test/java/com/baeldung/streams/flatmap/map/Java8MapAndFlatMapUnitTest.java
@@ -1,5 +1,6 @@
 package com.baeldung.streams.flatmap.map;
 
+import static java.lang.Math.max;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -119,4 +120,67 @@ class Java8MapAndFlatMapUnitTest {
 
         assertEquals(expectedMap, mergedMap);
     }
+
+    @Test
+    void givenMapsWithDuplicateKeysAndNullValues_whenCalledFlatMap_thenMultipleMapsMergedIntoOneMap() {
+
+        Map<String, Integer> playerMap1 = new HashMap<String, Integer>() {{
+            put("Kai", 92);
+            put("Liam", 100);
+        }};
+        Map<String, Integer> playerMap2 = new HashMap<String, Integer>() {{
+            put("Eric", 42);
+            put("Kevin", 77);
+        }};
+        Map<String, Integer> playerMap3 = new HashMap<String, Integer>() {{
+            put("Saajan", 35);
+        }};
+        Map<String, Integer> playerMap4 = new HashMap<String, Integer>() {{
+            put("Kai", 76);
+        }};
+        Map<String, Integer> playerMap5 = new HashMap<String, Integer>() {{
+            put("Kai", null);
+            put("Jerry", null);
+        }};
+
+        Map<String, Integer> expectedMap = new HashMap<String, Integer>() {{
+            put("Saajan", 35);
+            put("Liam", 100);
+            put("Kai", 92);
+            put("Eric", 42);
+            put("Kevin", 77);
+            put("Jerry", null);
+        }};
+
+        assertThrows(NullPointerException.class, () -> Stream.of(playerMap1, playerMap2, playerMap3, playerMap4, playerMap5)
+            .flatMap(map -> map.entrySet()
+                .stream())
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (existing, newValue) -> newValue)));
+
+        Map<String, Integer> mergedMap = new HashMap<>();
+        Stream.of(playerMap1, playerMap2, playerMap3, playerMap4, playerMap5)
+            .flatMap(map -> map.entrySet()
+                .stream())
+            .forEach(entry -> {
+                String k = entry.getKey();
+                Integer v = entry.getValue();
+                if (mergedMap.containsKey(k)) {
+                    mergedMap.put(k, maxInteger(mergedMap.get(k), v));
+                } else {
+                    mergedMap.put(k, v);
+                }
+            });
+        assertEquals(expectedMap, mergedMap);
+    }
+
+    private Integer maxInteger(Integer int1, Integer int2) {
+        if (int1 == null) {
+            return int2;
+        }
+        if (int2 == null) {
+            return int1;
+        }
+        return max(int1, int2);
+    }
+
 }
