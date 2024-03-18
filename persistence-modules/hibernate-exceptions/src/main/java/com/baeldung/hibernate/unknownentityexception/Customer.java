c521dae8d54fb93e43465a3fcbee02929fedd9da
davidmbochi <njonge.mbochi@students.ku.ac.ke>
2024-03-18 09:43:19 +0300

    removed increment strategy

--- persistence-modules/hibernate-exceptions/src/main/java/com/baeldung/hibernate/unknownentityexception/Customer.java
+++ persistence-modules/hibernate-exceptions/src/main/java/com/baeldung/hibernate/unknownentityexception/Customer.java
@@ -2,7 +2,6 @@ package com.baeldung.hibernate.unknownentityexception;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,7 +9,7 @@ import jakarta.persistence.Table;
 @Table(name = "customer")
 public class Customer {
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue
   private Long id;
 
   private String email;
