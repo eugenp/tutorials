f886c865b9f06206b9dd3a0b701c50fe88450bf4
DiegoMarti2 <150871541+DiegoMarti2@users.noreply.github.com>
2024-03-22 23:02:02 +0200

    Updating ConstructorsVersusSetterMethodsUnitTest.java
    Rename functions

--- core-java-modules/core-java-lang-oop-constructors-2/src/test/java/com/baeldung/objectcreation/constructorversussettermethod/ConstructorsVersusSetterMethodsUnitTest.java
+++ core-java-modules/core-java-lang-oop-constructors-2/src/test/java/com/baeldung/objectcreation/constructorversussettermethod/ConstructorsVersusSetterMethodsUnitTest.java
@@ -6,21 +6,21 @@ import static org.junit.Assert.assertEquals;
 
 public class ConstructorsVersusSetterMethodsUnitTest {
     @Test
-    public void testSetUsername() {
+    public void givenNewUser_whenSettingUsername_thenUsernameIsSet() {
         User user = new User();
         user.setUsername("john_doe");
         assertEquals("john_doe", user.getUsername());
     }
 
     @Test
-    public void testSetPassword() {
+    public void givenNewUser_whenSettingPassword_thenPasswordIsSet() {
         User user = new User();
         user.setPassword("strongPassword123");
         assertEquals("strongPassword123", user.getPassword());
     }
 
     @Test
-    public void testProductConstructor() {
+    public void givenProductDetails_whenCreatingProductWithConstructor_thenProductHasCorrectAttributes() {
         Product product = new Product("Smartphone", 599.99, "Electronics");
         assertEquals("Smartphone", product.getName());
         assertEquals(599.99, product.getPrice(), 0.001);
