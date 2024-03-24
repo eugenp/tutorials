0a45bfdd91de93bfc9df6e86b4d8ac16dde18dbb
Sachin kumar <sachin.n.kumar@oracle.com>
2024-03-24 12:13:35 +0530

    Prototype Scoped Bean With Runtime Arguments

--- spring-di-4/src/test/java/com/baeldung/prototypebean/dynamicarguments/DynamicBeanUnitTest.java
+++ spring-di-4/src/test/java/com/baeldung/prototypebean/dynamicarguments/DynamicBeanUnitTest.java
@@ -17,21 +17,18 @@ public class DynamicBeanUnitTest {
     @Test
     public void givenPrototypeBean_WhenFunction_ThenNewInstanceReturn() {
 
-        AbstractApplicationContext context = new AnnotationConfigApplicationContext(
-                EmployeeConfig.class);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
         EmployeeBeanUsingFunction firstContext = context.getBean(EmployeeBeanUsingFunction.class);
         EmployeeBeanUsingFunction secondContext = context.getBean(EmployeeBeanUsingFunction.class);
         Employee firstInstance = firstContext.getEmployee("sachin");
         Employee secondInstance = secondContext.getEmployee("kumar");
-
         Assert.assertTrue(firstInstance != secondInstance);
     }
 
     @Test
     public void givenPrototypeBean_WhenLookup_ThenNewInstanceReturn() {
 
-        AbstractApplicationContext context = new AnnotationConfigApplicationContext(
-                EmployeeConfig.class);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
         EmployeeBeanUsingLookUp firstContext = context.getBean(EmployeeBeanUsingLookUp.class);
         EmployeeBeanUsingLookUp secondContext = context.getBean(EmployeeBeanUsingLookUp.class);
         Employee firstInstance = firstContext.getEmployee("sachin");
@@ -41,12 +38,9 @@ public class DynamicBeanUnitTest {
 
     @Test
     public void givenPrototypeBean_WhenObjectProvider_ThenNewInstanceReturn() {
-        AbstractApplicationContext context = new AnnotationConfigApplicationContext(
-                EmployeeConfig.class);
-        EmployeeBeanUsingObjectProvider firstContext = context
-                .getBean(EmployeeBeanUsingObjectProvider.class);
-        EmployeeBeanUsingObjectProvider secondContext = context
-                .getBean(EmployeeBeanUsingObjectProvider.class);
+        AbstractApplicationContext context = new AnnotationConfigApplicationContext(EmployeeConfig.class);
+        EmployeeBeanUsingObjectProvider firstContext = context.getBean(EmployeeBeanUsingObjectProvider.class);
+        EmployeeBeanUsingObjectProvider secondContext = context.getBean(EmployeeBeanUsingObjectProvider.class);
         Employee firstInstance = firstContext.getEmployee("sachin");
         Employee secondInstance = secondContext.getEmployee("kumar");
         Assert.assertTrue(firstInstance != secondInstance);
