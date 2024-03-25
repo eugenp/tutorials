76c1a02aa83d3a19b32d725d51a5affcb56aafba
vunamtien <vunamtien16@gmail.com>
2024-03-26 05:20:02 +0700

    [JAVA-30997] upgrade spring-boot-mvc to boot 3 (#16190)
    * [JAVA-30997] upgrade spring-boot-mvc to boot 3
    
    * [JAVA-30997] upgrade spring-boot-mvc to boot 3

--- spring-boot-modules/spring-boot-mvc/src/main/java/com/baeldung/annotation/servletcomponentscan/components/EchoServlet.java
+++ spring-boot-modules/spring-boot-mvc/src/main/java/com/baeldung/annotation/servletcomponentscan/components/EchoServlet.java
@@ -1,9 +1,10 @@
 package com.baeldung.annotation.servletcomponentscan.components;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
