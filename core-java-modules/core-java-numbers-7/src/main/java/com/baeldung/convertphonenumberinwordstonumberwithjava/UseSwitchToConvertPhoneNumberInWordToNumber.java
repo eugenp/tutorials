ca56fbad66e0678886dca5ee3b817b613ffc792b
Mehvish Ashiq <62153684+mehvishashiq@users.noreply.github.com>
2024-03-26 22:22:16 +0500

    Update UseSwitchToConvertPhoneNumberInWordToNumber.java

--- core-java-modules/core-java-numbers-7/src/main/java/com/baeldung/convertphonenumberinwordstonumberwithjava/UseSwitchToConvertPhoneNumberInWordToNumber.java
+++ core-java-modules/core-java-numbers-7/src/main/java/com/baeldung/convertphonenumberinwordstonumberwithjava/UseSwitchToConvertPhoneNumberInWordToNumber.java
@@ -1,87 +1,40 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class UseSwitchToConvertPhoneNumberInWordToNumber {
 
-    public static String ConvertPhoneNumberInWordToNumber(String phoneNumberInWord) {
+    public static String convertPhoneNumberInWordToNumber(String phoneNumberInWord) {
 
-        boolean format = isValidPhoneNumberFormat(phoneNumberInWord);
-        String convertedNumber = "", temp = "";
+        StringBuilder output = new StringBuilder();
+        Integer currentMultiplier = null;
+        String[] words = phoneNumberInWord.split(" ");
 
-
-        if (format) {
-            List<String> phoneNumberInWordArray = Arrays.asList(phoneNumberInWord.split(" "));
-
-            for (int i = 0; i < phoneNumberInWordArray.size(); i++) {
-                temp = mapModifiers(phoneNumberInWordArray.get(i));
-                if (temp.equals("-1")) {
-                    convertedNumber += mapIndividualDigits(phoneNumberInWordArray.get(i));
-                } else {
-                    convertedNumber += String.join("", Collections.nCopies(Integer.parseInt(temp), mapIndividualDigits(phoneNumberInWordArray.get(i + 1))));
-                    i++;
+        for (String word : words) {
+            Integer multiplier = getWordAsMultiplier(word);
+            if (multiplier != null) {
+                if (currentMultiplier != null) {
+                    throw new IllegalArgumentException("Cannot have consecutive multipliers, at: " + word);
                 }
+                currentMultiplier = multiplier;
+            } else {
+                output.append(getWordAsDigit(word).repeat(currentMultiplier != null ? currentMultiplier : 1));
+                currentMultiplier = null;
             }
         }
-
-
-        if (convertedNumber.length() > 10) {
-            System.out.println("Incorrect Phone Number! The correct one is 10-digit long.");
-            return null;
-        }
-
-        return convertedNumber;
-
+        return output.toString();
     }
 
-    public static boolean isValidPhoneNumberFormat(String phoneNumberInWord) {
-
-        String regex = "^(double|triple|quadruple|zero|one|two|three|four|five|six|seven|eight|nine)$";
-
-        phoneNumberInWord = phoneNumberInWord.toLowerCase();
-
-        if (phoneNumberInWord.endsWith("double") ||
-                phoneNumberInWord.endsWith("triple") ||
-                phoneNumberInWord.endsWith("quadruple")) {
-            System.out.println("A Phone Number Can't End with a Numerical Prefix");
-            return false;
-        } else {
-            List<String> phoneNumberInWordArray = Arrays.asList(phoneNumberInWord.split(" "));
-            for (int i = 0; i < phoneNumberInWordArray.size(); i++) {
-                if ((isNumericalPrefix(phoneNumberInWordArray.get(i)) && isNumericalPrefix(phoneNumberInWordArray.get(i + 1)))) {
-                    System.out.println("Consecutive Numerical Prefixes Detected.");
-                    return false;
-                }
-                if (!phoneNumberInWordArray.get(i).matches(regex)) {
-                    System.out.println("Invalid Numberical Prefix or Digit in Words! Try again");
-                    return false;
-                }
-            }
-
-        }
-        return true;
-    }
-
-    public static boolean isNumericalPrefix(String word) {
-        return word.equals("double") || word.equals("triple") || word.equals("quadruple");
-    }
-
-    public static String mapModifiers(String word) {
-
+    public static Integer getWordAsMultiplier(String word) {
         switch (word) {
             case "double":
-                return "2";
+                return 2;
             case "triple":
-                return "3";
+                return 3;
             case "quadruple":
-                return "4";
+                return 4;
             default:
-                return "-1";
+                return null;
         }
-
     }
 
-    public static String mapIndividualDigits(String word) {
+    public static String getWordAsDigit(String word) {
         switch (word) {
             case "zero":
                 return "0";
@@ -104,7 +57,8 @@ public class UseSwitchToConvertPhoneNumberInWordToNumber {
             case "nine":
                 return "9";
             default:
-                return "-1";
+                throw new IllegalArgumentException("Invalid word: " + word);
         }
     }
+
 }
