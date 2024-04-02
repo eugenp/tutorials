41b4891ed5078f9940c963e539ecaa7f8a9942a4
Usman Mohyuddin <mr.usman76@gmail.com>
2024-04-02 12:21:01 +0500

    add tests for skip & limit in MongoRepository

--- persistence-modules/spring-data-mongodb-2/src/main/java/com/baeldung/projection/repository/StudentRepository.java
+++ persistence-modules/spring-data-mongodb-2/src/main/java/com/baeldung/projection/repository/StudentRepository.java
@@ -1,5 +1,7 @@
 package com.baeldung.projection.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -11,13 +13,13 @@ public interface StudentRepository extends MongoRepository<Student, String> {
     @Aggregation(pipeline = {"{ $skip: ?0 }, { $limit: ?1 }"})
     List<Student> findAll(Long skip, Long limit);
     @Aggregation(pipeline = {"{ '$match': { 'age': ?0 } }", "{ $skip: ?1 }", "{ $limit: ?2 }"})
-    List<Student> findAllWithAgeCriteria(Long age, Long skip, Long limit);
-
+    List<Student> findAllByAgeCriteria(Long age, Long skip, Long limit);
     @Aggregation(pipeline = {
             "{ '$match': { 'id' : ?0 } }",
             "{ '$sort' : { 'id' : 1 } }",
             "{ '$skip' : ?1 }",
             "{ '$limit' : ?2 }"
     })
-    List<Student> findByStudentId(final String id, Long skip, Long limit);
+    List<Student> findByStudentId(final String studentId, Long skip, Long limit);
+    Page<Student> findAll(Pageable pageable);
 }
--- persistence-modules/spring-data-mongodb-2/src/test/java/com/baeldung/projection/StudentIntegrationTest.java
+++ persistence-modules/spring-data-mongodb-2/src/test/java/com/baeldung/projection/StudentIntegrationTest.java
@@ -7,6 +7,10 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -35,23 +39,27 @@ public class StudentIntegrationTest {
     }
 
     @Test
-    public void testFindAllWithPagination() {
+    public void testFindAll() {
         // WHEN
         List<Student> result = studentRepository.findAll(0L, 5L);
         // THEN
         assertEquals(5, result.size());
+    }
 
+    @Test
+    public void testFindAllWithLimitAndSkip() {
         // WHEN
-        result = studentRepository.findAll(4L, 1L);
+        List<Student> result = studentRepository.findAll(3L, 2L);
         // THEN
-        assertEquals(1, result.size());
-        assertEquals("Maria", result.get(0).getName());
+        assertEquals(2, result.size());
+        assertEquals("Tina", result.get(0).getName());
+        assertEquals("Maria", result.get(1).getName());
     }
 
     @Test
     public void testFindAllWithAgeCriteria() {
         // WHEN
-        List<Student> result = studentRepository.findAllWithAgeCriteria(30L, 0L, 10L);
+        List<Student> result = studentRepository.findAllByAgeCriteria(30L, 0L, 10L);
         // THEN
         assertEquals(1, result.size());
         assertEquals("Usman", result.get(0).getName());
@@ -59,11 +67,24 @@ public class StudentIntegrationTest {
 
     @Test
     public void testFindByStudentId() {
-
         // WHEN
         List<Student> result = studentRepository.findByStudentId("A", 0L, 5L);
         // THEN
         assertEquals(1, result.size());
         assertEquals("Abraham", result.get(0).getName());
+    }
+
+    @Test
+    public void testFindByStudentIdUsingPageable() {
+        // GIVEN
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        Pageable pageable = PageRequest.of(0, 5, sort);
+
+        // WHEN
+        Page<Student> resultPage = studentRepository.findAll(pageable);
+
+        // THEN
+        assertEquals(5, resultPage.getTotalElements());
+        assertEquals("Maria", resultPage.getContent().get(0).getName());
     }
 }
