f1686f4b236f145fe7edd829294f28ca9c18ca53
LordMaduz <maduz.ruchira@gmail.com>
2024-04-03 12:14:48 +0800

    [BAEL-7546] - How to intercept a request and add headers in WebFlux * CICD Build Fix for PMD Violation

--- spring-reactive-modules/spring-reactive-4/src/test/java/com/baeldung/SpringReactiveContextTest.java
+++ spring-reactive-modules/spring-reactive-4/src/test/java/com/baeldung/SpringReactiveContextTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringReactiveApplicationTests {
+class SpringReactiveContextTest {
 
 	@Test
 	void whenSpringContextLoads_thenNoException() {
