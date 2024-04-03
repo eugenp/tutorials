4bd27967e83496e71e36c95d448afa3dbd70101b
LordMaduz <maduz.ruchira@gmail.com>
2024-04-03 11:41:09 +0800

    [BAEL-7546] - How to intercept a request and add headers in WebFlux * Added the Implementation Code.

---
+++ spring-reactive-modules/spring-reactive-4/src/main/java/com/baeldung/controller/TraceController.java
@@ -0,0 +1,16 @@
+package com.baeldung.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+public class TraceController {
+
+
+    @GetMapping(value = "/trace-annotated")
+    public Mono<String> trace(@RequestHeader(name = "traceId") final String traceId) {
+        return Mono.just("TraceId: ".concat(traceId));
+    }
+}
