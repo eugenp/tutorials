d8b5d22fa3ec3f12da62acd997df37a2644f4606
Kai.Yuan <kent.yuan@Gmail.com>
2024-04-08 11:36:50 +0200

    [improvement-init-Long-list] rename package

--- core-java-modules/core-java-collections-list-2/src/test/java/com/baeldung/java/listinitialization/ListInitializationUnitTest.java
+++ core-java-modules/core-java-collections-list-2/src/test/java/com/baeldung/java/listinitialization/ListInitializationUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.java.listInitialization;
+package com.baeldung.java.listinitialization;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
