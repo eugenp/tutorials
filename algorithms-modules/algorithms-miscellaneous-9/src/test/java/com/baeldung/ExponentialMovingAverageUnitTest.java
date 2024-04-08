ac32eebb539447741c6e80d8dc6473abac2e551d
Jagath Kumar <1611881-jagathks@users.noreply.gitlab.com>
2024-04-08 17:03:26 +0530

    calculating Moving averages - review fixes

--- algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/ExponentialMovingAverageUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/ExponentialMovingAverageUnitTest.java
@@ -1,8 +1,7 @@
 package com.baeldung;
 
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class ExponentialMovingAverageUnitTest {
 
--- algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/MovingAverageByCircularBufferUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/MovingAverageByCircularBufferUnitTest.java
@@ -1,7 +1,6 @@
 package com.baeldung;
 
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class MovingAverageByCircularBufferUnitTest {
