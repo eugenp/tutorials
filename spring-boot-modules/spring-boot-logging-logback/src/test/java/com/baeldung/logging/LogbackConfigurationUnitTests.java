32eed07794c3f5e60e452fe241e75c9d5ba17b59
achraftt <achraf.taitai1@gmail.com>
2024-04-10 21:53:53 +0200

    BAEL-7762: TEST

--- spring-boot-modules/spring-boot-logging-logback/src/test/java/com/baeldung/logging/LogbackConfigurationUnitTests.java
+++ spring-boot-modules/spring-boot-logging-logback/src/test/java/com/baeldung/logging/LogbackConfigurationUnitTests.java
@@ -1,14 +1,19 @@
-package com.baeldung;
+package com.baeldung.logging;
 
-import com.baeldung.logging.LogbackConfiguration;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {LogbackConfiguration.class})
 public class LogbackConfigurationUnitTests {
 
-    private LogbackConfiguration logbackConfiguration = new LogbackConfiguration();
+    @Autowired
+    LogbackConfiguration logbackConfiguration;
 
     @Test
     public void givenLogbackConfigurationFile_whenSettingLogbackConfiguration_thenFileLocationSet() {
