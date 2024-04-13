b54edcf99d2ceee744cc39d87f7aa2eadf3b1e11
Usman Mohyuddin <mr.usman76@gmail.com>
2024-04-13 13:36:10 +0500

    fix spaces

--- persistence-modules/spring-data-mongodb-2/src/main/java/com/baeldung/projection/repository/StudentRepository.java
+++ persistence-modules/spring-data-mongodb-2/src/main/java/com/baeldung/projection/repository/StudentRepository.java
@@ -10,15 +10,22 @@ import com.baeldung.projection.model.Student;
 import java.util.List;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
-    @Aggregation(pipeline = {"{ $skip: ?0 }, { $limit: ?1 }"})
+    @Aggregation(pipeline = {
+      "{ '$skip': ?0 }",
+      "{ '$limit': ?1 }"
+    })
     List<Student> findAll(Long skip, Long limit);
-    @Aggregation(pipeline = {"{ '$match': { 'age': ?0 } }", "{ $skip: ?1 }", "{ $limit: ?2 }"})
+    @Aggregation(pipeline = {
+      "{ '$match': { 'age': ?0 } }",
+      "{ $skip: ?1 }",
+      "{ $limit: ?2 }"
+    })
     List<Student> findAllByAgeCriteria(Long age, Long skip, Long limit);
     @Aggregation(pipeline = {
-            "{ '$match': { 'id' : ?0 } }",
-            "{ '$sort' : { 'id' : 1 } }",
-            "{ '$skip' : ?1 }",
-            "{ '$limit' : ?2 }"
+      "{ '$match': { 'id' : ?0 } }",
+      "{ '$sort' : { 'id' : 1 } }",
+      "{ '$skip' : ?1 }",
+      "{ '$limit' : ?2 }"
     })
     List<Student> findByStudentId(final String studentId, Long skip, Long limit);
     Page<Student> findAll(Pageable pageable);
