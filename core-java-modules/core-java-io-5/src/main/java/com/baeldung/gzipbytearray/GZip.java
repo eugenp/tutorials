1a1c32b4bdc3c1b7adbb982ce0d579b22827c5ea
p.kostopoulos <p.kostopoulos@astynomia.gr>
2024-04-18 10:11:13 +0300

    [BAEL-7659] Compress and Create a Byte Array Using GZip

--- core-java-modules/core-java-io-5/src/main/java/com/baeldung/gzipbytearray/GZip.java
+++ core-java-modules/core-java-io-5/src/main/java/com/baeldung/gzipbytearray/GZip.java
@@ -18,12 +18,5 @@ public class GZip {
 		}
 		gzipOs.close();
 	}
-	
-	public static String byteToHex(byte num) {
-	    char[] hexDigits = new char[2];
-	    hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
-	    hexDigits[1] = Character.forDigit((num & 0xF), 16);
-	    return new String(hexDigits);
-	}
 
 }
