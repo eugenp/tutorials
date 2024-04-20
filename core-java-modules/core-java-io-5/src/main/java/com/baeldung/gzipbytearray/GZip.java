4a17543dbf077fc7dc7c606e014cdbf449b374f3
p.kostopoulos <p.kostopoulos@astynomia.gr>
2024-04-20 14:47:46 +0300

    [BAEL-7659] Compress and create a byte array using GZip

--- core-java-modules/core-java-io-5/src/main/java/com/baeldung/gzipbytearray/GZip.java
+++ core-java-modules/core-java-io-5/src/main/java/com/baeldung/gzipbytearray/GZip.java
@@ -7,16 +7,16 @@ import java.util.zip.GZIPOutputStream;
 
 public class GZip {
 
-	private static final int BUFFER_SIZE = 512;
+    private static final int BUFFER_SIZE = 512;
 
-	public static void gzip(InputStream is, OutputStream os) throws IOException {
-		GZIPOutputStream gzipOs = new GZIPOutputStream(os);
-		byte[] buffer = new byte[BUFFER_SIZE];
-		int bytesRead = 0;
-		while ((bytesRead = is.read(buffer)) > -1) {
-			gzipOs.write(buffer, 0, bytesRead);
-		}
-		gzipOs.close();
-	}
+    public static void gzip(InputStream is, OutputStream os) throws IOException {
+        GZIPOutputStream gzipOs = new GZIPOutputStream(os);
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytesRead = 0;
+        while ((bytesRead = is.read(buffer)) > -1) {
+            gzipOs.write(buffer, 0, bytesRead);
+        }
+        gzipOs.close();
+    }
 
 }
--- core-java-modules/core-java-io-5/src/test/java/com/baeldung/gzipbytearray/GZipUnitTest.java
+++ core-java-modules/core-java-io-5/src/test/java/com/baeldung/gzipbytearray/GZipUnitTest.java
@@ -13,18 +13,18 @@ import org.slf4j.LoggerFactory;
 
 public class GZipUnitTest {
 
-	Logger logger = LoggerFactory.getLogger(GZipUnitTest.class);
+    Logger logger = LoggerFactory.getLogger(GZipUnitTest.class);
 
-	@Test
-	void whenCompressingUsingGZip_thenGetCompressedByteArray() throws IOException {
-		String payload = "This is a sample text to test method gzip. The gzip algorithm will compress this string. "
-				+ "The result will be smaller than this string.";
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		GZip.gzip(new ByteArrayInputStream(payload.getBytes()), os);
-		byte[] compressed = os.toByteArray();
-		assertTrue(payload.getBytes().length > compressed.length);
-		assertEquals("1f", Integer.toHexString(compressed[0] & 0xFF));
-		assertEquals("8b", Integer.toHexString(compressed[1] & 0xFF));
-	}
+    @Test
+    void whenCompressingUsingGZip_thenGetCompressedByteArray() throws IOException {
+        String payload = "This is a sample text to test method gzip. The gzip algorithm will compress this string. "
+            + "The result will be smaller than this string.";
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        GZip.gzip(new ByteArrayInputStream(payload.getBytes()), os);
+        byte[] compressed = os.toByteArray();
+        assertTrue(payload.getBytes().length > compressed.length);
+        assertEquals("1f", Integer.toHexString(compressed[0] & 0xFF));
+        assertEquals("8b", Integer.toHexString(compressed[1] & 0xFF));
+    }
 
 }
