66fedbf9a13c17efc7712d4534c9d56a2c2b9c35
yabetancourt <yadierbetanc@gmail.com>
2024-04-20 17:06:27 -0400

    BAEL-7831 Casting maps to objects

---
+++ core-java-modules/core-java-collections-maps-8/src/main/java/com/baeldung/map/castingmaptoobject/Country.java
@@ -0,0 +1,20 @@
+package com.baeldung.map.castingmaptoobject;
+
+public class Country {
+    private String name;
+
+    public Country() {
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/castingmaptoobject/CastingMapToObjectUnitTest.java
@@ -0,0 +1,68 @@
+package com.baeldung.map.castingmaptoobject;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
+import org.apache.commons.beanutils.BeanUtils;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CastingMapToObjectUnitTest {
+
+    private static final Map<String, Object> map = Map.of(
+        "id", 1L,
+        "name", "Baeldung",
+        "addresses", List.of(
+                new Address("La Havana", new Country("Cuba")),
+                new Address("Paris", new Country("France"))
+        )
+    );
+
+    @Test
+    void givenMap_whenUsingBeanUtils_thenConvertToObject() throws InvocationTargetException, IllegalAccessException {
+        User user = new User();
+        BeanUtils.populate(user, map);
+
+        assertEquals(user.getId(), map.get("id"));
+        assertEquals(user.getName(), map.get("name"));
+        assertEquals(user.getAddresses(), map.get("addresses"));
+    }
+
+    @Test
+    void givenMap_whenUsingJackson_thenConvertToObject() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = objectMapper.convertValue(map, User.class);
+
+        assertEquals(user.getId(), map.get("id"));
+        assertEquals(user.getName(), map.get("name"));
+        assertEquals(user.getAddresses(), map.get("addresses"));
+    }
+
+    @Test
+    void givenMap_whenUsingJacksonIgnoreUnknownProps_thenConvertToObject() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        User user = objectMapper.convertValue(map, User.class);
+
+        assertEquals(user.getId(), map.get("id"));
+        assertEquals(user.getName(), map.get("name"));
+        assertEquals(user.getAddresses(), map.get("addresses"));
+    }
+
+    @Test
+    void givenMap_whenUsingGson_thenConvertToObject() {
+        Gson gson = new Gson();
+        String jsonMap = gson.toJson(map);
+        User user = gson.fromJson(jsonMap, User.class);
+
+        assertEquals(user.getId(), map.get("id"));
+        assertEquals(user.getName(), map.get("name"));
+        assertEquals(user.getAddresses(), map.get("addresses"));
+    }
+
+}
