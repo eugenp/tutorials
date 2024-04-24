9a7230df97b11e985a467c8e7478711d10a6a3e0
Lucian Snare <lsnare1@gmail.com>
2024-04-24 08:33:18 -0400

    Move Book and BookService to separate classes

--- testing-modules/testng/src/test/java/com/baeldung/testng/softassert/SoftAssertionUnitTest.java
+++ testing-modules/testng/src/test/java/com/baeldung/testng/softassert/SoftAssertionUnitTest.java
@@ -40,41 +40,4 @@ public class SoftAssertionUnitTest {
         // softAssert.assertNotNull(gatsby.author, "author");
         // softAssert.assertAll();
     }
-
-    interface BookService {
-        Book getBook(String isbn);
-    }
-
-    static class Book {
-        private String isbn;
-        private String title;
-        private String author;
-
-        public Book() {
-        }
-
-        public String getIsbn() {
-            return isbn;
-        }
-
-        public void setIsbn(String isbn) {
-            this.isbn = isbn;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public void setTitle(String title) {
-            this.title = title;
-        }
-
-        public String getAuthor() {
-            return author;
-        }
-
-        public void setAuthor(String author) {
-            this.author = author;
-        }
-    }
 }
