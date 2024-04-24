7b3b52c224096a988f0be74007fdb385578bf188
Kai.Yuan <kent.yuan@Gmail.com>
2024-04-24 21:28:19 +0200

    [int-to-short] add intToShort()

--- core-java-modules/core-java-numbers-conversions-2/src/test/java/com/baeldung/inttoshort/ConvertIntToShortUnitTest.java
+++ core-java-modules/core-java-numbers-conversions-2/src/test/java/com/baeldung/inttoshort/ConvertIntToShortUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.inttoshort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -31,6 +32,24 @@ class ConvertIntToShortUnitTest {
         int twoMillion = 2_000_000;
         result = (short) twoMillion;
         assertEquals(-31616, result);
+    }
+
+    short intToShort(int i) {
+        if (i < Short.MIN_VALUE || i > Short.MAX_VALUE) {
+            throw new IllegalArgumentException("Int is out of short range");
+        }
+        return (short) i;
+    }
+
+    @Test
+    void whenCheckShortRangeBeforeCasting_thenGetExpectedResult() {
+        short expected = 42;
+        int int42 = 42;
+        assertEquals(expected, intToShort(int42));
+
+        int oneMillion = 1_000_000;
+        assertThrows(IllegalArgumentException.class, () -> intToShort(oneMillion));
+
     }
 
 }
