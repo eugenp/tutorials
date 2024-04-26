abed73ae6e3e2826c6bcf41dbd0fda7b03d86751
p.kostopoulos <p.kostopoulos@astynomia.gr>
2024-04-26 10:19:39 +0300

    [BAEL-7786] How to Unit test an ExecutorService without using Thread.sleep

---
+++ core-java-modules/core-java-concurrency-advanced-5/src/test/java/com/baeldung/executorservicetest/ExecutorServiceUnitTest.java
@@ -0,0 +1,54 @@
+package com.baeldung.executorservicetest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+
+public class ExecutorServiceUnitTest {
+
+    @Test
+    void whenNoBlocking_thenTestFails() {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        MyRunnable r = new MyRunnable();
+        executorService.submit(r);
+        assertEquals(null, r.getResult());
+    }
+
+    @Test
+    void whenUsingFuture_thenTestSucceeds() throws InterruptedException, ExecutionException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        MyRunnable r = new MyRunnable();
+        Future<?> future = executorService.submit(r);
+        future.get();
+        assertEquals(2305843005992468481L, r.getResult());
+    }
+
+    @Test
+    void whenShuttingDownExecutorAndWait_thenTestSucceeds() throws InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        MyRunnable r = new MyRunnable();
+        executorService.submit(r);
+        executorService.shutdown();
+        executorService.awaitTermination(10000, TimeUnit.SECONDS);
+        assertEquals(2305843005992468481L, r.getResult());
+    }
+
+    @Test
+    void whenTwoTasksAndShuttingDownExecutorAndWait_thenTestSucceeds() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        MyRunnable r1 = new MyRunnable();
+        MyRunnable r2 = new MyRunnable();
+        executorService.submit(r1);
+        executorService.submit(r2);
+        executorService.shutdown();
+        executorService.awaitTermination(10000, TimeUnit.SECONDS);
+        assertEquals(2305843005992468481L, r1.getResult());
+        assertEquals(2305843005992468481L, r2.getResult());
+    }
+}
