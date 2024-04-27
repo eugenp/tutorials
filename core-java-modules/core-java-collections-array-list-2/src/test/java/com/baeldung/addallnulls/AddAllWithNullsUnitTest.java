6d1ca7e4a7de8ece83ca7ad416227ee60fb19c8b
Eugene Kovko <eu.kovko@gmail.com>
2024-04-27 18:18:40 +0200

    BAEL-7885: Stream test example

--- core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/addallnulls/AddAllWithNullsUnitTest.java
+++ core-java-modules/core-java-collections-array-list-2/src/test/java/com/baeldung/addallnulls/AddAllWithNullsUnitTest.java
@@ -5,9 +5,12 @@ import static org.assertj.core.api.Assertions.assertThatNoException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 import org.apache.commons.collections4.CollectionUtils;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 
 class AddAllWithNullsUnitTest {
@@ -16,26 +19,25 @@ class AddAllWithNullsUnitTest {
     @NullSource
     void givenNull_whenAddAll_thenAddThrowsNPE(List<String> list) {
         ArrayList<String> strings = new ArrayList<>();
-        assertThatExceptionOfType(NullPointerException.class)
-          .isThrownBy(() -> strings.addAll(list));
+        assertThatExceptionOfType(NullPointerException.class).isThrownBy(() -> strings.addAll(list));
     }
 
     @ParameterizedTest
     @NullSource
     void givenNull_whenAddAllWithCheck_thenNoNPE(List<String> list) {
         ArrayList<String> strings = new ArrayList<>();
-        assertThatNoException().isThrownBy( () -> {
-              if (list != null) {
-                  strings.addAll(list);
-              }
-          });
+        assertThatNoException().isThrownBy(() -> {
+            if (list != null) {
+                strings.addAll(list);
+            }
+        });
     }
 
     @ParameterizedTest
     @NullSource
     void givenNull_whenAddAllWithCollectionCheck_thenNoNPE(List<String> list) {
         ArrayList<String> strings = new ArrayList<>();
-        assertThatNoException().isThrownBy( () -> {
+        assertThatNoException().isThrownBy(() -> {
             strings.addAll(CollectionUtils.emptyIfNull(list));
         });
     }
@@ -44,7 +46,7 @@ class AddAllWithNullsUnitTest {
     @NullSource
     void givenNull_whenAddAllWithExternalizedCheck_thenNoNPE(List<String> list) {
         ArrayList<String> strings = new ArrayList<>();
-        assertThatNoException().isThrownBy( () -> {
+        assertThatNoException().isThrownBy(() -> {
             addIfNonNull(list, strings);
         });
     }
@@ -59,8 +61,29 @@ class AddAllWithNullsUnitTest {
     @NullSource
     void givenNull_whenAddAllWithOptional_thenNoNPE(List<String> list) {
         ArrayList<String> strings = new ArrayList<>();
-        assertThatNoException().isThrownBy( () -> {
+        assertThatNoException().isThrownBy(() -> {
             Optional.ofNullable(list).ifPresent(strings::addAll);
         });
+    }
+
+    @ParameterizedTest
+    @MethodSource("listProvider")
+    void givenCollectionOfNullableLists_whenFilter_thenNoNPE(List<List<String>> listOfLists) {
+        ArrayList<String> strings = new ArrayList<>();
+        assertThatNoException().isThrownBy(() -> {
+            listOfLists.stream().filter(Objects::nonNull).forEach(strings::addAll);
+        });
+    }
+
+    static Stream<List<List<String>>> listProvider() {
+        ArrayList<List<String>> lists = new ArrayList<>();
+        lists.add(null);
+        lists.add(List.of("Hello"));
+        lists.add(null);
+        lists.add(List.of("World"));
+        lists.add(null);
+        return Stream.of(
+          lists
+        );
     }
 }
