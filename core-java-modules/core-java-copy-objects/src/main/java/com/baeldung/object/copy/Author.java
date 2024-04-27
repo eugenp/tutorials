673510f02915ab798756b91410d18bab9df5eaa4
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-04-27 20:01:52 +0530

    adding Author pojo

---
+++ core-java-modules/core-java-copy-objects/src/main/java/com/baeldung/object/copy/Author.java
@@ -0,0 +1,34 @@
+package com.baeldung.object.copy;
+
+public class Author {
+
+	private String name;
+	private String email;
+
+	public Author(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+
+	public Author(Author author) {
+		this.name = author.getName();
+		this.email = author.getEmail();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
