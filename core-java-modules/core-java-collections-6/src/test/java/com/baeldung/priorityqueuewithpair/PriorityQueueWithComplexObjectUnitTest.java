ca96626221c39f7d6f9d5252d8b1077ac09e0ed0
Eugene Kovko <eu.kovko@gmail.com>
2024-04-27 15:42:41 +0200

    BAEL-7853: Additional example with Meetings

--- core-java-modules/core-java-collections-6/src/test/java/com/baeldung/priorityqueuewithpair/PriorityQueueWithComplexObjectUnitTest.java
+++ core-java-modules/core-java-collections-6/src/test/java/com/baeldung/priorityqueuewithpair/PriorityQueueWithComplexObjectUnitTest.java
@@ -3,15 +3,39 @@ package com.baeldung.priorityqueuewithpair;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import java.time.LocalDateTime;
+import java.util.AbstractMap;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.stream.Stream;
 import org.apache.commons.math3.util.Pair;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class PriorityQueueWithComplexObjectUnitTest {
+
+    @Test
+    void givenMeetings_whenUseWithPriorityQueue_thenSortByStartDateTime() {
+        Meeting projectDiscussion = new Meeting(
+          LocalDateTime.parse("2025-11-10T19:00:00"),
+          LocalDateTime.parse("2025-11-10T20:00:00"),
+          "Project Discussion"
+        );
+        Meeting businessMeeting = new Meeting(
+          LocalDateTime.parse("2025-11-15T14:00:00"),
+          LocalDateTime.parse("2025-11-15T16:00:00"),
+          "Business Meeting"
+        );
+        PriorityQueue<Meeting> meetings = new PriorityQueue<>();
+        meetings.add(projectDiscussion);
+        meetings.add(businessMeeting);
+
+        assertThat(meetings.poll()).isEqualTo(projectDiscussion);
+        assertThat(meetings.poll()).isEqualTo(businessMeeting);
+    }
 
     @ParameterizedTest
     @MethodSource("pairProvider")
@@ -23,6 +47,20 @@ class PriorityQueueWithComplexObjectUnitTest {
         while (!queue.isEmpty()) {
             Pair<String, Integer> currentEntry = queue.poll();
             assertThat(previousEntry.getSecond()).isLessThanOrEqualTo(currentEntry.getSecond());
+            previousEntry = currentEntry;
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("mapEntryProvider")
+    void givenMapEntries_whenUsePriorityQueue_thenSortThemBySecondElement(List<Map.Entry<String, Integer>> pairs) {
+        PriorityQueue<Map.Entry<String, Integer>> queue = new PriorityQueue<>(Comparator.comparingInt(Map.Entry::getValue));
+
+        queue.addAll(pairs);
+        Map.Entry<String, Integer> previousEntry = queue.poll();
+        while (!queue.isEmpty()) {
+            Map.Entry<String, Integer> currentEntry = queue.poll();
+            assertThat(previousEntry.getValue()).isLessThanOrEqualTo(currentEntry.getValue());
             previousEntry = currentEntry;
         }
     }
@@ -83,6 +121,38 @@ class PriorityQueueWithComplexObjectUnitTest {
             new Pair<>("The Stars My Destination", 1956),
             new Pair<>("Rendezvous with Rama", 1973),
             new Pair<>("The Forever War", 1974)
+          )
+        );
+    }
+
+    static Stream<List> mapEntryProvider() {
+        return Stream.of(
+          List.of(
+            new AbstractMap.SimpleEntry<>("Dune", 1965),
+            new AbstractMap.SimpleEntry<>("Foundation", 1951),
+            new AbstractMap.SimpleEntry<>("2001: A Space Odyssey", 1968),
+            new AbstractMap.SimpleEntry<>("Do Androids Dream of Electric Sheep?", 1968),
+            new AbstractMap.SimpleEntry<>("Ender's Game", 1985),
+            new AbstractMap.SimpleEntry<>("The Hitchhiker's Guide to the Galaxy", 1979),
+            new AbstractMap.SimpleEntry<>("1984", 1949),
+            new AbstractMap.SimpleEntry<>("Brave New World", 1932),
+            new AbstractMap.SimpleEntry<>("Fahrenheit 451", 1953),
+            new AbstractMap.SimpleEntry<>("Neuromancer", 1984),
+            new AbstractMap.SimpleEntry<>("Stranger in a Strange Land", 1961),
+            new AbstractMap.SimpleEntry<>("The Handmaid's Tale", 1985),
+            new AbstractMap.SimpleEntry<>("Ubik", 1969),
+            new AbstractMap.SimpleEntry<>("Solaris", 1961),
+            new AbstractMap.SimpleEntry<>("Slaughterhouse-Five", 1969),
+            new AbstractMap.SimpleEntry<>("The War of the Worlds", 1898),
+            new AbstractMap.SimpleEntry<>("Childhood's End", 1953),
+            new AbstractMap.SimpleEntry<>("Snow Crash", 1992),
+            new AbstractMap.SimpleEntry<>("The Moon is a Harsh Mistress", 1966),
+            new AbstractMap.SimpleEntry<>("Hyperion", 1989),
+            new AbstractMap.SimpleEntry<>("The Left Hand of Darkness", 1969),
+            new AbstractMap.SimpleEntry<>("The Gods Themselves", 1972),
+            new AbstractMap.SimpleEntry<>("The Stars My Destination", 1956),
+            new AbstractMap.SimpleEntry<>("Rendezvous with Rama", 1973),
+            new AbstractMap.SimpleEntry<>("The Forever War", 1974)
           )
         );
     }
