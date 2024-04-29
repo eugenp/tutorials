64b174934c20b9d8f1dd3f359d83b307c628068e
DESKTOP-CR93LQ5\pkost <pkostop@gmail.com>
2024-04-29 23:31:47 +0300

    [BAEL-7786] How to Unit test an ExecutorService without using Thread.sleep

--- core-java-modules/core-java-concurrency-advanced-5/src/test/java/com/baeldung/executorservicetest/ExecutorServiceUnitTest.java
+++ core-java-modules/core-java-concurrency-advanced-5/src/test/java/com/baeldung/executorservicetest/ExecutorServiceUnitTest.java
@@ -2,10 +2,13 @@ package com.baeldung.executorservicetest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
@@ -50,5 +53,15 @@ public class ExecutorServiceUnitTest {
         executorService.awaitTermination(10000, TimeUnit.SECONDS);
         assertEquals(2305843005992468481L, r1.getResult());
         assertEquals(2305843005992468481L, r2.getResult());
+    }
+   
+    @Test
+    void whenDirectlyUsingThreadPoolExecutor_thenTestSucceeds() throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 1, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
+        MyRunnable r = new MyRunnable();
+        threadPoolExecutor.submit(r);
+        while (threadPoolExecutor.getCompletedTaskCount() < 1) {
+        }
+        assertEquals(2305843005992468481L, r.getResult());
     }
 }
