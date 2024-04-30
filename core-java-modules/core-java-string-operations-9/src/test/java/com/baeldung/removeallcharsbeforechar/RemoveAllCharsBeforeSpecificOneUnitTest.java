6de3922d601ef4f6e72205cc0a69ae72f669b85a
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-04-30 16:22:36 +0300

    Update RemoveAllCharsBeforeSpecificOneUnitTest.java

--- core-java-modules/core-java-string-operations-9/src/test/java/com/baeldung/removeallcharsbeforechar/RemoveAllCharsBeforeSpecificOneUnitTest.java
+++ core-java-modules/core-java-string-operations-9/src/test/java/com/baeldung/removeallcharsbeforechar/RemoveAllCharsBeforeSpecificOneUnitTest.java
@@ -6,27 +6,43 @@ import static org.junit.Assert.assertEquals;
 
 public class RemoveAllCharsBeforeSpecificOneUnitTest {
     String inputString = "Hello World!";
-    char targetCharacter = 'W';
+    char targetCharacter = 'Z';
 
     @Test
     public void givenString_whenUsingSubstring_thenCharactersRemoved() {
-        String result = inputString.substring(inputString.indexOf(targetCharacter));
-        assertEquals("World!", result);
+        int index = inputString.indexOf(targetCharacter);
+
+        if (index != -1) {
+            String result = inputString.substring(index);
+            assertEquals("World!", result);
+        } else {
+            assertEquals(inputString, inputString);
+        }
     }
 
     @Test
     public void givenString_whenUsingRegex_thenCharactersRemoved() {
-        String result = (targetCharacter) + inputString.replaceAll(".*" + targetCharacter, "");
-        assertEquals("World!", result);
+        int index = inputString.indexOf(targetCharacter);
+
+        if (index != -1) {
+            String result = targetCharacter + inputString.replaceAll(".*" + targetCharacter, "");
+            assertEquals("World!", result);
+        } else {
+            assertEquals(inputString, inputString);
+        }
     }
 
     @Test
     public void givenString_whenUsingStringBuilder_thenCharactersRemoved() {
         StringBuilder sb = new StringBuilder(inputString);
         int index = sb.indexOf(String.valueOf(targetCharacter));
+
         if (index != -1) {
             sb.delete(0, index);
+            assertEquals("World!", sb.toString());
+
+        } else {
+            assertEquals(inputString, inputString);
         }
-        assertEquals("World!", sb.toString());
     }
 }
