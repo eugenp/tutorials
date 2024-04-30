40e7d167328a817b2c89b82a715af10f55390aca
Kai.Yuan <kent.yuan@Gmail.com>
2024-04-30 15:34:55 +0200

    [replace-str-to-a-char] section 6. Removing All Characters Util a Specific One

--- core-java-modules/core-java-string-algorithms-2/src/test/java/com/baeldung/replaceremove/StringReplaceAndRemoveUnitTest.java
+++ core-java-modules/core-java-string-algorithms-2/src/test/java/com/baeldung/replaceremove/StringReplaceAndRemoveUnitTest.java
@@ -4,10 +4,13 @@ import org.apache.commons.lang3.RegExUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class StringReplaceAndRemoveUnitTest {
+
+    private static final String INPUT = "some prefix=Important Info: a=b";
 
     @Test
     public void givenTestStrings_whenReplace_thenProcessedString() {
@@ -76,7 +79,7 @@ public class StringReplaceAndRemoveUnitTest {
         String sentence = "A car is not the same as a carriage, and some planes can carry cars inside them!";
         String regexTarget = "\\bcar\\b";
         String exactWordReplaced = sentence.replaceAll(regexTarget, "truck");
-        assertTrue("A truck is not the same as a carriage, and some planes can carry cars inside them!".equals(exactWordReplaced));
+        assertEquals("A truck is not the same as a carriage, and some planes can carry cars inside them!", exactWordReplaced);
     }
 
     @Test
@@ -84,7 +87,31 @@ public class StringReplaceAndRemoveUnitTest {
         String sentence = "A car is not the same as a carriage, and some planes can carry cars inside them!";
         String regexTarget = "\\bcar\\b";
         String exactWordReplaced = RegExUtils.replaceAll(sentence, regexTarget, "truck");
-        assertTrue("A truck is not the same as a carriage, and some planes can carry cars inside them!".equals(exactWordReplaced));
+        assertEquals("A truck is not the same as a carriage, and some planes can carry cars inside them!", exactWordReplaced);
+    }
+
+    @Test
+    public void givenTestStrings_whenUsingIndexOfAndSubstring_thenGetExpectedResult() {
+        String result = INPUT.substring(INPUT.indexOf("=") + 1);
+        assertEquals("Important Info: a=b", result);
+    }
+
+    @Test
+    public void givenTestStrings_whenUsingSplit_thenGetExpectedResult() {
+        String result = INPUT.split("=", 2)[1];
+        assertEquals("Important Info: a=b", result);
+    }
+
+    @Test
+    public void givenTestStrings_whenUsingRegexReplaceAll_thenGetExpectedResult() {
+        String wrongResult = INPUT.replaceFirst(".*=", "");
+        assertEquals("b", wrongResult);
+
+        String result = INPUT.replaceFirst(".*?=", "");
+        assertEquals("Important Info: a=b", result);
+
+        result = INPUT.replaceFirst("[^=]*=", "");
+        assertEquals("Important Info: a=b", result);
     }
 
 }
