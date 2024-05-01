31f7fc00afb0abf41492f1870a6f25b32e1f6e79
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-05-01 20:05:45 +0300

    This commit is related to BAEL-6682
    This commit aims to add a test class "PositionKeyValueInLinkedHashMapUsingItsKeyUnitTest".

---
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/valuetokeyset/PositionKeyValueInLinkedHashMapUsingItsKeyUnitTest.java
@@ -0,0 +1,39 @@
+package com.baeldung.map.valuetokeyset;
+
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class PositionKeyValueInLinkedHashMapUsingItsKeyUnitTest {
+    LinkedHashMap<String, Integer> linkedHashMap = new LinkedHashMap<>();
+
+    {
+        linkedHashMap.put("apple", 10);
+        linkedHashMap.put("orange", 20);
+        linkedHashMap.put("banana", 15);
+    }
+
+    @Test
+    public void givenLinkedHashMap_whenIteratingThroughEntrySet_thenRetrievePositionByKey() {
+        int position = 0;
+        for (Map.Entry<String, Integer> entry : linkedHashMap.entrySet()) {
+            if (entry.getKey().equals("orange")) {
+                assertEquals("Position of 'orange' key should be 1", 1, position);
+                break;
+            }
+            position++;
+        }
+    }
+
+    @Test
+    public void givenLinkedHashMap_whenUsingJavaStreams_thenRetrievePositionByValue() {
+        Optional<String> key = linkedHashMap.keySet().stream()
+                .filter(integer -> Objects.equals(integer, "orange"))
+                .findFirst();
+
+        key.ifPresent(s -> assertEquals(1, new LinkedList<>(linkedHashMap.keySet()).indexOf(s)));
+    }
+
+}
