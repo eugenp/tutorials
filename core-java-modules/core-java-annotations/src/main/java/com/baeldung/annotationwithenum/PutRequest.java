459e83ac722b64d5d00ab348d24e1ee3168c22f3
vlad fernoaga <contact@fernoaga.com>
2024-05-04 21:42:18 +0300

    Implemented required code to support the article

---
+++ core-java-modules/core-java-annotations/src/main/java/com/baeldung/annotationwithenum/PutRequest.java
@@ -0,0 +1,14 @@
+package com.baeldung.annotationwithenum;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.METHOD })
+public @interface PutRequest {
+    RequestContentType contentType() default RequestContentType.JSON;
+    int intContentType() default 1;
+    String extendedContentType() default ExtendedRequestContentType.Constants.XML_VALUE;
+}
