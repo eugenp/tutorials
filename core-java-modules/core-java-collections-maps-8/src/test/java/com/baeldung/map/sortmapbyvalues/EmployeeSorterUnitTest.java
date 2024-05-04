f5332ea5d2c554738bf108098669efec275fdb03
Sagar Verma <sverma24@gmail.com>
2024-05-05 01:22:21 +0530

    refactoring

--- core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/sortmapbyvalues/EmployeeSorterUnitTest.java
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/sortmapbyvalues/EmployeeSorterUnitTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class EmployeeSorterTest {
+class EmployeeSorterUnitTest {
 
     @Test
     void testSortingBySalary() {
