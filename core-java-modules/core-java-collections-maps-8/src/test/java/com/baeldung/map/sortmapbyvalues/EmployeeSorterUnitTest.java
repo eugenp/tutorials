d89391bee369a844027c860f076ac0e900178753
Sagar Verma <sverma24@gmail.com>
2024-05-05 01:26:27 +0530

    refactoring imports

--- core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/sortmapbyvalues/EmployeeSorterUnitTest.java
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/sortmapbyvalues/EmployeeSorterUnitTest.java
@@ -1,5 +1,9 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class EmployeeSorterUnitTest {
 
