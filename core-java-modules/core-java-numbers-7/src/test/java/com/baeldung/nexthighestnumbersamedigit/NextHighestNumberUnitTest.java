84e11765c9e0fa3ecf975fe560b76efcdcc07c57
Wynn <sgwebfreelancer@gmail.com>
2024-05-04 14:40:38 +0800

    first draft

---
+++ core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/nexthighestnumbersamedigit/NextHighestNumberUnitTest.java
@@ -0,0 +1,37 @@
+package com.baeldung.nexthighestnumbersamedigit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NextHighestNumberUnitTest {
+
+    @Test
+    public void givenInput536479_whenUsingPermutation_thenOutputShouldBe536497() {
+        int input = 536479;
+        int result = NextHighestNumber.usingPermutation(input);
+        assertEquals(536497, result);
+    }
+
+    @Test
+    public void givenInput987654_whenUsingPermutation_thenOutputShouldBeMinusOne() {
+        int input = 987654;
+        int result = NextHighestNumber.usingPermutation(input);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenInput536479_whenUsingSorting_thenOutputShouldBe536497() {
+        int input = 536479;
+        int result = NextHighestNumber.usingSorting(input);
+        assertEquals(536497, result);
+    }
+
+    @Test
+    public void givenInput987654_whenUsingSorting_thenOutputShouldBeMinusOne() {
+        int input = 987654;
+        int result = NextHighestNumber.usingSorting(input);
+        assertEquals(-1, result);
+    }
+
+}
