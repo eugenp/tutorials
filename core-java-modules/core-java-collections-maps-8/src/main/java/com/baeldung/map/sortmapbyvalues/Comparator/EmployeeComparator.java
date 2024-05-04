06b10da199ace4adf441b33885b08568c1ce80a1
Sagar Verma <sverma24@gmail.com>
2024-05-05 00:50:43 +0530

    [BAEL-7825] changes of sorting a map contains list of elements as value using java streams

---
+++ core-java-modules/core-java-collections-maps-8/src/main/java/com/baeldung/map/sortmapbyvalues/Comparator/EmployeeComparator.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+import java.util.function.Function;
+
+public class EmployeeComparator {
+
+    public static Comparator<Employee> getComparator(String fieldName) {
+        switch (fieldName) {
+            case "name":
+                return Comparator.comparing(Employee::getName);
+            case "salary":
+                return Comparator.comparing(Employee::getSalary);
+            case "age":
+                return Comparator.comparingInt(Employee::getAge);
+            case "city":
+                return Comparator.comparing(Employee::getCity);
+            default:
+                throw new IllegalArgumentException("Unknown field: " + fieldName);
+        }
+    }
+}
---
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/sortmapbyvalues/EmployeeSorterTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeSorterTest {
+
+    @Test
+    void testSortingBySalary() {
+
+        Map<Integer, List<Employee>> employeeMap = new HashMap<>();
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee("Brent", 5000.0f, 30, "New York"));
+        employees.add(new Employee("Aarav", 4000.0f, 25, "Chicago"));
+        employees.add(new Employee("Cooper", 4400f, 32, "LA"));
+
+        employeeMap.put(1, employees);
+
+        Map<Integer, List<Employee>> sortedMap = EmployeeSorter.sortEmployeeMap(employeeMap, "salary");
+
+        assertThat(sortedMap.get(1)).containsExactly(
+                new Employee("Aarav", 5000.0f, 30, "New York"),
+                new Employee("Brent", 4000.0f, 25, "Chicago"),
+                new Employee("Cooper", 4400f, 32, "LA")
+        );
+
+    }
+
+    @Test
+    public void testSortingByAge() {
+        Map<Integer, List<Employee>> employeeMap = new HashMap<>();
+        List<Employee> employees = new ArrayList<>();
+
+        employees.add(new Employee("Brent", 5000.0f, 30, "New York"));
+        employees.add(new Employee("Aarav", 4000.0f, 25, "Chicago"));
+        employees.add(new Employee("Cooper", 5400f, 32, "LA"));
+
+        employeeMap.put(1, employees);
+
+        Map<Integer, List<Employee>> sortedMap = EmployeeSorter.sortEmployeeMap(employeeMap, "age");
+
+        assertThat(sortedMap.get(1)).containsExactly(
+                new Employee("Aarav", 5000.0f, 30, "New York"),
+                new Employee("Brent", 4000.0f, 25, "Chicago"),
+                new Employee("Cooper", 5400f, 32, "LA")
+        );
+    }
+}
