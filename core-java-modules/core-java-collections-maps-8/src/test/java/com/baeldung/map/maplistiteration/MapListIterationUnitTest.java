963adbef330a963899b45b6ae4bce6d4b033bade
Neetika Khandelwal <kwal.neetika2398@gmail.com>
2024-05-04 15:03:11 +0530

    Map List Iteration

--- core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/maplistiteration/MapListIterationUnitTest.java
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/maplistiteration/MapListIterationUnitTest.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -16,6 +17,7 @@ public class MapListIterationUnitTest {
     public void whenIterateListMapUsingTraditionalLoop_thenCountEntries() {
         List<Map<String, Object>> listOfMaps = new ArrayList<>();
         Map<String, Object> map1 = new HashMap<>();
+        int entryCounter = 0;
         map1.put("name", "Jack");
         map1.put("age", 30);
         listOfMaps.add(map1);
@@ -29,13 +31,10 @@ public class MapListIterationUnitTest {
             for (Map.Entry<String, Object> entry : map.entrySet()) {
                 String key = entry.getKey();
                 Object value = entry.getValue();
+                entryCounter++;
                 System.out.format("%s: %s\n", key, value);
             }
         }
-
-        int entryCounter = listOfMaps.stream()
-                .mapToInt(map -> map.size())
-                .sum();
 
         assertEquals(4, entryCounter);
     }
@@ -43,6 +42,7 @@ public class MapListIterationUnitTest {
     @Test
     public void whenIterateListMapUsingStream_thenCountEntries() {
         Map<String, Object> map1 = new HashMap<>();
+        AtomicInteger entryCounter = new AtomicInteger();
         map1.put("name", "Jack");
         map1.put("age", 30);
 
@@ -53,23 +53,22 @@ public class MapListIterationUnitTest {
         List<Map<String, Object>> listOfMaps = Arrays.asList(map1, map2);
 
         listOfMaps.stream()
-                .flatMap(map -> map.entrySet().stream())
-                .forEach(entry -> {
-                    String key = entry.getKey();
-                    Object value = entry.getValue();
-                    System.out.format("%s: %s\n", key, value);
-                });
+            .flatMap(map -> map.entrySet()
+                .stream())
+            .forEach(entry -> {
+                String key = entry.getKey();
+                Object value = entry.getValue();
+                entryCounter.incrementAndGet();
+                System.out.format("%s: %s\n", key, value);
+            });
 
-        int entryCounter = listOfMaps.stream()
-                .mapToInt(map -> map.size())
-                .sum();
-
-        assertEquals(4, entryCounter);
+        assertEquals(4, entryCounter.get());
     }
 
     @Test
     public void whenIterateListMapUsingKeySet_thenCountEntries() {
         Map<String, Object> map1 = new HashMap<>();
+        int entryCounter = 0;
         map1.put("name", "Jack");
         map1.put("age", 30);
 
@@ -82,13 +81,10 @@ public class MapListIterationUnitTest {
         for (Map<String, Object> map : listOfMaps) {
             for (String key : map.keySet()) {
                 Object value = map.get(key);
+                ++entryCounter;
                 System.out.format("%s: %s\n", key, value);
             }
         }
-
-        int entryCounter = listOfMaps.stream()
-                .mapToInt(map -> map.size())
-                .sum();
 
         assertEquals(4, entryCounter);
     }
@@ -96,6 +92,7 @@ public class MapListIterationUnitTest {
     @Test
     public void whenIterateListMapUsingForEach_thenCountEntries() {
         Map<String, Object> map1 = new HashMap<>();
+        AtomicInteger entryCounter = new AtomicInteger();
         map1.put("name", "Jack");
         map1.put("age", 30);
 
@@ -106,13 +103,10 @@ public class MapListIterationUnitTest {
         List<Map<String, Object>> listOfMaps = Arrays.asList(map1, map2);
 
         listOfMaps.forEach(map -> map.forEach((key, value) -> {
+            entryCounter.incrementAndGet();
             System.out.format("%s: %s\n", key, value);
         }));
 
-        int entryCounter = listOfMaps.stream()
-                .mapToInt(map -> map.size())
-                .sum();
-
-        assertEquals(4, entryCounter);
+        assertEquals(4, entryCounter.get());
     }
 }
