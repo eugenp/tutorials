b30143ad1b2305041fe3c420df92a7798a0be3f6
Sagar Verma <sverma24@gmail.com>
2024-05-05 02:02:25 +0530

    added missing methods in model

--- core-java-modules/core-java-collections-maps-8/src/main/java/com/baeldung/map/sortmapbyvalues/model/Employee.java
+++ core-java-modules/core-java-collections-maps-8/src/main/java/com/baeldung/map/sortmapbyvalues/model/Employee.java
@@ -37,4 +37,18 @@ public class Employee {
                 ", city='" + city + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return age == employee.age && Objects.equals(name, employee.name) && Objects.equals(salary, employee.salary) && Objects.equals(city, employee.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, salary, age, city);
+    }
+
 }
