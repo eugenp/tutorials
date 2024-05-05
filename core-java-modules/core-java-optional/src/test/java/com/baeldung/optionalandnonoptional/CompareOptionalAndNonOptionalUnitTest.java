b009b70d7439e10059113d9697253e6df5af65fd
YUAN <kent.yuan@Gmail.com>
2024-05-05 12:43:25 +0200

    replace Optional.ofNullable -> Optional.of

--- core-java-modules/core-java-optional/src/test/java/com/baeldung/optionalandnonoptional/CompareOptionalAndNonOptionalUnitTest.java
+++ core-java-modules/core-java-optional/src/test/java/com/baeldung/optionalandnonoptional/CompareOptionalAndNonOptionalUnitTest.java
@@ -12,26 +12,22 @@ public class CompareOptionalAndNonOptionalUnitTest {
     @Test
     void whenCovertToOptionalBeforeComparison_thenGetExpectedResult() {
         Optional<String> opt = Optional.empty();
-        assertFalse(opt.isPresent() && opt.equals(Optional.ofNullable("a b c")));
-        assertFalse(opt.isPresent() && opt.equals(Optional.ofNullable(null)));
+        assertFalse(opt.isPresent() && opt.equals(Optional.of("a b c")));
 
         opt = Optional.of("x y z");
-        assertFalse(opt.isPresent() && opt.equals(Optional.ofNullable("a b c")));
-        assertFalse(opt.isPresent() && opt.equals(Optional.ofNullable(null)));
+        assertFalse(opt.isPresent() && opt.equals(Optional.of("a b c")));
 
         opt = Optional.of("a b c");
-        assertTrue(opt.isPresent() && opt.equals(Optional.ofNullable("a b c")));
+        assertTrue(opt.isPresent() && opt.equals(Optional.of("a b c")));
     }
 
     @Test
     void whenOptionalGetBeforeComparison_thenGetExpectedResult() {
         Optional<String> opt = Optional.empty();
         assertFalse(opt.isPresent() && opt.get().equals("a b c"));
-        assertFalse(opt.isPresent() && opt.get().equals(null));
 
         opt = Optional.of("x y z");
         assertFalse(opt.isPresent() && opt.get().equals("a b c"));
-        assertFalse(opt.isPresent() && opt.get().equals(null));
 
         opt = Optional.of("a b c");
         assertTrue(opt.isPresent() && opt.get().equals("a b c"));
@@ -41,11 +37,9 @@ public class CompareOptionalAndNonOptionalUnitTest {
     void whenUsingOptionalMap_thenGetExpectedResult() {
         Optional<String> opt = Optional.empty();
         assertFalse(opt.map(c -> c.equals("a b c")).orElse(false));
-        assertFalse(opt.map(c -> c.equals(null)).orElse(false));
 
         opt = Optional.of("x y z");
         assertFalse(opt.map(c -> c.equals("a b c")).orElse(false));
-        assertFalse(opt.map(c -> c.equals(null)).orElse(false));
 
         opt = Optional.of("a b c");
         assertTrue(opt.map(c -> c.equals("a b c")).orElse(false));
