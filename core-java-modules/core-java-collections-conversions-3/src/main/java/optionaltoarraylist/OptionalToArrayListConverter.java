ba6386c14449edc1388671ce0bfe5fa7b0b7725d
Wynn <sgwebfreelancer@gmail.com>
2024-05-06 08:36:24 +0800

    updated non-typed list

--- core-java-modules/core-java-collections-conversions-3/src/main/java/optionaltoarraylist/OptionalToArrayListConverter.java
+++ core-java-modules/core-java-collections-conversions-3/src/main/java/optionaltoarraylist/OptionalToArrayListConverter.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 
 public class OptionalToArrayListConverter {
 
-    public static List usingIsPresent(Optional<String> obj) {
+    public static List<String> usingIsPresent(Optional<String> obj) {
         List<String> arrayList = new ArrayList<>();
         if (obj.isPresent()) {
             arrayList.add(obj.get());
@@ -15,32 +15,32 @@ public class OptionalToArrayListConverter {
         return arrayList;
     }
 
-    public static List usingOrElse(Optional<String> obj) {
+    public static List<String> usingOrElse(Optional<String> obj) {
         List<String> arrayList = new ArrayList<>();
         arrayList.add(obj.orElse("Hello, World!"));
         return arrayList;
     }
 
-    public static List usingOrElseGet(Optional<String> obj) {
+    public static List<String> usingOrElseGet(Optional<String> obj) {
         List<String> arrayList = new ArrayList<>();
         arrayList.add(obj.orElseGet(() -> "Hello, World!"));
         return arrayList;
     }
 
-    public static List usingStream(Optional<String> obj) {
+    public static List<String> usingStream(Optional<String> obj) {
         List<String> arrayList = obj.stream()
             .collect(Collectors.toList());
         return arrayList;
     }
 
-    public static List usingStreamFilter(Optional<String> obj) {
+    public static List<String> usingStreamFilter(Optional<String> obj) {
         List<String> arrayList = obj.filter(e -> e.startsWith("H"))
             .stream()
             .collect(Collectors.toList());
         return arrayList;
     }
 
-    public static List usingStreamFlatMap(Optional<List<String>> obj) {
+    public static List<String> usingStreamFlatMap(Optional<List<String>> obj) {
         List<String> arrayList = obj.stream()
             .flatMap(List::stream)
             .collect(Collectors.toList());
