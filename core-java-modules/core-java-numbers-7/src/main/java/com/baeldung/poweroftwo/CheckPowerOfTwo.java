3906245a3a0c5f0f237681cb8844a041c0ef423d
Wynn <sgwebfreelancer@gmail.com>
2024-05-07 11:20:19 +0800

    Rename the method name

--- core-java-modules/core-java-numbers-7/src/main/java/com/baeldung/poweroftwo/CheckPowerOfTwo.java
+++ core-java-modules/core-java-numbers-7/src/main/java/com/baeldung/poweroftwo/CheckPowerOfTwo.java
@@ -2,22 +2,22 @@ package com.baeldung.poweroftwo;
 
 public class CheckPowerOfTwo {
 
-    public static boolean usingLogarithm(int n) {
+    public static boolean isPowerOfTwoUsingLogarithm(int n) {
         return (Math.log(n) / Math.log(2)) % 1 == 0;
     }
 
-    public static boolean usingBitwiseOperations(int n) {
+    public static boolean isPowerOfTwoUsingBitwiseOperations(int n) {
         return (n != 0) && ((n & (n - 1)) == 0);
     }
 
-    public static boolean usingLoopDivision(int n) {
+    public static boolean isPowerOfTwoUsingLoopDivision(int n) {
         while (n != 1 && n % 2 == 0) {
             n /= 2;
         }
         return n == 1;
     }
 
-    public static boolean usingHighestOneBit(int n) {
+    public static boolean isPowerOfTwoUsingHighestOneBit(int n) {
         return n > 0 && (n == Integer.highestOneBit(n));
     }
 }
--- core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/poweroftwo/CheckPowerOfTwoUnitTest.java
+++ core-java-modules/core-java-numbers-7/src/test/java/com/baeldung/poweroftwo/CheckPowerOfTwoUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.poweroftwo;
 
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -10,34 +9,32 @@ public class CheckPowerOfTwoUnitTest {
 
     @Test
     public void givenANonePowerOfTwoNum_whenUsingLogarithm_thenReturnFalse() {
-        assertFalse(CheckPowerOfTwo.usingLogarithm(100));
+        assertFalse(CheckPowerOfTwo.isPowerOfTwoUsingLogarithm(100));
     }
 
     @Test
     public void givenAPowerOfTwoNum_whenUsingLogarithm_thenReturnTrue() {
-        assertTrue(CheckPowerOfTwo.usingLogarithm(256));
+        assertTrue(CheckPowerOfTwo.isPowerOfTwoUsingLogarithm(256));
     }
 
     @Test
     public void givenANonePowerOfTwoNum_whenUsingBitwiseOperations_thenReturnFalse() {
-        assertFalse(CheckPowerOfTwo.usingBitwiseOperations(100));
+        assertFalse(CheckPowerOfTwo.isPowerOfTwoUsingBitwiseOperations(100));
     }
-
 
     @Test
     public void givenAPowerOfTwoNum_whenUsingBitwiseOperations_thenReturnTrue() {
-        assertTrue(CheckPowerOfTwo.usingBitwiseOperations(256));
+        assertTrue(CheckPowerOfTwo.isPowerOfTwoUsingBitwiseOperations(256));
     }
 
     @Test
     public void givenANonePowerOfTwoNum_whenUsingHighestOneBit_thenReturnFalse() {
-        assertFalse(CheckPowerOfTwo.usingHighestOneBit(100));
+        assertFalse(CheckPowerOfTwo.isPowerOfTwoUsingHighestOneBit(100));
     }
-
 
     @Test
     public void givenAPowerOfTwoNum_whenUsingHighestOneBit_thenReturnTrue() {
-        assertTrue(CheckPowerOfTwo.usingHighestOneBit(256));
+        assertTrue(CheckPowerOfTwo.isPowerOfTwoUsingHighestOneBit(256));
     }
 
 }
