c0e2eab6a47f5b4bfb9ae1ad8a6cb92789d58e6d
Ayor94 <ayorjesu@gmail.com>
2024-05-09 22:06:17 +0100

    Add files via upload

---
+++ Scriptrunning.java
@@ -0,0 +1,28 @@
+package com.ayor;
+
+public class Scriptrunning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Product {
+
+	  private String name;
+	  private double price;
+
+	  public Product(String name, double price) {
+	    this.name = name;
+	    this.price = price;
+	  }
+
+	  public String getName() { return name; }
+	  public double getPrice() { return price; }
+
+	  public void setName(String name) { this.name = name; }
+	  public void setPrice(double price) { this.price = price; }
+
+	}
