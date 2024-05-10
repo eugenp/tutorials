1561b0f7244658128228ed16dfd5317f266fcec7
YUAN <kent.yuan@Gmail.com>
2024-05-10 11:19:08 +0200

    [array-1st-last] moved

--- core-java-modules/core-java-arrays-operations-basic-2/src/test/java/com/baeldung/array/access/RetrieveFirstAndLastElementUnitTest.java
+++ core-java-modules/core-java-arrays-operations-basic-2/src/test/java/com/baeldung/array/access/RetrieveFirstAndLastElementUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.arrays;
+package com.baeldung.array.access;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
