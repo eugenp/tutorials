79ca6a2456c88d69c355da78d15355507f2ad19a
luca <lucacambi77@gmail.com>
2024-05-11 09:08:36 +0200

    fix: tests names

--- algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/algorithms/permutation/StringPermutationUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/algorithms/permutation/StringPermutationUnitTest.java
@@ -13,62 +13,62 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class StringPermutationUnitTest {
 
     @Test
-    void givenTwoStringsArePermutation_whenSortingCharsArray_thenOk() {
+    void givenTwoStringsArePermutation_whenSortingCharsArray_thenPermutation() {
         assertTrue(isPermutationWithSorting("baeldung", "luaebngd"));
         assertTrue(isPermutationWithSorting("hello world", "world hello"));
     }
 
     @Test
-    void givenTwoStringsAreNotPermutation_whenSortingCharsArray_thenKo() {
+    void givenTwoStringsAreNotPermutation_whenSortingCharsArray_thenNotPermutation() {
         assertFalse(isPermutationWithSorting("baeldung", "luaebgd"));
         assertFalse(isPermutationWithSorting("baeldung", "luaebngq"));
     }
 
     @Test
-    void givenTwoStringsArePermutation_whenTwoCountersCharsFrequencies_thenOk() {
+    void givenTwoStringsArePermutation_whenTwoCountersCharsFrequencies_thenPermutation() {
         assertTrue(isPermutationWithTwoCounters("baeldung", "luaebngd"));
         assertTrue(isPermutationWithTwoCounters("hello world", "world hello"));
     }
 
     @Test
-    void givenTwoStringsAreNotPermutation_whenTwoCountersCharsFrequencies_thenKo() {
+    void givenTwoStringsAreNotPermutation_whenTwoCountersCharsFrequencies_thenNotPermutation() {
         assertFalse(isPermutationWithTwoCounters("baeldung", "luaebgd"));
         assertFalse(isPermutationWithTwoCounters("baeldung", "luaebngq"));
     }
 
     @Test
-    void givenTwoStringsArePermutation_whenOneCounterCharsFrequencies_thenOk() {
+    void givenTwoStringsArePermutation_whenOneCounterCharsFrequencies_thenPermutation() {
         assertTrue(isPermutationWithOneCounter("baeldung", "luaebngd"));
         assertTrue(isPermutationWithOneCounter("hello world", "world hello"));
     }
 
     @Test
-    void givenTwoStringsAreNotPermutation_whenOneCounterCharsFrequencies_thenKo() {
+    void givenTwoStringsAreNotPermutation_whenOneCounterCharsFrequencies_thenNotPermutation() {
         assertFalse(isPermutationWithOneCounter("baeldung", "luaebgd"));
         assertFalse(isPermutationWithOneCounter("baeldung", "luaebngq"));
     }
 
     @Test
-    void givenTwoStringsArePermutation_whenCountCharsFrequenciesWithMap_thenOk() {
+    void givenTwoStringsArePermutation_whenCountCharsFrequenciesWithMap_thenPermutation() {
         assertTrue(isPermutationWithMap("baelduňg", "luaebňgd"));
         assertTrue(isPermutationWithMap("hello world", "world hello"));
     }
 
     @Test
-    void givenTwoStringsAreNotPermutation_whenCountCharsFrequenciesWithMap_thenKo() {
+    void givenTwoStringsAreNotPermutation_whenCountCharsFrequenciesWithMap_thenNotPermutation() {
         assertFalse(isPermutationWithMap("baelduňg", "luaebgd"));
         assertFalse(isPermutationWithMap("baeldung", "luaebngq"));
         assertFalse(isPermutationWithMap("baeldung", "luaebngg"));
     }
 
     @Test
-    void givenTwoStrings_whenIncludePermutation_thenOk() {
+    void givenTwoStrings_whenIncludePermutation_thenPermutation() {
         assertTrue(isPermutationInclusion("baeldung", "ea"));
         assertTrue(isPermutationInclusion("baeldung", "ae"));
     }
 
     @Test
-    void givenTwoStrings_whenNotIncludePermutation_thenKo() {
+    void givenTwoStrings_whenNotIncludePermutation_thenNotPermutation() {
         assertFalse(isPermutationInclusion("baeldung", "au"));
         assertFalse(isPermutationInclusion("baeldung", "baeldunga"));
     }
