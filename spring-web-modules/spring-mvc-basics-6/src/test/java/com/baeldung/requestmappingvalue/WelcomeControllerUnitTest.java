c6ab459f1ab6de6bda1f9c8c310dce7818b01248
vunamtien <vunamtien16@gmail.com>
2024-05-13 13:45:32 +0700

    [JAVA-31846] Split-or-move-spring-mvc-basics-5-module (#16626)

--- spring-web-modules/spring-mvc-basics-6/src/test/java/com/baeldung/requestmappingvalue/WelcomeControllerUnitTest.java
+++ spring-web-modules/spring-mvc-basics-6/src/test/java/com/baeldung/requestmappingvalue/WelcomeControllerUnitTest.java
@@ -1,13 +1,13 @@
 package com.baeldung.requestmappingvalue;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
