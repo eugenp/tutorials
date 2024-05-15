574a63c76c7b3366053462d348abccad07758046
Eya Bey <eyabey98@gmail.com>
2024-05-15 22:06:44 +0100

    done

---
+++ eyabeytest/Address.java
@@ -0,0 +1,9 @@
+// Class representing an address
+class Address {
+    String city;
+
+    // Constructor to initialize the city
+    public Address(String city) {
+        this.city = city;
+    }
+}
