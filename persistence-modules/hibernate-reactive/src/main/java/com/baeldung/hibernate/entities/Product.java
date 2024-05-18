8c2d17d8186414c40af02f012ee136a2efae6bef
rezaganjis <Ganji@tosan.com>
2024-05-18 18:32:12 +0330

    Hibernate Reactive.

--- persistence-modules/hibernate-reactive/src/main/java/com/baeldung/hibernate/entities/Product.java
+++ persistence-modules/hibernate-reactive/src/main/java/com/baeldung/hibernate/entities/Product.java
@@ -5,4 +5,20 @@ public class Product {
     @Id
     private Long id;
     private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
