3f29ab752f76486afc284c26e7dbe104cd35856d
Harry9656 <singharpal9656@gmail.com>
2024-05-25 14:49:04 +0200

    Fix spring-boot-modules

--- spring-boot-modules/spring-boot-react/src/main/java/com/baeldung/springbootreact/repository/ClientRepository.java
+++ spring-boot-modules/spring-boot-react/src/main/java/com/baeldung/springbootreact/repository/ClientRepository.java
@@ -2,6 +2,8 @@ package com.baeldung.springbootreact.repository;
 
 import com.baeldung.springbootreact.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
