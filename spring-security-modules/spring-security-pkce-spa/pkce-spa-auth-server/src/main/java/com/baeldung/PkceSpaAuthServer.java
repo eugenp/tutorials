fbc3f18b705b3d2a1469ff1a771618424a77df1b
Somnath <musib.somnath@gmail.com>
2024-05-27 22:32:58 +1000

    BAEL-7691 Updated Test Class name for PMD validation

--- spring-security-modules/spring-security-pkce-spa/pkce-spa-auth-server/src/main/java/com/baeldung/PkceSpaAuthServer.java
+++ spring-security-modules/spring-security-pkce-spa/pkce-spa-auth-server/src/main/java/com/baeldung/PkceSpaAuthServer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAuthorizationServerDemoApplication {
+public class PkceSpaAuthServer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringAuthorizationServerDemoApplication.class, args);
+		SpringApplication.run(PkceSpaAuthServer.class, args);
 	}
 
 }
--- spring-security-modules/spring-security-pkce-spa/pkce-spa-auth-server/src/test/java/com/baeldung/PkceSpaAuthServerUnitTest.java
+++ spring-security-modules/spring-security-pkce-spa/pkce-spa-auth-server/src/test/java/com/baeldung/PkceSpaAuthServerUnitTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringAuthorizationServerDemoApplicationTests {
+class PkceSpaAuthServerUnitTest {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void whenContextLoads_thenSuccess() {
+        
+    }
 
 }
