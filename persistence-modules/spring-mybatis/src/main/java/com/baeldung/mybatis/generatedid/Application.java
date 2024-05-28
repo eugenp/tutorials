37029d3501e81ae51f18122414bdc32b24c5dc5d
Ana Peterlic <apeterlic@gmail.com>
2024-05-28 19:23:06 +0200

    Update Application.java

--- persistence-modules/spring-mybatis/src/main/java/com/baeldung/mybatis/generatedid/Application.java
+++ persistence-modules/spring-mybatis/src/main/java/com/baeldung/mybatis/generatedid/Application.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.baeldung.mybatis.generatedId")
+@MapperScan("com.baeldung.mybatis.generatedid")
 @SpringBootApplication
 public class Application {
 
