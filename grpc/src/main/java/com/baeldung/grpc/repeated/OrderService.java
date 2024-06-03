25a3b1625eb7204bc46eef987f0c4703f0b9059f
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2024-06-04 00:37:10 +0530

    BAEL-8055, Packed Repeated Fields in Protobuf in Java

---
+++ grpc/src/main/java/com/baeldung/grpc/repeated/OrderService.java
@@ -0,0 +1,25 @@
+package com.baeldung.grpc.repeated;
+
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.grpc.stub.StreamObserver;
+
+public class OrderService extends OrderServiceGrpc.OrderServiceImplBase {
+    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+
+    @Override
+    public void createOrder(UnPackedOrder unpackedOrder, StreamObserver<UnPackedOrder> responseObserver) {
+        int orderID = insertOrder(unpackedOrder);
+        logger.info("Order {} created successfully", orderID);
+        UnPackedOrder createdUnPackedOrder = UnPackedOrder.newBuilder(unpackedOrder).setOrderId(orderID).build();
+        responseObserver.onNext(createdUnPackedOrder);
+        responseObserver.onCompleted();
+    }
+
+    private int insertOrder(UnPackedOrder unpackedOrder) {
+        return new Random().nextInt(100);
+    }
+}
