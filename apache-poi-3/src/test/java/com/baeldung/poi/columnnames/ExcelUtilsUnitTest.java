bdf9c8c2575e41f65d148834fe1c80919f094139
uzma <u.khan@proxymity.io>
2024-06-03 16:29:03 +0100

    [bael-8061] code for using poi to get column names

---
+++ apache-poi-3/src/test/java/com/baeldung/poi/columnnames/ExcelUtilsUnitTest.java
@@ -0,0 +1,44 @@
+package com.baeldung.poi.columnnames;
+
+
+import org.apache.poi.ss.usermodel.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ExcelUtilsUnitTest {
+
+    private static final String XLS_TEST_FILE_PATH = "src/main/resources/names.xls";
+    private static final String XLSX_TEST_FILE_PATH = "src/main/resources/food_info.xlsx";
+    private static final String SHEET_NAME = "Sheet1";
+@Test
+public void givenExcelFileWithXLSXFormat_whentestGetColumnNames_thenReturnsColumnNames() throws IOException {
+    Workbook workbook = ExcelUtils.openWorkbook(XLSX_TEST_FILE_PATH);
+    Sheet sheet = ExcelUtils.getSheet(workbook, SHEET_NAME);
+    List<String> columnNames = ExcelUtils.getColumnNames(sheet);
+
+    assertEquals(4, columnNames.size());
+    assertTrue(columnNames.contains("Category"));
+    assertTrue(columnNames.contains("Name"));
+    assertTrue(columnNames.contains("Measure"));
+    assertTrue(columnNames.contains("Calories"));
+    workbook.close();
+}
+@Test
+public void givenExcelFileWithXLSFormat_whentestGetColumnNames_thenReturnsColumnNames() throws IOException {
+    Workbook workbook = ExcelUtils.openWorkbook(XLS_TEST_FILE_PATH);
+    Sheet sheet = ExcelUtils.getSheet(workbook, SHEET_NAME);
+    List<String> columnNames = ExcelUtils.getColumnNames(sheet);
+
+    assertEquals(3, columnNames.size());
+    assertTrue(columnNames.contains("name"));
+    assertTrue(columnNames.contains("age"));
+    assertTrue(columnNames.contains("city"));
+
+    workbook.close();
+}
+}
