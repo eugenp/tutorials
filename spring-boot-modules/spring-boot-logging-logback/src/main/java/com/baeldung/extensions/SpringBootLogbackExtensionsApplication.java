82f4f412eb34cd661fcde94518856d91349dd4f3
Omkar A <omkara.adg@gmail.com>
2024-06-04 01:33:45 +0530

    - code for logbook article

--- spring-boot-modules/spring-boot-logging-logback/src/main/java/com/baeldung/extensions/SpringBootLogbackExtensionsApplication.java
+++ spring-boot-modules/spring-boot-logging-logback/src/main/java/com/baeldung/extensions/SpringBootLogbackExtensionsApplication.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.baeldung.controller", "com.baeldung.logging", "com.baeldung.logbookconfig" })
 public class SpringBootLogbackExtensionsApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(SpringBootLogbackExtensionsApplication.class);
---
+++ spring-boot-modules/spring-boot-logging-logback/src/main/java/com/baeldung/logbookconfig/LogBookConfig.java
@@ -0,0 +1,23 @@
+package com.baeldung.logbookconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.Conditions;
+import org.zalando.logbook.core.DefaultHttpLogFormatter;
+import org.zalando.logbook.core.DefaultHttpLogWriter;
+import org.zalando.logbook.core.DefaultSink;
+
+@Configuration
+public class LogBookConfig {
+
+    @Bean
+    public Logbook logbook() {
+        Logbook logbook = Logbook.builder()
+            .condition(Conditions.exclude(Conditions.requestTo("/api/welcome"), Conditions.contentType(
+                "pplication/octet-stream"), Conditions.header("X-Secret", "true")))
+            .sink(new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter()))
+            .build();
+        return logbook;
+    }
+}
