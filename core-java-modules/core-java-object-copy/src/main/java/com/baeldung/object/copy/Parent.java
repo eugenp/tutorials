e7a86322ddb60369e7f4e7e97642b21dc1399fa1
Arjan Tijms <arjan.tijms@omnifish.ee>
2024-06-04 15:24:57 +0200

    Add copy constructor plus some cleanup

--- core-java-modules/core-java-object-copy/src/main/java/com/baeldung/object/copy/Parent.java
+++ core-java-modules/core-java-object-copy/src/main/java/com/baeldung/object/copy/Parent.java
@@ -7,8 +7,29 @@ public class Parent {
     public Child child1;
     public Child child2;
 
+    public Parent() {
+    }
+
     public Parent(String name) {
         this.name = name;
+    }
+
+    /**
+     * Copy constructor
+     * 
+     * @param other The other Parent to copy.
+     * @param deep Pass true if a deep copy is required, false otherwise.
+     */
+    public Parent(Parent other, boolean deep) {
+        this.name = other.name;
+
+        if (deep) {
+            this.child1 = new Child(other.child1.name);
+            this.child2 = new Child(other.child2.name);
+        } else {
+            this.child1 = other.child1;
+            this.child2 = other.child2;
+        }
     }
 
 }
--- core-java-modules/core-java-object-copy/src/test/java/com/baeldung/object/copy/DeepShallowCopyUnitTest.java
+++ core-java-modules/core-java-object-copy/src/test/java/com/baeldung/object/copy/DeepShallowCopyUnitTest.java
@@ -17,21 +17,18 @@ public class DeepShallowCopyUnitTest {
         originalDad.child2 = child2;
 
         // Copy parent shallow
-        Parent newDad = new Parent("john");
-        newDad.name = originalDad.name;
-        newDad.child1 = originalDad.child1;
-        newDad.child2 = originalDad.child2;
+        Parent newDad = new Parent(originalDad, false);
 
         // Check basic assumptions
-        assertEquals(newDad.name, "homer");
-        assertEquals(newDad.child1.name, "bart");
-        assertEquals(newDad.child2.name, "lisa");
+        assertEquals("homer", newDad.name);
+        assertEquals("bart", newDad.child1.name);
+        assertEquals("lisa", newDad.child2.name);
 
         // Check mutating child
         child1.name = "peter";
 
         // Assert name changed from point of view of newDad
-        assertEquals(newDad.child1.name, "peter");
+        assertEquals("peter", newDad.child1.name);
     }
 
     @Test
@@ -44,22 +41,19 @@ public class DeepShallowCopyUnitTest {
         originalDad.child1 = child1;
         originalDad.child2 = child2;
 
-        // Copy parent shallow
-        Parent newDad = new Parent("john");
-        newDad.name = originalDad.name;
-        newDad.child1 = new Child(originalDad.child1.name);
-        newDad.child2 = new Child(originalDad.child2.name);
+        // Copy parent deep
+        Parent newDad = new Parent(originalDad, true);
 
         // Check basic assumptions
-        assertEquals(newDad.name, "homer");
-        assertEquals(newDad.child1.name, "bart");
-        assertEquals(newDad.child2.name, "lisa");
+        assertEquals("homer", newDad.name);
+        assertEquals("bart", newDad.child1.name);
+        assertEquals("lisa", newDad.child2.name);
 
         // Check mutating child
         child1.name = "peter";
 
         // Assert name did not change from point of view of newDad
-        assertEquals(newDad.child1.name, "bart");
+        assertEquals("bart", newDad.child1.name);
     }
 
 }
