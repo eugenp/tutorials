558bb2a31fb1ad0c0b110d3b347a74fd659892f8
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-06-05 01:53:59 +0300

    Update ClearScannerBufferUnitTest.java (#16778)
    * Update ClearScannerBufferUnitTest.java
    
    * Update ClearScannerBufferUnitTest.java 
    
    Use skip("/n").

--- core-java-modules/core-java-io-6/src/test/java/com/baeldung/clearscannerbuffer/ClearScannerBufferUnitTest.java
+++ core-java-modules/core-java-io-6/src/test/java/com/baeldung/clearscannerbuffer/ClearScannerBufferUnitTest.java
@@ -3,10 +3,7 @@ package clearscannerbuffer;
 
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
 import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -25,44 +22,14 @@ public class ClearScannerBufferUnitTest {
         assertEquals(123, number);
         assertEquals("Hello World", text);
     }
-
+    
     @Test
-    public void givenInput_whenUsingReset_thenBufferCleared() {
-        String input = "123\nHello World";
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
-        Scanner scanner = new Scanner(inputStream);
-
+    public void givenInput_whenUsingSkip_thenBufferCleared() {
         int number = scanner.nextInt();
-
-        inputStream.reset();
-        scanner = new Scanner(inputStream);
-
-        int newNumber = scanner.nextInt();
-
-        scanner.nextLine();
-
+        scanner.skip("\n");
         String text = scanner.nextLine();
 
         assertEquals(123, number);
-        assertEquals(123, newNumber);
         assertEquals("Hello World", text);
-    }
-
-    @Test
-    public void givenInput_whenUsingStreamAPI_thenBufferCleared() {
-        Scanner scanner = new Scanner("123\nline1\nline2\nexit");
-
-        int number = scanner.nextInt();
-
-        scanner.nextLine();
-
-        String lines = Stream.generate(scanner::nextLine)
-                .takeWhile(line -> !line.equalsIgnoreCase("exit"))
-                .collect(Collectors.joining(System.lineSeparator()));
-
-        String expectedLines = "line1" + System.lineSeparator() + "line2";
-
-        assertEquals(123, number);
-        assertEquals(expectedLines, lines);
     }
 }
