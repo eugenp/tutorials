f50787024d8ac2afa99cf395261972010ca4e4e6
michaelin007 <michaelolayemi28@gmail.com>
2024-06-05 12:01:11 +0000

    https://jira.baeldung.com/browse/BAEL-7942

--- core-java-modules/core-java-networking-5/src/test/java/com/baeldung/getwebfilesize/GetWebFileSizeUnitTest.java
+++ core-java-modules/core-java-networking-5/src/test/java/com/baeldung/getwebfilesize/GetWebFileSizeUnitTest.java
@@ -14,7 +14,7 @@ class GetWebFileSizeUnitTest {
     String fileUrl = "https://www.ingka.com/wp-content/uploads/2020/11/dummy.pdf";
 
     @Test
-    void givenUrl_whenGetFileSizeUsingURLConnection_thenCorrect() throws IOException {
+    void givenUrl_whenGetFileSizeUsingURLConnectionAndGetContentLengthLong_thenCorrect() throws IOException {
         URL url = new URL(fileUrl);
         URLConnection urlConnection = url.openConnection();
 
@@ -26,4 +26,21 @@ class GetWebFileSizeUnitTest {
         }
 
     }
+
+    @Test
+    void givenUrl_whenGetFileSizeUsingURLConnectionAndGetHeaderField_thenCorrect() throws IOException {
+        URL url = new URL(fileUrl);
+        URLConnection urlConnection = url.openConnection();
+
+        String headerField = urlConnection.getHeaderField("Content-Length");
+        if (headerField != null && !headerField.isEmpty()) {
+            long fileSize = Long.parseLong(headerField);
+            assertEquals(29789, fileSize);
+
+        } else {
+            fail("Could not determine file size");
+        }
+
+    }
+
 }
