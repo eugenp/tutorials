b9f5e64232a3988d019d9d5dea130e447380680c
Omkar A <omkara.adg@gmail.com>
2024-06-07 20:04:50 +0530

    -- Added code for sink examples -- composite sink and logstash sink example

--- spring-boot-modules/spring-boot-logging-logback/src/main/java/com/baeldung/logbookconfig/LogBookConfig.java
+++ spring-boot-modules/spring-boot-logging-logback/src/main/java/com/baeldung/logbookconfig/LogBookConfig.java
@@ -2,11 +2,15 @@ package com.baeldung.logbookconfig;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.HttpLogFormatter;
 import org.zalando.logbook.Logbook;
+import org.zalando.logbook.core.CompositeSink;
 import org.zalando.logbook.core.Conditions;
 import org.zalando.logbook.core.DefaultHttpLogFormatter;
 import org.zalando.logbook.core.DefaultHttpLogWriter;
 import org.zalando.logbook.core.DefaultSink;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.logstash.LogstashLogbackSink;
 
 @Configuration
 public class LogBookConfig {
@@ -15,8 +19,28 @@ public class LogBookConfig {
     public Logbook logbook() {
         Logbook logbook = Logbook.builder()
             .condition(Conditions.exclude(Conditions.requestTo("/api/welcome"), Conditions.contentType(
-                "pplication/octet-stream"), Conditions.header("X-Secret", "true")))
+                "Application/octet-stream"), Conditions.header("X-Secret", "true")))
             .sink(new DefaultSink(new DefaultHttpLogFormatter(), new DefaultHttpLogWriter()))
+            .build();
+        return logbook;
+    }
+
+    public Logbook compositesink() {
+        CompositeSink comsink = new CompositeSink(null);
+        Logbook logbook = Logbook.builder()
+            .sink(comsink)
+            .build();
+        return logbook;
+    }
+
+    public Logbook logstashsink() {
+        HttpLogFormatter formatter = new JsonHttpLogFormatter();
+
+        LogstashLogbackSink logstashsink = new LogstashLogbackSink(formatter);
+        //LogstashLogbackSink logstashsink = new LogstashLogbackSink(formatter, Level.INFO);
+
+        Logbook logbook = Logbook.builder()
+            .sink(logstashsink)
             .build();
         return logbook;
     }
