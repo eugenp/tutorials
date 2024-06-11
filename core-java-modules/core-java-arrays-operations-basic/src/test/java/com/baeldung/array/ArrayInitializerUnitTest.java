50077e56b8e94d0856ee262b48f783550603d036
michaelin007 <michaelolayemi28@gmail.com>
2024-06-11 13:55:21 +0100

    https://jira.baeldung.com/browse/BAEL-8079

--- core-java-modules/core-java-arrays-operations-basic/src/test/java/com/baeldung/array/ArrayInitializerUnitTest.java
+++ core-java-modules/core-java-arrays-operations-basic/src/test/java/com/baeldung/array/ArrayInitializerUnitTest.java
@@ -1,17 +1,8 @@
 package com.baeldung.array;
 
-import static com.baeldung.array.ArrayInitializer.initializeArrayAtTimeOfDeclarationMethod1;
-import static com.baeldung.array.ArrayInitializer.initializeArrayAtTimeOfDeclarationMethod2;
-import static com.baeldung.array.ArrayInitializer.initializeArrayAtTimeOfDeclarationMethod3;
-import static com.baeldung.array.ArrayInitializer.initializeArrayInLoop;
-import static com.baeldung.array.ArrayInitializer.initializeArrayRangeUsingArraysFill;
-import static com.baeldung.array.ArrayInitializer.initializeArrayUsingArraysCopy;
-import static com.baeldung.array.ArrayInitializer.initializeArrayUsingArraysFill;
-import static com.baeldung.array.ArrayInitializer.initializeArrayUsingArraysSetAll;
-import static com.baeldung.array.ArrayInitializer.initializeArrayUsingArraysUtilClone;
-import static com.baeldung.array.ArrayInitializer.initializeLargerArrayUsingArraysCopy;
-import static com.baeldung.array.ArrayInitializer.initializeMultiDimensionalArrayInLoop;
+import static com.baeldung.array.ArrayInitializer.*;
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -70,5 +61,66 @@ public class ArrayInitializerUnitTest {
     @Test
     public void whenInitializeArrayUsingArraysUtilClone_thenCorrect() {
         assertArrayEquals(new char[] { 'a', 'b', 'c' }, initializeArrayUsingArraysUtilClone());
+    }
+
+    @Test
+    public void whenInitializeEmptyArray_thenCorrect() {
+        assertArrayEquals(new int[5], initializeEmptyArray());
+    }
+
+    @Test
+    public void whenAddingItemsToAnArray_thenCorrect() {
+        int[] numbers = initializeEmptyArray();
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 30;
+        numbers[3] = 40;
+        numbers[4] = 50;
+        assertEquals(5, numbers.length);
+        assertEquals(20, numbers[1]);
+    }
+
+    @Test
+    public void whenInitializeEmptyTwoDimensionalArray_thenCorrect() {
+        assertArrayEquals(new int[2][5], initializeEmptyTwoDimensionalArray());
+    }
+
+    @Test
+    public void whenAddingItemsToAnTwoDimensionalArray_thenCorrect() {
+        int[][] matrix = initializeEmptyTwoDimensionalArray();
+        matrix[0][0] = 10;
+        matrix[0][1] = 20;
+        matrix[0][2] = 30;
+        matrix[0][3] = 40;
+        matrix[0][4] = 50;
+        matrix[1][0] = 60;
+        matrix[1][1] = 70;
+        matrix[1][2] = 80;
+        matrix[1][3] = 90;
+        matrix[1][4] = 100;
+        assertEquals(2, matrix.length);
+        assertEquals(5, matrix[0].length);
+        assertEquals(5, matrix[1].length);
+        assertEquals(20, matrix[0][1]);
+    }
+
+    @Test
+    public void whenInitializeArrayWithStream_thenCorrect() {
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, initializeArrayUsingArraysStream());
+    }
+
+    @Test
+    public void whenInitializeArrayOfStringTypeWithStreamApi_thenCorrect() {
+        assertEquals(5, initializeArrayOfStringTypeUsingStreamApi().length);
+    }
+
+    @Test
+    public void whenInitializeArrayOfIntegerUsingIntStream_thenCorrect() {
+        assertArrayEquals(new int[] { 1, 2, 3, 4, 5 }, initializeArrayUsingIntStream());
+    }
+
+    @Test
+    public void whenInitializeArrayOfDoubleUsingStreamApi_thenCorrect() {
+        assertArrayEquals(new double[] { 1.1, 2.2, 3.3, 4.4, 5.5 }, initializeArrayOfDoubleTypeUsingStreamApi(), 0.0);
     }
 }
