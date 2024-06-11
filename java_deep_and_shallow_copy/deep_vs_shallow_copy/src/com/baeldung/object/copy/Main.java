0aae8506f3a3fbe313df46c3727cc5cb6b86ec49
thisara <thisara.udaya@gmail.com>
2024-06-12 01:19:11 +1000

    update deep and shallow copy

---
+++ java_deep_and_shallow_copy/deep_vs_shallow_copy/src/com/baeldung/object/copy/Main.java
@@ -0,0 +1,62 @@
+package com.baeldung.object.copy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+
+        ObjectCopyService objectCopyService = new ObjectCopyService();
+        
+        /* Initialize ShoppingCart */
+        ShoppingCart sourceShoppingCart = new ShoppingCart("MyCart", new ArrayList<String>(List.of("Bread")));
+
+        print(String.format("Source Shopping Cart : %s", sourceShoppingCart));
+
+        /* Shallow Copy of ShoppingCart */
+        print("Shallow Copy Of Shopping Cart:...");
+
+        sourceShoppingCart = new ShoppingCart("MyCart", new ArrayList<String>(List.of("Bread")));
+        ShoppingCart shallowCopy = objectCopyService.shallowCopy(sourceShoppingCart, "ShallowCopy");
+        shallowCopy.getItems().add("Butter");
+        
+        print(String.format("Source Shopping Cart : %s", sourceShoppingCart));
+        print(String.format("Shallow Copy of Shopping Cart : %s", shallowCopy));
+        
+        /* Deep Copy of ShoppingCart */
+        System.out.println("Deep Copy Of Shopping Cart:...");
+
+        sourceShoppingCart = new ShoppingCart("MyCart", new ArrayList<String>(List.of("Bread")));
+        ShoppingCart deepCopy = objectCopyService.deepCopy(sourceShoppingCart, "DeepCopy");
+        deepCopy.getItems().add("Butter");
+        
+        print(String.format("Source Shopping Cart : %s", sourceShoppingCart));
+        print(String.format("Deep Copy of Shopping Cart : %s", deepCopy));
+
+        /* Clone ShoppingCart */
+        print("Copy Of Shopping Cart By Cloning:...");
+
+        sourceShoppingCart = new ShoppingCart("MyCart", new ArrayList<String>(List.of("Bread")));
+        ShoppingCart cloneCopy = objectCopyService.cloneCopy(sourceShoppingCart, "CloneCopy");
+        cloneCopy.getItems().add("Butter");
+
+        print(String.format("Source Shopping Cart : %s", sourceShoppingCart));
+        print(String.format("Clone of Shopping Cart : %s", cloneCopy));
+
+        /* Deep Clone ShoppingCart */
+        print("Deep Clone Copy Of Shopping Cart By Cloning:...");
+
+        sourceShoppingCart = new ShoppingCart("MyCart", new ArrayList<String>(List.of("Bread")));
+        ShoppingCart deepClone = objectCopyService.deepCloneCopy(sourceShoppingCart, "DeepCloneCopy");
+        deepClone.getItems().add("Butter");
+
+        print(String.format("Initial Shopping Cart : %s", sourceShoppingCart));
+        print(String.format("Deep Copy By Cloning : %s", deepClone));
+        
+    }
+
+    private static void print(String printText) {
+        System.out.println(printText);
+    }
+}
---
+++ java_deep_and_shallow_copy/deep_vs_shallow_copy/src/com/baeldung/object/copy/ObjectCopyService.java
@@ -0,0 +1,56 @@
+package com.baeldung.object.copy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ObjectCopyService {
+
+    public ShoppingCart shallowCopy(ShoppingCart shoppingCart, String cartName) {
+
+        ShoppingCart shallowCopy = shoppingCart;
+        
+        shallowCopy.setCartName(cartName);
+
+        return shallowCopy;
+    }
+
+    public ShoppingCart deepCopy(ShoppingCart shoppingCart, String cartName) {
+
+        ShoppingCart deepCopy = new ShoppingCart();
+
+        ArrayList<String> itemList = new ArrayList<String>();
+
+        shoppingCart.setCartName(shoppingCart.getCartName());
+        shoppingCart.getItems()
+            .stream()
+            .forEach(t -> itemList.add(t));
+
+        deepCopy.setItems(itemList);
+        deepCopy.setCartName(cartName);
+
+        return deepCopy;
+    }
+
+    public ShoppingCart cloneCopy(ShoppingCart shoppingCart, String cartName) throws CloneNotSupportedException {
+
+        ShoppingCart cloneCopy = (ShoppingCart) shoppingCart.clone();
+        
+        cloneCopy.setCartName(cartName);
+        
+        return cloneCopy;
+    }
+    
+    @SuppressWarnings("unchecked")
+    public ShoppingCart deepCloneCopy(ShoppingCart shoppingCart, String cartName) throws CloneNotSupportedException {
+
+        ShoppingCart deepCloneCopy = (ShoppingCart) shoppingCart.clone();
+        
+        ArrayList<String> itemsList = (ArrayList<String>) shoppingCart.getItems();
+
+        deepCloneCopy.setItems((ArrayList<String>)itemsList.clone());
+        
+        deepCloneCopy.setCartName(cartName);
+        
+        return deepCloneCopy;
+    }
+}
