a3af575b32ad2d9f9a980e8f0f8ab2e6664bcafe
Esteban Sanchez Llobregat <me@fosanz.dev>
2024-06-12 21:21:42 +0200

    Corrected code for post "Deep vs Shallow Object Copy"

--- core-java-modules/core-java-18/src/test/java/com/baeldung/deep_vs_shallow_object_copy/ReferenceRectangleTest.java
+++ core-java-modules/core-java-18/src/test/java/com/baeldung/deep_vs_shallow_object_copy/ReferenceRectangleTest.java
@@ -1,35 +1,41 @@
 package com.baeldung.deep_vs_shallow_object_copy;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ReferenceRectangleTest {
 
     @Test
     public void givenReferenceRectangle_whenShallowCopy_thenOriginalAndCopyShouldShareReferences() {
+        // Given
         Dimension length = new Dimension(10);
         Dimension width = new Dimension(20);
         ReferenceRectangle original = new ReferenceRectangle(length, width);
+
+        // When
         ReferenceRectangle copy = original.shallowCopy(original);
 
+        // Then
         length.setValue(30);
         width.setValue(40);
-
-        System.out.println("Original reference rectangle: " + original);
-        System.out.println("Copy of reference rectangle: " + copy);
+        assertEquals(30, copy.getLength().getValue());
+        assertEquals(40, copy.getWidth().getValue());
     }
 
     @Test
     public void givenReferenceRectangle_whenDeepCopy_thenOriginalAndCopyShouldNotShareReferences() {
+        // Given
         Dimension length = new Dimension(10);
         Dimension width = new Dimension(20);
         ReferenceRectangle original = new ReferenceRectangle(length, width);
+
+        // When
         ReferenceRectangle copy = original.deepCopy(original);
 
+        // Then
         length.setValue(30);
         width.setValue(40);
-
-        System.out.println("Original reference rectangle: " + original);
-        System.out.println("Copy of reference rectangle: " + copy);
+        assertEquals(10, copy.getLength().getValue());
+        assertEquals(20, copy.getWidth().getValue());
     }
-
 }
