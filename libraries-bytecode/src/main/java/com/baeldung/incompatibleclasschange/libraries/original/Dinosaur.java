0d317fa32fee10d6c75385b2f87fc6e84aa3cd32
Bhaskar <dastidar.bhaskar@gmail.com>
2024-06-16 00:24:57 +0530

    [BAEL-5406] IncompatibleClassChange Error

--- libraries-bytecode/src/main/java/com/baeldung/incompatibleclasschange/libraries/original/Dinosaur.java
+++ libraries-bytecode/src/main/java/com/baeldung/incompatibleclasschange/libraries/original/Dinosaur.java
@@ -2,10 +2,10 @@ package com.baeldung.incompatibleclasschange.libraries.original;
 
 public class Dinosaur {
     public void species(String sp) {
-	if(sp == null) {
-		System.out.println("I am a generic Dinosaur");
-	} else {
-		System.out.println(sp);
-	}
+        if (sp == null) {
+            System.out.println("I am a generic Dinosaur");
+        } else {
+            System.out.println(sp);
+        }
     }
 }
