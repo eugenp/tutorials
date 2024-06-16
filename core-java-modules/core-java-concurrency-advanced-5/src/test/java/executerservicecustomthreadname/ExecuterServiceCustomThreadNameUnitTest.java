561d785b910543affda74cade96b7b16f96380ff
Sachin kumar <sachin.n.kumar@oracle.com>
2024-06-16 12:55:37 +0530

    bael-7921 updated jnuit

--- core-java-modules/core-java-concurrency-advanced-5/src/test/java/executerservicecustomthreadname/ExecuterServiceCustomThreadNameUnitTest.java
+++ core-java-modules/core-java-concurrency-advanced-5/src/test/java/executerservicecustomthreadname/ExecuterServiceCustomThreadNameUnitTest.java
@@ -1,53 +1,60 @@
-package com.baeldung.executerservicecustomthreadname;
+package executerservicecustomthreadname;
+
+import static org.junit.Assert.assertTrue;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
+
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
+import org.junit.jupiter.api.Test;
+
+import com.baeldung.executerservicecustomthreadname.CustomThreadFactory;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
-public class ExecuterServiceCustomThreadName {
+public class ExecuterServiceCustomThreadNameUnitTest {
 
-    public static void main(String[] args) {
-        usingDefaultFactory();
-        usingCommonsLang();
-        usingCustomFactory();
-        usingGuvava();
-    }
-
-    private static void usingDefaultFactory() {
+    @Test
+    void whenUsingDefaultThreadFactory_thenCustomThreadName() {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> assertTrue(
+                    Thread.currentThread().getName().startsWith("MyCustomThread")));
         }
         executorService.shutdown();
     }
 
-    private static void usingCustomFactory() {
+    @Test
+    void whenUsingCustomThreadFactory_thenCustomThreadName() {
         CustomThreadFactory myThreadFactory = new CustomThreadFactory("MyCustomThread-");
         ExecutorService executorService = Executors.newFixedThreadPool(3, myThreadFactory);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> assertTrue(
+                    Thread.currentThread().getName().startsWith("MyCustomThread")));
         }
         executorService.shutdown();
     }
 
-    private static void usingCommonsLang() {
+    @Test
+    void whenUsingCommonsLangThreadFactory_thenCustomThreadName() {
         BasicThreadFactory factory = new BasicThreadFactory.Builder()
                 .namingPattern("MyCustomThread-%d").priority(Thread.MAX_PRIORITY).build();
         ExecutorService executorService = Executors.newFixedThreadPool(3, factory);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> assertTrue(
+                    Thread.currentThread().getName().startsWith("MyCustomThread")));
         }
         executorService.shutdown();
     }
 
-    private static void usingGuvava() {
+    @Test
+    void whenUsingGuavaThreadFactory_thenCustomThreadName() {
         ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
                 .setNameFormat("MyCustomThread-%d").build();
         ExecutorService executorService = Executors.newFixedThreadPool(3, namedThreadFactory);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> assertTrue(
+                    Thread.currentThread().getName().startsWith("MyCustomThread")));
         }
         executorService.shutdown();
     }
