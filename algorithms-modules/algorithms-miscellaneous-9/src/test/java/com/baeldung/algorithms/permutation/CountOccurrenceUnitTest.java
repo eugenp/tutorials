a9a2274ac4f9d5ed6117badb93834dc14c7c4b6b
luca <lucacambi77@gmail.com>
2024-06-18 13:06:48 +0200

    fix: review

--- algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/algorithms/permutation/CountOccurrenceUnitTest.java
+++ algorithms-modules/algorithms-miscellaneous-9/src/test/java/com/baeldung/algorithms/permutation/CountOccurrenceUnitTest.java
@@ -1,6 +1,5 @@
 package com.baeldung.algorithms.permutation;
 
-import static com.baeldung.algorithms.permutation.CountOccurrence.countOccurrencesWithBinarySearch;
 import static com.baeldung.algorithms.permutation.CountOccurrence.countOccurrencesWithCounter;
 import static com.baeldung.algorithms.permutation.CountOccurrence.countOccurrencesWithMap;
 import static com.baeldung.algorithms.permutation.CountOccurrence.countOccurrencesWithStream;
@@ -14,13 +13,13 @@ public class CountOccurrenceUnitTest {
 
     @Test
     void givenArrayOfIntegers_whenCountOccurrenceWithCounter_thenAtIndexIsCount() {
-        int[] counter = countOccurrencesWithCounter(new int[] { 2, 3, 1, 1, 3, 4, 5, 6, 7, 8 });
+        int[] counter = countOccurrencesWithCounter(new int[] { 2, 3, 1, 1, 3, 4, 5, 6, 7, 8 }, 10);
         assertEquals(2, counter[3]);
     }
 
     @Test
     void givenArrayOfIntegers_whenCountOccurrenceWithMap_thenGetIsCount() {
-        Map<Integer, Integer> counter = CountOccurrence.countOccurrencesWithMap(new int[] { 2, 3, 1, -1, 3, 4, 5, 6, 7, 8, -1 });
+        Map<Integer, Integer> counter = countOccurrencesWithMap(new Integer[] { 2, 3, 1, -1, 3, 4, 5, 6, 7, 8, -1 });
         assertEquals(2, counter.get(-1));
     }
 
@@ -38,13 +37,7 @@ public class CountOccurrenceUnitTest {
 
     @Test
     void givenArrayOfIntegers_whenCountOccurrenceWithStream_thenGetIsCount() {
-        Map<Integer, Long> counter = countOccurrencesWithStream(new int[] { 2, 3, 1, -1, 3, 4, 5, 6, 7, 8, -1 });
+        Map<Integer, Long> counter = countOccurrencesWithStream(new Integer[] { 2, 3, 1, -1, 3, 4, 5, 6, 7, 8, -1 });
         assertEquals(2, counter.get(-1));
-    }
-
-    @Test
-    void givenArrayOfIntegers_whenCountOccurrenceOfSortedArray_thenReturnOccurrenceWithBinarySearch() {
-        int count = countOccurrencesWithBinarySearch(new int[] { 1, 2, 3, 3, 3, 4, 5, 6, 7, 8 }, 10, 3);
-        assertEquals(3, count);
     }
 }
