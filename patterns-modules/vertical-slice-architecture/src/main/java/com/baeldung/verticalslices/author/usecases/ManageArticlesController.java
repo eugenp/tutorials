da0945057316fc11ebf2f1f7122d1a67ed1a1cf5
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-06-18 16:10:19 +0200

    BAEL-7975: added missing annotations

--- patterns-modules/vertical-slice-architecture/src/main/java/com/baeldung/verticalslices/author/usecases/ManageArticlesController.java
+++ patterns-modules/vertical-slice-architecture/src/main/java/com/baeldung/verticalslices/author/usecases/ManageArticlesController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ import com.baeldung.verticalslices.author.usecases.CreateArticleUseCase.CreateAr
 import jakarta.websocket.server.PathParam;
 
 @RestController
+@RequestMapping("content")
 class ManageArticlesController {
 
     private final CreateArticleUseCase createArticle;
--- patterns-modules/vertical-slice-architecture/src/main/java/com/baeldung/verticalslices/reader/ReaderController.java
+++ patterns-modules/vertical-slice-architecture/src/main/java/com/baeldung/verticalslices/reader/ReaderController.java
@@ -3,6 +3,7 @@ package com.baeldung.verticalslices.reader;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import com.baeldung.hexagonal.dto.ArticleDto;
 
 
 @RestController
+@RequestMapping("articles")
 class ReaderController {
 
     private final ReadArticleUseCase viewArticle;
