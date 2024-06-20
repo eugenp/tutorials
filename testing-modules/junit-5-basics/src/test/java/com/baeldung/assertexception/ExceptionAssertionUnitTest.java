07f1f3108d1a94128404cac1b434c6dc08a33c7b
Yadukrishnan <yadavan88@gmail.com>
2024-06-20 19:07:29 +0200

    Methods to handle exceptions in JUnit tests

--- testing-modules/junit-5-basics/src/test/java/com/baeldung/assertexception/ExceptionAssertionUnitTest.java
+++ testing-modules/junit-5-basics/src/test/java/com/baeldung/assertexception/ExceptionAssertionUnitTest.java
@@ -2,8 +2,7 @@ package com.baeldung.assertexception;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ExceptionAssertionUnitTest {
 
@@ -25,6 +24,34 @@ class ExceptionAssertionUnitTest {
         String actualMessage = exception.getMessage();
 
         assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    void ensureNoExceptionThrown_thenAssertSucceed() {
+        assertDoesNotThrow(() -> {
+            Integer.parseInt("100");
+        });
+    }
+
+    private <T extends Exception> void assertSpecificExceptionIsNotThrown(Class<T> exceptionClass, Executable executable) {
+        try {
+            executable.execute();
+        } catch (Exception e) {
+            if (exceptionClass.isInstance(e)) {
+                fail(e.getClass().getSimpleName() + " was thrown");
+            } else {
+                // Any other exception types are ignored and test passes!
+                // Logging it here for debugging purpose
+                System.out.printf("Caught exception: %s, but ignoring since it it not an instance of %s", e.getClass().getName(), exceptionClass.getName());
+            }
+        }
+    }
+
+    @Test
+    public void givenASpecificExceptionType_thenEnsureThatExceptionIsNotThrown() {
+        assertSpecificExceptionIsNotThrown(IllegalArgumentException.class, () -> {
+            int i = 100 / 10;
+        });
     }
 
 }
