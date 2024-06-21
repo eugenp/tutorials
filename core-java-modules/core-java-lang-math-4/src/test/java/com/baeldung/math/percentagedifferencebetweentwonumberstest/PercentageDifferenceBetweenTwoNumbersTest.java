ed9f99f78b777dfb6a940e03af0f2d8c9328225f
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2024-06-21 15:16:35 +0000

    refactored test package to match source package
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

--- core-java-modules/core-java-lang-math-4/src/test/java/com/baeldung/math/percentagedifferencebetweentwonumberstest/PercentageDifferenceBetweenTwoNumbersTest.java
+++ core-java-modules/core-java-lang-math-4/src/test/java/com/baeldung/math/percentagedifferencebetweentwonumberstest/PercentageDifferenceBetweenTwoNumbersTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.PercentageDifferenceBetweenTwoNumbersTest;
+package com.baeldung.math.percentagedifferencebetweentwonumberstest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
