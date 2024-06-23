2b4d16bc49b725b920265a7496946ae2fd9a91a9
Andrei Branza <andrei.branza@yahoo.com>
2024-06-23 19:46:26 +0300

    BAEL-8043 - Changes according to comments.

--- apache-libraries-2/src/test/java/com/baeldung/avro/AvroDefaultValuesUnitTest.java
+++ apache-libraries-2/src/test/java/com/baeldung/avro/AvroDefaultValuesUnitTest.java
@@ -10,17 +10,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class AvroDefaultValuesUnitTest {
 
     @Test
-    public void givenCarJsonSchema_whenCarIsSerialized_thenCarIsSuccessfullyDeserialized() throws IOException {
+    public void givenCarJsonSchema_whenCarIsSerialized_thenCarIsSuccessfullyDeserialized() {
 
         Car car = new Car();
         car.setColor("blue");
 
         SerializationDeserializationLogic.serializeCar(car);
-        Car deserializedCar1 = SerializationDeserializationLogic.deserializeCar();
+        Car deserializedCar = SerializationDeserializationLogic.deserializeCar();
 
-        assertNull(deserializedCar1.getBrand());
-        assertNull(deserializedCar1.getNumberOfDoors());
-        assertEquals("blue", deserializedCar1.getColor().toString());
-        assertEquals(0, deserializedCar1.getNumberOfWheels());
+        assertNull(deserializedCar.getBrand());
+        assertNull(deserializedCar.getNumberOfDoors());
+        assertEquals("blue", deserializedCar.getColor().toString());
+        assertEquals(0, deserializedCar.getNumberOfWheels());
     }
 }
