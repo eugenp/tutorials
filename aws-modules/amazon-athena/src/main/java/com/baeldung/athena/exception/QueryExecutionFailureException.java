808a3f2c6851cba528a62746177554bc4f9ea0d0
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-06-25 20:14:52 +0530

    updating codebase

--- aws-modules/amazon-athena/src/main/java/com/baeldung/athena/exception/QueryExecutionFailureException.java
+++ aws-modules/amazon-athena/src/main/java/com/baeldung/athena/exception/QueryExecutionFailureException.java
@@ -4,8 +4,11 @@ public class QueryExecutionFailureException extends RuntimeException {
 
     private static final long serialVersionUID = 4359781704223584247L;
 
-    public QueryExecutionFailureException(String reason) {
-        super(reason);
+    public QueryExecutionFailureException() {
+    }
+
+    public QueryExecutionFailureException(String message) {
+        super(message);
     }
 
 }
--- aws-modules/amazon-athena/src/main/java/com/baeldung/athena/service/QueryService.java
+++ aws-modules/amazon-athena/src/main/java/com/baeldung/athena/service/QueryService.java
@@ -1,12 +1,9 @@
 package com.baeldung.athena.service;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.Collectors;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.baeldung.athena.exception.QueryExecutionFailureException;
@@ -14,13 +11,16 @@ import com.baeldung.athena.exception.QueryExecutionFailureException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.athena.AthenaClient;
 import software.amazon.awssdk.services.athena.model.Datum;
 import software.amazon.awssdk.services.athena.model.GetQueryResultsResponse;
+import software.amazon.awssdk.services.athena.model.InvalidRequestException;
 import software.amazon.awssdk.services.athena.model.QueryExecutionContext;
 import software.amazon.awssdk.services.athena.model.QueryExecutionState;
 import software.amazon.awssdk.services.athena.model.ResultConfiguration;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class QueryService {
@@ -31,12 +31,18 @@ public class QueryService {
 
     private static final long WAIT_PERIOD = 30;
 
-    public List<Map<String, String>> execute(@NonNull final String sqlQuery) {
-        final var queryExecutionId = athenaClient.startQueryExecution(query -> 
-            query.queryString(sqlQuery)
-                .queryExecutionContext(queryExecutionContext)
-                .resultConfiguration(resultConfiguration)
-        ).queryExecutionId();
+    public JSONArray execute(@NonNull final String sqlQuery) {
+        String queryExecutionId;
+        try {
+            queryExecutionId = athenaClient.startQueryExecution(query -> 
+                query.queryString(sqlQuery)
+                  .queryExecutionContext(queryExecutionContext)
+                  .resultConfiguration(resultConfiguration)
+            ).queryExecutionId();
+        } catch (final InvalidRequestException exception) {
+            log.error("Invalid SQL syntax detected in query {}", sqlQuery, exception);
+            throw new QueryExecutionFailureException();
+        }
 
         waitForQueryToComplete(queryExecutionId);
 
@@ -57,7 +63,9 @@ public class QueryService {
             switch (queryState) {
                 case FAILED:
                 case CANCELLED:
-                    throw new QueryExecutionFailureException("Query execution failed. Validate the query before resubmitting.");
+                    final var error = response.queryExecution().status().athenaError().errorMessage();
+                    log.error("Query execution failed: {}", error);
+                    throw new QueryExecutionFailureException();
                 case QUEUED:
                 case RUNNING:
                     TimeUnit.MILLISECONDS.sleep(WAIT_PERIOD);
@@ -72,27 +80,30 @@ public class QueryService {
     }
 
     @SneakyThrows
-    private List<Map<String, String>> processQueryResult(@NonNull final GetQueryResultsResponse queryResultsResponse) {
+    private JSONArray processQueryResult(@NonNull final GetQueryResultsResponse queryResultsResponse) {
+        final var response = new JSONArray();
         final var rows = queryResultsResponse.resultSet().rows();
         if (rows.isEmpty()) {
-            return Collections.emptyList();
+            return response;
         }
         final var headers = rows.get(0).data().stream()
-            .map(Datum::varCharValue)
-            .toList();
+          .map(Datum::varCharValue)
+          .toList();
 
-        return rows.stream()
+        rows.stream()
             .skip(1)
-            .map(row -> {
+            .forEach(row -> {
+                final var element = new JSONObject();
                 final var data = row.data();
-                final var rowMap = new HashMap<String, String>();
-
+               
                 for (int i = 0; i < headers.size(); i++) {
-                    rowMap.put(headers.get(i), data.get(i).varCharValue());
+                    final var key = headers.get(i);
+                    final var value = data.get(i).varCharValue();
+                    element.put(key, value);
                 }
-                return rowMap;
-            })
-            .collect(Collectors.toList());
+                response.put(element);
+            });
+        return response;
     }
     
 }
