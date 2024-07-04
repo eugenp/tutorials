b933b0fe568ea09f4c88e5862dd698765e5d9c11
Richard Danladi <richarddanladi7@gmail.com>
2024-07-04 21:51:25 +0100

    Refactored postgresoverssl

--- persistence-modules/spring-jpa-3/src/main/java/com/baeldung/pgoverssl/PgSpringboot.java
+++ persistence-modules/spring-jpa-3/src/main/java/com/baeldung/pgoverssl/PgSpringboot.java
@@ -3,7 +3,7 @@ package com.baeldung.pgoverssl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.baeldung")
 public class PgSpringboot {
 
     public static void main(String[] args) {
