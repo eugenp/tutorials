ed8bf1336d6601ca27485f06c9a41dc6f1fbc391
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-07-05 20:33:53 +0200

    BAEL-8202: add bael formatter

--- maven-modules/maven-plugins/spotless/src/main/java/com/baeldung/spotless/FizzBuzz.java
+++ maven-modules/maven-plugins/spotless/src/main/java/com/baeldung/spotless/FizzBuzz.java
@@ -4,27 +4,28 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FizzBuzz {
-	public static void main(String[] args) {
-		Map<Integer, String> dividers = new HashMap<>();
-		dividers.put(3, "Fizz");
-		dividers.put(5, "Buzz");
 
-		int limit = 100;
+    public static void main(String[] args) {
+        Map<Integer, String> dividers = new HashMap<>();
+        dividers.put(3, "Fizz");
+        dividers.put(5, "Buzz");
 
-		for (int i = 1; i <= limit; i++) {
-			StringBuilder output = new StringBuilder();
+        int limit = 100;
 
-			for (Map.Entry<Integer, String> entry : dividers.entrySet()) {
-				if (i % entry.getKey() == 0) {
-					output.append(entry
-							.getValue());
-				}
-			}
-			if (output.length() == 0) {
-				System.out.println(i);
-			} else {
-				System.out.println(output);
-			}
-		}
-	}
+        for (int i = 1; i <= limit; i++) {
+            StringBuilder output = new StringBuilder();
+
+            for (Map.Entry<Integer, String> entry : dividers.entrySet()) {
+                if (i % entry.getKey() == 0) {
+                    output.append(entry.getValue());
+                }
+            }
+            if (!(output.length() == 0)) {
+                System.out.println(output);
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
 }
