87148394729be69d7ce27a6064f940bde9cd01dd
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-07-06 17:59:45 +0200

    BAEL-8190: code review

--- spring-boot-modules/spring-boot-3-testcontainers/src/main/java/com/baeldung/testcontainers/jdbc/Hobbit.java
+++ spring-boot-modules/spring-boot-3-testcontainers/src/main/java/com/baeldung/testcontainers/jdbc/Hobbit.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity(name = "hobbits")
+@Entity
 public class Hobbit {
 
     @Id
