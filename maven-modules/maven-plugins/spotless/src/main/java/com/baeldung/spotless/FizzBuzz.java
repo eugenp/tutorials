14eab4e1e7183ce866c7ed007a9a184f7590e31f
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-07-06 17:19:49 +0200

    BAEL-8202: final changes

--- maven-modules/maven-plugins/spotless/src/main/java/com/baeldung/spotless/FizzBuzz.java
+++ maven-modules/maven-plugins/spotless/src/main/java/com/baeldung/spotless/FizzBuzz.java
@@ -2,39 +2,35 @@ package com.baeldung.spotless;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.io.File;
-import java.lang.annotation.Documented;
-import java.util.Map;
 import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
 
 public class FizzBuzz {
 
 	@Nullable
 	static String pi = "3.14";
 
-    public static void main(String[] args) {
-        Map<Integer, String> dividers = new HashMap<>();
-        dividers.put(3, "Fizz");
-        dividers.put(5, "Buzz");
+	public static void main(String[] args) {
+		Map<Integer, String> dividers = new HashMap<>();
+		dividers.put(3, "Fizz");
+		dividers.put(5, "Buzz");
 
-        int limit = 100;
+		int limit = 100;
 
-        for (int i = 1; i <= limit; i++) {
-            StringBuilder output = new StringBuilder();
+		for (int i = 1; i <= limit; i++) {
+			StringBuilder output = new StringBuilder();
 
-            for (Map.Entry<Integer, String> entry : dividers.entrySet()) {
-                if (i % entry.getKey() == 0) {
-                    output.append(entry.getValue());
-                }
-            }
-            if (!(output.length() == 0)) {
-                System.out.println(output);
-            } else {
-                System.out.println(i);
-            }
-        }
-    }
+			for (Map.Entry<Integer, String> entry : dividers.entrySet()) {
+				if (i % entry.getKey() == 0) {
+					output.append(entry.getValue());
+				}
+			}
+			if (!(output.length() == 0)) {
+				System.out.println(output);
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
 
 }
