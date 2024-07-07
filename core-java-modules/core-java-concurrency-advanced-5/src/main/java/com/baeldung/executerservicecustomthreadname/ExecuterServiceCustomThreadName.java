dbdf506ce73120e785f38c754b83db852de9edba
sachin <56427366+sachin071287@users.noreply.github.com>
2024-07-07 09:23:54 +0530

    Update ExecuterServiceCustomThreadName.java

--- core-java-modules/core-java-concurrency-advanced-5/src/main/java/com/baeldung/executerservicecustomthreadname/ExecuterServiceCustomThreadName.java
+++ core-java-modules/core-java-concurrency-advanced-5/src/main/java/com/baeldung/executerservicecustomthreadname/ExecuterServiceCustomThreadName.java
@@ -5,8 +5,12 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExecuterServiceCustomThreadName {
+
+    private static final Logger logger = LoggerFactory.getLogger(ExecuterServiceCustomThreadName.class);
 
     public static void main(String[] args) {
         usingDefaultFactory();
@@ -18,7 +22,7 @@ public class ExecuterServiceCustomThreadName {
     private static void usingDefaultFactory() {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> logger.info(Thread.currentThread().getName()));
         }
         executorService.shutdown();
     }
@@ -27,7 +31,7 @@ public class ExecuterServiceCustomThreadName {
         CustomThreadFactory myThreadFactory = new CustomThreadFactory("MyCustomThread-");
         ExecutorService executorService = Executors.newFixedThreadPool(3, myThreadFactory);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> logger.info(Thread.currentThread().getName()));
         }
         executorService.shutdown();
     }
@@ -37,7 +41,7 @@ public class ExecuterServiceCustomThreadName {
                 .namingPattern("MyCustomThread-%d").priority(Thread.MAX_PRIORITY).build();
         ExecutorService executorService = Executors.newFixedThreadPool(3, factory);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> logger.info(Thread.currentThread().getName()));
         }
         executorService.shutdown();
     }
@@ -47,7 +51,7 @@ public class ExecuterServiceCustomThreadName {
                 .setNameFormat("MyCustomThread-%d").build();
         ExecutorService executorService = Executors.newFixedThreadPool(3, namedThreadFactory);
         for (int i = 0; i < 5; i++) {
-            executorService.execute(() -> System.out.println(Thread.currentThread().getName()));
+            executorService.execute(() -> logger.info(Thread.currentThread().getName()));
         }
         executorService.shutdown();
     }
