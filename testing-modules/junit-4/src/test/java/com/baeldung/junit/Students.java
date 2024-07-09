06295e8dc0a0a341f75e768e5efb0c81069629b8
Anees1214 <aneesasghar.cs@gmail.com>
2024-07-10 00:08:00 +0500

    Create Students.java

---
+++ testing-modules/junit-4/src/test/java/com/baeldung/junit/Students.java
@@ -0,0 +1,15 @@
+package com.baeldung.junit;
+public class Students implements Cloneable {
+ String name;
+ Courses course;
+
+ public Students(String name, Courses course) {
+     this.name = name;
+     this.course = course;
+ }
+
+ @Override
+ protected Object clone() throws CloneNotSupportedException {
+     return super.clone();
+ }
+}
