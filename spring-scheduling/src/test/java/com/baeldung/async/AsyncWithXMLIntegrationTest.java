c2ded1332324686829780050ce551589bb179790
timis1 <12120641+timis1@users.noreply.github.com>
2024-07-10 09:34:30 +0300

    [JAVA-36167] Review log statements for projects - Week 27 - 2024 (#16961)

--- spring-scheduling/src/test/java/com/baeldung/async/AsyncWithXMLIntegrationTest.java
+++ spring-scheduling/src/test/java/com/baeldung/async/AsyncWithXMLIntegrationTest.java
@@ -17,10 +17,8 @@ public class AsyncWithXMLIntegrationTest {
 
     @Test
     public void testAsyncAnnotationForMethodsWithVoidReturnType() throws InterruptedException {
-        System.out.println("Start - invoking an asynchronous method. " + Thread.currentThread().getName());
         asyncAnnotationExample.asyncMethodWithVoidReturnType();
         Thread.sleep(2000);
-        System.out.println("End - invoking an asynchronous method. ");
     }
 
 }
