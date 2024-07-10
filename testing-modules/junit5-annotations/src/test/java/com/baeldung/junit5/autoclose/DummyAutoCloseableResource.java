7107f847b65b3ba6cbeb15c5d33a84b326a24b06
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-07-10 09:08:50 +0200

    BAEL-8104: swap sys-outs for logging

--- testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/autoclose/DummyAutoCloseableResource.java
+++ testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/autoclose/DummyAutoCloseableResource.java
@@ -1,12 +1,15 @@
 package com.baeldung.junit5.autoclose;
 
-class DummyAutoCloseableResource implements AutoCloseable {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+class DummyAutoCloseableResource implements AutoCloseable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DummyAutoCloseableResource.class);
     private boolean open = true;
 
     @Override
     public void close() {
-        System.out.println("Closing Dummy Resource");
+        LOGGER.info("Closing Dummy Resource");
         open = false;
     }
 
--- testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/autoclose/DummyClearableResource.java
+++ testing-modules/junit5-annotations/src/test/java/com/baeldung/junit5/autoclose/DummyClearableResource.java
@@ -1,11 +1,14 @@
 package com.baeldung.junit5.autoclose;
 
-class DummyClearableResource {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+class DummyClearableResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DummyClearableResource.class);
     private boolean cleared = false;
 
     public void clear() {
-        System.out.println("Clear Dummy Resource");
+        LOGGER.info("Clear Dummy Resource");
         cleared = true;
     }
 
