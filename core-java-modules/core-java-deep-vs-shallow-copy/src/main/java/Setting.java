8013f2c72c19defe04349dc97c84ced1ea84e83b
Shyaka L Tresor <shyakadev@gmail.com>
2024-07-11 11:51:24 +0200

    create classes to use

---
+++ core-java-modules/core-java-deep-vs-shallow-copy/src/main/java/Setting.java
@@ -0,0 +1,20 @@
+public class Setting {
+
+    private String name;
+    private String value;
+
+    public Setting(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public Setting(Setting setting) {
+        this.name = setting.name;
+        this.value = setting.value;
+    }
+
+    @Override
+    public String toString() {
+        return "Setting{name='" + name + "', value='" + value + "'}";
+    }
+}
---
+++ core-java-modules/core-java-deep-vs-shallow-copy/src/main/java/SystemConfig.java
@@ -0,0 +1,31 @@
+public class SystemConfig {
+
+    private int id;
+    private Setting setting;
+
+    public SystemConfig(int id, Setting setting) {
+        this.id = id;
+        this.setting = setting;
+    }
+
+    public SystemConfig(SystemConfig systemConfig) {
+        this.id = systemConfig.id;
+        this.setting = new Setting(systemConfig.setting);
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setSetting(Setting setting) {
+        this.setting = setting;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Setting getSetting() {
+        return setting;
+    }
+}
