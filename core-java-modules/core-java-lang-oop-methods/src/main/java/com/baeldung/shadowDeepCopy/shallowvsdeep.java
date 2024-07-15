0eeb316c62e2d9980405eb7649c4b703d03bc431
Qazi Moeed <101875663+qazimoeed@users.noreply.github.com>
2024-07-15 23:08:18 +0500

    Separation of Code

--- core-java-modules/core-java-lang-oop-methods/src/main/java/com/baeldung/shadowDeepCopy/shallowvsdeep.java
+++ core-java-modules/core-java-lang-oop-methods/src/main/java/com/baeldung/shadowDeepCopy/shallowvsdeep.java
@@ -1,10 +1,8 @@
+//For shallow Copy part.
 //import java.util.ArrayList;
 //
 //class BaeldungClass {
-//    // instance variable of the class ABC
 //    ArrayList<Object> list;
-//
-//    // constructor to initialize the ArrayList
 //    public BaeldungClass() {
 //        list = new ArrayList<>();
 //    }
@@ -12,43 +10,33 @@
 //
 //public class shallowvsdeep{
 //    public static void main(String[] args) {
-//        // creating an object of the class ABC
 //        BaeldungClass firstObj = new BaeldungClass();
-//        firstObj.list.add(20);       // adding an integer
-//        firstObj.list.add("Hello");  // adding a string
-//        firstObj.list.add(10.5f);    // adding a float
+//        firstObj.list.add(20); 
+//        firstObj.list.add("Hello"); 
+//        firstObj.list.add(10.5f); 
 //
-//        // it will copy the reference, not value
 //        BaeldungClass secObj = firstObj;
 //
-//        // updating the second element value to "World" using the reference variable secObj
 //        secObj.list.set(1, "World");
 //
-//        // printing the value of list using reference variable firstObj
 //        System.out.println("The value of list in firstObj is: " + firstObj.list);
-//
-//        // printing the value of list using reference variable secObj
 //        System.out.println("\nThe value of list in secObj is: " + secObj.list);
 //    }
 //}
 
 
 
+//For the  Deep Copy part
 
 package initialProj;
 
 import java.util.ArrayList;
 
 class BaeldungClass {
-    // Instance variable of the class BaeldungClass
-    ArrayList<Object> list;
-
-    // Constructor to initialize the ArrayList
-    public BaeldungClass() {
+     ArrayList<Object> list;
+     public BaeldungClass() {
         list = new ArrayList<>();
     }
-
-    // Constructor to create a deep copy
     public BaeldungClass(BaeldungClass original) {
         list = new ArrayList<>(original.list);
     }
@@ -56,7 +44,6 @@ class BaeldungClass {
 
 public class shallowvsdeep {
     public static void main(String[] args) {
-        // Creating an object of the class BaeldungClass
         BaeldungClass firstObj = new BaeldungClass();
         firstObj.list.add(20);       // Adding an integer
         firstObj.list.add("Hello");  // Adding a string
