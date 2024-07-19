24b45b5380f57c22582de0768af2398170ef57db
Gaetano Piazzolla <gae.piaz@gmail.com>
2024-07-19 17:27:52 +0200

    JAVA-37419 - jakarta

--- spring-exceptions/src/main/java/com/baeldung/ex/mappingexception/cause3/persistence/model/Foo.java
+++ spring-exceptions/src/main/java/com/baeldung/ex/mappingexception/cause3/persistence/model/Foo.java
@@ -2,10 +2,10 @@ package com.baeldung.ex.mappingexception.cause3.persistence.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Foo implements Serializable {
