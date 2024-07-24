64e14bc63fb391aea688afc88182609dcbefa851
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2024-07-24 14:59:24 +0000

    changed V2 to v2
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

--- core-java-modules/core-java-lang-math-4/src/main/java/com/baeldung/math/percentdiffbetweentwonumbers/PercentDiffBetweenTwoNumbers.java
+++ core-java-modules/core-java-lang-math-4/src/main/java/com/baeldung/math/percentdiffbetweentwonumbers/PercentDiffBetweenTwoNumbers.java
@@ -1,11 +1,11 @@
 package com.baeldung.math.percentdiffbetweentwonumbers;
 
 public class PercentDiffBetweenTwoNumbers {
-  public static double calculatePercentageDifference(double v1, double V2) {
-    double average = (v1 + V2) / 2;
+  public static double calculatePercentageDifference(double v1, double v2) {
+    double average = (v1 + v2) / 2;
     if (average == 0) {
       throw new IllegalArgumentException("The average of V1 and V2 cannot be zero.");
     }
-    return Math.abs((v1 - V2) / average) * 100;
+    return Math.abs((v1 - v2) / average) * 100;
   }
 }
