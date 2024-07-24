68bea61b07b818c4a324b70b2e6810373993c3d0
Bahaa El-Din Helmy <bahaavampire@yahoo.com>
2024-07-24 21:19:46 +0300

    Java Enums with all HTTP status codes
    I added the test classes EnumWithHttpStatusCodesLiveTest.java and EnumWithHttpStatusCodesUnitTest.java.

---
+++ apache-httpclient-2/src/test/java/com/baeldung/enumwithallhttpstatuscodes/EnumWithHttpStatusCodesUnitTest.java
@@ -0,0 +1,25 @@
+package com.baeldung.enumwithallhttpstatuscodes;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class EnumWithHttpStatusCodesUnitTest {
+    @Test
+    public void givenStatusCode_whenUsingBasicApproach_thenGetCorrectCode() {
+        assertEquals(100, HttpStatus.CONTINUE.getCode());
+        assertEquals(200, HttpStatus.OK.getCode());
+        assertEquals(300, HttpStatus.MULTIPLE_CHOICES.getCode());
+        assertEquals(400, HttpStatus.BAD_REQUEST.getCode());
+        assertEquals(500, HttpStatus.INTERNAL_SERVER_ERROR.getCode());
+    }
+
+    @Test
+    public void givenStatusCode_whenUsingBasicApproach_thenGetCorrectDescription() {
+        assertEquals("Continue", HttpStatus.CONTINUE.getDescription());
+        assertEquals("OK", HttpStatus.OK.getDescription());
+        assertEquals("Multiple Choices", HttpStatus.MULTIPLE_CHOICES.getDescription());
+        assertEquals("Bad Request", HttpStatus.BAD_REQUEST.getDescription());
+        assertEquals("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR.getDescription());
+    }
+}
