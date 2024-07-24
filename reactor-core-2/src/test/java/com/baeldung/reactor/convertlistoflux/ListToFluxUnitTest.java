c81e025369d39c30430805abea3c485eab8888b2
LordMaduz <maduz.ruchira@gmail.com>
2024-07-24 23:06:36 +0800

    BAEL-7567 - How to convert List<T> to Flux<T> in Spring Reactor (modify test cases)

--- reactor-core-2/src/test/java/com/baeldung/reactor/convertlistoflux/ListToFluxUnitTest.java
+++ reactor-core-2/src/test/java/com/baeldung/reactor/convertlistoflux/ListToFluxUnitTest.java
@@ -1,12 +1,8 @@
 package com.baeldung.reactor.convertlistoflux;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Supplier;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 public class ListToFluxUnitTest {
@@ -14,7 +10,7 @@ public class ListToFluxUnitTest {
     @Test
     public void givenList_whenCallingFromIterableOperator_thenListItemsTransformedAsFluxAndEmitted(){
 
-        List<Integer> list = integerList();
+        List<Integer> list = List.of(1, 2,3);
         Flux<Integer> flux = listToFluxUsingFromIterableOperator(list);
 
         StepVerifier.create(flux)
@@ -27,25 +23,10 @@ public class ListToFluxUnitTest {
 
 
     @Test
-    public void givenListWrappedInsideCompletableFuture_whenCallingFlatMapIterableOperator_thenListItemsTransformedAsFluxAndEmitted(){
-
-        CompletableFuture<List<Integer>> completableFuture = listToCompletableFutureWrappedList(integerList());
-        Mono<List<Integer>> monoList = completableFutureWrappedListToMonoListUsingFromFutureOperator(completableFuture);
-        Flux<Integer> flux = monoListToFluxUsingFlatMapIterableOperator(monoList);
-
-        StepVerifier.create(flux)
-            .expectNext(1)
-            .expectNext(2)
-            .expectNext(3)
-            .expectComplete()
-            .verify();
-    }
-
-    @Test
     public void givenList_whenCallingCreateOperator_thenListItemsTransformedAsFluxAndEmitted(){
 
-        Supplier<List<Integer>> supplier = this::integerList;
-        Flux<Integer> flux = listToFluxUsingCreateOperator(supplier);
+        List<Integer> list = List.of(1, 2,3);
+        Flux<Integer> flux = listToFluxUsingCreateOperator(list);
 
         StepVerifier.create(flux)
             .expectNext(1)
@@ -53,15 +34,6 @@ public class ListToFluxUnitTest {
             .expectNext(3)
             .expectComplete()
             .verify();
-    }
-
-    private List<Integer> integerList(){
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-
-        return list;
     }
 
     private <T> Flux<T> listToFluxUsingFromIterableOperator(List<T> list) {
@@ -70,23 +42,11 @@ public class ListToFluxUnitTest {
             .log();
     }
 
-    private <T> Flux<T> monoListToFluxUsingFlatMapIterableOperator(Mono<List<T>> monoList) {
-        return monoList.flatMapIterable(l-> l).log();
-    }
-
-    private <T> Flux<T> listToFluxUsingCreateOperator(Supplier<List<T>> supplier) {
+    private <T> Flux<T> listToFluxUsingCreateOperator(List<T> list) {
         return Flux.create(sink-> {
-            supplier.get().forEach(sink::next);
+            list.forEach(sink::next);
             sink.complete();
         });
-    }
-
-    private <T> Mono<List<T>> completableFutureWrappedListToMonoListUsingFromFutureOperator(CompletableFuture<List<T>> completableFuture) {
-        return Mono.fromFuture(completableFuture);
-    }
-
-    private <T> CompletableFuture<List<T>> listToCompletableFutureWrappedList(List<T> list) {
-        return CompletableFuture.supplyAsync(()-> list);
     }
 
 }
