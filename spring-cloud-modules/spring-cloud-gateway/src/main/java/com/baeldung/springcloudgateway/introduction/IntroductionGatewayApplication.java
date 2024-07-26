5867fb754f31e50945e7d147a95167a3bab95673
Amit Pandey <amitpandey@cloud.upwork.com>
2024-07-26 23:27:08 +0530

    JAVA-31506 :- Fix Gateway errors.

--- spring-cloud-modules/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/introduction/IntroductionGatewayApplication.java
+++ spring-cloud-modules/spring-cloud-gateway/src/main/java/com/baeldung/springcloudgateway/introduction/IntroductionGatewayApplication.java
@@ -2,14 +2,32 @@ package com.baeldung.springcloudgateway.introduction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @SpringBootApplication
 @PropertySource("classpath:introduction-application.properties")
+@EnableWebFluxSecurity
 public class IntroductionGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(IntroductionGatewayApplication.class, args);
     }
 
+}
+
+@Configuration
+ class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http.authorizeExchange(c -> {
+            c.anyExchange().permitAll();
+        });
+        return http.build();
+    }
 }
