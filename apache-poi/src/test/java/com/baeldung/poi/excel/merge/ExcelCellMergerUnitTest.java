9e212c647e4aeef78de51000c79bf72a9507ec6d
Wynn <sgwebfreelancer@gmail.com>
2024-07-26 10:56:55 +0800

    add in one test case to test merged cell alignment

--- apache-poi/src/test/java/com/baeldung/poi/excel/merge/ExcelCellMergerUnitTest.java
+++ apache-poi/src/test/java/com/baeldung/poi/excel/merge/ExcelCellMergerUnitTest.java
@@ -1,12 +1,20 @@
 package com.baeldung.poi.excel.merge;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -46,6 +54,41 @@ public class ExcelCellMergerUnitTest {
         assertEquals(0, sheet.getNumMergedRegions());        
         sheet.addMergedRegion(CellRangeAddress.valueOf("A1:C1"));
         assertEquals(1, sheet.getNumMergedRegions());
+
+        workbook.close();
+    }
+
+    @Test
+    public void givenCellIndex_whenAddMergeRegionWithCenterAlignment_thenMergeRegionWithCenterCreated() throws IOException {
+        Workbook workbook = new XSSFWorkbook(fileLocation);
+        Sheet sheet = workbook.getSheetAt(0);
+
+        assertEquals(0, sheet.getNumMergedRegions());
+        int firstRow = 0;
+        int lastRow = 0;
+        int firstCol = 0;
+        int lastCol = 2;
+        sheet.addMergedRegion(new CellRangeAddress(firstRow, lastRow, firstCol, lastCol));
+
+        // Create a cell style with center alignment
+        CellStyle cellStyle = workbook.createCellStyle();
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+
+        // Apply style to the merged cell
+        Row row = sheet.getRow(firstRow);
+        Cell cell = row.getCell(firstCol);
+        cell.setCellStyle(cellStyle);
+
+        // Verify merge region
+        assertEquals(1, sheet.getNumMergedRegions());
+
+        // Verify cell style (alignment)
+        Cell readCell = sheet.getRow(firstRow).getCell(firstCol);
+        assertNotNull(readCell);
+        CellStyle readCellStyle = readCell.getCellStyle();
+        assertEquals(HorizontalAlignment.CENTER, readCellStyle.getAlignment());
+        assertEquals(VerticalAlignment.CENTER, readCellStyle.getVerticalAlignment());
 
         workbook.close();
     }
