1bb63fe3c56351e6f76beaec56e559def0a2cf56
emanueltrandafir1993 <emanueltrandafir1993@gmail.com>
2024-07-27 18:17:51 +0200

    BAEL-8198: formatting

--- patterns-modules/data-oriented-programming/src/main/java/com/baeldung/patterns/data/Strategy.java
+++ patterns-modules/data-oriented-programming/src/main/java/com/baeldung/patterns/data/Strategy.java
@@ -1,6 +1,11 @@
 package com.baeldung.patterns.data;
 
-import static com.baeldung.patterns.data.Strategies.*;
+import static com.baeldung.patterns.data.Strategies.OnePair;
+import static com.baeldung.patterns.data.Strategies.Ones;
+import static com.baeldung.patterns.data.Strategies.ThreeOfaKind;
+import static com.baeldung.patterns.data.Strategies.TwoPairs;
+import static com.baeldung.patterns.data.Strategies.Twos;
 
 public sealed interface Strategy permits Ones, Twos, OnePair, TwoPairs, ThreeOfaKind {
+
 }
