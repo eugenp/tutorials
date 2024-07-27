550d7fb13894ffafd1b1fca51e258884f59a69d5
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2024-07-28 00:04:45 +0530

    Update SuccessivePairs.java

--- algorithms-modules/algorithms-miscellaneous-9/src/main/java/com/baledung/algorithms/successivepairs/SuccessivePairs.java
+++ algorithms-modules/algorithms-miscellaneous-9/src/main/java/com/baledung/algorithms/successivepairs/SuccessivePairs.java
@@ -2,8 +2,8 @@ package com.baledung.algorithms.successivepairs;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -17,29 +17,12 @@ public class SuccessivePairs {
             .collect(Collectors.toList());
     }
 
-    public static <T> Collector<T, List<SimpleEntry<T, T>>, List<SimpleEntry<T, T>>> toPairList() {
-        return Collector.of(
-            ArrayList::new, // Supplier
-            (list, item) -> { // Accumulator
-                if (!list.isEmpty()) {
-                    list.add(new SimpleEntry<>(list.get(list.size() - 1).getValue(), item));
-                }
-                list.add(new SimpleEntry<>(null, item)); // Add placeholder for the next pair
-            },
-            (left, right) -> { // Combiner
-                if (!left.isEmpty() && !right.isEmpty()) {
-                    left.add(new SimpleEntry<>(left.get(left.size() - 1).getValue(), right.get(0).getValue()));
-                }
-                left.addAll(right);
-                return left;
-            },
-            list -> { // Finisher
-                List<SimpleEntry<T, T>> pairs = new ArrayList<>();
-                for (int i = 1; i < list.size(); i += 2) {
-                    pairs.add(list.get(i));
-                }
-                return pairs;
-            }
-        );
+    public static <T> Stream<List<T>> pairwise(Stream<T> stream) {
+        List<T> list = stream.collect(Collectors.toList());
+        List<List<T>> pairs = new ArrayList<>();
+        for (int i = 0; i < list.size() - 1; i++) {
+            pairs.add(Arrays.asList(list.get(i), list.get(i + 1)));
+        }
+        return pairs.stream();
     }
 }
