ecb8dc887cfe6aca23a4a06809e28c3fd3a91ea9
Bahaa El-Din Helmy <bahaavampire@yahoo.com>
2024-07-29 02:16:02 +0300

    Update EnumWithHttpStatusCodesUnitTest.java

--- libraries-http-3/src/test/java/com/baeldung/enumwithallhttpstatuscodes/EnumWithHttpStatusCodesUnitTest.java
+++ libraries-http-3/src/test/java/com/baeldung/enumwithallhttpstatuscodes/EnumWithHttpStatusCodesUnitTest.java
@@ -1,8 +1,9 @@
 package com.baeldung.enumwithallhttpstatuscodes;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class EnumWithHttpStatusCodesUnitTest {
     @Test
