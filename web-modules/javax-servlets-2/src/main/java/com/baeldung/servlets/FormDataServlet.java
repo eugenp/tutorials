0476addc605001bf51ea1704854cfdf23c491a5b
Rajni Kant Pandey <rajnikantpandey50@gmail.com>
2024-08-01 01:18:48 +0530

    BAEL-5865 - Getting Request Payload from POST Request in Java servlet

---
+++ web-modules/javax-servlets-2/src/main/java/com/baeldung/servlets/FormDataServlet.java
@@ -0,0 +1,25 @@
+package com.baeldung.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "FormDataServlet", urlPatterns = "/form-data")
+public class FormDataServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String firstName = req.getParameter("first_name");
+        String lastName = req.getParameter("last_name");
+
+        resp.getWriter()
+            .append("Full Name: ")
+            .append(firstName)
+            .append(" ")
+            .append(lastName);
+    }
+}
