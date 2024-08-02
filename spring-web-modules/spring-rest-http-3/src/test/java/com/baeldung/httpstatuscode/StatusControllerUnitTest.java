1a8e3ae37a8a67c50d28d4ad80ccb3669bc2e2a1
Shyaka L Tresor <shyakadev@gmail.com>
2024-08-03 00:26:40 +0200

    Update StatusControllerUnitTest.java
    * Reformatted code
    * corrected return typo

--- spring-web-modules/spring-rest-http-3/src/test/java/com/baeldung/httpstatuscode/StatusControllerUnitTest.java
+++ spring-web-modules/spring-rest-http-3/src/test/java/com/baeldung/httpstatuscode/StatusControllerUnitTest.java
@@ -1,11 +1,12 @@
 package com.baeldung.httpstatuscode;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(StatusController.class)
 public class StatusControllerUnitTest {
@@ -15,15 +16,19 @@ public class StatusControllerUnitTest {
 
     @Test
     public void whenGetRequestSentToResource_thenReturnStatusOk() throws Exception {
-        mockMvc.perform(get("/resource")).andExpect(status().is2xxSuccessful());
+        mockMvc.perform(get("/resource"))
+            .andExpect(status().is2xxSuccessful());
     }
+
     @Test
-    public void whenGetRequestSentToException_thenRetunStatusNotFound() throws Exception {
-        mockMvc.perform(get("/exception")).andExpect(status().isNotFound());
+    public void whenGetRequestSentToException_thenReturnStatusNotFound() throws Exception {
+        mockMvc.perform(get("/exception"))
+            .andExpect(status().isNotFound());
     }
 
     @Test
     public void whenGetRequestSentToCustomException_thenReturnStatusGone() throws Exception {
-        mockMvc.perform(get("/custom-exception")).andExpect(status().is4xxClientError());
+        mockMvc.perform(get("/custom-exception"))
+            .andExpect(status().is4xxClientError());
     }
 }
