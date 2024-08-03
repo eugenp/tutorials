37e21c447aa1ee17de702831e630d6bbb957750c
pedrolopes9-7 <pedroinf97@gmail.com>
2024-08-03 18:44:22 -0300

    moving to different module

--- spring-ai-3/test/java/com/baeldung/ollamachatbot/HelpDeskControllerIntegrationTest.java
+++ spring-ai-3/test/java/com/baeldung/ollamachatbot/HelpDeskControllerIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -26,6 +27,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("integrationtest")
 class HelpDeskControllerIntegrationTest {
 
     @Autowired
