eed6e2e68af05eff0cfd9e7a9efab76d4e41af9c
pedrolopes9-7 <pedroinf97@gmail.com>
2024-08-03 22:30:54 -0300

    Revert "moving to different module"
    This reverts commit 37e21c447aa1ee17de702831e630d6bbb957750c.

--- spring-ai-2/src/test/java/com/baeldung/ollamachatbot/HelpDeskControllerIntegrationTest.java
+++ spring-ai-2/src/test/java/com/baeldung/ollamachatbot/HelpDeskControllerIntegrationTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,7 +26,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("integrationtest")
 class HelpDeskControllerIntegrationTest {
 
     @Autowired
