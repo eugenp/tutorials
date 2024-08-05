7aa2e5b709359eae14ef7324f847dc111fc571fa
McGiegger <philipnyankena92@gmail.com>
2024-08-05 21:00:14 +0300

    Renamed test file to NullAndEmptyArrayUnitTest and applied required modifications

--- core-java-modules/core-java-arrays-guides/src/test/java/com/baeldung/nullandemptyarray/NullAndEmptyArrayUnitTests.java
+++ core-java-modules/core-java-arrays-guides/src/test/java/com/baeldung/nullandemptyarray/NullAndEmptyArrayUnitTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class NullAndEmptyArrayTests {
+public class NullAndEmptyArrayUnitTests {
 
     @Test
     public void givenNullArray_whenAccessLength_thenThrowsNullPointerException() {
