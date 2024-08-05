0cd60cb1709398c889a3bb711cc02a86e4f46861
timis1 <noreplay@yahoo.com>
2024-08-05 04:48:04 +0300

    JAVA-37379 Changes for java-jpa-3

--- persistence-modules/java-jpa-3/src/main/java/com/baeldung/jpa/equality/EqualByBusinessKey.java
+++ persistence-modules/java-jpa-3/src/main/java/com/baeldung/jpa/equality/EqualByBusinessKey.java
@@ -1,6 +1,6 @@
 package com.baeldung.jpa.equality;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 
 @Entity
 public class EqualByBusinessKey {
--- persistence-modules/java-jpa-3/src/main/java/com/baeldung/jpa/index/Student.java
+++ persistence-modules/java-jpa-3/src/main/java/com/baeldung/jpa/index/Student.java
@@ -1,10 +1,10 @@
 package com.baeldung.jpa.index;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Index;
-import javax.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
