771d01f1c2fc21febab4dd20c5c7c278af337682
alexyang <alexyang0623@gmail.com>
2024-08-06 09:49:23 +0800

    add mapping

--- mapstruct-2/src/main/java/com/baeldung/mapstruct/date/mapper/UserConversionMapper.java
+++ mapstruct-2/src/main/java/com/baeldung/mapstruct/date/mapper/UserConversionMapper.java
@@ -3,9 +3,11 @@ package com.baeldung.mapstruct.date.mapper;
 import com.baeldung.mapstruct.date.model.User;
 import com.baeldung.mapstruct.date.model.UserDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(uses = DateMapper.class)
 public interface UserConversionMapper {
 
+    @Mapping(source = "birthDate", target = "birthDate")
     User toUser(UserDto userDto);
 }
