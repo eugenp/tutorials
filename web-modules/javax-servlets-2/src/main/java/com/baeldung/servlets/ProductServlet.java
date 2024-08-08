78214bb096971f37fd82e146cf75126ba4c3c498
Rajni Kant Pandey <rajnikantpandey50@gmail.com>
2024-08-08 23:24:07 +0530

    Addressed review comments

--- web-modules/javax-servlets-2/src/main/java/com/baeldung/servlets/ProductServlet.java
+++ web-modules/javax-servlets-2/src/main/java/com/baeldung/servlets/ProductServlet.java
@@ -3,7 +3,6 @@ package com.baeldung.servlets;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,20 +21,18 @@ public class ProductServlet extends HttpServlet {
             resp.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid content type");
             return;
         }
-        String payload = "";
-        try (BufferedReader reader = req.getReader()) {
-            StringBuilder sb = new StringBuilder();
+        StringBuilder payload = new StringBuilder();
+        try(BufferedReader reader = req.getReader()){
             String line;
-            while ((line = reader.readLine()) != null) {
-                sb.append(line);
+            while ((line = reader.readLine()) != null){
+                payload.append(line);
             }
-            payload = sb.toString();
         } catch (IOException ex) {
             req.setAttribute("message", "There was an error: " + ex.getMessage());
         }
 
         Gson gson = new Gson();
-        Product newProduct = gson.fromJson(payload, Product.class);
+        Product newProduct = gson.fromJson(payload.toString(), Product.class);
 
         resp.getWriter()
             .append("Added new Product with name: ")
