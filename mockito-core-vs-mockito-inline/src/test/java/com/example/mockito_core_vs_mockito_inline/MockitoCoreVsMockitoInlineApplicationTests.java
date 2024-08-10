77eabcb211521ad86a0855fbae0e162458f0bdf6
oscarramadhan <oscarramadhan.or@gmail.com>
2024-08-10 22:31:26 +0700

    init project for comparision mockito-core vs mockito-inline

---
+++ mockito-core-vs-mockito-inline/src/test/java/com/example/mockito_core_vs_mockito_inline/MockitoCoreVsMockitoInlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockito_core_vs_mockito_inline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoCoreVsMockitoInlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
