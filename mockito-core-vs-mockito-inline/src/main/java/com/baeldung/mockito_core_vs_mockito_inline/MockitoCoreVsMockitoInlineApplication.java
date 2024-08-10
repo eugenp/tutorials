d3d66b42fd389b2f8c7d89b42bc779790088abcf
oscarramadhan <oscarramadhan.or@gmail.com>
2024-08-11 01:41:14 +0700

    init project to compare mockito-core and mockito-inline

--- mockito-core-vs-mockito-inline/src/main/java/com/baeldung/mockito_core_vs_mockito_inline/MockitoCoreVsMockitoInlineApplication.java
+++ mockito-core-vs-mockito-inline/src/main/java/com/baeldung/mockito_core_vs_mockito_inline/MockitoCoreVsMockitoInlineApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MockitoCoreVsMockitoInlineApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MockitoCoreVsMockitoInlineApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        System.out.println("ASD");
+        SpringApplication.run(MockitoCoreVsMockitoInlineApplication.class, args);
+    }
 }
---
+++ mockito-core-vs-mockito-inline/src/test/java/com/baeldung/mockito_core_vs_mockito_inline/MockitoCoreVsMockitoInlineApplicationTests.java
@@ -0,0 +1,31 @@
+package com.baeldung.mockito_core_vs_mockito_inline;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.MockedStatic;
+import org.springframework.boot.test.context.SpringBootTest;
+import utils.FirstUtilClass;
+import utils.SecondUtilClass;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mockStatic;
+
+@SpringBootTest
+class MockitoCoreVsMockitoInlineApplicationTests {
+    @Test
+    void testMockMethod() {
+        FirstUtilClass firstUtilClass = Mockito.mock(FirstUtilClass.class);
+        Mockito.when(firstUtilClass.getMagicNumber()).thenReturn(41);
+        int result = firstUtilClass.getMagicNumber();
+        assertEquals(41, result);
+    }
+
+    @Test
+    void testMockStaticMethod() {
+        try (MockedStatic<SecondUtilClass> mockedStatic = mockStatic(SecondUtilClass.class)) {
+            mockedStatic.when(SecondUtilClass::getSecondMagicNumber).thenReturn(42);
+            int result = SecondUtilClass.getSecondMagicNumber();
+            assertEquals(42, result);
+        }
+    }
+}
