25fd315e5ebd41977ca9fa758165fe23896ddff7
tienvn <tienvn@mail.sample>
2024-08-10 15:19:22 +0700

    JAVA-12038-Move-code-to-common-module

---
+++ spring-boot-simple/actuator-boot-1/src/main/java/com/baeldung/actuator/HealthCheck.java
@@ -0,0 +1,23 @@
+package com.baeldung.actuator;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component("myHealthCheck")
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // perform some specific health check
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    public int check() {
+        // Our logic to check health
+        return 0;
+    }
+}
---
+++ spring-boot-simple/actuator-boot-1/src/main/java/com/baeldung/actuator/SpringBoot.java
@@ -0,0 +1,13 @@
+package com.baeldung.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot.class, args);
+    }
+
+}
