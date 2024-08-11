e6391d8e006528a35af49576c5f89ba9596f7bd7
Palaniappan Arunachalam <palaniappan.ar@gmail.com>
2024-08-11 06:30:44 +0530

    BAEL-5995: Fixed PR Review comments

--- testing-modules/junit-5-basics-2/src/test/java/com/baeldung/junit/norunnablemethods/NameUtilTest.java
+++ testing-modules/junit-5-basics-2/src/test/java/com/baeldung/junit/norunnablemethods/NameUtilTest.java
@@ -1,5 +1,11 @@
 package com.baeldung.junit.norunnablemethods;
 
+/**
+ * This class is actually an utility/helper class, 
+ * but incorrectly named so that it could be confused with a real Test class
+ * 
+ * @see @NameutilTestHelper
+ */
 public class NameUtilTest {
 
     public String formatName(String name) {
