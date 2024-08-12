a653a3c4e7e185a78d345f1de52db00744ae7a28
Philippe Sevestre <psevestre@gmail.com>
2024-08-11 21:50:00 -0300

    [BAEL-8063] Article code

--- spring-security-modules/spring-security-dynamic-registration/server/src/main/java/com/baeldung/spring/security/authserver/config/SecurityConfig.java
+++ spring-security-modules/spring-security-dynamic-registration/server/src/main/java/com/baeldung/spring/security/authserver/config/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.baeldung.spring.security.authserver.config;
 
+import com.baeldung.spring.security.authserver.repository.CustomRegisteredClientRepository;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -15,7 +18,10 @@ import org.springframework.security.oauth2.server.authorization.client.Registere
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
 
 import java.util.UUID;
 
@@ -24,6 +30,7 @@ import java.util.UUID;
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
     public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
         http.getConfigurer(OAuth2AuthorizationServerConfigurer.class)
@@ -31,10 +38,27 @@ public class SecurityConfig {
               oidc.clientRegistrationEndpoint(Customizer.withDefaults());
           });
 
-        http.oauth2ResourceServer(oauth2ResourceServer ->
-          oauth2ResourceServer.jwt(Customizer.withDefaults()));
+        // Redirect to login page when user not authenticated
+        http.exceptionHandling((exceptions) -> exceptions
+          .defaultAuthenticationEntryPointFor(
+            new LoginUrlAuthenticationEntryPoint("/login"),
+            new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+          )
+        );
+
+        // Accept access tokens for User Info and/or Client Registration
+        http.oauth2ResourceServer((resourceServer) -> resourceServer
+            .jwt(Customizer.withDefaults()));
 
         return http.build();
+    }
+
+    @Bean
+    @Order(2)
+    SecurityFilterChain loginFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests( r -> r.anyRequest().authenticated())
+          .formLogin(Customizer.withDefaults())
+          .build();
     }
 
     @Bean
@@ -44,11 +68,16 @@ public class SecurityConfig {
           .clientSecret(props.getRegistrarClientSecret())
           .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
           .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+          .clientSettings(ClientSettings.builder()
+            .requireProofKey(false)
+            .requireAuthorizationConsent(false)
+            .build())
           .scope("client.create")
           .scope("client.read")
           .build();
 
-        return new InMemoryRegisteredClientRepository(registrarClient);
+        RegisteredClientRepository delegate = new  InMemoryRegisteredClientRepository(registrarClient);
+        return new CustomRegisteredClientRepository(delegate);
     }
 
     @ConfigurationProperties(prefix = "baeldung.security.server.registration")
