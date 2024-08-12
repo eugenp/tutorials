fda2d9cecf8c533fa1232d063e35f41692531656
Raf <144442349+raf-97414@users.noreply.github.com>
2024-08-12 12:28:47 +0530

    Update cloning.java

--- rafelia-code-and-articlelink/src/main/java/org/example/cloning.java
+++ rafelia-code-and-articlelink/src/main/java/org/example/cloning.java
@@ -1,14 +1,1 @@
-public class Person implements Cloneable {
-    String name;
-    Food food;
-    
-    public Person(String name, Food food) {
-        this.name = name;
-        this.food = food;
-    }
-    
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
-}
+
