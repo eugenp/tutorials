1849d2640e4ad540e5bbd2f2b5f506584b2e4bae
pedrolopes9-7 <pedroinf97@gmail.com>
2024-08-12 19:46:10 -0300

    switching to assertj

--- core-java-modules/core-java-arrays-multidimensional/src/test/java/com/baeldung/array/reversearray/ReverseArrayElementsTest.java
+++ core-java-modules/core-java-arrays-multidimensional/src/test/java/com/baeldung/array/reversearray/ReverseArrayElementsTest.java
@@ -2,8 +2,7 @@ package com.baeldung.array.reversearray;
 
 import static com.baeldung.array.reversearray.ReverseArrayElements.toReversedList;
 import static java.util.Arrays.asList;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,7 +18,7 @@ class ReverseArrayElementsTest {
         int[][] expected = new int[][] { { 3, 2, 1 }, { 1, 2, 3 }, { 3, 1, 2 } };
         ReverseArrayElements.reverseRowsUsingSimpleForLoops(input);
 
-        assertThat(input, is(expected));
+        assertThat(input).isEqualTo(expected);
     }
 
     @Test
@@ -29,7 +28,7 @@ class ReverseArrayElementsTest {
         int[][] expected = new int[][] { { 3, 2, 1 }, { 1, 2, 3 }, { 3, 1, 2 } };
         ReverseArrayElements.reverseRowsUsingNestedIntStreams(input);
 
-        assertThat(input, is(expected));
+        assertThat(input).isEqualTo(expected);
     }
 
     @Test
@@ -39,7 +38,7 @@ class ReverseArrayElementsTest {
         int[][] expected = new int[][] { { 3, 2, 1 }, { 1, 2, 3 }, { 3, 1, 2 } };
         ReverseArrayElements.reverseRowsUsingCollectionsReverse(input);
 
-        assertThat(input, is(expected));
+        assertThat(input).isEqualTo(expected);
     }
 
     @Test
@@ -49,31 +48,31 @@ class ReverseArrayElementsTest {
 
         ReverseArrayElements.reverseRowsUsingCollectionsReverse(input);
 
-        assertThat(input, is(expected));
+        assertThat(input).isEqualTo(expected);
     }
 
     @Test
     void givenStreamOf2dArrays_whenMappingWithReverse_thenOutputStreamIsAlsoReversed() {
-        List<List<Integer>> array = asList(asList(1, 2, 3), asList(3, 2, 1), asList(2, 1, 3));
+        List<List<Integer>> input = asList(asList(1, 2, 3), asList(3, 2, 1), asList(2, 1, 3));
         List<List<Integer>> expected = asList(asList(3, 2, 1), asList(1, 2, 3), asList(3, 1, 2));
 
-        List<List<Integer>> result = array.stream()
+        List<List<Integer>> result = input.stream()
             .map(ReverseArrayElements::reverse)
             .collect(Collectors.toList());
 
-        assertThat(result, is(expected));
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     void givenStreamOf2dArrays_whenMappingWithCollectReversed_thenOutputStreamIsAlsoReversed() {
-        List<List<Integer>> array = asList(asList(1, 2, 3), asList(3, 2, 1), asList(2, 1, 3));
+        List<List<Integer>> input = asList(asList(1, 2, 3), asList(3, 2, 1), asList(2, 1, 3));
         List<List<Integer>> expected = asList(asList(3, 2, 1), asList(1, 2, 3), asList(3, 1, 2));
 
-        List<List<Integer>> result = array.stream()
+        List<List<Integer>> result = input.stream()
             .map(a -> a.stream()
                 .collect(toReversedList()))
             .collect(Collectors.toList());
 
-        assertThat(result, is(expected));
+        assertThat(result).isEqualTo(expected);
     }
 }
