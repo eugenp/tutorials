fcb234bba63f00f0846415bf9e06e4b444fbe5be
sagaofsilence <sagaofsilence@gmail.com>
2024-08-13 18:46:50 +0530

    Clean imports

--- core-java-modules/core-java-collections-6/src/test/java/com/baeldung/checkedcollections/DataProcessorUnitTest.java
+++ core-java-modules/core-java-collections-6/src/test/java/com/baeldung/checkedcollections/DataProcessorUnitTest.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DataProcessorUnitTest {
