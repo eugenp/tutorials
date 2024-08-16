b2329706a75a1a822d0c67e9bf0665f29298e268
hmdrz <hmdrzsharifi@gmail.com>
2024-08-16 12:31:20 +0330

    #BAEL-8207: move https://github.com/eugenp/tutorials/tree/master/spring-ai-2 source to the https://github.com/eugenp/tutorials/tree/master/spring-ai

---
+++ spring-ai/src/main/java/com/baeldung/airag/controller/ChatBotController.java
@@ -0,0 +1,20 @@
+package com.baeldung.airag.controller;
+
+import com.baeldung.airag.service.ChatBotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ChatBotController {
+    @Autowired
+    private ChatBotService chatBotService;
+
+    @GetMapping("/chat")
+    public Map chat(@RequestParam(name = "query") String query) {
+        return Map.of("answer", chatBotService.chat(query));
+    }
+}
