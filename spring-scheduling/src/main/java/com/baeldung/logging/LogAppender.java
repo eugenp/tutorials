96b6f62cdcecfe14f41199f8f026220676acf94b
technoddy <mail.technoddy@gmail.com>
2024-08-15 23:09:20 -0400

    bael-8382: implementation

---
+++ spring-scheduling/src/main/java/com/baeldung/logging/LogAppender.java
@@ -0,0 +1,24 @@
+package com.baeldung.logging;
+
+import java.util.ArrayList;
+import java.util.List;
+import ch.qos.logback.core.AppenderBase;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogAppender extends AppenderBase<ch.qos.logback.classic.spi.ILoggingEvent> {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogAppender.class);
+    private static final List<String> logMessages = new ArrayList<>();
+
+    @Override
+    protected void append(ch.qos.logback.classic.spi.ILoggingEvent eventObject) {
+        // Custom logic to capture logs
+        logMessages.add(eventObject.getFormattedMessage());
+    }
+
+    public static List<String> getLogMessages() {
+        return logMessages;
+    }
+}
