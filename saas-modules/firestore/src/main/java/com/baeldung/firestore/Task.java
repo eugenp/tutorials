6218663ce45ea745fbb5cdc3797cf0bb432977e0
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-08-20 15:38:37 +0530

    adding data model

---
+++ saas-modules/firestore/src/main/java/com/baeldung/firestore/Task.java
@@ -0,0 +1,39 @@
+package com.baeldung.firestore;
+
+import java.util.Date;
+
+public class Task {
+
+    public static final String PATH = "tasks";
+
+    private String title;
+
+    private String description;
+
+    private Date dueDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+}
