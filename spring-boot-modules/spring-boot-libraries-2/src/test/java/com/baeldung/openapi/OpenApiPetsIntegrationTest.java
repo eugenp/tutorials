fffe0ae96872231645279b8bf959dbb1ca07df8a
Amit Pandey <amitpandey@cloud.upwork.com>
2024-08-24 20:54:26 +0530

    JAVA-37799 :- Upgrade libraries 2.

--- spring-boot-modules/spring-boot-libraries-2/src/test/java/com/baeldung/openapi/OpenApiPetsIntegrationTest.java
+++ spring-boot-modules/spring-boot-libraries-2/src/test/java/com/baeldung/openapi/OpenApiPetsIntegrationTest.java
@@ -9,11 +9,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = OpenApiApplication.class)
+@ComponentScan("com.baeldung")
 @AutoConfigureMockMvc
 public class OpenApiPetsIntegrationTest {
 
@@ -30,7 +32,7 @@ public class OpenApiPetsIntegrationTest {
 
     @Test
     public void whenReadOne_thenStatusIsNotImplemented() throws Exception {
-        this.mockMvc.perform(get(PETS_PATH + 1))
+        this.mockMvc.perform(get(PETS_PATH +  "/"+ 1))
             .andExpect(status().isNotImplemented());
     }
 }
