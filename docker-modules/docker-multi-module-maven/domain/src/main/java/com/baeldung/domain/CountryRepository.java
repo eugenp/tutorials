d6839710c869956a944c10ac47638077e0e160df
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-08-25 21:26:19 +0200

    BAEL-6007: fixed typo

--- docker-modules/docker-multi-module-maven/domain/src/main/java/com/baeldung/domain/CountryRepository.java
+++ docker-modules/docker-multi-module-maven/domain/src/main/java/com/baeldung/domain/CountryRepository.java
@@ -1,6 +1,5 @@
 package com.baeldung.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
