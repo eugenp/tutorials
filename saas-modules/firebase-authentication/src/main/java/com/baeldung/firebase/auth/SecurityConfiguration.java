5fa1f7f8b450a31a52ea97be2471454f2e2389ad
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-08-26 10:40:54 +0530

    adding security configuration

---
+++ saas-modules/firebase-authentication/src/main/java/com/baeldung/firebase/auth/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.baeldung.firebase.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfiguration {
+
+    private static final String[] WHITELISTED_API_ENDPOINTS = { "/user", "/user/login" };
+    
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
+
+    public SecurityConfiguration(TokenAuthenticationFilter tokenAuthenticationFilter) {
+        this.tokenAuthenticationFilter = tokenAuthenticationFilter;
+    }
+    
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.csrf(csrfConfigurer -> csrfConfigurer.disable())
+            .sessionManagement(
+                sessionConfigurer -> sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(authManager -> {
+                authManager.requestMatchers(HttpMethod.POST, WHITELISTED_API_ENDPOINTS)
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+            })
+            .addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+}
---
+++ saas-modules/firebase-authentication/src/main/java/com/baeldung/firebase/auth/TokenAuthenticationFilter.java
@@ -0,0 +1,60 @@
+package com.baeldung.firebase.auth;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String USER_ID_CLAIM = "user_id";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private final FirebaseAuth firebaseAuth;
+
+    public TokenAuthenticationFilter(FirebaseAuth firebaseAuth) {
+        this.firebaseAuth = firebaseAuth;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        String authorizationHeader = request.getHeader(AUTHORIZATION_HEADER);
+
+        if (authorizationHeader != null && authorizationHeader.startsWith(BEARER_PREFIX)) {
+            String token = authorizationHeader.replace(BEARER_PREFIX, "");
+            Optional<String> userId = extractUserIdFromToken(token);
+
+            var authentication = new UsernamePasswordAuthenticationToken(userId, null, null);
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private Optional<String> extractUserIdFromToken(String token) {
+        try {
+            FirebaseToken firebaseToken = firebaseAuth.verifyIdToken(token);
+            String userId = String.valueOf(firebaseToken.getClaims().get(USER_ID_CLAIM));
+            return Optional.of(userId);
+        } catch (FirebaseAuthException exception) {
+            return Optional.empty();
+        }
+    }
+
+}
