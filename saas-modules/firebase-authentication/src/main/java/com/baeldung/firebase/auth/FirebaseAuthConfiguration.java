a391d9b60e499d8f8d898ffbec6782a2c8e4a56c
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-08-26 09:34:45 +0530

    externalizing configuration properties

--- saas-modules/firebase-authentication/src/main/java/com/baeldung/firebase/auth/FirebaseAuthConfiguration.java
+++ saas-modules/firebase-authentication/src/main/java/com/baeldung/firebase/auth/FirebaseAuthConfiguration.java
@@ -4,10 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -15,14 +14,19 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
 
 @Configuration
+@EnableConfigurationProperties(FirebaseConfigurationProperties.class)
 public class FirebaseAuthConfiguration {
 
-    @Value("classpath:/private-key.json")
-    private Resource privateKey;
+    private final FirebaseConfigurationProperties firebaseConfigurationProperties;
+
+    public FirebaseAuthConfiguration(FirebaseConfigurationProperties firebaseConfigurationProperties) {
+        this.firebaseConfigurationProperties = firebaseConfigurationProperties;
+    }
 
     @Bean
     public FirebaseAuth firebaseAuth() throws IOException {
-        InputStream credentials = new ByteArrayInputStream(privateKey.getContentAsByteArray());
+        byte[] privateKey = firebaseConfigurationProperties.getPrivateKey().getContentAsByteArray();
+        InputStream credentials = new ByteArrayInputStream(privateKey);
         FirebaseOptions firebaseOptions = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(credentials))
             .build();
