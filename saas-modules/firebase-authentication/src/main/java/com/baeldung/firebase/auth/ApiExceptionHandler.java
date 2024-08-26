8e85b95db7cda9552d30ae47e99a8dd7a2d87459
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-08-26 11:50:04 +0530

    adding API exception handler

---
+++ saas-modules/firebase-authentication/src/main/java/com/baeldung/firebase/auth/ApiExceptionHandler.java
@@ -0,0 +1,23 @@
+package com.baeldung.firebase.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ProblemDetail handle(ResponseStatusException exception) {
+        return ProblemDetail.forStatusAndDetail(exception.getStatusCode(), exception.getReason());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ProblemDetail handle(Exception exception) {
+        return ProblemDetail.forStatusAndDetail(HttpStatus.NOT_IMPLEMENTED, "Something went wrong.");
+    }
+
+}
