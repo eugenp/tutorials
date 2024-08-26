1d61fa96c4818743994839b6eca1ed837957b260
oscarramadhan <oscarramadhan.or@gmail.com>
2024-08-27 02:11:25 +0700

    update code base for mockito-inline

--- testing-modules/mockito-3/src/test/java/com/baeldung/mockito/mockito_core_vs_mockito_inline/MockitoCoreVsMockitoInlineTest.java
+++ testing-modules/mockito-3/src/test/java/com/baeldung/mockito/mockito_core_vs_mockito_inline/MockitoCoreVsMockitoInlineTest.java
@@ -3,25 +3,45 @@ package com.baeldung.mockito.mockito_core_vs_mockito_inline;
 import com.baeldung.mockito.utils.*;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
 import org.mockito.MockedStatic;
+import org.mockito.MockedConstruction;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.*;
 
 class MockitoCoreVsMockitoInlineTests {
     @Test
-    void testMockMethod() {
-        FirstUtilClass firstUtilClass = Mockito.mock(FirstUtilClass.class);
-        Mockito.when(firstUtilClass.getMagicNumber()).thenReturn(41);
-        int result = firstUtilClass.getMagicNumber();
-        assertEquals(41, result);
+    void testFinalClassMock() {
+        FinalClass finalClass = mock(FinalClass.class);
+        when(finalClass.greet()).thenReturn("Mocked Greeting");
+
+        assertEquals("Mocked Greeting", finalClass.greet());
     }
 
     @Test
-    void testMockStaticMethod() {
-        try (MockedStatic<SecondUtilClass> mockedStatic = mockStatic(SecondUtilClass.class)) {
-            mockedStatic.when(SecondUtilClass::getSecondMagicNumber).thenReturn(42);
-            int result = SecondUtilClass.getSecondMagicNumber();
-            assertEquals(42, result);
+    void testFinalFieldMock() {
+        ClassWithFinalField instance = mock(ClassWithFinalField.class);
+        when(instance.getFinalField()).thenReturn("Mocked Value");
+
+        assertEquals("Mocked Value", instance.getFinalField());
+    }
+
+    @Test
+    void testConstructorMock() {
+        try (MockedConstruction<ClassWithConstructor> mocked = mockConstruction(ClassWithConstructor.class,
+                (mock, context) -> when(mock.getName()).thenReturn("Mocked Name"))) {
+
+            ClassWithConstructor myClass = new ClassWithConstructor("test");
+            assertEquals("Mocked Name", myClass.getName());
+        }
+    }
+
+    @Test
+    void testStaticMethodMock() {
+        try (MockedStatic<ClassWithStaticMethod> mocked = mockStatic(ClassWithStaticMethod.class)) {
+            mocked.when(ClassWithStaticMethod::staticMethod).thenReturn("Mocked Static Value");
+
+            assertEquals("Mocked Static Value", ClassWithStaticMethod.staticMethod());
         }
     }
 }
