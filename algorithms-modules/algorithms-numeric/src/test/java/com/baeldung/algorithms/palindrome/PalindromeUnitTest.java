c4e42eb88894be12b1566f211ba6defe10c3b5d1
Neetika Khandelwal <kwal.neetika2398@gmail.com>
2024-08-31 08:52:56 +0530

    Palindrome Number

--- algorithms-modules/algorithms-numeric/src/test/java/com/baeldung/algorithms/palindrome/PalindromeUnitTest.java
+++ algorithms-modules/algorithms-numeric/src/test/java/com/baeldung/algorithms/palindrome/PalindromeUnitTest.java
@@ -10,29 +10,29 @@ import org.junit.jupiter.api.Test;
 public class PalindromeUnitTest {
 
     @Test
-    void givenNumber_whenUsingIterativeApproach_thenCheckPalidrome() {
+    void givenNumber_whenUsingIterativeApproach_thenCheckPalindrome() {
         assertTrue(PalindromeNumber.isPalindromeIterative(12321));
         assertFalse(PalindromeNumber.isPalindromeIterative(123));
     }
 
     @Test
-    void givenNumber_whenUsingStringApproach_thenCheckPalidrome() {
+    void givenNumber_whenUsingStringApproach_thenCheckPalindrome() {
         assertTrue(PalindromeNumber.isPalindromeString(12321));
         assertFalse(PalindromeNumber.isPalindromeString(123));
     }
     @Test
-    void givenNumber_whenUsingRecursiveApproach_thenCheckPalidrome() {
+    void givenNumber_whenUsingRecursiveApproach_thenCheckPalindrome() {
         assertTrue(PalindromeNumber.isPalindromeRecursive(12321));
         assertFalse(PalindromeNumber.isPalindromeRecursive(123));
     }
 
     @Test
-    void givenNumber_whenUsingHalfReversalApproach_thenCheckPalidrome() {
+    void givenNumber_whenUsingHalfReversalApproach_thenCheckPalindrome() {
         assertTrue(PalindromeNumber.isPalindromeHalfReversal(12321));
         assertFalse(PalindromeNumber.isPalindromeHalfReversal(123));
     }
     @Test
-    void givenNumber_whenUsingDigitByDigitApproach_thenCheckPalidrome() {
+    void givenNumber_whenUsingDigitByDigitApproach_thenCheckPalindrome() {
         assertTrue(PalindromeNumber.isPalindromeDigitByDigit(12321));
         assertFalse(PalindromeNumber.isPalindromeDigitByDigit(123));
     }
