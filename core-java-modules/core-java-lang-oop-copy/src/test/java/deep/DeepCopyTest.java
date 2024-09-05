7ebe085d799a23a2fb4a0b768ceed6033e25e74a
rim <rim@Ordinatblederim.lan>
2024-09-05 19:05:07 +0200

    change variables name

--- core-java-modules/core-java-lang-oop-copy/src/test/java/deep/DeepCopyTest.java
+++ core-java-modules/core-java-lang-oop-copy/src/test/java/deep/DeepCopyTest.java
@@ -9,14 +9,14 @@ public class DeepCopyTest {
     public void deep_copy_should_create_different_object() throws CloneNotSupportedException {
         // Given
         Human aHuman = new Human("human1");
-        Cat aCat1 = new Cat(4, true, aHuman);
+        Cat originalCat = new Cat(4, true, aHuman);
 
         // When
-        Cat aCat2 = (Cat) aCat1.clone();
-        aCat2.getOwner().setName("human2");
+        Cat copyCat = (Cat) originalCat.clone();
+        copyCat.getOwner().setName("human2");
 
         // Then
-        assertThat(aCat2.getOwner()).isNotSameAs(aCat1.getOwner());
+        assertThat(copyCat.getOwner()).isNotSameAs(originalCat.getOwner());
 
     }
 }
