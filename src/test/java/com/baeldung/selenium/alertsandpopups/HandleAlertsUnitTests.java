a19432b559ffabb07ca357badba1e2995b189a9f
Philip Nyankena <88268534+McGiegger@users.noreply.github.com>
2024-09-10 13:36:58 +0300

    Update src/test/java/com/baeldung/selenium/alertsandpopups/HandleAlertsUnitTests.java
    Co-authored-by: Liam Williams <liam.williams@zoho.com>

--- src/test/java/com/baeldung/selenium/alertsandpopups/HandleAlertsUnitTests.java
+++ src/test/java/com/baeldung/selenium/alertsandpopups/HandleAlertsUnitTests.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HandleAlertsUnitTests {
+public class HandleAlertsLiveTest {
 
     @Test
     public void whenSimpleAlertIsTriggered_thenHandleIt() {
