add2c37f505844f481d16a570ac9ba164cc2f695
Thiago Hora <thiago.santos.hora@gmail.com>
2024-09-11 06:41:12 +0200

    BAEL-8251: Quarkus chatbot using Langchain4j

---
+++ quarkus-modules/quarkus-langchain4j/src/main/java/com/baeldung/quarkus/langchain4j/ChatAPI.java
@@ -0,0 +1,37 @@
+package com.baeldung.quarkus.langchain4j;
+
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.UUID;
+
+@Path("/chat")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ChatAPI {
+
+    public record Message(String message, UUID chatId) {}
+
+    private ChatBot chatBot;
+
+    @Inject
+    public ChatAPI(ChatBot chatBot) {
+        this.chatBot = chatBot;
+    }
+
+    @POST
+    public Message mesage(@QueryParam("q") String question, @QueryParam("id") UUID chatId) {
+
+        chatId = chatId == null ? UUID.randomUUID() : chatId;
+
+        var message = chatBot.chat(chatId, question);
+
+        return new Message(message, chatId);
+    }
+
+}
