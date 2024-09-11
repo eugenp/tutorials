9d8a32a9c598dc444ce46eff2f330a1288558213
Bahaa El-Din Helmy <bahaavampire@yahoo.com>
2024-09-11 21:08:04 +0300

    PR: BAEL-7303
    PR to the article named Convert String XML Fragment to Document Node in Java [BAEL-7303].

---
+++ xml-3/src/test/java/com/baeldung/xml/XmlDocumentUnitTest.java
@@ -0,0 +1,48 @@
+package com.baeldung.xml;
+
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class XmlDocumentUnitTest {
+
+    @Test
+    public void givenXmlString_whenConvertToDocument_thenSuccess() throws Exception {
+        String xmlString = "<root><child>Example</child></root>";
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document document = builder.parse(new InputSource(new StringReader(xmlString)));
+
+        assertNotNull(document);
+        assertEquals("root", document.getDocumentElement().getNodeName());
+    }
+
+    @Test
+    public void givenDocument_whenInsertNode_thenNodeAdded() throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+        Document existingDocument = builder.newDocument();
+        Element rootElement = existingDocument.createElement("existingRoot");
+        existingDocument.appendChild(rootElement);
+
+        String xmlString = "<child>Example</child>";
+        Document newDocument = builder.parse(new InputSource(new StringReader(xmlString)));
+
+        Element newNode = (Element) existingDocument.importNode(newDocument.getDocumentElement(), true);
+        existingDocument.getDocumentElement().appendChild(newNode);
+
+        assertNotNull(existingDocument);
+        assertEquals(1, existingDocument.getDocumentElement().getChildNodes().getLength());
+        assertEquals("child", existingDocument.getDocumentElement().getChildNodes().item(0).getNodeName());
+    }
+}
+
