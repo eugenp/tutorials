4f1b9e26b4cbc0ca50c40a7dc7e6ce5714790b24
vunamtien <vunamtien16@gmail.com>
2024-09-12 01:32:15 +0700

    BAEL-117577-Moving article links for xml, xml-2 modules (#17486)

---
+++ xml-2/src/main/java/com/baeldung/xmlhtml/helpers/XMLRunner.java
@@ -0,0 +1,11 @@
+package com.baeldung.xmlhtml.helpers;
+
+
+import com.baeldung.xmlhtml.helpers.jaxb.JAXBHelper;
+
+public class XMLRunner {
+
+    public static void doWork() {
+        JAXBHelper.example();
+    }
+}
