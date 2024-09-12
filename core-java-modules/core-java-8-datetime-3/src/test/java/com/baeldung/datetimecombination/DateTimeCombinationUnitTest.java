57151796f326f4150234a84d622c5091a3d13701
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-09-12 13:43:13 +0300

    This commit is related to BAEL-8116
    This commit aims to add a Java class to the module.

---
+++ core-java-modules/core-java-8-datetime-3/src/test/java/com/baeldung/datetimecombination/DateTimeCombinationUnitTest.java
@@ -0,0 +1,38 @@
+package com.baeldung.datetimecombination;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DateTimeCombinationUnitTest {
+    LocalDate date = LocalDate.of(2023, 9, 12);
+    LocalTime time = LocalTime.of(14, 30);
+    LocalDateTime expectedDateTime = LocalDateTime.parse("2023-09-12T14:30");
+
+    @Test
+    public void givenDateAndTime_whenUsingLocalDateTimeOf_thenCombined() {
+        LocalDateTime combinedDateTime = LocalDateTime.of(date, time);
+
+        assertEquals(expectedDateTime, combinedDateTime);
+    }
+
+    @Test
+    public void givenDateAndTime_whenUsingAtTime_thenCombined() {
+        LocalDateTime combinedDateTime = date.atTime(time);
+
+        assertEquals(expectedDateTime, combinedDateTime);
+    }
+
+    @Test
+    public void givenDateAndTime_whenAdjustingTime_thenCombined() {
+        LocalTime adjustedTime = LocalTime.now().with(ChronoField.MINUTE_OF_HOUR, 0);
+        LocalDateTime expectedAdjustedDateTime = LocalDateTime.of(date, adjustedTime);
+
+        assertEquals(expectedAdjustedDateTime, date.atTime(adjustedTime));
+    }
+}
