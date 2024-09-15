ee94472bf3c308a36ef0e7b6e3624b187d57e968
Rajni Kant Pandey <rajnikantpandey50@gmail.com>
2024-09-15 10:29:41 +0530

    BAEL-8289 - updated imports

--- testing-modules/spring-testing-2/src/main/java/com/baeldung/configuremockbean/UserController.java
+++ testing-modules/spring-testing-2/src/main/java/com/baeldung/configuremockbean/UserController.java
@@ -1,6 +1,5 @@
 package com.baeldung.configuremockbean;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
--- testing-modules/spring-testing-2/src/main/java/com/baeldung/configuremockbean/UserService.java
+++ testing-modules/spring-testing-2/src/main/java/com/baeldung/configuremockbean/UserService.java
@@ -1,5 +1,8 @@
 package com.baeldung.configuremockbean;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface UserService {
     String getUserName(Long id);
 }
