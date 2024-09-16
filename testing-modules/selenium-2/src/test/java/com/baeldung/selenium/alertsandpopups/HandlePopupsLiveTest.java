4e292a44f2d6d460a445f3528d0d294dc8e24003
McGiegger <philipnyankena92@gmail.com>
2024-09-16 18:01:15 +0300

    renamed classnames using provided naming guide

--- testing-modules/selenium-2/src/test/java/com/baeldung/selenium/alertsandpopups/HandlePopupsLiveTest.java
+++ testing-modules/selenium-2/src/test/java/com/baeldung/selenium/alertsandpopups/HandlePopupsLiveTest.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class HandlePopupsLiveTests {
+public class HandlePopupsLiveTest {
 
     private WebDriver driver;
 
