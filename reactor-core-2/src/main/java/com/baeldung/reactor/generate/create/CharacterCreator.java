845034199a30f91a6c117973f69d1f6fd0db8967
vunamtien <vunamtien16@gmail.com>
2024-09-18 02:06:43 +0700

    [JAVA-39228] Move some article links on reactor-core (#17616)

--- reactor-core-2/src/main/java/com/baeldung/reactor/generate/create/CharacterCreator.java
+++ reactor-core-2/src/main/java/com/baeldung/reactor/generate/create/CharacterCreator.java
@@ -1,9 +1,9 @@
 package com.baeldung.reactor.generate.create;
 
+import reactor.core.publisher.Flux;
+
 import java.util.List;
 import java.util.function.Consumer;
-
-import reactor.core.publisher.Flux;
 
 public class CharacterCreator {
     public Consumer<List<Character>> consumer;
