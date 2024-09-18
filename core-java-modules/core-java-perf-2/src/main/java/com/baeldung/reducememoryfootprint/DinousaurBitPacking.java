bd6be15db80e72e16ad099f25e7a3a17bd79e3d9
michaelin007 <michaelolayemi28@gmail.com>
2024-09-18 20:58:50 +0100

    https://jira.baeldung.com/browse/BAEL-6324

---
+++ core-java-modules/core-java-perf-2/src/main/java/com/baeldung/reducememoryfootprint/DinousaurBitPacking.java
@@ -0,0 +1,41 @@
+package com.baeldung.reducememoryfootprint;
+
+public class DinousaurBitPacking {
+
+    static final short IS_EXTINCT = 0, IS_CARNIVOROUS = 1, IS_HERBIVOROUS = 2, IS_OMNIVOROUS = 3;
+    int id;
+    short age;
+    String feedingHabits;
+    String habitat;
+    short flag;
+    String kingdom;
+    String phylum;
+    String clazz;
+    String order;
+    String family;
+    String genus;
+    String species;
+
+    public DinousaurBitPacking(int id, short age, String feedingHabits, String habitat, short flag, String kingdom, String phylum, String clazz, String order, String family, String genus, String species) {
+        this.id = id;
+        this.age = age;
+        this.feedingHabits = feedingHabits;
+        this.habitat = habitat;
+        this.flag = flag;
+        this.kingdom = kingdom;
+        this.phylum = phylum;
+        this.clazz = clazz;
+        this.order = order;
+        this.family = family;
+        this.genus = genus;
+        this.species = species;
+    }
+
+    public static short convertToShort(boolean isExtinct, boolean isCarnivorous, boolean isHerbivorous, boolean isOmnivorous) {
+        return (short) ((isExtinct ? 1 << IS_EXTINCT : 0) | (isCarnivorous ? 1 << IS_CARNIVOROUS : 0) | (isHerbivorous ? 1 << IS_HERBIVOROUS : 0) | (isOmnivorous ? 1 << IS_OMNIVOROUS : 0));
+    }
+
+    public static boolean convertToBoolean(short value, short flagPosition) {
+        return (value >> flagPosition & 1) == 1;
+    }
+}
