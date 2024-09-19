a1f05d1139d89e617e1d95651a2cfe69dd160ec1
panos-kakos <kakos.programmer@gmail.com>
2024-09-19 13:25:49 +0300

    [JAVA-38958] Disabled test

--- xml-2/src/test/java/com/baeldung/xmlhtml/stax/StaxTransformerUnitTest.java
+++ xml-2/src/test/java/com/baeldung/xmlhtml/stax/StaxTransformerUnitTest.java
@@ -1,5 +1,6 @@
 package com.baeldung.xmlhtml.stax;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import javax.xml.stream.XMLStreamException;
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StaxTransformerUnitTest {
 
+    @Disabled // JAVA-39223
     @Test
     public void givenXml_whenTransform_thenGetHtml() throws IOException, URISyntaxException, XMLStreamException {
         String path = "src/test/resources/xmlhtml/notification.xml";
