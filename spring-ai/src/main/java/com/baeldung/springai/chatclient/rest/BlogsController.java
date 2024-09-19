76e2d1dbc25e844a172f6c10f71943b27ea5a1c3
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-09-19 20:01:25 +0200

    BAEL-8407: code review

--- spring-ai/src/main/java/com/baeldung/springai/chatclient/rest/BlogsController.java
+++ spring-ai/src/main/java/com/baeldung/springai/chatclient/rest/BlogsController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
@@ -11,6 +12,7 @@ import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +23,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/articles")
 public class BlogsController {
 
-    public static final String DEFAULT_QUESTION = "Can you suggest articles to learn Spring Boot?";
     private final ChatClient chatClient;
-    private final SimpleVectorStore vectorStore;
+    private final VectorStore vectorStore;
 
     public BlogsController(ChatClient.Builder chatClientBuilder, EmbeddingModel embeddingModel) throws IOException {
         this.chatClient = chatClientBuilder.build();
@@ -45,36 +46,27 @@ public class BlogsController {
         return chatClient.prompt()
             .user(question)
             .call()
-            .chatResponse()
-            .getResult()
-            .getOutput()
-            .getContent();
+            .content();
+    }
+
+    record Article(String title, Set<String> tags) {
     }
 
     @GetMapping("v2")
-    List<RecommendedArticle> askForRecommendation(@RequestParam(required = false, name = "question", defaultValue = DEFAULT_QUESTION) String question) {
+    List<Article> askQuestionAndRetrieveArticles(@RequestParam(name = "question") String question) {
         return chatClient.prompt()
             .user(question)
             .call()
-            .entity(new ParameterizedTypeReference<List<RecommendedArticle>>() {
-
-            });
+            .entity(new ParameterizedTypeReference<List<Article>>() {});
     }
 
     @GetMapping("v3")
-    List<RecommendedArticle> get3(@RequestParam(required = false, name = "question", defaultValue = DEFAULT_QUESTION) String question) {
-
+    List<Article> askQuestionWithContext(@RequestParam(name = "question") String question) {
         return chatClient.prompt()
             .advisors(new QuestionAnswerAdvisor(vectorStore, SearchRequest.defaults()))
             .user(question)
             .call()
-            .entity(new ParameterizedTypeReference<List<RecommendedArticle>>() {
-
-            });
-    }
-
-    record RecommendedArticle(String title, List<String> tags) {
-
+            .entity(new ParameterizedTypeReference<List<Article>>() {});
     }
 
 }
