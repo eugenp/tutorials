mannycepeda1989/Javatest

BAEL-7810: Change lena image with a free portrait from unsplash

image-processing/src/main/java/com/baeldung/imageprocessing/opencv/FaceDetection.java
@@ -1,5 +1,6 @@
 package com.baeldung.imageprocessing.opencv;
 
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfRect;
@@ -12,17 +13,24 @@ import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
 import javafx.scene.image.Image;
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.nio.file.Files;
 
 public class FaceDetection {
 
+    public static void main(String[] args) {
+        // Load the native library.
+        System.load("/opencv/build/lib/libopencv_java4100.dylib");
+        detectFace(FaceDetection.class.getClassLoader().getResource("portrait.jpg").getPath(),
+            "./processed.jpg");
+    }
+
     public static Mat loadImage(String imagePath) {
-        Imgcodecs imageCodecs = new Imgcodecs();
-        return imageCodecs.imread(imagePath);
+        return Imgcodecs.imread(imagePath);
     }
 
     public static void saveImage(Mat imageMatrix, String targetPath) {
-        Imgcodecs imgcodecs = new Imgcodecs();
-        imgcodecs.imwrite(targetPath, imageMatrix);
+        Imgcodecs.imwrite(targetPath, imageMatrix);
     }
 
     public static void detectFace(String sourceImagePath, String targetImagePath) {
@@ -30,18 +38,18 @@ public class FaceDetection {
         MatOfRect facesDetected = new MatOfRect();
         CascadeClassifier cascadeClassifier = new CascadeClassifier();
         int minFaceSize = Math.round(loadedImage.rows() * 0.1f);
-        cascadeClassifier.load("./src/main/resources/haarcascades/haarcascade_frontalface_alt.xml");
+        String filename = FaceDetection.class.getClassLoader().getResource("haarcascades/haarcascade_frontalface_alt.xml").getFile();
+        cascadeClassifier.load(filename);
         cascadeClassifier.detectMultiScale(loadedImage,
                 facesDetected,
                 1.1,
                 3,
                 Objdetect.CASCADE_SCALE_IMAGE,
                 new Size(minFaceSize, minFaceSize),
-                new Size()
-        );
+                new Size());
         Rect[] facesArray =  facesDetected.toArray();
         for(Rect face : facesArray) {
-            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 3 );
+            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 10);
         }
         saveImage(loadedImage, targetImagePath);
     }

eugenp/tutorials

image-processing/src/main/java/com/baeldung/imageprocessing/opencv/FaceDetection.java
@@ -1,5 +1,7 @@
 package com.baeldung.imageprocessing.opencv;
 
+import java.io.ByteArrayInputStream;
+
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfRect;
@@ -10,19 +12,24 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
 import org.opencv.objdetect.Objdetect;
+
 import javafx.scene.image.Image;
-import java.io.ByteArrayInputStream;
 
 public class FaceDetection {
 
+public static void main(String[] args) {
+    // Load the native library.
+    System.load("/opencv/build/lib/libopencv_java4100.dylib");
+    detectFace(FaceDetection.class.getClassLoader().getResource("portrait.jpg").getPath(),
+        "./processed.jpg");
+}
+
     public static Mat loadImage(String imagePath) {
-        Imgcodecs imageCodecs = new Imgcodecs();
-        return imageCodecs.imread(imagePath);
+        return Imgcodecs.imread(imagePath);
     }
 
     public static void saveImage(Mat imageMatrix, String targetPath) {
-        Imgcodecs imgcodecs = new Imgcodecs();
-        imgcodecs.imwrite(targetPath, imageMatrix);
+        Imgcodecs.imwrite(targetPath, imageMatrix);
     }
 
     public static void detectFace(String sourceImagePath, String targetImagePath) {
@@ -30,18 +37,18 @@ public class FaceDetection {
         MatOfRect facesDetected = new MatOfRect();
         CascadeClassifier cascadeClassifier = new CascadeClassifier();
         int minFaceSize = Math.round(loadedImage.rows() * 0.1f);
-        cascadeClassifier.load("./src/main/resources/haarcascades/haarcascade_frontalface_alt.xml");
+        String filename = FaceDetection.class.getClassLoader().getResource("haarcascades/haarcascade_frontalface_alt.xml").getFile();
+        cascadeClassifier.load(filename);
         cascadeClassifier.detectMultiScale(loadedImage,
                 facesDetected,
                 1.1,
                 3,
                 Objdetect.CASCADE_SCALE_IMAGE,
                 new Size(minFaceSize, minFaceSize),
-                new Size()
-        );
+                new Size());
         Rect[] facesArray =  facesDetected.toArray();
         for(Rect face : facesArray) {
-            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 3 );
+            Imgproc.rectangle(loadedImage, face.tl(), face.br(), new Scalar(0, 0, 255), 10);
         }
         saveImage(loadedImage, targetImagePath);
     }