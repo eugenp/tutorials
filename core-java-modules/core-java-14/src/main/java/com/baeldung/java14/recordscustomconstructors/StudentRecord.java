c6e3e98378f682455f7a3a89d09c38119fff9dc6
Kai Yuan <kent.yuan@Gmail.com>
2024-09-20 20:05:57 +0200

    [impr-record] first commit (#17599)
    * [impr-record] improvement
    
    * [impr-record] modifications for comments

--- core-java-modules/core-java-14/src/main/java/com/baeldung/java14/recordscustomconstructors/StudentRecord.java
+++ core-java-modules/core-java-14/src/main/java/com/baeldung/java14/recordscustomconstructors/StudentRecord.java
@@ -1,12 +1,11 @@
-package org.example;
-
-import java.util.Comparator;
-import java.util.List;
+package com.baeldung.java14.recordscustomconstructors;
 
 public record StudentRecord(String name, int rollNo, int marks) {
-    public StudentRecord(String name){
-        this(name, 0,0);
+
+    public StudentRecord(String name) {
+        this(name, 0, 0);
     }
+
     public StudentRecord {
         if (marks < 0 || marks > 100) {
             throw new IllegalArgumentException("Marks should be between 0 and 100.");
@@ -16,4 +15,3 @@ public record StudentRecord(String name, int rollNo, int marks) {
         }
     }
 }
-
