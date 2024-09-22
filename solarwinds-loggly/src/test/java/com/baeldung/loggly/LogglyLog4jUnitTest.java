5d8afdae1fd75bf06c51f8d4f0da86292f542ff9
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2024-09-22 23:27:47 +0530

    Bael-8248, Monitor Java application logs with Loggly

---
+++ solarwinds-loggly/src/test/java/com/baeldung/loggly/LogglyLog4jUnitTest.java
@@ -0,0 +1,20 @@
+package com.baeldung.loggly;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+
+public class LogglyLog4jUnitTest {
+
+//    Logger logger = LoggerFactory.getLogger(LogglyLog4jUnitTest.class);
+    Logger logger = Logger.getLogger(LogglyLog4jUnitTest.class);
+
+    @Test
+    void givenLoggly_thenLogError_thenPushErrorEventToLoggly() {
+        logger.info("Entering Test");
+        logger.error("This is a test error");
+
+    }
+
+
+}
