cfd858d8e026a48d4ac62ba379044be00c72ff83
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-09-23 11:20:55 +0300

    Update JsonCollectionComparisonUnitTest.java

--- json-modules/json-operations/src/test/jsoncollectioncomparison/JsonCollectionComparisonUnitTest.java
+++ json-modules/json-operations/src/test/jsoncollectioncomparison/JsonCollectionComparisonUnitTest.java
@@ -9,8 +9,8 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.co.datumedge.hamcrest.json.SameJSONAs.sameJSONAs;
 
 public class JsonCollectionComparisonUnitTest {
-    String jsonArray1 = "[{\"id\": 1, \"name\": \"Alice\"}, {\"id\": 2, \"name\": \"Bob\"}]";
-    String jsonArray2 = "[{\"id\": 2, \"name\": \"Bob\"}, {\"id\": 1, \"name\": \"Alice\"}]";
+    String jsonArray1 = "[{\"id\": 1, \"name\": \"Alice\", \"address\": {\"city\": \"NY\", \"street\": \"5th Ave\"}}, {\"id\": 2, \"name\": \"Bob\", \"address\": {\"city\": \"LA\", \"street\": \"Sunset Blvd\"}}]";
+    String jsonArray2 = "[{\"id\": 2, \"name\": \"Bob\", \"address\": {\"city\": \"LA\", \"street\": \"Sunset Blvd\"}}, {\"id\": 1, \"name\": \"Alice\", \"address\": {\"city\": \"NY\", \"street\": \"5th Ave\"}}]";
 
     @Test
     public void givenJsonArrays_whenUsingJSONAssertIgnoringOrder_thenEqual() throws JSONException {
@@ -18,11 +18,33 @@ public class JsonCollectionComparisonUnitTest {
     }
 
     @Test
+    public void givenJsonWithExtraFields_whenIgnoringExtraFields_thenEqual() throws JSONException {
+        String jsonWithExtraFields = "["
+                + "{\"id\": 1, \"name\": \"Alice\", \"address\": {\"city\": \"NY\", \"street\": \"5th Ave\"}, \"age\": 30}, "
+                + "{\"id\": 2, \"name\": \"Bob\", \"address\": {\"city\": \"LA\", \"street\": \"Sunset Blvd\"}, \"age\": 25}"
+                + "]";
+
+        JSONAssert.assertEquals(jsonArray1, jsonWithExtraFields, JSONCompareMode.LENIENT);
+    }
+    
+    @Test
     public void givenJsonCollection_whenIgnoringOrder_thenEqual() {
         assertThat(
                 jsonArray1,
                 sameJSONAs(jsonArray2)
-                        .allowingExtraUnexpectedFields()
                         .allowingAnyArrayOrdering());
+    }
+
+    @Test
+    public void givenJsonWithUnexpectedFields_whenIgnoringUnexpectedFields_thenEqual() {
+        String jsonWithUnexpectedFields = "["
+                + "{\"id\": 1, \"name\": \"Alice\", \"address\": {\"city\": \"NY\", \"street\": \"5th Ave\"}, \"extraField\": \"ignoreMe\"}, "
+                + "{\"id\": 2, \"name\": \"Bob\", \"address\": {\"city\": \"LA\", \"street\": \"Sunset Blvd\"}}"
+                + "]";
+
+        assertThat(
+                jsonWithUnexpectedFields,
+                sameJSONAs(jsonArray1)
+                        .allowingExtraUnexpectedFields());
     }
 }
