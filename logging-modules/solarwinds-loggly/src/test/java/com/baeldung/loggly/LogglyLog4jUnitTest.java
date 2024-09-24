e1787c2ce2c6a5e67f4a8a4292f1350a39958567
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2024-09-25 00:07:09 +0530

    Bael-8248, Monitor Java application logs with Loggly

--- logging-modules/solarwinds-loggly/src/test/java/com/baeldung/loggly/LogglyLog4jUnitTest.java
+++ logging-modules/solarwinds-loggly/src/test/java/com/baeldung/loggly/LogglyLog4jUnitTest.java
@@ -5,12 +5,12 @@ import org.junit.jupiter.api.Test;
 
 public class LogglyLog4jUnitTest {
 
-    Logger logger = Logger.getLogger(LogglyLog4jUnitTest.class);
+    private static final Logger logger = Logger.getLogger(LogglyLog4jUnitTest.class);
 
     @Test
-    void givenLoggly_thenLogError_thenPushErrorEventToLoggly() {
-        logger.info("This is a test info message");
-        logger.debug("This is a test debug message");
-        logger.error("This is a test error message");
+    void givenLoggly_whenLogEvent_thenPushEventToLoggly() {
+        logger.info("This is a log4j 1 test info message");
+        logger.debug("This is a log4j 1 test debug message");
+        logger.error("This is a log4j 1 test error message");
     }
 }
