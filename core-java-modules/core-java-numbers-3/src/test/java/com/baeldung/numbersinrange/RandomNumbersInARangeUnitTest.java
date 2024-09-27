7827baf969af45297b79638a64b92cbe8680139e
ACHRAF TAITAI <43656331+achraftt@users.noreply.github.com>
2024-09-27 04:28:36 +0200

    BAEL-8642: Generate random integers within a specific range in Java u… (#17656)
    * BAEL-8642: Generate random integers within a specific range in Java using ThreadLocalRandom
    
    * BAEL-8642: Generate random integers within a specific range in Java u… #17656

--- core-java-modules/core-java-numbers-3/src/test/java/com/baeldung/numbersinrange/RandomNumbersInARangeUnitTest.java
+++ core-java-modules/core-java-numbers-3/src/test/java/com/baeldung/numbersinrange/RandomNumbersInARangeUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.Test;
 public class RandomNumbersInARangeUnitTest {
 
     @Test
-    public void givenTheRange1To10_andUsingMathRandom_thenExpectCorrectResult() {
+    public void givenTheRange1To10_whenUsingMathRandom_thenExpectCorrectResult() {
         RandomNumbersInARange randomNumbersInARange = new RandomNumbersInARange();
         int number = randomNumbersInARange.getRandomNumber(1, 10);
 
@@ -16,7 +16,7 @@ public class RandomNumbersInARangeUnitTest {
     }
 
     @Test
-    public void givenTheRange1To10_andUsingRandomInts_thenExpectCorrectResult() {
+    public void givenTheRange1To10_whenUsingRandomInts_thenExpectCorrectResult() {
         RandomNumbersInARange randomNumbersInARange = new RandomNumbersInARange();
         int number = randomNumbersInARange.getRandomNumberUsingInts(1, 10);
 
@@ -25,9 +25,18 @@ public class RandomNumbersInARangeUnitTest {
     }
 
     @Test
-    public void givenTheRange1To10_andUsingRandomNextInt_thenExpectCorrectResult() {
+    public void givenTheRange1To10_whenUsingRandomNextInt_thenExpectCorrectResult() {
         RandomNumbersInARange randomNumbersInARange = new RandomNumbersInARange();
         int number = randomNumbersInARange.getRandomNumberUsingNextInt(1, 10);
+
+        assertTrue(number >= 1);
+        assertTrue(number < 10);
+    }
+
+    @Test
+    public void givenTheRange1To10_whenUsingThreadLocalRandom_thenExpectCorrectResult() {
+        RandomNumbersInARange randomNumbersInARange = new RandomNumbersInARange();
+        int number = randomNumbersInARange.getRandomNumberUsingThreadLocalRandom(1, 10);
 
         assertTrue(number >= 1);
         assertTrue(number < 10);
