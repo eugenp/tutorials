899eee3a85967c647363736af22c7be8ffa50786
Neetika Khandelwal <kwal.neetika2398@gmail.com>
2024-09-29 18:57:37 +0530

    constructor mock

---
+++ testing-modules/spring-testing-2/src/test/java/com/baeldung/valueannotationmock/ValueAnnotationMockConstructorUnitTest.java
@@ -0,0 +1,28 @@
+package com.baeldung.valueannotationmock;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ValueAnnotationMockConstructorUnitTest {
+
+    private ValueAnnotationConstructorMock valueAnnotationConstructorMock;
+
+    @BeforeEach
+    public void setUp() {
+        // Instantiate the class with test-specific values
+        valueAnnotationConstructorMock = new ValueAnnotationConstructorMock("testUrl", "testPassword");
+    }
+
+    @Test
+    public void testDefaultUrl() {
+        assertEquals("testUrl", valueAnnotationConstructorMock.getApiUrl());
+    }
+
+    @Test
+    public void testDefaultPassword() {
+        assertEquals("testPassword", valueAnnotationConstructorMock.getApiPassword());
+    }
+
+}
