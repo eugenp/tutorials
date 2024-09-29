df302ed4a9c28d20f78e58d7d9e7e67e2014fe99
rim <rim@Ordinateur-portable-de-rim.local>
2024-09-29 14:58:31 +0200

    modify tests names

--- testing-modules/mockito-3/src/test/java/com/baeldung/mockito/deep-stubs/NewsAgencyTest.java
+++ testing-modules/mockito-3/src/test/java/com/baeldung/mockito/deep-stubs/NewsAgencyTest.java
@@ -15,25 +15,27 @@ import static org.mockito.Mockito.when;
 
 public class NewsAgencyTest {
     @Test
-    void getAllArticlesTest(){
-
+    void whenInitializingAllObjects_ThenGetAllArticlesTest(){
+        //Given
         String title1 = "new study reveals the dimension where the single socks disappear";
         NewsArticle article1 = new NewsArticle(title1,"link1");
         Reporter reporter1 = new Reporter("Tom", article1);
-
         String title2 = "secret meeting of cats union against vacuum cleaners";
         NewsArticle article2 = new NewsArticle(title2,"link2");
         Reporter reporter2 = new Reporter("Maria", article2);
-
         List<String> expectedResults = List.of(title1, title2);
-
         NewsAgency newsAgency = new NewsAgency(List.of(reporter1, reporter2));
+
+        //When
         List<String> actualResults = newsAgency.getLatestArticlesNames();
+
+        //Then
         assertEquals(expectedResults, actualResults);
     }
 
     @Test
-    void getAllArticlesTestWithMocksAndDeepStubs(){
+    void GivenInitializeUsingMocksAndDeepStubs_WhenCallingGetAllArticles_ThenGetAllArticlesTest(){
+        //Given
         Reporter mockReporter1 = mock(Reporter.class, Mockito.RETURNS_DEEP_STUBS);
         String title1 = "cow flying in London, royal guard still did not move";
         when(mockReporter1.getLatestArticle().getName()).thenReturn(title1);
@@ -41,9 +43,13 @@ public class NewsAgencyTest {
         String title2 = "drunk man accidentally runs for mayor and wins";
         when(mockReporter2.getLatestArticle().getName()).thenReturn(title2);
         NewsAgency newsAgency = new NewsAgency(List.of(mockReporter1, mockReporter2));
-
         List<String> expectedResults = List.of(title1, title2);
-        assertEquals(newsAgency.getLatestArticlesNames(), expectedResults);
+
+        //When
+        List<String> actualResults = newsAgency.getLatestArticlesNames();
+
+        //Then
+        assertEquals(actualResults, expectedResults);
     }
 
 }
