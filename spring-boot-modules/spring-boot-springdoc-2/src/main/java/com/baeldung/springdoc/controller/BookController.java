82f3e94b788567e7bf2c8b7f26c71d8e381a1966
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2024-09-30 10:11:17 +0000

    fix: fixed build issues
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

--- spring-boot-modules/spring-boot-springdoc-2/src/main/java/com/baeldung/springdoc/controller/BookController.java
+++ spring-boot-modules/spring-boot-springdoc-2/src/main/java/com/baeldung/springdoc/controller/BookController.java
@@ -6,6 +6,7 @@ import com.baeldung.springdoc.repository.BookRepository;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -87,7 +88,7 @@ public class BookController {
             content = { @Content(mediaType = "application/json",
                 schema = @Schema(implementation = Book.class)) }),
         @ApiResponse(responseCode = "400", description = "Invalid input provided") })
-    @PostMapping
+    @PostMapping("/body-description")
     public Book createBook(@io.swagger.v3.oas.annotations.parameters.RequestBody(
         description = "Book to create", required = true,
         content = @Content(mediaType = "application/json",
