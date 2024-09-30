5a6c01ff383dad5cce86ec61d7acd15737464826
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-10-01 00:41:21 +0530

    reducing memory footprint

--- libraries-5/src/main/java/com/baeldung/yauaa/UserAgentAnalyzerConfiguration.java
+++ libraries-5/src/main/java/com/baeldung/yauaa/UserAgentAnalyzerConfiguration.java
@@ -3,6 +3,7 @@ package com.baeldung.yauaa;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import nl.basjes.parse.useragent.UserAgent;
 import nl.basjes.parse.useragent.UserAgentAnalyzer;
 
 @Configuration
@@ -13,6 +14,7 @@ public class UserAgentAnalyzerConfiguration {
         return UserAgentAnalyzer
             .newBuilder()
             .withCache(1000)
+            .withField(UserAgent.OPERATING_SYSTEM_CLASS)
             .build();
     }
 
