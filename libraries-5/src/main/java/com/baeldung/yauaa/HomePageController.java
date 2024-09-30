f49dbae36257e296a337297e5bc124ea7b6b05fb
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-10-01 00:41:47 +0530

    adding controller endpoint

---
+++ libraries-5/src/main/java/com/baeldung/yauaa/HomePageController.java
@@ -0,0 +1,32 @@
+package com.baeldung.yauaa;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.servlet.ModelAndView;
+
+import nl.basjes.parse.useragent.UserAgent;
+import nl.basjes.parse.useragent.UserAgentAnalyzer;
+
+@Controller
+public class HomePageController {
+
+    private final UserAgentAnalyzer userAgentAnalyzer;
+
+    public HomePageController(UserAgentAnalyzer userAgentAnalyzer) {
+        this.userAgentAnalyzer = userAgentAnalyzer;
+    }
+
+    @GetMapping(value = "/home")
+    public ModelAndView homePage(@RequestHeader(value = HttpHeaders.USER_AGENT) String userAgentString) {
+        UserAgent userAgent = userAgentAnalyzer.parse(userAgentString);
+        boolean isMobileDevice = userAgent.getValue(UserAgent.OPERATING_SYSTEM_CLASS).equals("Mobile");
+
+        if (isMobileDevice) {
+            return new ModelAndView("/mobile-home");
+        }
+        return new ModelAndView("error/open-in-mobile");
+    }
+
+}
