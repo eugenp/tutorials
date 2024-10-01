37c3a59b98ede24849f3f3ffc10ea3b980fe3666
h_sharifi <ujmUJM@1234567>
2024-10-01 18:17:46 +0330

    #BAEL-8207: code formatting

--- spring-ai/src/main/java/com/baeldung/springai/dto/HealthStatus.java
+++ spring-ai/src/main/java/com/baeldung/springai/dto/HealthStatus.java
@@ -2,4 +2,5 @@ package com.baeldung.springai.dto;
 
 import java.time.LocalDate;
 
-public record HealthStatus(String status, LocalDate changeDate) {}
+public record HealthStatus(String status, LocalDate changeDate) {
+}
