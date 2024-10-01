34c7e253311354ef7b537f70e2c3ebffb1819439
Mo Helmy <135069400+BenHelmyBen@users.noreply.github.com>
2024-10-01 22:01:51 +0300

    Update DetermineDelimiterInCSVFileUnitTest.java

--- core-java-modules/core-java-io-conversions-3/src/test/java/com/baeldung/determinedelimitersincsv/DetermineDelimiterInCSVFileUnitTest.java
+++ core-java-modules/core-java-io-conversions-3/src/test/java/com/baeldung/determinedelimitersincsv/DetermineDelimiterInCSVFileUnitTest.java
@@ -1,12 +1,8 @@
 package com.baeldung.determinedelimitersincsv;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.StringReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Pattern;
@@ -14,15 +10,23 @@ import java.util.regex.Pattern;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DetermineDelimiterInCSVFileUnitTest {
-    private static final char[] POSSIBLE_DELIMITERS = {',', ';', '\t', '|'};
-    String csvLine = "name,age,location\nJohn,30,New York";
+    String[] lines = {
+            "Location,Latitude,Longitude,Elevation(m)",
+            "New York,40.7128,-74.0060,10",
+            "Los Angeles,34.0522,-118.2437,71",
+            "Chicago,41.8781,-87.6298,181"
+    };
 
     @Test
-    public void givenCSVLine_whenDetectingDelimiter_thenCorrectDelimiterFound() {
+    public void givenCSVLines_whenDetectingDelimiterUsingFrequencyCount_thenCorrectDelimiterFound() {
+        char[] POSSIBLE_DELIMITERS = {',', ';', '\t', '|'};
         Map<Character, Integer> delimiterCounts = new HashMap<>();
 
         for (char delimiter : POSSIBLE_DELIMITERS) {
-            int count = csvLine.length() - csvLine.replace(String.valueOf(delimiter), "").length();
+            int count = 0;
+            for (String line : lines) {
+                count += line.length() - line.replace(String.valueOf(delimiter), "").length();
+            }
             delimiterCounts.put(delimiter, count);
         }
 
@@ -36,62 +40,15 @@ public class DetermineDelimiterInCSVFileUnitTest {
 
     @Test
     public void givenCSVLines_whenDetectingDelimiter_thenCorrectDelimiterFound() {
-        String[] lines = {
-                "Location,Latitude,Longitude,Elevation(m)",
-                "New York,40.7128,-74.0060,10",
-                "Los Angeles,34.0522,-118.2437,71",
-                "Chicago,41.8781,-87.6298,181"
-        };
-
         char detectedDelimiter = ',';
-        for (char delimiter : POSSIBLE_DELIMITERS) {
-            boolean isConsistent = true;
-            int columnCount = -1;
+        for (char delimiter : lines[0].toCharArray()) {
+            boolean allRowsHaveEqualColumnCounts = Arrays.stream(lines)
+                    .map(line -> line.split(Pattern.quote(String.valueOf(delimiter))))
+                    .map(columns -> columns.length)
+                    .distinct()
+                    .count() == 1;
 
-            for (String line : lines) {
-                String[] fields = line.split(Pattern.quote(String.valueOf(delimiter)));
-                if (columnCount == -1) {
-                    columnCount = fields.length;
-                } else if (fields.length != columnCount) {
-                    isConsistent = false;
-                    break;
-                }
-            }
-
-            if (isConsistent) {
-                detectedDelimiter = delimiter;
-                break;
-            }
-        }
-
-        assertEquals(',', detectedDelimiter);
-    }
-
-    @Test
-    public void givenCSVLine_whenUsingApacheCommons_thenCorrectDelimiterFound() throws IOException {
-        char detectedDelimiter = ',';
-
-        for (char delimiter : POSSIBLE_DELIMITERS) {
-            CSVFormat csvFormat = CSVFormat.DEFAULT.builder().setDelimiter(delimiter).build();
-            try (CSVParser parser = new CSVParser(new StringReader(csvLine), csvFormat)) {
-                CSVRecord firstRecord = parser.iterator().next();
-                if (firstRecord.size() == 3) {
-                    detectedDelimiter = delimiter;
-                    break;
-                }
-            }
-        }
-
-        assertEquals(',', detectedDelimiter);
-    }
-
-    @Test
-    public void givenCSVLine_whenUsingRegex_thenCorrectDelimiterFound() {
-        char detectedDelimiter = ',';
-
-        for (char delimiter : POSSIBLE_DELIMITERS) {
-            String[] fields = csvLine.split(Pattern.quote(String.valueOf(delimiter)));
-            if (fields.length > 1) {
+            if (allRowsHaveEqualColumnCounts) {
                 detectedDelimiter = delimiter;
                 break;
             }
