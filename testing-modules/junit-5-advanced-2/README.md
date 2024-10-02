ede0b6cf01d2ab975313e8e4e2528d1ba3989a4d
panos-kakos <kakos.programmer@gmail.com>
2024-10-02 12:23:09 +0300

    [JAVA-40675] Moved article "Solving the ParameterResolutionException in JUnit 5" from junit-5-advanced-2 to junit-5-advanced

--- testing-modules/junit-5-advanced-2/README.md
+++ testing-modules/junit-5-advanced-2/README.md
@@ -1,6 +1,5 @@
 ## Relevant Articles:
 
-- [Solving the ParameterResolutionException in JUnit 5](https://www.baeldung.com/junit-5-parameterresolutionexception)
 - [Run JUnit Test Cases From the Command Line](https://www.baeldung.com/junit-run-from-command-line)
 - [Get the Name of the Currently Executing Test in JUnit](https://www.baeldung.com/junit-get-name-of-currently-executing-test)
 - [JUnit 5 TestWatcher API](https://www.baeldung.com/junit-testwatcher)
---
+++ testing-modules/junit-5-advanced/src/test/java/com/baeldung/parameterresolver/ParameterResolverExceptionUnitTest.java
@@ -0,0 +1,31 @@
+package com.baeldung.parameterresolver;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(BookParameterResolver.class)
+public class ParameterResolverExceptionUnitTest {
+
+    @Test
+    void givenBook_whenConfiguringParameterResolver_thenParameterResolves(Book book) {
+        assertThat(book.getTitle()).isEqualTo("Wuthering Heights");
+    }
+
+    // Uncomment these annotations to reproduce the ParameterResolutionException
+    // @ParameterizedTest
+    // @CsvSource({"Wuthering Heights, Charlotte Bronte", "Jane Eyre, Charlotte Bronte"})
+    // @Test
+    void givenParameterizedTest_whenUsingTestAnnotation_thenParameterFailsToResolve(String title, String author) {
+        Book book = new Book(title, author);
+        assertThat(book.getTitle().length()).isGreaterThan(0);
+        assertThat(book.getAuthor().length()).isGreaterThan(0);
+    }
+
+    // Uncomment these annotations to reproduce the ParameterResolutionException
+    // @Test
+    void givenBasicTest_whenPassingParameters_thenParameterFailsToResolve(String title, String author) {
+        Book book = new Book(title, author);
+        assertThat(book.getTitle().length()).isGreaterThan(0);
+        assertThat(book.getAuthor().length()).isGreaterThan(0);
+    }
+}
