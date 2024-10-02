94e743e927b097946e269fc966dda737269cdc23
Sudarshan Hiray <sdhiray7@gmail.com>
2024-10-02 15:41:43 +0530

    Review comments

--- apache-poi-3/src/test/java/com/baeldung/fastexcel/FastexcelDetectEmptyRowUnitTest.java
+++ apache-poi-3/src/test/java/com/baeldung/fastexcel/FastexcelDetectEmptyRowUnitTest.java
@@ -19,24 +19,11 @@ import org.junit.Test;
 public class FastexcelDetectEmptyRowUnitTest {
 
     private FastexcelHelper fastexcelHelper;
-    private static final String FILE_PATH = "src/main/resources/consumer_info_with_empty_row.xlsx";
     private static final String EMPTY_FILE_PATH = "src/main/resources/empty_excel_file.xlsx";
 
     @Before
     public void loadExcelFile() throws IOException {
         fastexcelHelper = new FastexcelHelper();
-    }
-
-    @Test
-    public void givenXLSXFile_whenParsingFastExcelFile_thenDetectEmptyRow() throws IOException {
-        try (FileInputStream file = new FileInputStream(FILE_PATH); ReadableWorkbook wb = new ReadableWorkbook(file)) {
-            Sheet sheet = wb.getFirstSheet();
-            try (Stream<Row> rows = sheet.openStream()) {
-                Optional<Row> last = rows.reduce((first, second) -> second);
-                boolean isLastRowEmpty = last.map(fastexcelHelper::isRowEmpty).orElseThrow();
-                assertTrue(isLastRowEmpty);
-            }
-        }
     }
     
     @Test
--- apache-poi-3/src/test/java/com/baeldung/jexcel/JExcelDetectEmptyRowUnitTest.java
+++ apache-poi-3/src/test/java/com/baeldung/jexcel/JExcelDetectEmptyRowUnitTest.java
@@ -17,27 +17,11 @@ import jxl.read.biff.BiffException;
 public class JExcelDetectEmptyRowUnitTest {
 
     private JExcelHelper jexcelHelper;
-    private static final String FILE_PATH = "src/main/resources/consumer_info_with_empty_row.xls";
     private static final String EMPTY_FILE_PATH = "src/main/resources/empty_excel_file.xls";
 
     @Before
     public void loadExcelFile() throws IOException {
         jexcelHelper = new JExcelHelper();
-    }
-
-    @Test
-    public void givenXLSFile_whenParsingJExcelFile_thenDetectEmptyRow() throws IOException, BiffException {
-        Workbook workbook = Workbook.getWorkbook(new File(FILE_PATH));
-        Sheet sheet = workbook.getSheet(0);
-
-        Cell[] row = sheet.getRow(0);
-        assertFalse(jexcelHelper.isRowEmpty(row));
-
-        row = sheet.getRow(2);
-        assertFalse(jexcelHelper.isRowEmpty(row));
-
-        row = sheet.getRow(4);
-        assertTrue(jexcelHelper.isRowEmpty(row)); // check the file consumer_info_with_empty_row.xls in resources folder, we have inserted empty row at the end
     }
 
     @Test
