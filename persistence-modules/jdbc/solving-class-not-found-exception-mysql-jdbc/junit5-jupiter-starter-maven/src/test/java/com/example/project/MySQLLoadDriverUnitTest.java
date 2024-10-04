ea1edd39a736d340dd482f6fc714cb30b1218781
Deepak-Vohra <dvohra10@yahoo.com>
2024-10-04 09:44:38 -0700

    Update MySQLLoadDriverUnitTest.java

--- persistence-modules/jdbc/solving-class-not-found-exception-mysql-jdbc/junit5-jupiter-starter-maven/src/test/java/com/example/project/MySQLLoadDriverUnitTest.java
+++ persistence-modules/jdbc/solving-class-not-found-exception-mysql-jdbc/junit5-jupiter-starter-maven/src/test/java/com/example/project/MySQLLoadDriverUnitTest.java
@@ -1,13 +1,8 @@
-package com.example.project;
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 public class MySQLLoadDriverUnitTest {
 
     @Test
     void givenADriverClass_whenDriverLoaded_thenEnsureNoExceptionThrown() {
-        assertDoesNotThrow(() -> {
+        assertDoesNotThrow(() -&gt; {
             Class.forName("com.mysql.cj.jdbc.Driver");
         });
     }
