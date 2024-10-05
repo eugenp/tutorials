868c312cdd28c6ff794078b8d2785d5f58a13e63
robert.głowacki <r.glowacki@wp.pl>
2024-10-05 21:33:53 +0200

    BAEL-8259: Adjust to new version article.

--- core-java-modules/core-java-string-operations-7/src/main/java/com/baeldung/stringcontainscurrencysymbol/CurrencySymbolFinder.java
+++ core-java-modules/core-java-string-operations-7/src/main/java/com/baeldung/stringcontainscurrencysymbol/CurrencySymbolFinder.java
@@ -1,36 +1,34 @@
 package com.baeldung.stringcontainscurrencysymbol;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Currency;
 import java.util.Locale;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class CurrencySymbolFinder {
 
-    public static String singleSymbolMatcher(String symbol, String content) {
+    public static String currencySymbolMatcher(String symbol, String content) {
         Pattern pattern = Pattern.compile(symbol, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(content);
         return matcher.find() ? matcher.group() : "";
     }
 
     public static String currencyNameMatcher(Locale locale, String content) {
-        String symbol = Currency.getInstance(locale).getSymbol();
-        return singleSymbolMatcher(symbol, content);
+        String symbol = Currency.getInstance(locale)
+            .getSymbol();
+        return currencySymbolMatcher(symbol, content);
     }
 
-    public static Set<String> getAvailableCurrencies() {
-        return Currency.getAvailableCurrencies()
-                .stream()
-                .map(Currency::getSymbol)
-                .collect(Collectors.toSet());
+    public static Number parseCurrencyAmount(Locale locale, String input) {
+        Number value;
+        try {
+            value = NumberFormat.getCurrencyInstance(locale)
+                .parse(input);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return value;
     }
-
-    public static boolean multipleSymbolMatcher(String symbol, String content) {
-        Pattern pattern = Pattern.compile(symbol, Pattern.CASE_INSENSITIVE);
-        Matcher matcher = pattern.matcher(content);
-        return matcher.find();
-    }
-
 }
--- core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/stringcontainscurrencysymbol/CurrencySymbolFinderTest.java
+++ core-java-modules/core-java-string-operations-7/src/test/java/com/baeldung/stringcontainscurrencysymbol/CurrencySymbolFinderTest.java
@@ -1,37 +1,45 @@
 package com.baeldung.stringcontainscurrencysymbol;
 
-import org.junit.Test;
+import static com.baeldung.stringcontainscurrencysymbol.CurrencySymbolFinder.currencyNameMatcher;
+import static com.baeldung.stringcontainscurrencysymbol.CurrencySymbolFinder.currencySymbolMatcher;
+import static com.baeldung.stringcontainscurrencysymbol.CurrencySymbolFinder.parseCurrencyAmount;
+import static org.junit.Assert.assertEquals;
 
 import java.util.Locale;
 
-import static com.baeldung.stringcontainscurrencysymbol.CurrencySymbolFinder.*;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CurrencySymbolFinderTest {
 
-    private static final String SINGLE_SIGN_REGEX = "\\p{Sc}";
-    private static final String MULTIPLE_SIGNS_REGEX = "^\\p{Sc}{3}[1-9]";
+    private static final String REGEX = "^\\p{Sc}?\\d{1,3}(,\\d{3})*(\\.\\d{2})?$";
 
-    @Test
-    public void givenCurrencySymbol_whenCheckingString_returnSymbol() {
-        String usdValue = "$4,098.09";
-        String symbol = singleSymbolMatcher(SINGLE_SIGN_REGEX, usdValue);
-        assertEquals("$", symbol);
+    @ParameterizedTest
+    @ValueSource(strings = { "$4,100.00", "€85.50", "£75", "₹500.25" })
+    public void givenCurrencySymbol_whenCheckingString_returnSymbol(String input) {
+        String value = currencySymbolMatcher(REGEX, input);
+        assertEquals(input, value);
     }
 
     @Test
-    public void givenCurrencyName_whenCheckingString_returnSymbol() {
+    public void givenLocaleInstance_whenCheckingString_returnSymbol() {
         String chfValue = "750CHF";
         String currencyName = currencyNameMatcher(new Locale("", "CH"), chfValue);
         assertEquals("CHF", currencyName);
     }
 
     @Test
-    public void givenMultipleCurrencySymbols_whenCheckingString_returnTrue() {
-        String poundValue = "£££4,098.09";
-        boolean symbol = multipleSymbolMatcher(MULTIPLE_SIGNS_REGEX, poundValue);
-        assertTrue(symbol);
+    public void givenPoundsWithMatchingCountryCode_whenCheckingString_validationPass() {
+        String poundValue = "£599000";
+        float value = parseCurrencyAmount(Locale.UK, poundValue).floatValue();
+        assertEquals(599000.0, value, 0);
     }
 
+    @Test
+    public void givenRupeeWithMatchingCountryCode_whenCheckingString_validationPass() {
+        String rupeeValue = "₹1,00,00,000";
+        float value = parseCurrencyAmount(new Locale("hi", "IN"), rupeeValue).floatValue();
+        assertEquals(10000000, value, 0);
+    }
 }
