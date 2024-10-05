a7949645412e47d24913f598f415b2b5bb9ab497
McGiegger <philipnyankena92@gmail.com>
2024-10-05 11:29:28 +0300

    Renamed class DragAndDrop to DragAndDropLiveTest

--- testing-modules/selenium-2/src/test/java/com/baeldung/selenium/drag_and_drop/DragAndDropLiveTest.java
+++ testing-modules/selenium-2/src/test/java/com/baeldung/selenium/drag_and_drop/DragAndDropLiveTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DragAndDrop {
+public class DragAndDropLiveTest {
 
     private WebDriver driver;
 
