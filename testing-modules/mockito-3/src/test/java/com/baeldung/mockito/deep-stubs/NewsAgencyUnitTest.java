d836f62d34379ccb37b655aec167d8e81433621b
rim <rim@Ordinatblederim.lan>
2024-10-07 22:56:23 +0200

    renaming unit test file

--- testing-modules/mockito-3/src/test/java/com/baeldung/mockito/deep-stubs/NewsAgencyUnitTest.java
+++ testing-modules/mockito-3/src/test/java/com/baeldung/mockito/deep-stubs/NewsAgencyUnitTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class NewsAgencyTest {
+public class NewsAgencyUnitTest {
     @Test
     void whenInitializingAllObjects_thenGetAllArticlesTest(){
         //Given
