da2fbd9c706654cc66fd1f08ab1adf333597ea37
Bipin kumar <sayy2bipin@gmail.com>
2024-10-12 03:01:26 +0530

    JAVA-38226: Changes made for handle default build WARNING(has been deprecated and marked for removal) (#17804)

--- core-java-modules/core-java-functional/src/test/java/com/baeldung/functional/FirstClassFunctionsUnitTest.java
+++ core-java-modules/core-java-functional/src/test/java/com/baeldung/functional/FirstClassFunctionsUnitTest.java
@@ -11,16 +11,16 @@ public class FirstClassFunctionsUnitTest {
     @Test
     public void testSortingWithoutLambda() {
 
-        assertEquals(new Integer(8), FirstClassFunctions.sortWithoutLambda(Arrays.asList(new Integer(10), new Integer(8)))
-            .get(0));
+        assertEquals(Integer.valueOf(8),
+            FirstClassFunctions.sortWithoutLambda(Arrays.asList(10, 8)).get(0));
 
     }
 
     @Test
     public void testSortingWithLambda() {
 
-        assertEquals(new Integer(8), FirstClassFunctions.sortWithLambda(Arrays.asList(new Integer(10), new Integer(8)))
-            .get(0));
+        assertEquals(Integer.valueOf(8),
+            FirstClassFunctions.sortWithLambda(Arrays.asList(10, 8)).get(0));
 
     }
 
