921c9498389706c9e18b4c307f209a697e6a5c3e
MohamedHelmyKassab <137485958+MohamedHelmyKassab@users.noreply.github.com>
2024-10-11 16:58:56 +0300

    Update HeaderFooterEventHandler.java

--- core-java-modules/core-java-io-7/main/java/com/baeldung/addheaderfooterpdf/HeaderFooterEventHandler.java
+++ core-java-modules/core-java-io-7/main/java/com/baeldung/addheaderfooterpdf/HeaderFooterEventHandler.java
@@ -12,28 +12,29 @@ import com.itextpdf.kernel.font.PdfFontFactory;
 public class HeaderFooterEventHandler implements IEventHandler {
     @Override
     public void handleEvent(Event event) {
-        PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
-        PdfDocument pdfDoc = docEvent.getDocument();
-        PdfPage page = docEvent.getPage();
-        int pageNumber = pdfDoc.getPageNumber(page);
+        if (event instanceof PdfDocumentEvent docEvent) {
+            PdfDocument pdfDoc = docEvent.getDocument();
+            PdfPage page = docEvent.getPage();
+            int pageNumber = pdfDoc.getPageNumber(page);
 
-        PdfCanvas canvas = new PdfCanvas(page.newContentStreamBefore(), page.getResources(), pdfDoc);
+            PdfCanvas canvas = new PdfCanvas(page.newContentStreamBefore(), page.getResources(), pdfDoc);
 
-        canvas.beginText();
-        try {
-            canvas.setFontAndSize(PdfFontFactory.createFont(StandardFonts.HELVETICA), 12);
-        } catch (Exception e) {
-            e.printStackTrace();
+            canvas.beginText();
+            try {
+                canvas.setFontAndSize(PdfFontFactory.createFont(StandardFonts.HELVETICA), 12);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            canvas.moveText(36, page.getPageSize().getTop() - 20);
+            canvas.showText("Header text - Page " + pageNumber);
+            canvas.endText();
+
+            canvas.beginText();
+            canvas.moveText(36, 20);
+            canvas.showText("Footer text - Page " + pageNumber);
+            canvas.endText();
+
+            canvas.release();
         }
-        canvas.moveText(36, page.getPageSize().getTop() - 20);
-        canvas.showText("Header text - Page " + pageNumber);
-        canvas.endText();
-
-        canvas.beginText();
-        canvas.moveText(36, 20);
-        canvas.showText("Footer text - Page " + pageNumber);
-        canvas.endText();
-
-        canvas.release();
     }
 }
