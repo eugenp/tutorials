c4ef6f5b79c53f99f72132a227187f2c759deb45
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2024-10-13 12:21:42 +0300

    [JAVA-40680] Moving some article links on Github - selenium (#17749)

--- testing-modules/selenium-2/src/main/java/com/baeldung/selenium/stale/WebElementUtils.java
+++ testing-modules/selenium-2/src/main/java/com/baeldung/selenium/stale/WebElementUtils.java
@@ -1,11 +1,11 @@
 package com.baeldung.selenium.stale;
 
-import org.openqa.selenium.WebElement;
-
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+
+import org.openqa.selenium.WebElement;
 
 public class WebElementUtils {
 
