308e580b128886c427bb2890a04fbf3203cd9f43
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-10-17 16:08:54 +0530

    hardcoding region in local file system s3client

--- aws-modules/s3proxy/src/main/java/com/baeldung/s3proxy/LocalStorageConfiguration.java
+++ aws-modules/s3proxy/src/main/java/com/baeldung/s3proxy/LocalStorageConfiguration.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
@@ -49,6 +50,7 @@ public class LocalStorageConfiguration {
     public S3Client s3Client() {
         return S3Client
             .builder()
+            .region(Region.US_EAST_1)
             .endpointOverride(URI.create(storageProperties.getProxyEndpoint()))
             .build();
     }
