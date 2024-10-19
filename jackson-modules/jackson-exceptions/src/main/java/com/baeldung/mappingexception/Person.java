9eee264b9350da4ce997ce13cf555fbbe12a95ff
Azhwani <azh.abderrahim@gmail.com>
2024-10-19 23:02:19 +0200

    BAEL-8487: Fix the JsonMappingException: Can not deserialize instance of java.util.HashMap out of START_ARRAY token

---
+++ jackson-modules/jackson-exceptions/src/main/java/com/baeldung/mappingexception/Person.java
@@ -0,0 +1,33 @@
+package com.baeldung.mappingexception;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public Person() {
+
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
