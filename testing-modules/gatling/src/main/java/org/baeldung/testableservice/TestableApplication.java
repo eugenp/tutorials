b4641ec6782ac360673fa56f01fe395c15f53c17
Stelios Anastasakis <bpstelios10@hotmail.com>
2024-10-20 21:36:00 +0300

    [SCALA-568] Added spring boot app in gatling module
    to have an api to use for tests in this article and in future ones

---
+++ testing-modules/gatling/src/main/java/org/baeldung/testableservice/TestableApplication.java
@@ -0,0 +1,12 @@
+package org.baeldung.testableservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestableApplication.class, args);
+    }
+}
---
+++ testing-modules/gatling/src/main/java/org/baeldung/testableservice/controllers/HealthController.java
@@ -0,0 +1,16 @@
+package org.baeldung.testableservice.controllers;
+
+import static org.springframework.http.ResponseEntity.ok;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/health")
+public class HealthController {
+
+    @GetMapping("/status")
+    public ResponseEntity<String> health() {
+        return ok("OK!!");
+    }
+}
