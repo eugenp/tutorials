029ff2690737e6a7598d4fb2e8460ef2f96b5251
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-11-03 18:14:02 +0530

    add comment to explain auto configuration exclusions

--- spring-ai/src/main/java/com/baeldung/imagegen/Application.java
+++ spring-ai/src/main/java/com/baeldung/imagegen/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * Excluding the below vector store store auto-configurations to avoid start up
+ * failure. Their corresponding starters are present on the classpath but are
+ * only needed by other articles in the shared codebase.
+ */
 @SpringBootApplication(exclude = {
     RedisVectorStoreAutoConfiguration.class,
     MongoDBAtlasVectorStoreAutoConfiguration.class
