9e40a51b6d7e019de73fc4992705b55b5d2c397c
Deepak-Vohra <dvohra10@yahoo.com>
2024-11-06 05:01:10 -0800

    Update LinkedListJUnitTest.java

--- core-java-modules/core-java-collections-list-2/src/test/java/com/baeldung/linked-list/LinkedListJUnitTest.java
+++ core-java-modules/core-java-collections-list-2/src/test/java/com/baeldung/linked-list/LinkedListJUnitTest.java
@@ -1,47 +1,32 @@
 package com.baeldung.java.list;
 
-import org.junit.Assert;
-import org.junit.Test;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.HashSet;
-
-import java.util.stream.Collectors;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList; 
+import java.util.LinkedList;
 
 public class LinkedListJUnitTest {
 
-    private final List<String> list1 = Arrays.asList("1", "2", "3", "4");
-    private final List<String> list2 = Arrays.asList("1", "2", "3", "4");
-    private final List<String> list3 = Arrays.asList("1", "2", "4", "3");
-
     @Test
-    public void whenTestingForEquality_ShouldBeEqual() throws Exception {
-        Assert.assertEquals(list1, list2);
-        Assert.assertNotSame(list1, list2);
-        Assert.assertNotEquals(list1, list3);
+    public void whenInitializingLinkedList_ShouldReturnEmptyList() throws Exception {
+        LinkedList<String> linkedList=new LinkedList<String>();
+        
+        Assertions.assertTrue(linkedList.isEmpty());
     }
 
     @Test
-    public void whenIntersection_ShouldReturnCommonElements() throws Exception {
-        List<String> list = Arrays.asList("red", "blue", "blue", "green", "red");
-        List<String> otherList = Arrays.asList("red", "green", "green", "yellow");
+    public void whenInitializingListFromCollection_ShouldReturnCollectionsElements() throws Exception {
+        ArrayList<Integer> arrayList=new ArrayList<Integer>(3);
+        
+        arrayList.add(Integer.valueOf(1));
+        arrayList.add(Integer.valueOf(2));
+        arrayList.add(Integer.valueOf(3));
+ 
+        LinkedList<Integer> linkedList=new LinkedList<Integer>(arrayList);
 
-        Set<String> commonElements = new HashSet(Arrays.asList("red", "green"));
+        Object[] linkedListElements = linkedList.toArray();
+        Object[] collectionElements = arrayList.toArray();
 
-        Set<String> result = list.stream()
-          .distinct()
-          .filter(otherList::contains)
-          .collect(Collectors.toSet());
-
-        Assert.assertEquals(commonElements, result);
-
-        Set<String> inverseResult = otherList.stream()
-          .distinct()
-          .filter(list::contains)
-          .collect(Collectors.toSet());
-
-        Assert.assertEquals(commonElements, inverseResult);
+        Assertions.assertArrayEquals(linkedListElements,collectionElements);
     }
 }
