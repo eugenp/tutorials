f310021ef310e939847ee96b8b70d9973b618f8f
panos-kakos <kakos.programmer@gmail.com>
2024-11-06 11:52:53 +0200

    [40668] Moved article "Returning Custom Status Codes from Spring Controllers" from spring-rest-http to spring-rest-http-2

--- spring-web-modules/spring-rest-http-2/src/main/java/com/baeldung/status/MvcConfig.java
+++ spring-web-modules/spring-rest-http-2/src/main/java/com/baeldung/status/MvcConfig.java
@@ -1,27 +1,24 @@
-package com.baeldung.config;
+package com.baeldung.status;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
-
-import org.springframework.oxm.xstream.XStreamMarshaller;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.text.SimpleDateFormat;
-import java.util.List;
 
 /*
  * Please note that main web configuration is in src/main/webapp/WEB-INF/api-servlet.xml
  */
 @Configuration
 @EnableWebMvc
-@ComponentScan({"com.baeldung.requestmapping" })
+@ComponentScan({ "com.baeldung.status" })
 public class MvcConfig implements WebMvcConfigurer {
 
     public MvcConfig() {
