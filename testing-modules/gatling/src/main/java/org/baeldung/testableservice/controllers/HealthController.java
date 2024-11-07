213879519089799c286f2a01eeb5e594b655ccfb
Stelios Anastasakis <bpstelios10@hotmail.com>
2024-11-07 23:29:39 +0200

    [SCALA-568] Moved simulation to scala project
    * Configured gatling to accept more than 1 simulation executions

--- testing-modules/gatling/src/main/java/org/baeldung/testableservice/controllers/HealthController.java
+++ testing-modules/gatling/src/main/java/org/baeldung/testableservice/controllers/HealthController.java
@@ -4,9 +4,11 @@ import static org.springframework.http.ResponseEntity.ok;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/health")
+@RestController
+@RequestMapping("/health")
 public class HealthController {
 
     @GetMapping("/status")
