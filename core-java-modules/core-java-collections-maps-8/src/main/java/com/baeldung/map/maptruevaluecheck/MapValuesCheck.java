3784bb914b63cba737be3c0834d1e3c70c888e84
Sagar Verma <sgrverma23@gmail.com>
2024-11-10 00:16:58 +0530

    BAEL-8736: sgrverma23 - refactoring logic to check all values are same

--- core-java-modules/core-java-collections-maps-8/src/main/java/com/baeldung/map/maptruevaluecheck/MapValuesCheck.java
+++ core-java-modules/core-java-collections-maps-8/src/main/java/com/baeldung/map/maptruevaluecheck/MapValuesCheck.java
@@ -1,24 +1,35 @@
 package com.baeldung.map.maptruevaluecheck;
 
 import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MapValuesCheck {
 
-    public static boolean areAllValuesTrueWithReduce(Map<String, Boolean> map) {
-        return map.values().stream().reduce(true, Boolean::logicalAnd);
+    public static boolean areAllValuesSameWithReduce(Map<String, Integer> map) {
+        if (map.isEmpty()) return true;
+        int firstValue = map.values().iterator().next();
+        return map.values().stream().reduce(true,
+                (result, value) -> result && value.equals(firstValue),
+                Boolean::logicalAnd);
     }
 
-    public static boolean areAllValuesTrueWithContains(Map<String, Boolean> map) {
-        return !map.containsValue(false);
+    public static boolean areAllValuesSameWithSet(Map<String, Integer> map) {
+        Set<Integer> uniqueValues = new HashSet<>(map.values());
+        return uniqueValues.size() == 1;
     }
 
-    public static boolean areAllValuesTrueWithAllMatch(Map<String, Boolean> map) {
-        return !map.isEmpty() && map.values().stream().allMatch(value -> value);
+    public static boolean areAllValuesSameWithAllMatch(Map<String, Integer> map) {
+        if (map.isEmpty()) return true;
+        int firstValue = map.values().iterator().next();
+        return map.values().stream().allMatch(value -> value.equals(firstValue));
     }
 
-    public static boolean areAllValuesTrueWithLoop(Map<String, Boolean> map) {
-        for (Boolean value : map.values()) {
-            if (!value) {
+    public static boolean areAllValuesSameWithLoop(Map<String, Integer> map) {
+        if (map.isEmpty()) return true;
+        int firstValue = map.values().iterator().next();
+        for (int value : map.values()) {
+            if (value != firstValue) {
                 return false;
             }
         }
--- core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/maptruevaluecheck/MapValuesCheckUnitTest.java
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/maptruevaluecheck/MapValuesCheckUnitTest.java
@@ -7,31 +7,43 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MapValuesCheckUnitTest {
 
     @Test
-    public void givenMapWithAllTrueValues_whenCheckAllValuesTrue_thenReturnTrue() {
-        Map<String, Boolean> map = Map.of("Task1", true, "Task2", true, "Task3", true);
+    public void givenMapWithAllSameValues_whenCheckAllValuesSame_thenReturnTrue() {
+        Map<String, Integer> map = Map.of("English", 100, "Maths", 100, "Science", 100);
 
-        assertTrue(MapValuesCheck.areAllValuesTrueWithAllMatch(map), "All values should be true");
-        assertTrue(MapValuesCheck.areAllValuesTrueWithContains(map), "All values should be true");
-        assertTrue(MapValuesCheck.areAllValuesTrueWithReduce(map), "All values should be true");
-        assertTrue(MapValuesCheck.areAllValuesTrueWithLoop(map), "All values should be true");
+        assertTrue(MapValuesCheck.areAllValuesSameWithAllMatch(map));
+        assertTrue(MapValuesCheck.areAllValuesSameWithReduce(map));
+        assertTrue(MapValuesCheck.areAllValuesSameWithLoop(map));
     }
 
     @Test
-    public void givenMapWithSomeFalseValues_whenCheckAllValuesTrue_thenReturnFalse() {
-        Map<String, Boolean> map = Map.of("Task1", true, "Task2", false);
+    public void givenMapWithDifferentValues_whenCheckAllValuesSame_thenReturnFalse() {
+        Map<String, Integer> map = Map.of("English", 1000, "Maths", 100, "Science", 100);
 
-        assertFalse(MapValuesCheck.areAllValuesTrueWithAllMatch(map), "Not all values are true");
-        assertFalse(MapValuesCheck.areAllValuesTrueWithContains(map), "Not all values are true");
-        assertFalse(MapValuesCheck.areAllValuesTrueWithReduce(map), "Not all values are true");
-        assertFalse(MapValuesCheck.areAllValuesTrueWithLoop(map), "Not all values are true");
+        assertFalse(MapValuesCheck.areAllValuesSameWithAllMatch(map));
+        assertFalse(MapValuesCheck.areAllValuesSameWithReduce(map));
+        assertFalse(MapValuesCheck.areAllValuesSameWithLoop(map));
     }
 
     @Test
-    public void givenEmptyMap_whenCheckAllValuesTrue_thenReturnFalseForAllMatchAndTrueForOthers() {
-        Map<String, Boolean> map = Map.of();
+    public void givenEmptyMap_whenCheckAllValuesSame_thenReturnTrue() {
+        Map<String, Integer> map = Map.of();
 
-        assertFalse(MapValuesCheck.areAllValuesTrueWithAllMatch(map), "Empty map should return false");
-        assertTrue(MapValuesCheck.areAllValuesTrueWithContains(map), "Empty map should return true");
-        assertTrue(MapValuesCheck.areAllValuesTrueWithReduce(map), "Empty map should return true");
+        assertTrue(MapValuesCheck.areAllValuesSameWithAllMatch(map));
+        assertTrue(MapValuesCheck.areAllValuesSameWithReduce(map));
+        assertTrue(MapValuesCheck.areAllValuesSameWithLoop(map));
+    }
+
+    @Test
+    void givenMapWithAllSameValues_whenCheckingAllValuesSameWithSet_thenReturnTrue() {
+        Map<String, Integer> map = Map.of("English", 100, "Maths", 100, "Science", 100);
+
+        assertTrue(MapValuesCheck.areAllValuesSameWithSet(map));
+    }
+
+    @Test
+    void givenMapWithDifferentValues_whenCheckingAllValuesSameWithSet_thenReturnFalse() {
+        Map<String, Integer> map = Map.of("English", 1000, "Maths", 100, "Science", 100);
+
+        assertFalse(MapValuesCheck.areAllValuesSameWithSet(map));
     }
 }
