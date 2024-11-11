203c63753b1956c49e1851db0681fc5ad2a1616b
uzma <u.khan@proxymity.io>
2024-11-11 23:25:12 +0000

    [bael-8098] code for article remove non alphabetic charcters

--- core-java-modules/core-java-string-operations-9/src/test/java/com/baeldung/removenonalphabets/StringManipluatorTest.java
+++ core-java-modules/core-java-string-operations-9/src/test/java/com/baeldung/removenonalphabets/StringManipluatorTest.java
@@ -4,9 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class StringManipluatorTest {
+class StringManipluatorUnitTest {
+
     @Test
-    public void testRemoveNonAlphabeticUsingRegex() {
+    public void givenStringWithSomeNonAlphabeticCharacters_whenRemoveNonAlphabeticUsingRegex_thenOnlyAlphabeticRemains() {
         String[] inputArray = { "Hello123", "Java@Code", "Stack#Overflow" };
         String[] expectedArray = { "Hello", "JavaCode", "StackOverflow" };
         for (int i = 0; i < inputArray.length; i++) {
@@ -15,7 +16,7 @@ class StringManipluatorTest {
     }
 
     @Test
-    public void testRemoveNonAlphabeticUsingStreams() {
+    public void givenStringWithSomeNonAlphabeticCharacters_whenRemoveNonAlphabeticUsingStreams_thenOnlyAlphabeticRemains() {
         String[] inputArray = { "Stream123", "Functional!", "Lambda$Syntax" };
         String[] expectedArray = { "Stream", "Functional", "LambdaSyntax" };
         for (int i = 0; i < inputArray.length; i++) {
@@ -24,7 +25,7 @@ class StringManipluatorTest {
     }
 
     @Test
-    public void testRemoveNonAlphabeticUsingStringBuilder() {
+    public void givenStringWithSomeNonAlphabeticCharacters_whenRemoveNonAlphabeticUsingStringBuilder_thenOnlyAlphabeticRemains() {
         String[] inputArray = { "Builder@Example", "Remove123Chars", "Efficient*Code" };
         String[] expectedArray = { "BuilderExample", "RemoveChars", "EfficientCode" };
         for (int i = 0; i < inputArray.length; i++) {
@@ -33,12 +34,12 @@ class StringManipluatorTest {
     }
 
     @Test
-    public void testRemoveNonAlphabeticWithApacheCommons() {
+    public void givenStringWithSomeNonAlphabeticCharacters_whenRemoveNonAlphabeticWithApacheCommons_thenOnlyAlphabeticRemains() {
         String[] inputArray = { "Commons123Lang", "Apache@Utility", "String#Utils" };
         String[] expectedArray = { "CommonsLang", "ApacheUtility", "StringUtils" };
         for (int i = 0; i < inputArray.length; i++) {
             assertEquals(expectedArray[i], StringManipluator.removeNonAlphabeticWithApacheCommons(inputArray[i]));
         }
     }
-
 }
+
