631903ee60a07172ec14e71f2148e4887d2ff275
panos-kakos <kakos.programmer@gmail.com>
2024-11-14 13:35:00 +0200

    [JAVA-41262] Moved article "Auto-Generated Field for MongoDB using Spring Boot" from spring-boot-persistence-mongodb-3 to spring-boot-persistence-mongodb

--- persistence-modules/spring-boot-persistence-mongodb/README.md
+++ persistence-modules/spring-boot-persistence-mongodb/README.md
@@ -4,4 +4,5 @@
 - [Spring Data MongoDB – Configure Connection](https://www.baeldung.com/spring-data-mongodb-connection)
 - [Connect to Multiple Databases Using Spring Data MongoDB](https://www.baeldung.com/mongodb-multiple-databases-spring-data)
 - [Logging MongoDB Queries with Spring Boot](https://www.baeldung.com/spring-boot-mongodb-logging)
+- [Auto-Generated Field for MongoDB using Spring Boot](https://www.baeldung.com/spring-boot-mongodb-auto-generated-field)
 - More articles: [[next-->]](../spring-boot-persistence-mongodb-2)
--- persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/mongodb/services/SequenceGeneratorService.java
+++ persistence-modules/spring-boot-persistence-mongodb/src/main/java/com/baeldung/mongodb/services/SequenceGeneratorService.java
@@ -1,16 +1,17 @@
 package com.baeldung.mongodb.services;
 
-import com.baeldung.mongodb.models.DatabaseSequence;
+import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
-import java.util.Objects;
-
-import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
-import static org.springframework.data.mongodb.core.query.Criteria.where;
-import static org.springframework.data.mongodb.core.query.Query.query;
+import com.baeldung.mongodb.models.DatabaseSequence;
 
 
 @Service
