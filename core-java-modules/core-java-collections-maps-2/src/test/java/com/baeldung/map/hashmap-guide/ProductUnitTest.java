ca88597993c41043b690d8532c384696c94e666a
dvohra09 <dvohra09@yahoo.com>
2024-11-14 18:16:06 -0800

    PR For https://jira.baeldung.com/browse/BAEL-8062 (#17974)
    * Create HashMapJUnitTest.java
    
    * Rename core-java-modules/core-java-collections-maps-2/src/test/java/com/baeldung/map/ProductUnitTest.java to core-java-modules/core-java-collections-maps-2/src/test/java/com/baeldung/map/hashmap-guide/ProductUnitTest.java
    
    * Update ProductUnitTest.java
    
    * Update ProductUnitTest.java
    
    * Delete core-java-modules/core-java-collections-maps-2/src/test/java/com/baeldung/map/hashmap-guide/HashMapJUnitTest.java

--- core-java-modules/core-java-collections-maps-2/src/test/java/com/baeldung/map/hashmap-guide/ProductUnitTest.java
+++ core-java-modules/core-java-collections-maps-2/src/test/java/com/baeldung/map/hashmap-guide/ProductUnitTest.java
@@ -10,7 +10,58 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductUnitTest {
 
+    @Test
+    public void whenUsingObjectAsHashMapGenericParameter_ShouldRequireCast() throws Exception {
 
+        try {
+            HashMap<String,Object> objectMap = new HashMap<String,Object>();
+
+            Product eBike = new Product("E-Bike", "A bike with a battery");
+            Product roadBike = new Product("Road bike", "A bike for competition");
+
+            objectMap.put("E-Bike", eBike);
+            objectMap.put("Road bike", roadBike);
+
+            HashMap<String,Product> productMap = new HashMap<String,Product>();
+
+            for (Map.Entry<String, Object> entry: objectMap.entrySet()) {
+                if (entry.getValue() instanceof Product) {
+                    productMap.put(entry.getKey(), (Product) entry.getValue());
+                }
+            }
+
+            Product product = productMap.get("E-Bike");
+            String actualDescription = product.getDescription();
+            String expectedDescription = new String("A bike with a battery");
+
+            assertTrue(actualDescription.equals(expectedDescription));
+
+        } catch(ClassCastException e) {
+              System.out.println(e.getMessage());
+          }
+    }
+
+    @Test
+    public void whenUsingProperHashMapGenericParameters_ShouldNotRequireCast() throws Exception {
+        HashMap<String,Product> productMap = new HashMap<String,Product>();
+
+        Product eBike = new Product("E-Bike", "A bike with a battery");
+        Product roadBike = new Product("Road bike", "A bike for competition");
+
+        productMap.put(eBike.getName(), eBike);
+        productMap.put(roadBike.getName(), roadBike);
+
+        Product product = productMap.get("E-Bike");
+        String actualDescription = product.getDescription();
+        String expectedDescription = new String("A bike with a battery");
+
+        assertTrue(actualDescription.equals(expectedDescription));
+
+        HashMap<String,Product> objectMap = productMap;
+        assertSame(objectMap,productMap);
+    }
+
+    
     @Test
     public void getExistingValue() {
         HashMap<String, Product> productsByName = new HashMap<>();
