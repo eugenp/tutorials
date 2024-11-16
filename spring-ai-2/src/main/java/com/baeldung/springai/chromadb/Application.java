41caa0872d4f7f73e46d7a26147db0caf9cfbd44
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-11-16 14:25:28 +0530

    use individual .properties file

--- spring-ai-2/src/main/java/com/baeldung/springai/chromadb/Application.java
+++ spring-ai-2/src/main/java/com/baeldung/springai/chromadb/Application.java
@@ -2,8 +2,10 @@ package com.baeldung.springai.chromadb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-chromadb.properties")
 public class Application {
 
     public static void main(String[] args) {
