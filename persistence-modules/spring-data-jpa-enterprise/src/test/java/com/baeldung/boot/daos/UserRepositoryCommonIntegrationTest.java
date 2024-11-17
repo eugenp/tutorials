c35b58a7b6b40cb477e514f7fe8c636d96041592
anuragkumawat <anuragkumawat7@gmail.com>
2024-11-18 00:39:47 +0530

    JAVA-41265 Moving some article links on Github - spring-data-jpa-enterprise (#17919)

--- persistence-modules/spring-data-jpa-enterprise/src/test/java/com/baeldung/boot/daos/UserRepositoryCommonIntegrationTest.java
+++ persistence-modules/spring-data-jpa-enterprise/src/test/java/com/baeldung/boot/daos/UserRepositoryCommonIntegrationTest.java
@@ -1,5 +1,24 @@
 package com.baeldung.boot.daos;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +34,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.baeldung.boot.daos.user.UserRepository;
 import com.baeldung.boot.domain.User;
 
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
-import java.time.LocalDate;
-import java.util.*;
-import java.util.function.Predicate;
-import java.util.stream.Stream;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.*;
-
 @SpringBootTest
-public class UserRepositoryCommon {
+public class UserRepositoryCommonIntegrationTest {
 
     final String USER_EMAIL = "email@example.com";
     final String USER_EMAIL2 = "email2@example.com";
@@ -44,34 +52,6 @@ public class UserRepositoryCommon {
     protected UserRepository userRepository;
     @Autowired
     private EntityManager entityManager;
-
-    @Test
-    @Transactional
-    public void givenUsersWithSameNameInDB_WhenFindAllByName_ThenReturnStreamOfUsers() {
-        User user1 = new User();
-        user1.setName(USER_NAME_ADAM);
-        user1.setEmail(USER_EMAIL);
-        userRepository.save(user1);
-
-        User user2 = new User();
-        user2.setName(USER_NAME_ADAM);
-        user2.setEmail(USER_EMAIL2);
-        userRepository.save(user2);
-
-        User user3 = new User();
-        user3.setName(USER_NAME_ADAM);
-        user3.setEmail(USER_EMAIL3);
-        userRepository.save(user3);
-
-        User user4 = new User();
-        user4.setName("SAMPLE");
-        user4.setEmail(USER_EMAIL4);
-        userRepository.save(user4);
-
-        try (Stream<User> foundUsersStream = userRepository.findAllByName(USER_NAME_ADAM)) {
-            assertThat(foundUsersStream.count()).isEqualTo(3l);
-        }
-    }
 
     @Test
     public void givenUsersInDB_WhenFindAllWithQueryAnnotation_ThenReturnCollectionWithActiveUsers() {
@@ -406,34 +386,6 @@ public class UserRepositoryCommon {
         assertThat(userPeter.getEmail()).isEqualTo(USER_EMAIL2);
     }
 
-
-    @Test
-    @Transactional
-    public void givenTwoUsers_whenFindByNameUsr01_ThenUserUsr01() {
-        User usr01 = new User("usr01", LocalDate.now(), "usr01@baeldung.com", 1);
-        User usr02 = new User("usr02", LocalDate.now(), "usr02@baeldung.com", 1);
-
-        userRepository.save(usr01);
-        userRepository.save(usr02);
-
-        try (Stream<User> users = userRepository.findAllByName("usr01")) {
-            assertTrue(users.allMatch(usr -> usr.equals(usr01)));
-        }
-    }
-
-    @Test
-    @Transactional
-    public void givenTwoUsers_whenFindByNameUsr00_ThenNoUsers() {
-        User usr01 = new User("usr01", LocalDate.now(), "usr01@baeldung.com", 1);
-        User usr02 = new User("usr02", LocalDate.now(), "usr02@baeldung.com", 1);
-
-        userRepository.save(usr01);
-        userRepository.save(usr02);
-
-        try (Stream<User> users = userRepository.findAllByName("usr00")) {
-            assertEquals(0, users.count());
-        }
-    }
 
     @Test
     public void givenTwoUsers_whenFindUsersWithGmailAddress_ThenUserUsr02() {
