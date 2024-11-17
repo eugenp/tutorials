e94ea4bedb1b136e8254f6c13040ecef119c35ef
panos-kakos <kakos.programmer@gmail.com>
2024-11-17 08:09:16 +0200

    [JAVA-41885] Moved article "Checking for Empty or Blank Strings in Java" from core-java-string-operations-2 to core-java-string-operations

--- core-java-modules/core-java-string-operations/README.md
+++ core-java-modules/core-java-string-operations/README.md
@@ -8,4 +8,5 @@ This module contains articles about string operations.
 - [Get Substring from String in Java](https://www.baeldung.com/java-substring)
 - [Adding a Newline Character to a String in Java](https://www.baeldung.com/java-string-newline)
 - [Java Base64 Encoding and Decoding](https://www.baeldung.com/java-base64-encode-and-decode)
+- [Checking for Empty or Blank Strings in Java](https://www.baeldung.com/java-blank-empty-strings)
 - More articles: [[next -->]](../core-java-string-operations-2)
--- core-java-modules/core-java-string-operations/src/test/java/com/baeldung/emptystrings/EmptyStringsUnitTest.java
+++ core-java-modules/core-java-string-operations/src/test/java/com/baeldung/emptystrings/EmptyStringsUnitTest.java
@@ -1,18 +1,22 @@
 package com.baeldung.emptystrings;
 
-import com.google.common.base.Strings;
+import static org.hamcrest.Matchers.iterableWithSize;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Set;
+
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.springframework.util.ObjectUtils;
+
+import com.google.common.base.Strings;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
 import jakarta.validation.ValidatorFactory;
-import java.util.Set;
-
-import static org.hamcrest.Matchers.iterableWithSize;
-import static org.junit.Assert.*;
 
 public class EmptyStringsUnitTest {
 
