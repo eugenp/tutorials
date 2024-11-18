7c629e3f8213b688012e4fcc6b99ae8b235ba7f0
luca <lucacambi77@gmail.com>
2024-11-18 18:39:21 +0100

    feat: swagger vs hateoas app and tests

---
+++ spring-boot-rest-2/src/main/java/com/baeldung/SpringBootRestApplication.java
@@ -0,0 +1,13 @@
+package com.baeldung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApplication.class, args);
+    }
+
+}
---
+++ spring-boot-rest-2/src/main/java/com/baeldung/hateoasvsswagger/model/User.java
@@ -0,0 +1,52 @@
+package com.baeldung.hateoasvsswagger.model;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import java.time.LocalDateTime;
+
+public class User extends RepresentationModel<User> {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private LocalDateTime createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public User(int id, String name, String email, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.createdAt = createdAt;
+    }
+}
