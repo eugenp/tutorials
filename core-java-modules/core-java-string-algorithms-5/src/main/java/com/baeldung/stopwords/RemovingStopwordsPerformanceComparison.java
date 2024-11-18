94fb71a87f8971fffe55503d6173c6eb14bd6180
panos-kakos <kakos.programmer@gmail.com>
2024-11-18 23:20:30 +0200

    [JAVA-41882] Moved articles "Remove Emojis from a Java String", "Check if a String Is a Pangram in Java", "Removing Stopwords from a String in Java" from core-java-string-algorithms to core-java-string-algorithms-5

--- core-java-modules/core-java-string-algorithms-5/src/main/java/com/baeldung/stopwords/RemovingStopwordsPerformanceComparison.java
+++ core-java-modules/core-java-string-algorithms-5/src/main/java/com/baeldung/stopwords/RemovingStopwordsPerformanceComparison.java
@@ -1,7 +1,5 @@
 package com.baeldung.stopwords;
 
-import org.openjdk.jmh.annotations.*;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -11,6 +9,14 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 
 @Fork(value = 3, warmups = 1)
 @State(Scope.Benchmark)
