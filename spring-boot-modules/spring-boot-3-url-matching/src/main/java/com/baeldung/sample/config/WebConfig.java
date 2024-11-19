36eec4fb854a136cba56f22be146a92d5550a4f9
dvohra09 <dvohra09@yahoo.com>
2024-11-19 03:15:18 -0800

    Update WebConfig.java

--- spring-boot-modules/spring-boot-3-url-matching/src/main/java/com/baeldung/sample/config/WebConfig.java
+++ spring-boot-modules/spring-boot-3-url-matching/src/main/java/com/baeldung/sample/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.baeldung.sample.config;
-
+import static org.springframework.http.HttpStatus.PERMANENT_REDIRECT;
+import org.springframework.web.filter.UrlHandlerFilter;
 import com.baeldung.sample.filters.TrailingSlashRedirectFilterReactive;
 import jakarta.servlet.Filter;
 import org.springframework.web.server.WebFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+
 
 import com.baeldung.sample.filters.TrailingSlashRedirectFilter;
 
@@ -29,5 +32,16 @@ public class WebConfig {
         return registrationBean;
     }
 
-    
+    @Bean
+    public FilterRegistrationBean<OncePerRequestFilter> urlHandlerFilterRegistrationBean() {
+        FilterRegistrationBean<OncePerRequestFilter> registrationBean = new FilterRegistrationBean<>();
+        UrlHandlerFilter urlHandlerFilter = UrlHandlerFilter
+		  .trailingSlashHandler("/blog/**").redirect(PERMANENT_REDIRECT)
+		  .trailingSlashHandler("/greetings/**").wrapRequest()
+		  .build();
+        
+        registrationBean.setFilter(urlHandlerFilter);
+        
+        return registrationBean;
+    }
 }
