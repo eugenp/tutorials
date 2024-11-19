7abc0987a74a1f4623e02830b74c9f5085377b3e
dvohra09 <dvohra09@yahoo.com>
2024-11-19 05:42:12 -0800

    Update WebConfig.java

--- spring-boot-modules/spring-boot-3-url-matching/src/main/java/com/baeldung/sample/config/WebConfig.java
+++ spring-boot-modules/spring-boot-3-url-matching/src/main/java/com/baeldung/sample/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.UrlHandlerFilter;
-
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.baeldung.sample.filters.TrailingSlashRedirectFilter;
 
