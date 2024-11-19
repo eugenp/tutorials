92f8dbcc3addfcc973f4c015c0a425f62555fef5
emanuel.trandafir <emanuel.trandafir@betvictor.com>
2024-11-19 08:58:44 +0100

    BAEL-8819: final code examples

--- testing-modules/assertion-libraries-2/src/test/java/com/baeldung/jsonunit/JsonUnitAssertionsUnitTest.java
+++ testing-modules/assertion-libraries-2/src/test/java/com/baeldung/jsonunit/JsonUnitAssertionsUnitTest.java
@@ -1,10 +1,13 @@
 package com.baeldung.jsonunit;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.json;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import net.javacrumbs.jsonunit.core.Option;
 
 class JsonUnitAssertionsUnitTest {
 
@@ -23,6 +26,24 @@ class JsonUnitAssertionsUnitTest {
     }
 
     @Test
+    void whenWeVerifyAJsonObject_thenItMatchesAnExpectedJsonString() {
+        String articleJson = """ 
+                {
+                   "name": "A Guide to Spring Boot",
+                   "tags": ["java", "spring boot", "backend"]
+                }
+            """;
+
+        assertThatJson(articleJson).isObject()
+            .isEqualTo(json("""
+                {
+                   "name": "A Guide to Spring Boot",
+                   "tags": ["java", "spring boot", "backend"]
+                }
+                """));
+    }
+
+    @Test
     void whenWeVerifyAJsonArrayField_thenItContainsExpectedValues() {
         String articleJson = """ 
             		{
@@ -38,4 +59,55 @@ class JsonUnitAssertionsUnitTest {
             .containsExactlyInAnyOrder("java", "spring boot", "backend");
     }
 
+    @Test
+    void whenWeVerifyTheListOfTags_thenItContainsExpectedStrings() {
+        String articleJson = """ 
+            		{
+            		   "name": "A Guide to Spring Boot",
+            		   "tags": ["java", "spring boot", "backend"]
+            		}
+            """;
+
+        assertThatJson(articleJson).when(Option.IGNORING_ARRAY_ORDER)
+            .when(Option.IGNORING_EXTRA_ARRAY_ITEMS)
+            .node("tags")
+            .isEqualTo(json("""
+                    ["backend", "java"]
+                """));
+    }
+
+    @Test
+    void whenWeVerifyJsonObject_thenItMatchesAgainstPlaceholders() {
+        String articleJson = """ 
+            		{
+            		   "name": "A Guide to Spring Boot",
+            		   "tags": ["java", "spring boot", "backend"]
+            		}
+            """;
+
+        assertThatJson(articleJson).isEqualTo(json(""" 
+                    {
+                       "name": "${json-unit.any-string}",
+                       "tags": "${json-unit.ignore-element}"
+                    }
+            """));
+    }
+
+    @Test
+    void whenWeVerifyJsonObjectIgnoringTags_thenItEqualsTheExpectedJson() {
+        String articleJson = """ 
+            		{
+            		   "name": "A Guide to Spring Boot",
+            		   "tags": ["java", "spring boot", "backend"]
+            		}
+            """;
+
+        assertThatJson(articleJson).whenIgnoringPaths("tags")
+            .isEqualTo(json(""" 
+                        {
+                           "name": "A Guide to Spring Boot",
+                           "tags": [ "ignored", "tags" ]
+                        }
+                """));
+    }
 }
