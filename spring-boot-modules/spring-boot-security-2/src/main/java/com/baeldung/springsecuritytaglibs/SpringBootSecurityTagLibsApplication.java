5104d0dfd4b25f888601201e2ac6758ef075d863
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2024-11-23 01:05:13 +0200

    [JAVA-40697] (#18011)
    * [JAVA-40697] Moved article "Introduction to Spring Security Taglibs" from spring-boot-security to spring-boot-security-2
    
    * [JAVA-40697] Moved article "Spring @EnableWebSecurity vs. @EnableGlobalMethodSecurity" from spring-boot-security to spring-boot-security-2
    
    * [JAVA-40697] Moved article "Guide to @CurrentSecurityContext in Spring Security" from spring-boot-security to spring-boot-security-2

--- spring-boot-modules/spring-boot-security-2/src/main/java/com/baeldung/springsecuritytaglibs/SpringBootSecurityTagLibsApplication.java
+++ spring-boot-modules/spring-boot-security-2/src/main/java/com/baeldung/springsecuritytaglibs/SpringBootSecurityTagLibsApplication.java
@@ -1,5 +1,6 @@
 package com.baeldung.springsecuritytaglibs;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
