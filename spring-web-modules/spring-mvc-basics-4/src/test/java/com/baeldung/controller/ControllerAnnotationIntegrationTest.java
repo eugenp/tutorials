dd7e8cb80c1c5bf73905794cda74b463775d4c98
achraftt <achraf.taitai1@gmail.com>
2024-11-27 22:00:47 +0100

    BAEL-7935

--- spring-web-modules/spring-mvc-basics-4/src/test/java/com/baeldung/controller/ControllerAnnotationIntegrationTest.java
+++ spring-web-modules/spring-mvc-basics-4/src/test/java/com/baeldung/controller/ControllerAnnotationIntegrationTest.java
@@ -61,17 +61,4 @@ public class ControllerAnnotationIntegrationTest {
         Assert.assertEquals(selectedStudent, studentDetails);
 
     }
-
-    @Test
-    public void testRestAnnotatedController() throws Exception {
-
-        String responseBody = this.mockMvc.perform(MockMvcRequestBuilders.get("/annotated/student/{studentId}", 1)).andReturn().getResponse().getContentAsString();
-
-        ObjectMapper reader = new ObjectMapper();
-
-        Student studentDetails = reader.readValue(responseBody, Student.class);
-
-        Assert.assertEquals(selectedStudent, studentDetails);
-    }
-
 }
