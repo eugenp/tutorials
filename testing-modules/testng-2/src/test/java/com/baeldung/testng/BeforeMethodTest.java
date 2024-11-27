b3d88577bbc24cdbe95a94d19779b1b091744aa2
oscarramadhan <oscarramadhan.or@gmail.com>
2024-11-28 00:34:08 +0700

    update BeforeMethodTest file name and tests name as well according to baeldung guides

--- testing-modules/testng-2/src/test/java/com/baeldung/testng/BeforeMethodTest.java
+++ testing-modules/testng-2/src/test/java/com/baeldung/testng/BeforeMethodTest.java
@@ -10,32 +10,32 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @SpringBootTest
-public class BeforeMethodUnitTest extends AbstractTestNGSpringContextTests {
-    private static final Logger log = LoggerFactory.getLogger(BeforeMethodUnitTest.class);
+public class BeforeMethodTest extends AbstractTestNGSpringContextTests {
+    private static final Logger log = LoggerFactory.getLogger(BeforeMethodTest.class);
 
     Counter counter;
 
     @BeforeTest
-    public void counterInitialized() {
+    public void init() {
         log.info("Initializing ...");
         counter = new Counter(0);
     }
 
     @BeforeMethod
-    public void counterInitializedResetTotalCountValue() {
+    public void givenCounterInitialized_whenResetTotalCount_thenTotalCountValueReset() {
         log.info("resetting total counter value ...");
         counter.resetTotalCount();
     }
 
     @Test
-    public void counterInitializedAddingValueTotalCountIncreased() {
+    public void givenCounterInitialized_whenAddingValue_thenTotalCountIncreased() {
         log.info("total counter before added: " + counter.getTotalCount());
         counter.addCounter(2);
         log.info("total counter after added: " + counter.getTotalCount());
     }
 
     @Test
-    public void counterInitializedSubtractingValueTotalCountDecreased() {
+    public void givenCounterInitialized_whenSubtractingValue_thenTotalCountDecreased() {
         log.info("total counter before subtracted: " + counter.getTotalCount());
         counter.subtractCounter(2);
         log.info("total counter after subtracted: " + counter.getTotalCount());
