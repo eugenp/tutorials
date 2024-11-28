787bdf2e86edc71d09e336527b228a6ad64d2358
michaelk <michael.krimgen@postcodeloterij.nl>
2024-11-28 18:42:52 +0100

    BAEL-7989 Update integration test

--- persistence-modules/spring-boot-postgresql/src/test/java/com/baeldung/postgres/datetime/DateTimeIntegrationTest.java
+++ persistence-modules/spring-boot-postgresql/src/test/java/com/baeldung/postgres/datetime/DateTimeIntegrationTest.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.temporal.ChronoUnit;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -17,7 +19,7 @@ public class DateTimeIntegrationTest {
     private DateTimeValueRepository dateTimeValueRepository;
 
     @Test
-    public void when_persisted_then_all_values_are_stored() {
+    public void given_java_date_times_values_when_persisted_then_all_values_are_stored() {
 
         // Given
         DateTimeValues dateTimeValues = new DateTimeValues();
@@ -31,10 +33,12 @@ public class DateTimeIntegrationTest {
     }
 
     @Test
-    public void given_persisted_local_date_time_as_date_then_retrieved_without_time() {
+    public void given_java_instant_when_persisted_as_sql_date_then_retrieved_without_time() {
         DateTimeValues dateTimeValues = new DateTimeValues();
         DateTimeValues persisted = dateTimeValueRepository.save(dateTimeValues);
         DateTimeValues fromDatabase = dateTimeValueRepository.findById(persisted.getId()).get();
+
         Assertions.assertNotEquals(dateTimeValues.getInstantAsDate(), fromDatabase.getInstantAsDate());
+        Assertions.assertEquals(dateTimeValues.getInstantAsDate().truncatedTo(ChronoUnit.DAYS), fromDatabase.getInstantAsDate());
     }
 }
