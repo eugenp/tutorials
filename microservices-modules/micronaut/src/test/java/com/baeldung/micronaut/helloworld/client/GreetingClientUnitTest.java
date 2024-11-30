835d7748190e9b624924fada3e1dfcf6d319f896
panos-kakos <kakos.programmer@gmail.com>
2024-11-30 20:26:07 +0200

    [JAVA-42080] Create isolated contexts for micronaut tests

--- microservices-modules/micronaut/src/test/java/com/baeldung/micronaut/helloworld/client/GreetingClientUnitTest.java
+++ microservices-modules/micronaut/src/test/java/com/baeldung/micronaut/helloworld/client/GreetingClientUnitTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@MicronautTest
+@MicronautTest(rebuildContext = true)
 public class GreetingClientUnitTest {
 
     @Inject
