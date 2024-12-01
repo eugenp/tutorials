19edbabf3f5f0c1246c1757bee61976f68f9a2e1
apelanovic <apelanovic@gmail.com>
2024-12-01 12:25:06 +0100

    BAEL-6550 Add simple filter for chain.doFilter presentation

---
+++ spring-boot-modules/spring-boot-basic-customization-3/src/main/java/com/baeldung/chaindofilter/ChainDoFilterApplication.java
@@ -0,0 +1,11 @@
+package com.baeldung.chaindofilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.baeldung.chaindofilter")
+public class ChainDoFilterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChainDoFilterApplication .class, args);
+    }
+}
---
+++ spring-boot-modules/spring-boot-basic-customization-3/src/test/java/com/baeldung/chaindofilter/ChainExampleFilterTest.java
@@ -0,0 +1,27 @@
+package com.baeldung.chaindofilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ChainExampleFilterTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void givenHeaderInResponse_whenFilterDoChain_thenAssertHeaderPresent() throws Exception {
+        mockMvc.perform(get("/chain/do-filter"))
+            .andExpect(status().isOk())
+            .andExpect(header().string("Custom-Header-Key", "CUSTOM_HEADER_VALUE"));
+    }
+
+}
