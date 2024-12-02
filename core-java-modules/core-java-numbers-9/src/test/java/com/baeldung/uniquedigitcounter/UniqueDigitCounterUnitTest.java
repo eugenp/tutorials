3c99fe1287bc0df888620f9330897e2b60445dd7
anujgaud <146576725+anujgaud@users.noreply.github.com>
2024-12-02 23:29:29 +0530

    [BAEL-8923] Add public keyword to the test methods

--- core-java-modules/core-java-numbers-9/src/test/java/com/baeldung/uniquedigitcounter/UniqueDigitCounterUnitTest.java
+++ core-java-modules/core-java-numbers-9/src/test/java/com/baeldung/uniquedigitcounter/UniqueDigitCounterUnitTest.java
@@ -6,17 +6,17 @@ import static org.junit.Assert.assertEquals;
 public class UniqueDigitCounterUnitTest {
 
     @Test 
-    void givenNumber_whenUsingBruteForce_thenCountNumbersWithUniqueDigits() { 
+    public void givenNumber_whenUsingBruteForce_thenCountNumbersWithUniqueDigits() { 
         assertEquals(91, UniqueDigitCounter.bruteForce(2)); 
     }
 
     @Test 
-    void givenNumber_whenUsingCombinatorial_thenCountNumbersWithUniqueDigits() { 
+    public void givenNumber_whenUsingCombinatorial_thenCountNumbersWithUniqueDigits() { 
         assertEquals(91, UniqueDigitCounter.bruteForce(2)); 
     }
 
     @Test 
-    void givenNumber_whenUsingDp_thenCountNumbersWithUniqueDigits() { 
+    public void givenNumber_whenUsingDp_thenCountNumbersWithUniqueDigits() { 
         assertEquals(91, UniqueDigitCounter.bruteForce(2)); 
     }
 }
