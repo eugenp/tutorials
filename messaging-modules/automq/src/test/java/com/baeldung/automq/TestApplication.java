c75aebc708cfd83e3a6f54955363c6c870babdb1
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-12-09 18:13:09 +0530

    update indentation

--- messaging-modules/automq/src/test/java/com/baeldung/automq/TestApplication.java
+++ messaging-modules/automq/src/test/java/com/baeldung/automq/TestApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+        SpringApplication.from(Application::main)
+            .with(TestcontainersConfiguration.class)
+            .run(args);
     }
 
 }
