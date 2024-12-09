efd647d84d26595d12d4ac7a8494b1db78ba68ef
Thiago Hora <thiago.santos.hora@gmail.com>
2024-12-10 00:23:12 +0100

    Reactive kafka stream

---
+++ spring-reactive-modules/spring-reactive-kafka-stream-binder/src/main/java/com/baeldung/reactive/kafka/stream/binder/producer/StockPriceProducer.java
@@ -0,0 +1,54 @@
+package com.baeldung.reactive.kafka.stream.binder.producer;
+
+import com.baeldung.reactive.kafka.stream.binder.domain.StockUpdate;
+import com.baeldung.reactive.kafka.stream.binder.kafka.TopicConfig;
+import lombok.NonNull;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.http.MediaType;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderResult;
+
+import java.time.Instant;
+import java.util.Random;
+
+@Component
+public class StockPriceProducer {
+
+    private final ReactiveKafkaProducerTemplate<String, StockUpdate> kafkaProducer;
+    private final NewTopic topic;
+
+    private final String[] stocks = {"AAPL", "GOOG", "MSFT", "AMZN", "TSLA"};
+    private final Random random = new Random();
+
+    @SuppressWarnings("all")
+    public StockPriceProducer(@NonNull KafkaProperties properties, @Qualifier(TopicConfig.STOCK_PRICES_IN) NewTopic topic) {
+        this.kafkaProducer = new ReactiveKafkaProducerTemplate<>(SenderOptions.create(properties.buildProducerProperties()));
+        this.topic = topic;
+    }
+
+    public Flux<SenderResult<Void>> produceStockPrices(int count) {
+        return Flux.range(0, count)
+                .map(i -> {
+                    String stock = stocks[random.nextInt(stocks.length)];
+                    double price = 100 + (200 * random.nextDouble());
+                    return MessageBuilder.withPayload(new StockUpdate(stock, price, Instant.now()))
+                            .setHeader(MessageHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                            .build();
+                })
+                .flatMap(stock -> {
+                    var newRecord = new ProducerRecord<>(topic.name(), stock.getPayload().symbol(), stock.getPayload());
+
+                    stock.getHeaders().forEach((key, value) -> newRecord.headers().add(key, value.toString().getBytes()));
+
+                    return kafkaProducer.send(newRecord);
+                });
+    }
+}
