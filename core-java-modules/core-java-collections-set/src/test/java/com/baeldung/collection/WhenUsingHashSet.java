9dc16c3b1850ba6a92ddb65266e0f1d28988632a
Deepak-Vohra <dvohra10@yahoo.com>
2024-12-11 13:57:11 -0800

    Update WhenUsingHashSet.java

--- core-java-modules/core-java-collections-set/src/test/java/com/baeldung/collection/WhenUsingHashSet.java
+++ core-java-modules/core-java-collections-set/src/test/java/com/baeldung/collection/WhenUsingHashSet.java
@@ -1,10 +1,10 @@
 package com.baeldung.collection;
 
 import org.junit.jupiter.api.Test; 
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import org.junit.jupiter.api.Assertions.assertThrows;
-import org.junit.jupiter.api.Assertions.assertTrue;
-import org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ConcurrentModificationException;
 import java.util.HashSet;
