ec8343111e1641caa64d8bddec2f2d43aebc73de
Deepak-Vohra <dvohra10@yahoo.com>
2024-12-11 11:58:57 -0800

    Create Employee.java

---
+++ core-java-modules/core-java-collections-set/src/test/java/com/baeldung/collection/Employee.java
@@ -0,0 +1,33 @@
+public class Employee implements Comparable<Employee> {
+    private int employeeId;
+    private String employeeName;
+
+    Employee(int employeeId, String employeeName) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+    }
+
+    int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    @Override
+    public String toString() {
+        return employeeId + " " + employeeName;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        if (this.employeeId == o.employeeId) {
+            return 0;
+        } else if (this.employeeId < o.employeeId) {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
