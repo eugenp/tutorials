260c45ce450b026b833f5040db62addab8e64416
dvohra09 <dvohra09@yahoo.com>
2024-12-12 07:28:07 -0800

    Update SequencedArrayListUnitTest.java

--- core-java-modules/core-java-collections-array-list/src/test/java/com/baeldung/collections/SequencedArrayListUnitTest.java
+++ core-java-modules/core-java-collections-array-list/src/test/java/com/baeldung/collections/SequencedArrayListUnitTest.java
@@ -26,4 +26,42 @@ public class SequencedArrayListUnitTest {
         assertEquals(expectedElement, arrayList.getLast());
     } 
 
+    @Test
+    public void givenSequencedArrayList_whenAddFirst_thenFirstElementAddedCorrectly() {
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(3,1,2));
+        arrayList.addFirst(4);
+     
+        Integer expectedElement=4;
+        assertEquals(expectedElement, arrayList.getFirst());
+    } 
+
+    @Test
+    public void givenSequencedArrayList_whenAddLast_thenLastElementAddedCorrectly() {
+       
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(3,1,2));
+        arrayList.addLast(5);
+    
+        Integer expectedElement=5;
+        assertEquals(expectedElement, arrayList.getLast());
+    } 
+
+    @Test
+    public void givenSequencedArrayList_whenRemoveFirst_thenFirstElementRemovedCorrectly() {
+
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(3,1,2));
+       
+        Integer expectedElement=3;
+        assertEquals(expectedElement, arrayList.removeFirst());
+    } 
+
+    @Test
+    public void givenSequencedArrayList_whenRemoveLast_thenLastElementRemovedCorrectly() {
+       
+        ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(3,1,2));
+     
+        Integer expectedElement=2;
+        assertEquals(expectedElement, arrayList.arrayList.removeLast());
+    } 
+
 }
