6e840c2b113c4caa60ea2106e44b18fee15c03be
hmdrz <hmdrzsharifi@gmail.com>
2024-12-19 13:07:14 +0330

    #BAEL-8370: improve indentation

--- spring-boot-modules/spring-boot-simple/src/main/java/com/baeldung/actuator/SecurityConfig.java
+++ spring-boot-modules/spring-boot-simple/src/main/java/com/baeldung/actuator/SecurityConfig.java
@@ -13,7 +13,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityWebFilterChain(HttpSecurity http) throws Exception {
         return http
-          .authorizeHttpRequests(auth -> auth.requestMatchers("/actuator/**").permitAll()
+          .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/actuator/**").permitAll()
             .anyRequest().authenticated())
           .build();
     }
