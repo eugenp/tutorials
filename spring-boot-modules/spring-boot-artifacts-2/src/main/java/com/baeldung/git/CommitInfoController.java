dcffa32e581555a55d3bb55341ee7759a15817d2
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2024-12-24 11:36:13 +0200

    [JAVA-40685] Moving some article links on Github - spring-boot-artifacts (#18110)

--- spring-boot-modules/spring-boot-artifacts-2/src/main/java/com/baeldung/git/CommitInfoController.java
+++ spring-boot-modules/spring-boot-artifacts-2/src/main/java/com/baeldung/git/CommitInfoController.java
@@ -1,11 +1,11 @@
 package com.baeldung.git;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class CommitInfoController {
