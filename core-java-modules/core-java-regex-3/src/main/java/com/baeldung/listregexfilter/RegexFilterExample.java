4e702b06f54e5a738e8400305edd4038544f0fe6
programenth <139003443+programenth@users.noreply.github.com>
2024-12-25 16:56:49 +0000

    organizing files

--- core-java-modules/core-java-regex-3/src/main/java/com/baeldung/listregexfilter/RegexFilterExample.java
+++ core-java-modules/core-java-regex-3/src/main/java/com/baeldung/listregexfilter/RegexFilterExample.java
@@ -1,4 +1,4 @@
-package com.baeldung;
+package com.baeldung.listregexfilter;
 
 import java.util.ArrayList;
 import java.util.List;
--- core-java-modules/core-java-regex-3/src/test/java/com/baeldung/listregexfilter/RegexFilterExampleUnitTest.java
+++ core-java-modules/core-java-regex-3/src/test/java/com/baeldung/listregexfilter/RegexFilterExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung;
+package com.baeldung.listregexfilter;
 
 import java.util.List;
 import java.util.Map;
