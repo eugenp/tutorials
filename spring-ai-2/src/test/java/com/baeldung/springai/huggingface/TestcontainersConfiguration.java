7ea77149038d786dd09f0a7b9de6ca1a44232ed4
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-12-27 18:50:25 +0530

    remove @ServiceConnection in favor of DynamicPropertyRegistrar bean

--- spring-ai-2/src/test/java/com/baeldung/springai/huggingface/TestcontainersConfiguration.java
+++ spring-ai-2/src/test/java/com/baeldung/springai/huggingface/TestcontainersConfiguration.java
@@ -1,18 +1,22 @@
 package com.baeldung.springai.huggingface;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.ollama.OllamaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
 
     @Bean
-    @ServiceConnection
-    OllamaContainer ollamaContainer() {
+    public OllamaContainer ollamaContainer() {
         return new OllamaContainer("ollama/ollama:0.5.4");
+    }
+
+    @Bean
+    public DynamicPropertyRegistrar dynamicPropertyRegistrar(OllamaContainer ollamaContainer) {
+        return registry -> {
+            registry.add("spring.ai.ollama.base-url", ollamaContainer::getEndpoint);
+        };
     }
 
 }
