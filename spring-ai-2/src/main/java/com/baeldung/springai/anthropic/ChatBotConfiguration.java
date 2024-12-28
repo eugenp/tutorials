83580a9315d13cdc5670e89d07cfe236c7ffb9d4
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-12-29 00:40:29 +0530

    implement chatbot

--- spring-ai-2/src/main/java/com/baeldung/springai/anthropic/ChatBotConfiguration.java
+++ spring-ai-2/src/main/java/com/baeldung/springai/anthropic/ChatBotConfiguration.java
@@ -1,6 +1,9 @@
 package com.baeldung.springai.anthropic;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,13 +15,20 @@ import org.springframework.core.io.Resource;
 public class ChatBotConfiguration {
 
     @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
+    @Bean
     public ChatClient chatClient(
         @Qualifier("anthropicChatModel") ChatModel chatModel,
+        ChatMemory chatMemory,
         @Value("classpath:prompts/chatbot-system-prompt.st") Resource systemPrompt
     ) {
         return ChatClient
             .builder(chatModel)
             .defaultSystem(systemPrompt)
+            .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
             .build();
     }
 
---
+++ spring-ai-2/src/main/java/com/baeldung/springai/anthropic/ChatBotService.java
@@ -0,0 +1,33 @@
+package com.baeldung.springai.anthropic;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ChatBotService {
+
+    private final ChatClient chatClient;
+
+    public ChatBotService(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    public ChatResponse chat(ChatRequest chatRequest) {
+        UUID chatId = Optional
+            .ofNullable(chatRequest.chatId())
+            .orElse(UUID.randomUUID());
+        String answer = chatClient
+            .prompt()
+            .user(chatRequest.question())
+            .advisors(advisorSpec ->
+                advisorSpec
+                    .param("chat_memory_conversation_id", chatId))
+            .call()
+            .content();
+        return new ChatResponse(chatId, answer);
+    }
+
+}
