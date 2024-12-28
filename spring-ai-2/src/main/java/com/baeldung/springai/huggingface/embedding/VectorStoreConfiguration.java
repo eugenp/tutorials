79448e596de67928336346e32466079d6fc8b05a
Hardik Singh Behl <hardik.behl7444@gmail.com>
2024-12-28 23:09:55 +0530

    update vector store bean type

--- spring-ai-2/src/main/java/com/baeldung/springai/huggingface/embedding/VectorStoreConfiguration.java
+++ spring-ai-2/src/main/java/com/baeldung/springai/huggingface/embedding/VectorStoreConfiguration.java
@@ -2,6 +2,7 @@ package com.baeldung.springai.huggingface.embedding;
 
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Configuration;
 public class VectorStoreConfiguration {
 
     @Bean
-    public SimpleVectorStore vectorStore(@Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel) {
+    public VectorStore vectorStore(@Qualifier("ollamaEmbeddingModel") EmbeddingModel embeddingModel) {
         return SimpleVectorStore
             .builder(embeddingModel)
             .build();
