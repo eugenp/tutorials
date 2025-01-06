c7a21d094e92c2cfa768b4476d7fa248b715f8b2
anuragkumawat <anuragkumawat7@gmail.com>
2025-01-06 17:49:30 +0530

    JAVA-40763 Moving some article links on Github - spring-thymeleaf (#18112)

--- spring-web-modules/spring-thymeleaf/src/test/java/com/baeldung/thymeleaf/controller/ControllerIntegrationTest.java
+++ spring-web-modules/spring-thymeleaf/src/test/java/com/baeldung/thymeleaf/controller/ControllerIntegrationTest.java
@@ -1,14 +1,16 @@
 package com.baeldung.thymeleaf.controller;
 
-import com.baeldung.thymeleaf.config.InitSecurity;
-import com.baeldung.thymeleaf.config.WebApp;
-import com.baeldung.thymeleaf.config.WebMVCConfig;
-import com.baeldung.thymeleaf.config.WebMVCSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -17,24 +19,19 @@ import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import com.baeldung.thymeleaf.config.WebApp;
+import com.baeldung.thymeleaf.config.WebMVCConfig;
 
 import jakarta.servlet.Filter;
 
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
-@ContextConfiguration(classes = { WebApp.class, WebMVCConfig.class, WebMVCSecurity.class, InitSecurity.class })
-public class FunctionCallIntegrationTest {
+@ContextConfiguration(classes = { WebApp.class, WebMVCConfig.class})
+public class ControllerIntegrationTest {
 
     @Autowired
     WebApplicationContext wac;
-    @Autowired
-    MockHttpSession session;
 
     private MockMvc mockMvc;
 
@@ -51,8 +48,8 @@ public class FunctionCallIntegrationTest {
     }
 
     @Test
-    public void testGetDates() throws Exception {
-        mockMvc.perform(get("/function-call").with(testUser()).with(csrf())).andExpect(status().isOk()).andExpect(view().name("functionCall.html"));
+    public void testTeachers() throws Exception {
+        mockMvc.perform(get("/listTeachers").with(testUser()).with(csrf())).andExpect(status().isOk()).andExpect(view().name("listTeachers.html"));
     }
 
 }
