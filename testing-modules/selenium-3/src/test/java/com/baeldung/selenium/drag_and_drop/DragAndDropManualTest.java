15bfbfcb67abf642df6eedf818e71e9094275057
panos-kakos <kakos.programmer@gmail.com>
2025-01-08 17:46:14 +0200

    [JAVA-42732]Converted unit test to manual

--- testing-modules/selenium-3/src/test/java/com/baeldung/selenium/drag_and_drop/DragAndDropManualTest.java
+++ testing-modules/selenium-3/src/test/java/com/baeldung/selenium/drag_and_drop/DragAndDropManualTest.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class DragAndDropUnitTest {
+public class DragAndDropManualTest {
 
     private WebDriver driver;
 
