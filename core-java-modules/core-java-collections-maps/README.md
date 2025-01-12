d5c9a4c0a49dd258f67237bddc4fc676a313fec8
panos-kakos <kakos.programmer@gmail.com>
2025-01-12 09:24:13 +0200

    [JAVA-41835] Moved articles "Convert Hashmap to JSON Object in Java", "Converting Object To Map in Java"  from core-java-collections-maps-6 to core-java-collections-maps

--- core-java-modules/core-java-collections-maps/README.md
+++ core-java-modules/core-java-collections-maps/README.md
@@ -3,4 +3,6 @@
 This module contains articles about Map data structures in Java.
 
 ### Relevant Articles:
+- [Convert Hashmap to JSON Object in Java](https://www.baeldung.com/java-convert-hashmap-to-json-object)
+- [Converting Object To Map in Java](https://www.baeldung.com/java-convert-object-to-map)
 - More articles: [[next -->]](/core-java-modules/core-java-collections-maps-2)
--- core-java-modules/core-java-collections-maps/src/test/java/com/baeldung/objecttomap/ObjectToMapUnitTest.java
+++ core-java-modules/core-java-collections-maps/src/test/java/com/baeldung/objecttomap/ObjectToMapUnitTest.java
@@ -2,11 +2,6 @@ package com.baeldung.objecttomap;
 
 import static org.junit.Assert.assertEquals;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.reflect.TypeToken;
-import com.google.gson.Gson;
-import org.junit.Test;
 import java.lang.reflect.Field;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -16,6 +11,13 @@ import java.util.Map;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 public class ObjectToMapUnitTest {
     Employee employee = new Employee("John", 3000.0);
@@ -56,44 +58,6 @@ public class ObjectToMapUnitTest {
         assertEquals(employee.getSalary(), map.get("salary"));
     }
 
-    @Test
-    public void given_UnsortedMap_whenSortingByValueDescending_thenValuesAreInDescendingOrder() {
-        Map<String, Integer> unsortedMap = new HashMap<>();
-        unsortedMap.put("one", 1);
-        unsortedMap.put("three", 3);
-        unsortedMap.put("five", 5);
-        unsortedMap.put("two", 2);
-        unsortedMap.put("four", 4);
-
-        Map<String, Integer> sortedMap = sortMapByValueDescending(unsortedMap);
-
-        assertEquals(5, sortedMap.size());
-        final Iterator<Integer> iterator = sortedMap.values().iterator();
-        assertEquals(5, (int) iterator.next());
-        assertEquals(4, (int) iterator.next());
-        assertEquals(3, (int) iterator.next());
-        assertEquals(2, (int) iterator.next());
-        assertEquals(1, (int) iterator.next());
-    }
-
-    @Test
-    public void given_UnsortedMap_whenUsingTreeMap_thenKeysAreInDescendingOrder() {
-        SortedMap<String, Integer> treeMap = new TreeMap<>(Comparator.reverseOrder());
-        treeMap.put("one", 1);
-        treeMap.put("three", 3);
-        treeMap.put("five", 5);
-        treeMap.put("two", 2);
-        treeMap.put("four", 4);
-
-        assertEquals(5, treeMap.size());
-        final Iterator<String> iterator = treeMap.keySet().iterator();
-        assertEquals("two", iterator.next());
-        assertEquals("three", iterator.next());
-        assertEquals("one", iterator.next());
-        assertEquals("four", iterator.next());
-        assertEquals("five", iterator.next());
-    }
-
     private static class Employee {
         private String name;
         private Double salary;
@@ -120,9 +84,4 @@ public class ObjectToMapUnitTest {
         }
     }
 
-    public static <K, V extends Comparable<? super V>> Map<K, V> sortMapByValueDescending(Map<K, V> map) {
-        return map.entrySet().stream()
-                .sorted(Map.Entry.<K, V>comparingByValue().reversed())
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
-    }
 }
