b2376b6dd5eb3faccf445d9cc90012d7f827ac87
dvohra09 <dvohra09@yahoo.com>
2025-01-14 14:08:38 -0800

    Update AppToPrintGraph.java

--- data-structures/src/main/java/com/baeldung/graph/AppToPrintGraph.java
+++ data-structures/src/main/java/com/baeldung/graph/AppToPrintGraph.java
@@ -1,1 +1,95 @@
 package com.baeldung.graph;
+
+public class AppToPrintGraph {
+    private Map<Vertex, List<Vertex>> adjVertices;
+
+    Graph() {
+        this.adjVertices = new HashMap<Vertex, List<Vertex>>();
+    }
+
+    void addVertex(String label) {
+        adjVertices.putIfAbsent(new Vertex(label), new ArrayList<>());
+    }
+
+    void removeVertex(String label) {
+        Vertex v = new Vertex(label);
+        adjVertices.values().stream().forEach(e -> e.remove(v));
+        adjVertices.remove(new Vertex(label));
+    }
+
+    void addEdge(String label1, String label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        adjVertices.get(v1).add(v2);
+        adjVertices.get(v2).add(v1);
+    }
+
+    void removeEdge(String label1, String label2) {
+        Vertex v1 = new Vertex(label1);
+        Vertex v2 = new Vertex(label2);
+        List<Vertex> eV1 = adjVertices.get(v1);
+        List<Vertex> eV2 = adjVertices.get(v2);
+        if (eV1 != null)
+            eV1.remove(v2);
+        if (eV2 != null)
+            eV2.remove(v1);
+    }
+
+    List<Vertex> getAdjVertices(String label) {
+        return adjVertices.get(new Vertex(label));
+    }
+    
+    String printGraph() {
+        StringBuffer sb = new StringBuffer();
+        for(Vertex v : adjVertices.keySet()) {
+            sb.append(v);
+            sb.append(adjVertices.get(v));
+        }
+        return sb.toString();
+    }
+
+    class Vertex {
+        String label;
+        Vertex(String label) {
+            this.label = label;
+        }
+        
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + getOuterType().hashCode();
+            result = prime * result + ((label == null) ? 0 : label.hashCode());
+            return result;
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            Vertex other = (Vertex) obj;
+            if (!getOuterType().equals(other.getOuterType()))
+                return false;
+            if (label == null) {
+                if (other.label != null)
+                    return false;
+            } else if (!label.equals(other.label))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+
+
+        private Graph getOuterType() {
+            return Graph.this;
+        }
+    }
+}
