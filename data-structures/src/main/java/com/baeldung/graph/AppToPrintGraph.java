143d1767923a665772f05bf8d0125c12fb1c9cec
dvohra09 <dvohra09@yahoo.com>
2025-01-14 14:16:24 -0800

    Update AppToPrintGraph.java

--- data-structures/src/main/java/com/baeldung/graph/AppToPrintGraph.java
+++ data-structures/src/main/java/com/baeldung/graph/AppToPrintGraph.java
@@ -3,10 +3,27 @@ package com.baeldung.graph;
 import java.util.Set;
 
 public class AppToPrintGraph {
-    
+
+  static Graph createGraph() {
+        Graph graph = new Graph();
+        graph.addVertex("Bob");
+        graph.addVertex("Alice");
+        graph.addVertex("Mark");
+        graph.addVertex("Rob");
+        graph.addVertex("Maria");
+        graph.addEdge("Bob", "Alice");
+        graph.addEdge("Bob", "Rob");
+        graph.addEdge("Alice", "Mark");
+        graph.addEdge("Rob", "Mark");
+        graph.addEdge("Alice", "Maria");
+        graph.addEdge("Rob", "Maria");
+        return graph;
+    }
    
    public static void main(String[] args) {
     Graph graph = createGraph();
     Set<String> result = GraphTraversal.breadthFirstTraversal(graph, "Bob");
+       
+    System.out.println(result.toString());   
 }
 }
