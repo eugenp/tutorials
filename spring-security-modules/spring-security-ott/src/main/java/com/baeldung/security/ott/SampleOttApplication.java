3773f403d12c9832ea727c2de949a6a7c584743f
psevestre <phil@lighthouse.com.br>
2025-01-16 18:20:15 -0300

    [BAEL-8844] Article code (#18173)
    * [BAEL-8844] Article code
    
    * [BAEL-8844] Refactoring & Simplification
    
    * [BAEL-8844] Code cleanup

---
+++ spring-security-modules/spring-security-ott/src/main/java/com/baeldung/security/ott/SampleOttApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.security.ott;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleOttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleOttApplication.class, args);
+    }
+}
---
+++ spring-security-modules/spring-security-ott/src/main/java/com/baeldung/security/ott/web/OttLoginLinkSuccessHandler.java
@@ -0,0 +1,25 @@
+package com.baeldung.security.ott.web;
+
+import com.baeldung.security.ott.service.OttSenderService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.ott.OneTimeToken;
+import org.springframework.security.web.authentication.ott.OneTimeTokenGenerationSuccessHandler;
+import org.springframework.security.web.authentication.ott.RedirectOneTimeTokenGenerationSuccessHandler;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class OttLoginLinkSuccessHandler implements OneTimeTokenGenerationSuccessHandler {
+
+    private final OttSenderService senderService;
+    private final OneTimeTokenGenerationSuccessHandler redirectHandler = new RedirectOneTimeTokenGenerationSuccessHandler("/login/ott");
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, OneTimeToken oneTimeToken) throws IOException, ServletException {
+        senderService.sendTokenToUser(oneTimeToken.getUsername(),oneTimeToken.getTokenValue(),oneTimeToken.getExpiresAt());
+        redirectHandler.handle(request, response, oneTimeToken);
+    }
+}
