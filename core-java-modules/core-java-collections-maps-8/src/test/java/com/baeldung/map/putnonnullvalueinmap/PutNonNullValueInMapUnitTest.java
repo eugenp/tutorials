f5d600151ca31b224c80e9e01555ef3c609a266e
danielmcnally285 <144589379+danielmcnally285@users.noreply.github.com>
2025-01-19 13:46:43 +0000

    Prevent null values for map unit test

---
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/putnonnullvalueinmap/PutNonNullValueInMapUnitTest.java
@@ -0,0 +1,51 @@
+package com.baeldung.map.putnonnullvalueinmap;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PutNonNullValueInMapUnitTest {
+
+    private Map<Integer, String> map;
+    private String nullValue;
+
+    @Test
+    void givenMapWithNullValue_whenUsingStreamFilter_thenObtainMapWithNonNullValues() {
+        final Map<Integer, String> nullValueMap = new HashMap<>();
+        nullValueMap.put(1, nullValue);
+
+        final Map<Integer, String> mapWithoutNullValues = nullValueMap.entrySet()
+            .stream()
+            .filter(entry -> Objects.nonNull(entry.getValue()))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        assertThat(mapWithoutNullValues).isEmpty();
+    }
+
+    @Test
+    void givenNullValue_whenUsingIfStatementWithPutMethod_thenObtainMapWithNonNullValues() {
+        if (nullValue != null) {
+            map.put(1, nullValue);
+        }
+        assertThat(map).isEmpty();
+    }
+
+    @Test
+    void givenNullValue_whenUsingOptionalIfPresentWithPutMethod_thenObtainMapWithNonNullValues() {
+        Optional.ofNullable(nullValue)
+            .ifPresent(value -> map.put(1, value));
+        assertThat(map).isEmpty();
+    }
+
+    @BeforeEach
+    void init() {
+        nullValue = null;
+        map = new HashMap<>();
+    }
+}
