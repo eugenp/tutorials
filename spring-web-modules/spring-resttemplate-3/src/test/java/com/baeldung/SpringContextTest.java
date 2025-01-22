8a60ee96d8289a69e9dc194e333995b84ae03a23
panos-kakos <kakos.programmer@gmail.com>
2025-01-22 09:30:50 +0200

    [JAVA-43863]

--- spring-web-modules/spring-resttemplate-3/src/test/java/com/baeldung/SpringContextTest.java
+++ spring-web-modules/spring-resttemplate-3/src/test/java/com/baeldung/SpringContextTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = { com.baeldung.resttemplate.methods.RestTemplateMethodsApplication.class, })
+@SpringBootTest(classes = { com.baeldung.compress.SpringCompressRequestApplication.class, })
 public class SpringContextTest {
 
     @Test
