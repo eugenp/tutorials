d6f67ecc1335e08fc2dfda3f024e72aa0f55d4b0
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-01-24 03:55:17 +0530

    add chatbot configuration

---
+++ spring-ai-2/src/main/java/com/baeldung/springai/nova/ChatbotConfiguration.java
@@ -0,0 +1,35 @@
+package com.baeldung.springai.nova;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.InMemoryChatMemory;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class ChatbotConfiguration {
+
+    @Bean
+    public ChatMemory chatMemory() {
+        return new InMemoryChatMemory();
+    }
+
+    @Bean
+    public ChatClient chatClient(
+        ChatModel chatModel,
+        ChatMemory chatMemory,
+        @Value("classpath:prompts/nova-chatbot-system-prompt.st") Resource systemPrompt
+    ) {
+        return ChatClient
+            .builder(chatModel)
+            .defaultSystem(systemPrompt)
+            .defaultAdvisors(new MessageChatMemoryAdvisor(chatMemory))
+            .build();
+    }
+
+}
