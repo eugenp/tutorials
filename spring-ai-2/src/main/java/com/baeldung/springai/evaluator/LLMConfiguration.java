89d06563a034ac03ebc7936ea50c29ab13a1091a
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-01-24 19:37:03 +0530

    add LLM configurations

---
+++ spring-ai-2/src/main/java/com/baeldung/springai/evaluator/LLMConfiguration.java
@@ -0,0 +1,42 @@
+package com.baeldung.springai.evaluator;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.evaluation.FactCheckingEvaluator;
+import org.springframework.ai.evaluation.RelevancyEvaluator;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LLMConfiguration {
+
+    @Bean
+    public ChatClient contentGenerator(ChatModel chatModel) {
+        return ChatClient.builder(chatModel).build();
+    }
+
+    @Bean
+    public ChatClient contentEvaluator(OllamaApi olamaApi) {
+        ChatModel chatModel = OllamaChatModel.builder()
+            .ollamaApi(olamaApi)
+            .defaultOptions(OllamaOptions.builder()
+                .model("bespoke-minicheck")
+                .build())
+            .build();
+        return ChatClient.builder(chatModel).build();
+    }
+
+    @Bean
+    public FactCheckingEvaluator factCheckingEvaluator(ChatClient contentEvaluator) {
+        return new FactCheckingEvaluator(contentEvaluator.mutate());
+    }
+
+    @Bean
+    public RelevancyEvaluator relevancyEvaluator(ChatClient contentEvaluator) {
+        return new RelevancyEvaluator(contentEvaluator.mutate());
+    }
+
+}
