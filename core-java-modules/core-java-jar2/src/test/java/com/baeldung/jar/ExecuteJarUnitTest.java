44978336c48df439ead41dad8ff3b44813ded188
karthikeya.tata <karthikeya.tata@gmail.com>
2025-01-27 11:43:18 +0530

    Updated the Tests to handle exceptions

--- core-java-modules/core-java-jar2/src/test/java/com/baeldung/jar/ExecuteJarUnitTest.java
+++ core-java-modules/core-java-jar2/src/test/java/com/baeldung/jar/ExecuteJarUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.jar;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.Objects;
@@ -14,49 +15,55 @@ public class ExecuteJarUnitTest {
     private static final String NON_RUNNABLE_JAR_PATH = "sample-jars/NonRunnableJar.jar";
 
     @Test
-    public void givenRunnableJar_whenExecuted_thenShouldRunSuccessfully() throws Exception {
+    public void givenRunnableJar_whenExecuted_thenShouldRunSuccessfully() {
+        String jarFile;
+        try {
+            jarFile = new File(Objects.requireNonNull(getClass().getClassLoader()
+                .getResource(RUNNABLE_JAR_PATH))
+                .toURI()).getAbsolutePath();
 
-        String jarFile = getJarPath(RUNNABLE_JAR_PATH);
+            ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarFile);
+            processBuilder.redirectErrorStream(true);
 
-        ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", jarFile);
+            Process process = processBuilder.start();
 
-        Process process = processBuilder.start();
+            try (InputStream inputStream = process.getInputStream()) {
+                byte[] output = inputStream.readAllBytes();
+                System.out.println("Output: " + new String(output));
+            }
 
-        InputStream inputStream = process.getInputStream();
-        byte[] output = inputStream.readAllBytes();
-        System.out.println("Output: " + new String(output));
-
-        int exitCode = process.waitFor();
-        Assert.assertEquals(0, exitCode);
-
+            int exitCode = process.waitFor();
+            Assert.assertEquals("Process exited with an unexpected exit code", 0, exitCode);
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            Assert.fail("Test failed due to exception: " + e.getMessage());
+        }
     }
 
     @Test
-    public void givenNonRunnableJar_whenExecutedWithMainClass_thenShouldRunSuccessfully() throws Exception {
+    public void givenNonRunnableJar_whenExecutedWithMainClass_thenShouldRunSuccessfully() {
+        String jarFile;
+        try {
+            jarFile = new File(Objects.requireNonNull(getClass().getClassLoader()
+                .getResource(NON_RUNNABLE_JAR_PATH))
+                .toURI()).getAbsolutePath();
 
-        String jarFile = getJarPath(NON_RUNNABLE_JAR_PATH);
+            String[] command = { "java", "-cp", jarFile, "com.company.HelloWorld", "arg1", "arg2" };
 
-        String[] command = { "java", "-cp", jarFile, "com.company.HelloWorld" };
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            processBuilder.redirectErrorStream(true);
 
-        ProcessBuilder processBuilder = new ProcessBuilder(command);
-        processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
 
-        Process process = processBuilder.start();
+            try (InputStream inputStream = process.getInputStream()) {
+                byte[] output = inputStream.readAllBytes();
+                System.out.println("Output: " + new String(output));
+            }
 
-        InputStream inputStream = process.getInputStream();
-        byte[] output = inputStream.readAllBytes();
-        System.out.println("Output: " + new String(output));
-
-        int exitCode = process.waitFor();
-        Assert.assertEquals(0, exitCode);
-
-    }
-
-    private String getJarPath(String resourcePath) throws URISyntaxException {
-
-        return new File(Objects.requireNonNull(getClass().getClassLoader()
-            .getResource(resourcePath))
-            .toURI()).getAbsolutePath();
+            int exitCode = process.waitFor();
+            Assert.assertEquals("Process exited with an unexpected exit code", 0, exitCode);
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            Assert.fail("Test failed due to exception: " + e.getMessage());
+        }
     }
 
 }
