9bf4e705102429615a42fbaf3647af860e671148
dvohra09 <dvohra09@yahoo.com>
2025-01-30 23:47:01 -0800

    Create StoredProcedureNullParamUnitTest.java

---
+++ persistence-modules/java-jpa/src/test/java/com/baeldung/jpa/storedprocedure/StoredProcedureNullParamUnitTest.java
@@ -0,0 +1,69 @@
+package com.baeldung.jpa.storedprocedure;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.ParameterMode;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.StoredProcedureQuery;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import com.baeldung.jpa.model.Car;
+
+public class StoredProcedureNullParamUnitTest {
+
+    private static EntityManagerFactory factory = null;
+    private static EntityManager entityManager = null;
+
+    @BeforeEach
+    public static void init() {
+        factory = Persistence.createEntityManagerFactory("jpa-db");
+        entityManager = factory.createEntityManager();
+    }
+
+    @BeforeEach
+    public void setup() {
+    }
+
+    @Test
+    public void createCarTest() {
+        final EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            transaction.begin();
+            final Car car = new Car("Fiat Marea", 2015);
+            entityManager.persist(car);
+            transaction.commit();
+        } catch (final Exception e) {
+            System.out.println(e.getCause());
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+        }
+    }
+
+
+    @Test
+    public void givenStoredProc_whenNullParamPassed_thenNoExceptionThrown() {
+        final StoredProcedureQuery storedProcedure = entityManager.createStoredProcedureQuery("FIND_CAR_BY_YEAR", Car.class)
+            .registerStoredProcedureParameter(1, Integer.class, ParameterMode.IN)
+            .setParameter(1, null);
+        assertDoesNotThrow(() -> {
+            storedProcedure.getResultList();
+        });
+    }
+
+    @AfterEach
+    public static void destroy() {
+
+        if (entityManager != null) {
+            entityManager.close();
+        }
+        if (factory != null) {
+            factory.close();
+        }
+    }
+}
