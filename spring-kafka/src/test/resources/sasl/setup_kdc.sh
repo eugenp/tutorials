7633774652f3f71f03607b2963bc4e2d4065f239
saikat <saikatcse03@gmail.com>
2025-02-01 22:38:04 +0530

    more improvements in whole setup

--- spring-kafka/src/test/resources/sasl/setup_kdc.sh
+++ spring-kafka/src/test/resources/sasl/setup_kdc.sh
@@ -5,11 +5,11 @@ kdb5_util create -s -P masterpassword
 
 # Run the keytab creation script
 kadmin.local -q "addprinc -randkey kafka/localhost@BAELDUNG.COM"
-kadmin.local -q "addprinc -randkey zookeeper/zookeeper.sasl_kafka-network@BAELDUNG.COM"
+kadmin.local -q "addprinc -randkey zookeeper/zookeeper.sasl_default@BAELDUNG.COM"
 kadmin.local -q "addprinc -randkey client/localhost@BAELDUNG.COM"
 
 kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/kafka.keytab kafka/localhost@BAELDUNG.COM"
-kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/zookeeper.keytab zookeeper/zookeeper.sasl_kafka-network@BAELDUNG.COM"
+kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/zookeeper.keytab zookeeper/zookeeper.sasl_default@BAELDUNG.COM"
 kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/client.keytab client/localhost@BAELDUNG.COM"
 
 echo "All keytabs have been created"
