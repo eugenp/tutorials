0abb1a1abc7bcd615452cf38450564f0d86afd11
saikat <saikatcse03@gmail.com>
2025-02-02 20:20:33 +0530

    code refactor

--- spring-kafka/src/test/resources/sasl/setup_kdc.sh
+++ spring-kafka/src/test/resources/sasl/setup_kdc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Initialize the Kerberos database if it doesn't exist
 kdb5_util create -s -P masterpassword
 
-# Run the keytab creation script
 kadmin.local -q "addprinc -randkey kafka/localhost@BAELDUNG.COM"
 kadmin.local -q "addprinc -randkey zookeeper/zookeeper.sasl_kafka-network@BAELDUNG.COM"
 kadmin.local -q "addprinc -randkey client@BAELDUNG.COM"
@@ -12,8 +10,5 @@ kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/kafka.keytab kafka/localhost@BAEL
 kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/zookeeper.keytab zookeeper/zookeeper.sasl_kafka-network@BAELDUNG.COM"
 kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/client.keytab client@BAELDUNG.COM"
 
-echo "All keytabs have been created"
-
-# Start KDC and Admin services
 krb5kdc
 kadmind -nofork
