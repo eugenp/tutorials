cc4b46dbc86a744b02f947915739acf728991993
danielmcnally285 <144589379+danielmcnally285@users.noreply.github.com>
2025-02-02 11:57:11 +0000

    rename local var

--- core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/putnonnullvalueinmap/PutNonNullValueInMapUnitTest.java
+++ core-java-modules/core-java-collections-maps-8/src/test/java/com/baeldung/map/putnonnullvalueinmap/PutNonNullValueInMapUnitTest.java
@@ -14,12 +14,12 @@ import org.junit.jupiter.api.Test;
 class PutNonNullValueInMapUnitTest {
 
     private Map<Integer, String> map;
-    private String nullValue;
+    private String value;
 
     @Test
     void givenMapWithNullValue_whenUsingStreamFilter_thenObtainMapWithNonNullValues() {
         final Map<Integer, String> nullValueMap = new HashMap<>();
-        nullValueMap.put(1, nullValue);
+        nullValueMap.put(1, value);
 
         final Map<Integer, String> mapWithoutNullValues = nullValueMap.entrySet()
             .stream()
@@ -30,22 +30,22 @@ class PutNonNullValueInMapUnitTest {
 
     @Test
     void givenNullValue_whenUsingIfStatementWithPutMethod_thenObtainMapWithNonNullValues() {
-        if (nullValue != null) {
-            map.put(1, nullValue);
+        if (value != null) {
+            map.put(1, value);
         }
         assertThat(map).isEmpty();
     }
 
     @Test
     void givenNullValue_whenUsingOptionalIfPresentWithPutMethod_thenObtainMapWithNonNullValues() {
-        Optional.ofNullable(nullValue)
-            .ifPresent(value -> map.put(1, value));
+        Optional.ofNullable(value)
+            .ifPresent(nonNullValue -> map.put(1, nonNullValue));
         assertThat(map).isEmpty();
     }
 
     @BeforeEach
     void init() {
-        nullValue = null;
+        value = null;
         map = new HashMap<>();
     }
 }
