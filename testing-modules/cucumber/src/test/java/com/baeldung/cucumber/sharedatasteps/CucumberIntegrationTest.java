3296b03a7b97f8c79161f0ec021ee6ed424c6353
s9m33r <sam33r.driving@gmail.com>
2025-02-03 11:02:10 +0530

    removed duplicate cucumber configuration annotation

--- testing-modules/cucumber/src/test/java/com/baeldung/cucumber/sharedatasteps/CucumberIntegrationTest.java
+++ testing-modules/cucumber/src/test/java/com/baeldung/cucumber/sharedatasteps/CucumberIntegrationTest.java
@@ -2,9 +2,7 @@ package com.baeldung.cucumber.sharedatasteps;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import io.cucumber.spring.CucumberContextConfiguration;
-
-@CucumberContextConfiguration
 @SpringBootTest
 class CucumberIntegrationTest {
+
 }
