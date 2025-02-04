41c56270a72db44789eca55cfe20784cc36ce478
saikat <saikatcse03@gmail.com>
2025-02-04 22:18:17 +0530

    unit test included

--- spring-kafka/src/test/resources/sasl/setup_kdc.sh
+++ spring-kafka/src/test/resources/sasl/setup_kdc.sh
@@ -3,11 +3,11 @@
 kdb5_util create -s -P masterpassword
 
 kadmin.local -q "addprinc -randkey kafka/localhost@BAELDUNG.COM"
-kadmin.local -q "addprinc -randkey zookeeper/zookeeper.sasl_kafka-network@BAELDUNG.COM"
+kadmin.local -q "addprinc -randkey zookeeper/zookeeper.sasl_default@BAELDUNG.COM"
 kadmin.local -q "addprinc -randkey client@BAELDUNG.COM"
 
 kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/kafka.keytab kafka/localhost@BAELDUNG.COM"
-kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/zookeeper.keytab zookeeper/zookeeper.sasl_kafka-network@BAELDUNG.COM"
+kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/zookeeper.keytab zookeeper/zookeeper.sasl_default@BAELDUNG.COM"
 kadmin.local -q "ktadd -k /etc/krb5kdc/keytabs/client.keytab client@BAELDUNG.COM"
 
 krb5kdc
