6247015d2a180f570982960dda2e0721033463a2
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 10:46:56 +0500

    Create DoublyLinkedList.java

---
+++ core-java-modules/core-java-collections-list-6/src/main/java/com/baeldung/linkedlisttostring/DoublyLinkedList.java
@@ -0,0 +1,44 @@
+package com.baeldung.linkedlisttostring;
+public class DoublyLinkedList {
+    DoublyLinkedListNode head;
+    DoublyLinkedListNode tail;
+
+    public void add(Integer data) {
+        DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    public static String customToString(DoublyLinkedList list) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Custom Doubly LinkedList: ");
+        if (list.head == null) {
+            sb.append("Empty List");
+        } else {
+            DoublyLinkedListNode currentNode = list.head;
+            while (currentNode != null) {
+                sb.append(currentNode.data).append(" <-> ");
+                currentNode = currentNode.next;
+            }
+            sb.delete(sb.length() - 4, sb.length());  
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList idsList = new DoublyLinkedList();
+        idsList.add(101);
+        idsList.add(102);
+        idsList.add(103);
+
+        System.out.println(customToString(idsList));
+        DoublyLinkedList emptyList = new DoublyLinkedList();
+        System.out.println(customToString(emptyList)); 
+    }
+}
