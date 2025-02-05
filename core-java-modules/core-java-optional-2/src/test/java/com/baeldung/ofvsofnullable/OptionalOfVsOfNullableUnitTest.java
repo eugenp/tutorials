04f8ecb92c9575ee203d9db668978e0ba85fa26f
Bipin kumar <sayy2bipin@gmail.com>
2025-02-06 01:34:05 +0530

    JAVA-41858 Moved codes of article from core-java-optional to core-java-optional-2 (#18246)

--- core-java-modules/core-java-optional-2/src/test/java/com/baeldung/ofvsofnullable/OptionalOfVsOfNullableUnitTest.java
+++ core-java-modules/core-java-optional-2/src/test/java/com/baeldung/ofvsofnullable/OptionalOfVsOfNullableUnitTest.java
@@ -1,4 +1,4 @@
-package com.baeldung.optional.ofvsofnullable;
+package com.baeldung.ofvsofnullable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
