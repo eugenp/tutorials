18b7b59d57f8d6622fc58ea9366cd281ac954faf
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 11:56:05 +0500

    Update DoublyLinkedListTest.java

--- core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/linkedlisttostring/DoublyLinkedListTest.java
+++ core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/linkedlisttostring/DoublyLinkedListTest.java
@@ -10,9 +10,11 @@ public class DoublyLinkedListTest {
         list.add(101);
         list.add(102);
         list.add(103);
+        
         String expectedOutput = "Custom Doubly LinkedList: 101 - 102 - 103";
         String actualOutput = DoublyLinkedList.customToString(list);
         assertEquals(expectedOutput, actualOutput, "The list toString should match the expected format.");
+        
         DoublyLinkedList emptyList = new DoublyLinkedList();
         String expectedEmptyOutput = "Custom Doubly LinkedList: Empty List";
         String actualEmptyOutput = DoublyLinkedList.customToString(emptyList);
