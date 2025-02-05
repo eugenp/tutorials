f54717c567973875f0dd88dd2ec041775d6f220a
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 11:49:58 +0500

    Update DoublyLinkedListTest.java

--- core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/linkedlisttostring/DoublyLinkedListTest.java
+++ core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/linkedlisttostring/DoublyLinkedListTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DoublyLinkedListTest {
     @Test
-    public void testAddAndToString() {
+    void testAddAndToString() {
         DoublyLinkedList list = new DoublyLinkedList();
         list.add(101);
         list.add(102);
