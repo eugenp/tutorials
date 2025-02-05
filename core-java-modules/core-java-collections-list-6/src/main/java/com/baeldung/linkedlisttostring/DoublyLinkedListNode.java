01478ec7ead03bcf02cdc8f77422d4de08e35d37
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 10:47:51 +0500

    Create DoublyLinkedListNode.java

---
+++ core-java-modules/core-java-collections-list-6/src/main/java/com/baeldung/linkedlisttostring/DoublyLinkedListNode.java
@@ -0,0 +1,9 @@
+package com.baeldung.linkedlisttostring;
+public class DoublyLinkedListNode {
+    Integer data;
+    DoublyLinkedListNode next;
+    DoublyLinkedListNode prev;
+    DoublyLinkedListNode(Integer data) {
+        this.data = data;
+    }
+}
