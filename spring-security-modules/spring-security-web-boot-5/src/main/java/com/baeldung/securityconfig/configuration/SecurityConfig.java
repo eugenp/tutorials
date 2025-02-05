c328ad8f9ba274d147c39ca820f9bbea87b9bb91
h_sharifi <h_sharifi@modernisc.com>
2025-02-05 19:32:27 +0330

    #BAEL-8966: add main source

---
+++ spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/securityconfig/configuration/SecurityConfig.java
@@ -0,0 +1,86 @@
+package com.baeldung.securityconfig.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+          .authorizeHttpRequests((authz) -> authz
+            .requestMatchers("/").hasRole("USER")
+            .requestMatchers("/admin/**").hasRole("ADMIN")
+            .anyRequest().authenticated())
+          .formLogin(withDefaults())
+          .httpBasic(withDefaults())
+          .logout(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+          http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+          .inMemoryAuthentication()
+            .withUser("user")
+            .password(passwordEncoder().encode("password"))
+            .roles("USER")
+            .and()
+            .withUser("admin")
+            .password(passwordEncoder().encode("password"))
+            .roles("ADMIN");
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http, DataSource dataSource,
+      PasswordEncoder passwordEncoder) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+          .jdbcAuthentication()
+            .dataSource(dataSource)
+            .passwordEncoder(passwordEncoder)
+            .withDefaultSchema()
+          .withUser("user")
+          .password(passwordEncoder.encode("password"))
+          .roles("USER")
+          .and()
+          .withUser("admin")
+          .password(passwordEncoder.encode("password"))
+          .roles("ADMIN");
+
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
+        userDetailsManager.setDataSource(dataSource);
+        return userDetailsManager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
