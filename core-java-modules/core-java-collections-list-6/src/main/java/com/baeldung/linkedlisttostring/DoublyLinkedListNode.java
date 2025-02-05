a6cf6240721e320b65acced77a4ec5f7d9d46c24
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 11:37:55 +0500

    Delete core-java-modules/core-java-collections-list-6/src/main/java/com/baeldung/linkedlisttostring directory

--- core-java-modules/core-java-collections-list-6/src/main/java/com/baeldung/linkedlisttostring/DoublyLinkedListNode.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.linkedlisttostring;
-public class DoublyLinkedListNode {
-    Integer data;
-    DoublyLinkedListNode next;
-    DoublyLinkedListNode prev;
-    DoublyLinkedListNode(Integer data) {
-        this.data = data;
-    }
-}
