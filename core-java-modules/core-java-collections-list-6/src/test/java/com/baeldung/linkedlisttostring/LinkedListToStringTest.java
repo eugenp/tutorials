4947dad2b0d61d358714ab8890bed7ef98ff2d2c
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 10:55:15 +0500

    Create LinkedListToStringTest.java

---
+++ core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/linkedlisttostring/LinkedListToStringTest.java
@@ -0,0 +1,21 @@
+package com.baeldung.linkedlisttostring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.util.LinkedList;
+
+public class LinkedListToStringTest {
+    @Test
+    void testDefaultToString() {
+        LinkedList<Integer> idsList = new LinkedList<>();
+        idsList.add(101);
+        idsList.add(102);
+        idsList.add(103);
+        assertEquals("[101, 102, 103]", idsList.toString(), "Default LinkedList toString() should match expected format.");
+    }
+    @Test
+    void testEmptyDefaultToString() {
+        LinkedList<Integer> emptyList = new LinkedList<>();
+        assertEquals("[]", emptyList.toString(), "Empty LinkedList should return empty brackets.");
+    }
+}
