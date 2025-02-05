5015007877e3b1d1af8679841051eb59121adb2c
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-05 20:20:18 +0500

    Create LinkedListToString.java

---
+++ core-java-modules/core-java-collections-list-6/src/main/java/com/baeldung/listtostring/LinkedListToString.java
@@ -0,0 +1,14 @@
+package com.baeldung.listtostring;
+import java.util.LinkedList;
+
+public class LinkedListToString {
+    public static void main(String[] args) {
+        LinkedList<Integer> idsList = new LinkedList<>();
+        idsList.add(101);
+        idsList.add(102);
+        idsList.add(103);
+        System.out.println(idsList.toString());
+        LinkedList<Integer> emptyList = new LinkedList<>();
+        System.out.println(emptyList.toString());
+    }
+}
