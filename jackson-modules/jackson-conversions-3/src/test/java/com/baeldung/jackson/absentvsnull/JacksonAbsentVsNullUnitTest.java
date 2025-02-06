5672cd1ea31f0988926d8dda9677f05a4ffee605
ulisseslima <ulisses@dvlcube.com>
2025-02-06 14:55:51 -0300

    bael 7697 - review 3

--- jackson-modules/jackson-conversions-3/src/test/java/com/baeldung/jackson/absentvsnull/JacksonAbsentVsNullUnitTest.java
+++ jackson-modules/jackson-conversions-3/src/test/java/com/baeldung/jackson/absentvsnull/JacksonAbsentVsNullUnitTest.java
@@ -25,121 +25,6 @@ class JacksonAbsentVsNullUnitTest {
     static final TypeReference<Map<String, Serializable>> MAP_TYPE = new TypeReference<Map<String, Serializable>>() {
     };
 
-    static class Sample {
-
-        private Long id;
-        private String name;
-        private int amount = 1;
-        private List<String> keys;
-        private List<Integer> values;
-
-        public Long getId() {
-            return id;
-        }
-
-        public void setId(Long id) {
-            this.id = id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAmount() {
-            return amount;
-        }
-
-        public void setAmount(int amount) {
-            this.amount = amount;
-        }
-
-        public List<String> getKeys() {
-            return keys;
-        }
-
-        public void setKeys(List<String> names) {
-            this.keys = names;
-        }
-
-        public List<Integer> getValues() {
-            return values;
-        }
-
-        public void setValues(List<Integer> values) {
-            this.values = values;
-        }
-
-        static Sample basic() {
-            Sample defaults = new Sample();
-
-            List<String> keys = List.of("foo", "bar");
-            List<Integer> values = List.of(1, 2);
-
-            defaults.setId(1l);
-            defaults.setName("name");
-            defaults.setAmount(3);
-            defaults.setKeys(keys);
-            defaults.setValues(values);
-
-            return defaults;
-        }
-    }
-
-    static void updateIgnoringNulls(String json, Sample current) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        Sample update = mapper.readValue(json, Sample.class);
-        mapper.setSerializationInclusion(Include.NON_DEFAULT);
-
-        if (update.getId() != null) {
-            current.setId(update.getId());
-        }
-
-        if (update.getName() != null) {
-            current.setName(update.getName());
-        }
-
-        current.setAmount(update.getAmount());
-
-        if (update.getKeys() != null) {
-            current.setKeys(update.getKeys());
-        }
-
-        if (update.getValues() != null) {
-            current.setValues(update.getValues());
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    static void updateNonAbsent(String json, Sample current) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Serializable> update = mapper.readValue(json, MAP_TYPE);
-        mapper.setSerializationInclusion(Include.NON_DEFAULT);
-
-        if (update.containsKey("id")) {
-            current.setId((Long) update.get("id"));
-        }
-
-        if (update.containsKey("name")) {
-            current.setName((String) update.get("name"));
-        }
-
-        if (update.containsKey("amount")) {
-            current.setAmount((int) update.get("amount"));
-        }
-
-        if (update.containsKey("keys")) {
-            current.setKeys((List<String>) update.get("keys"));
-        }
-
-        if (update.containsKey("values")) {
-            current.setValues((List<Integer>) update.get("values"));
-        }
-    }
-
     @Test
     void whenSerializingWithDefaults_thenNullValuesIncluded() {
         Sample zeroArg = new Sample();
@@ -253,5 +138,120 @@ class JacksonAbsentVsNullUnitTest {
         assertEquals(values, defaults.getValues());
         assertNull(defaults.getKeys());
         assertNotNull(defaults.getId());
+    }
+
+    static void updateIgnoringNulls(String json, Sample current) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Sample update = mapper.readValue(json, Sample.class);
+        mapper.setSerializationInclusion(Include.NON_DEFAULT);
+
+        if (update.getId() != null) {
+            current.setId(update.getId());
+        }
+
+        if (update.getName() != null) {
+            current.setName(update.getName());
+        }
+
+        current.setAmount(update.getAmount());
+
+        if (update.getKeys() != null) {
+            current.setKeys(update.getKeys());
+        }
+
+        if (update.getValues() != null) {
+            current.setValues(update.getValues());
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    static void updateNonAbsent(String json, Sample current) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Serializable> update = mapper.readValue(json, MAP_TYPE);
+        mapper.setSerializationInclusion(Include.NON_DEFAULT);
+
+        if (update.containsKey("id")) {
+            current.setId((Long) update.get("id"));
+        }
+
+        if (update.containsKey("name")) {
+            current.setName((String) update.get("name"));
+        }
+
+        if (update.containsKey("amount")) {
+            current.setAmount((int) update.get("amount"));
+        }
+
+        if (update.containsKey("keys")) {
+            current.setKeys((List<String>) update.get("keys"));
+        }
+
+        if (update.containsKey("values")) {
+            current.setValues((List<Integer>) update.get("values"));
+        }
+    }
+
+    static class Sample {
+
+        private Long id;
+        private String name;
+        private int amount = 1;
+        private List<String> keys;
+        private List<Integer> values;
+
+        Long getId() {
+            return id;
+        }
+
+        void setId(Long id) {
+            this.id = id;
+        }
+
+        String getName() {
+            return name;
+        }
+
+        void setName(String name) {
+            this.name = name;
+        }
+
+        int getAmount() {
+            return amount;
+        }
+
+        void setAmount(int amount) {
+            this.amount = amount;
+        }
+
+        List<String> getKeys() {
+            return keys;
+        }
+
+        void setKeys(List<String> names) {
+            this.keys = names;
+        }
+
+        List<Integer> getValues() {
+            return values;
+        }
+
+        void setValues(List<Integer> values) {
+            this.values = values;
+        }
+
+        static Sample basic() {
+            Sample defaults = new Sample();
+
+            List<String> keys = List.of("foo", "bar");
+            List<Integer> values = List.of(1, 2);
+
+            defaults.setId(1l);
+            defaults.setName("name");
+            defaults.setAmount(3);
+            defaults.setKeys(keys);
+            defaults.setValues(values);
+
+            return defaults;
+        }
     }
 }
