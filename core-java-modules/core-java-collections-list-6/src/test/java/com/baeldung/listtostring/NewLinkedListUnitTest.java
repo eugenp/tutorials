c480d3559421e3c81fe33e57c9590a7492d4e4d1
Anees1214 <aneesasghar.cs@gmail.com>
2025-02-11 12:46:32 +0500

    Update NewLinkedListUnitTest.java

--- core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/listtostring/NewLinkedListUnitTest.java
+++ core-java-modules/core-java-collections-list-6/src/test/java/com/baeldung/listtostring/NewLinkedListUnitTest.java
@@ -1,30 +1,28 @@
 package com.baeldung.listtostring;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import java.util.LinkedList;
 
-public class NewLinkedListTest {
+public class NewLinkedListUnitTest {
+	@Test
+	void givenList_customToString_formattedOutput() {
+		LinkedList<Integer> idsList = new LinkedList<>();
+		idsList.add(101);
+		idsList.add(102);
+		idsList.add(103);
 
-    // Test for custom toString() when the list is not empty
-    @Test
-    void givenList_customToString_formattedOutput() {
-        LinkedList<Integer> idsList = new LinkedList<>();
-        idsList.add(101);
-        idsList.add(102);
-        idsList.add(103);
+		String expected = "Custom LinkedList: 101 - 102 - 103";
+		assertEquals(expected, NewLinkedList.customToString(idsList),
+				"Custom toString() should format the list elements with ' - ' separator.");
+	}
 
-        String expected = "Custom LinkedList: 101 - 102 - 103";
-        assertEquals(expected, NewLinkedList.customToString(idsList), 
-            "Custom toString() should format the list elements with ' - ' separator.");
-    }
+	@Test
+	void emptyList_customToString_formattedOutput() {
+		LinkedList<Integer> emptyList = new LinkedList<>();
 
-    // Test for custom toString() when the list is empty
-    @Test
-    void emptyList_customToString_formattedOutput() {
-        LinkedList<Integer> emptyList = new LinkedList<>();
-        
-        String expected = "Custom LinkedList: Empty List";
-        assertEquals(expected, NewLinkedList.customToString(emptyList), 
-            "Empty list should return 'Empty List' in custom toString().");
-    }
+		String expected = "Custom LinkedList: Empty List";
+		assertEquals(expected, NewLinkedList.customToString(emptyList),
+				"Empty list should return 'Empty List' in custom toString().");
+	}
 }
