04876cf45ef09099ba98f6c261316b955e48837b
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2025-02-14 14:29:02 +0200

    [JAVA-40886] Fix missing tests from junit-5-advanced and junit-5-advanced-2 (#18273)

--- testing-modules/junit-5-advanced/src/test/java/com/baeldung/testsuite/suites/JUnitRunWithSuite.java
+++ testing-modules/junit-5-advanced/src/test/java/com/baeldung/testsuite/suites/JUnitRunWithSuite.java
@@ -2,12 +2,14 @@ package com.baeldung.testsuite.suites;
 
 import com.baeldung.testsuite.ClassOneUnitTest;
 import com.baeldung.testsuite.subpackage.ClassTwoUnitTest;
-import org.junit.platform.runner.JUnitPlatform;
 import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
+//import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.RunWith;
 
-@RunWith(JUnitPlatform.class)
+//@RunWith(JUnitPlatform.class)  Add junit-platform-runner dependency
+@Suite
 @SuiteDisplayName("My Test Suite")
 @SelectClasses({ClassOneUnitTest.class, ClassTwoUnitTest.class})
 public class JUnitRunWithSuite {
