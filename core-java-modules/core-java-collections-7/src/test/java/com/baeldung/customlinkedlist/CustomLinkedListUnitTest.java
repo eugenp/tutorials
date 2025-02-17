f71e50149e8d8e997b6979595fa6280ccb0232f8
Michael Olayemi <michaelolayemi28@gmail.com>
2025-02-17 13:41:34 +0100

    https://jira.baeldung.com/browse/BAEL-9107 (#18289)
    * https://jira.baeldung.com/browse/BAEL-9107
    
    * https://jira.baeldung.com/browse/BAEL-9107
    
    * https://jira.baeldung.com/browse/BAEL-9107
    
    * https://jira.baeldung.com/browse/BAEL-9107
    
    * https://jira.baeldung.com/browse/BAEL-9107

---
+++ core-java-modules/core-java-collections-7/src/test/java/com/baeldung/customlinkedlist/CustomLinkedListUnitTest.java
@@ -0,0 +1,66 @@
+package com.baeldung.customlinkedlist;
+
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CustomLinkedListUnitTest {
+
+    @Test
+    public void givenLinkedList_whenBuildingReferenceBetweenNodes_thenCorrect() {
+        Node<Integer> node0 = new Node<>(1);
+        Node<Integer> node1 = new Node<>(2);
+        Node<Integer> node2 = new Node<>(3);
+        node0.next = node1;
+        node1.next = node2;
+
+        assertEquals(1, node0.value);
+        assertEquals(2, node0.next.value);
+        assertEquals(3, node0.next.next.value);
+    }
+
+    @Test
+    public void givenAnEmptyCustomLinkedList_whenInsertingNodes_thenReturnValueOfSpecifiedIndex() {
+        CustomLinkedList<String> fruit = new CustomLinkedList<>();
+        fruit.insertTail("Avocado");
+        fruit.insertTail("Banana");
+        fruit.insertTail("Apple");
+        assertEquals("Apple", fruit.get(2));
+    }
+
+    @Test
+    public void givenLinkedList_whenGettingElementByIndex_thenCorrect() {
+        CustomLinkedList<String> fruit = new CustomLinkedList<>();
+        fruit.insertTail("Avocado");
+        fruit.insertTail("Banana");
+        fruit.insertTail("Apple");
+        assertEquals("Avocado", fruit.get(0));
+    }
+
+    @Test
+    public void givenEmptyCustomLinkedList_whenInsertingElementAtTheHead_thenReturnCorrectSize() {
+        CustomLinkedList<String> fruit = new CustomLinkedList<>();
+        fruit.insertHead("Avocado");
+        fruit.insertHead("Banana");
+        fruit.insertHead("Apple");
+        assertEquals(3, fruit.size());
+    }
+
+    @Test
+    public void givenCustomLinkedList_whenRetrievingTheSizeOfTheList_thenCorrect() {
+        CustomLinkedList<String> fruit = new CustomLinkedList<>();
+        fruit.insertHead("Avocado");
+        fruit.insertHead("Banana");
+        fruit.insertHead("Apple");
+        assertEquals(3, fruit.size());
+    }
+
+    @Test
+    public void givenCustomLinkedList_whenRemovingANodeBaseOnIndex_thenReturnNewSize() {
+        CustomLinkedList<String> fruit = new CustomLinkedList<>();
+        fruit.insertHead("Avocado");
+        fruit.insertHead("Banana");
+        fruit.insertHead("Apple");
+        fruit.removeAtIndex(2);
+        assertEquals(2, fruit.size());
+    }
+}
