f2f7ae02e798d9af7e1e1f2677e978da5527ba3e
Bipinkumar27 <sayy2bipin@gmail.com>
2025-02-18 00:36:18 +0530

    JAVA-40638: Changes made for moving some article links on Github - spring-cloud-openfeign

--- spring-cloud-modules/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/patcherror/client/UserClient.java
+++ spring-cloud-modules/spring-cloud-openfeign/src/main/java/com/baeldung/cloud/openfeign/patcherror/client/UserClient.java
@@ -1,12 +1,12 @@
-/*
 package com.baeldung.cloud.openfeign.patcherror.client;
 
-import com.baeldung.cloud.openfeign.patcherror.model.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.baeldung.cloud.openfeign.patcherror.model.User;
 
 @FeignClient(name = "user-client", url = "${user.api.url}")
 public interface UserClient {
@@ -14,4 +14,4 @@ public interface UserClient {
     @RequestMapping(value = "{userId}", method = RequestMethod.PATCH)
     User updateUser(@PathVariable(value = "userId") String userId, @RequestBody User user);
 
-}*/
+}
