a0ebd2aae64f16348fbbb1ffe7e9e061ae241f96
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-02-18 15:47:50 +0530

    update chatbot configuration

--- spring-ai-2/src/main/java/com/baeldung/springai/mcp/client/ChatbotConfiguration.java
+++ spring-ai-2/src/main/java/com/baeldung/springai/mcp/client/ChatbotConfiguration.java
@@ -1,6 +1,5 @@
 package com.baeldung.springai.mcp.client;
 
-import io.modelcontextprotocol.client.McpSyncClient;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
@@ -13,10 +12,10 @@ import java.util.List;
 class ChatbotConfiguration {
 
     @Bean
-    ChatClient chatClient(ChatModel chatModel, List<McpSyncClient> mcpClients) {
+    ChatClient chatClient(ChatModel chatModel, SyncMcpToolCallbackProvider toolCallbackProvider) {
         return ChatClient
             .builder(chatModel)
-            .defaultTools(new SyncMcpToolCallbackProvider(mcpClients).getToolCallbacks())
+            .defaultTools(toolCallbackProvider.getToolCallbacks())
             .build();
     }
 
