0ebe38c29fd6e782d9b795920b96d66d8f411e9a
Akash <akashmari713@gmail.com>
2025-02-22 12:24:59 +0530

    BUG 18329 FIX IS PROVIDED

--- core-java-modules/core-java-io-conversions/src/main/java/com/baeldung/csv/WriteCsvFileExample.java
+++ core-java-modules/core-java-io-conversions/src/main/java/com/baeldung/csv/WriteCsvFileExample.java
@@ -1,26 +1,18 @@
-package com.baeldung.csv;
-
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-public class WriteCsvFileExample {
-
-    public String convertToCSV(String[] data) {
-        return Stream.of(data)
-            .map(this::escapeSpecialCharacters)
-            .collect(Collectors.joining(","));
+public String escapeSpecialCharacters(String data) {
+    if (data == null) {
+        throw new IllegalArgumentException("Input data cannot be null");
     }
+    // Replace all line breaks with spaces first
+    data = data.replaceAll("\\R", " ");
+    String escapedData = data;
 
-    public String escapeSpecialCharacters(String data) {
-        if (data == null) {
-            throw new IllegalArgumentException("Input data cannot be null");
-        }
-        
-        String escapedData = data.replaceAll("\\R", " ");
-        if (data.contains(",") || data.contains("\"") || data.contains("'")) {
-            data = data.replace("\"", "\"\"");
-            escapedData = "\"" + data + "\"";
-        }
-        return escapedData;
+    if (data.contains(",") || data.contains("\"") || data.contains("'")) {
+        data = data.replace("\"", "\"\"");
+        escapedData = "\"" + data + "\"";
     }
+    return escapedData;
+}
+
+public static void main(String[] args) {
+    System.out.println(escapeSpecialCharacters("hi\nbye,bye")); // "hi bye,bye"
 }
