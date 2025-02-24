c16fe188725fa7b3a680b1d6dca0030f3ee91254
Bipinkumar27 <sayy2bipin@gmail.com>
2025-02-24 20:22:36 +0530

    JAVA-40681 fixed some library compatibility issues

--- testing-modules/spring-testing-2/src/main/java/com/baeldung/config/WebConfig.java
+++ testing-modules/spring-testing-2/src/main/java/com/baeldung/config/WebConfig.java
@@ -1,11 +1,11 @@
 package com.baeldung.config;
 
+import javax.servlet.ServletContext;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import jakarta.servlet.ServletContext;
 
 @EnableWebMvc
 @Configuration
