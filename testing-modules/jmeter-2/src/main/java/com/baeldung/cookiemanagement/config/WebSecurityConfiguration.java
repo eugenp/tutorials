dc5f2bc115dd837f1247936e019909df9e6e033f
ulisseslima <ulisses@dvlcube.com>
2025-02-24 15:48:52 -0300

    bael-3843 - jmeter cookies
    * base code
    * spring security dependency for form login in
    CookieManagementApplication

---
+++ testing-modules/jmeter-2/src/main/java/com/baeldung/cookiemanagement/config/WebSecurityConfiguration.java
@@ -0,0 +1,44 @@
+package com.baeldung.cookiemanagement.config;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration {
+
+    private final Logger log = LoggerFactory.getLogger(WebSecurityConfiguration.class);
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth.requestMatchers("/public/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+            .formLogin(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() throws IOException {
+        List<UserDetails> list = CsvUserDetailsUtils.read(new ClassPathResource("users.csv", this.getClass()));
+        log.info("loading {} users", list.size());
+        return new InMemoryUserDetailsManager(list);
+    }
+}
