caaf238432add0dd9da358938195411ab0b02dca
Wynn <sgwebfreelancer@gmail.com>
2025-02-25 10:05:16 +0800

    BAEL-9166 added google translator

---
+++ google-cloud/src/test/java/com/baeldung/google/cloud/translator/TranslatorLiveTest.java
@@ -0,0 +1,39 @@
+package com.baeldung.google.cloud.translator;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class TranslatorLiveTest {
+
+    @Test
+    void whenTranslateTextIsCalledWithEnglishTextAndFrenchTargetLanguage_thenReturnTranslatedText() {
+        String originalText = "Hello, world!";
+        String originalHtml = "<p>Hello, world!</p>";
+        String targetLanguage = "es";
+        String expectedTranslatedText = "¡Hola Mundo!";
+        String expectedTranslatedHtml = "<p>¡Hola Mundo!</p>";
+        String translatedText = Translator.translateText(originalText, targetLanguage);
+        String translatedHtml = Translator.translateText(originalHtml, targetLanguage);
+        assertEquals(expectedTranslatedText, translatedText);
+        assertEquals(expectedTranslatedHtml, translatedHtml);
+    }
+
+    @Test
+    void whenDetectLanguageIsCalledWithSpanishText_thenReturnSpanishLanguageCode() {
+        String text = "Hola, mundo!";
+        String expectedLanguageCode = "es";
+        String detectedLanguage = Translator.detectLanguage(text);
+        assertEquals(expectedLanguageCode, detectedLanguage);
+    }
+
+    @Test
+    void whenTranslateBatchIsCalledWithMultipleTexts_thenReturnTranslatedTexts() {
+        List<String> originalTexts = List.of("Apple", "Banana", "Orange");
+        List<String> expectedTranslatedTexts = List.of("Pomme", "Banane", "Orange");
+        List<String> translatedTexts = Translator.translateBatch(originalTexts, "fr");
+        assertEquals(expectedTranslatedTexts, translatedTexts);
+    }
+}
