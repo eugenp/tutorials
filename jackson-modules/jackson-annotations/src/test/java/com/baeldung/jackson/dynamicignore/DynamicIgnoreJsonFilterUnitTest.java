3df255bd5913b5e011f0ffd7d693ee39cb042af5
Gaetano Piazzolla <gae.piaz@gmail.com>
2025-02-26 10:07:06 +0100

    BAEL-9174 | removing finals

--- jackson-modules/jackson-annotations/src/test/java/com/baeldung/jackson/dynamicignore/DynamicIgnoreJsonFilterUnitTest.java
+++ jackson-modules/jackson-annotations/src/test/java/com/baeldung/jackson/dynamicignore/DynamicIgnoreJsonFilterUnitTest.java
@@ -24,12 +24,12 @@ class DynamicIgnoreJsonFilterUnitTest {
 
     @Test
     void whenWritingWithoutFilter_idIsPresent() throws JsonProcessingException {
-        final UserFilter user = new UserFilter(1L, "John");
+        UserFilter user = new UserFilter(1L, "John");
 
         SimpleFilterProvider filterProvider = new SimpleFilterProvider();
         filterProvider.addFilter("publicFilter", SimpleBeanPropertyFilter.serializeAll());
 
-        final String result = mapper.setFilterProvider(filterProvider)
+        String result = mapper.setFilterProvider(filterProvider)
             .writeValueAsString(user);
 
         assertThat(result).contains("John");
@@ -38,12 +38,12 @@ class DynamicIgnoreJsonFilterUnitTest {
 
     @Test
     void whenWritingWithFilter_idIsIgnored() throws JsonProcessingException {
-        final UserFilter user = new UserFilter(1L, "John");
+        UserFilter user = new UserFilter(1L, "John");
 
         SimpleFilterProvider filterProvider = new SimpleFilterProvider();
         filterProvider.addFilter("publicFilter", SimpleBeanPropertyFilter.serializeAllExcept("id"));
 
-        final String result = mapper.writer(filterProvider)
+        String result = mapper.writer(filterProvider)
             .writeValueAsString(user);
         assertTrue(result.contains("John"));
         assertFalse(result.contains("1"));
@@ -54,12 +54,12 @@ class DynamicIgnoreJsonFilterUnitTest {
 
     @Test
     void whenReadingWithoutFilter_idIsPresent() throws JsonProcessingException {
-        final String json = "{\"id\":1,\"name\":\"John\"}";
+        String json = "{\"id\":1,\"name\":\"John\"}";
 
         SimpleFilterProvider filterProvider = new SimpleFilterProvider();
         filterProvider.addFilter("publicFilter", SimpleBeanPropertyFilter.serializeAll());
 
-        final UserFilter result = mapper.setFilterProvider(filterProvider)
+        UserFilter result = mapper.setFilterProvider(filterProvider)
             .readValue(json, UserFilter.class);
         assertEquals(1L, result.getId());
         assertEquals("John", result.getName());
@@ -67,12 +67,12 @@ class DynamicIgnoreJsonFilterUnitTest {
 
     @Test
     void whenReadingWithFilter_idIsStillPresent() throws JsonProcessingException {
-        final String json = "{\"id\":1,\"name\":\"John\"}";
+        String json = "{\"id\":1,\"name\":\"John\"}";
 
         SimpleFilterProvider filterProvider = new SimpleFilterProvider();
         filterProvider.addFilter("publicFilter", SimpleBeanPropertyFilter.serializeAllExcept("id"));
 
-        final UserFilter result = mapper.setFilterProvider(filterProvider)
+        UserFilter result = mapper.setFilterProvider(filterProvider)
             .readValue(json, UserFilter.class);
         assertEquals(1L, result.getId());
         assertEquals("John", result.getName());
