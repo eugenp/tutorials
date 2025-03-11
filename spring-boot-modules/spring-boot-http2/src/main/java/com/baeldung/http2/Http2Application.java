d05a0064d946310475072900efd2b4f56cb2c9c6
Manfred <77407079+manfred106@users.noreply.github.com>
2025-03-11 17:29:03 +0000

    BAEL-6005: Enable HTTP2 with Tomcat in Spring Boot (#18374)

---
+++ spring-boot-modules/spring-boot-http2/src/main/java/com/baeldung/http2/Http2Application.java
@@ -0,0 +1,13 @@
+package com.baeldung.http2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Http2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Http2Application.class, args);
+    }
+
+}
