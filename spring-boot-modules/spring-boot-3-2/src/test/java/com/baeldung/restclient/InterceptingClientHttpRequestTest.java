a627d5bca74c5759b8e4e0e379610f822d86e705
Deepak-Vohra <dvohra10@yahoo.com>
2025-03-11 11:12:08 -0700

    Update InterceptingClientHttpRequestTest.java

--- spring-boot-modules/spring-boot-3-2/src/test/java/com/baeldung/restclient/InterceptingClientHttpRequestTest.java
+++ spring-boot-modules/spring-boot-3-2/src/test/java/com/baeldung/restclient/InterceptingClientHttpRequestTest.java
@@ -1,52 +1,29 @@
 package com.baeldung.restclient;
 
-import java.io.IOException;
-import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.ClientHttpRequest;
-import org.springframework.http.client.InterceptingClientHttpRequestFactory;
 import org.springframework.http.client.support.HttpRequestWrapper;
-import org.springframework.mock.http.client.MockClientHttpRequest;
-import org.springframework.mock.http.client.MockClientHttpResponse;
-
-import static org.assertj.core.api.Assertions.assertThat;
+ 
 
 class InterceptingClientHttpRequestTest {
 
-	private RequestFactoryMock requestFactoryMock = new RequestFactoryMock();
+    @Test
+    void updateRequestAttribute() throws Exception {
+        final String attrName = "attr1";
+	final String attrValue = "value1";
 
-	private MockClientHttpRequest requestMock = new MockClientHttpRequest();
-
-	private MockClientHttpResponse responseMock = new MockClientHttpResponse();
-
-	private InterceptingClientHttpRequestFactory requestFactory;
-
-	@BeforeEach
-	void beforeEach() {
-		this.requestMock.setResponse(this.responseMock);
-	}
-
-	@Test
-	void updateRequestAttribute() throws Exception {
-		final String attrName = "attr1";
-		final String attrValue = "value1";
-
-		ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
-			request.getAttributes().put(attrName, attrValue);
-			return execution.execute(request, body);
-		};
-	}
-
+	assertDoesNotThrow(() -> { 
+	  ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+	  request.getAttributes().put(attrName, attrValue);
+	  return execution.execute(request, body);
+	  };
+	});			    
+    }
 }
