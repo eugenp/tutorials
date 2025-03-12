e931c97a7adca7e127ed4e7576e160dc05ef8ad2
Anees1214 <aneesasghar.cs@gmail.com>
2025-03-12 06:39:33 +0500

    Create ScannerExampleUnitTest.java

---
+++ core-java-modules/core-java-scanner/src/main/java/com/baeldung/scanner/closescanner/ScannerExampleUnitTest.java
@@ -0,0 +1,21 @@
+package com.baeldung.closescanner;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class ScannerExampleUnitTest {
+	@Test
+	void givenUserName_whenGetGreetingMessage_thenReturnsWelcomeMessage() {
+		String simulatedInput = "Anees\n";
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+		Scanner scanner = new Scanner(inputStream);
+
+		ScannerExample example = new ScannerExample();
+		String result = example.getGreetingMessage(scanner);
+
+		assertEquals("Hi, Anees Welcome to Baeldung", result);
+
+		scanner.close();
+	}
+}
