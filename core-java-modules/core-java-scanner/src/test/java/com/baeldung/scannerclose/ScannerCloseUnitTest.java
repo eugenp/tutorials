866162062b452851184e71422c1ad5bd062dab6d
Anees1214 <aneesasghar.cs@gmail.com>
2025-03-12 07:30:29 +0500

    Update and rename ScannerExampleUnitTest.java to ScannerCloseUnitTest.java

---
+++ core-java-modules/core-java-scanner/src/test/java/com/baeldung/scannerclose/ScannerCloseUnitTest.java
@@ -0,0 +1,20 @@
+package com.baeldung.scannerclose;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+public class ScannerCloseUnitTest {
+    @Test
+    void givenUserName_whenGetGreetingMessage_thenReturnsWelcomeMessage() {
+        String input = "Anees\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(inputStream);
+
+        ScannerClose example = new ScannerClose();
+        String result = example.getGreetingMessage(scanner);
+
+        assertEquals("Hi, Anees Welcome to Baeldung", result);
+        scanner.close();
+    }
+}
