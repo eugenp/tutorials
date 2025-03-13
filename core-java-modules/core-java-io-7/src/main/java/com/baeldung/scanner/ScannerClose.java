f5c2b2937d2d1351337bbe7560f7702c8aa061bf
Anees1214 <aneesasghar.cs@gmail.com>
2025-03-13 20:43:35 +0500

    Create ScannerClose.java

---
+++ core-java-modules/core-java-io-7/src/main/java/com/baeldung/scanner/ScannerClose.java
@@ -0,0 +1,22 @@
+package com.baeldung.scanner;
+
+import java.util.Scanner;
+
+public class ScannerClose {
+    public String getGreetingMessage(Scanner scanner) {
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        return "Hi, " + name + " Welcome to Baeldung";
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            ScannerClose example = new ScannerClose();
+            String message = example.getGreetingMessage(scanner);
+            System.out.println(message);
+        } finally {
+            scanner.close();
+        }
+    }
+}
