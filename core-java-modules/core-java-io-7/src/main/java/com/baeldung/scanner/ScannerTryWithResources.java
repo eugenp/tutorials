16d9565514fa9e0a0a109a76d8f3b574fa532d7c
Anees1214 <aneesasghar.cs@gmail.com>
2025-03-13 20:44:41 +0500

    Create ScannerTryWithResources.java

---
+++ core-java-modules/core-java-io-7/src/main/java/com/baeldung/scanner/ScannerTryWithResources.java
@@ -0,0 +1,20 @@
+package com.baeldung.scanner;
+
+import java.util.Scanner;
+
+public class ScannerTryWithResources {
+
+    public String getGreetingMessage(Scanner scanner) {
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        return "Hi, " + name + " Welcome to Baeldung";
+    }
+
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            ScannerTryWithResources example = new ScannerTryWithResources();
+            String message = example.getGreetingMessage(scanner);
+            System.out.println(message);
+        }
+    }
+}
