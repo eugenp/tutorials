77e04c596e5ff5e4cbb229d24ee05eaa078760f0
Anees1214 <aneesasghar.cs@gmail.com>
2025-03-14 19:52:33 +0500

    Create ScannerTryWithResourcesUnitTest.java

---
+++ core-java-modules/core-java-exceptions-5/src/test/java/com/baeldung/exceptions/scannerneverclosed/ScannerTryWithResourcesUnitTest.java
@@ -0,0 +1,23 @@
+package com.baeldung.exceptions.scannerneverclosed;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+class ScannerTryWithResourcesUnitTest {
+
+    @Test
+    void givenUserName_whenGetGreetingMessage_thenReturnsWelcomeMessage() {
+        String input = "Anees\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
+
+        String result;
+        try (Scanner scanner = new Scanner(inputStream)) {
+            ScannerTryWithResources example = new ScannerTryWithResources();
+            result = example.getGreetingMessage(scanner);
+        }
+
+        assertEquals("Hi, Anees Welcome to Baeldung", result);
+    }
+}
