42bca544a622c48a8d0abbaf21da2a350b3ae000
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-03-15 22:52:18 +0530

    minor updates

--- spring-kafka-4/src/main/java/com/baeldung/kafka/synchronous/NotificationDispatchListener.java
+++ spring-kafka-4/src/main/java/com/baeldung/kafka/synchronous/NotificationDispatchListener.java
@@ -2,11 +2,11 @@ package com.baeldung.kafka.synchronous;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
-@Service
+@Component
 class NotificationDispatchListener {
 
     @KafkaListener(topics = "${com.baeldung.kafka.synchronous.request-topic}")
--- spring-kafka-4/src/main/java/com/baeldung/kafka/synchronous/SynchronousKafkaProperties.java
+++ spring-kafka-4/src/main/java/com/baeldung/kafka/synchronous/SynchronousKafkaProperties.java
@@ -5,5 +5,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.time.Duration;
 
 @ConfigurationProperties(prefix = "com.baeldung.kafka.synchronous")
-record SynchronousKafkaProperties(String requestTopic, String replyTopic, Duration replyTimeout) {
+record SynchronousKafkaProperties(
+    String requestTopic,
+    String replyTopic,
+    Duration replyTimeout
+) {
 }
