dd6209d45ddb5a28f1be13b4db5c4793b39ea615
achraftt <achraf.taitai1@gmail.com>
2025-03-19 00:23:36 +0100

    BAEL-7456: Output the Version Number to a Text File using Maven

---
+++ maven-modules/maven-version-number/src/test/java/com/baeldung/dependency/ordering/DependencyOrderingUnitTest.java
@@ -0,0 +1,19 @@
+package com.baeldung.dependency.ordering;
+
+class DependencyOrderingUnitTest {
+
+    /*
+    @Test
+    void whenUsingResourcesPlugin_ThenGenerateVersionFile() {
+        File versionFile = new File("target/classes/version.txt");
+        assertTrue(versionFile.exists(), "Version file (Maven Resources Plugin) should exist in target/classes.");
+    }
+
+    @Test
+    void whenUsingAntrunPlugin_ThenGenerateVersionFile() {
+        File versionFile = new File("target/output/version.txt");
+        assertTrue(versionFile.exists(), "Version file should exist in target/generated.");
+    }
+    */
+
+}
