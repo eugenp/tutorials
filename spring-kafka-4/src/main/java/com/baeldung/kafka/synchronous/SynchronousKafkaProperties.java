07b33938cb241a85d0d1f07125ec38828db20aae
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-03-19 21:46:57 +0530

    add validation on kafka configuration properties

--- spring-kafka-4/src/main/java/com/baeldung/kafka/synchronous/SynchronousKafkaProperties.java
+++ spring-kafka-4/src/main/java/com/baeldung/kafka/synchronous/SynchronousKafkaProperties.java
@@ -1,13 +1,24 @@
 package com.baeldung.kafka.synchronous;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.time.DurationMax;
+import org.hibernate.validator.constraints.time.DurationMin;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 import java.time.Duration;
 
+@Validated
 @ConfigurationProperties(prefix = "com.baeldung.kafka.synchronous")
 record SynchronousKafkaProperties(
+    @NotBlank
     String requestTopic,
+
+    @NotBlank
     String replyTopic,
+
+    @NotNull @DurationMin(seconds = 10) @DurationMax(minutes = 2)
     Duration replyTimeout
 ) {
 }
