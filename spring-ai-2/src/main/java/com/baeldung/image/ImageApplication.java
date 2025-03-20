8f49eb820fc72da762e93897ce1aad6f71dfb9bd
Manfred Ng <manfred.ng@gmail.com>
2025-03-20 22:33:49 +0000

    BAEL-9226: Extracting Structured Data from Images using AI in Java

---
+++ spring-ai-2/src/main/java/com/baeldung/image/ImageApplication.java
@@ -0,0 +1,31 @@
+package com.baeldung.image;
+
+import org.springframework.ai.autoconfigure.anthropic.AnthropicAutoConfiguration;
+import org.springframework.ai.autoconfigure.bedrock.converse.BedrockConverseProxyChatAutoConfiguration;
+import org.springframework.ai.autoconfigure.ollama.OllamaAutoConfiguration;
+import org.springframework.ai.autoconfigure.vectorstore.chroma.ChromaVectorStoreAutoConfiguration;
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+ * Exclude the libraries that are from the shared codebase
+*/
+@SpringBootApplication(
+    exclude = {
+        AnthropicAutoConfiguration.class,
+        BedrockConverseProxyChatAutoConfiguration.class,
+        ChromaVectorStoreAutoConfiguration.class,
+        OllamaAutoConfiguration.class,
+        PgVectorStoreAutoConfiguration.class
+    }
+)
+public class ImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ImageApplication.class);
+        app.setAdditionalProfiles("image");
+        app.run(args);
+    }
+
+}
---
+++ spring-ai-2/src/main/java/com/baeldung/image/ImageController.java
@@ -0,0 +1,35 @@
+package com.baeldung.image;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+@RestController
+@RequestMapping("/image")
+public class ImageController {
+
+    @Autowired
+    private CarCountService carCountService;
+
+    @PostMapping("/car-count")
+    public ResponseEntity<?> getCarCounts(@RequestParam("colors") String colors,
+                                          @RequestParam("file") MultipartFile file) {
+
+        try (InputStream inputStream = file.getInputStream()) {
+            var carCount = carCountService.getCarCount(inputStream, file.getContentType(), colors);
+            return ResponseEntity.ok(carCount);
+        } catch (IOException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error uploading image");
+        }
+    }
+
+}
