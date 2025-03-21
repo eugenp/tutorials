893447ae1d0640144b7ba6bbc80ac0d9d3effcbb
Michael Olayemi <michaelolayemi28@gmail.com>
2025-03-21 13:34:44 +0100

    https://jira.baeldung.com/browse/BAEL-8799

--- persistence-modules/spring-boot-persistence-5/src/main/java/com/baeldung/db2database/ArticleApplication.java
+++ persistence-modules/spring-boot-persistence-5/src/main/java/com/baeldung/db2database/ArticleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ArticleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArticleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ArticleApplication.class, args);
+    }
 
 }
