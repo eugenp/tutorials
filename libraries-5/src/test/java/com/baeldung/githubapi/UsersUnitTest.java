f07a20bcd1c9e5c5c80552c3e12f20624087e84f
Graham Cox <graham@grahamcox.co.uk>
2025-03-22 15:41:26 +0000

    Update libraries-5/src/test/java/com/baeldung/githubapi/UsersUnitTest.java
    Co-authored-by: Liam Williams <liam.williams@zoho.com>

--- libraries-5/src/test/java/com/baeldung/githubapi/UsersUnitTest.java
+++ libraries-5/src/test/java/com/baeldung/githubapi/UsersUnitTest.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class UsersUnitTest {
     @Test
     @Disabled
-    void whenWeAccessMyself_WeCanQueryUserDetails() throws IOException {
+    void whenWeAccessMyself_thenWeCanQueryUserDetails() throws IOException {
         GitHub gitHub = GitHub.connect();
 
         GHMyself myself = gitHub.getMyself();
