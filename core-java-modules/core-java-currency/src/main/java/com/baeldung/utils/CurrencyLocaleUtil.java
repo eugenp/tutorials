704106ad779d4acc0eb79ea96d57f76c8b8d1638
Sagar Verma <sgrverma23@gmail.com>
2025-03-24 10:58:19 +0530

    BAEL-8493:sgrverma23 - Changes for coversation of currency code to symbol in java

---
+++ core-java-modules/core-java-currency/src/main/java/com/baeldung/utils/CurrencyLocaleUtil.java
@@ -0,0 +1,9 @@
+import java.util.Currency;
+import java.util.Locale;
+
+public class CurrencyLocaleUtil {
+    public String getSymbolForLocale(Locale locale) {
+        Currency currency = Currency.getInstance(locale);
+        return currency.getSymbol();
+    }
+}
---
+++ core-java-modules/core-java-currency/src/main/java/com/baeldung/utils/CurrencyMapUtil.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class CurrencyMapUtil {
+    private static final Map<String, String> currencymap = Map.of(
+            "USD", "$",
+            "EUR", "€",
+            "INR", "₹"
+    );
+
+    public static String getSymbol(String currencyCode) {
+        return currencymap.getOrDefault(currencyCode, "Unknown");
+    }
+}
