c7a18b57b9bb3b58e2adffc3e731d9a293bdf151
rajatgarg <rajatgarg@adobe.com>
2025-03-26 10:37:53 +0530

    [BAEL-9175] Add various approaches for copying specific fields

---
+++ spring-core-4/src/test/java/com/baeldung/copyproperties/SourceBean.java
@@ -0,0 +1,37 @@
+package com.baeldung.copyproperties;
+
+public class SourceBean {
+    public String name;
+    public int age;
+    public String address;
+
+    public SourceBean(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
