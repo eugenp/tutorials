ea82c9dd355daca428cd374ec619c2b6a2b9c82e
programenth <139003443+programenth@users.noreply.github.com>
2025-03-27 07:14:07 +0530

    Update EurekaClientApplication.java

--- spring-cloud-modules/spring-cloud-eureka/spring-cloud-eureka-client2/eureka-client/src/main/java/com/baeldung/eurekaclient/EurekaClientApplication.java
+++ spring-cloud-modules/spring-cloud-eureka/spring-cloud-eureka-client2/eureka-client/src/main/java/com/baeldung/eurekaclient/EurekaClientApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EurekaClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EurekaClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
 
 }
