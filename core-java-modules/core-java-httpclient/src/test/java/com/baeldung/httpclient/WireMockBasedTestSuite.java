6efa32a59aa20245a705336c818e7a8c010432a1
Reşat SABIQ <haqer@gmx.fr>
2025-03-28 23:40:32 +0100

    Rename suite to WireMockBasedTestSuite & exclude HttpClientPostUnitTest

--- core-java-modules/core-java-httpclient/src/test/java/com/baeldung/httpclient/WireMockBasedTestSuite.java
+++ core-java-modules/core-java-httpclient/src/test/java/com/baeldung/httpclient/WireMockBasedTestSuite.java
@@ -4,10 +4,10 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 /**
- * Log-analyzing testing necessitates sequential execution.
+ * Log-analyzing testing necessitates sequential execution of tests involving WireMock.
  */
 @Suite
 @SelectClasses({HttpClientConnectionManagementUnitTest.class
-        , HttpClientPostUnitTest.class, JavaHttpClientExceptionHandlingUnitTest.class})
-public class JavaHttpClientTestSuite {
+        , JavaHttpClientExceptionHandlingUnitTest.class})
+public class WireMockBasedTestSuite {
 }
