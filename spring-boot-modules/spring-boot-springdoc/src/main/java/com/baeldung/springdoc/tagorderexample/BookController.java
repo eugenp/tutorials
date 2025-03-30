4d66481c1cb3b4ba12d63260cb776474a848cb12
dvohra09 <dvohra09@yahoo.com>
2025-03-29 19:57:33 -0700

    Update BookController.java

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tagorderexample/BookController.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tagorderexample/BookController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-
+import org.springframework.validation.annotation.Valid;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
