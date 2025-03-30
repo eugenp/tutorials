06a328d37dc43e39e09ef30b88077c5f55efcfe5
dvohra09 <dvohra09@yahoo.com>
2025-03-29 19:21:21 -0700

    Update Book.java

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tag-order-example/Book.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tag-order-example/Book.java
@@ -1,7 +1,9 @@
+package com.baeldung.springdoc.tag-order-example;
+
 public class Book {
-	private long id;
+    private long id;
 	
-	public void setId(long id) {
-		this.id = id;
-	}
+    public void setId(long id) {
+        this.id = id;
+    }
 }
