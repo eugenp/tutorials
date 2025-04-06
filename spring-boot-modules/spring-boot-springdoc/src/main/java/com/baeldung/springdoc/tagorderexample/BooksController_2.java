99d768fa92f5b02b6ba9f792be9763972a8ffb8e
dvohra09 <dvohra09@yahoo.com>
2025-04-05 17:58:31 -0700

    Create BooksController_2.java

---
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tagorderexample/BooksController_2.java
@@ -0,0 +1,70 @@
+package com.baeldung.springdoc.tagorderexample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@RestController
+@Validated
+@OpenAPIDefinition(tags = { @Tag(name = "create", description = "Add book to inventory"),
+		@Tag(name = "delete", description = "Delete book from inventory"),
+		@Tag(name = "find", description = "Find book from inventory"),
+		@Tag(name = "update", description = "Update book in inventory"),
+		@Tag(name = "createBook", description = "Add book to inventory"),
+		@Tag(name = "deleteBook", description = "Delete book from inventory"),
+		@Tag(name = "findBook", description = "Find book from inventory"),
+		@Tag(name = "updateBook", description = "Update book in inventory") })
+public class BooksController_2 {
+
+	@Tag(name = "create")
+	@Tag(name = "createBook")
+	@PostMapping(path = "/book")
+	@io.swagger.v3.oas.annotations.parameters.RequestBody(required = true)
+	public Book book(@Valid @RequestBody Book book) {
+
+		return book;
+	}
+
+	@Tag(name = "find")
+	@Tag(name = "findBook")
+	@GetMapping(path = "/findBookById")
+	public List<Book> findById(@RequestParam(name = "id", required = true) @NotNull @NotBlank @Size(max = 10) long id) {
+		List<Book> bookList = new ArrayList<>();
+		Book book = new Book();
+
+		book.setId(1);
+		bookList.add(book);
+		return bookList;
+	}
+
+	@Tag(name = "delete")
+	@Tag(name = "deleteBook")
+	@DeleteMapping(path = "/deleteBookById")
+	public long deleteById(@RequestParam(name = "id", required = true) @NotNull @NotBlank @Size(max = 10) long id) {
+
+		return id;
+	}
+
+	@Tag(name = "update")
+	@Tag(name = "updateBook")
+	@PutMapping(path = "/updateBookById")
+	public long updateById(@RequestParam(name = "id", required = true) @NotNull @NotBlank @Size(max = 10) long id) {
+		return id;
+	}
+}
