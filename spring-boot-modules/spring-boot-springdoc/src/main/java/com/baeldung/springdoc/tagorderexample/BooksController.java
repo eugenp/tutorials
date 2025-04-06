f383486104f6145d3048986170171ef8f44adbfd
dvohra09 <dvohra09@yahoo.com>
2025-04-05 17:55:44 -0700

    Update BooksController.java

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tagorderexample/BooksController.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/springdoc/tagorderexample/BooksController.java
@@ -2,72 +2,68 @@ package com.baeldung.springdoc.tagorderexample;
 
 import java.util.ArrayList;
 import java.util.List;
- 
+
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import jakarta.validation.Valid;
+
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @Validated
-/**@OpenAPIDefinition(tags = {
-     @Tag(name = "books", description = "Books related tag"),
-     @Tag(name = "create", description = "Add book to inventory"),
-     @Tag(name = "delete", description = "Delete book from inventory"),
-     @Tag(name = "find", description = "Find book from inventory"),
-     @Tag(name = "update", description = "Update book in inventory"),
-     @Tag(name = "createBook", description = "Add book to inventory"),
-     @Tag(name = "deleteBook", description = "Delete book from inventory"),
-     @Tag(name = "findBook", description = "Find book from inventory"),
-     @Tag(name = "updateBook", description = "Update book in inventory")
-})**/
-@Tag(name = "books", description = "Books related tag")
+@Tag(name = "tag_at_class_level", description = "Books related class level tag")
 public class BooksController {
 
     @Tag(name = "create")
-    @Tag(name = "books")
+    @Tag(name = "common_tag_at_method_level")
     @Tag(name = "createBook")
-    @RequestMapping(path = "/book", method = RequestMethod.POST)
+    @PostMapping(path = "/book")
     @io.swagger.v3.oas.annotations.parameters.RequestBody(required = true)
     public Book book(@Valid @RequestBody Book book) {
+
         return book;
     }
 
     @Tag(name = "find")
-    @Tag(name = "books")
-    @Tag(name = "findBook")
-    @RequestMapping(path = "/findBookById", method = RequestMethod.GET)
-    public List<Book> findById(@RequestParam(name = "id", required = true) @NotNull @NotBlank @Size(max = 10) int id) {
-        List<Book> bookList = new ArrayList<>();
+    @Tag(name = "common_tag_at_method_level")
+    @Tag(name = "findBook", description = "Find Books related tag")
+    @GetMapping(path = "/findBookById")
+    public List findById(@RequestParam(name = "id", required = true) 
+      @NotNull @NotBlank @Size(max = 10) long id) {
+        List bookList = new ArrayList&lt;&gt;();
 	Book book = new Book();
+
 	book.setId(1);
 	bookList.add(book);
 	return bookList;
     }
 
-    @Tag(name = "delete")
-    @Tag(name = "books")
+    @Tag(name = "delete") 
+    @Tag(name = "common_tag_at_method_level")
     @Tag(name = "deleteBook")
-    @RequestMapping(path = "/deleteBookById", method = RequestMethod.DELETE)
-    public long deleteById(@RequestParam(name = "id", required = true) @NotNull @NotBlank @Size(max = 10) long id) {
+    @DeleteMapping(path = "/deleteBookById")
+    public long deleteById(@RequestParam(name = "id", required = true) 
+      @NotNull @NotBlank @Size(max = 10) long id) {
+
         return id;
     }
 
     @Tag(name = "update")
-    @Tag(name = "books")
+    @Tag(name = "common_tag_at_method_level")
     @Tag(name = "updateBook")
-    @RequestMapping(path = "/updateBookById", method = RequestMethod.PUT)
-    public long updateById(@RequestParam(name = "id", required = true) @NotNull @NotBlank @Size(max = 10) long id) {
+    @PutMapping(path = "/updateBookById")
+    public long updateById(@RequestParam(name = "id", required = true) 
+      @NotNull @NotBlank @Size(max = 10) long id) {
         return id;
-    }	 
+    }
 }
