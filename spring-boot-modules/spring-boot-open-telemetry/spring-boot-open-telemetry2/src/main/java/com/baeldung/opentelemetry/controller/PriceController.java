0f15676ee5d51db96fde61259d9a33e9bb999397
saikat <saikatcse03@gmail.com>
2025-04-07 20:59:57 +0200

    fixed lint issues

--- spring-boot-modules/spring-boot-open-telemetry/spring-boot-open-telemetry2/src/main/java/com/baeldung/opentelemetry/controller/PriceController.java
+++ spring-boot-modules/spring-boot-open-telemetry/spring-boot-open-telemetry2/src/main/java/com/baeldung/opentelemetry/controller/PriceController.java
@@ -2,15 +2,12 @@ package com.baeldung.opentelemetry.controller;
 
 import com.baeldung.opentelemetry.model.Price;
 import com.baeldung.opentelemetry.repository.PriceRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
 
 @RestController
 public class PriceController {
@@ -25,7 +22,7 @@ public class PriceController {
     }
 
     @GetMapping(path = "/price/{id}")
-    public Price getPrice(@PathVariable("id") long productId, HttpServletRequest request) {
+    public Price getPrice(@PathVariable("id") long productId) {
         LOGGER.info("Getting Price details for Product Id {}", productId);
         return priceRepository.getPrice(productId);
   }
