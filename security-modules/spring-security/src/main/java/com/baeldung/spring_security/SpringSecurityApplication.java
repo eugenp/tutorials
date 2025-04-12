2a588771f05e3c45cd40526eb58d6fa55aaa7b00
oscarramadhan <oscarramadhan.or@gmail.com>
2025-04-13 00:57:45 +0700

    fix formatting

--- security-modules/spring-security/src/main/java/com/baeldung/spring_security/SpringSecurityApplication.java
+++ security-modules/spring-security/src/main/java/com/baeldung/spring_security/SpringSecurityApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
 	}
-
 }
