0fe884751c8408d4b2b1e6258244448f512e3292
Gaetano Piazzolla <gae.piaz@gmail.com>
2025-04-15 20:06:51 +0200

    BAEL-8401 | using gradle env var in java

---
+++ gradle-modules/gradle-java-config/src/test/java/com/baeldung/gradle/config/GradleVarInJavaUnitTest.java
@@ -0,0 +1,37 @@
+package com.baeldung.gradle.config;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.junit.jupiter.api.Test;
+
+class GradleVarInJavaUnitTest {
+
+    @Test
+    void whenUsingGenerateBuildConfigTask_thenBuildConfigIsGeneratedWithCorrectValue() {
+        assertEquals("1.2.3", BuildConfig.MY_VERSION);
+    }
+
+    @Test
+    void whenUsingGenerateProperties_thenPropertiesFileIsGeneratedWithCorrectValue() throws IOException {
+        Properties props = new Properties();
+        props.load(Thread.currentThread()
+            .getContextClassLoader()
+            .getResourceAsStream("version.properties"));
+        String version = props.getProperty("MY_VERSION");
+        assertEquals("1.2.3", version);
+    }
+
+    @Test
+    void whenUsingJvmArg_thenValueReadCorrectly() {
+        assertEquals("1.2.3", System.getProperty("MY_VERSION"));
+    }
+
+    @Test
+    void whenUsingEmvArg_thenValueReadCorrectly() {
+        assertEquals("1.2.3", System.getenv("MY_VERSION"));
+    }
+
+}
