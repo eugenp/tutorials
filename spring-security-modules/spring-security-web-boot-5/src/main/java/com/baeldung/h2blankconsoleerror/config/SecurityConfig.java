8fcca96d781761f16c555d2dfee05202c9dfd26b
michaelin007 <michaelolayemi28@gmail.com>
2025-04-25 12:59:36 +0100

    https://jira.baeldung.com/browse/BAEL-8732

--- spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/h2blankconsoleerror/config/SecurityConfig.java
+++ spring-security-modules/spring-security-web-boot-5/src/main/java/com/baeldung/h2blankconsoleerror/config/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.baeldung.h2blankconsoleerror.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -13,12 +14,10 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
-            .cors(AbstractHttpConfigurer::disable)
             .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-            .authorizeHttpRequests(auth -> auth.requestMatchers("/h2-console/**")
-                .permitAll()
-                .anyRequest()
-                .authenticated());
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().authenticated())
+            .formLogin(withDefaults());
 
         return http.build();
     }
