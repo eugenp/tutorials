f0bc73369398b98791690e49681f572029465495
programenth <139003443+programenth@users.noreply.github.com>
2025-04-26 09:13:05 +0000

    organizing files

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggertags/DemoApplication.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggertags/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.baeldung.swaggertags.demo;
+package com.baeldung.swaggertags;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggertags/OrderController.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggertags/OrderController.java
@@ -1,4 +1,4 @@
-package com.baeldung.swaggertags.demo;
+package com.baeldung.swaggertags;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
