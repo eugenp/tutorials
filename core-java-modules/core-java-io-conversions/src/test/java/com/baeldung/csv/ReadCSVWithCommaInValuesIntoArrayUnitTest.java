a22dda810f23d8e5d4aea9a396ebce4e9479bd55
Deepak-Vohra <dvohra10@yahoo.com>
2025-04-26 18:57:30 -0700

    Update ReadCSVWithCommaInValuesIntoArrayUnitTest.java

--- core-java-modules/core-java-io-conversions/src/test/java/com/baeldung/csv/ReadCSVWithCommaInValuesIntoArrayUnitTest.java
+++ core-java-modules/core-java-io-conversions/src/test/java/com/baeldung/csv/ReadCSVWithCommaInValuesIntoArrayUnitTest.java
@@ -23,6 +23,7 @@ import com.opencsv.CSVReader;
 public class ReadCSVWithCommaInValuesIntoArrayUnitTest {
     public static final String COMMA_DELIMITER = " , ";
     public static final String CSV_FILE = "src/test/resources/book2.csv";
+    public static final String CSV_FILE_OpenCSV = "src/test/resources/book3.csv";
 
     public static final List<List<String>> EXPECTED_ARRAY = Collections.unmodifiableList(new ArrayList<List<String>>() {
         {
@@ -41,6 +42,43 @@ public class ReadCSVWithCommaInValuesIntoArrayUnitTest {
         }
     });
 
+    public static final List<List<String>> EXPECTED_ARRAY_OpenCSV = Collections.unmodifiableList(new ArrayList<List<String>>() {
+        {
+            add(new ArrayList<String>() {
+                {
+                    add("Kom, Mary");
+                    add("Unbreakable");
+                }
+            });
+            add(new ArrayList<String>() {
+                {
+                    add("Isapuari, Kapil");
+                    add("Farishta");
+                }
+            });
+        }
+    });
+
+
+    @Test
+    public void givenCSVFileWithCommaInValues_whenOpencsv_thenContentsAsExpected() throws IOException {
+        List<List<String>> records = new ArrayList<List<String>>();
+        try (CSVReader csvReader = new CSVReader(new FileReader(CSV_FILE_OpenCSV));) {
+            String[] values = null;
+            while ((values = csvReader.readNext()) != null) {
+                records.add(Arrays.asList(values));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < EXPECTED_ARRAY_OpenCSV.size(); i++) {
+            Assert.assertArrayEquals(EXPECTED_ARRAY_OpenCSV.get(i)
+                .toArray(),
+                records.get(i)
+                    .toArray());
+        }
+    }
+    
     @Test
     public void givenCSVFileWithCommaInValues_whenBufferedReader_thenContentsAsExpected() throws IOException {
         List<List<String>> records = new ArrayList<List<String>>();
