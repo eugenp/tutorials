c122002f0eaf1d661ac7282cabbc4037c38474e2
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-04-29 14:15:22 +0530

    add test cases

--- spring-cloud-modules/spring-cloud-aws-v3/src/test/java/com/baeldung/spring/cloud/aws/dynamodb/UserCRUDLiveTest.java
+++ spring-cloud-modules/spring-cloud-aws-v3/src/test/java/com/baeldung/spring/cloud/aws/dynamodb/UserCRUDLiveTest.java
@@ -7,7 +7,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.annotation.DirtiesContext;
+import software.amazon.awssdk.enhanced.dynamodb.Expression;
 import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.ScanEnhancedRequest;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -17,5 +23,98 @@ class UserCRUDLiveTest {
 
     @Autowired
     private DynamoDbTemplate dynamoDbTemplate;
+
+    @Test
+    void whenUserSaved_thenRecordCreatedInDynamoDB() {
+        User user = Instancio.create(User.class);
+
+        dynamoDbTemplate.save(user);
+
+        Key partitionKey = Key.builder().partitionValue(user.getId().toString()).build();
+        User retrievedUser = dynamoDbTemplate.load(partitionKey, User.class);
+        assertThat(retrievedUser)
+            .isNotNull()
+            .usingRecursiveComparison()
+            .isEqualTo(user);
+    }
+
+    @Test
+    void whenUserUpdated_thenRecordUpdatedInDynamoDB() {
+        User user = Instancio.create(User.class);
+        dynamoDbTemplate.save(user);
+
+        String updatedName = RandomString.make();
+        String updatedEmail = RandomString.make();
+        user.setName(updatedName);
+        user.setEmail(updatedEmail);
+        dynamoDbTemplate.update(user);
+
+        Key partitionKey = Key.builder().partitionValue(user.getId().toString()).build();
+        User updatedUser = dynamoDbTemplate.load(partitionKey, User.class);
+        assertThat(updatedUser)
+            .isNotNull();
+        assertThat(updatedUser.getName())
+            .isEqualTo(updatedName);
+        assertThat(updatedUser.getEmail())
+            .isEqualTo(updatedEmail);
+    }
+
+    @Test
+    void whenUserDeleted_thenRecordRemovedFromDynamoDB() {
+        User user = Instancio.create(User.class);
+        dynamoDbTemplate.save(user);
+
+        dynamoDbTemplate.delete(user);
+
+        Key partitionKey = Key.builder().partitionValue(user.getId().toString()).build();
+        User deletedUser = dynamoDbTemplate.load(partitionKey, User.class);
+        assertThat(deletedUser)
+            .isNull();
+    }
+
+    @Test
+    @DirtiesContext(methodMode = DirtiesContext.MethodMode.BEFORE_METHOD)
+    void whenUserEntityScanned_thenAllSavedRecordsReturned() {
+        int numberOfUsers = 10;
+        for (int i = 0; i < numberOfUsers; i++) {
+            User user = Instancio.create(User.class);
+            dynamoDbTemplate.save(user);
+        }
+
+        List<User> retrievedUsers = dynamoDbTemplate
+            .scanAll(User.class)
+            .items()
+            .stream()
+            .toList();
+
+        assertThat(retrievedUsers.size())
+            .isEqualTo(numberOfUsers);
+    }
+
+    @Test
+    void whenUserQueriedByEmail_thenCorrectRecordReturned() {
+        User user = Instancio.create(User.class);
+        dynamoDbTemplate.save(user);
+
+        Expression expression = Expression.builder()
+            .expression("#email = :email")
+            .putExpressionName("#email", "email")
+            .putExpressionValue(":email", AttributeValue.builder().s(user.getEmail()).build())
+            .build();
+        ScanEnhancedRequest scanRequest = ScanEnhancedRequest
+            .builder()
+            .filterExpression(expression)
+            .build();
+        User retrievedUser = dynamoDbTemplate.scan(scanRequest, User.class)
+            .items()
+            .stream()
+            .findFirst()
+            .get();
+
+        assertThat(retrievedUser)
+            .isNotNull()
+            .usingRecursiveComparison()
+            .isEqualTo(user);
+    }
 
 }
