98a98ccf11f3657a8b65775b6fb31d8d32f09ea3
oscarramadhan <oscarramadhan.or@gmail.com>
2025-05-02 01:30:52 +0700

    fix to 4 spaces instead of tab

--- security-modules/spring-security/src/main/java/com/baeldung/springsecurity/SpringSecurityApplication.java
+++ security-modules/spring-security/src/main/java/com/baeldung/springsecurity/SpringSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityApplication.class, args);
+    }
 }
