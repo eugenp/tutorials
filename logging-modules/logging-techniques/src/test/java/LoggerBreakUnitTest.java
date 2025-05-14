920bd4579965eb677044d51a04e494b9701f9049
Neetika Khandelwal <kwal.neetika2398@gmail.com>
2025-05-14 22:53:02 +0530

    BAEL-9172

--- logging-modules/logging-techniques/src/test/java/LoggerBreakUnitTest.java
+++ logging-modules/logging-techniques/src/test/java/LoggerBreakUnitTest.java
@@ -28,7 +28,7 @@ class LoggerBreakUnitTest {
     }
 
     @Test
-    void testLogWithEmptyLine() {
+    void givenLogMessage_whenEmptyLineLogged_thenLogContainsBlankEntry() {
         logger.info("Start process");
         logger.info("");
 
@@ -42,7 +42,7 @@ class LoggerBreakUnitTest {
     }
 
     @Test
-    void testLogWithSystemLineSeparator() {
+    void givenLogMessage_whenUsingSystemLineSeparator_thenLogContainsPlatformSpecificLineBreak() {
         logger.info("Processing done{}", System.lineSeparator());
 
         List<ILoggingEvent> logs = listAppender.list;
@@ -54,8 +54,8 @@ class LoggerBreakUnitTest {
     }
 
     @Test
-    void testLogWithLineBreakConcatenation() {
-        logger.info("Result saved" + System.lineSeparator());
+    void givenLogMessage_whenLineBreakIsConcatenated_thenLogSplitsCorrectly() {
+        logger.info("Processing done" + System.lineSeparator());
 
         List<ILoggingEvent> logs = listAppender.list;
 
@@ -63,6 +63,26 @@ class LoggerBreakUnitTest {
         assertTrue(logs.get(0)
             .getFormattedMessage()
             .endsWith(System.lineSeparator()));
+    }
+
+    @Test
+    void givenLineSeparator_whenAppendedToLogMessages_thenLogEndsWithCorrectLineBreak() {
+        String newline = System.getProperty("line.separator");
+
+        logger.info("Processing started" + newline);
+        logger.info("Processing ended");
+
+        List<ILoggingEvent> logs = listAppender.list;
+
+        assertEquals(2, logs.size());
+
+        String firstMessage = logs.get(0)
+            .getFormattedMessage();
+        String secondMessage = logs.get(1)
+            .getFormattedMessage();
+
+        assertTrue(firstMessage.endsWith(newline), "First message should end with the system line separator");
+        assertEquals("Processing ended", secondMessage);
     }
 }
 
