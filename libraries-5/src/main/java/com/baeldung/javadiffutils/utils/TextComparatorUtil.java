091a706807256eb8b98c9b5e87d047b323cb2931
sverma1-godaddy <sverma1@godaddy.com>
2025-05-15 23:18:19 +0530

    [BAEL-9236] by sgrverma23: comparing text using java-diff-utils

---
+++ libraries-5/src/main/java/com/baeldung/javadiffutils/utils/TextComparatorUtil.java
@@ -0,0 +1,11 @@
+package com.example.diff;
+
+import com.github.difflib.DiffUtils;
+import com.github.difflib.patch.Patch;
+import java.util.List;
+
+public class TextComparatorUtil {
+    public Patch<String> compare(List<String> original, List<String> revised) {
+        return DiffUtils.diff(original, revised);
+    }
+}
