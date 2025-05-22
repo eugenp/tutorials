a905db70cde556bcebb414cdd66ef717a22173c5
Manfred Ng <manfred.ng@gmail.com>
2025-05-22 22:26:51 +0100

    BAEL-9285: A Guide to Embeddings Model API in Spring AI - cleanup

--- spring-ai-3/src/main/java/com/baeldung/springai/embeddings/Application.java
+++ spring-ai-3/src/main/java/com/baeldung/springai/embeddings/Application.java
@@ -1,9 +1,6 @@
 package com.baeldung.springai.embeddings;
 
 import org.springframework.ai.autoconfigure.chat.client.ChatClientAutoConfiguration;
-import org.springframework.ai.autoconfigure.ollama.OllamaAutoConfiguration;
-import org.springframework.ai.autoconfigure.vectorstore.chroma.ChromaVectorStoreAutoConfiguration;
-import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
 import org.springframework.ai.model.openai.autoconfigure.OpenAiAudioSpeechAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,14 +9,11 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication(exclude = {
     ChatClientAutoConfiguration.class,
-    ChromaVectorStoreAutoConfiguration.class,
     MongoAutoConfiguration.class,
     MongoDataAutoConfiguration.class,
     org.springframework.ai.autoconfigure.vectorstore.mongo.MongoDBAtlasVectorStoreAutoConfiguration.class,
     org.springframework.ai.vectorstore.mongodb.autoconfigure.MongoDBAtlasVectorStoreAutoConfiguration.class,
-    OllamaAutoConfiguration.class,
-    OpenAiAudioSpeechAutoConfiguration.class,
-    PgVectorStoreAutoConfiguration.class})
+    OpenAiAudioSpeechAutoConfiguration.class})
 class Application {
 
     public static void main(String[] args) {
