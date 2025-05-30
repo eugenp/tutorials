e9651ec92445bfb4c3f3ef47fab72c81a93a8374
panos-kakos <kakos.programmer@gmail.com>
2025-05-30 19:47:08 +0300

    [JAVA-46871] Created new module core-java-lang-oop-methods-2 and moved code from core-java-lang-oop-methods

--- core-java-modules/core-java-lang-oop-methods-2/src/test/java/com/baeldung/methods/VehicleProcessorUnitTest.java
+++ core-java-modules/core-java-lang-oop-methods-2/src/test/java/com/baeldung/methods/VehicleProcessorUnitTest.java
@@ -1,8 +1,8 @@
 package com.baeldung.methods;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class VehicleProcessorUnitTest {
 
