46ac0084b84ec6b44ea65016fad6beb377ce6eb4
Kent <kent.yuan@Gmail.com>
2025-05-31 21:03:12 +0200

    [spring-jdbc-col-count] resolving runtime error when using queryForList()

---
+++ persistence-modules/spring-jdbc-2/src/main/resources/com/baeldung/spring/jdbc/queryforlist/init-student-data.sql
@@ -0,0 +1,11 @@
+CREATE TABLE STUDENT_TBL
+(
+    ID    int NOT NULL PRIMARY KEY,
+    NAME  varchar(255),
+    MAJOR varchar(255)
+);
+
+INSERT INTO STUDENT_TBL VALUES (1, 'Kai', 'Computer Science');
+INSERT INTO STUDENT_TBL VALUES (2, 'Eric', 'Computer Science');
+INSERT INTO STUDENT_TBL VALUES (3, 'Kevin', 'Banking');
+INSERT INTO STUDENT_TBL VALUES (4, 'Liam', 'Law');
