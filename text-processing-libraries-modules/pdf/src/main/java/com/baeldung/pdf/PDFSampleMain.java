16476911bc16a2ae1cf27b7ce681d138453ce7bc
dvohra09 <dvohra09@yahoo.com>
2025-05-31 23:06:38 -0700

    BAEL-9302 Set the Table Cell Width in PDF Using iText in Java (#18579)
    * BAEL-9302 : Update PDFSampleMain.java to add method setAbsoluteColumnWidths
    
    * Update PDFSampleMain.java
    
    * Update PDFSampleMain.java
    
    * BAEL-9302: Update PDFSampleMain.java to add method for relative widths
    
    * Update PDFSampleMain.java
    
    * Update PDFSampleMain.java
    
    * Update pom.xml

--- text-processing-libraries-modules/pdf/src/main/java/com/baeldung/pdf/PDFSampleMain.java
+++ text-processing-libraries-modules/pdf/src/main/java/com/baeldung/pdf/PDFSampleMain.java
@@ -30,6 +30,9 @@ public class PDFSampleMain {
 
             PdfPTable table = new PdfPTable(3);
             addTableHeader(table);
+            setAbsoluteColumnWidths(table);
+            //setAbsoluteColumnWidthsInTableWidth(table);
+            //setRelativeColumnWidths(table);
             addRows(table);
             addCustomRows(table);
 
@@ -52,6 +55,24 @@ public class PDFSampleMain {
         });
     }
 
+    private static void setAbsoluteColumnWidths(PdfPTable table) {
+       table.setTotalWidth(500); // Sets total table width to 500 points
+       table.setLockedWidth(true);
+       float[] columnWidths = {100f, 200f, 200f}; // Defines three columns with absolute widths
+       table.setWidths(columnWidths);
+    }
+
+    private static void setAbsoluteColumnWidthsInTableWidth(PdfPTable table) {
+       table.setTotalWidth(new float[] {72f, 144f, 216f}); // First column 1 inch, second 2 inches, third 3 inches 
+       table.setLockedWidth(true);
+    }
+
+    private static void setRelativeColumnWidths(PdfPTable table) {
+        // Set column widths (relative)
+        table.setWidths(new float[] {1, 2, 1});
+        table.setWidthPercentage(80); // Table width as 80% of page width
+    }
+    
     private static void addRows(PdfPTable table) {
         table.addCell("row 1, col 1");
         table.addCell("row 1, col 2");
