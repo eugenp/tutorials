b279d06ec5cb9249523510fc108434f7074e3a13
Bipinkumar27 <sayy2bipin@gmail.com>
2025-06-04 23:40:49 +0530

    JAVA-46416:  Changes made for restoring https://www.baeldung.com/soap-keycloak code

--- spring-boot-modules/spring-boot-keycloak-2/src/main/java/com/baeldung/keycloak/keycloaksoap/KeycloakRoleConverter.java
+++ spring-boot-modules/spring-boot-keycloak-2/src/main/java/com/baeldung/keycloak/keycloaksoap/KeycloakRoleConverter.java
@@ -12,34 +12,15 @@ public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedA
 
     @Override
     public Collection<GrantedAuthority> convert(Jwt jwt) {
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        // Extract client roles with ROLE_ prefix
-        authorities.addAll(extractClientRoles(jwt));
-
-        return authorities;
-    }
-
-
-
-    private Collection<GrantedAuthority> extractClientRoles(Jwt jwt) {
         Map<String, Object> resourceAccess = jwt.getClaim("resource_access");
-        if (resourceAccess == null || resourceAccess.isEmpty()) {
-            return Collections.emptyList();
-        }
         // Replace this with your actual client ID from Keycloak
         String clientId = "baeldung-soap-services";
 
         Map<String, Object> client = (Map<String, Object>) resourceAccess.get(clientId);
-        if (client == null || client.isEmpty()) {
-            return Collections.emptyList();
-        }
-        @SuppressWarnings("unchecked")
         List<String> roles = (List<String>) client.get("roles");
-        if (roles == null) {
-            return Collections.emptyList();
-        }
+
         return roles.stream()
-            .map(role -> new SimpleGrantedAuthority("ROLE_" + role)) // Add ROLE_ prefix here
+            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
             .collect(Collectors.toList());
     }
 }
--- spring-boot-modules/spring-boot-keycloak-2/src/main/java/com/baeldung/keycloak/keycloaksoap/KeycloakSecurityConfig.java
+++ spring-boot-modules/spring-boot-keycloak-2/src/main/java/com/baeldung/keycloak/keycloaksoap/KeycloakSecurityConfig.java
@@ -3,15 +3,12 @@ package com.baeldung.keycloak.keycloaksoap;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
