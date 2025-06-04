a14c07b7dbd7133ced3a431a2392bca076a35469
dvohra09 <dvohra09@yahoo.com>
2025-06-04 04:49:22 -0700

    Update PDFSampleMain.java

--- text-processing-libraries-modules/pdf/src/main/java/com/baeldung/pdf/PDFSampleMain.java
+++ text-processing-libraries-modules/pdf/src/main/java/com/baeldung/pdf/PDFSampleMain.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 import com.itextpdf.text.BadElementException;
+import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.Element;
@@ -23,25 +24,25 @@ public class PDFSampleMain {
         
         try {
             
-            Document document = new Document();
-            PdfWriter.getInstance(document, new FileOutputStream("iTextTable.pdf"));
+                Document document = new Document();
+                PdfWriter.getInstance(document, new FileOutputStream("iTextTable.pdf"));
 
-            document.open();
+                document.open();
 
-            PdfPTable table = new PdfPTable(3);
-            addTableHeader(table);
-            setAbsoluteColumnWidths(table);
-            //setAbsoluteColumnWidthsInTableWidth(table);
-            //setRelativeColumnWidths(table);
-            addRows(table);
-            addCustomRows(table);
+                PdfPTable table = new PdfPTable(3);
+                addTableHeader(table);
+                setAbsoluteColumnWidths(table);
+                //setAbsoluteColumnWidthsInTableWidth(table);
+                //setRelativeColumnWidths(table);
+                addRows(table);
+                addCustomRows(table);
 
-            document.add(table);
-            document.close();
+                document.add(table);
+                document.close();
             
         } catch (Exception e) {
-            e.printStackTrace();
-        }
+              e.printStackTrace();
+          }
     }
 
     private static void addTableHeader(PdfPTable table) {
@@ -56,21 +57,36 @@ public class PDFSampleMain {
     }
 
     private static void setAbsoluteColumnWidths(PdfPTable table) {
-       table.setTotalWidth(500); // Sets total table width to 500 points
-       table.setLockedWidth(true);
-       float[] columnWidths = {100f, 200f, 200f}; // Defines three columns with absolute widths
-       table.setWidths(columnWidths);
-    }
+       try {
+               table.setTotalWidth(500); // Sets total table width to 500 points
+               table.setLockedWidth(true);
+               float[] columnWidths = {100f, 200f, 200f}; // Defines three columns with absolute widths
+               table.setWidths(columnWidths);
+           
+       } catch (DocumentException e) {
+             e.printStackTrace();  
+         }  
+    }    
 
     private static void setAbsoluteColumnWidthsInTableWidth(PdfPTable table) {
-       table.setTotalWidth(new float[] {72f, 144f, 216f}); // First column 1 inch, second 2 inches, third 3 inches 
-       table.setLockedWidth(true);
+        try {
+                table.setTotalWidth(new float[] {72f, 144f, 216f}); // First column 1 inch, second 2 inches, third 3 inches 
+                table.setLockedWidth(true);
+           
+        } catch (DocumentException e) {
+              e.printStackTrace();  
+          }  
     }
 
     private static void setRelativeColumnWidths(PdfPTable table) {
-        // Set column widths (relative)
-        table.setWidths(new float[] {1, 2, 1});
-        table.setWidthPercentage(80); // Table width as 80% of page width
+        try {
+                // Set column widths (relative)
+                table.setWidths(new float[] {1, 2, 1});
+                table.setWidthPercentage(80); // Table width as 80% of page width
+           
+         } catch (DocumentException e) {
+               e.printStackTrace();  
+           }  
     }
     
     private static void addRows(PdfPTable table) {
