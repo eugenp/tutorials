d70412124f9e9d6ac8eb3f6eb03ee6139814396e
Deepak Vohra <dvohra10@yahoo.com>
2025-06-05 11:00:57 -0700

    Update ProcessWaitTest.java

--- core-java-modules/core-java-os-2/src/test/java/com/baeldung/java9/process/ProcessWaitTest.java
+++ core-java-modules/core-java-os-2/src/test/java/com/baeldung/java9/process/ProcessWaitTest.java
@@ -1,3 +1,5 @@
+package com.baeldung.java9.process;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
