381a292f406f5756e8e5e2cb42f084b18592e041
programenth <139003443+programenth@users.noreply.github.com>
2025-06-07 07:35:28 +0000

    moving SwaggerYML to main

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggeryml/SwaggerymlApplication.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggeryml/SwaggerymlApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class SwaggerymlApplication {
+public class SwaggerYMLApplication {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(SwaggerymlApplication.class)
+		new SpringApplicationBuilder(SwaggerYMLApplication.class)
 		    .properties("spring.config.name=application-yml")
 		    .run(args);
 	}
