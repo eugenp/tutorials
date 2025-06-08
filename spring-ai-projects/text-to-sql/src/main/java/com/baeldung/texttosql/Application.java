0f995f0c25e8da70f126d7d84581022672d0de48
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-06-08 18:48:42 +0530

    init project structure

---
+++ spring-ai-projects/text-to-sql/src/main/java/com/baeldung/texttosql/Application.java
@@ -0,0 +1,13 @@
+package com.baeldung.texttosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
