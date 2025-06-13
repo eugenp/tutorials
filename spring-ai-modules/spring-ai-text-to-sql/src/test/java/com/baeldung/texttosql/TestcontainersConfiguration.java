2cef572adac2ccb805a13a9dcac01c7feee23ee3
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-06-13 12:01:48 +0530

    downgrade MySQL version

--- spring-ai-modules/spring-ai-text-to-sql/src/test/java/com/baeldung/texttosql/TestcontainersConfiguration.java
+++ spring-ai-modules/spring-ai-text-to-sql/src/test/java/com/baeldung/texttosql/TestcontainersConfiguration.java
@@ -8,10 +8,14 @@ import org.testcontainers.containers.MySQLContainer;
 @TestConfiguration(proxyBeanMethods = false)
 class TestcontainersConfiguration {
 
+    /**
+     * Downgraded MySQL version since the container is unable to run when using the latest version.
+     * The relevant bug in testcontainers can be tracked here: https://github.com/testcontainers/testcontainers-java/issues/10184
+     */
     @Bean
     @ServiceConnection
     MySQLContainer mySQLContainer() {
-        return new MySQLContainer("mysql:9.3.0");
+        return new MySQLContainer("mysql:9.2.0");
     }
 
 }
