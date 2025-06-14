7497c74fdc41db8a262f7eb40edd204787752641
achraftt <achraf.taitai1@gmail.com>
2025-06-15 01:19:36 +0200

    BAEL-9319: Parsing the Response Body When the HTTP Request Has Return Status 401 in Java

--- spring-web-modules/spring-resttemplate/src/main/java/com/baeldung/resttemplate/RestTemplateConfigurationApplication.java
+++ spring-web-modules/spring-resttemplate/src/main/java/com/baeldung/resttemplate/RestTemplateConfigurationApplication.java
@@ -1,8 +1,12 @@
 package com.baeldung.resttemplate;
 
+import com.baeldung.resttemplate.web.handler.RestTemplateResponseErrorHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -10,5 +14,12 @@ public class RestTemplateConfigurationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestTemplateConfigurationApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .errorHandler(new RestTemplateResponseErrorHandler())
+                .build();
     }
 }
