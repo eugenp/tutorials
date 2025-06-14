f56e06338857f057e15fa7d6f568dfe75f908467
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-06-14 12:27:52 -0600

    Polish Formatting

--- json-modules/json/src/test/java/com/baeldung/jsonjava/ParseJsonBooleanUnitTest.java
+++ json-modules/json/src/test/java/com/baeldung/jsonjava/ParseJsonBooleanUnitTest.java
@@ -1,23 +1,26 @@
 package com.baeldung.jsonjava;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.json.JSONObject;
+
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class ParseJsonBooleanUnitTest {
 
-   private final String json = """
-                               { 
-                                   "name": "lorem ipsum",
-                                   "active": true,
-                                   "id": 1
-                               }
-                               """;
-    
+    private final String json = """
+        {
+            "name": "lorem ipsum",
+            "active": true,
+            "id": 1
+        }
+        """;
+
     @Test
     void givenJSONString_whenParsed_thenCorrectBooleanValueReturned() {
         JSONObject jsonObject = new JSONObject(json);
@@ -28,12 +31,12 @@ public class ParseJsonBooleanUnitTest {
     @Test
     void givenJSONWithBooleanAs0Or1_whenParsed_thenCorrectBooleanValueReturned() {
         String json = """
-                      {
-                          "name": "lorem ipsum",
-                          "active": 1,
-                          "id": 1
-                      }
-                      """;
+            {
+                "name": "lorem ipsum",
+                "active": 1,
+                "id": 1
+            }
+            """;
         JSONObject jsonObject = new JSONObject(json);
         assertThat(jsonObject.getInt("active")).isEqualTo(1);
     }
@@ -46,6 +49,6 @@ public class ParseJsonBooleanUnitTest {
             assertTrue(value == 1);
         } else if (activeObject instanceof Boolean value) {
             assertTrue(value);
-          }  
+        }
     }
 }
