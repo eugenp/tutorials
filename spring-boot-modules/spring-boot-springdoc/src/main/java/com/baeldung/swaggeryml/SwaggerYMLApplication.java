42eeeda11be6d68cdeca70de4b77bc52f207ecc5
programenth <139003443+programenth@users.noreply.github.com>
2025-06-14 05:15:20 +0000

    formating code

--- spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggeryml/SwaggerYMLApplication.java
+++ spring-boot-modules/spring-boot-springdoc/src/main/java/com/baeldung/swaggeryml/SwaggerYMLApplication.java
@@ -1,16 +1,13 @@
 package com.baeldung.swaggeryml;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class SwaggerYMLApplication {
-
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(SwaggerYMLApplication.class)
-		  .properties("spring.config.name=application-yml")
-		  .run(args);
-	}
-
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SwaggerYMLApplication.class)
+          .properties("spring.config.name=application-yml")
+          .run(args);
+    }
 }
