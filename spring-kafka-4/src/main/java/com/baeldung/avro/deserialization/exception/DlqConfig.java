c509aa3b7557801b2d2fbf7351ea42f15ba11b76
etrandafir93 <emanueltrandafir1993@gmail.com>
2025-06-15 17:28:22 +0300

    BAEL-7164: use service connection

--- spring-kafka-4/src/main/java/com/baeldung/avro/deserialization/exception/DlqConfig.java
+++ spring-kafka-4/src/main/java/com/baeldung/avro/deserialization/exception/DlqConfig.java
@@ -4,11 +4,10 @@ import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 
@@ -26,8 +25,7 @@ class DlqConfig {
     }
 
     @Bean("bytesKafkaTemplate")
-    KafkaTemplate<?, ?> bytesTemplate(KafkaProperties kafkaProperties) {
-        var kafkaProducerFactory = new DefaultKafkaProducerFactory<>(kafkaProperties.buildProducerProperties());
+    KafkaTemplate<?, ?> bytesTemplate(ProducerFactory<?, ?> kafkaProducerFactory) {
         return new KafkaTemplate<>(kafkaProducerFactory, Map.of(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName()));
     }
 
--- spring-kafka-4/src/test/java/com/baeldung/avro/deserialization/exception/AvroMagicByteLiveTest.java
+++ spring-kafka-4/src/test/java/com/baeldung/avro/deserialization/exception/AvroMagicByteLiveTest.java
@@ -12,46 +12,35 @@ import java.util.Map;
 
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
 
 import com.baeldung.avro.deserialization.exception.avro.Article;
 
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 
-@Testcontainers
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("avro-magic-byte")
-public class AvroMagicByteLiveTest {
+class AvroMagicByteLiveTest {
 
     @Container
-    static KafkaContainer kafka = new KafkaContainer();
-
-    @DynamicPropertySource
-    static void setKafkaProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-    }
+    @ServiceConnection
+    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("apache/kafka:4.0.0"));
 
     @Autowired
     private AvroMagicByteApp listener;
 
     @Test
     void whenSendingCorrectArticle_thenItsAddedToTheBlog() throws Exception {
-        Article article = new Article("Avro Magic Byte", "John Doe", List.of("avro", "kafka", "spring"));
-
-        avroKafkaTemplate().send("baeldung.article.published", article)
+        avroKafkaTemplate().send("baeldung.article.published", aTestArticle("Avro Magic Byte"))
             .get();
 
         await().untilAsserted(() -> assertThat(listener.blog).containsExactly("Avro Magic Byte"));
@@ -59,21 +48,13 @@ public class AvroMagicByteLiveTest {
 
     @Test
     void whenSendingMalformedMessage_thenSendToDLQ() throws Exception {
-        stringKafkaTemplate()
-            .send("baeldung.article.published", "not a valid avro message!")
+        stringKafkaTemplate().send("baeldung.article.published", "not a valid avro message!")
             .get();
 
-        var dlqRecord = KafkaTestUtils.getOneRecord(
-            kafka.getBootstrapServers(),
-            "test-group-id",
-            "baeldung.article.published-dlt", 0, false, true,
-            ofSeconds(5L)
-        );
+        var dlqRecord = KafkaTestUtils.getOneRecord(kafka.getBootstrapServers(), "test-group-id", "baeldung.article.published-dlt", 0, false, true,
+            ofSeconds(5L));
 
-        assertThat(dlqRecord)
-            .isNotNull()
-            .extracting(record -> record.value().toString())
-            .isEqualTo(MALFORMED_AVRO_MESSAGE);
+        assertThat(dlqRecord.value()).isEqualTo("not a valid avro message!");
     }
 
     private static KafkaTemplate<Object, Object> avroKafkaTemplate() {
@@ -90,5 +71,8 @@ public class AvroMagicByteLiveTest {
                 VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName(), "schema.registry.url", "mock://test"));
     }
 
-    public static final String MALFORMED_AVRO_MESSAGE = "not a valid avro message!";
+    private static Article aTestArticle(String title) {
+        return new Article(title, "John Doe", List.of("avro", "kafka", "spring"));
+    }
+
 }
