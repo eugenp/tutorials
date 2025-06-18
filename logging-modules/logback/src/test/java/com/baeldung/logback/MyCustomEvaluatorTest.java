caeeb089d29263c46194b32ac0c15b8b104538c0
dvohra09 <dvohra09@yahoo.com>
2025-06-18 11:45:40 -0700

    Create MyCustomEvaluatorTest.java

---
+++ logging-modules/logback/src/test/java/com/baeldung/logback/MyCustomEvaluatorTest.java
@@ -0,0 +1,23 @@
+package com.baeldung.logback;
+
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.classic.Level;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MyCustomEvaluatorTest {
+
+    @Test
+    public void testEvaluate_containsBilling() {
+        MyCustomEvaluator evaluator = new MyCustomEvaluator();
+        LoggingEvent event = new LoggingEvent("fqcn", null, Level.INFO, "This message contains billing information.", null, null);
+        assertTrue(evaluator.evaluate(event));
+    }
+
+    @Test
+    public void testEvaluate_doesNotContainBilling() {
+        MyCustomEvaluator evaluator = new MyCustomEvaluator();
+        LoggingEvent event = new LoggingEvent("fqcn", null, Level.INFO, "This message does not.", null, null);
+        assertFalse(evaluator.evaluate(event));
+    }
+}
