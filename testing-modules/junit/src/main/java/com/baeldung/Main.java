52b3560c6567cce64a8b02f4f9f8c0de94575923
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2025-06-18 20:16:55 +0000

    fix: used 4-space indentation instead of tabs
    Signed-off-by: Emmanuel Mireku Omari <omariemmanuel91@gmail.com>

--- testing-modules/junit/src/main/java/com/baeldung/Main.java
+++ testing-modules/junit/src/main/java/com/baeldung/Main.java
@@ -1,7 +1,7 @@
 package com.baeldung;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
