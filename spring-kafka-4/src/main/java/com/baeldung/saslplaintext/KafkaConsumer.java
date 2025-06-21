ca4cd6696174264ee652bb510111e501f11e1d7e
saikat <saikatcse03@gmail.com>
2025-06-21 14:48:29 +0200

    refactoring error fixes

--- spring-kafka-4/src/main/java/com/baeldung/saslplaintext/KafkaConsumer.java
+++ spring-kafka-4/src/main/java/com/baeldung/saslplaintext/KafkaConsumer.java
@@ -1,7 +1,8 @@
 package com.baeldung.saslplaintext;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +10,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Slf4j
 public class KafkaConsumer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaConsumer.class);
     public static final String TOPIC = "test-topic";
     public final List<String> messages = new ArrayList<>();
 
     @KafkaListener(topics = TOPIC)
     public void receive(ConsumerRecord<String, String> consumerRecord) {
-        log.info("Received payload: '{}'", consumerRecord.toString());
+        LOGGER.info("Received payload: '{}'", consumerRecord.toString());
         messages.add(consumerRecord.value());
     }
 }
--- spring-kafka-4/src/main/java/com/baeldung/saslplaintext/KafkaProducer.java
+++ spring-kafka-4/src/main/java/com/baeldung/saslplaintext/KafkaProducer.java
@@ -1,25 +1,28 @@
 package com.baeldung.saslplaintext;
 
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
-@Slf4j
-@AllArgsConstructor
 @Component
 public class KafkaProducer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
     private final KafkaTemplate<String, String> kafkaTemplate;
 
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
     public void sendMessage(String message, String topic) {
-        log.info("Producing message: {}", message);
+        LOGGER.info("Producing message: {}", message);
         kafkaTemplate.send(topic, "key", message)
             .whenComplete((result, ex) -> {
                 if (ex == null) {
-                    log.info("Message sent to topic: {}", message);
+                    LOGGER.info("Message sent to topic: {}", message);
                 } else {
-                    log.error("Failed to send message", ex);
+                    LOGGER.error("Failed to send message", ex);
                 }
             });
     }
