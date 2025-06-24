c54ae2a97987639f6936d6decff7be13759368a9
panos-kakos <kakos.programmer@gmail.com>
2025-06-24 19:09:18 +0300

    [JAVA-42103] Clean up spring-boot-libraries

--- spring-boot-modules/spring-boot-libraries/src/test/java/com/baeldung/openapi/OpenApiPetsIntegrationTest.java
+++ spring-boot-modules/spring-boot-libraries/src/test/java/com/baeldung/openapi/OpenApiPetsIntegrationTest.java
@@ -3,17 +3,20 @@ package com.baeldung.openapi;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@Ignore
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = OpenApiApplication.class)
 @ComponentScan("com.baeldung.openapi")
@@ -24,6 +27,15 @@ public class OpenApiPetsIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @TestConfiguration
+    static class NoSecurityConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
+            return http.build();
+        }
+    }
 
     @Test
     public void whenReadAll_thenStatusIsNotImplemented() throws Exception {
