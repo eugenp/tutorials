1510fa95f04749d53780b232a69f49b30d78f760
LordMaduz <maduz.ruchira@gmail.com>
2025-06-26 15:35:19 +0800

    BAEL-5969 - Parallel Flux vs Flux in Project Reactor 1) Disable Fibonacci Tests marked them as manual.

--- reactor-core-2/src/test/java/com/baeldung/reactor/flux/parallelflux/FluxUnitTest.java
+++ reactor-core-2/src/test/java/com/baeldung/reactor/flux/parallelflux/FluxUnitTest.java
@@ -1,7 +1,8 @@
 package com.baeldung.reactor.flux.parallelflux;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
@@ -11,6 +12,7 @@ import java.time.Duration;
 @Slf4j
 public class FluxUnitTest {
 
+    @Disabled("Manual test - takes time due to heavy computation")
     @Test
     public void givenFibonacciIndices_whenComputingWithFlux_thenCorrectResults() {
 
