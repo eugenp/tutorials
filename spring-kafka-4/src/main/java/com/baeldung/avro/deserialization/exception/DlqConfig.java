bb77b94f53ce6c274df07b4e3235caa2435023e0
Emanuel Trandafir <75391049+etrandafir93@users.noreply.github.com>
2025-06-28 17:36:10 +0300

    BAEL-7164: Avro Magic Byte (#18631)
    * BAEL-7164: sample app
    
    * BAEL-7164: error handling deser + dlq
    
    * BAEL-7164: use service connection
    
    * BAEL-7164: extract test helper
    
    * BAEL-7164: use getter
    
    * BAEL-7164: code reivew

---
+++ spring-kafka-4/src/main/java/com/baeldung/avro/deserialization/exception/DlqConfig.java
@@ -0,0 +1,32 @@
+package com.baeldung.avro.deserialization.exception;
+
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+
+@Configuration
+class DlqConfig {
+
+    @Bean
+    DefaultErrorHandler errorHandler(DeadLetterPublishingRecoverer dlqPublishingRecoverer) {
+        return new DefaultErrorHandler(dlqPublishingRecoverer);
+    }
+
+    @Bean
+    DeadLetterPublishingRecoverer dlqPublishingRecoverer(KafkaTemplate<byte[], byte[]> bytesKafkaTemplate) {
+        return new DeadLetterPublishingRecoverer(bytesKafkaTemplate);
+    }
+
+    @Bean("bytesKafkaTemplate")
+    KafkaTemplate<?, ?> bytesTemplate(ProducerFactory<?, ?> kafkaProducerFactory) {
+        return new KafkaTemplate<>(kafkaProducerFactory, Map.of(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName()));
+    }
+
+}
