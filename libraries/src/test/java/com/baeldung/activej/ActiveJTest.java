66976602abda8e81b696a8c17b314510f743f652
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2025-06-29 12:21:30 +0300

    [JAVA-47302] Moving some article links on Github - libraries  (#18629)

---
+++ libraries/src/test/java/com/baeldung/activej/ActiveJTest.java
@@ -0,0 +1,44 @@
+package com.baeldung.activej;
+
+import com.baeldung.activej.config.PersonModule;
+import com.baeldung.activej.repository.PersonRepository;
+import com.baeldung.activej.service.PersonService;
+
+import io.activej.eventloop.Eventloop;
+import io.activej.inject.Injector;
+
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ActiveJTest {
+
+    @Test
+    void givenPersonModule_whenGetTheServiceBean_thenAllTheDependenciesShouldBePresent() {
+        PersonModule personModule = new PersonModule();
+
+        PersonService personService = Injector.of(personModule)
+            .getInstance(PersonService.class);
+        assertNotNull(personService);
+        PersonRepository personRepository = personService.getPersonRepository();
+        assertNotNull(personRepository);
+        DataSource dataSource = personRepository.getDataSource();
+        assertNotNull(dataSource);
+    }
+
+    @Test
+    void givenEventloop_whenCallFindAndVerifyPerson_thenExpectedVerificationResultShouldBePresent() {
+        PersonModule personModule = new PersonModule();
+
+        PersonService personService = Injector.of(personModule)
+            .getInstance(PersonService.class);
+
+        Eventloop eventloop = Eventloop.create();
+        eventloop.run();
+        personService.findAndVerifyPerson("Good person")
+            .whenResult(verifiedPerson -> assertEquals("SUCCESS", verifiedPerson.result()));
+    }
+}
