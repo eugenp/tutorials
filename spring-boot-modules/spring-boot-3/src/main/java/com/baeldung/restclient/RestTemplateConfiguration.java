762cf69fad70c73de8aa945e0ff24e12ef0ac467
achraftt <achraf.taitai1@gmail.com>
2025-06-29 23:54:09 +0200

    BAEL-9334: Resolving Apache HttpClient Error When Migrating Spring Boot Version 2 to 3

--- spring-boot-modules/spring-boot-3/src/main/java/com/baeldung/restclient/RestTemplateConfiguration.java
+++ spring-boot-modules/spring-boot-3/src/main/java/com/baeldung/restclient/RestTemplateConfiguration.java
@@ -6,18 +6,12 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
-import org.apache.hc.client5.http.ssl.DefaultClientTlsStrategy;
-import org.apache.hc.client5.http.ssl.HostnameVerificationPolicy;
-import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
 import org.apache.hc.core5.http.io.SocketConfig;
-import org.apache.hc.core5.ssl.SSLContexts;
 import org.apache.hc.core5.util.Timeout;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-
-import javax.net.ssl.SSLContext;
 
 @Configuration
 public class RestTemplateConfiguration {
@@ -25,40 +19,27 @@ public class RestTemplateConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         try {
-            // WARNING: Disabling SSL verification is unsafe for production!
-            SSLContext sslContext = SSLContexts.custom()
-                    .loadTrustMaterial((chain, authType) -> true)  // Trust all certificates
-                    .build();
-
-            DefaultClientTlsStrategy tlsStrategy = new DefaultClientTlsStrategy(
-                    sslContext,
-                    HostnameVerificationPolicy.CLIENT,
-                    NoopHostnameVerifier.INSTANCE  // Disables hostname checks
-            );
-
             // Timeout configurations
             SocketConfig socketConfig = SocketConfig.custom()
                     .setSoTimeout(Timeout.ofSeconds(30))  // Read timeout
                     .build();
 
             ConnectionConfig connectionConfig = ConnectionConfig.custom()
-                    .setConnectTimeout(Timeout.ofSeconds(10))  // Connection timeout
+                    .setConnectTimeout(Timeout.ofSeconds(30))  // Connect timeout
                     .build();
 
-            // Connection pool (optimized for production)
+            RequestConfig requestConfig = RequestConfig.custom()
+                    .setConnectionRequestTimeout(Timeout.ofSeconds(30))  // Pool wait timeout
+                    .build();
+
+            // Connection pool configuration
             PoolingHttpClientConnectionManager connectionManager =
                     PoolingHttpClientConnectionManagerBuilder.create()
                             .setMaxConnPerRoute(20)
                             .setMaxConnTotal(100)
-                            .setTlsSocketStrategy(tlsStrategy)
                             .setDefaultSocketConfig(socketConfig)
                             .setDefaultConnectionConfig(connectionConfig)
                             .build();
-
-            // Request-level timeout (waiting for a connection from the pool)
-            RequestConfig requestConfig = RequestConfig.custom()
-                    .setConnectionRequestTimeout(Timeout.ofSeconds(5))
-                    .build();
 
             CloseableHttpClient httpClient = HttpClients.custom()
                     .setConnectionManager(connectionManager)
