0ad250dcef95d78e00203940979bef17dc2a8155
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2025-07-02 00:47:43 +0300

    [JAVA-42103] Moving some article links on Github - spring-boot-libraries (#18621)

--- spring-boot-modules/spring-boot-libraries-2/src/main/java/com/baeldung/boot/problem/controller/ProblemDemoController.java
+++ spring-boot-modules/spring-boot-libraries-2/src/main/java/com/baeldung/boot/problem/controller/ProblemDemoController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.http.MediaType;
+//import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
