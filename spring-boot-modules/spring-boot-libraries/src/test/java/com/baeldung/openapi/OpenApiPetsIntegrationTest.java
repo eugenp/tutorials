d69850d1ac074ea5d4cb318b58006319fea10eed
panos-kakos <102670093+panos-kakos@users.noreply.github.com>
2025-07-02 00:47:43 +0300

    [JAVA-42103] Moving some article links on Github - spring-boot-libraries (#18621)

--- spring-boot-modules/spring-boot-libraries/src/test/java/com/baeldung/openapi/OpenApiPetsIntegrationTest.java
+++ spring-boot-modules/spring-boot-libraries/src/test/java/com/baeldung/openapi/OpenApiPetsIntegrationTest.java
@@ -8,9 +8,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = OpenApiApplication.class)
@@ -22,6 +27,15 @@ public class OpenApiPetsIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @TestConfiguration
+    static class NoSecurityConfig {
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http.csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
+            return http.build();
+        }
+    }
 
     @Test
     public void whenReadAll_thenStatusIsNotImplemented() throws Exception {
