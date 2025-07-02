a91cc8fb71e072d6d7b6c3ad56bf6ec7880a7aa6
Deepak Vohra <dvohra10@yahoo.com>
2025-07-01 19:39:27 -0700

    Update MyCustomEvaluator.java

--- logging-modules/logback/src/main/java/com/baeldung/logback/MyCustomEvaluator.java
+++ logging-modules/logback/src/main/java/com/baeldung/logback/MyCustomEvaluator.java
@@ -1,6 +1,7 @@
 package com.baeldung.logback;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.boolex.EventEvaluatorBase;
+import ch.qos.logback.core.boolex.EvaluationException;
 
 public class MyCustomEvaluator extends EventEvaluatorBase<ILoggingEvent> {
 
