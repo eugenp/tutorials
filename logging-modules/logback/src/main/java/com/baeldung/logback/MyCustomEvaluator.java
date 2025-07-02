06e8277d764975c36035ff8bbebd7c46e50537e2
Deepak Vohra <dvohra10@yahoo.com>
2025-07-01 19:09:57 -0700

    Create MyCustomEvaluator.java

---
+++ logging-modules/logback/src/main/java/com/baeldung/logback/MyCustomEvaluator.java
@@ -0,0 +1,12 @@
+package com.baeldung.logback;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.boolex.EventEvaluatorBase;
+
+public class MyCustomEvaluator extends EventEvaluatorBase<ILoggingEvent> {
+
+    @Override
+    public boolean evaluate(ILoggingEvent event) throws NullPointerException, EvaluationException {
+        String message = event.getMessage();
+        return message != null && message.contains("billing");
+    }
+}
