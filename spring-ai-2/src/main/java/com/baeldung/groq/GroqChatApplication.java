1fc41eb8661ec0460d38127588c272c21815cf6f
parthiv39731 <70740707+parthiv39731@users.noreply.github.com>
2025-07-07 23:33:19 +0530

    BAEL-9284, Using Groq Chat with Spring AI

---
+++ spring-ai-2/src/main/java/com/baeldung/groq/GroqChatApplication.java
@@ -0,0 +1,26 @@
+package com.baeldung.groq;
+
+import org.springframework.ai.autoconfigure.anthropic.AnthropicAutoConfiguration;
+import org.springframework.ai.autoconfigure.bedrock.converse.BedrockConverseProxyChatAutoConfiguration;
+import org.springframework.ai.autoconfigure.ollama.OllamaAutoConfiguration;
+import org.springframework.ai.autoconfigure.vectorstore.chroma.ChromaVectorStoreAutoConfiguration;
+import org.springframework.ai.autoconfigure.vectorstore.pgvector.PgVectorStoreAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication(exclude = {
+    OllamaAutoConfiguration.class,
+    AnthropicAutoConfiguration.class,
+    PgVectorStoreAutoConfiguration.class,
+    ChromaVectorStoreAutoConfiguration.class,
+    BedrockConverseProxyChatAutoConfiguration.class,
+    RedisAutoConfiguration.class
+})
+@PropertySource("classpath:application-groq.properties")
+public class GroqChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GroqChatApplication.class, args);
+    }
+}
