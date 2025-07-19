36753c45e866db7260ca9301bf73c078600f3222
michaelin007 <michaelolayemi28@gmail.com>
2025-07-19 16:23:36 +0100

    https://jira.baeldung.com/browse/BAEL-8313

---
+++ core-java-modules/core-java-networking-6/src/main/java/com/baeldung/inlineimagesinemail/InlineImage.java
@@ -0,0 +1,101 @@
+package com.baeldung.inlineimagesinemail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeBodyPart;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.mail.internet.MimeMultipart;
+
+import com.icegreen.greenmail.util.GreenMail;
+
+public class InlineImage {
+
+    private final String USERNAME = "YOUR_SMTP_USERNAME";
+    private final String PASSWORD = "YOUR_SMTP_PASSWORD";
+    private final String HOST = "SMTP HOST";
+    private final String PORT = "SMTP PORT";
+
+    public Properties smtpProperties() {
+        Properties prop = new Properties();
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true");
+        prop.put("mail.smtp.host", HOST);
+        prop.put("mail.smtp.port", PORT);
+        prop.put("mail.smtp.user", USERNAME);
+        prop.put("mail.smtp.password", PASSWORD);
+
+        return prop;
+    }
+
+    public Session smtpsession(Properties props) {
+        final String username = props.getProperty("mail.smtp.user");
+        final String password = props.getProperty("mail.smtp.password");
+
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+        });
+
+        return session;
+    }
+
+    public Session smtpsession(GreenMail greenMail) {
+
+        Session session = greenMail.getSmtp()
+            .createSession();
+
+        return session;
+    }
+
+    public void sendEmail(Session session, String to, String subject, String body, String filePath) throws MessagingException, IOException {
+
+        MimeMessage message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("example@gmail.com"));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+        message.setSubject(subject);
+
+        MimeBodyPart htmlPart = new MimeBodyPart();
+        htmlPart.setContent(body, "text/html");
+
+        MimeBodyPart imagePart = new MimeBodyPart();
+        imagePart.attachFile(new File(filePath));
+        imagePart.setContentID("<image1>");
+        imagePart.setDisposition(MimeBodyPart.INLINE);
+
+        MimeMultipart mimeMultipart = new MimeMultipart("related");
+        mimeMultipart.addBodyPart(htmlPart);
+        mimeMultipart.addBodyPart(imagePart);
+
+        message.setContent(mimeMultipart);
+
+        Transport.send(message);
+    }
+
+    public static void main(String[] args) throws MessagingException, IOException {
+        InlineImage inlineImage = new InlineImage();
+        Properties properties = inlineImage.smtpProperties();
+        Session session = inlineImage.smtpsession(properties);
+
+        String to = "receiver@gmail.com";
+        String subject = "Baeldung";
+        String body = """
+             <p>Welcome to Baeldung, home of Java and its frameworks.</p>
+             <img src='cid:image1'></img>
+             <p> Explore and learn. </p>
+            """;
+        String imagePath = "java.png";
+
+        inlineImage.sendEmail(session, to, subject, body, imagePath);
+    }
+}
---
+++ core-java-modules/core-java-networking-6/src/test/java/com/baeldung/inlineimagesinemail/InlineImageUnitTest.java
@@ -0,0 +1,59 @@
+package com.baeldung.inlineimagesinemail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import jakarta.mail.BodyPart;
+import jakarta.mail.Multipart;
+import jakarta.mail.Part;
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
+
+import org.junit.jupiter.api.Test;
+
+import com.icegreen.greenmail.util.GreenMail;
+import com.icegreen.greenmail.util.ServerSetupTest;
+
+class InlineImageUnitTest {
+
+    @Test
+    void givenHtmlEmailWithInlineImage_whenSentViaGreenMailSmtp_thenReceivesEmailWithInlineImage() throws Exception {
+        GreenMail greenMail = new GreenMail(ServerSetupTest.SMTP);
+        greenMail.start();
+        InlineImage inlineImage = new InlineImage();
+        Session session = inlineImage.smtpsession(greenMail);
+
+        String to = "receiver@localhost";
+        String subject = "Test Subject";
+        String body = """
+             <p>Welcome to Baeldung, home of Java and its frameworks.</p>
+             <img src='cid:image1'></img>
+             <p> Explore and learn. </p>
+            """;
+        String imagePath = "java.png";
+
+        inlineImage.sendEmail(session, to, subject, body, imagePath);
+
+        MimeMessage[] receivedMessages = greenMail.getReceivedMessages();
+        assertEquals(1, receivedMessages.length);
+
+        MimeMessage message = receivedMessages[0];
+
+        Multipart multipart = (Multipart) message.getContent();
+        assertEquals(2, multipart.getCount());
+
+        BodyPart htmlPart = multipart.getBodyPart(0);
+        assertTrue(htmlPart.getContentType()
+            .contains("text/html"));
+        String htmlContent = (String) htmlPart.getContent();
+        assertTrue(htmlContent.contains("cid:image1"));
+
+        BodyPart imagePart = multipart.getBodyPart(1);
+        String contentId = imagePart.getHeader("Content-ID")[0];
+        assertTrue(contentId.contains("image1") || contentId.contains("<image1>"));
+
+        assertEquals(Part.INLINE, imagePart.getDisposition());
+        greenMail.stop();
+    }
+
+}
