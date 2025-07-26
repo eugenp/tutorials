c30aff7230755566eb16e5d2e5f941ecfcc35e4c
AndreiBranza <91902093+AndreiBranza@users.noreply.github.com>
2025-07-27 00:09:26 +0300

    BAEL-5775 | Add classes and tests relevant to BAEL-5775 article. (#18674)
    * BAEL-5775 | Add classes and tests relevant to BAEL-5775 article.
    
    * BAEL-5775 | Updates according to editor feedback
    
    * BAEL-5775 | Removed some not needed code.
    
    * BAEL-5775 | Update spring-webflux and reactor versions in pom.xml
    
    * BAEL-5775 | Refactor code for improved readability and consistency
    
    * Add method to fetch users list using existing ParameterizedTypeReference
    
    * BAEL-5775 | Update Java version in pom.xml to 17
    
    ---------
    
    Co-authored-by: Andrei Branza <abranza@greenfly.com>

---
+++ spring-core-4/src/main/java/com/baeldung/parametrizedtypereference/ApiException.java
@@ -0,0 +1,12 @@
+package com.baeldung.parametrizedtypereference;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String message) {
+        super(message);
+    }
+
+    public ApiException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
