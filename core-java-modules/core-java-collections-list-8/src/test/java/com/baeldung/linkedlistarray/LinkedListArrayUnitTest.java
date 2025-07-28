0586fea45d9f7406e3dfab043fb77364c251cbd4
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2025-07-28 20:43:14 +0300

    move list articles

--- core-java-modules/core-java-collections-list-8/src/test/java/com/baeldung/linkedlistarray/LinkedListArrayUnitTest.java
+++ core-java-modules/core-java-collections-list-8/src/test/java/com/baeldung/linkedlistarray/LinkedListArrayUnitTest.java
@@ -1,4 +1,4 @@
-package baeldung.linkedlistarray;
+package com.baeldung.linkedlistarray;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
