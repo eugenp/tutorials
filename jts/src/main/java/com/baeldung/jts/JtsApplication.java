e4249a62f8b4d8feab9d5ccc07ea6156955aa230
oscarramadhan <oscarramadhan.or@gmail.com>
2025-07-30 21:39:09 +0700

    init jts operations implementations

---
+++ jts/src/main/java/com/baeldung/jts/JtsApplication.java
@@ -0,0 +1,12 @@
+package com.baeldung.jts;
+
+import com.baeldung.jts.utils.GeometryFactoryUtil;
+import org.locationtech.jts.geom.Geometry;
+
+public class JtsApplication {
+
+	public static void main(String[] args) throws Exception {
+
+	}
+
+}
---
+++ jts/src/test/java/com/baeldung/jts/JtsApplicationTests.java
@@ -0,0 +1,69 @@
+package com.baeldung.jts;
+
+import com.baeldung.jts.operations.JTSOperationUtils;
+import com.baeldung.jts.utils.GeometryFactoryUtil;
+import org.junit.Assert;
+import org.junit.Test;
+import org.locationtech.jts.geom.Geometry;
+
+public class JtsApplicationTests {
+    @Test
+    public void givenPolygon2D_whenContainPoint_thenContainmentIsTrue() throws Exception {
+        Geometry point = GeometryFactoryUtil.readWKT("POINT (10 20)");
+        Geometry polygon = GeometryFactoryUtil.readWKT("POLYGON ((0 0, 0 40, 40 40, 40 0, 0 0))");
+        Assert.assertTrue(JTSOperationUtils.isContainment(point, polygon));
+    }
+
+    @Test
+    public void givenRectangle1_whenIntersectWithRectangle2_thenIntersectionIsTrue() throws Exception {
+        Geometry rectangle1 = GeometryFactoryUtil.readWKT("POLYGON ((10 10, 10 30, 30 30, 30 10, 10 10))");
+        Geometry rectangle2 = GeometryFactoryUtil.readWKT("POLYGON ((20 20, 20 40, 40 40, 40 20, 20 20))");
+        Assert.assertTrue(JTSOperationUtils.checkIntersect(rectangle1, rectangle2));
+    }
+
+    @Test
+    public void givenPoint_whenAddedBuffer_thenPointIsInsideTheBuffer() throws Exception {
+        Geometry point = GeometryFactoryUtil.readWKT("POINT (10 10)");
+        Geometry bufferArea = JTSOperationUtils.getBuffer(point, 5);
+        Assert.assertTrue(JTSOperationUtils.isContainment(point, bufferArea));
+    }
+
+    @Test
+    public void givenTwoPoints_whenGetDistanceBetween_thenGetTheDistance() throws Exception {
+        Geometry point1 = GeometryFactoryUtil.readWKT("POINT (10 10)");
+        Geometry point2 = GeometryFactoryUtil.readWKT("POINT (13 14)");
+        double distance = JTSOperationUtils.getDistance(point1, point2);
+        double expectedResult = 5.00;
+        double delta = 0.00;
+        Assert.assertEquals(expectedResult, distance, delta);
+    }
+
+    @Test
+    public void givenTwoGeometries_whenGetUnionOfBoth_thenGetTheUnion() throws Exception {
+        Geometry geometry1 = GeometryFactoryUtil.readWKT("POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))");
+        Geometry geometry2 = GeometryFactoryUtil.readWKT("POLYGON ((10 0, 10 10, 20 10, 20 0, 10 0))");
+
+        Geometry union = JTSOperationUtils.getUnion(geometry1, geometry2);
+        Geometry expectedResult = GeometryFactoryUtil.readWKT("POLYGON ((0 0, 0 10, 10 10, 20 10, 20 0, 10 0, 0 0))");
+        Assert.assertEquals(expectedResult, union);
+    }
+
+    @Test
+    public void givenBaseRectangle_whenAnotherRectangleOverlapping_GetTheDifferenceRectangle() throws Exception {
+        Geometry base = GeometryFactoryUtil.readWKT("POLYGON ((0 0, 0 10, 10 10, 10 0, 0 0))");
+        Geometry cut = GeometryFactoryUtil.readWKT("POLYGON ((5 0, 5 10, 10 10, 10 0, 5 0))");
+
+        Geometry result = JTSOperationUtils.getDifference(base, cut);
+        Geometry expectedResult = GeometryFactoryUtil.readWKT("POLYGON ((0 0, 0 10, 5 10, 5 0, 0 0))");
+        Assert.assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void givenInvalidGeometryValue_whenValidated_thenGiveFixedResult() throws Exception {
+        Geometry invalidGeo = GeometryFactoryUtil.readWKT("POLYGON ((0 0, 5 5, 5 0, 0 5, 0 0))");
+        Geometry result = JTSOperationUtils.validateAndRepair(invalidGeo);
+
+        Geometry expectedResult = GeometryFactoryUtil.readWKT("POLYGON ((2.5 2.5, 5 5, 5 0, 2.5 2.5))");
+        Assert.assertEquals(expectedResult, result);
+    }
+}
