b36774d1557dd39ece8b2e667cc27ae2a9d3ab44
Neetika Khandelwal <kwal.neetika2398@gmail.com>
2025-08-01 21:29:16 +0530

    BAEL-9323

---
+++ spring-batch-2/src/main/java/com/baeldung/dataloaderbatchprocessing/entity/User.java
@@ -0,0 +1,13 @@
+package com.baeldung.dataloaderbatchprocessing.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class User {
+
+    private final String id;
+    private final String name;
+}
+
