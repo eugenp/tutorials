2ba9f39b7022ba15e29bb6a46d923712c6d3ec8e
rajatgarg <rajatgarg@adobe.com>
2025-08-03 22:52:58 +0530

    [BAEL-9328] Add Live Tests for Web Crawler using WebMagic Library

--- libraries-4/src/main/java/com/baeldung/webmagic/BookScraper.java
+++ libraries-4/src/main/java/com/baeldung/webmagic/BookScraper.java
@@ -5,21 +5,26 @@ import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class BookScraper implements PageProcessor {
 
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+    private final List<Book> books = new ArrayList<>();
 
     @Override
     public void process(Page page) {
-        var books = page.getHtml().css("article.product_pod");
+        var bookNodes = page.getHtml().css("article.product_pod");
 
-        for (int i = 0; i < Math.min(10, books.nodes().size()); i++) {
-            var book = books.nodes().get(i);
+        for (int i = 0; i < Math.min(10, bookNodes.nodes().size()); i++) {
+            var book = bookNodes.nodes().get(i);
 
             String title = book.css("h3 a", "title").get();
             String price = book.css(".price_color", "text").get();
 
-            System.out.println("Title: " + title + " | Price: " + price);
+            books.add(new Book(title, price));
         }
     }
 
@@ -28,10 +33,18 @@ public class BookScraper implements PageProcessor {
         return site;
     }
 
+    public List<Book> getBooks() {
+        return Collections.unmodifiableList(books);
+    }
+
     public static void main(String[] args) {
-        Spider.create(new BookScraper())
+        BookScraper bookScraper = new BookScraper();
+        Spider.create(bookScraper)
                 .addUrl("https://books.toscrape.com/")
                 .thread(1)
                 .run();
+
+        bookScraper.getBooks().forEach(book ->
+                System.out.println("Title: " + book.getTitle() + " | Price: " + book.getPrice()));
     }
 }
