17aa1b517dc7b40e9a74cb0531ee37ccc1890154
Alexandru Borza <alex.borza@external.wolt.com>
2025-08-07 10:25:34 +0300

    Java reflection api (#18738)
    * BAEL-9216 - Java Reflection Beans Property API
    
    * BAEL-9216 - Java Reflection Beans Property API
    
    * BAEL-9216 - Java Reflection Beans Property API

---
+++ core-java-modules/core-java-reflection-3/src/main/java/com/baeldung/reflectionbeans/PropertyDescriptorDemo.java
@@ -0,0 +1,29 @@
+package com.baeldung.reflectionbeans;
+
+import java.beans.BeanInfo;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.Method;
+
+public class PropertyDescriptorDemo {
+
+    public static void main(String[] args) throws Exception {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Post.class);
+
+        for (PropertyDescriptor pd : beanInfo.getPropertyDescriptors()) {
+            System.out.println(pd.getName());
+        }
+
+        Post post = new Post();
+        PropertyDescriptor titlePd = new PropertyDescriptor("title", Post.class);
+
+        Method write = titlePd.getWriteMethod();
+        Method read = titlePd.getReadMethod();
+
+
+        write.invoke(post, "Reflections in Java");
+        String value = (String) read.invoke(post);
+
+        System.out.println(value);
+    }
+}
