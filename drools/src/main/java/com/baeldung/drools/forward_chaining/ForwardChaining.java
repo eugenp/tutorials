60c04d156b935bd69e59582b11a856719f86847b
Daniel Fintinariu <fintinariu.daniel@gmail.com>
2025-08-13 08:19:44 +0200

    Updated pom

--- drools/src/main/java/com/baeldung/drools/forward_chaining/ForwardChaining.java
+++ drools/src/main/java/com/baeldung/drools/forward_chaining/ForwardChaining.java
@@ -2,7 +2,6 @@ package com.baeldung.drools.forward_chaining;
 
 import com.baeldung.drools.config.DroolsBeanFactory;
 import com.baeldung.drools.model.Applicant;
-import com.baeldung.drools.model.Product;
 import com.baeldung.drools.model.SuggestedRole;
 import org.kie.api.runtime.KieSession;
 
@@ -12,20 +11,19 @@ public class ForwardChaining {
         result.forwardChaining();
     }
 
-    private void forwardChaining() {
-        KieSession ksession = new DroolsBeanFactory().getKieSession();
-        try {
-            Applicant applicant = new Applicant("Daniel", 38, 1_600_000.0, 11);
-            SuggestedRole suggestedRole = new SuggestedRole();
+private void forwardChaining() {
+    KieSession ksession = new DroolsBeanFactory().getKieSession();
+    Applicant applicant = new Applicant("Daniel", 38, 1_600_000.0, 11);
+    SuggestedRole suggestedRole = new SuggestedRole();
 
-            ksession.setGlobal("suggestedRole", suggestedRole);
-            ksession.insert(applicant);
+    ksession.setGlobal("suggestedRole", suggestedRole);
+    ksession.insert(applicant);
 
-            int fired = ksession.fireAllRules();
-            System.out.println("Rules fired: " + fired);
-            System.out.println("Suggested role: " + suggestedRole.getRole());
-        } finally {
-            ksession.dispose();
-        }
-    }
+    int fired = ksession.fireAllRules();
+    System.out.println("Rules fired: " + fired);
+    System.out.println("Suggested role: " + suggestedRole.getRole());
+
+    ksession.dispose();
+}
+
 }
