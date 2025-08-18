a88df9591fe5ec4b481ebc3f53882ee5af829c44
Wynn Teo <wynnteo@Wynns-MacBook-Pro.local>
2025-08-18 10:45:15 +0800

    BAEL-9410

---
+++ logging-modules/log4j/src/test/java/com/baeldung/logging/log4j2/DynamicFileAppenderUnitTest.java
@@ -0,0 +1,48 @@
+package com.baeldung.logging.log4j2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.junit.jupiter.api.Test;
+
+public class DynamicFileAppenderUnitTest {
+    @Test
+    public void givenLog4j2DynamicFileNameConfig_whenLogToFile_thenFileIsCreated() throws Exception {
+        System.setProperty("log4j.configurationFile", "src/test/resources/log4j2-dynamic.xml");
+        System.setProperty("logfilename", "app-dynamic-log");
+
+        Logger logger = LogManager.getLogger(DynamicFileAppenderUnitTest.class);
+        String expectedMessage = "This is an ERROR log message to the same file.";
+        logger.error(expectedMessage);
+
+        File file = new File("app-dynamic-log.log");
+        assertTrue(file.exists(), "Log file should be created dynamically.");
+
+        String content = Files.readString(file.toPath());
+        assertTrue(content.contains(expectedMessage), "Log file should contain the logged message.");
+    }
+
+    @Test
+    public void givenLog4j1DynamicFileNameConfig_whenLogToFile_thenFileIsCreated() throws IOException {
+        System.setProperty("logfilename", "log4j1-dynamic-log");
+        DOMConfigurator.configure("src/test/resources/log4j1-dynamic.xml");
+
+        org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(DynamicFileAppenderUnitTest.class);
+        String expectedMessage = "This is a test ERROR log for Log4j 1";
+        logger.error(expectedMessage);
+
+        File file = new File("log4j1-dynamic-log.log");
+        assertTrue(file.exists(), "Log file should be created dynamically for Log4j 1");
+
+        String content = Files.readString(file.toPath());
+        assertTrue(content.contains(expectedMessage), "Log file should contain the logged message.");
+    }
+}
