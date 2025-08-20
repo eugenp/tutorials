8b07b79da419afd7bfc78314b0e58017080cb174
Anika <anikanawer2107089@gmail.com>
2025-08-21 00:47:48 +0600

    Added Calculator.cpp edited to feature branch

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,7 +5,7 @@ class Calculator {
 public:
     int add(int a, int b) { return a + b; }
     int subtract(int a, int b) { return a - b; }
-    int multiply(int a, int b) { return a * b; }
+    int multiply(int a, int b) { return a / b; }
     double divide(int a, int b) {
         if (b == 0) {
             cout << "Error: Division by zero!" << endl;
