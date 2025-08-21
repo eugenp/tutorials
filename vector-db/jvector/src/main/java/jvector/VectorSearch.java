89f824ecaeac20f4575092f93c269a3e6e86b4f1
Pedro Lopes <pedroinf97@gmail.com>
2025-08-21 17:47:28 -0300

    final revision

--- vector-db/jvector/src/main/java/jvector/VectorSearch.java
+++ vector-db/jvector/src/main/java/jvector/VectorSearch.java
@@ -2,19 +2,14 @@ package jvector;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
-import io.github.jbellis.jvector.graph.GraphIndex;
 import io.github.jbellis.jvector.graph.GraphIndexBuilder;
-import io.github.jbellis.jvector.graph.GraphSearcher;
 import io.github.jbellis.jvector.graph.ListRandomAccessVectorValues;
 import io.github.jbellis.jvector.graph.OnHeapGraphIndex;
 import io.github.jbellis.jvector.graph.RandomAccessVectorValues;
-import io.github.jbellis.jvector.graph.SearchResult;
 import io.github.jbellis.jvector.graph.disk.OnDiskGraphIndex;
 import io.github.jbellis.jvector.graph.similarity.BuildScoreProvider;
-import io.github.jbellis.jvector.util.Bits;
 import io.github.jbellis.jvector.vector.VectorSimilarityFunction;
 import io.github.jbellis.jvector.vector.types.VectorFloat;
 
@@ -28,15 +23,10 @@ public class VectorSearch {
 
         BuildScoreProvider scoreProvider = BuildScoreProvider.randomAccessScoreProvider(vectorValues, VectorSimilarityFunction.EUCLIDEAN);
 
-        try (GraphIndexBuilder builder = new GraphIndexBuilder(scoreProvider, vectorValues.dimension(), 16, 100, 1.2f, 1.2f, true)) {
+        try (GraphIndexBuilder builder = new GraphIndexBuilder(scoreProvider, vectorValues.dimension(), 28, 100, 1.2f, 1.2f, true)) {
             OnHeapGraphIndex index = builder.build(vectorValues);
 
             OnDiskGraphIndex.write(index, vectorValues, indexPath);
         }
     }
-
-    public static SearchResult searchForVector(VectorFloat<?> queryVector, RandomAccessVectorValues vectorValues, GraphIndex index) {
-        return GraphSearcher.search(queryVector, 10, vectorValues, VectorSimilarityFunction.EUCLIDEAN, index, Bits.ALL);
-    }
-
 }
--- vector-db/jvector/src/test/java/jvector/VectorSearchTest.java
+++ vector-db/jvector/src/test/java/jvector/VectorSearchTest.java
@@ -1,6 +1,9 @@
 package jvector;
 
 import static jvector.VectorSearch.persistIndex;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -12,8 +15,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import io.github.jbellis.jvector.disk.ReaderSupplier;
@@ -21,7 +26,6 @@ import io.github.jbellis.jvector.disk.ReaderSupplierFactory;
 import io.github.jbellis.jvector.graph.GraphIndex;
 import io.github.jbellis.jvector.graph.GraphSearcher;
 import io.github.jbellis.jvector.graph.ListRandomAccessVectorValues;
-import io.github.jbellis.jvector.graph.RandomAccessVectorValues;
 import io.github.jbellis.jvector.graph.SearchResult;
 import io.github.jbellis.jvector.graph.disk.OnDiskGraphIndex;
 import io.github.jbellis.jvector.util.Bits;
@@ -34,39 +38,49 @@ class VectorSearchTest {
 
     private static final VectorTypeSupport VECTOR_TYPE_SUPPORT = VectorizationProvider.getInstance()
         .getVectorTypeSupport();
+    private static Path indexPath;
+    private static Map<String, VectorFloat<?>> datasetVectors;
+
+    @BeforeAll
+    static void setup() throws IOException {
+        datasetVectors = new VectorSearchTest().loadGlove6B50dDataSet(1000);
+        indexPath = Files.createTempFile("sample", ".inline");
+        persistIndex(new ArrayList<>(datasetVectors.values()), indexPath);
+    }
 
     @Test
-    void givenLoadedDataset_whenSearchingForWord_thenPersistIndexInDiskAndFindSimilarResults() throws IOException {
-        URL datasetResource = getClass().getClassLoader()
-            .getResource("glove.6B.50d.txt");
-        Objects.requireNonNull(datasetResource);
+    void givenLoadedDataset_whenPersistingIndex_thenPersistIndexInDisk() throws IOException {
+        try (ReaderSupplier readerSupplier = ReaderSupplierFactory.open(indexPath)) {
+            GraphIndex index = OnDiskGraphIndex.load(readerSupplier);
+            assertInstanceOf(OnDiskGraphIndex.class, index);
+        }
+    }
 
-        Map<String, VectorFloat<?>> vectors = loadGlove6B50dDataSet(datasetResource.getPath(), 1000);
-        VectorFloat<?> queryVector = vectors.get("said");
-
-        Path indexPath = Files.createTempFile("sample", ".inline");
-
-        ArrayList<VectorFloat<?>> vectorsList = new ArrayList<>(vectors.values());
-
-        persistIndex(vectorsList, indexPath);
+    @Test
+    void givenLoadedDataset_whenSearchingSimilarVectors_thenReturnValidSearchResult() throws IOException {
+        VectorFloat<?> queryVector = datasetVectors.get("said");
+        ArrayList<VectorFloat<?>> vectorsList = new ArrayList<>(datasetVectors.values());
 
         try (ReaderSupplier readerSupplier = ReaderSupplierFactory.open(indexPath)) {
             GraphIndex index = OnDiskGraphIndex.load(readerSupplier);
 
-            int originalDimension = vectorsList.get(0)
-                .length();
+            SearchResult result = GraphSearcher.search(queryVector, 10,
+                new ListRandomAccessVectorValues(vectorsList, vectorsList.get(0).length()),
+                VectorSimilarityFunction.EUCLIDEAN, index, Bits.ALL);
 
-            RandomAccessVectorValues vectorValues = new ListRandomAccessVectorValues(vectorsList, originalDimension);
-
-            SearchResult result = GraphSearcher.search(queryVector, 10, vectorValues, VectorSimilarityFunction.EUCLIDEAN, index, Bits.ALL);
-            System.out.println(Arrays.toString(result.getNodes()));
+            assertNotNull(result.getNodes());
+            assertEquals(10, result.getNodes().length);
         }
     }
 
-    private static Map<String, VectorFloat<?>> loadGlove6B50dDataSet(String filePath, int limit) throws IOException {
+    private Map<String, VectorFloat<?>> loadGlove6B50dDataSet(int limit) throws IOException {
+        URL datasetResource = getClass().getClassLoader()
+            .getResource("glove.6B.50d.txt");
+        assertNotNull(datasetResource);
+
         Map<String, VectorFloat<?>> vectors = new HashMap<>();
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(datasetResource.getFile()))) {
             String line;
             int count = 0;
             while ((line = reader.readLine()) != null && count < limit) {
@@ -80,6 +94,7 @@ class VectorSearchTest {
                 count++;
             }
         }
+        assertEquals(1000, vectors.size());
         return vectors;
     }
 }
