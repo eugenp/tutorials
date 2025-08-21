e983d5575b22567a90648285655161d35ba3fa1f
Deepak Vohra <dvohra10@yahoo.com>
2025-08-21 14:58:23 -0400

    Update Point.java

--- core-java-modules/core-java-lang-math-5/src/main/java/com/baeldung/storingXYcoordinates/Point.java
+++ core-java-modules/core-java-lang-math-5/src/main/java/com/baeldung/storingXYcoordinates/Point.java
@@ -3,23 +3,37 @@ package com.baeldung.storingXYcoordinates;
 import java.util.Objects;
 
 public class Point {
-    private int x;
-    private int y;
+    private final double x;
+    private final double y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public int getX() { return x; }
-    public int getY() { return y; }
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        Point that = (Point) o;
-        return x == that.x && y == that.y;
+        Point point = (Point) o;
+        return Double.compare(point.x, x) == 0 &&
+               Double.compare(point.y, y) == 0;
     }
 
     @Override
