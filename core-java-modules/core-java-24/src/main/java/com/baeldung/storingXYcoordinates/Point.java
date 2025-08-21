51c7ed7f8c055d3ebf2c23bbb22ad98414c2deba
Deepak Vohra <dvohra10@yahoo.com>
2025-08-21 13:39:35 -0400

    Create Point.java

---
+++ core-java-modules/core-java-24/src/main/java/com/baeldung/storingXYcoordinates/Point.java
@@ -0,0 +1,29 @@
+package com.baeldung.storingXYcoordinates;
+
+import java.util.Objects;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point that = (Point) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
