33d9dec9329ab5ea95fef34bbe58ae6ced721b48
Deepak Vohra <dvohra10@yahoo.com>
2025-08-21 13:46:58 -0400

    Update XYCoordinatesUnitTest.java

--- core-java-modules/core-java-24/src/test/java/com/baeldung/storingXYcoordinates/XYCoordinatesUnitTest.java
+++ core-java-modules/core-java-24/src/test/java/com/baeldung/storingXYcoordinates/XYCoordinatesUnitTest.java
@@ -1,8 +1,27 @@
 package com.baeldung.storingXYcoordinates;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class XYCoordinatesUnitTest {
+
+@Test
+void givenAPoint_whenUsingGetter_thenPointReturnsCoordinatesCorrectly() {
+    // Given a point with coordinates (10, 20)
+    Point point = new Point(10, 20);
+
+    // Then its getters should return the correct values
+    assertEquals(10, point.getX(), "X coordinate should be 10");
+    assertEquals(20, point.getY(), "Y coordinate should be 20");
+}
+  
+@Test
+void givenTwoPointsWithSameCoordinates_whenComparedForEquality_thenShouldBeEqual() {
+    Point point1 = new CustomPoint(5, -3);
+    Point point2 = new CustomPoint(5, -3);
+        
+    assertEquals(point1, point2, "Points with same coordinates should be equal");
+}
+ 
 
 }
