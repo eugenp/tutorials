58467761d44e4baeaa0ec9673fc2ee46038dccbb
Deepak Vohra <dvohra10@yahoo.com>
2025-08-21 15:09:04 -0400

    Update XYCoordinatesUnitTest.java

--- core-java-modules/core-java-lang-math-5/src/test/java/com/baeldung/storingXYcoordinates/XYCoordinatesUnitTest.java
+++ core-java-modules/core-java-lang-math-5/src/test/java/com/baeldung/storingXYcoordinates/XYCoordinatesUnitTest.java
@@ -2,41 +2,42 @@ package com.baeldung.storingXYcoordinates;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.awt.geom.Point2D;
 
 class XYCoordinatesUnitTest {
 
     @Test
     void givenAPoint_whenUsingGetter_thenPointReturnsCoordinatesCorrectly() {
-        // Given a point with coordinates (10, 20)
-        Point point = new Point(10, 20);
+        // Given a point with coordinates (10.0, 20.5)
+        Point point = new Point(10.0, 20.5);
 
         // Then its getters should return the correct values
-        assertEquals(10, point.getX(), "X coordinate should be 10");
-        assertEquals(20, point.getY(), "Y coordinate should be 20");
+        assertEquals(10.0, point.getX(), "X coordinate should be 10.0");
+        assertEquals(20.5, point.getY(), "Y coordinate should be 20.5");
     }
   
     @Test
     void givenTwoPointsWithSameCoordinates_whenComparedForEquality_thenShouldBeEqual() {
-        Point point1 = new Point(5, -3);
-        Point point2 = new Point(5, -3);
+        Point point1 = new Point(5.1, -3.5);
+        Point point2 = new Point(5.1, -3.5);
         
         assertEquals(point1, point2, "Points with same coordinates should be equal");
     }
 
     @Test
     void givenAPointRecord_whenUsingAccessorMethods_thenRecordReturnsCoordinatesCorrectly() {
-        // Given a record with coordinates (30, 40)
-        PointRecord point = new PointRecord(30, 40);
+        // Given a record with coordinates (30.5, 40.0)
+        PointRecord point = new PointRecord(30.5, 40.0);
 
         // Then its accessor methods should return the correct values
-        assertEquals(30, point.x(), "X coordinate should be 30");
-        assertEquals(40, point.y(), "Y coordinate should be 40");
+        assertEquals(30.5, point.x(), "X coordinate should be 30.5");
+        assertEquals(40.0, point.y(), "Y coordinate should be 40.0");
     }
 
     @Test
     void givenTwoRecordsWithSameCoordinates_whenComparedForEquality_thenShouldBeEqual() {
-        PointRecord point1 = new PointRecord(7, 8);
-        PointRecord point2 = new PointRecord(7, 8);
+        PointRecord point1 = new PointRecord(7.0, 8.5);
+        PointRecord point2 = new PointRecord(7.0, 8.5);
 
         assertEquals(point1, point2, "Records with same coordinates should be equal");
     }
@@ -44,26 +45,26 @@ class XYCoordinatesUnitTest {
     @Test
     void givenAnAWTPoint_whenAccessingItsFieldsAndGetters_thenReturnsCoordinatesCorrectly() {
         // Given an AWT Point
-        java.awt.Point point = new java.awt.Point(50, 60);
+        Point2D.Double point = new Point2D.Double(10.5, 20.5);
 
         // Then its public fields should hold the correct values
-        assertEquals(50, point.x);
-        assertEquals(60, point.y);
+        assertEquals(10.5, point.x);
+        assertEquals(20.5, point.y);
 
         // And its getters should also work
-        assertEquals(50.0, point.getX());
-        assertEquals(60.0, point.getY());
+        assertEquals(10.5, point.getX());
+        assertEquals(20.5, point.getY());
     }
 
     @Test
     void givenArrayOfCoordinates_whenAccessingArrayIndices_thenReturnsCoordinatesAtCorrectIndices() {
-        // Given an array representing coordinates (15, 25)
-        int[] coordinates = new int[2];
-        coordinates[0] = 15; // X
-        coordinates[1] = 25; // Y
+        // Given an array representing coordinates (15.0, 25.0)
+        double[] coordinates = new double[2];
+        coordinates[0] = 15.0; // X
+        coordinates[1] = 25.0; // Y
 
         // Then index 0 should be X and index 1 should be Y
-        assertEquals(15, coordinates[0], "Index 0 should be the X coordinate");
-        assertEquals(25, coordinates[1], "Index 1 should be the Y coordinate");
+        assertEquals(15.0, coordinates[0], "Index 0 should be the X coordinate");
+        assertEquals(25.0, coordinates[1], "Index 1 should be the Y coordinate");
     }
 }
