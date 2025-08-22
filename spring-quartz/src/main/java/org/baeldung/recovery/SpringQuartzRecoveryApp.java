3518be75af94da1a8d97944cde2b99e80c4b8c68
hmdrz <hmdrzsharifi@gmail.com>
2025-08-22 23:50:42 +0330

    #BAEL-8084: add SpringBoot main class

---
+++ spring-quartz/src/main/java/org/baeldung/recovery/SpringQuartzRecoveryApp.java
@@ -0,0 +1,12 @@
+package org.baeldung.recovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuartzRecoveryApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringQuartzRecoveryApp.class, args);
+    }
+}
