f5d07eb99a988d3a31617ab9b18e87387790be29
vshanbha <vshanbhag@neurobyte.in>
2025-08-23 13:40:41 +0200

    BAEL-9141 unit test class name modified

--- quarkus-modules/quarkus-extension/quarkus-infinispan-embedded/src/test/java/com/baeldung/quarkus/infinispan/InfinispanCacheServiceUnitTest.java
+++ quarkus-modules/quarkus-extension/quarkus-infinispan-embedded/src/test/java/com/baeldung/quarkus/infinispan/InfinispanCacheServiceUnitTest.java
@@ -15,7 +15,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import jakarta.inject.Inject;
 
 @QuarkusTest
-class InfinispanCacheServiceTest {
+class InfinispanCacheServiceUnitTest {
 
     @Inject
     InfinispanCacheService cacheService;
