eee7e2c1d2832f085f855226271376457881bda3
hmdrz <hmdrzsharifi@gmail.com>
2025-08-24 19:27:15 +0330

    #BAEL-8084: add profile

--- spring-quartz/src/main/java/org/baeldung/recovery/SpringQuartzRecoveryApp.java
+++ spring-quartz/src/main/java/org/baeldung/recovery/SpringQuartzRecoveryApp.java
@@ -2,11 +2,17 @@ package org.baeldung.recovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ComponentScan
+@EnableScheduling
 @SpringBootApplication
 public class SpringQuartzRecoveryApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringQuartzRecoveryApp.class, args);
+        SpringApplication app = new SpringApplication(SpringQuartzRecoveryApp.class);
+        app.setAdditionalProfiles("recovery");
+        app.run(args);
     }
 }
