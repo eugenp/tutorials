e09b651bba5e3b59d2afb8bda7b28b074b3fb1bb
leohelfferich <leo@helfferich.net>
2025-08-25 18:32:06 +0200

    initial

---
+++ testing-modules/junit-5-advanced/src/test/java/com/baeldung/before_all_global/ExampleTest.java
@@ -0,0 +1,25 @@
+package com.baeldung.before_all_global;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @BeforeAll
+    static void setup() {
+        System.out.println("Execute: BeforeAll");
+        // Initialize class-specific resources, e.g., a mock database
+    }
+
+    @Test
+    void test1() {
+        System.out.println("Execute test 1");
+        // Test logic
+    }
+
+    @Test
+    void test2() {
+        System.out.println("Execute test 2");
+        // Test logic
+    }
+}
