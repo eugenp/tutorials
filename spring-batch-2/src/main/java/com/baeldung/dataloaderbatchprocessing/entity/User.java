7a2d9a3c68b47c67355ea84609340b8564c0eb4b
Neetika23 <kwal.neetika@gmail.com>
2025-08-25 19:44:39 +0530

    spy changes

--- spring-batch-2/src/main/java/com/baeldung/dataloaderbatchprocessing/entity/User.java
+++ spring-batch-2/src/main/java/com/baeldung/dataloaderbatchprocessing/entity/User.java
@@ -2,11 +2,13 @@ package com.baeldung.dataloaderbatchprocessing.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
--- spring-batch-2/src/test/java/com/baeldung/dataloaderbatchprocessing/DataLoaderUnitTest.java
+++ spring-batch-2/src/test/java/com/baeldung/dataloaderbatchprocessing/DataLoaderUnitTest.java
@@ -1,57 +1,60 @@
 package com.baeldung.dataloaderbatchprocessing;
 
 import com.baeldung.dataloaderbatchprocessing.entity.User;
+import com.baeldung.dataloaderbatchprocessing.repository.UserRepository;
 import com.baeldung.dataloaderbatchprocessing.service.UserService;
-
 import org.dataloader.DataLoader;
+import org.dataloader.DataLoaderRegistry;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = DataLoaderApp.class)
-public class DataLoaderUnitTest {
+class UserDataLoaderIntegrationTest {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @SpyBean
     private UserService userService;
-    private UserDataLoader loaderFactory;
+
+    private DataLoader<String, User> userDataLoader;
 
     @BeforeEach
     void setUp() {
-        userService = mock(UserService.class);
-        loaderFactory = new UserDataLoader(userService);
-    }
+        userRepository.deleteAll();
 
-    @Test
-    void givenThreeUsers_whenUsingDataLoader_thenLoadUsersInSingleDBCall() throws Exception {
         User user1 = new User("101", "User_101");
         User user2 = new User("102", "User_102");
         User user3 = new User("103", "User_103");
+        userRepository.saveAll(Arrays.asList(user1, user2, user3));
 
-        List<User> users = Arrays.asList(user1, user2, user3);
+        userDataLoader = new DataLoader<>(userService::getUsersByIds);
+        DataLoaderRegistry registry = new DataLoaderRegistry();
+        registry.register("userDataLoader", userDataLoader);
+    }
 
-        when(userService.getUsersByIds(Arrays.asList("101", "102", "103"))).thenReturn(CompletableFuture.completedFuture(users));
+    @Test
+    void whenLoadingUsers_thenBatchLoaderIsInvokedAndResultsReturned() {
+        CompletableFuture<User> userFuture1 = userDataLoader.load("101");
+        CompletableFuture<User> userFuture2 = userDataLoader.load("102");
+        CompletableFuture<User> userFuture3 = userDataLoader.load("103");
 
-        DataLoader<String, User> userLoader = loaderFactory.createUserLoader();
+        userDataLoader.dispatchAndJoin();
 
-        CompletableFuture<User> future1 = userLoader.load("101");
-        CompletableFuture<User> future2 = userLoader.load("102");
-        CompletableFuture<User> future3 = userLoader.load("103");
+        verify(userService, times(1)).getUsersByIds(anyList());
 
-        userLoader.dispatch();
-
-        assertEquals("User_101", future1.get()
-            .getName());
-        assertEquals("User_102", future2.get()
-            .getName());
-        assertEquals("User_103", future3.get()
-            .getName());
-
-        verify(userService, times(1)).getUsersByIds(Arrays.asList("101", "102", "103"));
+        assertThat(userFuture1.join().getName()).isEqualTo("User_101");
+        assertThat(userFuture2.join().getName()).isEqualTo("User_102");
+        assertThat(userFuture3.join().getName()).isEqualTo("User_103");
     }
 }
+
