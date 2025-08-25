3a3df389ea4f76daa593cad82b64f2dd8ce5442f
leohelfferich <leo@helfferich.net>
2025-08-25 18:38:42 +0200

    revert

--- persistence-modules/spring-boot-persistence-5/src/main/java/com/baeldung/transactional/TestOrderRepository.java
+++
@@ -1,8 +0,0 @@
-package com.baeldung.transactional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TestOrderRepository extends JpaRepository<TestOrder, Long> {
-}
