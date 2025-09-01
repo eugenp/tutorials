f52185e482928d2fa16050ba1ed1af7b571dfa5d
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-09-01 13:51:18 +0530

    explicitly enable cascade and lazy loading

--- persistence-modules/hibernate6/src/main/java/com/baeldung/statelesssession/Article.java
+++ persistence-modules/hibernate6/src/main/java/com/baeldung/statelesssession/Article.java
@@ -2,6 +2,7 @@ package com.baeldung.statelesssession;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -20,7 +21,7 @@ class Article {
     @Column(nullable = false)
     private String title;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "author_id", nullable = false)
     private Author author;
 
--- persistence-modules/hibernate6/src/main/java/com/baeldung/statelesssession/Author.java
+++ persistence-modules/hibernate6/src/main/java/com/baeldung/statelesssession/Author.java
@@ -1,7 +1,9 @@
 package com.baeldung.statelesssession;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +23,7 @@ class Author {
     @Column(nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "author")
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Article> articles;
 
     public Long getId() {
