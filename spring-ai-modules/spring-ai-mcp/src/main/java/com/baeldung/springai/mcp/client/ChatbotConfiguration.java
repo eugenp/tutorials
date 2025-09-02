3dc141a3352725b1ee801074d388f5b631d511ea
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-09-02 21:42:38 +0530

    codebase/spring-ai-mcp [BAEL-9372] [Improvement] (#18780)
    * fix: remove pom packaging from executable module
    
    * upgrade spring-ai version
    
    * add conditional tool registration process
    
    * add notifyToolsListChanged() invocation dusring conditional tool registration
    
    * configure McpSyncClientCustomizer bean to detect tool changes

--- spring-ai-modules/spring-ai-mcp/src/main/java/com/baeldung/springai/mcp/client/ChatbotConfiguration.java
+++ spring-ai-modules/spring-ai-mcp/src/main/java/com/baeldung/springai/mcp/client/ChatbotConfiguration.java
@@ -1,13 +1,19 @@
 package com.baeldung.springai.mcp.client;
 
+import io.modelcontextprotocol.spec.McpSchema;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.mcp.SyncMcpToolCallbackProvider;
+import org.springframework.ai.mcp.customizer.McpSyncClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 class ChatbotConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(ChatbotConfiguration.class);
 
     @Bean
     ChatClient chatClient(ChatModel chatModel, SyncMcpToolCallbackProvider toolCallbackProvider) {
@@ -15,6 +21,15 @@ class ChatbotConfiguration {
             .builder(chatModel)
             .defaultToolCallbacks(toolCallbackProvider.getToolCallbacks())
             .build();
+    }
+
+    @Bean
+    McpSyncClientCustomizer mcpSyncClientCustomizer() {
+        return (name, mcpClientSpec) -> {
+            mcpClientSpec.toolsChangeConsumer(tools -> {
+                logger.info("Detected tools changes.");
+            });
+        };
     }
 
 }
