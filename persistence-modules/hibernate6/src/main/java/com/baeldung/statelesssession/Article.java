9fb8de1f2246773e61123042732a168859d4edb9
Hardik Singh Behl <hardik.behl7444@gmail.com>
2025-09-05 23:55:48 +0530

    codebase/hibernate-stateless-session [BAEL-8322] (#18778)
    * add domain entities
    
    * add main application entry
    
    * add test cases
    
    * explicitly enable cascade and lazy loading
    
    * improve indentation
    
    * add test cases for dirty checking
    
    * add test case for loading association eagerly using EntityGraph
    
    * refactor: update variable name

---
+++ persistence-modules/hibernate6/src/main/java/com/baeldung/statelesssession/Article.java
@@ -0,0 +1,48 @@
+package com.baeldung.statelesssession;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "articles")
+class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+}
