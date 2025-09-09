289203d86bccd5326c100f77f6332f6a7e59e6ef
Pedro Lopes <pedroinf97@gmail.com>
2025-09-08 21:47:11 -0300

    new maven execution. adds dependencies. adds jackson types dependencies

---
+++ spring-boot-modules/spring-boot-openapi/src/main/java/com/baeldung/tutorials/openapi/generatehttpclients/GenerateHttpClientSpringApplication.java
@@ -0,0 +1,14 @@
+package com.baeldung.tutorials.openapi.generatehttpclients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.baeldung.tutorials.openapi.quotes.QuotesApplication;
+
+@SpringBootApplication
+public class GenerateHttpClientSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuotesApplication.class, args);
+    }
+}
