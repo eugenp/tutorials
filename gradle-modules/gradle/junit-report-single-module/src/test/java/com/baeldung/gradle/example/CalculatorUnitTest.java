478ba808c31f85c6d2483bd6afce7f88824ef947
sverma1-godaddy <sverma1@godaddy.com>
2025-09-13 14:26:30 +0530

    renaming test file

--- gradle-modules/gradle/junit-report-single-module/src/test/java/com/baeldung/gradle/example/CalculatorUnitTest.java
+++ gradle-modules/gradle/junit-report-single-module/src/test/java/com/baeldung/gradle/example/CalculatorUnitTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class CalculatorTest {
+public class CalculatorUnitTest {
 
     private Calculator calculator;
 
