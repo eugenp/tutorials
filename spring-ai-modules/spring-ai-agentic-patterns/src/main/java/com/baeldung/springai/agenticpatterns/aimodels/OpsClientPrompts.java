c4730d1a450a96c91fe77fa6c8873b1f45b8a18c
Stelios Anastasakis <bpstelios10@hotmail.com>
2025-09-16 21:04:29 +0300

    [BAEL-9419] Introduce the orchestrator-workers-workflow pattern

--- spring-ai-modules/spring-ai-agentic-patterns/src/main/java/com/baeldung/springai/agenticpatterns/aimodels/OpsClientPrompts.java
+++ spring-ai-modules/spring-ai-agentic-patterns/src/main/java/com/baeldung/springai/agenticpatterns/aimodels/OpsClientPrompts.java
@@ -2,7 +2,8 @@ package com.baeldung.springai.agenticpatterns.aimodels;
 
 public final class OpsClientPrompts {
 
-    private OpsClientPrompts() {}
+    private OpsClientPrompts() {
+    }
 
     /**
      * Array of steps to be taken for the dev pipeline
@@ -35,7 +36,7 @@ public final class OpsClientPrompts {
 					   else return success""" };
 
     /**
-     * Array of steps to be taken for the dev pipeline
+     * Prompt for the deployment of a container to one or many environments
      */
     public static final String NON_PROD_DEPLOYMENT_PROMPT =
       // Prompt For Deployment
@@ -44,4 +45,21 @@ public final class OpsClientPrompts {
         If any prod environment is requested, fail.
         If error occurs, return the error,
            else return success message.""";
+
+    /**
+     * Array of steps to be taken for deployment and test execution against this environment
+     */
+    public static final String[] EXECUTE_TEST_ON_DEPLOYED_ENV_STEPS = {
+      // Prompt For Deployment. If successful, pass to the next step the PR link and the environment.
+      """
+        Deploy the container associated to the given PR, on the given environment.
+        If any prod environment is requested, fail.
+        If error occurs, return the error,
+           else return an array of strings: '{the PR link] on {the environment}'.""",
+      // Continue with running the tests from the code base that are related to this env.
+      // Which means, run the Functional Tests on 'test' env, the Integration Tests on 'int', etc.
+      """
+        Execute the tests from the codebase version provided, that are related to the environment provided.
+        If error occurs, return the error,
+          else return the environment as title and then the test outcome.""" };
 }
