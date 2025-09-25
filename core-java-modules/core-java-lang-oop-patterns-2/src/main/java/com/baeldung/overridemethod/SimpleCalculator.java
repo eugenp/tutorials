1301b5f2bee0d522ce95060ba40dc5a15cd0aca7
Deepak Vohra <dvohra10@yahoo.com>
2025-09-25 16:04:57 -0700

    Update SimpleCalculator.java

--- core-java-modules/core-java-lang-oop-patterns-2/src/main/java/com/baeldung/overridemethod/SimpleCalculator.java
+++ core-java-modules/core-java-lang-oop-patterns-2/src/main/java/com/baeldung/overridemethod/SimpleCalculator.java
@@ -1,13 +1,20 @@
 package com.baeldung.overridemethod;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SimpleCalculator implements Calculator {
+    private static final Logger log = LoggerFactory.getLogger(SimpleCalculator.class);
+
     @Override
     public int add(int a, int b) {
+        log.info("SimpleCalculator: Adding {} and {}", a, b); // Use parameterized logging {}
         return a + b;
     }
 
     @Override
     public int subtract(int a, int b) {
+        log.info("SimpleCalculator: Subtracting {} from {}", b, a);
         return a - b;
     }
 }
