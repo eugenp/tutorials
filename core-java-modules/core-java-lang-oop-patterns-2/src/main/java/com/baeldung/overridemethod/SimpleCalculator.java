94fbeb1dc047e128d8462a1bfc1dfc9afb1d8c35
Deepak Vohra <dvohra10@yahoo.com>
2025-09-25 15:39:18 -0700

    Create SimpleCalculator.java

---
+++ core-java-modules/core-java-lang-oop-patterns-2/src/main/java/com/baeldung/overridemethod/SimpleCalculator.java
@@ -0,0 +1,13 @@
+package com.baeldung.overridemethod;
+
+public class SimpleCalculator implements Calculator {
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+}
