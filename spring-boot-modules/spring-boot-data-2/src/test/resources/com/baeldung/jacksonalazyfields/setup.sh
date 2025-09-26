6445e75ea3d1b05095084c101d5e33a0b00e11c1
ulisseslima <ulisses@dvlcube.com>
2025-09-26 17:33:33 -0300

    removing setup

--- spring-boot-modules/spring-boot-data-2/src/test/resources/com/baeldung/jacksonalazyfields/setup.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-curl -X POST http://localhost:8080/departments -H "Content-Type: application/json" -d '{"name":"Computer Science"}'
-curl -X POST http://localhost:8080/courses -H "Content-Type: application/json" -d '{"name":"machine learning", "department":{"id":1} }'
-curl -X POST http://localhost:8080/courses -H "Content-Type: application/json" -d '{"name":"algorithms", "department":{"id":1} }'
-curl -X GET http://localhost:8080/courses/1 | jq
-curl -X GET http://localhost:8080/departments/1 | jq
-
-curl -X POST http://localhost:8080/departments/1/course -H "Content-Type: application/json" -d '{"name":"crazy course"}' | jq
