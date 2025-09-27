486d06cbac1159f895e8bd8e781f9b38a93bb0ef
programenth <139003443+programenth@users.noreply.github.com>
2025-09-27 13:58:24 +0000

    reorganizing code and adding unit test

--- testing-modules/selenium/src/main/java/com/baeldung/selenium/avoidbot/AvoidBotDetectionSelenium.java
+++ testing-modules/selenium/src/main/java/com/baeldung/selenium/avoidbot/AvoidBotDetectionSelenium.java
@@ -1,36 +1,15 @@
-package com.baeldung.selenium.avoidbotDetectionSelenium;
+package com.baeldung.selenium.avoidbot;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebElement;
+import com.baeldung.selenium.avoidbot.GoogleSearchService;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AvoidBotDetectionSelenium {
 
     public static void main(String[] args) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-blink-features=AutomationControlled");
-
-        ChromeDriver driver = new ChromeDriver(options);
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("source", "Object.defineProperty(navigator, 'webdriver', { get: () => undefined })");
-        driver.executeCdpCommand("Page.addScriptToEvaluateOnNewDocument", params);
-        driver.get("https://www.google.com");
-        System.out.println("Navigated to Google's homepage.");
-
-        WebElement searchBox = driver.findElement(By.name("q"));
-        System.out.println("Found the search box.");
-
-        searchBox.sendKeys("baeldung");
-        System.out.println("Entered 'baeldung' into the search box.");
-
-        searchBox.sendKeys(Keys.ENTER);
-        System.out.println("Submitted the search query.");
-        System.out.println("Page title is: " + driver.getTitle());
-
-        driver.quit();
+        ChromeDriver driver = WebDriverFactory.createDriver();
+        GoogleSearchService googleService = new GoogleSearchService(driver);
+        googleService.navigateToGoogle();
+        googleService.search("baeldung");
+        googleService.quit();
     }
 }
