fe27dba34749fda897c439eb01ff97aacced2862
Pedro Lopes <pedroinf97@gmail.com>
2025-10-01 19:38:54 -0300

    final version changes

--- spring-boot-modules/spring-boot-openapi/src/main/java/com/baeldung/tutorials/openapi/generatehttpclients/GenerateHttpClientSpringApplication.java
+++ spring-boot-modules/spring-boot-openapi/src/main/java/com/baeldung/tutorials/openapi/generatehttpclients/GenerateHttpClientSpringApplication.java
@@ -3,13 +3,11 @@ package com.baeldung.tutorials.openapi.generatehttpclients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.baeldung.tutorials.openapi.quotes.QuotesApplication;
-
 @SpringBootApplication
 public class GenerateHttpClientSpringApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(QuotesApplication.class, args);
+        SpringApplication.run(GenerateHttpClientSpringApplication.class, args);
 
     }
 }
