89450307bd0cbb5736da300a3fdc74e40cc0f041
Loredana Crusoveanu <lore.crusoveanu@gmail.com>
2025-10-10 12:18:09 +0300

    add missing code for http interface

--- spring-boot-modules/spring-boot-3-2/src/test/java/com/baeldung/httpinterface/BooksServiceMockitoUnitTest.java
+++ spring-boot-modules/spring-boot-3-2/src/test/java/com/baeldung/httpinterface/BooksServiceMockitoUnitTest.java
@@ -1,5 +1,12 @@
 package com.baeldung.httpinterface;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Answers;
@@ -8,16 +15,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.reactive.function.client.WebClient;
+
 import reactor.core.publisher.Mono;
-
-import static org.mockito.BDDMockito.*;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
 class BooksServiceMockitoUnitTest {
@@ -25,11 +25,8 @@ class BooksServiceMockitoUnitTest {
     @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     private WebClient webClient;
 
-    //@InjectMocks
-    private BooksClient booksClient;
-    
     @InjectMocks
-    private BooksService booksService;
+    private BooksClient booksClient;
 
     @Test
     void givenMockedWebClientReturnsTwoBooks_whenGetBooksServiceMethodIsCalled_thenListOfTwoBooksIsReturned() {
@@ -41,7 +38,8 @@ class BooksServiceMockitoUnitTest {
             new Book(1,"Book_1", "Author_1", 1998),
             new Book(2, "Book_2", "Author_2", 1999)
           )));
-        // BooksService booksService = booksClient.getBooksService();
+
+        BooksService booksService = booksClient.getBooksService();
         List<Book> books = booksService.getBooks();
         assertEquals(2, books.size());
     }
@@ -54,7 +52,7 @@ class BooksServiceMockitoUnitTest {
           .bodyToMono(new ParameterizedTypeReference<Book>(){}))
           .willReturn(Mono.just(new Book(1,"Book_1", "Author_1", 1998)));
 
-        //BooksService booksService = booksClient.getBooksService();
+        BooksService booksService = booksClient.getBooksService();
         Book book = booksService.getBook(1);
         assertEquals("Book_1", book.title());
     }
@@ -67,24 +65,9 @@ class BooksServiceMockitoUnitTest {
           .bodyToMono(new ParameterizedTypeReference<Book>(){}))
           .willReturn(Mono.just(new Book(3, "Book_3", "Author_3", 2000)));
 
-        //BooksService booksService = booksClient.getBooksService();
+        BooksService booksService = booksClient.getBooksService();
         Book book = booksService.saveBook(new Book(3, "Book_3", "Author_3", 2000));
         assertEquals("Book_3", book.title());
-    }
-
-    @Test
-    void givenMockedWebClientReturnsOk_whenDeleteBookServiceMethodIsCalled_thenOkCodeIsReturned() {
-        given(webClient.method(HttpMethod.DELETE)
-          .uri(anyString(), anyMap())
-          .retrieve()
-          .toBodilessEntity()
-          .block(any())
-          .getStatusCode())
-          .willReturn(HttpStatusCode.valueOf(200));
-
-        //BooksService booksService = booksClient.getBooksService();
-        ResponseEntity<Void> response = booksService.deleteBook(3);
-        assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
     }
 
 }
