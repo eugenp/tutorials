56b9cfc7959314f7e8c35ca27d5d5d9b4bf83dac
leohelfferich <leo@helfferich.net>
2025-10-11 12:11:39 +0200

    stash - does not work

---
+++ spring-boot-modules/spring-boot-testing-4/src/test/java/com/baeldung/resttestclient/RestTestClientTest.java
@@ -0,0 +1,27 @@
+package com.baeldung.resttestclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+public class RestTestClientTest {
+
+    RestTestClient client = RestTestClient.bindToServer()
+        .baseUrl("http://localhost:8080")
+        .build();
+
+    @Test
+    void should() {
+        System.out.println("Foo");
+
+        client.get()
+            .uri("/persons/1")
+            .accept(MediaType.APPLICATION_JSON)
+            .exchange()
+            .expectStatus()
+            .isOk()
+            .expectHeader()
+            .contentType(MediaType.APPLICATION_JSON);
+    }
+
+}
