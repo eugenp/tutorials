03828e0b17ae25c831a737fea7f17ebd6879f0cf
sverma1-godaddy <sverma1@godaddy.com>
2025-10-12 22:42:33 +0530

    BAEL-9453 by sgrverma23: changes of ways to configure the Order of Configuration in Spring Boot

---
+++ spring-boot-modules/spring-boot-config-order/src/test/java/com/baeldung/defaultconfig/DefaultConfigOrderTest.java
@@ -0,0 +1,23 @@
+package com.baeldung.defaultconfig;
+
+import com.baeldung.application.defaultconfig.ConfigA;
+import com.baeldung.application.defaultconfig.ConfigB;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(classes = {ConfigA.class, ConfigB.class})
+class DefaultConfigOrderTest {
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Test
+    void givenConfigsWithoutOrder_whenLoaded_thenBeansExistRegardlessOfOrder() {
+        assertThat(context.getBean("beanA")).isEqualTo("Bean A");
+        assertThat(context.getBean("beanB")).isEqualTo("Bean B");
+    }
+}
