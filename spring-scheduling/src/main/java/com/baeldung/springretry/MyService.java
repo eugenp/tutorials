0e5b5ecf019e01563c06102e2445a81512ea3aa5
Deepak Vohra <dvohra10@yahoo.com>
2025-10-17 12:47:06 -0700

    Remove unnecessary blank line in MyService.java

--- spring-scheduling/src/main/java/com/baeldung/springretry/MyService.java
+++ spring-scheduling/src/main/java/com/baeldung/springretry/MyService.java
@@ -1,7 +1,7 @@
 package com.baeldung.springretry;
 
 import java.sql.SQLException;
- 
+
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
@@ -24,6 +24,6 @@ public interface MyService {
 
     @Recover
     void recover(SQLException e, String sql);
-    
+ 
     void templateRetryService();
 }
