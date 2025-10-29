0b4212b032882959a7ee9fe97da09f55c248ad1e
Nikhil Bhargava <nikhil1.bhargava@ril.com>
2025-10-29 09:27:34 +0530

    BAEL-8807: Check if a number can be written as a sum of two squares in Java

---
+++ algorithms-modules/algorithms-numeric/src/test/java/com/baeldung/algorithms/sumoftwosquares/NumberAsSumOfTwoSquaresTest.java
@@ -0,0 +1,43 @@
+package com.baeldung.algorithms.sumoftwosquares;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+class NumberAsSumOfTwoSquaresTest {
+
+    @Test
+    void IsNumberSumOfTwoSquaresValid() {
+        // Simple cases
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(0)); // 0^2 + 0^2
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(1)); // 1^2 + 0^2
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(5)); // 1^2 + 2^2
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(8)); // 2^2 + 2^2
+
+        // Cases from Fermat theorem
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(50)); // 2 * 5^2. No 4k+3 primes.
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(45)); // 3^2 * 5. 4k+3 prime (3) has even exp.
+        assertTrue(NumberAsSumOfTwoSquares.isSumOfTwoSquares(18)); // 2 * 3^2. 4k+3 prime (3) has even exp.
+    }
+
+    @Test
+    void IsNumberNotSumOfTwoSquaresValid() {
+        // Simple cases
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(3));  // 3 (4k+3, exp 1)
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(6));  // 2 * 3 (3 has exp 1)
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(7));  // 7 (4k+3, exp 1)
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(11)); // 11 (4k+3, exp 1)
+
+        // Cases from theorem
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(12)); // 2^2 * 3 (3 has exp 1)
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(21)); // 3 * 7 (both 3 and 7 have exp 1)
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(28)); // 2^2 * 7 (7 has exp 1)
+    }
+
+    @Test
+    void IsNumberNegative() {
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(-1)); // Negatives as hygiene
+        assertFalse(NumberAsSumOfTwoSquares.isSumOfTwoSquares(-10)); // Negatives as hygiene
+    }
+}
