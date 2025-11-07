c5a6ece6b7a1492275a0e4924d4399e9619bfdbc
Bipinkumar27 <sayy2bipin@gmail.com>
2025-11-07 21:12:13 +0530

    JAVA-48967: Changes made for moving below modules into spring-remoting-modules: 1.spring-boot-azure-deployment 2.spring-boot-heroku-deployment

--- spring-remoting-modules/spring-boot-azure-deployment/src/main/java/com/baeldung/springboot/azure/User.java
+++ spring-remoting-modules/spring-boot-azure-deployment/src/main/java/com/baeldung/springboot/azure/User.java
@@ -1,9 +1,9 @@
 package com.baeldung.springboot.azure;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * @author aiet
