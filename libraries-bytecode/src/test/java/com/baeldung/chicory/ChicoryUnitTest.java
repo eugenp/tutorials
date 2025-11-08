4784da54e5dc61f5b884d74fead117fb526086d4
Francesco Galgani <francesco.galgani@protonmail.com>
2025-11-08 22:10:15 +0100

    BAEL-9118

---
+++ libraries-bytecode/src/test/java/com/baeldung/chicory/ChicoryUnitTest.java
@@ -0,0 +1,14 @@
+package com.baeldung.chicory;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class ChicoryUnitTest {
+
+    @Test
+    void whenProjectLoads_thenTestsRun() {
+        assertTrue(true);
+    }
+}
+
