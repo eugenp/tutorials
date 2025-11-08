19eb42aad93aa9600e3202d9c9a930095912f625
Wynn Teo <wynnteo@Wynns-MacBook-Pro.local>
2025-11-08 12:41:44 +0800

    BAEL-9468

--- spring-boot-modules/spring-boot-mvc-2/src/main/java/com/baeldung/postman/controller/PostmanUploadController.java
+++ spring-boot-modules/spring-boot-mvc-2/src/main/java/com/baeldung/postman/controller/PostmanUploadController.java
@@ -1,5 +1,6 @@
 package com.baeldung.postman.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,18 +17,28 @@ public class PostmanUploadController {
     @PostMapping("/uploadFile")
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
         return ResponseEntity.ok()
-          .body("file received successfully");
+            .body("file received successfully");
     }
 
     @PostMapping("/uploadJson")
     public ResponseEntity<String> handleJsonInput(@RequestBody JsonRequest json) {
         return ResponseEntity.ok()
-          .body(json.getId() + json.getName());
+            .body(json.getId() + json.getName());
     }
 
     @PostMapping("/uploadJsonAndMultipartData")
     public ResponseEntity<String> handleJsonAndMultipartInput(@RequestPart("data") JsonRequest json, @RequestPart("file") MultipartFile file) {
         return ResponseEntity.ok()
-          .body(json.getId() + json.getName());
+            .body(json.getId() + json.getName());
+    }
+
+    @PostMapping(value = "/uploadSingleFile", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> handleSingleFileUpload(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok("file received successfully");
+    }
+
+    @PostMapping(value = "/uploadJsonAndMultipartData", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<String> handleUploadeJsonAndMultipartInput(@RequestPart("data") JsonRequest json, @RequestPart("file") MultipartFile file) {
+        return ResponseEntity.ok(json.getId() + json.getName());
     }
 }
