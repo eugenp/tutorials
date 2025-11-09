b4a026e14bbaa1e5559cb654d36a0b7d9e4f1161
saikat <saikatcse03@gmail.com>
2025-11-09 19:18:46 +0100

    refactor code

--- apache-kafka-2/src/main/java/com/baeldung/kafkastreams/UserSerializer.java
+++ apache-kafka-2/src/main/java/com/baeldung/kafkastreams/UserSerializer.java
@@ -2,10 +2,7 @@ package com.baeldung.kafkastreams;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 import org.apache.kafka.common.serialization.Serializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserSerializer implements Serializer<User> {
     private final ObjectMapper mapper = new ObjectMapper();
