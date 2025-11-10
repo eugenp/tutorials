7439a870ac9a0a388097d05cc3195477e40a1e10
Francesco Galgani <francesco.galgani@protonmail.com>
2025-11-10 21:08:58 +0100

    BAEL-9118: givenAddModule_whenCallingAddWithTwoAndForty_thenResultIsFortyTwo

--- libraries-bytecode/src/test/java/com/baeldung/chicory/ChicoryUnitTest.java
+++ libraries-bytecode/src/test/java/com/baeldung/chicory/ChicoryUnitTest.java
@@ -1,14 +1,28 @@
 package com.baeldung.chicory;
 
-import static org.junit.Assert.assertTrue;
-
+import com.dylibso.chicory.runtime.ExportFunction;
+import com.dylibso.chicory.runtime.Instance;
+import com.dylibso.chicory.wasm.Parser;
 import org.junit.jupiter.api.Test;
+
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ChicoryUnitTest {
 
     @Test
-    void whenProjectLoads_thenTestsRun() {
-        assertTrue(true);
+    void givenAddModule_whenCallingAddWithTwoAndForty_thenResultIsFortyTwo() {
+        InputStream wasm = getClass().getResourceAsStream("/wasm/add.wasm");
+        assertNotNull(wasm);
+
+        var module = Parser.parse(wasm);
+        Instance instance = Instance.builder(module).build();
+        ExportFunction add = instance.export("add");
+
+        long[] result = add.apply(2, 40);
+
+        assertEquals(42, (int) result[0]);
     }
 }
-
