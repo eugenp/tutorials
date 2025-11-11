1675e932eaa7c7a5878d9198d4255b35ee146b98
Wynn Teo <wynnteo@Wynns-MacBook-Pro.local>
2025-11-11 17:12:10 +0800

    BAEL-9464

---
+++ json-modules/json-3/src/main/java/com/baeldung/pojomapping/Address.java
@@ -0,0 +1,22 @@
+package com.baeldung.pojomapping;
+
+public class Address {
+    private String city;
+    private String postalCode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
