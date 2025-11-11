7a3ee93b722bc384d02bc021fa08f45eff67a612
dvohra09 <dvohra09@yahoo.com>
2025-11-10 18:28:38 -0800

    Update BlobStringConverterUnitTest.java

--- core-java-modules/core-java-string-conversions-4/src/test/java/com/baeldung/blobstringconverter/BlobStringConverterUnitTest.java
+++ core-java-modules/core-java-string-conversions-4/src/test/java/com/baeldung/blobstringconverter/BlobStringConverterUnitTest.java
@@ -1,8 +1,11 @@
+package com.baeldung.blobstringconverter;
+
 import org.junit.jupiter.api.Test;
 import java.sql.Blob;
 import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class BlobStringConverterUnitTest {
 
