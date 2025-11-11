3552a7a28e37516496757db9c5b1d1cceb01b611
Amar Wadhwani <ds24m023@technikum-wien.at>
2025-11-12 00:55:16 +0100

    BAEL-9308: Java SDK for MCP

---
+++ java-mcp/src/main/java/mcp/LoggingTool.java
@@ -0,0 +1,26 @@
+package mcp;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import io.modelcontextprotocol.server.McpServerFeatures;
+import io.modelcontextprotocol.spec.McpSchema;
+
+public class LoggingTool {
+
+    public static McpServerFeatures.SyncToolSpecification logPromptTool() {
+        McpSchema.JsonSchema inputSchema = new McpSchema.JsonSchema("object", Map.of("prompt", String.class), List.of("prompt"), false, null, null);
+        Map<String, Object> outputSchema = new HashMap<>();
+        outputSchema.put("logged", "string");
+
+        return new McpServerFeatures.SyncToolSpecification(
+            new McpSchema.Tool("logPrompt", "Log Prompt", "Logs a provided prompt", inputSchema, outputSchema, null, null), (exchange, args) -> {
+            String prompt = (String) args.get("prompt");
+            return McpSchema.CallToolResult.builder()
+                .content(List.of(new McpSchema.TextContent("Input Prompt: " + prompt)))
+                .isError(false)
+                .build();
+        });
+    }
+}
