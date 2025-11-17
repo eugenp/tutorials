cc2841c1384645eba22bca5e721cc0ca532a18b3
Deepak Vohra <dvohra10@yahoo.com>
2025-11-16 17:00:05 -0800

    Add ConcurrencyLimit import to MyService

--- spring-scheduling/src/main/java/com/baeldung/springretry/MyService.java
+++ spring-scheduling/src/main/java/com/baeldung/springretry/MyService.java
@@ -1,7 +1,7 @@
 package com.baeldung.springretry;
 
 import java.sql.SQLException;
-
+import org.springframework.resilience.annotation.ConcurrencyLimit;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
 import org.springframework.retry.annotation.Retryable;
