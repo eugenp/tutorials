f15e95918130fc58abb9eeaad46d14dde887d91e
MBuczkowski2025 <mbuczkowski2021@gmail.com>
2025-11-18 03:11:46 +0100

    BAEL-5641 How to Fix Slow SecureRandom Generator (#18897)
    * BAEL-5641_add_sample_code
    
    * BAEL-5641 add JHM benchmark
    
    * BAEL-5641 parametrized version number in POM and formatting fix

---
+++ core-java-modules/core-java-security-2/src/main/java/com/baeldung/securerandomtester/SecureRandomPerformanceTest.java
@@ -0,0 +1,51 @@
+package com.baeldung.securerandomtester;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.concurrent.TimeUnit;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class SecureRandomPerformanceTest {
+
+    SecureRandom randomNativePRNGBlocking;
+    SecureRandom randomNativePRNGNonBlocking;
+
+    final int NBYTES = 256;
+    final int NSAMPLES = 20_000;
+
+    @Setup(Level.Trial)
+    public void setup() throws NoSuchAlgorithmException {
+        randomNativePRNGBlocking = SecureRandom.getInstance("NativePRNGBlocking");
+        randomNativePRNGNonBlocking = SecureRandom.getInstance("NativePRNGNonBlocking");
+    }
+
+    @Benchmark
+    public void measureTimePRNGBlocking() {
+        byte[] randomBytes = new byte[NBYTES];
+        for (int i = 0; i < NSAMPLES; i++) {
+            randomNativePRNGBlocking.nextBytes(randomBytes);
+        }
+    }
+
+    @Benchmark
+    public void measureTimePRNGNonBlocking() {
+        byte[] randomBytes = new byte[NBYTES];
+        for (int i = 0; i < NSAMPLES; i++) {
+            randomNativePRNGNonBlocking.nextBytes(randomBytes);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
