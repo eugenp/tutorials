837d004e4cbe5e3dc79c9bfbe2ea2b4ab106d2f0
Deepak Vohra <dvohra10@yahoo.com>
2025-11-22 09:27:50 -0800

    Migrate AsyncAnnotationExampleIntegrationTest to JUnit 5
    Updated test class to use JUnit 5 and CompletableFuture.

--- spring-scheduling/src/test/java/com/baeldung/async/AsyncAnnotationExampleIntegrationTest.java
+++ spring-scheduling/src/test/java/com/baeldung/async/AsyncAnnotationExampleIntegrationTest.java
@@ -1,43 +1,59 @@
 package com.baeldung.async;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.CompletableFuture;
 
-import com.baeldung.async.config.SpringAsyncConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+// JUnit 5 Imports
+import org.junit.jupiter.api.Test; 
+import org.junit.jupiter.api.extension.ExtendWith;
+
+// Spring Imports
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
-@RunWith(SpringJUnit5ClassRunner.class)
+// Note: Assuming AsyncComponent and SimpleAsyncService are mock interfaces 
+// or actual service implementations used in your application context.
+// The configuration class (SpringAsyncConfig) needs to be imported here.
+import com.baeldung.async.config.SpringAsyncConfig;
+import com.baeldung.async.SimpleAsyncService; // Added import based on usage in test
+
+// Replace @RunWith(SpringJUnit4ClassRunner.class) with @ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { SpringAsyncConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class AsyncAnnotationExampleIntegrationTest {
 
+    // Assuming AsyncComponent is the bean for void methods
     @Autowired
-    private AsyncComponent asyncAnnotationExample;
-
-    // tests
+    private AsyncComponent asyncAnnotationExample; 
+    
+    // Assuming SimpleAsyncService is the bean for CompletableFuture methods
+    // NOTE: If both methods are on one service, you should only inject one bean.
+    @Autowired 
+    private SimpleAsyncService simpleAsyncService;
 
     @Test
     public void testAsyncAnnotationForMethodsWithVoidReturnType() {
         asyncAnnotationExample.asyncMethodWithVoidReturnType();
+        // Since this is void, we typically assert that the calling thread continues immediately,
+        // but for a simple integration test, invoking it is sufficient.
     }
 
     @Test
     public void testAsyncAnnotationForMethodsWithReturnType() throws InterruptedException, ExecutionException {
  
+        // Using the SimpleAsyncService bean for the CompletableFuture test, as per the original snippet
         CompletableFuture<String> future = simpleAsyncService.asyncMethodWithReturnType();
         System.out.println("Invoking an asynchronous method. " + Thread.currentThread().getName());
-    
+        
         while (true) {
             if (future.isDone()) {
                 System.out.println("Result from asynchronous process - " + future.get()); 
                 break;
             }
-        System.out.println("Continue doing something else. ");
-        Thread.sleep(1000);
+            System.out.println("Continue doing something else. ");
+            Thread.sleep(1000);
         }
     }
 
@@ -48,6 +64,8 @@ public class AsyncAnnotationExampleIntegrationTest {
 
     @Test
     public void testAsyncAnnotationForMethodsWithException() throws Exception {
+        // This test relies on an exception handler (AsyncUncaughtExceptionHandler) 
+        // being configured for void return types.
         asyncAnnotationExample.asyncMethodWithExceptions();
     }
 }
