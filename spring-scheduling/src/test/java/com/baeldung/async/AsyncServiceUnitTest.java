0761855533b2640315c8032e41ed77b7d8408a7d
Deepak Vohra <dvohra10@yahoo.com>
2025-11-22 09:28:47 -0800

    Migrate AsyncServiceUnitTest to JUnit 5

--- spring-scheduling/src/test/java/com/baeldung/async/AsyncServiceUnitTest.java
+++ spring-scheduling/src/test/java/com/baeldung/async/AsyncServiceUnitTest.java
@@ -3,34 +3,42 @@ package com.baeldung.async;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+// JUnit 5 Imports
+import org.junit.jupiter.api.Test; 
+import org.junit.jupiter.api.extension.ExtendWith;
+
+// Spring Imports
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension; // Use JUnit 5 Extension
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import com.baeldung.async.config.SpringAsyncConfig;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+// Replace @RunWith(SpringJUnit4ClassRunner.class) with @ExtendWith(SpringExtension.class)
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { SpringAsyncConfig.class }, loader = AnnotationConfigContextLoader.class)
 public class AsyncServiceUnitTest {
 
     @Autowired
     private AsyncService asyncServiceExample;
 
-    // tests
-
     @Test
     public void testAsyncAnnotationForMergedServicesResponse() throws InterruptedException, ExecutionException {
+         
+        //   we use the injected bean 'asyncServiceExample'.
         CompletableFuture<String> completableFuture = asyncServiceExample.asyncMergeServicesResponse();
 
+        System.out.println("Invoking asynchronous methods. " + Thread.currentThread().getName());
+
+        // This loop simulates the calling thread continuing its work while waiting for the async result.
         while (true) {
             if (completableFuture.isDone()) {
+                System.out.println("Result from asynchronous process - " + completableFuture.get());
                 break;
             }
+            System.out.println("Continue doing something else. ");
             Thread.sleep(1000);
         }
     }
-
 }
