4796bf98b55f59bb68257187017fdfbf8bd15116
dvohra09 <dvohra09@yahoo.com>
2025-11-22 13:42:37 -0800

    Update IntegrationTestSuite.java

--- persistence-modules/spring-data-jpa-query/src/test/java/com/baeldung/persistence/IntegrationTestSuite.java
+++ persistence-modules/spring-data-jpa-query/src/test/java/com/baeldung/persistence/IntegrationTestSuite.java
@@ -1,12 +1,15 @@
 package com.baeldung.persistence;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 
 import com.baeldung.persistence.audit.AuditTestSuite;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ // @formatter:off
+/**
+ * JUnit 5 equivalent of @RunWith(Suite.class) and @Suite.SuiteClasses
+ */
+@Suite
+@SelectClasses({ // @formatter:off
     AuditTestSuite.class
 }) // @formatter:on
 public class IntegrationTestSuite {
