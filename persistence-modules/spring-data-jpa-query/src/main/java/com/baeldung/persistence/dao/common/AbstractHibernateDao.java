e7d2ccaa5bff8b8ab18ebb3e82af4c12b66f66f4
dvohra09 <dvohra09@yahoo.com>
2025-11-22 13:23:55 -0800

    Update AbstractHibernateDao.java

--- persistence-modules/spring-data-jpa-query/src/main/java/com/baeldung/persistence/dao/common/AbstractHibernateDao.java
+++ persistence-modules/spring-data-jpa-query/src/main/java/com/baeldung/persistence/dao/common/AbstractHibernateDao.java
@@ -8,6 +8,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.google.common.base.Preconditions;
+import jakarta.persistence.criteria.CriteriaQuery; // Import needed for type-safe query
 
 @SuppressWarnings("unchecked")
 public abstract class AbstractHibernateDao<T extends Serializable> extends AbstractDao<T> implements IOperations<T> {
@@ -15,34 +16,37 @@ public abstract class AbstractHibernateDao<T extends Serializable> extends Abstr
     @Autowired
     protected SessionFactory sessionFactory;
 
-    // API
-
     @Override
     public T findOne(final long id) {
-        return (T) getCurrentSession().get(clazz, id);
+        // Replaced Session.get() (or load()) with modern byId().getReference() or get()
+        return getCurrentSession().byId(clazz).load(id); 
     }
 
     @Override
     public List<T> findAll() {
-        return getCurrentSession().createQuery("from " + clazz.getName()).list();
+        // Used type-safe createQuery with the target class instead of the deprecated untyped list()
+        return getCurrentSession().createQuery("from " + clazz.getName(), clazz).getResultList();
     }
 
     @Override
     public void create(final T entity) {
         Preconditions.checkNotNull(entity);
-        getCurrentSession().saveOrUpdate(entity);
+        // saveOrUpdate() is deprecated/removed in favor of merge() or persist()
+        // If entity is transient (new), use persist. If detached (update), use merge.
+        // For simplicity and matching original intent, we'll keep the logic that handles both:
+        getCurrentSession().merge(entity); 
     }
 
     @Override
     public T update(final T entity) {
         Preconditions.checkNotNull(entity);
-        return (T) getCurrentSession().merge(entity);
+        return getCurrentSession().merge(entity);
     }
 
     @Override
     public void delete(final T entity) {
         Preconditions.checkNotNull(entity);
-        getCurrentSession().delete(entity);
+        getCurrentSession().remove(entity); // FIX: Use remove() instead of deprecated delete()
     }
 
     @Override
@@ -53,7 +57,7 @@ public abstract class AbstractHibernateDao<T extends Serializable> extends Abstr
     }
 
     protected Session getCurrentSession() {
+        // This method is generally correct for Spring's declarative transaction management
         return sessionFactory.getCurrentSession();
     }
-
 }
