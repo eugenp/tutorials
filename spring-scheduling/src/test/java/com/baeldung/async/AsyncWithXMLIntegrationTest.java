722fc78142ff78ff0d2a10b8dba1f95b33c5b726
Deepak Vohra <dvohra10@yahoo.com>
2025-11-22 09:29:33 -0800

    Migrate AsyncWithXMLIntegrationTest to JUnit 5
    Updated the test class to use JUnit 5 annotations and extensions, replacing JUnit 4 imports.

--- spring-scheduling/src/test/java/com/baeldung/async/AsyncWithXMLIntegrationTest.java
+++ spring-scheduling/src/test/java/com/baeldung/async/AsyncWithXMLIntegrationTest.java
@@ -1,24 +1,33 @@
 package com.baeldung.async;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+// JUnit 5 Imports
+import org.junit.jupiter.api.Test; 
+import org.junit.jupiter.api.extension.ExtendWith;
+
+// Spring Imports
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension; // Use JUnit 5 Extension
 
-@RunWith(SpringJUnit4ClassRunner.class)
+/**
+ * Integration test for methods annotated with @Async, using XML configuration
+ * instead of Java @Configuration classes.
+ * * Replaces @RunWith(SpringJUnit4ClassRunner.class) with @ExtendWith(SpringExtension.class)
+ */
+@ExtendWith(SpringExtension.class)
 @ContextConfiguration("classpath:springAsync-config.xml")
 public class AsyncWithXMLIntegrationTest {
 
     @Autowired
     private AsyncComponent asyncAnnotationExample;
 
-    // tests
-
     @Test
     public void testAsyncAnnotationForMethodsWithVoidReturnType() throws InterruptedException {
+        // Invoking the async method. The calling thread will immediately proceed.
         asyncAnnotationExample.asyncMethodWithVoidReturnType();
+        
+        // Sleep is required in void methods to ensure the asynchronous thread 
+        // has time to execute before the test completes and the application context shuts down.
         Thread.sleep(2000);
     }
-
 }
