c92cf7d17ce26e76af7af4339848ddfbb306c736
Deepak Vohra <dvohra10@yahoo.com>
2025-11-22 08:46:09 -0800

    Refactor LogAppender to simplify ILoggingEvent usage

--- spring-scheduling/src/main/java/com/baeldung/springretry/logging/LogAppender.java
+++ spring-scheduling/src/main/java/com/baeldung/springretry/logging/LogAppender.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import ch.qos.logback.classic.spi.ILoggingEvent; 
 import ch.qos.logback.core.AppenderBase;
 
-public class LogAppender extends AppenderBase<ch.qos.logback.classic.spi.ILoggingEvent> {
+public class LogAppender extends AppenderBase<ILoggingEvent> {
 
     private static final Logger logger = LoggerFactory.getLogger(LogAppender.class);
     private static final List<String> logMessages = new ArrayList<>();
