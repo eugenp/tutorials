6942739e36358719e0c188ccb3ee63e4df81c19d
Deepak Vohra <dvohra10@yahoo.com>
2025-11-24 08:06:51 -0800

    Update LogAppender.java

--- spring-scheduling/src/main/java/com/baeldung/springretry/logging/LogAppender.java
+++ spring-scheduling/src/main/java/com/baeldung/springretry/logging/LogAppender.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ch.qos.logback.classic.spi.ILoggingEvent; 
+import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 
 public class LogAppender extends AppenderBase<ILoggingEvent> {
@@ -21,5 +21,13 @@ public class LogAppender extends AppenderBase<ILoggingEvent> {
 
     public static List<String> getLogMessages() {
         return logMessages;
+    }
+
+    /**
+     * Clears the static list of captured log messages.
+     * Used in test setup (@Before) to ensure a clean slate for each test case.
+     */
+    public static void clearLogMessages() {
+        logMessages.clear();
     }
 }
