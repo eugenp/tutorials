d006b7202d4b1cf7731951b98e2f8d71f529ba8a
Sourov Jajodia <46421338+Sourov72@users.noreply.github.com>
2025-11-23 23:57:03 -0500

    Refactor EBCDIC to ASCII example with logging

--- core-java-modules/core-java-string-conversions-4/EBCDIC_to_ASCII/ASCII_EBCDIC_Example.java
+++ core-java-modules/core-java-string-conversions-4/EBCDIC_to_ASCII/ASCII_EBCDIC_Example.java
@@ -1,12 +1,16 @@
-import java.nio.charset.Charset; 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.nio.charset.Charset;
 
-class Main {
+public class EbcdicExample {
+    private static final Logger logger = LoggerFactory.getLogger(EbcdicExample.class);
+    
     public static void main(String[] args) {
         // Example: EBCDIC bytes for "ABC" (in Cp037)
         byte[] ebcdicBytes = new byte[] { (byte)0xC1, (byte)0xC2, (byte)0xC3 };
 
         // Convert to String using EBCDIC Cp037 charset
         String text = new String(ebcdicBytes, Charset.forName("Cp037"));
-        System.out.println(text);
+        logger.info(text);
     }
 }
