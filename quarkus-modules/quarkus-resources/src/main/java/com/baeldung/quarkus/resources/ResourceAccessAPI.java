5ef385f6b94f45bc1cfbf8208f6abcfd6f0fe07c
vshanbha <vshanbhag@neurobyte.in>
2025-11-27 23:22:21 +0100

    code written for accessing resources

--- quarkus-modules/quarkus-resources/src/main/java/com/baeldung/quarkus/resources/ResourceAccessAPI.java
+++ quarkus-modules/quarkus-resources/src/main/java/com/baeldung/quarkus/resources/ResourceAccessAPI.java
@@ -1,16 +1,64 @@
 package com.baeldung.quarkus.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
-@Path("/hello")
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+@Path("/resources")
 public class ResourceAccessAPI {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ResourceAccessAPI.class);
+
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public Response getHello() throws IOException {
+        return Response.ok("Hello Quarkus!").build();
+    }
+
+    @GET
+    @Path("/default")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getDefaultResource() throws IOException {
+        return Response.ok(readResource("default-resource.txt")).build();
+    }
+
+    @GET
+    @Path("/default-nested")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getDefaultNestedResource() throws IOException {
+        return Response.ok(readResource("text/another-resource.txt")).build();
+    }
+
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getJsonResource() throws IOException {
+        return Response.ok(readResource("config.json")).build();
+    }
+
+
+    private String readResource(String resourcePath) throws IOException {
+        LOGGER.info("Reading resource from path: {}", resourcePath);
+        try (InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(resourcePath)) {
+            if (in == null) {
+                LOGGER.error("Resource not found at path: {}", resourcePath);
+                throw new IOException("Resource not found: " + resourcePath);
+            }
+            LOGGER.info("Successfully read resource: {}", resourcePath);
+            return new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)).lines()
+                .collect(Collectors.joining("\n"));
+        }
     }
 }
--- quarkus-modules/quarkus-resources/src/test/java/com/baeldung/quarkus/resources/ResourceAccessAPIUnitTest.java
+++ quarkus-modules/quarkus-resources/src/test/java/com/baeldung/quarkus/resources/ResourceAccessAPIUnitTest.java
@@ -1,6 +1,7 @@
 package com.baeldung.quarkus.resources;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,12 +10,33 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 class ResourceAccessAPIUnitTest {
     @Test
-    void testHelloEndpoint() {
+    @DisplayName("should return content from default resource")
+    void whenGetDefaultResource_thenReturnsContent() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+            .when().get("/resources/default")
+            .then()
+                .statusCode(200)
+                .body(is("This is the default resource."));
+    }
+
+    @Test
+    @DisplayName("should return content from nested default resource")
+    void whenGetDefaultNestedResource_thenReturnsContent() {
+        given()
+            .when().get("/resources/default-nested")
+            .then()
+                .statusCode(200)
+                .body(is("This is another resource from a sub-directory."));
+    }
+
+    @Test
+    @DisplayName("should return content from included json resource")
+    void whenGetJsonResource_thenReturnsContent() {
+        given()
+            .when().get("/resources/json")
+            .then()
+            .statusCode(200)
+            .body("version", is("1.0.0"));
     }
 
 }
