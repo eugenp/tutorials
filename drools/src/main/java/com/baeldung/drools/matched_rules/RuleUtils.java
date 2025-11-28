78992b0867f3e99ccc61bee0951fb9d178dd1235
hmdrz <hmdrzsharifi@gmail.com>
2025-11-28 18:53:44 +0330

    #BAEL-7912: add RuleContext approach

---
+++ drools/src/main/java/com/baeldung/drools/matched_rules/RuleUtils.java
@@ -0,0 +1,13 @@
+package com.baeldung.drools.matched_rules;
+
+import org.kie.api.runtime.rule.RuleContext;
+
+public class RuleUtils {
+
+    public static void track(RuleContext ctx, RuleTracker tracker) {
+        String ruleName = ctx.getRule().getName();
+        tracker.add(ruleName);
+
+        System.out.println("Rule fired (via RuleUtils): " + ruleName);
+    }
+}
