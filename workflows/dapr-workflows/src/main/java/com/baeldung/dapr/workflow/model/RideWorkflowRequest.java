930482e74f463d51da53ef9a62cf3ba113a787d8
Ulisses Lima <ulisseslima@users.noreply.github.com>
2025-11-30 23:55:52 -0300

    Bael-9483 - dapr workflows - code move (#19003)
    * bael 9483 experiments
    
    * baeldung formatter, adjustments
    
    * draft ready
    
    * removing unecessary files
    
    * moving code to new module
    
    ---------
    
    Co-authored-by: ulisses <ulisses@dvlcube>

---
+++ workflows/dapr-workflows/src/main/java/com/baeldung/dapr/workflow/model/RideWorkflowRequest.java
@@ -0,0 +1,57 @@
+package com.baeldung.dapr.workflow.model;
+
+import com.baeldung.dapr.pubsub.model.RideRequest;
+
+public class RideWorkflowRequest {
+    private String rideId;
+    private RideRequest rideRequest;
+    private String driverId;
+    private String workflowInstanceId;
+
+    public RideWorkflowRequest() {
+    }
+
+    public RideWorkflowRequest(String rideId, RideRequest rideRequest, String driverId, String workflowInstanceId) {
+        this.rideId = rideId;
+        this.rideRequest = rideRequest;
+        this.driverId = driverId;
+        this.workflowInstanceId = workflowInstanceId;
+    }
+
+    public String getRideId() {
+        return rideId;
+    }
+
+    public void setRideId(String rideId) {
+        this.rideId = rideId;
+    }
+
+    public RideRequest getRideRequest() {
+        return rideRequest;
+    }
+
+    public void setRideRequest(RideRequest rideRequest) {
+        this.rideRequest = rideRequest;
+    }
+
+    public String getDriverId() {
+        return driverId;
+    }
+
+    public void setDriverId(String driverId) {
+        this.driverId = driverId;
+    }
+
+    public String getWorkflowInstanceId() {
+        return workflowInstanceId;
+    }
+
+    public void setWorkflowInstanceId(String workflowInstanceId) {
+        this.workflowInstanceId = workflowInstanceId;
+    }
+
+    @Override
+    public String toString() {
+        return "RideWorkflowRequest{" + "rideId='" + rideId + '\'' + ", rideRequest=" + rideRequest + ", driverId='" + driverId + '\'' + ", workflowInstanceId='" + workflowInstanceId + '\'' + '}';
+    }
+}
