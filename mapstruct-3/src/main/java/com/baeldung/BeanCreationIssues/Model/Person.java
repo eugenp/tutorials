88683399cb7185ca51159ff2d7a40fc5dfae59f4
umara-123 <umara_111@yahoo.com>
2025-12-01 14:19:22 +0500

    BAEL-9092 Fixing Bean Creation Issues with MapStruct’s @Mapper Annotation in Spring Applications

--- mapstruct-3/src/main/java/com/baeldung/BeanCreationIssues/Model/Person.java
+++ mapstruct-3/src/main/java/com/baeldung/BeanCreationIssues/Model/Person.java
@@ -1,15 +1,22 @@
 package com.example.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Person {
     private String name;
     private int age;
+
+     public Person() {}
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
 }
 
 
--- mapstruct-3/src/main/java/com/baeldung/BeanCreationIssues/Model/PersonDto.java
+++ mapstruct-3/src/main/java/com/baeldung/BeanCreationIssues/Model/PersonDto.java
@@ -1,13 +1,20 @@
 package com.example.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class PersonDto {
     private String name;
     private int age;
+
+     public PersonDto() {}
+
+    public PersonDto(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
 }
