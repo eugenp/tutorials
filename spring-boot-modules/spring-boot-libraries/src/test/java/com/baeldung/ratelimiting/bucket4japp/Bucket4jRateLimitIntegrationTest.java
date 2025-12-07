3d86c76ddb2cf27d86d2ba0a9478fdad7be36691
panos-kakos <kakos.programmer@gmail.com>
2025-12-07 20:00:48 +0200

    [JAVA-49665]

--- spring-boot-modules/spring-boot-libraries/src/test/java/com/baeldung/ratelimiting/bucket4japp/Bucket4jRateLimitIntegrationTest.java
+++ spring-boot-modules/spring-boot-libraries/src/test/java/com/baeldung/ratelimiting/bucket4japp/Bucket4jRateLimitIntegrationTest.java
@@ -21,7 +21,7 @@ import com.baeldung.ratelimiting.bucket4japp.service.PricingPlan;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Bucket4jRateLimitApp.class)
-@TestPropertySource(properties = "spring.config.location=classpath:ratelimiting/application-bucket4j.yml")
+@TestPropertySource(properties = "bucket4j.enabled=false")
 @AutoConfigureMockMvc
 public class Bucket4jRateLimitIntegrationTest {
 
