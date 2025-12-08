535667a77d8ffba37c2ab57b0e5e7fb24ca010b7
Sandip Kumar Roy <sandipk09.roy@gmail.com>
2025-12-08 10:01:16 +0530

    Refactored API versioning examples into spring-boot-mvc-5, fixed imports and POM

--- spring-boot-modules/spring-boot-mvc-5/src/main/java/com/baeldung/versioning/controller/UserParamController.java
+++ spring-boot-modules/spring-boot-mvc-5/src/main/java/com/baeldung/versioning/controller/UserParamController.java
@@ -1,6 +1,6 @@
 package com.baeldung.versioning.controller;
 
-import com.baeldung.example.apiversioning.model.UserV2;
+import com.baeldung.versioning.model.UserV2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
