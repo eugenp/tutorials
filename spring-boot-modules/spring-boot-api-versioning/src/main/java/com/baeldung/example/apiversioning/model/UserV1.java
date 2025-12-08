0ddeb4adb0e50d1d399a0dd46c53eb4ba645d40e
Sandip Kumar Roy <sandipk09.roy@gmail.com>
2025-12-08 09:21:37 +0530

    Remove spring-boot-api-versioning module and integrate code into spring-boot-mvc-5

--- spring-boot-modules/spring-boot-api-versioning/src/main/java/com/baeldung/example/apiversioning/model/UserV1.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung.example.apiversioning.model;
-
-public class UserV1 {
-    private String name;
-    public UserV1() {}
-    public UserV1(String name) { this.name = name; }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-}
