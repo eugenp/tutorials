73b677aae69c16fe1cd8b1f8f4db962f6dd0119c
kriti20041 <kritia2004@gmail.com>
2025-12-21 14:47:29 +0530

    Add documentation to LifoStack interface

--- core-java-modules/core-java-collections-4/src/main/java/com/baeldung/collections/dequestack/LifoStack.java
+++ core-java-modules/core-java-collections-4/src/main/java/com/baeldung/collections/dequestack/LifoStack.java
@@ -2,6 +2,11 @@ package com.baeldung.collections.dequestack;
 
 import java.util.Collection;
 
+/**
+ * A simple Last-In-First-Out (LIFO) stack abstraction.
+ *
+ * @param <E> the type of elements held in this stack
+ */
 public interface LifoStack<E> extends Collection<E> {
 
     E peek();
