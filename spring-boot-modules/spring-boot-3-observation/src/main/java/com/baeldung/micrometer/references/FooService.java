1b5e4795d8fb319042ede25d8c7e5d4c4702a735
Emanuel Trandafir <emanueltrandafir1993@gmail.com>
2026-01-15 13:56:24 +0200

    BAEL-8881: small fix

--- spring-boot-modules/spring-boot-3-observation/src/main/java/com/baeldung/micrometer/references/FooService.java
+++ spring-boot-modules/spring-boot-3-observation/src/main/java/com/baeldung/micrometer/references/FooService.java
@@ -1,7 +1,5 @@
 package com.baeldung.micrometer.references;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,62 +13,42 @@ import io.micrometer.core.instrument.MeterRegistry;
 @RequestMapping("/micrometer/references")
 class FooService {
 
-	private static final Logger logger = LoggerFactory.getLogger(FooService.class);
-	private final MeterRegistry registry;
+    private final MeterRegistry registry;
 
-	Foo fooField = new Foo(10);
+    public FooService(MeterRegistry registry) {
+        this.registry = registry;
+    }
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void setupGauges() {
+        setupWeakReferenceGauge();
+        setupStrongReferenceGauge();
+    }
 
-	public FooService(MeterRegistry registry) {
-		this.registry = registry;
-	}
+    private void setupWeakReferenceGauge() {
+        Foo foo = new Foo(10);
 
-	@EventListener(ApplicationReadyEvent.class)
-	public void setupGauges() {
-		setupWeakReferenceGauge();
-		setupStrongReferenceGauge();
-		setupFieldGauge();
-	}
+        Gauge.builder("foo.weak", foo, Foo::value)
+            .description("Foo value - weak reference (will show NaN after GC)")
+            .register(registry);
+    }
 
-	// todo: rename
-	private void setupFieldGauge() {
-		Gauge.builder("foo.value.field", fooField, Foo::value)
-				.description("Foo value - weak reference (will show NaN after GC)")
-				.register(registry);
+    private void setupStrongReferenceGauge() {
+        Foo foo = new Foo(10);
 
-		logger.info("Created weak reference gauge with value: {}", fooField.value());
-	}
+        Gauge.builder("foo.strong", foo, Foo::value)
+            .description("Foo value - strong reference (will persist)")
+            .strongReference(true)
+            .register(registry);
+    }
 
-	private void setupWeakReferenceGauge() {
-		Foo foo = new Foo(10);
+    @GetMapping("/gc")
+    public void triggerGC() {
+        System.gc();
+    }
 
-		Gauge.builder("foo.value.weak", foo, Foo::value)
-				.description("Foo value - weak reference (will show NaN after GC)")
-				.register(registry);
+    record Foo(int value) {
 
-		logger.info("Created weak reference gauge with value: {}", foo.value());
-	}
-
-	private void setupStrongReferenceGauge() {
-		Foo foo = new Foo(20);
-
-		Gauge.builder("foo.value.strong", foo, Foo::value)
-				.description("Foo value - strong reference (will persist)")
-				.strongReference(true).register(registry);
-
-		logger.info("Created strong reference gauge with value: {}",
-				foo.value());
-	}
-
-	@GetMapping("/gc")
-	public String triggerGC() {
-		logger.info("Suggesting garbage collection...");
-		System.gc();
-		logger.info("Garbage collection suggested");
-		return "Garbage collection triggered. Check metrics after a few seconds.";
-	}
-
-	record Foo(int value) {
-	}
+    }
 
 }
