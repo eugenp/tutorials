36988843291ad1c9d1b57427a1ffa4b2ed650cca
samuelnjoki29 <samuelnjoki29@gmail.com>
2026-01-20 01:13:26 +0300

    BAEL-8521: Mapping to String in Mapstruct (#19086)
    * BAEL-8521: Mapping to String in Mapstruct
    
    * BAEL-9429: Clean up pom.xml for MapStruct string mapping examples
    
    * BAEL-8521: Mapping to String in Mapstruct

---
+++ core-java-modules/core-java-string-conversions-4/src/main/java/com/baeldung/mapstructstringmapping/Person.java
@@ -0,0 +1,23 @@
+package com.baeldung.mapstructstringmapping;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
