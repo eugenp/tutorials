1dec49f2fef424492099fd7c05675558f3e1c4d3
Graham Cox <graham@grahamcox.co.uk>
2026-01-20 11:01:06 +0000

    BAEL-8033: Getting Started with Compile-Time Templates With Spring

---
+++ spring-web-modules/spring-java-templates/jte/src/main/java/com/baeldung/templates/SpringJavaTemplatesApplication.java
@@ -0,0 +1,14 @@
+package com.baeldung.templates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaTemplatesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJavaTemplatesApplication.class, args);
+    }
+    
+}
+
