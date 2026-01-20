2bdb38aaca7dddf3ffc222304fe51ddc87e9f3ca
Maiklins <kruemel@quantentunnel.de>
2026-01-21 00:41:28 +0100

    Create JteTemplateTest.java

---
+++ libraries-7/src/test/java/com/baeldung/jte/JteTemplateTest.java
@@ -0,0 +1,31 @@
+package com.baeldung.jte;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JteTemplateTest {
+
+    @Test
+    public void createArticleViewTest() {
+
+        ArticleView articleView = new ArticleView();
+
+        String output = articleView.createHtml(
+                "article.jte",
+                new Article("Java Template Engine", "Baeldung", "Helpful article", 42)
+        );
+
+        assertEquals("""
+        <html>
+            <body>
+                <h1>Java Template Engine</h1>
+                <h2>Baeldung</h2>
+                <p>Helpful article</p>
+                <p>42</p>
+            </body>
+        </html>""",
+        output);
+    }
+
+}
