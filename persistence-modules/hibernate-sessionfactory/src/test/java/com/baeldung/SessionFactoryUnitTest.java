a1c0255085ee7aa9ba8d1906b81562412022795f
sgrverma23 <sgrverma23@gmail.com>
2026-01-24 19:42:35 +0530

    renaming test

--- persistence-modules/hibernate-sessionfactory/src/test/java/com/baeldung/SessionFactoryUnitTest.java
+++ persistence-modules/hibernate-sessionfactory/src/test/java/com/baeldung/SessionFactoryUnitTest.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-public class SessionFactorySpringBootTest {
+public class SessionFactoryUnitTest {
 
     @Autowired
     private SessionFactory sessionFactory;
