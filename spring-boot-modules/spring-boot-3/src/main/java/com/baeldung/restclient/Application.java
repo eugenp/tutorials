400f7be07254d6506b39eb8f565a6a1fc7562920
Hardik Singh Behl <hardik.behl7444@gmail.com>
2026-01-27 13:37:19 +0530

    rename main class

--- spring-boot-modules/spring-boot-3/src/main/java/com/baeldung/restclient/Application.java
+++ spring-boot-modules/spring-boot-3/src/main/java/com/baeldung/restclient/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestClientApplication {
+class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(RestClientApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
-
 }
