f58bc16ce68a7403e000b8660160865b20e908e1
Leonardo Colman Lopes <dev@leonardo.colman.com.br>
2026-02-11 23:17:22 -0300

    [Bael-7734] Improve Examples

--- testing-modules/testng-2/src/test/java/com/baeldung/testng/DataDrivenNameTest.java
+++ testing-modules/testng-2/src/test/java/com/baeldung/testng/DataDrivenNameTest.java
@@ -1,5 +1,7 @@
 package com.baeldung.testng;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -11,14 +13,20 @@ import static org.testng.Assert.assertEquals;
 
 public class DataDrivenNameTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(DataDrivenNameTest.class);
+
+    private String testName;
+
     @BeforeMethod
     public void capture(Method method, Object[] params) {
         String testName = method.getName() + Arrays.toString(params);
-        System.out.println("→ " + testName);
+        this.testName = testName;
+        logger.info("Executing test {}", testName);
     }
 
     @Test(dataProvider = "numbers")
     public void shouldSquare(int input, int expected) {
+        logger.info("Executing scenario from {}", testName);
         assertEquals(Math.pow(input, 2), expected);
     }
 
--- testing-modules/testng-2/src/test/java/com/baeldung/testng/FactoryNameTest.java
+++ testing-modules/testng-2/src/test/java/com/baeldung/testng/FactoryNameTest.java
@@ -1,5 +1,8 @@
 package com.baeldung.testng;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Factory;
@@ -8,6 +11,7 @@ import org.testng.annotations.Test;
 import java.lang.reflect.Method;
 
 public class FactoryNameTest {
+    private static final Logger logger = LoggerFactory.getLogger(FactoryNameTest.class);
     private final String instanceLabel;
 
     public FactoryNameTest(String label) {
@@ -23,9 +27,12 @@ public class FactoryNameTest {
     public void capture(Method method, ITestResult result) {
         String fullName = method.getName() + "[" + instanceLabel + "]";
         result.setAttribute("displayName", fullName);
-        System.out.println("→ " + fullName);
+        logger.info("capturing {}", fullName);
     }
 
     @Test
-    public void executeScenario() { /* … */ }
+    public void givenSimpleAssertionThenShouldPass() {
+        logger.info("Executing scenario {}", instanceLabel);
+        Assert.assertEquals(4, 2 + 2);
+    }
 }
