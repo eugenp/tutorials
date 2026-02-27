98862a7acaad47721df5153f75ff5897a5695b87
hmdrz <hmdrzsharifi@gmail.com>
2026-02-27 17:42:09 +0330

    #BAEL-7818: refactor UserController to use TokenExchange

--- spring-security-modules/spring-security-oidc/src/main/java/com/baeldung/tokenexchange/userservice/UserController.java
+++ spring-security-modules/spring-security-oidc/src/main/java/com/baeldung/tokenexchange/userservice/UserController.java
@@ -1,14 +1,10 @@
 package com.baeldung.tokenexchange.userservice;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
-
-import org.springframework.http.HttpHeaders;
-import org.springframework.security.core.Authentication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,76 +12,35 @@ import org.springframework.web.client.RestClient;
 
 @RestController
 public class UserController {
-
+    private static final String TARGET_RESOURCE_SERVER_URL = "http://localhost:8082/message";
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
     private final RestClient restClient;
-//    private final OAuth2AuthorizedClientManager clientManager;
 
-   /* public UserController(RestClient restClient, OAuth2AuthorizedClientManager clientManager) {
+    public UserController(OAuth2AuthorizedClientManager authorizedClientManager, RestClient restClient) {
+        this.authorizedClientManager = authorizedClientManager;
         this.restClient = restClient;
-        this.clientManager = clientManager;
-    }
-*/
-    @GetMapping("/user")
-    public String getUser(Authentication authentication){
-//        return "baeldung";
-        return restClient.get()
-            .uri("/message")
-            .header(HttpHeaders.AUTHORIZATION, "Bearer " + ((JwtAuthenticationToken) authentication).getToken().getTokenValue())
-            .retrieve()
-            .body(String.class);
     }
 
-   /* @GetMapping("/messages")
-    public String getUserMessages(Authentication authentication) {
+    @GetMapping("/user/message")
+    public String message(JwtAuthenticationToken jwtAuthentication) {
 
-        OAuth2AuthorizeRequest authorizeRequest =
-          OAuth2AuthorizeRequest.withClientRegistrationId("token-exchange-client")
-            .principal(authentication)
-            .build();
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId("my-token-exchange-client")
+                .principal(jwtAuthentication)
+                .build();
 
-        OAuth2AuthorizedClient authorizedClient = clientManager.authorize(authorizeRequest);
+        OAuth2AuthorizedClient authorizedClient = this.authorizedClientManager.authorize(authorizeRequest);
 
-        if (authorizedClient == null) {
-            throw new IllegalStateException("Token exchange failed");
+        assert authorizedClient != null;
+
+        OAuth2AccessToken accessToken = authorizedClient.getAccessToken();
+        if (accessToken == null) {
+            return "token exchange resource server";
         }
 
-        String exchangedToken = authorizedClient
-            .getAccessToken()
-            .getTokenValue();
-
-        return restClient.get()
-            .uri("/messages")
-            .header(HttpHeaders.AUTHORIZATION, "Bearer " + exchangedToken)
-            .retrieve()
-            .body(String.class);
-    }*/
-
-    public UserController(RestClient restClient) {
-        this.restClient = restClient;
+        RestClient.ResponseSpec responseSpec = restClient.get().uri(TARGET_RESOURCE_SERVER_URL)
+          .headers(headers -> headers.setBearerAuth(accessToken.getTokenValue()))
+          .retrieve();
+        ResponseEntity<String> responseEntity = responseSpec.toEntity(String.class);
+        return responseEntity.getBody();
     }
-
-    @GetMapping(value = "/user/message")
-    public List<String> getUserMessage(
-        @RegisteredOAuth2AuthorizedClient("my-token-exchange-client")
-        OAuth2AuthorizedClient authorizedClient) {
-        return getUserMessages(authorizedClient);
-    }
-
-    private List<String> getUserMessages(OAuth2AuthorizedClient authorizedClient) {
-        // @formatter:off
-        String[] messages = Objects.requireNonNull(
-            this.restClient.get()
-                .uri("/messages")
-                .headers((headers) -> headers.setBearerAuth(authorizedClient.getAccessToken().getTokenValue()))
-                .retrieve()
-                .body(String[].class)
-        );
-        // @formatter:on
-
-        List<String> userMessages = new ArrayList<>(Arrays.asList(messages));
-        userMessages.add("%s has %d unread messages".formatted(authorizedClient.getPrincipalName(), messages.length));
-
-        return userMessages;
-    }
-
 }
