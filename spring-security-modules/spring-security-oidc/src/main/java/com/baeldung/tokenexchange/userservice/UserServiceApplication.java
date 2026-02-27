eba22866c74cc0c66efedeb2aca578f56dd93959
hmdrz <hmdrzsharifi@gmail.com>
2026-02-27 17:41:26 +0330

    #BAEL-7818: move RestClient to SecurityConfig

--- spring-security-modules/spring-security-oidc/src/main/java/com/baeldung/tokenexchange/userservice/UserServiceApplication.java
+++ spring-security-modules/spring-security-oidc/src/main/java/com/baeldung/tokenexchange/userservice/UserServiceApplication.java
@@ -2,10 +2,6 @@ package com.baeldung.tokenexchange.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
-import org.springframework.security.oauth2.client.TokenExchangeOAuth2AuthorizedClientProvider;
-import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class UserServiceApplication {
@@ -14,18 +10,6 @@ public class UserServiceApplication {
         SpringApplication application = new SpringApplication(UserServiceApplication.class);
         application.setAdditionalProfiles("userservice");
         application.run(args);
-    }
-
-    @Bean
-    public RestClient messageServiceRestClient() {
-        return RestClient.builder()
-            .baseUrl("http://localhost:8082")
-            .build();
-    }
-
-    @Bean
-    public OAuth2AuthorizedClientProvider tokenExchange() {
-        return new TokenExchangeOAuth2AuthorizedClientProvider();
     }
 
 }
