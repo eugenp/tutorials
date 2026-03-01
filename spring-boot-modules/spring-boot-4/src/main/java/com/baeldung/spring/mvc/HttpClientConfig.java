42c25c0175e630c29631cd690cd35df235ff2c8d
ACHRAF TAITAI <43656331+achraftt@users.noreply.github.com>
2026-03-01 21:09:37 +0100

    BAEL-9622: Update article "Spring Boot 4 & Spring Framework 7 – What’s New" (#19168)

--- spring-boot-modules/spring-boot-4/src/main/java/com/baeldung/spring/mvc/HttpClientConfig.java
+++ spring-boot-modules/spring-boot-4/src/main/java/com/baeldung/spring/mvc/HttpClientConfig.java
@@ -1,36 +1,28 @@
 package com.baeldung.spring.mvc;
 
-import java.util.List;
-
-import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.type.AnnotationMetadata;
-import org.springframework.web.client.support.RestClientHttpServiceGroupConfigurer;
-import org.springframework.web.service.registry.AbstractClientHttpServiceRegistrar;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
-@Import(HttpClientConfig.HelloWorldClientHttpServiceRegistrar.class)
 public class HttpClientConfig {
 
-    static class HelloWorldClientHttpServiceRegistrar extends AbstractClientHttpServiceRegistrar {
-
-        @Override
-        protected void registerHttpServices(@NonNull GroupRegistry registry, @NonNull AnnotationMetadata metadata) {
-            findAndRegisterHttpServiceClients(registry, List.of("com.baeldung.spring.mvc"));
-        }
-    }
-
     @Bean
-    RestClientHttpServiceGroupConfigurer christmasJoyServiceGroupConfigurer(@Value("${application.rest.services.christmasJoy.baseUrl}") String baseUrl) {
-        return groups -> {
-            groups.filterByName("christmasJoy")
-                .forEachClient((group, clientBuilder) -> {
-                    clientBuilder.baseUrl(baseUrl);
-                });
-        };
+    public ChristmasJoyClient christmasJoyClient(
+      @Value("${application.rest.services.christmasJoy.baseUrl}") String baseUrl
+    ) {
+        RestClient restClient = RestClient.builder()
+          .baseUrl(baseUrl)
+          .build();
+
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory
+          .builderFor(RestClientAdapter.create(restClient))
+          .build();
+
+        return factory.createClient(ChristmasJoyClient.class);
     }
 
 }
