703ec0fe66a28fdbd4b4a887e1aba10ea9136893
Sagar Verma <sverma1@godaddy.com>
2026-03-15 19:39:26 +0530

    changes with spring security 7 in spring boot app

---
+++ spring-security-modules/spring-security-mfa/src/test/java/com/baeldung/Mfa7ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.baeldung;
+
+import com.baeldung.mfa.Application;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Application.class)
+class Mfa7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
