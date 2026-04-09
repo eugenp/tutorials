06b09cbf652c03123b753a4dde2c9d50da1c26df
michaelin007 <michaelolayemi28@gmail.com>
2026-04-09 19:05:18 +0100

    https://jira.baeldung.com/browse/BAEL-9654

--- spring-scheduling/src/main/resources/RetryConfig.java
+++
@@ -1,9 +0,0 @@
-package com.baeldung;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.retry.annotation.EnableRetry;
-
-@Configuration
-@EnableRetry
-public class RetryConfig {
-}
