553f3536b114cc3cb9c7b6fa770fdc10283189f2
ACHRAF TAITAI <43656331+achraftt@users.noreply.github.com>
2026-05-06 03:03:13 +0200

    BAEL-9618: Query records between two dates with Hibernate (#19215)

---
+++ persistence-modules/hibernate-queries-2/src/main/java/com/baeldung/hibernate/daterange/entity/Order.java
@@ -0,0 +1,50 @@
+package com.baeldung.hibernate.daterange.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String trackingNumber;
+
+    private LocalDateTime creationDate;
+
+    public Order() {
+    }
+
+    public Order(String trackingNumber, LocalDateTime creationDate) {
+        this.trackingNumber = trackingNumber;
+        this.creationDate = creationDate;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+}
