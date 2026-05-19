bfb35a37251edf04ad5b917c996d67ffad130832
rajatgarg <rajatgarg@adobe.com>
2026-05-19 14:22:36 +0530

    [BAEL-9586] Add HTTP/3 Support

---
+++ core-java-modules/core-java-httpclient/src/main/java/com/baeldung/http3/Http3Demo.java
@@ -0,0 +1,36 @@
+package com.baeldung.http3;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class Http3Demo {
+
+    private final HttpClient client;
+
+    public Http3Demo() {
+        this.client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_3)
+                .build();
+    }
+
+    public HttpResponse<String> fetch(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url))
+                .GET()
+                .build();
+        return client.send(request, BodyHandlers.ofString());
+    }
+
+    public static void main(String[] args) throws Exception {
+        Http3Demo demo = new Http3Demo();
+        HttpResponse<String> response = demo.fetch("https://cloudflare-quic.com/");
+
+        System.out.println("Protocol: " + response.version());
+        System.out.println("Status:   " + response.statusCode());
+        System.out.println("Body:     " + response.body().length() + " bytes");
+    }
+}
+
