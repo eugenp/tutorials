6df01de590f14cb24e851f422ce044c355402842
bkk07 <180952443+bkk07@users.noreply.github.com>
2026-06-01 10:09:25 +0530

    Fix thread safety issues in producer-consumer implementation

--- core-java-modules/core-java-concurrency-advanced-7/src/main/java/com/baeldung/producerconsumer/DataQueue.java
+++ core-java-modules/core-java-concurrency-advanced-7/src/main/java/com/baeldung/producerconsumer/DataQueue.java
@@ -4,59 +4,41 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class DataQueue {
+
     private final Queue<Message> queue = new LinkedList<>();
     private final int maxSize;
-    private final Object IS_NOT_FULL = new Object();
-    private final Object IS_NOT_EMPTY = new Object();
 
     DataQueue(int maxSize) {
         this.maxSize = maxSize;
     }
 
-    public boolean isFull() {
-        return queue.size() == maxSize;
-    }
+    public synchronized void add(Message message)
+            throws InterruptedException {
 
-    public boolean isEmpty() {
-        return queue.isEmpty();
-    }
-
-    public void waitIsNotFull() throws InterruptedException {
-        synchronized (IS_NOT_FULL) {
-            IS_NOT_FULL.wait();
+        while (queue.size() == maxSize) {
+            wait();
         }
-    }
 
-    public void waitIsNotEmpty() throws InterruptedException {
-        synchronized (IS_NOT_EMPTY) {
-            IS_NOT_EMPTY.wait();
-        }
-    }
-
-    public void add(Message message) {
         queue.add(message);
-        notifyIsNotEmpty();
+
+        notifyAll();
     }
 
-    public Message poll() {
-        Message mess = queue.poll();
-        notifyIsNotFull();
-        return mess;
+    public synchronized Message poll()
+            throws InterruptedException {
+
+        while (queue.isEmpty()) {
+            wait();
+        }
+
+        Message message = queue.poll();
+
+        notifyAll();
+
+        return message;
     }
 
-    public Integer getSize() {
+    public synchronized Integer getSize() {
         return queue.size();
-    }
-
-    private void notifyIsNotFull() {
-        synchronized (IS_NOT_FULL) {
-            IS_NOT_FULL.notify();
-        }
-    }
-
-    private void notifyIsNotEmpty() {
-        synchronized (IS_NOT_EMPTY) {
-            IS_NOT_EMPTY.notify();
-        }
     }
 }
