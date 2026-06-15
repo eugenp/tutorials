7e886795f15b8149ead653c7fb211f5c2415fde1
Kent <kent.yuan@Gmail.com>
2026-06-15 23:57:21 +0200

    [hibernate7] minor chg on the test file

--- persistence-modules/hibernate7/src/test/java/com/baeldung/likewithregex/LikeWithRegexUnitTest.java
+++ persistence-modules/hibernate7/src/test/java/com/baeldung/likewithregex/LikeWithRegexUnitTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class LikeWithRegexUnitTest {
 
