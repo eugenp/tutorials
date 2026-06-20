1b372a730bc57f0038aadb6f2187d2649b7a369b
umara-123 <umara_111@yahoo.com>
2026-06-20 17:03:57 +0500

    BAEL-8030  Bean Background Initialization in Spring Framework

---
+++ spring-core-5/src/main/java/com/baeldung/beanbackgroundinitialization/AppConfig.java
@@ -0,0 +1,14 @@
+package com.baeldung.beanbackgroundinitialization;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean(bootstrap = Bean.Bootstrap.BACKGROUND)
+    public ProductCatalogInitializer productCatalogInitializer() {
+
+        return new ProductCatalogInitializer();
+    }
+}
