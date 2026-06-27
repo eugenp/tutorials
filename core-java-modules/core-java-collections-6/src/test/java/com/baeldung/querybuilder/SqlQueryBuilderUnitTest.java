c0ceacc26adc20ef3edee4027e2e79108880f5fc
tecofers <islaah.91@gmail.com>
2026-06-27 07:23:16 +0530

    BAEL-8258: Add examples for building SQL query strings

---
+++ core-java-modules/core-java-collections-6/src/test/java/com/baeldung/querybuilder/SqlQueryBuilderUnitTest.java
@@ -0,0 +1,79 @@
+package com.baeldung.sql.querybuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class SqlQueryBuilderUnitTest {
+
+    private final SqlQueryBuilder queryBuilder =
+            new SqlQueryBuilder();
+
+    @Test
+    void givenNoFilters_whenUsingWhereOneEqualsOne_thenReturnBaseQuery() {
+
+        String query =
+                queryBuilder.buildStudentQueryUsingWhereOneEqualsOne(
+                        null,
+                        null);
+
+        assertEquals(
+                "SELECT id, name FROM Student WHERE 1=1",
+                query);
+    }
+
+    @Test
+    void givenEnrollmentDate_whenUsingWhereOneEqualsOne_thenAppendCondition() {
+
+        String query =
+                queryBuilder.buildStudentQueryUsingWhereOneEqualsOne(
+                        "2024-01-01",
+                        null);
+
+        assertEquals(
+                "SELECT id, name FROM Student WHERE 1=1 AND enrollment_date = ?",
+                query);
+    }
+
+    @Test
+    void givenAllFilters_whenUsingWhereOneEqualsOne_thenAppendAllConditions() {
+
+        String query =
+                queryBuilder.buildStudentQueryUsingWhereOneEqualsOne(
+                        "2024-01-01",
+                        "2000-01-01");
+
+        assertEquals(
+                "SELECT id, name FROM Student WHERE 1=1 AND enrollment_date = ? AND birth_date = ?",
+                query);
+    }
+
+    @Test
+    void givenSelectedFilters_whenUsingStringJoiner_thenBuildWhereClause() {
+
+        String query =
+                queryBuilder.buildQueryUsingStringJoiner(
+                        true,
+                        false,
+                        true);
+
+        assertEquals(
+                "SELECT * FROM Student WHERE name LIKE ? AND enrollment_date = ?",
+                query);
+    }
+
+    @Test
+    void givenNoFilters_whenUsingStringJoiner_thenReturnBaseQuery() {
+
+        String query =
+                queryBuilder.buildQueryUsingStringJoiner(
+                        false,
+                        false,
+                        false);
+
+        assertEquals(
+                "SELECT * FROM Student",
+                query);
+    }
+
+}
