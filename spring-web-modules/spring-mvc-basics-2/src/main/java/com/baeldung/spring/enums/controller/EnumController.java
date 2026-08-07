77bee9c5413a159f2b42eaa69fc84454d3d593a0
michaelin007 <michaelolayemi28@gmail.com>
2026-08-07 17:57:12 +0100

    https://jira.baeldung.com/browse/BAEL-9712

--- spring-web-modules/spring-mvc-basics-2/src/main/java/com/baeldung/spring/enums/controller/EnumController.java
+++ spring-web-modules/spring-mvc-basics-2/src/main/java/com/baeldung/spring/enums/controller/EnumController.java
@@ -1,11 +1,14 @@
-package com.baeldung.spring.enums;
+package com.baeldung.spring.enums.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.baeldung.spring.enums.entity.ModeEntity;
 import com.baeldung.spring.model.Modes;
 
 @RestController
@@ -21,4 +24,10 @@ public class EnumController {
     public String findByEnum(@PathVariable Modes mode) {
         return "good";
     }
+
+    @PostMapping("/create-modes")
+    public String create(@RequestBody ModeEntity entity) {
+        return "created";
+    }
+
 }
--- spring-web-modules/spring-mvc-basics-2/src/main/java/com/baeldung/spring/model/Modes.java
+++ spring-web-modules/spring-mvc-basics-2/src/main/java/com/baeldung/spring/model/Modes.java
@@ -1,5 +1,35 @@
 package com.baeldung.spring.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Modes {
-    ALPHA, BETA;
+    ALPHA("A"),
+    BETA("B");
+
+    private String text;
+
+    Modes(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    @JsonCreator
+    public static Modes fromText(String text) {
+        for (Modes modes : Modes.values()) {
+            if (modes.getText()
+                .equals(text)) {
+                return modes;
+            }
+        }
+        throw new IllegalArgumentException();
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
 }
