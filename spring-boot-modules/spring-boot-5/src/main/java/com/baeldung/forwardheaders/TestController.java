081b8e06f1569dfc95efd32755aa18e43e83139a
saikatcse03 <saikatcse03@gmail.com>
2026-08-20 08:21:21 +0530

    update controller

--- spring-boot-modules/spring-boot-5/src/main/java/com/baeldung/forwardheaders/TestController.java
+++ spring-boot-modules/spring-boot-5/src/main/java/com/baeldung/forwardheaders/TestController.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class TestController {
